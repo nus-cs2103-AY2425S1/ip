c5f104da3634e4817f0117ff3d70267211abb065
EthanLoww <lowethan11@gmail.com>
2024-08-09 14:45:01 +0800

    Update README.md and add SkibidiSigma.java

---
+++ src/main/java/SkibidiSigma.java
@@ -0,0 +1,14 @@
+public class SkibidiSigma {
+    public static void main(String[] args) {
+        String logo = " _____ _    _ _     _     _ _   _____ _                       \n"
+                + "/  ___| |  (_) |   (_)   | (_) /  ___(_)                      \n"
+                + "\\ `--.| | ___| |__  _  __| |_  \\ `--. _  __ _ _ __ ___   __ _ \n"
+                + " `--. \\ |/ / | '_ \\| |/ _` | |  `--. \\ |/ _` | '_ ` _ \\ / _` |\n"
+                + "/\\__/ /   <| | |_) | | (_| | | /\\__/ / | (_| | | | | | | (_| |\n"
+                + "\\____/|_|\\_\\_|_.__/|_|\\__,_|_| \\____/|_|\\__, |_| |_| |_|\\__,_|\n"
+                + "                                         __/ |                \n"
+                + "                                        |___/                 \n";
+        System.out.println("Hello! I'm SkibidiSigma\n" + logo + "\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
