8a429ba8b6966c34617f7ee5238a762da490391f
EthanLoww <lowethan11@gmail.com>
2024-08-09 14:55:55 +0800

    Implement command echo and exit functionality

--- src/main/java/SkibidiSigma.java
+++ src/main/java/SkibidiSigma.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SkibidiSigma {
     public static void main(String[] args) {
         String logo = " _____ _    _ _     _     _ _   _____ _                       \n"
@@ -8,7 +10,26 @@ public class SkibidiSigma {
                 + "\\____/|_|\\_\\_|_.__/|_|\\__,_|_| \\____/|_|\\__, |_| |_| |_|\\__,_|\n"
                 + "                                         __/ |                \n"
                 + "                                        |___/                 \n";
-        System.out.println("Hello! I'm SkibidiSigma\n" + logo + "\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String horizontalLine = "_____________________________________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(horizontalLine + "\nHello! I'm SkibidiSigma\n" + logo + "\nWhat can I do for you?\n" + horizontalLine);
+
+        while (true) {
+            String userInput = scanner.nextLine().trim();
+
+            if ("bye".equalsIgnoreCase(userInput)) {
+                System.out.println(
+                        horizontalLine +
+                        "\nCatch ya on the flip side, my dude! \uD83D\uDE80\uD83E\uDD2F See ya soon!\n" +
+                        horizontalLine);
+                break;
+            }
+
+            System.out.println(horizontalLine + "\n" + userInput + "\n" + horizontalLine);
+        }
+
+        scanner.close();
     }
 }
