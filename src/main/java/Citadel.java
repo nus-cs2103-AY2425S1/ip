fe1dc493e4ba35092ad84f4dd6a4d8d46c332356
Reallyeasy1 <loyongzhe@gmail.com>
2024-08-13 15:45:33 +0800

    Rename, Greet and Exit

--- src/main/java/Citadel.java
+++ src/main/java/Citadel.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Citadel {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String name = "Citadel";
         String intro = "Hello! I'm " + name + "\n";
         String question = "What can I do for you?\n";
+
+        String input = "";
+
+        String start = intro + question;
+        System.out.println(start);
+
+        while(!input.equalsIgnoreCase("bye")) {
+            input = scanner.nextLine();
+            System.out.println(input);
+            System.out.println();
+        }
+
         String goodbye = "Bye. Hope to see you again soon!\n";
-        String op = intro + question + goodbye;
-        System.out.println(op);
+        System.out.println(goodbye);
     }
 }
