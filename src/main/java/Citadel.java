f7942a31cd6b8192b595b6765cb25a58487e2798
Reallyeasy1 <loyongzhe@gmail.com>
2024-08-13 15:36:37 +0800

    Removed Print Duke

--- src/main/java/Citadel.java
+++ src/main/java/Citadel.java
@@ -1,17 +1,10 @@
 public class Citadel {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         String name = "Citadel";
         String intro = "Hello! I'm " + name + "\n";
         String question = "What can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!\n";
         String op = intro + question + goodbye;
         System.out.println(op);
-
     }
 }
