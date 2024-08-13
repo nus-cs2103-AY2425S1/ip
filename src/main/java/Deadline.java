b7aae6cca3968f6b1735df490c403be29dbb0475
Reallyeasy1 <loyongzhe@gmail.com>
2024-08-14 00:24:39 +0800

    Added Deadline, Event and ToDo classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task{
+    protected String dateTime;
+    public Deadline(String description, String dateTime) {
+        super(description);
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + this.dateTime + ")";
+    }
+}
