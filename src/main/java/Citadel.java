0295c97bae4d1636cf9e8f39b39a06fab55780f9
Reallyeasy1 <loyongzhe@gmail.com>
2024-08-14 15:45:28 +0800

    Refactor code

--- src/main/java/Citadel.java
+++ src/main/java/Citadel.java
@@ -24,44 +24,74 @@ public class Citadel {
                     System.out.println((i + 1) + ". " + items.get(i));
                 }
             } else if (input.startsWith("mark")) {
-                String[] words = input.split(" ");
-                int index = Integer.parseInt(words[1]);
-                items.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(items.get(index - 1));
+                mark(input);
             } else if (input.startsWith("unmark")) {
-                String[] words = input.split(" ");
-                int index = Integer.parseInt(words[1]);
-                items.get(index - 1).unMark();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(items.get(index - 1));
-        } else {
-                Task t = new Task("-1");
-
+                unmark(input);
+            } else {
                 if (input.startsWith("deadline")) {
-                    String[] words = input.split(" /");
-                    String task = words[0];
-                    String deadline = words[1];
-                    t = new Deadline(task, deadline);
+                    handleDeadline(input);
                 } else if (input.startsWith("event")) {
-                    String[] words = input.split(" /");
-                    String task = words[0];
-                    String to = words[1];
-                    String from = words[2];
-                    t = new Event(task, to, from);
+                    handleEvent(input);
                 } else if (input.startsWith("todo")) {
-                    String[] words = input.split(" /");
-                    String todo = words[0];
-                    t =  new ToDo(todo);
+                    handleTodo(input);
                 }
-                items.add(t);
-                System.out.println("added: " + input);
-                System.out.println();
-                System.out.println("Now you have " + items.size() + " tasks in the list");
+
             }
             }
 
         String goodbye = "Bye. Hope to see you again soon!\n";
         System.out.println(goodbye);
+    }
+
+    private static void unmark(String input) {
+        String[] words = input.split(" ");
+        int index = Integer.parseInt(words[1]);
+        items.get(index - 1).markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(items.get(index - 1));
+    }
+
+    private static void mark(String input) {
+        String[] words = input.split(" ");
+        int index = Integer.parseInt(words[1]);
+        items.get(index - 1).unMark();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(items.get(index - 1));
+    }
+
+    private static void handleDeadline(String input) {
+        Task t;
+        String[] words = input.split(" /");
+        String task = words[0];
+        String deadline = words[1];
+        t = new Deadline(task, deadline);
+        items.add(t);
+        System.out.println("added: " + input);
+        System.out.println();
+        System.out.println("Now you have " + items.size() + " tasks in the list");
+    }
+
+    private static void handleEvent(String input) {
+        Task t;
+        String[] words = input.split(" /");
+        String task = words[0];
+        String to = words[1];
+        String from = words[2];
+        t = new Event(task, to, from);
+        items.add(t);
+        System.out.println("added: " + input);
+        System.out.println();
+        System.out.println("Now you have " + items.size() + " tasks in the list");
+    }
+
+    private static void handleTodo(String input) {
+        Task t;
+        String[] words = input.split(" /");
+        String todo = words[0];
+        t =  new ToDo(todo);
+        items.add(t);
+        System.out.println("added: " + input);
+        System.out.println();
+        System.out.println("Now you have " + items.size() + " tasks in the list");
     }
 }
