64526f779094da06b10c35bb5bb83e6f0ee32d2a
yihuididi <tohyihui11@yahoo.com>
2024-08-14 19:55:27 +0800

    YihuiBot.java: create wrapStringWithHorizontalLines method
    There will likely be more functions that would require
    the print statement to be wrapped in two horizontal lines.
    Having this method will simplify things.
    
    YihuiBot.java: Rename 'printGreetings' and 'printExit' to
    'greetings' and 'exit' respectively
    
    There will likely bre many more functions like the above two.
    Removing the 'print' in the above methods make these methods
    less verbose.

--- src/main/java/YihuiBot.java
+++ src/main/java/YihuiBot.java
@@ -3,19 +3,26 @@ public class YihuiBot {
     private static final String HORIZONTAL_LINE = "-----------------------------------------------------";
 
     public static void main(String[] args) {
-        printGreetings();
-        printExit();
+        greetings();
+        exit();
     }
 
-    private static void printGreetings() {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(String.format("Hello! I am %s!", NAME));
-        System.out.println("What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
+    private static String wrapStringWithHorizontalLines(String s) {
+        return HORIZONTAL_LINE + "\n" + s + "\n" + HORIZONTAL_LINE;
     }
 
-    private static void printExit() {
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
+    private static void greetings() {
+        String s = String.format(
+            "Hello! I am %s!\nWhat can I do for you?",
+            NAME
+        );
+        String wrapped = wrapStringWithHorizontalLines(s);
+        System.out.println(wrapped);
+    }
+
+    private static void exit() {
+        String s = "Bye. Hope to see you again soon!";
+        String wrapped = wrapStringWithHorizontalLines(s);
+        System.out.println(wrapped);
     }
 }
