c34ce5d603bddf3420b68dabf83fc3efacb8bc16
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-08-15 00:59:44 +0800

    Level-1: Echo

--- src/main/java/Skibidi.java
+++ src/main/java/Skibidi.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Skibidi {
@@ -7,19 +8,38 @@ public class Skibidi {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
+    static void parseAndExecuteCommand(String line) {
+        System.out.printf("COMMAND ENTERED: %s\n", line);
+        Skibidi.printSeparator();
+    }
+
     public static void main(String[] args) {
         String filePath = "resources/skibidi-ascii.txt";
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (IOException err) {
             err.printStackTrace();
         }
 
-        Skibidi.printSeparator();
-        System.out.println("EXITING APPLICATION");
-        Skibidi.printSeparator();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while (true) {
+            System.out.print("> ");
+            try {
+                line = reader.readLine();
+                if (line == null || line.equals("bye")) {
+                    Skibidi.printSeparator();
+                    System.out.println("EXITING APPLICATION");
+                    Skibidi.printSeparator();
+                    break;
+                }
+                parseAndExecuteCommand(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
