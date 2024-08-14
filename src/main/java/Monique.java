bfaafc8f761b81b5aa810b3462ea60e71d6314ce
EMMANUEL\Emmanuel <emmanueler97@gmail.com>
2024-08-14 10:15:08 +0800

    Merge branch 'master' of https://github.com/emmannyyy/ip
    # Conflicts:
    #	src/main/java/Monique.java

--- src/main/java/Monique.java
+++ src/main/java/Monique.java
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
 //imports for user input
 import java.io.*;
 
 public class Monique {
     public static void main(String[] args) throws IOException{
+=======
+public class Duke {
+    public static void main(String[] args) {
+>>>>>>> 3d8de7a8dc3c1fd17ce7b9ea8507221017ee77e8
         String HORIZONTAL_LINE = "_____________________________________________";
         System.out.println(HORIZONTAL_LINE + "\n");
         System.out.println("Hello, I am Monique,\nWhat can I do for you today?");
         System.out.println(HORIZONTAL_LINE + "\n");
+<<<<<<< HEAD
         boolean active = true;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (active) {
@@ -20,5 +26,8 @@ public class Monique {
             }
         }
 
+=======
+        System.out.println("Ciao!");
+>>>>>>> 3d8de7a8dc3c1fd17ce7b9ea8507221017ee77e8
     }
 }
