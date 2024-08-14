88bfb10a6794dfbaa7b622559015e34c3e178b7c
yihuididi <tohyihui11@yahoo.com>
2024-08-14 20:32:40 +0800

    Improve the skeletal version of YihuiBot so that it echos commands entered by the user, and exits when the user types the command 'bye'.

--- src/main/java/YihuiBot.java
+++ src/main/java/YihuiBot.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
+import java.util.NoSuchElementException;
+
 public class YihuiBot {
     private static final String NAME = "YihuiBot";
 
     public static void main(String[] args) {
         greetings();
+
+        Scanner userInput = new Scanner(System.in);
+        try {
+            while (true) {
+                String input = userInput.nextLine();
+                if (input.equals("bye")) {
+                    break;
+                }
+                callSuitableFunction(input);
+            }
+        } catch (IllegalStateException | NoSuchElementException e) {
+            System.out.println("An error has occured.");
+        }
+
         exit();
+    }
+
+    private static void callSuitableFunction(String input) {
+        switch (input) {
+        case "bye":
+            break;
+        default:
+            echo(input);
+        }
     }
 
     private static String wrapStringWithHorizontalLines(String s) {
@@ -23,6 +50,11 @@ public class YihuiBot {
     private static void exit() {
         String s = "Bye. Hope to see you again soon!";
         String wrapped = wrapStringWithHorizontalLines(s);
+        System.out.println(wrapped);
+    }
+
+    private static void echo(String input) {
+        String wrapped = wrapStringWithHorizontalLines(input);
         System.out.println(wrapped);
     }
 }
