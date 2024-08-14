d8b0b7ac5aba1ea26d404fbdcd65153c0e58bfd9
EthanLoww <lowethan11@gmail.com>
2024-08-14 15:52:10 +0800

    Revert "Merge branch 'branch-Level-8' into add-gradle-support"
    This reverts commit ec8c38f27672d334904d8c376f84a3d4be05cd98, reversing
    changes made to 81a9c537afe8492675eb253968939364d65db091.

--- src/main/java/sigma/exception/SigmaInvalidDateRangeException.java
+++
@@ -1,9 +0,0 @@
-package sigma.exception;
-
-public class SigmaInvalidDateRangeException extends SigmaException {
-    @Override
-    public String toString() {
-        return String.format("%s End date cannot be earlier than start date.", super.toString());
-    }
-
-}
