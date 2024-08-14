d9be2431e6f3424a4c10cdbc4ada8bc644763b4c
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-08-14 23:06:22 +0800

    Changed the name of chatbot and added exit message

---
+++ src/main/java/Hoodini.java
@@ -0,0 +1,10 @@
+
+public class Hoodini {
+    public static void main(String[] args) {
+
+        System.out.println("Hello I am Hoodini!");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------------------------");
+        System.out.println("Bye! Hope to see your again!");
+    }
+}
