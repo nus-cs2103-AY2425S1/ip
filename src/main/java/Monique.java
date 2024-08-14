06c73906aae759f6cead133fc66fb6564422417c
EMMANUEL\Emmanuel <emmanueler97@gmail.com>
2024-08-14 09:33:44 +0800

    Add level 0

---
+++ src/main/java/Monique.java
@@ -0,0 +1,24 @@
+//imports for user input
+import java.io.*;
+
+public class Monique {
+    public static void main(String[] args) throws IOException{
+        String HORIZONTAL_LINE = "_____________________________________________";
+        System.out.println(HORIZONTAL_LINE + "\n");
+        System.out.println("Hello, I am Monique,\nWhat can I do for you today?");
+        System.out.println(HORIZONTAL_LINE + "\n");
+        boolean active = true;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (active) {
+            System.out.println("user: ");
+            String userInput = br.readLine();
+            if (userInput.equalsIgnoreCase("bye")){
+                active = false;
+                System.out.println("Monique: Goodbye! Have a great day!");
+            } else {
+                System.out.println(userInput);
+            }
+        }
+
+    }
+}
