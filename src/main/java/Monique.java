3d8de7a8dc3c1fd17ce7b9ea8507221017ee77e8
EMMANUEL\Emmanuel <emmanueler97@gmail.com>
2024-08-14 09:33:44 +0800

    Add level 0

---
+++ src/main/java/Monique.java
@@ -0,0 +1,9 @@
+public class Duke {
+    public static void main(String[] args) {
+        String HORIZONTAL_LINE = "_____________________________________________";
+        System.out.println(HORIZONTAL_LINE + "\n");
+        System.out.println("Hello, I am Monique,\nWhat can I do for you today?");
+        System.out.println(HORIZONTAL_LINE + "\n");
+        System.out.println("Ciao!");
+    }
+}
