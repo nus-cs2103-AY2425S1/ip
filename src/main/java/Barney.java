5bcf7c480eacf388a76c52e006e3afeabd863e65
davidgohzk <david.gohzhekai@gmail.com>
2024-08-15 16:55:45 +0800

    put the text into functions

--- src/main/java/Barney.java
+++ src/main/java/Barney.java
@@ -1,8 +1,21 @@
 public class Barney {
+    private static String LONG_LINE = "_________________________________________________";
+
     public static void main(String[] args) {
-        String welcomeText = "Hello, I am Barney <RAWR>, what can I do for you?";
-        System.out.println(welcomeText);
-        String endingText = "Goodbye, I am Barney <RAWR>, see you next time!";
-        System.out.println(endingText);
+        welcomeText();
+        endingText();
     }
+
+    public static void welcomeText() {
+        String text = "Hello, I am Barney <RAWR>, what can I do for you?";
+        System.out.println(text);
+        System.out.println(LONG_LINE);
+    }
+
+    public static void endingText() {
+        String text = "Goodbye, I am Barney <RAWR>, see you next time!";
+        System.out.println(text);
+        System.out.println(LONG_LINE);
+    }
+
 }
