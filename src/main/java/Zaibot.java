d1e94aa043e3091b5c6cc728fa0e3a766184b0af
zaidansani <mzaidanbs@gmail.com>
2024-08-16 00:20:06 +0800

    Added the ability to store text entered by user, and display them back when requested.

--- src/main/java/Zaibot.java
+++ src/main/java/Zaibot.java
@@ -1,21 +1,50 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Zaibot {
 
+    private static final ArrayList<String> tasks = new ArrayList<>();
+
+    /**
+     * Prints a message, with the horizontal line above and below it
+     * @param message   The message to be printed
+     */
     public static void printMessage(String message) {
         String line = "---------------------------------------\n";
         System.out.println(line + message + line);
     }
 
-    public static void respond (String command) {
+    /**
+     * Processes a command, based on the input.
+     * @param command The command input
+     */
+    public static void processCommand(String command) {
         String goodbyeMessage = "Bye. Hope to see you again soon!\n";
         switch (command) {
             case "bye":
                 printMessage(goodbyeMessage);
                 break;
+            case "list":
+                printMessage(tasksListToString());
+                break;
             default:
-                printMessage(command + "\n");
+                tasks.add(command);
+                printMessage("added: " + command + "\n");
         }
+    }
+
+    /**
+     * Takes in the list of tasks, and converts it into a string.
+     * @return A string of all the tasks, enumerated.
+     */
+    public static String tasksListToString() {
+        StringBuilder result = new StringBuilder();
+        int current = 0;
+        for (String task : tasks) {
+            current++;
+            result.append(current).append(". ").append(task).append("\n");
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
@@ -27,7 +56,7 @@ public class Zaibot {
         while (!currentCommand.equals("bye")) {
             Scanner input = new Scanner(System.in);
             currentCommand = input.nextLine();
-            respond(currentCommand);
+            processCommand(currentCommand);
         }
 
     }
