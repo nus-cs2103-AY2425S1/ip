54576de49c96e450075855077aaa3fee08323215
davidgohzk <david.gohzhekai@gmail.com>
2024-08-15 16:44:34 +0800

    renamed from Duke to Barney

---
+++ src/main/java/Barney.java
@@ -0,0 +1,8 @@
+public class Barney {
+    public static void main(String[] args) {
+        String welcomeText = "Hello, I am Barney <RAWR>, what can I do for you?";
+        System.out.println(welcomeText);
+        String endingText = "Goodbye, I am Barney <RAWR>, see you next time!";
+        System.out.println(endingText);
+    }
+}
