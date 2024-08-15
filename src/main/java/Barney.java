fb5da39e2e98148514d42e66e5b7f32bca36c8db
davidgohzk <david.gohzhekai@gmail.com>
2024-08-15 17:08:27 +0800

    added echo functionality

--- src/main/java/Barney.java
+++ src/main/java/Barney.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Barney {
     private static String LONG_LINE = "_________________________________________________";
+    private static Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         welcomeText();
+        chat();
         endingText();
     }
 
@@ -10,6 +14,24 @@ public class Barney {
         String text = "Hello, I am Barney <RAWR>, what can I do for you?";
         System.out.println(text);
         System.out.println(LONG_LINE);
+    }
+
+    public static void chat() {
+        String input = "";
+        Boolean isChatting = true;
+        while (isChatting) {
+            switch (input) {
+                case "bye":
+                    isChatting = false;
+                    break;
+                default:
+                    System.out.println(">>>");
+                    input = SCANNER.nextLine();
+                    System.out.println("<<<");
+                    System.out.println(input);
+                    System.out.println(LONG_LINE);
+            }
+        }
     }
 
     public static void endingText() {
