c96629b7d6610b9fb2283fc50bb9585883d2639d
zaidansani <mzaidanbs@gmail.com>
2024-08-15 23:32:16 +0800

    Added functionality to echo back command, as well as exit with bye

--- src/main/java/Zaibot.java
+++ src/main/java/Zaibot.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class Zaibot {
-    public static void main(String[] args) {
+
+    public static void printMessage(String message) {
         String line = "---------------------------------------\n";
-        String greetingMessage = "Hello! I'm zAIbot.\nWhat can I do for you?\n";
+        System.out.println(line + message + line);
+    }
+
+    public static void respond (String command) {
         String goodbyeMessage = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + greetingMessage + line + goodbyeMessage + line);
+        switch (command) {
+            case "bye":
+                printMessage(goodbyeMessage);
+                break;
+            default:
+                printMessage(command + "\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        String greetingMessage = "Hello! I'm zAIbot.\nWhat can I do for you?\n";
+        printMessage(greetingMessage);
+
+        String currentCommand = "";
+
+        while (!currentCommand.equals("bye")) {
+            Scanner input = new Scanner(System.in);
+            currentCommand = input.nextLine();
+            respond(currentCommand);
+        }
+
     }
 }
