271ac77718b7fc8fa3a2be493d399b281ecf4ee9
sooyitao <sooyitao@gmail.com>
2024-08-15 23:01:59 +0800

    Level-0

---
+++ src/main/java/Hana.java
@@ -0,0 +1,12 @@
+public class Hana {
+    public static void main(String[] args) {
+        String name = "Hana";
+        String line = "___________________________________________";
+        System.out.println(line);
+        System.out.println(" Hello! I'm " + name);
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
