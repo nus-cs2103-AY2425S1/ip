23eaf1eaf4b155fbd819a0629e13d67894ec4a55
zaidansani <mzaidanbs@gmail.com>
2024-08-15 22:53:10 +0800

    Changed greeting message to match the name of the chatbot

---
+++ src/main/java/Zaibot.java
@@ -0,0 +1,8 @@
+public class Zaibot {
+    public static void main(String[] args) {
+        String line = "---------------------------------------\n";
+        String greetingMessage = "Hello! I'm zAIbot.\nWhat can I do for you?\n";
+        String goodbyeMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + greetingMessage + line + goodbyeMessage + line);
+    }
+}
