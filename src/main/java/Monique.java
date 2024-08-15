ea3b6a49ef13f3f198c0017b5a932f8c8191e80b
EMMANUEL\Emmanuel <emmanueler97@gmail.com>
2024-08-15 23:25:40 +0800

    Added level-2

--- src/main/java/Monique.java
+++ src/main/java/Monique.java
@@ -1,7 +1,14 @@
 //imports for user input
 import java.io.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Monique {
+    //Create array to store tasks
+    private static String[] taskList = new String[100];
+    //Create counter to store the number of items in taskList;
+    private static int numItems =0;
+
     public static void main(String[] args) throws IOException{
         String HORIZONTAL_LINE = "_____________________________________________";
         System.out.println(HORIZONTAL_LINE + "\n");
@@ -9,14 +16,26 @@ public class Monique {
         System.out.println(HORIZONTAL_LINE + "\n");
         boolean active = true;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         while (active) {
             System.out.println("user: ");
             String userInput = br.readLine();
             if (userInput.equalsIgnoreCase("bye")){
                 active = false;
                 System.out.println("Monique: Goodbye! Have a great day!");
+            }
+            else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println(HORIZONTAL_LINE);
+                IntStream.range(0,numItems)
+                        .forEach(i -> {
+                            System.out.println((i+1) + "." + taskList[i]);
+                        });
+                System.out.println(HORIZONTAL_LINE);
             } else {
-                System.out.println(userInput);
+                //add to taskList
+                taskList[numItems] = userInput;
+                numItems++;
+                System.out.println("added: " + userInput);
             }
         }
 
