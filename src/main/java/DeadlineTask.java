23313b9431b56e7c33c0d7f5faa462c5858bb2eb
zaidansani <mzaidanbs@gmail.com>
2024-08-16 16:39:27 +0800

    Added support to track ToDos, Deadlines, and Events.
    Fixed formatting issues with string messages. Added classes for each task.

---
+++ src/main/java/DeadlineTask.java
@@ -0,0 +1,15 @@
+public class DeadlineTask extends Task {
+
+    private String deadline;
+
+    public DeadlineTask(String name, String deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)", super.toString(), deadline);
+    }
+}
--- src/main/java/Zaibot.java
+++ src/main/java/Zaibot.java
@@ -10,8 +10,46 @@ public class Zaibot {
      * @param message   The message to be printed
      */
     public static void printMessage(String message) {
-        String line = "---------------------------------------\n";
+        String line = "-----------------------------------------------------------\n";
         System.out.println(line + message + line);
+    }
+
+    /**
+     * This takes a command split by spaces, and processes them into separate commands
+     * The array returned is a String array of size 3, separating the string
+     * If the parameter is not applicable, it is left empty.
+     *
+     * @param options A command, split by space into an array
+     * @return A string array containing 3 strings, the name, to and by
+     */
+    public static String[] processCommand(String[] options){
+        String[] result = new String[]{"", "", ""};
+
+        int current = 0;
+
+        for (int i = 1; i < options.length; i++) {
+            String option = options[i];
+            if (option.charAt(0) == '/'){
+                current++;
+                continue;
+            }
+
+            result[current] += option + " ";
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = result[i].trim();
+        }
+
+        return result;
+    }
+
+    public static void addTask(Task task){
+        String taskAddMessage = "Got it. I've added this task\n";
+        String taskTotalMessage = "Now you have %d tasks in the list.\n";
+        tasks.add(task);
+        printMessage(taskAddMessage + task.toString() + "\n" +
+                String.format(taskTotalMessage, tasks.size()));
     }
 
     /**
@@ -22,6 +60,8 @@ public class Zaibot {
         String goodbyeMessage = "Bye. Hope to see you again soon!\n";
         String markTaskMessage = "Nice! I've marked this task as done:\n";
         String unmarkTaskMessage = "OK, I've marked this task as not done yet:\n";
+        String taskListMessage = "Here are the tasks in your list:\n";
+
 
         String[] options = command.split("\\s+");
 
@@ -32,7 +72,7 @@ public class Zaibot {
                 printMessage(goodbyeMessage);
                 break;
             case "list":
-                printMessage(tasksListToString());
+                printMessage(taskListMessage + tasksListToString());
                 break;
             case "mark":
                 current = tasks.get(Integer.parseInt(options[1]) - 1);
@@ -42,11 +82,25 @@ public class Zaibot {
             case "unmark":
                 current = tasks.get(Integer.parseInt(options[1]) - 1);
                 current.setCompletionStatus(false);
-                printMessage(unmarkTaskMessage + current.toString() + "\nr");
+                printMessage(unmarkTaskMessage + current.toString() + "\n");
+                break;
+            case "todo":
+                options = processCommand(options);
+                current = new ToDoTask(options[0]);
+                addTask(current);
+                break;
+            case "deadline":
+                options = processCommand(options);
+                current = new DeadlineTask(options[0], options[1]);
+                addTask(current);
+                break;
+            case "event":
+                options = processCommand(options);
+                current = new EventTask(options[0], options[1], options[2]);
+                addTask(current);
                 break;
             default:
-                tasks.add(new Task(command));
-                printMessage("added: " + command + "\n");
+                printMessage("Invalid event type\n");
         }
     }
 
