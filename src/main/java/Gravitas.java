2684a7e3af0f0541ce1d8d6f423bbee1ca1823de
hweinian <hweinian@gmail.com>
2024-08-16 15:24:46 +0800

    level 1

--- src/main/java/Gravitas.java
+++ src/main/java/Gravitas.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Gravitas {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String name = "Gravitas";
-        String Message =
-                "____________________________________________________________ \nHello! I'm " + name + "\nWhat can I do for you? \n____________________________________________________________ \nBye. Hope to see you again soon! \n____________________________________________________________";
-        System.out.println(Message);
+        System.out.println("____________________________________________________________ \nHello! I'm " + name + "\nWhat can I do for you? \n____________________________________________________________\n");
+        while (true) {
+            String msg = sc.nextLine();
+            if (msg.equals("bye")) {
+                System.out.println("____________________________________________________________ \nBye. Hope to see you again soon! \n____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________ \n" + msg + "\n____________________________________________________________\n");
+        }
     }
 }
