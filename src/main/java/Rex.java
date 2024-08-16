cb655b736718d6cf3bf1a7aaf2edf9685e4ec659
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-17 04:03:29 +0800

    Add implementation of chatbot echo

--- src/main/java/Rex.java
+++ src/main/java/Rex.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
+
 public class Rex {
     public static void main(String[] args) {
         // Horizontal line separation
         String separation = "____________________________________________________________";
 
-        // Skeletal version of chatbot
+        // "rawr" string added to end of each print statement for customization
+        String rawr = " rawr";
+
+        // Rex's greeting message
         System.out.println(separation);
-        System.out.println("Hello! I'm Rex!");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm Rex!" + rawr);
+        System.out.println("What can I do for you?" + rawr);
         System.out.println(separation);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        // Loop echos commands entered by user
+        do {
+            // Takes in user input
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            // Exit loop if input is "bye"
+            if (input.equals("bye")) {
+                break;
+            }
+
+            // Echo user input through print statement, but with a rawr
+            System.out.println(separation);
+            System.out.println(input + rawr);
+            System.out.println(separation);
+        }  while (true);
+
+        // Rex's exit message
+        System.out.println(separation);
+        System.out.println("Bye. Hope to see you again soon!" + rawr);
+        System.out.println(separation);
     }
 }
