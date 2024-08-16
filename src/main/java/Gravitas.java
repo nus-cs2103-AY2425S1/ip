da9491d36a1b3212b6009656f5d8cfb6735b50f2
hweinian <hweinian@gmail.com>
2024-08-16 23:23:50 +0800

    Level 5

--- src/main/java/Gravitas.java
+++ src/main/java/Gravitas.java
@@ -27,6 +27,11 @@ public class Gravitas {
         String mark = "Nice! I've marked this task as done:";
         String unMark = "OK, I've marked this task as not done yet:";
         String added = "Got it. I've added this task:";
+        String err = "OOPS!!! I'm sorry, but I don't know what that means :-(";
+        String outOfBound = "The task that you wish to mark is invalid! please try again!";
+        String emptyTodo = "OOPS!!! The description of a todo cannot be empty.";
+        String emptyDeadline = "OOPS!!! The description of a Deadline cannot be empty and must contain /from and /to.";
+        String emptyEvent = "OOPS!!! The description of a Event cannot be empty. and must contain /by ";
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
         System.out.println(greet);
@@ -35,51 +40,86 @@ public class Gravitas {
             String msg = sc.nextLine();
             String[] msgFrag = msg.split(" ", 2);
             System.out.println(header);
-            if (msg.equals("bye")) {
-                sc.close(); //close scanner
-                System.out.println(bye);
-                break;
-            } else if (msg.equals("list")) {
-                Gravitas.printList(tasks);
-            } else if (msgFrag[0].equals("mark")) {
-                int index = Integer.parseInt((msgFrag[1]));
-                Task t = tasks.get(index - 1);
-                t.markTask();
-                System.out.println(mark);
-                System.out.println("\t[" + t.getStatusIcon() + "] " + t.description);
-            } else if (msgFrag[0].equals("unmark")) {
-                int index = Integer.parseInt((msgFrag[1]));
-                Task t = tasks.get(index - 1);
-                t.unMarkTask();
-                System.out.println(unMark);
-                System.out.println("\t[" + t.getStatusIcon() + "] " + t.description);
-            } else if (msgFrag[0].equals("deadline")) {
-                String[] deadline = msg.split("/by ", 2);
-                String[] editedMsg = deadline[0].split(" ", 2);
-                String formattedMsg = editedMsg[1] + "(by: " + deadline[1] + ")";
-                Deadline d = new Deadline(formattedMsg);
-                tasks.add(d);
-                Gravitas.numOfTask += 1;
-                System.out.println(added);
-                Gravitas.printTask(d);
-            } else if (msgFrag[0].equals("event")) {
-                String[] deadline = msg.split("/from ", 2);
-                String[] editedMsg = deadline[0].split(" ", 2);
-                String[] formattedDeadline = deadline[1].split("/to", 2);
-                String formattedMsg = editedMsg[1] + "(from: " + formattedDeadline[0] + "to:" + formattedDeadline[1] + ")";
-                Event e = new Event(formattedMsg);
-                tasks.add(e);
-                Gravitas.numOfTask += 1;
-                System.out.println(added);
-                Gravitas.printTask(e);
-            } else if (msgFrag[0].equals("todo")){
-                Todo t = new Todo(msgFrag[1]);
-                tasks.add(t);
-                Gravitas.numOfTask += 1;
-                System.out.println(added);
-                Gravitas.printTask(t);
+
+            try {
+                if (msg.equals("bye")) {
+                    sc.close(); //close scanner
+                    System.out.println(bye);
+                    break;
+                } else if (msg.equals("list")) {
+                    Gravitas.printList(tasks);
+                } else if (msgFrag[0].equals("mark")) {
+                    int index = Integer.parseInt((msgFrag[1]));
+
+                    if (index >= Gravitas.numOfTask || index < 0) {
+                        throw new DukeException(outOfBound);
+                    }
+
+                    Task t = tasks.get(index - 1);
+                    t.markTask();
+                    System.out.println(mark);
+                    System.out.println("\t[" + t.getStatusIcon() + "] " + t.description);
+                } else if (msgFrag[0].equals("unmark")) {
+                    int index = Integer.parseInt((msgFrag[1]));
+
+                    if (index >= Gravitas.numOfTask || index < 0) {
+                        throw new DukeException(outOfBound);
+                    }
+
+                    Task t = tasks.get(index - 1);
+                    t.unMarkTask();
+                    System.out.println(unMark);
+                    System.out.println("\t[" + t.getStatusIcon() + "] " + t.description);
+                } else if (msgFrag[0].equals("deadline")) {
+                    if (msgFrag.length <= 1 || !msg.contains("/by ")) {
+                        throw new DukeException(emptyDeadline);
+                    }
+
+                    String[] deadline = msg.split("/by ", 2);
+                    String[] editedMsg = deadline[0].split(" ", 2);
+                    String formattedMsg = editedMsg[1] + "(by: " + deadline[1] + ")";
+
+                    Deadline d = new Deadline(formattedMsg);
+                    tasks.add(d);
+                    Gravitas.numOfTask += 1;
+
+                    System.out.println(added);
+                    Gravitas.printTask(d);
+                } else if (msgFrag[0].equals("event")) {
+                    if (msgFrag.length <= 1 || !msg.contains("/from ") || !msg.contains("/to ")) {
+                        throw new DukeException(emptyEvent);
+                    }
+
+                    String[] deadline = msg.split("/from ", 2);
+                    String[] editedMsg = deadline[0].split(" ", 2);
+                    String[] formattedDeadline = deadline[1].split("/to", 2);
+                    String formattedMsg = editedMsg[1] + "(from: " + formattedDeadline[0] + "to:" + formattedDeadline[1] + ")";
+
+                    Event e = new Event(formattedMsg);
+                    tasks.add(e);
+                    Gravitas.numOfTask += 1;
+
+                    System.out.println(added);
+                    Gravitas.printTask(e);
+                } else if (msgFrag[0].equals("todo")) {
+                    if (msgFrag.length <= 1) {
+                        throw new DukeException(emptyTodo);
+                    }
+
+                    Todo t = new Todo(msgFrag[1]);
+                    tasks.add(t);
+                    Gravitas.numOfTask += 1;
+
+                    System.out.println(added);
+                    Gravitas.printTask(t);
+                } else {
+                    throw new DukeException(err);
+                }
+            } catch (DukeException e) {
+                System.out.println(e);
+            } finally {
+                System.out.println(footer);
             }
-            System.out.println(footer);
         }
     }
 }
