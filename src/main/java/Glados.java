95720fc432d6457ee4ad51f0efb9ff16ade57c9c
positivelyjon <positivelyjon@gmail.com>
2024-08-16 22:38:04 +0800

    feat: create add and list functions

--- src/main/java/Glados.java
+++ src/main/java/Glados.java
@@ -11,6 +11,8 @@ public class Glados {
             + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ \n"
             + " ░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░  \n"
             + "\n";
+    private static String[] list = new String[100];    
+    private static int listIndex = 0;
     public static void main(String[] args) {
         greet();
 
@@ -21,7 +23,16 @@ public class Glados {
                 exit();
                 break;
             } else {
-                echo(input);
+                String query = input.split(" ")[0];
+                if (query.equals("echo")) {
+                    echo(input.substring(5, input.length()));
+                } else if (query.equals("add")) {
+                    add(input.substring(4, input.length()));
+                } else if (query.equals("list")) {
+                    list();
+                } else {
+                    error();
+                }
             }
         }
         sc.close();
@@ -43,6 +54,32 @@ public class Glados {
             + "\nGLaDOS: " + input + "\n"
             + HORIZONTAL_LINE
         );
+    }
+
+    public static void error() {
+        System.out.println(
+            HORIZONTAL_LINE
+            + "\nGLaDOS: I don't know what this command is...\n"
+            + HORIZONTAL_LINE
+        );
+    }
+
+    public static void add(String input) {
+        list[listIndex] = input;
+        listIndex++;
+        System.out.println(
+            HORIZONTAL_LINE
+            + "\nGLaDOS: I have added '" + input + "' to the list\n"
+            + HORIZONTAL_LINE);
+    }
+
+    public static void list() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("GLaDOS: Here is the list\n");
+        for (int i = 0; i < listIndex; i++) {
+            System.out.println(i + 1 + ". " + list[i]); 
+        }
+        System.out.println(HORIZONTAL_LINE);
     }
 
     public static void exit() {
