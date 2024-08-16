07eb3857ff0cd80bb459c08d575fdefccaa67513
yhanyi <yeohhanyi0916@gmail.com>
2024-08-16 22:48:39 +0800

    A-Exceptions

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,10 @@
+public class DukeException extends Exception {
+    public DukeException(String e) {
+        super(e);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
