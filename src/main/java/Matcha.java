58c5e7e695635384232f3374afecb689a707716b
yong-zhuo <yap.aloysious@gmail.com>
2024-08-16 15:04:15 +0800

    Rename Chatbot

---
+++ src/main/java/Matcha.java
@@ -0,0 +1,11 @@
+public class Matcha {
+    public static void main(String[] args) {
+        String intro = "____________________________________________________________\n" +
+                " Hi there! I am Matcha, your personal chatbot.\n" +
+                " How can I help you today?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(intro);
+    }
+}
