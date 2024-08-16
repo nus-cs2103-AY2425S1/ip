51ec066f42bdb60a75b47f460cea9336fe504cbf
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-17 03:14:00 +0800

    Rename Duke.java to Rex.java Rex.java: Add skeletal implementation of Rex chatbot

---
+++ src/main/java/Rex.java
@@ -0,0 +1,13 @@
+public class Rex {
+    public static void main(String[] args) {
+        // Horizontal line separation
+        String separation = "____________________________________________________________";
+
+        // Skeletal version of chatbot
+        System.out.println(separation);
+        System.out.println("Hello! I'm Rex!");
+        System.out.println("What can I do for you?");
+        System.out.println(separation);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
