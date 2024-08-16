f3bf17ed44da1350c67cfbb7c2374189cd328ad8
yxtan02 <yxtan02@gmail.com>
2024-08-16 17:02:19 +0800

    Add input storage and listing

--- src/main/java/Thanos.java
+++ src/main/java/Thanos.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Thanos {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         System.out.println("Hello! I'm Thanos!\nWhat can I do for you?");
 
-        Scanner sc = new Scanner(System.in);
         while (true) {
-            String command = sc.nextLine();
-            if (command.equals("bye")) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+                }
+            } else {
+                tasks.add(userInput);
+                System.out.println("added: " + userInput);
             }
-            System.out.println(command);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
