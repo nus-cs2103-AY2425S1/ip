2cf8838782d83c40b22521cfcd068ccd2925102d
yhanyi <yeohhanyi0916@gmail.com>
2024-08-16 16:43:17 +0800

    A-Classes

---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+    public void markAsDone() {
+        if (this.isDone) System.out.println("Task already marked as done:\n" + this);
+        else {
+            this.isDone = true;
+            System.out.println("Nice! I've marked this task as done:\n" + this);
+        }
+    }
+
+    public void markAsNotDone() {
+        if (!this.isDone) System.out.println("Task already marked as not done:\n" + this);
+        else {
+            this.isDone = false;
+            System.out.println("Ok, I've marked this task as not done yet:\n" + this);
+        }
+    }
+}
