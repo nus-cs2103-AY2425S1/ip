942c3addeb6b490a9f2f5346c1e99ca63e10bd50
yxtan02 <yxtan02@gmail.com>
2024-08-16 16:25:12 +0800

    Rename chatbot to Thanos and add greeting

---
+++ src/main/java/Thanos.java
@@ -0,0 +1,8 @@
+public class Thanos {
+    public static void main(String[] args) {
+        String welcomeText = "Hello! I'm Thanos!\nWhat can I do for you?";
+        String farewellText = "Bye. Hope to see you again soon!";
+        System.out.println(welcomeText);
+        System.out.println(farewellText);
+    }
+}
