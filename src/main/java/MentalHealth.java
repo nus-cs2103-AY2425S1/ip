d02340d4861445968e6a8752ef36c702d6897a76
thony-ui <anthonyhermanto1@gmail.com>
2024-08-16 16:10:30 +0800

    completed level 0 requirements

--- src/main/java/MentalHealth.java
+++ src/main/java/MentalHealth.java
@@ -1,5 +1,8 @@
 public class Duke {
+
+    private final static String separator = "------------------------------";
     public static void main(String[] args) {
+        System.out.println(separator);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
