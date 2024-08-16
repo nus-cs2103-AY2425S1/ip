184648187f4b67a66d411ccfae0dc06185a78bbf
yhanyi <yeohhanyi0916@gmail.com>
2024-08-16 18:03:49 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,15 +23,16 @@ public class Duke {
                 scan.close();
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < this.taskStore.size(); ++i) {
                     System.out.printf("%d. %s\n", i+1, taskStore.get(i));
                 }
             } else if (input.contains("unmark")) {
                 char num = input.charAt(input.length() - 1);
-                if (!Character.isDigit(num)) System.out.println("Invalid task number provided!");
+                if (!Character.isDigit(num)) System.err.println("Invalid task number provided!");
                 else {
                     int id = num - '0';
-                    if (id > this.taskStore.size()) System.out.println("Invalid task number provided!");
+                    if (id > this.taskStore.size()) System.err.println("Invalid task number provided!");
                     else {
                         Task task = taskStore.get(id - 1);
                         task.markAsNotDone();
@@ -39,20 +40,37 @@ public class Duke {
                 }
             } else if (input.contains("mark")) {
                 char num = input.charAt(input.length() - 1);
-                if (!Character.isDigit(num)) System.out.println("Invalid task number provided!");
+                if (!Character.isDigit(num)) System.err.println("Invalid task number provided!");
                 else {
                     int id = num - '0';
-                    if (id > this.taskStore.size()) System.out.println("Invalid task number provided!");
+                    if (id > this.taskStore.size()) System.err.println("Invalid task number provided!");
                     else {
                         Task task = taskStore.get(id - 1);
                         task.markAsDone();
                     }
                 }
-            } else {
-                Task task = new Task(input);
+            } else if (input.contains("todo") || input.contains("deadline") || input.contains("event")) {
+                System.out.println("Got it. I've added this task:");
+                Task task = createTask(input);
                 taskStore.add(task);
-                System.out.printf("Added %s\n", input);
+                System.out.println(task);
+                System.out.printf("Now you have %d tasks in the list.\n", this.taskStore.size());
+            } else {
+                System.err.println("Invalid command provided!");
             }
+        }
+    }
+
+    private Task createTask(String input) {
+        if (input.contains("todo")) {
+            return new Todo(input.substring(5));
+        } else if (input.contains("deadline")) {
+            int byDate = input.indexOf("/by");
+            return new Deadline(input.substring(9, byDate - 1), input.substring(byDate+4));
+        } else {
+            int fromDate = input.indexOf("/from");
+            int toDate = input.indexOf("/to");
+            return new Event(input.substring(6, fromDate - 1), input.substring(fromDate + 6, toDate - 1), input.substring(toDate + 4));
         }
     }
 }
