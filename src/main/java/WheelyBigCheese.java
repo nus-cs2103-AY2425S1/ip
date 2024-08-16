c7ee3445f162d438f7bbb551cf2f26a3990f0863
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-08-16 22:05:21 +0800

    Level 1. Echo. Added ability for chat bot to echo user input

--- src/main/java/WheelyBigCheese.java
+++ src/main/java/WheelyBigCheese.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class WheelyBigCheese {
     private static final String greeting = "Hello! I'm Wheely Big Cheese\nWhat can I do for you?";
     private static final String ending = "Bye. Hope to see you again soon!";
@@ -11,8 +14,18 @@ public class WheelyBigCheese {
         System.out.println(s);
         System.out.println("____________________________________________________________");
     }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner to get user input
+        String input = "";
+
         WheelyBigCheese.Say(greeting);
+        while (true){
+            input = sc.nextLine();
+            if (input.equals("bye")) break;
+            WheelyBigCheese.Say(input);
+        }
+
         WheelyBigCheese.Say(ending);
     }
 }
