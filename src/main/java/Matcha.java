69b6c94702c015e7b3b0315c5fb95cf1388561a9
yong-zhuo <yap.aloysious@gmail.com>
2024-08-16 16:31:42 +0800

    Add marking/unmarking tasks

--- src/main/java/Matcha.java
+++ src/main/java/Matcha.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class Matcha {
     public static void main(String[] args) {
 
         //scanner for user input
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
-        String greet = "____________________________________________________________\n" +
-                " Hi there! I am Matcha, your personal chatbot.\n" +
-                " How can I help you today?\n" +
-                "____________________________________________________________\n" ;
-        System.out.println(greet);
+        Matcha.greet();
 
         while (true) {
 
             //read user input
             String input = scanner.nextLine();
+
+            //if user exits program
             if (input.equals("bye")) {
                 String exit = "____________________________________________________________\n" +
                         " Bye. Hope to see you again!\n" +
@@ -27,21 +26,60 @@ public class Matcha {
                 break;
             }
 
-            if (input.equals("list")) {
-                System.out.println("____________________________________________________________\n");
-                for (int i = 0; i < tasks.size(); i++) {
-                    String task = (i + 1) + ". " + tasks.get(i);
-                    System.out.println(task);
-                }
-                System.out.println("____________________________________________________________\n");
-            } else {
-                tasks.add(input);
-                String addedTask = "____________________________________________________________\n" +
-                        "added: " + input + "\n" +
-                        "____________________________________________________________\n";
-                System.out.println(addedTask);
+            //get first word of user input
+            String firstWord = input.split(" ")[0];
+
+            switch (firstWord) {
+                case "list":
+                    printLine();
+                    System.out.println("Here are your tasks:\n");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        String task = (i + 1) + ". " + tasks.get(i);
+                        System.out.println(task);
+                    }
+                    printLine();
+                    break;
+                case "mark":
+                    //get the tasks from the list of tasks based on given task number
+                    Task markTask = tasks.get(Integer.parseInt(input.split(" ")[1])- 1);
+                    markTask.markDone();
+                    printLine();
+                    System.out.println("I have successfully marked this task as done: ");
+                    System.out.println(markTask.toString());
+                    printLine();
+                    break;
+                case "unmark":
+                    Task unmarkTask = tasks.get(Integer.parseInt(input.split(" ")[1])- 1);
+                    unmarkTask.markNotDone();
+                    printLine();
+                    System.out.println("Alright, I have marked this task as not done yet: ");
+                    System.out.println(unmarkTask.toString());
+                    printLine();
+                    break;
+                default:
+                    printLine();
+                    System.out.println("added: " + input);
+                    printLine();
+                    tasks.add(new Task(input));
+                    break;
             }
 
         }
+        //once user has exited program, close scanner
+        scanner.close();
+    }
+
+    public static void greet() {
+        printLine();
+        System.out.println(" Hi there! I am Matcha, your personal chatbot.\n");
+        System.out.println(" How can I help you today?\n");
+        printLine();
+    }
+
+    public static void printLine() {
+        String line = "____________________________________________________________\n";
+        System.out.println(line);
     }
 }
+
+
