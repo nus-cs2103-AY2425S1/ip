90d2067ebe4354edbab593b3885c3bee9da8d47d
yxtan02 <yxtan02@gmail.com>
2024-08-16 16:41:01 +0800

    Add echo functionality

--- src/main/java/Thanos.java
+++ src/main/java/Thanos.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Thanos {
     public static void main(String[] args) {
-        String welcomeText = "Hello! I'm Thanos!\nWhat can I do for you?";
-        String farewellText = "Bye. Hope to see you again soon!";
-        System.out.println(welcomeText);
-        System.out.println(farewellText);
+        System.out.println("Hello! I'm Thanos!\nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
