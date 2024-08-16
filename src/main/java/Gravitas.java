0b74387351fc7ad86d71dc11ca497020fcdf4c5e
hweinian <hweinian@gmail.com>
2024-08-16 15:04:27 +0800

    Level-0: rename chatbot

---
+++ src/main/java/Gravitas.java
@@ -0,0 +1,8 @@
+public class Gravitas {
+    public static void main(String[] args) {
+        String name = "Gravitas";
+        String Message =
+                "____________________________________________________________ \nHello! I'm " + name + "\nWhat can I do for you? \n____________________________________________________________ \nBye. Hope to see you again soon! \n____________________________________________________________";
+        System.out.println(Message);
+    }
+}
