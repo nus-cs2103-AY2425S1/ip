3d25c4400bcaeb615239b8ac7f32d8c4afaedff4
thony-ui <anthonyhermanto1@gmail.com>
2024-08-16 16:23:39 +0800

    Amending Level 0 requirements

--- src/main/java/MentalHealth.java
+++ src/main/java/MentalHealth.java
@@ -4,7 +4,7 @@ public class MentalHealth {
     private final static String greetingMessage = "Hello! I'm your friendly ChatBot assistant called MentalHealth :)\n" +
             "What can I do for you?";
 
-    private final static String goodbyeMessage = "Bye. Hope to see you again soon!";
+    private final static String goodbyeMessage = "Bye. Hope to see you again soon! If you ever need help don't forget to reach out :)";
     public static void main(String[] args) {
         System.out.println(separator);
         System.out.println(greetingMessage);
