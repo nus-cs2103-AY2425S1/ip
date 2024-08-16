34bc834f805b5e465f6d083a29d3ebcd8a6b735c
yong-zhuo <yap.aloysious@gmail.com>
2024-08-16 15:26:57 +0800

    Add chatbot echo

--- src/main/java/Matcha.java
+++ src/main/java/Matcha.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Matcha {
     public static void main(String[] args) {
-        String intro = "____________________________________________________________\n" +
+        String greet = "____________________________________________________________\n" +
                 " Hi there! I am Matcha, your personal chatbot.\n" +
                 " How can I help you today?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(intro);
+                "____________________________________________________________\n" ;
+        System.out.println(greet);
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                String exit = "____________________________________________________________\n" +
+                        " Bye. Hope to see you again!\n" +
+                        "____________________________________________________________\n";
+                System.out.println(exit);
+                break;
+            } else {
+                String reply = "____________________________________________________________\n" +
+                        input + "\n" +
+                        "____________________________________________________________\n";
+                System.out.println(reply);
+            }
+
+        }
     }
 }
