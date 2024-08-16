0dbbb9de57d91dbeb0c82dc528fb5dc587d50120
yhanyi <yeohhanyi0916@gmail.com>
2024-08-16 16:00:10 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+//import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+//        Scanner scan = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye! Hope to see you again soon!");
+//        while (scan.hasNext()) {
+//            String input = scan.nextLine();
+//            if (input.equals("bye")) {
+//                System.out.println("Bye! Hope to see you again soon!");
+//                scan.close();
+//                break;
+//            }
+//            System.out.println(input);
+//        }
     }
 }
