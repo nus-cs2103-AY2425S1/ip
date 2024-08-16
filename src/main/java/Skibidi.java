5ae263f14924569c66df33309fe1c22e471f3a0c
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-08-16 15:49:14 +0800

    Level-4: ToDos, Events, Deadlines

--- src/main/java/Skibidi.java
+++ src/main/java/Skibidi.java
@@ -6,51 +6,82 @@ import java.util.List;
 import java.io.IOException;
 
 public class Skibidi {
-    List<Task> store = new ArrayList<>();
+    static class InvalidCommandException extends Exception {
+        public InvalidCommandException() {
+            super("INVALID COMMAND GIVEN");
+        }
+    }
+    List<Task> tasks = new ArrayList<>();
 
     static void printSeparator() {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
     void printList() {
-        if (store.size() == 0) {
+        if (tasks.size() == 0) {
             System.out.println("\tNO ITEMS");
             return;
         }
         System.out.println("\tLISTING ITEMS:");
-        for (int i = 0; i < store.size(); i++) {
-            System.out.printf("\t  %d. %s\n", i + 1, store.get(i));
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("\t  %d. %s\n", i + 1, tasks.get(i));
         }
     }
 
     void markTask(int taskId) {
-        Task task = store.get(taskId);
+        Task task = tasks.get(taskId);
         task.mark();
         System.out.printf("\tMARKING TASK\n");
         System.out.printf("\t%s\n", task.toString());
     }
 
     void unmarkTask(int taskId) {
-        Task task = store.get(taskId);
+        Task task = tasks.get(taskId);
         task.unmark();
         System.out.printf("\tUNMARKING TASK\n");
         System.out.printf("\t%s\n", task.toString());
     }
 
     void parseAndExecuteCommand(String line) {
-        String[] args = line.split(" ");
+        String[] args = line.split(" ", 2);
+        if (args.length == 0) {
+            System.out.println("\tNO COMMANDS GIVEN");
+            Skibidi.printSeparator();
+            return;
+        }
+        String cmdArgs[];
         switch (args[0]) {
             case "list":
                 printList();
                 break;
             case "mark":
-                markTask(Integer.parseInt(args[1]) - 1);
+                try {
+                    markTask(Integer.parseInt(args[1].strip()) - 1);
+                } catch (NumberFormatException e) {
+                    System.out.println("\tINVALID NUMBER GIVEN");
+                }
                 break;
             case "unmark":
-                unmarkTask(Integer.parseInt(args[1]) - 1);
+                try {
+                    unmarkTask(Integer.parseInt(args[1].strip()) - 1);
+                } catch (NumberFormatException e) {
+                    System.out.println("\tINVALID NUMBER GIVEN");
+                }
+                break;
+            case "todo":
+                tasks.add(new Todo(args[1].strip()));
+                break;
+            case "deadline":
+                cmdArgs = args[1].split("/by");
+                tasks.add(new Deadline(cmdArgs[0].strip(), cmdArgs[1].strip()));
+                break;
+            case "event":
+                // Assume order of arguments is always /from followed by /to
+                cmdArgs = args[1].split("/from|/to");
+                tasks.add(new Event(cmdArgs[0].strip(), cmdArgs[1].strip(), cmdArgs[2].strip()));
                 break;
             default:
-                store.add(new Task(line));
+                System.out.println("\tINVALID COMMAND");
                 break;
         }
         Skibidi.printSeparator();
