310bf61fa35fe2a9759206c60d802651416b24eb
davidgohzk <david.gohzhekai@gmail.com>
2024-08-16 15:26:25 +0800

    added mark as done functionality

--- src/main/java/Barney.java
+++ src/main/java/Barney.java
@@ -1,55 +1,89 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Barney {
-    private static String LONG_LINE = "_________________________________________________";
-    private static Scanner SCANNER = new Scanner(System.in);
+    private static String LONG_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
-        welcomeText();
-        chat();
-        endingText();
-    }
-
-    public static void welcomeText() {
-        String text = "Hello, I am Barney <RAWR>, what can I do for you?";
-        System.out.println(text);
+        // Welcome text
+        String welcomeText = "Hello, I am Barney <RAWR>, what can I do for you?";
+        System.out.println(welcomeText);
         System.out.println(LONG_LINE);
-    }
 
-    public static void chat() {
+        Scanner SCANNER = new Scanner(System.in);
         String input;
+
         Boolean isChatting = true;
-        String[] list = new String[100];
+
+        Task[] taskList = new Task[100];
         int listLength = 0;
 
         while (isChatting) {
             System.out.println(">>>");
-            input = SCANNER.nextLine();
+            input = SCANNER.nextLine().trim();
             System.out.println("<<<");
 
-            switch (input) {
+            String[] inputArray = input.split(" ");
+            String keyword = inputArray[0];
+
+            switch (keyword) {
                 case "list":
-                    for (int i = 1; i <= listLength; i++) {
-                        System.out.println(i + ". " + list[i - 1]);
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < listLength; i++) {
+                        System.out.println(String.format("%d. %s", i + 1, taskList[i].toString()));
                     }
+                    break;
+                case "mark":
+                    String markStr = inputArray[1];
+                    if (!markStr.matches("\\d+")) {
+                        System.out.println("Invalid task number");
+                        break;
+                    }
+
+                    int markIndex = Integer.parseInt(markStr) - 1;
+
+                    if (markIndex >= listLength || markIndex < 0) {
+                        System.out.println("Invalid task number");
+                        break;
+                    }
+
+                    taskList[markIndex].mark();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(taskList[markIndex].toString());
+                    break;
+                case "unmark":
+                    String unmarkStr = inputArray[1];
+                    if (!unmarkStr.matches("\\d+")) {
+                        System.out.println("Invalid task number");
+                        break;
+                    }
+
+                    int unmarkIndex = Integer.parseInt(unmarkStr) - 1;
+                    if (unmarkIndex >= listLength || unmarkIndex < 0) {
+                        System.out.println("Invalid task number");
+                        break;
+                    }
+
+                    taskList[unmarkIndex].unmark();
+                    System.out.println("OK, I've unmarked this task as not done yet:");
+                    System.out.println(taskList[unmarkIndex].toString());
                     break;
                 case "bye":
                     isChatting = false;
                     break;
                 default:
-                    list[listLength] = input;
+                    taskList[listLength] = new Task(input);
                     listLength++;
                     System.out.println("added: " + input);
                     System.out.println(LONG_LINE);
+
             }
         }
-    }
 
-    public static void endingText() {
-        String text = "Goodbye, I am Barney <RAWR>, see you next time!";
-        System.out.println(text);
+        // Ending text
+        String endingText = "Goodbye, I am Barney <RAWR>, see you next time!";
+        System.out.println(endingText);
         System.out.println(LONG_LINE);
-    }
 
+        SCANNER.close();
+    }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String description;
+    private boolean isMarked;
+
+    public Task(String description) {
+        this.description = description;
+        this.isMarked = false;
+    }
+
+    public String getStatusIcon() {
+        return (isMarked ? "X" : " "); // mark done task with X
+    }
+
+    public void mark() {
+        this.isMarked = true;
+    }
+
+    public void unmark() {
+        this.isMarked = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
