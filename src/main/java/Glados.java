3400e9eeddcfa8d629da406a0ef1d78771ab539c
positivelyjon <positivelyjon@gmail.com>
2024-08-16 18:49:45 +0800

    feat: create greet and exit interface with glados

---
+++ src/main/java/Glados.java
@@ -0,0 +1,34 @@
+public class Glados {
+    private static final String HORIZONTAL_LINE = "\n" +"-----------------------------------------------------------------------------\n";
+    private static final String LOGO = "\n"
+            + " ░▒▓██████▓▒░░▒▓█▓▒░       ░▒▓██████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓███████▓▒░ \n"
+            + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        \n"
+            + "░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        \n"
+            + "░▒▓█▓▒▒▓███▓▒░▒▓█▓▒░      ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  \n"
+            + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ \n"
+            + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ \n"
+            + " ░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░  \n"
+            + "\n";
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println(
+            Glados.HORIZONTAL_LINE
+            + "\nHello, welcome to the Aperture Science computer-aided enrichment center! My name is:\n"
+            + Glados.LOGO
+            + "What task would you like me to perform today?\n"
+            + Glados.HORIZONTAL_LINE
+        );
+    }
+
+    public static void exit() {
+        System.out.println(
+            "Goodbye, user.\n"
+            + Glados.HORIZONTAL_LINE
+        );
+    }
+
+}
