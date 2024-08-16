f7209da662bfcac1f6229702d2c24be564c383fd
Incogdino <javiertmw@gmail.com>
2024-08-16 23:35:08 +0800

    Added Level 0: Rename, Greet, Exit

---
+++ src/main/java/David.java
@@ -0,0 +1,16 @@
+public class David {
+    public static void main(String[] args) {
+        String introduction = "____________________________________________________________\n" +
+                " Hello! I'm David\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________";
+
+
+
+        String outro = "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(introduction);
+        System.out.println(outro);
+    }
+}
