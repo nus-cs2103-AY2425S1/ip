4070575e21f806da50df1498c41f43376bb207d0
ttzuche <tzuche2406@gmail.com>
2024-08-17 22:28:43 +0800

    Add methods to follow OOP and logic for echo

--- src/main/java/Bean.java
+++ src/main/java/Bean.java
@@ -1,11 +1,54 @@
+import java.util.Scanner;
+
 public class Bean {
-    public static final String NAME = "Bean";
-    public static void main(String[] args) {
+    private static final String NAME = "Bean";
+    private Scanner scanner;
+
+    public Bean() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void startConversation() {
+        greetAndAsk();
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (isBye(input)) {
+                break;
+            } else {
+                echo(input);
+            }
+        }
+        exit();
+
+    }
+
+    private void greetAndAsk() {
         System.out.println("______________________________");
         System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
         System.out.println("______________________________");
+    }
+
+    private void exit() {
+        System.out.println("______________________________");
         System.out.println("Bye. Hope to see you again.");
         System.out.println("______________________________");
+        scanner.close();
+    }
+
+    private boolean isBye(String s) {
+        return s.equalsIgnoreCase("bye");  // Ignoring case sensitivity for better user experience
+    }
+
+    private void echo(String s) {
+        System.out.println("______________________________");
+        System.out.println(s);
+        System.out.println("______________________________");
+    }
+
+    public static void main(String[] args) {
+        Bean bean = new Bean();
+        bean.startConversation();
     }
 }
