74f43b7d0d66fce461b1925875a038abc54de9a9
RezwanAhmed123 <maxtalon94@gmail.com>
2024-08-17 14:30:58 +0800

    Updated Level-0. Changed all non-main methods from static to non-static types.

--- src/main/java/MortalReminder.java
+++ src/main/java/MortalReminder.java
@@ -1,7 +1,13 @@
 public class MortalReminder {
-    private static final String separatorLine = "_________________________________";
+    private final String separatorLine = "_________________________________";
 
-    private static void Welcome() {
+    public static void main(String[] args) {
+        MortalReminder mortalReminder = new MortalReminder();
+        mortalReminder.Welcome();
+        mortalReminder.GoodBye();
+    }
+
+    private void Welcome() {
         String welcomeMessage = separatorLine
                 + "\n"
                 + "Hello I'm Mortal Reminder! \n"
@@ -10,14 +16,9 @@ public class MortalReminder {
         System.out.println(welcomeMessage);
     }
 
-    private static void GoodBye() {
+    private void GoodBye() {
         String goodByeMessage = "Bye. Hope to see you again soon! \n"
                 + separatorLine;
         System.out.println(goodByeMessage);
-    }
-
-    public static void main(String[] args) {
-        Welcome();
-        GoodBye();
     }
 }
