3d022ce4724fd44dade13ddd533002d90c28535f
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-08-18 00:38:05 +0800

    Implement skeletal version

---
+++ src/main/java/Qwerty.java
@@ -0,0 +1,12 @@
+public class Qwerty {
+    public static void main(String[] args) {
+        System.out.println("""
+
+                Hello! I'm Qwerty.
+                What can I do for you?
+                
+                Bye. Hope to see you again soon!
+
+                """);
+    }
+}
