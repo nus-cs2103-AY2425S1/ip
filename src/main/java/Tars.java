79d2dcd71154f9e3c379d640fbbea238f584568e
appleshill <eliot@u.nus.edu>
2024-08-17 17:08:35 +0800

    Level-2

--- src/main/java/Tars.java
+++ src/main/java/Tars.java
@@ -1,15 +1,36 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Tars {
 
-    public static String echo(String input) {
+    private List<String> tasks;
+
+    public Tars() {
+        this.tasks = new ArrayList<>();
+    }
+    public String echo(String input) {
         return switch (input) {
             case "bye" -> ("Bye. Hope to see you again soon!");
-            case "humour", "humour setting" -> ("100%");
-            case "caution" -> ("Cooper, this is no time for caution!");
-            default -> input;
+            case "list" -> listAllTasks();
+            default -> {
+                addTask(input);
+                yield ("added: " + input);
+            }
         };
+    }
+
+    private String listAllTasks() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < this.tasks.size(); i++) {
+            result.append(i + 1).append(". ").append(tasks.get(i)).append("\n");
+        }
+        return result.toString().trim();
+    }
+
+    private void addTask(String input) {
+        this.tasks.add(input);
     }
     public static void main(String[] args) {
         System.out.println("____________________________________");
@@ -17,12 +38,13 @@ public class Tars {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________");
 
+        Tars tars = new Tars();
         Scanner scanner = new Scanner(System.in);
         String input;
         String output;
         do {
             input = scanner.nextLine();
-            output = echo(input);
+            output = tars.echo(input);
             System.out.println("____________________________________");
             System.out.println(output);
             System.out.println("____________________________________");
