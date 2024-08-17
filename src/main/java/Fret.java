352ae6e620942a12884d5c9e6cfa4b3ddc1c1191
Dinoman44 <66869201+Dinoman44@users.noreply.github.com>
2024-08-17 18:27:23 +0800

    Add files via upload

---
+++ src/main/java/Fret.java
@@ -0,0 +1,16 @@
+public class Fret {
+    public static void main(String[] args) {
+        String logo = "________                ___ \n"
+                + "| _____|             ___| |___ \n"
+                + "| |___  __   _   ___ |__   __|\n"
+                + "| ___|  | |/ /  / _ \\   | |\n"
+                + "| |     |   /  <  __/   | |__\n"
+                + "|_|     |__|    \\___|   |___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("---------------------------");
+        System.out.println("Hello! I'm Fret\nWhat can I do for you?");
+        System.out.println("---------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------");
+    }
+}
