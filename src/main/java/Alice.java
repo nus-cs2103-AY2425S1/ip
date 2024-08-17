336d5e46df05e9d15b3bf8c5a2afd60e19e56b13
beginner-web-developer <e1156242@u.nus.edu>
2024-08-17 10:21:32 +0800

    Update Alice.java to be able to save tasks and list them using the "list" command

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Alice {
+    private static final ArrayList<String> tasks = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Hello! I am Alice! \nWhat can I do for you?");
         System.out.println("------------------------------------------");
@@ -10,13 +12,28 @@ public class Alice {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             response = scanner.nextLine();
+
+            // if user says bye, exit the loop
             if (response.equals("bye")) {
                 break;
             }
+            // if user wants list, print out all the tasks added
+            else if (response.equals("list")) {
+                System.out.println("------------------------------------------");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s%n", i + 1, tasks.get(i));
+                }
+                System.out.println("------------------------------------------");
+                continue;
+            }
+            
+            // all other strings are tasks that should be added
+            tasks.add(response);
             System.out.println("------------------------------------------");
-            System.out.println(response);
+            System.out.println("added: " + response);
             System.out.println("------------------------------------------");
         }
+
         System.out.println("------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
     }
