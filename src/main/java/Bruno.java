c7104c35afc3287df181fd8e02aedcdb9b595d3b
Eru111 <>
2024-08-18 00:58:27 +0800

    Update Bruno to echo and exit on command

--- src/main/java/Bruno.java
+++ src/main/java/Bruno.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
 public class Bruno {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userResponse;
+
+        printGreetingMessage();
+
+        boolean running = true;
+        while (running) {
+            userResponse = input.nextLine();
+            if (userResponse.equals("bye")) {
+                running = false;
+                printByeMessage();
+            } else {
+                echo(userResponse);
+            }
+        }
+    }
+
+    public static void printGreetingMessage() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Bruno\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void printByeMessage() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void echo(String msg) {
+        System.out.println("____________________________________________________________");
+        System.out.println(msg);
         System.out.println("____________________________________________________________");
     }
 }
