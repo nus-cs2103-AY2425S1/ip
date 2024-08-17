b826cab87bdd5b8b16a110c517dfe2411b18ed27
ttzuche <tzuche2406@gmail.com>
2024-08-17 21:30:51 +0800

    Rename file, add greet and exit lines

---
+++ src/main/java/Bean.java
@@ -0,0 +1,11 @@
+public class Bean {
+    public static final String NAME = "Bean";
+    public static void main(String[] args) {
+        System.out.println("______________________________");
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________");
+        System.out.println("Bye. Hope to see you again.");
+        System.out.println("______________________________");
+    }
+}
