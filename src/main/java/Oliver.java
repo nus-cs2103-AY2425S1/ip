a3cdff68af6fbb857a4ff8e2b5d2b7d7b7af13d2
SQ77 <andrewzsq@gmail.com>
2024-08-17 18:00:32 +0800

    Add, List

--- src/main/java/Oliver.java
+++ src/main/java/Oliver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Oliver {
@@ -5,13 +6,20 @@ public class Oliver {
         System.out.println("Hello! I'm Oliver.");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("\tBye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("\t" + (i+1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("\tadded: " + input);
             }
-            System.out.println("\t" + input);
         }
         sc.close();
     }
