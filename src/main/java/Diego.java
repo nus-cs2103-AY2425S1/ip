cac2e198e72238e6025037ed7918b896f5aae0d2
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-08-17 15:31:18 +0800

    Complete Week 2 Level-0 tasks
    Implemented basic chatbot functionality by adding greeting and exit messages.
    Renamed the chatbot class to reflect the new name and removed all instances
    of 'Duke' from the codebase.

---
+++ src/main/java/Diego.java
@@ -0,0 +1,11 @@
+public class Diego {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Diego");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
