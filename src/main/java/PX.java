50d22716eaa544e3820f0229473b7fb6c320f307
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-08-17 17:11:36 +0800

    Rename Duke to PX, add Greet and Exit

---
+++ src/main/java/PX.java
@@ -0,0 +1,14 @@
+public class PX {
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Hello! I'm PX");
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+}
