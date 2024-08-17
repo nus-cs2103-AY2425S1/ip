7eb7d6bd356f1597ab66fba28ef95e2ea14e0a3e
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-08-17 15:45:23 +0800

    Finish Week 2 Level-1
    Added in the echo feature

--- src/main/java/Diego.java
+++ src/main/java/Diego.java
@@ -1,11 +1,38 @@
-public class Diego {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Diego");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+import java.util.Scanner;
 
+public class Diego {
+
+    public static void main(String[] args) {
+        String hiMessage = """
+            ____________________________________________________________
+            Hello! I'm Diego
+            What can I do for you?
+            ____________________________________________________________
+            """;
+
+        String byeMessage = """
+            ____________________________________________________________
+            Bye. Hope to see you again soon!
+            ____________________________________________________________
+            """;
+
+        System.out.println(hiMessage);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        String output;
+        while (true){
+            input = scanner.nextLine();
+
+            if (input.equals("bye")){
+                break;
+            }
+
+            output = "____________________________________________________________\n" +
+                    " " + input + "\n" +
+                    "____________________________________________________________";
+            System.out.println(output);
+        }
+        System.out.println(byeMessage);
     }
 }
