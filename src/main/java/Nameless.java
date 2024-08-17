4ea35c26a410d068a68cc3150bc7dfd1cb24ba43
benjiBase <blim0e89@gmail.com>
2024-08-17 23:56:21 +0800

    Add level-1 function

--- src/main/java/Nameless.java
+++ src/main/java/Nameless.java
@@ -1,10 +1,27 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class Nameless {
+
     public static void main(String[] args) {
         String line = "_____________________________________________________________";
         String name = "Nameless";
         String greetings = "Hello, I'm " + name + "\n" + "What can I do for you?";
         String goodbye = "Bye. Hope to see you again!";
-        System.out.println(line + "\n" + greetings + "\n" + line + "\n" + goodbye + "\n" + line);
+
+        System.out.println(line + "\n" + greetings + "\n" + line);
+
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if(input.equals("bye")){
+                break;
+            }
+
+            System.out.println(line + "\n" + input  + "\n" + line);
+        }
+
+        System.out.println(line + "\n" + goodbye + "\n" + line);
     }
 }
 
