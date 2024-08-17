8187a8290559eb2ff7be871a07ab9e89fc25359b
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-17 15:38:08 +0800

    Refactor main method in Rex.java

--- src/main/java/Rex.java
+++ src/main/java/Rex.java
@@ -12,45 +12,43 @@ public class Rex {
         System.out.println(separation);
         System.out.println("Hello! I'm Rex!" + rawr);
         System.out.println("What can I do for you?" + rawr);
-        System.out.println(separation);
 
         // Fixed size array to store items
         ArrayList<String> list = new ArrayList<>();
 
-        // Loop echos commands entered by user
-        do {
+        while (true) {
+            System.out.println(separation);
             // Takes in user input to add to list
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-            // Echo user input, unless specific input is given
-            if (input.equals("bye")) {
-                // exit loop if input is "bye"
-                break;
-            } else if (input.equals("list")) {
-                // Display numbered list of items in sequence if input is "list"
+            // Process user command
+            switch (input) {
+            // Rex's goodbye message, exit program
+            case "bye":
+                System.out.println(separation);
+                System.out.println("Bye. Hope to see you again soon!" + rawr);
+                System.out.println(separation);
+                return;
+           // Display items added as a numbered list
+            case "list":
                 System.out.println(separation);
                 for (int i = 1; i <= list.size(); i++) {
                     System.out.println(i + ". " + list.get(i - 1));
                 }
-                System.out.println(separation);
-            } else {
-                // Echo user input and add to list
-                echo(input);
-                list.add(input);
+                break;
+            // Echo user input otherwise
+            default:
+                 echo(input);
+                 list.add(input);
+                 break;
             }
-        }  while (true);
-
-        // Rex's exit message
-        System.out.println(separation);
-        System.out.println("Bye. Hope to see you again soon!" + rawr);
-        System.out.println(separation);
+        }
     }
 
     private static void echo(String input) {
         // Display input text back to user through print statement
         System.out.println(separation);
         System.out.println("added: " + input);
-        System.out.println(separation);
     }
 }
