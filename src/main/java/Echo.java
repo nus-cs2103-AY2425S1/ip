371be23190ab3c898ac0d57fbd7cbd7cdb9a2764
kellywsq03 <kellywsq03@gmail.com>
2024-08-17 17:35:13 +0800

    Modified chatbot's name to Echo. Modified welcome message.

---
+++ src/main/java/Echo.java
@@ -0,0 +1,12 @@
+public class Echo {
+    public static void main(String[] args) {
+        String welcomeMsg =
+                "_________________________________________\n" +
+                "Hello! I'm Echo!\n" +
+                "What can I do for you?\n" +
+                "_________________________________________\n"+
+                "Bye. Hope to see you again soon!\n" +
+                "_________________________________________\n";
+        System.out.println(welcomeMsg);
+    }
+}
