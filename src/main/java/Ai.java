dd2a521e6bd030a17bdc26d0aebc1199e468c0bd
Uselesstrash06 <uselesstrash06@gmail.com>
2024-08-17 11:23:44 +0800

    Echo

--- src/main/java/Ai.java
+++ src/main/java/Ai.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Ai {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+
         String greetings = "Hi, I'm your favourite idol, Ai!!! \n"
                         + "What shall we do today? Teehee o(◠u◠)o \n";
 
@@ -7,7 +11,19 @@ public class Ai {
                         + "Aww okie :,( See ya!! \n";
 
         String line = "____________________________________________________________ \n";
-        System.out.println(line + greetings);
-        System.out.println(line + closing + line);
+
+
+
+        System.out.println(line + greetings + line);
+
+        String reply = scanner.nextLine();  // Read user input
+        System.out.println(line);
+
+        while (!reply.equals("bye")) {
+            System.out.println(reply + "\n" + line);
+            reply = scanner.nextLine();  // Read user input
+            System.out.println(line);
+        }
+        System.out.println(closing + line);
     }
 }
