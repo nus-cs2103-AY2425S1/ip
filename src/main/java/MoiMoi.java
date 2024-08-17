95ffbc75932c5f0f448b28badc615379c8bfe52f
wkxcass <e1129889@u.nus.edu>
2024-08-17 15:20:14 +0800

    MoiMoi.java: implement "add" and "list" functionalities
    Let's,
    * add a functionality for the chatbot to store whatever text entered by
    the user
    * add a functionality to display (in list-form) all inputs that have
    been stored, when requested by the user

--- src/main/java/MoiMoi.java
+++ src/main/java/MoiMoi.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MoiMoi {
@@ -27,21 +28,47 @@ public class MoiMoi {
             + "Hello, master! How may I help you today? ><\n\n" + MoiMoi.userHeader;
     private static final String exitMessage = MoiMoi.moiMoiHeader
             + "I'll always be here for you~ See ya, master! ^^\n";
+    private Scanner sc = new Scanner(System.in);
+    private ArrayList<String> tasks = new ArrayList<String>();
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
+    public void run() {
 
         System.out.println(MoiMoi.greeting);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println("\n" + MoiMoi.moiMoiHeader + input + "\n\n" + MoiMoi.userHeader);
+            System.out.print("\n" + MoiMoi.moiMoiHeader);
+            if (input.equals("list")) {
+                this.list();
+            }
+            else {
+                this.addTask(input);
+            }
+            System.out.println("\n" + MoiMoi.userHeader);
             input = sc.nextLine();
         }
 
-        System.out.println("\n" + MoiMoi.exitMessage);
+        System.out.print("\n" + MoiMoi.exitMessage);
 
+    }
+
+    public void addTask(String task) {
+        this.tasks.add(task);
+        System.out.println("Aight! Task added: " + task);
+    }
+
+    public void list() {
+        System.out.println("Here's your list of tasks!");
+        int index;
+        for (String task : this.tasks) {
+            index = this.tasks.indexOf(task) + 1;
+            System.out.println(index + ". " + task);
+        }
+    }
+
+    public static void main(String[] args) {
+        MoiMoi moiMoi = new MoiMoi();
+        moiMoi.run();
     }
 
 }
