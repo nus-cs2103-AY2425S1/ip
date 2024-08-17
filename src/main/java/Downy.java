e470191276333c353dacfe455f5f4dac4f005cc7
Dominic-Khoo <dominickhooyx@gmail.com>
2024-08-18 01:00:33 +0800

    Add list to store user inputs

--- src/main/java/Downy.java
+++ src/main/java/Downy.java
@@ -4,13 +4,28 @@ public class Downy {
     public static void main(String[] args) {
         String divider = "________________________________________\n";
         Scanner scanner = new Scanner(System.in);
+        InputList inputs = new InputList();
+        int inputCount = 0;
         System.out.println(divider + "Hello! I'm Downy.\nHow can I help?\n" + divider);
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                if (inputCount == 0) {
+                    System.out.println(divider + "There is nothing in the list!\n" + divider);
+                    continue;
+                }
+                System.out.printf(divider);
+                for (int i = 0; i < inputCount; i++) {
+                    System.out.println((i + 1) + ". " + inputs.getEntry(i));
+                }
+                System.out.println(divider);
+                continue;
             }
-            System.out.println(divider + userInput + "\n" + divider);
+            inputs.setEntry(userInput, inputCount);
+            inputCount++;
+            System.out.println(divider + "added: " + userInput + "\n" + divider);
         }
         scanner.close();
         System.out.println(divider + "Bye! Yippee!");
