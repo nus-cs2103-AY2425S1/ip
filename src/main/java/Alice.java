bed73a61db738670822e718b06f93f16f40fdd40
beginner-web-developer <e1156242@u.nus.edu>
2024-08-17 09:49:23 +0800

    Update Alice.java to greet the user and exit

---
+++ src/main/java/Alice.java
@@ -0,0 +1,7 @@
+public class Alice {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Alice! \nWhat can I do for you?");
+        System.out.println("------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
