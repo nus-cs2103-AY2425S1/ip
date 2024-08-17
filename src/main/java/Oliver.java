929a5e438851ed9d10ce95fdc89a1c04b9d85324
SQ77 <andrewzsq@gmail.com>
2024-08-17 17:39:16 +0800

    Echo commands by user

--- src/main/java/Oliver.java
+++ src/main/java/Oliver.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Oliver {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Oliver.");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("\t" + input);
+        }
+        sc.close();
     }
 }
