a42eaafc6b64ca07d302411a18580dd7060ff754
liauzhanyi <liauzhanyi@gmail.com>
2024-08-17 17:03:51 +0800

    Refactor ZBot class to improve code structure and readability

--- src/main/java/ZBot.java
+++ src/main/java/ZBot.java
@@ -1,7 +1,15 @@
 public class ZBot {
     public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    public static void greet() {
         System.out.println("Hello! I'm ZBot!");
         System.out.println("What can I do for you?\n");
+    }
+
+    public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
