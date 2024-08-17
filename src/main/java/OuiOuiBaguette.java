2f28cd468c72a6980f844fc4fcf8ea4a39c42a60
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-08-17 20:50:10 +0800

    Adding initial skeletal version of OuiOuiBaguette
    * Rename Duke.java to OuiOuiBaguette.java
    * Change class name to OuiOuiBaguette

---
+++ src/main/java/OuiOuiBaguette.java
@@ -0,0 +1,22 @@
+public class OuiOuiBaguette {
+    public static void main(String[] args) {
+        String greetings = """
+                ____________________________________________________________
+                Bone-jaw! I'm Oui Oui Baguette.
+                What can I do for you?
+                Oui Oui!
+                ____________________________________________________________
+                """;
+
+        String farewell = """
+                ____________________________________________________________
+                Bye! Hope to see you soon!
+                Oui Oui!
+                ____________________________________________________________
+                """;
+    
+
+        System.out.println(greetings);
+        System.out.println(farewell);
+    }
+}
