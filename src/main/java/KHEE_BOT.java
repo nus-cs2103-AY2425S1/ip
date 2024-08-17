a1629348ac0e9fd70924a498f52d40115727ba69
Yong Khee Hou <yongkheehou@gmail.com>
2024-08-17 19:33:00 +0800

    complete level 0

---
+++ src/main/java/KHEE_BOT.java
@@ -0,0 +1,13 @@
+public class KHEE_BOT {
+    public static final String BOT_NAME = "KHEE_BOT";
+    public static final String GREETING =
+            "____________________________________________________________\n" +
+            " Hello! I'm " + KHEE_BOT.BOT_NAME + ", a talking keyboard!" + "\n" +
+            " What can I do for you?\n" +
+            "____________________________________________________________\n" +
+            " Bye. Hope to see you again soon!\n" +
+            "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(KHEE_BOT.GREETING);
+    }
+}
