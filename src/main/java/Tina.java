c8c8558e1913bfcad23ef480bd117a746f897b00
Justincjr <jchoojr@hotmail.com>
2024-08-17 18:16:37 +0800

    rename, add greeting and exit

---
+++ src/main/java/Tina.java
@@ -0,0 +1,23 @@
+public class Tina {
+    public static void greet() {
+        System.out.println("Hello! I'm Tina");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+
+
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+}
