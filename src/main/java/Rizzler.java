97c7474ffdd02e98190457f4efb2af224f33e3aa
Green-Tea-123 <junboonsoo@gmail.com>
2024-08-18 02:55:45 +0800

    Added ability for rizzler to echo inputs

--- src/main/java/Rizzler.java
+++ src/main/java/Rizzler.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class Rizzler {
     public static void main(String[] args) {
         String separator = "_______________________________________________________\n";
-
-        String output = separator
+        String greeting = separator
                 + "Hello! I'm the Rizzler.\n"
                 + "What can I do for you today?\n"
-                + separator
+                + separator;
+        String goodbye = separator
                 + "Bye! Rizz you later!\n"
                 + separator;
-        System.out.println(output);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(greeting);
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(goodbye);
+                break;
+            } else {
+                System.out.println(separator + input + "\n" + separator);
+            }
+        }
     }
 }
