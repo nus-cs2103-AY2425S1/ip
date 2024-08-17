33fb2e8884f252df24f8b452334f261fa6ebe487
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-08-18 00:22:45 +0800

    Added horizontal lines

--- src/main/java/Velma.java
+++ src/main/java/Velma.java
@@ -1,10 +1,23 @@
 public class Velma {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    public static void printLine() {
+        for (int i = 0; i < 50; i ++) {
+            System.out.print("_");
+        }
+        System.out.print("\n");
     }
+
+    public static void main(String[] args) {
+
+        printLine();
+        System.out.println("Hello! I am Velma!" );
+        System.out.println("What can I do for you?");
+
+        printLine();
+        System.out.println("Bye. Hope to see you again soon.");
+
+        printLine();
+    }
+
+
 }
