881b51b50aac625b25a2d96d48c918bf0dad9962
Green-Tea-123 <junboonsoo@gmail.com>
2024-08-18 02:39:35 +0800

    Renamed duke to rizzler

---
+++ src/main/java/Rizzler.java
@@ -0,0 +1,13 @@
+public class Rizzler {
+    public static void main(String[] args) {
+        String separator = "_______________________________________________________\n";
+
+        String output = separator
+                + "Hello! I'm the Rizzler.\n"
+                + "What can I do for you today?\n"
+                + separator
+                + "Bye! Rizz you later!\n"
+                + separator;
+        System.out.println(output);
+    }
+}
