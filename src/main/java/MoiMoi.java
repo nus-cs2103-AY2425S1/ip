21afdc17105bc797c9cfdefb022b4b42d627f9ed
wkxcass <e1129889@u.nus.edu>
2024-08-17 13:51:32 +0800

    MoiMoi.java: implement skeletal version
    Let's rename Duke.java into MoiMoi.java, and implement a skeletal
    version of the chatbot that simply greets the user and exits.

---
+++ src/main/java/MoiMoi.java
@@ -0,0 +1,31 @@
+public class MoiMoi {
+
+    private static final String logo = "        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⣀⣀⡀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠋⠀⠀⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⢀⣠⠴⠞⠛⠉⠉⠉⠉⠉⠉⠛⠒⠾⢤⣀⣀⣠⣤⠀⠀⠀\n"
+            + "        ⠀⠀⠀⣠⡶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢭⡀⠀⠈⣷⠀⠀⠀\n"
+            + "        ⠀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⢀⡟⠀⠀⠀\n"
+            + "        ⠀⣾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡅⠀⠀⠀\n"
+            + "        ⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣄⣀⠀\n"
+            + "        ⣾⠀⠀⣠⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣄⠀⠀⠀⠀⠀⠀⠸⡇⠉⣷\n"
+            + "        ⣿⠀⠰⣿⣿⣿⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣧⡴⠋\n"
+            + "        ⣿⠀⠀ // ⠀    v ⠀⠀⠀  //⠀⠀⠀⠀⠀⢰⡏⠀⠀\n"
+            + "        ⢸⡄⠀   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⠀⡼⣄⠀⠀\n"
+            + "        ⠀⢳⡄⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⢀⡼⠁⢸⡇⠀\n"
+            + "        ⠀⠀⠙⢦   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⠀⣀⡴⠟⠒⠚⠋⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠈⠛⠾⢤⣤⣀⣀⡀⠀⠀⠀⠀⣀⣈⣇⡤⣷⠚⠉⠀⠀⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⠀⠀⣰⠇⠀⠩⣉⠉⠉⠉⣩⠍⠁⠀⢷⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⠀⠀⡟⠐⠦⠤⠼⠂⠀⠸⠥⠤⠔⠂⠘⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⠀⣸⣧⡟⠳⠒⡄⠀⠀⠀⡔⠲⠚⣧⣀⣿⠿⠷⣶⡆⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⠀⠻⣄⢀⠀⠀⡗⠀⠀⠀⡇⠄⢠⠀⣼⠟⠀⢀⣨⠇⠀⠀⠀⠀⠀\n"
+            + "        ⠀⠀⠀⠀⠀⠀⠀⠙⢶⠬⠴⢧⣤⣤⣤⣽⣬⡥⠞⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀\n\n";
+    private static final String greeting = "Hello, master! How may I help you today? ><\n\n";
+    private static final String exitMessage = "I'll always be here for you~ See ya, master! ^^\n";
+    private static final String moiMoiHeader = "⋆⭒˚.⋆MoiMoi⋆⭒˚.⋆\n";
+    private static final String userHeader = "⋆⭒˚.⋆User⋆⭒˚.⋆\n";
+
+    public static void main(String[] args) {
+        System.out.println(MoiMoi.logo + MoiMoi.moiMoiHeader + MoiMoi.greeting + MoiMoi.exitMessage);
+    }
+
+}
