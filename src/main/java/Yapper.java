6055712e3a33b745c192b5c16b420171d0f17472
valhrd <valentinhn872@gmail.com>
2024-08-18 00:10:51 +0800

    Renamed file and provided skeletal code

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Yapper {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        String name = "Yapper";
+        System.out.println(divider);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
+    }
+}
