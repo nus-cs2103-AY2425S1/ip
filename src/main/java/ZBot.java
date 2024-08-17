e5a913f75b38764d61e37f51424239c192704deb
liauzhanyi <liauzhanyi@gmail.com>
2024-08-17 14:47:04 +0800

    Replace Duke class with ZBot class
    The Duke class has been replaced with the ZBot class to match the new chatbot name and all traces of Duke are removed.

---
+++ src/main/java/ZBot.java
@@ -0,0 +1,7 @@
+public class ZBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ZBot!");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
