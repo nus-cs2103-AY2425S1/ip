ec50e88133a923cd5a5f8c5c89f70898ec36d5fa
benjiBase <blim0e89@gmail.com>
2024-08-17 21:37:28 +0800

    Add level-0 function

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Nameless User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Nameless.java
@@ -0,0 +1,10 @@
+public class Nameless {
+    public static void main(String[] args) {
+        String line = "_____________________________________________________________";
+        String name = "Nameless";
+        String greetings = "Hello, I'm " + name + "\n" + "What can I do for you?";
+        String goodbye = "Bye. Hope to see you again!";
+        System.out.println(line + "\n" + greetings + "\n" + line + "\n" + goodbye + "\n" + line);
+    }
+}
+
