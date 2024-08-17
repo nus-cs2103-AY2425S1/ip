a75b4ef7b3a229e291240970e557ff9f8412eefa
wkxcass <e1129889@u.nus.edu>
2024-08-17 14:24:34 +0800

    MoiMoi.java: add echoing functionality
    Let's add an echoing functionality to MoiMoi.java such that it echoes
    commands entered by the user, and exits when the user types the command
    "bye".

--- src/main/java/MoiMoi.java
+++ src/main/java/MoiMoi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MoiMoi {
 
     private static final String logo = "        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⣀⣀⡀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
@@ -19,13 +21,27 @@ public class MoiMoi {
             + "        ⠀⠀⠀⠀⠀⠀⣸⣧⡟⠳⠒⡄⠀⠀⠀⡔⠲⠚⣧⣀⣿⠿⠷⣶⡆⠀⠀⠀⠀⠀\n"
             + "        ⠀⠀⠀⠀⠀⠀⠻⣄⢀⠀⠀⡗⠀⠀⠀⡇⠄⢠⠀⣼⠟⠀⢀⣨⠇⠀⠀⠀⠀⠀\n"
             + "        ⠀⠀⠀⠀⠀⠀⠀⠙⢶⠬⠴⢧⣤⣤⣤⣽⣬⡥⠞⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀\n\n";
-    private static final String greeting = "Hello, master! How may I help you today? ><\n\n";
-    private static final String exitMessage = "I'll always be here for you~ See ya, master! ^^\n";
     private static final String moiMoiHeader = "⋆⭒˚.⋆MoiMoi⋆⭒˚.⋆\n";
-    private static final String userHeader = "⋆⭒˚.⋆User⋆⭒˚.⋆\n";
+    private static final String userHeader = "⋆⭒˚.⋆User⋆⭒˚.⋆";
+    private static final String greeting = MoiMoi.logo + MoiMoi.moiMoiHeader
+            + "Hello, master! How may I help you today? ><\n\n" + MoiMoi.userHeader;
+    private static final String exitMessage = MoiMoi.moiMoiHeader
+            + "I'll always be here for you~ See ya, master! ^^\n";
 
     public static void main(String[] args) {
-        System.out.println(MoiMoi.logo + MoiMoi.moiMoiHeader + MoiMoi.greeting + MoiMoi.exitMessage);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(MoiMoi.greeting);
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("\n" + MoiMoi.moiMoiHeader + input + "\n\n" + MoiMoi.userHeader);
+            input = sc.nextLine();
+        }
+
+        System.out.println("\n" + MoiMoi.exitMessage);
+
     }
 
 }
