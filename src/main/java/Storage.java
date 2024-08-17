9342af6dbb1ea8dc41a8bc6dbd7741dd05d72131
RezwanAhmed123 <maxtalon94@gmail.com>
2024-08-17 22:38:54 +0800

    Updated Storage.java
    The list inside storage only stores Strings which are primitive structures.
    
    Primitive Structures does not allow for complex representation of data which is what we eventually want to achieve with the Todos, Events and Deadlines in this project.
    
    Thus, the list structure in the storage class needs to be modified and the accompanying methods also need to be fixed accordingly to allow a more complex data structure,  the Task class, to be stored in the list.
    
    This allows each item inside the list to be packaged with both the task name and the mark status together in one object, and eventually we can extend this to other information we want to store together instead of separately.

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
 
 public class Storage {
-    private final ArrayList<String> toDoList = new ArrayList<String>();
+    private final ArrayList<Task> storage = new ArrayList<Task>();
 
-    public String readItem(int index) {
-        return this.toDoList.get(index);
+    public Task readItem(int index) {
+        return this.storage.get(index);
     }
 
     public String addItem(String item) {
-        this.toDoList.add(item);
-        return "added: " + item;
+        Task task = new Task(item);
+        this.storage.add(task);
+        return "added: " + task.printTask();
     }
 
     public String processCommand(String command) {
         if (command.equals("list")) {
             return this.printList();
+        } else if (command.contains("mark") || command.contains("unmark")) {
+            String[] commandParts = command.split(" ");
+            int index = Integer.parseInt(commandParts[1]);
+            if (commandParts[0].equals("mark")) {
+                return this.storage.get(index).markDone();
+            } else {
+                return this.storage.get(index).markUndone();
+            }
         } else {
             return this.addItem(command);
         }
@@ -22,9 +31,9 @@ public class Storage {
 
     public String printList() {
         StringBuilder currentList = new StringBuilder();
-        for (int i = 1; i < toDoList.size() + 1; i++) {
-            currentList.append(Integer.toString(i)).append(". ").append(this.readItem(i - 1));
-            if (i < toDoList.size()) {
+        for (int i = 1; i < storage.size() + 1; i++) {
+            currentList.append(Integer.toString(i)).append(".").append(this.readItem(i - 1).printTask());
+            if (i < storage.size()) {
                 currentList.append("\n");
             }
         }
