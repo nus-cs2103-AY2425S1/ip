21c4d5fb7ef06ab3dcbe50d0cb6d6010a0042d39
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-08-17 22:12:14 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Xizi.java
@@ -0,0 +1,15 @@
+public class Xizi { 
+
+    public static void main(String[] args) {
+        
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Xizi"); 
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
