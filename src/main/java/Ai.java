423f9d7c02b847efa1e493e30b0261699a58bcbc
Uselesstrash06 <uselesstrash06@gmail.com>
2024-08-17 11:04:29 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Ai.java
@@ -0,0 +1,13 @@
+public class Ai {
+    public static void main(String[] args) {
+        String greetings = "Hi, I'm your favourite idol, Ai!!! \n"
+                        + "What shall we do today? Teehee o(◠u◠)o \n";
+
+        String closing = "Don't you wanna get my autograph first? \n"
+                        + "Aww okie :,( See ya!! \n";
+
+        String line = "____________________________________________________________ \n";
+        System.out.println(line + greetings);
+        System.out.println(line + closing + line);
+    }
+}
