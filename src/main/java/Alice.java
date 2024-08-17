d81bc12385a77cb81348f3bedd5f8dbf5528b22c
beginner-web-developer <e1156242@u.nus.edu>
2024-08-17 10:02:19 +0800

    Update Alice.java to echo the user's command and exit upon command "bye"

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Alice {
     public static void main(String[] args) {
         System.out.println("Hello! I am Alice! \nWhat can I do for you?");
         System.out.println("------------------------------------------");
+
+        // get commands from the user while response is not "bye"
+        String response = "";
+        while (!response.equals("bye")) {
+            Scanner scanner = new Scanner(System.in);
+            response = scanner.nextLine();
+            System.out.println("------------------------------------------");
+            System.out.println(response);
+            System.out.println("------------------------------------------");
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
