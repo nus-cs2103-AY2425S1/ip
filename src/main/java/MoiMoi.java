32c762dbe7e2c9d1eebd4749d6c652137c0c5dfc
wkxcass <e1129889@u.nus.edu>
2024-08-17 16:46:11 +0800

    Add mark/unmark functionalities
    Let's,
    * create a class named "Task" to represent tasks stored and tracked by
    the chatbot
    * add a functionality for the chatbot to mark tasks as done
    * add a functionality for the chatbot to unmark completed tasks (ie.
    mark tasks as undone)
    
    The "Task" class encapsulates a String description, and the completion
    status of a task.

--- src/main/java/MoiMoi.java
+++ src/main/java/MoiMoi.java
@@ -29,40 +29,58 @@ public class MoiMoi {
     private static final String exitMessage = MoiMoi.moiMoiHeader
             + "I'll always be here for you~ See ya, master! ^^\n";
     private Scanner sc = new Scanner(System.in);
-    private ArrayList<String> tasks = new ArrayList<String>();
+    private ArrayList<Task> tasks = new ArrayList<Task>();
 
     public void run() {
 
         System.out.println(MoiMoi.greeting);
         String input = sc.nextLine();
+        String[] commandArgs = input.split(" ", 2);
 
         while (!input.equals("bye")) {
             System.out.print("\n" + MoiMoi.moiMoiHeader);
             if (input.equals("list")) {
                 this.list();
-            }
-            else {
+            } else if (commandArgs[0].equals("mark")) {
+                this.mark(Integer.parseInt(commandArgs[1]) - 1);
+            } else if (commandArgs[0].equals("unmark")) {
+                this.unmark(Integer.parseInt(commandArgs[1]) - 1);
+            } else {
                 this.addTask(input);
             }
             System.out.println("\n" + MoiMoi.userHeader);
             input = sc.nextLine();
+            commandArgs = input.split(" ", 2);
         }
 
         System.out.print("\n" + MoiMoi.exitMessage);
 
     }
 
-    public void addTask(String task) {
+    public void addTask(String description) {
+        Task task = new Task(description);
         this.tasks.add(task);
-        System.out.println("Aight! Task added: " + task);
+        System.out.println("Aight! Task added: " + task.toString());
+    }
+
+    public void mark(int index) {
+        Task task = tasks.get(index);
+        task.mark();
+        System.out.println("YAY!! One down!!\n" + task.toString());
+    }
+
+    public void unmark(int index) {
+        Task task = tasks.get(index);
+        task.unmark();
+        System.out.println("Oof, it's OK! Let's get it done soon ;)\n" + task.toString());
     }
 
     public void list() {
         System.out.println("Here's your list of tasks!");
         int index;
-        for (String task : this.tasks) {
+        for (Task task : this.tasks) {
             index = this.tasks.indexOf(task) + 1;
-            System.out.println(index + ". " + task);
+            System.out.println(index + ". " + task.toString());
         }
     }
 
