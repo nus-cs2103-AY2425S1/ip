1ccba95fed48b5a9ba102e05d275b568ad3329e5
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-08-17 21:42:44 +0800

    Extract formatting for chatbot response
    Each chatbot response requires the message to be formatted,
    with horizontal lines and indentation.
    
    Response format is standard,
    causing duplication in formatting each response.
    
    Let's isolate response formatting to 2 utility functions,
    which take in message(s) as
    a string or an array of strings.
    
    Using functions allows us to reuse formatting logic,
    reducing code duplication.

--- src/main/java/OuiOuiBaguette.java
+++ src/main/java/OuiOuiBaguette.java
@@ -2,20 +2,12 @@ import java.util.Scanner;
 
 public class OuiOuiBaguette {
     public static void main(String[] args) {
-        String greetings = """
-                ____________________________________________________________
-                Bone-jaw! I'm Oui Oui Baguette.
-                What can I do for you?
-                Oui Oui!
-                ____________________________________________________________
-                """;
 
-        String farewell = """
-                ____________________________________________________________
-                Bye! Hope to see you soon!
-                Oui Oui!
-                ____________________________________________________________
-                """;
+        String greetings = formatBotSpeech(new String[] {
+                "Bone-jaw! I'm Oui Oui Baguette.",
+                "What can I do for you? Oui Oui"});
+
+        String farewell = formatBotSpeech("Bye. Hope to see you soon! Oui Oui");
     
         // Say greetings
         System.out.println(greetings);
@@ -25,19 +17,47 @@ public class OuiOuiBaguette {
 
         // Main event loop
         while (true) {
+            // Read input
             String cmd = sc.nextLine();
 
+            // "bye" command exits loop
             if (cmd.equals("bye")) {
                 break;
             }
 
             // Simply echo back command
-            System.out.println("____________________________________________________________\n"
-                    + cmd
-                    + "\n____________________________________________________________");
+            System.out.println(formatBotSpeech(cmd));
         }
 
         System.out.println(farewell);
     }
-    
+
+
+    /**
+     * Returns a message formatted with for a CLI chat response
+     *
+     * @param s response message
+     * @return response delimited with horizontal lines and indented
+     */
+    public static String formatBotSpeech(String s) {
+        return formatBotSpeech(new String[]{s});
+    }
+
+
+    /**
+     * Returns a message formatted with for a CLI chat response
+     *
+     * @param strs list of response message
+     * @return response delimited with horizontal lines and indented
+     */
+    public static String formatBotSpeech(String[] strs) {
+        StringBuilder res = new StringBuilder("\t____________________________________________________________");
+
+        for (String s : strs) {
+            res.append("\n\t " + s);
+        }
+
+        res.append("\n\t____________________________________________________________\n");
+        return res.toString();
+    }
 }
