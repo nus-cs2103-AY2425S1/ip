1ae2ebfefc13d32c5754a5c30884df5af643dcde
jonushzw <jonushzw@gmail.com>
2024-08-17 22:01:30 +0800

    Level-1 Commit

--- src/main/java/Wolfie.java
+++ src/main/java/Wolfie.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Wolfie {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -6,11 +8,25 @@ public class Wolfie {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Wolfie");
+        System.out.println(" Hello Dean's Lister! I'm Wolfie");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye Dean's Lister. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
