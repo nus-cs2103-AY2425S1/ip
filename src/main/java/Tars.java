87cdf4bbc28875e5d4f9635c25de6547ba78c91e
appleshill <eliot@u.nus.edu>
2024-08-17 15:55:55 +0800

    Level-0

---
+++ src/main/java/Tars.java
@@ -0,0 +1,10 @@
+public class Tars {
+    public static void main(String[] args) {
+        System.out.println("____________________________________");
+        System.out.println("Hello! I'm TARS");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________");
+    }
+}
