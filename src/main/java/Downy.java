213373366d0eb3f70ebf6eaff4535eab728489f1
Dominic-Khoo <dominickhooyx@gmail.com>
2024-08-18 00:19:58 +0800

    Add echoing of user input and exit command

--- src/main/java/Downy.java
+++ src/main/java/Downy.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Downy {
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Downy.\nHow can I help?\n\nBye! Yippee!" );
+        String divider = "________________________________________\n";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(divider + "Hello! I'm Downy.\nHow can I help?\n" + divider);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(divider + userInput + "\n" + divider);
+        }
+        scanner.close();
+        System.out.println(divider + "Bye! Yippee!");
     }
 }
