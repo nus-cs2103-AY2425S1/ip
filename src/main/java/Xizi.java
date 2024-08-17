174f051566a3e47f0326c227840fe911cba2a888
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-08-17 22:43:20 +0800

    implemented the functionality of level 1

--- src/main/java/Xizi.java
+++ src/main/java/Xizi.java
@@ -1,15 +1,36 @@
-public class Xizi { 
+import java.util.Scanner;
+public class Xizi {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Xizi"); 
+        System.out.println(" Hello! I'm Xizi");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+
+    while(true) {
+        String userInput = scanner.nextLine();
+
+        // Echo the input back to the user
+        System.out.println("____________________________________________________________");
+        System.out.println(" " + userInput);
+        System.out.println("____________________________________________________________");
+
+        // Exit the loop if the user types "bye"
+        if (userInput.equalsIgnoreCase("bye")) {
+            break;
+        }
+    }
+
+    // Print the goodbye message
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+    // Close the scanner
+        scanner.close();
     }
 }
 
