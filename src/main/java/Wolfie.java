ce1c50cd5c2fa13b048fd639ce9cdb1b68a41c3e
jonushzw <jonushzw@gmail.com>
2024-08-17 22:22:58 +0800

    Level-2 Commit

--- src/main/java/Wolfie.java
+++ src/main/java/Wolfie.java
@@ -9,6 +9,9 @@ public class Wolfie {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100]; // array to store tasks
+        int taskCount = 0; // counter for number of tasks
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello Dean's Lister! I'm Wolfie");
         System.out.println(" What can I do for you?");
@@ -21,9 +24,17 @@ public class Wolfie {
                 System.out.println(" Bye Dean's Lister. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
+            } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]); // add number tag and print task
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input; // add task to array
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input); // print added task
                 System.out.println("____________________________________________________________");
             }
         }
