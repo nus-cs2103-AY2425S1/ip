3b7eeafb418ef9b12f03c5dabbb8da829b10175b
FooChao <foochao01@gmail.com>
2024-08-17 12:29:05 +0800

    level-0.1 - Rename, Greet and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Note: those extra spaces before \n is done intentionally
+        // so it is easier to see the logo and edit it subsequently
+        // I know it is waste of space and unnecessary
+        String logo = "           ___           \n"
+                    + "          |* *|          \n"
+                    + "          |\\_/|          \n"
+                    + "  ---------------------  \n"
+                    + "  |                   |  \n"
+                    + "-----     SUMO      -----\n"
+                    + "| | |               | | |\n"
+                    + "-----      DE       -----\n"
+                    + "  |                   |  \n"
+                    + "  ---------------------  \n"
+                    + "  |                   |  \n"
+                    + "  |      /-----\\      |  \n"
+                    + "  |_____/       \\_____|  \n";
+
+        String goodbye = "------------------------------------\n"
+                        + "Goodbye! Sumo hope to see you again!\n"
+                        + "------------------------------------\n";
+
+
+        System.out.println("    Hello, I am Sumo-DE \n"
+                + logo
+                + '\n'
+                + " How can Sumo help you?"
+        );
+
+        System.out.println(goodbye);
     }
 }
