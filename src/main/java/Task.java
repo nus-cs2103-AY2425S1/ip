f1ffe6ed49f32e10d30f7ec1e5d3867794abb15a
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-17 15:14:12 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(this);
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(this);
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        String output = "[" + getStatusIcon() + "] ";
+        output += description;
+        return output;
+    }
+}
