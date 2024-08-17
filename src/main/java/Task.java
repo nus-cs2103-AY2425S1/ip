ce58fdda189742528777ed87c8605b6dbce930c3
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-17 16:13:08 +0800

    Fix text format for markDone and unmarkDone methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,12 +10,14 @@ public class Task {
     public void markDone() {
         this.isDone = true;
         System.out.println("Nice! I've marked this task as done:");
+        System.out.print("  ");
         System.out.println(this);
     }
 
     public void unmarkDone() {
         this.isDone = false;
         System.out.println("OK, I've marked this task as not done yet:");
+        System.out.print("  ");
         System.out.println(this);
     }
 
