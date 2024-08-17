2ed2d68660ff11fbe07bb7cb8e8915364c9bd58d
RezwanAhmed123 <maxtalon94@gmail.com>
2024-08-17 21:11:32 +0800

    Updated ToDoList.java.

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -1,15 +1,34 @@
 import java.util.ArrayList;
 
 public class ToDoList {
-    private final ArrayList<String> ToDoList = new ArrayList<String>();
+    private final ArrayList<String> toDoList = new ArrayList<String>();
 
     public String readItem(int index) {
-        return ToDoList.get(index);
+        return this.toDoList.get(index);
     }
 
     public String addItem(String item) {
-        ToDoList.add(item);
+        this.toDoList.add(item);
         return "added: " + item;
+    }
+
+    public String processCommand(String command) {
+        if (command.equals("list")) {
+            return this.printList();
+        } else {
+            return this.addItem(command);
+        }
+    }
+
+    public String printList() {
+        String currentList = "";
+        for (int i = 1; i < toDoList.size() + 1; i++) {
+            currentList += Integer.toString(i) + ". " + this.readItem(i - 1);
+            if (i < toDoList.size()) {
+                currentList += "\n";
+            }
+        }
+        return currentList;
     }
 
     public static void main(String[] args) {
