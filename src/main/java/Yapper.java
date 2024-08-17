b48feb08fe41fce32a7015d8c675f4bed0ddf636
valhrd <valentinhn872@gmail.com>
2024-08-18 00:17:32 +0800

    Level-1: Echo

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class Yapper {
     public static void main(String[] args) {
+
         String divider = "____________________________________________________________";
         String name = "Yapper";
+
         System.out.println(divider);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(divider);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                break;
+            } else {
+                System.out.println(divider);
+                System.out.println(input);
+                System.out.println(divider);
+            }
+        }
     }
 }
