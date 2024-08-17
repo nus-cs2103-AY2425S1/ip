fdcba8d575d91c040e064ff618656230305effec
Uselesstrash06 <uselesstrash06@gmail.com>
2024-08-17 13:54:53 +0800

    Add, List

--- src/main/java/Ai.java
+++ src/main/java/Ai.java
@@ -1,29 +1,47 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class Ai {
+    String[] tasks = new String[101];
+    int counter = 0;
+    static final String greetings = "Hi, I'm your favourite idol, Ai!!! \n"
+            + "What shall we do today? Teehee o(◠u◠)o \n";
+    static final String closing = "Don't you wanna get my autograph first? \n"
+            + "Aww okie :,( See ya!! \n";
+    static final String line = "____________________________________________________________ \n";
+
+    public void list() {
+        for(int i = 0; i < counter; i++) {
+            System.out.println((i + 1) + ". " + tasks[i] + "\n");
+        }
+        System.out.println(line);
+    }
+
+    public void add(String task) {
+        tasks[counter] = task;
+        counter++;
+        System.out.println("added: " + task + "\n" + line);
+    }
+
     public static void main(String[] args) {
+        Ai ai = new Ai();
+
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-
-        String greetings = "Hi, I'm your favourite idol, Ai!!! \n"
-                        + "What shall we do today? Teehee o(◠u◠)o \n";
-
-        String closing = "Don't you wanna get my autograph first? \n"
-                        + "Aww okie :,( See ya!! \n";
-
-        String line = "____________________________________________________________ \n";
-
 
 
         System.out.println(line + greetings + line);
 
-        String reply = scanner.nextLine();  // Read user input
-        System.out.println(line);
+        String reply;
 
-        while (!reply.equals("bye")) {
-            System.out.println(reply + "\n" + line);
+        do {
             reply = scanner.nextLine();  // Read user input
             System.out.println(line);
-        }
+
+            if(reply.equals("list")) {
+                ai.list();
+            } else {
+                ai.add(reply);
+            }
+        }  while(!reply.equals("bye"));
         System.out.println(closing + line);
     }
 }
