f32d80aae6d60c96e899d89714a98e317d8509ee
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-08-17 18:20:51 +0800

    Add Echo

--- src/main/java/PX.java
+++ src/main/java/PX.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class PX {
+    private static String name = "PX";
+
     private static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void PXSays(String arg) {
+        System.out.println("    " + arg);
     }
 
     public static void main(String[] args) {
         printLine();
-        System.out.println("Hello! I'm PX");
-        System.out.println("What can I do for you?");
+        PXSays("Hello! I'm " + name);
+        PXSays("What can I do for you?");
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            PXSays("");
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                printLine();
+                PXSays("Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            } else {
+                printLine();
+                PXSays(input);
+                printLine();
+
+            }
+        }
     }
 }
