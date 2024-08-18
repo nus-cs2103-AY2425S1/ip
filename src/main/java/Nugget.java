09be9df4f2d1684c440bd40d1a8e0fe009b317ec
tzuennn <tzuen2406@gmail.com>
2024-08-19 00:28:48 +0800

    Add Nugget.java

---
+++ src/main/java/Nugget.java
@@ -0,0 +1,10 @@
+public class Nugget {
+    public static void main(String[] args) {
+        System.out.println("________________________________________");
+        System.out.println("Hello! I'm Nugget");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________");
+    }
+}
