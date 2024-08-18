488f548d7380d99428097d3eb92267b5d5885011
leowolf275 <leo27wolf@gmail.com>
2024-08-18 11:32:47 +0800

    Level-1: Echo

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Boss {
     public static void main(String[] args) {
         System.out.println("Hello! I'm the boss.");
         System.out.println("What can I do for you?");
+        Scanner myObj = new Scanner(System.in);
+
+        String text = myObj.nextLine();
+        System.out.println(text);
+        System.out.println();
+
+        while (!text.equals("bye")) {
+            text = myObj.nextLine();
+            System.out.println(text);
+            System.out.println();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
