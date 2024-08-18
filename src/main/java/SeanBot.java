264ae5ca26550e3d5dd3bb2b25b4e3379afa7696
SeanWong2 <seanwcx02@gmail.com>
2024-08-19 00:04:41 +0800

    Created scanner to copy user inputs and end when saying Bye

--- src/main/java/SeanBot.java
+++ src/main/java/SeanBot.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 public class SeanBot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm SeanBot\n" + //
                             "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
-                            
+        
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String userInput = scanner.nextLine();
+            System.out.println(userInput);
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }         
+        }
+        scanner.close();
     }
 }
