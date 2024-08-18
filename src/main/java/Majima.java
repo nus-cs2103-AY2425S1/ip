4e7fa84990612354a253ce290e9f582761139720
Meowloid <tochiex3@gmail.com>
2024-08-18 13:45:45 +0800

    personality change and level 1

--- src/main/java/Majima.java
+++ src/main/java/Majima.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Majima {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String linegap = "____________________________________________________________";
         System.out.println(linegap);
-        System.out.println("Hello! I'm Majima");
-        System.out.println("What can I do for you?");
+        System.out.println("KIIIIIRYU-CHAN! It's ya old pal, Majima!");
+        System.out.println("What can I do fer ya?");
         System.out.println(linegap);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(linegap);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("Bye")) {
+                System.out.println(linegap);
+                System.out.println("Bye bye! Don't keep me waiting fer too long now, ya hear?");
+                System.out.println(linegap);
+                break;
+            }
+
+            System.out.println(linegap);
+            System.out.println(input);
+            System.out.println(linegap);
+        }
+
+        scanner.close();
     }
 }
