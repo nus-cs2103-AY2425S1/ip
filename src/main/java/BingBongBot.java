ba697a621c4ffa6fb7eb8179381be89bbe21aa7a
BuffWuff1712 <lowbingheng14@gmail.com>
2024-08-18 12:04:01 +0800

    Add and List features

--- src/main/java/BingBongBot.java
+++ src/main/java/BingBongBot.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BingBongBot {
     private final BingBongUI ui;
+    private List<String> commandStorage;
 
     public BingBongBot(BingBongUI ui) {
         this.ui = ui;
+        this.commandStorage = new ArrayList<>();
     }
 
     public void run() {
@@ -14,9 +19,24 @@ public class BingBongBot {
             if (command.equalsIgnoreCase("bye")) {
                 isRunning = false;
                 ui.showGoodbye();
+            } else if (command.equalsIgnoreCase("list")) {
+                listCommands();
             } else {
-                ui.showResponse("BingBong: " + command);
+                this.commandStorage.add(command);
+                ui.showResponse("added: " + command);
             }
+        }
+    }
+
+    private void listCommands() {
+        if (commandStorage.isEmpty()) {
+            ui.showResponse("No commands have been saved.");
+        } else {
+            StringBuilder list = new StringBuilder();
+            for (int i = 0; i < commandStorage.size(); i++) {
+                list.append(i + 1).append(". ").append(commandStorage.get(i)).append("\n");
+            }
+            ui.showResponse(list.toString());
         }
     }
 }
