8736b214241765a80543d102259f252d428cc04b
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-08-19 01:18:35 +0800

    Add Justbot.java

---
+++ src/main/java/Justbot.java
@@ -0,0 +1,14 @@
+public class Justbot {
+    public static void main(String[] args) {
+        final String chatbotName = "JustBot";
+
+        System.out.println("------------------------------------------");
+        System.out.println("Hello I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println("------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------");
+
+    }
+
+}
