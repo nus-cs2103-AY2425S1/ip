6b0b5d757b2bbc1457659118469da53313d6dc8b
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-08-18 23:14:36 +0800

    Exceptions to handle errors in user inputs

---
+++ src/main/java/EmptyDescriptionException.java
@@ -0,0 +1,13 @@
+/**
+ * Exception thrown when a task description is empty.
+ */
+public class EmptyDescriptionException extends OllieException {
+    /**
+     * Constructs an EmptyDescriptionException with a predefined message based on the task type.
+     *
+     * @param taskType the type of task that caused the exception
+     */
+    public EmptyDescriptionException(String taskType) {
+        super("Please add a description for a " + taskType + "task!");
+    }
+}
