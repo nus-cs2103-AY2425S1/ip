a0a87c46a4560dcf964af89f2c23fdcdd948c0b4
DennieDan <e1129903@u.nus.edu>
2024-08-18 21:45:40 +0800

    build: add and list functions

--- src/main/java/Tecna.java
+++ src/main/java/Tecna.java
@@ -1,20 +1,65 @@
 import java.util.Scanner;
 
 public class Tecna {
+    private String[] todo;
+    private int todoSize;
+
+    public Tecna() {
+        this.todo = new String[100];
+        this.todoSize = 0;
+    }
+
+    public void exitChatBot() {
+        System.out.println("----------------------------------------------");
+        System.out.println("Pleased to help you! See you again ^_^");
+        System.out.println("----------------------------------------------");
+    }
+
     public void echo() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+
         if (input.equalsIgnoreCase("bye")) {
-            System.out.println("----------------------------------------------");
-            System.out.println("Pleased to help you! See you again ^_^");
-            System.out.println("----------------------------------------------");
+            this.exitChatBot();
         } else {
             System.out.println("----------------------------------------------");
             System.out.println(input);
             System.out.println("----------------------------------------------");
-            this.echo();
+        }
+        sc.close();
+    }
+
+    public void getRequest() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println("----------------------------------------------");
+            if (input.equalsIgnoreCase("list")) {
+                this.listItems();
+            } else {
+                this.addItem(input);
+            }
+
+            System.out.println("----------------------------------------------");
+
+            input = sc.nextLine();
+        }
+        this.exitChatBot();
+        sc.close();
+    }
+
+    public void addItem(String item) {
+        this.todo[this.todoSize] = item;
+        ++this.todoSize;
+        System.out.println("added: " + item);
+    }
+
+    public void listItems() {
+        for (int i = 0; i < this.todoSize; ++i) {
+            System.out.println(i + 1 + ". " + this.todo[i]);
         }
     }
+
     public static void main(String[] args) {
         String logo = " **          **\n" +
                       "*  *        *  *\n" +
@@ -34,6 +79,6 @@ public class Tecna {
         System.out.println("I'm Tecna!\nHow can I help you?");
         System.out.println("----------------------------------------------");
         Tecna tecna = new Tecna();
-        tecna.echo();
+        tecna.getRequest();
     }
 }
