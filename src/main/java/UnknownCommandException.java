f547e14feff48b3f76a7659e301ac09e92ab2975
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-08-18 15:49:17 +0800

    Add unknown command exception
    Thrown when an unknown command is input by the user

---
+++ src/main/java/UnknownCommandException.java
@@ -0,0 +1,5 @@
+public class UnknownCommandException extends OuiOuiBaguetteException {
+    public UnknownCommandException() {
+        super("I'm sorry, but I don't know what that means :-(");
+    }
+}
