c9f628b8fc6768aa01cf5b29b9df5ce6428bf102
KrashKart <e1113755@u.nus.edu>
2024-08-18 17:25:59 +0800

    added add and list functions, segregated add and list into separate global methods

--- src/main/java/Jackson.java
+++ src/main/java/Jackson.java
@@ -1,16 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jackson {
+
+    private static ArrayList<String> list = new ArrayList<String>(10);
+
+    public static void add_list(String task) {
+        System.out.printf("Adding '%s' to list!\n", task);
+        list.add(task);
+    }
+
+    public static void show_list() {
+        if (list.isEmpty()) {
+            System.out.println("Nothing in list!");
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%d. %s\n", i + 1, list.get(i));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String name = "Jackson";
-        String response = "";
         Scanner sc = new Scanner(System.in);
 
         System.out.printf("Hello! My name is %s!\nWhat can I do for you today?\n> ", name);
-        response = sc.nextLine();
+        String response = sc.nextLine().strip();
 
         while (!response.equals("bye")) {
-            System.out.println(response);
+            if (response.equals("list")) {
+                show_list();
+            } else {
+                add_list(response);
+            }
             System.out.print("> ");
             response = sc.nextLine();
         }
