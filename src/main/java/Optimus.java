ef4810079330c6cf4e90d6c5b618c074ec6d467d
tayxuenye <tayxuenye@gmail.com>
2024-08-18 22:41:43 +0800

    echos commands entered by user, and exits when user types bye

--- src/main/java/Optimus.java
+++ src/main/java/Optimus.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Optimus {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Optimus.\nWhat can I do for you?\n\nBye. Hope to see you again soon!";
+        String line = "____________________________________________________________\n";
+
+        //greeting for user
+        String greeting = line +
+                "Hello! I'm Optimus.\n" +
+                "What can I do for you?\n" +
+                line;
         System.out.println(greeting);
+
+        //Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        //while loop to keep taking in user input
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            //exit program if user enters "bye"
+            if (userInput.equals("bye")) {
+                System.out.println(line +
+                        "Bye. Hope to see you again soon!\n" +
+                        line);
+                scanner.close();
+                System.exit(0);
+            }
+
+            // else echo user input
+            System.out.println(line +
+                    userInput + "\n" + line);
+        }
     }
 }
