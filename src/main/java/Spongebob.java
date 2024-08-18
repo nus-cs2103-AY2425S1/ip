98e54bfab7b3b1b5e68d9557800233ebcd485845
Jaynon <jaynonfoong@hotmail.com>
2024-08-18 17:48:53 +0800

    added echo

--- src/main/java/Spongebob.java
+++ src/main/java/Spongebob.java
@@ -1,18 +1,32 @@
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Spongebob {
+
+    final static String LINE = "____________________________________________________________\n";
+    final static String lOGO = "▕╮╭┻┻╮╭┻┻╮╭▕╮╲\n" +
+                        "▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏\n" +
+                        "▕╭┻┻┻┛┗┻┻┛ ╰▏\n" +
+                        "▕╰━━━┓┈┈┈╭╮▕╭╮▏\n" +
+                        "▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏\n" +
+                        "▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏";
+
     public static void main(String[] args) {
-        final String LINE = "____________________________________________________________\n";
-        final String logo = "▕╮╭┻┻╮╭┻┻╮╭▕╮╲\n" +
-                            "▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏\n" +
-                            "▕╭┻┻┻┛┗┻┻┛ ╰▏\n" +
-                            "▕╰━━━┓┈┈┈╭╮▕╭╮▏\n" +
-                            "▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏\n" +
-                            "▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏";
+
+        Scanner scanner= new Scanner(System.in);
 
         String greetings = "Hello! I'm Spongebob! \nWhat can I do for you?\n";
         String goodbye =  "Bye. Hope to see you again soon!\n";
 
+        System.out.println(LINE + lOGO + "\n" + greetings + LINE);
 
+        String usrInput = scanner.nextLine();
 
-        System.out.println(LINE + logo + "\n" + greetings + LINE + goodbye);
+        while (!Objects.equals(usrInput, "bye")) {
+            System.out.println(LINE + usrInput + "\n" +LINE);
+            usrInput = scanner.nextLine();
+        }
+        System.out.println(LINE + goodbye + LINE);
     }
 }
