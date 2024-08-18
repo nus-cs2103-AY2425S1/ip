bf8a127da5ed3088a39d3bac95c62789bae2a514
Dinoman44 <prakamya.singh08@gmail.com>
2024-08-18 11:34:19 +0800

    Complete Level-1 requirements

--- src/main/java/Fret.java
+++ src/main/java/Fret.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Fret {
     public static void main(String[] args) {
         String logo = "________                ___ \n"
@@ -6,11 +8,29 @@ public class Fret {
                 + "| ___|  | |/ /  / _ \\   | |\n"
                 + "| |     |   /  <  __/   | |__\n"
                 + "|_|     |__|    \\___|   |___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("---------------------------");
-        System.out.println("Hello! I'm Fret\nWhat can I do for you?");
-        System.out.println("---------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("---------------------------");
+        
+        System.out.println("Initiating...\n" + logo);
+        
+        System.out.println("\t-----------------------------------------");
+        System.out.println("\tPersonal AI Fret, coming online!\n\tHey, how can I be of assistance?");
+        System.out.println("\t-----------------------------------------");
+
+        Scanner input = new Scanner(System.in);
+        String userInput;
+
+        do {
+            userInput = input.nextLine();
+            if (!userInput.equals("bye")) {
+                System.out.println("\t-----------------------------------------");
+                System.out.println("\t" + userInput);
+                System.out.println("\t-----------------------------------------");
+            }
+        } while (!userInput.equals("bye"));
+
+        input.close();
+
+        System.out.println("\t-----------------------------------------");
+        System.out.println("\tOh well, it was fun while it lasted. Goodbye!");
+        System.out.println("\t-----------------------------------------");
     }
 }
