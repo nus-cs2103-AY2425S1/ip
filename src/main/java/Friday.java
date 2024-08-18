7f35dc0c574ccb967dc9f0d4928ce4ce87862cd0
blanklogic <koh.junxian.jaymeson@icloud.com>
2024-08-18 20:43:05 +0800

    Level 1. Implemented logic for accepting and echoing inputs of the user as well as exiting once exit flag has been triggered by the user

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,10 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Friday {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Friday");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String horizontalLine = "\t____________________________________________________________"; // For better readability
+
+        System.out.println(horizontalLine);
+        System.out.println("\tWelcome Back! I'm Friday");
+        System.out.println("\tWhat can I do for you today?");
+        System.out.println(horizontalLine);
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (Objects.equals(input, "bye")) break;
+            System.out.println(horizontalLine + "\n\t" + input + "\n" + horizontalLine);
+        }
+        System.out.println(horizontalLine);
+        System.out.println("\tGood Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
