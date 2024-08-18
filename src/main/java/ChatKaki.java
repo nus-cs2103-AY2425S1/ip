f9f8b45175fce6500fc83733d6f9cc161c55804e
limyuquan <limyuquan02@gmail.com>
2024-08-18 10:31:25 +0800

    Finished Level-1

--- src/main/java/ChatKaki.java
+++ src/main/java/ChatKaki.java
@@ -1,18 +1,44 @@
+import java.util.Scanner;
+
 public class ChatKaki {
     private static final String CHATBOT_NAME = "ChatKaki";
 
-    public static void greet() {
+    public static void sayGreeting() {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + CHATBOT_NAME);
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
+    }
 
-        // Exit message
+    public static void sayMessage(String message) {
+        System.out.println("\n____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void sayBye() {
+        System.out.println("\n____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+    }
+
+    public static void chatService() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                sayBye();
+                break;
+            }
+            sayMessage(userInput);
+        }
     }
 
     public static void main(String[] args) {
-        greet();
+        sayGreeting();
+        chatService();
     }
 }
