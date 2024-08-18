ce2a640f6a7c317d5c9d7e0a41996023e4239fa2
leowolf275 <leo27wolf@gmail.com>
2024-08-18 15:43:37 +0800

    Level 3: Mark as Done

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Boss {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numTasks = 0;
 
         System.out.println("Hello! I'm the boss.");
@@ -18,9 +18,28 @@ public class Boss {
                     i++;
                 }
             }
+            else if (task.contains("unmark") && isDigit(task)) {
+                // replace all characters with nothing, in order to extract number!
+                String taskNum = task.replaceAll("[^0-9]", "");
+                int num = Integer.parseInt(taskNum);
+                Task item = tasks[num - 1];
+                item.markAsUnDone();
+                System.out.println("Ok! I have marked this task as not done yet!");
+                System.out.println(item);
+            }
+            // need to ensure that the string contains a number!!!!
+            else if (task.contains("mark") && isDigit(task)) {
+                // replace all characters with nothing, in order to extract number!
+                String taskNum = task.replaceAll("[^0-9]", "");
+                int num = Integer.parseInt(taskNum);
+                Task item = tasks[num-1];
+                item.markAsDone();
+                System.out.println("Nice! I have marked this task as done!");
+                System.out.println(item);
+            }
             else {
                 System.out.println("added: " + task);
-                tasks[numTasks] = task;
+                tasks[numTasks] = new Task(task);
                 numTasks++;
             }
             task = myObj.nextLine();
@@ -29,4 +48,15 @@ public class Boss {
         System.out.println("Bye. Hope to see you again soon!");
 
     }
+
+    public static boolean isDigit(String s) {
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
