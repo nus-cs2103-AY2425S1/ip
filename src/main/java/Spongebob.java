e787764e729f295e7b914fb3c168e14029000d9a
Jaynon <jaynonfoong@hotmail.com>
2024-08-18 18:39:09 +0800

    added Mark as Done feature

--- src/main/java/Spongebob.java
+++ src/main/java/Spongebob.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Spongebob {
 
-    static List<String> cache = new ArrayList<>();
+     static List<Task> cache = new ArrayList<>();
 
     final static String LINE = "____________________________________________________________\n";
     final static String lOGO = "▕╮╭┻┻╮╭┻┻╮╭▕╮╲\n" +
@@ -21,27 +21,73 @@ public class Spongebob {
 
         System.out.println(LINE + lOGO + "\n" + greetings + LINE);
 
-        String usrInput = scanner.nextLine();
+        String usrInput = scanner.nextLine();   // get user input
 
         while (!usrInput.equals("bye")) {
             System.out.println(LINE + "\n");
 
-            if (usrInput.equals("list")) {
-                ListIterator<String> iter = cache.listIterator();
+            // gets first 2 words of command
+            String[] command = usrInput.split(" ", 2);
 
-                while (iter.hasNext()) {
-                    System.out.println(iter.nextIndex() + ". " + iter.next());
-                }
+            switch (command[0]) {
 
-            } else {
-                System.out.println("added: " + usrInput + "\n");
-                cache.add(usrInput);
+                // enumerate list
+                case "list":
+                    ListIterator<Task> iter = cache.listIterator();
+
+                    System.out.println("Here are the tasks in your list:\n");
+                    while (iter.hasNext()) {
+                        Task cur = iter.next();
+                        System.out.println((iter.previousIndex() + 1) + "." + cur.getStatusIcon() + cur.getDescription());
+                    }
+
+                    break;
+
+                case "mark":
+                    try {
+
+                        // get index of task
+                        int index = Integer.parseInt(command[1]);
+
+                        // mark 
+                        cache.get(index - 1).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(cache.get(index - 1).getStatusIcon() + cache.get(index - 1).getDescription());
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error, index must be an integer!");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Index out of bounds!");
+                    }
+                    break;
+
+                case "unmark":
+                    try {
+
+                        // get index of task
+                        int index = Integer.parseInt(command[1]);
+
+                        // unmark
+                        cache.get(index - 1).unmarkAsDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println(cache.get(index - 1).getStatusIcon() + cache.get(index - 1).getDescription());
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error, index must be an integer!");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Index out of bounds!");
+                    }
+                    break;
+
+                default:
+                    System.out.println("added: " + usrInput + "\n");
+                    cache.add(new Task(usrInput));
+                    break;
             }
 
             System.out.println(LINE + "\n");
-            usrInput = scanner.nextLine();
-
+            usrInput = scanner.nextLine();  // get next input
         }
-        System.out.println(LINE + goodbye + LINE);
+        System.out.println(LINE + goodbye + LINE);  // end
     }
 }
