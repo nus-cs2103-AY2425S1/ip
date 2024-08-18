40625d4923698a821df12c8c799f9085c938eb9c
Nicholas-Cheng-De-Fei <nicholas_cheng@u.nus.edu>
2024-08-18 21:32:33 +0800

    Chatbot can now echo user input and will exit when the user inputs bye

--- src/main/java/Quack.java
+++ src/main/java/Quack.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Quack {
 
     // Variables
@@ -10,11 +12,15 @@ public class Quack {
         "\\_____\\ \\_/____/(____  /\\___  >__|_ \\ \n" +
         "       \\__>          \\/     \\/     \\/\n";
 
+    private boolean isRunning;
+    
     public String spacer = "-".repeat(40);
     
     // Functions
 
-    Quack() {}
+    Quack() {
+        this.isRunning = true;
+    }
 
     private void printLogo() {
         System.out.println(logo + "\n" + spacer);
@@ -28,10 +34,32 @@ public class Quack {
         System.out.println("Bye. Hope to see you again soon!\n" + spacer);
     }
 
+    private void echo (String text) {
+        System.out.println(text + "\n" + spacer);
+    }
+
     private void run() {
 
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+
+        // Chatbot is running for the first time, display the logo and greet the user
         this.printLogo();
         this.greeting();
+        
+        // Keep taking inputs from the user
+        while (isRunning) {
+            String command = sc.nextLine();
+            System.out.println(spacer);
+            
+            // As long as the user types bye in any format it will terminate
+            if (command.toLowerCase().equals("bye")) {
+                this.isRunning = false;
+            } else {
+                echo(command);
+            }
+        }
+
+        // Chatbot has exited, display the farewell message
         this.farewell();
     }
 
