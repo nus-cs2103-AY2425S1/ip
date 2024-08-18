c338b62aac512cff82bead2eef2943311994b6e1
zyshi's mac <ziyuan.shi@yahoo.com>
2024-08-18 14:39:53 +0800

    Added echoing and the bye command

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Bobby {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hello, I'm Bobby");
         System.out.println("What can I do for you?");
+        while (true) {
+            String s = input.nextLine();
+            if (s.equals("bye")) {
+                break;
+            }
+            System.out.println(s);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
