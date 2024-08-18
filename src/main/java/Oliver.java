199fb7ee53c90132b0b2da40e3e79942458a8278
SQ77 <andrewzsq@gmail.com>
2024-08-18 14:12:49 +0800

    ToDos, Events, Deadlines

--- src/main/java/Oliver.java
+++ src/main/java/Oliver.java
@@ -18,24 +18,47 @@ public class Oliver {
             } else if (input.equals("list")) {
                 System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("\t" + (i+1) + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i));
+                    System.out.println("\t" + (i+1) + "." + tasks.get(i));
                 }
             } else if (command.equals("mark")) {
                 int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks.get(index).markAsDone();
                 System.out.println("\tNice! I've marked this task as done:");
-                System.out.println("\t[X] " + tasks.get(index));
+                System.out.println("\t" + tasks.get(index));
             } else if (command.equals("unmark")) {
                 int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks.get(index).markAsUndone();
                 System.out.println("\tOk, I've marked this task as not done yet:");
-                System.out.println("\t[ ] " + tasks.get(index));
-            } else {
-                Task t = new Task(input);
+                System.out.println("\t" + tasks.get(index));
+            } else if (command.equals("todo")) {
+                ToDo t = new ToDo(input);
                 tasks.add(t);
-                System.out.println("\tadded: " + input);
+                addSuccess(t, tasks);
+            } else if (command.equals("deadline")) {
+                String[] parts = input.split("/by ");
+                String time = parts[1];
+                String action = parts[0].trim();
+                Deadline d = new Deadline(action.split(" ", 2)[1], time);
+                tasks.add(d);
+                addSuccess(d, tasks);
+            } else if (command.equals("event")) {
+                String[] parts = input.split("/from |/to ");
+                String action = parts[0].trim();
+                String start = parts[1].trim();
+                String end = parts[2].trim();
+                Event e = new Event(action.split(" ", 2)[1], start, end);
+                tasks.add(e);
+                addSuccess(e, tasks);
+            } else {
+                System.out.println("Invalid input.");
             }
         }
         sc.close();
+    }
+
+    public static void addSuccess(Task t, ArrayList<Task>tasks) {
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t" + t);
+        System.out.println("\tNow you have " + tasks.size() + " tasks in the list.");
     }
 }
