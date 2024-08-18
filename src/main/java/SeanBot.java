69ade35b151c85dbe840770614e38f7efccfca8b
SeanWong2 <seanwcx02@gmail.com>
2024-08-18 20:00:20 +0800

    level 0

---
+++ src/main/java/SeanBot.java
@@ -0,0 +1,8 @@
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm SeanBot\n" + //
+                            "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+                            
+    }
+}
