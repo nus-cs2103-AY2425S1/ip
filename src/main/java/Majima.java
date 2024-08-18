45a8e9fbdf015f302da922faba60530874b62785
Meowloid <tochiex3@gmail.com>
2024-08-18 13:19:10 +0800

    Level-0 implemented here, don't forget to tag

---
+++ src/main/java/Majima.java
@@ -0,0 +1,11 @@
+public class Majima {
+    public static void main(String[] args) {
+        String linegap = "____________________________________________________________";
+        System.out.println(linegap);
+        System.out.println("Hello! I'm Majima");
+        System.out.println("What can I do for you?");
+        System.out.println(linegap);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(linegap);
+    }
+}
