9169c6d00b18cd65f34d047c95d98bc181dab39f
blanklogic <koh.junxian.jaymeson@icloud.com>
2024-08-18 23:05:14 +0800

    Level 3. Mark as Done. Implemented Task class and refactored code to utilize OOP concepts and modularized code for improved readability

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -2,35 +2,63 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Friday {
-    public static void main(String[] args) {
-
-        String horizontalLine = "\t____________________________________________________________"; // For better readability
-
-        System.out.println(horizontalLine);
+    private final Task[] items = new Task[100];
+    private int index;
+    private void horizontalLine() {
+        System.out.println("\t____________________________________________________________");
+    }
+    private void welcomeMessage() {
+        horizontalLine();
         System.out.println("\tWelcome Back! I'm Friday");
         System.out.println("\tWhat can I do for you today?");
-        System.out.println(horizontalLine);
-
+        horizontalLine();
+    }
+    private void readInput() {
         Scanner sc = new Scanner(System.in);
         String input;
-        String[] items = new String[100];
-        int index = 0;
         while (true) {
             input = sc.nextLine();
+            horizontalLine();
             if (Objects.equals(input, "bye")) break;
             if (Objects.equals(input, "list")) {
-                System.out.println(horizontalLine);
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
-                    System.out.println("\t" + (i + 1) + ". " + items[i]);
+                    System.out.println("\t" + items[i].toPrettyString());
                 }
-                System.out.println(horizontalLine);
+                horizontalLine();
+                continue;
+            } else if (input.startsWith("mark")) {
+                try {
+                    items[Integer.parseInt(input.substring(5)) - 1].markAsDone();
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
+                horizontalLine();
+                continue;
+            } else if (input.startsWith("unmark")) {
+                try {
+                    items[Integer.parseInt(input.substring(7)) - 1].unmarkAsDone();
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
+                }
+                horizontalLine();
                 continue;
             }
-            items[index++] = input;
-            System.out.println(horizontalLine + "\n\tadded: " + input + "\n" + horizontalLine);
+            items[index++] = new Task(input);
+            horizontalLine();
         }
-        System.out.println(horizontalLine);
+    }
+    private void exit() {
         System.out.println("\tGood Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        horizontalLine();
+    }
+    private void initialize() {
+        welcomeMessage();
+        readInput();
+        exit();
+    }
+    public static void main(String[] args) {
+        Friday bot = new Friday();
+        bot.initialize();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    private static int COUNT = 1;
+    protected int id;
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.id = COUNT++;
+        this.isDone = false;
+        System.out.println("\tadded: " + description);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+        System.out.println("\tNice! I've marked this task as done:\n\t  " + this);
+    }
+
+    public void unmarkAsDone() {
+        isDone = false;
+        System.out.println("\tOK, I've marked this task as not done yet:\n\t  " + this);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + description;
+    }
+
+    public String toPrettyString() {
+        return id + "." + this;
+    }
+}
