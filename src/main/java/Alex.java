894f4a8371980d7f0ec4abb2afc53c3ba4356d64
cshao02 <cshao02@gmail.com>
2024-08-18 16:03:12 +0800

    Added echo feature

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class Alex {
     public static void main(String[] args) {
-        String logo =
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String line = "____________________________________________________________";
+        String greeting =
                 """
                         ____________________________________________________________
                          Hello! I'm Alex
                          What can I do for you?
-                        ____________________________________________________________
-                         Bye. Hope to see you again soon!
                         ____________________________________________________________""";
-        System.out.println(logo);
+
+        System.out.println(greeting);
+
+        while(true) {
+            String response = myObj.nextLine();
+            if (response.equals("bye")) {
+                break;
+            }
+            System.out.println(line + "\n" + response + "\n" + line);
+        }
+
+        String farewell =
+                """
+                        ____________________________________________________________
+                        Bye. Hope to see you again soon!
+                        ____________________________________________________________""";
+
+        System.out.println(farewell);
     }
 }
