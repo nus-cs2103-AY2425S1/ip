17ee7d55cd3cdeeac02dd494a15bebec991d2338
calebyyy <caleb.ng.yy@gmail.com>
2024-08-18 21:55:59 +0800

    bug: deadline should not throw exception

--- src/main/java/calebyyy/Tasks/Deadline.java
+++ src/main/java/calebyyy/Tasks/Deadline.java
@@ -8,7 +8,7 @@ public class Deadline extends Task {
     }
 
     @Override
-    public String toString() throws invalidArgumentException {
+    public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
