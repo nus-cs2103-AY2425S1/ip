8b5b87a70adaf007e3e2def831e39747e9054352
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-08-19 01:34:15 +0800

    Added mark task functionality

--- src/main/java/FutureYou.java
+++ src/main/java/FutureYou.java
@@ -2,7 +2,7 @@ import java.util.Scanner; // Import the Scanner class
 import java.util.ArrayList; // import the ArrayList class
 
 public class FutureYou {
-        static ArrayList<String> list = new ArrayList<String>();
+        static ArrayList<Task> taskList = new ArrayList<Task>();
 
         public static void bye() {
                 System.out.println("Bye. Hope to see you again soon!\n" +
@@ -12,9 +12,16 @@ public class FutureYou {
         public static void printList() {
                 int count = 1;
                 System.out.println("Items in List:");
-                for (String item : list) {
-                        System.out.println(count++ + ". " + item);
+                for (Task item : taskList) {
+                        String cross = (item.taskStatus() ? "X" : " ");
+                        System.out.println(count++ + ". " + "[" + cross + "] " + item.taskName());
                 }
+        }
+
+        public static void markTask(int n) {
+                taskList.get(n).markTask();
+                System.out.println("Marked as Done:");
+                System.out.println("[X] " + taskList.get(n).taskName());
         }
 
         public static void main(String[] args) {
@@ -42,8 +49,12 @@ public class FutureYou {
                                 break;
                         } else if (input.toLowerCase().trim().equals("list")) {
                                 printList();
+                        } else if (input.toLowerCase().trim().contains("mark")) {
+                                int num = Integer.parseInt(input.substring(5, input.length()));
+                                markTask(num - 1);
                         } else {
-                                list.add(input);
+                                Task newTask = new Task(input);
+                                taskList.add(newTask);
                                 System.out.println("Item: " + input + " added to list"); // Add user input to list
                         }
                 }
