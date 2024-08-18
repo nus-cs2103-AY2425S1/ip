ad55937fff77a5ccff4e2c5e90799cb21afd38c4
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-08-18 11:03:06 +0800

    add echo function

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,17 +1,52 @@
-public class EchoBot {
-    public static void main(String[] args) {
-        String logo = " _ _ _  _ _ _         _ _ _\n"
-                + "|      |      |     ||     | \n"
-                + "|_ _ _ |      |_ _ _||     |\n"
-                + "|      |      |     ||     |\n"
-                + "|_ _ _ |_ _ _ |     ||_ _ _|\n";
-        System.out.println("Hello from\n" + logo);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class EchoBot {
+    private static final String LIST = "list";
+    private static final String BYE = "bye";
+    private static String Logo = " _ _ _  _ _ _         _ _ _\n"
+            + "|      |      |     ||     | \n"
+            + "|_ _ _ |      |_ _ _||     |\n"
+            + "|      |      |     ||     |\n"
+            + "|_ _ _ |_ _ _ |     ||_ _ _|\n";
+
+    private static void dashline() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        System.out.println("Hello from\n" + Logo);
+        dashline();
         System.out.println("Hello! I'm EchoBot");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        dashline();
+    }
+
+    public static void listAllTask() {
+        dashline();
+        System.out.println("list");
+        dashline();
+    }
+
+    public static void bye() {
+        dashline();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        dashline();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        EchoBot.greet();
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals(EchoBot.LIST)) {
+                EchoBot.listAllTask();
+            }
+            if (userInput.equals(EchoBot.BYE)) {
+                EchoBot.bye();
+                break;
+            }
+        }
     }
 }
