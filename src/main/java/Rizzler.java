1dfb84e008cbfd1b565cc61b877f7995186bfdee
Green-Tea-123 <junboonsoo@gmail.com>
2024-08-18 22:58:04 +0800

    Added ability to store and list tasks

--- src/main/java/Rizzler.java
+++ src/main/java/Rizzler.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Rizzler {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class Rizzler {
                 + "Bye! Rizz you later!\n"
                 + separator;
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
 
         System.out.println(greeting);
         while(true) {
@@ -18,8 +20,23 @@ public class Rizzler {
             if (input.equals("bye")) {
                 System.out.println(goodbye);
                 break;
+            } else if (input.equals("list")) {
+                if (taskList.isEmpty()) {
+                    System.out.println(separator
+                            + "No tasks here yet\n"
+                            + separator);
+                } else {
+                    System.out.println(separator);
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println(String.valueOf(i + 1) + ". " + taskList.get(i));
+                    }
+                    System.out.println(separator);
+                }
             } else {
-                System.out.println(separator + input + "\n" + separator);
+                taskList.add(input);
+                System.out.println(separator
+                        + "added: " + input + "\n"
+                        + separator);
             }
         }
     }
