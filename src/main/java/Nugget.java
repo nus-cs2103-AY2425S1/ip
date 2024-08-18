c757a6141d6aab0741748718104063099ca09cab
tzuennn <tzuen2406@gmail.com>
2024-08-19 01:09:12 +0800

    Store and display text by user

--- src/main/java/Nugget.java
+++ src/main/java/Nugget.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nugget {
     public static void main(String[] args) {
+        ArrayList<String> messages = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("________________________________________");
         System.out.println("Hello! I'm Nugget");
         System.out.println("What can I do for you?");
         System.out.println("________________________________________");
         while (true) {
-            String text = scanner.next();
+            String text = scanner.nextLine();
             if (text.equals("bye")) {
                 System.out.println("________________________________________");
                 break;
+            } else if (text.equals("list")) {
+                Integer len = messages.size();
+                for (int i = 0; i < len; i++) {
+                    String formattedMessage = String.format("%d. %s", i + 1, messages.get(i));
+                    System.out.println(formattedMessage);
+                }
+                System.out.println("________________________________________");
+            } else {
+                System.out.println("________________________________________");
+                System.out.println("added: " + text);
+                System.out.println("________________________________________");
+                messages.add(text);
             }
-            System.out.println("________________________________________");
-            System.out.println(text);
-            System.out.println("________________________________________");
         }
 
         System.out.println("Bye. Hope to see you again soon!");
