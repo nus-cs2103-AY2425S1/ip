670d46e3d78c4ea234972182a0b0982be454702d
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-08-18 16:20:20 +0800

    Make the chatbot echo
    **Current Situation**
    The chatbot only greets and exits
    
    **Why it needs to change**
    The chatbot should echo as well, and it has some personality
    
    **What is being done about it**
    * The chatbot returns the commands said by the user
    * The chatbot greets and says bye
    * The chatbot has cutomised message borders
    * Chatbot personality: The chatbot has :) in the messages

--- src/main/java/Ollie.java
+++ src/main/java/Ollie.java
@@ -1,8 +1,48 @@
+import java.util.Scanner;
 public class Ollie {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm OLLIE");
-        System.out.println("What can I do for you?");
+
+    public static void messageWrapper(String message) {
+        String border = "*-".repeat(30);
+        System.out.println(border);
         System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(message);
+        System.out.println();
+        System.out.println(border);
+    }
+
+    public static void greeting() {
+        messageWrapper("Hello there! I'm OLLIE :) \nWhat can I do for you?");
+    }
+
+    public static void exit() {
+        messageWrapper("Bye. Have a great day. Hope to see you again soon! :) ");
+    }
+
+    public static void commandWrapper(String message) {
+        String border = "--".repeat(30);
+        System.out.println(border);
+        System.out.println(message);
+        System.out.println(border);
+    }
+
+    public static void echo(String userCommand) {
+        if (userCommand.equals("bye")) {
+            exit();
+        } else {
+            commandWrapper(userCommand);
+        }
+    }
+
+    public static void main(String[] args) {
+        greeting();
+        System.out.println();
+
+        String command;
+
+        do {
+            Scanner input = new Scanner(System.in);
+            command = input.nextLine();
+            echo(command);
+        } while (!command.equals("bye"));
     }
 }
