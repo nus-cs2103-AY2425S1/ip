fd4ba409890c94c87c1dd79719f24bdde54bd695
Yong Khee Hou <yongkheehou@gmail.com>
2024-08-18 15:25:10 +0800

    rename bot

--- src/main/java/DailyTasks.java
+++ src/main/java/DailyTasks.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DailyTasks {
     public static final String BOT_NAME = "DailyTasks";
-    public static final String GREETING = "Hello! I'm " + DailyTasks.BOT_NAME + ", a talking keyboard!";
+    public static final String GREETING = "Hello! I'm " + DailyTasks.BOT_NAME + ", your awesome task planner!";
     public static final String GOODBYE = "Bye. Hope to see you again soon!";
 
     public String[] tasks;
@@ -10,8 +10,8 @@ public class DailyTasks {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        DailyTasks kheeBot = new DailyTasks();
-        kheeBot.tasks = new String[100];
+        DailyTasks dailyTasks = new DailyTasks();
+        dailyTasks.tasks = new String[100];
         int taskCounter = 0;
 
         System.out.println(Formatter.formatOutputMessage(DailyTasks.GREETING));
@@ -24,11 +24,11 @@ public class DailyTasks {
             } else if (userInput.equals("list")) {
                 System.out.print(Formatter.formattedBorder());
                 for (int i = 0; i < taskCounter; i++) {
-                    System.out.print(Formatter.formatTaskListing(i + 1, kheeBot.tasks[i]));
+                    System.out.print(Formatter.formatTaskListing(i + 1, dailyTasks.tasks[i]));
                 }
                 System.out.println(Formatter.formattedBorder());
             } else {
-                kheeBot.tasks[taskCounter++] = userInput;
+                dailyTasks.tasks[taskCounter++] = userInput;
 
                 String formattedTask = Formatter.formatOutputMessage("added: " + userInput);
                 System.out.println(formattedTask);
