d90c3c2baf1de9b75b6312cc917d80f12c3007f5
cshao02 <cshao02@gmail.com>
2024-08-18 15:45:26 +0800

    Renamed chatbot and changed greeting

---
+++ src/main/java/Alex.java
@@ -0,0 +1,13 @@
+public class Alex {
+    public static void main(String[] args) {
+        String logo =
+                """
+                        ____________________________________________________________
+                         Hello! I'm Alex
+                         What can I do for you?
+                        ____________________________________________________________
+                         Bye. Hope to see you again soon!
+                        ____________________________________________________________""";
+        System.out.println(logo);
+    }
+}
