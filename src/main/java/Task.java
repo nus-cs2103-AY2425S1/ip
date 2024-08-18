600a102bb582d23d36b668248221de45be3cefa5
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-19 03:55:04 +0800

    Add removeTask method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -31,6 +31,10 @@ public class Task {
         return numberOfTasks;
     }
 
+    public static void removeTask() {
+        numberOfTasks--;
+    }
+
     @Override
     public String toString() {
         String output = "[" + getStatusIcon() + "] ";
