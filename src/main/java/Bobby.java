26f022dff777da5aad38f008336c62663741926c
zyshi's mac <ziyuan.shi@yahoo.com>
2024-08-18 14:47:53 +0800

    Added list feature

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bobby {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println("Hello, I'm Bobby");
         System.out.println("What can I do for you?");
@@ -10,8 +12,15 @@ public class Bobby {
             String s = input.nextLine();
             if (s.equals("bye")) {
                 break;
+            } else if (s.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(s);
+                String text = String.format("Added: %s", s);
+                System.out.println(text);
             }
-            System.out.println(s);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
