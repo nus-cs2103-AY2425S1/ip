6d5efc6badde9aeff1afedba3ca9c61d248bb5a1
cshao02 <cshao02@gmail.com>
2024-08-18 17:25:50 +0800

    Add the ability to mark tasks as done or undone

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Alex {
+
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String line = "____________________________________________________________";
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         String greeting =
                 """
                         ____________________________________________________________
@@ -20,14 +22,35 @@ public class Alex {
             if (response.equals("bye")) {
                 break;
             } else if (response.equals("list")) {
-                System.out.println(line);
+                System.out.println(line + "\nHere are the tasks in your list: ");
                 for (int i = 1; i <= list.size(); i++) {
-                    System.out.println(i + ". " + list.get(i - 1));
+                    System.out.println(list.get(i - 1));
                 }
                 System.out.println(line);
-            } else {
-                list.add(response);
-                System.out.println(line + "\nadded: " + response + "\n" + line);
+           } else {
+                String[] arrOfStr = response.split(" ");
+
+                if ((arrOfStr[0].equals("mark") || arrOfStr[0].equals("unmark")
+                ) && arrOfStr.length == 2) {
+                    try {
+                        int taskNumber = Integer.valueOf(arrOfStr[1]);
+
+                        Task task = list.get(taskNumber - 1);
+                        if (arrOfStr[0].equals("mark")) {
+                            task.markAsDone();
+                            System.out.println(line + "\nNice! I've marked this task as done: \n" + task.stringForMark() + "\n" + line);
+                        } else {
+                            task.markAsUndone();
+                            System.out.println(line + "\nOK, I've marked this task as not done yet: \n" + task.stringForMark() + "\n" + line);
+                        }
+                    } catch (NumberFormatException e) {
+                        list.add(new Task(list.size() + 1, response, false));
+                        System.out.println(line + "\nadded: " + response + "\n" + line);
+                    }
+                } else {
+                    list.add(new Task(list.size() + 1, response, false));
+                    System.out.println(line + "\nadded: " + response + "\n" + line);
+                }
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+public class Task {
+    private String taskName;
+
+    private int taskNumber;
+
+    private boolean taskCompletionStatus;
+    public Task(int taskNumber, String taskName, boolean taskCompletionStatus) {
+        this.taskNumber = taskNumber;
+        this.taskName = taskName;
+        this.taskCompletionStatus = taskCompletionStatus;
+    }
+
+    public void markAsDone() {
+        this.taskCompletionStatus = true;
+    }
+
+    public void markAsUndone() {
+        this.taskCompletionStatus = false;
+    }
+    @Override
+    public String toString() {
+        String box = "";
+        if (this.taskCompletionStatus) {
+            box = "[X]";
+        } else {
+            box = "[ ]";
+        }
+
+        return String.format("%d.%s %s", this.taskNumber, box, this.taskName);
+    }
+
+    public String stringForMark() {
+        String box = "";
+        if (this.taskCompletionStatus) {
+            box = "[X]";
+        } else {
+            box = "[ ]";
+        }
+        return String.format("%s %s", box, this.taskName);
+    }
+}
