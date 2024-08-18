f5ef710c5546e4e9ab915897131dec5a4d6540a3
CYX22222003 <e1155533@u.nus.edu>
2024-08-18 18:37:00 +0800

    First implementation of echo

--- src/main/java/DukeKorolev.java
+++ src/main/java/DukeKorolev.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class DukeKorolev {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -6,9 +7,17 @@ public class DukeKorolev {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
         String newLogo = "Hello! I'm DukeKorolev\n"
-                + "What can I do for you?\n\n"
-                + "Bye. Hope to see you again soon\n";
+                + "What can I do for you?\n\n";
+        String input = "";
+        String end = "Bye. Hope to see you again soon\n";
+
         System.out.println(newLogo);
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+            System.out.println(input);
+        }
+        System.out.println(end);
     }
 }
