0c79a9215dde9466a60d26e504e3521d2088ada8
tzuennn <tzuen2406@gmail.com>
2024-08-19 00:51:20 +0800

    Add bot echo feature

--- src/main/java/Nugget.java
+++ src/main/java/Nugget.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Nugget {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("________________________________________");
         System.out.println("Hello! I'm Nugget");
         System.out.println("What can I do for you?");
         System.out.println("________________________________________");
+        while (true) {
+            String text = scanner.next();
+            if (text.equals("bye")) {
+                System.out.println("________________________________________");
+                break;
+            }
+            System.out.println("________________________________________");
+            System.out.println(text);
+            System.out.println("________________________________________");
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("________________________________________");
     }
