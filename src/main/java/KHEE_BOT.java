a0515077bb991af308fd09294129c8e2e696e011
Yong Khee Hou <yongkheehou@gmail.com>
2024-08-18 15:05:19 +0800

    complete level 2: add + list tasks as strings

--- src/main/java/KHEE_BOT.java
+++ src/main/java/KHEE_BOT.java
@@ -5,24 +5,33 @@ public class KHEE_BOT {
     public static final String SPACING = "    ";
     public static final String BORDER = "____________________________________________________________\n";
     public static final String GREETING =
-            KHEE_BOT.SPACING + KHEE_BOT.BORDER +
-            KHEE_BOT.SPACING + " Hello! I'm " + KHEE_BOT.BOT_NAME + ", a talking keyboard!" + "\n" +
-            KHEE_BOT.SPACING + " What can I do for you?\n";
+        KHEE_BOT.SPACING + KHEE_BOT.BORDER +
+        KHEE_BOT.SPACING + " Hello! I'm " + KHEE_BOT.BOT_NAME + ", a talking keyboard!" + "\n" +
+        KHEE_BOT.SPACING + " What can I do for you?\n";
     public static final String GOODBYE =
-            KHEE_BOT.SPACING + KHEE_BOT.BORDER +
-            KHEE_BOT.SPACING + " Bye. Hope to see you again soon!\n" +
-            KHEE_BOT.SPACING + KHEE_BOT.BORDER;
+        KHEE_BOT.SPACING + KHEE_BOT.BORDER +
+        KHEE_BOT.SPACING + " Bye. Hope to see you again soon!\n" +
+        KHEE_BOT.SPACING + KHEE_BOT.BORDER;
 
-    public static String formatOutput(String output) {
+    public static String formatTaskListing(int i, String task) {
+        return KHEE_BOT.SPACING + " " + i + ". " + task + "\n";
+    }
+
+    public static String formatTaskAddition(String input) {
         return (
-                KHEE_BOT.SPACING + KHEE_BOT.BORDER +
-                KHEE_BOT.SPACING + " " + output + "\n" +
-                KHEE_BOT.SPACING + KHEE_BOT.BORDER
+            KHEE_BOT.SPACING + KHEE_BOT.BORDER +
+            KHEE_BOT.SPACING + " " + "added: " +  input + "\n" +
+            KHEE_BOT.SPACING + KHEE_BOT.BORDER
         );
     }
 
+    public String[] tasks;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        KHEE_BOT kheeBot = new KHEE_BOT();
+        kheeBot.tasks = new String[100];
+        int taskCounter = 0;
 
         System.out.println(KHEE_BOT.GREETING);
 
@@ -31,9 +40,18 @@ public class KHEE_BOT {
 
             if (userInput.equals("bye")) {
                 break;
-            }
+            } else if (userInput.equals("list")) {
+                System.out.print(KHEE_BOT.SPACING + KHEE_BOT.BORDER);
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.print(KHEE_BOT.formatTaskListing(i + 1, kheeBot.tasks[i]));
+                }
+                System.out.print(KHEE_BOT.SPACING + KHEE_BOT.BORDER);
+            } else {
+                kheeBot.tasks[taskCounter++] = userInput;
 
-            System.out.println(KHEE_BOT.formatOutput(userInput));
+                String formattedTask = KHEE_BOT.formatTaskAddition(userInput);
+                System.out.println(formattedTask);
+            }
         }
 
         System.out.println(KHEE_BOT.GOODBYE);
