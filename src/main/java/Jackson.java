158101b023f18e2f66b908971a95c86210c8551a
KrashKart <e1113755@u.nus.edu>
2024-08-18 17:10:01 +0800

    added conversation loop

--- src/main/java/Jackson.java
+++ src/main/java/Jackson.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Jackson {
     public static void main(String[] args) {
         String name = "Jackson";
+        String response = "";
+        Scanner sc = new Scanner(System.in);
 
-        System.out.printf("Hello! My name is %s!\nWhat can I do for you today?", name);
+        System.out.printf("Hello! My name is %s!\nWhat can I do for you today?\n> ", name);
+        response = sc.nextLine();
+
+        while (!response.equals("bye")) {
+            System.out.println(response);
+            System.out.print("> ");
+            response = sc.nextLine();
+        }
+
         System.out.println("Goodbye! See you soon!");
     }
 }
