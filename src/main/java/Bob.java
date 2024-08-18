69a01ae07a0bddafef336e97a11d5d3290d55305
engyuhan <e1136866@u.nus.edu>
2024-08-18 23:31:00 +0800

    feat: add echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,11 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
-        String msg = "____________________________________________________________\n"
-                + " Hello! I'm Bob\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(msg);
+        String greeting = """
+                ____________________________________________________________
+                 Hello! I'm Bob
+                 What can I do for you?
+                ____________________________________________________________
+                """;
+        System.out.println(greeting);
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String input = in.nextLine();
+            if (!Objects.equals(input, "bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + " " + input + "\n"
+                        + "____________________________________________________________\n"
+                );
+            } else {
+                String exit = """
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+                System.out.println(exit);
+                System.exit(0);
+            }
+        }
     }
 }
