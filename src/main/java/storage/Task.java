f3d631346c0e0ecb9a1192b9731c37ef3990d799
jeanyang04 <jeanyang.chen@gmail.com>
2024-08-18 13:35:31 +0800

    Add task class

---
+++ src/main/java/storage/Task.java
@@ -0,0 +1,28 @@
+package storage;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // Mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
