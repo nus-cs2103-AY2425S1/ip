3111dcf05f4a59799284d2cc7f258bc4beff2f0e
DennieDan <e1129903@u.nus.edu>
2024-08-18 17:51:53 +0800

    refactor: rename file from Duke to Tecna

---
+++ src/main/java/Tecna.java
@@ -0,0 +1,22 @@
+public class Tecna {
+    public static void main(String[] args) {
+        String logo = " **          **\n" +
+                      "*  *        *  *\n" +
+                      "*   *      *   *\n" +
+                      "*    *    *    *\n" +
+                      "*     *  *     *\n" +
+                      " *     **     *\n" +
+                      "  *    **    *\n" +
+                      "   *   **   *\n" +
+                      "    *  **  *\n" +
+                      "     ******\n" +
+                      "      ****\n" +
+                      "     * ** *\n" +
+                      "    *  **  *\n" +
+                      "    ***  ***\n";
+        System.out.println(logo);
+        System.out.println("I'm Tecna!\nHow can I help you?");
+        System.out.println("----------------------------------------------");
+        System.out.println("Pleased to help you! See you again ^_^");
+    }
+}
