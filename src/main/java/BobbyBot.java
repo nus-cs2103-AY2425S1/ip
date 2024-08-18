9044a197f79c4d476b1af53d51a9e56a44bfc168
Zen Koh <zenkoh193@gmail.com>
2024-08-18 15:37:38 +0800

    Change BotName to BobbyBot and change output

---
+++ src/main/java/BobbyBot.java
@@ -0,0 +1,15 @@
+public class BobbyBot {
+    public static void main(String[] args) {
+        String chatBotName = "BobbyBot";
+        printLine();
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+}
