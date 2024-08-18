62a6d1d18c3f558d4905f4a1f6864fd3e9fb1845
appleshill <eliot@u.nus.edu>
2024-08-18 16:26:44 +0800

    A-TextUiTesting

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -13,6 +13,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.date + ")";
+        return "[D] " + super.toString() + " (by: " + this.date + ")";
     }
 }
