de74fb890421c0abedcf9962ff2ecba0a19c32c7
KrashKart <e1113755@u.nus.edu>
2024-08-18 15:37:58 +0800

    changed Duke.java to Jackson.java changed greeting

---
+++ src/main/java/Jackson.java
@@ -0,0 +1,8 @@
+public class Jackson {
+    public static void main(String[] args) {
+        String name = "Jackson";
+
+        System.out.printf("Hello! My name is %s!\nWhat can I do for you today?", name);
+        System.out.println("Goodbye! See you soon!");
+    }
+}
