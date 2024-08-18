0cc61bd3d63352db4c0b193526ea4bd258c1d215
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-08-18 21:01:24 +0800

    Automated Text UI testing
    Test using the I/O redirection technique

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-
 # create bin directory if it doesn't exist
 if [ ! -d "../bin" ]
 then
     mkdir ../bin
 fi
-
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
 then
     rm ACTUAL.TXT
 fi
-
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
 then
@@ -20,6 +17,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input.txt > ACTUAL.TXT
 java -classpath ../bin Ollie < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
