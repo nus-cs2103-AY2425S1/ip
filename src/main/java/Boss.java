4ec68f8393153de5acdc2baa846033d829c8b9bf
leowolf275 <leo27wolf@gmail.com>
2024-08-18 11:04:03 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Boss.java
@@ -0,0 +1,7 @@
+public class Boss {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm the boss.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
