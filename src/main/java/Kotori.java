abb2932e6d5b94c10623a2841adaadd191a18538
Jianwen <jianwenlei2003@gmail.com>
2024-08-18 12:42:29 +0800

    Level 0 is done, chatbot is renamed  to Kotori

--- src/main/java/Kotori.java
+++ src/main/java/Kotori.java
@@ -1,10 +1,21 @@
-public class Duke {
+public class Kotori {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLine();
+        printGreeting();
+        printLine();
+        printExit();
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("___________________________________________");
+    }
+
+    public static void printGreeting() {
+        System.out.println("Hello! I'm Kotori.\n What can I do for you?");
+    }
+
+    public static void printExit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
