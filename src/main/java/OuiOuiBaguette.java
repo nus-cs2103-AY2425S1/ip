b9118f499f147d736972a48e631cebd1cd506648
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-08-18 16:22:08 +0800

    Add error-handling to chatbot

--- src/main/java/OuiOuiBaguette.java
+++ src/main/java/OuiOuiBaguette.java
@@ -24,96 +24,159 @@ public class OuiOuiBaguette {
             // Read input
             String cmd = sc.nextLine();
 
-            // "bye" command exits loop
-            if (cmd.equals("bye")) {
-                break;
-            }
-
-            if (cmd.equals("list")) {
-                // List tasks
-                // Convert ArrayList to array of strings
-                String[] tasksArr = new String[tasks.size()];
-
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task currTask = tasks.get(i);
-                    tasksArr[i] = (i + 1) + ". " + currTask;
+            try {
+                // "bye" command exits loop
+                if (cmd.equals("bye")) {
+                    break;
                 }
 
-                System.out.println(formatBotSpeech(tasksArr));
+                if (cmd.equals("list")) {
+                    // List tasks
+                    // Convert ArrayList to array of strings
+                    String[] tasksArr = new String[tasks.size()];
 
-            } else if (cmd.startsWith("mark")) {
-                // Parse command to get index of task
-                int taskIndex = Integer.parseInt(cmd.split(" ")[1]) - 1;
-                
-                Task taskMarked = tasks.get(taskIndex);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task currTask = tasks.get(i);
+                        tasksArr[i] = (i + 1) + ". " + currTask;
+                    }
 
-                taskMarked.mark();
+                    System.out.println(formatBotSpeech(tasksArr));
 
-                System.out.println(formatBotSpeech(new String[]{
-                        "Nice! I've marked this task as done: ",
-                        "  " + taskMarked}));
+                } else if (cmd.startsWith("mark")) {
+                    // Parse command to get index of task
+                    int taskIndex = Integer.parseInt(cmd.split(" ")[1]) - 1;
+                    
+                    Task taskMarked = tasks.get(taskIndex);
 
-            } else if (cmd.startsWith("unmark")) {
-                // Parse command to get index of task
-                int taskIndex = Integer.parseInt(cmd.split(" ")[1]) - 1;
-                
-                Task taskUnmarked = tasks.get(taskIndex);
+                    taskMarked.mark();
 
-                taskUnmarked.unmark();
+                    System.out.println(formatBotSpeech(new String[]{
+                            "Nice! I've marked this task as done: ",
+                            "  " + taskMarked}));
 
-                System.out.println(formatBotSpeech(new String[]{
-                        "OK, I've marked this task as not done yet: ",
-                        "  " + taskUnmarked}));
+                } else if (cmd.startsWith("unmark")) {
+                    // Parse command to get index of task
+                    int taskIndex = Integer.parseInt(cmd.split(" ")[1]) - 1;
+                    
+                    Task taskUnmarked = tasks.get(taskIndex);
 
-            } else if (cmd.startsWith("todo")) {
-                // Add ToDo
-                String desc = cmd.substring(("todo ").length());
+                    taskUnmarked.unmark();
 
-                ToDo todo = new ToDo(desc);
+                    System.out.println(formatBotSpeech(new String[]{
+                            "OK, I've marked this task as not done yet: ",
+                            "  " + taskUnmarked}));
 
-                tasks.add(todo);
+                } else if (cmd.startsWith("todo")) {
+                    // Add ToDo
+                    if (cmd.length() <= ("todo ").length()) {
+                        throw new ToDoException("The description of a todo cannot be empty.");
+                    }
 
-                System.out.println(formatBotSpeech(new String[]{
-                    "Got it. I've added this task:",
-                    "  " + todo,
-                    "Now you have " + tasks.size() + " tasks in the list."
-                }));
-            
-            } else if (cmd.startsWith("deadline")) {
-                // Add Deadline
-                String descAndDate = cmd.substring(("deadline ").length());
-                String desc = descAndDate.split(" /by ")[0];
-                String date = descAndDate.split(" /by ")[1];
+                    String desc = cmd.substring(("todo ").length());
 
-                Deadline deadline = new Deadline(desc, date);
+                    ToDo todo = new ToDo(desc);
 
-                tasks.add(deadline);
+                    tasks.add(todo);
 
-                System.out.println(formatBotSpeech(new String[]{
-                    "Got it. I've added this task:",
-                    "  " + deadline,
-                    "Now you have " + tasks.size() + " tasks in the list."
-                }));
+                    System.out.println(formatBotSpeech(new String[]{
+                        "Got it. I've added this task:",
+                        "  " + todo,
+                        "Now you have " + tasks.size() + " tasks in the list."
+                    }));
 
-            } else if (cmd.startsWith("event")) {
-                // Add Event
-                String descAndStartEnd = cmd.substring(("event ").length());
-                String desc = descAndStartEnd.split(" /from ")[0];
-                String start = descAndStartEnd.substring(
-                        descAndStartEnd.indexOf("/from ") + ("/from ").length(),
-                        descAndStartEnd.indexOf(" /to"));
-                String end = descAndStartEnd.split(" /to ")[1];
+                } else if (cmd.startsWith("deadline")) {
+                    // Add Deadline
+                    if (cmd.length() <= ("deadline ").length()) {
+                        throw new DeadlineException("The description of a deadline cannot be empty.");
+                    }
 
-                Event event = new Event(desc, start, end);
+                    String descAndDate = cmd.substring(("deadline ").length());
 
-                tasks.add(event);
+                    // Check if format is correct
+                    if (!descAndDate.contains(" /by ")) {
+                        throw new DeadlineException("""
+                            The format entered is wrong.
+                            \t Please follow the format: deadline <description> /by <due date>""");
+                    }
 
-                System.out.println(formatBotSpeech(new String[]{
-                    "Got it. I've added this task:",
-                    "  " + event,
-                    "Now you have " + tasks.size() + " tasks in the list."
-                }));
+                    String desc = descAndDate.split(" /by ")[0];
+                    // Check if there is a valid description
+                    if (desc.length() == 0) {
+                        throw new DeadlineException("The description of a deadline cannot be empty.");
+                    } 
+
+                    // Check if there is a valid due date
+                    if (descAndDate.split(" /by ").length < 2) {
+                        throw new DeadlineException("The due date of a deadline cannot be empty.");
+                    } 
+                    String date = descAndDate.split(" /by ")[1];
+
+                    Deadline deadline = new Deadline(desc, date);
+
+                    tasks.add(deadline);
+
+                    System.out.println(formatBotSpeech(new String[]{
+                        "Got it. I've added this task:",
+                        "  " + deadline,
+                        "Now you have " + tasks.size() + " tasks in the list."
+                    }));
+
+                } else if (cmd.startsWith("event")) {
+                    // Add Event
+                    if (cmd.length() <= ("event ").length()) {
+                        throw new EventException("The description of a deadline cannot be empty.");
+                    }
+
+                    String descAndStartEnd = cmd.substring(("event ").length());
+                    
+                    // Check if format is correct
+                    if (!descAndStartEnd.contains(" /from ") || !descAndStartEnd.contains(" /to ")) {
+                        throw new EventException("""
+                            The format entered is wrong.
+                            \t Please follow the format: event <description> /from <start> /to <end>""");
+                    }
+
+                    String desc = descAndStartEnd.split(" /from ")[0];
+                    // Check if there is a valid description
+                    if (desc.length() == 0) {
+                        throw new EventException("The description of an event cannot be empty.");
+                    } 
+
+                    // Check if there is a valid start
+                    if (descAndStartEnd.indexOf("/from ") + ("/from ").length()
+                            >= descAndStartEnd.indexOf(" /to")) {
+                        throw new EventException("The start of an event cannot be empty.");
+                            }
+                    String start = descAndStartEnd.substring(
+                            descAndStartEnd.indexOf("/from ") + ("/from ").length(),
+                            descAndStartEnd.indexOf(" /to"));
+
+                    // Check if there is a valid end
+                    if (descAndStartEnd.split(" /to ").length < 2) {
+                        throw new EventException("The end of an event cannot be empty.");
+                    }
+                    String end = descAndStartEnd.split(" /to ")[1];
+
+                    Event event = new Event(desc, start, end);
+
+                    tasks.add(event);
+
+                    System.out.println(formatBotSpeech(new String[]{
+                        "Got it. I've added this task:",
+                        "  " + event,
+                        "Now you have " + tasks.size() + " tasks in the list."
+                    }));
+
+                } else {
+                    // Unknown command
+                    throw new UnknownCommandException();
+                }
+
+            } catch (OuiOuiBaguetteException e) {
+                System.out.println(formatBotSpeech(e.getMessage()));
             }
+
+            
         }
 
         System.out.println(farewell);
