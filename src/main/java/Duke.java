bf675997265a434d8295166a1204979a9d404e26
jolwnn <zhuolinwee@gmail.com>
2024-08-18 11:42:34 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String greeting = "   *        *        *        __o    *       *\n"
@@ -15,6 +16,20 @@ public class Duke {
                         "˚ ˛ •˛• ˚ ｜ 田田 ｜門｜ ˚\n" +
                         "And a happy new year!";
         System.out.println(greeting + "Hello, I am Rudolf, Santa's trusty red-nose reindeer");
-        System.out.println(festiveMessage);
+        System.out.println("Christmas is nearing, what can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        } while (!input.equals("bye"));
+
+        System.out.println("Bye~ Hope to see you again soon! \n" + festiveMessage);
+        System.out.println("____________________________________________________________");
     }
 }
