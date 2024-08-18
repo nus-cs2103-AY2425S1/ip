3dcb3f86dd88c29c35414a70e3f2c7b7aa335a2b
benjiBase <blim0e89@gmail.com>
2024-08-18 23:17:57 +0800

    Add Level-4 features

--- src/main/java/Nameless.java
+++ src/main/java/Nameless.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Nameless {
@@ -6,13 +8,20 @@ public class Nameless {
     private static final String greetings = "Hello, I'm " + name + "\n" + "What can I do for you?";
     private static final String goodbye = "Bye. Hope to see you again!";
     private static final Task[] tasks = new Task[100];
-    private static int splitWord(String input){
-        String words[] = input.split(" ");
+    private static int splitGetNum(String input){
+        String[] words = input.split(" ");
         return Integer.parseInt(words[1]) - 1;
+    }
+
+    private static String splitGetWords(String input){
+        String[] words = input.split(" ", 2);
+        return words[1];
     }
     public static void main(String[] args) {
         System.out.println(line + "\n" + greetings + "\n" + line);
         int counter = 0;
+
+        String temp = "";
         while(true){
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
@@ -22,22 +31,43 @@ public class Nameless {
                 //list tasking
                 System.out.println(line + "\n Here are the tasks in your list:");
                 for(int i = 0; i < counter; i++){
-                    System.out.println(tasks[i].getDescription(i));
+                    //print out all tasking
+                    System.out.println("    " + (i + 1) + "." + tasks[i].toString());
                 }
                 System.out.println(line);
             } else if (input.matches("mark \\d+")) {
                 System.out.println(line);
-                tasks[splitWord(input)].markTask();
+                tasks[splitGetNum(input)].markTask();
                 System.out.println(line);
             } else if (input.matches("unmark \\d+")) {
                 System.out.println(line);
-                tasks[splitWord(input)].unMarkTask();
+                tasks[splitGetNum(input)].unMarkTask();
                 System.out.println(line);
-            } else {
-                //store tasking
-                tasks[counter] = new Task(input);
-                System.out.println(line + "\n" + tasks[counter].toString() + "\n" + line);
+            } else if(input.matches("deadline .+")) {
+                temp = splitGetWords(input);
+                String[] words = temp.split(" /by ");
+                tasks[counter] = new Deadline(words[0], words[1]);
                 counter++;
+                System.out.println(line + "\n" + "Got it. I've added this task:" +
+                        "\n     " + tasks[counter - 1].toString() + "\n" +
+                        "Now you have " + counter + " task left \n" + line);
+            } else if(input.matches("event .+")) {
+                temp = splitGetWords(input);
+                String[] words = temp.split(" /from | /to ");
+                tasks[counter] = new Event(words[0], words[1], words[2]);
+                counter++;
+                System.out.println(line + "\n" + "Got it. I've added this task:" +
+                        "\n     " + tasks[counter - 1].toString() + "\n" +
+                        "Now you have " + counter + " task left \n" + line);
+            }
+            else {
+                //store tasking
+                tasks[counter] = new Todo(splitGetWords(input));
+                counter++;
+                System.out.println(line + "\n" + "Got it. I've added this task:" +
+                        "\n     " + tasks[counter - 1].toString() + "\n" +
+                        "Now you have " + counter + " task left \n" + line);
+
             }
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,30 +11,23 @@ public class Task {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    public void markAsDone() {
-        this.isDone = true;
-    }
-
-    public void unmarkAsDone() {
-        this.isDone = false;
-    }
-
-    public String getDescription(int i) {
-        return (i + 1) + ".[" + getStatusIcon() + "] " + description;
-    }
+//    public String getDescription(int i) {
+//        return (i + 1) + ".[" + getStatusIcon() + "] " + description;
+//    }
 
     public void unMarkTask() {
         this.isDone = false;
-        System.out.println("OK, I've marked this task as not done yet: \n" +
+        System.out.println("OK, I've unmarked this task as not done yet: \n" +
                 "   [" + this.getStatusIcon() + "] " + this.description);
     }
     public void markTask() {
         this.isDone = true;
-        System.out.println("Nice! I've unmarked this task as done: \n" +
+        System.out.println("Nice! I've marked this task as done: \n" +
                 "   [" + this.getStatusIcon() + "] " + this.description);
     }
     @Override
     public String toString() {
-        return "added: " + description;
+        return "[" + getStatusIcon() + "] " + description;
     }
+
 }
