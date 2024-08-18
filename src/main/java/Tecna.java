2b189a163449829e534bf3211701f3f2549c99a4
DennieDan <e1129903@u.nus.edu>
2024-08-18 21:05:14 +0800

    build: echo method

--- src/main/java/Tecna.java
+++ src/main/java/Tecna.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Tecna {
+    public void echo() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.equalsIgnoreCase("bye")) {
+            System.out.println("----------------------------------------------");
+            System.out.println("Pleased to help you! See you again ^_^");
+            System.out.println("----------------------------------------------");
+        } else {
+            System.out.println("----------------------------------------------");
+            System.out.println(input);
+            System.out.println("----------------------------------------------");
+            this.echo();
+        }
+    }
     public static void main(String[] args) {
         String logo = " **          **\n" +
                       "*  *        *  *\n" +
@@ -17,6 +33,7 @@ public class Tecna {
         System.out.println(logo);
         System.out.println("I'm Tecna!\nHow can I help you?");
         System.out.println("----------------------------------------------");
-        System.out.println("Pleased to help you! See you again ^_^");
+        Tecna tecna = new Tecna();
+        tecna.echo();
     }
 }
