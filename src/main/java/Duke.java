a3e53a44d4056e4337efb9b8e0c47459410fd04d
jolwnn <zhuolinwee@gmail.com>
2024-08-18 11:36:00 +0800

    Modify greet and Add exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "   *        *        *        __o    *       *\n"
+                + "*      *       *        *    /_| _     *\n"
+                + "   K  *     K      *        O'_)/ \\  *    *\n"
+                + "  <')____  <')____    __*   V   \\  ) __  *\n"
+                + "   \\ ___ )--\\ ___ )--( (    (___|__)/ /*     *\n"
+                + " *  |   |    |   |  * \\ \\____| |___/ /  *\n"
+                + "    |*  |    |   |     \\____________/       *\n";
+        String festiveMessage =
+                "Merry★* 。 • ˚ ˚ ˛ ˚ ˛ •\n" +
+                        "•。★Christmas★ 。* 。\n" +
+                        "° 。 ° ˛˚˛ * Π__。˚\n" +
+                        "˚ ˛ •˛•˚ */__/~＼。˚ ˚ ˛\n" +
+                        "˚ ˛ •˛• ˚ ｜ 田田 ｜門｜ ˚\n" +
+                        "And a happy new year!";
+        System.out.println(greeting + "Hello, I am Rudolf, Santa's trusty red-nose reindeer");
+        System.out.println(festiveMessage);
     }
 }
