2c5e36b9d053d99af7af970d05cb1393b23d0b48
calebyyy <caleb.ng.yy@gmail.com>
2024-08-18 16:12:02 +0800

    Add echo feature

--- src/main/java/Calebyyy.java
+++ src/main/java/Calebyyy.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Calebyyy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Calebyyy");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+        scanner.close();
     }
 }
