b79823ce466ac02d7dcdc37829b474b52a1a95ab
BuffWuff1712 <lowbingheng14@gmail.com>
2024-08-18 11:15:43 +0800

    Level-0

---
+++ src/main/java/BingBong.java
@@ -0,0 +1,10 @@
+public class BingBong {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________\n";
+        String greeting = "Hello! I'm BingBong\n" +
+                "What can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+        System.out.println(horizontalLine + greeting +
+                horizontalLine + goodbye + horizontalLine);
+    }
+}
