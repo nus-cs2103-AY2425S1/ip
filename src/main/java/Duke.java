4a9aba205c5da2aa0cb87f1c0ae0a38de9d6d47f
jolwnn <zhuolinwee@gmail.com>
2024-08-18 11:54:49 +0800

    Add feature to add, list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duke {
     public static void main(String[] args) {
         String greeting = "   *        *        *        __o    *       *\n"
@@ -16,18 +19,32 @@ public class Duke {
                         "˚ ˛ •˛• ˚ ｜ 田田 ｜門｜ ˚\n" +
                         "And a happy new year!";
         System.out.println(greeting + "Hello, I am Rudolf, Santa's trusty red-nose reindeer");
-        System.out.println("Christmas is nearing, what can I do for you?");
+        System.out.println("Christmas is nearing, and I am here to help you with your christmas preparations. Tell me the tasks that you need to complete.");
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        String input;
+        String input = "";
+        List<String> entries = new ArrayList<>();
 
-        do {
+        while (!input.equals("bye")) {
             input = scanner.nextLine();
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                if (entries.isEmpty()) {
+                    System.out.println("No entries to display.");
+                } else {
+                    for (int i = 0; i < entries.size(); i++) {
+                        System.out.println((i + 1) + ". " + entries.get(i));
+                    }
+                }
+            } else {
+                entries.add(input);
+                System.out.println("added: " + input);
+            }
+
             System.out.println("____________________________________________________________");
-        } while (!input.equals("bye"));
+        }
 
         System.out.println("Bye~ Hope to see you again soon! \n" + festiveMessage);
         System.out.println("____________________________________________________________");
