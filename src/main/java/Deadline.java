fc4f48dcf76c43abd55e234c9bc27f53e5b4195d
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-08-19 01:44:54 +0800

    Added Deadline and Events Classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task {
+    private String date;
+
+    public Deadline(String name, String date) {
+        super(name);
+        this.date = date;
+    }
+
+    public String getDeadline() {
+        return this.date;
+    }
+}
---
+++ src/main/java/Events.java
@@ -0,0 +1,19 @@
+public class Events extends Task {
+
+    private String startDate;
+    private String endDate;
+
+    public Events(String name, String startDate, String endDate) {
+        super(name);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getstartDate() {
+        return this.startDate;
+    }
+
+    public String getendDate() {
+        return this.endDate;
+    }
+}
