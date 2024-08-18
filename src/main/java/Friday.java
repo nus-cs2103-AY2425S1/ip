42991dfaa23e27ee3b2dbccfbcda010b91590615
Edwin1022 <wongedwin93@gmail.com>
2024-08-18 16:39:27 +0800

    Add support for tracking ToDos, Deadlines and Events

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -40,13 +40,15 @@ public class Friday {
         );
     }
 
-    public void addTask(String description) {
+    public void addTask(Task task) {
         if (taskCount < tasks.length) {
-            tasks[taskCount] = new Task(description);
+            tasks[taskCount] = task;
             taskCount++;
             System.out.println(
                     "    ____________________________________________________________\n" +
-                            "     added: " + description + "\n" +
+                            "     Got it. I've added this task:\n" +
+                            "       " + task + "\n" +
+                            "     Now you have " + taskCount + " tasks in the list.\n" +
                             "    ____________________________________________________________"
             );
         } else {
@@ -120,8 +122,18 @@ public class Friday {
             } else if (userInput.startsWith("unmark ")) {
                 int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                 unmarkTaskAsDone(taskIndex);
+            } else if (userInput.startsWith("todo ")) {
+                addTask(new Todo(userInput.substring(5)));
+            } else if (userInput.startsWith("deadline ")) {
+                String[] parts = userInput.substring(9).split(" /by ");
+                addTask(new Deadline(parts[0], parts[1]));
+            } else if (userInput.startsWith("event ")) {
+                String[] parts = userInput.substring(6).split(" /from | /to ");
+                addTask(new Event(parts[0], parts[1], parts[2]));
             } else {
-                addTask(userInput);
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Sorry, I don't understand that command.\n" +
+                        "    ____________________________________________________________");
             }
         }
 
