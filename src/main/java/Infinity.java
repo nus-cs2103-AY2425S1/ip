fe26116b03972fbf56604ce429ef85f68c78bb52
InfinityTwo <2poh.junkang@gmail.com>
2024-08-18 22:25:47 +0800

    Add deletable tasks

--- src/main/java/Infinity.java
+++ src/main/java/Infinity.java
@@ -1,32 +1,70 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Infinity <T extends Task> {
   private static final String BOTNAME = "Infinity";
   private static final String BREAKLINE = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-  private Task[] tasks = new Task[100];
-  private int nextTastIndex = 0;
+  private static final int MAXSIZE = 100;
+
+  private ArrayList<Task> tasks = new ArrayList<>(MAXSIZE);
+  private int nextTaskIndex = 0;
 
   private static String botReply(String input) {
     return String.format("%s: %s", BOTNAME, input);
   }
 
-  private <T extends Task> void addTask(T task) {
-    if (nextTastIndex >= tasks.length) {
-      System.out.println(botReply("I'm sorry, but I can't remember more tasks."));
-      return;
+  private <T extends Task> void addTask(T task) throws InfinityException {
+    if (nextTaskIndex >= MAXSIZE) {
+      throw new InfinityException(botReply("I'm sorry, but I can't remember more tasks."));
     }
-    tasks[nextTastIndex] = (Task) task;
-    nextTastIndex++;
+    tasks.add(task);
+    nextTaskIndex++;
     System.out.println(botReply(String.format("I've added '%s'", task)));
   }
 
+  private void deleteTask(String currentInput) throws InfinityException {
+    int taskIndex = 0;
+    try {
+      taskIndex = Integer.parseInt(currentInput);
+    } catch (NumberFormatException e) {
+      System.out.println(botReply("Hey! That's not a number"));
+      System.out.println(BREAKLINE);
+    }
+    taskIndex--;
+    if (taskIndex >= nextTaskIndex || taskIndex < 0) {
+      throw new InfinityException(botReply("Hmmm, you seem to have chose a task that doesn't exist. Nice try :)"));
+    } else {
+      try {
+        Task removedTask = tasks.remove(taskIndex);
+        System.out.println(botReply(String.format("I've removed task %d:", taskIndex + 1)));
+        System.out.println(removedTask.toString());
+      } catch (IndexOutOfBoundsException e) {
+        throw new InfinityException(botReply("Hmmm, you seem to have chose a task that doesn't exist. Nice try :)"));
+      }
+    }
+  }
+
+  private void markTask(String currentInput) throws InfinityException {
+    String[] words = currentInput.split(" ");
+    int taskIndex = Integer.parseInt(words[1]) - 1;
+    if (taskIndex >= nextTaskIndex || taskIndex < 0) {
+      throw new InfinityException(botReply("Hmmm, I can't find that task. Please try again."));
+    }
+    tasks.get(taskIndex).markAsDone();
+    System.out.println(botReply(String.format("I've marked task %d as done:", taskIndex + 1)));
+    System.out.println(tasks.get(taskIndex).toString());
+    System.out.println(BREAKLINE);
+  }
+
   private void listTasks() {
-    if (nextTastIndex == 0) {
+    if (nextTaskIndex == 0) {
       System.out.println(botReply("Task list is empty :("));
     } else {
       System.out.println(botReply(""));
-      for (int i = 0; i < nextTastIndex; i++) {
-        System.out.println(String.format("    %d. %s", i + 1, tasks[i].toString()));
+      int i = 1;
+      for (Task task : tasks) {
+        System.err.println(String.format("    %d. %s", i, task.toString()));
+        i++;
       }
     }
   }
@@ -47,41 +85,50 @@ public class Infinity <T extends Task> {
       String currentInput = userInputs.nextLine();
       System.out.println(BREAKLINE);
       try {
+
         if (currentInput.equals("bye")) {
+
           System.out.println(botReply("Well, if you are leaving, then I must be infinitely too dumb :("));
           System.out.println(BREAKLINE);
           userInputs.close();
           System.exit(0);
+
         } else if (currentInput.equals("list")) {
+
           this.listTasks();
           System.out.println(BREAKLINE);
-        } else if (currentInput.startsWith("mark")) {
-          try {
-            String[] words = currentInput.split(" ");
-            int taskIndex = Integer.parseInt(words[1]) - 1;
-            if (taskIndex >= nextTastIndex || taskIndex < 0) {
-              throw new IndexOutOfBoundsException();
-            }
-            tasks[taskIndex].markAsDone();
-            System.out.println(botReply(String.format("I've marked task %d as done:", taskIndex + 1)));
-            System.out.println(tasks[taskIndex].toString());
-            System.out.println(BREAKLINE);
-          } catch (IndexOutOfBoundsException e) {
-            System.out.println(botReply("Hmmm, I can't find that task. Please try again."));
-            System.out.println(BREAKLINE);
-          }
-        } else if (currentInput.startsWith("todo")) {
+
+        } else if (currentInput.startsWith("mark") && currentInput.length() > 5) {
+
+          this.markTask(currentInput);
+
+        } else if (currentInput.startsWith("todo") && currentInput.length() > 5) {
+
           this.addTask(new ToDos(currentInput.substring(5)));
-        } else if (currentInput.startsWith("deadline")) {
+
+        } else if (currentInput.startsWith("deadline") && currentInput.length() > 9) {
+
           this.addTask(new Deadline(currentInput.substring(9)));
-        } else if (currentInput.startsWith("event")) {
+
+        } else if (currentInput.startsWith("event") && currentInput.length() > 6) {
+
           this.addTask(new Event(currentInput.substring(6)));
+
+        } else if (currentInput.startsWith("delete") && currentInput.length() > 7) {
+
+          this.deleteTask(currentInput.substring(7));
+
         } else {
+
           this.echo(currentInput);
+
         }
+
       } catch (InfinityException e) {
+
         System.out.println(botReply(e.getMessage()));
         System.out.println(BREAKLINE);
+
       }
     }
   }
