3ec83683b8af600f5b89a1a02bec28bb91b76103
gohsl99 <gohsl@u.nus.edu>
2024-08-18 17:07:09 +0800

    Duke.java: Rename to Blitz.java and implement initial skeletal version README.md, docs/README.md, text-ui-test/runtest.bat: Replace Duke with Blitz

---
+++ src/main/java/Blitz.java
@@ -0,0 +1,10 @@
+public class Blitz {
+    public static void main(String[] args) {
+        String divider = "--------------------------------------\n";
+        String greet = "Hello! I'm Blitz.\n" +
+                "What can I do for you?\n";
+        String end = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(divider + greet + divider + end + divider);
+    }
+}
