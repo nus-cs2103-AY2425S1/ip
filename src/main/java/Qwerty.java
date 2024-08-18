cfd2f73ffca8993fba0e5fd6b4dbac433c87ec99
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-08-18 17:32:39 +0800

    Add ability to mark and unmark

--- src/main/java/Qwerty.java
+++ src/main/java/Qwerty.java
@@ -9,11 +9,11 @@ public class Qwerty {
     /** True if the bot is currently chatting and accepting input */
     private boolean isChatting;
     /** List of tasks entered by the user */
-    private ArrayList<String> tasks;
+    private ArrayList<Task> tasks;
 
     public Qwerty() {
         this.isChatting = true;
-        this.tasks = new ArrayList<String>(100);
+        this.tasks = new ArrayList<Task>();
     }
 
     /**
@@ -38,11 +38,12 @@ public class Qwerty {
     /**
      * Adds a task to the tasklist.
      *
-     * @param task String describing the task.
+     * @param description String describing the task.
      */
-    public void add_task(String task) {
+    public void add_task(String description) {
+        Task task = new Task(description);
         this.tasks.add(task);
-        System.out.println("\nadded: " + task);
+        System.out.println("\nadded: " + description);
     }
 
     /**
@@ -51,10 +52,22 @@ public class Qwerty {
     public void list_tasks() {
         System.out.println();
         int taskNumber = 1;
-        for (String task: tasks) {
-            System.out.println(taskNumber + ". " + task);
+        for (Task task: tasks) {
+            System.out.println(taskNumber + "." + task);
             taskNumber++;
         }
+    }
+
+    public void markTaskAsDone(int index) {
+        Task task = tasks.get(index - 1);
+        task.markAsDone();
+        System.out.println("\nNice! I've marked this task as done:\n" + task);
+    }
+
+    public void markTaskAsNotDone(int index) {
+        Task task = tasks.get(index - 1);
+        task.markAsNotDone();
+        System.out.println("\nOK, I've marked this task as not done yet:\n" + task);
     }
 
     /**
@@ -67,16 +80,36 @@ public class Qwerty {
 
         while (isChatting) {
             System.out.println(); // blank line before user input
-            String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                isChatting = false;
-                say_goodbye();
-            } else if (input.equals("list")) {
-                list_tasks();
-            } else {
-                add_task(input);
-            }
+            String rawInput = scanner.nextLine();
 
+            // Parse user input into command and arguments
+            String[] inputs = rawInput.split(" ", 2);
+            String command = inputs[0];
+            String args;
+
+            switch (command) {
+                case "bye":
+                    isChatting = false;
+                    say_goodbye();
+                    break;
+                case "list":
+                    list_tasks();
+                    break;
+                case "mark":
+                    args = inputs[1];
+                    int indexToMark = Integer.parseInt(args);
+                    markTaskAsDone(indexToMark);
+                    break;
+                case "unmark":
+                    args = inputs[1];
+                    int indexToUnmark = Integer.parseInt(args);
+                    markTaskAsNotDone(indexToUnmark);
+                    break;
+                case "":
+                    break;
+                default:
+                    add_task(rawInput);
+            }
         }
     }
 
