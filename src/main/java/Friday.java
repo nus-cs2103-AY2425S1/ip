f0206cec5c9d560b1d46189ef5aae308258996c5
blanklogic <koh.junxian.jaymeson@icloud.com>
2024-08-18 20:56:13 +0800

    Level 2. Implemented ability to log user input and displaying them back to the user when requested

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -13,10 +13,21 @@ public class Friday {
 
         Scanner sc = new Scanner(System.in);
         String input;
+        String[] items = new String[100];
+        int index = 0;
         while (true) {
             input = sc.nextLine();
             if (Objects.equals(input, "bye")) break;
-            System.out.println(horizontalLine + "\n\t" + input + "\n" + horizontalLine);
+            if (Objects.equals(input, "list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < index; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + items[i]);
+                }
+                System.out.println(horizontalLine);
+                continue;
+            }
+            items[index++] = input;
+            System.out.println(horizontalLine + "\n\tadded: " + input + "\n" + horizontalLine);
         }
         System.out.println(horizontalLine);
         System.out.println("\tGood Bye. Hope to see you again soon!");
