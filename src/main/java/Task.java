664055d03cb3efd922d682a6e7de4b89b21c2b66
DennieDan <e1129903@u.nus.edu>
2024-08-18 21:59:52 +0800

    build: Task class in A-classes extension

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unMarkAsDone() {
+        this.isDone = false;
+    }
+}
