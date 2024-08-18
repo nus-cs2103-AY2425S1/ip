ced375c3282ed3e8463aa4deb60cdf517470908d
Meowloid <tochiex3@gmail.com>
2024-08-18 14:12:53 +0800

    level 2

--- src/main/java/Majima.java
+++ src/main/java/Majima.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Majima {
+    //leave no magic numbers
+    static int MAX_TASKS = 100;
+    static String[] tasks = new String[MAX_TASKS];
+    //counter, used in main
+    static int task_count = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,18 +18,35 @@ public class Majima {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("Bye")) {
+            if (input.equalsIgnoreCase("bye")) {
+                //Exit main, close everything
                 System.out.println(linegap);
                 System.out.println("Bye bye! Don't keep me waiting fer too long now, ya hear?");
                 System.out.println(linegap);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                //Print out all tasks so far
+                System.out.println(linegap);
+                System.out.println("Here's whatcha gotta do, Kiryu-chan!");
+                for (int i = 0; i < task_count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(linegap);
+            } else {
+                //Becomes new task
+                if (task_count < MAX_TASKS) {
+                    tasks[task_count] = input;
+                    task_count++;
+                    System.out.println(linegap);
+                    System.out.println("Understood, Kiryu-chan! Adding that to the list.");
+                    System.out.println(linegap);
+                } else {
+                    System.out.println(linegap);
+                    System.out.println("O-oi, Kiryu-chan! Ya can't expect me to 'member all this crap!");
+                    System.out.println(linegap);
+                }
             }
-
-            System.out.println(linegap);
-            System.out.println(input);
-            System.out.println(linegap);
         }
-
         scanner.close();
     }
 }
