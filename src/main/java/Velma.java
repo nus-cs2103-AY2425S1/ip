941e84b3a3e067afdf4eae11eb3e89a43ddaa265
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-08-19 00:08:35 +0800

    added command enum

--- src/main/java/Velma.java
+++ src/main/java/Velma.java
@@ -10,6 +10,40 @@ public class Velma {
         System.out.print("\n");
     }
 
+    public enum Command {
+        TODO,
+        DEADLINE,
+        EVENT,
+        LIST,
+        MARK,
+        UNMARK,
+        DELETE,
+        BYE,
+        UNKNOWN
+    }
+    public static Command getCommand(String input) {
+        if (input.startsWith("todo")) {
+            return Command.TODO;
+        } else if (input.startsWith("deadline")) {
+            return Command.DEADLINE;
+        } else if (input.startsWith("event")) {
+            return Command.EVENT;
+        } else if (input.equals("list")) {
+            return Command.LIST;
+        } else if (input.startsWith("mark")) {
+            return Command.MARK;
+        } else if (input.startsWith("unmark")) {
+            return Command.UNMARK;
+        } else if (input.startsWith("delete")) {
+            return Command.DELETE;
+        } else if (input.equals("bye")) {
+            return Command.BYE;
+        } else {
+            return Command.UNKNOWN;
+        }
+    }
+
+
     public static void main(String[] args) {
 
         int count = 1;
