98788eb333949076683b8e82570dd1ede57a5592
InfinityTwo <2poh.junkang@gmail.com>
2024-08-18 18:48:50 +0800

    Customise bot to Echo

--- src/main/java/Infinity.java
+++ src/main/java/Infinity.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Infinity {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello, I'm Infinity");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+    public static final String BOTNAME = "Infinity";
+    public static final String BREAKLINE = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+    public static String botReply(String input) {
+        return String.format("%s: %s", BOTNAME, input);
     }
+
+    public static void main(String[] args) {
+        Scanner userInputs = new Scanner(System.in);
+
+        System.out.println(BREAKLINE);
+        System.out.println(botReply(String.format("Hello, I'm a dummy bot called %s", BOTNAME)));
+        System.out.println(botReply("What can I not do for you?"));
+        System.out.println(BREAKLINE);
+
+        while (true) {
+            String currentInput = userInputs.nextLine();
+            System.out.println(BREAKLINE);
+            if (currentInput.equals("bye")) {
+                System.out.println(botReply("Well, if you are leaving, then I must be infinitely too dumb :("));
+                System.out.println(BREAKLINE);
+                userInputs.close();
+                System.exit(0);
+            } else {
+                System.out.println(botReply(currentInput));
+                System.out.println(BREAKLINE);
+            }
+        }
+    }
+
 }
