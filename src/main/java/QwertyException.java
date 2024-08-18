92e0fd58e623472f6fdbe0025a6dd404d5b667e3
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-08-18 23:21:00 +0800

    Add QwertyException.java

---
+++ src/main/java/QwertyException.java
@@ -0,0 +1,9 @@
+/**
+ * This class encapsulates exceptions unique to the Qwerty chatbot.
+ * An example is missing or invalid arguments.
+ */
+public class QwertyException extends Exception {
+    public QwertyException(String message) {
+        super(message);
+    }
+}
