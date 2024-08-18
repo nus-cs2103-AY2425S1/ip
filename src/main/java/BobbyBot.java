d7e5c0ff91e64f7b0895d3e7d6fa13633874ddab
Zen Koh <zenkoh193@gmail.com>
2024-08-18 15:52:08 +0800

    Allow bot to reply to user

--- src/main/java/BobbyBot.java
+++ src/main/java/BobbyBot.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
 public class BobbyBot {
     public static void main(String[] args) {
         String chatBotName = "BobbyBot";
+
+        Scanner myScanner = new Scanner(System.in);
+        printInput("Hello! I'm " + chatBotName, "What can I do for you?");
+        while (true) {
+            String input = myScanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                printInput(input);
+            }
+        }
+
+        printInput("Bye. Hope to see you again soon!");
+    }
+
+    private static void printInput(String ...input) {
+        System.out.print("\t");
         printLine();
-        System.out.println("Hello! I'm " + chatBotName);
-        System.out.println("What can I do for you?");
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        for (String i : input) {
+            System.out.println("\t" + i);
+        }
+        System.out.print("\t");
         printLine();
     }
 
