ad2a0f3c7fbc05d2e93204951d01dd5d11586c2b
ZShunRen <zhengshunren@outlook.com>
2024-08-18 21:57:56 +0800

    Add level 0

---
+++ src/main/java/Ned.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Ned {
+    public static void main(String[] args) {
+        String logo = " ____  _____              __  \n"
+                + "|_   \\|_   _|            |  ] \n"
+                + "  |   \\ | |  .---.   .--.| |  \n"
+                + "  | |\\ \\| | / /__\\\\/ /'`\\' |  \n"
+                + " _| |_\\   |_| \\__.,| \\__/  |  \n"
+                + "|_____|\\____|'.__.' '.__.;__]";
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Hello! I'm\n" + logo + "\n");
+        System.out.println("Lord of Winterfell and Warden Of The North\n");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("I wish you good fortune in the wars to come, m' lord\n");
+        System.out.println("____________________________________________________________\n");
+    }
+}
