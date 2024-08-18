a3eb620309045038dcd9d1d4a9b0c8affeeb17a4
Zen Koh <zenkoh193@gmail.com>
2024-08-18 16:10:41 +0800

    Add ability to store and display stored text entered by user

--- src/main/java/BobbyBot.java
+++ src/main/java/BobbyBot.java
@@ -1,20 +1,40 @@
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class BobbyBot {
     public static void main(String[] args) {
         String chatBotName = "BobbyBot";
 
+        String[] messages = new String[100];
         Scanner myScanner = new Scanner(System.in);
         printInput("Hello! I'm " + chatBotName, "What can I do for you?");
         while (true) {
             String input = myScanner.nextLine();
-            if (input.equals("bye")) {
-                break;
-            } else {
-                printInput(input);
+            switch (input) {
+                case "list":
+                    String[] formattedMessages = IntStream.
+                            range(0, messages.length).
+                            filter(i -> messages[i] != null).
+                            mapToObj(i -> i + 1 + ". " + messages[i]).
+                            toArray(String[]::new);
+                    printInput(formattedMessages);
+                    break;
+                case "bye":
+                    printInput("Bye. Hope to see you again soon!");
+                    return;
+                default:
+                    printInput("added: " + input);
+                    for (int i = 0; i < messages.length; i++) {
+                        if (messages[i] == null) {
+                            messages[i] = input;
+                            break;
+                        }
+                    }
+                    break;
             }
         }
-
-        printInput("Bye. Hope to see you again soon!");
     }
 
     private static void printInput(String ...input) {
