cd8d30d0662653f221a50f57f0cc6b175bafc982
Gilbb <gilbert.iskandar2002@gmail.com>
2024-08-18 10:36:16 +0800

    Add greet and exit texts

---
+++ src/main/java/Cookie.java
@@ -0,0 +1,19 @@
+public class Cookie {
+    public static void main(String[] args) {
+        String logo = "    o      o    \n"
+                + " ____\\____/____\n"
+                + "|   _      _   |\n"
+                + "|  / \\    / \\  |   /\n"
+                + "|  \\_/    \\_/  |  /\n"
+                + "|              | /\n"
+                + "|______________|/\n"
+                + "\n";
+
+        String greet = "Hello! I'm Cookie\n"
+                + "How can I help you?\n"
+                + "\n";
+
+        String quit = "Bye. See you soon!";
+        System.out.println(logo + greet + quit);
+    }
+}
