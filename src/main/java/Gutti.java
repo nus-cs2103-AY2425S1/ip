a22e96d221d3ff22b05069ade32c547cdcc2d8ee
PresleyChew <E1115692@u.nus.edu>
2024-08-19 18:26:17 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Gutti.java
@@ -0,0 +1,13 @@
+public class Gutti {
+    public static void main(String[] args) {
+        // Display the greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Gutti");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
