6eb5bf224de1fad6987b3c4003bdf9adbc36ea64
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-19 15:20:26 +0800

    Added ability to echo commands sent in terminal by a user

--- docs/README.md
+++ docs/README.md
@@ -1,30 +1,5 @@
-# Duke User Guide
+# That One Guy
 
-// Update the title above to match the actual product name
+// Individual project for CS2103T
 
-// Product screenshot goes here
-
-// Product intro goes here
-
-## Adding deadlines
-
-// Describe the action and its outcome.
-
-// Give examples of usage
-
-Example: `keyword (optional arguments)`
-
-// A description of the expected outcome goes here
-
-```
-expected output
-```
-
-## Feature ABC
-
-// Feature details
-
-
-## Feature XYZ
-
-// Feature details
+// Features to be added later
--- src/main/java/ThatOneGuy.java
+++ src/main/java/ThatOneGuy.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class ThatOneGuy {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
+    private static String line = "____________________________________________________________";
+    public static void greet() {
         String name = "That one guy";
         System.out.println(line + "\nHello! I'm " + name);
         System.out.println("What can I do for you?\n" + line);
-        System.out.println("Bye.  Hope to see you again soon!\n"  + line);
+    }
+
+    public static void farewell() {
+        System.out.println(line + "\nBye.  Hope to see you again soon!\n"  + line);
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String next;
+        while (true) {
+            next = sc.nextLine();
+            if (next.equals("bye")) {
+                break;
+            }
+            System.out.println(line + "\n" + next + "\n" + line + "\n");
+        }
+    }
+    public static void main(String[] args) {
+        greet();
+        echo();
+        farewell();
     }
 }
