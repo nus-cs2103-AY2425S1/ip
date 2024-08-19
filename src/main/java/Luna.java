7095006cbc1b189979fcc872a90ff987bbdf3b13
gladwintan <gladwintan@hotmail.sg>
2024-08-19 23:20:35 +0800

    Luna.java: add echo function to output user command
    Let's
    1. add function to output user command and exit when the user types the
    	command "bye"

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
         String greetings = "Hello! I'm Luna\n" +
                 "What can I do for you?";
         System.out.println(greetings);
 
-        String exit = "Bye. Hope to see you again soon!";
-        System.out.println(exit);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                String exit = "Bye! Hope to see you again soon!";
+                System.out.println(exit);
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
+
     }
 }
