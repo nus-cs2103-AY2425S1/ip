fb7ea8eba14ceb0f9873fe3341c7461f4e993e1c
wujiaweijoanna <e1155577@u.nus.edu>
2024-08-19 21:10:14 +0800

    Echo commands

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Alice {
     public static void main(String[] args) {
         String name = "Alice";
@@ -8,7 +10,19 @@ public class Alice {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
+
+        scanner.close();
     }
 }
