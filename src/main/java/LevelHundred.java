be4d070797839ccc87d58d7789040a4717dd9bba
Zac <zacheryngky@gmail.com>
2024-08-20 00:44:21 +0800

    Finish Level 1. Echo
    - Echo commands entered by user
    - Exits when user types command bye

--- src/main/java/LevelHundred.java
+++ src/main/java/LevelHundred.java
@@ -1,4 +1,5 @@
 import java.util.logging.Level;
+import java.util.Scanner;
 
 public class LevelHundred {
     private final String name = "LevelHundred";
@@ -10,19 +11,31 @@ public class LevelHundred {
 
     private void greet() {
         this.ui.printHorizontalLine();
-        System.out.println("Hello, I'm " + this.name);
-        System.out.println("What can I do for you?");
+        System.out.println("\t" + "Hello, I'm " + this.name);
+        System.out.println("\t" + "What can I do for you?");
         this.ui.printHorizontalLine();
     }
 
     private void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
         ui.printHorizontalLine();
+    }
+
+    private void run() {
+        this.greet();
+        Scanner sc = new Scanner(System.in);
+        String END = "bye";
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals(END)) {
+            this.ui.echo(userInput);
+            userInput = sc.nextLine();
+        }
+
+        this.exit();
     }
     public static void main(String[] args) {
         LevelHundred chatbot = new LevelHundred();
-        chatbot.greet();
-        chatbot.exit();
-
+        chatbot.run();
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,8 +1,14 @@
 public class Ui {
     private final int lineLength = 50;
-    private final String horizontalLine = "-".repeat(this.lineLength);
+    private final String horizontalLine = "\t" + "-".repeat(this.lineLength);
 
     public void printHorizontalLine() {
         System.out.println(this.horizontalLine);
+    }
+
+    public void echo(String msg) {
+        this.printHorizontalLine();
+        System.out.println("\t" + msg);
+        this.printHorizontalLine();
     }
 }
