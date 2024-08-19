de3f18c86df69f8685854f3803c17d6a9e23b650
NicT5 <nicctang@gmail.com>
2024-08-19 17:42:12 +0800

    Added echoing of user input

--- src/main/java/YapBot.java
+++ src/main/java/YapBot.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class YapBot {
     public static void main(String[] args) {
-        System.out.println("Hello, I am YapBot. \nHow can I help ? \n---------- \nAlright, enough yapping for one day.");
+        String templateLine = "\n-------------------------------------------\n";
+        System.out.println(templateLine + "Hello, I am YapBot. \nHow can I help?" + templateLine);
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(templateLine + input + templateLine);
+            input = in.nextLine();
+
+        }
+
+        in.close();
+        System.out.println(templateLine + "Alright, enough yapping for one day." + templateLine);
     }
 }
