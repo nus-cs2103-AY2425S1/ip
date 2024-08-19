6cf74aa4354dfc057abfe1c7f06cf495e1277b65
bmanara <bmanaranus@gmail.com>
2024-08-19 14:19:20 +0800

    Rename Duke to Llama

---
+++ src/main/java/Llama.java
@@ -0,0 +1,7 @@
+public class Llama {
+    public static void main(String[] args) {
+        String logo = "";
+        System.out.println("Hello! I'm Llama!\n" + logo);
+        System.out.println("What can I do for you?");
+    }
+}
