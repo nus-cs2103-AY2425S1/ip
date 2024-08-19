a2bd06a9038be96ffa6ecb3b2fa0afba4a794f53
Vatinius <102.23.dylan.chan@gmail.com>
2024-08-19 09:29:32 +0800

    Added greeting and goodbye

---
+++ src/main/java/Evelyn.java
@@ -0,0 +1,12 @@
+public class Evelyn {
+    private static String chatbotName = "Evelyn";
+    private static String horizontalLine = "-----------------------------------------";
+    public static void main(String[] args) {
+        System.out.println(horizontalLine);
+        System.out.println("Hi! I am Evelyn");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+
+    }
+}
