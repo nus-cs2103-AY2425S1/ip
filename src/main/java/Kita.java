bea75544ce857f35b1ff4d496f4d433f27f1f9a8
tkaixiang <tkaixiang@gmail.com>
2024-08-19 16:44:31 +0800

    level_2

--- src/main/java/Kita.java
+++ src/main/java/Kita.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Kita {
     private static void printLine() {
         System.out.println("____________________________________________________________\n");
     }
+    private static ArrayList<String> commandsList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner getInput = new Scanner(System.in);
@@ -14,14 +17,21 @@ public class Kita {
         printLine();
         while (true) {
             String command = getInput.nextLine();
+            printLine();
             if (command.equals("bye")) {
-                printLine();
                 System.out.println(" Bye. Hope to see you again soon!\n");
-                printLine();
                 break;
             }
-            printLine();
-            System.out.println(command);
+            else if (command.equals("list")) {
+                for (int i = 1; i <= commandsList.size(); i++) {
+                    System.out.println(i + ". " + commandsList.get(i-1));
+                }
+            }
+            else {
+                System.out.println("added: " + command);
+                commandsList.add(command);
+            }
+
             printLine();
         }
     }
