b34f1ba350b904b14ad658a29e380e7fe52b3bb0
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-08-19 11:08:09 +0800

    Add list feature to Justbot

--- src/main/java/Justbot.java
+++ src/main/java/Justbot.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Justbot {
     public static void main(String[] args) {
         final String chatbotName = "JustBot";
+        String[] tasks = new String[100];
+        int tasksIndex = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("------------------------------------------");
@@ -10,16 +12,30 @@ public class Justbot {
         System.out.println("What can I do for you?");
         System.out.println("------------------------------------------");
         String input = "";
+
         while (!input.equals("bye")) {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("------------------------------------------");
                 break;
             }
+
+            if (input.equals("list")) {
+                System.out.println("------------------------------------------");
+                for(int i =0; i < tasksIndex; i++) {
+                    int taskListCount = i + 1;
+                    System.out.print(taskListCount + ". " + tasks[i] + "\n");
+                }
+                System.out.println("------------------------------------------");
+                continue;
+            }
             System.out.println("------------------------------------------");
-            System.out.println(input);
+            tasks[tasksIndex] = input;
+            tasksIndex += 1;
+            System.out.println( "added: " + input);
             System.out.println("------------------------------------------");
         }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------------------");
 
