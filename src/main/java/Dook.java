0e2aad2efd74d0a83d86aa7f6da147ff35ef2b46
lrongyi <iygnormal@gmail.com>
2024-08-19 09:41:22 +0800

    Rename, Greet, Exit

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -1,10 +1,11 @@
-public class Duke {
+public class Dook {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String separator = "____________________________________________________________";
+        String greeting = "Hello! I'm Dook\nWhat can I do for you?\n" + separator;
+        String exit = "Bye. Hope to see you again soon!\n" + separator;
+
+        System.out.println(separator);
+        System.out.println(greeting);
+        System.out.print(exit);
     }
 }
