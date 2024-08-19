b0677b2efe094578ff25e69bdef6551248079a47
Karthikraj Sivakumar <techraj0207@gmail.com>
2024-08-19 12:15:29 +0800

    Echo

--- src/main/java/ProYapper.java
+++ src/main/java/ProYapper.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class ProYapper {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greeting = "Hello! I am Pro Yapper!\nWhat can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!\n";
         System.out.println(greeting);
-        System.out.println(goodbye);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("\n" + goodbye);
+                break;
+            } else {
+                System.out.println("\n" + userInput + "\n");
+            }
+        }
+        scanner.close();
     }
 }
+
