1c42e7b3612b8ff874aba98a97d8efe779a2b1a2
yooplo <jjwr2002@gmail.com>
2024-08-19 17:56:26 +0800

    Implement greeting and exit

---
+++ src/main/java/Regina.java
@@ -0,0 +1,14 @@
+public class Regina {
+    public static void main(String[] args) {
+        // name
+        String name = "Regina";
+        // greet
+        System.out.printf("-------------------------------\n" +
+                "Hello! I'm %s \n" +
+                "What can I do for you?", name);
+        // exit
+        System.out.println("\n-------------------------------\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "-------------------------------");
+    }
+}
