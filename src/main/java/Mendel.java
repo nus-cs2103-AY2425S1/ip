bc63404033ca57b69293652b1ea7117d01e0950c
Virusrwj223 <mhrishiraj@gmail.com>
2024-08-19 18:08:41 +0800

    Setup testing

--- src/main/java/Mendel.java
+++ src/main/java/Mendel.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class Mendel {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = "Mendel";
         System.out.println("Hello from\n" + logo);
         String currAction = "hello";
         TaskManager taskManager = new TaskManager();
         new Welcome().speak();
         while (!currAction.equals("bye")) {
-            Scanner sc = new Scanner(System.in);
             currAction = sc.nextLine();
             taskManager.manage(currAction);
-
         }
-
     }
 }
