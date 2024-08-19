91a6428a31653dcc10ab207d17f2169ada7d1776
Jun Long <>
2024-08-19 15:52:21 +0800

    Completed Level 0. Rename, Greet, Exit

---
+++ src/main/java/GPT.java
@@ -0,0 +1,8 @@
+public class GPT {
+    public static void main(String[] args) {
+        String chatbotName = "GPT";
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
