a4ae1197e8214192648f769946c5d455d3457330
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2024-08-19 09:53:04 +0800

    refactor: Abstract greeting and farewell

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,8 +1,15 @@
 public class Joe {
     public static final String horizontalLine = "____________________________________________________________";
     public static final String chatbotName = "Joe";
-    public static void main(String[] args) {
+
+    public static void greet() {
         System.out.printf("%s\nHello! I'm %s \nWhat can I do for you? \n%s\n", horizontalLine, chatbotName, horizontalLine);
+    }
+
+    public static void farewell() {
         System.out.printf("Bye. Hope to see you again soon!\n%s", horizontalLine);
     }
+    public static void main(String[] args) {
+        greet();
+        farewell();
 }
