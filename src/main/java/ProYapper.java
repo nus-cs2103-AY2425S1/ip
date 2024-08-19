b32fcc436b79a591c9e1ea3eb42f5c799340d187
Karthikraj Sivakumar <techraj0207@gmail.com>
2024-08-19 12:00:36 +0800

    Rename, Greet, Exit

---
+++ src/main/java/ProYapper.java
@@ -0,0 +1,8 @@
+public class ProYapper {
+    public static void main(String[] args) {
+        String greeting = "Hello! I am Pro Yapper!\nWhat can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+        System.out.println(greeting);
+        System.out.println(goodbye);
+    }
+}
