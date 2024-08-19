b8f192c5b627dcfd089ba455db36c6eeee908a5c
LAPTOP-20936SS7\kusha <e1121075@u.nus.edu>
2024-08-19 23:55:57 +0800

    Added List functionality

--- src/main/java/Elon.java
+++ src/main/java/Elon.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Elon {
-
-    private static void Echo() {
+    private static void List() {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         String input = scanner.next();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 1; i < list.size() + 1; i++) {
+                    System.out.println(String.format("%d. ", i) + list.get(i - 1));
+                }
+                input = scanner.next();
+                continue;
+            }
+            System.out.println("added: " + input);
             System.out.println("-------------------------------------------------------");
+            list.add(input);
             input = scanner.next();
         }
         scanner.close();
@@ -18,6 +27,6 @@ public class Elon {
         System.out.println(" Hello! I'm Elon");
         System.out.println(" What can I do for you?");
         System.out.println("-------------------------------------------------------");
-        Elon.Echo();
+        Elon.List();
     }
 }
