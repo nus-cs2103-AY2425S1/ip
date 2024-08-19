b7a184dec138a7a1ce102073020a6a0bb394746b
dominopizzaaaa <domksj23@gmail.com>
2024-08-19 22:33:30 +0800

    Added Javadocs

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,20 +1,50 @@
+/**
+ * Represents a task with a description and a status indicating whether it is done or not.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a Task with the specified description.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
+    /**
+     * Returns the status icon of the task.
+     * An "X" indicates the task is done, and a space " " indicates it is not done.
+     *
+     * @return The status icon representing whether the task is done.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
+
+    /**
+     * Returns the task description along with its status icon.
+     *
+     * @return A string representing the task's status and description.
+     */
     public String getDescription() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
+
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
+
+    /**
+     * Unmarks the task, setting it as not done.
+     */
     public void unmark() {
         this.isDone = false;
     }
