c29746dba52cf3faeba38cc82a9a6fb819e78a81
zwe <zwezeya02@gmail.com>
2024-08-19 23:19:43 +0800

    Rename chatbot and update greeting and exit messages

---
+++ src/main/java/Avo.java
@@ -0,0 +1,8 @@
+public class Avo {
+    public static void main(String[] args) {
+        String greetingMessage = "Hello, I am Avo.\nWhat can I do for you?";
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(greetingMessage);
+        System.out.println(exitMessage);
+    }
+}
