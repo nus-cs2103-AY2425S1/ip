5a9b2df1808db0460e037fcd28f2a400dfe06713
dominopizzaaaa <domksj23@gmail.com>
2024-08-19 17:40:47 +0800

    Made system able to take inputs, exit upon bye command

--- src/main/java/Papadom.java
+++ src/main/java/Papadom.java
@@ -1,11 +1,30 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Papadom {
     public static void main(String[] args) {
+
         String logo = "____________________________________________________________\n"
                 + "Hello! I'm Papadom\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + "____________________________________________________________\n";
+
+        String exitMessage = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+
         System.out.println(logo);
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+
+            if (Objects.equals(text, "bye")) break;
+            String response = "____________________________________________________________\n"
+                    + " " + text + "\n"
+                    + "____________________________________________________________";
+
+            System.out.println(response);
+        }
+        System.out.println(exitMessage);
     }
 }
