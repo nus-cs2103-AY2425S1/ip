c036780d967b680c97c5862741fd87cef8c89c71
Lim Yixiang <limyixiang2@gmail.com>
2024-08-19 14:19:12 +0800

    Renamed Chatbot to Friendly Bot. Implemented skeletal version of greet and exit.

---
+++ src/main/java/FriendlyBot.java
@@ -0,0 +1,14 @@
+public class FriendlyBot {
+    private static void printHorizontalBar() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        FriendlyBot.printHorizontalBar();
+        System.out.println("Hello! I'm Friendly Bot");
+        System.out.println("What can I do for you?");
+        FriendlyBot.printHorizontalBar();
+        System.out.println("Bye. Hope to see you again soon!");
+        FriendlyBot.printHorizontalBar();
+    }
+}
