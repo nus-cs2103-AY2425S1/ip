79597c89d399051b204550d0ba99afe8d5851ba2
engyuhan <e1136866@u.nus.edu>
2024-08-19 16:34:41 +0800

    feat: add todo, deadline and event commands

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,4 +1,8 @@
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Bob {
     private static String formatMessage(String msg) {
         return "___________________________________________________________\n"
@@ -7,41 +11,56 @@ public class Bob {
     }
 
     private static String parseInput(String input, ChatBot bot) {
-        String[] inputWords = input.split(" ");
-        if (inputWords.length == 0) {
+        if (Objects.equals(input, "")) {
             return "";
         }
 
         String command = input.split(" ")[0];
-        switch (command) {
-            case "bye":
-                return bot.exit();
-            case "list":
-                return bot.list();
+        try {
+            switch (command) {
+                case "bye":
+                    return bot.exit();
+                case "list":
+                    return bot.list();
             case "mark": {
-                try {
+                    String[] inputWords = input.split(" ");
                     if (inputWords.length != 2) {
-                        throw new InvalidInputException("Task number not found.");
+                        throw new InvalidInputException("Please specify which task to mark.");
                     }
                     int itemNum = Integer.parseInt(inputWords[1]);
                     return bot.mark(true, itemNum);
-                } catch (InvalidInputException e) {
-                    return e.toString();
                 }
-            }
-            case "unmark": {
-                try {
+                case "unmark": {
+                    String[] inputWords = input.split(" ");
                     if (inputWords.length != 2) {
-                        throw new InvalidInputException("Task number not found.");
+                        throw new InvalidInputException("Please specify which task to unmark.");
                     }
                     int itemNum = Integer.parseInt(inputWords[1]);
                     return bot.mark(false, itemNum);
-                } catch (InvalidInputException e) {
-                    return e.toString();
                 }
+                case "todo":
+                    return bot.todo(input.substring(5));
+                case "deadline": {
+                    Matcher matcher = Pattern.compile("^deadline (.*) /by (.*)$").matcher(input);
+                    if (matcher.find()) {
+                        return bot.deadline(matcher.group(1), matcher.group(2));
+                    } else {
+                        throw new InvalidInputException("Please specify the deadline using \"/by\".");
+                    }
+                }
+                case "event": {
+                    Matcher matcher = Pattern.compile("^event (.*) /from (.*) /to (.*)$").matcher(input);
+                    if (matcher.find()) {
+                        return bot.event(matcher.group(1), matcher.group(2), matcher.group(3));
+                    } else {
+                        throw new InvalidInputException("Please specify the timing using \"/from\" and \"/to\".");
+                    }
+                }
+                default:
+                    throw new InvalidInputException("I'm sorry, I did not understand your message.");
             }
-            default:
-                return bot.add(input);
+        } catch (InvalidInputException e) {
+            return e.toString();
         }
     }
 
