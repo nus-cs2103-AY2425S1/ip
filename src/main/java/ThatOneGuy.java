a7d84c2bb1228ed8092658b2e387104edcf780fa
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-19 14:52:04 +0800

    Reworked greeting

---
+++ src/main/java/ThatOneGuy.java
@@ -0,0 +1,9 @@
+public class ThatOneGuy {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String name = "That one guy";
+        System.out.println(line + "\nHello! I'm " + name);
+        System.out.println("What can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
