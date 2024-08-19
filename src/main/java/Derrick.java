9a52041030c76cc7da90bca1ab7e3c36aeb9c904
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2024-08-19 08:38:27 +0800

    Add greetings and exit

---
+++ src/main/java/Derrick.java
@@ -0,0 +1,15 @@
+public class Derrick {
+
+    public static void greetings() {
+        System.out.println("Hello, I am Derrick");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Goodbye!");
+    }
+    public static void main(String[] args) {
+        Derrick.greetings();
+        Derrick.exit();
+    }
+}
