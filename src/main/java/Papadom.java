6933514f686bf61addae23bad24d81345d6aca4a
dominopizzaaaa <domksj23@gmail.com>
2024-08-19 18:05:52 +0800

    Used array for storing task instead for easy access and tracking

--- src/main/java/Papadom.java
+++ src/main/java/Papadom.java
@@ -1,6 +1,15 @@
 import java.util.Objects;
 import java.util.Scanner;
 public class Papadom {
+    public static String printList(String[] list) {
+        String finalList = "";
+        int current = 1;
+        for (String task : list) {
+            if (task == null) break;
+            finalList += current++ + ". " + task + "\n";
+        }
+        return finalList;
+    }
     public static void main(String[] args) {
 
         String logo = "____________________________________________________________\n"
@@ -8,8 +17,8 @@ public class Papadom {
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
 
-        String list = "";
-        int number = 1;
+        String[] list = new String[100];
+        int current = 0;
 
         String exitMessage = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
@@ -21,19 +30,18 @@ public class Papadom {
             Scanner scanner = new Scanner(System.in);
             String text = scanner.nextLine();
             if (Objects.equals(text, "list")) {
-                System.out.println(list);
+                String currentList = printList(list);
+                System.out.println(currentList);
                 continue;
+            } else if (Objects.equals(text, "bye")) {
+                break;
+            } else {
+                list[current++] = text;
+                String response = "____________________________________________________________\n"
+                        + " added: " + text + "\n"
+                        + "____________________________________________________________";
+                System.out.println(response);
             }
-
-            list += number + ". " + text + "\n";
-            number++;
-
-            if (Objects.equals(text, "bye")) break;
-            String response = "____________________________________________________________\n"
-                    + " added: " + text + "\n"
-                    + "____________________________________________________________";
-
-            System.out.println(response);
         }
         System.out.println(exitMessage);
     }
