c1955eb27761c517c5c97e5c6928eaf6b7a70597
dominopizzaaaa <domksj23@gmail.com>
2024-08-19 17:24:41 +0800

    Renamed Papadom.java and Changed its contents

---
+++ src/main/java/Papadom.java
@@ -0,0 +1,11 @@
+public class Papadom {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Papadom\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
