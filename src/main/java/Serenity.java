926884163891d3172a95ac7b96a61a94f84b35ba
kngys <kristiangys@gmail.com>
2024-08-19 19:06:45 +0800

    Level 1. Echo

--- src/main/java/Serenity.java
+++ src/main/java/Serenity.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class Serenity {
     public static void main(String[] args) {
+
         String horizontalLine = "__________________________________________";
         System.out.println(horizontalLine);
-        greet();
-        System.out.println(horizontalLine);
-        exit();
-        System.out.println(horizontalLine);
-    }
-
-    public static void greet() {
         System.out.println("Hi, I'm Serenity!\n" + "What can I do for you?");
-    }
+        System.out.println(horizontalLine);
 
-    public static void exit() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+            input = sc.next();
+        }
+        sc.close();
+
+        System.out.println(horizontalLine);
         System.out.println("Goodbye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
