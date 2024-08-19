dfd0327a99bac6c49d89f8bf1e030de301e63dac
xqtann <xqxinquan@gmail.com>
2024-08-19 23:24:03 +0800

    added Add, List commands

--- src/main/java/Pikappi.java
+++ src/main/java/Pikappi.java
@@ -7,6 +7,8 @@ public class Pikappi {
         System.out.println("____________________________________________________________");
         Scanner reader = new Scanner(System.in);
         String command;
+        String[] tasks = new String[100];
+        int numTasks = 0;
         while (true) {
             command = reader.nextLine();
             if (command.equals("bye")) {
@@ -14,9 +16,24 @@ public class Pikappi {
                 System.out.println("Pi-kapi! See you again~\n");
                 System.out.println("____________________________________________________________");
                 return;
+            } else if (command.equals("list")) {
+                System.out.println("____________________________________________________________");
+                if (numTasks == 0) {
+                    System.out.println("Pika-ka! You have no tasks!");
+                }
+                for (int i = 0; i < numTasks; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("____________________________________________________________");
             } else {
                 System.out.println("____________________________________________________________");
-                System.out.println(command);
+                System.out.println("added: " + command);
+                tasks[numTasks] = command;
+                numTasks++;
                 System.out.println("____________________________________________________________");
             }
         }
