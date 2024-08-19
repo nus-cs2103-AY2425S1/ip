ef418df69473e5f7e429f068305e5192ed0e6856
gladwintan <gladwintan@hotmail.sg>
2024-08-19 23:53:09 +0800

    Luna.java: add function to store and display user text
    Let's
    * update main to store user text
    * return user text when requested

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class Luna {
+
     public static void main(String[] args) {
         String greetings = "Hello! I'm Luna\n" +
                 "What can I do for you?";
         System.out.println(greetings);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskNum = 0;
 
         while (true) {
             String input = scanner.nextLine();
@@ -17,7 +20,17 @@ public class Luna {
                 break;
             }
 
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < taskNum; i++) {
+                    String taskStr = String.format("%d. %s", i + 1, tasks[i]);
+                    System.out.println(taskStr);
+                }
+            } else {
+                tasks[taskNum] = input;
+                taskNum++;
+                System.out.println("added: " + input);
+            }
+
         }
 
         scanner.close();
