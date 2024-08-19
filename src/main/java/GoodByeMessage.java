0d3396975522838913cc58e566a3c5aa921ccba5
Ponharish <e1123017@u.nus.edu>
2024-08-19 12:32:27 +0800

    Rename Duke.java to Meeju.java Add GoodByeMessage.java

---
+++ src/main/java/GoodByeMessage.java
@@ -0,0 +1,5 @@
+public class GoodByeMessage {
+    public static String goodByeMessage() {
+        return " Bye. Hope to see you again soon!";
+    }
+}
---
+++ src/main/java/Meeju.java
@@ -0,0 +1,21 @@
+public class Meeju {
+    public static void main(String[] args) {
+
+
+        String logo = " __  __ _____ _____    _ _   _ \n" +
+                "|  \\/  | ____| ____|  | | | | |\n" +
+                "| |\\/| |  _| |  _| _  | | | | |\n" +
+                "| |  | | |___| |__| |_| | |_| |\n" +
+                "|_|  |_|_____|_____\\___/ \\___/ ";
+
+        System.out.println(logo + "\n\t Meow!");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Meeju\n" +
+                " What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(GoodByeMessage.goodByeMessage());
+        System.out.println("____________________________________________________________");
+
+
+    }
+}
