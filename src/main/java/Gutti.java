075894464edb33e6956b306b7b5a11c07ce3e213
PresleyChew <E1115692@u.nus.edu>
2024-08-19 18:41:13 +0800

    Level 1. Echo

--- src/main/java/Gutti.java
+++ src/main/java/Gutti.java
@@ -1,13 +1,44 @@
+import java.util.Scanner;
 public class Gutti {
-    public static void main(String[] args) {
-        // Display the greeting message
+
+    private static void greetings() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Gutti");
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you? Meow");
         System.out.println("____________________________________________________________");
+    }
 
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
+    private static void goodBye() {
         System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon! Meow");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void echo(){
+        Scanner scanner = new Scanner(System.in);
+        // User's input
+        String input;
+        // Loops until user says bye
+        while (true) {
+            input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            // Echo the command entered by the user
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        // Display the greeting message
+        greetings();
+        // Echos
+        echo();
+        // Exit message
+        goodBye();
     }
 }
