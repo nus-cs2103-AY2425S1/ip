175f64f2eb2f3a2c28bb8e94dd9bb40eb402871c
hovanleong <hovanleong1@gmail.com>
2024-08-20 01:56:57 +0800

    Add greeting

--- src/main/java/SlothingWaffler.java
+++ src/main/java/SlothingWaffler.java
@@ -1,10 +1,7 @@
 public class SlothingWaffler {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm the Slothing Wafller!\n" +
+                "Let's stop slothing and get cracking!\n" +
+                "See you next time! Remember to grab a waffle!");
     }
 }
