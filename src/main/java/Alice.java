450212ef7b438c244377a6f661495c7650333111
wujiaweijoanna <e1155577@u.nus.edu>
2024-08-19 17:04:20 +0800

    Rename, greet, exit

---
+++ src/main/java/Alice.java
@@ -0,0 +1,14 @@
+public class Alice {
+    public static void main(String[] args) {
+        String name = "Alice";
+        String line =
+                "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
