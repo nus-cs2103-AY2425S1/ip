6e451999491750eb794eea53c52c9f5598b034b9
lm-44 <yauchengyong.literallyme@gmail.com>
2024-08-19 21:44:36 +0800

    added Level-2 functionality and changed text formatting

--- src/main/java/Cook.java
+++ src/main/java/Cook.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Cook {
     public static void main(String[] args) {
@@ -10,32 +11,42 @@ public class Cook {
                          |  \\__| \\_/|| \\_/||   \\\s
                          \\____/\\____/\\____/\\_|\\_\\
                                                 \s
-
                          """;
-        System.out.println(logo);
-        start();
+        System.out.print(logo);
+        formatting("Hello, I'm Cook!\nWhat can I do for you?");
+
+        // Solution below inspired by https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/ArrayList.html
+        ArrayList<String> toDoList = new ArrayList<String>();
 
         while (true) {
             // Solution below inspired by https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Scanner.html
             Scanner input = new Scanner(System.in);
             String echo = input.nextLine();
+            // Breaks loop
             if (echo.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println(echo + "\n");
+            // Prints out to do list
+            else if (echo.equalsIgnoreCase("list")) {
+                StringBuilder taskList = new StringBuilder();
+                for (int i = 0; i < toDoList.size(); i++) {
+                    int taskNo = i + 1;
+                    taskList.append(taskNo).append(". ").append(toDoList.get(i)).append("\n");
+                }
+                formatting(taskList.toString().stripTrailing());
+            }
+            else {
+                toDoList.add(echo);
+                formatting("I've added " + echo + " to your to do list!");
+            }
         }
 
-        exit();
+        formatting("Bye. Hope to see you again soon!");
     }
 
-    public static void start() {
-        System.out.println("""
-                Hello, I'm Cook!
-                What can I do for you?
-                """);
-    }
-
-    public static void exit() {
-        System.out.println("Bye. Hope to see you again soon!\n");
+    public static void formatting(String text) {
+        System.out.println("____________________________________________________________");
+        System.out.println(text);
+        System.out.println("____________________________________________________________");
     }
 }
