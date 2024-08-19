2221b45b2c01647e1366b9ac128e3b31644225fc
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-08-20 02:46:31 +0800

    Fixed the class name to be Wen Jie, and added a missing semi colon

--- src/main/java/WenJie.java
+++ src/main/java/WenJie.java
@@ -1,11 +1,11 @@
-public class Duke {
+public class WenJie {
     public static void main(String[] args) {
         String logo = " ____________________________________________________________\n" +
                 " Hello! I'm Wen Jie\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n\n"
+                "____________________________________________________________\n\n";
 
         System.out.println(logo);
     }
