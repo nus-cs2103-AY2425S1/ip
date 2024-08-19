f309e05163257d735c451a58457c156d42ca118f
kaixuan477 <kxuan477@gmail.com>
2024-08-19 18:36:06 +0800

    level 0. change Duke to Mel

---
+++ src/main/java/Mel.java
@@ -0,0 +1,15 @@
+public class Mel {
+    public static void main(String[] args) {
+        String logo = " __    __        __  \n"
+                    + "|   \\/   | ____ |  | \n"
+                    + "| |\\  /| |/ __ \\|  | \n"
+                    + "| | \\/ | |  ___/|  | \n"
+                    + "|_|    |_|\\____||__| ";
+        String line = "____________________________________";
+
+        System.out.println(logo + "\n" + line + "\n"
+                            + "Hello! I'm Mel\n"
+                            + "What can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
