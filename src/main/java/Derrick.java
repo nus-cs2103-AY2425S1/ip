76a0ead13364f4802573c5f31c445a0539875750
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2024-08-19 08:49:40 +0800

    Add echoing of user input

--- src/main/java/Derrick.java
+++ src/main/java/Derrick.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Derrick {
 
     public static void greetings() {
@@ -5,11 +7,24 @@ public class Derrick {
         System.out.println("What can I do for you?");
     }
 
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            }
+            else System.out.println(input);
+        }
+    }
+
+
     public static void exit() {
         System.out.println("Goodbye!");
     }
     public static void main(String[] args) {
         Derrick.greetings();
-        Derrick.exit();
+        Derrick.echo();
     }
 }
