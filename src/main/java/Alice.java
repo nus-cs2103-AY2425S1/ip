b8953a78d1ceb2192df383cd19307c7ed611230f
wujiaweijoanna <e1155577@u.nus.edu>
2024-08-19 23:58:45 +0800

    Mark task as done

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -6,7 +6,7 @@ public class Alice {
     private static final String name = "Alice";
     private static final String line =
             "____________________________________________________________";
-    private static List<String> tasks = new ArrayList<>();
+    private static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         greet();
@@ -17,8 +17,43 @@ public class Alice {
                  break;
              } else if (input.equals("list")) {
                  listTasks();
+             } else if (input.startsWith("mark")) {
+                 String[] phrases = input.split(" ");
+                 if (phrases.length > 1) {
+                     try {
+                         int num = Integer.parseInt(phrases[1]);
+                         Task currTask = tasks.get(num - 1);
+                         currTask.markDone();
+                         System.out.println(line);
+                         System.out.println("Nice! I've marked this task as done:");
+                         System.out.println(currTask.toString());
+                         System.out.println(line);
+                     } catch (IndexOutOfBoundsException e) {
+                         System.err.println("Invalid number");
+                     }
+                 } else {
+                     System.err.println("Need to state the number");
+                 }
+             } else if (input.startsWith("unmark")) {
+                 String[] phrases = input.split(" ");
+                 if (phrases.length > 1) {
+                     try {
+                         int num = Integer.parseInt(phrases[1]);
+                         Task currTask = tasks.get(num - 1);
+                         currTask.markUndone();
+                         System.out.println(line);
+                         System.out.println("OK, I've marked this task as not done yet:");
+                         System.out.println(currTask.toString());
+                         System.out.println(line);
+                     } catch (IndexOutOfBoundsException e) {
+                         System.err.println("Invalid number");
+                     }
+                 } else {
+                     System.err.println("Need to state the number");
+                 }
              } else {
-                 tasks.add(input);
+                 Task newTask = new Task(input);
+                 tasks.add(newTask);
                  String s = "added: " + input;
                  System.out.println(line);
                  System.out.println(s);
@@ -30,10 +65,11 @@ public class Alice {
 
     public static void listTasks() {
         System.out.println(line);
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
-            String currTask = tasks.get(i);
+            Task currTask = tasks.get(i);
             int currNum = i + 1;
-            String s = currNum + ". " + currTask;
+            String s = currNum + "." + currTask.toString();
             System.out.println(s);
         }
         System.out.println(line);
