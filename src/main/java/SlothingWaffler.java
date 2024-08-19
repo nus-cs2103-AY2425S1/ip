a49d99c2747a63f2573015438ab80e609df6ff79
hovanleong <hovanleong1@gmail.com>
2024-08-20 02:03:04 +0800

    Tidy up main method

--- src/main/java/SlothingWaffler.java
+++ src/main/java/SlothingWaffler.java
@@ -1,7 +1,8 @@
 public class SlothingWaffler {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm the Slothing Wafller!\n" +
-                "Let's stop slothing and get cracking!\n" +
-                "See you next time! Remember to grab a waffle!");
+        System.out.println("""
+                Hello! I'm the Slothing Wafller!
+                Let's stop slothing and get cracking!
+                See you next time! Remember to grab a waffle!""");
     }
 }
