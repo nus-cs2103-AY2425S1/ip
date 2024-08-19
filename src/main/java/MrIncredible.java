18a56fcf1ecfbe98dcf3e909b1e3343632189247
Lee_Wei_Chong <leechewie13@gmail.com>
2024-08-19 19:31:52 +0800

    Get chatbot to echo user inputs loudly

--- src/main/java/MrIncredible.java
+++ src/main/java/MrIncredible.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class MrIncredible {
     public static void main(String[] args) {
-        String logo = "MrIncredible";
+        Scanner scanner = new Scanner(System.in);
         String greeting = "What can I do for you?";
-        String exit = "Bye. Hope to see you again soon!";
-        System.out.println("Hello from\n" + logo);
+        String byeText = "Bye. Hope to see you again soon!";
+        // Initial greeting
+        System.out.println("    ____________________________________________________________");
+        System.out.println("Hello! I'm MrIncredible");
         System.out.println(greeting);
-        System.out.println(exit);
+        System.out.println("    ____________________________________________________________");
+
+        // Start an infinite loop to handle user input
+        while (true) {
+            // Read user input
+            String input = scanner.nextLine();
+
+            // Exit condition
+            if (input.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println(byeText);
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+
+            // Echo the user's input
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + input.toUpperCase());
+            System.out.println("    ____________________________________________________________");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
