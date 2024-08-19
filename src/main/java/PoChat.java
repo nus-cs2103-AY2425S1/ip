6553c697a2a1a6cda4569b706b6a470b2eb95036
matthewyip1511 <e0938566@u.nus.ed>
2024-08-19 11:22:47 +0800

    Set up level 1

--- src/main/java/PoChat.java
+++ src/main/java/PoChat.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class PoChat {
     public static void main(String[] args) {
         String introMessage = "Hello! I'm PoChat, the chatbot in your pocket.\nWhat can I do for you?";
         String goodbyeMessage = "Bye. Hope to see you again soon!";
-        System.out.println(introMessage + "\n\n" + goodbyeMessage);
+
+        System.out.println(introMessage);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(goodbyeMessage);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
