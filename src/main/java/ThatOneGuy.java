35cc399d0bf3051b3cc6f2192ec77617372baa46
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-19 14:56:08 +0800

    Revert "Reworked greeting"
    This reverts commit a7d84c2bb1228ed8092658b2e387104edcf780fa.

--- src/main/java/ThatOneGuy.java
+++
@@ -1,9 +0,0 @@
-public class ThatOneGuy {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String name = "That one guy";
-        System.out.println(line + "\nHello! I'm " + name);
-        System.out.println("What can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
-    }
-}
