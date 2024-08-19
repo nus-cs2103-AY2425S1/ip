4b42e54ca2d0aef0804d21ce3026f79d236980f8
PresleyChew <E1115692@u.nus.edu>
2024-08-19 18:57:52 +0800

    Level 2. Add, List

--- src/main/java/Gutti.java
+++ src/main/java/Gutti.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+import java.util.List;
+
 public class Gutti {
+    private static String[] tasks = new String[100];
+    private static int noOfTasks = 0;
 
     private static void greetings() {
         System.out.println("____________________________________________________________");
@@ -21,18 +25,32 @@ public class Gutti {
         // Loops until user says bye
         while (true) {
             input = scanner.nextLine();
-
+            // Stop when user types bye
             if(input.equalsIgnoreCase("bye")) {
                 break;
             }
-            // Echo the command entered by the user
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+            // Lists out all the tasks
+            if(input.equalsIgnoreCase("list")) {
+                listsTask();
+            }
+            // Adds tasks
+            else {
+                System.out.println("____________________________________________________________");
+                System.out.println("Gutti painstakingly added: " + input);
+                System.out.println("____________________________________________________________");
+                tasks[noOfTasks] = input;
+                Gutti.noOfTasks++;
+            }
         }
         scanner.close();
     }
-
+    private static void listsTask() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < noOfTasks; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
         // Display the greeting message
         greetings();
