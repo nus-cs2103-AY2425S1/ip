af9b4b9b368c2efc9acbc5b1c398aaf6417b6824
engyuhan <e1136866@u.nus.edu>
2024-08-19 16:29:44 +0800

    feat: add ToDo class

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,19 @@
+public class ToDo extends Task {
+    /**
+     * Constructor for a new to-do task.
+     *
+     * @param name the name of the to-do task
+     */
+    public ToDo(String name) {
+        super(name);
+    }
+
+    /**
+     * Get the string representation of the to-do task.
+     * @return the string representation of the to-do task
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
