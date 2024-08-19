6028c9fdf1c3cfe554d00d278a7a7a540697dde4
Potatoad88 <>
2024-08-19 15:14:04 +0800

    Add, List

--- src/main/java/DemureBot.java
+++ src/main/java/DemureBot.java
@@ -1,28 +1,44 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class DemureBot {
     public static void main(String[] args) {
-        boolean finished = false;
         Scanner scanner = new Scanner(System.in);
+
+        // check if user ended session
+        boolean finished = false;
+        // list of items to do
+        ArrayList<String> list = new ArrayList<>();
+
+        // introduction to chatbot
         System.out.println("____________________________________________________________\n" +
             " Hello! I'm DemureBot\n" +
             " What can I do for you?\n" +
             "____________________________________________________________\n" +
             "\n"
         );
+
+        // while user hasn't ended session
         while (!finished) {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
                 finished = true;
+            } else if(command.equals("list")) {
+                for (int i = 0; i < list.size(); i ++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
             } else {
+                list.add(command);
                 System.out.println("____________________________________________________________\n" +
-                        command +
-                        "\n" +
-                        "____________________________________________________________\n" +
-                        "\n"
+                    "added: " + command +
+                    "\n" +
+                    "____________________________________________________________\n" +
+                    "\n"
                 );
             }
         }
+        // close scanner and end session
+        scanner.close();
         System.out.println("____________________________________________________________\n" +
             " Bye. Hope to see you again soon!\n" +
             "____________________________________________________________\n" +
