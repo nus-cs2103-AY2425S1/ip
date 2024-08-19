171f5abda983960f358d2c460ea1a938aab3ef83
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-19 15:03:57 +0800

    Reworked greeting

--- src/main/java/ThatOneGuy.java
+++ src/main/java/ThatOneGuy.java
@@ -1,10 +1,9 @@
-public class Duke {
+public class ThatOneGuy {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        String name = "That one guy";
+        System.out.println(line + "\nHello! I'm " + name);
+        System.out.println("What can I do for you?\n" + line);
+        System.out.println("Bye.  Hope to see you again soon!\n"  + line);
     }
 }
