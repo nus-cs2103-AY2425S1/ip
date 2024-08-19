9c552990e8c321a6d0658c28e4a3c73abc0ee90a
tkaixiang <tkaixiang@gmail.com>
2024-08-19 16:36:19 +0800

    Level_1

--- src/main/java/Kita.java
+++ src/main/java/Kita.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Kita {
-    private static String lines = "____________________________________________________________\n";
+    private static void printLine() {
+        System.out.println("____________________________________________________________\n");
+    }
+
     public static void main(String[] args) {
-        System.out.println(lines);
+        Scanner getInput = new Scanner(System.in);
+
+        printLine();
         System.out.println(" Hello! I'm Kita");
         System.out.println(" What can I do for you?");
-        System.out.println(lines);
-        System.out.println(" Bye. Hope to see you again soon!\n");
-        System.out.println(lines);
-
+        printLine();
+        while (true) {
+            String command = getInput.nextLine();
+            if (command.equals("bye")) {
+                printLine();
+                System.out.println(" Bye. Hope to see you again soon!\n");
+                printLine();
+                break;
+            }
+            printLine();
+            System.out.println(command);
+            printLine();
+        }
     }
 }
