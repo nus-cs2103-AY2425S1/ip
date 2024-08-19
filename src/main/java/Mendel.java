d959fbf3b36836212a043f0bca0bf4404a41039e
Virusrwj223 <mhrishiraj@gmail.com>
2024-08-19 11:18:18 +0800

    Add echo task feature

--- src/main/java/Mendel.java
+++ src/main/java/Mendel.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Mendel {
     public static void main(String[] args) {
         String logo = "Mendel";
         System.out.println("Hello from\n" + logo);
-        new Welcome().speak();
+        String currAction = "hello";
+        while (!currAction.equals("bye")) {
+            if (currAction.equals("hello")) {
+                new Welcome().speak();
+            } else {
+                new Task(currAction).speak();
+            }
+            Scanner sc = new Scanner(System.in);
+            currAction = sc.nextLine();
+        }
         new Leave().speak();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Task extends MendelAction{
+    private final String message;
 
-public class Task {
+    public Task(String message) {
+        this.message = message;
+    }
+
+    public void speak() {
+        System.out.println(new FormatText(this.toString()).wrapLines());
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
 }
