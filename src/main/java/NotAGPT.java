1f321434efb1406fc6986357416d042d2d556c57
chiangqinkang <chiangqinkang@gmail.com>
2024-08-19 14:10:04 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/NotAGPT.java
@@ -0,0 +1,17 @@
+public class NotAGPT {
+    public static void main(String[] args) {
+        String logo = " _   _       _      _    ____ ____ _____ \n" +
+                "| \\ | | ___ | |_   / \\  / ___|  _ \\_   _|\n" +
+                "|  \\| |/ _ \\| __| / _ \\| |  _| |_) || |  \n" +
+                "| |\\  | (_) | |_ / ___ \\ |_| |  __/ | |  \n" +
+                "|_| \\_|\\___/ \\__/_/   \\_\\____|_|    |_|  ";
+        String horizontalLine = "____________________________________________________________ \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm NotAGPT");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
