2de514628f32155372df28cf719fe36441d55a02
Michael Yang <yql.michael@gmail.com>
2024-08-19 21:59:36 +0800

    add mark and unmark task functionality

--- src/main/java/Terminator.java
+++ src/main/java/Terminator.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Terminator {
     public static void main(String[] args) {
@@ -27,22 +29,55 @@ public class Terminator {
                 hline;
         System.out.println(greeting);
 
-        String exit = hline + "\tAll objectives fulfilled. Deactivating...\n" + hline;
+        String exit = hline + "\tAll mission objectives fulfilled. Terminating...\n" + hline;
 
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
 
-        ArrayList<String> todoList = new ArrayList<>();
+        Pattern markPattern = Pattern.compile("^mark \\d+$");
+        Matcher markMatcher;
+
+        Pattern unmarkPattern = Pattern.compile("^unmark \\d+$");
+        Matcher unmarkMatcher;
+
+        ArrayList<Task> todoList = new ArrayList<>();
+
         while (!userInput.equals("bye")) {
             System.out.print(hline);
+
+            markMatcher = markPattern.matcher(userInput);
+            unmarkMatcher = unmarkPattern.matcher(userInput);
+
+            // Handle user input
             if (userInput.equals("list")) {
+                System.out.println("Listing current mission objectives:");
                 for (int i = 0; i < todoList.size(); i++) {
-                    System.out.println("\t" + (i + 1) + ". " + todoList.get(i));
+                    Task currentTask = todoList.get(i);
+                    System.out.println("\t" + (i + 1) + ".[" + currentTask.getStatusIcon() + "] "
+                            + currentTask.description);
+                }
+            } else if (markMatcher.find()) {
+                int idx = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (idx >= 0 && idx < todoList.size()) {
+                    System.out.println("Task fulfilled. Awaiting next directive:");
+                    Task currentTask = todoList.get(idx);
+                    currentTask.markAsDone();
+                    System.out.println("\t\t[" + currentTask.getStatusIcon() + "] " + currentTask.description);
+                }
+            } else if (unmarkMatcher.find()) {
+                int idx = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (idx >= 0 && idx < todoList.size()) {
+                    System.out.println("Objective reopened:");
+                    Task currentTask = todoList.get(idx);
+                    currentTask.markAsIncomplete();
+                    System.out.println("\t\t[" + currentTask.getStatusIcon() + "] " + currentTask.description);
                 }
             } else {
-                todoList.add(userInput);
+                Task newTask = new Task(userInput);
+                todoList.add(newTask);
                 System.out.println("\tadded: " + userInput);
             }
+
             System.out.println(hline);
             userInput = sc.nextLine();
         }
