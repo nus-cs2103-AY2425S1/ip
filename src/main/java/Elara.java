51044defc16ac65880fbc08fdf8d5cbb08bb2b28
ylchin <chin.yiling0715@gmail.com>
2024-08-19 15:23:40 +0800

    Added ability to add and list

--- src/main/java/Elara.java
+++ src/main/java/Elara.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Elara {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
+
         System.out.println("Hello! I'm Elara");
         System.out.println("What can I do for you?");
 
@@ -10,8 +13,19 @@ public class Elara {
             String text = scanner.nextLine();
             if (text.toLowerCase().strip().equals("bye")) {
                 break;
+            } else if (text.toLowerCase().strip().equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    if (list[i] == null) {
+                        break;
+                    }
+                    int j = i + 1;
+                    System.out.println(j + ". " + list[i]);
+                }
+            } else {
+                System.out.println("added: " + text);
+                list[count] = text;
+                count++;
             }
-            System.out.println(text);
         }
 
         System.out.println("Bye. Hope to see you again!");
