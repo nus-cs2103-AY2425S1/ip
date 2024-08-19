a1142e821069164d72cac007587f433876cd5670
bmanara <bmanaranus@gmail.com>
2024-08-19 14:51:55 +0800

    Add echo in Llama class

--- src/main/java/Llama.java
+++ src/main/java/Llama.java
@@ -3,28 +3,32 @@ import java.util.Scanner;
 public class Llama {
     public static void main(String[] args) {
         String logo = "";
-        String hr = "____________________________________________________________" ;
+        String hr = "\t____________________________________________________________" ;
         Scanner sc = new Scanner(System.in);
 
         // Initializing message
         System.out.println(hr);
-        System.out.println("Hello! I'm Llama!\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println(hr);
+        System.out.println("\tHello! I'm Llama!\n" + logo);
+        System.out.println("\tWhat can I do for you?");
 
         // Get user input
         boolean shouldContinue = true;
         while (shouldContinue) {
+            System.out.println(hr);
             String input = sc.nextLine();
+            System.out.println(hr);
 
             if (input.equals("bye")) {
                 shouldContinue = false;
                 sc.close();
+            } else {
+                System.out.println("\t" + input);
             }
+
+
         }
 
         // Exit message
-        System.out.println(hr);
         System.out.println("Baaaaaa byeeee. Come baaaaack soon!");
 
     }
