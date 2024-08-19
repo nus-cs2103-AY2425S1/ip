b7194021bcd3a0a319255ba860dc64ae3d4d9884
CliffKoes <cliffkoes@gmail.com>
2024-08-20 00:11:58 +0800

    Added echo of input

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Bibi {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = """
                         ########   #######   ########   #######\s
                         #       #     #      #       #     #   \s
@@ -11,7 +14,21 @@ public class Bibi {
 
         System.out.println("Hello from\n" + logo + "\n"
                     + "How can I help you?");
+        System.out.println("---------------------------------");
 
+        String cmd;
+        while (scanner.hasNext()) {
+            cmd = scanner.next();
+            if (cmd.equals("bye")) {
+                break;
+            }
+
+            System.out.println("--------------------------------");
+            System.out.println(cmd);
+            System.out.println("--------------------------------");
+        }
+
+        // Exit
         System.out.println("---------------------------------");
         System.out.println("See you soon :3");
         System.out.println("---------------------------------");
