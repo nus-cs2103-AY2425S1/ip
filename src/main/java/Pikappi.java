78c066b6fc2905c8cd304d4a373c87cb32cf8ebc
xqtann <xqxinquan@gmail.com>
2024-08-19 22:27:45 +0800

    added echo commands

--- src/main/java/Pikappi.java
+++ src/main/java/Pikappi.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Pikappi {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Pika! I'm Pikappi!\nWhat can I do for you?\n");
         System.out.println("____________________________________________________________");
-        System.out.println("Pi-kapi! See you again~\n");
-        System.out.println("____________________________________________________________");
+        Scanner reader = new Scanner(System.in);
+        String command;
+        while (true) {
+            command = reader.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Pi-kapi! See you again~\n");
+                System.out.println("____________________________________________________________");
+                return;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(command);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
