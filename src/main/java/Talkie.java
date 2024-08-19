970d93d6b3389c07b87ea3d4f9f2141f7c6db768
Jeremy <e1155908@u.nus.edu>
2024-08-19 16:17:48 +0800

    Level-1

--- src/main/java/Talkie.java
+++ src/main/java/Talkie.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class Talkie {
     public static void main(String[] args) {
+
+        // Initialisation of Scanner
+        Scanner scanner = new Scanner(System.in);
+
         String logo = """
                 -----------------------------------------------\s
                 Hello! I'm Talkie, your friendly ChatBot.\s
                 What can I do for you?\s
                 -----------------------------------------------\s
+                """;
+
+        String byeMessage = """ 
+                -----------------------------------------------\s
                 Bye. Hope to see you again soon!\s
                 -----------------------------------------------\s
-               """;
+                """;
 
         System.out.println(logo);
+
+        // Program runs until user inputs "bye"
+        while (true) {
+            String toDo = scanner.nextLine();
+
+            if (toDo.equalsIgnoreCase("bye")) {
+                System.out.println(byeMessage);
+                break;
+            }
+
+
+            String toDoMessage = "-----------------------------------------------\n"
+                    + toDo + "\n"
+                    + "-----------------------------------------------\n";
+
+            System.out.println(toDoMessage);
+        }
+
+        // Close the scanner when the program ends
+        scanner.close();
     }
 }
