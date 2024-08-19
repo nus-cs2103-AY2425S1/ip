739d962c474b10fc6c3b24d66a28ee5306d9a1d5
benjiBase <blim0e89@gmail.com>
2024-08-19 14:59:23 +0800

    Add A-TextUiTesting updated txt

--- src/main/java/Nameless.java
+++ src/main/java/Nameless.java
@@ -15,59 +15,76 @@ public class Nameless {
 
     private static String splitGetWords(String input){
         String[] words = input.split(" ", 2);
-        return words[1];
+        return words.length > 1 ? words[1] : "";
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         System.out.println(line + "\n" + greetings + "\n" + line);
         int counter = 0;
 
-        String temp = "";
+        String temp;
+
         while(true){
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
-            if(input.equals("bye")){
-                break;
-            } else if (input.equals("list")) {
-                //list tasking
-                System.out.println(line + "\n Here are the tasks in your list:");
-                for(int i = 0; i < counter; i++){
-                    //print out all tasking
-                    System.out.println("    " + (i + 1) + "." + tasks[i].toString());
-                }
-                System.out.println(line);
-            } else if (input.matches("mark \\d+")) {
-                System.out.println(line);
-                tasks[splitGetNum(input)].markTask();
-                System.out.println(line);
-            } else if (input.matches("unmark \\d+")) {
-                System.out.println(line);
-                tasks[splitGetNum(input)].unMarkTask();
-                System.out.println(line);
-            } else if(input.matches("deadline .+")) {
-                temp = splitGetWords(input);
-                String[] words = temp.split(" /by ");
-                tasks[counter] = new Deadline(words[0], words[1]);
-                counter++;
-                System.out.println(line + "\n" + "Got it. I've added this task:" +
-                        "\n     " + tasks[counter - 1].toString() + "\n" +
-                        "Now you have " + counter + " task left \n" + line);
-            } else if(input.matches("event .+")) {
-                temp = splitGetWords(input);
-                String[] words = temp.split(" /from | /to ");
-                tasks[counter] = new Event(words[0], words[1], words[2]);
-                counter++;
-                System.out.println(line + "\n" + "Got it. I've added this task:" +
-                        "\n     " + tasks[counter - 1].toString() + "\n" +
-                        "Now you have " + counter + " task left \n" + line);
-            }
-            else {
-                //store tasking
-                tasks[counter] = new Todo(splitGetWords(input));
-                counter++;
-                System.out.println(line + "\n" + "Got it. I've added this task:" +
-                        "\n     " + tasks[counter - 1].toString() + "\n" +
-                        "Now you have " + counter + " task left \n" + line);
+            try {
+                if (input.equals("bye")) {
+                    break;
+                } else if (input.equals("list")) {
+                    //list tasking
+                    System.out.println(line + "\n Here are the tasks in your list:");
+                    for (int i = 0; i < counter; i++) {
+                        //print out all tasking
+                        System.out.println("    " + (i + 1) + "." + tasks[i].toString());
+                    }
+                    System.out.println(line);
+                } else if (input.matches("mark \\d+")) {
+                    System.out.println(line);
+                    tasks[splitGetNum(input)].markTask();
+                    System.out.println(line);
+                } else if (input.matches("unmark \\d+")) {
+                    System.out.println(line);
+                    tasks[splitGetNum(input)].unMarkTask();
+                    System.out.println(line);
+                } else if (input.matches("deadline(?: .+)?")) {
+                    temp = splitGetWords(input);
+                    if (temp.length() != 2) {
+                        throw new DukeException("incorrect format use 'deadline <task> /by <date>'");
+                    }
+                    String[] words = temp.split(" /by ");
+                    tasks[counter] = new Deadline(words[0], words[1]);
+                    counter++;
+                    System.out.println(line + "\n" + "Got it. I've added this task:" +
+                            "\n     " + tasks[counter - 1].toString() + "\n" +
+                            "Now you have " + counter + " task left \n" + line);
+                } else if (input.matches("event(?: .+)?")) {
+                    temp = splitGetWords(input);
+                    if (temp.length() != 3) {
+                        throw new DukeException("incorrect format use 'event <task> /from <date> /to <date>'");
+                    }
+                    String[] words = temp.split(" /from | /to ");
+                    tasks[counter] = new Event(words[0], words[1], words[2]);
+                    counter++;
+                    System.out.println(line + "\n" + "Got it. I've added this task:" +
+                            "\n     " + tasks[counter - 1].toString() + "\n" +
+                            "Now you have " + counter + " task left \n" + line);
+                } else if (input.matches("todo(?: .+)?")) {
+                    //store tasking
+                    String words = splitGetWords(input);
+                    if (words.isEmpty()) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    }
+                    tasks[counter] = new Todo(words);
+                    counter++;
+                    System.out.println(line + "\n" + "Got it. I've added this task:" +
+                            "\n     " + tasks[counter - 1].toString() + "\n" +
+                            "Now you have " + counter + " task left \n" + line);
 
+                } else {
+                    throw new DukeException("Sorry! I don't understand what you are saying." +
+                            " Please enter a valid command. todo, deadline, event, list, mark/unmark, bye");
+                }
+            } catch (DukeException e) {
+                System.out.println(line + "\n" + e.getMessage() + "\n" + line);
             }
         }
 
