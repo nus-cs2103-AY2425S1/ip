cdc75757eea355f75030a0e091c1b6570258e3bf
lrongyi <iygnormal@gmail.com>
2024-08-19 09:59:30 +0800

    Add, List

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Dook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
         String separator = "____________________________________________________________";
         String greeting = "Hello! I'm Dook\nWhat can I do for you?\n" + separator;
         String exit = "Bye. Hope to see you again soon!\n" + separator;
@@ -10,6 +11,7 @@ public class Dook {
         System.out.println(greeting);
 
         String response;
+        int count = 0;
 
         while (true) {
             response = scanner.nextLine();
@@ -17,9 +19,17 @@ public class Dook {
                 System.out.println(separator);
                 System.out.println(exit);
                 break;
-            } else {
+            } else if (response.equals("list")) {
                 System.out.println(separator);
-                System.out.println(response);
+                for (int i = 1; i <= count; i++) {
+                    System.out.println(i + ". " + tasks[i - 1]);
+                }
+                System.out.println(separator);
+            } else {
+                tasks[count] = response;
+                count++;
+                System.out.println(separator);
+                System.out.println("added: " + response);
                 System.out.println(separator);
             }
         }
