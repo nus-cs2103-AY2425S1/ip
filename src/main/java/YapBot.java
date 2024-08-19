0b9b14dabdd8803da5b342d2fed162ba017d447e
NicT5 <nicctang@gmail.com>
2024-08-19 18:05:22 +0800

    Add list for storing user input

--- src/main/java/YapBot.java
+++ src/main/java/YapBot.java
@@ -1,19 +1,43 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class YapBot {
+    private static final String PREFIXLINE = "\n-------------------------------------------";
+    private static final String POSTFIXLINE = "-------------------------------------------\n";
+
+    public static void list(ArrayList<String> stored) {
+        Iterator<String> iterateStored = stored.iterator();
+        System.out.println(PREFIXLINE);
+        int index = 1;
+        while (iterateStored.hasNext()) {
+            System.out.println(index + ". " + iterateStored.next());
+            index++;
+        }
+        System.out.println(POSTFIXLINE);
+    }
+
     public static void main(String[] args) {
-        String templateLine = "\n-------------------------------------------\n";
-        System.out.println(templateLine + "Hello, I am YapBot. \nHow can I help?" + templateLine);
+
+        System.out.println(PREFIXLINE + "\nHello, I am YapBot. \nHow can I help?\n" + POSTFIXLINE);
+
         Scanner in = new Scanner(System.in);
+        ArrayList<String> stored = new ArrayList<>();
         String input = in.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(templateLine + input + templateLine);
-            input = in.nextLine();
+            if (input.equals("list")) {
+                list(stored);
+                input = in.nextLine();
+            } else {
+                stored.add(input);
+                System.out.println(PREFIXLINE + "\nAdded: " + input + "\n" + POSTFIXLINE);
+                input = in.nextLine();
+            }
 
         }
 
         in.close();
-        System.out.println(templateLine + "Alright, enough yapping for one day." + templateLine);
+        System.out.println(PREFIXLINE + "\nAlright, enough yapping for one day.\n" + POSTFIXLINE);
     }
 }
