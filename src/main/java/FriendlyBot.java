e816298ccaf2f407cb37607df9440198585452d0
Lim Yixiang <limyixiang2@gmail.com>
2024-08-19 14:33:32 +0800

    Added abiity for Friendly Bot to store tasks and return to user when requested.

--- src/main/java/FriendlyBot.java
+++ src/main/java/FriendlyBot.java
@@ -1,24 +1,38 @@
 import java.util.Scanner;
 
 public class FriendlyBot {
+    private static String[] tasks;
+    private static int numTasks;
+
     private static void printHorizontalBar() {
         System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
+        // Initialise variables
         Scanner reader = new Scanner(System.in);
+        tasks = new String[100];
+        numTasks = 0;
+        // Start of Chat Bot
         FriendlyBot.printHorizontalBar();
         System.out.println("    Hello! I'm Friendly Bot");
         System.out.println("    What can I do for you?");
         FriendlyBot.printHorizontalBar();
         String response = "";
+        // Commands
         while (!response.equals("bye")) {
-            response = reader.next();
+            response = reader.nextLine();
             FriendlyBot.printHorizontalBar();
             if (response.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
+            } else if (response.equals("list")) {
+                for (int i = 1; i <= numTasks; i++) {
+                    System.out.println("    " + i + ". " + tasks[i - 1]);
+                }
             } else {
-                System.out.println("    " + response);
+                tasks[numTasks] = response;
+                numTasks++;
+                System.out.println("    added: " + response);
             }
             FriendlyBot.printHorizontalBar();
         }
