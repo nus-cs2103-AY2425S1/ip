01a17dc77461ade1a0a79be9f8ac5a58ff75d975
Lim Yixiang <limyixiang2@gmail.com>
2024-08-19 21:40:49 +0800

    Added support for deleting tasks in the list. Changed the data structure used for storing tasks from using Array to ArrayList.

--- src/main/java/FriendlyBot.java
+++ src/main/java/FriendlyBot.java
@@ -1,9 +1,9 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.lang.NumberFormatException;
+import java.util.ArrayList;
 
 public class FriendlyBot {
-    private static Task[] tasks;
+    private static ArrayList<Task> tasks;
     private static int numTasks;
 
     private static void printHorizontalBar() {
@@ -13,7 +13,7 @@ public class FriendlyBot {
     public static void main(String[] args) {
         // Initialise variables
         Scanner reader = new Scanner(System.in);
-        tasks = new Task[100];
+        tasks = new ArrayList<>();
         numTasks = 0;
         // Start of Chat Bot
         FriendlyBot.printHorizontalBar();
@@ -30,7 +30,7 @@ public class FriendlyBot {
             } else if (response.equals("list")) {
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 1; i <= numTasks; i++) {
-                    Task task = tasks[i - 1];
+                    Task task = tasks.get(i - 1);
                     System.out.println("    " + i + "." + task.toString());
                 }
             } else if (response.startsWith("mark")) {
@@ -39,7 +39,7 @@ public class FriendlyBot {
                     if (index > numTasks) {
                         System.out.println("    There's no such task yet!");
                     } else {
-                        Task task = tasks[index - 1];
+                        Task task = tasks.get(index - 1);
                         task.markAsDone();
                         System.out.println("    Nice! I've marked this task as done:");
                         System.out.println("      " + task.toString());
@@ -53,10 +53,31 @@ public class FriendlyBot {
                     if (index > numTasks) {
                         System.out.println("    There's no such task yet!");
                     } else {
-                        Task task = tasks[index - 1];
+                        Task task = tasks.get(index - 1);
                         task.markAsUndone();
                         System.out.println("    OK, I've marked this task as not done yet:");
                         System.out.println("      " + task.toString());
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("    Please input a valid task index!");
+                }
+            } else if (response.startsWith("delete")) {
+                try {
+                    int index = Integer.parseInt(response.split(" ")[1]);
+                    if (index > numTasks) {
+                        System.out.println("    There's no such task yet!");
+                    } else {
+                        Task task = tasks.remove(index - 1);
+                        numTasks--;
+                        System.out.println("    Noted. I've removed this task:");
+                        System.out.println("      " + task.toString());
+                        if (numTasks == 1) {
+                            System.out.println("    Now you have 1 task in the list.");
+                        } else if (numTasks == 0) {
+                            System.out.println("    You have no more tasks!");
+                        } else {
+                            System.out.println("    Now you have " + numTasks + " tasks in the list.");
+                        }
                     }
                 } catch (NumberFormatException e) {
                     System.out.println("    Please input a valid task index!");
@@ -65,7 +86,7 @@ public class FriendlyBot {
                 try {
                     String input = response.split("todo ", 2)[1];
                     Task newTask = new ToDo(input);
-                    tasks[numTasks] = newTask;
+                    tasks.add(newTask);
                     numTasks++;
                     System.out.println("    Got it. I've added this task:");
                     System.out.println("      " + newTask.toString());
@@ -82,7 +103,7 @@ public class FriendlyBot {
                     String input = response.split("deadline ", 2)[1];
                     String[] descriptions = input.split(" /by ");
                     Task newTask = new Deadline(descriptions[0], descriptions[1]);
-                    tasks[numTasks] = newTask;
+                    tasks.add(newTask);
                     numTasks++;
                     System.out.println("    Got it. I've added this task:");
                     System.out.println("      " + newTask.toString());
@@ -99,7 +120,7 @@ public class FriendlyBot {
                     String input = response.split("event ", 2)[1];
                     String[] descriptions = input.split(" /from | /to ");
                     Task newTask = new Event(descriptions[0], descriptions[1], descriptions[2]);
-                    tasks[numTasks] = newTask;
+                    tasks.add(newTask);
                     numTasks++;
                     System.out.println("    Got it. I've added this task:");
                     System.out.println("      " + newTask.toString());
@@ -112,7 +133,7 @@ public class FriendlyBot {
                     System.out.println("    Please follow this format: event {task_description} /from {date} /to {date}");
                 }
             } else {
-                System.out.println("OOPS!! I'm sorry, that's not a command :-(");
+                System.out.println("    OOPS!! I'm sorry, that's not a command :-(");
             }
             FriendlyBot.printHorizontalBar();
         }
