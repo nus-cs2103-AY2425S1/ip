d5753bcae9494f25906adbc04dc0ef1ccd3db006
JagdeepSinghNUS <E1186181@u.nus.edu>
2024-08-19 13:59:44 +0800

    Update Jag.java Echo

--- src/main/java/Jag.java
+++ src/main/java/Jag.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class Jag {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = """
                 ------------------
                 Hello! I'm Jag
                 What can I do for you?
-                ------------------
-                Bye. Hope ot see you again soon!
                 ------------------""";
+
+        String bye = """
+                ------------------
+                Bye. Hope to see you again soon!
+                ------------------""";
+
         System.out.println(logo);
+        String answer = scanner.nextLine();
+
+        System.out.println("------------------");
+        System.out.println(answer);
+        System.out.println("------------------");
+
+        while (!answer.equals("bye")) {
+            answer = scanner.nextLine();
+            System.out.println("------------------");
+            System.out.println(answer);
+            System.out.println("------------------");
+        }
+
+        System.out.println(bye);
+        scanner.close();
+
     }
 }
