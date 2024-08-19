7619ef3ca29e68c9b2e182b3f9fe46adcb56a995
GavinSin <51268353+GavinSin@users.noreply.github.com>
2024-08-19 17:43:48 +0800

    Refactor class Duke to Mira.
    This is to reflect the new chat bot name with new welcome messages.
    
    Other files with word 'Duke' is replaced with 'Mira' as well.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mira User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mira.java
@@ -0,0 +1,15 @@
+public class Mira {
+    public static void main(String[] args) {
+        String helloMessage = """
+ ____________________________________________________________
+ Hello! I'm Mira
+ What can I do for you?
+ ____________________________________________________________
+ """;
+        String exitMessage = """
+Bye. Hope to see you again soon!
+____________________________________________________________
+""";
+        System.out.println(helloMessage + exitMessage);
+    }
+}
