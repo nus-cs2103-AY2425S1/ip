109ba4ca6cdf6a448dc92a562641f7625dc284f7
Potatoad88 <>
2024-08-19 14:12:30 +0800

    Rename, Greet, Exit

---
+++ src/main/java/DemureBot.java
@@ -0,0 +1,11 @@
+public class DemureBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+            " Hello! I'm DemureBot\n" +
+            " What can I do for you?\n" +
+            "____________________________________________________________\n" +
+            " Bye. Hope to see you again soon!\n" +
+            "____________________________________________________________\n" +
+            "\n");
+    }
+}
