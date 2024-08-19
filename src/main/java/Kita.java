422540fca1e80a5f5bdd6b0e0474668b15651e44
tkaixiang <tkaixiang@gmail.com>
2024-08-19 16:17:33 +0800

    Level_0

---
+++ src/main/java/Kita.java
@@ -0,0 +1,11 @@
+public class Kita {
+    private static String lines = "____________________________________________________________\n";
+    public static void main(String[] args) {
+        System.out.println(lines);
+        System.out.println(" Hello! I'm Kita");
+        System.out.println(" What can I do for you?");
+        System.out.println(lines);
+        System.out.println(" Bye. Hope to see you again soon!\n");
+        System.out.println(lines);
+    }
+}
