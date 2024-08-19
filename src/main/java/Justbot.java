768e42e4886ffa2b20a351ab325772cb92c1e222
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-08-19 10:50:51 +0800

    Add Echo feature for Justbot

--- src/main/java/Justbot.java
+++ src/main/java/Justbot.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Justbot {
     public static void main(String[] args) {
         final String chatbotName = "JustBot";
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("------------------------------------------");
         System.out.println("Hello I'm " + chatbotName);
         System.out.println("What can I do for you?");
         System.out.println("------------------------------------------");
+        String input = "";
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("------------------------------------------");
+                break;
+            }
+            System.out.println("------------------------------------------");
+            System.out.println(input);
+            System.out.println("------------------------------------------");
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------------------");
 
