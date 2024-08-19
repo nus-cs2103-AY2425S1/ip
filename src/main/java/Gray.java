b537fb46a76330f9ddbeb4cba06c5f99e11dc5fd
Yan Hwee <yanhweeyee@gmail.com>
2024-08-20 00:00:34 +0800

    Add echo feature

--- src/main/java/Gray.java
+++ src/main/java/Gray.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Gray {
     public static void main(String[] args) {
-        System.out.println("""
+        Scanner reader = new Scanner(System.in);
+        say("""
                 Hello! I'm Gray.
-                What can I do for you?
+                What can I do for you?""");
+        while (true) {
+            String user = reader.next();
+            if (user == null || user.equals("bye")) break;
+            say(user);
+        }
+        say("Bye. Hope to see you again soon!");
+    }
 
-                Bye. Hope to see you again soon!""");
+    private static void say(String text) {
+        System.out.println("\t____________________________________________________________");
+        System.out.print("\t");
+        System.out.println(text.replace("\n", "\n\t"));
+        System.out.println("\t____________________________________________________________");
     }
 }
