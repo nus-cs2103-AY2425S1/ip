662da587cf70e00126f9afb72c54cc1b207665bf
Yan Hwee <yanhweeyee@gmail.com>
2024-08-19 23:46:07 +0800

    Rename chatbot to gray

---
+++ src/main/java/Gray.java
@@ -0,0 +1,9 @@
+public class Gray {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hello! I'm Gray.
+                What can I do for you?
+
+                Bye. Hope to see you again soon!""");
+    }
+}
