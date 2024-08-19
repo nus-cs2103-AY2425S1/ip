f4d0f788b9f0fc6fe263687b79ca09ffa6a93306
ITLimJiaWei <73027268+ITLimJiaWei@users.noreply.github.com>
2024-08-19 11:53:12 +0800

    Update task to be abstract and added toString method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public class Task {
+public abstract class Task {
 
     protected String description;
     protected boolean isDone;
@@ -18,6 +18,11 @@ public class Task {
 
     public String getDesc() {
         return description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + description;
     }
 
 }
