fcd949dc17dbfb6289fd2e31c569ead33c7d86ae
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-08-20 01:41:17 +0800

    Renamed Duke to Wen Jie and added his greeting + exit

---
+++ src/main/java/WenJie.java
@@ -0,0 +1,12 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____________________________________________________________\n" +
+                " Hello! I'm Wen Jie\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n\n"
+
+        System.out.println(logo);
+    }
+}
