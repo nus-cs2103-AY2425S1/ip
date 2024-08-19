3543d942f1c6e8b7abdd9061862a1abcecf45eab
lrongyi <iygnormal@gmail.com>
2024-08-19 09:51:24 +0800

    Echo

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 public class Dook {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String separator = "____________________________________________________________";
         String greeting = "Hello! I'm Dook\nWhat can I do for you?\n" + separator;
         String exit = "Bye. Hope to see you again soon!\n" + separator;
 
         System.out.println(separator);
         System.out.println(greeting);
-        System.out.print(exit);
+
+        String response;
+
+        while (true) {
+            response = scanner.nextLine();
+            if (response.equals("bye")) {
+                System.out.println(separator);
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(separator);
+                System.out.println(response);
+                System.out.println(separator);
+            }
+        }
     }
 }
