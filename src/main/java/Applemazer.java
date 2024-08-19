5e9eedb162825d8eb208c8519332f4223145cf6c
DESKTOP-RRESQ08\User <nicchan0207@gmail.com>
2024-08-20 01:35:10 +0800

    Added support for echo and removed horizontal spacers. Reformatted greeting and farewell into functions.

--- src/main/java/Applemazer.java
+++ src/main/java/Applemazer.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Applemazer {
-    public static void main(String[] args) {
-        String spacer = "____________________________________________________________";
-        String greeting = String.format("%s\nHello! I'm Applemazer.\nWhat can I do for you?\n%s", spacer, spacer);
-        String farewell = String.format("Bye. Hope to see you again soon!\n%s", spacer);
+
+    private void greeting() {
+        String greeting = "Hello! I'm Applemazer.\nWhat can I do for you?\n";
         System.out.println(greeting);
+    }
+
+    private void farewell() {
+        String farewell = "Bye. Hope to see you again soon!";
         System.out.println(farewell);
+    }
+
+    public static void main(String[] args) {
+        Applemazer chatBot = new Applemazer();
+        chatBot.greeting();
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command + "\n");
+            command = sc.nextLine();
+        }
+
+        chatBot.farewell();
     }
 }
