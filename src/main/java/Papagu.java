b9cb939b68a588b89b45ef8175109369a324a206
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-08-19 14:57:01 +0800

    Add echo functionality

--- src/main/java/Papagu.java
+++ src/main/java/Papagu.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Papagu {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello from Papagu!");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+            userInput = scanner.nextLine();
+        } 
         System.out.println("Bye! Hope to see you again soon!");
     }
 }
