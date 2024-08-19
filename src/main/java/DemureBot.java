bc824e3f83c7a917c1ed7bb21140a43b5cf38e97
Potatoad88 <>
2024-08-19 15:01:30 +0800

    Echo

--- src/main/java/DemureBot.java
+++ src/main/java/DemureBot.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class DemureBot {
     public static void main(String[] args) {
+        boolean finished = false;
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
             " Hello! I'm DemureBot\n" +
             " What can I do for you?\n" +
             "____________________________________________________________\n" +
+            "\n"
+        );
+        while (!finished) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                finished = true;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        command +
+                        "\n" +
+                        "____________________________________________________________\n" +
+                        "\n"
+                );
+            }
+        }
+        System.out.println("____________________________________________________________\n" +
             " Bye. Hope to see you again soon!\n" +
             "____________________________________________________________\n" +
-            "\n");
+            "\n"
+        );
     }
 }
