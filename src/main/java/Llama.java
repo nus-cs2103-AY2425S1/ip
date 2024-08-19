9eea56a952956f06ffda4d5aebf4b1d49990128b
bmanara <bmanaranus@gmail.com>
2024-08-19 14:19:43 +0800

    Add exit message

--- src/main/java/Llama.java
+++ src/main/java/Llama.java
@@ -1,7 +1,16 @@
 public class Llama {
     public static void main(String[] args) {
         String logo = "";
+        String hr = "____________________________________________________________" ;
+
+        // Initializing message
+        System.out.println(hr);
         System.out.println("Hello! I'm Llama!\n" + logo);
         System.out.println("What can I do for you?");
+
+        // Exit message
+        System.out.println(hr);
+        System.out.println("Baaaaaa byeeee. Come baaaaack soon!");
+        
     }
 }
