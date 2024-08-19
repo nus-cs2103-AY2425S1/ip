7118c222038a20777e7bab6089ee79573c6c3187
JagdeepSinghNUS <E1186181@u.nus.edu>
2024-08-19 13:56:12 +0800

    Update Jag.java Rename, Greet, Exit

--- src/main/java/Jag.java
+++ src/main/java/Jag.java
@@ -1,10 +1,12 @@
-public class Duke {
+public class Jag {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = """
+                ------------------
+                Hello! I'm Jag
+                What can I do for you?
+                ------------------
+                Bye. Hope ot see you again soon!
+                ------------------""";
+        System.out.println(logo);
     }
 }
