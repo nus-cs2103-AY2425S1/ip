b663bdc98e74015c6b1e7709d1ae9b2a6ef8706b
Karthikraj Sivakumar <techraj0207@gmail.com>
2024-08-19 13:12:04 +0800

    Mark as Done

--- src/main/java/ProYapper.java
+++ src/main/java/ProYapper.java
@@ -9,7 +9,7 @@ public class ProYapper {
         String greeting = "Hello! I am Pro Yapper!\nWhat can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!\n";
 
-        List<String> list = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
 
         System.out.println(greeting);
         while (true) {
@@ -18,14 +18,35 @@ public class ProYapper {
                 System.out.println("\n" + goodbye);
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("\n");
-                for (int i = 0; i < list.size(); i++){
+                System.out.println("\nHere are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
                     int lstNum = i + 1;
-                    System.out.println(lstNum + ". " + list.get(i));
+                    Task next = taskList.get(i);
+                    String name = next.getDescription();
+                    String icon = next.getStatusIcon();
+                    System.out.println(lstNum + ".[" + icon + "] " + name);
                 }
                 System.out.println("\n");
+            } else if (userInput.startsWith("mark")) {
+                System.out.println("\nNice! I've marked this task as done:");
+                String[] parts = userInput.split(" ");
+                int lstNum = Integer.parseInt(parts[1]);
+                Task marked = taskList.get(lstNum - 1);
+                marked.markAsDone();
+                String name = marked.getDescription();
+                String icon = marked.getStatusIcon();
+                System.out.println("[" + icon + "] " + name + "\n");
+            } else if (userInput.startsWith("unmark")) {
+                System.out.println("\nOK, I've marked this task as not done yet:");
+                String[] parts = userInput.split(" ");
+                int lstNum = Integer.parseInt(parts[1]);
+                Task marked = taskList.get(lstNum - 1);
+                marked.markAsUndone();
+                String name = marked.getDescription();
+                String icon = marked.getStatusIcon();
+                System.out.println("[" + icon + "] " + name + "\n");
             } else {
-                list.add(userInput);
+                taskList.add(new Task(userInput));
                 System.out.println("\nadded: " + userInput + "\n");
             }
         }
