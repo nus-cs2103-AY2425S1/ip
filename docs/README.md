0374af949c18a2d02b71815c0015e094088f541e
matthewyip1511 <e0938566@u.nus.ed>
2024-08-19 11:03:02 +0800

    Set up level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# PoChat User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/PoChat.java
+++ src/main/java/PoChat.java
@@ -1,10 +1,7 @@
-public class Duke {
+public class PoChat {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String introMessage = "Hello! I'm PoChat, the chatbot in your pocket.\nWhat can I do for you?";
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(introMessage + "\n\n" + goodbyeMessage);
     }
 }
