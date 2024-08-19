4aa3877f7789387fb41e0061d8de38f02593687c
Ved <ved.t.joshi@gmail.com>
2024-08-19 21:48:40 +0800

    Add run method as driver code, implemented testing

--- src/main/java/Agave.java
+++ src/main/java/Agave.java
@@ -1,7 +1,30 @@
 public class Agave {
 
+    private UserInterface ui;
+    private boolean isRunning;
 
+    public Agave() {
+        this.ui = new UserInterface();
+        this.isRunning = true;
+    }
+
+    public void run() {
+        ui.showWelcome();
+
+        while (isRunning) {
+            String userInput = ui.getUserInput("Enter a command: ");
+            System.out.println(userInput);
+            if (userInput.equalsIgnoreCase("bye")) {
+                isRunning = false;
+                ui.showBye();
+            } else {
+                ui.showEcho(userInput);
+            }
+        }
+    }
 
     public static void main(String[] args) {
+        Agave agave = new Agave();
+        agave.run();
     }
 }
