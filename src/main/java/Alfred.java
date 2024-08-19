4a3a4e4a6f638777cd18aeac86ed6de34823281f
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2024-08-19 23:48:44 +0800

    Replaced Duke with Alfred. Completed Level 0 implementation: Rename, Greet, Exit

---
+++ src/main/java/Alfred.java
@@ -0,0 +1,7 @@
+public class Alfred {
+    public static void main(String[] args) {
+        System.out.println("Good day Sir. I trust you are well. ");
+        System.out.println("Might I offer you some tea, or perhaps something stronger to suit the occasion?");
+        System.out.println("Farewell Sir. Should you need anything, you know where to find me");
+    }
+}
