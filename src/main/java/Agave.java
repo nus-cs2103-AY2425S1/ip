5e065016a5ed59c9884bb9adc608bd9fe3d01a35
Ved <ved.t.joshi@gmail.com>
2024-08-19 20:27:17 +0800

    Level 0, new name (Agave), greeting and exit

---
+++ src/main/java/Agave.java
@@ -0,0 +1,16 @@
+public class Agave {
+
+    public static void makeLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        makeLine();
+        System.out.println("Hello! I am Agave.\n" +
+                "What can I do for you?\n");
+        makeLine();
+        System.out.println("Bye! Hope to see you again soon!\n");
+        makeLine();
+
+    }
+}
