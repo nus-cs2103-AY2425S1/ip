6fe7115ff899d9e1842a68bca2f4d1f9cc436f2f
anselmlong <anselmpius@gmail.com>
2024-08-19 20:22:40 +0800

    Rename project and added exit message

---
+++ src/main/java/Sigma.java
@@ -0,0 +1,15 @@
+public class Sigma {
+    public static void main(String[] args) {
+        String line = "----------------------------------------------------------------------------------------------";
+        String name = "SIGMA";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name + "\nLooking forward to slaying with you!");
+        System.out.println(line);
+        exit();
+        System.out.println(line);
+    }
+
+    private static void exit() {
+        System.out.println("What the sigma? You're leaving so soon? Bye chat, see you again!");
+    }
+}
