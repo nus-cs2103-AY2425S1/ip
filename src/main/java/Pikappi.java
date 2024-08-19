5ce0157c838544497b812b46c96d1c0c69a4ce22
xqtann <xqxinquan@gmail.com>
2024-08-19 20:52:54 +0800

    add Pikappi.java

---
+++ src/main/java/Pikappi.java
@@ -0,0 +1,9 @@
+public class Pikappi {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Pika! I'm Pikappi!\nWhat can I do for you?\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Pi-kapi! See you again~\n");
+        System.out.println("____________________________________________________________");
+    }
+}
