b581969761c40b67d66091ea6c3434bb7c1b2bdd
nihalramesh12629 <>
2024-08-19 19:22:02 +0800

    Level-1 Completed

--- src/main/java/LeBron.java
+++ src/main/java/LeBron.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class LeBron {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm LeBron");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        String name = "LeBron";
+        System.out.println("------------------------");
+        System.out.println(String.format("Hello! I'm %s", name));
+        System.out.println("What can I do for you?");
+        System.out.println("------------------------");
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(String.format("%s: Bye! I'm leaving now.", name));
+                System.out.println("------------------------");
+                break;
+            } else {
+                System.out.println(String.format("%s: %s", name, input));
+                System.out.println("------------------------");
+            }
+        }
+
     }
 }
