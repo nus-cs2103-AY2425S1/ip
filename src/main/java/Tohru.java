b3f4b5ef980d0f780abeeb4209962f5f01732c5e
Dong Qianbo <pencilate@outlook.com>
2024-08-19 15:58:48 +0800

    Add support for mark and unmark commands

--- src/main/java/Tohru.java
+++ src/main/java/Tohru.java
@@ -16,16 +16,41 @@ public class Tohru {
         // Process User response
         while (!toExit && userInput.hasNext()) {
             String prompt = userInput.nextLine();
+
+            String[] dissectedPrompt = prompt.split(" ", 2);
+            String command = dissectedPrompt[0];
+
             printDivider();
-            switch (prompt) {
+            switch (command) {
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 toExit = true;
                 break;
             case "list":
+                System.out.println("These are on your todo:");
                 for (String todoItem : todoList.listItems()) {
                     System.out.println(todoItem);
                 }
+                break;
+            case "mark":
+                String markArgument = dissectedPrompt[1];
+                int markItemIndex = Integer.parseInt(markArgument) - 1;
+                if (todoList.markComplete(markItemIndex)) {
+                    System.out.println("Alright! I have set this task as done:");
+                } else {
+                    System.out.println("Unable to set this task as done:");
+                }
+                System.out.println(todoList.getItemStatus(markItemIndex));
+                break;
+            case "unmark":
+                String unmarkArgument = dissectedPrompt[1];
+                int unmarkItemIndex = Integer.parseInt(unmarkArgument) - 1;
+                if (todoList.markIncomplete(unmarkItemIndex)) {
+                    System.out.println("Alright! I have set this task as not done:");
+                } else {
+                    System.out.println("Unable to set this task as not done:");
+                }
+                System.out.println(todoList.getItemStatus(unmarkItemIndex));
                 break;
             default:
                 if (todoList.addItem(prompt)) {
