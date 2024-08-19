baf9dba0288f679ffc36d3c24cffeb3263d164a2
zwe <zwezeya02@gmail.com>
2024-08-19 23:34:21 +0800

    Add echo feature based on user input

--- src/main/java/Avo.java
+++ src/main/java/Avo.java
@@ -1,8 +1,18 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Avo {
     public static void main(String[] args) {
         String greetingMessage = "Hello, I am Avo.\nWhat can I do for you?";
         String exitMessage = "Bye. Hope to see you again soon!";
         System.out.println(greetingMessage);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println(userInput);
+            if (Objects.equals(userInput, "exit")) {
+                break;
+            }
+        }
         System.out.println(exitMessage);
     }
 }
