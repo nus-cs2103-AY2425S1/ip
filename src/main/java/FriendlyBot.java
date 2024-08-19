f7827e7f5febc124dc01161f1e357333d933a91f
Lim Yixiang <limyixiang2@gmail.com>
2024-08-19 14:21:45 +0800

    Improved skeletal version: Echoes commands entered by user and exits upon "bye"

--- src/main/java/FriendlyBot.java
+++ src/main/java/FriendlyBot.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class FriendlyBot {
     private static void printHorizontalBar() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         FriendlyBot.printHorizontalBar();
-        System.out.println("Hello! I'm Friendly Bot");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Friendly Bot");
+        System.out.println("    What can I do for you?");
         FriendlyBot.printHorizontalBar();
-        System.out.println("Bye. Hope to see you again soon!");
-        FriendlyBot.printHorizontalBar();
+        String response = "";
+        while (!response.equals("bye")) {
+            response = reader.next();
+            FriendlyBot.printHorizontalBar();
+            if (response.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+            } else {
+                System.out.println("    " + response);
+            }
+            FriendlyBot.printHorizontalBar();
+        }
     }
 }
