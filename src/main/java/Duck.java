cf5730e92d3d1070e0e7139ee0dbfdd5c6a25d69
Feng1231 <chenfeng021231@gmail.com>
2024-08-19 21:54:57 +0800

    Echo user input

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duck {
     public static void main(String[] args) {
         String logo = """
@@ -21,7 +23,20 @@ public class Duck {
 
         System.out.println("Hello! I'm Duck");
         System.out.println("What can I do for you? \n");
+
+        getInputTillBye();
         System.out.println("Bye. Hope to see you again soon!");
 
+    }
+
+    public static void getInputTillBye() {
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        while (!message.equalsIgnoreCase("bye")) {
+
+            System.out.println(message + "\n");
+            in = new Scanner(System.in);
+            message = in.nextLine();
+        }
     }
 }
