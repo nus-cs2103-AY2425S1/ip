cd1957fcbd5b8c03a32c58ca6cc39e1f539ed390
Lee_Wei_Chong <leechewie13@gmail.com>
2024-08-19 19:26:08 +0800

    Get chatbot to greet and say goodbye

--- src/main/java/MrIncredible.java
+++ src/main/java/MrIncredible.java
@@ -1,6 +1,10 @@
 public class MrIncredible {
     public static void main(String[] args) {
         String logo = "MrIncredible";
+        String greeting = "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
+        System.out.println(greeting);
+        System.out.println(exit);
     }
 }
