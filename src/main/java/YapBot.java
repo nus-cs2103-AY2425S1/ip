33d3429694708c36aeed5ca48288f3005fa99fbf
NicT5 <nicctang@gmail.com>
2024-08-19 17:17:12 +0800

    Renamed Duke to YapBot and changed greeting message

---
+++ src/main/java/YapBot.java
@@ -0,0 +1,5 @@
+public class YapBot {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am YapBot. \nHow can I help ? \n---------- \nAlright, enough yapping for one day.");
+    }
+}
