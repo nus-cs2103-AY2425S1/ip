28425b40a945d847669cdf828d74ca3a81494750
thenosewizard <abicharan.b@gmail.com>
2024-08-19 21:05:08 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
         String greet = "Hello! I'm Bob \n"
                 + "What can I do for you? \n";
         String bye = "Bye. Hope to see you again soon!";
+        String input = "";
+
+        boolean bool = true;
 
         System.out.println(greet);
-        System.out.println(bye);
+        while (bool) {
+            input = reader.nextLine();
+
+            if(input.equals("bye")){
+                System.out.println(bye);
+                break;
+            }
+
+            System.out.println(input);
+
+        }
     }
 }
