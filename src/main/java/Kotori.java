fa54491b73a66c307bbed693c33c65711f11bfb0
Jianwen <jianwenlei2003@gmail.com>
2024-08-19 19:09:46 +0800

    Level 2.0 is completed, Now the chatbot is able to keep track of a list of events.

--- src/main/java/Kotori.java
+++ src/main/java/Kotori.java
@@ -1,16 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kotori {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
         printGreeting();
         Scanner s = new Scanner(System.in);
         while (s.hasNext()){
-            String input = s.next();
+            String input = s.nextLine();
             if (input.equals("bye")) {
                 printExit();
                 break;
             } else {
-                printMessage(input);
+                list.add(input);
+                printList(list);
             }
         }
 
@@ -25,6 +29,14 @@ public class Kotori {
     public static void printMessage(String input) {
         printLine();
         System.out.println("    " + input);
+        printLine();
+    }
+
+    public static void printList(List<String> list) {
+        printLine();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(String.format("    %s. %s",i + 1, list.get(i)));
+        }
         printLine();
     }
 
