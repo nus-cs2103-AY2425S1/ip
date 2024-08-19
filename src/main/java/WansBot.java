d94f2d3b5968d6c383d7d51bd84e826c216039a3
bigismols <bigismols239@gmail.com>
2024-08-19 19:56:26 +0800

    Added the echo function and changed some bot messages

--- src/main/java/WansBot.java
+++ src/main/java/WansBot.java
@@ -9,27 +9,33 @@ public class WansBot {
 
         String hr = "----------------------------------------------------------------------";
 
-        System.out.println(hr + "\nHey, I'm\n" + logo);
-        System.out.println("What can I do for you?\n" + hr);
+        System.out.println(hr + "\nWans:");
+        System.out.println("Hey, I'm\n" + logo);
+        System.out.println("Can I help? (even though I lowkey don't want to)\n" + hr);
 
         while (true) {
+            System.out.println("User: ");
             String userInput = sc.nextLine();
 
             if (userInput.equalsIgnoreCase("")) {
 
             } else if (userInput.equalsIgnoreCase("")) {
 
-            } else if (userInput.equalsIgnoreCase("exit")) {
+            } else if (userInput.equalsIgnoreCase("bye")) {
                 String exit = "|  _ \\ \\   / /  ____|"
                         + "\n| |_) \\ \\_/ /| |__"
                         + "\n|  _ < \\   / |  __|"
                         + "\n| |_) | | |  | |____"
                         + "\n|____/  |_|  |______";
+                System.out.println("Wans: ");
                 System.out.println(exit + "\nWe won't miss you :(");
                 System.out.println(hr);
                 System.exit(0);
+            } else {
+                System.out.println("Wans:");
+                System.out.println(userInput);
+                System.out.println(hr);
             }
-            System.out.println(hr);
         }
     }
 }
