0d1034876d100c83f10d05fe64f236abc3b32371
engyuhan <e1136866@u.nus.edu>
2024-08-19 16:29:31 +0800

    feat: change Task to concrete class to replace BasicTask

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,44 @@
-public interface Task {
+public class Task {
+    /**
+     * Whether the task is completed.
+     */
+    private boolean isCompleted;
+
+    /**
+     * The name of the task.
+     */
+    private String name;
+
+    /**
+     * Constructor for a new basic task.
+     * @param name the name of the task
+     */
+    public Task(String name) {
+        this.name = name;
+        this.isCompleted = false;
+    }
+
     /**
      * Mark the task as completed or not completed.
      * @param isCompleted whether the task is completed
      */
-    void mark(boolean isCompleted);
+    public void mark(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
 
     /**
-     * Get the task data as a string.
-     * @return the task data as a string
+     * Get the string representation of the task.
+     * @return the string representation of the task.
      */
-    String toString();
+    public String toString() {
+        String string;
+        if (this.isCompleted) {
+            string = "[X] ";
+        } else {
+            string = "[ ] ";
+        }
+
+        string = string + this.name;
+        return string;
+    }
 }
