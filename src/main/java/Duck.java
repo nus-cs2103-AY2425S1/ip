5aa665b6d517f0e9434cf55bfa6bb319756e0f14
Feng1231 <chenfeng021231@gmail.com>
2024-08-19 21:37:59 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Duck.java
@@ -0,0 +1,27 @@
+public class Duck {
+    public static void main(String[] args) {
+        String logo = """
+                        ,---,                                  ,-. \s
+                      .'  .' `\\                            ,--/ /| \s
+                    ,---.'     \\          ,--,           ,--. :/ | \s
+                    |   |  .`\\  |       ,'_ /|           :  : ' /  \s
+                    :   : |  '  |  .--. |  | :    ,---.  |  '  /   \s
+                    |   ' '  ;  :,'_ /| :  . |   /     \\ '  |  :   \s
+                    '   | ;  .  ||  ' | |  . .  /    / ' |  |   \\  \s
+                    |   | :  |  '|  | ' |  | | .    ' /  '  : |. \\ \s
+                    '   : | /  ; :  | : ;  ; | '   ; :__ |  | ' \\ \\\s
+                    |   | '` ,/  '  :  `--'   \\'   | '.'|'  : |--' \s
+                    ;   :  .'    :  ,      .-./|   :    :;  |,'    \s
+                    |   ,.'       `--`----'     \\   \\  / '--'      \s
+                    '---'                        `----'            \s
+                """;
+
+
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duck");
+        System.out.println("What can I do for you? \n");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
