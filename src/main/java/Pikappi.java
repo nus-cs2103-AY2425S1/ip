f8fe42e9780034de9049e323c46efd34c5d8134c
xqtann <xqxinquan@gmail.com>
2024-08-19 23:33:36 +0800

    change to oop

--- src/main/java/Pikappi.java
+++ src/main/java/Pikappi.java
@@ -1,40 +1,58 @@
 import java.util.Scanner;
 
 public class Pikappi {
-    public static void main(String[] args) {
+    static Scanner reader = new Scanner(System.in);
+    static String command;
+    static String[] tasks = new String[100];
+    static int numTasks = 0;
+
+    public static void greet() {
         System.out.println("____________________________________________________________");
         System.out.println("Pika! I'm Pikappi!\nWhat can I do for you?\n");
         System.out.println("____________________________________________________________");
-        Scanner reader = new Scanner(System.in);
-        String command;
-        String[] tasks = new String[100];
-        int numTasks = 0;
+    }
+
+    public static void goodbye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Pi-kapi! See you again~\n");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void addTask(String task) {
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + task);
+        System.out.println("____________________________________________________________");
+        tasks[numTasks] = task;
+        numTasks++;
+    }
+    
+    public static void listTasks() {
+        System.out.println("____________________________________________________________");
+        if (numTasks == 0) {
+            System.out.println("Pika-ka! You have no tasks!");
+        }
+        for (int i = 0; i < numTasks; i++) {
+            if (tasks[i] != null) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            } else {
+                break;
+            }
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        greet();
+
         while (true) {
             command = reader.nextLine();
             if (command.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Pi-kapi! See you again~\n");
-                System.out.println("____________________________________________________________");
+                goodbye();
                 return;
             } else if (command.equals("list")) {
-                System.out.println("____________________________________________________________");
-                if (numTasks == 0) {
-                    System.out.println("Pika-ka! You have no tasks!");
-                }
-                for (int i = 0; i < numTasks; i++) {
-                    if (tasks[i] != null) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
-                    } else {
-                        break;
-                    }
-                }
-                System.out.println("____________________________________________________________");
+                listTasks();
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + command);
-                tasks[numTasks] = command;
-                numTasks++;
-                System.out.println("____________________________________________________________");
+                addTask(command);
             }
         }
     }
