d9f9c0c45afc0b73046be18fe43cda77bd1076d9
ylchin <chin.yiling0715@gmail.com>
2024-08-19 15:10:50 +0800

    Added support for echo

--- src/main/java/Elara.java
+++ src/main/java/Elara.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Elara {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Elara");
         System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again!")
+
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.toLowerCase().strip().equals("bye")) {
+                break;
+            }
+            System.out.println(text);
+        }
+
+        System.out.println("Bye. Hope to see you again!");
     }
 }
