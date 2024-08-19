372073c2d935b96bb63cd49e9cce3dc32df4ce16
Feng1231 <chenfeng021231@gmail.com>
2024-08-19 23:09:26 +0800

    Mark as Done

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Duck {
-    static ArrayList<String> ls = new ArrayList<>();
+    static ArrayList<Task> ls = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = """
                         ,---,                                  ,-. \s
@@ -36,6 +39,7 @@ public class Duck {
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
         while (!message.equalsIgnoreCase("bye")) {
+
             reactTo(message);
             in = new Scanner(System.in);
             message = in.nextLine();
@@ -45,12 +49,46 @@ public class Duck {
     public static void reactTo(String message) {
         if (message.equalsIgnoreCase("list")) {
             int idx = 1;
-            for (String str : ls) {
-                System.out.println(idx++ + ". " + str);
+            System.out.println("Here are the tasks in your list:");
+            for (Task t : ls) {
+                System.out.println(idx++ + "." + t.toString());
             }
+        } else if (isUpdatingStatus(message)) {
+            updateStatus(message);
         } else {
-            ls.add(message);
+            ls.add(new Task(message));
             System.out.println("added: " + message);
+        }
+    }
+
+    public static boolean isUpdatingStatus(String message) {
+        String[] words = message.split(" ");
+        return words.length == 2
+                && (words[0].equals("mark") || words[0].equals("unmark"))
+                && isInteger(words[1]);
+    }
+
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void updateStatus(String message) {
+        String[] words = message.split(" ");
+        try {
+            if (words[0].equals("mark")) {
+                ls.get(Integer.parseInt(words[1]) - 1).markAsDone();
+            } else {
+                ls.get(Integer.parseInt(words[1]) - 1).markAsIncomplete();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Oops! you have to indicate a valid task index!");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Oops! Index out of bound :( Input a valid task index.");
         }
     }
 
