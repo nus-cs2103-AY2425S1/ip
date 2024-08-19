6296905891920bd3b67513f0eccb89e53d4a3384
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2024-08-19 10:31:31 +0800

    chore: Fix indentation

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,24 +1,24 @@
 public class Task {
 
-  private String task;
-  private boolean isDone = false;
+    private String task;
+    private boolean isDone = false;
 
-  public Task(String task) {
-    this.task = task;
-  }
+    public Task(String task) {
+        this.task = task;
+    }
 
-  @Override
-  public String toString() {
-    String status = isDone ? "[X]" : "[ ]";
-    return String.format("%s  %s", status, this.task);
-  }
+    @Override
+    public String toString() {
+      String status = isDone ? "[X]" : "[ ]";
+      return String.format("%s  %s", status, this.task);
+    }
 
-  public void printTask() {
-    String status = isDone ? "[X]" : "[ ]";
-    System.out.printf("%s  %s", status, this.task);
-  }
+    public void printTask() {
+      String status = isDone ? "[X]" : "[ ]";
+      System.out.printf("%s  %s", status, this.task);
+    }
 
-  public void toggleDone() {
-    this.isDone = !this.isDone;
-  }
+    public void toggleDone() {
+        this.isDone = !this.isDone;
+    }
 }
