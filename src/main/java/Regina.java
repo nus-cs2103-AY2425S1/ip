f1097b736eed0995ac7bbbfd0af83b2fce497713
yooplo <jjwr2002@gmail.com>
2024-08-19 22:38:42 +0800

    Update Regina.java class

--- src/main/java/Regina.java
+++ src/main/java/Regina.java
@@ -3,52 +3,71 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Regina {
-    public static void main(String[] args) {
-        // Name
-        final String NAME = "Regina";
-        final String INDENT = "    ";
-        final String LINE = INDENT + "-------------------------------";
+    final static String NAME = "Regina";
+    final static String INDENT = "    ";
+    final static String LINE = INDENT + "-------------------------------";
+    private final ArrayList<String> listOfTasks;
+    private final Scanner scanner = new Scanner(System.in);
 
-        // Greet
+    public Regina() {
+        listOfTasks = new ArrayList<>();
+    }
+
+    public void greet() {
         System.out.printf(LINE + "\n" + INDENT + "Hello! I'm %s \n" +
                 INDENT + "What can I do for you?\n" + LINE + "\n", NAME);
+    }
 
-        // Create a Scanner object for user input
-        Scanner scanner = new Scanner(System.in);
+    public String readInput() {
+        return this.scanner.nextLine();
+    }
+
+    public void add(String input) {
+        listOfTasks.add(input);
+        System.out.println(LINE + "\n" + INDENT + "added: "
+                + input + "\n" + LINE); // show that input was added
+    }
+
+    public String list() {
+        int length = listOfTasks.size();
+        StringBuilder inputList = new StringBuilder();
+        for (int i = 1; i <= length; i++) {
+            inputList.append(INDENT)
+                    .append(i)
+                    .append(". ")
+                    .append(listOfTasks.get(i - 1))
+                    .append("\n");
+        }
+        return inputList.toString();
+    }
+
+    public void exit() {
+        System.out.println(LINE + "\n" + INDENT +
+                "Bye. Hope to see you again soon!\n" + LINE);
+        this.scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Regina regina = new Regina(); // create instance of Regina chatbot
+        regina.greet(); // greet
         String userInput;
 
-        List<String> list = new ArrayList<>();
-
         while (true) {
-            userInput = scanner.nextLine();   // Read user input
+            userInput = regina.readInput();   // Read user input
             if (userInput.equals("bye")) {
                 break;
             }
 
             // Print out the list
-            int length = list.size();
             if (userInput.equals("list")) {
-                StringBuilder inputList = new StringBuilder();
-                for (int i = 1; i <= length; i++) {
-                    inputList.append(INDENT)
-                            .append(i)
-                            .append(". ")
-                            .append(list.get(i - 1))
-                            .append("\n");
-                }
+                String inputList = regina.list();
                 System.out.println(LINE + "\n" + inputList + LINE);
             } else {
                 // Add input to list
-                list.add(userInput);
-                System.out.println(LINE + "\n" + INDENT + "added: "
-                        + userInput + "\n" + LINE); // show that input was added
+                regina.add(userInput);
             }
         }
-
         // Exit
-        System.out.println(LINE + "\n" + INDENT +
-                "Bye. Hope to see you again soon!\n" + LINE);
-
-        scanner.close();
+        regina.exit();
     }
 }
