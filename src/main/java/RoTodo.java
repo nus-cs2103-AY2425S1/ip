39aa1a2b2bb39407cabe07fafa1b0ae226ce9f7c
Ng Kay Hian <e1113766@u.nus.edu>
2024-08-19 19:54:43 +0800

    Add input echo
    - RoTodo echoes input back to user
    - RoTodo exits program when receiving "bye" input

--- src/main/java/RoTodo.java
+++ src/main/java/RoTodo.java
@@ -7,37 +7,58 @@
            \/
  */
 
+import java.util.Scanner;
+
 public class RoTodo {
-    public static void line() {
-        System.out.println(new String(new char[100]).replace("\0", "-"));
+    public static void line(boolean newLine) {
+        System.out.println("  " + new String(new char[100]).replace("\0", "-"));
+        if (newLine) {
+            System.out.println();
+        }
     }
 
     public static void banner() {
         // Declaring ANSI_COLOR 
         String ansiReset = "\u001B[0m"; 
         String ansiRed = "\u001B[31m"; 
-
-        System.out.println("Hello! I'm \n" 
-            + ("R__________E       __________            __   _____\n"
-            + "R\\______   \\E  ____\\__   ___/____    ___|  | /  _  \\   ____\n"
-            + " R|       _/E /  _ \\ |   |  /  _ \\  /  _   ||  / \\  | /  _ \\   ___\n"
-            + " R|    |   \\E(  <_> ||   | (  <_> |(  <_>  ||  \\_/  |(  <_> | / o \\  _\n"
-            + " R|____|_  /E \\____/ |___|  \\____/  \\_____/  \\_____/  \\____/  \\___/ (_) O o .\n"
-            + "        R\\/E\n").replace("R", ansiRed).replace("E", ansiReset)
-            + "Your very own Robot Todo List!\n"
-            + "How can I help you help yourself?");
+        
+        RoTodo.line(false);
+        System.out.println("    Hello! I'm \n" 
+            + ("    R__________E       __________            __   _____\n"
+            + "    R\\______   \\E  ____\\__   ___/____    ___|  | /  _  \\   ____\n"
+            + "     R|       _/E /  _ \\ |   |  /  _ \\  /  _   ||  / \\  | /  _ \\   ___\n"
+            + "     R|    |   \\E(  <_> ||   | (  <_> |(  <_>  ||  \\_/  |(  <_> | / o \\  _\n"
+            + "     R|____|_  /E \\____/ |___|  \\____/  \\_____/  \\_____/  \\____/  \\___/ (_) O o .\n"
+            + "            R\\/E\n").replace("R", ansiRed).replace("E", ansiReset)
+            + "    Your very own Robot Todo List!\n"
+            + "    How can I help you help yourself?");
+        RoTodo.line(true);
     }
 
     public static void exit() {
-        System.out.println("Bye, remember to finish all your tasks!");
-        RoTodo.line();
+        RoTodo.line(false);
+        System.out.println("    Bye, remember to finish all your tasks!");
+        RoTodo.line(false);
         System.exit(0);
     }
 
     public static void main(String[] args) {
-        RoTodo.line();
         RoTodo.banner();
-        RoTodo.line();
-        RoTodo.exit();
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            switch (input) {
+                case "bye":
+                    RoTodo.exit();
+                    break;
+            
+                default:
+                    RoTodo.line(false);
+                    System.out.println("    " + input);
+                    RoTodo.line(true);
+                    break;
+            }
+        }
     }
 }
