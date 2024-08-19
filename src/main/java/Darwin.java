7398b60d9740e23c73d215d46a25778c2d24f7fa
raageth <chaygareth@gmail.com>
2024-08-19 22:26:58 +0800

    Add echo functionality

--- src/main/java/Darwin.java
+++ src/main/java/Darwin.java
@@ -1,7 +1,31 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Darwin {
     static final String NAME = "Darwin";
+    static final String END = "bye";
+
+    private static void reply(String in) {
+        System.out.println(in);
+        String line = "-".repeat(30);
+        System.out.println(line);
+    }
 
     public static void main(String[] args) {
-        System.out.printf("Hello! I'm %s\nWhat can I do for you?\nBye. Hope to see you again soon!", NAME);
+        String startMsg = String.format("Hello! I'm %s\nWhat can I do for you?", NAME);
+        String endMsg = "Bye. Hope to see you again soon!";
+
+        reply(startMsg);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String in = scanner.nextLine();
+            if (in.equals(END)) {
+                reply(endMsg);
+                break;
+            }
+            reply(in);
+        }
+        scanner.close();
     }
 }
