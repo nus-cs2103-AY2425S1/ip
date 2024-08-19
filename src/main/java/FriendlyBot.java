8354d99c68635089be5b78e1a1608fd8ffac084c
Lim Yixiang <limyixiang2@gmail.com>
2024-08-19 16:14:08 +0800

    Added the ability to mark tasks as done. Added the ability to mark tasks as undone. Catches exceptions where given task index is out of range or when given task index is not an integer.

--- src/main/java/FriendlyBot.java
+++ src/main/java/FriendlyBot.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
+import java.lang.NumberFormatException;
 
 public class FriendlyBot {
-    private static String[] tasks;
+    private static Task[] tasks;
     private static int numTasks;
 
     private static void printHorizontalBar() {
         System.out.println("    ____________________________________________________________");
     }
 
+    private static String makeTaskDescription(Task task) {
+        return "[" + task.getStatusIcon() + "] " + task.description;
+    }
+
     public static void main(String[] args) {
         // Initialise variables
         Scanner reader = new Scanner(System.in);
-        tasks = new String[100];
+        tasks = new Task[100];
         numTasks = 0;
         // Start of Chat Bot
         FriendlyBot.printHorizontalBar();
@@ -26,11 +31,37 @@ public class FriendlyBot {
             if (response.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
             } else if (response.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= numTasks; i++) {
-                    System.out.println("    " + i + ". " + tasks[i - 1]);
+                    Task task = tasks[i - 1];
+                    System.out.println("    " + i + "." + FriendlyBot.makeTaskDescription(task));
+                }
+            } else if (response.startsWith("mark")) {
+                try {
+                    int index = Integer.parseInt(response.split(" ")[1]);
+                    if (index > numTasks) {
+                        System.out.println("    There's no such task yet!");
+                    } else {
+                        Task task = tasks[index - 1];
+                        task.markAsDone();
+                        System.out.println("    Nice! I've marked this task as done:");
+                        System.out.println("      " + FriendlyBot.makeTaskDescription(task));
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("    Please input a valid task index!");
+                }
+            } else if (response.startsWith("unmark")) {
+                int index = Integer.parseInt(response.split(" ")[1]);
+                if (index > numTasks) {
+                    System.out.println("    There's no such task yet!");
+                } else {
+                    Task task = tasks[index - 1];
+                    task.markAsUndone();
+                    System.out.println("    OK, I've marked this task as not done yet:");
+                    System.out.println("      " + FriendlyBot.makeTaskDescription(task));
                 }
             } else {
-                tasks[numTasks] = response;
+                tasks[numTasks] = new Task(response);
                 numTasks++;
                 System.out.println("    added: " + response);
             }
