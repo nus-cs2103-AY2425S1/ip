43a925fd1e95ab2f130b2eff04f1dd272ca7e08f
LAPTOP-20936SS7\kusha <e1121075@u.nus.edu>
2024-08-19 23:40:54 +0800

    Add Echo and Bye functionality

--- src/main/java/Elon.java
+++ src/main/java/Elon.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Elon {
+
+    private static void Echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            System.out.println("-------------------------------------------------------");
+            input = scanner.next();
+        }
+        scanner.close();
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------------------------------");
+    }
     public static void main(String[] args) {
         System.out.println("-------------------------------------------------------");
         System.out.println(" Hello! I'm Elon");
         System.out.println(" What can I do for you?");
         System.out.println("-------------------------------------------------------");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("-------------------------------------------------------");
+        Elon.Echo();
     }
 }
