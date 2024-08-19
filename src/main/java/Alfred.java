3585daab262909621310e59306efb62426e30c2d
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2024-08-20 00:13:28 +0800

    Alfred echos the commands entered by the user, and exits when the user types the command bye

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 public class Alfred {
     public static void main(String[] args) {
-        System.out.println("Good day Sir. I trust you are well. ");
-        System.out.println("Might I offer you some tea, or perhaps something stronger to suit the occasion?");
-        System.out.println("Farewell Sir. Should you need anything, you know where to find me");
+        // Create a Scanner Object
+        Scanner in = new Scanner(System.in);
+
+        // Greet user
+        greet();
+
+        // Echo user input
+        String input = in.nextLine();
+        while (!input.equals("bye")) {
+            echo(input);
+            input = in.nextLine();
+        }
+        farewell();
+    }
+
+    public static void greet() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Good day Sir. I trust you are well.");
+        System.out.println("    Might I offer you some tea, or perhaps something stronger to suit the occasion?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void farewell() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Farewell Sir. Should you need anything, you know where to find me");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void echo(String input) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    " + input);
+        System.out.println("    ____________________________________________________________");
     }
 }
