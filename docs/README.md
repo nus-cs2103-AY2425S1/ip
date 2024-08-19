265c01c54e41af2fe13bf5c3fa3175ac4ea3ebc6
raageth <chaygareth@gmail.com>
2024-08-19 21:57:55 +0800

    Replace Duke with Darwin

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Darwin User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Darwin.java
@@ -0,0 +1,7 @@
+public class Darwin {
+    static final String NAME = "Darwin";
+
+    public static void main(String[] args) {
+        System.out.printf("Hello! I'm %s\nWhat can I do for you?\nBye. Hope to see you again soon!", NAME);
+    }
+}
