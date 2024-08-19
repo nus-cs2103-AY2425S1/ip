02ce4d74d759eabaee9eef142a08d49a69d4afeb
chenle <e1156512@u.nus.edu>
2024-08-19 14:45:39 +0800

    Implement an initial skeletal version of the bot

---
+++ src/main/java/Lolo.java
@@ -0,0 +1,14 @@
+public class Lolo {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String greeting1 = "Hello! I'm Lolo!";
+        String greeting2 = "What can I do for you?";
+        String greeting3 = "Bye. Hope to see you again soon!";
+        System.out.println(line);
+        System.out.println(greeting1);
+        System.out.println(greeting2);
+        System.out.println(line);
+        System.out.println(greeting3);
+        System.out.println(line);
+    }
+}
