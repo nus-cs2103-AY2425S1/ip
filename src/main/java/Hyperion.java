28703c2a293b0daadc628ad809968b01482f86d8
IanCheah <iancheah24@gmail.com>
2024-08-21 00:47:12 +0800

    Greet and exits

--- src/main/java/Hyperion.java
+++ src/main/java/Hyperion.java
@@ -1,10 +1,9 @@
 public class Hyperion {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String solidLine = "_".repeat(50) + "\n";
+        String greet1 = "Hello! I'm Hyperion\n";
+        String greet2 = "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(solidLine + greet1 + greet2 + solidLine + exit);
     }
 }
