3cbe6f5ef62a21eac70a7ef19e6523d3e90b989d
ywllowsensor <isyazani@gmail.com>
2024-08-20 16:37:10 +0800

    Added echo

--- src/main/java/Prince.java
+++ src/main/java/Prince.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Prince {
+
+    private static String LINE = "--------------------------------------";
+    private static boolean TOGGLER = true;
+
     public static void main(String[] args) {
-        String line = "--------------------------------------";
-        System.out.println(line);
+
+        Scanner sc = new Scanner(System.in);
+
+        printline();
+
         System.out.println("Hello! I'm Prince");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        printline();
+
+        while (TOGGLER) {
+            String input = sc.nextLine();
+            printline();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+                printline();
+            } else {
+                TOGGLER = false;
+            }
+        }
+        
+        sc.close();
         System.out.println("Bye. Hope to see you again soon!");
+
+    }
+
+    private static void printline() {
+        System.out.println(LINE);
     }
 }
