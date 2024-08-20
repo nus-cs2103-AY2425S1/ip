5059ebde1bfdcbe6688f4615c3b3531238c71450
sean_wong_fromtp <sean_wong_from.tp@mom.gov.sg>
2024-08-20 10:24:59 +0800

    added lists and ability to add to the list

--- src/main/java/SeanBot.java
+++ src/main/java/SeanBot.java
@@ -3,6 +3,8 @@ public class SeanBot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm SeanBot\n" + //
                             "What can I do for you?");
+        String[] tasks = new String[100];
+        int counter = 0;
         
         Scanner scanner = new Scanner(System.in);
         while(true){
@@ -11,7 +13,15 @@ public class SeanBot {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            }         
+            } else if (userInput.equals("list")){
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[counter] = userInput;
+                counter++;
+                System.out.println("added: " + userInput);
+            }
         }
         scanner.close();
     }
