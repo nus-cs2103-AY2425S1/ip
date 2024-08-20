9d8a1b76d57be8512613d0759c2d95b52cb66ba6
Tanziyanghenry <e0725822@u.nus.edu>
2024-08-20 13:30:47 +0800

    add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns whether the task is done or not
+     *
+     * @return status of task
+     */
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Returns description of task
+     *
+     * @return description of task
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Indicates that the task is done
+     */
+    public void mark() {
+        this.isDone = true;
+    }
+
+    /**
+     * Indicates that the task is not done
+     */
+    public void unmark() {
+        this.isDone = false;
+    }
+}
