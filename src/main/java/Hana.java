68f652da2363d23e62a00b25c19c3b2be1687ae4
hansneddyanto <hans.neddy@gmail.com>
2024-08-20 17:37:58 +0800

    Make minor modifications to code structure Adds echo functionality

--- src/main/java/Hana.java
+++ src/main/java/Hana.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Hana {
     public static void main(String[] args) {
-        String line = "____________________________________________";
-        System.out.println(line);
-        System.out.println("Hello I'm Hana");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+        String line = "    ____________________________________________________________\n";
+        String greeting = "    Hello I'm Hana\n" +
+                "    What can I do for you?\n";
+        String closing = "    Bye. Hope to see you again soon!\n";
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println(line + greeting + line);
+        String input = scanner.nextLine();  // Read user input
+        String output = "";
+        while (!input.equals("bye")) {
+            output = "    " + input + "\n";
+            System.out.println(line + output + line);
+            input = scanner.nextLine();
+        }
+        System.out.println(line + closing + line);
     }
 }
