55aa352c8a2aacd380e7c3dbeb03917676572c6b
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-20 21:43:23 +0800

    Add support for echo
    Use a scanner object to take in inputs, and a while loop to repeat those inputs until the exit command is given

--- src/main/java/Testament.java
+++ src/main/java/Testament.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Testament {
     final static String LINE = "____________________________________________________________";
     public static void main(String[] args) {
+        boolean powerOn = true;
+        Scanner scanner = new Scanner(System.in);
         System.out.println(LINE);
         System.out.println(" Hello! I'm Testament\n What can I do for you?");
         System.out.println(LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        while (powerOn) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                powerOn = false;
+            } else {
+                System.out.println(userInput);
+                System.out.println(LINE);
+            }
+        }
     }
 }
