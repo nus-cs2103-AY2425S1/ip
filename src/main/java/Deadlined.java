812abbcff8ab186b7b2347faac4f110ea88a697f
bigismols <bigismols239@gmail.com>
2024-08-20 15:26:49 +0800

    Updated todos and deadlines formatting

--- src/main/java/Deadlined.java
+++ src/main/java/Deadlined.java
@@ -1,2 +1,22 @@
-public class Deadlined {
+public class Deadlined extends Task {
+    private String deadline;
+
+    public Deadlined(String name, String deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        String myDeadline = "[ D ] ";
+
+        if (this.isDone()) {
+            myDeadline += "[ X ]";
+        } else {
+            myDeadline += "[   ]";
+        }
+
+        return myDeadline + super.toString() + " (by: " + this.deadline + ")";
+    }
+
 }
--- src/main/java/Todos.java
+++ src/main/java/Todos.java
@@ -1,2 +1,17 @@
-public class Todos {
+public class Todos extends Task{
+    public Todos(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        String myTodo = "[ T ] ";
+
+        if (this.isDone()) {
+            myTodo += "[ X ]";
+        } else {
+            myTodo += "[   ]";
+        }
+        return myTodo + super.toString();
+    }
 }
