09c26d9040cd63bf16849fd80db3beb462a10273
ywllowsensor <isyazani@gmail.com>
2024-08-20 20:10:37 +0800

    Added todo, event and deadline

--- src/main/java/Prince.java
+++ src/main/java/Prince.java
@@ -26,14 +26,35 @@ public class Prince {
             if (!input.equals("bye") && !input.equals("list")
                     && !input.contains("mark") && !input.contains("unmark")) {
 
-                // create a new task
-                Task task = new Task(input);
-                // get task id to add into array
-                int taskID = task.getTaskID();
-                // add the task to the arraylist
-                tasksArray[taskID] = task;
+                System.out.println("    Got it. I've added this task:");
 
-                System.out.println("    added: " + input);
+                if (input.contains("todo")) {
+                    String desc = getTodo(input);
+                    Todo todo = new Todo(desc);
+                    int todoID = todo.getTaskID();
+                    tasksArray[todoID] = todo;
+                    System.out.println("      " + todo.toString());
+                }
+                if (input.contains("deadline")) {
+                    String desc = getDeadline(input);
+                    String by = getBy(input);
+                    Deadline deadlineTask = new Deadline(desc, by);
+                    int deadlineID = deadlineTask.getTaskID();
+                    tasksArray[deadlineID] = deadlineTask;
+                    System.out.println("      " + deadlineTask.toString());
+                }
+                if (input.contains("event")) {
+                    String desc = getEvent(input);
+                    String from = getFrom(input);
+                    String to = getTo(input);
+                    Event event = new Event(desc, from, to);
+                    int eventID = event.getTaskID();
+                    tasksArray[eventID] = event;
+                    System.out.println("      " + event.toString());
+                }
+
+                System.out.println("Now you have " + getTaskArrayLength(tasksArray) +
+                        " tasks in the list.");
                 printline();
             } else if (input.equals("list")) {
 
@@ -43,9 +64,7 @@ public class Prince {
                 // print the list of inputs
                 for (int i = 0; i < length; i++) {
                     if (tasksArray[i] != null) {
-                        // get the task
                         Task task = tasksArray[i];
-
                         // formatting for numbering of list
                         int listNum = i + 1;
                         String numDot = listNum + ".";
@@ -92,13 +111,13 @@ public class Prince {
         // close and cleanup scanner
         sc.close();
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 
     private static void printline() {
         System.out.println(LINE);
     }
 
+    // method to get the integer when inputting mark or unmark
     private static int getIndex(String input) {
         if (input.contains("unmark")) {
             // get character value of index in the input
@@ -116,5 +135,65 @@ public class Prince {
             // should not reach here
             return -1;
         }
+    }
+
+    // method to get length of task array
+    private static int getTaskArrayLength(Task[] arr) {
+        int x = 0;
+        for (int i = 0; i < 100; i++) {
+            if (arr[i] != null) {
+                x++;
+            }
+        }
+        return x;
+    }
+
+    /*
+     * Mehthods to get descriptions of each respective task
+     */
+    // method to get description of the todo input
+    private static String getTodo(String input) {
+        String[] arr = input.split("todo");
+        String todo = arr[1].trim();
+        return todo;
+    }
+
+    // method to get description of the deadline input
+    private static String getDeadline(String input) {
+        String[] arr = input.split("deadline|/by");
+        String deadline = arr[1].trim();
+        return deadline;
+    }
+
+    // method to get description of the event input
+    private static String getEvent(String input) {
+        String[] arr = input.split("event|/from|/to");
+        String event = arr[1].trim();
+        return event;
+    }
+    
+
+    /*
+     * Methods to get dates of respective inputs
+     */
+    // method to get the deadline of deadline task input
+    private static String getBy(String input) {
+        String[] arr = input.split("/by");
+        String by = arr[1].trim();
+        return by;
+    }
+
+    // method to get from of the event input
+    private static String getFrom(String input) {
+        String[] arr = input.split("/from|/to");
+        String from = arr[1].trim();
+        return from;
+    }
+
+    // method to get to of the event input
+    private static String getTo(String input) {
+        String[] arr = input.split("/from|/to");
+        String to = arr[2].trim();
+        return to;
     }
 }
