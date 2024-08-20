e9fc2d06c084033df24481476fc23248e09c6f98
celeschai <celes.chai@gmail.com>
2024-08-20 16:54:37 +0800

    feat: Add chatbot

---
+++ src/main/java/Bee.java
@@ -0,0 +1,16 @@
+public class Bee {
+    public static void main(String[] args) {
+        String logo = " ____                 \n"
+                + "|  _ \\  ___   ___ \n"
+                + "| |_/  / _ \\ / _ \\\n"
+                + "| |_\\ |  __/|  __/ \n"
+                + "|____/ \\___| \\___|\n";
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello, I'm Bee!");
+        System.out.println(" What can I do for you?\n");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
