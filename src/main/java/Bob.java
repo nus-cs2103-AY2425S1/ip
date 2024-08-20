88d2edb218313d28ffd1d9edf3d5a9ef8c26bc34
jessica2828 <jessica.sylviana.js@gmail.com>
2024-08-20 13:02:42 +0800

    Add support for add

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,6 +4,9 @@ public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
+        int taskCounter = 0;
+        String[] tasks = new String[100];
+
         System.out.println("\t------------------------------------------");
         System.out.println("\tHello! I'm Bob!\n\tHow can I help you today?");
         System.out.println("\t------------------------------------------");
@@ -11,11 +14,16 @@ public class Bob {
         while (!input.equals("bye")) {
             input = scanner.nextLine();
 
+            // add task to tasks array
             if (!input.equals("bye")) {
+                tasks[taskCounter] = input;
+                taskCounter++;
+
                 System.out.println("\t------------------------------------------");
-                System.out.println("\t" + input);
+                System.out.println("\tadded: " + input);
                 System.out.println("\t------------------------------------------");
             }
+
         }
 
         System.out.println("\t------------------------------------------");
