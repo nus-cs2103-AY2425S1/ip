29301d768c1470a59a2d398d90c0302fcf0162ca
TYiFan-hub <tyifan123@gmail.com>
2024-08-20 21:48:56 +0800

    Add ability to echo to commands

--- src/main/java/Fanny.java
+++ src/main/java/Fanny.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Fanny {
     public static void main(String[] args) {
         System.out.println("_____________________________________________");
         System.out.println("Hello! I'm Fanny");
         System.out.println("What can I do for you?");
         System.out.println("_____________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________________");
+
+        //while ensure continuous reading of user inputs
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("User: ");
+            String cmd = scanner.nextLine();
+
+            //condition to exit when cmd is bye
+            if(cmd.equals("bye")) {
+                System.out.println("_____________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("_____________________________________________");
+                break;
+            }
+
+            System.out.println("_____________________________________________");
+            System.out.println("Fanny: ");
+            System.out.println(cmd);
+            System.out.println("_____________________________________________");
+
+        }
+
     }
 }
