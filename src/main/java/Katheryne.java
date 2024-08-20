337a2851bc9dce3c83d87f274018db40df4d0a3b
YANYAN-w <e0773610@u.nus.edu>
2024-08-20 18:21:08 +0800

    Level-1

--- src/main/java/Katheryne.java
+++ src/main/java/Katheryne.java
@@ -8,17 +8,22 @@ public class Katheryne {
     }
 
     public void run() {
+        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
         System.out.println("Katheryne: " + "Ad astra abyssosque! I'm Katheryne, the receptionist here at the Adventurers' Guild. Welcome!");
 
         String userInput;
         boolean finish = false;
         while (!finish) {
+            System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
             userInput = sc.nextLine();
-            if (!userInput.equalsIgnoreCase("bye")) {
+            if (!userInput.equals("bye")) {
+                System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
                 System.out.println("Katheryne: " + userInput);
             } else {
                 finish = true;
+                System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
                 System.out.println("Katheryne: " + "Farewell, Adventurer, and thank you for supporting the Adventurers' Guild.");
+                System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
                 sc.close();
             }
         }
