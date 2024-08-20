a375b283c74e1942f8389c9c6bc887c61174fa8c
dominic2412 <521hxt@gmail.com>
2024-08-20 17:22:10 +0800

    Level-1

--- src/main/java/Skywalker.java
+++ src/main/java/Skywalker.java
@@ -1,7 +1,30 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.sql.SQLOutput;
+import java.util.Objects;
+import java.util.Scanner;
 public class Skywalker {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Skywalker");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String printable = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (Objects.equals(printable, "bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+
+                break;
+            }
+            System.out.println(printable);
+            System.out.println("____________________________________________________________");
+
+        }
     }
 }
