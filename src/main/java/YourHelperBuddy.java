4e3fa1849bd6d7a06d9c3eaca3323e92f785b2c9
calebchongsj <calebchongsj@gmail.com>
2024-08-20 16:14:24 +0800

    Level-2 increment. Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/YourHelperBuddy.java
+++ src/main/java/YourHelperBuddy.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
+import java.util.*;
 public class YourHelperBuddy {
     public static void main(String[] args) {
         System.out.println("Hello! I'm YourHelperBuddy.");
         System.out.println("How may I assist you today?");
         System.out.println("________________________________________________");
         Scanner myObj = new Scanner(System.in);
-        // User enters commands until he types bye
-        System.out.println("Command: ");
-        String userInput = myObj.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println("________________________________________________");
-            System.out.println("     " + userInput);
-            System.out.println("________________________________________________");
-            System.out.println("Command: ");
-            userInput = myObj.nextLine();
+        ArrayList<String> taskList = new ArrayList<>();
+        // User enters the tasks until he types bye
+        System.out.println("Enter your task: ");
+        String userTask = myObj.nextLine();
+        while (!userTask.equals("bye")) {
+            if (userTask.equals("list")) {
+                System.out.println("________________________________________________");
+                for (int i = 0; i < taskList.size(); i++) {
+                    String taskLabel = String.valueOf(i + 1);
+                    System.out.println(" " + taskLabel + ". " + taskList.get(i));
+                }
+                System.out.println("________________________________________________");
+                System.out.println("Enter your task: ");
+                userTask = myObj.nextLine();
+            }
+            else {
+                System.out.println("________________________________________________");
+                taskList.add(userTask);
+                System.out.println("  added: " + userTask);
+                System.out.println("________________________________________________");
+                System.out.println("Enter your task: ");
+                userTask = myObj.nextLine();
+            }
         }
         System.out.println("________________________________________________");
         System.out.println("Goodbye. Take care and see you again!");
