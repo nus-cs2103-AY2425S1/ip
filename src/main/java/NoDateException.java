c66cd16155755900a13b8a27fc61e11a8ba95790
dominopizzaaaa <domksj23@gmail.com>
2024-08-20 21:07:36 +0800

    Follow Java Conventions, added Javadocs

--- src/main/java/NoDateException.java
+++ src/main/java/NoDateException.java
@@ -1,6 +1,7 @@
 public class NoDateException extends Exception {
     public NoDateException() {
-        super("I don't see the correct format of dates given to me. Please ensure you input the date or input it in the correct format!\n" +
+        super("I don't see the correct format of dates given to me. " +
+                "Please ensure you input the date or input it in the correct format!\n" +
                 "For deadlines: 'deadline (description) /by (date)'\n" +
                 "For events: 'event (description) /from (from date) /to (to date)'");
     }
--- src/main/java/Papadom.java
+++ src/main/java/Papadom.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 public class Papadom {
     enum Command {
@@ -20,20 +19,41 @@ public class Papadom {
     }
     private static final ArrayList<Task> tasks = new ArrayList<>();
     private static final Scanner scanner = new Scanner(System.in);
+    /**
+     * Outputs a formatted message with a surrounding border to the console.
+     *
+     * @param message The message to be printed to the console.
+     */
     public static void output(String message) {
         System.out.println("____________________________________________________________\n"
                 + message
                 + "\n____________________________________________________________");
     }
+
+    /**
+     * Returns a formatted string of all tasks in the list.
+     *
+     * @return A formatted string containing the list of tasks.
+     */
     public static String printList() {
         String finalList = " Here are the tasks in your list:";
         for (int i = 0; i < Papadom.tasks.size(); i++) {
             Task task = Papadom.tasks.get(i);
-            if (task == null) break;
+            if (task == null) {
+                break;
+            }
             finalList += "\n " + (i + 1) + "." + task.toString();
         }
         return finalList;
     }
+
+    /**
+     * Adds a task to the list and returns a confirmation message.
+     *
+     * @param task The task to be added to the list.
+     * @return A confirmation message indicating the task has been added.
+     * @throws NoTaskException If the task description is empty.
+     */
     public static String addToList(Task task) throws NoTaskException {
         if (task.description == "") {
             throw new NoTaskException();
@@ -43,12 +63,25 @@ public class Papadom {
                 + " Now you have " + (Papadom.tasks.size()) + " tasks in the list.";
         return response;
     }
+
+    /**
+     * Marks a task as done based on the given input string.
+     *
+     * @param text The input string containing the task number to be marked.
+     * @return A message confirming the task has been marked as done.
+     * @throws NoTaskNumberException If the task number is missing or not a number.
+     * @throws WrongTaskNumberException If the task number is out of range.
+     */
     public static String markTask(String text) throws NoTaskNumberException, WrongTaskNumberException {
         String[] parts = text.split(" ");
-        if (parts.length <= 1) throw new NoTaskNumberException();
+        if (parts.length <= 1) {
+            throw new NoTaskNumberException();
+        }
         try {
             int taskIndex = Integer.parseInt(parts[1]) - 1;
-            if (taskIndex >= Papadom.tasks.size()) throw new WrongTaskNumberException();
+            if (taskIndex >= Papadom.tasks.size()) {
+                throw new WrongTaskNumberException();
+            }
             Task task = Papadom.tasks.get(taskIndex);
             task.markAsDone();
             return " Nice! I've marked this task as done:\n  " + task;
@@ -56,12 +89,25 @@ public class Papadom {
             throw new NoTaskNumberException(); // Throw the custom exception if parts[1] is not an integer
         }
     }
+
+    /**
+     * Unmarks a task as not done based on the given input string.
+     *
+     * @param text The input string containing the task number to be unmarked.
+     * @return A message confirming the task has been unmarked as not done.
+     * @throws NoTaskNumberException If the task number is missing or not a number.
+     * @throws WrongTaskNumberException If the task number is out of range.
+     */
     private static String unmarkTask(String text) throws NoTaskNumberException, WrongTaskNumberException {
         String[] parts = text.split(" ");
-        if (parts.length <= 1) throw new NoTaskNumberException();
+        if (parts.length <= 1) {
+            throw new NoTaskNumberException();
+        }
         try {
             int taskIndex = Integer.parseInt(parts[1]) - 1;
-            if (taskIndex >= Papadom.tasks.size()) throw new WrongTaskNumberException();
+            if (taskIndex >= Papadom.tasks.size()) {
+                throw new WrongTaskNumberException();
+            }
             Task task = Papadom.tasks.get(taskIndex);
             task.unmark();
             return " OK, I've marked this task as not done yet:\n  " + task;
@@ -70,24 +116,61 @@ public class Papadom {
             throw new NoTaskNumberException(); // Throw the custom exception if parts[1] is not an integer
         }
     }
+
+    /**
+     * Adds a deadline task to the list based on the given details.
+     *
+     * @param details The details of the task, including the description and deadline.
+     * @return A confirmation message indicating the deadline has been added.
+     * @throws NoTaskException If the task description is empty.
+     * @throws NoDateException If the deadline date is missing.
+     */
     private static String addDeadline(String details) throws NoTaskException, NoDateException {
         String[] parts = details.split(" /by ");
-        if (parts[0] == "") throw new NoTaskException();
-        else if (parts.length == 1) throw new NoDateException();
+        if (parts[0] == "") {
+            throw new NoTaskException();
+        } else if (parts.length == 1) {
+            throw new NoDateException();
+        }
         return addToList(new Deadline(parts[0], parts[1]));
     }
+
+    /**
+     * Adds an event task to the list based on the given details.
+     *
+     * @param details The details of the task, including the description, start time, and end time.
+     * @return A confirmation message indicating the event has been added.
+     * @throws NoTaskException If the task description is empty.
+     * @throws NoDateException If the event dates are missing.
+     */
     private static String addEvent(String details) throws NoTaskException, NoDateException {
         String[] parts = details.split(" /from | /to ");
-        if (parts[0] == "") throw new NoTaskException();
-        else if (parts.length <= 2) throw new NoDateException();
+        if (parts[0] == "") {
+            throw new NoTaskException();
+        } else if (parts.length <= 2) {
+            throw new NoDateException();
+        }
         return addToList(new Event(parts[0], parts[1], parts[2]));
     }
+
+    /**
+     * Deletes a task from the list based on the given input string.
+     *
+     * @param text The input string containing the task number to be deleted.
+     * @return A message confirming the task has been deleted.
+     * @throws NoTaskNumberException If the task number is missing or not a number.
+     * @throws WrongTaskNumberException If the task number is out of range.
+     */
     private static String deleteEvent(String text) throws NoTaskNumberException, WrongTaskNumberException {
         String[] parts = text.split(" ");
-        if (parts.length <= 1) throw new NoTaskNumberException();
+        if (parts.length <= 1) {
+            throw new NoTaskNumberException();
+        }
         try {
             int taskIndex = Integer.parseInt(parts[1]) - 1;
-            if (taskIndex >= Papadom.tasks.size()) throw new WrongTaskNumberException();
+            if (taskIndex >= Papadom.tasks.size()) {
+                throw new WrongTaskNumberException();
+            }
             Task taskToBeDeleted = Papadom.tasks.get(taskIndex);
             Papadom.tasks.remove(taskIndex);
             return " Noted. I've removed this task:\n  " + taskToBeDeleted + "\n Now you have " + Papadom.tasks.size() + " tasks in the list.";
@@ -106,32 +189,32 @@ public class Papadom {
                 String commandText = text.split(" ")[0];
                 Command command = Command.fromString(commandText);
                 switch (command) {
-                    case LIST:
-                        output(printList());
-                        break;
-                    case BYE:
-                        output(" Bye. Hope to see you again soon!");
-                        return;
-                    case MARK:
-                        output(markTask(text));
-                        break;
-                    case UNMARK:
-                        output(unmarkTask(text));
-                        break;
-                    case TODO:
-                        output(addToList(new Todo(text.substring(5))));
-                        break;
-                    case DEADLINE:
-                        output(addDeadline(text.substring(9)));
-                        break;
-                    case EVENT:
-                        output(addEvent(text.substring(6)));
-                        break;
-                    case DELETE:
-                        output(deleteEvent(text));
-                        break;
-                    default:
-                        throw new UnknownCommandException();
+                case LIST:
+                    output(printList());
+                    break;
+                case BYE:
+                    output(" Bye. Hope to see you again soon!");
+                    return;
+                case MARK:
+                    output(markTask(text));
+                    break;
+                case UNMARK:
+                    output(unmarkTask(text));
+                    break;
+                case TODO:
+                    output(addToList(new Todo(text.substring(5))));
+                    break;
+                case DEADLINE:
+                    output(addDeadline(text.substring(9)));
+                    break;
+                case EVENT:
+                    output(addEvent(text.substring(6)));
+                    break;
+                case DELETE:
+                    output(deleteEvent(text));
+                    break;
+                default:
+                    throw new UnknownCommandException();
                 }
             } catch (Exception e) {
                 output(e.getMessage());
