5e24bdd5b0b7691012506acc82a812b80839605b
KrashKart <e1113755@u.nus.edu>
2024-08-20 20:53:01 +0800

    Added enums for Response type checking

---
+++ src/main/java/Actions.java
@@ -0,0 +1,14 @@
+public class Actions {
+    public enum ACTIONS {
+        LIST,
+        TODO,
+        DEADLINE,
+        EVENT,
+        MARK,
+        UNMARK,
+        DELETE,
+        BYE,
+        SECRET,
+        INVALID
+    }
+}
