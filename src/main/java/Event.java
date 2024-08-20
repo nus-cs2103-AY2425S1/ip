b7f949655144275f77f7cc9554bb8ba620a0aa19
Christopher Tan <phertan02@gmail.com>
2024-08-20 19:15:05 +0800

    Create Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    protected String start;
+    protected String end;
+
+    public Event(String desc, String start, String end) {
+        super(desc);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + String.format(" from: %s to: %s", this.start, this.end);
+    }
+}
