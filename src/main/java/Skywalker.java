04641c7de3c014f403e6f2cabb78cc7932ad76e8
dominic2412 <521hxt@gmail.com>
2024-08-20 16:52:49 +0800

    Level-0

---
+++ src/main/java/Skywalker.java
@@ -0,0 +1,7 @@
+public class Skywalker {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Skywalker");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
