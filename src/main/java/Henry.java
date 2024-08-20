dbcd82367098a5016215c419e5723ed91becf0bd
Tanziyanghenry <e0725822@u.nus.edu>
2024-08-20 10:14:38 +0800

    Add greeting

--- src/main/java/Henry.java
+++ src/main/java/Henry.java
@@ -1,10 +1,25 @@
 public class Henry {
+
+    /**
+     * Prints greetings
+     *
+     */
+    public static void greetings() {
+        String greetings = "Hello! I'm Henry\n"
+                + "What can I do for you?\n";
+        System.out.println(greetings);
+    }
+
+    /**
+     * Prints exit
+     *
+     */
+    public static void bye() {
+        String bye = "Bye. Hope to see you again soon!\n";
+        System.out.println(bye);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetings();
+        bye();
     }
 }
