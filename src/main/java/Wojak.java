f19b9d3cff160d7d8309205cc120ace1ccdf256e
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-08-20 13:38:56 +0000

    feat: Rename chatbot

---
+++ src/main/java/Wojak.java
@@ -0,0 +1,12 @@
+public class Wojak {
+    public static void main(String[] args) {
+
+        System.out.println(
+                "____________________________________________________________\n" +
+                " Hello! I'm Wojak\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
