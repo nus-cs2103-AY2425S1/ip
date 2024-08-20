f6b11bcdaf26e0f8d4e2e272051a46793792453f
uniqly <simontong09@gmail.com>
2024-08-20 16:48:35 +0800

    Ekud.java: Rename Duke.java, Change greeting and exit

---
+++ src/main/java/Ekud.java
@@ -0,0 +1,14 @@
+public class Ekud {
+    public static final String LINE_SEPARATOR =
+            "_____________________________________________________________";
+    public static final String GREETING = "Hello! I'm EKuD\nWhat can I do fo you?";
+    public static final String GOODBYE = "Bye. Hope to see you again soon!";
+
+    public static void main(String[] args) {
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(GREETING);
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(GOODBYE);
+        System.out.println(LINE_SEPARATOR);
+    }
+}
