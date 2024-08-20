7e20b82a6110cd7efe173e0508eb5d8425b6ea51
calebchongsj <calebchongsj@gmail.com>
2024-08-20 15:36:43 +0800

    Level-0 increment. Renamed Duke.java to YourHelperBuddy.java and implemented a simple chatbot which greets the user and exits.

---
+++ src/main/java/YourHelperBuddy.java
@@ -0,0 +1,8 @@
+public class YourHelperBuddy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm YourHelperBuddy.");
+        System.out.println("How may I assist you today?");
+        System.out.println("________________________________________________");
+        System.out.println("Goodbye. Take care and see you again!");
+    }
+}
