95defdd5956ece67edaeaeb9d80ce049d07b835b
wujiaweijoanna <e1155577@u.nus.edu>
2024-08-20 23:13:04 +0800

    Todo, Event, Deadline

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -6,57 +6,58 @@ public class Alice {
     private static final String name = "Alice";
     private static final String line =
             "____________________________________________________________";
-    private static List<Task> tasks = new ArrayList<>();
+    private static TaskList list = new TaskList();
 
     public static void main(String[] args) {
         greet();
         Scanner scanner = new Scanner(System.in);
          while (true) {
-             String input = scanner.nextLine();
+             String input = scanner.nextLine().trim();
+             String[] parts = input.split(" ", 2);
+             String verb = parts[0];
+
              if (input.equals("bye")) {
                  break;
              } else if (input.equals("list")) {
                  listTasks();
-             } else if (input.startsWith("mark")) {
-                 String[] phrases = input.split(" ");
-                 if (phrases.length > 1) {
-                     try {
-                         int num = Integer.parseInt(phrases[1]);
-                         Task currTask = tasks.get(num - 1);
-                         currTask.markDone();
-                         System.out.println(line);
-                         System.out.println("Nice! I've marked this task as done:");
-                         System.out.println(currTask.toString());
-                         System.out.println(line);
-                     } catch (IndexOutOfBoundsException e) {
-                         System.err.println("Invalid number");
-                     }
-                 } else {
-                     System.err.println("Need to state the number");
+             } else if (verb.equals("mark")) {
+                 int num = Integer.parseInt(input.split(" ")[1]);
+                 list.markTask(num);
+             } else if (verb.equals("unmark")) {
+                 int num = Integer.parseInt(input.split(" ")[1]);
+                 list.unmarkTask(num);
+             } else if (verb.equals("todo")) {
+                 try {
+                     Todo toDo = new Todo(parts[1]);
+                     list.addToList(toDo);
+                 } catch (Exception e) {
+                     System.out.println("Command Format: todo [description]");
                  }
-             } else if (input.startsWith("unmark")) {
-                 String[] phrases = input.split(" ");
-                 if (phrases.length > 1) {
-                     try {
-                         int num = Integer.parseInt(phrases[1]);
-                         Task currTask = tasks.get(num - 1);
-                         currTask.markUndone();
-                         System.out.println(line);
-                         System.out.println("OK, I've marked this task as not done yet:");
-                         System.out.println(currTask.toString());
-                         System.out.println(line);
-                     } catch (IndexOutOfBoundsException e) {
-                         System.err.println("Invalid number");
-                     }
-                 } else {
-                     System.err.println("Need to state the number");
+             } else if (verb.equals("event")) {
+                 try {
+                     String[] detail = parts[1].split("/from");
+                     String description = detail[0].trim();
+                     String[] time = detail[1].trim().split("/to");
+                     String start = time[0].trim();
+                     String end = time[1].trim();
+                     Event event = new Event(description, start, end);
+                     list.addToList(event);
+                 } catch (Exception e) {
+                     System.out.println("Command Format: event [description] /from [time] /to [time]");
+                 }
+             } else if (verb.equals("deadline")) {
+                 try {
+                     String[] detail = parts[1].split("/by");
+                     String description = detail[0].trim();
+                     String time = detail[1].trim();
+                     Deadline deadline = new Deadline(description, time);
+                     list.addToList(deadline);
+                 } catch (Exception e) {
+                     System.out.println("Command Format: deadline [description] /by [time]");
                  }
              } else {
-                 Task newTask = new Task(input);
-                 tasks.add(newTask);
-                 String s = "added: " + input;
                  System.out.println(line);
-                 System.out.println(s);
+                 System.out.println("Invalid command, use command words: list, todo, deadline & event");
                  System.out.println(line);
              }
          }
@@ -66,12 +67,7 @@ public class Alice {
     public static void listTasks() {
         System.out.println(line);
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            Task currTask = tasks.get(i);
-            int currNum = i + 1;
-            String s = currNum + "." + currTask.toString();
-            System.out.println(s);
-        }
+        System.out.println(list.toString());
         System.out.println(line);
     }
 
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    ArrayList<Task> list;
+    private static final String line =
+            "____________________________________________________________";
+
+    public TaskList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void addToList(Task task) {
+        this.list.add(task);
+        System.out.println(line);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task.toString());
+        System.out.println("Now you have " + list.size() + " tasks in the list.");
+        System.out.println(line);
+    }
+
+    public void markTask(int num) {
+        if (num <= list.size() && num > 0) {
+            Task currTask = list.get(num - 1);
+            currTask.markDone();
+            System.out.println(line);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(currTask.toString());
+            System.out.println(line);
+        } else {
+            System.out.println(line);
+            System.out.println("Invalid number");
+            System.out.println(line);
+        }
+    }
+
+    public void unmarkTask(int num) {
+        if (num <= list.size() && num > 0) {
+            Task currTask = list.get(num - 1);
+            currTask.markUndone();
+            System.out.println(line);
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(currTask.toString());
+            System.out.println(line);
+        } else {
+            System.out.println(line);
+            System.out.println("Invalid number");
+            System.out.println(line);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            Task currTask = list.get(i);
+            int currNum = i + 1;
+            sb.append(currNum).append(".").append(currTask.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
