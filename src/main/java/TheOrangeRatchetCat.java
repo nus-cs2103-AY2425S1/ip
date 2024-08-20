c6faf29743b0577ad0317d040a070425b17bff23
OrangeCatLoves <e1121707@u.nus.edu>
2024-08-21 00:00:02 +0800

    Level-1 v1

--- src/main/java/TheOrangeRatchetCat.java
+++ src/main/java/TheOrangeRatchetCat.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
+
 public class TheOrangeRatchetCat {
     public static void main(String[] args) {
-        
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm TheOrangeRatchetCat");
         System.out.println("What can I do for you?");
@@ -8,5 +9,17 @@ public class TheOrangeRatchetCat {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
 
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine(); // Reads a line of text
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine(); // Reads the next line of input text again
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+        scanner.close(); // Close the scanner to avoid resource leaks
     }
 }
