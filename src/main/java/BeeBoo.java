efa688623966efcf53ae5b4a99968db54aceb483
rubinnn <e1155263.u.nus.edu>
2024-08-20 15:16:02 +0800

    Level-1

--- src/main/java/BeeBoo.java
+++ src/main/java/BeeBoo.java
@@ -1,24 +1,38 @@
+import java.util.Scanner;
 
 public class BeeBoo {
 
-    protected void chatBox(String str) {
-        //creating top horizontal line
+    private void chatBox(String str) {
+        //creating top horizontal lines
         for (int i = 0; i < 60; i++) {
             System.out.print("-");
         }
         System.out.println();
-        //printing out string
+        //printing chat output
         System.out.println(" " + str);
-        //creating bottom horizontal line
+        System.out.println();
+        //creating bottom horizontal lines
         for (int i = 0; i < 60; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
-        //Initialising chatbot
+        //Initialising BeeBoo and Scanner
         BeeBoo beeBoo = new BeeBoo();
+        Scanner input = new Scanner(System.in);
+
+        //getting user input
         beeBoo.chatBox("Hello! I'm BeeBoo\n What can i do for you?");
-        beeBoo.chatBox("Bye. Hope to see you soon!");
+        String text = input.nextLine().toLowerCase();
+
+        //Keeping chatbot open till bye is typed
+        while (!text.equals("bye")) {
+            beeBoo.chatBox(text);
+            text = input.nextLine().toLowerCase();
+        }
+        beeBoo.chatBox("Bye. Hope to see you again soon!");
+        input.close();
     }
 }
