f8fb146c697dae478534c63e5ae15b6ac7cedad1
waihongteh <tehwaihong1126@gmail.com>
2024-08-20 18:56:12 +0800

    Separate welcome and bye message

--- src/main/java/Waterfall.java
+++ src/main/java/Waterfall.java
@@ -1,12 +1,15 @@
 public class Waterfall {
     public static void main(String[] args) {
         String chatBotName = "Waterfall";
-        System.out.println(
+        String welcomeMessage =
             "____________________________________________________________\n"
             + "Hello I'm " + chatBotName + "\n"
-            + "What can I do for you?\n"
-            + "____________________________________________________________\n"
+            + "What can I do for you?\n";
+        String byeMessage =
+            "____________________________________________________________\n"
             + "Bye. Hope to see you again soon!\n"
-            + "____________________________________________________________");
+            + "____________________________________________________________";
+        System.out.println(welcomeMessage);
+        System.out.println(byeMessage);
     }
 }
