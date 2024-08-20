44a612c2fa3f6a167b8496514c91699fa05b6d8b
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-08-20 18:42:39 +0800

    Level 3 with completion of tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,26 @@
 public class Task {
+    protected String description;
+    protected boolean status;
+//  false status indicates incomplete item
+
+    public Task(String description) {
+        this.description = description;
+        this.status = false;
+    }
+
+    public String toString() {
+        if (this.status) {
+            return "[X] " + this.description;
+        } else {
+            return "[ ] " + this.description;
+        }
+    }
+
+    public void done() {
+        this.status = true;
+    }
+
+    public void unDone() {
+        this.status = false;
+    }
 }
--- src/main/java/Twilight.java
+++ src/main/java/Twilight.java
@@ -5,15 +5,23 @@ public class Twilight {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! I am Twilight your personal assistant\nWhat can I do for you?" );
-        ArrayList<String> tasks =  new ArrayList<String>();
+        ArrayList<Task> tasks =  new ArrayList<Task>();
         String command = input.nextLine();
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("" + (i + 1) + ". " + tasks.get(i));
+                    System.out.println("" + (i + 1) + ". " + tasks.get(i).toString());
                 }
+            } else if (command.substring(0,4).equals("mark")) {
+                int task_num = Integer.valueOf(command.substring(5)) - 1;
+                tasks.get(task_num).done();
+                System.out.println("Excellent I have marked it: " + tasks.get(task_num).toString());
+            } else if (command.substring(0,6).equals("unmark")) {
+                int task_num = Integer.valueOf(command.substring(7)) - 1;
+                tasks.get(task_num).unDone();
+                System.out.println("Fine I have unmarked it: " + tasks.get(task_num).toString());
             } else {
-                tasks.add(command);
+                tasks.add(new Task(command));
                 System.out.println("added: " + command);
             }
             command = input.nextLine();
