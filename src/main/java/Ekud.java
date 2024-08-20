bbe599135b4b7119c0299116e196e697a4459b87
uniqly <simontong09@gmail.com>
2024-08-20 17:44:48 +0800

    Ekud.java: Add echo requirement to EKuD

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,23 +1,45 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Ekud {
     public static final String LINE_SEPARATOR =
             "\t_____________________________________________________________";
+    public static final String END_COMMAND = "bye";
 
     public static void greet() {
         String greeting = "\tHeyo! My name is EKuD!! You can call me Eku-chan :)"
                 + "\n\tHow may I be of assistance!";
         System.out.println(LINE_SEPARATOR);
         System.out.println(greeting);
+        System.out.println(LINE_SEPARATOR);
     }
 
     public static void sayGoodbye() {
         String goodbye = "\tI hope you enjoyed your stay!\n\tSee you next time!";
-        System.out.println(LINE_SEPARATOR);
         System.out.println(goodbye);
         System.out.println(LINE_SEPARATOR);
     }
 
+    public static void echo(String message) {
+        System.out.println("\t" + message);
+        System.out.println(LINE_SEPARATOR);
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+
         greet();
+        while (true) {
+            System.out.println();
+            command = sc.nextLine();
+            System.out.println(LINE_SEPARATOR);
+            // check for user escape
+            if (Objects.equals(command, END_COMMAND)) {
+                break;
+            }
+            echo(command);
+        }
         sayGoodbye();
     }
 }
