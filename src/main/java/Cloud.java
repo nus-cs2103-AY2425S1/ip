f6878a22179514d27fab0b24710aa03d86be106d
Christopher Tan <phertan02@gmail.com>
2024-08-20 16:26:10 +0800

    echo user input, exit on command

--- src/main/java/Cloud.java
+++ src/main/java/Cloud.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Cloud {
+
+    private static final String EXIT_COMMAND = "bye";
 
     private static void greet() {
         System.out.println(
@@ -13,15 +17,27 @@ public class Cloud {
         );
     }
 
+    private static void echo(String message) {
+        System.out.println(message);
+    }
+
     private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         printHorizLine();
         greet();
-        printHorizLine();
-        exit();
+        while (true) {
+            printHorizLine();
+            String userInput = sc.nextLine().strip();
+            if (userInput.equals(EXIT_COMMAND)) {
+                exit();
+                break;
+            }
+            System.out.println(userInput);
+        }
         printHorizLine();
     }
 }
