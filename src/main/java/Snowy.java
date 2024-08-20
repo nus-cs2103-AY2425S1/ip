a1dd703c525ed0d8994a436dda51e10331904b1b
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-08-20 23:58:13 +0800

    Add greeting message at start

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -1,5 +1,14 @@
+
 public class Snowy {
+
+
+    private static final String LINE = "-----------------------------------\n";
+    private static final String GREETING = LINE
+            + "Hello! My name is Snowy \n"
+            + "What can I do for you? \n \n"
+            + LINE;
+
     public static void main(String[] args) {
-        System.out.println("Hello from Snowy\n");
+        System.out.println(GREETING);
     }
 }
