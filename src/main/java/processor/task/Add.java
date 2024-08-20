c9e6b16ea2abcb9279d00bfcf7f4f05e1d1ca5e1
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-08-20 22:59:19 +0800

    Add general function for task adder.

--- src/main/java/processor/task/Add.java
+++ src/main/java/processor/task/Add.java
@@ -6,24 +6,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Add {
+  private static Response process(Task newTask) {
+    processor.task.TaskList.addTask(newTask);
+    return new Response(java.util.List.of("added: " + newTask));
+  }
+
   public static Response todo(String prompt) {
     final List<String> prompts = Arrays.asList(prompt.split("todo "));
     final Task newTask = Task.of("todo", prompts.get(1));
-    processor.task.TaskList.addTask(newTask);
-    return new Response(java.util.List.of("added: " + newTask));
+    return Add.process(newTask);
   }
 
   public static Response deadline(String prompt) {
     final List<String> prompts = Arrays.asList(prompt.split("deadline "));
     final Task newTask = Task.of("deadline", prompts.get(1));
-    processor.task.TaskList.addTask(newTask);
-    return new Response(java.util.List.of("added: " + newTask));
+    return Add.process(newTask);
   }
 
   public static Response event(String prompt) {
     final List<String> prompts = Arrays.asList(prompt.split("event "));
     final Task newTask = Task.of("event", prompts.get(1));
-    processor.task.TaskList.addTask(newTask);
-    return new Response(java.util.List.of("added: " + newTask));
+    return Add.process(newTask);
   }
 }
