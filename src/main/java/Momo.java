cf2b145fc7b73570cae20bd8b3aa98dfadee0d7a
dovlin <dovlincs01@gmail.com>
2024-08-20 19:15:11 +0800

    Added echo

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class Momo {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String horizontalLine = "____________________________________________________________";
         String logo = "⣿⣿⣿⡉⢀⣾⣿⡟⣩⣭⣭⡈⠙⢿⣿⣿⣿⣿⣿⡿⣻⣿⣿⣿⣿⣿⣿⣿⡇⠄\n" +
                 "⣿⣿⡗⠄⣼⣿⣿⢸⡿⠉⠉⢻⡆⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠄\n" +
@@ -13,8 +17,20 @@ public class Momo {
                 "⠄⠄⠄⠄⠄⢻⣧⡙⢿⣿⣿⣿⣿⣿⡿⣿⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄⢠⣿⣿⣿\n" +
                 "⠄⠄⠄⡀⠄⠈⣿⣿⣶⣭⣭⣭⣿⣾⡿⠟⠋⠁⠄⠄⠄⠄⠄⠄⠄⢠⣿⣿⣿⣿\n" +
                 "⠄⠄⠎⠄⠄⣨⣿⣿⣿⣿⣿⣿⠋⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⡲⣿⣿⣿⣿";
+
         System.out.println("Hello! I'm Momo\nWhat can I do for you?");
         System.out.println(horizontalLine);
+        String input = "";
+
+        while (!Objects.equals(input, "bye")) {
+            input = sc.next();
+
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+
+        }
+
         System.out.println("Bye. Hope to see you again soon!\n" + logo);
         System.out.println(horizontalLine);
     }
