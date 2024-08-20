571c2e1fe0bafce8f7a6c083f61a690b7e10d960
superb-sushi <caleb.ang.zx@gmail.com>
2024-08-20 20:31:31 +0800

    Level-1

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -1,14 +1,26 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Blob {
     private static boolean active = true;
     public static void main(String[] args) {
+        Scanner human = new Scanner(System.in);
+        System.out.println("______________________________________________");
+        System.out.println("Hello! I'm Blob");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________________");
+
         while (active) {
+            String action = human.nextLine();
             System.out.println("______________________________________________");
-            System.out.println("Hello! I'm Blob");
-            System.out.println("What can I do for you?");
-            active = !active;
+            if (Objects.equals(action.toLowerCase(), "bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("______________________________________________");
+                active = false;
+            } else {
+                System.out.println(action);
+                System.out.println("______________________________________________");
+            }
         }
-        System.out.println("______________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________");
     }
 }
