1e1f994e7c6ea972770aa4fa16e233fce2df771f
yhanyi <yeohhanyi0916@gmail.com>
2024-08-21 06:30:48 +0800

    A-Packages

---
+++ src/main/java/ui/Ui.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class Ui {
+
+    private static String logo =  " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public Ui() {}
+}
