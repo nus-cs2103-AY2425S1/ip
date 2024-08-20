f340c08b576490dd484073243af4a8af5615a1e7
waihongteh <tehwaihong1126@gmail.com>
2024-08-20 20:52:46 +0800

    Add list functionalities

--- src/main/java/Waterfall.java
+++ src/main/java/Waterfall.java
@@ -13,16 +13,31 @@ public class Waterfall {
             ("____________________________________________________________\n"
             + "Shhhhhhhhhhhh. Hope to see you again soon!\n"
             + "____________________________________________________________\n").indent(indentSpace);
+        String[] taskList = new String[100];
+        int num = 0;
         System.out.println(welcomeMessage);
         Scanner userInput = new Scanner(System.in);
         while (true) {
             String nextInput = userInput.nextLine();
             if (nextInput.equals("bye")) {
                 break;
+            } else if (nextInput.equals("list")) {
+                System.out.println("___________________________________________________________".indent(indentSpace));
+                for (int i = 0; i < num; i++) {
+                    if (taskList[i] != null) {
+                        String taskString = (Integer.toString(i + 1) + ". " + taskList[i]).indent(indentSpace + 1);
+                        System.out.println(taskString);
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("____________________________________________________________".indent(indentSpace));
             } else {
                 String echoString = ("____________________________________________________________\n"
-                + nextInput + "\n"
+                + "added: " + nextInput + "\n"
                 + "____________________________________________________________\n").indent(indentSpace);
+                taskList[num] = nextInput;
+                num++;
                 System.out.println(echoString);
             }
         }
