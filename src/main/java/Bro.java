ce68c028a6da9b605e036727c46a1a41fa7af0a4
karthickkc <>
2024-08-20 19:40:22 +0800

    did level-0

---
+++ src/main/java/Bro.java
@@ -0,0 +1,7 @@
+public class Bro {
+    public static void main(String[] args) {
+        String line = "______________________________________________________\n";
+        System.out.println(line + " Hello! I'm Bro\n What can I do for you?\n" + line +
+                           " Bye. Hope to see you again soon!\n" + line);
+    }
+}
