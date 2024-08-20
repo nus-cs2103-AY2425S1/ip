de3b8d01c975030659780db16a803aa92da1186f
OrangeCatLoves <e1121707@u.nus.edu>
2024-08-21 00:17:14 +0800

    Level-3 v1

--- src/main/java/TheOrangeRatchetCat.java
+++ src/main/java/TheOrangeRatchetCat.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TheOrangeRatchetCat {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm TheOrangeRatchetCat");
         System.out.println("What can I do for you?");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
 
+        List<String> items = new ArrayList<>();
+        int index = 1;
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine(); // Reads a line of text
         while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                for (String item : items) {
+                    System.out.println(index + ". " + item);
+                    index++;
+                }
+                input = scanner.nextLine(); // Reads the next line of input text again
+                continue;
+            }
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            System.out.println("added " + input);
             System.out.println("____________________________________________________________");
+            items.add(input);
             input = scanner.nextLine(); // Reads the next line of input text again
         }
         System.out.println("____________________________________________________________");
