fa332c5d52df8a9c5124dd9822776c02ff048b29
Wong Wei Han <56218253+WeiHanWong@users.noreply.github.com>
2024-08-20 17:19:45 +0800

    Level 2. Add, List

--- src/main/java/Will.java
+++ src/main/java/Will.java
@@ -1,9 +1,11 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Will {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         String logo = "WILL";
         System.out.println("Hello! I'm" + logo);
         System.out.println("What can I do for you?");
@@ -15,7 +17,15 @@ public class Will {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(userInput);
+            if(Objects.equals(userInput, "list")){
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                System.out.println("_____________________________________");
+                continue;
+            }
+            list.add(userInput);
+            System.out.println("added: " + userInput);
             System.out.println("_____________________________________");
         }
     }
