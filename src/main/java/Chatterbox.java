d401c9badad0c3ffb7ebbc8ccb7e10f3e21e73df
LAPTOP-37SSO5I3\Han Bin <ngiowhb@gmail.com>
2024-08-20 20:48:28 +0800

    Level-0

---
+++ src/main/java/Chatterbox.java
@@ -0,0 +1,13 @@
+public class Chatterbox {
+    public static void main(String[] args) {
+        String welcomeMessage = """
+                ____________________________________________________________
+                 Hello! I'm Chatterbox
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(welcomeMessage);
+    }
+}
