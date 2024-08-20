9b95aaca5f779e2b12d7c7c69b7cd31ec46e90b4
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2024-08-20 17:16:46 +0800

    Add javadocs * Added javadocs * Created method to prompt user

--- src/main/java/Maxine.java
+++ src/main/java/Maxine.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 
 public class Maxine {
+    /**
+     * This is the main method
+     * @param args Command-line arguments passed to the program
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Hi! Nice to meet you :) I am Maxine");
         String answer;
         do {
-            System.out.print("What can I do for you today? : ");
-            answer = scanner.nextLine();
-
-            if (!answer.equals("Bye Max!")) {
-                System.out.println(answer);
-            }
+            answer = answer();
         }
         while (!answer.equals("Bye Max!"));
 
         System.out.println("\nBye! It was nice chatting with you. Hope to see you again soon!");
     }
+
+    /**
+     * This method prompts the user to ask for their answer
+     * @return The user's input
+     */
+    public static String answer() {
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        System.out.print("What can I do for you today? : ");
+        answer = scanner.nextLine();
+        return answer;
+    }
+
+
 }
