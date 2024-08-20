16cd4d890a925d0700737118fb2d781b63df5c25
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-08-20 13:52:59 +0800

    added echo commands and will end the program if user types the command bye

--- src/main/java/SilverWolf.java
+++ src/main/java/SilverWolf.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class SilverWolf {
     public static void main(String[] args) {
-
+        // initialize the scanner to read user input
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Silver Wolf\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+                " What can I help you with?\n" +
+                "____________________________________________________________\n");
+        while(true) {
+            // reading the user input
+            String input = scanner.nextLine();
+
+            // echo the input back to the user
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+
+
+            // exit if the user types "bye"
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Till we meet again!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+
+        // close the scanner
+        scanner.close();
+
     }
 }
