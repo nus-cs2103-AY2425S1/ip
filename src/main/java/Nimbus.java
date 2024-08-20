259aaaa99f2c172606b2d6a4d05203d567ee75ab
rimgik <karon503.cpp@gmail.com>
2024-08-20 15:46:46 +0800

    Week 2: Level-2

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Nimbus {
     private static String name = "Nimbus";
+    private static String[] texts = new String[100];
+    private static int textCount = 0;
+
     private static void printDash() {
         System.out.println("____________________________________________________________");
     }
@@ -19,6 +22,21 @@ public class Nimbus {
         printDash();
     }
 
+    public static void logMessage(String Message) {
+        printDash();
+        texts[textCount++] = Message;
+        System.out.println("added: " + Message);
+        printDash();
+    }
+
+    public static void printAllMessage() {
+        printDash();
+        for (int i = 0; i < textCount; ++i) {
+            System.out.println((i + 1) + ". " + texts[i]);
+        }
+        printDash();
+    }
+
     public static void main(String[] args) {
         printWelcomeMessage();
 
@@ -26,7 +44,11 @@ public class Nimbus {
         String line;
 
         while(!(line = scanner.nextLine()).equals("bye")) {
-            System.out.println(line);
+            if (line.equals("list")) {
+                printAllMessage();
+            } else {
+                logMessage(line);
+            }
         }
 
         printGoodbyeMessage();
