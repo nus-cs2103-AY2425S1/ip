1ea91ae0d44eb4ba623b8369978c7f784d2da567
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-08-20 13:48:57 +0800

    Rename the file, added greet and exit

---
+++ src/main/java/SilverWolf.java
@@ -0,0 +1,11 @@
+public class SilverWolf {
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Silver Wolf\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+    }
+}
