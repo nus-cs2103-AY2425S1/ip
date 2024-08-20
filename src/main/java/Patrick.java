a4f06a66f26a9116e38701c4b8d2052c8f25800c
Chronoxy <adriannpcc@gmail.com>
2024-08-20 14:55:49 +0800

    Add, List

--- src/main/java/Patrick.java
+++ src/main/java/Patrick.java
@@ -1,18 +1,30 @@
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Patrick {
     public static void main(String[] args) {
         String horizontalLine = "-----------------------------------------------------------------------\n";
         String greetingMsg = horizontalLine + "Hello! I'm Patrick, Spongebob bestie\nHow can I help you?\n" + horizontalLine;
-        String exitMsg = "Bye. Hope to see you again soon!\n";
+        String exitMsg = horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine;
         String input;
+        ArrayList list = new ArrayList();
+        int count = 0;
 
         Scanner inputMsg = new Scanner(System.in);
         System.out.println(greetingMsg);
         do {
             input = inputMsg.nextLine();
-            System.out.println(horizontalLine + input + "\n" + horizontalLine);
-        } while (!input.equals("bye"));
+            if (input.equals("list")) {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
+            } else if (input.equals("bye")) {
+                break;
+            }
+            else {
+                list.add(input);
+                System.out.println("added: " + input + "\n");
+            }
+        } while (true);
         System.out.println(exitMsg);
     }
 }
