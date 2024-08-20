4f390a533b213552d465a6e76c6811b82b6905bb
ywllowsensor <isyazani@gmail.com>
2024-08-20 15:55:57 +0800

    Added greet and exit

---
+++ src/main/java/Prince.java
@@ -0,0 +1,10 @@
+public class Prince {
+    public static void main(String[] args) {
+        String line = "--------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Prince");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
