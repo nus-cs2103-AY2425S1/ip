362ed7fd820b10fbe22269e40cd009f4b69703aa
uniqly <simontong09@gmail.com>
2024-08-20 17:10:21 +0800

    Ekud.java: Add greeting and exit static methods

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,14 +1,23 @@
 public class Ekud {
     public static final String LINE_SEPARATOR =
             "_____________________________________________________________";
-    public static final String GREETING = "Hello! I'm EKuD\nWhat can I do fo you?";
-    public static final String GOODBYE = "Bye. Hope to see you again soon!";
+
+    public static void greet() {
+        String greeting = "Heyo! My name is EKuD!! You can call me Eku-chan"
+                + "\nHow may I be of assistance";
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(greeting);
+    }
+
+    public static void sayGoodbye() {
+        String goodbye = "I hope you enjoyed your stay!\nSee you next time!";
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(goodbye);
+        System.out.println(LINE_SEPARATOR);
+    }
 
     public static void main(String[] args) {
-        System.out.println(LINE_SEPARATOR);
-        System.out.println(GREETING);
-        System.out.println(LINE_SEPARATOR);
-        System.out.println(GOODBYE);
-        System.out.println(LINE_SEPARATOR);
+        greet();
+        sayGoodbye();
     }
 }
