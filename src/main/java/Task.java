633d7a745a38971aaf0a2c7888fc02ee79620c0f
jktang14 <tangboy14@gmail.com>
2024-08-20 23:05:07 +0800

    Add String representation for Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,16 @@
 public class Task {
+    private String description;
+    private boolean isDone;
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
 
+    @Override
+    public String toString() {
+        if (this.isDone) {
+            return String.format("[X] %s", this.description);
+        }
+        return String.format("[] %s", this.description);
+    }
 }
