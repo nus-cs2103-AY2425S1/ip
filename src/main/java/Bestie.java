294c811acd2e9585456fe067962e9c13c186c300
tingxuanp <peh.tingx@gmail.com>
2024-08-20 16:39:14 +0800

    Implemented ability to store and display text entered by user

--- src/main/java/Bestie.java
+++ src/main/java/Bestie.java
@@ -1,5 +1,6 @@
 // use Scanner class to get user inputs
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bestie {
     public static void main(String[] args) {
@@ -13,14 +14,25 @@ public class Bestie {
         System.out.println("What can I do for you today :)?");
 
         String userInput;
+        ArrayList<String> tasks = new ArrayList<>();
         // echoes commands entered by the user, and exits when the user types the command "bye"
         while (true) {
             userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 break;
             }
-            System.out.println(userInput);
 
+            if (userInput.equals("list")) {
+                // display the list of all items entered by the user
+                for (int i = 0; i < tasks.size(); i++){
+                    int index = i + 1;
+                    System.out.println(index +". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(userInput);
+                System.out.println("added: " + userInput);
+
+            }
 
         }
 
