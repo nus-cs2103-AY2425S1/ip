91d4c91c7ff05661fc04a1b0a49e10fc97f91780
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-08-20 16:47:15 +0800

    added greetings and goodbye

---
+++ src/main/java/Twilight.java
@@ -0,0 +1,6 @@
+public class Twilight {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Twilight your personal assistant\nWhat can I do for you?" );
+        System.out.println("Bye!");
+    }
+}
