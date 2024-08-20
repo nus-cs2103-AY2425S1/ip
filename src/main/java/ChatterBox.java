6d21cb6063edd72ddef5d58028f11a396cccdd2d
Trisha Patodia <trishapatodia@Trishas-MacBook-Air.local>
2024-08-21 01:13:24 +0800

    Renamed Duke.java to ChatterBox.java and added initial functionality

---
+++ src/main/java/ChatterBox.java
@@ -0,0 +1,10 @@
+public class ChatterBox {
+    public static void main(String[] args) {
+        System.out.println("_".repeat(50));
+        System.out.println("Hello! I'm ChatterBox");
+        System.out.println("What can I do for you?");
+        System.out.println("_".repeat(50));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(50));
+    }
+}
