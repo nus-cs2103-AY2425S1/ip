accb0a6016d13743910810ba9a1b99bc978a76ca
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-08-20 13:39:54 +0000

    feat: Add echo

--- src/main/java/Wojak.java
+++ src/main/java/Wojak.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class Wojak {
     public static void main(String[] args) {
 
-        System.out.println(
-                "____________________________________________________________\n" +
+        System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Wojak\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+        Scanner sc = new Scanner(System.in);
+        String nextLine = sc.nextLine();
+        while (!(nextLine.equals("bye"))) {
+            System.out.println(nextLine);
+            nextLine = sc.nextLine();
+        }
+
+
+        System.out.println(
+                " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
     }
 }
