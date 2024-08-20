cd4ad8fffed22b192c17446db84141b914356d25
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-08-20 21:33:39 +0800

    created the Task class and refactored the code to use Task instead of String

--- src/main/java/SilverWolf.java
+++ src/main/java/SilverWolf.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 
 public class SilverWolf {
     // using arraylist
-    private static ArrayList<String> list = new ArrayList<>();
+    private static ArrayList<Task> list = new ArrayList<>();
 
     // output the list
     public static void outputList(){
         int index = 1;
-        for(String str : list){
-            System.out.println(index + ". "+list.get((index-1)));
+        for(Task t : list){
+            System.out.println(index + ". "+t.getDescription());
             index++;
         }
     }
 
     public static void main(String[] args) {
         // initialize the scanner to read user input
-        Scanner scanner = new Scanner(System.in);
         System.out.print("  _________.__.__                       __      __      .__   _____ \n");
         System.out.print(" /   _____/|__|  |___  __ ___________  /  \\    /  \\____ |  |_/ ____\\\n");
         System.out.print(" \\_____  \\ |  |  |\\  \\/ // __ \\_  __ \\ \\   \\/\\/   /  _ \\|  |\\   __\\ \n");
@@ -28,6 +27,7 @@ public class SilverWolf {
                 " Hello! I'm Silver Wolf\n" +
                 " What can I help you with?\n" +
                 "____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
         while(true) {
             // reading the user input
             String input = scanner.nextLine();
@@ -48,7 +48,7 @@ public class SilverWolf {
                 // echo the input back to the user
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("____________________________________________________________");
 
             }
