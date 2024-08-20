ecce8af6e2925bedfe05ada2661c55fedb4ba699
YANYAN-w <e0773610@u.nus.edu>
2024-08-20 17:36:12 +0800

    level 1-0

---
+++ src/main/java/Katheryne.java
@@ -0,0 +1,11 @@
+
+public class Katheryne {
+
+    public static void main(String[] args){
+        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
+        System.out.println("Ad astra abyssosque! I am Katheryne, the receptionist here at the Adventurers' Guild. Welcome to the Adventurers' Guild!");
+        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
+        System.out.println("Farewell, Adventurer, and thank you for supporting the Adventurers' Guild.");
+        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
+    }
+}
