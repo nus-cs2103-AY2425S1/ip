a14d94584065f6d9253d88e4a0254aa8e188e582
Kieran's Laptop <kieran.koh@gmail.com>
2024-08-20 17:00:34 +0800

    Add Level-2 requirements

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -1,20 +1,42 @@
 import java.util.*;
 
 public class Count {
-    public final String spacer = "\n____________________________________________________________\n";
-    public void reply(String output) {
+    private ArrayList<String> ls;
+
+    public Count() {
+        this.ls = new ArrayList<>();
+    }
+    private void reply(String output) {
+        String spacer = "\n____________________________________________________________\n";
         System.out.println(spacer + output + spacer);
     }
-    public void greet() {
+    private void greet() {
         reply("Hello! I'm Count!\nWhat can I do for you?");
     }
 
-    public void farewell() {
+    private void farewell() {
         reply("Bye. Hope to see you again soon!");
     }
 
-    // Default case just echoes
-    public void parser(String input) {
+    private void addToList(String toDo) {
+        reply("Added: " + toDo);
+        ls.add(toDo);
+    }
+
+    private void listReply() {
+        String ans = "";
+        for (int i = 0 ; i < ls.size() ; i++) {
+            if (i != ls.size() - 1) {
+                ans += (i + 1) + ". " + ls.get(i) + "\n";
+            } else {
+                ans += (i + 1) + ". " + ls.get(i);
+            }
+        }
+        reply(ans);
+    }
+
+    // Default case is add
+    private void parser(String input) {
         switch (input.toLowerCase()) {
             case "hello":
                 greet();
@@ -22,8 +44,11 @@ public class Count {
             case "bye":
                 farewell();
                 break;
+            case "list":
+                listReply();
+                break;
             default:
-                reply(input);
+                addToList(input);
         }
     }
 
