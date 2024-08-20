326d4a74674969641319f5d9895a40af55aef789
Chronoxy <adriannpcc@gmail.com>
2024-08-20 13:30:21 +0800

    Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Patrick User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Patrick.java
@@ -0,0 +1,8 @@
+public class Patrick {
+    public static void main(String[] args) {
+        String horizontalLine = "-----------------------------------------------------------------------\n";
+        String greetingMsg = "Hello! I'm Patrick\nWhat can I do for you?\n";
+        String exitMsg = "Bye. Hope to see you again soon!\n";
+        System.out.println(horizontalLine + greetingMsg + horizontalLine + exitMsg + horizontalLine);
+    }
+}
