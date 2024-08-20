6104fa3deba726b1569d4f561b1e5f7aff372b74
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-08-20 23:59:34 +0800

    Add exit statement

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -8,7 +8,10 @@ public class Snowy {
             + "What can I do for you? \n \n"
             + LINE;
 
+    private static final String ENDING = "Bye! Hope to see you again soon! \n"
+            + LINE;
     public static void main(String[] args) {
         System.out.println(GREETING);
+        System.out.println(ENDING);
     }
 }
