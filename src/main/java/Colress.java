ee225f9a60f63b26ddbaa463e710324e439accf8
JYL27 <junyulee27@gmail.com>
2024-08-20 21:32:26 +0800

    Replaced Duke.java with Colress.java. Updated greeting and exit messages.

---
+++ src/main/java/Colress.java
@@ -0,0 +1,8 @@
+public class Colress {
+    public static void main(String[] args) {
+        String spacer = "______________________________________\n";
+        System.out.println(spacer + "Hello. My Name is Colress.\n"
+                + "What can I do for you?\n" + spacer
+                + "Well then. I hope to see you soon.\n" + spacer);
+    }
+}
