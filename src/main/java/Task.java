bd351b91ffaf58602dca7646822eed4da94c01fb
dovlin <dovlincs01@gmail.com>
2024-08-20 22:33:09 +0800

    Added a class for task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,25 @@
 public class Task {
+    String task;
+    boolean isComplete = false;
+
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public void markComplete() {
+        this.isComplete = true;
+        System.out.println("Nice! I've marked this task as done");
+        System.out.println(this);
+    }
+
+    public void unmark() {
+        this.isComplete = false;
+        System.out.println("OK, I've marked this task as not done yet");
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return isComplete ? "[X] " + task : "[ ] " + task;
+    }
 }
