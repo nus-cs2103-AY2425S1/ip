f91da0e3accfba615f58055c5421c12fc01aa4b2
jessica2828 <jessica.sylviana.js@gmail.com>
2024-08-20 12:01:16 +0800

    Add greeting and exit

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,9 @@
 public class Bob {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("------------------------------------------");
+        System.out.println("Hello! I'm Bob!\nHow can I help you today?");
+        System.out.println("------------------------------------------");
+        System.out.println("Bye, see you again!");
+        System.out.println("------------------------------------------");
     }
 }
