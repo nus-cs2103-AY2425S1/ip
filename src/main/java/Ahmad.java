4b8349554573c81cf8a4c35110a4b0dbb5dbcd48
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-08-20 11:09:26 +0800

    Add IO formatter.

--- src/main/java/Ahmad.java
+++ src/main/java/Ahmad.java
@@ -2,6 +2,6 @@ public class Ahmad {
 
   public static void main(String[] args) {
     String welcomeMsg = "Hello! I'm Ahmad\nWhat can I do for you?\n\nOkay Byeee!";
-    System.out.println(welcomeMsg);
+    IO.print(welcomeMsg);
   }
 }
