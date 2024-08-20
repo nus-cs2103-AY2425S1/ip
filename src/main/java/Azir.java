5241d7d568f5143fc99de35454d0fc03b722a0ab
jktang14 <tangboy14@gmail.com>
2024-08-20 19:43:03 +0800

    Add echo functionality

--- src/main/java/Azir.java
+++ src/main/java/Azir.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Azir {
     public static void main(String[] args) {
+        String input;
         System.out.println("----------------------------------");
         System.out.println("Hello! I'm Azir");
         System.out.println("What can I do for you?");
+        System.out.println("----------------------------------");
+
+        Scanner obj = new Scanner(System.in);
+
+        while (!(input = obj.nextLine()).equals("bye")) {
+            System.out.println("----------------------------------");
+            System.out.println(input);
+            System.out.println("----------------------------------");
+        }
         System.out.println("----------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("----------------------------------");
