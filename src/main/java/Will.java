0bf0f28338b2c429af70ba707db3c71e8e40fa23
Wong Wei Han <56218253+WeiHanWong@users.noreply.github.com>
2024-08-20 16:56:27 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Will.java
@@ -0,0 +1,8 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo = "WILL";
+        System.out.println("Hello! I'm" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
