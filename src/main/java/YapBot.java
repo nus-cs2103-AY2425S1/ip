966811a0ea2b1809beb441f249a53667fc959b11
NicT5 <nicctang@gmail.com>
2024-08-20 21:51:49 +0800

    Added automated testing

--- src/main/java/YapBot.java
+++ src/main/java/YapBot.java
@@ -9,7 +9,7 @@ public class YapBot {
 
     public static void list() {
         Iterator<Task> iterateStored = storedTasks.iterator();
-        System.out.println(PREFIXLINE + "\nRetrieving Tasks... \nSuccess \nCurrent Tasks:");
+        System.out.println(PREFIXLINE + "\nRetrieving Tasks...\nSuccess\nCurrent Tasks:");
         int index = 1;
         while (iterateStored.hasNext()) {
             System.out.println("  " + index + "." + iterateStored.next());
@@ -23,22 +23,22 @@ public class YapBot {
             int index = Integer.parseInt(input.substring(7)) - 1;
 
             if (index == -1 || index >= storedTasks.size()) {
-                System.out.println(PREFIXLINE + "\nFinding Task... \nFailure \nError, requested Task does not exist.\n" + POSTFIXLINE);
+                System.out.println(PREFIXLINE + "\nFinding Task...\nFailure\nError, requested Task does not exist.\n" + POSTFIXLINE);
                 return;
             }
 
             Task task = storedTasks.get(index);
             task.changeDone(false);
-            System.out.println(PREFIXLINE + "\nFinding Task... \nSuccess \nTask Incomplete:\n" + task + "\n" + POSTFIXLINE);
+            System.out.println(PREFIXLINE + "\nFinding Task...\nSuccess\nTask Incomplete:\n" + task + "\n" + POSTFIXLINE);
         } else {
             int index = Integer.parseInt(input.substring(5)) - 1;
             if (index == -1 || index >= storedTasks.size()) {
-                System.out.println(PREFIXLINE + "\nFinding Task... \nFailure \nError, requested Task does not exist.\n" + POSTFIXLINE);
+                System.out.println(PREFIXLINE + "\nFinding Task...\nFailure\nError, requested Task does not exist.\n" + POSTFIXLINE);
                 return;
             }
             Task task = storedTasks.get(index);
             task.changeDone(true);
-            System.out.println(PREFIXLINE + "\nFinding Task... \nSuccess \nTask Completed:\n" + task + "\n" + POSTFIXLINE);
+            System.out.println(PREFIXLINE + "\nFinding Task...\nSuccess\nTask Completed:\n" + task + "\n" + POSTFIXLINE);
         }
     }
 
@@ -48,33 +48,42 @@ public class YapBot {
         Task task = new Task("Dummy");
 
         switch (taskType) {
-            case "todo" -> {
+            case "todo":
                 task = new ToDo(taskDetails);
                 storedTasks.add(task);
-            }
-            case "deadline" -> {
+                System.out.println(PREFIXLINE + "\nAdding Task...\nSuccess\nTask Added:\n" + "  " + task + "\n" + "Total tasks: " + storedTasks.size() +"\n" + POSTFIXLINE);
+                break;
+
+            case "deadline":
                 String taskName = taskDetails.substring(0, taskDetails.indexOf("/by")).strip();
                 String deadline = taskDetails.substring(taskDetails.indexOf("/by") + 3).strip();
                 task = new Deadline(taskName, deadline);
                 storedTasks.add(task);
-            }
-            case "event" -> {
-                String taskName = taskDetails.substring(0, taskDetails.indexOf("/")).strip();
+                System.out.println(PREFIXLINE + "\nAdding Task...\nSuccess\nTask Added:\n" + "  " + task + "\n" + "Total tasks: " + storedTasks.size() +"\n" + POSTFIXLINE);
+                break;
+
+            case "event":
+                taskName = taskDetails.substring(0, taskDetails.indexOf("/")).strip();
                 String taskDeadlines = taskDetails.substring(taskDetails.indexOf("/"));
                 String from = taskDeadlines.substring(taskDeadlines.indexOf("/from") + 5, taskDeadlines.indexOf("/to")).strip();
                 String to = taskDeadlines.substring(taskDeadlines.indexOf("/to") + 3).strip();
 
                 task = new Event(taskName, from, to);
                 storedTasks.add(task);
-            }
+                System.out.println(PREFIXLINE + "\nAdding Task...\nSuccess\nTask Added:\n" + "  " + task + "\n" + "Total tasks: " + storedTasks.size() +"\n" + POSTFIXLINE);
+                break;
+
+            default:
+                System.out.println(PREFIXLINE + "\nAdding Task...\nFailure\nInvalid Task Type: " + taskType + "\n" + POSTFIXLINE);
+
         }
 
-        System.out.println(PREFIXLINE + "\nAdding Task... \nSuccess \nTask Added: \n" + "  " + task + "\n" + "Total tasks: " + storedTasks.size() +"\n" + POSTFIXLINE);
+
     }
 
     public static void main(String[] args) {
 
-        System.out.println(PREFIXLINE + "\nPowering up... \nSystem booted successfully.  \nYapBot online.\n" + POSTFIXLINE);
+        System.out.println(PREFIXLINE + "\nPowering up...\nSystem booted successfully.\nYapBot online.\n" + POSTFIXLINE);
         Scanner in = new Scanner(System.in);
 
         String input = in.nextLine();
@@ -93,6 +102,6 @@ public class YapBot {
         }
 
         in.close();
-        System.out.println(PREFIXLINE + "\nShutting down... \nYapBot process terminated.\n" + POSTFIXLINE);
+        System.out.println(PREFIXLINE + "\nShutting down...\nYapBot process terminated.\n" + POSTFIXLINE);
     }
 }
