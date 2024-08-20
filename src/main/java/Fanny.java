df0134178c8a12244d6d5c3c6dac161f2f8f76e2
TYiFan-hub <tyifan123@gmail.com>
2024-08-20 21:21:22 +0800

    Add greeting

--- src/main/java/Fanny.java
+++ src/main/java/Fanny.java
@@ -1,10 +1,10 @@
 public class Fanny {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("_____________________________________________");
+        System.out.println("Hello! I'm Fanny");
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________________________");
     }
 }
