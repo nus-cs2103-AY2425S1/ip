00f6d81b9f4702bf3aa7069a616913b0a8a41081
wassilililily <e0725370@u.nus.edu>
2024-08-20 13:18:03 +0800

    Changed new line command and added more dash

--- src/main/java/BottleOpener.java
+++ src/main/java/BottleOpener.java
@@ -1,10 +1,10 @@
 public class BottleOpener {
     public static void main(String[] args) {
-        String spacer = "---------------------/n";
+        String spacer = "---------------------\n";
         String botName = "BottleOpener";
-        String greeting = "Hello! I'm " + botName + "/n";
+        String greeting = "Hello! I'm " + botName + "\n";
 
-        String goodbye = "Bye! See you next time!/n";
+        String goodbye = "Bye! See you next time!\n";
 
         System.out.println(spacer + greeting + spacer
                 + goodbye + spacer);
