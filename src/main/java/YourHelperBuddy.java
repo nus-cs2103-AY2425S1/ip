04d0d06b505b67ce7fed9e4413eef61b8a8fcf07
calebchongsj <calebchongsj@gmail.com>
2024-08-20 15:53:01 +0800

    Level-1 increment. Implemented my chatbot to echo whatever command the user inputs until he enters "bye".

--- src/main/java/YourHelperBuddy.java
+++ src/main/java/YourHelperBuddy.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class YourHelperBuddy {
     public static void main(String[] args) {
         System.out.println("Hello! I'm YourHelperBuddy.");
         System.out.println("How may I assist you today?");
         System.out.println("________________________________________________");
+        Scanner myObj = new Scanner(System.in);
+        // User enters commands until he types bye
+        System.out.println("Command: ");
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("________________________________________________");
+            System.out.println("     " + userInput);
+            System.out.println("________________________________________________");
+            System.out.println("Command: ");
+            userInput = myObj.nextLine();
+        }
+        System.out.println("________________________________________________");
         System.out.println("Goodbye. Take care and see you again!");
+        System.out.println("________________________________________________");
     }
 }
