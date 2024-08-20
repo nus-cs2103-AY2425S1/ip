053af9ba7cf840da37e25f4f545529dabc895bad
Chronoxy <adriannpcc@gmail.com>
2024-08-20 13:49:30 +0800

    Echo: Personalized messages

--- src/main/java/Patrick.java
+++ src/main/java/Patrick.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Patrick {
     public static void main(String[] args) {
         String horizontalLine = "-----------------------------------------------------------------------\n";
-        String greetingMsg = horizontalLine + "Hello! I'm Patrick\nWhat can I do for you?\n" + horizontalLine;
+        String greetingMsg = horizontalLine + "Hello! I'm Patrick, Spongebob bestie\nHow can I help you?\n" + horizontalLine;
         String exitMsg = "Bye. Hope to see you again soon!\n";
         String input;
 
