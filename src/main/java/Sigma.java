e185ed4804b55c304ecb706cb77be418a2f74ad5
anselmlong <anselmpius@gmail.com>
2024-08-20 12:03:46 +0800

    Add list feature

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -1,13 +1,35 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sigma {
     public static void main(String[] args) {
         String name = "SIGMA";
         Scanner input = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<>();
+
         print("Hello! I'm " + name + "\nLooking forward to slaying with you!\nWhat do you need today?");
         String userPrompt = input.nextLine();
-        while (!userPrompt.toLowerCase().equals("bye")) {
-            print(userPrompt);
+        while (!userPrompt.isEmpty()) {
+            switch (userPrompt) {
+                case "bye":
+                    exit();
+                    System.exit(0);
+                    break;
+                case "list":
+                    StringBuilder s = new StringBuilder();
+                    int i = 1;
+                    for (String item : items) {
+                        s.append(i + ". " + item + "\n");
+                        i++;
+                    }
+                    print(s.toString());
+                    break;
+                default:
+                    items.add(userPrompt);
+                    print("Added: " + userPrompt);
+                    break;
+            }
+
             userPrompt = input.nextLine();
         }
         exit();
