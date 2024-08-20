b897df8211cbe118fa58445b4ae8eed81206f7fa
jessica2828 <jessica.sylviana.js@gmail.com>
2024-08-20 12:26:27 +0800

    Add support for echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
-        System.out.println("------------------------------------------");
-        System.out.println("Hello! I'm Bob!\nHow can I help you today?");
-        System.out.println("------------------------------------------");
-        System.out.println("Bye, see you again!");
-        System.out.println("------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        System.out.println("\t------------------------------------------");
+        System.out.println("\tHello! I'm Bob!\n\tHow can I help you today?");
+        System.out.println("\t------------------------------------------");
+
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+
+            if (!input.equals("bye")) {
+                System.out.println("\t------------------------------------------");
+                System.out.println("\t" + input);
+                System.out.println("\t------------------------------------------");
+            }
+        }
+
+        System.out.println("\t------------------------------------------");
+        System.out.println("\tBye, see you again!");
+        System.out.println("\t------------------------------------------");
     }
 }
+
+
