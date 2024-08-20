14dcd0f9d7d05c6633f9dc634a2268ca5db44b2f
waihongteh <tehwaihong1126@gmail.com>
2024-08-20 19:04:41 +0800

    Implement scanner and echo system

--- src/main/java/Waterfall.java
+++ src/main/java/Waterfall.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class Waterfall {
     public static void main(String[] args) {
         String chatBotName = "Waterfall";
         String welcomeMessage =
             "____________________________________________________________\n"
             + "Hello I'm " + chatBotName + "\n"
-            + "What can I do for you?\n";
+            + "What can I do for you?\n"
+            + "____________________________________________________________\n";
         String byeMessage =
             "____________________________________________________________\n"
             + "Bye. Hope to see you again soon!\n"
-            + "____________________________________________________________";
+            + "____________________________________________________________\n";
         System.out.println(welcomeMessage);
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            String nextInput = userInput.nextLine();
+            if (nextInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n");
+                System.out.println(nextInput);
+                System.out.println("____________________________________________________________\n");
+            }
+        }
         System.out.println(byeMessage);
     }
 }
