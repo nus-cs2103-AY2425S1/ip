a62f71f0537be4e8fb635529743c3e32c477ab06
suhayl13 <suhayl.pervez@gmail.com>
2024-08-20 12:21:54 +0800

    Add support for echo

--- src/main/java/Lexi.java
+++ src/main/java/Lexi.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class Lexi {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         greet();
+        String response = userInput.nextLine();
+        while (!response.equals("bye")) {
+            echo(response);
+            response = userInput.nextLine();
+        }
         bye();
     }
     public static void greet() {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Lexi\n What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
     }
     public static void bye() {
-        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void echo(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.printf(" %s%n", message);
+        System.out.println("____________________________________________________________\n");
     }
 
 }
