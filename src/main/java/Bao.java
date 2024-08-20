3a1f10e978f158f9e8b5fd769611256fbd0ebb29
kvernnn <156299531+kvernnn@users.noreply.github.com>
2024-08-20 14:25:02 +0800

    Echo logic

--- src/main/java/Bao.java
+++ src/main/java/Bao.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
 public class Bao {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String baoHi =   "     ___   \n"
+                        + "   /     \\  \n"
+                        + "  /       \\ \n"
+                        + " |  ^   ^  |\n"
+                        + " |    3    |\n"
+                        + " \\________/ \n";
+
+        String baoBye =   "     ___   \n"
+                        + "   /     \\  \n"
+                        + "  /       \\ \n"
+                        + " |  T   T  |\n"
+                        + " |    ^    |\n"
+                        + " \\________/ \n";
+
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Bao");
+        System.out.println(baoHi);
+        System.out.println("Hello! I'm Bao but you can call me Bao");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(baoBye);
+                System.out.println("Bye :( Come back soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
