9916ba5f60dd10658310e45291a8ceea78d31ab6
Wu Zengfu <wuzengfu666@gmail.com>
2024-08-20 18:39:01 +0800

    Add echo method

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class EchoBot {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         UI ui = new UI();
         ui.greeting();
+
+        String command = scan.next();
+        while (!"bye".equals(command)) {
+            ui.echo(command);
+            command = scan.next();
+        }
         ui.exit();
     }
 }
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,11 +1,17 @@
 public class UI {
     public void greeting() {
-        System.out.println("---------------------------------------------------------------\n"
-                                   + "Hello! I'm EchoChat\nWhat can I do for you?\n\n" + "---------------------------------------------------------------");
+        System.out.println("\t\t\t---------------------------------------------------------------\n"
+                                   + "\t\t\tHello! I'm EchoChat\n\t\t\tWhat can I do for you?\n\n" + "\t\t\t---------------------------------------------------------------");
+    }
+
+    public void echo(String command) {
+        System.out.println("\t\t\t---------------------------------------------------------------\n"
+                                   + "\t\t\t" + command + "\n\t\t\t---------------------------------------------------------------");
     }
 
     public void exit() {
-        System.out.println("Bye. Hope to see you again soon!\n"
-                                   + "---------------------------------------------------------------\n");
+        System.out.println("\t\t\t---------------------------------------------------------------\n"
+                                   + "\t\t\tBye. Hope to see you again soon!\n"
+                                   + "\t\t\t---------------------------------------------------------------\n");
     }
 }
