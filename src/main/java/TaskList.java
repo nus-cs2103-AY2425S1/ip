e405685004025ecc10ddec24ca3393bc2774d068
Christopher Tan <phertan02@gmail.com>
2024-08-20 16:58:05 +0800

    create TaskList for storing input tasks

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,23 @@
+public class TaskList {
+
+    private String[] tasks;
+    private int head;
+    public TaskList() {
+        // Assume no more than 100 tasks
+        this.tasks = new String[100];
+        this.head = 0;
+    }
+
+    public void add(String task) {
+        this.tasks[this.head] = task;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int taskId = 0; taskId < this.tasks.length; taskId++) {
+            sb.append(String.format("%d: %s", taskId + 1, tasks[taskId]));
+        }
+        return sb.toString();
+    }
+}
