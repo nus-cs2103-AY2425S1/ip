7c79df840a48b153c045a6576f26f71fcd207487
IanCheah <iancheah24@gmail.com>
2024-08-21 01:00:36 +0800

    exits only when user types the command "bye"

--- src/main/java/Hyperion.java
+++ src/main/java/Hyperion.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Hyperion {
     public static void main(String[] args) {
         String solidLine = "_".repeat(50) + "\n";
         String greet1 = "Hello! I'm Hyperion\n";
         String greet2 = "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!";
-        System.out.println(solidLine + greet1 + greet2 + solidLine + exit);
+        // greets the user
+        System.out.println(solidLine + greet1 + greet2 + solidLine);
+        // obtains user input
+        Scanner firstScannerObj = new Scanner(System.in);
+        String userInput = firstScannerObj.nextLine();
+        System.out.println(solidLine + userInput + "\n" + solidLine);
+        // prints out the user input
+        while (!userInput.equals("bye")) {
+            Scanner scannerObj = new Scanner(System.in);
+            userInput = scannerObj.nextLine();
+            System.out.println(solidLine + userInput + "\n" + solidLine);
+        }
     }
 }
