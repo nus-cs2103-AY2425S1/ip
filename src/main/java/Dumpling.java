8791f961cee2e4cbc90f6039d24a7132df09347d
Tan Yi Long <yiiilonggg@gmail.com>
2024-08-21 01:11:03 +0800

    Complete Level-3.
    Add mark and unmark functionality.
    Add Task class.

--- src/main/java/Dumpling.java
+++ src/main/java/Dumpling.java
@@ -8,20 +8,30 @@ public class Dumpling {
 
     private static final String TERMINATE_STRING = "bye";
     private static final String LIST_COMMAND_STRING = "list";
-
+    private static final String MARK_COMMAND_STRING = "mark";
+    private static final String UNMARK_COMMAND_STRING = "unmark";
 
     public Dumpling(){
         this.todoList = new Todo();
     }
 
-    public boolean commandRouter(String userInput) {
+    private boolean commandRouter(String userInput) {
         String operationMessage = "";
-        switch (userInput) {
+        String command = userInput.split(" ")[0];
+        switch (command) {
             case TERMINATE_STRING:
                 this.exit();
                 return false;
             case LIST_COMMAND_STRING:
                 operationMessage += this.todoList.list();
+                break;
+            case MARK_COMMAND_STRING:
+                operationMessage += this.todoList.mark(
+                        Integer.parseInt(userInput.split(" ")[1]));
+                break;
+            case UNMARK_COMMAND_STRING:
+                operationMessage += this.todoList.unmark(
+                        Integer.parseInt(userInput.split(" ")[1]));
                 break;
             default:
                 operationMessage += this.todoList.add(userInput);
@@ -31,20 +41,20 @@ public class Dumpling {
         return true;
     }
 
-    public void greet() {
+    private void greet() {
         String greetingMessage = "    Hello! I'm Dumpling\n" +
                 "    What can I do for you?\n" +
                 DIVIDER + "\n";
         System.out.println(greetingMessage);
     }
 
-    public void exit() {
+    private void exit() {
         String exitMessage = "    Bye. Hope to see you again soon!\n" +
                 DIVIDER;
         System.out.println(exitMessage);
     }
 
-    public void echo(String message) {
+    private void echo(String message) {
         String updatedMessage = "    " + message + "\n" +
                 DIVIDER + "\n";
         System.out.println(updatedMessage);
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,25 +1,39 @@
 public class Todo {
 
     private static final int MAX_TASKS = 100;
-    private String[] items;
+    private Task[] items;
     private int numItems;
 
     public Todo() {
-        this.items = new String[MAX_TASKS];
+        this.items = new Task[MAX_TASKS];
         this.numItems = 0;
     }
 
     public String add(String item) {
-        this.items[this.numItems] = item;
+        this.items[this.numItems] = new Task(item);
         this.numItems++;
         return "     added: " + item + "\n";
     }
 
     public String list() {
-        String message = "";
+        String message = "     Here are the tasks in your list:\n";
         for (int i = 1; i <= this.numItems; i++) {
             message += String.format("     %d. %s\n", i, this.items[i - 1]);
         }
+        return message;
+    }
+
+    public String mark(int itemIdx) {
+        String message = "     Nice! I've marked this task as done:\n";
+        this.items[itemIdx - 1].markAsDone();
+        message += String.format("       %s\n", this.items[itemIdx - 1]);
+        return message;
+    }
+
+    public String unmark(int itemIdx) {
+        String message = "     OK, I've marked this task as not done yet:\n";
+        this.items[itemIdx - 1].markAsUndone();
+        message += String.format("       %s\n", this.items[itemIdx - 1]);
         return message;
     }
 }
