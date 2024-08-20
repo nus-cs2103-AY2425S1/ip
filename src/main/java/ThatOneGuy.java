d1098d9d3978431d9e2650ae6c037f09f54de23c
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-20 15:28:50 +0800

    Replaced echo functionality with the ability to add and view tasks. Also made the chatbot really rude for no apparent reason.

--- src/main/java/ThatOneGuy.java
+++ src/main/java/ThatOneGuy.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ThatOneGuy {
     private static String line = "____________________________________________________________";
+    private static ArrayList<String> tasks = new ArrayList<String>();
     public static void greet() {
-        String name = "That one guy";
-        System.out.println(line + "\nHello! I'm " + name);
-        System.out.println("What can I do for you?\n" + line);
+        String name = "that one guy";
+        System.out.println(line + "\nI'm " + name + ".");
+        System.out.println("Make it quick, I don't have much time.\n" + line);
     }
 
     public static void farewell() {
-        System.out.println(line + "\nBye.  Hope to see you again soon!\n"  + line);
+        System.out.println(line + "\nWhatever. Hope you never come back.\n"  + line);
     }
 
-    public static void echo() {
+    public static void list() {
+        System.out.println(line);
+        int len = tasks.size();
+        if (len == 0) {
+            System.out.println("You really don't have anything better to do?");
+        }
+        else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(String.valueOf(i + 1) + ". " + tasks.get(i));
+            }
+        }
+        System.out.println(line);
+    }
+
+    public static void cmd() {
         Scanner sc = new Scanner(System.in);
         String next;
         while (true) {
@@ -20,12 +36,18 @@ public class ThatOneGuy {
             if (next.equals("bye")) {
                 break;
             }
-            System.out.println(line + "\n" + next + "\n" + line + "\n");
+            else if (next.equals("list")) {
+                list();
+            }
+            else {
+                tasks.add(next);
+                System.out.println(line + "\n" + "added: " + next + "\n" + line + "\n");
+            }
         }
     }
     public static void main(String[] args) {
         greet();
-        echo();
+        cmd();
         farewell();
     }
 }
