f20e97bb51433f441e109d1b0b56cd36cdd085ac
suhayl13 <suhayl.pervez@gmail.com>
2024-08-20 12:41:50 +0800

    Add support for list

--- src/main/java/Lexi.java
+++ src/main/java/Lexi.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
 
 public class Lexi {
+    static String[] tasks = new String[100];
+    static int count = 0;
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         greet();
         String response = userInput.nextLine();
         while (!response.equals("bye")) {
-            echo(response);
+            if(response.equals("list")) {
+                listTasks();
+            } else {
+                addTask(response);
+            }
             response = userInput.nextLine();
         }
         bye();
     }
+
+    public static void listTasks() {
+        System.out.println("____________________________________________________________");
+        for(int i = 0;i<count;i++) {
+            String currTask = tasks[i];
+            System.out.printf(" %d. %s%n", i+1, currTask);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
     public static void greet() {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Lexi\n What can I do for you?");
@@ -21,10 +38,11 @@ public class Lexi {
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________\n");
     }
-
-    public static void echo(String message) {
+    public static void addTask(String task) {
+        tasks[count] = task;
+        count++;
         System.out.println("____________________________________________________________");
-        System.out.printf(" %s%n", message);
+        System.out.printf(" added: %s%n", task);
         System.out.println("____________________________________________________________\n");
     }
 
