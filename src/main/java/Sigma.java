4f852ba507256a29600cb4d3b3b5cc8956fee8bb
anselmlong <anselmpius@gmail.com>
2024-08-20 13:00:21 +0800

    Add task class, mark and unmark feature

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -5,31 +5,67 @@ public class Sigma {
     public static void main(String[] args) {
         String name = "SIGMA";
         Scanner input = new Scanner(System.in);
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Task> items = new ArrayList<>();
 
         print("Hello! I'm " + name + "\nLooking forward to slaying with you!\nWhat do you need today?");
         String userPrompt = input.nextLine();
         while (!userPrompt.isEmpty()) {
-            switch (userPrompt) {
+            String[] split = userPrompt.split(" ");
+            String command = split[0];
+            switch (command) {
                 case "bye":
                     exit();
                     System.exit(0);
                     break;
                 case "list":
                     StringBuilder s = new StringBuilder();
-                    int i = 1;
-                    for (String item : items) {
-                        s.append(i + ". " + item + "\n");
-                        i++;
+                    for (Task item : items) {
+                        s.append(String.format("%d. [%s] %s\n",
+                                item.getId(), item.getStatusString(), item.getDesc()));
                     }
-                    print(s.toString());
+                    print("You want a list? You got it!\n" + s.toString());
+                    break;
+                case "mark":
+                    if (split.length > 1) {
+                        int index = Integer.parseInt(split[1]);
+                        if (index > 0 && index <= items.size()) {
+                            Task item = items.get(index - 1);
+                            if (item.getStatusString() == "X") {
+                                print("What the sigma? Task already marked!");
+                                break;
+                            }
+                            item.setStatus(true);
+                            print(String.format("SLAYYY! I'm going to mark this done for you:\n [%s] %s", item.getStatusString(), item.getDesc()));
+                        } else {
+                            print("What the skibidi? Invalid task number!");
+                        }
+                    } else {
+                        print("Bro's dreaming. Add a number bozo!");
+                    }
+                    break;
+                case "unmark":
+                    if (split.length > 1) {
+                        int index = Integer.parseInt(split[1]);
+                        if (index > 0 && index <= items.size()) {
+                            Task item = items.get(index - 1);
+                            if (item.getStatusString() == " ") {
+                                print("What the sigma? Task already unmarked!");
+                                break;
+                            }
+                            item.setStatus(false);
+                            print(String.format("Dang, I'm going to unmark this for you:\n [%s] %s", item.getStatusString(), item.getDesc()));
+                        } else {
+                            print("What the skibidi? Invalid task number!");
+                        }
+                    } else {
+                        print("Bro's dreaming. Add a number bozo!");
+                    }
                     break;
                 default:
-                    items.add(userPrompt);
+                    items.add(new Task(userPrompt));
                     print("Added: " + userPrompt);
                     break;
             }
-
             userPrompt = input.nextLine();
         }
         exit();
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private int id;
+    private boolean status;
+    private String desc;
+    private static int count = 1;
+
+    public Task(String desc) {
+        this.id = count;
+        this.status = false;
+        this.desc = desc;
+        count++;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getStatusString() {
+        return status ? "X" : " ";
+    }
+}
