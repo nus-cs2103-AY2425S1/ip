9fb6ab450de234b73ea9c6d13604956327e696f5
dovlin <dovlincs01@gmail.com>
2024-08-20 21:37:51 +0800

    Added support for list

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -20,14 +20,31 @@ public class Momo {
 
         System.out.println("Hello! I'm Momo\nWhat can I do for you?");
         String input = sc.next();
+        String[] list = new String[100];
+        int count = 0;
 
         while (!Objects.equals(input, "bye")) {
+
+            if (Objects.equals(input, "list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; list[i] != null; i++) {
+                    System.out.println(i + 1 + ". " + list[i]);
+                }
+                System.out.println(horizontalLine);
+                input = sc.next();
+                continue;
+
+            }
+            list[count] = input;
             System.out.println(horizontalLine);
-            System.out.println(input);
+            System.out.println("added: " + input);
+            System.out.println(horizontalLine);
             input = sc.next();
+            count++;
         }
 
-        System.out.println("Bye. Hope to see you again soon!\n" + logo);
+        System.out.println(horizontalLine);
+        System.out.println("Farewell... for now. I'll be waiting for your return, taking refuge in your shadows. Rest well... while you still can.\n" + logo);
         System.out.println(horizontalLine);
     }
 }
