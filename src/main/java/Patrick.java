da93a2f909ba2055c45bca7e93f7dd5d4a88c44a
Chronoxy <adriannpcc@gmail.com>
2024-08-20 17:48:30 +0800

    Mark as Done v2 unmark is completed

--- src/main/java/Patrick.java
+++ src/main/java/Patrick.java
@@ -16,12 +16,13 @@ public class Patrick {
             if (input.equals("list")) {
                 for (int i = 1; i <= list.size(); i++) {
                     Task curr = (Task) list.get(i - 1);
-                    System.out.println(i + ". [" + curr.getStatusIcon() + "] "
-                            + curr.description);
+                    System.out.println(i + ". " + curr.toString());
                 }
-            } else if (input.equals("bye")) {
+            }
+            else if (input.equals("bye")) {
                 break;
-            } else if (input.startsWith("mark")) {
+            }
+            else if (input.startsWith("mark")) {
                 String taskNo = input.replace("mark", "").trim();
                 int num = Integer.parseInt(taskNo);
                 Task curr = (Task) list.get(num - 1);
@@ -29,6 +30,15 @@ public class Patrick {
 
                 System.out.println(horizontalLine + "Nice! I've marked this task as done:\n  "
                 + curr.toString() + "\n" + horizontalLine);
+            }
+            else if (input.startsWith("unmark")) {
+                String taskNo = input.replace("unmark", "").trim();
+                int num = Integer.parseInt(taskNo);
+                Task curr = (Task) list.get(num - 1);
+                curr.markAsUndone();
+
+                System.out.println(horizontalLine + "Nice! I've marked this task as not done yet:\n  "
+                        + curr.toString() + "\n" + horizontalLine);
             }
             else {
                 System.out.println("added: " + input + "\n");
