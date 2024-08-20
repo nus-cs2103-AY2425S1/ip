402b539c92ec298344b20e5adbe856dd415e5c7f
hovanleong <hovanleong1@gmail.com>
2024-08-20 20:49:46 +0800

    Add support for deleting tasks from the list

--- src/main/java/SlothingWaffler.java
+++ src/main/java/SlothingWaffler.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SlothingWaffler {
@@ -6,9 +7,7 @@ public class SlothingWaffler {
                 Hello! I'm the Slothing Waffler!
                 Let's stop slothing and get cracking!""");
 
-        Task[] tasks = new Task[100];
-        int tasksCount = 0;
-
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -22,17 +21,17 @@ public class SlothingWaffler {
                     break;
                 }
                 switch (split[0].strip()) {
-                    case "list" -> displayTaskList(tasks, tasksCount);
+                    case "list" -> displayTaskList(tasks);
                     case "mark" -> markTask(tasks, Integer.parseInt(split[1]) - 1);
+                    case "delete" -> deleteTask(tasks, Integer.parseInt(split[1]) - 1);
                     case "todo" -> {
                         if (split.length < 2 || split[1].isBlank()) {
                             throw new SlothingWafflerException("The description of a Todo Task cannot be empty!");
                         }
-                        tasks[tasksCount] = new Todo(split[1]);
-                        tasksCount++;
+                        tasks.add(new Todo(split[1]));
                         System.out.println("Got it. I've added this task:");
-                        System.out.println("  " + tasks[tasksCount - 1].toString());
-                        System.out.println("Now you have " + tasksCount + " tasks in the list.");
+                        System.out.println("  " + tasks.get(tasks.size() - 1).toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     }
                     case "deadline" -> {
                         if (split.length < 2 || split[1].isBlank()) {
@@ -42,11 +41,10 @@ public class SlothingWaffler {
                         if (desc.length < 2) {
                             throw new SlothingWafflerException("The Deadline Task must have a description AND a due date.");
                         }
-                        tasks[tasksCount] = new Deadline(desc[0], desc[1]);
-                        tasksCount++;
+                        tasks.add(new Deadline(desc[0], desc[1]));
                         System.out.println("Got it. I've added this task:");
-                        System.out.println("  " + tasks[tasksCount - 1].toString());
-                        System.out.println("Now you have " + tasksCount + " tasks in the list.");
+                        System.out.println("  " + tasks.get(tasks.size() - 1).toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     }
                     case "event" -> {
                         if (split.length < 2 || split[1].strip().isEmpty()) {
@@ -56,11 +54,10 @@ public class SlothingWaffler {
                         if (desc.length < 3) {
                             throw new SlothingWafflerException("An event must have a description, start time, and end time.");
                         }
-                        tasks[tasksCount] = new Event(desc[0], desc[1], desc[2]);
-                        tasksCount++;
+                        tasks.add(new Event(desc[0], desc[1], desc[2]));
                         System.out.println("Got it. I've added this task:");
-                        System.out.println("  " + tasks[tasksCount - 1].toString());
-                        System.out.println("Now you have " + tasksCount + " tasks in the list.");
+                        System.out.println("  " + tasks.get(tasks.size() - 1).toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     }
                     default -> throw new SlothingWafflerException("The Waffler will continuing slothing!! " +
                             "Please give me instructions that I can understand :(");
@@ -74,16 +71,23 @@ public class SlothingWaffler {
         scanner.close();
     }
 
-    private static void displayTaskList(Task[] tasks, int tasksCount) {
+    private static void displayTaskList(ArrayList<Task> tasks) {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 1; i <= tasksCount; i++) {
-            System.out.println((i) + "." + tasks[i - 1].toString());
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + "." + tasks.get(i).toString());
         }
     }
 
-    private static void markTask(Task[] tasks, int taskNum) {
-        tasks[taskNum].markAsDone();
+    private static void markTask(ArrayList<Task> tasks, int taskNum) {
+        tasks.get(taskNum).markAsDone();
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + tasks[taskNum].toString());
+        System.out.println("  " + tasks.get(taskNum).toString());
+    }
+
+    private static void deleteTask(ArrayList<Task> tasks, int taskNum) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("  " + tasks.get(taskNum));
+        tasks.remove(taskNum);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 }
