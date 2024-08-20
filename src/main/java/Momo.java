fe8ff24ada0b33f94daff5e4359b7cf26940c44c
dovlin <dovlincs01@gmail.com>
2024-08-20 18:55:58 +0800

    Added greetings and renamed main class

---
+++ src/main/java/Momo.java
@@ -0,0 +1,10 @@
+public class Momo {
+
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println("Hello! I'm Momo\nWhat can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!" );
+        System.out.println(horizontalLine);
+    }
+}
