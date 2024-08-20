8b58b26d326db0f3828be23d75ae3e9fe96d7cbb
Darren-Tung <40617230+Darren-Tung@users.noreply.github.com>
2024-08-20 22:14:57 +0800

    Week 2 Level 0
    Added the Tuesday.java file and added the dialogues

---
+++ src/main/java/Tuesday.java
@@ -0,0 +1,12 @@
+public class Tuesday {
+    public static void main(String[] args) {
+        String welcome_msg = "_______________________________\n"
+                + "Hello! I'm Tuesday\n"
+                + "What can I do for you?\n"
+                + "_______________________________";
+        String exit_msg = "Bye. Hope to see you again soon!\n"
+                + "_______________________________\n";
+        System.out.println(welcome_msg);
+        System.out.println(exit_msg);
+    }
+}
