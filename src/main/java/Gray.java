79b0300e0623ab6803d9f225a53edb710523c9a6
Yan Hwee <yanhweeyee@gmail.com>
2024-08-20 10:49:14 +0800

    Add list feature

--- src/main/java/Gray.java
+++ src/main/java/Gray.java
@@ -1,15 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
 
 public class Gray {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        ArrayList<String> userList = new ArrayList<>();
         say("""
                 Hello! I'm Gray.
                 What can I do for you?""");
         while (true) {
-            String user = reader.next();
-            if (user == null || user.equals("bye")) break;
-            say(user);
+            String user = reader.nextLine();
+            if (user == null) break;
+            else if (user.equals("bye")) break;
+            else if (user.equals("list")) {
+                StringJoiner joiner = new StringJoiner("\n");
+                for (int i = 0; i < userList.size(); i++) {
+                    joiner.add(String.format("%d. %s", i + 1, userList.get(i)));
+                }
+                say(joiner.toString());
+            }
+            else {
+                userList.add(user);
+                say(String.format("added: %s", user));
+            }
         }
         say("Bye. Hope to see you again soon!");
     }
@@ -19,5 +34,6 @@ public class Gray {
         System.out.print("\t");
         System.out.println(text.replace("\n", "\n\t"));
         System.out.println("\t____________________________________________________________");
+        System.out.println();
     }
 }
