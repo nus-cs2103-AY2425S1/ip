8edc4350d933b106463d0bfeba4a5416a2f89686
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-08-20 17:44:09 +0800

    Added echo

--- src/main/java/Twilight.java
+++ src/main/java/Twilight.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Twilight {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello! I am Twilight your personal assistant\nWhat can I do for you?" );
-        System.out.println("Bye!");
+        String s = input.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(s);
+            s = input.nextLine();
+        }
+        System.out.println("See you");
     }
 }
