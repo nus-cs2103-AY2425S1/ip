c6d16646d357beabbd32ed13b174e491a51a0f63
Wong Wei Han <56218253+WeiHanWong@users.noreply.github.com>
2024-08-20 17:14:07 +0800

    Level 1. Echo

--- src/main/java/Will.java
+++ src/main/java/Will.java
@@ -1,8 +1,23 @@
-public class Duke {
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Will {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = "WILL";
         System.out.println("Hello! I'm" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________________");
+        while(true) {
+            String userInput = scanner.nextLine();
+            System.out.println("_____________________________________");
+            if(Objects.equals(userInput, "bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(userInput);
+            System.out.println("_____________________________________");
+        }
     }
 }
+
