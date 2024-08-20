f8847e15b2681310e82ecee90e08b27323d07225
Yan Hwee <yanhweeyee@gmail.com>
2024-08-20 12:02:52 +0800

    Add mark/unmark feature

--- src/main/java/Gray.java
+++ src/main/java/Gray.java
@@ -1,29 +1,61 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringJoiner;
-import java.util.stream.IntStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Gray {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        ArrayList<String> userList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
+
         say("""
                 Hello! I'm Gray.
                 What can I do for you?""");
+
         while (true) {
+
             String user = reader.nextLine();
+
             if (user == null) break;
-            else if (user.equals("bye")) break;
-            else if (user.equals("list")) {
+
+            if (user.equals("bye")) break;
+
+            if (user.equals("list")) {
                 StringJoiner joiner = new StringJoiner("\n");
-                for (int i = 0; i < userList.size(); i++) {
-                    joiner.add(String.format("%d. %s", i + 1, userList.get(i)));
+                for (int i = 0; i < taskList.size(); i++) {
+                    joiner.add(String.format("%d. %s", i + 1, taskList.get(i)));
                 }
                 say(joiner.toString());
+                continue;
             }
-            else {
-                userList.add(user);
-                say(String.format("added: %s", user));
+
+            {
+                Pattern pattern = Pattern.compile("(mark|unmark) (-?\\d+)");
+                Matcher matcher = pattern.matcher(user);
+                if (matcher.matches()) {
+                    String command = matcher.group(1);
+                    int index = Integer.parseInt(matcher.group(2));
+                    if (index <= 0 || index > taskList.size()) {
+                        say("Not a valid index");
+                        continue;
+                    }
+                    Task task = taskList.get(index - 1);
+                    if (command.equals("mark")) {
+                        task.mark();
+                        say(String.format("Nice! I've marked this task as done\n\t%s", task));
+                    } else {
+                        task.unmark();
+                        say(String.format("Ok, I've unmarked this task as not done yet:\n\t%s", task));
+                    }
+                    continue;
+                }
+            }
+
+            {
+                Task task = new Task(user);
+                taskList.add(task);
+                say(String.format("added: %s", task));
             }
         }
         say("Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        char icon = isDone ? 'X' : ' ';
+        return String.format("[%c] %s", icon, description);
+    }
+}
