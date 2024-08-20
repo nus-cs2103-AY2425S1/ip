54120a37fe1232a1d59ed632d9388d68a929633e
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2024-08-20 12:58:41 +0800

    Add the ability to mark tasks as done and change status back to not done
    - Implemented Task class

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -3,14 +3,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Alfred {
-    private static List<String> lis;
+    private static List<Task> lis;
 
     public static void main(String[] args) {
         // Create a Scanner Object
         Scanner in = new Scanner(System.in);
 
         // List to store text entered by used
-        lis = new ArrayList<String>();
+        lis = new ArrayList<>();
 
         // Greet user
         greet();
@@ -20,8 +20,32 @@ public class Alfred {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 printList();
+            } else if (isMarkCommand(input)) {
+                int taskNumber = getTaskNumberFromInput(input);
+                Task task = lis.get(taskNumber - 1);
+
+                task.markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Indeed, Sir, the task has been duly completed:");
+                System.out.println("    " + task);
+                System.out.println("____________________________________________________________");
+
+            } else if (isUnmarkCommand(input)) {
+                int taskNumber = getTaskNumberFromInput(input);
+                Task task = lis.get(taskNumber - 1);
+
+                task.unmark();
+                System.out.println("____________________________________________________________");
+                System.out.println("Very well, Sir, the task remains outstanding:");
+                System.out.println("    " + task);
+                System.out.println("A reminder that even small tasks deserve attention.");
+                System.out.println("____________________________________________________________");
+
             } else {
-                addToList(input);
+                Task task = new Task(input);
+                lis.add(task);
+
+                printAddedMessage(input);
             }
             input = in.nextLine();
         }
@@ -29,32 +53,52 @@ public class Alfred {
     }
 
     public static void greet() {
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    Good day Sir. I am Alfred, your English butler.");
-        System.out.println("    Might I offer you some tea, or perhaps something stronger to suit the occasion?");
-        System.out.println("    ____________________________________________________________");
+        System.out.println("____________________________________________________________");
+        System.out.println("Good day Sir. I am Alfred, your English butler.");
+        System.out.println("Might I offer you some tea, or perhaps something stronger to suit the occasion?");
+        System.out.println("____________________________________________________________");
     }
 
     public static void farewell() {
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    Farewell Sir. Should you need anything, you know where to find me.");
-        System.out.println("    ____________________________________________________________");
+        System.out.println("____________________________________________________________");
+        System.out.println("Farewell Sir. Should you need anything, you know where to find me.");
+        System.out.println("____________________________________________________________");
     }
 
-    public static void addToList(String input) {
-        lis.add(input);
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    added: " + input);
-        System.out.println("    ____________________________________________________________");
+    public static void printAddedMessage(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + input);
+        System.out.println("____________________________________________________________");
     }
 
     public static void printList() {
-        System.out.println("    ____________________________________________________________");
+        System.out.println("____________________________________________________________");
         int counter = 1;
-        for (String s : lis) {
-            System.out.println("    " + counter + ". " + s);
+        for (Task task : lis) {
+            System.out.println(counter + ". " + task);
             counter++;
         }
-        System.out.println("    ____________________________________________________________");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static boolean isMarkCommand(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 2) {
+            return false;
+        }
+        return parts[0].equals("mark");
+    }
+
+    public static boolean isUnmarkCommand(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 2) {
+            return false;
+        }
+        return parts[0].equals("unmark");
+    }
+
+    public static int getTaskNumberFromInput(String input) {
+        String[] parts = input.split(" ");
+        return Integer.parseInt(parts[1]);
     }
 }
