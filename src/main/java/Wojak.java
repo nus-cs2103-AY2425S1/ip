6965f17abab4c594ff1bedb2917a71fcf9aa1377
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-08-20 13:52:53 +0000

    feat: add List and add to list

--- src/main/java/Wojak.java
+++ src/main/java/Wojak.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Wojak {
+
+    public static void printList(ArrayList<String> ls) {
+        if (!ls.isEmpty()) {
+            System.out.println("____________________________________________________________");
+            for (int i = 0; i < ls.size(); i++) {
+                System.out.println((i+1) + ". " + ls.get(i));
+            }
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println("List is empty");
+            System.out.println("____________________________________________________________");
+        }
+
+    }
+
     public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Wojak\n" +
@@ -10,7 +29,15 @@ public class Wojak {
         Scanner sc = new Scanner(System.in);
         String nextLine = sc.nextLine();
         while (!(nextLine.equals("bye"))) {
-            System.out.println(nextLine);
+            if (nextLine.equals("list")) {
+                printList(list);
+            } else {
+                list.add(nextLine);
+                System.out.println("____________________________________________________________\n" +
+                        "added: " + nextLine + "\n" +
+                        "____________________________________________________________\n");
+            }
+
             nextLine = sc.nextLine();
         }
 
