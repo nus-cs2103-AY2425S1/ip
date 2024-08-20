46a78c44a0f4e29c159aebb60be1c9db1ea5d853
Chronoxy <adriannpcc@gmail.com>
2024-08-20 13:42:09 +0800

    Echo

--- src/main/java/Patrick.java
+++ src/main/java/Patrick.java
@@ -1,8 +1,18 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Patrick {
     public static void main(String[] args) {
         String horizontalLine = "-----------------------------------------------------------------------\n";
-        String greetingMsg = "Hello! I'm Patrick\nWhat can I do for you?\n";
+        String greetingMsg = horizontalLine + "Hello! I'm Patrick\nWhat can I do for you?\n" + horizontalLine;
         String exitMsg = "Bye. Hope to see you again soon!\n";
-        System.out.println(horizontalLine + greetingMsg + horizontalLine + exitMsg + horizontalLine);
+        String input;
+
+        Scanner inputMsg = new Scanner(System.in);
+        System.out.println(greetingMsg);
+        do {
+            input = inputMsg.nextLine();
+            System.out.println(horizontalLine + input + "\n" + horizontalLine);
+        } while (!input.equals("bye"));
+        System.out.println(exitMsg);
     }
 }
