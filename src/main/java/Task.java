bcb01700335fcbd2836bc41a53b84abf634b04c9
Zac <zacheryngky@gmail.com>
2024-08-20 17:17:44 +0800

    Add feature to mark task as done / undone

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,30 @@
 // TODO: Change this into an interface for later levels
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return this.isDone ? "[X]" : "[ ]";
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
     @Override
     public String toString() {
-        return this.description;
+        return this.getStatusIcon() + " " + this.description;
     }
 }
