2671ca649e3d092f681c4a53f5bc70b78ba0eb23
Darren-Tung <40617230+Darren-Tung@users.noreply.github.com>
2024-08-20 23:05:54 +0800

    Update Tuesday.java
    Changed the messages to methods and added list and blah

--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -1,12 +1,27 @@
 public class Tuesday {
-    public static void main(String[] args) {
-        String welcome_msg = "_______________________________\n"
-                + "Hello! I'm Tuesday\n"
+    public static String Msg_welcome() {
+        return "_______________________________\n"
+                + "Hello! I'm Tuesday, a randomly created bot.\n"
                 + "What can I do for you?\n"
                 + "_______________________________";
-        String exit_msg = "Bye. Hope to see you again soon!\n"
-                + "_______________________________\n";
-        System.out.println(welcome_msg);
-        System.out.println(exit_msg);
+    }
+    public static String Msg_bye() {
+        return "Bye bye. Hope to see you again soon!\n"
+                + "_______________________________";
+    }
+    public static String Msg_list() {
+        return "list\n"
+                + "_______________________________";
+    }
+    public static String Msg_blah() {
+        return "blah\n"
+                + "_______________________________";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Msg_welcome());
+        System.out.println(Msg_list());
+        System.out.println(Msg_blah());
+        System.out.println(Msg_bye());
     }
 }
