d41f55d735ed77dfe571d23e27008f564ee7d3c3
tingxuanp <peh.tingx@gmail.com>
2024-08-20 15:57:54 +0800

    Implemented skeletal version of chatbot

--- src/main/java/Bestie.java
+++ src/main/java/Bestie.java
@@ -1,10 +1,11 @@
 public class Bestie {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // greets the user
+        System.out.println("Hello! I'm Bestie.");
+        System.out.println("What can I do for you?");
+
+        // exit
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
