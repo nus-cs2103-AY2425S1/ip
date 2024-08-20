b2075f7f9732f319d8041df27e4a60d92d0a3b56
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-20 20:46:01 +0800

    Properly implemented Task types

--- src/main/java/ThatOneGuy.java
+++ src/main/java/ThatOneGuy.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ThatOneGuy {
     private static String line = "____________________________________________________________";
@@ -29,43 +28,119 @@ public class ThatOneGuy {
         System.out.println(line);
     }
 
+    public static void markTask(String next) {
+        Integer id = Integer.parseInt(next.substring(5)) - 1;
+        if (id < 0 || id >= tasks.size()) {
+            System.out.println(line + '\n' + "That's not a valid ID, bozo.");
+        } else {
+            tasks.get(id).mark();
+            System.out.println(line);
+            System.out.println("Eh. Consider this task done:");
+            System.out.println(tasks.get(id).toString());
+            System.out.println(line);
+        }
+    }
+
+    public static void unmarkTask(String next) {
+        Integer id = Integer.parseInt(next.substring(7)) - 1;
+        if (id < 0 || id >= tasks.size()) {
+            System.out.println(line + '\n' + "That's not a valid ID, bozo.");
+        } else {
+            tasks.get(id).unmark();
+            System.out.println(line);
+            System.out.println("Sucks to be you. Looks like you haven't done this task:");
+            System.out.println(tasks.get(id).toString());
+            System.out.println(line);
+        }
+    }
+    /*
+    public static void addTask(String next)  {
+        Task nextTask = new Task(next);
+        tasks.add(nextTask);
+        System.out.println(line + "\n" + "added: " + next);
+        System.out.println("That's " + tasks.size() + " tasks for your ass to handle.\n" + line);
+    }
+
+     */
+
+    public static int locate(String keyword, String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            if (keyword.equals(input[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String chain(String[] input, int start, int end) {
+        String res = "";
+        for (int i = start; i < end; i++) {
+            res += input[i] + " ";
+        }
+        res += input[end];
+        return res;
+    }
+
+    public static void addToDo(String[] splitStr) {
+        int len = splitStr.length;
+        String task = chain(splitStr, 1, len - 1);
+        ToDo res = new ToDo(task);
+        tasks.add(res);
+        System.out.println(line + "\n" + "added: " + res);
+        System.out.println("That's " + tasks.size() + " tasks for your ass to handle.\n" + line);
+    }
+
+    public static void addDeadline(String[] splitStr) {
+        int len = splitStr.length;
+        int byId = locate("/by", splitStr);
+        String task = chain(splitStr, 1, byId - 1);
+        String deadline = chain(splitStr, byId + 1, len - 1);
+
+        Deadline res = new Deadline(task, deadline);
+        tasks.add(res);
+        System.out.println(line + "\n" + "added: " + res);
+        System.out.println("That's " + tasks.size() + " tasks for your ass to handle.\n" + line);
+    }
+
+    public static void addEvent(String[] splitStr) {
+        int len = splitStr.length;
+        int fromId = locate("/from", splitStr);
+        int toId = locate("/to", splitStr);
+        String task = chain(splitStr, 1, fromId - 1);
+        String from = chain(splitStr, fromId + 1, toId - 1);
+        String to = chain(splitStr, toId + 1, len - 1);
+
+        Event res = new Event(task, from, to);
+        tasks.add(res);
+        System.out.println(line + "\n" + "added: " + res);
+        System.out.println("That's " + tasks.size() + " tasks for your ass to handle.\n" + line);
+    }
+
     public static void cmd() {
         Scanner sc = new Scanner(System.in);
-        String next;
+        String next, command;
+        String[] splitStr;
         while (true) {
             next = sc.nextLine();
             if (next.equals("bye")) {
                 break;
             } else if (next.equals("list")) {
                 list();
-            } else if (next.startsWith("mark ")) {
-                Integer id = Integer.parseInt(next.substring(5)) - 1;
-                if (id < 0 || id >= tasks.size()) {
-                    System.out.println(line + '\n' + "That's not a valid ID, bozo.");
-                } else {
-                    tasks.get(id).mark();
-                    System.out.println(line);
-                    System.out.println("Eh. Consider this task done:");
-                    System.out.println(tasks.get(id).toString());
-                    System.out.println(line);
-                }
-
-            } else if (next.startsWith("unmark ")) {
-                Integer id = Integer.parseInt(next.substring(7)) - 1;
-                if (id < 0 || id >= tasks.size()) {
-                    System.out.println(line + '\n' + "That's not a valid ID, bozo.");
-                } else {
-                    tasks.get(id).unmark();
-                    System.out.println(line);
-                    System.out.println("Sucks to be you. Looks like you haven't done this task:");
-                    System.out.println(tasks.get(id).toString());
-                    System.out.println(line);
-                }
             } else {
-                Task nextTask = new Task(next);
-                tasks.add(nextTask);
-                System.out.println(line + "\n" + "added: " + next);
-                System.out.println("That's " + tasks.size() + " tasks for your ass to handle.\n" + line);
+                splitStr = next.split(" ");
+                command = splitStr[0];
+
+                if (command.equals("mark")) {
+                    markTask(next);
+                } else if (command.equals("unmark")) {
+                    unmarkTask(next);
+                } else if (command.equals("todo")){
+                    addToDo(splitStr);
+                } else if (command.equals("deadline")){
+                    addDeadline(splitStr);
+                } else if (command.equals("event")){
+                    addEvent(splitStr);
+                }
             }
         }
         sc.close();
