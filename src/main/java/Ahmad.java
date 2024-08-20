e8224d2a93b36f8ce7b47f4d80556c13d51bdcf7
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-08-20 10:59:58 +0800

    Rename bot to Ahmad.

---
+++ src/main/java/Ahmad.java
@@ -0,0 +1,7 @@
+public class Ahmad {
+
+  public static void main(String[] args) {
+    String welcomeMsg = "Hello! I'm Ahmad\nWhat can I do for you?\n\nOkay Byeee!";
+    System.out.println(welcomeMsg);
+  }
+}
