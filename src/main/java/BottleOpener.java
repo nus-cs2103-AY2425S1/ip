a9c2662a710cf654329d34f4fcf917d74cf9e80d
wassilililily <e0725370@u.nus.edu>
2024-08-20 22:34:44 +0800

    Add list

--- src/main/java/BottleOpener.java
+++ src/main/java/BottleOpener.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BottleOpener {
     public static void main(String[] args) {
@@ -14,16 +15,32 @@ public class BottleOpener {
         System.out.println(spacer + greeting + spacer);
 
         String userInput = "";
+        int index = 0;
+        ArrayList<String> tasks = new ArrayList<String>();
+
         while (true) {
             try {
                 userInput = br.readLine();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            if (!userInput.equalsIgnoreCase("bye")) {
-                System.out.println(spacer + userInput + "\n" + spacer);
-            } else {
+            if (userInput.equalsIgnoreCase("bye")) {
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+
+                String taskOutput = "";
+                for (int i = 0; i < tasks.size(); i++) {
+                    String eachTask = String.format("%d. %s",i+1, tasks.get(i));
+                    taskOutput = taskOutput + eachTask + "\n";
+                }
+                System.out.println(spacer + taskOutput + spacer);
+
+            } else {
+                System.out.println(spacer + "added: " +
+                        userInput + "\n" + spacer);
+
+                tasks.add(index, userInput);
+                index += 1;
             }
         }
 
