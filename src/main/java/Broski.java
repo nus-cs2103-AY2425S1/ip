05fe8199e3813e5a4d2ae2e7ad50b8b7a0cd6202
AlphaPyke <jng@Joshuas-MacBook-Pro-2.local>
2024-08-20 15:19:53 +0800

    Echo

--- src/main/java/Broski.java
+++ src/main/java/Broski.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Broski {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "_________________________________________";
+        String exit = "Bye. See ya around!";
         System.out.println(line);
         System.out.println("Wassup! I'm Broski!");
         System.out.println("What can I do for you bro?");
         System.out.println(line);
-        System.out.println("Bye. See ya around!");
+        String reply = scanner.nextLine();
+        System.out.println(line);
+        while (!reply.equals("bye")) {
+            System.out.println(reply);
+            System.out.println(line);
+            reply = scanner.nextLine();
+            System.out.println(line);
+        }
+        System.out.println(exit);
+        System.out.println(line);
     }
 }
