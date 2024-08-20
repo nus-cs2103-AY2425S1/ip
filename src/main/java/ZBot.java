50300d2c3b71f2b559294ad386066b6d38128307
liauzhanyi <liauzhanyi@gmail.com>
2024-08-20 21:38:31 +0800

    refactor: Update ZBot's addTask method to use ArrayList for tasks
    The ZBot class previously used an array to store tasks, but it has been refactored to use an ArrayList instead. This change improves the task display format and allows for dynamic resizing of the task list. The addTask method has been updated to use the add() method of ArrayList to add tasks, and the taskIndex variable has been removed.

--- src/main/java/ZBot.java
+++ src/main/java/ZBot.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ZBot {
-    private static final Task[] tasks = new Task[100];
-    private static int taskIndex = 0;
+    private static final ArrayList<Task> tasks = new ArrayList<Task>();
 
     public static void main(String[] args) {
         greet();
@@ -86,35 +86,34 @@ public class ZBot {
             }
         }
 
-        tasks[taskIndex] = task;
-        taskIndex = (taskIndex + 1) % 100;
+        tasks.add(task);
 
         System.out.println("Got it. I've added this task:");
         System.out.println(task);
-        System.out.println(String.format("Now you have %d tasks in the list.\n", taskIndex));
+        System.out.println(String.format("Now you have %d tasks in the list.\n", tasks.size()));
     }
 
     public static void listTask() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.length; i++) {
-            if (tasks[i] != null) {
-                System.out.println(String.format("%d. %s", i + 1, tasks[i]));
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i) != null) {
+                System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
             }
         }
         System.out.println();
     }
 
     public static void markTaskAsDone(int taskNumber) {
-        tasks[taskNumber - 1].markAsDone();
+        tasks.get(taskNumber - 1).markAsDone();
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println(tasks[taskNumber - 1]);
+        System.out.println(tasks.get(taskNumber - 1));
         System.out.println();
     }
 
     public static void markTaskAsUndone(int taskNumber) {
-        tasks[taskNumber - 1].markAsUndone();
+        tasks.get(taskNumber - 1).markAsUndone();
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(tasks[taskNumber - 1]);
+        System.out.println(tasks.get(taskNumber - 1));
         System.out.println();
     }
 }
