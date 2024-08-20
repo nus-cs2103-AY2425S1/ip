726997e9cbe6832b5d4c473843dee1e4771c21b2
jialegitgud <jiale.official@gmail.com>
2024-08-20 14:48:26 +0800

    add l2 functionalities

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class Peter {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Peter\nWhat can I do for you?");
+        System.out.println("Hello! I'm Peter\nWhat can I do for you?\n");
 
+        // Init Globals
+        String[] tasks = new String[100];
+        int lastIndex = 0;
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine().strip();
 
         while (!command.equals("bye")) {
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < lastIndex; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[lastIndex++] = command;
+                System.out.println("added: " + command);
+            }
+
             command = scanner.nextLine();
         }
 
