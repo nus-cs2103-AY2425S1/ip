bff090a8899ed6aa1185591a4cbd721476c72d53
Soumyadip <soumyadip1107@gmail.com>
2024-08-20 12:35:17 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Assistinator.java
@@ -0,0 +1,12 @@
+public class Assistinator {
+    public static void main(String[] args) {
+        System.out.println("______________________________________________");
+        System.out.println("Hello! I'm Assistinator");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________________");
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________");
+
+    }
+}
