a47114043073c7428fda6d51700bf6faa28240d1
dominic2412 <521hxt@gmail.com>
2024-08-20 23:53:47 +0800

    Level-3

--- src/main/java/Skywalker.java
+++ src/main/java/Skywalker.java
@@ -1,6 +1,7 @@
 import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 public class Skywalker {
@@ -9,12 +10,12 @@ public class Skywalker {
         System.out.println("Hello! I'm Skywalker");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        Task[] tasks = new Task[100];
-        int count = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
 
 
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             String printable = scanner.nextLine();
             System.out.println("____________________________________________________________");
             try {
@@ -26,65 +27,87 @@ public class Skywalker {
                     break;
                 } else if (Objects.equals(printable, "list")) {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < count; i++) {
-                        System.out.println(i + 1 + ". " + tasks[i].toString());
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + ". " + tasks.get(i).toString());
                     }
                     System.out.println("____________________________________________________________");
                 } else if (printable.startsWith("mark ")) {
-                    System.out.println("Nice! I've marked this task as done:");
                     int index = Integer.parseInt(printable.split(" ")[1]) - 1;
-                    tasks[index].markDone();
-                    System.out.println(tasks[index].toString());
+                    if(index<0 || index >= tasks.size()) {
+                        throw new IndexOutOfBoundsException("Task number is out of range!");
+                    }
+                    tasks.get(index).markDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks.get(index).toString());
+                    System.out.println("____________________________________________________________");
 
                 } else if (printable.startsWith("unmark ")) {
-                    System.out.println("OK, I've marked this task as not done yet:");
                     int index = Integer.parseInt(printable.split(" ")[1]) - 1;
-                    tasks[index].unmarkDone();
-                    System.out.println(tasks[index].toString());
+                    if(index<0 || index >= tasks.size()) {
+                        throw new IndexOutOfBoundsException("Task number is out of range!");
+                    }
+                    tasks.get(index).unmarkDone();
+                    System.out.println(tasks.get(index).toString());
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("____________________________________________________________");
                 } else if (printable.startsWith("todo ")) {
-                    //exceptions handling to check if the command is coorect
+                    //exceptions handling to check if the command is correct
                     if (printable.substring(5).isEmpty()) {
                         throw new EmptyDescriptionException("the todo task description cannot be empty!!!!");
                     }
                     //System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
                     Task task = new Todo(printable.substring(5));
-                    tasks[count++] = task;
+                    tasks.add(task);
+                    System.out.println("Got it. I've added this task:");
                     System.out.println(task.toString());
-                    System.out.println("Now you have " + (count) + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
 
                 } else if (printable.startsWith("deadline ")) {
                     // System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
+
                     String[] information = printable.substring(9).split("/by ");
                     String description = information[0];
                     String by = information[1];
-                    //emptyexception catching
-                    if (description.isEmpty() || by.isEmpty()) {
+                    //empty exception catching
+                    if (information.length<2 || description.isEmpty() || by.isEmpty()) {
                         throw new EmptyDescriptionException("the deadline task description/date cannot be empty!!!!");
                     }
+                    System.out.println("Got it. I've added this task:");
                     Deadline task = new Deadline(description, by);
-                    tasks[count++] = task;
+                    tasks.add(task);
                     System.out.println(task.toString());
-                    System.out.println("Now you have " + (count) + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else if (printable.startsWith("event ")) {
                     //  System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
                     String[] information = printable.substring(6).split("/from |/to ");
                     String description = information[0];
                     String from = information[1];
                     String to = information[2];
-                    if (description.isEmpty() || from.isEmpty() || to.isEmpty()) {
+                    if (information.length<3 || from.isEmpty() || to.isEmpty()) {
                         throw new EmptyDescriptionException("the deadline task description/ from date/ to date cannot be empty!!!!");
                     }
+                    System.out.println("Got it. I've added this task:");
                     Event task = new Event(description, from, to);
-                    tasks[count++] = task;
+                    tasks.add(task);
                     System.out.println(task.toString());
-                    System.out.println("Now you have " + (count) + " tasks in the list.");
-                } else {
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } else if (printable.startsWith("delete ")) {
+
+                    int index = Integer.parseInt(printable.substring(7))-1;
+                    if(index<0 || index >= tasks.size()) {
+                        throw new IndexOutOfBoundsException("Task number is out of range!");
+                    }
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(tasks.get(index).toString());
+                    tasks.remove(index);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                }
+
+                else {
                     throw new UnknownCommandException("the command is not correct :( check again!");
                 }
-            } catch (EmptyDescriptionException | UnknownCommandException e) {
+            } catch (EmptyDescriptionException | IndexOutOfBoundsException| UnknownCommandException e) {
                 System.out.println("OOPS!" + e.getMessage());
             } catch (Exception e) { // safety net catch error
                 System.out.println("OOPS! Something is wrong: " + e.getMessage());
