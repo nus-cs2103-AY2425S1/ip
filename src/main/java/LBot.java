26363e8a4e0fad84d6a154c0c23f1e7285134e92
Lewis <elyreum7@gmail.com>
2024-08-20 19:29:27 +0800

    Level 1 Echo

--- src/main/java/LBot.java
+++ src/main/java/LBot.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class LBot {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm LBot";
-        String help = "What can I do for you?";
+        String greeting = "Hello! I'm LBot, your dedicated personal assistant ;)\nWhat can I do for you?";
         String exitMsg = "Bye. Hope to smell you again!";
+        // Initialise Scanner object
+        Scanner scanner = new Scanner(System.in);
         System.out.println(greeting);
-        System.out.println(help);
+        String userInput = "";
+        while (!userInput.equals("bye")){
+            userInput = scanner.nextLine();
+            if (!userInput.equals("bye")){
+                System.out.println(userInput);
+            }
+        }
         System.out.println(exitMsg);
     }
 }
