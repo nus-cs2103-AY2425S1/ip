21f62230811cea516ab9abe78d7c1a22fc77d1b3
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-20 22:44:25 +0800

    Testament.java: implement add, list
    implement add and list function through the use of Task.java and TaskList.java

--- src/main/java/Testament.java
+++ src/main/java/Testament.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 
 public class Testament {
     final static String LINE = "____________________________________________________________";
+    static TaskList taskList;
     public static void main(String[] args) {
+
         boolean powerOn = true;
+        taskList = new TaskList();
         Scanner scanner = new Scanner(System.in);
+
         printDialogue(" Morning!\n Nice day for a stroll, don't you think?");
         while (powerOn) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 printDialogue(" I'd say it's time for a tea break. Milk and sugar for you?");
                 powerOn = false;
+            } else if (userInput.equals("schedule")) {
+                printDialogue(taskList.toString());
             } else {
-                printDialogue(userInput);
+                printDialogue(userInput + " has been added to your schedule.");
+                taskList.add(userInput);
             }
         }
     }
