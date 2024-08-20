75bdc6a9a000bab431ca782437820bb81515902b
Kieran's Laptop <kieran.koh@gmail.com>
2024-08-20 16:40:40 +0800

    Add Level-1 requirements

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -1,16 +1,40 @@
+import java.util.*;
+
 public class Count {
     public final String spacer = "\n____________________________________________________________\n";
+    public void reply(String output) {
+        System.out.println(spacer + output + spacer);
+    }
     public void greet() {
-        System.out.println(spacer + "Hello! I'm Count!\nWhat can I do for you?" + spacer);
+        reply("Hello! I'm Count!\nWhat can I do for you?");
     }
 
     public void farewell() {
-        System.out.println("Bye. Hope to see you again soon!" + spacer);
+        reply("Bye. Hope to see you again soon!");
+    }
+
+    // Default case just echoes
+    public void parser(String input) {
+        switch (input.toLowerCase()) {
+            case "hello":
+                greet();
+                break;
+            case "bye":
+                farewell();
+                break;
+            default:
+                reply(input);
+        }
     }
 
     public static void main(String[] args) {
         Count c = new Count();
+        Scanner sc = new Scanner(System.in);
         c.greet();
-        c.farewell();
+
+        while (true) {
+            String input = sc.nextLine();
+            c.parser(input);
+        }
     }
 }
