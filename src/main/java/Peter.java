9aef9caa70eee0795e57afe02cdc88cb439d92c9
jialegitgud <jiale.official@gmail.com>
2024-08-20 14:08:32 +0800

    add l0 functionalities

---
+++ src/main/java/Peter.java
@@ -0,0 +1,6 @@
+public class Peter {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Peter\\nWhat can I do for you?");
+        System.out.println("Bye, hope to see you again soon!");
+    }
+}
