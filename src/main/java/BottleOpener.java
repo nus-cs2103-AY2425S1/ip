818141564c62a5d366a05bf386dafcd4a1ecbab5
wassilililily <e0725370@u.nus.edu>
2024-08-20 17:40:34 +0800

    Added echo

--- src/main/java/BottleOpener.java
+++ src/main/java/BottleOpener.java
@@ -1,12 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BottleOpener {
     public static void main(String[] args) {
         String spacer = "-----------------------------------\n";
         String botName = "BottleOpener";
         String greeting = "Hello! I'm " + botName + ".\n";
-
         String goodbye = "Bye! See you next time!\n";
 
-        System.out.println(spacer + greeting + spacer
-                + goodbye + spacer);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(spacer + greeting + spacer);
+
+        String userInput = "";
+        while (true) {
+            try {
+                userInput = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if (!userInput.equalsIgnoreCase("bye")) {
+                System.out.println(spacer + userInput + "\n" + spacer);
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(spacer + goodbye + spacer);
     }
 }
