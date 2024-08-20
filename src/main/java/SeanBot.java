94dd92807ba8af717a2a1c11523555996c3288d1
sean_wong_fromtp <sean_wong_from.tp@mom.gov.sg>
2024-08-20 16:17:41 +0800

    Added delete function and testing for it

--- src/main/java/SeanBot.java
+++ src/main/java/SeanBot.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class SeanBot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm SeanBot\n" + "What can I do for you?");
-        Task[] tasks = new Task[100];
-        int counter = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -18,52 +17,50 @@ public class SeanBot {
                     break;
                 } else if (first.equals("list")) {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < counter; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
                     }
                 } else if (first.equals("mark")) {
                     if (part.length < 2) {
                         throw new SeanBotException("The task number of mark cannot be empty");
                     }
                     int second = Integer.parseInt(part[1]) - 1;
-                    if (second < 0 || second >= counter) {
+                    if (second < 0 || second >= tasks.size()) {
                         throw new SeanBotException("The task number must be valid");
                     }
-                    tasks[second].Done();
+                    tasks.get(second).Done();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[second]);
+                    System.out.println("  " + tasks.get(second));
                 } else if (first.equals("unmark")) {
                     if (part.length < 2) {
                         throw new SeanBotException("The task number of unmark cannot be empty");
                     }
                     int second = Integer.parseInt(part[1]) - 1;
-                    if (second < 0 || second >= counter) {
+                    if (second < 0 || second >= tasks.size()) {
                         throw new SeanBotException("The task number must be valid");
                     }
-                    tasks[second].Undone();
+                    tasks.get(second).Undone();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[second]);
+                    System.out.println("  " + tasks.get(second));
                 } else if (first.equals("todo")) {
                     if (part.length < 2) {
                         throw new SeanBotException("The description of a todo cannot be empty.");
                     }
                     Task todo = new Todo(part[1]);
-                    tasks[counter] = todo;
-                    counter++;
+                    tasks.add(todo);
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  " + todo);
-                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else if (first.equals("deadline")) {
                     String[] specifications = part[1].split(" /by ");
                     if (specifications.length < 2) {
                         throw new SeanBotException("The description or a deadline cannot be empty.");
                     }
                     Task deadline = new Deadline(specifications[0], specifications[1]);
-                    tasks[counter] = deadline;
-                    counter++;
+                    tasks.add(deadline);
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  " + deadline);
-                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else if (first.equals("event")) {
                     String[] firstPart = part[1].split(" /from ", 2);
                     if (firstPart.length < 2) {
@@ -74,11 +71,22 @@ public class SeanBot {
                         throw new SeanBotException("The end time of an event cannot be empty.");
                     }
                     Task event = new Event(firstPart[0].trim(), secondPart[0].trim(), secondPart[1].trim());
-                    tasks[counter] = event;
-                    counter++;
+                    tasks.add(event);
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  " + event);
-                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } else if (first.equals("delete")) {
+                    if (part.length < 2) {
+                        throw new SeanBotException("The task number to delete must be valid.");
+                    }
+                    int second = Integer.parseInt(part[1]) - 1;
+                    if (second < 0 || second >= tasks.size()) {
+                        throw new SeanBotException("The task number to delete must be valid.");
+                    }
+                    Task remove = tasks.remove(second);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println("  " + remove);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else {
                     throw new SeanBotException("I'm sorry, but I don't know what that means :-(");
                 }
