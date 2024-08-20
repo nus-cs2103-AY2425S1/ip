154853c5b93afd4886d05fbc508ebdb10cfa0b93
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2024-08-20 12:56:57 +0800

    implemented add and list functionalities

--- src/main/java/Mahesh.java
+++ src/main/java/Mahesh.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Mahesh {
+
+    private static String[] list = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         String divider = "-------------------------------------------------------";
         String logo = 
@@ -20,17 +24,39 @@ public class Mahesh {
         System.out.println(divider);
         
         Scanner scan = new Scanner(System.in);
+        boolean exit = false;
         
-        while (true) {
+        while (!exit) {
             String input = scan.nextLine();
             System.out.println(divider);
-            if (input.equals("bye")) break;
-            System.out.println(input);
+            switch (input) {
+                case "list":
+                    Mahesh.printList();
+                    break;
+                case "bye":
+                    exit = true;
+                    break;
+                default:
+                    Mahesh.addToList(input);
+            }
             System.out.println(divider);
         }
 
         System.out.println("Bye. Hope to see you again soon!\n");
         System.out.println(divider);
 
+    }
+
+    private static void addToList(String task) {
+        Mahesh.list[taskCount++] = task;
+        System.out.println("added: " + task);
+    }
+
+    private static void printList() {
+        int count = 1;
+        for (String task : Mahesh.list) {
+            if (task == null) break;
+            System.out.println(count++ + ". " + task);
+        }
     }
 }
