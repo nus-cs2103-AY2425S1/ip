22b12be4078853dc934493a545759cea92208eba
AlphaPyke <jng@Joshuas-MacBook-Pro-2.local>
2024-08-20 15:02:29 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Broski.java
@@ -0,0 +1,10 @@
+public class Broski {
+    public static void main(String[] args) {
+        String line = "_________________________________________";
+        System.out.println(line);
+        System.out.println("Wassup! I'm Broski!");
+        System.out.println("What can I do for you bro?");
+        System.out.println(line);
+        System.out.println("Bye. See ya around!");
+    }
+}
