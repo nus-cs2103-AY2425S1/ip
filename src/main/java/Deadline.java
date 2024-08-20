cf8eab9393595d2920cad1ca284d45d97cdb920a
Wu Zengfu <wuzengfu666@gmail.com>
2024-08-20 21:25:31 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task {
+    private final String deadline;
+    public Deadline (String taskName, String deadline) {
+        super(taskName);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+    }
+}
