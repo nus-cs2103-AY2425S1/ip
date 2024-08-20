ceb029ef95690b8a7997226f0de2a770ba741a80
Xingye Zhou <xingyechou@gmail.com>
2024-08-21 01:48:16 +0800

    add list and add functions

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Miku {
+    public static ArrayList<String> itemList = new ArrayList<>();
+    public static String sectionBreak = "\n_______________________________________________";
+
     public static void main(String[] args) {
         String logo = " __  __   __   _   _   _    _\n"
                 + "|  \\/  | |  | | | / / | |  | |\n"
@@ -40,46 +44,63 @@ public class Miku {
 
 
 //        System.out.println(mikuFace);
-        String sectionBreak = "\n_______________________________________________";
 
         System.out.println(sectionBreak);
 
         Miku.greet();
 
+        //Variables
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        System.out.println(sectionBreak);
+        //Variables End
 
         while (!input.equals("bye")){
             input = scanner.nextLine();
             System.out.println(sectionBreak);
 
-            if (!input.equals("bye")){
-                Miku.echo(input);
-                System.out.println(sectionBreak);
+            if (input.equals("bye")){
+                break;
+            } else if (input.equals("list")) {
+                Miku.printList();
+            } else {
+                Miku.addItem(input);
             }
         }
 
         Miku.farewell();
+
+
+    }
+
+    public static void addItem(String item){
+        System.out.println("added: " + item);
+        itemList.add(item);
         System.out.println(sectionBreak);
-
-
+    }
+    public static void printList(){
+        for (String item: itemList) {
+            System.out.println(item);
+        }
+        System.out.println(sectionBreak);
     }
 
     public static void greet(){
         String greeting = "Hello! I'm Miku\n"
                 + "What song do you want to listen to today?";
         System.out.println(greeting);
+        System.out.println(sectionBreak);
     }
 
     public static void farewell(){
 
         String farewell = "Bye. Hope to see you again soon!";
         System.out.println(farewell);
+        System.out.println(sectionBreak);
     }
 
     public static void echo(String input){
         System.out.println(input);
+        System.out.println(sectionBreak);
     }
 
 }
