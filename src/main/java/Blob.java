fccb91cdf930e83e10ea87e3ffc9392f096f6232
superb-sushi <caleb.ang.zx@gmail.com>
2024-08-20 20:54:37 +0800

    Level-2

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Blob {
     private static boolean active = true;
+    private static ArrayList<String> db = new ArrayList<>();
     public static void main(String[] args) {
         Scanner human = new Scanner(System.in);
         System.out.println("______________________________________________");
@@ -17,8 +19,16 @@ public class Blob {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("______________________________________________");
                 active = false;
+            } else if (Objects.equals(action.toLowerCase(), "list")){
+                int i = 1;
+                for (String act : db) {
+                    System.out.println(String.format("%d. %s", i, act));
+                    i++;
+                }
+                System.out.println("______________________________________________");
             } else {
-                System.out.println(action);
+                db.add(action);
+                System.out.println(String.format("added: %s", action));
                 System.out.println("______________________________________________");
             }
         }
