38351b01aa86c9d01f43848955280820359d782d
IanCheah <iancheah24@gmail.com>
2024-08-21 01:28:15 +0800

    included add and list logic

--- src/main/java/Hyperion.java
+++ src/main/java/Hyperion.java
@@ -6,6 +6,9 @@ public class Hyperion {
         String greet1 = "Hello! I'm Hyperion\n";
         String greet2 = "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!";
+        // initialize an array of size 100 to store user input
+        String[] allUserInputs = new String[100];
+        int count = 0;
         // greets the user
         System.out.println(solidLine + greet1 + greet2 + solidLine);
         // obtains user input and exits only when user types bye
@@ -13,7 +16,19 @@ public class Hyperion {
         do {
             Scanner scannerObj = new Scanner(System.in);
             input = scannerObj.nextLine();
-            System.out.println(solidLine + input + "\n" + solidLine);
+            System.out.println(solidLine);
+            if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    String index = String.format("%d. ", i + 1);
+                    System.out.println(index + allUserInputs[i] + "\n");
+                }
+            } else {
+                System.out.println("added: " + input);
+                allUserInputs[count] = input;
+                count++;
+            }
+            System.out.println(solidLine);
         } while (!input.equals("bye"));
+        System.out.println(solidLine + exit + "\n" + solidLine);
     }
 }
