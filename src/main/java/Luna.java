d6b0218610b8d5b58e94e98774a4b0067ec56de6
gladwintan <gladwintan@hotmail.sg>
2024-08-20 17:51:40 +0800

    Luna.java: Add error handling
    Lack of error handling for incorrect inputs from user
    
    Let's create new exception class to deal with erroneous inputs from user
    and update function to catch potential errors
    
    Using new exception class allows us to isolate the error to bad inputs
    from user

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -14,68 +14,143 @@ public class Luna {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
-                String exit = "Bye! Hope to see you again soon!";
-                System.out.println(exit);
-                break;
-            }
-
-            if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskNum; i++) {
-                    String taskStr = String.format("%d.%s",
-                            i + 1, tasks[i].toString());
-                    System.out.println(taskStr);
+            try {
+                if (input.isEmpty()) {
+                    throw new LunaException("""
+                            Please enter one of the task type followed by the task description:
+                            "todo": todo return book
+                            "deadline": deadline return book /by Sunday
+                            "event": event project meeting /from Mon 2pm /to 4pm
+                            """);
                 }
-            } else {
-                String[] str = input.split(" ", 2);
-                String command = str[0];
 
-                if (command.equals("mark")) {
-                    int i = Integer.parseInt(str[1]) - 1;
-                    tasks[i].markAsDone();
+                if (input.equals("bye")) {
+                    String exit = "Bye! Hope to see you again soon!";
+                    System.out.println(exit);
+                    break;
+                }
 
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[i].toString());
-
-                } else if (command.equals("unmark")) {
-                    int i = Integer.parseInt(str[1]) - 1;
-                    tasks[i].unmark();
-
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[i].toString());
-
-                } else {
-                    String[] taskDescription = str[1].split(" /");
-                    Task task = null;
-
-                    switch (command) {
-                        case "todo":
-                            task = new ToDo(taskDescription[0]);
-                            tasks[taskNum] = task;
-                            break;
-
-                        case "deadline":
-                            task = new Deadline(taskDescription[0],
-                                    taskDescription[1].substring(3));
-                            tasks[taskNum] = task;
-                            break;
-
-                        case "event":
-                            task = new Event(taskDescription[0],
-                                    taskDescription[1].substring(5),
-                                    taskDescription[2].substring(3));
-                            tasks[taskNum] = task;
-                            break;
+                if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskNum; i++) {
+                        String taskStr = String.format("%d.%s",
+                                i + 1, tasks[i].toString());
+                        System.out.println(taskStr);
                     }
+                } else {
+                    String[] str = input.split(" ", 2);
+                    String command = str[0];
 
-                    taskNum++;
-                    assert task != null;
+                    if (command.equals("mark")) {
+                        if (str.length == 1) {
+                            throw new LunaException("Indicate the task number to mark as done e.g. mark 2");
+                        }
 
-                    String s = String.format("Now you have %d tasks in the list.", taskNum);
-                    System.out.println("Got it. I've added this task:\n" +
-                            "  " + task.toString() + "\n" + s);
+                        int i = Integer.parseInt(str[1]) - 1;
+
+                        if (i >= taskNum || i < 0) {
+                            throw new LunaException("Invalid task number. Type \"list\" to view tasks.");
+                        }
+
+                        tasks[i].markAsDone();
+
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks[i].toString());
+
+                    } else if (command.equals("unmark")) {
+                        if (str.length == 1) {
+                            throw new LunaException("Indicate the task number to unmark e.g. unmark 2");
+                        }
+
+                        int i = Integer.parseInt(str[1]) - 1;
+
+                        if (i >= taskNum || i < 0) {
+                            throw new LunaException("Invalid task number. Type \"list\" to view tasks.");
+                        }
+
+                        tasks[i].unmark();
+
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  " + tasks[i].toString());
+
+                    } else {
+                        Task task = null;
+
+                        switch (command) {
+                            case "todo":
+                                if (str.length == 1) {
+                                    throw new LunaException("Enter description for todo e.g. todo [description]");
+                                }
+
+                                task = new ToDo(str[1]);
+                                tasks[taskNum] = task;
+                                break;
+
+                            case "deadline":
+                                if (str.length == 1 || str[1].trim().indexOf("/") == 0) {
+                                    throw new LunaException("Enter description for deadline e.g. " +
+                                                            "deadline return book /by Sunday");
+                                }
+
+                                String[] deadline = str[1].split(" /");
+
+                                if (deadline.length == 1) {
+                                    throw new LunaException("Enter deadline for task " +
+                                                            "e.g. deadline [task] /by [deadline]");
+                                }
+
+                                if (!deadline[1].contains("by ") && deadline[1].length() <= 3) {
+                                    throw new LunaException("Enter deadline for task " +
+                                                            "e.g. deadline [task] /by [deadline]");
+                                }
+
+                                task = new Deadline(deadline[0], deadline[1].substring(3));
+                                tasks[taskNum] = task;
+                                break;
+
+                            case "event":
+                                if (str.length == 1 || str[1].trim().indexOf("/") == 0) {
+                                    throw new LunaException("Enter description for event e.g. " +
+                                                            "event project meeting /from Mon 2pm /to 4pm");
+                                }
+
+                                if (!str[1].contains("/from ") || !str[1].contains("/to ")) {
+                                    throw new LunaException("Enter start and end time for event " +
+                                                            "e.g. event [task] /from [startTime] /to [endTime]");
+                                }
+
+                                String[] event = str[1].split(" /");
+
+                                if (!(event[1].contains("from ") && event[1].length() > 5) ||
+                                    !(event[2].contains("to ") && event[2].length() > 3)) {
+                                    throw new LunaException("Enter start and end time for event using the format: " +
+                                                            "event [task] /from [startTime] /to [endTime]");
+                                }
+
+                                task = new Event(event[0],
+                                        event[1].substring(5),
+                                        event[2].substring(3));
+                                tasks[taskNum] = task;
+                                break;
+
+                            default:
+                                throw new LunaException("""
+                                        Invalid Command!
+                                        Please enter one of the task type followed by the task description:
+                                        "todo": todo return book
+                                        "deadline": deadline return book /by Sunday
+                                        "event": event project meeting /from Mon 2pm /to 4pm
+                                        """);
+                        }
+
+                        taskNum++;
+                        String s = String.format("Now you have %d tasks in the list.", taskNum);
+                        System.out.println("Got it. I've added this task:\n" +
+                                "  " + task.toString() + "\n" + s);
+                    }
                 }
+            } catch (LunaException e) {
+                System.out.println(e.getMessage());
             }
         }
 
