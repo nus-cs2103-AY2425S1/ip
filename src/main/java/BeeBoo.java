cfdbf34c4cc3344818493fe73188aa9c04189f83
rubinnn <e1155263.u.nus.edu>
2024-08-20 15:21:22 +0800

    Level-2

--- src/main/java/BeeBoo.java
+++ src/main/java/BeeBoo.java
@@ -1,17 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BeeBoo {
 
+    //Arraylist for the tasklist
+    private ArrayList<String> list;
+
+    //Initialise tasklist when beeboo is created
+    public BeeBoo() {
+        list = new ArrayList<>();
+    }
+
+    //Adding to the tasklist
+    private String addList(String str) {
+        list.add(str);
+        return "added: " + str;
+    }
+
+    //Returning list when prompted
+    private String produceList() {
+        String result = "";
+        for (String str : list) {
+            result = result + (list.indexOf(str) + 1) + ". " + str + "\n";
+        }
+        return result;
+    }
+
+    //Creating chatbox
     private void chatBox(String str) {
-        //creating top horizontal lines
         for (int i = 0; i < 60; i++) {
             System.out.print("-");
         }
         System.out.println();
-        //printing chat output
-        System.out.println(" " + str);
+        System.out.println(str);
         System.out.println();
-        //creating bottom horizontal lines
         for (int i = 0; i < 60; i++) {
             System.out.print("-");
         }
@@ -19,17 +41,23 @@ public class BeeBoo {
     }
 
     public static void main(String[] args) {
-        //Initialising BeeBoo and Scanner
+        //Initialise the chatbot and scanner
         BeeBoo beeBoo = new BeeBoo();
         Scanner input = new Scanner(System.in);
 
-        //getting user input
-        beeBoo.chatBox("Hello! I'm BeeBoo\n What can i do for you?");
+        //Getting user input
+        beeBoo.chatBox("Hello! I'm BeeBoo\nWhat can i do for you?");
         String text = input.nextLine().toLowerCase();
 
-        //Keeping chatbot open till bye is typed
+        //add user input till user types bye
         while (!text.equals("bye")) {
-            beeBoo.chatBox(text);
+            if (text.equals("list")) {
+                //produces list when user types list
+                beeBoo.chatBox(beeBoo.produceList());
+            } else {
+                //else add items to list
+                beeBoo.chatBox(beeBoo.addList(text));
+            }
             text = input.nextLine().toLowerCase();
         }
         beeBoo.chatBox("Bye. Hope to see you again soon!");
