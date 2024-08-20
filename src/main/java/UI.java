6e4eb92b370ae796e2d81bc6dcbcc0ba6817dbe6
Wu Zengfu <wuzengfu666@gmail.com>
2024-08-20 18:28:16 +0800

    Rename chatbot, add UI.java to greet users and then exit

---
+++ src/main/java/UI.java
@@ -0,0 +1,11 @@
+public class UI {
+    public void greeting() {
+        System.out.println("---------------------------------------------------------------\n"
+                                   + "Hello! I'm EchoChat\nWhat can I do for you?\n\n" + "---------------------------------------------------------------");
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!\n"
+                                   + "---------------------------------------------------------------\n");
+    }
+}
