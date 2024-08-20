3a3b0a735c84a2cc92a5db9372eca3135c680cac
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2024-08-20 12:37:16 +0800

    implemented echo functionality

--- src/main/java/Mahesh.java
+++ src/main/java/Mahesh.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Mahesh {
     public static void main(String[] args) {
+        String divider = "-------------------------------------------------------";
         String logo = 
                   "#     #                                       ######                               ###### \n"
                 + "##   ##   ##   #    # ######  ####  #    #    #     #   ##   #      #              #      \n"
@@ -8,15 +11,26 @@ public class Mahesh {
                 + "#     # ###### #    # #           # #    #    #     # ###### #      #              #      \n"
                 + "#     # #    # #    # #      #    # #    #    #     # #    # #      #              #      \n"
                 + "#     # #    # #    # ######  ####  #    #    ######  #    # ###### ######         ###### \n";
+        
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("-------------------------------------------------------");
+        System.out.println(divider);
         System.out.println("Hello! I'm Mahesh Dall-E [but you can call me Mahesh ;)]");
         System.out.println("What can I do for you?\n");
-        System.out.println("-------------------------------------------------------");
+        System.out.println(divider);
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while (true) {
+            String input = scan.nextLine();
+            System.out.println(divider);
+            if (input.equals("bye")) break;
+            System.out.println(input);
+            System.out.println(divider);
+        }
 
         System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("-------------------------------------------------------");
+        System.out.println(divider);
 
     }
 }
