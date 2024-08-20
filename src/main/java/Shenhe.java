fdfe5287c317d05edb812213c309cb133530e27b
lr <lruniapp1020@gmail.com>
2024-08-20 22:00:18 +0800

    Add Shenhe's introduction, repeat function and bye function

--- src/main/java/Shenhe.java
+++ src/main/java/Shenhe.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class Shenhe {
     public static void main(String[] args) {
         String logo = "Shenhe";
-        System.out.println("Hello from" + " " + logo);
+        System.out.println("Hello, Ying. I'm Shenhe. The assumption that every person has somewhere to call home is \n" +
+                "naive. I got used to living in the mountains alongside the birds and beasts a long time ago. \n" +
+                "You, are not the only traveller, but the most interesting one.");
+        System.out.println("What do you want today?");
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Shenhe");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        // Main loop
+        while (true) {
+            userInput = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+        }
+
+        System.out.println("Bye travaller. Hope to see you again soon.");
         System.out.println("____________________________________________________________");
     }
 }
