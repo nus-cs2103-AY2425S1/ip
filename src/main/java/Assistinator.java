346a234812007145a8a3b6e1c719eeeea679d9e9
Soumyadip <soumyadip1107@gmail.com>
2024-08-20 13:21:43 +0800

    Echo

--- src/main/java/Assistinator.java
+++ src/main/java/Assistinator.java
@@ -1,12 +1,24 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Assistinator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String command = "";
         System.out.println("______________________________________________");
         System.out.println("Hello! I'm Assistinator");
         System.out.println("What can I do for you?");
         System.out.println("______________________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________");
-
+        while(!Objects.equals(command, "bye")) {
+            command = input.nextLine();
+            System.out.println("______________________________________________");
+            if (!Objects.equals(command, "bye")) {
+                System.out.println(command);
+            } else {
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+            System.out.println("______________________________________________");
+        }
     }
 }
