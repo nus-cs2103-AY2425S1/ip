cc3ee6ab133d077be3436858abffd025953fdf5c
YANYAN-w <e0773610@u.nus.edu>
2024-08-20 18:14:52 +0800

    Level-1

--- src/main/java/Katheryne.java
+++ src/main/java/Katheryne.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
 
 public class Katheryne {
+    private Scanner sc;
 
-    public static void main(String[] args){
-        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
-        System.out.println("Ad astra abyssosque! I'm Katheryne, the receptionist here at the Adventurers' Guild. Welcome to the Adventurers' Guild!");
-        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
-        System.out.println("Farewell, Adventurer, and thank you for supporting the Adventurers' Guild.");
-        System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
+    public Katheryne() {
+        this.sc= new Scanner(System.in);
+    }
+
+    public void run() {
+        System.out.println("Katheryne: " + "Ad astra abyssosque! I'm Katheryne, the receptionist here at the Adventurers' Guild. Welcome!");
+
+        String userInput;
+        boolean finish = false;
+        while (!finish) {
+            userInput = sc.nextLine();
+            if (!userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Katheryne: " + userInput);
+            } else {
+                finish = true;
+                System.out.println("Katheryne: " + "Farewell, Adventurer, and thank you for supporting the Adventurers' Guild.");
+                sc.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Katheryne k = new Katheryne();
+        k.run();
     }
 }
+
+
