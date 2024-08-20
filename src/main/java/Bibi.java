b0efbf3ba43bb69bcbdf69c4877900ebfe5d9527
CliffKoes <cliffkoes@gmail.com>
2024-08-20 23:18:19 +0800

    Added add and list feature

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -12,25 +12,46 @@ public class Bibi {
                         ########   #######   ########   #######\s
                       """;
 
+        // Mini Database
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello from\n" + logo + "\n"
                     + "How can I help you?");
-        System.out.println("---------------------------------");
+        printHorizontalLine();
 
         String cmd;
         while (scanner.hasNext()) {
-            cmd = scanner.next();
+            cmd = scanner.nextLine();
+
+            // Preconfigured commands
             if (cmd.equals("bye")) {
                 break;
+            } else if (cmd.equals("list")) {
+                printHorizontalLine();
+                printTaskList(tasks, taskCount);
+                printHorizontalLine();
+            } else {
+                tasks[taskCount++] = cmd;
+                printHorizontalLine();
+                System.out.printf("added: \"%s\" to task list%n", cmd);
+                printHorizontalLine();
             }
-
-            System.out.println("--------------------------------");
-            System.out.println(cmd);
-            System.out.println("--------------------------------");
         }
 
         // Exit
-        System.out.println("---------------------------------");
+        printHorizontalLine();
         System.out.println("See you soon :3");
-        System.out.println("---------------------------------");
+        printHorizontalLine();
+    }
+
+    private static void printTaskList(String[] tasks, int taskCount) {
+        for (int i = 1; i <= taskCount; i++) {
+            System.out.printf("%d: %s%n", i, tasks[i - 1]);
+        }
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("--------------------------------");
     }
 }
