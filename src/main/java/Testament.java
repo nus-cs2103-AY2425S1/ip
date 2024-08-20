ecf2f938eaf374e0c371de678b77f38f747283a4
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-20 21:31:24 +0800

    Adjust spacing in dialogue

--- src/main/java/Testament.java
+++ src/main/java/Testament.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Testament {
     final static String LINE = "____________________________________________________________";
     public static void main(String[] args) {
         System.out.println(LINE);
-        System.out.println(" Hello! I'm Testament\n What can I do for you?\n");
+        System.out.println(" Hello! I'm Testament\n What can I do for you?");
         System.out.println(LINE);
-        System.out.println(" Bye. Hope to see you again soon!\n");
+        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(LINE);
     }
 }
