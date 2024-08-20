6d2ff6d62d9c400df4c7c04dc2d2790531a4fbf3
jktang14 <tangboy14@gmail.com>
2024-08-20 14:51:18 +0800

    Add greeting message to bot

--- src/main/java/Azir.java
+++ src/main/java/Azir.java
@@ -1,4 +1,10 @@
 public class Azir {
     public static void main(String[] args) {
+        System.out.println("----------------------------------");
+        System.out.println("Hello! I'm Azir");
+        System.out.println("What can I do for you?");
+        System.out.println("----------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------");
     }
 }
