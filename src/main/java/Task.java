d7c97889b228cc8d6ebd1afd76b5ad7366d7ca38
bigismols <bigismols239@gmail.com>
2024-08-20 13:39:23 +0800

    Added skeleton for todos and deadlines

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,18 @@
 public class Task {
     private String name;
+    private boolean finished;
 
     public Task(String name) {
         this.name = name;
+        this.finished = false;
+    }
+
+    public boolean isDone() {
+        return this.finished;
+    }
+
+    public void finish() {
+        this.finished = true;
     }
 
     @Override
