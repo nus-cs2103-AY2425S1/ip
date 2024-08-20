689ce16e447d0e2f1d341701b0774610eada7ccd
AlphaPyke <jng@Joshuas-MacBook-Pro-2.local>
2024-08-20 16:22:32 +0800

    Add, List

--- src/main/java/Broski.java
+++ src/main/java/Broski.java
@@ -1,23 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Broski {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String line = "_________________________________________";
-        String exit = "Bye. See ya around!";
+    private String line = "_________________________________________";
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<String> list = new ArrayList<>(100);
+    public void start() {
         System.out.println(line);
         System.out.println("Wassup! I'm Broski!");
         System.out.println("What can I do for you bro?");
         System.out.println(line);
+    }
+
+    public void chatbot() {
         String reply = scanner.nextLine();
-        System.out.println(line);
-        while (!reply.equals("bye")) {
-            System.out.println(reply);
+        if (reply.equals("list")) {
             System.out.println(line);
-            reply = scanner.nextLine();
+            for (int i = 1; i <= list.size(); i++) {
+                System.out.println(i + ". " + list.get(i - 1));
+            }
             System.out.println(line);
+            this.chatbot();
+        } else if (reply.equals("bye")) {
+            System.out.println(line);
+            this.exit();
+        } else {
+            System.out.println(line);
+            list.add(reply);
+            System.out.println("added: " + reply);
+            System.out.println(line);
+            this.chatbot();
         }
-        System.out.println(exit);
+    }
+
+    public void exit() {
+        System.out.println("Bye. See ya around!");
         System.out.println(line);
+    }
+
+    public static void main(String[] args) {
+        Broski bot = new Broski();
+        bot.start();
+        bot.chatbot();
     }
 }
