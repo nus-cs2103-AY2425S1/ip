059a60e671df3b805c0c45d4d710215ea5b870da
Aditi Gawande <>
2024-08-20 23:37:37 +0800

    Level 1 Echo

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -1,21 +1,50 @@
+import java.util.Scanner;
+
 public class Max {
+    private final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Max max = new Max();
         max.runMax();
     }
 
     public void runMax() {
-        printLine();
-        printMessage("Hello! I'm Max\nWhat can I do for you?");
-        printMessage("Bye. Hope to see you again soon!");
+        printHello();
+
+        boolean running = true;
+
+        while (running) {
+            String text = scanner.nextLine();
+            if (text.equals("bye")) {
+                running = false;
+            } else {
+                echo(text);
+            }
+        }
+        printBye();
     }
 
     public void printMessage(String message) {
-        System.out.println(message);
+        System.out.println("\t" + message);
         printLine();
     }
 
+    public void printHello() {
+        printLine();
+        printMessage("Hello! I'm Max\n\tWhat can I do for you?");
+    }
+
+    public void printBye() {
+        printLine();
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
     public void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public void echo(String text) {
+        printLine();
+        printMessage(text);
     }
 }
