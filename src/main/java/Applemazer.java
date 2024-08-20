19164400afabf1c5b27e6a2af3fc229a613dae51
DESKTOP-RRESQ08\User <nicchan0207@gmail.com>
2024-08-20 18:14:08 +0800

    Added support for add/list

--- src/main/java/Applemazer.java
+++ src/main/java/Applemazer.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Applemazer {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> items = new ArrayList<>();
 
     private void greeting() {
         String greeting = "Hello! I'm Applemazer.\nWhat can I do for you?\n";
@@ -12,17 +15,32 @@ public class Applemazer {
         System.out.println(farewell);
     }
 
+    private void process() {
+        boolean processing = true;
+        while (processing) {
+            String command = sc.nextLine();
+
+            switch (command) {
+                case "bye" :
+                    processing = false;
+                    break;
+                case "list" :
+                    for (int i = 0; i < items.size(); ++i) {
+                        System.out.println((i+1) + ". " + items.get(i));
+                    }
+                    System.out.println(); // Leave empty line.
+                    break;
+                default:
+                    items.add(command);
+                    System.out.println("added: " + command + "\n");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Applemazer chatBot = new Applemazer();
         chatBot.greeting();
-
-        Scanner sc = new Scanner(System.in);
-        String command = sc.nextLine();
-        while (!command.equals("bye")) {
-            System.out.println(command + "\n");
-            command = sc.nextLine();
-        }
-
+        chatBot.process();
         chatBot.farewell();
     }
 }
