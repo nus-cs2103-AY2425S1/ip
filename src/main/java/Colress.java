39aa02f4c9c2c32b8f8273f115fde461c13eb5cc
JYL27 <junyulee27@gmail.com>
2024-08-20 21:43:44 +0800

    Added echo method and class-level fields to initialise scanner, receive and store inputs, initialise messages and detect exit command.

--- src/main/java/Colress.java
+++ src/main/java/Colress.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Colress {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String input = "";
+    private static String spacer = "______________________________________\n";
+    private static String greetingMessage = "Hello! My name is Colress.\n"
+            + "What can I do for you?";
+    private static String farewellMessage = "Well then, I hope to see you around soon!";
+    private static String exitCommand = "bye";
+    public static void print(String s) {
+        System.out.println(spacer + s + "\n" + spacer);
+    }
+
+    public static void echo() {
+        Colress.input = Colress.scanner.nextLine();
+        while (!Colress.input.equals(exitCommand)) {
+            print(input);
+            Colress.input = Colress.scanner.nextLine();
+        }
+    }
     public static void main(String[] args) {
-        String spacer = "______________________________________\n";
-        System.out.println(spacer + "Hello. My Name is Colress.\n"
-                + "What can I do for you?\n" + spacer
-                + "Well then. I hope to see you soon.\n" + spacer);
+        print(greetingMessage);
+        Colress.echo();
+        print(farewellMessage);
     }
 }
