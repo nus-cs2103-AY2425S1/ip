32564bb2c3bcff4d348f07f1ee67a42a881f96d4
suhayl13 <suhayl.pervez@gmail.com>
2024-08-20 17:27:29 +0800

    Add support for mark as done

--- src/main/java/Lexi.java
+++ src/main/java/Lexi.java
@@ -1,31 +1,60 @@
 import java.util.Scanner;
 
 public class Lexi {
-    static String[] tasks = new String[100];
+    static Task[] tasks = new Task[100];
     static int count = 0;
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         greet();
         String response = userInput.nextLine();
-        while (!response.equals("bye")) {
-            if(response.equals("list")) {
+        while(!response.equals("bye")) {
+            if(response.contains("mark") || response.contains("unmark")) {
+                String[] parts = response.split(" ");
+                int taskNumber =  Integer.parseInt(parts[1]) - 1;
+                Task taskToBeMarked = tasks[taskNumber];
+                if (response.contains("unmark")) {
+                    unmarkTask(taskToBeMarked);
+                } else {
+                    markTask(taskToBeMarked);
+                }
+
+            } else if(response.equals("list")) {
                 listTasks();
             } else {
                 addTask(response);
             }
             response = userInput.nextLine();
         }
+        userInput.close();
         bye();
     }
 
+    private static void unmarkTask(Task taskToBeMarked) {
+        taskToBeMarked.undoTask();
+        System.out.println("____________________________________________________________");
+        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println("   " + taskToBeMarked);
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void markTask(Task taskToBeMarked) {
+        taskToBeMarked.doTask();
+        System.out.println("____________________________________________________________");
+        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println("   " + taskToBeMarked);
+        System.out.println("____________________________________________________________\n");
+    }
+
+
     public static void listTasks() {
         System.out.println("____________________________________________________________");
+        System.out.println(" Here are the tasks in your list:");
         for(int i = 0;i<count;i++) {
-            String currTask = tasks[i];
-            System.out.printf(" %d. %s%n", i+1, currTask);
+            Task currTask = tasks[i];
+            System.out.printf("  %d. %s%n", i+1, currTask);
         }
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
     }
 
     public static void greet() {
@@ -38,11 +67,11 @@ public class Lexi {
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________\n");
     }
-    public static void addTask(String task) {
-        tasks[count] = task;
+    public static void addTask(String taskName) {
+        tasks[count] = new Task(taskName);
         count++;
         System.out.println("____________________________________________________________");
-        System.out.printf(" added: %s%n", task);
+        System.out.printf(" added: %s%n", taskName);
         System.out.println("____________________________________________________________\n");
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    private boolean isDone;
+    private final String taskName;
+
+    public Task(String taskName){
+        this.isDone = false;
+        this.taskName = taskName;
+    }
+    public void doTask() {
+        this.isDone = true;
+    }
+    public void undoTask() {
+        this.isDone = false;
+    }
+    public String toString() {
+        String status = isDone ? "X" : " ";
+        return "[" + status + "] " + this.taskName;
+    }
+
+}
