01cb4054c978e8ea82352e7f150bbded8c0a817d
jialegitgud <jiale.official@gmail.com>
2024-08-20 14:33:47 +0800

    add l1 functionalities

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Peter {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Peter\nWhat can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine().strip();
+
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = scanner.nextLine();
+        }
+
+        // EXIT
         System.out.println("\nBye, hope to see you again soon!");
+        scanner.close();
     }
 }
