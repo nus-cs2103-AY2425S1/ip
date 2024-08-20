0308e669998830fcb90ebeb81cf355093d76fbf4
sean_wong_fromtp <sean_wong_from.tp@mom.gov.sg>
2024-08-20 11:03:09 +0800

    created ability to mark and unmark tasks

--- src/main/java/SeanBot.java
+++ src/main/java/SeanBot.java
@@ -1,24 +1,37 @@
 import java.util.Scanner;
 public class SeanBot {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm SeanBot\n" + //
-                            "What can I do for you?");
-        String[] tasks = new String[100];
+        System.out.println("Hello! I'm SeanBot\n" + "What can I do for you?");
+        Task[] tasks = new Task[100];
         int counter = 0;
         
         Scanner scanner = new Scanner(System.in);
         while(true){
             String userInput = scanner.nextLine();
-            System.out.println(userInput);
-            if (userInput.equals("bye")) {
+            String[] part = userInput.split(" ", 2);
+            String first = part[0];
+            if (first.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (userInput.equals("list")){
+            } else if (first.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < counter; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
-            } else {
-                tasks[counter] = userInput;
+            } else if (first.equals("mark")) {
+                int second = Integer.parseInt(part[1]) - 1;
+                tasks[second].Done();
+                tasks[second].Done();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[second]);
+            } else if (first.equals("unmark")) {
+                int second = Integer.parseInt(part[1]) - 1;
+                tasks[second].Undone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[second]);
+            }
+            else {
+                tasks[counter] = new Task(userInput);
                 counter++;
                 System.out.println("added: " + userInput);
             }
