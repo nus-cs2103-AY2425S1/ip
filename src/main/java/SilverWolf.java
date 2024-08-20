548acb370bbfffc89c9184cc8d2b96ea5359a844
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-08-20 21:20:41 +0800

    added the ability to read and store user input and print them as list. in addition, logo is added

--- src/main/java/SilverWolf.java
+++ src/main/java/SilverWolf.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SilverWolf {
+    // using arraylist
+    private static ArrayList<String> list = new ArrayList<>();
+
+    // output the list
+    public static void outputList(){
+        int index = 1;
+        for(String str : list){
+            System.out.println(index + ". "+list.get((index-1)));
+            index++;
+        }
+    }
+
     public static void main(String[] args) {
         // initialize the scanner to read user input
         Scanner scanner = new Scanner(System.in);
+        System.out.print("  _________.__.__                       __      __      .__   _____ \n");
+        System.out.print(" /   _____/|__|  |___  __ ___________  /  \\    /  \\____ |  |_/ ____\\\n");
+        System.out.print(" \\_____  \\ |  |  |\\  \\/ // __ \\_  __ \\ \\   \\/\\/   /  _ \\|  |\\   __\\ \n");
+        System.out.print(" /        \\|  |  |_\\   /\\  ___/|  | \\/  \\        (  <_> )  |_|  |   \n");
+        System.out.print("/_______  /|__|____/\\_/  \\___  >__|      \\__/\\  / \\____/|____/__|   \n");
+        System.out.print("        \\/                   \\/               \\/                    ");
+
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Silver Wolf\n" +
                 " What can I help you with?\n" +
@@ -12,18 +32,25 @@ public class SilverWolf {
             // reading the user input
             String input = scanner.nextLine();
 
-            // echo the input back to the user
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
-
-
-            // exit if the user types "bye"
+            // exit if the user types "bye" or "list"
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Till we meet again!");
                 System.out.println("____________________________________________________________");
                 break;
+                // display the list that the user entered
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                outputList();
+                System.out.println("____________________________________________________________");
+
+            } else {
+                // echo the input back to the user
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                list.add(input);
+                System.out.println("____________________________________________________________");
+
             }
         }
 
