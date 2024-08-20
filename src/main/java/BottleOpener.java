084973753a8ee85cde64da2822ae2cb8e9bf0a97
wassilililily <e0725370@u.nus.edu>
2024-08-20 12:59:05 +0800

    Renamed main class

---
+++ src/main/java/BottleOpener.java
@@ -0,0 +1,8 @@
+public class BottleOpener {
+    public static void main(String[] args) {
+        String spacer = "---------------------/n";
+        String botName = "BottleOpener";
+        String greeting = "Hello! I'm " + botName + "/n";
+        System.out.println(spacer + greeting + spacer);
+    }
+}
