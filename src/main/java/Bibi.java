b7aa15c331214ac21d592aa5a4f98e65e6a25207
CliffKoes <cliffkoes@gmail.com>
2024-08-21 02:06:19 +0800

    Stricter Regex Checker Code Cleanip

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -25,92 +25,98 @@ public class Bibi {
             cmd = scanner.nextLine();
 
             // Preconfigured commands
-            if (cmd.equals("bye")) {
-                break;
-            } else if (cmd.equals("list")) {
-                printHorizontalLine();
-                printTaskList(tasks, taskCount);
-                printHorizontalLine();
-            } else if (cmd.split(" ")[0].equals("mark")) {
-                String[] cmdArr = cmd.split(" ");
-                printHorizontalLine();
-                if (cmdArr.length != 2) {
-                    System.out.println("Invalid command syntax: Please use \"mark <int>\"");
-                } else if (Integer.parseInt(cmdArr[1]) <= 0 || Integer.parseInt(cmdArr[1]) - 1 >= taskCount) {
-                    System.out.println("Invalid task index");
-                } else {
-                    System.out.printf("Alrighty, marked the following task as done:%n");
-                    Task t = tasks[Integer.parseInt(cmdArr[1]) - 1];
-                    t.markAsDone();
-                    System.out.println(t);
-                    printHorizontalLine();
+            switch (cmd.split(" ")[0]) {
+                case "bye": {
+                    break;
                 }
-            } else if (cmd.split(" ")[0].equals("unmark")) {
-                String[] cmdArr = cmd.split(" ");
-                printHorizontalLine();
-                if (cmdArr.length != 2) {
-                    System.out.println("Invalid command syntax: Please use \"mark <int>\"");
-                } else if (Integer.parseInt(cmdArr[1]) <= 0 || Integer.parseInt(cmdArr[1]) - 1 >= taskCount) {
-                    System.out.println("Invalid task index");
-                } else {
-                    System.out.printf("Oops, we'll get 'em next time:%n");
-                    Task t = tasks[Integer.parseInt(cmdArr[1]) - 1];
-                    t.markAsNotDone();
-                    System.out.println(t);
+                case "list": {
                     printHorizontalLine();
+                    printTaskList(tasks, taskCount);
+                    printHorizontalLine();
+                    break;
                 }
-            } else { // Add to task list
-                switch (cmd.split(" ")[0]) {
-                    case "todo": {
+                case "mark": {
+                    String[] cmdArr = cmd.split(" ");
+                    printHorizontalLine();
+                    if (cmdArr.length != 2) {
+                        System.out.println("Invalid command syntax: Please use \"mark <int>\"");
+                    } else if (Integer.parseInt(cmdArr[1]) <= 0 || Integer.parseInt(cmdArr[1]) - 1 >= taskCount) {
+                        System.out.println("Invalid task index");
+                    } else {
+                        System.out.printf("Alrighty, marked the following task as done:%n");
+                        Task t = tasks[Integer.parseInt(cmdArr[1]) - 1];
+                        t.markAsDone();
+                        System.out.println(t);
                         printHorizontalLine();
-                        if (cmd.length() <= 5) {
-                            System.out.println("Invalid todo syntax: Please use \"todo <description>\"");
-                        } else {
-                            ToDo td = new ToDo(cmd.substring(5));
-                            tasks[taskCount++] = td;
-
-                            // Console
-                            System.out.printf("added: \"%s\" to task list%n", td);
-                        }
-                        break;
                     }
-                    case "deadline": {
+                    break;
+                }
+                case "unmark": {
+                    String[] cmdArr = cmd.split(" ");
+                    printHorizontalLine();
+                    if (cmdArr.length != 2) {
+                        System.out.println("Invalid command syntax: Please use \"mark <int>\"");
+                    } else if (Integer.parseInt(cmdArr[1]) <= 0 || Integer.parseInt(cmdArr[1]) - 1 >= taskCount) {
+                        System.out.println("Invalid task index");
+                    } else {
+                        System.out.printf("Oops, we'll get 'em next time:%n");
+                        Task t = tasks[Integer.parseInt(cmdArr[1]) - 1];
+                        t.markAsNotDone();
+                        System.out.println(t);
                         printHorizontalLine();
-                        if (cmd.length() <= 9) {
-                            System.out.println("Invalid deadline syntax: Please use \"deadline <description> /by <deadline>\"");
-                        } else {
-                            String[] input = cmd.substring(9).split(" /by ");
-                            Deadline dl = new Deadline(input[0], input[1]);
-                            tasks[taskCount++] = dl;
-
-                            // Console
-                            System.out.printf("added: \"%s\" to task list%n", dl);
-                        }
-                        break;
                     }
-                    case "event": {
-                        printHorizontalLine();
-                        if (cmd.length() <= 6) {
-                            System.out.println("Invalid event syntax: Please use \"event <description> /from <time> /to <time>\"");
-                        } else {
-                            String[] input = cmd.substring(6).split(" /from ");
-                            String[] interval = input[1].split(" /to ");
-                            Event e = new Event(input[0], interval[0], interval[1]);
-                            tasks[taskCount++] = e;
+                    break;
+                }
+                case "todo": {
+                    printHorizontalLine();
+                    if (!cmd.matches("todo .+")) {
+                        System.out.println("Invalid todo syntax: Please use \"todo <description>\"");
+                    } else {
+                        ToDo td = new ToDo(cmd.substring(5));
+                        tasks[taskCount++] = td;
 
-                            // Console
-                            System.out.printf("added: \"%s\" to task list%n", e);
-                        }
-                        break;
-                    }
-                    default: {
                         // Console
-                        printHorizontalLine();
-                        System.out.printf("%s is an unknown command%n", cmd);
+                        System.out.printf("added: \"%s\" to task list%n", td);
+                        System.out.printf("You now have %d task(s) to do%n", taskCount);
                     }
+                    break;
                 }
-                System.out.printf("You now have %d task(s) to do%n", taskCount);
-                printHorizontalLine();
+                case "deadline": {
+                    printHorizontalLine();
+                    if (!cmd.matches("deadline .+ /by .+")) {
+                        System.out.println("Invalid deadline syntax: Please use \"deadline <description> /by <deadline>\"");
+                    } else {
+                        String[] input = cmd.substring(9).split(" /by ");
+                        Deadline dl = new Deadline(input[0], input[1]);
+                        tasks[taskCount++] = dl;
+
+                        // Console
+                        System.out.printf("added: \"%s\" to task list%n", dl);
+                        System.out.printf("You now have %d task(s) to do%n", taskCount);
+                    }
+                    break;
+                }
+                case "event": {
+                    printHorizontalLine();
+                    if (!cmd.matches("event .+ /from .+ /to .+")) {
+                        System.out.println("Invalid event syntax: Please use \"event <description> /from <time> /to <time>\"");
+                    } else {
+                        String[] input = cmd.substring(6).split(" /from ");
+                        String[] interval = input[1].split(" /to ");
+                        Event e = new Event(input[0], interval[0], interval[1]);
+                        tasks[taskCount++] = e;
+
+                        // Console
+                        System.out.printf("added: \"%s\" to task list%n", e);
+                        System.out.printf("You now have %d task(s) to do%n", taskCount);
+                    }
+                    break;
+                }
+                default: {
+                    // Console
+                    printHorizontalLine();
+                    System.out.printf("%s is an unknown command%n", cmd);
+                }
             }
         }
 
@@ -118,7 +124,7 @@ public class Bibi {
         printHorizontalLine();
         System.out.println("See you soon :3");
         printHorizontalLine();
-    }
+        }
 
     private static void printTaskList(Task[] tasks, int taskCount) {
         if (taskCount == 0) {
