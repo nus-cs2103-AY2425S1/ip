d759c5f6391494c51d96cda2e9e79e6bbdf77616
lr <lruniapp1020@gmail.com>
2024-08-20 21:10:59 +0800

    Change Duke.java to Shenhe.java and complete level 0

---
+++ src/main/java/Shenhe.java
@@ -0,0 +1,12 @@
+public class Shenhe {
+    public static void main(String[] args) {
+        String logo = "Shenhe";
+        System.out.println("Hello from" + " " + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Shenhe");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
