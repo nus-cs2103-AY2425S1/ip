33d05f83314572b25365bdd59d2cd3a58a5a6186
CliffKoes <cliffkoes@gmail.com>
2024-08-21 01:39:21 +0800

    Added invalid syntax warning for ToDo, Deadline and Event

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -62,41 +62,51 @@ public class Bibi {
             } else { // Add to task list
                 switch (cmd.split(" ")[0]) {
                     case "todo": {
-                        ToDo td = new ToDo(cmd.substring(5));
-                        tasks[taskCount++] = td;
-
-                        // Console
                         printHorizontalLine();
-                        System.out.printf("added: \"%s\" to task list%n", td);
+                        if (cmd.length() <= 5) {
+                            System.out.println("Invalid todo syntax: Please use \"todo <description>\"");
+                        } else {
+                            ToDo td = new ToDo(cmd.substring(5));
+                            tasks[taskCount++] = td;
+
+                            // Console
+                            System.out.printf("added: \"%s\" to task list%n", td);
+                        }
                         break;
                     }
                     case "deadline": {
-                        String[] input = cmd.substring(9).split(" /by ");
-                        Deadline dl = new Deadline(input[0], input[1]);
-                        tasks[taskCount++] = dl;
-
-                        // Console
                         printHorizontalLine();
-                        System.out.printf("added: \"%s\" to task list%n", dl);
+                        if (cmd.length() <= 9) {
+                            System.out.println("Invalid deadline syntax: Please use \"deadline <description> /by <deadline>\"");
+                        } else {
+                            String[] input = cmd.substring(9).split(" /by ");
+                            Deadline dl = new Deadline(input[0], input[1]);
+                            tasks[taskCount++] = dl;
+
+                            // Console
+                            System.out.printf("added: \"%s\" to task list%n", dl);
+                        }
                         break;
                     }
                     case "event": {
-                        String[] input = cmd.substring(6).split(" /from ");
-                        String[] interval = input[1].split(" /to ");
-                        Event e = new Event(input[0], interval[0], interval[1]);
-                        tasks[taskCount++] = e;
-
-                        // Console
                         printHorizontalLine();
-                        System.out.printf("added: \"%s\" to task list%n", e);
+                        if (cmd.length() <= 6) {
+                            System.out.println("Invalid event syntax: Please use \"event <description> /from <time> /to <time>\"");
+                        } else {
+                            String[] input = cmd.substring(6).split(" /from ");
+                            String[] interval = input[1].split(" /to ");
+                            Event e = new Event(input[0], interval[0], interval[1]);
+                            tasks[taskCount++] = e;
+
+                            // Console
+                            System.out.printf("added: \"%s\" to task list%n", e);
+                        }
                         break;
                     }
                     default: {
-                        tasks[taskCount++] = new Task(cmd);
-
                         // Console
                         printHorizontalLine();
-                        System.out.printf("added: \"%s\" to task list%n", cmd);
+                        System.out.printf("%s is an unknown command%n", cmd);
                     }
                 }
                 System.out.printf("You now have %d task(s) to do%n", taskCount);
