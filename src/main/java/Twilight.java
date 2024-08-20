c6def8399e2ac58c77daccec0d957a223634ade3
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-08-20 18:03:25 +0800

    Added the list and add capabilities

--- src/main/java/Twilight.java
+++ src/main/java/Twilight.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Twilight {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! I am Twilight your personal assistant\nWhat can I do for you?" );
-        String s = input.nextLine();
-        while (!s.equals("bye")) {
-            System.out.println(s);
-            s = input.nextLine();
+        ArrayList<String> tasks =  new ArrayList<String>();
+        String command = input.nextLine();
+        while (!command.equals("bye")) {
+            if (command.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("" + (i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(command);
+                System.out.println("added: " + command);
+            }
+            command = input.nextLine();
         }
         System.out.println("See you");
     }
