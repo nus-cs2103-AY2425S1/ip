281e545b8c5a667b79a00231744de8c327a50a3b
yockcheng <e1121069@u.nus.edu>
2024-08-21 16:56:14 +0800

    Change Duke.java

---
+++ src/main/java/Lict.java
@@ -0,0 +1,13 @@
+public class Lict {
+    private final static String name = "Lict";
+    private final static String horizontal_line = "__________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(horizontal_line);
+        System.out.println("Hello! I'm "+ name);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontal_line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal_line);
+    }
+}
