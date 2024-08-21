5c8670517c1371cbec34bac89006276ab53391e8
jktang14 <tangboy14@gmail.com>
2024-08-21 15:11:13 +0800

    Add string representation for Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,12 @@
-public class Deadline {
+public class Deadline extends Task {
+    private String day;
+    public Deadline(String description, String day) {
+        super(description);
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D] %s (by: %s)", super.toString(), this.day);
+    }
 }
