04ef9e2f9aa4f2d84a6a64d38f66774e83c17e05
Jack Bean <144975629+bryanjhc@users.noreply.github.com>
2024-08-21 20:59:25 +0800

    renamed Duke to JackBean, did greetings and exit, added AI.md file

---
+++ AI.md
@@ -0,0 +1,2 @@
+Hello there! I am using GitHub CoPilot for my individual project.
+Will update this AI.md file soon!
---
+++ src/main/java/JackBean.java
@@ -0,0 +1,11 @@
+public class JackBean {
+    public static String horizontalLine = "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(horizontalLine);
+        System.out.println("Hello homie! I'm JackBean, a chatbot designed to help you with your daily tasks!");
+        System.out.println("How may I help you today my homie?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye homie! Come back if you need anything else. JackBean, signing off!");
+        System.out.println(horizontalLine);
+    }
+}
