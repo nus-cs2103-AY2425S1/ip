d5a559f2f33ca64f00d0895d79a8154b2e25f8d7
Siow Rui Ming <siowruiming@gmail.com>
2024-08-21 17:46:11 +0800

    ToDos, Events, Deadlines

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+
+    private final String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -5,23 +5,26 @@ public class Neko {
     private static final String LIST_COMMAND = "list";
     private static final String MARK_COMMAND = "mark";
     private static final String UNMARK_COMMAND = "unmark";
-    private static final String greetingMessage = "  ∧,,,∧  \n( ̳̳• · ̳• ) \n づ Meow! I'm Neko\nWhat can I do for you? ";
-    private static final String exitMessage = "Bye! Hope to see you again soon meow ฅ ฅ";
+    private static final String TODO_COMMAND = "todo";
+    private static final String DEADLINE_COMMAND = "deadline";
+    private static final String EVENT_COMMAND = "event";
+    private static final String GREETING_MESSAGE = "  ∧,,,∧  \n( ̳̳• · ̳• ) \n づ Meow! I'm Neko\nWhat can I do for you? ";
+    private static final String EXIT_MESSAGE = "Bye! Hope to see you again soon meow ฅ ฅ";
     private static int numOfTask = 0;
-    private static final ArrayList<Task> taskList = new ArrayList<>(100);
+    private static final Task[] taskList = new Task[100];
 
     public static void main(String[] args) {
 
-        System.out.println(greetingMessage);
+        System.out.println(GREETING_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        while (!input.equals("bye")) {
+        while (!input.equals(EXIT_COMMAND)) {
             handleInput(input);
             input = scanner.nextLine();
         }
 
-        System.out.println(exitMessage);
+        System.out.println(EXIT_MESSAGE);
         scanner.close();
     }
 
@@ -32,35 +35,50 @@ public class Neko {
             markTask(input);
         } else if (input.startsWith(UNMARK_COMMAND)) {
             unmarkTask(input);
-        } else {
+        } else if (input.startsWith(TODO_COMMAND) || input.startsWith(DEADLINE_COMMAND) || input.startsWith(EVENT_COMMAND)) {
             addTask(input);
+        } else {
+            System.out.println("Invalid command meow. Please try again.");
         }
     }
 
     private static void addTask(String input) {
-        taskList.add(new Task(input));
-        numOfTask++;
-        System.out.println("added: " + input + " ฅ/ᐠᓀ ﻌ ᓂマ");
+        Task task;
+        if (input.startsWith(TODO_COMMAND)) {
+            task = new Todo(input);
+        } else if (input.startsWith(DEADLINE_COMMAND)) {
+            String[] parts = input.split("/", 2);
+            String taskName = parts[0].trim();
+            String deadline = parts[1].substring(3).trim();
+            task = new Deadline(taskName, deadline);
+        } else {
+            String[] parts = input.split("/", 3);
+            String taskName = parts[0].trim();
+            String start = parts[1].substring(5).trim();
+            String end = parts[2].substring(3).trim();
+            task = new Event(taskName, start, end);
+        }
+        taskList[numOfTask++] = task;
+        System.out.println("Got it meow. I've added this task" + task + " ฅ/ᐠᓀ ﻌ ᓂマ");
     }
-
     private static void listTask() {
         System.out.println("Here are the task in your list meow: ");
         for (int i = 0; i < numOfTask; i++) {
-            Task task = taskList.get(i);
+            Task task = taskList[i];
             System.out.println(i + 1 + "." + task);
         }
     }
 
     private static void markTask(String input) {
         int index = Integer.parseInt(input.substring(5).trim()) - 1;
-        Task task = taskList.get(index);
+        Task task = taskList[index];
         task.markAsDone();
         System.out.println("Nice meow! I've marked this task as done: \n " + task);
     }
 
     private static void unmarkTask(String input) {
         int index = Integer.parseInt(input.substring(7).trim()) - 1;
-        Task task = taskList.get(index);
+        Task task = taskList[index];
         task.markAsNotDone();
         System.out.println("Ok meow, I've marked this task as not done yet: \n " + task);
     }
