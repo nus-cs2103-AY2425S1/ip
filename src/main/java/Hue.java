75273af13536b9692a6563411d8a26aefc33ffd6
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-21 15:37:50 +0800

    Finish Level1-3

--- src/main/java/Hue.java
+++ src/main/java/Hue.java
@@ -9,7 +9,7 @@ public class Hue {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String name = "Hue";
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("____________________________________________________________" );
@@ -29,11 +29,30 @@ public class Hue {
             }
 
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here aer the tasks in your list: ");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
+                }
+            } else if (input.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(" " + tasks[taskIndex]);
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (input.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].unmarkDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(" " + tasks[taskIndex]);
+                } else {
+                    System.out.println("Invalid task number");
                 }
             } else if (!input.equalsIgnoreCase("bye")) {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
