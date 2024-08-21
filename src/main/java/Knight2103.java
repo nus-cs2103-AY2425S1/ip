7dc3882de675c3d97628275003cbf59d31601519
Chua Tse Hui <htcylsg@gmail.com>
2024-08-21 09:25:04 +0800

    add & list success

--- src/main/java/Knight2103.java
+++ src/main/java/Knight2103.java
@@ -1,19 +1,37 @@
 import java.util.Scanner;
 
 public class Knight2103 {
+    public static String printList(String[] list, int length) {
+        String stringToPrint = "";
+        for (int i = 0; i < length; i++) {
+            int bulletPoint = i + 1;
+            stringToPrint += bulletPoint + ". " + list[i] + "\n";
+        }
+        return stringToPrint;
+    }
+
     public static void main(String[] args) {
+        String[] taskList = new String[100];
+        int totalTaskNumber = 0;
         String botName = "Knight2103";
         String horiLine = "_____________";
         String intro = horiLine + "\n"
                 + "Hello! I'm " + botName + " \n"
                 + "What can I do for you?\n"
                 + horiLine + "\n";
-        // put scanner object here
+
         System.out.println(intro);
         Scanner scanObject = new Scanner(System.in);
         String input = scanObject.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(horiLine + "\n" + input + "\n" + horiLine);
+            if (input.equals("list")) {
+                String listContents = printList(taskList, totalTaskNumber);
+                System.out.println(horiLine + "\n" + listContents + horiLine);
+            } else {
+                taskList[totalTaskNumber] = input;
+                totalTaskNumber++;
+                System.out.println(horiLine + "\n added: " + input + "\n" + horiLine);
+            }
             input = scanObject.nextLine();
         }
         System.out.println(horiLine + "\n" + "Bye. Hope to see you again soon!" + "\n" + horiLine);
