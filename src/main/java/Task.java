e5cbbebf363fa8015c0244aaf254e7207c15facf
izruff <rafaelky.dev@gmail.com>
2024-08-21 21:42:25 +0800

    Task.java: Add toString method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,4 +22,8 @@ public class Task {
     public void markAsNotDone() {
         this.isDone = false;
     }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
 }
