0a39f3683fd5af58b351a11ad5ebfb4a8ca5622a
M-kew <e0958776@u.nus.edu>
2024-08-22 01:16:22 +0800

    Implemented mark as done feature on chatbot.

--- src/main/java/Nuffle.java
+++ src/main/java/Nuffle.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Nuffle {
 
-    private static ArrayList<String> inputList = new ArrayList<>();
+    // This list contains all Task Class objects
+    private static ArrayList<Task> inputList = new ArrayList<>();
 
     private static void printLine() {
         // This method will be used to print the border
@@ -18,6 +20,39 @@ public class Nuffle {
             for (int index = 0; index < inputList.size(); index++) {
                 System.out.println("" + (index + 1) + ". " + inputList.get(index));
             }
+        }
+    }
+
+    private static void markTask(int index) {
+        // check that index is always more than or equals to 0 and index must be within the inputList size
+        if (index >= 0 && index < inputList.size()) {
+            Task currTask = inputList.get(index);
+            currTask.markAsDone();
+            printLine();
+            System.out.println("Nice! I have marked this task as done!");
+            System.out.println(" " + currTask);
+            printLine();
+        } else {
+            // print as error message
+            printLine();
+            System.out.println("Opps! There appears to be an index error!");
+            printLine();
+        }
+    }
+    private static void unMarkTask(int index) {
+        // check that index is always more than or equals to 0 and index must be within the inputList size
+        if (index >= 0 && index < inputList.size()) {
+            Task currTask = inputList.get(index);
+            currTask.markNotDone();
+            printLine();
+            System.out.println("OK! I have marked this task as not done yet.");
+            System.out.println(" " + currTask);
+            printLine();
+        } else {
+            // print as error message
+            printLine();
+            System.out.println("Opps! There appears to be an index error!");
+            printLine();
         }
     }
 
@@ -50,16 +85,33 @@ public class Nuffle {
                 break;
             } else if (userInput.equals("list")) {
                 // Program will list all the tasks that was input by the user
+                System.out.println("Here are the tasks in your list: ");
                 printLine();
                 outputList();
                 printLine();
+            } else if (userInput.startsWith("mark")) {
+                // Program will mark the specified task based on the index provided
+
+                // Get the index from the user input (e.g. mark 2)
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                // Mark the task as done by calling the function
+                markTask(index);
+            } else if (userInput.startsWith("unmark")) {
+                // Program will unmark the specified task based on the index provided
+
+                // Get the index from the user input (e.g. unmark 2)
+                int index = Integer.parseInt(userInput.substring(7)) - 1;
+                // Unmark the task by calling the function
+                unMarkTask(index);
             }
             else {
                 // Echo the user command
                 printLine();
                 System.out.println("Nuffle > added: " + userInput);
+                // Create a Task object
+                Task task = new Task(userInput);
                 // Add the user input into the list
-                inputList.add(userInput);
+                inputList.add(task);
                 printLine();
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusInfo() {
+        return (isDone ? "X" : " ");
+    }
+    public void markAsDone() {
+        this.isDone = true;
+    }
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String mark = getStatusInfo();
+        return "[" + mark + "] " + description;
+    }
+
+
+
+
+}
