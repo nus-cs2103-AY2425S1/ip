ee56e74f37b48355ac406dcb55bf67990587a4c3
Trisha Patodia <trishapatodia@Trishas-MacBook-Air.local>
2024-08-21 10:15:57 +0800

    Added supoort for list and personalised the chatbot

--- src/main/java/ChatterBox.java
+++ src/main/java/ChatterBox.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class ChatterBox {
     public static void main(String[] args) {
 
+        String[] taskList = new String[100];
+        int counter = 0;
         Scanner sc = new Scanner(System.in);
         //Greeting the user
         System.out.println("\t\t" + "_".repeat(50));
@@ -10,17 +12,26 @@ public class ChatterBox {
         System.out.println("\t\t" + "_".repeat(50));
 
         while (true) {
-            String input = sc.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
+            taskList[counter] = sc.nextLine();
+            if (taskList[counter].equalsIgnoreCase("bye")) {
                 System.out.println("\t\t" + "_".repeat(50));
                 System.out.println("\t\t" + "Bye. Hope to see you again soon!");
                 System.out.println("\t\t" + "_".repeat(50));
                 break;
+            } else if (taskList[counter].equalsIgnoreCase("list")) {
+                System.out.println("\t\t" + "_".repeat(50));
+                System.out.println("\t\t Here's what you've got on your to-do list so far:");
+                for(int i = 0; i < counter; i++) {
+                    System.out.println("\t\t\t- " + taskList[i]);
+                }
+                System.out.println("\t\t" + "_".repeat(50));
             } else {
                 System.out.println("\t\t" + "_".repeat(50));
-                System.out.println("\t\t" + input);
+                System.out.println("\t\t" + taskList[counter] + " is added to your list");
                 System.out.println("\t\t" + "_".repeat(50));
+                counter++;
             }
+
         }
     }
 }
