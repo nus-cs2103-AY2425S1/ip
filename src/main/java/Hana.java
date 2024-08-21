50f70dbe2b43203e2bbc8198cc75c36b6cfae0ab
hansneddyanto <hans.neddy@gmail.com>
2024-08-21 14:18:20 +0800

    Add the features to add and list tasks

--- src/main/java/Hana.java
+++ src/main/java/Hana.java
@@ -7,13 +7,27 @@ public class Hana {
                 "    What can I do for you?\n";
         String closing = "    Bye. Hope to see you again soon!\n";
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        Task[] tasks = new Task[100];
+        int numberOfTasks = 0;
 
         System.out.println(line + greeting + line);
         String input = scanner.nextLine();  // Read user input
         String output = "";
         while (!input.equals("bye")) {
-            output = "    " + input + "\n";
-            System.out.println(line + output + line);
+            if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < numberOfTasks; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            }
+            else {
+                Task task = new Task(input);
+                tasks[numberOfTasks] = task;
+                numberOfTasks++;
+                output = "    added: " + input + "\n";
+                System.out.println(line + output + line);
+            }
             input = scanner.nextLine();
         }
         System.out.println(line + closing + line);
