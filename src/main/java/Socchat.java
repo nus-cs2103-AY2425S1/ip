801e3df800b1ca6cffa50e781b28756a823c30f4
Lynette Lee <sayhitosiying@Lynettes-Laptop.local>
2024-08-21 16:43:20 +0800

    echo messages

--- src/main/java/Socchat.java
+++ src/main/java/Socchat.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Socchat {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        greet();
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.print("> ");
+            if (input.equals("bye")) {
+                exit();
+                break;
+            }
+            System.out.println(input);
+        }
     }
     public static void greet() {
         System.out.println("Hello! I'm Socchat!");
