618ab4fa4d860f152c9527a2ad40179282e00e08
JeremyChiaHanSheng <chiajeremy1@gmail.com>
2024-08-21 23:17:34 +0800

    Level-3

--- src/main/java/NextGPT.java
+++ src/main/java/NextGPT.java
@@ -12,24 +12,43 @@ public class NextGPT {
                 "_______________________________________________________\n";
 
         System.out.println(greeting);
-        List<String> todo_list = new ArrayList<>();
+        List<Task> todo_list = new ArrayList<>();
         while(true) {
             Scanner sc = new Scanner(System.in);
 
-            String todo = sc.nextLine();
-            if (todo.equals("bye")) break;
-            else if (todo.equals("list")) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) break;
+            else if (input.equals("list")) {
+
                 System.out.println("_______________________________________________________\n");
                 for (int i = 0; i <todo_list.size() ; i++) {
-                    System.out.println(i+1 + "." + " " + todo_list.get(i));
+                    System.out.println(i+1 + "." + " " + "[" + todo_list.get(i).getStatusIcon() + "]" + " "  + todo_list.get(i).description);
                 }
                 System.out.println("_______________________________________________________\n");
+            } else if (input.length() >= 6 && input.substring(0, 4).equals("mark")) {
+                int index = Integer.parseInt(input.substring(input.length() - 1));
+                Task task = todo_list.get(index - 1);
+                task.mark();
+                System.out.println("_______________________________________________________\n " +
+                        "Nice! I've marked this task as done:\n   " + "[" + task.getStatusIcon() + "]" + " "+ task.description +
+                        "\n_______________________________________________________\n");
+            }
+            else if (input.length() >= 8 && input.substring(0, 6).equals("unmark")) {
+                int index = Integer.parseInt(input.substring(input.length() - 1));
+                Task task = todo_list.get(index - 1);
+                task.unmark();
+                System.out.println("_______________________________________________________\n " +
+                        "Ok, I've marked this task as not done yet:\n   " + "[" + task.getStatusIcon() + "]" + " " + task.description +
+                        "\n_______________________________________________________\n");
             } else {
-                todo_list.add(todo);
+
+                /** create a task here **/
+                Task new_task = new Task(input);
+                todo_list.add(new_task);
                 System.out.println(
-                        todo + "\n" +
+                        input + "\n" +
                                 "_______________________________________________________\n"
-                                + "added: " + todo + "\n"
+                                + "added: " + new_task.description + "\n"
                                 + "_______________________________________________________\n"
                 );
             }
