295f998bc333176f6b07393758043beed27a6205
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-08-21 21:13:43 +0800

    Add error handling for invalid commands or inputs

--- src/main/java/Neuro.java
+++ src/main/java/Neuro.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Neuro {
-    private static Task getTask(String input) {
+    private static Task getTask(String input) throws IllegalArgumentException {
         // String split inspired by https://www.w3schools.com/java/ref_string_split.asp
         // Array copyOfRange inspired by https://www.geeksforgeeks.org/java-util-arrays-copyofrange-java/
         String[] inputComponents = input.split("[\s]");
@@ -13,6 +13,12 @@ public class Neuro {
         if (taskType.equals("todo")) {
             String description = String.join(" ",
                     Arrays.copyOfRange(inputComponents, 1, inputComponents.length));
+
+            if (description.isEmpty()) {
+                throw new IllegalArgumentException("UH OH! The description of a 'todo' cannot be empty! Try adding" +
+                        " a description like 'todo read book'.");
+            }
+
             task = new Todo(description);
         } else if (taskType.equals("deadline")) {
             int byIndex = -1;
@@ -22,10 +28,25 @@ public class Neuro {
                     break;
                 }
             }
+
+            // Missing /by
+            if (byIndex < 0) {
+                throw new IllegalArgumentException("UH OH! The command given is missing the '/by' input for deadline." +
+                        "Try updating the command like 'deadline finish homework /by Mon 2359'.");
+            }
+
             String description = String.join(" ",
                     Arrays.copyOfRange(inputComponents, 1, byIndex));
             String deadline = String.join(" ",
                     Arrays.copyOfRange(inputComponents, byIndex + 1, inputComponents.length));
+
+            if (description.isEmpty()) {
+                throw new IllegalArgumentException("UH OH! The description of a 'deadline' cannot be empty! Try adding" +
+                        " a description like 'deadline finish homework /by Mon 2359'.");
+            } else if (deadline.isEmpty()) {
+                throw new IllegalArgumentException("UH OH! The deadline time/date of a 'deadline' cannot be empty! Try adding" +
+                        " a valid deadline like 'deadline finish homework /by Mon 2359'.");
+            }
 
             task = new Deadline(description, deadline);
         } else if (taskType.equals("event")) {
@@ -41,6 +62,18 @@ public class Neuro {
                 }
             }
 
+            // Missing /from
+            if (fromIndex < 0) {
+                throw new IllegalArgumentException("UH OH! The command given is missing the '/from' input for event." +
+                        "Try updating the command like 'event project meeting /from Mon 2pm /to 5pm'.");
+            }
+
+            // Missing /to
+            if (toIndex < 0) {
+                throw new IllegalArgumentException("UH OH! The command given is missing the '/to' input for event." +
+                        "Try updating the command like 'event project meeting /from Mon 2pm /to 5pm'.");
+            }
+
             String description = String.join(" ",
                     Arrays.copyOfRange(inputComponents, 1, fromIndex));
             String from = String.join(" ",
@@ -48,9 +81,24 @@ public class Neuro {
             String to = String.join(" ",
                     Arrays.copyOfRange(inputComponents, toIndex + 1, inputComponents.length));
 
+            if (description.isEmpty()) {
+                throw new IllegalArgumentException("UH OH! The description of an 'event' cannot be empty! Try adding" +
+                        " a description like 'event project meeting /from Mon 2pm /to 5pm'.");
+            } else if (from.isEmpty()) {
+                throw new IllegalArgumentException("UH OH! The from time/date of an 'event' cannot be empty! Try adding" +
+                        " a valid from time/date like 'event project meeting /from Mon 2pm /to 5pm'.");
+            } else if (to.isEmpty()) {
+                throw new IllegalArgumentException("UH OH! The to time/date of an 'event' cannot be empty! Try adding" +
+                        " a valid to time/date like 'event project meeting /from Mon 2pm /to 5pm'.");
+            }
+
             task = new Event(description, from, to);
         }
 
+        if (task == null) {
+            throw new IllegalArgumentException("UH OH! I'm sorry but I don't recognise that command. Try the following:" +
+                    " 'todo', 'deadline', 'event', 'list', 'mark', 'unmark', 'bye'.");
+        }
         return task;
     }
 
@@ -70,41 +118,73 @@ public class Neuro {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("    ___________________________________________________");
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println("    " + (i + 1) + ". " + taskList.get(i));
+
+                if (taskList.isEmpty()) {
+                    System.out.println("    You currently have no tasks.");
+                } else {
+                    System.out.println("    Here is a list of all your tasks:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println("    " + (i + 1) + ". " + taskList.get(i));
+                    }
                 }
+
                 System.out.println("    ___________________________________________________");
             } else if (input.startsWith("mark")) {
                 // String split inspired by https://www.w3schools.com/java/ref_string_split.asp
                 String[] inputComponents = input.split("[\s]");
-                int taskIndex = Integer.valueOf(inputComponents[1]);
-                Task task = taskList.get(taskIndex - 1);
-                task.markDone();
 
-                System.out.println("    ___________________________________________________");
-                System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("    " + task);
-                System.out.println("    ___________________________________________________");
+                try {
+                    int taskIndex = Integer.valueOf(inputComponents[1]);
+                    Task task = taskList.get(taskIndex - 1);
+                    task.markDone();
+
+                    System.out.println("    ___________________________________________________");
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("    " + task);
+                    System.out.println("    ___________________________________________________");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("    UH OH! Missing index for 'mark' command! Add a valid index for a task" +
+                            " to mark, like 'mark 2'.");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("    UH OH! Index out of bounds! Try calling the command 'list' to verify the" +
+                            " index of the desired task.");
+                }
+
             } else if (input.startsWith("unmark")) {
                 // String split inspired by https://www.w3schools.com/java/ref_string_split.asp
                 String[] inputComponents = input.split("[\s]");
-                int taskIndex = Integer.valueOf(inputComponents[1]);
-                Task task = taskList.get(taskIndex - 1);
-                task.markUndone();
 
-                System.out.println("    ___________________________________________________");
-                System.out.println("    Ok, I've marked this task as not done:");
-                System.out.println("    " + task);
-                System.out.println("    ___________________________________________________");
+                try {
+                    int taskIndex = Integer.valueOf(inputComponents[1]);
+                    Task task = taskList.get(taskIndex - 1);
+                    task.markUndone();
+
+                    System.out.println("    ___________________________________________________");
+                    System.out.println("    Ok, I've marked this task as not done:");
+                    System.out.println("    " + task);
+                    System.out.println("    ___________________________________________________");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("    UH OH! Missing index for 'unmark' command! Add a valid index for a task" +
+                            " to unmark, like 'unmark 2'.");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("    UH OH! Index out of bounds! Try calling the command 'list' to verify the" +
+                            " index of the desired task.");
+                }
             } else {
-                Task task = getTask(input);
-                taskList.add(task);
-                
-                System.out.println("    ___________________________________________________");
-                System.out.println("    Ok, I've added this task:");
-                System.out.println("        " + task);
-                System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
-                System.out.println("    ___________________________________________________");
+                try {
+                    Task task = getTask(input);
+                    taskList.add(task);
+
+                    System.out.println("    ___________________________________________________");
+                    System.out.println("    Ok, I've added this task:");
+                    System.out.println("        " + task);
+                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+                    System.out.println("    ___________________________________________________");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("    ___________________________________________________");
+                    System.out.println("    " + e.getMessage());
+                    System.out.println("    ___________________________________________________");
+                }
             }
         }
 
