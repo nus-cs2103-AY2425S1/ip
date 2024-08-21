57ea0a7f6a95944789c84d217f965f12aa3432e3
Ansel-Ch <achong2014@gmail.com>
2024-08-21 14:46:03 +0800

    Add echo method and bye method

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Joe {
+    private static final String line =
+            "____________________________________________________________";
+
+    public static void bye() {
+        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+    }
+
+    public static void echo() {
+        Scanner reader = new Scanner(System.in);
+        String userCmd = reader.nextLine();
+        while (!userCmd.equalsIgnoreCase("bye")) {
+            System.out.println(line + "\n" + userCmd + "\n" + line);
+            userCmd = reader.nextLine();
+        }
+        bye();
+    }
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
         System.out.println(line +
                 "\nHello! I'm Joe\n" +
                 "What can I do for you?\n"+
                 line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        echo();
     }
 }
