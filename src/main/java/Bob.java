92da69ca987ec0f8c166d1b93dbdfb05c6ac71e3
Chen Zi Yi <ziyichen2003@gmail.com>
2024-08-21 22:01:24 +0800

    Add user input history

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,25 +1,54 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bob {
+    private static final List<String> userInputHistory = new ArrayList<>();
+    private static int numUserInputs = 0;
+
     public static void main(String[] args) {
-        String name = "Bob";
-        System.out.println("Hello! I'm " + name + "!\nWhat can I do for you?");
+        System.out.println("Hello! I'm Bob the chatbot!\nHow can I help you?");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String userInput = scanner.nextLine();
             String endKeyword = "bye";
+            String historyKeyword = "list";
 
             if (userInput.equalsIgnoreCase(endKeyword)) {
                 System.out.println("Bye! Hope to see you again :)");
                 break;
             }
 
-            // Echo response
-            System.out.println(userInput);
+            // Get input history
+            if (userInput.equalsIgnoreCase(historyKeyword)) {
+                String history = getHistory();
+                System.out.println(history);
+            } else {
+                System.out.println(addUserInput(userInput));
+            }
         }
 
         scanner.close();
+    }
+
+    public static String addUserInput(String userInput) {
+        userInputHistory.add(userInput);
+        numUserInputs++;
+        return "added: " + userInput;
+    }
+
+    public static String getHistory() {
+        StringBuilder history = new StringBuilder();
+        for (int i = 1; i <= numUserInputs; i++) {
+            String userInput = userInputHistory.get(i - 1);
+            if (i == numUserInputs) {
+                history.append(i).append(". ").append(userInput);
+                continue;
+            }
+            history.append(i).append(". ").append(userInput).append("\n");
+        }
+        return history.toString();
     }
 }
