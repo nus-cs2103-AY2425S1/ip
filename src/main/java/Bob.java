6007d9ffa472843408c8de23d0789b2b1e0bd8f7
jessica2828 <jessica.sylviana.js@gmail.com>
2024-08-21 21:54:49 +0800

    Add support for exception handling

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -17,93 +17,110 @@ public class Bob {
             String command = inputParts[0].toLowerCase(); // lowercase for comparison
             String taskDescription = (inputParts.length <= 1) ? "" : inputParts[1];
 
-            switch (command) {
-                case "list":
-                    // print tasks
-                    System.out.println("------------------------------------------");
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < taskCounter; i++) {
-                        int j = i + 1;
-                        System.out.println(j + ". " + tasks[i].toString());
-                    }
-                    System.out.println("------------------------------------------");
-                    break;
-
-                case "mark":
-                    int taskIndexMark = Integer.parseInt(taskDescription) - 1;
-                    if (taskIndexMark < taskCounter && taskIndexMark >= 0) {
-                        tasks[taskIndexMark].mark();
+            try {
+                switch (command) {
+                    case "list":
+                        // print tasks
                         System.out.println("------------------------------------------");
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("[" + tasks[taskIndexMark].getStatusIcon() + "] " + tasks[taskIndexMark].getDescription());
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < taskCounter; i++) {
+                            int j = i + 1;
+                            System.out.println(j + ". " + tasks[i].toString());
+                        }
                         System.out.println("------------------------------------------");
-                    } else {
-                        System.out.println("Invalid index..");
-                    }
-                    break;
+                        break;
 
-                case "unmark":
-                    int taskIndexUnmark = Integer.parseInt(taskDescription) - 1;
-                    if (taskIndexUnmark < taskCounter && taskIndexUnmark >= 0) {
-                        tasks[taskIndexUnmark].unmark();
+                    case "mark":
+                        int taskIndexMark = Integer.parseInt(taskDescription) - 1;
+                        if (taskIndexMark < taskCounter && taskIndexMark >= 0) {
+                            tasks[taskIndexMark].mark();
+                            System.out.println("------------------------------------------");
+                            System.out.println("Yay! I've marked this task as done:");
+                            System.out.println("[" + tasks[taskIndexMark].getStatusIcon() + "] " + tasks[taskIndexMark].getDescription());
+                            System.out.println("------------------------------------------");
+                        } else {
+                            System.out.println("------------------------------------------");
+                            System.out.println("Invalid index :(");
+                            System.out.println("------------------------------------------");
+                        }
+                        break;
+
+                    case "unmark":
+                        int taskIndexUnmark = Integer.parseInt(taskDescription) - 1;
+                        if (taskIndexUnmark < taskCounter && taskIndexUnmark >= 0) {
+                            tasks[taskIndexUnmark].unmark();
+                            System.out.println("------------------------------------------");
+                            System.out.println("Alright, I've marked this task as not done yet:");
+                            System.out.println("[" + tasks[taskIndexUnmark].getStatusIcon() + "] " + tasks[taskIndexUnmark].getDescription());
+                            System.out.println("------------------------------------------");
+                        } else {
+                            System.out.println("------------------------------------------");
+                            System.out.println("Invalid index :(");
+                            System.out.println("------------------------------------------");
+                        }
+                        break;
+
+                    case "todo":
+                        if (taskDescription.isEmpty()) {
+                            throw new BobException("Description of the todo cannot be empty :(");
+                        }
+                        tasks[taskCounter] = new Todo(taskDescription);
+                        taskCounter++;
                         System.out.println("------------------------------------------");
-                        System.out.println("Ok, I've marked this task as not done yet:");
-                        System.out.println("[" + tasks[taskIndexUnmark].getStatusIcon() + "] " + tasks[taskIndexUnmark].getDescription());
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(tasks[taskCounter - 1].toString());
+                        System.out.println("Now you have " + taskCounter + " task(s) in the list.");
                         System.out.println("------------------------------------------");
-                    } else {
-                        System.out.println("Invalid index..");
-                    }
-                    break;
+                        break;
 
-                case "todo":
-                    tasks[taskCounter] = new Todo(taskDescription);
-                    taskCounter++;
-                    System.out.println("------------------------------------------");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCounter - 1].toString());
-                    System.out.println("Now you have " + taskCounter + " task(s) in the list.");
-                    System.out.println("------------------------------------------");
-                    break;
+                    case "deadline":
+                        if (taskDescription.isEmpty()) {
+                            throw new BobException("Missing deadline description :(");
+                        }
+                        String[] dlParts = taskDescription.split(" /by ");
+                        if (dlParts.length < 2) {
+                            throw new BobException("Missing details :(\nPlease use this format: 'deadline [description] /by [deadline]' :(");
+                        }
+                        tasks[taskCounter] = new Deadline(dlParts[0], dlParts[1]);
+                        taskCounter++;
+                        System.out.println("------------------------------------------");
+                        System.out.println("Ok! I've added this task:");
+                        System.out.println(tasks[taskCounter - 1].toString());
+                        System.out.println("Now, you have " + taskCounter + " task(s) in the list.");
+                        System.out.println("------------------------------------------");
+                        break;
 
-                case "deadline":
-                    String[] dlParts = taskDescription.split(" /by ");
-                    tasks[taskCounter] = new Deadline(dlParts[0], dlParts[1]);
-                    taskCounter++;
-                    System.out.println("------------------------------------------");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCounter - 1].toString());
-                    System.out.println("Now you have " + taskCounter + " task(s) in the list.");
-                    System.out.println("------------------------------------------");
-                    break;
+                    case "event":
+                        if (taskDescription.isEmpty()) {
+                            throw new BobException("Description of the event is missing :(");
+                        } else if (!taskDescription.contains(" /from ") || !taskDescription.contains(" /to ")) {
+                            throw new BobException("Missing details :(\nPlease use this format: event [description] /from [start] /to [end]");
+                        }
+                        String[] eventParts = taskDescription.split(" /from | /to ");
+                        tasks[taskCounter] = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                        taskCounter++;
+                        System.out.println("------------------------------------------");
+                        System.out.println("Ok! I've added this task:");
+                        System.out.println(tasks[taskCounter - 1].toString());
+                        System.out.println("Now you have " + taskCounter + " task(s) in the list.");
+                        System.out.println("------------------------------------------");
+                        break;
 
-                case "event":
-                    String[] eventParts = taskDescription.split(" /from | /to ");
-                    tasks[taskCounter] = new Event(eventParts[0], eventParts[1], eventParts[2]);
-                    taskCounter++;
-                    System.out.println("------------------------------------------");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCounter - 1].toString());
-                    System.out.println("Now you have " + taskCounter + " task(s) in the list.");
-                    System.out.println("------------------------------------------");
-                    break;
+                    case "bye":
+                        // exit
+                        System.out.println("------------------------------------------");
+                        System.out.println("Bye, see you again :)");
+                        System.out.println("------------------------------------------");
+                        scanner.close();
+                        return;
 
-                case "bye":
-                    // exit
-                    System.out.println("------------------------------------------");
-                    System.out.println("Bye, see you again :)");
-                    System.out.println("------------------------------------------");
-                    scanner.close();
-                    return;
-
-                default:
-                    // add task to tasks array
-                    tasks[taskCounter] = new Task(input);
-                    taskCounter++;
-
-                    System.out.println("------------------------------------------");
-                    System.out.println("added: " + input);
-                    System.out.println("------------------------------------------");
-                    break;
+                    default:
+                        throw new BobException("Bob does not understand that command, sorry :(");
+                }
+            } catch (BobException e) {
+                System.out.println("------------------------------------------");
+                System.out.println(e.getMessage());
+                System.out.println("------------------------------------------");
             }
         }
     }
