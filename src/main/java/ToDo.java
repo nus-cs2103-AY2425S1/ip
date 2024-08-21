e6dfa0ac0c029dee90005a320a2e63000d978aec
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-08-21 11:54:43 +0800

    Add ToDo class

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ToDo {
+public class ToDo extends Task {
+    public ToDo(String description) {
+        super(description, false);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
