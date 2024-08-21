d6940f636605341813682847a1aa5f572d80974e
Christopher Tan <phertan02@gmail.com>
2024-08-21 20:30:28 +0800

    Change array to ArrayList, add javadoc to some methods

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,35 +1,71 @@
+import java.util.ArrayList;
+
 public class TaskList {
 
-    private Task[] tasks;
-    private int head;
+    private final ArrayList<Task> tasks;
     public TaskList() {
-        // Assume no more than 100 tasks
-        this.tasks = new Task[100];
-        this.head = 0;
+        this.tasks = new ArrayList<>();
     }
 
-    public void add(String task) {
-        this.tasks[this.head] = new Task(task);
-        this.head++;
+    /**
+     * Add a task to the end of the list
+     * @param task Task object to be added
+     */
+    public void add(Task task) {
+        this.tasks.add(task);
     }
 
+    /**
+     * Mark a task as done
+     * @param taskId Index of the task to be marked
+     */
     public void mark(int taskId) {
-        tasks[taskId - 1].markDone();
+        tasks.get(taskId - 1).markDone();
     }
 
+    /**
+     * Mark a task as not done
+     * @param taskId Index of the task to be unmarked
+     */
     public void unmark(int taskId) {
-        tasks[taskId - 1].markNotDone();
+        tasks.get(taskId - 1).markNotDone();
     }
 
+    /**
+     *
+     * @param taskId
+     * @return
+     */
     public String getTaskStatus(int taskId) {
-        return tasks[taskId - 1].toString();
+        return tasks.get(taskId - 1).toString();
+    }
+
+    /**
+     * Get the status of the last added task
+     * @return String representation of the task
+     */
+    public String getLatestTask() {
+        if (tasks.isEmpty()) {
+            return "";
+        }
+        return tasks.get(tasks.size() - 1).toString();
+    }
+
+
+    /**
+     * Get the total number of tasks
+     * @return Count of all marked and unmarked tasks
+     */
+    public int getTaskCount() {
+        return tasks.size();
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int taskId = 0; taskId < this.head; taskId++) {
-            sb.append(String.format("%d: %s\n", taskId + 1, tasks[taskId]));
+        int nTasks = getTaskCount();
+        for (int index = 0; index < nTasks; index++) {
+            sb.append(String.format("%d: %s\n", index + 1, tasks.get(index)));
         }
         return "Here are the tasks in your list:\n" + sb.toString();
     }
