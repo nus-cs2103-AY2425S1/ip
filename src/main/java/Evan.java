3608853d110f5da73f2d4ee0947dbb53b2e4fc4b
zackjh <zacharykangjh@gmail.com>
2024-08-21 22:15:36 +0800

    Rename chatbot to Evan

---
+++ src/main/java/Evan.java
@@ -0,0 +1,13 @@
+public class Evan {
+    public static void main(String[] args) {
+        String divider = "_".repeat(50);
+
+        System.out.println(divider);
+        System.out.println("Hello! I'm Evan!");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
+    }
+
+}
