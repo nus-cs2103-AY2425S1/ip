edb91cb9ae1df34801e278742dbe72caa8f63f26
ethan-goh <ethan.goh.ye@gmail.com>
2024-08-22 01:08:47 +0800

    completed level 0

---
+++ src/main/java/Genesis.java
@@ -0,0 +1,13 @@
+public class Genesis {
+    public static void main(String[] args) {
+
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("Hello! I'm Genesis!\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!");
+    }
+}
