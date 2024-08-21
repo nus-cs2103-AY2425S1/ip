94ea2079cb167fb33083e28e78134c3365c152da
yhanyi <yeohhanyi0916@gmail.com>
2024-08-21 22:52:42 +0800

    A-Gradle

---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,57 @@
+package parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import exceptions.DukeException;
+
+
+
+class ParserTest {
+
+    private static final ByteArrayOutputStream outContent =
+            new ByteArrayOutputStream();
+    private static final ByteArrayOutputStream errContent =
+            new ByteArrayOutputStream();
+    private static final PrintStream originalOut = System.out;
+    private static final PrintStream originalErr = System.err;
+
+    @BeforeAll
+    public static void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterAll
+    public static void resetStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void testInvalidCommand1() {
+        Parser p = new Parser(new Scanner(new ByteArrayInputStream("go to school".getBytes())));
+        outContent.reset();
+        p.handleUserInput();
+        assertEquals("Invalid command provided.", outContent.toString().trim());
+    }
+
+    @Test
+    void testExit() {
+        Parser p = new Parser(new Scanner(new ByteArrayInputStream("bye".getBytes())));
+        outContent.reset();
+        p.handleUserInput();
+//        assertEquals("Bye! Hope to see you again soon!", outContent.toString().trim());
+        assertEquals("", outContent.toString().trim());
+    }
+
+}
