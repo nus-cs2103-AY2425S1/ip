f41525e35a7141410c7a0fbd8b24e1dcde7614d4
Martin Ng <martinng2002@gmail.com>
2024-08-21 15:27:01 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/brainRot.java
@@ -0,0 +1,13 @@
+public class brainRot {
+    public static void main(String[] args) {
+
+        String message = "____________________________________________________________ \n"
+        + "Hello! I'm fanumTaxRizzlerOhioSigmaLooksmaxxer\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________ \n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________";
+
+        System.out.println(message);
+    }
+}
