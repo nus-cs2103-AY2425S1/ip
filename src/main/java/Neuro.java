6c22bde449895255d0ac16ce89e486908d1d8b89
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-08-21 17:17:13 +0800

    Add functionality for different task types

--- src/main/java/Neuro.java
+++ src/main/java/Neuro.java
@@ -1,7 +1,59 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Neuro {
+    private static Task getTask(String input) {
+        // String split inspired by https://www.w3schools.com/java/ref_string_split.asp
+        // Array copyOfRange inspired by https://www.geeksforgeeks.org/java-util-arrays-copyofrange-java/
+        String[] inputComponents = input.split("[\s]");
+        String taskType = inputComponents[0];
+        Task task = null;
+
+        if (taskType.equals("todo")) {
+            String description = String.join(" ",
+                    Arrays.copyOfRange(inputComponents, 1, inputComponents.length));
+            task = new Todo(description);
+        } else if (taskType.equals("deadline")) {
+            int byIndex = -1;
+            for (int i = 1; i < inputComponents.length; i++) {
+                if (inputComponents[i].equals("/by")) {
+                    byIndex = i;
+                    break;
+                }
+            }
+            String description = String.join(" ",
+                    Arrays.copyOfRange(inputComponents, 1, byIndex));
+            String deadline = String.join(" ",
+                    Arrays.copyOfRange(inputComponents, byIndex + 1, inputComponents.length));
+
+            task = new Deadline(description, deadline);
+        } else if (taskType.equals("event")) {
+            int fromIndex = -1;
+            int toIndex = -1;
+
+            for (int i = 1; i < inputComponents.length; i++) {
+                if (inputComponents[i].equals("/from")) {
+                    fromIndex = i;
+                } else if (inputComponents[i].equals("/to")) {
+                    toIndex = i;
+                    break;
+                }
+            }
+
+            String description = String.join(" ",
+                    Arrays.copyOfRange(inputComponents, 1, fromIndex));
+            String from = String.join(" ",
+                    Arrays.copyOfRange(inputComponents, fromIndex + 1, toIndex));
+            String to = String.join(" ",
+                    Arrays.copyOfRange(inputComponents, toIndex + 1, inputComponents.length));
+
+            task = new Event(description, from, to);
+        }
+
+        return task;
+    }
+
     public static void main(String[] args) {
         ArrayList<Task> taskList = new ArrayList<>();
 
@@ -23,6 +75,7 @@ public class Neuro {
                 }
                 System.out.println("    ___________________________________________________");
             } else if (input.startsWith("mark")) {
+                // String split inspired by https://www.w3schools.com/java/ref_string_split.asp
                 String[] inputComponents = input.split("[\s]");
                 int taskIndex = Integer.valueOf(inputComponents[1]);
                 Task task = taskList.get(taskIndex - 1);
@@ -33,6 +86,7 @@ public class Neuro {
                 System.out.println("    " + task);
                 System.out.println("    ___________________________________________________");
             } else if (input.startsWith("unmark")) {
+                // String split inspired by https://www.w3schools.com/java/ref_string_split.asp
                 String[] inputComponents = input.split("[\s]");
                 int taskIndex = Integer.valueOf(inputComponents[1]);
                 Task task = taskList.get(taskIndex - 1);
@@ -43,9 +97,13 @@ public class Neuro {
                 System.out.println("    " + task);
                 System.out.println("    ___________________________________________________");
             } else {
-                taskList.add(new Task(input));
+                Task task = getTask(input);
+                taskList.add(task);
+                
                 System.out.println("    ___________________________________________________");
-                System.out.println("    added: " + input);
+                System.out.println("    Ok, I've added this task:");
+                System.out.println("        " + task);
+                System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
                 System.out.println("    ___________________________________________________");
             }
         }
