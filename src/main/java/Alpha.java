e70b136fd4f23ad88a4fa943e95943212c77f1de
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2024-08-21 23:02:07 +0800

    Add Scanner to take in user input

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Alpha {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String Response = "____________________________________________________________\n"
         + "Hello! I'm Alpha \n"
         + "What can I do for you? \n"
