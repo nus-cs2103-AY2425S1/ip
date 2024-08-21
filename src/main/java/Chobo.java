1754150444298c2bdbfb5ba5138c2ef7c0845366
tohjh <h1710141@nushigh.edu.sg>
2024-08-21 16:34:04 +0800

    add Level 0

---
+++ src/main/java/Chobo.java
@@ -0,0 +1,13 @@
+public class Chobo {
+    public static void main(String[] args) {
+        String line = "----------------------------------------";
+        System.out.println("Hello! I'm Chobo\nWhat can i do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
+
+
+
+
