f34b671577675f8b92d484fbb458e2c9eb3f7b86
LAPTOP-37SSO5I3\Han Bin <ngiowhb@gmail.com>
2024-08-21 21:49:55 +0800

    Level-1

--- src/main/java/Chatterbox.java
+++ src/main/java/Chatterbox.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chatterbox {
     public static void main(String[] args) {
         String welcomeMessage = """
@@ -5,9 +7,33 @@ public class Chatterbox {
                  Hello! I'm Chatterbox
                  What can I do for you?
                 ____________________________________________________________
-                 Bye. Hope to see you again soon!
+                """;
+        printMessage(welcomeMessage);
+        boolean done = false;
+        while (!done) {
+            String command = echo();
+            if (command.equals("bye")) {
+                String byeMessage = """
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
                 ____________________________________________________________
                 """;
-        System.out.println(welcomeMessage);
+                printMessage(byeMessage);
+                done = true;
+            } else {
+                String echoMessage = "____________________________________________________________\n"
+                        + command
+                        +"\n____________________________________________________________\n";
+                printMessage(echoMessage);
+            }
+        }
+    }
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String echo() {
+        Scanner scan  = new Scanner(System.in);
+        return scan.nextLine();
     }
 }
