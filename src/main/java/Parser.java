e914e9af30a61898e699cbd2dd2cf71ca53996e2
Christopher Tan <phertan02@gmail.com>
2024-08-22 00:21:20 +0800

    Fix splitter

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,8 +1,12 @@
 public class Parser {
     public static Query parse(String raw) {
-        String[] split = raw.split(" ", 1);
-        String command = split[0];
-        String details = split[1];
-        return new Query(command, details);
+        String[] split = raw.split(" ", 2);
+        String command = split[0].strip();
+        if (split.length > 1) {
+            String details = split[1];
+            return new Query(command, details);
+        }
+        // for standalone commands (list, etc)
+        return new Query(command);
     }
 }
