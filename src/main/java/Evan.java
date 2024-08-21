04c39a8c839686ce686e9ee720ba0a93ebb18d50
zackjh <zacharykangjh@gmail.com>
2024-08-21 22:30:26 +0800

    Update Evan so that it echoes the user's input

--- src/main/java/Evan.java
+++ src/main/java/Evan.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Evan {
     public static void main(String[] args) {
         String divider = "_".repeat(50);
+        Scanner scanner = new Scanner(System.in);
+        String input;
 
         System.out.println(divider);
         System.out.println("Hello! I'm Evan!");
         System.out.println("What can I do for you?");
-        System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println(divider);
+
+            // Check for exit condition
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                break;
+            }
+
+            // Echo the user's input
+            System.out.println(input);
+            System.out.println(divider);
+        }
+
+        // Close the scanner before exiting
+        scanner.close();
     }
 
 }
