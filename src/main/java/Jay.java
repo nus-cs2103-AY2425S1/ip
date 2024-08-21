7d3dde8717b459c8c8529156459cd78485c15071
tckeong <teo238540@gmail.com>
2024-08-21 20:56:37 +0800

    Level-0

---
+++ src/main/java/Jay.java
@@ -0,0 +1,12 @@
+public class Jay {
+    public static void main(String[] args) {
+        String greet =
+                "____________________________________________________________\n" +
+                " Hello! I'm Jay\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(greet);
+    }
+}
