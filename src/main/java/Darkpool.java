204335f96d18274b555c6fe93c6c334ff1dd2855
arnav-goel10 <e1130141@u.nus.edu>
2024-08-21 19:18:51 +0800

    Changed Duke to Darkpool

---
+++ src/main/java/Darkpool.java
@@ -0,0 +1,9 @@
+public class Darkpool {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                "it’s darkpool. what twisted reason dragged me into your misery?\n" +
+                "____________________________________________________________\n" +
+                "contact me again and you'll regret it.\n" +
+                "____________________________________________________________\n");
+    }
+}
