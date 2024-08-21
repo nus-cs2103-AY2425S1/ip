851b3db703e8ac2ff6d8a75afaf50a3c77da6131
Elliot Tang <elliot.tang.379@gmail.com>
2024-08-21 16:53:37 +0800

    Add echo functionality

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Orion {
 
     public static final String LOGO = "             .__               \n"
@@ -9,17 +10,47 @@ public class Orion {
 
     public static final String BAR = "______________________________________________\n";
 
-    public static void main(String[] args) {
+    public static final String INDENT = "    ";
+
+    public static void printBar() {
         System.out.println(Orion.BAR);
+    }
+
+    public static void greet() {
+        Orion.printBar();
         System.out.println(Orion.LOGO);
+        Orion.printBar();
 
-        System.out.println(Orion.BAR);
-        System.out.println("Hello from Orion!");
-        System.out.println("What do you want to talk about today?");
+        System.out.println(Orion.INDENT + "Hello from Orion!");
+        System.out.println(Orion.INDENT + "What do you want to talk about today?");
+        Orion.printBar();
+    }
 
-        System.out.println(Orion.BAR);
-        System.out.println("Bye! Hope to see you again soon!");
+    public static void echo(String command) {
+        System.out.println(Orion.INDENT + command);
+        Orion.printBar();
+    }
 
-        System.out.println(Orion.BAR);
+    public static void sayGoodbye() {
+        System.out.println(Orion.INDENT + "Bye! Hope to see you again soon!");
+        Orion.printBar();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Orion.greet();
+
+        while (true) {
+            String command = sc.nextLine();
+            Orion.printBar();
+            if (command.equals("bye")) {
+                sc.close();
+                break;
+            } else {
+                Orion.echo(command);
+            }
+        }
+
+        Orion.sayGoodbye();
     }
 }
