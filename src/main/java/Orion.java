778e63d07af9e4be2b0032384cd64c414fa9274c
pradyuprasad <pradyu.sp@gmail.com>
2024-08-22 07:51:15 +0800

    Completed Level 1. Echo

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Orion {
-    static String  horizontalLine =  "────────────────────────────────────────";
+    static Scanner scanner = new Scanner(System.in);
+    static String horizontalLine = "────────────────────────────────────────";
+
     public static void main(String[] args) {
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Orion");
         System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("\n");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;  // Exit the loop if the user types "bye"
+            }
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+        }
+
         System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
