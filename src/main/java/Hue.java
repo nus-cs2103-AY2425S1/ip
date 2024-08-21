d9683a653d7874995b85368f0678dc26f667f379
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-21 15:24:18 +0800

    Finish Level 1-2

--- src/main/java/Hue.java
+++ src/main/java/Hue.java
@@ -9,6 +9,9 @@ public class Hue {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String name = "Hue";
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("____________________________________________________________" );
         System.out.println("Hello! I'm [" + name + "]");
         System.out.println("What can I do for you?");
@@ -19,12 +22,28 @@ public class Hue {
 
         do {
             input = scanner.nextLine();
+
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            if (input.equals("bye")) {
+                break;
+            }
+
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else if (!input.equalsIgnoreCase("bye")) {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
             System.out.println("____________________________________________________________");
         } while (!input.equalsIgnoreCase("bye"));
 
             System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("____________________________________________________________");
+            scanner.close();
+
 
     }
 
