1c682dcfdbc9b7ee36e401402c1f306478cfd65c
Jack Bean <144975629+bryanjhc@users.noreply.github.com>
2024-08-21 21:20:32 +0800

    added echo

--- AI.md
+++ AI.md
@@ -1,2 +1,7 @@
+# Intro
 Hello there! I am using GitHub CoPilot for my individual project.
 Will update this AI.md file soon!
+
+### Update (21/8/2024):
+GitHub copilot helped me to code the while loop including the logic for the echo feature.
+It was spot on except for the scanner which I had to edit.
--- src/main/java/JackBean.java
+++ src/main/java/JackBean.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class JackBean {
     public static String horizontalLine = "____________________________________________________________";
     public static String greeting = "Hello homie! I'm JackBean, a chatbot designed to help you with your daily tasks!\nHow may I help you today my homie?";
@@ -6,6 +8,22 @@ public class JackBean {
     public static void main(String[] args) {
         System.out.println(horizontalLine);
         System.out.println(greeting);
+        System.out.println(horizontalLine);
+
+        Scanner userInput = new Scanner(System.in);
+
+        // GitHub Copilot helped to code this entire while loop (except personalisation)
+        // and I have no idea how it knew what to do HAHAHA
+        while (true) {
+            String input = userInput.nextLine();
+            if (input.equalsIgnoreCase("bye")) { // I added equalsIgnoreCase() by myself
+                break;
+            }
+            System.out.println(horizontalLine);
+            System.out.println("Wassup homie, you said this: " + input);
+            System.out.println(horizontalLine);
+        }
+
         System.out.println(horizontalLine);
         System.out.println(exitMessage);
         System.out.println(horizontalLine);
