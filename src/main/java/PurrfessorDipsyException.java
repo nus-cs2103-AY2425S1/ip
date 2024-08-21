8318387061a93bb7df28b21451448b3a3238aac6
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-08-21 18:30:14 +0800

    Add invalid delete error catching into Exception and update naming of invalid mark command

--- src/main/java/PurrfessorDipsyException.java
+++ src/main/java/PurrfessorDipsyException.java
@@ -2,11 +2,13 @@ public class PurrfessorDipsyException extends Exception {
     private final ErrorType errorType;
     public enum ErrorType {
         UNKNOWN_COMMAND,
-        INVALID_MARK,
         INVALID_TODO,
         INVALID_DEADLINE,
         INVALID_EVENT,
-        INVALID_MARK_INDEX
+        INVALID_MARK_COMMAND,
+        INVALID_MARK_INDEX,
+        INVALID_DELETE_COMMAND,
+        INVALID_DELETE_INDEX
     }
 
     public PurrfessorDipsyException(ErrorType errorType) {
@@ -22,10 +24,14 @@ public class PurrfessorDipsyException extends Exception {
                     "'deadline' command requires a 'by' date.\nUsage: deadline <description> /by <day/date/time>";
             case INVALID_EVENT ->
                     "'event' command requires a description, a '/from' time, and a '/to' time.\nUsage: event <description> /from <day/date/time> /to <day/date/time>";
-            case INVALID_MARK ->
+            case INVALID_MARK_COMMAND ->
                     "'mark' or 'unmark' command requires a valid index.\nUsage: mark <index> or unmark <index>";
             case INVALID_MARK_INDEX ->
-                    "index given in 'mark <index>' lies outside of the table's valid range.";
+                    "index given in 'mark/unmark <index>' lies outside of the table's valid range.";
+            case INVALID_DELETE_COMMAND ->
+                    "'delete' command requires a valid index.\nUsage: delete <index>";
+            case INVALID_DELETE_INDEX ->
+                    "index given in 'delete <index>' lies outside of the table's valid range.";
             default -> "Unrecognized command.\n\n" + getUsage();
         };
     }
