ebc28788ac1d6f92b94f47680e4cd7b052c5a7f2
Sean Lim <seanlimdev856@gmail.com>
2024-08-21 17:26:31 +0800

    Add support for echo

--- src/main/java/Deez.java
+++ src/main/java/Deez.java
@@ -1,5 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Deez {
-    public static void main(String[] args) {
+    static void say(String msg) {
+        System.out.println("> " + msg);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String CMD_bye = "bye";
         String seperator = "____________________________________________________________";
         String logo = """
                     ____  _________________
@@ -11,10 +19,18 @@ public class Deez {
                 """;
         System.out.println(logo);
         System.out.println(seperator);
-        System.out.println("Hello! I'm Deez!");
-        System.out.println("What can I do you for?");
+        Deez.say("Hello! I'm Deez!");
+        Deez.say("What can I do you for?");
+        while (1 == 1) {
+            String str = br.readLine();
+            if (str.equals(CMD_bye)) {
+                break;
+            }
+            Deez.say(str);
+        }
         System.out.println(seperator);
-        System.out.println("Bye. Hope to see you soon!");
+        Deez.say("Bye. Hope to see you soon!");
         System.out.println(seperator);
     }
+
 }
