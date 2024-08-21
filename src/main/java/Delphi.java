9e5e6b38530367ea7013fec313d2ae12ee93c7e4
naythee169 <jordan.mitchell.chan@gmail.com>
2024-08-22 00:53:22 +0800

    Add mark as done function

--- src/main/java/Delphi.java
+++ src/main/java/Delphi.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class Delphi {
+
+    public static boolean checkStringPrefix(String original, int index, String comparison) {
+        // Ensure the index is within the bounds of the original string
+        if (index > original.length()) {
+            index = original.length();
+        }
+
+        // Get the substring from the original string up to the specified index
+        String prefix = original.substring(0, index);
+
+        // Compare the prefix with the comparison string
+        return prefix.equals(comparison);
+    }
     public static void main(String[] args) {
         TaskList t = new TaskList();
         Scanner scanner = new Scanner(System.in);
@@ -11,6 +24,15 @@ public class Delphi {
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) break;
+            if (checkStringPrefix(input, 4, "mark")) {
+                t.markTaskAsDone(Integer.parseInt(String.valueOf(input.charAt(5))));
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      [X] " + t.getTask(Integer.parseInt(String.valueOf(input.charAt(5)))));
+            } else if (checkStringPrefix(input, 6, "unmark")) {
+                t.markTaskAsUndone(Integer.parseInt(String.valueOf(input.charAt(7))));
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println("      [ ] " + t.getTask(Integer.parseInt(String.valueOf(input.charAt(7)))));
+            }
             else {
                 t.addTask(input);
             }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,20 +1,54 @@
 import java.util.*;
 public class TaskList {
-    List<String> tasks;
+    private List<Task> tasks;
+
+    private class Task {
+        protected String name;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.name = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+        public void complete() {
+            this.isDone = true;
+        }
+        public void uncomplete() {
+            this.isDone = false;
+        }
+        @Override
+        public String toString() {
+            return this.name;
+        }
+    }
 
     public TaskList() {
         tasks = new ArrayList<>();
     }
     public void addTask(String task) {
         if (!task.equals("list")) {
-            tasks.add(task);
+            tasks.add(new Task(task));
             System.out.println("    added: " + task);
         }
         else printTasks();
     }
+    public void markTaskAsDone(int i) {
+        if (i <= tasks.size()) tasks.get(i-1).complete();
+    }
+    public void markTaskAsUndone(int i) {
+        if (i <= tasks.size()) tasks.get(i-1).uncomplete();
+    }
+    public String getTask(int i) {
+        if (i <= tasks.size()) return tasks.get(i-1).toString();
+        else return "";
+    }
     public void printTasks() {
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+            System.out.println("    " + (i + 1) + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).toString());
         }
     }
 }
