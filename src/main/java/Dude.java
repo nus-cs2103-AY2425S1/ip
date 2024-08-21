f27a48df4427f89e4b4af589992cd051e61b7bc5
fusin <fusin02@gmail.com>
2024-08-22 02:24:28 +0800

    Renamed and implemented greet and exit

---
+++ src/main/java/Dude.java
@@ -0,0 +1,11 @@
+public class Dude {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Dude!\nWhat can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println(line);
+    }
+}
