4b136262761fde18547849b40ed8910dedf8416e
ckaayy <e1122115@u.nus.edu>
2024-08-21 17:01:54 +0800

    Add, List

--- src/main/java/YapperBot.java
+++ src/main/java/YapperBot.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class YapperBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] inputHistory = new String[100];
+        int numTask = 0;
 
         System.out.println("________________________________");
         System.out.println("Hello! I'm YapperBot");
@@ -14,11 +16,19 @@ public class YapperBot {
 
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("________________________________");
+                for (int i = 0; i < numTask; i++) {
+                    System.out.println((i + 1) + ". " + inputHistory[i]);
+                }
+                System.out.println("________________________________");
+            } else {
+                inputHistory[numTask] = userInput;
+                numTask++;
+                System.out.println("________________________________");
+                System.out.println("added: " + userInput);
+                System.out.println("________________________________");
             }
-
-            System.out.println("________________________________");
-            System.out.println(userInput);
-            System.out.println("________________________________");
         }
 
         System.out.println("________________________________");
