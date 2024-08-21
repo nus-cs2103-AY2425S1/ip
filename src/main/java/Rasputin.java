d756957aef97c65bc8e9168da47bb37a3dc0cf06
harithh07 <harith.hakim2002@gmail.com>
2024-08-21 21:34:39 +0800

    Added echo function

--- src/main/java/Rasputin.java
+++ src/main/java/Rasputin.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Rasputin {
+
+    private static void printText(String text) {
+        System.out.println(lineBreak);
+        System.out.println(text);
+        System.out.println(lineBreak + "\n");
+    }
+
+    private static String lineBreak = "____________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,8 +18,21 @@ public class Rasputin {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         String name = "Rasputin";
-        System.out.println("Hello, I'm " + name + "!\nWhat can I do for you?");
-        System.out.print("\n");
-        System.out.println("Bye. Hope to see you soon!");
+
+
+        Scanner scanner = new Scanner(System.in);
+        printText("Hello, I'm " + name + "!\nWhat can I do for you?");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            printText(input);
+        }
+
+        printText("Bye. Hope to see you soon!");
     }
+
+
 }
