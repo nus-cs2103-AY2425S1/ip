de34f86d35ea3bef7c8164ada1fbf0b2963b6773
Hans Delano <hanscau@hotmail.com>
2024-08-21 13:51:37 +0800

    Add format and echo

--- src/main/java/Snah.java
+++ src/main/java/Snah.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Snah {
+
+    private final static String EXIT_INPUT = "bye";
+    private final static String CHAT_NAME = "Snah";
+    private final static String START_DIVIDER = "___________________________________________";
+    private final static String END_DIVIDER = "___________________________________________\n";
+
     public static void main(String[] args) {
-        String chatBotName = "Snah";
-        System.out.printf("====================================\n");
-        System.out.printf("Hello! I'm %s\n", chatBotName);
-        System.out.printf("What can I do for you?\n");
-        System.out.printf("====================================\n");
-        System.out.printf("Bye. Hope to see you again soon!\n");
+        greet();
+        chatLoop();
+    }
+
+    public static void chatbotPrint(String message) {
+        System.out.printf("\t%s\n", message);
+    }
+
+    public static void greet() {
+        chatbotPrint(START_DIVIDER);
+        chatbotPrint(String.format("Hello! I'm %s", CHAT_NAME));
+        chatbotPrint("What can I do for you?");
+        chatbotPrint(END_DIVIDER);
+    }
+
+    public static void chatLoop() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while (true) {
+            userInput = scanner.nextLine();
+
+            if (userInput.equals(EXIT_INPUT)) {
+                chatbotPrint(START_DIVIDER);
+                chatbotPrint("Goodbye! See you sooooonnn!");
+                chatbotPrint(END_DIVIDER);
+                break;
+            }
+
+            chatbotPrint(START_DIVIDER);
+            chatbotPrint(userInput);
+            chatbotPrint(END_DIVIDER);
+        }
+        scanner.close();
     }
 }
