c807941c0a7d6d4824fc118a50e07e2b799ce23c
IanCheah <iancheah24@gmail.com>
2024-08-22 01:06:30 +0800

    handled invalid input by throwing exceptions

--- src/main/java/Hyperion.java
+++ src/main/java/Hyperion.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Hyperion {
@@ -13,20 +14,23 @@ public class Hyperion {
         System.out.println(solidLine + greet1 + greet2 + solidLine);
         // obtains user input and exits only when user types bye
         String input = "";
+        Scanner scannerObj = new Scanner(System.in);
         do {
-            Scanner scannerObj = new Scanner(System.in);
-            input = scannerObj.nextLine();
-            System.out.print(solidLine);
-            if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list: ");
-                for (int i = 0; i < count; i++) {
-                    String index = String.format("%d. ", i + 1);
-                    System.out.println(index + allUserInputs[i].toString());
-                }
+            try {
+                input = scannerObj.nextLine();
                 System.out.print(solidLine);
-            } else if (input.startsWith("mark") || input.startsWith("unmark")) {
-                try {
+                if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < count; i++) {
+                        String index = String.format("%d. ", i + 1);
+                        System.out.println(index + allUserInputs[i].toString());
+                    }
+                    System.out.print(solidLine);
+                } else if (input.startsWith("mark") || input.startsWith("unmark")) {
                     String[] stringsInInput = input.split(" ");
+                    if (stringsInInput.length == 1) {
+                        throw new EmptyDescriptionException(stringsInInput[0]);
+                    }
                     String command = " ";
                     int index = -1;
                     for (int i = 0; i < stringsInInput.length; i++) {
@@ -54,52 +58,74 @@ public class Hyperion {
                         }
                     }
                     System.out.print(solidLine);
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid Input");
-                    System.out.print(solidLine);
-                }
-            } else {
-                String str1 = "Got it. I've added this task:\n ";
-                String str2 = String.format("Now you have %d tasks in the list", count + 1);
-                if (input.startsWith("todo")) {
-                    String task = input.substring(4).trim();
-                    ToDos current = new ToDos(task);
-                    allUserInputs[count] = current;
-                    count++;
-                    System.out.println(str1 + current.toString() + "\n" + str2);
-                } else if (input.startsWith("deadline")) {
-                    String removeHead = input.substring(8);
-                    String[] inputArray = removeHead.split("/by");
-                    if (inputArray.length == 2) {
-                        Deadlines current = new Deadlines(inputArray[0].trim(), inputArray[1].trim());
-                        allUserInputs[count] = current;
-                        count++;
-                        System.out.println(str1 + current.toString() + "\n" + str2);
-                    } else {
-                        System.out.println("Invalid Input");
-                    }
-                } else if (input.startsWith("event")) {
-                    String removeHead = input.substring(5);
-                    String[] firstSplit = removeHead.split("/from", 2);
-                    if (firstSplit.length == 2) {
-                        String[] secondSplit = firstSplit[1].split("/to", 2);
-                        if (secondSplit.length == 2) {
-                            String desc = firstSplit[0].trim();
-                            String start = secondSplit[0].trim();
-                            String end = secondSplit[1].trim();
-                            Events current = new Events(desc, start, end);
+                } else {
+                    String str1 = "Got it. I've added this task:\n ";
+                    String str2 = String.format("Now you have %d tasks in the list", count + 1);
+                    if (input.startsWith("todo")) {
+                        // should do split also
+                        String[] inputArray = input.split(" ", 2);
+                        if (inputArray.length == 1 && inputArray[0].equals("todo")) {
+                            throw new EmptyDescriptionException("todo");
+                        }
+                        if (inputArray.length == 2) {
+                            ToDos current = new ToDos(inputArray[1]);
                             allUserInputs[count] = current;
                             count++;
                             System.out.println(str1 + current.toString() + "\n" + str2);
+                            System.out.print(solidLine);
                         } else {
-                            System.out.println("Invalid Input");
+                            throw new InvalidSyntaxException("todo");
+                        }
+
+                    } else if (input.startsWith("deadline")) {
+                        String removeHead = input.substring(8);
+                        String[] inputArray = removeHead.split("/by");
+                        if (inputArray.length == 1 && inputArray[0].equals("deadline")) {
+                            throw new EmptyDescriptionException("deadline");
+                        }
+                        if (inputArray.length == 2) {
+                            Deadlines current = new Deadlines(inputArray[0].trim(), inputArray[1].trim());
+                            allUserInputs[count] = current;
+                            count++;
+                            System.out.println(str1 + current.toString() + "\n" + str2);
+                            System.out.print(solidLine);
+                        } else {
+                            throw new InvalidSyntaxException("deadline");
+                        }
+                    } else if (input.startsWith("event")) {
+                        String removeHead = input.substring(5);
+                        String[] firstSplit = removeHead.split("/from", 2);
+                        if (firstSplit.length == 1 && firstSplit[0].trim().equals("event")) {
+                            throw new EmptyDescriptionException("event");
+                        }
+                        if (firstSplit.length == 2) {
+                            String[] secondSplit = firstSplit[1].split("/to", 2);
+                            if (secondSplit.length == 2) {
+                                String desc = firstSplit[0].trim();
+                                String start = secondSplit[0].trim();
+                                String end = secondSplit[1].trim();
+                                Events current = new Events(desc, start, end);
+                                allUserInputs[count] = current;
+                                count++;
+                                System.out.println(str1 + current.toString() + "\n" + str2);
+                            } else {
+                                throw new InvalidSyntaxException("event");
+                            }
+                            System.out.print(solidLine);
+                        } else {
+                            throw new InvalidSyntaxException("event");
                         }
                     } else {
-                        System.out.println("Invalid Input");
+                        if (!input.equals("bye")) {
+                            throw new CommandNotFoundException(input);
+                        }
                     }
-                } else {
-                    System.out.println("Invalid Input");
                 }
+            } catch (CommandNotFoundException | CommandFoundButInvalidException e) {
+                System.out.println(e.getMessage());
+                System.out.print(solidLine);
+            } catch (NumberFormatException e) {
+                System.out.println("You can only mark and unmark integers shown in the list");
                 System.out.print(solidLine);
             }
         } while (!input.equals("bye"));
