0bbdf65b8a56b32673f4140287d210d77c522c92
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-08-21 16:10:43 +0800

    Minor bug fix - change class name to Sunny

--- src/main/java/Sunny.java
+++ src/main/java/Sunny.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Sunny {
     public static void main(String[] args) {
         String logo = "  _____   _    _  _   _  _   _  __     __  \n" +
                 " / ____| | |  | || \\ | || \\ | | \\ \\   / /  \n" +
