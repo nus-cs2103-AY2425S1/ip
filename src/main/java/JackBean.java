6de5259b58a109c9d478d7d23125843f85fac69a
Jack Bean <144975629+bryanjhc@users.noreply.github.com>
2024-08-21 21:04:55 +0800

    declared and defined greeting and exitMessage strings for use in main()

--- src/main/java/JackBean.java
+++ src/main/java/JackBean.java
@@ -1,11 +1,13 @@
 public class JackBean {
     public static String horizontalLine = "____________________________________________________________";
+    public static String greeting = "Hello homie! I'm JackBean, a chatbot designed to help you with your daily tasks!\nHow may I help you today my homie?";
+    public static String exitMessage = "Bye homie! Come back if you need anything else. JackBean, signing off!";
+
     public static void main(String[] args) {
         System.out.println(horizontalLine);
-        System.out.println("Hello homie! I'm JackBean, a chatbot designed to help you with your daily tasks!");
-        System.out.println("How may I help you today my homie?");
+        System.out.println(greeting);
         System.out.println(horizontalLine);
-        System.out.println("Bye homie! Come back if you need anything else. JackBean, signing off!");
+        System.out.println(exitMessage);
         System.out.println(horizontalLine);
     }
 }
