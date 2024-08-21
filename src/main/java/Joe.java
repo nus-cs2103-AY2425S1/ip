de796fcd72cd45b02bedf1379133cf8d8b3b43a1
Ansel-Ch <achong2014@gmail.com>
2024-08-21 16:51:42 +0800

    Added mark method and renamed static variables

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Joe {
     private static final String line =
             "____________________________________________________________";
-    private static Task[] userInputs = new Task[100];
-    private static int inputs = 0;
+    private static Task[] userTasks = new Task[100];
+    private static int taskCount = 0;
     private static void bye() {
         System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
     }
@@ -51,9 +51,9 @@ public class Joe {
 
     public static void add(String s) {
         System.out.println(line);
-        if (inputs < 100) {
+        if (taskCount < 100) {
             Task newTask = new Task(s);
-            userInputs[inputs++] = newTask;
+            userTasks[taskCount++] = newTask;
             System.out.printf("added: %s\n", newTask);
         } else {
             System.out.println("Input has reached max capacity!");
@@ -63,10 +63,25 @@ public class Joe {
 
     public static void list() {
         System.out.println(line);
-        for (int i = 0; i < inputs; i++) {
-            Task currTask = userInputs[i];
-            char mark = currTask.isDone() ? 'X' : ' ';
-            System.out.printf("%d.[%c] %s\n", i+1, mark, currTask);
+        for (int i = 0; i < taskCount; i++) {
+            Task currTask = userTasks[i];
+            System.out.printf("%d. %s\n", i+1, currTask);
+        }
+        System.out.println(line);
+    }
+
+    public static void mark(int idx) {
+        System.out.println(line);
+        if (idx-1 < taskCount) {
+            Task toBeMarked = userTasks[idx-1];
+            if (!toBeMarked.isDone()) {
+                toBeMarked.setDone(true);
+                System.out.printf("Nice! I've marked this task as done:\n  %s\n", toBeMarked);
+            } else {
+                System.out.println("This task has already been marked.");
+            }
+        } else {
+            System.out.printf("There is no task %d\n", idx);
         }
         System.out.println(line);
     }
@@ -74,11 +89,25 @@ public class Joe {
     public static void main(String[] args) {
         System.out.printf("%s\nHello! I'm Joe\nWhat can I do for you?\n%s\n",
                 line, line);
+//        echo();
+
         Scanner reader = new Scanner(System.in);
-        String userIn = reader.nextLine();
+        String userIn = reader.nextLine().strip();
         while (!userIn.equalsIgnoreCase("bye")) {
             if (userIn.equalsIgnoreCase("list")) {
                 list();
+            } else if (userIn.startsWith("mark")) {
+                int idx = -1;
+                int n = userIn.length();
+                for (int i = 5; i < n; i++) {
+                    while (i < n && Character.isDigit(userIn.charAt(i))) {
+                        if (idx < 0) {
+                            idx = 0;
+                        }
+                        idx = idx * 10 + (userIn.charAt(i++) - '0');
+                    }
+                }
+                mark(idx);
             } else {
                 add(userIn);
             }
