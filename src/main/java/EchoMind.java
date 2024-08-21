ac4d5ca9cf1741f9b44879e88386c94ad2ebeebb
slye20 <samuellim2011@gmail.com>
2024-08-21 15:17:57 +0800

    Removed whitespace from before and after message

--- src/main/java/EchoMind.java
+++ src/main/java/EchoMind.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class EchoMind {
+//    private String[]  =
 
-    public static void sendMessage(String msg) {
+    public void sendMessage(String msg) {
         System.out.println("[EchoMind] " + msg);
     }
 
     public static void main(String[] args) {
-        sendMessage("Hello! I'm EchoMind!");
-        sendMessage("What can I do for you?");
+        EchoMind em = new EchoMind();
+        em.sendMessage("Hello! I'm EchoMind!");
+        em.sendMessage("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
         String message = "";
         while (true) {
-            message = scanner.nextLine();
+            message = scanner.nextLine().trim();
             if (message.equalsIgnoreCase("bye")) {
                 break;
             }
-            sendMessage(message);
+            em.sendMessage(message);
         }
-        sendMessage("Bye. Hope to see you again soon!");
+        em.sendMessage("Bye. Hope to see you again soon!");
     }
 }
