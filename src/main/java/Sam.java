6d38258280d6d3d11c7b4dd3192ea1bf0b19330c
Varun Shukla <vslpahm@gmail.com>
2024-08-21 23:13:09 +0800

    Finished level-1 increment

--- src/main/java/Sam.java
+++ src/main/java/Sam.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Sam {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(getHorizontalLine());
         System.out.println("Hello! I'm Sam\nWhat can I do for you?");
         System.out.println(getHorizontalLine());
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(getHorizontalLine());
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(getHorizontalLine());
+
+            if (input.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(getHorizontalLine());
+                break;
+            } else {
+                System.out.println("   "+input);
+                System.out.println(getHorizontalLine());
+            }
+        }
+
+        scanner.close();
     }
 
     private static String getHorizontalLine() {
