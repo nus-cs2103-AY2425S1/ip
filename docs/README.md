ed5a92e5c1b29bc425deb181f9eff0a4d69b2719
M-kew <e0958776@u.nus.edu>
2024-08-21 21:51:38 +0800

    Implemented echo feature for Nuffle chatbot.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Nuffle User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Nuffle.java
+++ src/main/java/Nuffle.java
@@ -1,10 +1,45 @@
-public class Duke {
+import java.util.Scanner;
+public class Nuffle {
+
+    private static void printLine() {
+        // This method will be used to print the border
+        System.out.println("╔═══════════════════════════════════════════════════════════╗");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // This will be starting point of the application
+
+        //Variable to store user inputs
+        String userInput;
+
+        // Create a scanner for user input
+        Scanner user_s = new Scanner(System.in);
+
+        // Greeting the users
+        printLine();
+        System.out.println("Nuffle > Good day! I'm Nuffle.");
+        System.out.println("Nuffle > What can I do for you today?");
+        printLine();
+
+        // Read the user input until the command "bye" is provided by the user
+        while(true) {
+            // Get the user input from the scanner
+            userInput = user_s.nextLine();
+
+            // Check if the user input provided is "bye", has to be lowercase
+            if (userInput.equals("bye")) {
+                // Program will exit
+                printLine();
+                System.out.println("Nuffle > Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            } else {
+                // Echo the user command
+                printLine();
+                System.out.println("Nuffle > " + userInput);
+                printLine();
+            }
+        }
+        // Once loop breaks, terminate/close the scanner
+        user_s.close();
     }
 }
