5d52c1fb30d7e229dd7ec802ed1d62d9dd5e0c49
yxshrk <yrk17122002@gmail.com>
2024-08-21 12:36:43 +0800

    Level 1. Echo

--- src/main/java/Molly.java
+++ src/main/java/Molly.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Molly {
     public static String name = "Molly";
 
@@ -6,18 +8,34 @@ public class Molly {
     }
 
     public static void greetUser() {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm " + Molly.name);
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
     }
 
     public static void sayBye() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void echoUser() {
+        Scanner botScanner = new Scanner(System.in);
+        System.out.println("____________________________________________________________");
+        String userInput = botScanner.nextLine();
+        while (!userInput.toLowerCase().equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+            userInput = botScanner.nextLine();
+        }
+        Molly.sayBye();
+
     }
 
 
     public static void main(String[] args) {
         Molly.greetUser();
-        Molly.sayBye();
+        Molly.echoUser();
     }
 }
