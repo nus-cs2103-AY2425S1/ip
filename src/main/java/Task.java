76180ded213c5c829fd6c390586c52660eea2bce
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-21 15:25:43 +0800

    Task.java: create factory method
    create a factory method that takes in a string and returns different types of tasks depending on the first word of the string

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,6 +6,21 @@ public class Task {
         done = false;
     }
 
+    public static Task of(String userInput) {
+        String[] splitUserInput = userInput.split(" ", 2);
+        String identifier = splitUserInput[0];
+
+        if (identifier.equals("todo")) {
+            return new ToDo(splitUserInput[1]);
+        } else if (identifier.equals("deadline")) {
+            String[] details = splitUserInput[1].split("/by ", 2);
+            return new Deadline(details[0], details[1]);
+        } else {
+            //task not recognised
+            return null;
+        }
+    }
+
     public void done() {
         done = true;
     }
