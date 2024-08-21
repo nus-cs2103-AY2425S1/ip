4caa385f0048ce884130a8635b3157811f57dd60
KennyLewi <kennylewi27@gmail.com>
2024-08-21 17:36:10 +0800

    renamed chatbot to peridot, and added greeting

---
+++ src/main/java/Peridot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Peridot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello I'm Peridot!");
+        System.out.println("What's up?");
+        String userResponse = scanner.nextLine();
+        System.out.println("Bye!");
+    }
+}
