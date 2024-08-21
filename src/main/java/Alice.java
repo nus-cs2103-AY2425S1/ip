1e2286a0da669f36030530d792cc7bd3d230d4bd
Leslie Yip <leslieyip02@gmail.com>
2024-08-21 13:12:16 +0800

    Add task tracking

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Alice {
     private static final String NAME = "Alice";
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    private List<String> tasks;
+
+    public Alice() {
+        this.tasks = new ArrayList<>();
+    }
 
     private void greet() {
         System.out.println(HORIZONTAL_LINE);
@@ -17,18 +23,45 @@ public class Alice {
         System.out.println(HORIZONTAL_LINE);
     }
 
+    private void echo(String message) {
+        // echo user inputs
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(String.format("> %s", message));
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private void addTask(String task) {
+        tasks.add(task);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(String.format("> added: %s", task));
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private void listTasks() {
+        System.out.println(HORIZONTAL_LINE);
+
+        if (tasks.isEmpty()) {
+            System.out.println("> no tasks added");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(String.format(" %d. %s", i + 1, tasks.get(i)));
+            }
+        }
+
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     private void listen() {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
-            String current = scanner.nextLine();
-            if (current.trim().toLowerCase().equals("bye")) {
+            String current = scanner.nextLine().trim().toLowerCase();
+            if (current.equals("bye")) {
                 break;
+            } else if (current.equals("list")) {
+                listTasks();
+            } else {
+                addTask(current);
             }
-
-            // echo user inputs
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println(String.format("> %s", current));
-            System.out.println(HORIZONTAL_LINE);
         }
         scanner.close();
     }
