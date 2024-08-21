2e2118aaef6a7f7d1560e3d6c294332ea8871682
Jack Bean <144975629+bryanjhc@users.noreply.github.com>
2024-08-21 22:03:50 +0800

    added marking as done and task class

--- AI.md
+++ AI.md
@@ -3,5 +3,6 @@ Hello there! I am using GitHub CoPilot for my individual project.
 Will update this AI.md file soon!
 
 ### Update (21/8/2024):
-GitHub copilot helped me to code the while loop including the logic for the echo feature.
-It was spot on except for the scanner which I had to edit.
+GitHub copilot helped a lot with autocomplete and suggestions.
+It was spot on except for a few things such as the scanner and my personalised messages.
+Overall I would say it cut time for me but I could still just hardcode everything by myself.
--- src/main/java/JackBean.java
+++ src/main/java/JackBean.java
@@ -5,7 +5,35 @@ public class JackBean {
     public static String horizontalLine = "____________________________________________________________";
     public static String greeting = "Hello homie! I'm JackBean, a chatbot designed to help you with your daily tasks!\nHow may I help you today my homie?";
     public static String exitMessage = "Bye homie! Come back if you need anything else. JackBean, signing off!";
-    public static ArrayList<String> taskList = new ArrayList<String>();
+    public static ArrayList<Task> taskList = new ArrayList<Task>();
+
+    // Partial solution reused from course website
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsUndone() {
+            this.isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + getStatusIcon() + "] " + description;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(horizontalLine);
@@ -14,31 +42,66 @@ public class JackBean {
 
         Scanner userInput = new Scanner(System.in);
 
-        // GitHub Copilot helped to code this entire while loop (except personalisation)
-        // and I have no idea how it knew what to do HAHAHA
+        // GitHub CoPilot helped a lot with auto-complete (except personalisation)
+        // it is actually really smart haha
         while (true) {
             String input = userInput.nextLine();
 
             if (input.equalsIgnoreCase("bye")) { // I added equalsIgnoreCase() by myself
                 break;
-            }
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(horizontalLine);
+                System.out.println("Yo homie!, here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+                System.out.println(horizontalLine);
+            } else if (input.toLowerCase().startsWith("mark")) {
+                // first use split and then parse the integer
+                String[] splitInput = input.split(" ");
+                int taskNumber = Integer.parseInt(splitInput[1]);
 
-            switch (input) {
-                case "list":
+                // check if task is already done
+                if (taskList.get(taskNumber - 1).isDone) {
                     System.out.println(horizontalLine);
-                    System.out.println("Yo homie!, here are the tasks in your list:");
-                    for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println((i + 1) + ". " + taskList.get(i));
-                    }
+                    System.out.println("This task is already done my homie:");
+                    System.out.println(taskList.get(taskNumber - 1));
                     System.out.println(horizontalLine);
-                    break;
-                default:
-                    taskList.add(input);
+                    continue;
+                }
+
+                // now handle implementation
+                taskList.get(taskNumber - 1).markAsDone();
+                System.out.println(horizontalLine);
+                System.out.println("LESGOOO homie! Good job on finishing this task:");
+                System.out.println(taskList.get(taskNumber - 1));
+                System.out.println(horizontalLine);
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                // first use split and then parse the integer
+                String[] splitInput = input.split(" ");
+                int taskNumber = Integer.parseInt(splitInput[1]);
+
+                // check if task is already undone
+                if (!taskList.get(taskNumber - 1).isDone) {
                     System.out.println(horizontalLine);
-                    System.out.println("Got it homie! I've added this task: " + input);
-                    System.out.println("Homie!, you have " + taskList.size() + " task(s) in the list now.");
+                    System.out.println("This task hasn't been marked done yet my homie:");
+                    System.out.println(taskList.get(taskNumber - 1));
                     System.out.println(horizontalLine);
-                    break;
+                    continue;
+                }
+
+                // now handle implementation
+                taskList.get(taskNumber - 1).markAsUndone();
+                System.out.println(horizontalLine);
+                System.out.println("All good my homie! You've got this, I've undone this task:");
+                System.out.println(taskList.get(taskNumber - 1));
+                System.out.println(horizontalLine);
+            } else {
+                taskList.add(new Task(input));
+                System.out.println(horizontalLine);
+                System.out.println("Got it homie! I've added this task:\n" + input);
+                System.out.println("Homie!, you have " + taskList.size() + " task(s) in the list now.");
+                System.out.println(horizontalLine);
             }
         }
 
