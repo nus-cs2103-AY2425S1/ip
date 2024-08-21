2a283b0b4eb0765e4c9e642305270c1a6f6c7476
weibinlim <limweibin0816@gmail.com>
2024-08-22 02:39:38 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Bigdog.java
@@ -0,0 +1,11 @@
+public class Bigdog {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println(STR."Hello from\n\{logo}");
+        System.out.println("Hello! I'm Bigdog.\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+    }
+}
