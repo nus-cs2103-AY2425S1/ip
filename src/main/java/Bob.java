d659c40628ff4bea814ee776d8346c3b833b0af8
sarahteonin <E1249329@u.nus.edu>
2024-08-22 01:30:56 +0800

    level 2 completed

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,11 +4,23 @@ public class Bob {
         String logo = "Bob";
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
+        String[] list;
+        list = new String[100];
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         while(!input.equals("bye")) {
-            System.out.println(input);
+            if(input.equals("list")) {
+                for(int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                input = scanner.nextLine();
+                continue;
+            }
+            System.out.println("added: " + input);
+            list[count] = input;
+            count++;
             input = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
