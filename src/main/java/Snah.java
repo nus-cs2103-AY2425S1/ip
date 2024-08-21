b60e9424c4a32ce7e467b47470d73b07eb34e2ad
Hans Delano <hanscau@hotmail.com>
2024-08-21 12:56:34 +0800

    Rename chatbot

---
+++ src/main/java/Snah.java
@@ -0,0 +1,10 @@
+public class Snah {
+    public static void main(String[] args) {
+        String chatBotName = "Snah";
+        System.out.printf("====================================\n");
+        System.out.printf("Hello! I'm %s\n", chatBotName);
+        System.out.printf("What can I do for you?\n");
+        System.out.printf("====================================\n");
+        System.out.printf("Bye. Hope to see you again soon!\n");
+    }
+}
