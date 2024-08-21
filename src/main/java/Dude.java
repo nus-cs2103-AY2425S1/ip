885c493204a108980491e20559eefe995d75659c
fusin <fusin02@gmail.com>
2024-08-22 02:47:27 +0800

    Implemented Echo

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Dude {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Dude!\nWhat can I do for you?");
         System.out.println(line);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+            input = sc.nextLine();
+        }
 
         System.out.println("Bye. Hope to see you again!");
         System.out.println(line);
