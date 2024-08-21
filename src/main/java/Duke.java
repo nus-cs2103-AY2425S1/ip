a12e031260eb1d15db67ed54ceb8dee01c443f83
leroytan <mingxiang130@gmail.com>
2024-08-22 04:18:40 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String lineBreak = "____________________________________________________________\n";
-        String intro = "Hello! I'm Light\nWhat can I do for you?\n" + lineBreak;
-        String exit = " Bye. Hope to see you again soon!\n" + lineBreak;
-        System.out.println(intro + exit);
+        String intro = lineBreak + "Hello! I'm Light\nWhat can I do for you?\n" + lineBreak;
+        String exit = lineBreak + " Bye. Hope to see you again soon!\n" + lineBreak;
+        System.out.println(intro);
+        String command;
+        Scanner scanner = new Scanner(System.in);
+        command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(lineBreak + command +"\n" + lineBreak);
+            command = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println(exit);
     }
 }
