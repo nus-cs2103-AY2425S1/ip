ecfd2c68557e8811de0474aaed9ef5abdbfb001a
daronoh <142883378+daronoh@users.noreply.github.com>
2024-08-21 15:39:50 +0800

    Implement Echo functionality

--- src/main/java/DGPT.java
+++ src/main/java/DGPT.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class DGPT {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // Starting Message
         System.out.println("-----------------------");
-        System.out.println("Hello! I'm DGPT");
-        System.out.println("What can I do for you?");
+        System.out.println("DGPT: Hello! I'm DGPT");
+        System.out.println("DGPT: What can I do for you?");
         System.out.println("-----------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        do {
+            System.out.print("User: ");
+            input = scanner.nextLine();
+
+            if (!input.equals("bye")) {
+                System.out.println("-----------------------");
+                System.out.println("DGPT: " + input);
+                System.out.println("-----------------------");
+            }
+        } while (!input.equals("bye"));
+
+        scanner.close();
+
+        // Closing Message
+        System.out.println("-----------------------");
+        System.out.println("DGPT: Bye. Hope to see you again soon!");
         System.out.println("-----------------------");
     }
 }
