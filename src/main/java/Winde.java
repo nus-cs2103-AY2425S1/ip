11c051f0de8ffafe8db96abc62eea88339e42500
WindiStone <e0958609@u.nus.edu>
2024-08-21 14:03:13 +0800

    Add src/main/java/Winde.java

---
+++ src/main/java/Winde.java
@@ -0,0 +1,22 @@
+public class Winde {
+    public static void main(String[] args) {
+        /* String logo =
+                  " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+         */
+        // System.out.println("Hello from\n" + "Winde");
+        greet();
+        exit();
+    }
+    public static void greet() {
+        System.out.println("Hello! I'm Winde\n" + "What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
