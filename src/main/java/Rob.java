fec6f12a3ea4f9c4bea5e3a5b5c1b5e58ca69790
linette-g <156193856+linette-g@users.noreply.github.com>
2024-08-21 14:41:33 +0800

    Add echo

--- src/main/java/Rob.java
+++ src/main/java/Rob.java
@@ -1,8 +1,28 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Rob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greet = "Hello! I'm Rob\n" +
                 "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(greet + exit);
+
+        System.out.println(greet);
+
+        while (true) {
+            String input = scanner.nextLine();
+            // echo
+            String echo = "____________________________________________________________\n" +
+                    input + "\n" +
+                    "____________________________________________________________\n";
+            System.out.println(echo);
+
+            // exit
+            if (Objects.equals(input, "bye")) {
+                System.out.println(exit);
+                break;
+            }
+        }
+        scanner.close();
     }
 }
