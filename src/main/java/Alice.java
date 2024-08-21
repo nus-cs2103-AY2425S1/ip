67c96645222c8181854f34cd836d529b5e716294
Leslie Yip <leslieyip02@gmail.com>
2024-08-21 13:03:48 +0800

    Add user input echo

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,12 +1,43 @@
+import java.util.Scanner;
+
 public class Alice {
+    private static final String NAME = "Alice";
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
+    private void greet() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(String.format("> Hello! I'm %s.", NAME));
+        System.out.println("> What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private void bye() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("> Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private void listen() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String current = scanner.nextLine();
+            if (current.trim().toLowerCase().equals("bye")) {
+                break;
+            }
+
+            // echo user inputs
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(String.format("> %s", current));
+            System.out.println(HORIZONTAL_LINE);
+        }
+        scanner.close();
+    }
+
     public static void main(String[] args) {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Hello! I'm Alice.");
-        System.out.println("What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
+        Alice alice = new Alice();
+
+        alice.greet();
+        alice.listen();
+        alice.bye();
     }
 }
