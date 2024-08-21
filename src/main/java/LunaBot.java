5c634af967a7e12e673b1ff62048dc6840155b27
Hartihhh06 <e1156421@u.nus.edu>
2024-08-21 12:58:55 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/LunaBot.java
@@ -0,0 +1,11 @@
+public class LunaBot {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println(" Hello! I'm LunaBot");
+        System.out.println(" What can I do for you?");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("--------------------------------------------------------------");
+
+    }
+}
