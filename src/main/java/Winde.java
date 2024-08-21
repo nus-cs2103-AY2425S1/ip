d4830f5ac86fb35e7e28aa97980c09e7cd88f59c
WindiStone <e0958609@u.nus.edu>
2024-08-21 15:06:23 +0800

    Add src/main/java/Winde.java

--- src/main/java/Winde.java
+++ src/main/java/Winde.java
@@ -16,8 +16,13 @@ public class Winde {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!(input.equals("bye"))) {
-            if (input.equals("list")) {
+            String[] command = input.split(" ");
+            if (command[0].equals("list")) {
                 list();
+            } else if (command [0].equals("mark")) {
+                mark(Integer.parseInt(command[1]));
+            } else if (command[0].equals("unmark")) {
+                unmark(Integer.parseInt(command[1]));
             } else {
                 add(input);
             }
@@ -27,16 +32,38 @@ public class Winde {
         exit();
     }
 
-    private static List<String> reminder = new ArrayList<String>();
+    private final static List<String> reminder = new ArrayList<String>();
+    private final static List<Boolean> complete = new ArrayList<Boolean>();
 
+    public static void mark(int i) {
+        System.out.print("Nice! I've marked this task as done:\n" + "    ");
+        complete.set(i - 1, true);
+        task(i);
+        System.out.println();
+    }
+
+    public static void unmark(int i) {
+        System.out.print("OK, I've marked this task as not done yet:\n" + "    ");
+        complete.set(i - 1, false);
+        task(i);
+        System.out.println();
+    }
+
+    public static void task(int i) {
+        System.out.print((complete.get(i - 1) ? "[X] " : "[ ] ") + reminder.get(i - 1));
+    }
     public static void add(String action) {
         reminder.add(action);
+        complete.add(false);
         System.out.println("added: " + action);
     }
 
     public static void list() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 1; i <= reminder.size(); i++) {
-            System.out.println(i + ". " + reminder.get(i - 1));
+            System.out.print(i + ".");
+            task(i);
+            System.out.println();
         }
     }
 
