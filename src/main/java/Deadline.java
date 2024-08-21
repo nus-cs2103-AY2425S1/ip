bfdd25887adfb99b23beec2af87b1c1d04a274b7
MAOXIONGKAI <conan7153@gmail.com>
2024-08-21 18:11:47 +0800

    Add Deadline.java for Task of type Deadline

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,15 @@
+public class Deadline extends Task{
+    private String dueDate;
+    public Deadline(String name, String dueDate) {
+        super(name);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D][%s] %s (by %s)",
+                getStatusIcon(),
+                this.name,
+                this.dueDate);
+    }
+}
