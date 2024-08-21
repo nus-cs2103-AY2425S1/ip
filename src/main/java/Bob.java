747bbcf16b9f63fbf997f9f7eddf394527ee3440
hj235 <hongjing235@gmail.com>
2024-08-21 22:37:22 +0800

    Level-2: Add, List

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bob {
     private static final String line = "____________________________________________________________\n";
+    private static ArrayList<String> commands = new ArrayList<>();
 
     private static String greeting() {
         return Bob.lineFormat(" Hello! I'm Bob\n" +
@@ -13,6 +15,18 @@ public class Bob {
     private static String lineFormat(String input) {
         return Bob.line + input + "\n" + Bob.line;
     }
+    private static void addCommand(String command) {
+        Bob.commands.add(command);
+        System.out.println(Bob.lineFormat("Added: " + command));
+    }
+    private static void listCommands() {
+        System.out.print(Bob.line);
+        for (int i = 0; i < Bob.commands.size(); i++) {
+            System.out.println(String.valueOf(i) + ". " + commands.get(i));
+        }
+        System.out.print(Bob.line);
+    }
+
     public static void main(String[] args) {
         System.out.println(Bob.greeting());
         String input = "";
@@ -20,12 +34,18 @@ public class Bob {
 
         while (true) {
             System.out.print("Enter your command: ");
-            input = scanner.next();
+            input = scanner.nextLine();
+
             if (input.compareTo("bye") == 0) {
                 System.out.println(Bob.farewell());
                 break;
             }
-            System.out.println(Bob.lineFormat(input));
+            if (input.compareTo("list") == 0) {
+                Bob.listCommands();
+                continue;
+            }
+
+            Bob.addCommand(input);
         }
     }
 }
