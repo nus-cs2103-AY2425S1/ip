f92f692d020109edba42b1cf22a2074e361e1ed2
Denon Chong <e0958801@u.nus.edu>
2024-08-21 18:09:30 +0800

    Added echoBack function so that the bot will echo whatever things user input except bye

--- src/main/java/Mentos.java
+++ src/main/java/Mentos.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Mentos
 {
     public static void main(String[] args) {
         Mentos mentos = new Mentos();
         mentos.startConversation();
+        mentos.echoBack();
         mentos.endConversation();
     }
 
@@ -16,6 +18,19 @@ public class Mentos
         System.out.println("____________________________");
         System.out.println("Pop a Mentos, stay fresh! See you next time!");
         System.out.println("____________________________");
+    }
+
+    public void echoBack(){
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("bye")){
+                return;
+            }
+            System.out.println("____________________________");
+            System.out.println(input + "?");
+            System.out.println("____________________________");
+        }
     }
 }
 
