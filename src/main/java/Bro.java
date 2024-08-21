c1e40c7ba0a8188e03cdc958771dac2855df38c8
karthickkc <>
2024-08-21 14:23:29 +0800

    did Level-3, added Task class

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -6,19 +6,31 @@ public class Bro {
         String line = "______________________________________________________\n";
         System.out.println("   " + line + "   Hello! I'm Bro\n   What can I do for you?\n"
                            + "   " + line);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         Scanner prompt = new Scanner(System.in);
         String word = prompt.nextLine();
         while (!word.equalsIgnoreCase("bye")) {
             if (word.equalsIgnoreCase("list")) {
                 int len = list.size();
-                System.out.print("   " + line);
+                System.out.print("   " + line + "   Here are the tasks in your list:\n");
                 for (int i = 0; i < len; i++) {
-                    System.out.printf("   %d. %s\n", i + 1, list.get(i));
+                    System.out.printf("   %d.%s\n", i + 1, list.get(i));
                 }
                 System.out.print("   " + line);
+            } else if (word.length() > 5 &&
+                       word.substring(0, 5).equalsIgnoreCase("mark ")) {
+                int index = Integer.parseInt(word.substring(5));
+                list.get(index - 1).mark();
+                System.out.print("   " + line + "   Nice! I've marked this task as done:\n"
+                                 + "   " + list.get(index - 1) + "\n   " + line);
+            } else if (word.length() > 7 &&
+                       word.substring(0, 7).equalsIgnoreCase("unmark ")) {
+                int index = Integer.parseInt(word.substring(7));
+                list.get(index - 1).unmark();
+                System.out.print("   " + line + "   OK, I've marked this task as not done yet:\n"
+                                 + "   " + list.get(index - 1) + "\n   " + line);
             } else {
-                list.add(word);
+                list.add(new Task(word));
                 System.out.println("   " + line + "   added: " + word + "\n" + "   " + line);
             }
             Scanner next_prompt = new Scanner(System.in);
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    String name;
+    boolean done = false;
+
+    public Task(String s) {
+        name = s;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (done) {
+            return "[X] " + name;
+        } else {
+            return "[ ] " + name;
+        }
+    }
+}
