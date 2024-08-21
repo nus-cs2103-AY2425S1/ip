12a4639b83d972e140241f2bace2d4a590352470
ITZXITZX <isaacteozixian@hotmail.com>
2024-08-22 01:51:13 +0800

    listRecords function: list a record of all user inputs
    Able to return a list of all user inputs
    
    Let's
    * add counter to store the latest count of input
    * add records to store all records in an Arraylist
    * add getters and setters to retrieve and update these fields
    * add listRecord function to list all records
    * update the chat function to store each input by the user

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * This is a chatbot class named Bob.
  */
 public class Bob {
+    // Stores all user inputs.
+    private ArrayList<String> records;
+    // Keeps the count of the number of user inputs stored.
+    private int counter;
+
+    /**
+     * Initialises an instance of Bob.
+     */
+    public Bob() {
+        this.records = new ArrayList<>();
+        counter = 0;
+    }
+
 
     public static void main(String[] args) {
         String welcome = "Hello! I'm Bob\n"
                 + "\tWhat can I do for you?";
         Bob.printLines(welcome);
         Bob bob = new Bob();
-        bob.chat();
+        Bob.chat(bob);
     }
 
     /**
      * This is a chat function by Bob.
      */
-    void chat() {
+    static void chat(Bob bob) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
+        String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            Bob.printLines(input);
-            input = scanner.next();
+            if (input.equals("list")) {
+                bob.listRecords();
+                input = scanner.nextLine();
+                continue;
+            }
+            String immediateAdd = "added: " + input;
+            Bob.printLines(immediateAdd);
+            bob.records.add(bob.getCounter(), input);
+            bob.setCounter(bob.getCounter() + 1);
+            input = scanner.nextLine();
         }
         printLines("Bye. Hope to see you again soon!bye");
     }
@@ -37,5 +59,35 @@ public class Bob {
                 + "\n"
                 + "\t____________________________________________________________\n";
         System.out.println(textToPrint);
+    }
+
+    /**
+     * Returns the index of the next available slot in the arraylist of records.
+     */
+    public int getCounter() {
+        return this.counter;
+    }
+
+    /**
+     * Sets the count of the total number of recorded items in records.
+     */
+    public void setCounter(int x) {
+        this.counter = x;
+    }
+
+    /**
+     * Prints a list of all recorded user inputs.
+     */
+    public void listRecords() {
+        String allRecords = "";
+        for (int i = 0; i < records.size(); i++) {
+            int num = i + 1;
+            if (num == records.size()) {
+                allRecords += num + ". " + records.get(i);
+            } else {
+                allRecords += num + ". " + records.get(i) + "\n\t";
+            }
+        }
+        Bob.printLines(allRecords);
     }
 }
