bc2d5fa60b1bb6517178df7b8c585ed083b639a4
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-22 06:16:03 +0800

    TaskTracker.java: Changed type of taskList from String to Task

--- src/main/java/TaskTracker.java
+++ src/main/java/TaskTracker.java
@@ -1,9 +1,9 @@
 public class TaskTracker {
-    private String[] taskList;
+    private Task[] taskList;
     private int counter;
 
     public TaskTracker() {
-        this.taskList = new String[100];
+        this.taskList = new Task[100];
         this.counter = 0;
     }
     public void getList() {
@@ -17,7 +17,7 @@ public class TaskTracker {
     }
 
     public void updateList(String s) {
-        taskList[this.counter] = s;
+        taskList[this.counter] = new Task(s);
         this.counter++;
     }
 }
