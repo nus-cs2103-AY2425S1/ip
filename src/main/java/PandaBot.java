9259d2c8d4cb0cbcf1bf227826d3576a2a065398
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-08-21 17:44:09 +0800

    Add support for echo

--- src/main/java/PandaBot.java
+++ src/main/java/PandaBot.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class PandaBot {
     public static void main(String[] args) {
         String line = "_________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
+        // Simple greeting to the user by PandaBot
         System.out.println(line);
         System.out.println("Hello! I'm PandaBot");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        // Echo user input until the user inputs "bye"
+        while (true) {
+            String input = scanner.nextLine();
+
+            // if user has said "bye", stop echo and exit
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            // echo user input
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
