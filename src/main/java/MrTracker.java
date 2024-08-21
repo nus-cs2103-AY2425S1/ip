8d929d6f8b455becad9868a735f1b47394d09f51
Clarenceeey <clarencey100@gmail.com>
2024-08-21 20:54:40 +0800

    Greet and exit

--- src/main/java/MrTracker.java
+++ src/main/java/MrTracker.java
@@ -1,6 +1,22 @@
-public class Duke {
+public class MrTracker {
+
+    public static void printLine() {
+        int length = 75;
+        for (int i = 0; i < length; i++) {
+            System.out.print('-');
+            if (i == length - 1) {
+                System.out.println();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String name = "Mr Tracker";
-        System.out.println("Hello from " + name);
+        MrTracker.printLine();
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you? \n");
+        MrTracker.printLine();
+        System.out.println("Bye. Hope to see you again soon! \n");
+        MrTracker.printLine();
     }
 }
