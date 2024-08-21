ec591fdde7a82e9e4d40680629f72e5cd01f6891
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-08-21 15:17:30 +0800

    Add simple greeting and exit message

--- src/main/java/Neuro.java
+++ src/main/java/Neuro.java
@@ -1,10 +1,10 @@
 public class Neuro {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("___________________________________________________");
+        System.out.println("Hi, I'm Neuro, your chatbot assistant!");
+        System.out.println("How can I help you?");
+        System.out.println("___________________________________________________");
+        System.out.println("Goodbye!");
+        System.out.println("___________________________________________________");
     }
 }
