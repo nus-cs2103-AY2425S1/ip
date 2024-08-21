706849c52c41b9036387fbd8f7ef4887481f4fbb
fusin <fusin02@gmail.com>
2024-08-22 04:30:52 +0800

    Added exception handling

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -5,14 +5,27 @@ public class Dude {
 
     private static ArrayList<Task> taskList = new ArrayList<>();
 
-    public static void addTask(String input) {
+    public static void addTask(String input) throws DudeException{
         if (input.startsWith("todo ")) {
+            if (input.substring(5).isEmpty()) {
+                throw new DudeException("The description of a todo cannot be empty!");
+            }
             taskList.add(new Todo(input.substring(5)));
         } else if (input.startsWith("deadline ")) {
             String[] arr = input.substring(9).split(" /by ");
+            if (arr[0].isEmpty()) {
+                throw new DudeException("The description of a deadline cannot be empty!");
+            } else if (arr.length < 2) {
+                throw new DudeException("The deadline of a deadline cannot be empty!");
+            }
             taskList.add(new Deadline(arr[0], arr[1]));
         } else if (input.startsWith("event ")) {
             String[] arr = input.substring(6).split(" /");
+            if (arr[0].isEmpty()) {
+                throw new DudeException("The description of an event cannot be empty!");
+            } else if (arr.length < 3 || !arr[1].startsWith("from ") || !arr[2].startsWith("to ")) {
+                throw new DudeException("The format of timings of the event is wrong!");
+            }
             taskList.add(new Event(arr[0], arr[1].substring(5), arr[2].substring(3)));
         }
         System.out.println("Got it. I've added this task:");
@@ -20,11 +33,11 @@ public class Dude {
         System.out.println("Now you have " + taskList.size() + " tasks in the list.");
     }
 
-    public static void markTaskAsDone(int index) {
+    public static void markTaskAsDone(int index) throws DudeException {
         if (index > taskList.size() || index < 1) {
-            System.out.println("This task does not exist!");
+            throw new DudeException("This task does not exist!");
         } else if (taskList.get(index - 1).isDone) {
-            System.out.println("This task is already marked as done!");
+            throw new DudeException("This task is already marked as done!");
         } else {
             taskList.get(index - 1).markAsDone();
             System.out.println("Nice! I've marked this task as done:");
@@ -32,11 +45,11 @@ public class Dude {
         }
     }
 
-    public static void markTaskAsUndone(int index) {
+    public static void markTaskAsUndone(int index) throws DudeException {
         if (index > taskList.size() || index < 1) {
-            System.out.println("This task does not exist!");
+            throw new DudeException("This task does not exist!");
         } else if (!taskList.get(index - 1).isDone) {
-            System.out.println("This task is already marked as not done!");
+            throw new DudeException("This task is already marked as not done!");
         } else {
             taskList.get(index - 1).markAsUndone();
             System.out.println("OK, I've marked this task as not done yet:");
@@ -44,18 +57,18 @@ public class Dude {
         }
     }
 
-    public static void printList() {
+    public static void printList() throws DudeException {
         if (taskList.isEmpty()) {
             System.out.println("There are no tasks in your list!");
-            return;
-        }
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskList.size(); i++) {
-            System.out.println(i + 1 + "." + taskList.get(i));
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.println(i + 1 + "." + taskList.get(i));
+            }
         }
     }
 
-    public static void action(String input) {
+    public static void action(String input) throws DudeException {
         if (input.startsWith("mark ")) {
             markTaskAsDone(Integer.parseInt(input.substring(5)));
         } else if (input.startsWith("unmark ")) {
@@ -65,7 +78,7 @@ public class Dude {
         } else if (input.startsWith("todo ") || input.startsWith("deadline ") || input.startsWith("event ")) {
             addTask(input);
         } else {
-            System.out.println("I'm sorry, but I don't know what that means :(");
+            throw new DudeException("I'm sorry, but I don't know what that means :(");
         }
     }
     public static void main(String[] args) {
@@ -77,11 +90,14 @@ public class Dude {
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println(line);
-            action(input);
+            try {
+                action(input);
+            } catch (DudeException e) {
+                System.out.println(e.getMessage());
+            }
             System.out.println(line);
             input = sc.nextLine();
         }
-
         System.out.println(line);
         System.out.println("Bye. Hope to see you again!");
         System.out.println(line);
