4882659507dffd6260f336d89d49f22339e8d21a
Liu Yinze <>
2024-08-21 20:21:33 +0800

    add Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
+    private static String NAME = "Genji";
+    public static String bye() {
+        return "Bye. Hope to see you again soon!";
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //String logo = " ____        _        \n"
+        //        + "|  _ \\ _   _| | _____ \n"
+        //        + "| | | | | | | |/ / _ \\\n"
+        //        + "| |_| | |_| |   <  __/\n"
+        //        + "|____/ \\__,_|_|\\_\\___|\n";
+        //System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm " + NAME + "\nWhat can I do for you?");
+        System.out.println(Duke.bye());
     }
 }
