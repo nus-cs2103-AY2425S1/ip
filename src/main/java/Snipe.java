7410c43383e528db881c35ed107de645f3294cb0
DKHB0207 <daryl.koh.hb@gmail.com>
2024-08-22 04:13:45 +0800

    add mark and unmark tasks

--- src/main/java/Snipe.java
+++ src/main/java/Snipe.java
@@ -9,7 +9,7 @@ public class Snipe {
             + "/_______  /___|  /__|   __/ \\___  >\n"
             + "        \\/     \\/   |__|        \\/ \n";
     private static final String HORIZONTAL_LINE = "_".repeat(60);
-    private static ArrayList<String> list = new ArrayList<String>();
+    private static ArrayList<String[]> list = new ArrayList<String[]>();
     public void initChat() {
         greetUser();
         handleUserInput();
@@ -33,16 +33,38 @@ public class Snipe {
     private void handleSpecialInputs(String userInput, Scanner scanner) {
         if (userInput.equalsIgnoreCase("list")) {
             returnList();
+        } else if(userInput.startsWith("mark")) {
+            String[] split = userInput.split(" ");
+            int index = Integer.valueOf(split[1]) - 1;
+            if (list.get(index)[1] == "X") {
+                printWithLines("This task is already marked done!");
+            } else {
+                list.get(index)[1] = "X";
+                printWithLines("Nice! I've marked this task as done:\n" +
+                        "[X] " + list.get(index)[0]);
+            }
+        } else if(userInput.startsWith("unmark")) {
+            String[] split = userInput.split(" ");
+            int index = Integer.valueOf(split[1]) - 1;
+            if (list.get(index)[1] == " ") {
+                printWithLines("This task is currently not done yet!");
+            } else {
+                list.get(index)[1] = " ";
+                printWithLines("OK, I've marked this task as not done yet:\n" +
+                        "[ ] " + list.get(index)[0]);
+            }
         } else {
-            list.add(userInput);
+            String[] input = new String[]{userInput, " "};
+            list.add(input);
             String message = "added: " + userInput;
             printWithLines(message);
         }
     }
     private void returnList() {
         System.out.println(HORIZONTAL_LINE);
+        System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < list.size(); i++) {
-            String item = String.format("%d. ", i + 1) + list.get(i);
+            String item = String.format("%d.", i + 1) + "[" + list.get(i)[1] + "] " + list.get(i)[0];
             System.out.println(item);
         }
         System.out.println(HORIZONTAL_LINE);
