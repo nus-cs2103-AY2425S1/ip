83824af574aa86352bd70c59d3cec1ad8c855c01
jktang14 <tangboy14@gmail.com>
2024-08-21 15:10:59 +0800

    Add string representation for Todo

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,2 +1,10 @@
-public class Todo {
+public class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[T] %s", super.toString());
+    }
 }
