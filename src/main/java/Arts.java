966397c21b77efd654cbf9a3ed6d6148276b48ee
LiHaixin000 <lihaixin000@gmail.com>
2024-08-21 16:53:06 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Arts.java
@@ -0,0 +1,19 @@
+public class Arts {
+    public static void main(String[] args) {
+        String logo = "     _    _____  _______  _____  \n"
+                + "    / \\  |  __ \\|__   __|/ ____| \n"
+                + "   / _ \\ | |__) |  | |  | (___   \n"
+                + "  / ___ \\|  _  /   | |   \\___ \\  \n"
+                + " /_/   \\_\\_| \\_\\   |_|   |_____/ \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Arts");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
