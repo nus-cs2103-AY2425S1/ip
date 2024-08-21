ae9d907537efe48ddfaab2ee8bf44d5a7f4af1d1
Lynette Lee <sayhitosiying@Lynettes-Laptop.local>
2024-08-21 16:20:51 +0800

    Add greeting and exit

---
+++ src/main/java/Socchat.java
@@ -0,0 +1,12 @@
+public class Socchat {
+    public static void main(String[] args) {
+        
+    }
+    public static void greet() {
+        System.out.println("Hello! I'm Socchat!");
+        System.out.println("What can I do for you?");
+    }
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!    ");
+    }
+}
