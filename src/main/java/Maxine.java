b21aea98b8de9308e1c9bd0a8ee9ca8fc6665587
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2024-08-21 13:22:42 +0800

    Add logic to handle different task types * Handle Todo, deadlines, event

--- src/main/java/Maxine.java
+++ src/main/java/Maxine.java
@@ -45,15 +45,62 @@ public class Maxine {
                     sb.append(word);
                 }
                 Todo task = new Todo(sb.toString());
+                list.add(task);
+            } else if (arr[0].equals("deadline")) {
+                StringBuilder desc = new StringBuilder();
+                StringBuilder ddl = new StringBuilder();
+                desc.append(arr[1]);
+                boolean by = false;
+                for (int i = 2; i < arr.length; i++) {
+                    if (arr[i].equals("/by")) {
+                        by = true;
+                    }
+                    else if (by) {
+                        String word = " " + arr[i];
+                        ddl.append(word);
+                    } else {
+                        String word = " " + arr[i];
+                        desc.append(word);
+                    }
+                }
+                Deadline task = new Deadline(desc.toString(), ddl.toString());
+                list.add(task);
+            } else if (arr[0].equals("event")) {
+                StringBuilder desc = new StringBuilder();
+                StringBuilder start = new StringBuilder();
+                StringBuilder end = new StringBuilder();
+                desc.append(arr[1]);
+                boolean from = false;
+                boolean to = false;
+                for (int i = 2; i < arr.length; i++) {
+                    if (arr[i].equals("/from")) {
+                        from = true;
+                    }
+                    else if (arr[i].equals("/to")) {
+                        from = false;
+                        to = true;
+                    } else if (from) {
+                        String word = " " + arr[i];
+                        start.append(word);
+                    } else if (to) {
+                        String word = " " + arr[i];
+                        end.append(word);
+                    } else {
+                        String word = " " + arr[i];
+                        desc.append(word);
+                    }
+                }
+                Event task = new Event(desc.toString(), start.toString(), end.toString());
+                list.add(task);
             } else {
                 Task task = new Task(answer);
                 list.add(task);
             }
 
         }
-        while (!answer.equals("bye max!"));
+        while (!answer.equals("bye"));
 
-        System.out.println("\nBye! I have been maxxed out and am going to sleep. Hope to see you again soon!");
+        System.out.println("\nBye! I have been maxed out and am going to sleep. Hope to see you again soon!");
     }
 
 
