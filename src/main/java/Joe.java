482d3e2dbef1f3fd4f89341517dfff0a2e3cccc2
Ansel-Ch <achong2014@gmail.com>
2024-08-21 15:17:56 +0800

    Update to echo to include joe's responeses

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -10,19 +10,47 @@ public class Joe {
 
     public static void echo() {
         Scanner reader = new Scanner(System.in);
+        int count = 0;
         String userCmd = reader.nextLine();
+        String joesThoughts = null;
+
         while (!userCmd.equalsIgnoreCase("bye")) {
-            System.out.println(line + "\n" + userCmd + "\n" + line);
+            count++;
+            switch (count) {
+                case 1:
+                    joesThoughts = "Joe: I'm just an echo chamber...";
+                    break;
+                case 5:
+                    joesThoughts =  "Joe: I'm being overworked here ;~;";
+                    break;
+                case 10:
+                    joesThoughts = "Joe: Please type bye";
+                    break;
+                case 15:
+                    System.out.printf("%s\nJoe: I'm shutting this operation down!\n", line);
+                    break;
+                default:
+                    joesThoughts = null;
+            }
+
+            if (count == 15) {
+                break;
+            }
+
+            if (joesThoughts != null) {
+                System.out.printf("%s\n%s\n%s\n%s\n", line, userCmd, joesThoughts, line);
+            } else {
+                System.out.printf("%s\n%s\n%s\n", line, userCmd, line);
+            }
+
             userCmd = reader.nextLine();
         }
         bye();
     }
 
     public static void main(String[] args) {
-        System.out.println(line +
-                "\nHello! I'm Joe\n" +
-                "What can I do for you?\n"+
-                line);
+        System.out.printf("%s\nHello! I'm Joe\nWhat can I do for you?\n%s\n",
+                line, line);
         echo();
     }
 }
