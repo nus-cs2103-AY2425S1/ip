33994c168b1dd34a1ed5cb026d954586a2e81493
slye20 <samuellim2011@gmail.com>
2024-08-21 14:49:45 +0800

    Change bot name from Duke to EchoMind

---
+++ src/main/java/EchoMind.java
@@ -0,0 +1,11 @@
+public class EchoMind {
+    public static void main(String[] args) {
+        String message = "_____________________________________________________________\n"
+                + "Hello! I'm EchoMind! \n"
+                + "What can I do for you? \n"
+                + "_____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "_____________________________________________________________\n";
+        System.out.println(message);
+    }
+}
