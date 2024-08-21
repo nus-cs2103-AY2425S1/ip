71e1fe00f3075acc6e640aa45e925ac0aee83451
Siow Rui Ming <siowruiming@gmail.com>
2024-08-21 15:56:59 +0800

    Echo

--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -4,9 +4,15 @@ public class Neko {
         Scanner scanner = new Scanner(System.in);
         String greetingMessage =  "  ∧,,,∧  \n( ̳̳• · ̳• ) \n づ Meow! I'm Neko\nWhat can I do for you? ";
         String exitMessage = "Bye! Hope to see you again soon meow ฅ ฅ";
-        
+
         System.out.println(greetingMessage);
-        scanner.nextLine();
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
         System.out.println(exitMessage);
     }
 }
