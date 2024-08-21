46f2372db85ce0718f8567bb41d4b8c50560d85e
Ling Ziyan <lingziyan2015@gmail.com>
2024-08-21 20:52:56 +0800

    Added Level 0 and Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String name = "Duke";
+
+    private static void exit() {
+        System.out.println("Bye! Hope to see you again my G");
+    }
+
+    private static void greet() {
+        Boolean endChat = false;
+        System.out.println("Hello! I'm " + name + " aka ChatGPT on Crack!\nWhat assistance are you in need of today?");
+        while (!endChat) {
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            if (command.toLowerCase().equals("bye")) {
+                System.out.println("See you loser");
+            } else {
+                System.out.println("I see, you need help with " + command + "!");
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        exit();
+        greet();
+
     }
 }
