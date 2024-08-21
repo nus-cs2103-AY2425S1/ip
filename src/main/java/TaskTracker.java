b638584517a4627ca373605ef7b7be988014100a
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-22 05:56:34 +0800

    TaskTracker.java: Include methods for adding and previewing tasks

--- src/main/java/TaskTracker.java
+++ src/main/java/TaskTracker.java
@@ -6,5 +6,18 @@ public class TaskTracker {
         this.taskList = new String[100];
         this.counter = 0;
     }
-    
+    public void getList() {
+        if (this.tracker == 0) {
+            System.out.println("No tasks currently!");
+        } else {
+            for (int i = 0; i < this.tracker; i++) {
+                System.out.println("" + (i + 1) + ". " + this.taskList[i]);
+            }
+        }
+    }
+
+    public void updateList(String s) {
+        taskList[this.tracker] = s;
+        this.tracker++;
+    }
 }
