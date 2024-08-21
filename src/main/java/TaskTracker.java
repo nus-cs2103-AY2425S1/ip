8bf5db5e2039ca95d258dc371570deaf779d38e3
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-22 05:54:26 +0800

    TaskTracker.java: Add new TaskTracker class

---
+++ src/main/java/TaskTracker.java
@@ -0,0 +1,10 @@
+public class TaskTracker {
+    private String[] taskList;
+    private int counter;
+
+    public TaskTracker() {
+        this.taskList = new String[100];
+        this.counter = 0;
+    }
+    
+}
