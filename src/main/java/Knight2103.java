42aea8548325898555e311e41824247e57fa824f
Chua Tse Hui <htcylsg@gmail.com>
2024-08-21 23:05:48 +0800

    delete function done

--- src/main/java/Knight2103.java
+++ src/main/java/Knight2103.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Knight2103 {
-    public static String printList(Task[] list, int length) {
+    public static String printList(ArrayList<Task> list, int length) {
         String stringToPrint = "";
         for (int i = 0; i < length; i++) {
             int bulletPoint = i + 1;
-            stringToPrint += bulletPoint + ". " + list[i] + "\n";
+            stringToPrint += bulletPoint + ". " + list.get(i) + "\n";
         }
         return stringToPrint;
     }
@@ -21,8 +22,7 @@ public class Knight2103 {
         System.out.println(intro);
 
         // Initialise Task list
-        Task[] taskList = new Task[100];
-        int totalTaskNumber = 0;
+        ArrayList<Task> taskList = new ArrayList<Task>();
 
         // Enable Input
         Scanner scanObject = new Scanner(System.in);
@@ -34,44 +34,48 @@ public class Knight2103 {
             try {
                 switch (inputArray[0]) {
                     case "list":
-                        String listContents = printList(taskList, totalTaskNumber);
+                        String listContents = printList(taskList, taskList.size());
                         System.out.println(horiLine + "\n" + listContents + horiLine);
                         break;
                     case "todo":
                         taskToAdd = new Todo(inputArray[1]);
-                        taskList[totalTaskNumber] = taskToAdd;
-                        totalTaskNumber++;
-                        System.out.println(horiLine + "\n Got it. I've added this task:\n" + taskToAdd + "\n Now you have " + totalTaskNumber + " tasks in the list.\n" + horiLine);
+                        taskList.add(taskToAdd);
+                        System.out.println(horiLine + "\nGot it. I've added this task:\n" + taskToAdd + "\n Now you have " + taskList.size() + " tasks in the list.\n" + horiLine);
                         break;
                     case "deadline":
                         String[] deadlineArray = inputArray[1].split(" /by ");
                         taskToAdd = new Deadline(deadlineArray[0], deadlineArray[1]);
-                        taskList[totalTaskNumber] = taskToAdd;
-                        totalTaskNumber++;
-                        System.out.println(horiLine + "\n Got it. I've added this task:\n" + taskToAdd + "\n Now you have " + totalTaskNumber + " tasks in the list.\n" + horiLine);
+                        taskList.add(taskToAdd);
+                        System.out.println(horiLine + "\nGot it. I've added this task:\n" + taskToAdd + "\n Now you have " + taskList.size() + " tasks in the list.\n" + horiLine);
                         break;
                     case "event":
                         String[] eventArray = inputArray[1].split(" /from | /to ");
                         taskToAdd = new Event(eventArray[0], eventArray[1], eventArray[2]);
-                        taskList[totalTaskNumber] = taskToAdd;
-                        totalTaskNumber++;
-                        System.out.println(horiLine + "\n Got it. I've added this task:\n" + taskToAdd + "\n Now you have " + totalTaskNumber + " tasks in the list.\n" + horiLine);
+                        taskList.add(taskToAdd);
+                        System.out.println(horiLine + "\nGot it. I've added this task:\n" + taskToAdd + "\n Now you have " + taskList.size() + " tasks in the list.\n" + horiLine);
                         break;
                     case "mark":
                         int taskMarkIndex = Integer.parseInt(inputArray[1]) - 1; // can try
-                        taskList[taskMarkIndex].markDone(); // need to check if it works
-                        System.out.println(horiLine + "\nNice! I've marked this task as done:\n" + taskList[taskMarkIndex] + "\n" + horiLine);
+                        taskList.get(taskMarkIndex).markDone(); // need to check if it works
+                        System.out.println(horiLine + "\nNice! I've marked this task as done:\n" + taskList.get(taskMarkIndex) + "\n" + horiLine);
                         break;
                     case "unmark":
                         int taskUnmarkIndex = Integer.parseInt(inputArray[1]) - 1; // can try
-                        taskList[taskUnmarkIndex].unmarkDone();
-                        System.out.println(horiLine + "\nOK, I've marked this task as not done yet:\n" + taskList[taskUnmarkIndex] + "\n" + horiLine);
+                        taskList.get(taskUnmarkIndex).unmarkDone();
+                        System.out.println(horiLine + "\nOK, I've marked this task as not done yet:\n" + taskList.get(taskUnmarkIndex) + "\n" + horiLine);
                         break;
+                    case "delete":
+                        int taskDeleteIndex = Integer.parseInt(inputArray[1]) - 1; // can try
+                        Task taskToDelete = taskList.get(taskDeleteIndex);
+                        taskList.remove(taskDeleteIndex);
+                        System.out.println(horiLine + "\nNoted. I've removed this task:\n" + taskToDelete + "\n Now you have " + taskList.size() + " tasks in the list.\n" + horiLine);
+                        break;
+
                     default:
                         throw new InstructionInvalid();
                 }
             } catch (InstructionInvalid e) {
-                System.out.println("Invalid Instruction. Only valid Instructions are list, todo, deadline, event, mark, unmark");
+                System.out.println("Invalid Instruction. Only valid Instructions are list, todo, deadline, event, mark, unmark, delete");
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("There's an issue in the instruction format");
             } catch (NullPointerException e) { // only happen in mark and unmark I think due to TaskList dynamic allocation specified
