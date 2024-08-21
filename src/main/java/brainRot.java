ba0004b3c439535b1db71bc7429d128bdbd4125f
Martin Ng <martinng2002@gmail.com>
2024-08-21 16:08:54 +0800

    Level 2. Add, List

--- src/main/java/brainRot.java
+++ src/main/java/brainRot.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class brainRot {
     public static void main(String[] args) {
 
+        //creating an array with a number to memorise how many memory slots have been taken
+        String[] arr = new String[100];
+        int mem = 0;
+
         Scanner reader = new Scanner(System.in);
 
         String greeting = "____________________________________________________________ \n"
@@ -15,10 +19,23 @@ public class brainRot {
         System.out.println(greeting);
         String answer = reader.nextLine();
 
+        //logic to check if "bye" or "list" has been said
         while(!answer.equals("bye")) {
-            System.out.println(answer);
-            reader = new Scanner(System.in);
-            answer = reader.nextLine();
+            if(answer.equals("list")) {
+                for(int i = 0 ; i < mem; i ++) {
+                    System.out.println((i+1) + "." + arr[i]);
+                }
+                reader = new Scanner(System.in);
+                answer = reader.nextLine();
+
+            } else {
+                System.out.println(answer);
+                arr[mem] = answer;
+                mem++;
+                reader = new Scanner(System.in);
+                answer = reader.nextLine();
+            }
+
         }
         System.out.println(goodBye);
     }
