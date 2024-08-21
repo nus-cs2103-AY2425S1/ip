34e34d2d2a6a071505d0dd59f90422dd88809993
Kelly Wong <justwjyk@gmail.com>
2024-08-21 21:21:11 +0800

    Level-0: rename, greeting, exit

---
+++ src/main/java/Chatsy.java
@@ -0,0 +1,13 @@
+public class Chatsy {
+    static final String line = "\t____________________________________________________________";
+    public static void main(String[] args) {
+        String greetingMessage = "\tHello! I'm Chatsy\n\tWhat can I do for you?";
+        String exitMessage = "\tBye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(greetingMessage);
+        System.out.println(line);
+        System.out.println(exitMessage);
+        System.out.println(line);
+    }
+}
