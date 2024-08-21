f01ab8c93b1ccf150c9ebf7095706c4c86e4e8a6
RuijianLu <e1297772@u.nus.edu>
2024-08-21 16:55:23 +0800

    Initialize the bot with static greeting and exiting.

---
+++ src/main/java/LuToDo.java
@@ -0,0 +1,6 @@
+public class LuToDo {
+    public static void main(String[] args) {
+
+        System.out.print(Utility.greeting() + Utility.bye());
+    }
+}
---
+++ src/main/java/Utility.java
@@ -0,0 +1,13 @@
+public class Utility {
+
+    public static final String line = "____________________________________________________________\n";
+    public static String greeting() {
+        return line +
+                "Hello! I'm LuToDo\n" +
+                "What can I do for you?\n";
+    }
+
+    public static String bye() {
+        return line + "Bye. Hope to see you again soon!\n" + line;
+    }
+}
