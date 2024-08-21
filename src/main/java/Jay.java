903a370f8359d4e0f3d28109956742304ff38036
tckeong <teo238540@gmail.com>
2024-08-21 21:09:54 +0800

    Level-1

--- src/main/java/Jay.java
+++ src/main/java/Jay.java
@@ -1,12 +1,51 @@
+import java.util.Scanner;
+
 public class Jay {
     public static void main(String[] args) {
-        String greet =
-                "____________________________________________________________\n" +
-                " Hello! I'm Jay\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(greet);
+        Jay jay = new Jay("Jay");
+        jay.start();
     }
+
+    private final String name;
+
+    public Jay(String name) {
+        this.name = name;
+    }
+
+    public void start() {
+        System.out.println(this.greet());
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(this.quit());
+                break;
+            } else {
+                System.out.println(this.processCommand(command));
+            }
+        }
+    }
+
+    private String greet() {
+        String greet =
+                " Hello! I'm " + this.name + "\n" +
+                " What can I do for you?";
+        return formatCommand(greet);
+    }
+
+    private String quit() {
+        return formatCommand("Bye. Hope to see you again soon!");
+    }
+
+    private String processCommand(String command) {
+        return formatCommand(command);
+    }
+
+    private String formatCommand(String command) {
+        return "____________________________________________________________\n" +
+                command + "\n" +
+                "____________________________________________________________\n";
+    }
+
 }
