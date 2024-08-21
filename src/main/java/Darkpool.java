313dbd484582ab8619ac8f6e4ffbaa3bb9a8ccae
arnav-goel10 <e1130141@u.nus.edu>
2024-08-21 21:39:16 +0800

    Implement Task in Darkpool

--- src/main/java/Darkpool.java
+++ src/main/java/Darkpool.java
@@ -5,11 +5,11 @@ public class Darkpool {
     public static void main(String[] args) {
         final String greeting = "it’s darkpool. what twisted reason dragged me into your misery?";
         final String bye = "contact me again and you'll regret it.";
-        ArrayList<String> todoList = new ArrayList<String>();
+        ArrayList<Task> todoList = new ArrayList<>();
         output(greeting);
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.nextLine();
+            String userInput = scanner.next();
 
             switch (userInput) {
                 case "bye" -> {
@@ -17,23 +17,35 @@ public class Darkpool {
                     scanner.close();
                     return;
                 }
+
+                case "mark" -> {
+                    int num = Integer.parseInt(scanner.next()) - 1;
+                    todoList.get(num).markDone();
+                    output("why do i have to wipe this mess\n\t" + todoList.get(num).toString());
+                }
+
+                case "unmark" -> {
+                    int num = Integer.parseInt(scanner.next()) - 1;
+                    todoList.get(num).markUndone();
+                    output("perfect! i have undone this crap\n\t" + todoList.get(num).toString());
+                }
+
                 case "list" -> {
-                    StringBuilder temp = new StringBuilder();
+                    StringBuilder temp = new StringBuilder("why am i stuck here\n\t");
                     for (int i = 0; i < todoList.size(); i++) {
-                        temp.append((i + 1)).append(". ").append(todoList.get(i)+"\n\t");
+                        temp.append((i + 1)).append(". ").append(todoList.get(i).toString()).append("\n\t");
                     }
                     temp.setLength(temp.length() - 2);
                     output(String.valueOf(temp));
-                    break;
                 }
 
                 default -> {
-                    todoList.add(userInput);
-                    output("added: " + userInput);
+                    String desc = userInput + scanner.nextLine();
+                    todoList.add(new Task(desc));
+                    output("added: " + desc);
                 }
             }
         }
-
     }
 
     private static void output(String input) {
