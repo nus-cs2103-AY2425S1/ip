dbe93509e5727b069e3c695d831909a103a5e70b
Wong Wei Han <e0958752@nus.edu.sg>
2024-08-21 08:44:37 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Will.java
+++ src/main/java/Will.java
@@ -21,9 +21,64 @@ public class Will {
             this.isDone = false;
         }
 
+        String getType() {
+            return "[ ]";
+        }
+
         @Override
         public String toString() {
-            return (isDone ? "[X] " : "[ ] ") + desc;
+            return getType() + (isDone ? "[X] " : "[ ] ") + desc;
+        }
+    }
+
+    static class ToDo extends Task {
+        ToDo(String desc) {
+            super(desc);
+        }
+
+        @Override
+        String getType() {
+            return "[T]";
+        }
+    }
+
+    static class Deadline extends Task {
+        String by;
+
+        Deadline(String desc, String by) {
+            super(desc);
+            this.by = by;
+        }
+
+        @Override
+        String getType() {
+            return "[D]";
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    static class Event extends Task {
+        String from;
+        String to;
+
+        Event(String desc, String from, String to) {
+            super(desc);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        String getType() {
+            return "[E]";
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " (from: " + from + " to: " + to + ")";
         }
     }
 
@@ -42,10 +97,10 @@ public class Will {
             System.out.println("_____________________________________");
             System.out.println("Nice! I've marked this task as done:");
             System.out.println("  " + tasks.get(index - 1));
-            System.out.println("_____________________________________");
         } else {
             System.out.println("No Task Found");
         }
+        System.out.println("_____________________________________");
     }
 
     static void unmarkTask(ArrayList<Task> tasks, int index) {
@@ -54,10 +109,37 @@ public class Will {
             System.out.println("_____________________________________");
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println("  " + tasks.get(index - 1));
-            System.out.println("_____________________________________");
         } else {
             System.out.println("No Task Found");
         }
+        System.out.println("_____________________________________");
+    }
+
+    static void todoTask(ArrayList<Task> tasks, String desc){
+        tasks.add(new ToDo(desc));
+        System.out.println("_____________________________________");
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + tasks.get(tasks.size() - 1));
+        System.out.println("  Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("_____________________________________");
+    }
+
+    static void deadTask(ArrayList<Task> tasks, String desc, String by){
+        tasks.add(new Deadline(desc, by));
+        System.out.println("_____________________________________");
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + tasks.get(tasks.size() - 1));
+        System.out.println("  Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("_____________________________________");
+    }
+
+    static void eventTask(ArrayList<Task> tasks, String desc, String from, String to){
+        tasks.add(new Event(desc, from, to));
+        System.out.println("_____________________________________");
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + tasks.get(tasks.size() - 1));
+        System.out.println("  Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("_____________________________________");
     }
 
     public static void main(String[] args) {
@@ -65,39 +147,62 @@ public class Will {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
         String logo = "WILL";
-        System.out.println("Hello! I'm" + logo);
+        System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
         System.out.println("_____________________________________");
 
-        while(true) {
+        while (true) {
             String userInput = scanner.nextLine();
             System.out.println("_____________________________________");
 
-            if(Objects.equals(userInput, "bye")){
+            if (Objects.equals(userInput, "bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
-            if(Objects.equals(userInput, "list")){
+            if (Objects.equals(userInput, "list")) {
                 printTask(tasks);
                 continue;
             }
 
-            if(userInput.startsWith("mark ")){
+            if (userInput.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(userInput.substring(5).trim());
                 markTask(tasks, taskNumber);
                 continue;
             }
 
-            if(userInput.startsWith("unmark ")){
+            if (userInput.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(userInput.substring(7).trim());
                 unmarkTask(tasks, taskNumber);
                 continue;
             }
+
+            if (userInput.startsWith("todo ")) {
+                String desc = userInput.substring(5).trim();
+                todoTask(tasks, desc);
+                continue;
+            }
+
+            if (userInput.startsWith("deadline ")) {
+                String[] parts = userInput.substring(9).split(" /by ");
+                String desc = parts[0].trim();
+                String by = parts[1].trim();
+                deadTask(tasks, desc, by);
+                continue;
+            }
+
+            if (userInput.startsWith("event ")) {
+                String[] parts = userInput.substring(6).split(" /from | /to ");
+                String desc = parts[0].trim();
+                String from = parts[1].trim();
+                String to = parts[2].trim();
+                eventTask(tasks, desc, from, to);
+                continue;
+            }
+
             tasks.add(new Task(userInput));
             System.out.println("added: " + userInput);
             System.out.println("_____________________________________");
         }
     }
 }
-
