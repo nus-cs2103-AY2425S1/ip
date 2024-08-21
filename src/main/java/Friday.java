31be709406e808dc659371752ab2652a9580986a
mingyang <limingyanglmy10@gmail.com>
2024-08-21 11:10:01 +0800

    Added list for Friday.java

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -5,6 +7,7 @@ public class Friday {
     public static void main(String[] args) {
         Scanner scannerObj = new Scanner(System.in);
         boolean endScanner = false;
+        List<String> toDoList = new ArrayList<>();
         System.out.println("___________________________________________________________");
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
@@ -13,13 +16,24 @@ public class Friday {
             String toDo = scannerObj.nextLine();
             if (Objects.equals(toDo, "bye")) {
                 endScanner = true;
-            } else {
-                System.out.println("    ___________________________________________________________");
-                System.out.println("    " + toDo);
-                System.out.println("    ___________________________________________________________");
+            } else if(Objects.equals(toDo, "list")) {
+                System.out.println("    _______________________________________________________");
+                for (int i = 0; i < toDoList.size(); i++) {
+                    String returnString = "    " + (i + 1) + ". " + toDoList.get(i);
+                    System.out.println(returnString);
+                }
+                System.out.println("    _______________________________________________________");
+            }
+            else {
+                String returnString = "added: " + toDo;
+                toDoList.add(toDo);
+                System.out.println("    _______________________________________________________");
+                System.out.println("    " + returnString);
+                System.out.println("    _______________________________________________________");
 
             }
         }
+        System.out.println("___________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("___________________________________________________________");
     }
