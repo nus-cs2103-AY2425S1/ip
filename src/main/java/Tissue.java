c0c4dd9c4a2ba7a3648dd0f19b60a20144f954fd
currynia <teewenrui31@gmail.com>
2024-08-21 17:25:07 +0800

    Implement Add and List

--- src/main/java/Tissue.java
+++ src/main/java/Tissue.java
@@ -1,31 +1,58 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Tissue {
+    private static ArrayList<String> textArray = new ArrayList<>();
+    private static final String LINE =
+            "--------------------------------------------------------------";
+    private static final String INDENT = "       ";
+
     public static void main(String[] args) {
         chatFunction();
     }
 
     private static void chatFunction() {
-        String line = "--------------------------------------------------------------";
-        String indent = "       ";
-        System.out.println(line);
+
+
+
+        System.out.println(LINE);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Tissue");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
+
         String in = scanner.nextLine();
 
         while (!in.equals("bye")) {
-            System.out.println(line);
-            System.out.print(indent);
-            System.out.println(in);
-            System.out.println(line);
+            System.out.println(LINE);
+            if (in.equals("list")) {
+                System.out.println(listText());
+            } else {
+                System.out.println(INDENT + "added: " + in);
+                storeText(in);
+            }
+
+            System.out.println(LINE);
             in = scanner.nextLine();
         }
+
         scanner.close();
-        System.out.println(line);
-        System.out.print(indent);
+
+        System.out.println(LINE);
+        System.out.print(INDENT);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    private static void storeText(String value) {
+        textArray.add(value);
+    }
+
+    private static String listText() {
+        String parsedText = "";
+        for (int i = 0; i < textArray.size(); i++) {
+            parsedText += INDENT + String.valueOf(i + 1) + "." + " " + textArray.get(i) + "\n";
+        }
+        return parsedText;
     }
 }
