0f2c5e25045c647fe5a500e902aa6763bcb56682
Al-ez <meowincat911@gmail.com>
2024-08-21 14:08:00 +0800

    Completed increment Level-0

---
+++ src/main/java/Meow.java
@@ -0,0 +1,12 @@
+public class Meow {
+    private static String name = "Meow";
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm " + name + "\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+    }
+}
