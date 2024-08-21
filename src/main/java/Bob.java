adfefb32e46be91a703bafff2f5ca14c41d16812
jessica2828 <jessica.sylviana.js@gmail.com>
2024-08-21 15:35:58 +0800

    Fix bug (tabs in messages) to allow testing to work

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -7,9 +7,9 @@ public class Bob {
         int taskCounter = 0;
         Task[] tasks = new Task[100];
 
-        System.out.println("\t------------------------------------------");
-        System.out.println("\tHello! I'm Bob!\n\tHow can I help you today?");
-        System.out.println("\t------------------------------------------");
+        System.out.println("------------------------------------------");
+        System.out.println("Hello! I'm Bob!\nHow can I help you today?");
+        System.out.println("------------------------------------------");
 
         while (!input.equals("bye")) {
             input = scanner.nextLine().trim(); // trim to remove any whitespace before input
@@ -20,25 +20,25 @@ public class Bob {
             switch (command) {
                 case "list":
                     // print tasks
-                    System.out.println("\t------------------------------------------");
-                    System.out.println("\tHere are the tasks in your list:");
+                    System.out.println("------------------------------------------");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCounter; i++) {
                         int j = i + 1;
-                        System.out.println("\t" + j + ". " + tasks[i].toString());
+                        System.out.println(j + ". " + tasks[i].toString());
                     }
-                    System.out.println("\t------------------------------------------");
+                    System.out.println("------------------------------------------");
                     break;
 
                 case "mark":
                     int taskIndexMark = Integer.parseInt(taskDescription) - 1;
                     if (taskIndexMark < taskCounter && taskIndexMark >= 0) {
                         tasks[taskIndexMark].mark();
-                        System.out.println("\t------------------------------------------");
-                        System.out.println("\tNice! I've marked this task as done:");
-                        System.out.println("\t\t[" + tasks[taskIndexMark].getStatusIcon() + "] " + tasks[taskIndexMark].getDescription());
-                        System.out.println("\t------------------------------------------");
+                        System.out.println("------------------------------------------");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("[" + tasks[taskIndexMark].getStatusIcon() + "] " + tasks[taskIndexMark].getDescription());
+                        System.out.println("------------------------------------------");
                     } else {
-                        System.out.println("\tInvalid index..");
+                        System.out.println("Invalid index..");
                     }
                     break;
 
@@ -46,52 +46,52 @@ public class Bob {
                     int taskIndexUnmark = Integer.parseInt(taskDescription) - 1;
                     if (taskIndexUnmark < taskCounter && taskIndexUnmark >= 0) {
                         tasks[taskIndexUnmark].unmark();
-                        System.out.println("\t------------------------------------------");
-                        System.out.println("\tOk, I've marked this task as not done yet:");
-                        System.out.println("\t\t[" + tasks[taskIndexUnmark].getStatusIcon() + "] " + tasks[taskIndexUnmark].getDescription());
-                        System.out.println("\t------------------------------------------");
+                        System.out.println("------------------------------------------");
+                        System.out.println("Ok, I've marked this task as not done yet:");
+                        System.out.println("[" + tasks[taskIndexUnmark].getStatusIcon() + "] " + tasks[taskIndexUnmark].getDescription());
+                        System.out.println("------------------------------------------");
                     } else {
-                        System.out.println("\tInvalid index..");
+                        System.out.println("Invalid index..");
                     }
                     break;
 
                 case "todo":
                     tasks[taskCounter] = new Todo(taskDescription);
                     taskCounter++;
-                    System.out.println("\t------------------------------------------");
-                    System.out.println("\tGot it. I've added this task:");
-                    System.out.println("\t\t" + tasks[taskCounter - 1].toString());
-                    System.out.println("\tNow you have " + taskCounter + " task(s) in the list.");
-                    System.out.println("\t------------------------------------------");
+                    System.out.println("------------------------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks[taskCounter - 1].toString());
+                    System.out.println("Now you have " + taskCounter + " task(s) in the list.");
+                    System.out.println("------------------------------------------");
                     break;
 
                 case "deadline":
                     String[] dlParts = taskDescription.split(" /by ");
                     tasks[taskCounter] = new Deadline(dlParts[0], dlParts[1]);
                     taskCounter++;
-                    System.out.println("\t------------------------------------------");
-                    System.out.println("\tGot it. I've added this task:");
-                    System.out.println("\t\t" + tasks[taskCounter - 1].toString());
-                    System.out.println("\tNow you have " + taskCounter + " task(s) in the list.");
-                    System.out.println("\t------------------------------------------");
+                    System.out.println("------------------------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks[taskCounter - 1].toString());
+                    System.out.println("Now you have " + taskCounter + " task(s) in the list.");
+                    System.out.println("------------------------------------------");
                     break;
 
                 case "event":
                     String[] eventParts = taskDescription.split(" /from | /to ");
                     tasks[taskCounter] = new Event(eventParts[0], eventParts[1], eventParts[2]);
                     taskCounter++;
-                    System.out.println("\t------------------------------------------");
-                    System.out.println("\tGot it. I've added this task:");
-                    System.out.println("\t\t" + tasks[taskCounter - 1].toString());
-                    System.out.println("\tNow you have " + taskCounter + " task(s) in the list.");
-                    System.out.println("\t------------------------------------------");
+                    System.out.println("------------------------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks[taskCounter - 1].toString());
+                    System.out.println("Now you have " + taskCounter + " task(s) in the list.");
+                    System.out.println("------------------------------------------");
                     break;
 
                 case "bye":
                     // exit
-                    System.out.println("\t------------------------------------------");
-                    System.out.println("\tBye, see you again :)");
-                    System.out.println("\t------------------------------------------");
+                    System.out.println("------------------------------------------");
+                    System.out.println("Bye, see you again :)");
+                    System.out.println("------------------------------------------");
                     scanner.close();
                     return;
 
@@ -100,9 +100,9 @@ public class Bob {
                     tasks[taskCounter] = new Task(input);
                     taskCounter++;
 
-                    System.out.println("\t------------------------------------------");
-                    System.out.println("\tadded: " + input);
-                    System.out.println("\t------------------------------------------");
+                    System.out.println("------------------------------------------");
+                    System.out.println("added: " + input);
+                    System.out.println("------------------------------------------");
                     break;
             }
         }
