c15a000176260378554856a49c10bb028221b300
Leslie Yip <leslieyip02@gmail.com>
2024-08-21 12:54:30 +0800

    Rename chatbot

---
+++ src/main/java/Alice.java
@@ -0,0 +1,12 @@
+public class Alice {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Alice.");
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
