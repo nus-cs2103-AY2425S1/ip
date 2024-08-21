69b3947d13e33f0e32312c3b8fc0cbe00dec54e4
kuiktzecheng <kuiktzecheng17@gmail.com>
2024-08-21 16:50:47 +0100

    Added Echo Feature

--- src/main/java/Gary.java
+++ src/main/java/Gary.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
 public class Gary {
     public static void main(String[] args) {
+        Scanner detector = new Scanner(System.in);
         String greeting = """
                 ────────────────────────────────────────────
                  Hello! I'm Gary
-                 What can I do for you?      
-                          
-                ────────────────────────────────────────────
-                 Bye. Hope to see you again soon!
-                 
-                ────────────────────────────────────────────
+                 What can I do for you?     
+                ──────────────────────────────────────────── 
                 """;
-
         System.out.println(greeting);
+
+        while (true) {
+            String userInput = detector.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("\t" + userInput);
+        }
+        detector.close();
     }
 }
