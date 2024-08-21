067238bad263aa6e5c4f57cbb11161150a65f630
currynia <teewenrui31@gmail.com>
2024-08-21 16:28:40 +0800

    Add initial skeletal version

---
+++ src/main/java/Tissue.java
@@ -0,0 +1,9 @@
+public class Tissue {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Tissue\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+
+    }
+}
