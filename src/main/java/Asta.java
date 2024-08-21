40c066157a38d4220064b4f695326f1b82a06940
Yang Zhi <yangzhi.see@gmail.com>
2024-08-21 23:49:30 +0800

    feat: echo

--- src/main/java/Asta.java
+++ src/main/java/Asta.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Asta {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Asta\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Asta\n" + "What can I do for you?\n");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
