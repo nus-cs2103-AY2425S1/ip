8dd30e8d50b3688b3beda3ad4fe9218b3b254901
ywllowsensor <isyazani@gmail.com>
2024-08-21 17:32:44 +0800

    Added a footnote of list of errors to handle

--- src/main/java/PrinceException.java
+++ src/main/java/PrinceException.java
@@ -2,9 +2,20 @@ public class PrinceException extends Exception {
     public PrinceException(String msg) {
         super(msg);
     }
-    
+
     @Override
     public String toString() {
         return getMessage();
     }
 }
+
+/*
+ * ERRORS TO HANDLE
+ * 
+ * todo/deadline/event with spaces before or after the words
+ * incorrect formatting of deadline/event
+ * words such as unmarking/marking on its own should throw errors
+ * "todo unmarking a player" should be added as a legit event
+ * case sensitivity
+ * punctuations
+ */
