109077fb7d7aecf4b86a24232ed2db50e23ba3aa
wenjebs <wenjieb2@gmail.com>
2024-08-22 00:58:59 +0800

    Rename bot, add greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# WenJigglyBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/WenJigglyBot.java
@@ -0,0 +1,8 @@
+public class WenJigglyBot {
+    public static void main(String[] args) {
+        String name = "WenJigglyBot";
+        System.out.println("Sup im " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Goodbye!");
+    }
+}
