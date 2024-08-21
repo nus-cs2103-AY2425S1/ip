ef4e50fd9e6d381a8a727c23e4c863d2dc121b37
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-08-21 15:29:02 +0800

    Add echo functionality

--- src/main/java/Neuro.java
+++ src/main/java/Neuro.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Neuro {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("___________________________________________________");
         System.out.println("Hi, I'm Neuro, your chatbot assistant!");
         System.out.println("How can I help you?");
+        System.out.println("___________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println("___________________________________________________");
+                System.out.println(input);
+                System.out.println("___________________________________________________");
+            }
+        }
+
         System.out.println("___________________________________________________");
         System.out.println("Goodbye!");
         System.out.println("___________________________________________________");
