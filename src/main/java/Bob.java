7bc89adc1c2db742d09e8256930613bc324e57d8
ITZXITZX <isaacteozixian@hotmail.com>
2024-08-21 23:26:42 +0800

    Renamed Duke to Bob and changed print output.

---
+++ src/main/java/Bob.java
@@ -0,0 +1,11 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Bob\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
