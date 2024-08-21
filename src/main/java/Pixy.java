3e7946fbf321daa8a4cdb4e604881b19dbc6d755
ritvi12 <ritvi0205@gmail.com>
2024-08-22 00:13:49 +0800

    Level-2: Add, List

--- src/main/java/Pixy.java
+++ src/main/java/Pixy.java
@@ -1,23 +1,49 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Pixy {
 
-    public static String inputCommand() {
+    private String[] list = new String[100];
+    private int numOfItems;
+    private String inputCommand() {
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
         return sc.next();
+    }
+    private void addToList(String item) {
+        list[numOfItems++] = item;
+    }
+    private void printList() {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == null) {
+                break;
+            }
+            System.out.println((i + 1) + ". " + list[i]);
+        }
     }
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Pixy. \n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
-        String command = inputCommand();
-        while (!command.equalsIgnoreCase("Bye")) {
-            System.out.println("____________________________________________________________\n" + command + "\n" +
-                    "____________________________________________________________\n");
-            command = inputCommand();
+        Pixy pixy = new Pixy();
+        while (true) {
+            String item = pixy.inputCommand();
+            System.out.println("____________________________________________________________\n");
+            if (item.equalsIgnoreCase("list")) {
+                pixy.printList();
+                System.out.println("____________________________________________________________\n");
+                continue;
+            }
+            if (item.equalsIgnoreCase("Bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            }
+            pixy.addToList(item);
+            System.out.println("added: " +
+                    item + "\n____________________________________________________________\n");
         }
-        System.out.println("____________________________________________________________\n" + "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+
     }
 }
