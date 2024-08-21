8d3491aab176cf3afe0ae9468fb685f0adc90077
IanCheah <iancheah24@gmail.com>
2024-08-21 14:32:14 +0800

    validation of user input

--- src/main/java/Hyperion.java
+++ src/main/java/Hyperion.java
@@ -24,34 +24,39 @@ public class Hyperion {
                 }
                 System.out.print(solidLine);
             } else if (input.startsWith("mark") || input.startsWith("unmark")) {
-                String[] stringsInInput = input.split(" ");
-                String command = " ";
-                int index = -1;
-                for (int i = 0; i < stringsInInput.length; i++) {
-                    if (i == 0) {
-                        command = stringsInInput[i];
+                try {
+                    String[] stringsInInput = input.split(" ");
+                    String command = " ";
+                    int index = -1;
+                    for (int i = 0; i < stringsInInput.length; i++) {
+                        if (i == 0) {
+                            command = stringsInInput[i];
+                        }
+                        if (i == 1) {
+                            index = Integer.parseInt(stringsInInput[i]) - 1;
+                        }
                     }
-                    if (i == 1) {
-                        index = Integer.parseInt(stringsInInput[i]) - 1;
+                    if (command.equals("mark")) {
+                        if (index >= 0 && index < count) {
+                            allUserInputs[index].markAsDone();
+                            System.out.println(
+                                    "Nice! I've marked this task as done:\n " +
+                                            allUserInputs[index].toString());
+                        }
                     }
+                    if (command.equals("unmark")) {
+                        if (index >= 0 && index < count) {
+                            allUserInputs[index].markAsNotDone();
+                            System.out.println(
+                                    "OK, I've marked this task as not done yet:\n " +
+                                            allUserInputs[index].toString());
+                        }
+                    }
+                    System.out.print(solidLine);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid Input");
+                    System.out.print(solidLine);
                 }
-                if (command.equals("mark")) {
-                    if (index >= 0 && index < count) {
-                        allUserInputs[index].markAsDone();
-                        System.out.println(
-                            "Nice! I've marked this task as done:\n " +
-                            allUserInputs[index].toString());
-                    }
-                }
-                if (command.equals("unmark")) {
-                    if (index >= 0 && index < count) {
-                        allUserInputs[index].markAsNotDone();
-                        System.out.println(
-                            "OK, I've marked this task as not done yet:\n " +
-                            allUserInputs[index].toString());
-                    }
-                }
-                System.out.print(solidLine);
             } else {
                 if (!input.equals("bye")) {
                     System.out.println("added: " + input);
