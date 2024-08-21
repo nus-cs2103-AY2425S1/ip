fd24039b6f6cdc59d64ab68f5513840a281354e8
sarahteonin <E1249329@u.nus.edu>
2024-08-22 00:07:10 +0800

    level 0 completed

---
+++ src/main/java/Bob.java
@@ -0,0 +1,8 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo = "Bob";
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("\nBye. Hope to see you again soon!");
+    }
+}
