5199fbbd03c2cdc416db43de60d3f20ff81dec8f
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-08-22 02:24:34 +0800

    Add echo functionality

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,17 +1,35 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Luke {
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (Objects.equals(input, "bye")) {
+            System.out.println("____________________________________________________________\n"
+                    + "yeah bye bye to you too human being\n"
+                    + "____________________________________________________________\n");
+        } else {
+            System.out.println("____________________________________________________________\n"
+                    + input + "\n"
+                    + "____________________________________________________________\n");
+            echo();
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = ".____           __           \n"
+                + "|    |    __ __|  | __ ____  \n"
+                + "|    |   |  |  \\  |/ // __ \\ \n"
+                + "|    |___|  |  /    <\\  ___/ \n"
+                + "|_______ \\____/|__|_ \\\\___  >\n"
+                + "        \\/          \\/    \\/";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm [YOUR CHATBOT NAME]\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println("____________________________________________________________\n"
+                + "i'm luke, your chatbot.\n"
+                + "say something and i will echo it (unless...you wanna say bye to me)\n"
+                + "____________________________________________________________\n");
+        echo();
     }
 }
