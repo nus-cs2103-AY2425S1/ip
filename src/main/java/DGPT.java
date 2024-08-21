36d201c66326da429735759f9a0f1d0aeaf266bb
daronoh <142883378+daronoh@users.noreply.github.com>
2024-08-21 15:23:08 +0800

    Renamed the chatbot, as well as greet and exit feature

---
+++ src/main/java/DGPT.java
@@ -0,0 +1,10 @@
+public class DGPT {
+    public static void main(String[] args) {
+        System.out.println("-----------------------");
+        System.out.println("Hello! I'm DGPT");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-----------------------");
+    }
+}
