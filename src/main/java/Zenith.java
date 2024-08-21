1e3cba53af6fd96e87f4b9155036bdcd8b093ffa
Arnaxx54 <arnavgoel182@gmail.com>
2024-08-22 05:38:03 +0800

    Update ability to echo user response if not bye

--- src/main/java/Zenith.java
+++ src/main/java/Zenith.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 public class Zenith {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(Message.getGreeting());
-        System.out.println();
-        System.out.println(Message.getExit());
+
+        while (true) {
+
+            String input = scanner.nextLine();
+            switch (input) {
+                case "bye":
+                    System.out.println(Message.getExit());
+                    break;
+                default:
+                    System.out.println(input + "\n");
+                    break;
+            }
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+        }
     }
 }
