38d0654f9d6fa736bd30bd75f69fd037698aa8c5
uniqly <simontong09@gmail.com>
2024-08-21 15:47:20 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        // formats task as "[statusIcon] description"
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+}
