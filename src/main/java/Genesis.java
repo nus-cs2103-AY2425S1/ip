78385e41e27553d229ae32ee8743c8fb1dee05bd
ethan-goh <ethan.goh.ye@gmail.com>
2024-08-22 02:03:35 +0800

    level 1

--- src/main/java/Genesis.java
+++ src/main/java/Genesis.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Genesis {
     public static void main(String[] args) {
 
@@ -7,7 +8,17 @@ public class Genesis {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";*/
         System.out.println("Hello! I'm Genesis!\n"
-                + "What can I do for you?\n"
-                + "Bye. Hope to see you again soon!");
+                + "What can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
     }
 }
