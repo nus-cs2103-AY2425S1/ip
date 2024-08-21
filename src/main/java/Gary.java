00ce913029a9c981771721333f0582983f77e220
kuiktzecheng <kuiktzecheng17@gmail.com>
2024-08-21 16:12:49 +0100

    Add Gary.java

---
+++ src/main/java/Gary.java
@@ -0,0 +1,16 @@
+public class Gary {
+    public static void main(String[] args) {
+        String greeting = """
+                ────────────────────────────────────────────
+                 Hello! I'm Gary
+                 What can I do for you?      
+                          
+                ────────────────────────────────────────────
+                 Bye. Hope to see you again soon!
+                 
+                ────────────────────────────────────────────
+                """;
+
+        System.out.println(greeting);
+    }
+}
