44425c29db29e84652d3408b6cc41e70cbde5566
Chen Zi Yi <ziyichen2003@gmail.com>
2024-08-21 20:33:56 +0800

    Add echo function

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         String name = "Bob";
         System.out.println("Hello! I'm " + name + "!\nWhat can I do for you?");
-        System.out.println("Bye! Hope to see you again :)");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            String endKeyword = "bye";
+
+            if (userInput.equalsIgnoreCase(endKeyword)) {
+                System.out.println("Bye! Hope to see you again :)");
+                break;
+            }
+
+            // Echo response
+            System.out.println(userInput);
+        }
+
+        scanner.close();
     }
 }
