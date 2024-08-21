e1b25f11dd8932851e09f508fa4dfebc9276fa01
SViswa02 <sviswa2002@gmail.com>
2024-08-21 16:14:17 +0800

    changed DUKE to BitBot

--- src/main/java/BitBot.java
+++ src/main/java/BitBot.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class BitBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String intro = " ____________________________________ \n Hello! I'm BitBot \n What can I do for you? \n ____________________________________";
+        String conclusion = " \n Bye. Hope to see you again soon! \n ____________________________________ \n ";
+        String initialSkeletal = intro + conclusion;
+        System.out.println(initialSkeletal);
     }
 }
