264cc890cd7aac5f15e304eaf94b8a8865a23d18
maahir-garg <>
2024-08-21 18:44:54 +0800

    Level-1: Echo

--- src/main/java/TheBotFather.java
+++ src/main/java/TheBotFather.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class TheBotFather {
 
     public static void main(String[] args) {
@@ -5,22 +8,38 @@ public class TheBotFather {
         printLine();
         printGreeting();
         printLine();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            printLine();
+            if (Objects.equals(input, "bye")) break;
+            else print(input);
+            printLine();
+        }
+
         printGoodBye();
         printLine();
 
 
+
+    }
+
+    public static void print(String str) {
+        System.out.println("    " + str);
     }
 
     public static void printGreeting() {
-        System.out.println("Hello! I'm The BotFather");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm The BotFather");
+        System.out.println("    What can I do for you?");
     }
 
     public static void printGoodBye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    Bye. Hope to see you again soon!");
     }
 
     public static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 }
