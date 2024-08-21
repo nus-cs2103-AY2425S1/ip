eff2369152d0b41d6f3ce471db6921a2b7f28805
M-kew <e0958776@u.nus.edu>
2024-08-21 22:22:05 +0800

    Implemented list feature to the chatbot.

--- src/main/java/Nuffle.java
+++ src/main/java/Nuffle.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Nuffle {
+
+    private static ArrayList<String> inputList = new ArrayList<>();
 
     private static void printLine() {
         // This method will be used to print the border
         System.out.println("╔═══════════════════════════════════════════════════════════╗");
     }
+
+    private static void outputList() {
+        // If there is no user input in the list
+        if (inputList.isEmpty()) {
+            System.out.println("List is empty. No input added.");
+        } else {
+            // Iterate over the list to print out all the user inputs
+            for (int index = 0; index < inputList.size(); index++) {
+                System.out.println("" + (index + 1) + ". " + inputList.get(index));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // This will be starting point of the application
 
@@ -32,10 +48,18 @@ public class Nuffle {
                 System.out.println("Nuffle > Bye. Hope to see you again soon!");
                 printLine();
                 break;
-            } else {
+            } else if (userInput.equals("list")) {
+                // Program will list all the tasks that was input by the user
+                printLine();
+                outputList();
+                printLine();
+            }
+            else {
                 // Echo the user command
                 printLine();
-                System.out.println("Nuffle > " + userInput);
+                System.out.println("Nuffle > added: " + userInput);
+                // Add the user input into the list
+                inputList.add(userInput);
                 printLine();
             }
         }
