76463d3c0e851f697d04c30bed632d6f0eba2708
NicholasCWK <e1114227@u.nus.edu>
2024-08-22 00:37:39 +0800

    Add greeting and farewell

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,5 +1,15 @@
 public class Echo {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String name = "Echo";
+        String greeting = "Hello! I'm " + name + "\n" +
+                "What can I do for you?";
+        String farewell = " Bye. Hope to see you again soon!";
 
+        System.out.println(line);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(farewell);
+        System.out.println(line);
     }
 }
