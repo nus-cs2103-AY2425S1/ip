ff6e3c7b3a81291ecafca48d702f09183dc84bff
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-08-21 15:18:06 +0800

    Changed name to Sunny, added in welcome and goodbye message

---
+++ src/main/java/Sunny.java
@@ -0,0 +1,11 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo = "  _____   _    _  _   _  _   _  __     __  \n" +
+                " / ____| | |  | || \\ | || \\ | | \\ \\   / /  \n" +
+                "| (___   | |  | ||  \\| ||  \\| |  \\ \\_/ /   \n" +
+                " \\___ \\  | |  | || . ` || . ` |   \\   /    \n" +
+                " ____) | | |__| || |\\  || |\\  |    | |     \n" +
+                "|_____/   \\____/ |_| \\_||_| \\_|    |_|     \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
