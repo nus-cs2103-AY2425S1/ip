856b9efd497239d399248b1d86f0d092a6ac8100
ckaayy <e1122115@u.nus.edu>
2024-08-21 17:44:53 +0800

    Mark as Done

--- src/main/java/YapperBot.java
+++ src/main/java/YapperBot.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class YapperBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] inputHistory = new String[100];
+        Task[] tasks = new Task[100];
         int numTask = 0;
 
         System.out.println("________________________________");
@@ -18,12 +18,27 @@ public class YapperBot {
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < numTask; i++) {
-                    System.out.println((i + 1) + ". " + inputHistory[i]);
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("________________________________");
+            } else if (userInput.startsWith("mark")) {
+                int targetTask = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                tasks[targetTask].mark();
+                System.out.println("________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[targetTask]);
+                System.out.println("________________________________");
+            } else if (userInput.startsWith("unmark")) {
+                int targetTask = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                tasks[targetTask].unmark();
+                System.out.println("________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[targetTask]);
+                System.out.println("________________________________");
             } else {
-                inputHistory[numTask] = userInput;
+                tasks[numTask] = new Task(userInput);
                 numTask++;
                 System.out.println("________________________________");
                 System.out.println("added: " + userInput);
