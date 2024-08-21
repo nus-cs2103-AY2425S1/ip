beb5999ce07043ad0179537c43cd604d2bbdbaa5
pradyuprasad <pradyu.sp@gmail.com>
2024-08-22 07:36:35 +0800

    Completed Level 0. Rename, Greet, Exit

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,10 +1,11 @@
 public class Orion {
+    static String  horizontalLine =  "────────────────────────────────────────";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Orion");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
