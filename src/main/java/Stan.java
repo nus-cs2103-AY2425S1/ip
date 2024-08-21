3080fa284964db4a4a9d9772c6ea48527ce7e857
stanleytangzh <111631172+stanleytangzh@users.noreply.github.com>
2024-08-21 10:46:53 +0800

    implemented an initial skeletal version that simply greets and user and exits.

---
+++ src/main/java/Stan.java
@@ -0,0 +1,11 @@
+public class Stan {
+    public static void main(String[] args) {
+        String logo = "Stan";
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + logo);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
