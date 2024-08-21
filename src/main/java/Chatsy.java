71506570258fd35ff035f83ed94df7e79f199c06
Kelly Wong <justwjyk@gmail.com>
2024-08-21 21:29:13 +0800

    Level-1:Echo

--- src/main/java/Chatsy.java
+++ src/main/java/Chatsy.java
@@ -1,13 +1,40 @@
-public class Chatsy {
-    static final String line = "\t____________________________________________________________";
-    public static void main(String[] args) {
-        String greetingMessage = "\tHello! I'm Chatsy\n\tWhat can I do for you?";
-        String exitMessage = "\tBye. Hope to see you again soon!";
+import java.util.Scanner;
 
+public class Chatsy {
+    static final String name = "Chatsy";
+    static final String line = "\t____________________________________________________________";
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void greet() {
         System.out.println(line);
-        System.out.println(greetingMessage);
+        System.out.println("\tHello! I'm Chatsy\n\tWhat can I do for you?");
         System.out.println(line);
-        System.out.println(exitMessage);
+    }
+
+    public static void output(String output) {
         System.out.println(line);
+        System.out.println(output);
+        System.out.println(line);
+    }
+
+    public static void exit() {
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+
+    public static void nextCommand() {
+        String command = scanner.nextLine();
+        if (command.equals("bye")) {
+            exit();
+        } else {
+            output("\t" + command);
+            nextCommand();
+        }
+    }
+
+    public static void main(String[] args) {
+        greet();
+        nextCommand();
+
     }
 }
