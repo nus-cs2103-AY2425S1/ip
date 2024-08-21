3fc16dc8082d3d7c6f435b7526b6afd53c997d14
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-08-21 17:01:59 +0800

    final fix for Level-0

--- src/main/java/Sunny.java
+++ src/main/java/Sunny.java
@@ -1,11 +1,20 @@
 public class Sunny {
+    public static final String logo = "       _____   _    _  _   _  _   _  __     __  \n" +
+            "      / ____| | |  | || \\ | || \\ | | \\ \\   / /  \n" +
+            "     | (___   | |  | ||  \\| ||  \\| |  \\ \\_/ /   \n" +
+            "      \\___ \\  | |  | || . ` || . ` |   \\   /    \n" +
+            "      ____) | | |__| || |\\  || |\\  |    | |     \n" +
+            "     |_____/   \\____/ |_| \\_||_| \\_|    |_|     \n";
+
+    public static final String line = "     ────────────────────";
+    public static final String welcome = "     HELLO! I am Sunny:)\n     How can I help you?";
+    public static final String goodbye = "     You are leaving? Ok bye:( come back soon";
     public static void main(String[] args) {
-        String logo = "  _____   _    _  _   _  _   _  __     __  \n" +
-                " / ____| | |  | || \\ | || \\ | | \\ \\   / /  \n" +
-                "| (___   | |  | ||  \\| ||  \\| |  \\ \\_/ /   \n" +
-                " \\___ \\  | |  | || . ` || . ` |   \\   /    \n" +
-                " ____) | | |__| || |\\  || |\\  |    | |     \n" +
-                "|_____/   \\____/ |_| \\_||_| \\_|    |_|     \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(welcome);
+        System.out.println(line);
+        System.out.println(line);
+        System.out.println(goodbye);
     }
 }
