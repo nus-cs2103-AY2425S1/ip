72dae6bc819cc238ea27681018716b73c35ba5d2
darkmoongreatsword <chinkuanjak02@gmail.com>
2024-08-22 01:53:46 +0800

    -Changed Duke to Lewis

---
+++ src/main/java/Lewis.java
@@ -0,0 +1,11 @@
+public class Lewis {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        text.append("---------------------------------------\n");
+        text.append("Hello! I'm Lewis.\nWhat can I do for you?\n");
+        text.append("---------------------------------------\n");
+        text.append("Bye. Hope to see you again soon!\n");
+        text.append("---------------------------------------\n");
+        System.out.println(text);
+    }
+}
