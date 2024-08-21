ae56d45a60c3de02a442427cfe8c560261db815c
leroychiu20 <e1122486@u.nus.edu>
2024-08-21 20:59:53 +0800

    rename greet and exit

---
+++ src/main/java/Alex.java
@@ -0,0 +1,16 @@
+public class Alex {
+    public static void main(String[] args) {
+
+        String byeMessage = "Bye. Hope to see you again soon!";
+        String line = "----------------------------------------------------";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm Alex \n" +
+                "What can I do for you? ");
+
+        System.out.println(line);
+        System.out.println(byeMessage);
+        System.out.println(line);
+
+    }
+}
