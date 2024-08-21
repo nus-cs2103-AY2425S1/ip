ab277fa946c3e6c71400a06ddb6788609cb719d6
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-22 05:40:55 +0800

    Rainy.java: Change name Add automatic messages

---
+++ src/main/java/Rainy.java
@@ -0,0 +1,11 @@
+public class Rainy {
+    private static String name = "Rainy!!!";
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I am RAINY - Responsive, Automated, Intelligence Network for You.");
+        System.out.println("What can I do for you on this marvelous day?");
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("Goodbye! Have a nice day ahead!!");
+    }
+}
