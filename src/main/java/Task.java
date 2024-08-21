97e077042e2869e00f90d3a684e9680c5686e4df
arnav-goel10 <e1130141@u.nus.edu>
2024-08-21 20:57:39 +0800

    Add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+}
