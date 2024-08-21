7881ad34cf6db53ea6e0bdb33caba814b67c250b
Ansel-Ch <achong2014@gmail.com>
2024-08-21 14:29:01 +0800

    Add greeting

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,10 +1,10 @@
 public class Joe {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        System.out.println(line +
+                "\nHello! I'm Joe\n" +
+                "What can I do for you?\n"+
+                line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
