0e0786ba061759376424427636b0c9aef6aedb01
waihin26 <138366109+waihin26@users.noreply.github.com>
2024-08-22 02:17:15 +0800

    Initialised skeleton of chatbot Blue

---
+++ src/main/java/Blue.java
@@ -0,0 +1,12 @@
+public class Blue {
+    public static void main(String[] args) {
+
+        System.out.println("--------------------------------------------" );
+        System.out.println("Hello! I'm Blue" );
+        System.out.println("What can I do for you?" );
+        System.out.println("--------------------------------------------" );
+        System.out.println("Bye. Hope to see you again soon!" );
+        System.out.println("--------------------------------------------" );
+        //testing
+    }
+}
