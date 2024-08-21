4843d5ffb1b97e50e1e8cf9f48e9fa5f47a9c7c8
karthickkc <>
2024-08-21 13:38:00 +0800

    did Level-2

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bro {
@@ -6,10 +6,21 @@ public class Bro {
         String line = "______________________________________________________\n";
         System.out.println("   " + line + "   Hello! I'm Bro\n   What can I do for you?\n"
                            + "   " + line);
+        ArrayList<String> list = new ArrayList<>();
         Scanner prompt = new Scanner(System.in);
         String word = prompt.nextLine();
         while (!word.equalsIgnoreCase("bye")) {
-            System.out.println("   " + line + "   " + word + "\n" + "   " + line);
+            if (word.equalsIgnoreCase("list")) {
+                int len = list.size();
+                System.out.print("   " + line);
+                for (int i = 0; i < len; i++) {
+                    System.out.printf("   %d. %s\n", i + 1, list.get(i));
+                }
+                System.out.print("   " + line);
+            } else {
+                list.add(word);
+                System.out.println("   " + line + "   added: " + word + "\n" + "   " + line);
+            }
             Scanner next_prompt = new Scanner(System.in);
             word = next_prompt.nextLine();
         }
