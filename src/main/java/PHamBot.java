25331f86946d273b20f891f8330408664b909bd7
antgyf <anthonygohyf@gmail.com>
2024-08-22 01:58:56 +0800

    Week2 Level-1

--- src/main/java/PHamBot.java
+++ src/main/java/PHamBot.java
@@ -1,15 +1,37 @@
-public class PHamBot {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class PHamBot {
+    private static final String line = "____________________________________________________________\n";
+
+    public static void main(String[] args) {
         Greet();
-        SayGoodbye();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                SayGoodbye();
+                break;
+            }
+
+            input = OutlineMessage(input);
+            System.out.println(input);
+        }
+    }
+
+    private static String OutlineMessage(String msg) {
+        String res= line + msg + "\n" + line;
+        return res;
     }
 
     public static void Greet() {
-        System.out.println("Hi! I'm PHamBot\nHappy to be of service to you today!");
+        String greeting = "Hi! I'm PHamBot\nHappy to be of service to you today!";
+        System.out.println(OutlineMessage(greeting));
     }
 
     public static void SayGoodbye() {
-        System.out.println("Hope I was able to help\nGoodbye!");
+        String goodbye = "Hope I was able to help\nGoodbye!";
+        System.out.println(OutlineMessage(goodbye));
     }
 }
