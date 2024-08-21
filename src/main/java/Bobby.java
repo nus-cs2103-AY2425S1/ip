b315d6e422ad4aa3c53b27199a4a320ebc8b2e32
vinceg4 <vincegoh04@gmail.com>
2024-08-21 12:08:07 +0800

    Add horizontalLine method

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,10 +1,12 @@
 public class Bobby {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        
+    }
+
+    // Make a horizontal line of x dashes
+    public static void horizontalLine(int x) {
+        for (int i = 0; i < x; i++) {
+            System.out.print("-");
+        }
     }
 }
