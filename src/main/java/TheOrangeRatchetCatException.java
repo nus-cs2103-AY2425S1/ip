ccd3c2ed78c378210885d15bdb21687e97687940
OrangeCatLoves <e1121707@u.nus.edu>
2024-08-22 00:32:56 +0800

    A-TextUiTesting v1

---
+++ src/main/java/TheOrangeRatchetCatException.java
@@ -0,0 +1,6 @@
+public class TheOrangeRatchetCatException extends Exception {
+    // Constructor that accepts a message
+    public TheOrangeRatchetCatException(String message) {
+        super(message);
+    }
+}
