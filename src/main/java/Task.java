a32ae38d7ddc087deb8afeaf527ffca26c2f5670
Ansel-Ch <achong2014@gmail.com>
2024-08-21 16:09:22 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+}
