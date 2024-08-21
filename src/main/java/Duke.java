bf13e31d0e24baa0c79c79d7fa325713942a71a6
ethantyh692 <ethantyh692@gmail.com>
2024-08-21 16:50:36 +0800

    Done with Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
 
     private static void greet() {
@@ -10,7 +12,19 @@ public class Duke {
 
     public static void main(String[] args) {
         greet();
+        Scanner sc = new Scanner(System.in);
+        String command;
+        while (sc.hasNext()) {
+            command = sc.next();
+            if (command.equals("list")) {
+                System.out.println("list");
+            } else if (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+        sc.close();
         bye();
     }
 }
- a
