eaa07994f723abbffcb3c49073413659c0becf33
linette-g <156193856+linette-g@users.noreply.github.com>
2024-08-21 14:56:29 +0800

    Add list

--- src/main/java/Rob.java
+++ src/main/java/Rob.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Rob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String greet = "Hello! I'm Rob\n" +
                 "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
@@ -11,16 +13,27 @@ public class Rob {
 
         while (true) {
             String input = scanner.nextLine();
-            // echo
-            String echo = "____________________________________________________________\n" +
-                    input + "\n" +
-                    "____________________________________________________________\n";
-            System.out.println(echo);
 
             // exit
             if (Objects.equals(input, "bye")) {
                 System.out.println(exit);
                 break;
+            } else if (Objects.equals(input, "list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++)
+                {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________\n");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+
+                // echo
+                String echo = "____________________________________________________________\n" +
+                        "added: " + input + "\n" +
+                        "____________________________________________________________\n";
+                System.out.println(echo);
             }
         }
         scanner.close();
