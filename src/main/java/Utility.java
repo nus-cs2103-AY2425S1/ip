18d557ecc8d8582005b309c749351b888e06262d
RuijianLu <e1297772@u.nus.edu>
2024-08-21 21:52:18 +0800

    Enable mark as done and unmark. Also restructured.

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Utility {
 
     public static final String LINE = "____________________________________________________________\n";
-    private static ArrayList<String> toDo = new ArrayList<String>();
+    private static ArrayList<Task> toDo = new ArrayList<>();
 
     public static void greeting() {
         System.out.print(LINE + " Hello! I'm LuToDo \n" +
@@ -16,7 +16,8 @@ public class Utility {
     }
 
     public static void addToDo(String s) {
-        toDo.add(s);
+        Task task = new Task(s);
+        toDo.add(task);
         System.out.println("added: " + s);
     }
 
@@ -25,12 +26,23 @@ public class Utility {
     }
 
     public static void handleMessage(String message) {
-
         if (message.equals("list")) {
-
+            System.out.println("Here are the tasks in your list:");
             for (int i = 1; i <= toDo.size(); i++) {
-                System.out.println(i + ". " + toDo.get(i - 1));
+                System.out.println(i + "." + toDo.get(i - 1));
             }
+            return ;
+        }
+        if (divideMessage(message)[0].equals("mark")) {
+            int taskIndex = divideMessage(message)[1].charAt(0) - 49;
+            toDo.get(taskIndex).markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n" + toDo.get(taskIndex));
+            return ;
+        }
+        if (divideMessage(message)[0].equals("unmark")) {
+            int taskIndex = divideMessage(message)[1].charAt(0) - 49;
+            toDo.get(taskIndex).markAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:\n" + toDo.get(taskIndex));
             return ;
         }
         addToDo(message);
