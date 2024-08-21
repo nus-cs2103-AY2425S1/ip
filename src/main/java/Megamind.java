354714ad4227c1ae2f52fa73010fbca1be4e757a
Otto Tan <OttoTan>
2024-08-21 13:56:40 +0800

    Added functions in Megamind class for skeletal version

---
+++ src/main/java/Megamind.java
@@ -0,0 +1,26 @@
+public class Megamind {
+    private static final String horizontalLine = "____________________________________________________________";
+    public static void main(String[] args) {
+        String logo = "  _____   \n"
+                + " /     \\  \n"
+                + "| () () | \n"
+                + " \\  ^  /  \n"
+                + "  |||||   \n"
+                + "  |||||   ";
+        System.out.println(horizontalLine);
+        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println(horizontalLine);
+        System.out.println("Whats good, I'm Megamind.\nWhat can I do for you? \n");
+    }
+
+    public static void exit() {
+        System.out.println(horizontalLine);
+        System.out.println("See you around! \n");
+        System.out.println(horizontalLine);
+    }
+}
