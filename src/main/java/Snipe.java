660e791cebf7e1e4bc1fd000b106db816cf8ddc1
DKHB0207 <daryl.koh.hb@gmail.com>
2024-08-22 04:09:50 +0800

    add echo and bye commands

--- src/main/java/Snipe.java
+++ src/main/java/Snipe.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Snipe {
     private static final String NAME = "Snipe";
     private static final String LOGO = "  _________      .__               \n"
@@ -9,23 +10,38 @@ public class Snipe {
     private static final String HORIZONTAL_LINE = "_".repeat(60);
     public void initChat() {
         greetUser();
+        handleUserInput();
     }
 
     private void greetUser() {
         String OPENING_MESSAGE = "Hello! I'm\n" + LOGO +"\nWhat can I do for you?";
-        String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(OPENING_MESSAGE);
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(CLOSING_MESSAGE);
-        System.out.println(HORIZONTAL_LINE);
+        printWithLines(OPENING_MESSAGE);
+    }
+    private void handleUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                exitChat(scanner);
+                break;
+            }
+            printWithLines(userInput);
+        }
     }
     private void printWithLines(String message) {
-
         System.out.println(HORIZONTAL_LINE);
+        System.out.println(message);
+        System.out.println(HORIZONTAL_LINE);
+    }
+    private void exitChat(Scanner scanner) {
+        String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
+        printWithLines(CLOSING_MESSAGE);
+        scanner.close();
     }
     public static void main(String[] args) {
         Snipe snipe = new Snipe();
         snipe.initChat();
     }
 }
+
+
