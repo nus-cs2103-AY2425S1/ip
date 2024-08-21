a6f2156612748f114299513fd9afa232b37bb37e
Sean Lim <seanlimdev856@gmail.com>
2024-08-21 23:17:36 +0800

    Add mark/unmark parsing

--- src/main/java/Deez.java
+++ src/main/java/Deez.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.spec.ECField;
 import java.util.ArrayList;
 
 public class Deez {
@@ -11,10 +12,20 @@ public class Deez {
         }
         System.out.println("____________________________________________________________");
     }
+
+    static int parseInt(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String CMD_bye = "bye";
         String CMD_list = "list";
+        String CMD_markDone = "mark";
+        String CMD_unmarkDone = "unmark";
         String logo = """
                     ____  _________________
                    / __ \\/ ____/ ____/__  /
@@ -25,20 +36,22 @@ public class Deez {
                 """;
         System.out.println(logo);
         ArrayList<String> items = new ArrayList<String>();
-        Deez.say("Hello! I'm Deez!", "What can I do you for?");
+        say("Hello! I'm Deez!", "What can I do you for?");
         while (1 == 1) {
-            String str = br.readLine();
-            if (str.isEmpty()) {
+            String inputStr = br.readLine();
+
+            // Simple commands
+            if (inputStr.isEmpty()) {
                 // Don't do anything
                 continue;
             }
-            if (str.equals(CMD_bye)) {
+            if (inputStr.equals(CMD_bye)) {
                 // Exit
                 break;
             }
-            if (str.equals(CMD_list)) {
+            if (inputStr.equals(CMD_list)) {
                 if (items.isEmpty()) {
-                    Deez.say("<No items in list>");
+                    say("<No items in list>");
                     continue;
                 }
                 // Print list
@@ -51,11 +64,35 @@ public class Deez {
                 System.out.println("____________________________________________________________");
                 continue;
             }
+
+            // Commands with input
+            String[] tkns = inputStr.split(" ", 2);
+            if (tkns.length > 1 ) {
+                String cmd = tkns[0];
+                String param = tkns[1];
+
+                if (cmd.equals(CMD_markDone) || cmd.equals(CMD_unmarkDone)) {
+                    int taskIdx = parseInt(param);
+                    if (taskIdx > 0) {
+                        try {
+                            items.get(taskIdx-1);
+                        } catch (Exception e) {
+                            say("No task at index " + param, "Please try again.");
+                        }
+                        continue;
+                    } else {
+                        say("Invalid input!", "Please enter a valid number.");
+                        continue;
+                    }
+                }
+            }
+
             // Add item to items list
-            items.add(str);
-            Deez.say("Added " + str);
+            items.add(inputStr);
+            say("Added " + inputStr);
         }
-        Deez.say("Bye. Hope to see you soon!");
+        say("Bye. Hope to see you soon!");
     }
+
 
 }
