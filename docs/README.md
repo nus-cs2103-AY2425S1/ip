4dcd12abec28af5aa3b6ae29e491ff5a91b18e2c
nigellqx <lownigel79@gmail.com>
2024-08-21 15:18:46 +0800

    Rename the chatbot, implemented greeting of chatbot and also exiting of the chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Speed User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Speed.java
@@ -0,0 +1,9 @@
+public class Speed {
+    public static void main(String[] args) {
+        String name = "Speed";
+
+        System.out.println("Hello! I'm " + name + "\n" +
+                           "What can I do for you?\n" +
+                           "Bye. Hope to see you again soon!");
+    }
+}
