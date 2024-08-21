712f4c6ca36228f526b969fb79b50b5e9f5a9323
Nicholas-Cheng-De-Fei <nicholas_cheng@u.nus.edu>
2024-08-21 10:38:20 +0800

    Add JavaDoc to Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,28 @@
+/**
+* This is an abstract class to define a task object.
+*/
 public abstract class Task {
     
+    /** Description of the task */
     protected String taskName;
+    /** Determine if the task is marked or not */
     protected boolean isChecked;
 
+    /**
+     * Constructor to create a Task object.
+     */
     Task(String taskName) {
         this.taskName = taskName;
         isChecked = false;
     }
 
+    /**
+     * This function updates the task to be marked.
+     * It checks if the task has been marked already.
+     */
     public void mark() {
+
+        // Check if the task is already marked or not
         if (!isChecked) {
             this.isChecked = true;
             System.out.println("Nice! I've marked this task as done:");
@@ -17,10 +31,17 @@ public abstract class Task {
             System.out.println("This task is already marked as done!");
         }
 
+        // Print the task
         System.out.println(this.toString());
     }
 
+    /**
+     * This function updates the task to be unmarked.
+     * It checks if the task has been unmarked already.
+     */
     public void unMark() {
+
+        // Check if the task is already unmarked
         if (isChecked) {
             this.isChecked = false;
             System.out.println("OK, I've marked this task as not done yet:");
@@ -29,6 +50,7 @@ public abstract class Task {
             System.out.println("This task is already unmarked!");
         }
 
+        // Print the task
         System.out.println(this.toString());
     }
 
