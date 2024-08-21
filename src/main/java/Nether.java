31e47eef673bd608dfa37c68bc7a07469e67f2cc
PeterHW963 <peterhadiwijaya963@gmail.com>
2024-08-21 12:24:12 +0800

    Add "Add" and "List" features and refactored code
    Chatbot echoes user input unless the user inputs "bye"
    
    The assignment given is to have the chatbot add whatever the user inputs
    into a list of tasks and to add another check for the input "list", to
    which the chatbot should return a numbered list of all the tasks the
    user has input so far.
    
    Let's create an array of size 100 to store the tasks input by user along
    with a counter to keep track of the current index in the array. When
    the user inputs "list", a for loop is used to iterate through the array
    and print out the entries.
    
    Additionally, an abstraction for printing the horizontal line output is
    made and the constants MAX_TASKS, EXIT_COMMAND, and LIST_COMMAND were
    created to improve reusability and readability.

--- src/main/java/Nether.java
+++ src/main/java/Nether.java
@@ -5,32 +5,65 @@
 import java.util.Scanner;
 
 public class Nether {
+    private static final int MAX_TASKS = 100;
+    private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
+
+    /**
+     * The main method where the program starts.
+     * Initializes the application, takes user input, and responds based on commands.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         String logo = " _   _      _   _        \n"
                 + "| \\ | | ___| |_| |__  ___ _ __ \n"
                 + "|  \\| |/ _ \\ __| '_ \\/ _ \\ '__|\n"
                 + "| |\\  |  __/ |_| | | ||__/ |  \n"
                 + "|_| \\_|\\___|\\__|_| |_\\___|_|\n";
+        String[] tasks = new String[MAX_TASKS];
+        int counter = 0;
+
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+        printHorizontalLine();
         System.out.println("Hello sir! I'm Nether");
         System.out.println("What can I do for you today?");
-        System.out.println("____________________________________________________________\n");
+        printHorizontalLine();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println();
+        System.out.print("");
         while (true) {
             String userInput = scanner.nextLine();
-            if (userInput.equalsIgnoreCase("bye")) {
+            if (userInput.equalsIgnoreCase(EXIT_COMMAND)) {
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+
+            if (userInput.equalsIgnoreCase(LIST_COMMAND)) {
+                for (int i = 0; tasks[i] != null; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                printHorizontalLine();
+                continue;
+            }
+
+            printHorizontalLine();
+            System.out.println("added: " + userInput);
+            printHorizontalLine();
+
+            // update tasks array and increment counter
+            tasks[counter] = userInput;
+            counter++;
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. If you have any more queries in the future, feel free to ask. Enjoy your day!");
+        printHorizontalLine();
+        System.out.println("Bye. If you need any more help in the future, feel free to ask me. Enjoy your day!");
+        printHorizontalLine();
+    }
+
+    /**
+     * Prints out a long horizontal line to act as separator in the chat
+     */
+    public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
 }
