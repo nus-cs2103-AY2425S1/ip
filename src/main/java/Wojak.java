44b7b16c99dfb914f18abbe2f64a9155913075d7
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-08-21 16:57:37 +0000

    feat: Mark as done

--- src/main/java/Wojak.java
+++ src/main/java/Wojak.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Wojak {
 
-    public static void printList(ArrayList<String> ls) {
+    public static void printList(ArrayList<Task> ls) {
         if (!ls.isEmpty()) {
             System.out.println("____________________________________________________________");
             for (int i = 0; i < ls.size(); i++) {
-                System.out.println((i+1) + ". " + ls.get(i));
+                System.out.println(ls.get(i).getTaskString(i+1));
             }
             System.out.println("____________________________________________________________");
         } else {
@@ -20,7 +22,7 @@ public class Wojak {
 
     public static void main(String[] args) {
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Wojak\n" +
@@ -28,13 +30,49 @@ public class Wojak {
                 "____________________________________________________________\n");
         Scanner sc = new Scanner(System.in);
         String nextLine = sc.nextLine();
+
+        String markRegex = "mark (100|[1-9]|[1-9][0-9])";
+        String unmarkRegex = "unmark (100|[1-9]|[1-9][0-9])";
+        Pattern markPattern = Pattern.compile(markRegex);
+        Pattern unmarkPattern = Pattern.compile(unmarkRegex);
+
         while (!(nextLine.equals("bye"))) {
+            Matcher markMatcher = markPattern.matcher(nextLine);
+            Matcher unmarkMatcher = unmarkPattern.matcher(nextLine);
             if (nextLine.equals("list")) {
                 printList(list);
+            }
+            else if (markMatcher.matches()) {
+                String numberString = markMatcher.group(1);
+                int number = Integer.parseInt(numberString);
+                try {
+                    Task retrievedTask = list.get(number-1);
+                    retrievedTask.mark();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(retrievedTask.getTaskString(number));
+                    System.out.println("____________________________________________________________");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Task doesn't exist");
+                }
+            }else if (unmarkMatcher.matches()) {
+                String numberString = unmarkMatcher.group(1);
+                int number = Integer.parseInt(numberString);
+                try {
+                    Task retrievedTask = list.get(number-1);
+                    retrievedTask.unmark();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(retrievedTask.getTaskString(number));
+                    System.out.println("____________________________________________________________");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Task doesn't exist");
+                }
             } else {
-                list.add(nextLine);
+                Task newTask = new Task(nextLine);
+                list.add(newTask);
                 System.out.println("____________________________________________________________\n" +
-                        "added: " + nextLine + "\n" +
+                        "added: " + newTask.getDescription() + "\n" +
                         "____________________________________________________________\n");
             }
 
