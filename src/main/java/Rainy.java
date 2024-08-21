c7c1196dfee403f33766b5435dac14eb664dd620
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-22 06:05:56 +0800

    Rainy.java: Add functionality to add tasks and view tasks

--- src/main/java/Rainy.java
+++ src/main/java/Rainy.java
@@ -12,11 +12,16 @@ public class Rainy {
         System.out.println("Hello! I am RAINY - Responsive, Automated, Intelligence Network for You.");
         System.out.println("What can I do for you on this marvelous day?");
         System.out.println("\n");
+        TaskTracker tm = new TaskTracker();
         Scanner sc = new Scanner(System.in);
         String message = sc.nextLine();
         while(!message.equals("bye")) {
-            System.out.println("added " + message);
-
+            if (message.equals("list")) {
+                tm.getList();
+            } else {
+                System.out.println("added " + message);
+                tm.updateList(message);
+            }
             message = sc.nextLine();
         }
         System.out.println("Goodbye! Have a nice day ahead!!");
