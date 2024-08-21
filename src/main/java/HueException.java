1b96ff8d3c2fb79f36f9e48be871f58357ef157c
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-21 16:51:18 +0800

    Revert "Finish Level1-4"
    This reverts commit f54993478c6347b21242eebffdbb7a318f91a381.

--- src/main/java/HueException.java
+++ src/main/java/HueException.java
@@ -1,10 +1,5 @@
 public class HueException extends Exception {
     public HueException(String message) {
-        super(message);
-    }
-
-    @Override
-    public String getMessage() {
-        return "ERROR!!! " + super.getMessage() + "\n";
+        super("OOPS!!" + message);
     }
 }
