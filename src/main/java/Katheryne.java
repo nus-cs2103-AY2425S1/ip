b14bf6dd0389b6b618b18e66cb31036bf54ce6f7
YANYAN-w <e0773610@u.nus.edu>
2024-08-21 10:35:09 +0800

    edited response genereated and added test case with expected output.

--- src/main/java/Katheryne.java
+++ src/main/java/Katheryne.java
@@ -22,73 +22,74 @@ public class Katheryne {
 
     public void run() {
         System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
-        System.out.println("Katheryne: " + "Ad astra abyssosque! I'm Katheryne, the receptionist here at the Adventurers' Guild. Welcome!");
+        System.out.println("Katheryne: " +
+                "Ad astra abyssosque! I'm Katheryne, the receptionist here at the Adventurers' Guild. Welcome!");
 
         String userInput;
         boolean finish = false;
         while (!finish) {
             System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
             userInput = sc.nextLine();
-            if (!userInput.equals("bye")) {
-                System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
-                if (userInput.equals("list")) {
-                    System.out.println(this.getList());
-                } else if (userInput.startsWith("mark")) {
-                    String[] input = userInput.split(" ");
-                    int id = Integer.parseInt(input[1]) - 1;
-                    Task target = list.get(id);
-                    target.mark();
-                    System.out.println("Katheryne: Nice! I've marked this task as done:" + '\n' + target.toString());
-                } else if (userInput.startsWith("unmark")) {
-                    String[] input = userInput.split(" ");
-                    int id = Integer.parseInt(input[1]) - 1;
-                    Task target = list.get(id);
-                    target.unmark();
-                    System.out.println("Katheryne: OK, I've marked this task as not done yet:" + '\n' + target.toString());
-                } else if (userInput.startsWith("todo")) {
-                    String[] input = userInput.split(" ",2);
-                    String des = input[1];
-                    ToDo task = new ToDo(des);
-                    list.add(task);
-                    String str = String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.",task.toString(),list.size());
-                    System.out.println(str);
-                } else if (userInput.startsWith("event")) {
-                    int fromIndex = userInput.indexOf("/from");
-                    int toIndex = userInput.indexOf("/to");
-                    String fromTime = "";
-                    String toTime = "";
-                    if (fromIndex != -1) {
-                        int startOfFromTime = fromIndex + "/from".length();
-                        fromTime = userInput.substring(startOfFromTime, toIndex).trim();
-                    }
-                    if (toIndex != -1) {
-                        int startOfToTime = toIndex + "/to".length();
-                        toTime = userInput.substring(startOfToTime).trim();
-                    }
-
-                    int firstSpaceIndex = userInput.indexOf(" ");
-                    String description = userInput.substring(firstSpaceIndex + 1, fromIndex);
-                    Event task = new Event(description, fromTime, toTime);
-                    list.add(task);
-                    String str = String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.",task.toString(),list.size());
-                    System.out.println(str);
-                } else if (userInput.startsWith("deadline")) {
-                    String[] input = userInput.split(" /by ",2);
-                    String des = input[0];
-                    int firstSpaceIndex = des.indexOf(" ");
-                    String description = des.substring(firstSpaceIndex + 1);
-                    String byTime = input[1];
-                    Deadline task = new Deadline(description,byTime);
-                    list.add(task);
-                    String str = String.format("Got it. I've added this task:\n%s\nNow you have %d tasks in the list.",task.toString(),list.size());
-                    System.out.println(str);
+            if (userInput.equals("list")) {
+                System.out.println(this.getList());
+            } else if (userInput.startsWith("mark")) {
+                String[] input = userInput.split(" ");
+                int id = Integer.parseInt(input[1]) - 1;
+                Task target = list.get(id);
+                target.mark();
+                System.out.println("Katheryne: " +
+                        "Nice! I've marked this task as done:" + '\n' + target.toString());
+            } else if (userInput.startsWith("unmark")) {
+                String[] input = userInput.split(" ");
+                int id = Integer.parseInt(input[1]) - 1;
+                Task target = list.get(id);
+                target.unmark();
+                System.out.println("Katheryne: OK, I've marked this task as not done yet:" + '\n' + target.toString());
+            } else if (userInput.startsWith("todo")) {
+                String[] input = userInput.split(" ",2);
+                String des = input[1];
+                ToDo task = new ToDo(des);
+                list.add(task);
+                String str = String.format("Katheryne: " +
+                        "Got it. I've added this task:\n%s\nNow you have %d tasks in the list.",task.toString(),list.size());
+                System.out.println(str);
+            } else if (userInput.startsWith("event")) {
+                int fromIndex = userInput.indexOf("/from");
+                int toIndex = userInput.indexOf("/to");
+                String fromTime = "";
+                String toTime = "";
+                if (fromIndex != -1) {
+                    int startOfFromTime = fromIndex + "/from".length();
+                    fromTime = userInput.substring(startOfFromTime, toIndex).trim();
+                }
+                if (toIndex != -1) {
+                    int startOfToTime = toIndex + "/to".length();
+                    toTime = userInput.substring(startOfToTime).trim();
                 }
 
-            }
-            else{
+                int firstSpaceIndex = userInput.indexOf(" ");
+                String description = userInput.substring(firstSpaceIndex + 1, fromIndex);
+                Event task = new Event(description, fromTime, toTime);
+                list.add(task);
+                String str = String.format("Katheryne: " +
+                        "Got it. I've added this task:\n%s\nNow you have %d tasks in the list.",task.toString(),list.size());
+                System.out.println(str);
+            } else if (userInput.startsWith("deadline")) {
+                String[] input = userInput.split(" /by ",2);
+                String des = input[0];
+                int firstSpaceIndex = des.indexOf(" ");
+                String description = des.substring(firstSpaceIndex + 1);
+                String byTime = input[1];
+                Deadline task = new Deadline(description,byTime);
+                list.add(task);
+                String str = String.format("Katheryne:" +
+                        " Got it. I've added this task:\n%s\nNow you have %d tasks in the list.",task.toString(),list.size());
+                System.out.println(str);
+            } else if (userInput.equals("bye")) {
                 finish = true;
                 System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
-                System.out.println("Katheryne: " + "Farewell, Adventurer, and thank you for supporting the Adventurers' Guild.");
+                System.out.println("Katheryne: " +
+                        "Farewell, Adventurer, and thank you for supporting the Adventurers' Guild.");
                 System.out.println("✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧");
                 sc.close();
             }
