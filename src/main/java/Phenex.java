2538812539cc26693b421380475f34c3f9ae3058
marchjlim <marchjlim@gmail.com>
2024-08-21 22:28:06 +0800

    Added echo feature for Phenex

--- src/main/java/Phenex.java
+++ src/main/java/Phenex.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Phenex {
     public static void main(String[] args) {
         String logo = "  _____    _    _   ______   _   _   ______  __   __ \n"
@@ -16,6 +18,19 @@ public class Phenex {
         String farewellMsg = "Goodbye. Extinguish the Zeon forces on your way out!";
 
         System.out.println(greetMsg);
+
+        // begin echoing
+        Scanner scanner = new Scanner(System.in);
+        String terminatingStr = "bye";
+        String userInput = scanner.next();
+
+        while (!userInput.equals(terminatingStr)) {
+            String echoMsg = line + "\t" + userInput + " \n" + line;
+            System.out.println(echoMsg);
+            userInput = scanner.next();
+        }
+
+        scanner.close();
         System.out.println(farewellMsg);
     }
 }
