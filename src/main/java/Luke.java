d09250a878030dce86dfa788e54c2a1ed0aceef0
MinkhantEain <khantkyaw.pkk@gmail.com>
2024-08-21 14:16:47 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Luke.java
@@ -0,0 +1,13 @@
+public class Luke {
+    public static void main(String[] args) {
+        String logo =
+	          " __                \n"
+                + "| |    _   _ _  _____ \n"
+                + "| |   | | | | |/ / _ \\\n"
+                + "| |__ | |_| |   <  __/\n"
+                + "|____| \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+	System.out.println("Hello! I'm Luke.\nWhat can I do for you?");
+	System.out.println("Bye. Hope to see you again soon!");
+    }
+}
