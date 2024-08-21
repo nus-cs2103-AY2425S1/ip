64dcc5a7e00658994f71bfa3513bc92c05c13c96
ckaayy <e1122115@u.nus.edu>
2024-08-21 16:25:42 +0800

    Added Echo feature

--- src/main/java/YapperBot.java
+++ src/main/java/YapperBot.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class YapperBot {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("________________________________");
         System.out.println("Hello! I'm YapperBot");
         System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            System.out.println("________________________________");
+            System.out.println(userInput);
+            System.out.println("________________________________");
+        }
+
         System.out.println("________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("________________________________");
