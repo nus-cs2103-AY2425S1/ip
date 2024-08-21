795a324982286b3d4e3b506e38dad8d3fbf40bdb
Aditi Gawande <>
2024-08-21 18:09:20 +0800

    Level 3 - Mark as Done

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Max {
     private final Scanner scanner = new Scanner(System.in);
-    private String[] storedTexts;
-    private int storedTextsIndex;
+    private Task[] storedTasks;
+    private int storedTasksIndex;
 
     public static void main(String[] args) {
         Max max = new Max();
@@ -11,8 +11,8 @@ public class Max {
     }
 
     public Max() {
-        storedTexts = new String[100];
-        storedTextsIndex = 0;
+        storedTasks = new Task[100];
+        storedTasksIndex= 0;
     }
     public void runMax() {
         printHello();
@@ -24,11 +24,17 @@ public class Max {
 
             if (text.equals("bye")) {
                 running = false;
-            } else if (text.equals("list")){
+            } else if (text.equals("list")) {
                 list();
+            } else if (text.startsWith("mark")) {
+                int index = Integer.parseInt(text.replace("mark ", ""));
+                markDone(index);
+            } else if (text.startsWith("unmark")) {
+                int index = Integer.parseInt(text.replace("unmark ", ""));
+                markNotDone(index);
             } else {
-                this.storedTexts[this.storedTextsIndex] = text;
-                this.storedTextsIndex++;
+                this.storedTasks[this.storedTasksIndex] = new Task(text);
+                this.storedTasksIndex++;
                 echo(text);
             }
         }
@@ -36,13 +42,13 @@ public class Max {
     }
 
     public void printMessage(String message) {
-        System.out.println("\t" + message);
+        System.out.println("\t " + message);
         printLine();
     }
 
     public void printHello() {
         printLine();
-        printMessage("Hello! I'm Max\n\tWhat can I do for you?");
+        printMessage("Hello! I'm Max\n\t What can I do for you?");
     }
 
     public void printBye() {
@@ -61,10 +67,31 @@ public class Max {
 
     public void list() {
         printLine();
-        for (int i = 0; i < this.storedTextsIndex; i++) {
+        for (int i = 0; i < this.storedTasksIndex; i++) {
             int count = i + 1;
-            System.out.println("\t" + count + ". " + this.storedTexts[i]);
+            System.out.println("\t " + count + ".[" + this.storedTasks[i].getStatusIcon() + "] "
+                    + this.storedTasks[i].getDescription());
         }
+        printLine();
+    }
+
+    public void markDone(int index) {
+        int arrayIndex = index - 1;
+        storedTasks[arrayIndex].markDone();
+        printLine();
+        System.out.println("\t Nice! I've marked this task as done:");
+        System.out.println("\t   [" + storedTasks[arrayIndex].getStatusIcon() + "] "
+                + storedTasks[arrayIndex].getDescription());
+        printLine();
+    }
+
+    public void markNotDone(int index) {
+        int arrayIndex = index - 1;
+        storedTasks[arrayIndex].markNotDone();
+        printLine();
+        System.out.println("\t OK, I've marked this task as not done yet:");
+        System.out.println("\t   [" + storedTasks[arrayIndex].getStatusIcon() + "] "
+                + storedTasks[arrayIndex].getDescription());
         printLine();
     }
 }
