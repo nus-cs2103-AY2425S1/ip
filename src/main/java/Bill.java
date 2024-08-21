47c8452d52695f0461b2f00edadf5ff72dc3a3f9
Wnayar <wdnconsultancy@yahoo.com>
2024-08-22 03:22:37 +0800

    New chatbot name with initial skeletal version

---
+++ src/main/java/Bill.java
@@ -0,0 +1,7 @@
+public class Bill {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bill");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
