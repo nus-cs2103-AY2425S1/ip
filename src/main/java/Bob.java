3b1d4eeba31193bda9c01722fc5e7aadd712d5f9
JumpyJay <iuswtric1909@gmail.com>
2024-08-21 17:16:39 +0800

    Modify Duke.java
    Skeletal implementation that greets the user.
    
    Rename the bot's name from Duke to Bob- changed file name and class name

---
+++ src/main/java/Bob.java
@@ -0,0 +1,13 @@
+public class Bob {
+    public static void main(String[] args) {
+        String hLine = "____________________________________________________________\n";
+        String greeting = "Hello! I'm Bob\nWhat can I do for you?\n";
+        String bye = "Bye. Hope to see you again soon!\n";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(hLine + greeting + hLine + bye + hLine);
+    }
+}
