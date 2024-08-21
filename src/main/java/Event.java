5590194789205fb6ca0069b73cd1a3f6d2c23dfb
Chua Tse Hui <htcylsg@gmail.com>
2024-08-21 10:24:03 +0800

    added Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+    protected final String startTime;
+    protected final String endTime;
+    Event(String description, String startTime, String endTime) {
+        super(description);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.startTime, this.endTime);
+    }
+}
