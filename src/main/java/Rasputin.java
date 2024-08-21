6870de6d7d4cf3b4c8f4a44fb906ee8285688ba4
harithh07 <harith.hakim2002@gmail.com>
2024-08-21 21:04:51 +0800

    Added greeting and exit message

--- src/main/java/Rasputin.java
+++ src/main/java/Rasputin.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class Rasputin {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String name = "Rasputin";
+        System.out.println("Hello, I'm " + name + "!\nWhat can I do for you?");
+        System.out.print("\n");
+        System.out.println("Bye. Hope to see you soon!");
     }
 }
