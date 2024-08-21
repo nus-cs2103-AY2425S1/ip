81b32011fee4b985566a6005a1bff2f963fb4bd4
linette-g <156193856+linette-g@users.noreply.github.com>
2024-08-21 14:18:22 +0800

    Add skeletal version of chatbot Rob

---
+++ src/main/java/Rob.java
@@ -0,0 +1,8 @@
+public class Rob {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Rob\n" +
+                "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+        System.out.println(greet + exit);
+    }
+}
