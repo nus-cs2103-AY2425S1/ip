f536731a090f909a8ae557ebeffde96916786370
vinceg4 <vincegoh04@gmail.com>
2024-08-21 12:09:00 +0800

    Add greeting

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,6 +1,13 @@
 public class Bobby {
     public static void main(String[] args) {
-        
+        horizontalLine(35);
+        System.out.println();
+        System.out.println("Hello! I'm Bobby");
+        System.out.println("What can I do for you?");
+        horizontalLine(35);
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+        horizontalLine(35);
     }
 
     // Make a horizontal line of x dashes
