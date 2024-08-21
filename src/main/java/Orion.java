a6eb0dbe529455ec453c8de21e8e967ecb57d434
Elliot Tang <elliot.tang.379@gmail.com>
2024-08-21 15:52:17 +0800

    Add greeting

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -7,7 +7,19 @@ public class Orion {
                                     + " \\____/|__|  |__|\\____/|___|  /\n"
                                     + "                            \\/ \n";
 
+    public static final String BAR = "______________________________________________\n";
+
     public static void main(String[] args) {
-        System.out.println("Hello from\n" + Orion.LOGO);
+        System.out.println(Orion.BAR);
+        System.out.println(Orion.LOGO);
+
+        System.out.println(Orion.BAR);
+        System.out.println("Hello from Orion!");
+        System.out.println("What do you want to talk about today?");
+
+        System.out.println(Orion.BAR);
+        System.out.println("Bye! Hope to see you again soon!");
+
+        System.out.println(Orion.BAR);
     }
 }
