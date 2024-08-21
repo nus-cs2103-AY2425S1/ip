99dd95df8804e7129d93519e5be5194a71a008c4
JeremyChiaHanSheng <chiajeremy1@gmail.com>
2024-08-21 21:51:32 +0800

    Changed default bot name + renamed file + welcome & exit message

---
+++ src/main/java/NextGPT.java
@@ -0,0 +1,12 @@
+public class NextGPT {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n";
+
+        System.out.println("Hello! I know you heard about ChatGPT but im the next BIG THING NextGPT\n"
+                + logo
+                +"How may I support you today\n"
+                + logo
+                +"Hope you have an awseome day!!\n"
+                );
+    }
+}
