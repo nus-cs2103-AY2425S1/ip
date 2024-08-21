7779c921fc4509a4ca1f3586505b2f4bd8c18000
Hartihhh06 <e1156421@u.nus.edu>
2024-08-21 13:35:25 +0800

    Level 1. Echo

--- src/main/java/LunaBot.java
+++ src/main/java/LunaBot.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class LunaBot {
     public static void main(String[] args) {
-        System.out.println("--------------------------------------------------------------");
+        // Greet the user
+        System.out.println("___________________________________________________________________");
         System.out.println(" Hello! I'm LunaBot");
         System.out.println(" What can I do for you?");
-        System.out.println("--------------------------------------------------------------");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("--------------------------------------------------------------");
 
+        // Create scanner for user input
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // loop scanner to echo user input
+        while (true) {
+            input = scanner.nextLine();
+
+            // exit if input is "bye"
+            if (input.equals("bye")) {
+                System.out.println("___________________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("___________________________________________________________________");
+                break;
+            }
+            System.out.println("___________________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("___________________________________________________________________");
+        }
+        scanner.close();
     }
 }
