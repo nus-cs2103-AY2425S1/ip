814ce3d4ec5dc9cb78cf9568995152b97405eacd
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-08-21 09:01:58 +0800

    Level-1

--- src/main/java/MichaelScott.java
+++ src/main/java/MichaelScott.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class MichaelScott {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        boolean flag = true;
+
         String logo = " __  __ _      _                _   ____            _   _  \n"
                 + "|  \\/  (_) ___| |__   __ _  ___| | / ___|  ___ ___ | |_| |_ \n"
                 + "| |\\/| | |/ __| '_ \\ / _` |/ _ \\ | \\___ \\ / __/ _ \\| __| __|\n"
@@ -10,5 +14,26 @@ public class MichaelScott {
         System.out.println("Hello! I'm Michael Scott");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while(flag) {
+            String command = myScanner.nextLine();
+
+            if (command.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("list");
+                System.out.println("____________________________________________________________");
+
+            } else if (command.equals("blah")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("blah");
+                System.out.println("____________________________________________________________");
+            } else if (command.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Catch you on the flippity flip! ");
+                System.out.println("____________________________________________________________");
+                flag = false;
+            }
+
+        }
     }
 }
