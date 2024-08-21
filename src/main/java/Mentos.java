b1d3a8edb9b54487755ec31c919556225039e7f4
Denon Chong <e0958801@u.nus.edu>
2024-08-21 21:52:31 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Mentos.java
+++ src/main/java/Mentos.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Mentos
 {
-    private String[] tasks = new String[100];
+    private String MARKED = "mark";
+    private String UNMARKED = "unmark";
+    private Task[] tasks = new Task[100];
     private int noTasks = 0;
     public static void main(String[] args) {
         Mentos mentos = new Mentos();
@@ -45,9 +47,24 @@ public class Mentos
                 displayTasks();
                 continue;
             }
+            else if (input.startsWith(MARKED)){
+                int index = Integer.parseInt(input.split(" ")[1]);
+                tasks[index-1].markAsDone();
+                System.out.println("Nicely done! This task is marked as done!");
+                System.out.println(tasks[index-1].toString());
+                System.out.println("____________________________");
+                continue;
+            } else if (input.startsWith(UNMARKED)){
+                int index = Integer.parseInt(input.split(" ")[1]);
+                tasks[index-1].markAsNotDone();
+                System.out.println("Holdup this task is not done!");
+                System.out.println(tasks[index-1].toString());
+                System.out.println("____________________________");
+                continue;
+            }
             System.out.println("____________________________");
             noTasks++;
-            tasks[noTasks-1] = input;
+            tasks[noTasks-1] = new Task(input);
             System.out.println("added: "+ input );
             System.out.println("____________________________");
         }
@@ -55,7 +72,7 @@ public class Mentos
 
     public void displayTasks(){
         for (int i = 0; i < noTasks; i++) {
-            String task_out = String.format("%d. %s",i+1,tasks[i]);
+            String task_out = String.format("%d. %s",i+1,tasks[i].toString());
             System.out.println(task_out);
         }
         System.out.println("____________________________");
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected boolean isDone;
+    protected String description;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon(){
+        return (isDone ? "X" : " ");
+    }
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public void markAsNotDone(){
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[%s] %s",this.getStatusIcon(),this.description);
+    }
+}
