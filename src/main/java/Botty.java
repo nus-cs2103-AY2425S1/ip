6cc727b2b960a20f2c21de4ca1c163e6584d58f0
IzN432 <isaac.ng432@gmail.com>
2024-08-21 14:53:32 +0800

    Add list functionality

--- src/main/java/Botty.java
+++ src/main/java/Botty.java
@@ -20,24 +20,45 @@ public class Botty {
                 "        \\|__|  \\|__|\\|__|\\|_______|        \\|_______|\\|_______|    \\|__|";
 
         String bottySymbol = "|┐∵|┘: ";
+        String bottyIndentation = "       ";
 
         System.out.println(logo);
 
         System.out.println();
         System.out.println(bottySymbol + "Hello, I am Botty the Bot, how may I be of service today?");
 
-        System.out.println();
-
         Scanner inputScanner = new Scanner(System.in);
 
-        while (true) {
+        String[] dataList = new String[100];
+        int currentIndex = 0;
+
+        boolean exitFlag = false;
+
+        while (!exitFlag) {
+            System.out.println();
+
             String userInput = inputScanner.nextLine();
-            if (userInput.equals("bye")) {
-                break;
+
+            switch (userInput) {
+                case "bye":
+                    exitFlag = true;
+                    break;
+                case "list":
+                    System.out.println(bottySymbol + "Here you go!");
+                    for (int i = 1; i < currentIndex + 1; i++) {
+                        System.out.println(bottyIndentation + "  " + i + ". " + dataList[i - 1]);
+                    }
+                    break;
+                default:
+                    if (currentIndex < dataList.length) {
+                        dataList[currentIndex] = userInput;
+                        currentIndex++;
+                        System.out.println(bottySymbol + "I have added \"" + userInput + "\" to the list!");
+                    } else {
+                        System.out.println(bottySymbol + "I have run out of space, sorry! Here's a cookie \uD83C\uDF6A");
+                    }
             }
 
-            System.out.println(bottySymbol + userInput);
-            System.out.println();
         }
 
         inputScanner.close();
