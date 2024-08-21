d7c000f0a3b2accae927cdde2c6e73989725ff6b
nigellqx <lownigel79@gmail.com>
2024-08-21 16:35:36 +0800

    Update Tako.java
    The chatbot will echo the command given by the users.
    If the command given is 'bye', the chatbot will exit.

--- src/main/java/Tako.java
+++ src/main/java/Tako.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Tako {
     public static void main(String[] args) {
         String name = "Tako";
 
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hello! I'm " + name + "\n" +
-                           "What can I do for you?\n" +
-                           "Bye. Hope to see you again soon!");
+                           "What can I do for you?\n");
+
+        String command = input.next();
+
+        while(!command.equals("bye")) {
+            System.out.println(command);
+            command = input.next();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
