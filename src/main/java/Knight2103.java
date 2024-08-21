c1e3379bd1d28d0885d6141c2cefb8e6bb9218e0
Chua Tse Hui <htcylsg@gmail.com>
2024-08-21 09:04:41 +0800

    echo success

--- src/main/java/Knight2103.java
+++ src/main/java/Knight2103.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class Knight2103 {
     public static void main(String[] args) {
+        String botName = "Knight2103";
         String horiLine = "_____________";
-        System.out.println(
-                horiLine + "\n"
-                + "Hello! I'm Knight2103\n"
+        String intro = horiLine + "\n"
+                + "Hello! I'm " + botName + " \n"
                 + "What can I do for you?\n"
-                + horiLine + "\n"
-                + "Bye. Hope to see you again soon!\n"
-                + horiLine
-        );
+                + horiLine + "\n";
+        // put scanner object here
+        System.out.println(intro);
+        Scanner scanObject = new Scanner(System.in);
+        String input = scanObject.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(horiLine + "\n" + input + "\n" + horiLine);
+            input = scanObject.nextLine();
+        }
+        System.out.println(horiLine + "\n" + "Bye. Hope to see you again soon!" + "\n" + horiLine);
+        scanObject.close();
     }
 }
