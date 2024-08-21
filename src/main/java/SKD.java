640fd1dafef0e6d25b6caf6eaea75997181edac9
seandias <seankdias@gmail.com>
2024-08-21 15:24:39 +0800

    Level 1

--- src/main/java/SKD.java
+++ src/main/java/SKD.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
 public class SKD {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm SKD");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        String line = "    ____________________________________________________________";
+        System.out.println(line);
+        System.out.println("     Hello! I'm SKD");
+        System.out.println("     What can I do for you?");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println();
+                System.out.println(input);
+                System.out.println(line);
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println();
+            System.out.println(input);
+            System.out.println(line);
+            System.out.println("     " + input);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
