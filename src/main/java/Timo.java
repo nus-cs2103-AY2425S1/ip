30d1ca2dea5ad52e50602254790da2b918e000aa
teamotitan <timothy.tanzijing@gmail.com>
2024-08-21 15:19:07 +0800

    Level 0

---
+++ src/main/java/Timo.java
@@ -0,0 +1,10 @@
+public class Timo {
+    public static void main(String[] args) {
+        System.out.println("----------------------------\n");
+        System.out.println("Hello! I'm Timo\nWhat can I do for you?\n");
+        System.out.println("----------------------------\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("----------------------------\n");
+
+    }
+}
