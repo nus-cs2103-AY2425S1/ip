ef0b145cf49c6b7cc7d36a2290b8ac38a4c74668
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-21 15:04:06 +0800

    Finish level 0

---
+++ src/main/java/Hue.java
@@ -0,0 +1,19 @@
+public class Hue {
+
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String name = "Hue";
+
+        System.out.println("____________________________________________________________" );
+        System.out.println("Hello! I'm [" + name + "]");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________" );
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+
+}
