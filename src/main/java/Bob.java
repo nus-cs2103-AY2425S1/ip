90a3837ca31573f4df9300f5dade75d62afbecd1
sarahteonin <E1249329@u.nus.edu>
2024-08-22 00:57:40 +0800

    level 1 completed

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         String logo = "Bob";
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        return;
     }
 }
