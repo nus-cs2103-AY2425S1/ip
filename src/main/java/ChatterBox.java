6932900732e71c6b27345e0b956814ab023d4927
Trisha Patodia <trishapatodia@Trishas-MacBook-Air.local>
2024-08-21 09:46:55 +0800

    Level 1 Echo: Implemented echo functionality and exit command

--- src/main/java/ChatterBox.java
+++ src/main/java/ChatterBox.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class ChatterBox {
     public static void main(String[] args) {
-        System.out.println("_".repeat(50));
-        System.out.println("Hello! I'm ChatterBox");
-        System.out.println("What can I do for you?");
-        System.out.println("_".repeat(50));
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_".repeat(50));
+
+        Scanner sc = new Scanner(System.in);
+        //Greeting the user
+        System.out.println("\t\t" + "_".repeat(50));
+        System.out.println("\t\t" + "Hello! I'm ChatterBox");
+        System.out.println("\t\t" + "What can I do for you?");
+        System.out.println("\t\t" + "_".repeat(50));
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("\t\t" + "_".repeat(50));
+                System.out.println("\t\t" + "Bye. Hope to see you again soon!");
+                System.out.println("\t\t" + "_".repeat(50));
+                break;
+            } else {
+                System.out.println("\t\t" + "_".repeat(50));
+                System.out.println("\t\t" + input);
+                System.out.println("\t\t" + "_".repeat(50));
+            }
+        }
     }
 }
