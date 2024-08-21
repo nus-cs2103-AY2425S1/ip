64a58753a4d77f6d4868a38ccb1452b0f4764a7f
Lynette Lee <sayhitosiying@Lynettes-Laptop.local>
2024-08-21 17:02:49 +0800

    Add and list tasks

--- src/main/java/Socchat.java
+++ src/main/java/Socchat.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Socchat {
+
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         greet();
         while (true) {
@@ -10,8 +13,14 @@ public class Socchat {
             if (input.equals("bye")) {
                 exit();
                 break;
+            } else if (input.equals("list")) {
+                list(tasks);
+            } else {
+                tasks.add(input);
+                System.out.print("added: ");
+                System.out.println(input);
             }
-            System.out.println(input);
+
         }
     }
     public static void greet() {
@@ -20,5 +29,11 @@ public class Socchat {
     }
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!    ");
+    }
+    public static void list(ArrayList<String> tasks) {
+        for(int i = 0; i < tasks.size(); i++) {
+            System.out.print((i + 1) + ": ");
+            System.out.println(tasks.get(i));
+        }
     }
 }
