a7691aa702ec9fca7ead0fb7f167a7823f805d51
Arnaxx54 <arnavgoel182@gmail.com>
2024-08-22 05:24:35 +0800

    Update name and add greeting/exit messages

---
+++ src/main/java/Message.java
@@ -0,0 +1,14 @@
+public class Message {
+
+    private static final String greeting = "Hello! I'm Zenith\nWhat can I do for you?";
+    private static final String exit = "Bye. Hope to see you again soon!";
+
+    public static String getGreeting() {
+        return Message.greeting;
+    }
+
+    public static String getExit() {
+        return Message.exit;
+    }
+
+}
---
+++ src/main/java/Zenith.java
@@ -0,0 +1,7 @@
+public class Zenith {
+    public static void main(String[] args) {
+        System.out.println(Message.getGreeting());
+        System.out.println();
+        System.out.println(Message.getExit());
+    }
+}
