383fedfab5a384ec770e568fdfe9acd75b76bc51
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-08-21 11:48:11 +0800

    Refactor Task class into a separate file

--- src/main/java/PurrfessorDipsy.java
+++ src/main/java/PurrfessorDipsy.java
@@ -2,33 +2,6 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class Task {
-    String taskName;
-    boolean isDone;
-
-    Task(String taskName, boolean isDone) {
-        this.taskName = taskName;
-        this.isDone = isDone;
-    }
-
-    public void markAsDone() {
-        isDone = true;
-    }
-
-    public void markAsUndone() {
-        isDone = false;
-    }
-
-    public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
-    }
-
-    @Override
-    public String toString() {
-        return "[" + getStatusIcon() + "] " + taskName;
-    }
-}
-
 public class PurrfessorDipsy {
     private static final Task[] taskTable = new Task[100];
     private static int taskTableRowCount = 0;
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    String taskName;
+    boolean isDone;
+
+    Task(String taskName, boolean isDone) {
+        this.taskName = taskName;
+        this.isDone = isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + taskName;
+    }
+}
