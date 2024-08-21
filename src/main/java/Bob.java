de26fd67386017d72ce8ed0070b71e5529c454e3
keerthigkaarthik <keerthigkaarthik@gmail.com>
2024-08-21 20:53:49 +0800

    Chang Duke to Bob

---
+++ src/main/java/Bob.java
@@ -0,0 +1,7 @@
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bob \nWhat can I do for you?");
+        System.out.println("\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
