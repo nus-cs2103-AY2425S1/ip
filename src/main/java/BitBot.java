6728f10413c4edc900cdac9aed9dea9617e91e17
SViswa02 <sviswa2002@gmail.com>
2024-08-21 16:45:32 +0800

    added the scanner to read user input

--- src/main/java/BitBot.java
+++ src/main/java/BitBot.java
@@ -1,14 +1,29 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class BitBot {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        String inputData = "";
         String intro = " ____________________________________ \n Hello! I'm BitBot \n What can I do for you? \n ____________________________________";
-        String conclusion = " \n Bye. Hope to see you again soon! \n ____________________________________ \n ";
+        String conclusion = " ____________________________________ \n Bye. Hope to see you again soon! \n ____________________________________ \n ";
         String initialSkeletal = intro + conclusion;
-        System.out.println(initialSkeletal);
+
+        //printing out the intro
+        System.out.println(intro);
+        //creating a scanner
+        Scanner sc = new Scanner(System.in);
+
+        //listening to the user's input & only terminating when the user says "bye"
+        while (true) {
+            inputData = sc.nextLine();
+            // if the user keys in "bye" break out of the while loop and print out the conclusion.
+            if (Objects.equals(inputData, "bye")) {
+                break;
+            }
+            System.out.println(" ____________________________________ \n " + inputData + "\n ____________________________________");
+        }
+
+        System.out.println(conclusion);
+
+
     }
 }
