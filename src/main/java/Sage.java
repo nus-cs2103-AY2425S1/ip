f11d04f672e7ca1ea4ca9ec577e601ff90a35689
AmadeusEka <00amadeus@gmail.com>
2024-08-21 15:11:52 +0800

    feat: Add greeting

---
+++ src/main/java/Sage.java
@@ -0,0 +1,10 @@
+public class Sage {
+    public static final String NAME = "Sage";
+    public static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE + "\nHello! I'm " + NAME + "\nWhat can i do for you?\n" + HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
+
+    }
+}
