3f225283a24481c24afd56811984f199dc0d6a9b
mingyang <limingyanglmy10@gmail.com>
2024-08-21 10:50:19 +0800

    updated Friday.java with interactive UI

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,9 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Friday {
     public static void main(String[] args) {
+        Scanner scannerObj = new Scanner(System.in);
+        boolean endScanner = false;
         System.out.println("___________________________________________________________");
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
         System.out.println("___________________________________________________________");
+        while (!endScanner) {
+            String toDo = scannerObj.nextLine();
+            if (Objects.equals(toDo, "bye")) {
+                endScanner = true;
+            } else {
+                System.out.println("    ___________________________________________________________");
+                System.out.println("    " + toDo);
+                System.out.println("    ___________________________________________________________");
+
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("___________________________________________________________");
     }
