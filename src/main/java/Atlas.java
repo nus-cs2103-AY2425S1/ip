8dd08d41a8b125762a668f47cc2c239c39210ef7
Raghava <chinnu4148@gmail.com>
2024-08-21 12:34:27 +0800

    feat: Add greet and exit methods

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -2,6 +2,7 @@ public class Atlas {
     // @@author patorjk.com
     // Reused from https://patorjk.com/software/taag/#p=display&f=Isometric1&t=Atlas
     // with minor modifications
+    public static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
         String logo = """
                       ___           ___           ___       ___           ___    \s
@@ -18,5 +19,22 @@ public class Atlas {
                 """;
 
         System.out.println("Hello from\n" + logo);
+        System.out.println(Atlas.LINE);
+        Atlas.greet();
+        Atlas.exit();
+    }
+
+    // Prints messages with lines underneath them
+    public static void print(String message) {
+        System.out.println(message);
+        System.out.println(Atlas.LINE);
+    }
+
+    public static void greet() {
+        Atlas.print("Hello! I'm Atlas \n" + "What can I do for you?");
+    }
+
+    public static void exit() {
+        Atlas.print("Bye. Hope to see you again soon!");
     }
 }
