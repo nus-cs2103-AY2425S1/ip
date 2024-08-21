61468b0ae100157c9acfaefaf197126e179ce9e3
tckeong <teo238540@gmail.com>
2024-08-21 22:10:26 +0800

    Level-4

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,15 @@
+public class EventTask extends Task {
+    private final String startTime;
+    private final String endTime;
+
+    public EventTask(String description, String startTime, String endTime) {
+        super(description);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + this.startTime + " to: " + this.endTime + ")";
+    }
+}
--- src/main/java/Jay.java
+++ src/main/java/Jay.java
@@ -35,11 +35,11 @@ public class Jay {
         String greet =
                 " Hello! I'm " + this.name + "\n" +
                 " What can I do for you?";
-        return formatCommand(greet);
+        return formattedCommand(greet);
     }
 
     private String quit() {
-        return formatCommand("Bye. Hope to see you again soon!");
+        return formattedCommand("Bye. Hope to see you again soon!");
     }
 
     private String processCommand(String command) {
@@ -52,29 +52,29 @@ public class Jay {
                 int taskNumber = Integer.parseInt(commands[1]);
 
                 if (taskNumber > this.taskCount) {
-                    return formatCommand("Invalid task number");
+                    return formattedCommand("Invalid task number");
                 }
 
                 this.tasks[taskNumber - 1].markAsDone();
-                return formatCommand("Nice! I've marked this task as done:\n" + this.tasks[taskNumber - 1]);
+                return formattedCommand("Nice! I've marked this task as done:\n" + this.tasks[taskNumber - 1]);
             } catch (NumberFormatException e) {
-                return formatCommand("Invalid task number");
+                return formattedCommand("Invalid task number");
             }
         } else if (commands[0].equals("unmark")) {
             try {
                 int taskNumber = Integer.parseInt(commands[1]);
 
                 if (taskNumber > this.taskCount) {
-                    return formatCommand("Invalid task number");
+                    return formattedCommand("Invalid task number");
                 }
 
                 this.tasks[taskNumber - 1].markAsNotDone();
-                return formatCommand("OK, I've marked this task as not done yet:\n" + this.tasks[taskNumber - 1]);
+                return formattedCommand("OK, I've marked this task as not done yet:\n" + this.tasks[taskNumber - 1]);
             } catch (NumberFormatException e) {
-                return formatCommand("Invalid task number");
+                return formattedCommand("Invalid task number");
             }
         } else {
-            return this.addTask(command);
+            return this.addTask(this.parseTask(command));
         }
     }
 
@@ -89,16 +89,46 @@ public class Jay {
             }
         }
 
-        return formatCommand("Here are the tasks in your list:\n" + tasks);
+        if (this.taskCount == 0) {
+            return formattedCommand("You have no tasks in the list.");
+        } else {
+            return formattedCommand("Here are the tasks in your list:\n" + tasks);
+        }
     }
 
-    private String addTask(String task) {
-        this.tasks[this.taskCount] = new Task(task);
+    private Task parseTask(String command) {
+        String taskType = command.split(" ")[0];
+
+        switch (taskType) {
+            case "todo": {
+                String description = command.split(" ", 2)[1].trim();
+                return new ToDoTask(description);
+            }
+            case "deadline": {
+                String description = command.split("/")[0].split(" ", 2)[1].trim();
+                String by = command.split("/")[1].split(" ", 2)[1].trim();
+                return new DeadlineTask(description, by);
+            }
+            case "event": {
+                String[] commands = command.split("/");
+                String description = commands[0].split(" ", 2)[1].trim();
+                String startTime = commands[1].split(" ", 2)[1].trim();
+                String endTime = commands[2].split(" ", 2)[1].trim();
+                return new EventTask(description, startTime, endTime);
+            }
+            default:
+                return new Task(command);
+        }
+    }
+
+    private String addTask(Task task) {
+        this.tasks[this.taskCount] = task;
         this.taskCount++;
-        return formatCommand("added: " + task);
+        return formattedCommand("Got it. I've added this task:\n" + task +
+                "\nNow you have " + this.taskCount + " tasks in the list.");
     }
 
-    private String formatCommand(String command) {
+    private String formattedCommand(String command) {
         return "____________________________________________________________\n" +
                 command + "\n" +
                 "____________________________________________________________\n";
