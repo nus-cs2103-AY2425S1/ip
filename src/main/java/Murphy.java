303bbb5c8c796b4defd8e4b710908bb4670a0761
Xan <iamxanng@gmail.com>
2024-08-22 00:08:10 +0800

    added greet and exit

--- src/main/java/Murphy.java
+++ src/main/java/Murphy.java
@@ -1,10 +1,19 @@
 public class Murphy {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        /*
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println("Hello from\n" + logo);
+        */
+
+        System.out.println("________________");
+        System.out.println("Hello! I'm Murphy");
+        System.out.println("What can I do for you?");
+        System.out.println("________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________");
     }
 }
