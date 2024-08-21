afee66318c3add9cc3960eec603ee02cbb1a2b02
currynia <teewenrui31@gmail.com>
2024-08-21 17:06:49 +0800

    Add echo

--- src/main/java/Tissue.java
+++ src/main/java/Tissue.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class Tissue {
     public static void main(String[] args) {
+        chatFunction();
+    }
 
-        System.out.println("Hello! I'm Tissue\n");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+    private static void chatFunction() {
+        String line = "--------------------------------------------------------------";
+        String indent = "       ";
+        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Tissue");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        String in = scanner.nextLine();
 
+        while (!in.equals("bye")) {
+            System.out.println(line);
+            System.out.print(indent);
+            System.out.println(in);
+            System.out.println(line);
+            in = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println(line);
+        System.out.print(indent);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
