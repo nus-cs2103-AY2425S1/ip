bb401c82b56d3e565326c73b2014bb724bd56f9f
hansneddyanto <hans.neddy@gmail.com>
2024-08-21 16:20:37 +0800

    Add features for ToDos, Events, and Deadlines

--- src/main/java/Hana.java
+++ src/main/java/Hana.java
@@ -11,6 +11,7 @@ public class Hana {
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         Task[] tasks = new Task[100];
         int numberOfTasks = 0;
+
         // Regular expressions for mark and unmark commands
         Pattern markPattern = Pattern.compile("^mark (\\d+)$");
         Pattern unmarkPattern = Pattern.compile("^unmark (\\d+)$");
@@ -48,13 +49,29 @@ public class Hana {
                 } else {
                     System.out.println(line + "     Task number out of range.\n" + line);
                 }
-            }
-            else {
-                Task task = new Task(input);
-                tasks[numberOfTasks] = task;
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                tasks[numberOfTasks] = new ToDo(description);
                 numberOfTasks++;
-                output = "    added: " + input + "\n";
-                System.out.println(line + output + line);
+                System.out.println(line + "     Got it. I've added this task:\n" +
+                        "       " + tasks[numberOfTasks - 1] + "\n" +
+                        "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+            } else if (input.startsWith("deadline ")) {
+                String[] parts = input.substring(9).split(" /by ");
+                tasks[numberOfTasks] = new Deadline(parts[0], parts[1]);
+                numberOfTasks++;
+                System.out.println(line + "     Got it. I've added this task:\n" +
+                        "       " + tasks[numberOfTasks - 1] + "\n" +
+                        "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+            } else if (input.startsWith("event ")) {
+                String[] parts = input.substring(6).split(" /from | /to ");
+                tasks[numberOfTasks] = new Event(parts[0], parts[1], parts[2]);
+                numberOfTasks++;
+                System.out.println(line + "     Got it. I've added this task:\n" +
+                        "       " + tasks[numberOfTasks - 1] + "\n" +
+                        "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+            } else {
+                System.out.println(line + "     I'm sorry, I don't understand that command.\n" + line);
             }
             input = scanner.nextLine();
         }
