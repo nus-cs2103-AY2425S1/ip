ee22922e4951e239ac64db568524731f0eaf37ee
PeterHW963 <peterhadiwijaya963@gmail.com>
2024-08-21 10:25:02 +0800

    Change Duke.java to Nether.java
    Duke.java visually prints out "Duke"
    
    The assignment given is to rename the chatbot, and create a greet and
    exit message.
    
    Let's rename the file to Nether.java, its new name and create the
    greet and exit messages, along with a new visual print out of its name.
    
    "println" statements are the only lines edited as that is the only
    requirement for this increment.

---
+++ src/main/java/Nether.java
@@ -0,0 +1,20 @@
+/**
+ * Produces greetings for users and initializes chat.
+ */
+
+public class Nether {
+    public static void main(String[] args) {
+        String logo = " _   _      _   _        \n"
+                + "| \\ | | ___| |_| |__  ___ _ __ \n"
+                + "|  \\| |/ _ \\ __| '_ \\/ _ \\ '__|\n"
+                + "| |\\  |  __/ |_| | | ||__/ |  \n"
+                + "|_| \\_|\\___|\\__|_| |_\\___|_|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Nether");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
