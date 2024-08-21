d079cc6a42ddb3e3b77ced94476b3ce8b3e102d8
Casper Ng <casperngeen@gmail.com>
2024-08-21 22:05:51 +0800

    Added tasktype field

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,17 +1,21 @@
 package main.java;
 
+import java.util.HashMap;
+
 public class Task {
     private String description;
+    private String taskType;
     private boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, String taskType) {
         this.description = description;
         this.isDone = false;
+        this.taskType = taskType;
     }
 
     @Override
     public String toString() {
-        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+        return String.format("[%s][%s] %s", this.taskType, this.getStatusIcon(), this.description);
     }
 
     public String getStatusIcon() {
