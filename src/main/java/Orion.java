10eb4c09bcbe7a926f7e432d7a2c3161f3153992
Elliot Tang <elliot.tang.379@gmail.com>
2024-08-21 15:45:17 +0800

    Rename main class

---
+++ src/main/java/Orion.java
@@ -0,0 +1,13 @@
+public class Orion {
+
+    public static final String LOGO = "             .__               \n"
+                                    + "  ___________|__| ____   ____  \n"
+                                    + " /  _ \\_  __ \\  |/  _ \\ /    \\ \n"
+                                    + "(  <_> )  | \\/  (  <_> )   |  \\\n"
+                                    + " \\____/|__|  |__|\\____/|___|  /\n"
+                                    + "                            \\/ \n";
+
+    public static void main(String[] args) {
+        System.out.println("Hello from\n" + Orion.LOGO);
+    }
+}
