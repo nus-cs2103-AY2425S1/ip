ced9ae13a554a75b3ffb2a4d1705ec2b0bed6518
leroytan <mingxiang130@gmail.com>
2024-08-22 04:46:42 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Duke {
     public static void main(String[] args) {
@@ -7,10 +9,22 @@ public class Duke {
         String exit = lineBreak + " Bye. Hope to see you again soon!\n" + lineBreak;
         System.out.println(intro);
         String command;
+
         Scanner scanner = new Scanner(System.in);
         command = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+
         while (!command.equals("bye")) {
-            System.out.println(lineBreak + command +"\n" + lineBreak);
+            if (!command.equals("list")) {
+                list.add(command);
+                System.out.println(lineBreak + "added:" + command +"\n" + lineBreak);
+            } else {
+                System.out.print(lineBreak);
+                IntStream.range(0, list.size()).mapToObj(number -> number + 1 + ". " + list.get(number)).forEach(System.out::println);
+                System.out.print(lineBreak);
+            }
+
             command = scanner.nextLine();
         }
         scanner.close();
