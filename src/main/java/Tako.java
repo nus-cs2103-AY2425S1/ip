020605fd386e49db4a904b96ef683b61ac43235c
nigellqx <lownigel79@gmail.com>
2024-08-21 17:46:38 +0800

    Add Task.java Update Tako.java
    Task.java is added so that the function of marking and unmarking of task can be done in a more natural way.

--- src/main/java/Tako.java
+++ src/main/java/Tako.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Tako {
     public static void main(String[] args) {
         String name = "Tako";
-        ArrayList<String> storageCommand = new ArrayList<String>();
+        ArrayList<Task> listOfTask = new ArrayList<Task>();
 
         Scanner input = new Scanner(System.in);
 
@@ -15,10 +15,17 @@ public class Tako {
 
         while(!command.equals("bye")) {
             if (command.equals("list")) {
-                listStorage(storageCommand);
+                listStorage(listOfTask);
+            } else if (command.length() > 4 && command.substring(0, 5).equals("mark ")) {
+                int taskNumber = Integer.parseInt(command.substring(5));
+                markTask(listOfTask, taskNumber);
+            } else if (command.length() > 6 && command.substring(0, 7).equals("unmark ")) {
+                int taskNumber = Integer.parseInt(command.substring(7));
+                unmarkTask(listOfTask, taskNumber);
             } else {
                 System.out.println("added: " + command);
-                addStorage(storageCommand, command);
+                Task newTask = new Task(command);
+                addStorage(listOfTask, newTask);
             }
             command = input.nextLine();
         }
@@ -26,13 +33,29 @@ public class Tako {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void listStorage(ArrayList<String> storageCommand) {
-        for (int i = 0; i < storageCommand.size(); i++) {
-            System.out.println(i+1 + ". " + storageCommand.get(i));
+    public static void listStorage(ArrayList<Task> listOfTask) {
+        System.out.println("Here are the tasks in your list:\n");
+        for (int i = 0; i < listOfTask.size(); i++) {
+            System.out.println((i + 1) + ".[" + listOfTask.get(i).getStatusIcon() +
+                               "] " + listOfTask.get(i).getDescription());
         }
     }
 
-    public static void addStorage(ArrayList<String> storageCommand, String command) {
-        storageCommand.add(command);
+    public static void addStorage(ArrayList<Task> listOfTask, Task newTask) {
+        listOfTask.add(newTask);
+    }
+
+    public static void markTask(ArrayList<Task> listOfTask, int taskNumber) {
+        listOfTask.get(taskNumber - 1).markAsDone();
+        System.out.println("Nice! I've marked this task as done:\n" +
+                           "  [" + listOfTask.get(taskNumber - 1).getStatusIcon() +
+                           "] " + listOfTask.get(taskNumber - 1).getDescription());
+    }
+
+    public static void unmarkTask(ArrayList<Task> listOfTask, int taskNumber) {
+        listOfTask.get(taskNumber - 1).markAsUndone();
+        System.out.println("OK, I've marked this task as not done yet:\n" +
+                           "  [" + listOfTask.get(taskNumber - 1).getStatusIcon() +
+                           "] " + listOfTask.get(taskNumber - 1).getDescription());
     }
 }
