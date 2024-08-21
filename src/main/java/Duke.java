eda226d000bb5d51ebf1efe1038d713a7ce7cc6b
ethantyh692 <ethantyh692@gmail.com>
2024-08-21 17:08:35 +0800

    Done with Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static ArrayList<String> toDoList = new ArrayList<String>();
+    private static int counter = 1;
 
     private static void greet() {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
@@ -10,18 +14,26 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private static void printList() {
+        for (String task : toDoList) {
+            System.out.println(counter + ". " + task);
+            counter += 1;
+        }
+    }
+
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
         String command;
         while (sc.hasNext()) {
-            command = sc.next();
+            command = sc.nextLine();
             if (command.equals("list")) {
-                System.out.println("list");
+                printList();
             } else if (command.equals("bye")) {
                 break;
             } else {
-                System.out.println(command);
+                toDoList.add(command);
+                System.out.println("added: " + command);
             }
         }
         sc.close();
