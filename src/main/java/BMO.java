ac640bd3b037f6e9092033d213cae6614599063b
aldentantan <aldentanfk@gmail.com>
2024-08-21 12:13:54 +0800

    Change chatbot name and exit message

---
+++ src/main/java/BMO.java
@@ -0,0 +1,8 @@
+public class BMO {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println("Hello! I'm BMO!\nWhat can I do for you?\n" + line);
+
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
