0bf84c74146a0144b65173e03dcb1a6531ffc470
fusin <fusin02@gmail.com>
2024-08-22 03:01:07 +0800

    Implemented Add, List

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dude {
+
+    private static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         System.out.println(line);
@@ -10,11 +14,19 @@ public class Dude {
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println(line);
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i + 1 + ". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
             System.out.println(line);
             input = sc.nextLine();
         }
 
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again!");
         System.out.println(line);
     }
