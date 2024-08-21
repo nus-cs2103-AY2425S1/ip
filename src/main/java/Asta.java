eceb64bd4af5680e0a6a7f64903828a7a468280f
Yang Zhi <yangzhi.see@gmail.com>
2024-08-21 23:54:57 +0800

    feat: add memory and list function

--- src/main/java/Asta.java
+++ src/main/java/Asta.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Asta {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Asta\n" + "What can I do for you?\n");
 
         while (true) {
@@ -10,8 +13,15 @@ public class Asta {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
 
         scanner.close();
