acd50b484dfd7fcc3a205e3494131a9dbc54342b
Colin Chiang <colinchiang.cc@gmail.com>
2024-08-21 11:01:48 +0800

    Add echo functionality

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm your chatbot, Bob.\n" +
                 "How may I assist you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again!");
 
+        Scanner reader = new Scanner(System.in);
+        String response;
+
+
+        while (!(response = readUserInput(reader)).equals("bye")) {
+            System.out.println("____________________________________________________________");
+           System.out.println(response);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static String readUserInput(Scanner s) {
+        System.out.print("> ");
+        return s.next();
     }
 }
