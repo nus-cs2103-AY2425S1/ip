50193c4e3b2360baad59d61f2b9716a2a0a1403e
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-22 05:48:54 +0800

    Rainy.java: Add Scanner

--- src/main/java/Rainy.java
+++ src/main/java/Rainy.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Rainy {
     private static String name = "Rainy!!!";
 
@@ -5,7 +7,8 @@ public class Rainy {
         System.out.println("Hello! I am RAINY - Responsive, Automated, Intelligence Network for You.");
         System.out.println("What can I do for you on this marvelous day?");
         System.out.println("\n");
-        System.out.println("\n");
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
         System.out.println("Goodbye! Have a nice day ahead!!");
     }
 }
