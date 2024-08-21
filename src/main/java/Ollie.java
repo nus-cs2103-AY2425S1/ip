f22ba5ba87c7928307eed187a8d7167aebce3100
NgZiXin <ngzixin02@gmail.com>
2024-08-21 13:43:20 +0800

    Added Echo Functionality

--- src/main/java/Ollie.java
+++ src/main/java/Ollie.java
@@ -1,14 +1,32 @@
-public class Ollie {
+import java.util.Scanner;
 
+public class Ollie {
     // Private Types
     private static final String NAME = "Ollie";
-    private static final String DIVIDER = "____________________________________________________________\n";
+    private static final String DIVIDER = "____________________________________________________________";
 
     public static void main(String[] args) {
-        String greet = String.format("Hello! I'm %s!\n" + "What can I do for you?\n", Ollie.NAME);
-        String exit = "Bye. Hope to see you again soon!\n";
 
-        System.out.println(Ollie.DIVIDER + greet + Ollie.DIVIDER + exit + Ollie.DIVIDER);
+        Scanner scanner = new Scanner(System.in);
+
+        // Skeletal Beginning
+        System.out.println(Ollie.DIVIDER);
+
+        // Greet
+        String greet = String.format("Hello! I'm %s!\nWhat can I do for you?\n%s", Ollie.NAME, Ollie.DIVIDER);
+        System.out.println(greet);
+
+        // Echo
+        Object input = scanner.nextLine();
+        while(!input.equals("bye")) {
+            String echo = String.format("%s\n%s",input.toString(), Ollie.DIVIDER);
+            System.out.println(echo);
+            input = scanner.nextLine();
+        }
+
+        // Exit
+        String exit = String.format("Bye. Hope to see you again soon!\n%s",Ollie.DIVIDER);
+        System.out.println(exit);
     }
 
 }
