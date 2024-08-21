bf4b562c6d0652b4ba590f429c7983b7389dc63c
JumpyJay <iuswtric1909@gmail.com>
2024-08-21 18:07:32 +0800

    Modify Bob.java
    Remove logo string variable
    
    Implemented a switch to better check for input- would be useful when there are more cases
    
    Added a list to store user text input
    
    Created a separate printList method inside the Bob class

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,30 +1,47 @@
-import java.sql.SQLOutput;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bob {
+    private static final String hLine = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String hLine = "____________________________________________________________\n";
         String greeting = "Hello! I'm Bob\nWhat can I do for you?\n";
         String bye = "Bye. Hope to see you again soon!\n";
         Scanner myScanner = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String greetingMessage = hLine + greeting + hLine;
         String byeMessage = hLine + bye + hLine;
+        // Database to store text
+        List<String> todoList = new ArrayList<>();
 
         // Greets user
         System.out.println(greetingMessage);
         // Await user input
         String userInput = myScanner.nextLine();
-        // Loop when user input != "bye"
-        while (!userInput.toLowerCase().strip().equals("bye")) {
-            System.out.println(hLine + userInput + "\n" + hLine);
-            userInput = myScanner.nextLine();
-        }
+        // Loop for user input
+        do {
+            switch (userInput) {
+                case "list":
+                    printList(todoList);
+                    userInput = myScanner.nextLine();
+                    break;
+                default:
+                    todoList.add(userInput);
+                    System.out.println(hLine + "added: " + userInput + "\n" + hLine);
+                    userInput = myScanner.nextLine();
+            }
+        } while (!userInput.toLowerCase().strip().equals("bye"));
+
         // User input == "bye"
-        System.out.println(byeMessage);
+        System.out.print(byeMessage);
+    }
+
+    // Method to print todolist
+    private static void printList(List<String> todoList) {
+        System.out.print(hLine);
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(i+1 + ". " + todoList.get(i));
+        }
+        System.out.println(hLine);
     }
 }
