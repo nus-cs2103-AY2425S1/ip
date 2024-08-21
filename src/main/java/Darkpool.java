ae971ef610ad2dedd3e8154940899a9d02d25241
arnav-goel10 <e1130141@u.nus.edu>
2024-08-21 20:31:31 +0800

    Change text colour

--- src/main/java/Darkpool.java
+++ src/main/java/Darkpool.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Darkpool {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "it’s darkpool. what twisted reason dragged me into your misery?\n" +
-                "____________________________________________________________\n" +
-                "contact me again and you'll regret it.\n" +
-                "____________________________________________________________\n");
+        final String greeting = "it’s darkpool. what twisted reason dragged me into your misery?";
+        final String bye = "contact me again and you'll regret it.";
+
+        output(greeting);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "bye" -> {
+                    output(bye);
+                    scanner.close();
+                    return;
+                }
+                default -> output(userInput);
+            }
+        }
+
+    }
+
+    private static void output(String input) {
+        System.out.println("\u001B[31m\t—————————————————————————————————————————————————————————————————\n\t" + input + "\n" + "\t—————————————————————————————————————————————————————————————————\u001B[0m");
     }
 }
