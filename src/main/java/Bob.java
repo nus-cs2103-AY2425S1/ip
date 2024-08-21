06928197d75bd796b2319feb068abbef1083b52c
ITZXITZX <isaacteozixian@hotmail.com>
2024-08-22 00:38:33 +0800

    Updated Bob class to echo user input.
    Bob class could only print a standard output.
    
    Add 1 main chat function and 1 print helper function.
    
    Extracting common print output format reduces repeated manual code that
    may give inconsistent output. Chat is now responsive to user input.
    
    Let's,
    * include a chat function that echos user input.
    * include a helper print function that formats the printed output.

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * This is a chatbot class named Bob.
+ */
 public class Bob {
+
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + "Hello! I'm Bob\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(logo);
+        String welcome = "Hello! I'm Bob\n"
+                + "\tWhat can I do for you?";
+        Bob.printLines(welcome);
+        Bob bob = new Bob();
+        bob.chat();
+    }
+
+    /**
+     * This is a chat function by Bob.
+     */
+    void chat() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        while (!input.equals("bye")) {
+            Bob.printLines(input);
+            input = scanner.next();
+        }
+        printLines("Bye. Hope to see you again soon!bye");
+    }
+
+    /**
+     * Prints the text input with lines above and below it.
+     * @param text The user input into the chatbot
+     */
+    static void printLines(String text) {
+        String textToPrint = "\t____________________________________________________________\n"
+                + "\t"
+                + text
+                + "\n"
+                + "\t____________________________________________________________\n";
+        System.out.println(textToPrint);
     }
 }
