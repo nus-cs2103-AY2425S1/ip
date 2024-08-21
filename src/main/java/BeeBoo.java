33cf59d8c4ed29653282b09689ccbea57afa6b55
rubinnn <e1155263.u.nus.edu>
2024-08-21 10:18:31 +0800

    Level-5

--- src/main/java/BeeBoo.java
+++ src/main/java/BeeBoo.java
@@ -55,72 +55,117 @@ public class BeeBoo {
         System.out.println();
     }
 
+    //To create ToDos
+    private void createToDo(String text) throws NoDescriptionException {
+        String description = text.substring(4).trim();
+        if (description.length() == 0) {
+            throw new NoDescriptionException("No description");
+        }
+        chatBox(addList(new ToDos(description)));
+    }
+
+    //To create Deadlines
+    private void createDeadlines(String text) throws NoDescriptionException {
+        int descriptionEnd = text.indexOf('/');
+        String description = text.substring(9, descriptionEnd - 1).trim();
+        if (descriptionEnd == -1 || description.isEmpty()) {
+            throw new NoDescriptionException("No description");
+        }
+        String date = text.substring(descriptionEnd + 1);
+        chatBox(addList(new Deadlines(description, date)));
+    }
+
+    //Creates events
+    private void createEvent(String text) throws NoDescriptionException {
+        int descriptionEnd = text.indexOf('/');
+        String description = text.substring(6, descriptionEnd - 1);
+        if (descriptionEnd == -1 || description.isEmpty()) {
+            throw new NoDescriptionException("No description");
+        }
+        String dateSubstring = text.substring(descriptionEnd + 6);
+        int startDateEnd = dateSubstring.indexOf('/');
+        String startDate = dateSubstring.substring(0, startDateEnd - 1);
+        String endDate = dateSubstring.substring(startDateEnd + 4);
+        chatBox(addList(new Events(description, startDate, endDate)));
+    }
+
+    //Checks the command the user uses and returns a number for chatbot to check against
+    private int checkCommand(String text) {
+        if (text.startsWith("mark")) {
+            return 1;
+        } else if (text.startsWith("unmark")) {
+            return 2;
+        } else if (text.equals("list")) {
+            return 3;
+        } else if (text.startsWith("deadline") || text.startsWith("event") || text.startsWith("todo")) {
+            return 4;
+        } else {
+            return -1;
+        }
+    }
+
+    //Creates the tasks
+    private void createTasks(String text) throws NoDescriptionException {
+        if (text.startsWith("deadline")) {
+            createDeadlines(text);
+        } else if (text.startsWith("event")) {
+            createEvent(text);
+        } else {
+            createToDo(text);
+        }
+    }
+
+    //Handles the command the user uses
+    private void handleCommands(String text) throws InvalidCommandException, NoDescriptionException {
+        int commandCheck = checkCommand(text);
+        if (commandCheck == 1) {
+            //Command is mark so chatbot marks the indexed item
+            String number = "";
+            for (int i = 5; i < text.length(); i++) {
+                number = number + text.charAt(i);
+            }
+            int index = Integer.parseInt(number);
+            markDone(index - 1);
+        } else if (commandCheck == 2) {
+            //Command is unmark so the chatbot unmarks the indexed item
+            String number = "";
+            for (int i = 7; i < text.length(); i++) {
+                number += text.charAt(i);
+            }
+            int index = Integer.parseInt(number);
+            unmarkDone(index - 1);
+        } else if (commandCheck == 3) {
+            //Command is list so the chatbot lists out the items
+            chatBox(produceList());
+        } else if (commandCheck == 4) {
+            //Command is task creation so the chatbot creates tasks
+            createTasks(text);
+        } else {
+            //Unknown command so the chatbot throws invalid command exception
+            throw new InvalidCommandException("Invalid Command");
+        }
+    }
+
     public static void main(String[] args) {
-        //Initialise the chatbot and scanner
+
+        //Initialise the chatbot
         BeeBoo beeBoo = new BeeBoo();
         Scanner input = new Scanner(System.in);
+        String text;
 
-        //Getting user input
         beeBoo.chatBox("Hello! I'm BeeBoo\nWhat can i do for you?");
-        String text = input.nextLine().trim().toLowerCase();
+        text = input.nextLine().trim().toLowerCase();
 
-        //Gets user input till user types bye
+        //Prompts user while user doesn't enter bye
         while (!text.equals("bye")) {
-            if (text.equals("list")) {
-                //Produces list when user types list
-                beeBoo.chatBox(beeBoo.produceList());
-            } else if (text.startsWith("mark")) {
-                //Getting the index from the user input
-                String number = "";
-                for (int i = 5; i < text.length(); i++) {
-                    number = number + text.charAt(i);
-                }
-                int index = Integer.parseInt(number);
-                //Marking item at index as completed
-                beeBoo.markDone(index - 1);
-            } else if (text.startsWith("unmark")) {
-                String number = "";
-                for (int i = 7; i < text.length(); i++) {
-                    number = number + text.charAt(i);
-                }
-                int index = Integer.parseInt(number);
-                //Marking item at index as not done
-                beeBoo.unmarkDone(index - 1);
-
-            } else if (text.startsWith("deadline")) {
-                //Getting the ending index of the description ending at /
-                int descriptionEnd = text.indexOf('/');
-                //Creating a substring of description
-                String description = text.substring(9, descriptionEnd - 1);
-                //Creating substring of date
-                String date = text.substring(descriptionEnd + 1, text.length());
-                //Adding Deadline to list and then chatboxing it
-                beeBoo.chatBox(beeBoo.addList(new Deadlines(description, date)));
-
-            } else if (text.startsWith("event")) {
-                //Getting the ending index of the description ending at /
-                int descriptionEnd = text.indexOf('/');
-                //Creating substring of description
-                String description = text.substring(6, descriptionEnd - 1);
-
-                //Creating Date substring
-                String dateSubstring = text.substring(descriptionEnd + 6, text.length());
-
-                int startDateEnd = dateSubstring.indexOf('/');
-                //Creating startDate substring
-                String startDate = dateSubstring.substring(0, startDateEnd - 1);
-                //Creating endDate substring
-                String endDate = dateSubstring.substring(startDateEnd + 4, dateSubstring.length());
-
-                //Adding Event to list and then chatboxing it
-                beeBoo.chatBox(beeBoo.addList(new Events(description, startDate, endDate)));
-
-            } else {
-                //Creating substring of description
-                String description = text.substring(5, text.length());
-                //Adding ToDo to list and then chatboxing it
-                beeBoo.chatBox(beeBoo.addList(new ToDos(description)));
+            try {
+                beeBoo.handleCommands(text);
+            } catch (InvalidCommandException e) {
+                beeBoo.chatBox("Invalid Command!Me no understand");
+            } catch (NoDescriptionException e) {
+                beeBoo.chatBox("No description silly");
             }
+
             text = input.nextLine().trim().toLowerCase();
         }
         beeBoo.chatBox("Bye. Hope to see you again soon!");
