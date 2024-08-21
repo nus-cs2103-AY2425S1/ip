c59d46e78ede1c581bb5b0d3a58c703a4c9f18dd
Xan <iamxanng@gmail.com>
2024-08-22 00:24:45 +0800

    added echo

--- src/main/java/Murphy.java
+++ src/main/java/Murphy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Murphy {
     public static void main(String[] args) {
         /*
@@ -8,11 +10,22 @@ public class Murphy {
                     + "|____/ \\__,_|_|\\_\\___|\n";
             System.out.println("Hello from\n" + logo);
         */
-
         System.out.println("________________");
         System.out.println("Hello! I'm Murphy");
         System.out.println("What can I do for you?");
         System.out.println("________________");
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                Murphy.bye();
+                scanner.close();
+                return;
+            }
+            System.out.println(input);
+        }
+    }
+    private static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("________________");
     }
