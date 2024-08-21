6593124e5364f4c02ff0ae00e49b4246de212433
OrangeCatLoves <e1121707@u.nus.edu>
2024-08-21 15:24:26 +0800

    Level-3 v1

--- src/main/java/TheOrangeRatchetCat.java
+++ src/main/java/TheOrangeRatchetCat.java
@@ -9,23 +9,60 @@ public class TheOrangeRatchetCat {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        List<String> items = new ArrayList<>();
+        List<Task> items = new ArrayList<>();
         int index = 1;
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine(); // Reads a line of text
+        // Add some error checking in case index > len of list
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                for (String item : items) {
-                    System.out.println(index + ". " + item);
+                for (Task item : items) {
+                    System.out.println(index + ".[" + item.getStatusIcon() + "] " + item.description);
                     index++;
                 }
+                index = 1;
                 input = scanner.nextLine(); // Reads the next line of input text again
+                continue;
+            }
+            if (input.startsWith("mark")) {
+                String numberPart = input.substring(4).trim();
+                try {
+                    int taskIndex = Integer.parseInt(numberPart) - 1;
+                    Task markingTask = items.get(taskIndex);
+                    markingTask.isDone = true;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + markingTask.getStatusIcon() + "] " + markingTask.description);
+                    System.out.println("____________________________________________________________");
+                    input = scanner.nextLine(); // Reads the next line of input text again
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Might want to reconsider your action. Please Try Again");
+                    input = scanner.nextLine(); // Reads the next line of input text again
+                }
+                continue;
+            }
+            if (input.startsWith("unmark")) {
+                String numberPart = input.substring(6).trim();
+                try {
+                    int taskIndex = Integer.parseInt(numberPart) - 1;
+                    Task markingTask = items.get(taskIndex);
+                    markingTask.isDone = false;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[" + markingTask.getStatusIcon() + "] " + markingTask.description);
+                    System.out.println("____________________________________________________________");
+                    input = scanner.nextLine(); // Reads the next line of input text again
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Might want to reconsider your action. Please Try Again");
+                    input = scanner.nextLine(); // Reads the next line of input text again
+                }
                 continue;
             }
             System.out.println("____________________________________________________________");
             System.out.println("added " + input);
             System.out.println("____________________________________________________________");
-            items.add(input);
+            Task task = new Task(input);
+            items.add(task);
             input = scanner.nextLine(); // Reads the next line of input text again
         }
         System.out.println("____________________________________________________________");
