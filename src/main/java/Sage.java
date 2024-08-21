2ad891d6b203cc39c4dce9ab339ff16fcbc8922b
AmadeusEka <00amadeus@gmail.com>
2024-08-21 17:37:01 +0800

    Add support for add list

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sage {
     public static final String NAME = "Sage";
     public static final String HORIZONTAL_LINE = "_________________________________________________";
 
-    public static void segmentedText(String text) {
-        String indentedText = text.indent(4);
-        System.out.println(HORIZONTAL_LINE + "\n" + indentedText + HORIZONTAL_LINE);
-    }
-
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        segmentedText("Hello! I'm " + NAME + "\nWhat can i do for you?");
+        List<String> list = new ArrayList<>();
+
+        segmentedText(String.format("Hello! I'm %s \nWhat can i do for you?", NAME));
 
         while (true) {
             String echo = userInput.nextLine();
@@ -19,9 +18,22 @@ public class Sage {
             if (echo.equalsIgnoreCase("bye")) {
                 segmentedText("Bye. Hope to see you again soon!");
                 break;
+            } else if (echo.equals("list")) {
+                StringBuilder result = new StringBuilder();
+
+                for (int i = 0; i < list.size(); i++) {
+                    result.append(String.format("%d. %s\n", i + 1, list.get(i)));
+                }
+                segmentedText(String.valueOf(result));
             } else {
-                segmentedText(echo);
+                list.add(echo);
+                segmentedText("added: " + echo);
             }
         }
+    }
+
+    public static void segmentedText(String text) {
+        String indentedText = text.indent(4);
+        System.out.println(HORIZONTAL_LINE + "\n" + indentedText + HORIZONTAL_LINE);
     }
 }
