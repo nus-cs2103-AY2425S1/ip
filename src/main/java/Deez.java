5943de5b74eff04d9f85efab6930975c6eb4125c
Sean Lim <seanlimdev856@gmail.com>
2024-08-21 17:33:57 +0800

    Refactor `Deez.say`, don't say anything if no input

--- src/main/java/Deez.java
+++ src/main/java/Deez.java
@@ -1,14 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class Deez {
-    static void say(String msg) {
-        System.out.println("> " + msg);
+    static void say(String... msgs) {
+        System.out.println("____________________________________________________________");
+        for (String msg: msgs) {
+            System.out.println("> " + msg);
+        }
+        System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String CMD_bye = "bye";
-        String seperator = "____________________________________________________________";
         String logo = """
                     ____  _________________
                    / __ \\/ ____/ ____/__  /
@@ -18,19 +22,18 @@ public class Deez {
                                           \s
                 """;
         System.out.println(logo);
-        System.out.println(seperator);
-        Deez.say("Hello! I'm Deez!");
-        Deez.say("What can I do you for?");
+        Deez.say("Hello! I'm Deez!", "What can I do you for?");
         while (1 == 1) {
             String str = br.readLine();
+            if (str.isEmpty()) {
+                continue;
+            }
             if (str.equals(CMD_bye)) {
                 break;
             }
             Deez.say(str);
         }
-        System.out.println(seperator);
         Deez.say("Bye. Hope to see you soon!");
-        System.out.println(seperator);
     }
 
 }
