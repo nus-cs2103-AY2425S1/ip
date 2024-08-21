11fe66233e9689fcc77ff8fcaa89519caafd7c29
ritvi12 <ritvi0205@gmail.com>
2024-08-21 23:35:27 +0800

    Level 1: Echo

--- src/main/java/Pixy.java
+++ src/main/java/Pixy.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Pixy {
+
+    public static String inputCommand() {
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
+    }
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Pixy \n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+                "____________________________________________________________\n");
+        String command = inputCommand();
+        while (!command.equalsIgnoreCase("Bye")) {
+            System.out.println("____________________________________________________________\n" + command + "\n" +
+                    "____________________________________________________________\n");
+            command = inputCommand();
+        }
+        System.out.println("____________________________________________________________\n" + "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
     }
 }
