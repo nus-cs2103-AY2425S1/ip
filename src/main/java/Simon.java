f74779694e90124d11254cf3b6790a7245c0c8bd
yuvrajaryan <142132073+yuvrajaryan@users.noreply.github.com>
2024-08-21 18:37:42 +0800

    Added echo functionality

--- src/main/java/Simon.java
+++ src/main/java/Simon.java
@@ -1,18 +1,24 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Simon {
     final String HOR_LINE = "\t____________________________________________________________\n";
-    final String WLC_MSG = " Hello! I'm Simon \n" +
-            " What can I do for you?\n";
+    final String WLC_MSG = "Hello! I'm Simon \n" +
+            " \tWhat can I do for you?";
     final String EXT_MSG = " Bye. Hope to see you again soon!";
     private String printMessage(String msg) {
         return HOR_LINE + "\t" + msg + "\n" + HOR_LINE;
     }
     public void run() {
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm [YOUR CHATBOT NAME]\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.print(printMessage(WLC_MSG));
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (!(input = sc.nextLine().trim()).equals("bye")) {
+            String output = printMessage(input);
+            System.out.println(output);
+
+        }
+        System.out.println(printMessage(EXT_MSG));
     }
     public static void main(String[] args) {
         new Simon().run();
