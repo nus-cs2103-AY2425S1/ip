47bb38c6809a62c0cc6fd244e1401e215300da84
Qiao Yi <limqiaoyi141@gmail.com>
2024-08-21 23:24:10 +0800

    Implemented skeletal version

---
+++ src/main/java/Sigma.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class Sigma {
+    public static void main(String[] args) {
+        String welcomeMessage = "Hello! I'm Sigma \nWhat can I do for you? \nBye. See you soon!";
+        System.out.println(welcomeMessage);
+    }
+}
