f351d0295839a040dadd98549f846042411e1bb6
waihongteh <tehwaihong1126@gmail.com>
2024-08-21 11:57:42 +0800

    Finish integration with todo, deadline and event for chatbot

--- src/main/java/Waterfall.java
+++ src/main/java/Waterfall.java
@@ -29,6 +29,7 @@ public class Waterfall {
                     break;
                 case "list":
                     System.out.println(" ".repeat(indentSpace) + "___________________________________________________________");
+                    System.out.println(" ".repeat(indentSpace) + "Here's the tasks in your waterfall hualalala");
                     for (int i = 0; i < num; i++) {
                         Task task = taskList[i];
                         if (task != null) {
@@ -78,11 +79,57 @@ public class Waterfall {
                                 + "____________________________________________________________\n").indent(indentSpace + 1);
                         System.out.println(unmarkResponse);
                     } else {
-                        String echoString = ("____________________________________________________________\n"
-                                + "added: " + nextInput + "\n"
-                                + "____________________________________________________________\n").indent(indentSpace);
-                        taskList[num] = new Task(nextInput);
+                        Task newTask;
+                        if (nextInput.startsWith("todo ")) {
+                            newTask = new ToDo(nextInput.substring(5));
+                        } else if (nextInput.startsWith("deadline ")) {
+                            int index = nextInput.indexOf("/");
+                            String title = nextInput.substring(9, index);
+                            String description = nextInput.substring(index + 1);
+                            if (!description.startsWith("by ")) {
+                                System.out.println("unrecognised comment: " + description);
+                                break;
+                            }
+                            newTask = new Deadline(title, description.substring(3));
+                        } else if (nextInput.startsWith("event ")) {
+                            String[] inputs = nextInput.split("/");
+                            if (inputs.length != 3) {
+                                System.out.println("invalid event format: An event must contain only from and to comments");
+                                break;
+                            }
+                            String title = inputs[0].substring(6);
+                            String from;
+                            String to;
+                            if (inputs[1].startsWith("from ")) {
+                                from = inputs[1].substring(5);
+                                if (inputs[2].startsWith("to ")) {
+                                    to = inputs[2].substring(3);
+                                } else {
+                                    System.out.println("invalid format: missing to comment");
+                                    break;
+                                }
+                            } else if (inputs[1].startsWith("to")) {
+                                to = inputs[1].substring(3);
+                                if (inputs[2].startsWith("from ")) {
+                                    from = inputs[2].substring(5);
+                                } else {
+                                    System.out.println("invalid format: missing from comment");
+                                    break;
+                                }
+                            } else {
+                                System.out.println("invalid format: missing to and from comment");
+                                break;
+                            }
+                            newTask = new Event(title, from, to);
+                        } else {
+                            newTask = new Task(nextInput);
+                        }
+                        taskList[num] = newTask;
                         num++;
+                        String echoString = ("____________________________________________________________\n"
+                                + "Nice man, I have added the following task: \n" + newTask.toString() + "\n"
+                                + "Now the waterfall has " + num + " tasks flowing! \n"
+                                + "____________________________________________________________\n").indent(indentSpace);
                         System.out.println(echoString);
                     }
             }
