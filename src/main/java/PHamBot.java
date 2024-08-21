d9d8cb6709565805cc767ea20d2a3247f2707b0d
antgyf <anthonygohyf@gmail.com>
2024-08-22 01:46:23 +0800

    Week 2 level 0

---
+++ src/main/java/PHamBot.java
@@ -0,0 +1,15 @@
+public class PHamBot {
+    public static void main(String[] args) {
+
+        Greet();
+        SayGoodbye();
+    }
+
+    public static void Greet() {
+        System.out.println("Hi! I'm PHamBot\nHappy to be of service to you today!");
+    }
+
+    public static void SayGoodbye() {
+        System.out.println("Hope I was able to help\nGoodbye!");
+    }
+}
