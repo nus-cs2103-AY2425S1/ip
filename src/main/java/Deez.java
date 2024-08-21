723ddf688411e0831f468a2ce092ab803173abdc
Sean Lim <seanlimdev856@gmail.com>
2024-08-21 18:05:58 +0800

    Add list functionality

--- src/main/java/Deez.java
+++ src/main/java/Deez.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Deez {
     static void say(String... msgs) {
@@ -13,6 +14,7 @@ public class Deez {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String CMD_bye = "bye";
+        String CMD_list = "list";
         String logo = """
                     ____  _________________
                    / __ \\/ ____/ ____/__  /
@@ -22,16 +24,32 @@ public class Deez {
                                           \s
                 """;
         System.out.println(logo);
+        ArrayList<String> items = new ArrayList<String>();
         Deez.say("Hello! I'm Deez!", "What can I do you for?");
         while (1 == 1) {
             String str = br.readLine();
             if (str.isEmpty()) {
+                // Don't do anything
                 continue;
             }
             if (str.equals(CMD_bye)) {
+                // Exit
                 break;
             }
-            Deez.say(str);
+            if (str.equals(CMD_list)) {
+                // Print list
+                int cnt = 1;
+                System.out.println("____________________________________________________________");
+                for (String s: items) {
+                    System.out.println(cnt + ". " + s);
+                    cnt+=1;
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+            // Add item to items list
+            items.add(str);
+            Deez.say("Added " + str);
         }
         Deez.say("Bye. Hope to see you soon!");
     }
