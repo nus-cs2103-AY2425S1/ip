6d005e16e5bb59f102a673f1cefb385bfe2dd99d
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-08-21 18:48:32 +0800

    Add printLine method for convenience

--- src/main/java/PandaBot.java
+++ src/main/java/PandaBot.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class PandaBot {
-    public static void main(String[] args) {
+    private static void printLine() {
         String line = "_________________________________________";
+        System.out.println(line);
+    }
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // statically storing tasks
@@ -10,10 +13,10 @@ public class PandaBot {
         int taskCount = 0;
 
         // Simple greeting to the user by PandaBot
-        System.out.println(line);
+        printLine();
         System.out.println("Hello! I'm PandaBot");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        printLine();
 
         // Echo user input until the user inputs "bye"
         while (true) {
@@ -21,24 +24,24 @@ public class PandaBot {
 
             // if user has said "bye", stop echo and exit
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println(line);
+                printLine();
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
+                printLine();
                 break;
             } else if (input.equalsIgnoreCase("list")){
-                System.out.println(line);
+                printLine();
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + taskList[i]);
                 }
-                System.out.println(line);
+                printLine();
             } else if (input.startsWith("mark ")) {
                 int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
                 if (taskNum >= 0 && taskNum < taskCount) {
                     taskList[taskNum].markAsDone();
-                    System.out.println(line);
+                    printLine();
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println(taskList[taskNum]);
-                    System.out.println(line);
+                    printLine();
                 } else {
                     System.out.println("The specified task does not exist");
                 }
@@ -46,10 +49,10 @@ public class PandaBot {
                 int taskNum = Integer.parseInt(input.split(" ")[1]) - 1;
                 if (taskNum >= 0 && taskNum < taskCount) {
                     taskList[taskNum].unmark();
-                    System.out.println(line);
+                    printLine();
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println(taskList[taskNum]);
-                    System.out.println(line);
+                    printLine();
                 } else {
                     System.out.println("The specified task does not exist");
                 }
@@ -58,9 +61,9 @@ public class PandaBot {
                 taskCount++;
 
                 // echo user input
-                System.out.println(line);
+                printLine();
                 System.out.println("added: " + input);
-                System.out.println(line);
+                printLine();
             }
         }
         scanner.close();
