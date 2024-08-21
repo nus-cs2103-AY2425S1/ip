f17058962541cd17997821d25181c763333a5881
LimErnest <e1122433@u.nus.edu>
2024-08-21 16:22:24 +0800

    Added the ability to store text entered by user and display them back when user types in "list"

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Echo {
     public static void main(String[] args) {
+        ArrayList<String> commands = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         String userReply = "";
         System.out.println("-------------------------------------");
@@ -8,17 +10,22 @@ public class Echo {
         System.out.println("-------------------------------------");
         while (!userReply.equals("bye")) {
             userReply = input.nextLine();
-            if (userReply.equals("bye")) {
+            if (userReply.equals("bye")) {   //Terminate chatbot if user types in "bye"
                 System.out.println("-------------------------------------");
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("-------------------------------------");
-            } else {
+            } else if (userReply.equals("list")){ //Iterate through the String array and list out the commands
                 System.out.println("-------------------------------------");
-                System.out.println("\t" + userReply);
+                for(int i = 0; i < commands.size(); i ++) {
+                    System.out.println("\t" + (i + 1) + ". " + commands.get(i));
+                }
+                System.out.println("-------------------------------------");
+            } else { //Add the input provided into the String array and return a message "added:   "
+                System.out.println("-------------------------------------");
+                commands.add(userReply);
+                System.out.println("\tadded: " + userReply);
                 System.out.println("-------------------------------------");
             }
-
         }
-
     }
 }
