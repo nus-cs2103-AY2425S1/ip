633b45f7bbb520e40c933181a066a510ff7cd949
naythee169 <jordan.mitchell.chan@gmail.com>
2024-08-21 19:29:47 +0800

    Add echo, new personality

--- src/main/java/Delphi.java
+++ src/main/java/Delphi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Delphi {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,8 +7,16 @@ public class Delphi {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Delphi.");
-        System.out.println("What can I do for you?\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Delphi, the greatest oracle in all of the classical world.");
+        System.out.println("To quote a very famous person who will arrive a bit later, ask and ye shall find");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) break;
+            else System.out.println("    " + input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
