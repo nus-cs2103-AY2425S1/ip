2fdc6dbdae665f6088adef4cae936705276dcbde
Colin Chiang <colinchiang.cc@gmail.com>
2024-08-21 10:27:28 +0800

    Change greeting and bot name and remove traces of Duke from source code

---
+++ src/main/java/Bob.java
@@ -0,0 +1,10 @@
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm your chatbot, Bob.\n" +
+                "How may I assist you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again!");
+
+    }
+}
