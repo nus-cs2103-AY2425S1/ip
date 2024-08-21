382e0cc00aa6d81a515cbbdb34d36c9d7e596f23
Chen Zi Yi <ziyichen2003@gmail.com>
2024-08-21 16:36:52 +0800

    Update name and greetings of chatbot.

---
+++ src/main/java/Bob.java
@@ -0,0 +1,7 @@
+public class Bob {
+    public static void main(String[] args) {
+        String name = "Bob";
+        System.out.println("Hello! I'm " + name + "!\nWhat can I do for you?");
+        System.out.println("Bye! Hope to see you again :)");
+    }
+}
