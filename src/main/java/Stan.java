9167d2586678befe1631b8170813893c093f0f41
stanleytangzh <111631172+stanleytangzh@users.noreply.github.com>
2024-08-21 11:42:31 +0800

    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Stan.java
+++ src/main/java/Stan.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Stan {
     public static void main(String[] args) {
         String logo = "Stan";
+        String[] tasks = new String[100];
+        int taskCount = 0;
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + logo);
         System.out.println(" What can I do for you today?");
@@ -19,6 +21,16 @@ public class Stan {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
         }
 
