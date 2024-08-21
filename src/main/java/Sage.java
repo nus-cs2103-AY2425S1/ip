ff6ccddd41ada51db92b0a87fa5a23b4a8b88ada
AmadeusEka <00amadeus@gmail.com>
2024-08-21 17:07:15 +0800

    Add support for echo

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Sage {
     public static final String NAME = "Sage";
-    public static final String HORIZONTAL_LINE = "____________________________________________________________";
+    public static final String HORIZONTAL_LINE = "_________________________________________________";
+
+    public static void segmentedText(String text) {
+        String indentedText = text.indent(4);
+        System.out.println(HORIZONTAL_LINE + "\n" + indentedText + HORIZONTAL_LINE);
+    }
 
     public static void main(String[] args) {
-        System.out.println(HORIZONTAL_LINE + "\nHello! I'm " + NAME + "\nWhat can i do for you?\n" + HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
+        Scanner userInput = new Scanner(System.in);
+        segmentedText("Hello! I'm " + NAME + "\nWhat can i do for you?");
 
+        while (true) {
+            String echo = userInput.nextLine();
+
+            if (echo.equalsIgnoreCase("bye")) {
+                segmentedText("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                segmentedText(echo);
+            }
+        }
     }
 }
