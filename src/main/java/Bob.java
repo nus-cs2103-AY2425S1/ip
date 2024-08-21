3d9f02357706351db1169ac7366be1e593c0edf8
keerthigkaarthik <keerthigkaarthik@gmail.com>
2024-08-21 21:40:33 +0800

    Add echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,18 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Bob \nWhat can I do for you?");
-        System.out.println("\n");
+        System.out.println();
+        String s = "";
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            s = input.nextLine();
+            if (s.equals("bye")) {
+                break;
+            }
+            System.out.println(s+"\n");
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
