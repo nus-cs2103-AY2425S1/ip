f7e24d23518ede4acf472aa713a9f16ac3d2aca5
jktang14 <tangboy14@gmail.com>
2024-08-21 15:11:20 +0800

    Add string representation for Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,14 @@
-public class Event {
+public class Event extends Task {
+    private String startDay;
+    private String endDay;
+    public Event(String description, String startDay, String endDay) {
+        super(description);
+        this.startDay = startDay;
+        this.endDay = endDay;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E] %s (from: %s to: %s)", super.toString(), this.startDay, this.endDay);
+    }
 }
