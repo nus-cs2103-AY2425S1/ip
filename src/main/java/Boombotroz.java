aab1f57793c7054e987e610236f4cf9807e952a7
nws321 <nws321@gmail.com>
2024-08-21 15:11:46 +0800

    Echo

--- src/main/java/Boombotroz.java
+++ src/main/java/Boombotroz.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Boombotroz {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Boombotroz\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        String echo = scanner.nextLine();
+        while (!echo.equals("bye")) {
+            System.out.println(echo);
+            echo =  scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
