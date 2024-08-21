9ad226fb8a36d31d02775b274d163705652ee38b
Chua Tse Hui <htcylsg@gmail.com>
2024-08-21 09:37:19 +0800

    enable isDone toggling

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,18 @@ public class Task {
         this.isDone = false;
     }
 
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
     @Override
     public String toString() {
         return this.description;
