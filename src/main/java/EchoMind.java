71260d559662f93c57c2527b26517e85371ce1fd
slye20 <samuellim2011@gmail.com>
2024-08-21 15:37:32 +0800

    Store text and display back when requested

--- src/main/java/EchoMind.java
+++ src/main/java/EchoMind.java
@@ -1,26 +1,50 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EchoMind {
-//    private String[]  =
+    private ArrayList<String> toDoList = new ArrayList<>();
 
-    public void sendMessage(String msg) {
-        System.out.println("[EchoMind] " + msg);
+    public EchoMind() {
+        sendMessage("Hello! I'm EchoMind!");
+        sendMessage("What can I do for you?");
     }
 
-    public static void main(String[] args) {
-        EchoMind em = new EchoMind();
-        em.sendMessage("Hello! I'm EchoMind!");
-        em.sendMessage("What can I do for you?");
+    public void sendMessage(String message) {
+        System.out.println("[EchoMind] " + message);
+    }
 
+    public boolean receiveMessage(String message) {
+        if (message.equalsIgnoreCase("bye")) {
+            sendMessage("Bye. Hope to see you again soon!");
+            return false;
+        } else if (message.equalsIgnoreCase("list")) {
+            if (toDoList.size() == 0) {
+                sendMessage("No todo items yet!");
+                return true;
+            }
+            for (int i = 0; i < toDoList.size(); i++) {
+                sendMessage((i + 1) + ". " + toDoList.get(i));
+            }
+        } else {
+            toDoList.add(message);
+            sendMessage("added: " + message);
+        }
+        return true;
+    }
+
+    public void start() {
         Scanner scanner = new Scanner(System.in);
         String message = "";
         while (true) {
             message = scanner.nextLine().trim();
-            if (message.equalsIgnoreCase("bye")) {
+            if (!receiveMessage(message)) {
                 break;
             }
-            em.sendMessage(message);
         }
-        em.sendMessage("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        EchoMind em = new EchoMind();
+        em.start();
     }
 }
