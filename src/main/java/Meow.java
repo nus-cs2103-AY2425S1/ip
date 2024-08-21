43ca20101f26fd75ce13c139ccd51b92f0d3da2a
Al-ez <meowincat911@gmail.com>
2024-08-21 14:21:49 +0800

    Level-1

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Meow {
     private static String name = "Meow";
+
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("_____________________________________________________________\n" +
                 " Hello! I'm " + name + "\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+                "____________________________________________________________\n");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
     }
 }
