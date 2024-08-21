b4e3b8237aeee2254f2c60a34dbc9bfea8b326d2
stanleytangzh <111631172+stanleytangzh@users.noreply.github.com>
2024-08-21 11:18:44 +0800

    echo user command and exit upon bye command.

--- src/main/java/Stan.java
+++ src/main/java/Stan.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
 public class Stan {
     public static void main(String[] args) {
         String logo = "Stan";
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + logo);
-        System.out.println(" What can I do for you?");
+        System.out.println(" What can I do for you today?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+
+        scanner.close();
+
     }
 }
