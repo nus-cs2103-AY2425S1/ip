406cb75239ae45b80084af5869eb5cff8d1142f8
arnav-goel10 <e1130141@u.nus.edu>
2024-08-21 20:50:40 +0800

    Save user data

--- src/main/java/Darkpool.java
+++ src/main/java/Darkpool.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Darkpool {
     public static void main(String[] args) {
         final String greeting = "it’s darkpool. what twisted reason dragged me into your misery?";
         final String bye = "contact me again and you'll regret it.";
-
+        ArrayList<String> todoList = new ArrayList<String>();
         output(greeting);
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -16,8 +17,19 @@ public class Darkpool {
                     scanner.close();
                     return;
                 }
+                case "list" -> {
+                    StringBuilder temp = new StringBuilder();
+                    for (int i = 0; i < todoList.size(); i++) {
+                        temp.append((i + 1)).append(". ").append(todoList.get(i)+"\n\t");
+                    }
+                    temp.setLength(temp.length() - 2);
+                    output(String.valueOf(temp));
+                    break;
+                }
+
                 default -> {
-                    output(userInput);
+                    todoList.add(userInput);
+                    output("added: " + userInput);
                 }
             }
         }
