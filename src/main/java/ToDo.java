74127a2ea531ec027de30225cbc620fb31b234f1
MAOXIONGKAI <conan7153@gmail.com>
2024-08-21 18:11:22 +0800

    Add ToDo.java for ToDo Task

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,12 @@
+public class ToDo extends Task{
+    public ToDo (String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[T][%s] %s",
+                getStatusIcon(),
+                this.name);
+    }
+}
