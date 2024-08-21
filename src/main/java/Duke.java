c9c3208296f016307b486017a3a09cc185e3c79a
Rahul Agarwal <agarwalrahul1008@gmail.com>
2024-08-21 16:41:49 +0800

    Level 0: Rename, Greet, Exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "Sir Potato";
+        String horizontal_line = "___________________________ \n";
+        System.out.println(horizontal_line);
         System.out.println("Hello from\n" + logo);
+        System.out.println(horizontal_line);
+        System.out.println("What can I do ya for mate");
+        System.out.println(horizontal_line);
+        System.out.println("See ya around brev.\n" + horizontal_line);
     }
 }
