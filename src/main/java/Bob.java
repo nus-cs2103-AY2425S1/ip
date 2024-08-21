024c6c13c1c32298a1450eb6fb2f28e25322b017
Colin Chiang <colinchiang.cc@gmail.com>
2024-08-21 11:24:07 +0800

    Add ability to store text and display them back to user when requested

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,29 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Bob {
+    private static List<String> memory = new ArrayList<String>();
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm your chatbot, Bob.\n" +
+        printBob("Hello! I'm your chatbot, Bob.\n" +
                 "How may I assist you?");
-        System.out.println("____________________________________________________________");
 
         Scanner reader = new Scanner(System.in);
         String response;
 
-
         while (!(response = readUserInput(reader)).equals("bye")) {
-            System.out.println("____________________________________________________________");
-           System.out.println(response);
-            System.out.println("____________________________________________________________");
+
+            if (response.equals("list")) {
+                String list = "";
+                for (int i = 0; i < memory.size() - 1; i++){
+                    list += (i + 1) +": " + memory.get(i) + "\n";
+                }
+                list += memory.size() + ": " + memory.get(memory.size() - 1);
+                printBob(list);
+                continue;
+            }
+
+            memory.add(response);
+            printBob("Added: " + response);
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye.");
-        System.out.println("____________________________________________________________");
+        printBob("Bye.");
     }
 
     private static String readUserInput(Scanner s) {
         System.out.print("> ");
         return s.next();
+    }
+
+    private static void printBob(String s) {
+        System.out.println("____________________________________________________________");
+        System.out.println(s);
+        System.out.println("____________________________________________________________");
     }
 }
