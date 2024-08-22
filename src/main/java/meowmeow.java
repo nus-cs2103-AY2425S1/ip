aec312b91f6a905b4972def0a36bae1cb1f3e52b
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-08-23 04:07:17 +0800

    Level 1. Echo

--- src/main/java/meowmeow.java
+++ src/main/java/meowmeow.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class meowmeow {
     public static void main(String[] args) {
        /* String logo = " ____        _        \n"
@@ -6,7 +7,13 @@ public class meowmeow {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        System.out.println("Hello! I'm meowmeow\n" + "What can I do for you?\n"
-                            + "Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm meowmeow\n" + "What can I do for you?\n");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = s.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
