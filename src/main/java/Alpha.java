d0f33411dd64c6ca14dd17627406c86c3eaf5aee
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2024-08-22 12:01:42 +0800

    Implement Mark as Done status

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -22,16 +22,35 @@ public class Alpha {
                 break;  // Exit the loop
             }
     
-            if (s1.equals("list")) {
-                String echoResponse = "____________________________________________________________ \n"
+            else if (s1.equals("list")) {
+                String echoResponse = "____________________________________________________________ \nHere are the tasks in your list: \n"
                         + storage.listWord() +"\n"
                         + "____________________________________________________________ \n";
                 System.out.println(echoResponse);
             }
             
+            else if (s1.split(" ")[0].equals("mark")) {
+                Integer indexInvolved = Integer.valueOf(s1.split(" ")[1]);
+                String modifiedRecord = storage.modifyOperation(indexInvolved, true);
+                String echoResponse = "____________________________________________________________ \nNice! I've marked this task as done: \n"
+                        + modifiedRecord +"\n"
+                        + "____________________________________________________________ \n";
+                System.out.println(echoResponse);
+            }
+    
+            else if (s1.split(" ")[0].equals("unmark")) {
+                Integer indexInvolved = Integer.valueOf(s1.split(" ")[1]);
+                String modifiedRecord = storage.modifyOperation(indexInvolved, false);
+                String echoResponse = "____________________________________________________________ \nOK, I've marked this task as not done yet:\n "
+                        + modifiedRecord +"\n"
+                        + "____________________________________________________________ \n";
+                System.out.println(echoResponse);
+            }
+            
             else {
-                storage.storeWord(s1);
-                String userInput = storage.lastWord();
+                Task TaskOne = new Task(s1);
+                storage.storeTask(TaskOne);
+                String userInput = storage.lastTask();
                 String echoResponse = "____________________________________________________________ \n"
                         + "added: " + userInput + "\n"
                         + "____________________________________________________________ \n";
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,23 +1,22 @@
 import java.util.ArrayList;
 
 public class Storage {
-    String InputWord;
-    ArrayList<String> UserInputs;
+    ArrayList<Task> TaskLists;
     
     public Storage() {
-        this.UserInputs = new ArrayList<String>();
+        this.TaskLists = new ArrayList<Task>();
     }
     
-    public void storeWord(String InputWord) {
-        this.UserInputs.add(InputWord);
+    public void storeTask(Task newTask) {
+        this.TaskLists.add(newTask);
     }
     
-    public String lastWord() {
-        if (!this.UserInputs.isEmpty()) {
-            if (this.UserInputs.get(this.UserInputs.size() - 1).equals("bye"))
+    public String lastTask() {
+        if (!this.TaskLists.isEmpty()) {
+            if (this.TaskLists.get(this.TaskLists.size() - 1).getDescription().equals("bye"))
                 return "Bye. Hope to see you again soon!";
             else {
-                return this.UserInputs.get(this.UserInputs.size() - 1);
+                return this.TaskLists.get(this.TaskLists.size() - 1).getDescription();
             }
         }
         else {
@@ -27,9 +26,27 @@ public class Storage {
     
     public String listWord() {
         StringBuilder result = new StringBuilder();
-            for (int i =0; i < this.UserInputs.size(); i++) {
-                result.append(i + 1).append(". ").append(this.UserInputs.get(i)).append("\n");
+            for (int i =0; i < this.TaskLists.size(); i++) {
+                result.append(i + 1).append(". ")
+                        .append(this.TaskLists.get(i).getStatusIcon())
+                        .append(" ")
+                        .append(this.TaskLists.get(i).getDescription()).append("\n");
             }
         return result.toString();
     }
+    
+    public String modifyOperation(int index, boolean markStatus) {
+        int normalizedIndex = index -1;
+        StringBuilder result = new StringBuilder();
+        if (markStatus) {
+            this.TaskLists.get(normalizedIndex).markAsDone();
+        } else {
+            this.TaskLists.get(normalizedIndex).markAsUndone();
+        }
+        result.append(this.TaskLists.get(normalizedIndex).getStatusIcon())
+                .append(" ")  // Adding a space here
+                .append(this.TaskLists.get(normalizedIndex).getDescription());
+        return result.toString();
+    }
+    
 }
