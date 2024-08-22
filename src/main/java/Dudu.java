ff9bfedf66e5c5a64bea7ce4ed979e0774d10c89
ngjianyi <ngjianyi2002@gmail.com>
2024-08-22 16:56:51 +0800

    Removed nested classes from main

--- src/main/java/Dudu.java
+++ src/main/java/Dudu.java
@@ -1,48 +1,28 @@
 import java.util.Scanner;
-
+import java.lang.StringBuilder;
 public class Dudu {
     public static void main(String[] args) {
-        class LineWrapper {
-            static final String lineRule = "--------------------------------------------\n";
-            public static String wrap(String content) {
-                return lineRule + content + "\n" + lineRule;
-            }
-        }
-        class Task {
-            boolean marked = false;
-            String name;
-            public Task(String name) {
-                this.name = name;
-            }
-
-            @Override
-            public String toString() {
-                String status = this.marked ? "[X]" : "[ ]";
-                return status + " " + this.name;
-            }
-        }
-        String lineRule = "--------------------------------------------\n";
         String welcomeMessage = LineWrapper.wrap("Hello! I'm dudu\n"
                                                 + "What can I do for you?");
         String goodbyeMessage = LineWrapper.wrap("Bye. Hope to see you again soon!");
 
         System.out.println(welcomeMessage);
+
         Scanner sc = new Scanner(System.in);
         Task[] tasks = new Task[100];
         int count = 0;
         while (sc.hasNextLine()) {
             String message = sc.nextLine();
             if (message.equals("list")) {
-                System.out.println(lineRule);
                 if (count == 0) {
-                    System.out.println("No tasks");
+                    System.out.println(LineWrapper.wrap("No tasks"));
                 } else {
-                    System.out.println("Here are the tasks in your list:");
+                    StringBuilder output = new StringBuilder("Here are the tasks in your list:");
                     for (int i = 0; i < count; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        output.append("\n" + (i + 1) + ". " + tasks[i]);
                     }
+                    System.out.println(LineWrapper.wrap(output.toString()));
                 }
-                System.out.println(lineRule);
             } else if (message.equals("bye")) {
                 break;
             } else if (message.matches("mark \\d")) {
@@ -64,6 +44,7 @@ public class Dudu {
                 System.out.println(LineWrapper.wrap("added: " + message));
             }
         }
+
         System.out.println(goodbyeMessage);
     }
 }
