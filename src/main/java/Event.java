f6e08e4dfdd35ce016eb338b86302f636aba2872
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-08-22 20:05:37 +0800

    Add Event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,16 @@
-public class Event {
+public class Event extends Task{
+    private String fromDate;
+    private String toDate;
+
+    public Event(String name, String fromDate, String toDate) {
+        super(name);
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    @Override
+    public String toString() {
+        String temp = super.toString();
+        return String.format("[E]%s (from %s to: %s)", temp, fromDate, toDate);
+    }
 }
