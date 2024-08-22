1c2f0f6aac05b03f744e413b7ebd27bacc4f8c89
QinHaichen12 <haichensurfs@gmail.com>
2024-08-22 16:13:19 +0800

    Level 3

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -1,12 +1,33 @@
 import java.util.Scanner;
 public class Bottle {
     final static String lineBreak = "\n____________________________________________________________\n";
+    static class Task {
+        private boolean isChecked;
+        private String taskDesc;
 
+        public Task(String taskDesc) {
+            this.taskDesc = taskDesc;
+            this.isChecked = false;
+        }
+        public void mark() {
+            this.isChecked = true;
+        }
+
+        public void unMark() {
+            this.isChecked = false;
+        }
+
+        @Override
+        public String toString() {
+            String box = isChecked ? "[X] " : "[ ] ";
+            return box + taskDesc;
+        }
+    }
     public static void printwithBreak(String str) {
         System.out.println(lineBreak + str + lineBreak);
     }
     public static void main(String[] args) {
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCount = 0;
         String welcomeMsg =
                 " Hello! I'm Bottle\n" +
@@ -27,8 +48,24 @@ public class Bottle {
                     System.out.println(idx + ". " + taskList[i]);
                 }
                 System.out.println(lineBreak);
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    Task task = taskList[taskIndex];
+                    task.mark();
+                    String msg = "Nice! I've marked this task as done:\n";
+                    printwithBreak(msg + task);
+                }
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    Task task = taskList[taskIndex];
+                    task.unMark();
+                    String msg = "OK, I've marked this task as not done yet:\n";
+                    printwithBreak(msg + task);
+                }
             } else {
-                taskList[taskCount] = input;
+                taskList[taskCount] = new Task(input);
                 taskCount++;
                 printwithBreak("added " + input);
             }
