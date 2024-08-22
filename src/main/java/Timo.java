7f34934d7fbcf4baf00651d6ab4fcc8d6043ca17
teamotitan <timothy.tanzijing@gmail.com>
2024-08-22 09:37:24 +0800

    level 1

--- src/main/java/Timo.java
+++ src/main/java/Timo.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Timo {
     public static void main(String[] args) {
         System.out.println("----------------------------\n");
         System.out.println("Hello! I'm Timo\nWhat can I do for you?\n");
-        System.out.println("----------------------------\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("----------------------------\n");
+
+        Scanner echo = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            input = echo.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println("----------------------------\n");
+                System.out.println(input);
+                System.out.println("----------------------------\n");
+            } else {
+                System.out.println("----------------------------\n");
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("----------------------------\n");
+                break;
+            }
+        }
+
+
 
     }
 }
