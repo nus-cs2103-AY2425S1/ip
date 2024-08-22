e2cc7fe36b907703cc4858075a61248535c8181b
SamuelFoo <fooenzesamuel@gmail.com>
2024-08-22 23:21:14 +0800

    Semi-automate testing with multiple input files

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -7,9 +7,14 @@ then
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
+if [ -d "./debug" ]
 then
-    rm ACTUAL.TXT
+    if [ ! -z "$( ls -A './debug' )" ]
+    then
+        rm -r ./debug/*
+    fi
+else
+    mkdir ./debug
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -19,20 +24,43 @@ then
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duck < input.txt > ACTUAL.TXT
-
-# convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
-
-# compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
-if [ $? -eq 0 ]
+# make a temp folder
+if [ ! -d "./temp" ]
 then
-    echo "Test result: PASSED"
-    exit 0
-else
-    echo "Test result: FAILED"
-    exit 1
+    mkdir ./temp
 fi
+
+function unit_test {
+    local input_file_path=./input/input_$1.txt
+    local expected_file_path=./expected/EXPECTED_$1.txt
+    local debug_file_path=./debug/ACTUAL.txt
+
+    # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.txt
+    java -classpath ../bin Duck < $input_file_path > temp/ACTUAL.txt
+
+    # convert to UNIX format
+    cp $expected_file_path temp/EXPECTED-UNIX.txt
+    dos2unix temp/ACTUAL.txt temp/EXPECTED-UNIX.txt
+
+    # compare the output to the expected output
+    diff temp/ACTUAL.txt temp/EXPECTED-UNIX.txt
+    if [ $? -eq 0 ]
+    then
+        echo "Test for $1: PASSED"
+    else
+        echo "Test for $1: FAILED"
+        # Copy the actual and expected outputs for the failed tests to the debug folder
+        cp temp/ACTUAL.txt debug/ACTUAL_$1.txt
+        cp temp/EXPECTED-UNIX.txt debug/EXPECTED_$1.txt
+    fi
+}
+
+for input_file in ./input/input_*.txt; do
+    test_name=$(echo $input_file | sed -n "s/^.*_\(\S*\)\..*$/\1/p")
+    unit_test $test_name
+done
+
+# remove the temp folder
+rm -r ./temp
+
+exit 0
