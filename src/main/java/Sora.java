48d5553e7ceb45727084d3d07fa664bedc94c3f3
S-K-Y-Light <s84915656@gmail.com>
2024-08-22 20:24:57 +0800

    Rename Duke.java to Sora.java Change Sora greeting & exit message

---
+++ src/main/java/Sora.java
@@ -0,0 +1,9 @@
+public class Sora {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Sora!\nWhat can I do for you?\n";
+        System.out.println(greeting);
+
+        String exit = "Bye. Hope to see you again soon!\n";
+        System.out.println(exit);
+    }
+}
