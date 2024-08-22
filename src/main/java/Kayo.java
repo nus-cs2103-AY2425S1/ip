2f599a45e1a879355e0c747cda21d84365a7e2cd
arnav12344 <64593433+arnav12344@users.noreply.github.com>
2024-08-22 16:41:58 +0800

    Added skeleton code

---
+++ src/main/java/Kayo.java
@@ -0,0 +1,8 @@
+public class Kayo {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! I'm Kayo! ");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. I hope to see you again soon!");
+    }
+}
