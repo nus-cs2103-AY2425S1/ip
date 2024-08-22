a993b46ccea2474b22fb3fba4403fb7b9ea5c7ad
Chen Lei Yu <128605764+ChenLeiyu@users.noreply.github.com>
2024-08-22 20:07:44 +0800

    Add basic chatbot input
    - Add input
    - Add chatbot termination on "bye"

--- src/main/java/Froggy.java
+++ src/main/java/Froggy.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Froggy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
         String greeting = "Hello! I'm Froggy!\n"
                 + "What can I do for you?\n"
                 + "_______________________________\n";
         String exit = "Bye. Hope to see you again soon!\n"
                 + "_______________________________\n";
 
-        System.out.println(greeting + exit);
+        System.out.println(greeting);
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(exit);
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
