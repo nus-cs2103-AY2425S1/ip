09cdc778ffef22bf11dda5a702a5a724ee1066b9
kahsuann <kahsuan@hotmail.sg>
2024-08-22 18:14:10 +0800

    Echo

--- src/main/java/Susan.java
+++ src/main/java/Susan.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Susan {
     public static void main(String[] args) {
         // Greet
         System.out.println("Hi there, Susan's at your service!");
         System.out.println("How may I assist you?");
+
+        // Read user input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        // Echo
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
         // Exit
         System.out.println("Good bye, slay the day!");
     }
