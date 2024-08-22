d1ffec8ea63d6895d54084e14f972f157d93a505
Ivan <ivanjerrickkoh@gmail.com>
2024-08-22 10:47:02 +0800

    BotimusPrime.java: Bot now echoes commands entered by user

--- src/main/java/BotimusPrime.java
+++ src/main/java/BotimusPrime.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class BotimusPrime {
     public static void main(String[] args) {
-        String name = "BotimusPrime";
+        Scanner sc = new Scanner(System.in);
 
-        String message =
+        String name = "BotimusPrime";
+        String greetingMessage =
                 "____________________________________________________________\n" +
                 " Hello! I'm " + name + "\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
+        String byeMessage =
+                "____________________________________________________________\n" +
+                " Bye! Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
 
-        System.out.println(message);
+        System.out.println(greetingMessage);
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println("\n");
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(byeMessage);
+                break;
+            } else {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                        input + "\n" +
+                        "____________________________________________________________\n");
+            }
+        }
+
+        sc.close();
     }
 }
