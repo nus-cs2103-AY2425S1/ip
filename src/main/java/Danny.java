be467764911ff91cc0cefe743bb760045d80f10d
TheRareFox <zhang.yuxiang290902@gmail.com>
2024-08-22 23:23:11 +0800

    - Added "add" command to add items to list - Added "list" command to display list

--- src/main/java/Danny.java
+++ src/main/java/Danny.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 public class Danny {
@@ -8,10 +10,26 @@ public class Danny {
         System.out.println("What can I do for you?");
         System.out.println(seperator);
         Scanner input = new Scanner(System.in);
-        String in = input.next();
+        List<String> arr = new ArrayList<String>();
+        String in = input.nextLine();
         while(!Objects.equals(in, "bye")){
-            System.out.println(in);
-            in = input.next();
+            System.out.println(seperator);
+            switch (in.split(" ")[0]){
+                case "list":
+                    for (int i = 0; i < arr.size(); i++) {
+                        System.out.println((i+1) + ". " + arr.get(i));
+                    }
+                    break;
+                case "add":
+                    arr.add(in);
+                    System.out.println("added: " + in);
+                    break;
+                default:
+                    System.out.println(in);
+                    break;
+            }
+            System.out.println(seperator);
+            in = input.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(seperator);
