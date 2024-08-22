e1fd1b902ab48128a445b9545d0cd080746565e6
Karthikeyan <145328000+SKarthikeyan28@users.noreply.github.com>
2024-08-22 14:43:40 +0800

    Changed Duke to Tars and welcome message

---
+++ src/main/java/Tars.java
@@ -0,0 +1,9 @@
+public class Tars {
+    public static void main(String[] args) {
+        System.out.println("_____________________________________________");
+        System.out.println("Hello! I'm Tars\n" + "What can I do for you");
+        System.out.println("_____________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________________________");
+    }
+}
