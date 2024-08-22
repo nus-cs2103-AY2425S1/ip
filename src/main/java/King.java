1cc39af3502f1aa793937a2a9b89c8aeae2d92e4
ryenl <ryanljh9@gmail.com>
2024-08-22 13:45:40 +0800

    Level-0

---
+++ src/main/java/King.java
@@ -0,0 +1,17 @@
+public class King {
+    public static void main(String[] args) {
+        String logo = " _  __ _             \n" +
+                "| |/ /(_)_ __   __ _ \n" +
+                "| ' / | | '_ \\ / _` |\n" +
+                "| . \\ | | | | | (_| |\n" +
+                "|_|\\_\\|_|_| |_|\\__, |\n" +
+                "               |___/ ";
+        System.out.println("Greetings from the\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("How may I assist you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye! Have a pleasant day my Queen.");
+        System.out.println("____________________________________________________________");
+
+    }
+}
