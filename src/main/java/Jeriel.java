1662d0d86aaf3d8d0e09a5ad64b1b6a5a432f339
Nimastic <jerielchanzy@gmail.com>
2024-08-23 05:19:56 +0800

    First Commit

---
+++ src/main/java/Jeriel.java
@@ -0,0 +1,20 @@
+public class Jeriel {
+    public static void main(String[] args) {  
+        String logo = "     _  ____  ___   _  ____  _      \n"
+                    + "    | || ___|| _ \\| || ___|| |     \n"
+                    + "    | |||___ ||_> || |||___ | |     \n"
+                    + " _  | || ___||  _/ | || ___|| |     \n"
+                    + "| |_| |||___ ||\\  | |||___ | |____ \n"
+                    + " \\___/|____||| \\ |_||____||______|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Jeriel");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
