90ac21307b0506da86eb9e52a62e566683c3bcde
Edsel-Tan <riwk2015@gmail.com>
2024-08-22 21:46:37 +0800

    added mark and unmark functionality

--- src/main/java/Makima.java
+++ src/main/java/Makima.java
@@ -6,6 +6,9 @@ public class Makima {
     public static final String LINE_SEPERATOR = "__________________";
     private boolean running = true;
     private ArrayList<String> lines = new ArrayList<String>();
+    private ArrayList<Boolean> isDone = new ArrayList<>();
+    private String userInput = "";
+    private Scanner sc;
 
     public void greeting() {
         System.out.println("Yahallo! I'm your friendly chatbot, Makima!");
@@ -18,17 +21,51 @@ public class Makima {
         System.out.println(LINE_SEPERATOR);
     }
 
+    public String displayIfDone(int i) {
+        if (isDone.get(i)) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
+    }
+
+    public void displayList() {
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println(i+1 + ":" + displayIfDone(i) + " " + lines.get(i));
+        }
+    }
+
+    public int getListIndex() {
+        while (true) {
+            getInput();
+            try {
+                int index = Integer.parseInt(userInput);
+                if (index < 1 || index > lines.size()) {
+                    System.out.println("Invalid index");
+                } else {
+                    return index-1;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number!");
+            }
+        }
+    }
+
+    public void getInput() {
+        userInput = sc.nextLine();
+        System.out.println(LINE_SEPERATOR);
+    }
+
     public static void main(String[] args) {
         new Makima();
     }
 
     public Makima() {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         greeting();
 
         while (running) {
-            String userInput = sc.nextLine();
-            System.out.println(LINE_SEPERATOR);
+            getInput();
 
             switch (userInput) {
                 case "bye":
@@ -36,15 +73,31 @@ public class Makima {
                     break;
 
                 case "list":
-                    for (int i = 0; i < lines.size(); i++) {
-                        System.out.println(i + ": " + lines.get(i));
-                    }
+                    displayList();
                     System.out.println(LINE_SEPERATOR);
                     break;
+
+                case "mark":
+                    displayList();
+                    System.out.println("Which item would you like to mark?");
+                    isDone.set(getListIndex(), true);
+                    System.out.println("Done!");
+                    System.out.println(LINE_SEPERATOR);
+                    break;
+
+                case "unmark":
+                    displayList();
+                    System.out.println("Which item would you like to unmark?");
+                    isDone.set(getListIndex(), false);
+                    System.out.println("Done!");
+                    System.out.println(LINE_SEPERATOR);
+                    break;
+
 
                 default:
                     System.out.println("added: " + userInput);
                     lines.add(userInput);
+                    isDone.add(false);
                     System.out.println(LINE_SEPERATOR);
             }
         }
