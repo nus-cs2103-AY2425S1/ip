a73c15436a89659c19195b5aec651ef13ad01356
KennethTeo2002 <kenneth.teo2002@gmail.com>
2024-08-22 09:37:24 +0800

    Implement initial greetings and exit

--- src/main/java/Nerf.java
+++ src/main/java/Nerf.java
@@ -1,19 +1,37 @@
 public class Nerf {
-    private static final int LINE_LENGTH = 20;
-    private static final String LOGO = "##  ##   ######   #####    ######\n" 
-                                    +  "### ##   ##       ##  ##   ##    \n"
-                                    +  "######   ##       ##  ##   ##    \n"
-                                    +  "######   ####     #####    ####  \n"
-                                    +  "## ###   ##       ####     ##    \n"
-                                    +  "##  ##   ##       ## ##    ##    \n"
-                                    +  "##  ##   ######   ##  ##   ##    \n";
+    private static final int LINE_LENGTH = 60;
+    private static final String LOGO = """
+                                       ##  ##   ######   #####    ######
+                                       ### ##   ##       ##  ##   ##    
+                                       ######   ##       ##  ##   ##    
+                                       ######   ####     #####    ####  
+                                       ## ###   ##       ####     ##    
+                                       ##  ##   ##       ## ##    ##    
+                                       ##  ##   ######   ##  ##   ##    
+                                       """;
 
     public static void main(String[] args) {
-        
+        greetings();
+        exit();
+    }
+
+    private static void printLogo(){
+        System.out.println(Nerf.LOGO);
+    }
+    private static void printDivider(){
+        System.out.println("_".repeat(Nerf.LINE_LENGTH));
     }
 
     public static void greetings(){
-        System.out.println("-".repeat(Nerf.LINE_LENGTH));
-        System.out.println("Hello from\n");
+        printDivider();
+        System.out.println("Hello there! I am Nerf, your personal chatbot assistant.\n");
+        printLogo();
+        System.out.println("How may I help you today?");
+        printDivider();
+    }
+
+    public static void exit(){
+        System.out.println("Goodbye. Hope to see you soon!");
+        printDivider();
     }
 }
