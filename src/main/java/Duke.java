3d3e3689f24f7b722caf97c44e5f66276fc59f78
FlyingDonkeys <puppylongears@gmail.com>
2024-08-22 23:30:59 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String initial = "What can I do for you?\n";
+        System.out.println("Hello from\n" + logo + initial);
     }
 }
