354823599f91e979e0ae1af4ab012b446d4ea611
nathanshew <92252099+nathanshew@users.noreply.github.com>
2024-08-22 11:06:47 +0800

    Rename, Greet, Exit

---
+++ src/main/java/NathanBot.java
@@ -0,0 +1,9 @@
+public class NathanBot {
+    public static void main(String[] args) {
+        String Line = "____________________________________________________________\n";
+        String Greet = " Hello! I'm NathanBot\n" +
+                " What can I do for you?\n";
+        String Exit = " Bye. Hope to see you again soon!\n";
+        System.out.println(Line + Greet + Line + Exit + Line);
+    }
+}
