807a8b9e923951e0170dc9e648f131cf673042c5
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 01:14:26 +0800

    Update input manager to handle new task commands

--- src/main/java/InputManager.java
+++ src/main/java/InputManager.java
@@ -10,11 +10,22 @@ public class InputManager {
     public void handleInput(String input) {
         input = input.trim();
         if (input.equalsIgnoreCase("list")) {
-            String response = taskManager.listTasks();
-            messageBuilder.sendMessage(response);
+            messageBuilder.sendMessage(taskManager.listTasks());
+        } else if (input.startsWith("mark ")) {
+            handleMarking(input.substring(5), true);
+        } else if (input.startsWith("unmark ")) {
+            handleMarking(input.substring(7), false);
         } else if (!input.equalsIgnoreCase("bye")) {
-            String response = taskManager.addTask(input);
-            messageBuilder.sendMessage(response);
+            messageBuilder.sendMessage(taskManager.addTask(input));
+        }
+    }
+
+    private void handleMarking(String numberString, boolean isDone) {
+        try {
+            int taskNumber = Integer.parseInt(numberString);
+            messageBuilder.sendMessage(taskManager.markTask(taskNumber, isDone));
+        } catch (NumberFormatException e) {
+            messageBuilder.sendMessage("Invalid task number format!!!!");
         }
     }
 }
