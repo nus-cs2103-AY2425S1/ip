f5358c0d858bea5e129f7bd9dd5c8761bbe52634
jiayr99 <e1249294@u.nus.edu>
2024-08-22 22:41:41 +0800

    Add exception to event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,10 +2,13 @@ public class Event extends Task {
     protected String from;
     protected String to;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, String from, String to) throws JiaException {
         super(description);
         this.from = from;
         this.to = to;
+        if (description.isEmpty() || this.from.isEmpty() || this.to.isEmpty()) {
+            throw new JiaException("What time is your event?? :o");
+        }
     }
 
     @Override
