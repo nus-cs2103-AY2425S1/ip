0d193fb1074a1fd7a687c602871ee7e2a0f1dfbc
minhao <heminhao120@gmail.com>
2024-08-22 19:08:01 +0800

    Level 0. Rename, Greet, Exit

---
+++ ip/src/main/java/Jarvis.java
@@ -0,0 +1,15 @@
+public class Jarvis {
+    public static void main(String[] args) {
+        String logo =
+                "       _                  _     \n"+
+                        "      (_)___ _______   __(_)____\n"+
+                        "     / / __ `/ ___/ | / / / ___/\n"+
+                        "    / / /_/ / /   | |/ / (__  )\n"+
+                        " __/ /\\__,_/_/    |___/_/____/\n"+
+                        "/___/\n";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println(" What can I do for you?\n\n");
+        System.out.println( "Bye. Hope to see you again soon!");
+    }
+}
