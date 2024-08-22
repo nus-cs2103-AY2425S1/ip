7b911d683225d21cb2baedc9614025f15c804ccd
yixianggg <e1186210@u.nus.edu>
2024-08-22 16:26:11 +0800

    added in Task class, mark and unmark commands for Bob

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -9,20 +9,30 @@ public class Bob {
         System.out.printf("Hello! I'm %s!\n", name);
         System.out.println("What can I do for you?");
         System.out.println(line);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 0;
         Scanner scanner = new Scanner(System.in);
         String word = scanner.nextLine();
         while (!word.equals("bye")) {
             System.out.println(line);
             if (word.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
-                    System.out.println(list[i]);
+                    System.out.printf("%d.%s\n", i + 1, list[i]);
                 }
             } else {
-                list[index] = String.valueOf(index + 1) + ". " + word;
-                System.out.printf("added: %s\n", word);
-                index++;
+                String[] tmp = word.split(" ", 3);
+                if (tmp[0].equals("mark")) {
+                    int num = Integer.parseInt(tmp[1]) - 1;
+                    System.out.println(list[num].mark());
+                } else if (tmp[0].equals("unmark")) {
+                    int num = Integer.parseInt(tmp[1]) - 1;
+                    System.out.println(list[num].unmark());
+                } else {
+                    list[index] = new Task(word);
+                    System.out.printf("added: %s\n", word);
+                    index++;
+                }
             }
             System.out.println(line);
             word = scanner.nextLine();
