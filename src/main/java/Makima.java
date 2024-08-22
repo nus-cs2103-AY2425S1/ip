c9e063382335fce043169e2eb5e2f17b2155a60e
Edsel-Tan <riwk2015@gmail.com>
2024-08-22 20:59:24 +0800

    added user input functionality

--- src/main/java/Makima.java
+++ src/main/java/Makima.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Makima {
 
-    public static final String LINE_SEPERATOR = "__________________\n";
+    public static final String LINE_SEPERATOR = "__________________";
+    private boolean running = true;
 
     public void greeting() {
         System.out.println("Yahallo! I'm your friendly chatbot, Makima!");
@@ -10,6 +13,7 @@ public class Makima {
 
     public void farewell() {
         System.out.println("Baibai. Hope to see you soon! ^_^");
+        System.out.println(LINE_SEPERATOR);
     }
 
     public static void main(String[] args) {
@@ -17,7 +21,24 @@ public class Makima {
     }
 
     public Makima() {
+        Scanner sc = new Scanner(System.in);
         greeting();
+
+        while (running) {
+            String userInput = sc.nextLine();
+            System.out.println(LINE_SEPERATOR);
+
+            switch (userInput) {
+                case "bye":
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println(userInput);
+            }
+            System.out.println(LINE_SEPERATOR);
+        }
+
         farewell();
     }
 
