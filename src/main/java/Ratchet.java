b14f4f36c640be48be641361d45215976ae646f3
ckclion <118067503+ckclion@users.noreply.github.com>
2024-08-22 17:31:36 +0800

    Add list functionality to Ratchet.java

--- src/main/java/Ratchet.java
+++ src/main/java/Ratchet.java
@@ -1,39 +1,55 @@
 import java.util.Scanner;
 
 public class Ratchet {
+    static String INDENT = "    ";
+    static String[] list = new String[100];
+    static int listCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         greet();
         String input = "";
         while (!input.equalsIgnoreCase("bye")) {
             input = scanner.nextLine();
-            if (!input.equalsIgnoreCase("bye")) {
-                echo(input);
-            } else {
+
+            if (input.equalsIgnoreCase("list")) {
+                displayList();
+            } else if (input.equalsIgnoreCase("bye")) {
                 exit();
+            } else {
+                addList(input);
             }
         }
     }
 
-    public static void lineBreak() {
+    private static void lineBreak() {
         System.out.println("   ________________________________________________________");
     }
 
-    public static void greet() {
+    private static void greet() {
         lineBreak();
-        System.out.println("    Hello! I'm Ratchet\n" + "    What can I do for you?");
-        lineBreak();
-    }
-
-    public static void exit() {
-        lineBreak();
-        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(INDENT + "Hello! I'm Ratchet\n" + INDENT + "What can I do for you?");
         lineBreak();
     }
 
-    public static void echo(String s) {
+    private static void exit() {
         lineBreak();
-        System.out.println("    " + s);
+        System.out.println(INDENT + "Bye. Hope to see you again soon!");
+        lineBreak();
+    }
+
+    private static void addList(String text) {
+        list[listCount++] = text;
+        lineBreak();
+        System.out.println(INDENT + "added: " + text);
+        lineBreak();
+    }
+
+    private static void displayList() {
+        lineBreak();
+        for (int i = 0; i < listCount; i++) {
+            System.out.println(INDENT + (i + 1) + ". " + list[i]);
+        }
         lineBreak();
     }
 }
