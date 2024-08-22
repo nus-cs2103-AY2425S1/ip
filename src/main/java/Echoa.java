1d393919395fe574da59e2a74929bec18c052d7d
jowhee3011 <joeygoh3011@gmail.com>
2024-08-22 16:19:22 +0800

    Renamed and edited Echoa.java

---
+++ src/main/java/Echoa.java
@@ -0,0 +1,8 @@
+public class Echoa {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Echoa.");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
