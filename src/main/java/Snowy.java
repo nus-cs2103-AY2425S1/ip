ff6cf8a830b6c77fbf4b360f25697aec9ab2a2f0
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-08-22 21:50:19 +0800

    Adjust to solve spaces deleted at end of lines

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -5,11 +5,11 @@ public class Snowy {
 
     private static final String LINE = "-----------------------------------\n";
     private static final String GREETING = LINE
-            + "Hello! My name is Snowy \n"
-            + "What can I do for you? \n"
+            + "Hello! My name is Snowy\n"
+            + "What can I do for you?\n"
             + LINE;
 
-    private static final String ENDING = "Bye! Hope to see you again soon! \n"
+    private static final String ENDING = "Bye! Hope to see you again soon!\n"
             + LINE;
 
     private static Task[] tasks = new Task[100];
@@ -45,7 +45,7 @@ public class Snowy {
                     try {
                         int index = Integer.parseInt(description);
                         tasks[index - 1].markComplete();
-                        System.out.println("Nice! I've marked this task as done:\n  "
+                        System.out.println("Nice! I've marked this task as done:\n"
                                 + tasks[index - 1].toString());
                     }catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
                         System.out.println("Invalid index provided. Please try again");
@@ -56,7 +56,7 @@ public class Snowy {
                     try {
                         int index = Integer.parseInt(description);
                         tasks[index - 1].markIncomplete();
-                        System.out.println("Ok, I've marked this task as not done yet:\n  "
+                        System.out.println("Ok, I've marked this task as not done yet:\n"
                                 + tasks[index - 1].toString());
                     }catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
                         System.out.println("Invalid index provided. Please try again");
@@ -64,27 +64,83 @@ public class Snowy {
                     break;
 
                 case "todo":
+                    if (description.isEmpty()) {
+                        System.out.println("Error: Description of todos cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
                     tasks[numOfTasks] = new ToDo(description);
-                    System.out.println("New todo task added:\n " + tasks[numOfTasks]);
+                    System.out.println("New todo task added:\n" + tasks[numOfTasks]);
                     numOfTasks++;
                     break;
 
                 case "deadline":
-                    int byIndex = description.indexOf(" /by ");
+                    if (description.isEmpty()) {
+                        System.out.println("Error: Description of deadlines cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+
+                    int byIndex = description.indexOf("/by ");
+
+                    if (byIndex == -1) {
+                        System.out.println("Error: Please include name, /by and deadline separated by space.");
+                        break;
+                    }
                     String deadlineName = description.substring(0, byIndex);
-                    String date = description.substring(byIndex + 5);
+                    String date = description.substring(byIndex + 4);
+
+                    if (deadlineName.isEmpty()) {
+                        System.out.println("Error: name of deadlines cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+
+                    if (date.isEmpty()) {
+                        System.out.println("Error: dueDate of deadlines cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+
                     tasks[numOfTasks] = new Deadline(deadlineName, date);
-                    System.out.println("New Deadline task added:\n " + tasks[numOfTasks]);
+                    System.out.println("New Deadline task added:\n" + tasks[numOfTasks]);
                     numOfTasks++;
                     break;
 
                 case "event":
-                    int fromIndex = description.indexOf(" /from ");
-                    int toIndex = description.indexOf(" /to ");
+                    if (description.isEmpty()) {
+                        System.out.println("Error: Description of events cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+                    int fromIndex = description.indexOf("/from ");
+                    int toIndex = description.indexOf("/to ");
+
+                    if (toIndex == -1 || fromIndex == -1) {
+                        System.out.println("Error: incorrect styling.\n"
+                        + "Please include name, /from, fromDate, /to, toDate separated by space");
+                        break;
+                    }
 
                     String eventName = description.substring(0, fromIndex);
-                    String fromDate = description.substring(fromIndex + 7, toIndex);
-                    String toDate = description.substring(toIndex + 5);
+                    String fromDate = description.substring(fromIndex + 6, toIndex);
+                    String toDate = description.substring(toIndex + 4);
+                    if (eventName.isEmpty()) {
+                        System.out.println("Error: eventName of deadlines cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+                    if (fromDate.isEmpty()) {
+                        System.out.println("Error: fromDate of deadlines cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+                    if (toDate.isEmpty()) {
+                        System.out.println("Error: toDate of deadlines cannot be empty.\n"
+                                + "Please try again.");
+                        break;
+                    }
+
                     tasks[numOfTasks] = new Event(eventName, fromDate, toDate);
                     System.out.println("New Event task added:\n " + tasks[numOfTasks]);
                     numOfTasks++;
