88795de5cd26109e94e1a329e75710c263b8f0b9
antgyf <anthonygohyf@gmail.com>
2024-08-23 03:41:41 +0800

    Week2 Level-5

--- src/main/java/PHamBot.java
+++ src/main/java/PHamBot.java
@@ -1,10 +1,17 @@
-import java.util.List;
+import javax.lang.model.type.ErrorType;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class PHamBot {
     private static final String line = "____________________________________________________________\n";
     private static TaskList tasks = new TaskList();
+
+    private static final String[] UserGreetings = {"Hello", "Hi", "What's up"};
+    public enum Errors {
+        MissingTask,
+        MissingDivider,
+        MissingDate,
+        UnknownCommand
+    }
 
     public static void main(String[] args) {
         Greet();
@@ -21,43 +28,78 @@ public class PHamBot {
                 ListTasks();
             }
             if (input.contains("todo")) {
-                String task = input.substring(5);
-                System.out.println(OutlineMessage(addToDo(task)));
+                if (input.length() < 6) {
+                    handleErrors(Errors.MissingTask);
+                }
+                else {
+                    String task = input.substring(5);
+                    OutlineMessage(addToDo(task));
+                }
             }
             if (input.contains("deadline")) {
                 int dateIndex = input.indexOf("/");
-                String task = input.substring(9, dateIndex);
-                String deadline = input.substring(dateIndex + 1);
-                System.out.println(OutlineMessage(addDeadline(task, deadline)));
+                //handle error for missing /
+
+                if (input.length() < 10) {
+                    handleErrors(Errors.MissingTask);
+                } else if (dateIndex == -1) {
+                    handleErrors(Errors.MissingDivider);
+                } else if (input.substring(dateIndex).length() == 1) {
+                    handleErrors(Errors.MissingDate);
+                }
+                else {
+                    String task = input.substring(9, dateIndex);
+                    String deadline = input.substring(dateIndex + 1);
+                    OutlineMessage(addDeadline(task, deadline));
+                }
             }
             if (input.contains("event")) {
                 int dateIndex = input.indexOf("/");
-                String task = input.substring(6, dateIndex);
-                String deadline = input.substring(dateIndex + 1);
-                System.out.println(OutlineMessage(addEvent(task, deadline)));
+
+                if (input.length() < 7) {
+                    handleErrors(Errors.MissingTask);
+                }
+                else if (dateIndex == -1) {
+                    handleErrors(Errors.MissingDivider);
+                } else if (input.substring(dateIndex).length() == 1) {
+                    handleErrors(Errors.MissingDate);
+                }
+                else {
+                    String task = input.substring(6, dateIndex);
+                    String deadline = input.substring(dateIndex + 1);
+                    OutlineMessage(addEvent(task, deadline));
+                }
             }
             if (input.contains("unmark")) {
                 unmark(Integer.parseInt(input.substring(7)));
             } else if (input.contains("mark")) {
                 mark(Integer.parseInt(input.substring(5)));
             }
+            else {
+                for (String userGreeting : UserGreetings) {
+                    if (input.contains(userGreeting)) {
+                        OutlineMessage("Hi! How can I help you?");
+                        break;
+                    }
+                }
+            }
 
 
         }
     }
 
-    private static String OutlineMessage(String msg) {
-        return line + msg + "\n" + line;
+    private static void OutlineMessage(String msg) {
+        System.out.println(line + msg + "\n" + line);
     }
 
     public static void Greet() {
         String greeting = "Hi! I'm PHamBot\nHappy to be of service to you today!";
-        System.out.println(OutlineMessage(greeting));
+        OutlineMessage(greeting);
     }
 
     public static void SayGoodbye() {
         String goodbye = "Hope I was able to help\nGoodbye!";
-        System.out.println(OutlineMessage(goodbye));
+        OutlineMessage(goodbye);
     }
 
     public static String addToDo(String task) {
@@ -76,20 +118,41 @@ public class PHamBot {
     }
 
     public static void ListTasks() {
-        System.out.println(OutlineMessage(tasks.toString()));
+        OutlineMessage(tasks.toString());
     }
 
-    public static boolean mark(int index) {
+    public static void mark(int index) {
         int i = index - 1;
         tasks.markTask(i);
-        System.out.println(OutlineMessage("I've marked the following task as done!\n" + tasks.getTask(i)));
-        return true;
+        OutlineMessage("I've marked the following task as done!\n" + tasks.getTask(i));
     }
 
-    public static boolean unmark(int index) {
+    public static void unmark(int index) {
         int i = index - 1;
         tasks.unmarkTask(i);
-        System.out.println(OutlineMessage("I've marked the following task as not done.\n" + tasks.getTask(i)));
-        return true;
+        OutlineMessage("I've marked the following task as not done.\n" + tasks.getTask(i));
     }
+
+    public static void handleErrors(Errors error) {
+        switch (error) {
+            case MissingTask:
+                OutlineMessage("I'm sorry, I can't add a task without knowing what to add!\n" +
+                        "Please try again, and enter the intended task after the task type");
+                break;
+            case MissingDivider:
+                OutlineMessage("I'm sorry, I can't identify the date.\n" +
+                        "Please try again and add a slash (/) after the task.");
+                break;
+            case MissingDate:
+                OutlineMessage("Sorry, I couldn't find a date.\n" +
+                        "Try again with the date after the slash (/)");
+                break;
+            case UnknownCommand:
+                OutlineMessage("...\n" +
+                        "Huh?");
+                break;
+        }
+    }
+
+
 }
