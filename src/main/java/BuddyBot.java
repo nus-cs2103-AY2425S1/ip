dc052805fa5f38329d96c5a3cb087606738cfae3
Jalenleeruixian <e1122262@u.nus.edu>
2024-08-22 17:29:08 +0800

    level 0

--- src/main/java/BuddyBot.java
+++ src/main/java/BuddyBot.java
@@ -1,8 +1,16 @@
 public class BuddyBot {
     public static void main(String[] args) {
-        String logo = " Hello! I'm BuddyBot\n"
-                + "What can I do for you?\n"
-                + "Bye. Hope to see you again soon!";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("__________________________");
+        System.out.println(" Hello! I'm BuddyBot");
+        System.out.println(" What can I do for you?");
+        System.out.println("__________________________");
+        System.out.println(" Bye, Hope to see you again");
     }
 }
