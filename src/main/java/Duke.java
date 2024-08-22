835935b157179b41d0829614937f0e7a1b83ff5d
SleepyDay <107453007+flyingsalsa@users.noreply.github.com>
2024-08-23 00:23:57 +0800

    Duke.java: modify for Level-0 Add Parser.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+
+    private static void lnDiv() {
+        System.out.println("________________________________");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _   _       _                 _   \n"
+                + "| \\ | | ___ | |_    ____ ____ | |_ \n"
+                + "|  \\| |/ _ \\  __|  / _` | '_ \\| __|\n"
+                + "| |\\  | (_) | |_  | (_| | |_) | |_ \n"
+                + "|_| \\_|\\___/ \\__|  \\__, | .__/ \\__|\n"
+                + "                   |___/|_|        \n";
+        System.out.println(logo+ "Hello I'm Not-gpt,");
+        System.out.println("what can I do for you?");
+        lnDiv();
+        System.out.println("Good bye, Thank you for using me!");
+        lnDiv();
+
     }
 }
