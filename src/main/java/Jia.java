3e5c11d094c8ee35d2c2af1245d4270af86b9dca
jiayr99 <e1249294@u.nus.edu>
2024-08-22 23:00:13 +0800

    Changed to an array list of tasks

--- src/main/java/Jia.java
+++ src/main/java/Jia.java
@@ -1,11 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jia {
     public static void main(String[] args) {
         //Create a scanner object to read user input
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         String input;
 
         //Greet the user
@@ -22,61 +22,55 @@ public class Jia {
                     if (input.equalsIgnoreCase("list")) {
                         System.out.println("______________________________________________________");
                         System.out.println("Here are the tasks in your list:");
-                        for (int i = 0; i < taskCount; i++) {
-                            System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println(" " + (i + 1) + ". " + tasks.get(i));
                         }
                         System.out.println("______________________________________________________");
                     } else if (input.startsWith("mark")) {
                         int taskNumber = Integer.parseInt(input.substring(4).trim()) - 1;
-                        if (taskNumber >= 0 && taskNumber < taskCount) {
-                            tasks[taskNumber].markAsDone();
+                        if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                            tasks.get(taskNumber).markAsDone();
                             System.out.println("______________________________________________________");
                             System.out.println("Nice! I've marked this task as done:");
-                            System.out.println(" " + tasks[taskNumber]);
+                            System.out.println(" " + tasks.get(taskNumber));
                             System.out.println("______________________________________________________");
                         } else {
                             System.out.println("Invalid task number :(");
                         }
                     } else if (input.startsWith("unmark")) {
                         int taskNumber = Integer.parseInt(input.substring(6).trim()) - 1;
-                        if (taskNumber >= 0 && taskNumber < taskCount) {
-                            tasks[taskNumber].markAsNotDone();
+                        if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                            tasks.get(taskNumber).markAsNotDone();
                             System.out.println("______________________________________________________");
                             System.out.println("OK, I've marked this task as not done yet:");
-                            System.out.println(" " + tasks[taskNumber]);
+                            System.out.println(" " + tasks.get(taskNumber));
                             System.out.println("______________________________________________________");
                         } else {
                             System.out.println("Invalid task number :(");
                         }
+                    } else if (input.startsWith("delete")) {
+
                     } else if (input.startsWith("todo")) {
                         String description = input.substring(4).trim();
                         if (description.isEmpty()) {
                             throw new JiaException("Oh No! Please tell me what task to do :(");
                         }
-                        tasks[taskCount++] = new ToDo(description);
+                        tasks.add(new ToDo(description));
                         System.out.println("______________________________________________________");
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(" " + tasks[taskCount - 1]);
-                        if (taskCount > 1) {
-                            System.out.println("Now you have " + taskCount + " tasks in the list.");
-                        } else {
-                            System.out.println("Now you have " + taskCount + " task in the list.");
-                        }
+                        System.out.println(" " + tasks.get(tasks.size() - 1));
+                        System.out.println("Now you have " + tasks.size() + (tasks.size() > 1 ? " tasks" : " task") + " in the list.");
                         System.out.println("______________________________________________________");
                     } else if (input.startsWith("deadline")) {
                         String[] parts = input.substring(8).split(" /by ");
                         if (parts.length == 2) {
                             String description = parts[0].trim();
                             String by = parts[1].trim();
-                            tasks[taskCount++] = new Deadline(description, by);
+                            tasks.add(new Deadline(description, by));
                             System.out.println("______________________________________________________");
                             System.out.println("Got it. I've added this task:");
-                            System.out.println(" " + tasks[taskCount - 1]);
-                            if (taskCount > 1) {
-                                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                            } else {
-                                System.out.println("Now you have " + taskCount + " task in the list.");
-                            }
+                            System.out.println(" " + tasks.get(tasks.size() - 1));
+                            System.out.println("Now you have " + tasks.size() + (tasks.size() > 1 ? " tasks" : " task") + " in the list.");
                             System.out.println("______________________________________________________");
                         } else {
                             throw new JiaException("You need to add a task and a deadline!! -_-");
@@ -87,15 +81,11 @@ public class Jia {
                             String description = parts[0].trim();
                             String from = parts[1].trim();
                             String to = parts[2].trim();
-                            tasks[taskCount++] = new Event(description, from, to);
+                            tasks.add(new Event(description, from, to));
                             System.out.println("______________________________________________________");
                             System.out.println("Got it. I've added this task:");
-                            System.out.println(" " + tasks[taskCount - 1]);
-                            if (taskCount > 1) {
-                                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                            } else {
-                                System.out.println("Now you have " + taskCount + " task in the list.");
-                            }
+                            System.out.println(" " + tasks.get(tasks.size() - 1));
+                            System.out.println("Now you have " + tasks.size() + (tasks.size() > 1 ? " tasks" : " task") + " in the list.");
                             System.out.println("______________________________________________________");
                         } else {
                             throw new JiaException("What time is your event?? :o");
