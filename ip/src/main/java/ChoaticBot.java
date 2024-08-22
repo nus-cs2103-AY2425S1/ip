1c39a2ff1b1ab73483351ef35d651911ee29b235
choaticman <dylanchan66@yahoo.com.sg>
2024-08-23 02:00:55 +0800

    Rename bot and add welcome & bye message

---
+++ ip/src/main/java/ChoaticBot.java
@@ -0,0 +1,12 @@
+public class ChoaticBot {
+    public static void main(String[] args) {
+        String welcomeMsg = "____________________________________________________________ \n"
+                + "Hello! I'm ChoaticBot\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+
+        System.out.println(welcomeMsg);
+    }
+}
