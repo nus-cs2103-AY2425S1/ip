e0f511e9a6d315ac612f15b4dbca28a9da8c28b5
Wnayar <wdnconsultancy@yahoo.com>
2024-08-22 10:57:49 +0800

    Chatbot echo commands entered by the user

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Bill {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Bill");
         System.out.println("What can I do for you?");
+
+        Scanner userScanner = new Scanner(System.in);
+        String userCommand = userScanner.nextLine();
+
+        while (!userCommand.equals("bye")) {
+            System.out.println(userCommand);
+            userCommand = userScanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
