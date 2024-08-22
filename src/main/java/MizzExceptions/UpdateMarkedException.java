a7cb0fb0e5f9c3fc9b1372a9108da0d618623f16
wesley <wesleywong08@gmail.com>
2024-08-22 22:56:54 +0800

    Update mark exception
    - Update constructor to include mark or unmark
    - Update toString to only return relevant command

--- src/main/java/MizzExceptions/UpdateMarkedException.java
+++ src/main/java/MizzExceptions/UpdateMarkedException.java
@@ -3,15 +3,19 @@ package MizzExceptions;
 import util.Utility;
 
 public class UpdateMarkedException extends MizzException {
-  public UpdateMarkedException(String msg) {
+  private final boolean mark;
+
+  public UpdateMarkedException(String msg, String mark) {
     super(msg);
+    this.mark = mark.equals("mark");
   }
 
   @Override
   public String toString() {
     return Utility.INDENT + "Thats bad marking >:( "
         + super.getMessage() + "\n"
-        + Utility.INDENT + "Example usage: mark <valid_idx_from_1>\n"
-        + Utility.INDENT + "Example usage: unmark <valid_idx_from_1>";
+        + (this.mark
+            ? Utility.INDENT + "Example usage: mark <valid_idx_from_1>"
+            : Utility.INDENT + "Example usage: unmark <valid_idx_from_1>");
   }
 }
