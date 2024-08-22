33bcf34c10a7bd4aaafb60064b96227289b82a25
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-08-23 01:26:38 +0800

    changed to use java collections ArrayList

--- src/main/java/YapMeister.java
+++ src/main/java/YapMeister.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
 
 public class YapMeister {
     final static int MAX_TASKS = 100;
-    private static int taskIndex = 0;
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -19,7 +19,7 @@ public class YapMeister {
                                """);
         String input = "";
         boolean running = true;
-        Task[] tasks = new Task[MAX_TASKS];
+        ArrayList<Task> tasks = new ArrayList<>();
         String[] inputParsed;
         while (running) {
             System.out.print("\n");
@@ -31,38 +31,39 @@ public class YapMeister {
                         running = false;
                         break;
                     case "list":
-                        for (int i = 0; i < taskIndex; i++) {
-                            System.out.println(String.format("%d. %s", i + 1, tasks[i].toString()));
+                        int i = 0;
+                        for (Task task : tasks) {
+                            System.out.println(String.format("%d. %s", i + 1, task.toString()));
+                            i++;
                         }
                         break;
                     case "mark":
                         int index = parseInt(command[1]) - 1;
-                        if (index >= taskIndex || index < 0) {
+                        if (index >= tasks.size() || index < 0) {
                             throw new InvalidMarkException("No task at that index");
                         }
-                        tasks[index].setCompleted(true);
+                        tasks.get(index).setCompleted(true);
                         System.out.println("You did this:");
-                        System.out.println(tasks[index].toString());
+                        System.out.println(tasks.get(index).toString());
                         break;
                     case "unmark":
                         int umindex = parseInt(command[1]) - 1;
-                        if (umindex >= taskIndex || umindex < 0) {
+                        if (umindex >= tasks.size() || umindex < 0) {
                             throw new InvalidMarkException("No task at that index");
                         }
-                        tasks[umindex].setCompleted(false);
+                        tasks.get(umindex).setCompleted(false);
                         System.out.println("You undid this:");
-                        System.out.println(tasks[umindex].toString());
+                        System.out.println(tasks.get(umindex).toString());
                         break;
                     case "todo":
                     case "deadline":
                     case "event":
                         Task task = null;
                         task = createTask(command[0], input);
-                        tasks[taskIndex] = task;
                         System.out.println("Added:");
-                        tasks[taskIndex].toString();
-                        taskIndex++;
-                        System.out.println(String.format("You have %d tasks", taskIndex));
+                        task.toString();
+                        tasks.add(task);
+                        System.out.println(String.format("You have %d tasks", tasks.size()));
                         break;
                     default:
                         System.out.println("Invalid input please yap yapology");
