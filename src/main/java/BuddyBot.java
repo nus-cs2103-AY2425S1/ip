385b3bd16cc1dfda1eb165ec2396d3d32fcff4f4
Jalenleeruixian <e1122262@u.nus.edu>
2024-08-22 18:40:23 +0800

    Level 1

--- src/main/java/BuddyBot.java
+++ src/main/java/BuddyBot.java
@@ -1,16 +1,17 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class BuddyBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
+        Scanner myObj = new Scanner(System.in);
         System.out.println("__________________________");
         System.out.println(" Hello! I'm BuddyBot");
         System.out.println(" What can I do for you?");
-        System.out.println("__________________________");
-        System.out.println(" Bye, Hope to see you again");
+        String input = myObj.nextLine();
+        if (Objects.equals(input, "bye")) {
+            System.out.println(" Bye, Hope to see you again soon1");
+        } else {
+            System.out.println(input);
+        }
     }
 }
