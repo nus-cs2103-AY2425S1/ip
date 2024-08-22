8e26d2a683d6759f9074b20c18c2f026a0195c50
QuekBingHeng <quekbingheng.17@gmail.com>
2024-08-22 20:26:59 +0800

    add Level-1, Angel.java

--- src/main/java/Angel.java
+++ src/main/java/Angel.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Angel {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = "____________________________________________________________\n";
-        String greeting = " Hello! I'm Angel\n What can I do for you?\n";
-        String farewell = " Bye. Hope to see you again soon!\n";
 
-        System.out.println(logo + greeting + logo);
-        System.out.println(farewell + logo);
+        System.out.println(logo + " Hello! I'm Angel\n What can I do for you?\n" + logo);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(logo + " Bye. Hope to see you again soon!\n" + logo);
+                break;
+            } else {
+                System.out.println(logo + " " + userInput + "\n" + logo);
+            }
+        }
+
+        scanner.close();
     }
 }
