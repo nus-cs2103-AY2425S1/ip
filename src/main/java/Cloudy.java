906d0909fd569ace1dc7ce34d35b909a7a2b44d5
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-08-22 14:16:56 +0800

    Level 2 task

--- src/main/java/Cloudy.java
+++ src/main/java/Cloudy.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Cloudy {
     public static void main(String[] args) {
         Scanner echo = new Scanner(System.in);
+        ArrayList<String> userList = new ArrayList<String>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Cloudy.");
@@ -18,10 +20,19 @@ public class Cloudy {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (Objects.equals(userInput, "list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < userList.size(); i++) {
+                    System.out.println((i + 1) + ". " + userList.get(i));
+                }
+                System.out.println("____________________________________________________________");
+                userInput = echo.nextLine();
             } else {
                 System.out.println("____________________________________________________________");
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                userList.add(userInput);
                 System.out.println("____________________________________________________________");
+
 
                 userInput = echo.nextLine();
             }
