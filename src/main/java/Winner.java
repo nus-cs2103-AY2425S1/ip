4732441119741eb0d7812049e1c2ab3db42f9118
yuancheng <>
2024-08-22 17:00:25 +0800

    level 1 - echo commands, exits when bye, indentation included

--- src/main/java/Winner.java
+++ src/main/java/Winner.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Winner {
 
     public static void main(String[] args) {
-        System.out.println("--------------------------------------\n" +
-                           "Hello! I am Winner.\n" +
-                           "What can I do for you today?\n" +
-                           "--------------------------------------");
-        System.out.println("--------------------------------------\n" +
-                           "Hope I have been of service!\n" +
-                           "Thank you and see you again soon :D\n" +
-                           "--------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                --------------------------------------
+                Hello! I am Winner.
+                What can I do for you today?
+                --------------------------------------""".indent(10));
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("""
+                        --------------------------------------
+                        Hope I have been of service!
+                        Thank you and see you again soon :D
+                        --------------------------------------""".indent(10));
+                break;
+            }
+            System.out.println("-------------------------------------- \n".indent(10) +
+                    input.indent(10) +
+                    "--------------------------------------".indent(10));
+        }
+
     }
 }
