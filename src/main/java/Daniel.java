07657346c5976a5f9836295a00ded88eda9a6def
LiuYJ2002 <liuyujia2002@gmail.com>
2024-08-23 00:01:17 +0800

    Level 3

--- src/main/java/Daniel.java
+++ src/main/java/Daniel.java
@@ -7,21 +7,32 @@ public class Daniel {
         System.out.println("Hello! I'm Daniel\nWhat can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
         boolean val = true;
-        List<String> array = new ArrayList<>();
+        List<Task> array = new ArrayList<>();
         while(val){
             String input = scanner.nextLine();
             if (input.equals("bye")){
                 val = false;
                 System.out.println("Bye hope to see you again soon");
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list");
                 int i = 1;
-                for ( String element : array) {
-                    System.out.println(i + "." + element);
+                for ( Task element : array) {
+                    System.out.println(i + "." + element.toString());
                     i += 1;
                 }
+            }else if (input.startsWith("mark")) {
+                int index = input.charAt(input.length() - 1) - '0';
+                array.get(index - 1).markAsDone();
+                System.out.println("Nice I have marked this task as done:");
+                System.out.println(array.get(index - 1).toString());
+            } else if (input.startsWith("unmark")) {
+                int index = input.charAt(input.length() - 1) - '0';
+                array.get(index - 1).markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(array.get(index - 1).toString());
             } else {
                 System.out.println("added: " + input);
-                array.add(input);
+                array.add(new Task(input));
             }
         }
     }
