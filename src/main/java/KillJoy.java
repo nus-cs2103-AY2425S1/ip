0e622e74098ee4b24e2c32b66bb266cb1bfa55c5
thisisaditya17 <joshi.adi1734@gmail.com>
2024-08-22 23:26:37 +0800

    KillJoy.java: fix some bad logic

--- src/main/java/KillJoy.java
+++ src/main/java/KillJoy.java
@@ -3,21 +3,21 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class KillJoy {
-    private String welcomeString = "   Hello! I'm KillJoy \n   I'm GENIUS!!!\n   What can I do for you?\n" +
-                                   "   ------------------------------------";
-    private String exitString = "   ------------------------------------\n" +
+    private String welcomeString = "    Hello! I'm KillJoy\n    I'm GENIUS!!!\n    What can I do for you?\n" +
+                                   "    ------------------------------------";
+    private String exitString = "    ------------------------------------\n" +
                                 "    Bubyyeee & Don't Stwesszz. Time to hide now!!\n" +
-                                "   ------------------------------------";
-    private String logoString = "   ------------------------------------\n" +
-                                "    _  ___ _ _      _             \n" +
-                                "   | |/ (_) | |    | |            \n" +
-                                "   | ' / _| | |    | | ___  _   _ \n" +
-                                "   |  < | | | |_   | |/ _ \\| | | |\n" +
-                                "   | . \\| | | | |__| | (_) | |_| |\n" +
-                                "   |_|\\_\\_|_|_|\\____/ \\___/ \\__, |\n" +
-                                "                             __/ |\n" +
-                                "                            |___/ \n" +
-                                "   ------------------------------------";
+                                "    ------------------------------------";
+    private String logoString = "    ------------------------------------\n" +
+                                "     _  ___ _ _      _\n" +
+                                "    | |/ (_) | |    | |\n" +
+                                "    | ' / _| | |    | | ___  _   _\n" +
+                                "    |  < | | | |_   | |/ _ \\| | | |\n" +
+                                "    | . \\| | | | |__| | (_) | |_| |\n" +
+                                "    |_|\\_\\_|_|_|\\____/ \\___/ \\__, |\n" +
+                                "                              __/ |\n" +
+                                "                             |___/\n" +
+                                "    ------------------------------------";
     private String markString = "Ayee Yooo! I've marked this task okaayyyyy:";
     private String unmarkString = "Yo big guy! I've unmarked this for you:";
     private ArrayList<Task> taskList = new ArrayList<>();
@@ -41,11 +41,15 @@ public class KillJoy {
             } else if (inputAsList[0].equals("mark")) {
                 taskNum = Integer.parseInt(inputAsList[1]) - 1;
                 this.taskList.get(taskNum).changeStatus();
-                this.printTask(this.taskList.get(taskNum));
+                this.printLine();
+                System.out.println("    " + markString + "\n        " + this.taskList.get(taskNum));
+                this.printLine();
             } else if (inputAsList[0].equals("unmark")) {
                 taskNum = Integer.parseInt(inputAsList[1]) - 1;
                 this.taskList.get(taskNum).changeStatus();
-                this.printTask(this.taskList.get(taskNum));
+                this.printLine();
+                System.out.println("    " + unmarkString + "\n        " + this.taskList.get(taskNum));
+                this.printLine();
             }
             else {
                 this.processUserInput(input);
@@ -54,19 +58,20 @@ public class KillJoy {
     }
 
     private void processUserInput(String input) {
-        int taskNum;
         String[] inputSplit = input.split("/");
         String[] inputSplitBySpace = inputSplit[0].split(" ");
         String typeOfTask = inputSplitBySpace[0];
-        String description = inputSplitBySpace[1];
 
-        System.out.println("   ------------------------------------");
+        this.printLine();
         if (typeOfTask.equals("todo")) {
+            String description = inputSplit[0].replaceFirst("todo ", "");
             taskList.add(new Todo(description));
         } else if (typeOfTask.equals("deadline")) {
+            String description = inputSplit[0].replaceFirst("deadline ", "");
             String by = inputSplit[1].replaceFirst("by ", "");
             taskList.add(new Deadline(description, by));
         } else if (typeOfTask.equals("event")) {
+            String description = inputSplit[0].replaceFirst("event ", "");
             String from = inputSplit[1].replaceFirst("from ", "");
             String to = inputSplit[2].replaceFirst("to ", "");
             taskList.add(new Event(description, from, to));
@@ -79,23 +84,21 @@ public class KillJoy {
         } else {
             System.out.println("    Now you have " + taskCount + " tasks in the list.");
         }
-        System.out.println("   ------------------------------------");
+        this.printLine();
     }
 
 
-    private void printTask(Task task) {
-        System.out.println("   ------------------------------------");
-        System.out.println("    " + markString + "\n        " + task);
-        System.out.println("   ------------------------------------");
+    private void printLine() {
+        System.out.println("    ------------------------------------");
     }
 
     private void printTaskList() {
-        System.out.println("    ------------------------------------");
+        this.printLine();
         System.out.println("    Here are your tasks lah!! Don't die:");
         for (int i = 0; i < this.taskCount; i++) {
             System.out.println("    " + (i + 1) + ". " + this.taskList.get(i));
         }
-        System.out.println("   ------------------------------------");
+        this.printLine();
     }
 
     public static void main(String[] args) {
