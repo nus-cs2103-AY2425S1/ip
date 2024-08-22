62c1089aeea1f42fc39b245f45e52da8fecd2abb
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-08-22 11:06:43 +0800

    enabled user input history

--- src/main/java/Revir.java
+++ src/main/java/Revir.java
@@ -1,16 +1,28 @@
+import java.util.ArrayList;
+
 public class Revir {
     public static void main(String[] args) {
         String name = "Revir";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        // Echo user input until user types "bye"
+        
+        ArrayList<String> userInputList =  new ArrayList<String>();
         while (true) {
             String input = System.console().readLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("history")) {
+                // Print the user input history
+                System.out.println("History:");
+                for (String userInput : userInputList) {
+                    System.out.println(userInput);
+                }
+            } else {
+                // Add user input to the list
+                userInputList.add(input);
+                // Differentiate the print from the user input
+                System.out.println("You wrote: " + input);
             }
-            // Differentiate the print from the user input
-            System.out.println("You said: ");
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
