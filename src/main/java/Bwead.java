dbd03006505df0743dfac1fc3d8153cda881a881
jiaaaaen <girljiaen@gmail.com>
2024-08-22 22:41:29 +0800

    rename, greet, exit

---
+++ src/main/java/Bwead.java
@@ -0,0 +1,9 @@
+public class Bwead {
+
+    public static String name = "Bwead";
+
+    public static void main(String[] args) {
+        System.out.println("Hello from " + name + "!");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
