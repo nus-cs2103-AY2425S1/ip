9389218e9a2b3f90340b5c4495a68d0d67a78ad5
JellyPenguinnn <>
2024-08-22 23:46:45 +0800

    Level-4

--- java/ChatBuddy.java
+++ java/ChatBuddy.java
@@ -13,10 +13,11 @@ public class ChatBuddy {
 
             String userInput = scanner.nextLine();
             while (!userInput.equals("bye")) {
-                if (userInput.equals("list")) {
+                if (userInput.equals("list")) { // Display all stored tasks
                     System.out.println("_____________________________________________");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        System.out.println((i + 1) + "." + tasks[i]);
                     }
                     System.out.println("_____________________________________________");
                 } else if (userInput.startsWith("mark ")) {
@@ -39,12 +40,44 @@ public class ChatBuddy {
                         System.out.println(" " + tasks[taskNumber]);
                         System.out.println("_____________________________________________");
                     }
-                } else {
-                    // Store the user input as a new task
-                    tasks[taskCount] = new Task(userInput);
+                } else if (userInput.startsWith("todo ")) {
+                    // Add a ToDo task
+                    String description = userInput.substring(5);
+                    tasks[taskCount] = new ToDo(description);
                     taskCount++;
                     System.out.println("_____________________________________________");
-                    System.out.println("Added: " + userInput);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(" " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("_____________________________________________");
+                } else if (userInput.startsWith("deadline ")) {
+                    // Add a Deadline task
+                    String[] parts = userInput.split(" /by ");
+                    String description = parts[0].substring(9);
+                    String by = parts[1];
+                    tasks[taskCount] = new Deadline(description, by);
+                    taskCount++;
+                    System.out.println("_____________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(" " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("_____________________________________________");
+                } else if (userInput.startsWith("event ")) {
+                    // Add a Event task
+                    String[] parts = userInput.split(" /from | /to ");
+                    String description = parts[0].substring(6);
+                    String from = parts[1];
+                    String to = parts[2];
+                    tasks[taskCount] = new Event(description, from, to);
+                    taskCount++;
+                    System.out.println("_____________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(" " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("_____________________________________________");
+                } else {
+                    System.out.println("_____________________________________________");
+                    System.out.println("Sorry, I don't understand that command.");
                     System.out.println("_____________________________________________");
                 }
                 userInput = scanner.nextLine();
