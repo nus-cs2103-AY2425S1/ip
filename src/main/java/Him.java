df826e47a0200ce5774970b4ef71b32cafada0a5
Isaac Pang <isaac.pangth@gmail.com>
2024-08-23 04:37:37 +0800

    added greeting and exit

--- src/main/java/Him.java
+++ src/main/java/Him.java
@@ -1,3 +1,15 @@
 public class Him{
+    private static final String DIVIDER = "\n____________________________________________________________\n";
+    private static void greet() {
+        System.out.print(DIVIDER + " Hello! I'm Him\n What can I do for you?" + DIVIDER);
+    }
 
+    private static void exit() {
+        System.out.print(" Bye. Hope to see you again soon!" + DIVIDER);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
 }
