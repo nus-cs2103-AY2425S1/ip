1e7b47600d393474bdb79abcd2c9c9e23bb993a2
adriantan101 <atjy102@gmail.com>
2024-08-22 15:18:11 +0800

    renamed Duke to Ruby and added firts message

---
+++ src/main/java/Ruby.java
@@ -0,0 +1,8 @@
+public class Ruby {
+    public static void main(String[] args) {
+        String message = "Hello! I'm Ruby\n" +
+                "What can I do for you?\n" +
+                "Bye. Hope to see you again soon!";
+        System.out.println(message);
+    }
+}
