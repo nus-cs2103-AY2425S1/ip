ab358b7c8e70b6568efb7728f91a113f6ed9b874
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-08-22 10:11:51 +0800

    Change name and say hi and bye

---
+++ src/main/java/Colby.java
@@ -0,0 +1,13 @@
+public class Colby {
+    public static void main(String[] args) {
+        String logo = "  ____      _ _           \n"
+                    + " / ___|___ | | |__  _   _ \n"
+                    + "| |   / _ \\| | '_ \\| | | |\n"
+                    + "| |__| (_) | | |_) | |_| |\n"
+                    + "\\____\\___/|_|_.__/ \\__, /\n"
+                    + "                   |___/ \n";
+
+        //String logo = " ";
+        System.out.println("Hello! I'm \n"+ logo + "\n" + "What can I do for you?\n" + "Bye! Hope to see you again soon!" );
+    }
+}
