9b3851c4ea0aeb8403adae5e6742df302b40767b
jevan13 <jevan@jevans-MacBook-Pro.local>
2024-08-23 00:02:49 +0800

    Add, List

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,26 +1,53 @@
 import java.util.Scanner;
 
 public class Yapper {
+    public static int itemsInList = 0;
+    public static String[] list = new String[100];
     public static void main(String[] args) {
         // greeting message
-        System.out.println("Hello! I'm Yapper \n What can I do for you? \n");
-        
+        System.out.println("Hello! I'm Yapper \n" +
+                "What can I do for you? \n");
+
         while (true) {
             Scanner sc = new Scanner(System.in);
-            String text = sc.next();
-            if (text.equals("bye")) {
-                exit();
-                break;
-            } else {
-                echo(text);
-            }
+            String text = sc.nextLine();
+            read(text);
         }
     }
 
-    // echos command entered by the user
-    public static void echo(String text)
+    // reads commands
+    public static void read(String command)
     {
-        System.out.println(text);
+        if (command.equals("bye"))
+        {
+            exit();
+            System.exit(0);
+        }
+        else if (command.equals("list"))
+        {
+            returnList();
+        }
+        else
+        {
+            addToList(command);
+        }
+    }
+
+    // return list
+    public static void returnList()
+    {
+        for (int i = 1; i <= itemsInList; i++)
+        {
+            System.out.println(i + ". " + list[i]);
+        }
+    }
+
+    // add to List
+    public static void addToList(String text)
+    {
+        itemsInList++;
+        list[itemsInList] = text;
+        System.out.println("Added: " + text);
     }
 
     // exits when the user types the command "bye"
