7bcd768754c34a69a443bd9b4f6953fbbaff9f87
myrilla12 <myrilla12@gmail.com>
2024-08-22 13:31:47 +0800

    Update format and print statements
    The format is not organised due to blank lines. Print statements are a bit ambiguous and could use more detail.
    
    Update format to remove blank spaces. Rewrite 'added: ' print statement to include more detail.

--- src/main/java/Eevee.java
+++ src/main/java/Eevee.java
@@ -8,26 +8,27 @@ public class Eevee {
         String greeting = "Hello! I'm Eevee\nWhat can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
 
-        System.out.println(divider + greeting + divider);
+        System.out.print(divider + greeting + divider);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            System.out.println(divider);
+            System.out.print(divider);
 
             if (input.equals("bye")) {
                 System.out.println(exit);
                 break;
             } else if (input.equals("list")) {
-                tasks.forEach((task) -> {
-                    System.out.println((tasks.indexOf(task) + 1) + ". " + task);
-                });
+                System.out.println("Here are your tasks:");
+                tasks.forEach((task) ->
+                    System.out.println((tasks.indexOf(task) + 1) + ". " + task)
+                );
             } else {
                 tasks.add(input);
-                System.out.println("added: " + input);
+                System.out.println("Added '" + input + "' to your task list");
             }
 
-            System.out.println(divider);
+            System.out.print(divider);
         }
     }
 }
