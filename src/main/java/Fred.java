b64e44d80ba44a2a80ab8e1983d228e10c81e932
Nicholas Chua <e1114227@u.nus.edu>
2024-08-22 16:46:59 +0800

    add exit and echo methods

--- src/main/java/Fred.java
+++ src/main/java/Fred.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Fred {
-    static String line = "____________________________________________________________";
+    static String line = "li____________________________________________________________";
     static String name = "Fred";
     public static void main(String[] args) {
         greet();
+        echo();
         sayFarewell();
+        exit();
     }
 
     private static void greet() {
@@ -15,9 +19,29 @@ public class Fred {
     }
 
     private static void sayFarewell() {
-        String farewell = " Bye. Hope to see you again soon!";
+        String farewell = "Bye. Hope to see you again soon!";
         System.out.println(line);
         System.out.println(farewell);
         System.out.println(line);
+    }
+
+    private static void exit(){
+        System.exit(0);
+    }
+
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+        sayFarewell();
+        exit();
     }
 }
