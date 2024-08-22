3b1649e8ad6969960998cc592d004d8e9d0379f1
MiloTruck <milotruckctf@gmail.com>
2024-08-22 18:19:11 +0800

    Implement ability to store and list input

--- src/main/java/MiluTrock.java
+++ src/main/java/MiluTrock.java
@@ -1,16 +1,28 @@
+import java.util.ArrayList;
+
 public class MiluTrock {
     public static void main(String[] args) {
         String name = "MiluTrock";
         System.out.println("Hello! I'm " + name + "!");
         System.out.println("What can I do for you?");
         
-        // Echo user input and exit if "bye" is entered
+        ArrayList<String> items = new ArrayList<String>();
         while (true) {
             String input = System.console().readLine();
+            
             if (input.toLowerCase().equals("bye")) {
+                // Exit if "bye" is entered
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                // List all previous inputs if "list" is entered
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i+1) + ". " + items.get(i));
+                }
+            } else {
+                // Otherwise, store input
+                System.out.println("Added: " + input);
+                items.add(input);
             }
-            System.out.println(input);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
