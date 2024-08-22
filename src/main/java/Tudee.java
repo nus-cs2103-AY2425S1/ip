0126bd672fc89b5b63f3e1952655f3978597b5b2
avrilmohh <avrilmoh1234@gmail.com>
2024-08-23 01:43:32 +0800

    Modified the code to use enumerations

--- src/main/java/Tudee.java
+++ src/main/java/Tudee.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Tudee {
+    enum Command {
+        LIST, BYE, TODO, DEADLINE, EVENT, MARK, UNMARK, DELETE, UNKNOWN
+    }
     public static void main(String[] args) {
         String logo = "____________________________________________________________ \n"
                 + "Hello! I'm Tudee, your chatbot bestie! \n"
@@ -19,42 +22,42 @@ public class Tudee {
                 input = sc.nextLine();
                 Task currentTask;
                 String[] inputArray = input.split(" ", 2);
-                String cmd = inputArray[0];
-                if (cmd.equalsIgnoreCase("list")) {
+                Command cmd = getCmd(inputArray[0]);
+                if (cmd == Command.LIST) {
                     System.out.println("____________________________________________________________ \n");
                     for (int i = 0; i < list.size(); i++) {
                         System.out.println(i + 1 + ". " + list.get(i));
                     }
                     System.out.println("____________________________________________________________ \n");
-                } else if (cmd.equalsIgnoreCase("bye")) {
+                } else if (cmd == Command.BYE) {
                     output = "____________________________________________________________ \n"
                             + "Bye. Hope to see you again soon! Don't forget about me :( \n"
                             + "____________________________________________________________";
                     System.out.println(output);
                     break;
-                } else if (cmd.contains("mark")) {
+                } else if (cmd == Command.MARK) {
                     int index = Integer.parseInt(inputArray[1]) - 1;
                     Task target = list.get(index);
-                    if (inputArray[0].equals("mark")) {
-                        target.markAsDone();
-                        System.out.println("____________________________________________________________ \n");
-                        System.out.println("Nice! I've marked this task as done: \n");
-                        System.out.println(target);
-                        System.out.println("____________________________________________________________ \n");
-                    } else if (inputArray[0].equals("unmark")) {
-                        target.markAsNotDone();
-                        System.out.println("____________________________________________________________ \n");
-                        System.out.println("Ok, I've marked this task as not done yet: \n");
-                        System.out.println(target);
-                        System.out.println("____________________________________________________________ \n");
-                    }
-                } else if (cmd.equalsIgnoreCase("todo") || cmd.equalsIgnoreCase("deadline") || cmd.equalsIgnoreCase("event")) {
+                    target.markAsDone();
+                    System.out.println("____________________________________________________________ \n");
+                    System.out.println("Nice! I've marked this task as done: \n");
+                    System.out.println(target);
+                    System.out.println("____________________________________________________________ \n");
+                } else if (cmd == Command.UNMARK) {
+                    int index = Integer.parseInt(inputArray[1]) - 1;
+                    Task target = list.get(index);
+                    target.markAsNotDone();
+                    System.out.println("____________________________________________________________ \n");
+                    System.out.println("Ok, I've marked this task as not done yet: \n");
+                    System.out.println(target);
+                    System.out.println("____________________________________________________________ \n");
+                } else if (cmd == Command.TODO || cmd == Command.DEADLINE || cmd == Command.EVENT) {
                     if (inputArray.length <= 1) {
                         throw new TudeeException("Oopsie daisy, are you not aware of my capabilities?!?! I need to know what you want to add.");
                     } else {
-                        if (cmd.equalsIgnoreCase("todo")) {
+                        if (cmd == Command.TODO) {
                             currentTask = new ToDo(inputArray[1]);
-                        } else if (cmd.equalsIgnoreCase("deadline")) {
+                        } else if (cmd == Command.DEADLINE) {
                             String[] deadline = inputArray[1].split(" /by ");
                             currentTask = new Deadline(deadline[0], deadline[1]);
                         } else {
@@ -70,7 +73,7 @@ public class Tudee {
                         System.out.println("____________________________________________________________ \n");
                     }
                 }
-                else if (cmd.equalsIgnoreCase("delete")) {
+                else if (cmd == Command.DELETE) {
                     int index = Integer.parseInt(inputArray[1]) - 1;
                     Task temp = list.get(index);
                     list.remove(index);
@@ -92,5 +95,35 @@ public class Tudee {
             }
         }
         sc.close();
+    }
+
+    private static Command getCmd(String command) {
+        if (command.equalsIgnoreCase("list")) {
+            return Command.LIST;
+        }
+        else if (command.equalsIgnoreCase("bye")) {
+            return Command.BYE;
+        }
+        else if (command.equalsIgnoreCase("todo")) {
+            return Command.TODO;
+        }
+        else if (command.equalsIgnoreCase("deadline")) {
+            return Command.DEADLINE;
+        }
+        else if (command.equalsIgnoreCase("event")) {
+            return Command.EVENT;
+        }
+        else if (command.equalsIgnoreCase("mark")) {
+            return Command.MARK;
+        }
+        else if (command.equalsIgnoreCase("unmark")) {
+            return Command.UNMARK;
+        }
+        else if (command.equalsIgnoreCase("delete")) {
+            return Command.DELETE;
+        }
+        else {
+            return Command.UNKNOWN;
+        }
     }
 }
