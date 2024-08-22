4da3c58bbbd5ee817ef928af0686e73c660f2d5f
MiloTruck <milotruckctf@gmail.com>
2024-08-22 19:09:49 +0800

    Add task types

--- src/main/java/MiluTrock.java
+++ src/main/java/MiluTrock.java
@@ -15,29 +15,49 @@ public class MiluTrock {
                 // Exit if "bye" is entered
                 break;
             } else if (input.equals("list")) {
-                // List all previous inputs if "list" is entered
+                // List all tasks
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
             } else if (words.length == 2 && words[0].equals("mark")) {
-                // Mark task as done if "mark" is entered as the first word
+                // Mark task as done
                 int i = Integer.parseInt(words[1]) - 1;
                 tasks.get(i).markDone();
 
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(tasks.get(i));
             } else if (words.length == 2 && words[0].equals("unmark")) {
-                // Unmark task as done if "unmark" is entered as the first word
+                // Unmark task as done
                 int i = Integer.parseInt(words[1]) - 1;
                 tasks.get(i).unmarkDone();
 
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(tasks.get(i));
-            } else {
-                // Otherwise, store input
-                System.out.println("Added: " + input);
-                tasks.add(new Task(input));
+            } else if (words[0].equals("todo")){
+                // Add todo task
+                String taskName = input.substring(5);
+                ToDo task = new ToDo(taskName);
+                tasks.add(task);
+                
+                System.out.println("Got it. I've added this task:\n" + task);
+            } else if (words[0].equals("deadline")){
+                // Add deadline task
+                String taskInput = input.substring(9);
+                String[] parts = taskInput.split(" /by ");
+                Deadline task = new Deadline(parts[0], parts[1]);
+                tasks.add(task);
+                
+                System.out.println("Got it. I've added this task:\n" + task);
+            } else if (words[0].equals("event")){
+                // Add event task
+                String taskInput = input.substring(6);
+                String[] parts = taskInput.split(" /from ");
+                String[] dates = parts[1].split(" /to ");
+                Event task = new Event(parts[0], dates[0], dates[1]);
+                tasks.add(task);
+                
+                System.out.println("Got it. I've added this task:\n" + task);
             }
         }
 
