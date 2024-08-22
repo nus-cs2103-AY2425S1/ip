afae620e4394f2a00869459ba225750bb4f56f6d
thisisaditya17 <joshi.adi1734@gmail.com>
2024-08-22 10:35:43 +0800

    KillJoy.java: update chatbot name and implement basic skeleton
    - Rename the chatbot from 'Duke' to 'KillJoy'.
    - Implement a skeletal version of the chatbot, including greeting and exit functionalities.

---
+++ src/main/java/KillJoy.java
@@ -0,0 +1,20 @@
+public class KillJoy {
+    public static void main(String[] args) {
+        String logo = " _  ___ _ _      _             \n" +
+                        "| |/ (_) | |    | |            \n" +
+                        "| ' / _| | |    | | ___  _   _ \n" +
+                        "|  < | | | |_   | |/ _ \\| | | |\n" +
+                        "| . \\| | | | |__| | (_) | |_| |\n" +
+                        "|_|\\_\\_|_|_|\\____/ \\___/ \\__, |\n" +
+                        "                          __/ |\n" +
+                        "                         |___/ \n";
+        System.out.println("------------------------------------");
+        System.out.println(logo);
+        System.out.println("------------------------------------");
+        System.out.println(" Hello! I'm KillJoy \n What can I do for you?");
+        System.out.println("------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------");
+
+    }
+}
