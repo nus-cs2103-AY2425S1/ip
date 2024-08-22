c7e640504e6a49a73318269e004e963ef876f085
jerrelllee <leejerrell337@gmail.com>
2024-08-22 23:03:06 +0800

    Renamed chatbot and added greeting

---
+++ src/main/java/Fence.java
@@ -0,0 +1,7 @@
+public class Fence {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Fence");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
