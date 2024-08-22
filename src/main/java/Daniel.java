ed4731fe869f0631774be6cf1257024201641862
LiuYJ2002 <liuyujia2002@gmail.com>
2024-08-22 18:42:07 +0800

    level 0

---
+++ src/main/java/Daniel.java
@@ -0,0 +1,5 @@
+public class Daniel {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Daniel\nWhat can I do for you?\nBye hope to see you again soon");
+    }
+}
