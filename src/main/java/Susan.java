0c6368273569f02237e85d23326b82558eee7a0f
kahsuann <kahsuan@hotmail.sg>
2024-08-22 17:57:27 +0800

    Initialised Susan's Greet and Exit

---
+++ src/main/java/Susan.java
@@ -0,0 +1,9 @@
+public class Susan {
+    public static void main(String[] args) {
+        // Greet
+        System.out.println("Hi there, Susan's at your service!");
+        System.out.println("How may I assist you?");
+        // Exit
+        System.out.println("Good bye, slay the day!");
+    }
+}
