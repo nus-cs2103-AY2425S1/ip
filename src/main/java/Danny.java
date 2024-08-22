5231ecfd8dfc73dbfeb289777d335bf744e403f5
TheRareFox <zhang.yuxiang290902@gmail.com>
2024-08-22 22:40:18 +0800

    Updated Chatbot with echoing functionalities and ability to say bye

--- src/main/java/Danny.java
+++ src/main/java/Danny.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Danny {
     public static void main(String[] args) {
         String seperator = "____________________________________________________________";
@@ -5,6 +7,12 @@ public class Danny {
         System.out.println("Hello! I'm Danny");
         System.out.println("What can I do for you?");
         System.out.println(seperator);
+        Scanner input = new Scanner(System.in);
+        String in = input.next();
+        while(!Objects.equals(in, "bye")){
+            System.out.println(in);
+            in = input.next();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(seperator);
         return;
