1599f5cc459a68ab69d85a6b4590f5cb05700e9b
miloaisdino <miloaisdino@gmail.com>
2024-08-23 01:14:18 +0800

    Change Duke to IpMan

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ip Man User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/IpMan.java
@@ -0,0 +1,5 @@
+public class IpMan {
+    public static void main(String[] args) {
+        System.out.println("Hello from Ip Man!");
+    }
+}
