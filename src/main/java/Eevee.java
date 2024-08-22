35d1c40372e8939450cc32c6d62893a241be2fc8
myrilla12 <myrilla12@gmail.com>
2024-08-23 01:19:45 +0800

    Enumerate chatbot
    Code is convoluted and does not have good readability.
    
    Enumerate commands to handle them in a more organised manner.

--- src/main/java/Eevee.java
+++ src/main/java/Eevee.java
@@ -2,6 +2,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Eevee {
+    enum Command {
+        BYE, LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT;
+
+        public static Command enumerate(String command) {
+            return Command.valueOf(command.toUpperCase());
+        }
+    }
+
     public static void main(String[] args) {
         ArrayList<Task> tasks = new ArrayList<>();
         String divider = "____________________________________________________________\n";
@@ -16,22 +24,21 @@ public class Eevee {
             System.out.print(divider);
 
             try {
-                if (input.equals("bye")) {
-                    // response to command 'bye'
+                Command command = Command.enumerate(input);
+                switch (command) {
+                case BYE:
                     System.out.println(exit);
-                    break;
-                } else if (input.equals("list")) {
-                    // response to command 'list'
+                    return;
+                case LIST:
                     if (tasks.isEmpty()) {
-                        System.out.println("No tasks yet! Start adding tasks :)");
-                    } else {
-                        System.out.println("Here are your tasks:");
-                        tasks.forEach((task) ->
-                                System.out.println((tasks.indexOf(task) + 1) + ". " + task)
-                        );
+                        throw new EeveeException("No tasks yet! Start adding tasks :)");
                     }
-                } else if (input.equals("mark")) {
-                    // response to command 'mark'
+                    System.out.println("Here are your tasks:");
+                    tasks.forEach((task) ->
+                            System.out.println((tasks.indexOf(task) + 1) + ". " + task)
+                    );
+                    break;
+                case MARK: {
                     int taskNumber = scanner.nextInt();
                     if (taskNumber > tasks.size()) {
                         throw new EeveeException("No task under the given task number. Did you type the wrong number?");
@@ -42,8 +49,9 @@ public class Eevee {
                     }
                     t.markAsDone();
                     System.out.println("Congratulations! I've marked the following task as done:\n  " + t);
-                } else if (input.equals("unmark")) {
-                    // response to command 'unmark'
+                    break;
+                }
+                case UNMARK: {
                     int taskNumber = scanner.nextInt();
                     if (taskNumber > tasks.size()) {
                         throw new EeveeException("No task under the given task number. Did you type the wrong number?");
@@ -55,53 +63,63 @@ public class Eevee {
                     }
                     t.unmarkAsDone();
                     System.out.println("Ok! Task no longer marked as done:\n  " + t);
-                } else if (input.equals("delete")) {
-                    // response to command 'delete'
+                    break;
+                }
+                case DELETE: {
                     int taskNumber = scanner.nextInt();
                     if (taskNumber > tasks.size()) {
                         throw new EeveeException("No task under the given task number. Did you type the wrong number?");
                     }
                     Task t = tasks.get(taskNumber - 1);
                     tasks.remove(taskNumber - 1);
-                    System.out.println("As you wish, this task has been removed:\n " + t + "\nYou now have " + tasks.size() + " task(s).");
-                } else {
+                    System.out.println("As you wish, this task has been removed:\n " + t);
+                    break;
+                }
+                case TODO: {
                     String s = scanner.nextLine().trim();
-
                     if (s.isEmpty()) {
                         throw new EeveeException("No task found :( Please input the task details and description correctly");
                     }
-
-                    if (input.equals("todo")) {
-                        // response to command for T task
-                        Todo t = new Todo(s);
-                        tasks.add(t);
-                        System.out.println("Added the following task to your list:\n" + t);
-                    } else if (input.equals("deadline")) {
-                        // response to command for D task
-                        String[] info = s.split("/", 2);
-                        if (info.length < 2) {
-                            throw new EeveeException("Deadline not given for task type 'deadline'. "
-                                    + "Please input a deadline denoted by '/by' or use task type 'todo' instead.");
-                        }
-                        Deadline d = new Deadline(info[0], info[1]);
-                        tasks.add(d);
-                        System.out.println("Added the following task to your list:\n" + d);
-                    } else if (input.equals("event")) {
-                        // response to command for E task
-                        String[] info = s.split("/", 3);
-                        if (info.length < 3) {
-                            throw new EeveeException("Event start and/or end timings not provided."
-                                    + "Please input a start time denoted by '/from' "
-                                    + "and an end time denoted by '/to' when using task type Event");
-                        }
-                        Event e = new Event(info[0], info[1], info[2]);
-                        tasks.add(e);
-                        System.out.println("Added the following task to your list:\n" + e);
-                    } else {
-                        throw new EeveeException("You seemed to have typed wrong. This is not a valid command.");
-                    }
-                    System.out.println("You now have " + tasks.size() + " task(s).");
+                    Todo t = new Todo(s);
+                    tasks.add(t);
+                    System.out.println("Added the following task to your list:\n" + t);
+                    break;
                 }
+                case DEADLINE: {
+                    String s = scanner.nextLine().trim();
+                    if (s.isEmpty()) {
+                        throw new EeveeException("No task found :( Please input the task details and description correctly");
+                    }
+                    String[] info = s.split("/", 2);
+                    if (info.length < 2) {
+                        throw new EeveeException("Deadline not given for task type 'deadline'. "
+                                + "Please input a deadline denoted by '/by' or use task type 'todo' instead.");
+                    }
+                    Deadline d = new Deadline(info[0], info[1]);
+                    tasks.add(d);
+                    System.out.println("Added the following task to your list:\n" + d);
+                    break;
+                }
+                case EVENT: {
+                    String s = scanner.nextLine().trim();
+                    if (s.isEmpty()) {
+                        throw new EeveeException("No task found :( Please input the task details and description correctly");
+                    }
+                    String[] info = s.split("/", 3);
+                    if (info.length < 3) {
+                        throw new EeveeException("Event start and/or end timings not provided."
+                                + "Please input a start time denoted by '/from' "
+                                + "and an end time denoted by '/to' when using task type Event");
+                    }
+                    Event e = new Event(info[0], info[1], info[2]);
+                    tasks.add(e);
+                    System.out.println("Added the following task to your list:\n" + e);
+                    break;
+                }
+                default:
+                    throw new EeveeException("You seemed to have typed wrong. This is not a valid command.");
+                }
+                System.out.println("You now have " + tasks.size() + " task(s).");
             } catch (EeveeException e) {
                 System.out.println(e.getMessage());
             }
