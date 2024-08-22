8a8bf4aa56baf0b2aeb4a71156a447c67a9451c6
jiayr99 <e1249294@u.nus.edu>
2024-08-22 22:40:17 +0800

    Add exception to deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,12 @@
 public class Deadline extends Task{
     protected String by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws JiaException {
         super(description);
         this.by = by;
+        if (description.isEmpty() || by.isEmpty()) {
+            throw new JiaException("You need to add a task and a deadline!! -_-");
+        }
     }
 
     @Override
