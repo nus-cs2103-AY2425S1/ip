31783c46245e7106948842a61325a96920b1422e
tanwartushar <>
2024-08-23 01:55:07 +0800

    text UI testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
-# create bin directory if it doesn't exist
+#create bin directory if it doesnt exist
 if [ ! -d "../bin" ]
 then
-    mkdir ../bin
+  mkdir ../bin
 fi
 
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
 then
-    rm ACTUAL.TXT
+  rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
 then
-    echo "********** BUILD FAILURE **********"
-    exit 1
+  echo "********** BUILD FAILURE **********"
+  exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Donna < input.txt > ACTUAL.TXT
 
-# convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# remove any trailing spaces from ACTUAL.TXT
+sed -i '' 's/[ \t]*$//' ACTUAL.TXT
 
-# compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+# compare thew output tp the expected output
+diff ACTUAL.TXT EXPECTED.TXT
+#diff --strip-trailing-cr ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
-    echo "Test result: PASSED"
-    exit 0
+  echo "Test result: PASSED"
+  exit 0
 else
-    echo "Test result: FAILED"
-    exit 1
+  echo "Test result: FAILED"
+  exit 1
 fi
