6fb8213064eef410a10207f0cbea230455944a7b
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-08-22 17:25:57 +0800

    Implement add-to-list functionality

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,35 +1,59 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Luke {
-    public static void echo() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        if (Objects.equals(input, "bye")) {
-            System.out.println("____________________________________________________________\n"
-                    + "yeah bye bye to you too human being\n"
-                    + "____________________________________________________________\n");
-        } else {
-            System.out.println("____________________________________________________________\n"
-                    + input + "\n"
-                    + "____________________________________________________________\n");
-            echo();
+    private static ArrayList<String> list = new ArrayList<>();
+    public static void command() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (Objects.equals(input, "bye")) {
+                System.out.println("""
+                        ____________________________________________________________
+                        yeah bye bye to you too human being
+                        ____________________________________________________________
+                        """);
+                System.exit(0);
+            } else if (Objects.equals(input, "list")) {
+                showList();
+            } else {
+                addToList(input);
+            }
         }
+    }
+    public static void addToList(String input) {
+        list.add(input);
+        System.out.println("____________________________________________________________\n"
+                + "added: " + input + "\n"
+                + "____________________________________________________________\n");
+        command();
+    }
+
+    public static void showList() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i+1 + ". " + list.get(i));
+        }
+        System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
-        String logo = ".____           __           \n"
-                + "|    |    __ __|  | __ ____  \n"
-                + "|    |   |  |  \\  |/ // __ \\ \n"
-                + "|    |___|  |  /    <\\  ___/ \n"
-                + "|_______ \\____/|__|_ \\\\___  >\n"
-                + "        \\/          \\/    \\/";
+        String logo = """
+                .____           __          \s
+                |    |    __ __|  | __ ____ \s
+                |    |   |  |  \\  |/ // __ \\\s
+                |    |___|  |  /    <\\  ___/\s
+                |_______ \\____/|__|_ \\\\___  >
+                        \\/          \\/    \\/""";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________\n"
-                + "i'm luke, your chatbot.\n"
-                + "say something and i will echo it (unless...you wanna say bye to me)\n"
-                + "____________________________________________________________\n");
-        echo();
+        System.out.println("""
+                ____________________________________________________________
+                i'm luke, your chatbot.
+                say something and i will echo it (unless...you wanna say bye to me)
+                ____________________________________________________________
+                """);
+        command();
     }
 }
