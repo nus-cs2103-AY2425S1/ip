e07e8e7b2e65578aa7f7caf44da9cee080052f9a
Vicelya Visakha <>
2024-08-22 16:41:17 +0800

    Add Echo Feature
    Echo feature uses printWriter for efficiency.
    Loops as long as no "bye" command is written

--- src/main/java/Tira.java
+++ src/main/java/Tira.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Tira {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        PrintWriter printer = new PrintWriter(System.out);
         String logo = "TIRAMISU THE CAFE CAT (TIRA)";
         System.out.println("MIAO (Hello) from\n" + logo + "\n" +
-                "What can I do for you, miao? + \n" +
-                "Bye. Hope to see you again, miao!") ;
+                "What can I do for you, miao?\n");
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                printer.print(command + "\n");
+                printer.flush();
+            }
+        }
+       printer.println("Bye. Come back with treats, miao!");
+        printer.close();
+        // add scanner to take inputs (in string)
+        // change all the commands into a lower case
+        // have a list of words that could be a command
     }
 }
