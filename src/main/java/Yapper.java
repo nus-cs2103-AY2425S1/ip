c4164b1d3b566b46fcb323b1f104ec9f3812e4c4
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-08-22 14:55:43 +0800

    Level-0: Rename main class to Yapper and add greeting and exit functionality

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,14 @@
+public class Yapper {
+
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Yapper");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        
+        // Logic to wait for user input would go here
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
