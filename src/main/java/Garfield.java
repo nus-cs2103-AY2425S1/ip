f0f9d917ceba800bd84710188030dfd4e1199ca8
chuajunyu <chuajunyu1@gmail.com>
2024-08-23 00:17:13 +0800

    add user input and echo functionality

--- src/main/java/Garfield.java
+++ src/main/java/Garfield.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Garfield {
     public static void main(String[] args) {
         String logo = """
@@ -13,9 +15,24 @@ public class Garfield {
         System.out.println("Meowlo! I'm\n\n" + logo);
         System.out.println("What can I do for you?");
         Garfield.line(75);
+
+        // for loop
+        Scanner inputScanner = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            userInput = inputScanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                Garfield.line(75);
+                System.out.println(userInput);
+                Garfield.line(75);
+            }
+        }
+
+        Garfield.line(75);
         System.out.println("Bye. Hope you bring me more tuna next time. Meow!");
         Garfield.line(75);
-
     }
 
     private static void line(int length) {
