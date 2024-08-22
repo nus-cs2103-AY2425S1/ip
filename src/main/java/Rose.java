0c5034557eeb1925091dfcf0cb2e610f06a8bb22
hastutiherah <hastherah@gmail.com>
2024-08-22 21:47:52 +0800

    Create Echo

--- src/main/java/Rose.java
+++ src/main/java/Rose.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Rose {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String horizontal = "____________________________________________________________";
         String name = "Rose";
 
-        System.out.println(horizontal);
-        System.out.println("Hi gorgeous! I'm " + name + "~~");
-        System.out.println("How can I help you today?");
-        System.out.println(horizontal);
-        System.out.println("See you~~ Don't forget to drink some water ^_^");
-        System.out.println(horizontal);
+        String opening = horizontal + "\nHi gorgeous! I'm " + name + "~~\n"
+                + "How can I help you today?\n" + horizontal;
+        String closing = horizontal + "\nSee you~~ Don't forget to drink some water ^_^\n" + horizontal;
+
+        System.out.println(opening.indent(4));
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println((horizontal + '\n' + command +
+                    '\n' + horizontal).indent(4));
+            command = scanner.nextLine();
+        }
+
+        System.out.println(closing.indent(4));
     }
 }
