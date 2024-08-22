24553df81952096a03e8acdbe83b2fa8f1efd9b7
LiuYJ2002 <liuyujia2002@gmail.com>
2024-08-22 18:56:00 +0800

    Level 1

--- src/main/java/Daniel.java
+++ src/main/java/Daniel.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Daniel {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Daniel\nWhat can I do for you?\nBye hope to see you again soon");
+        System.out.println("Hello! I'm Daniel\nWhat can I do for you?\n");
+        Scanner scanner = new Scanner(System.in);
+        boolean val = true;
+        while(val){
+            String input = scanner.nextLine();
+            if (input.equals("bye")){
+                val = false;
+                System.out.println("Bye hope to see you again soon");
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
