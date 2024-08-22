550a8f434159280f0be9cec7f93705ec254e7be3
CJianzhi <cjianzhi23@gmail.com>
2024-08-22 14:16:19 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task{
+    private String dueDate;
+
+    public Deadline(String details, String dueDate) {
+        super(details);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D] %s (by: %s )", super.toString(), dueDate);
+    }
+}
