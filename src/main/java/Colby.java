8684d4f76f49a7fc0829c0d00ee6aeb0d0420f92
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-08-22 10:33:36 +0800

    Echoes user input and says bye

--- src/main/java/Colby.java
+++ src/main/java/Colby.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Colby {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = "  ____      _ _           \n"
                     + " / ___|___ | | |__  _   _ \n"
                     + "| |   / _ \\| | '_ \\| | | |\n"
@@ -7,7 +11,17 @@ public class Colby {
                     + "\\____\\___/|_|_.__/ \\__, /\n"
                     + "                   |___/ \n";
 
-        //String logo = " ";
-        System.out.println("Hello! I'm \n"+ logo + "\n" + "What can I do for you?\n" + "Bye! Hope to see you again soon!" );
+        System.out.println("Hello! I'm \n"+ logo + "\n" + "What can I do for you?\n");
+        while (true) {
+            String task = scanner.nextLine();
+
+            if (task.equalsIgnoreCase("bye")) {
+                System.out.println("  Bye bye! Hope to see you again soon! :)");
+                System.out.println("_________________________________________________");
+                break;
+            }
+            System.out.println("  " + task);
+        }
+        scanner.close();
     }
 }
