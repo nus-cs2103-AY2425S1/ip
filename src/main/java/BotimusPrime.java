700ab675e6d7b7743172ee0d0e517780c7b2c1aa
Ivan <ivanjerrickkoh@gmail.com>
2024-08-22 10:25:26 +0800

    Changed chatbot name, implemented initial skeleton

---
+++ src/main/java/BotimusPrime.java
@@ -0,0 +1,15 @@
+public class BotimusPrime {
+    public static void main(String[] args) {
+        String name = "BotimusPrime";
+
+        String message =
+                "____________________________________________________________\n" +
+                " Hello! I'm " + name + "\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(message);
+    }
+}
