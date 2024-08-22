0f6dfede0dc63b1e8ff6ee296e1b215e2dbd05b2
yugan01 <E1032496@u.nus.edu>
2024-08-22 23:37:09 +0800

    Renaming to Atreides

---
+++ src/main/java/Atreides.java
@@ -0,0 +1,18 @@
+public class Atreides {
+    public static void main(String[] args) {
+        String logo =  "          _            _     _\n"
+                    +  "    / \\  | |          (_)   | |\n"
+                    +  "   /   \\ | |_ _ __ ___ _  __| | ___  ___\n"
+                    +  "  / / \\ \\| __| '__/ _ \\ |/ _` |/ _ \\/ __|\n"
+                    +  " / _____ \\ |_| | |  __/ | (_| |  __/\\__ \\ \n"
+                    +  "/_/     \\_\\__|_|  \\___|_|\\__,_|\\___||___/\n";
+
+//        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
