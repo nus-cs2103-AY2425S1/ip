4bb7e67bcc5eca41d92fed5f7e6c8757bd9540d5
QuekBingHeng <quekbingheng.17@gmail.com>
2024-08-22 20:16:06 +0800

    add Level-0, Angel.java

---
+++ src/main/java/Angel.java
@@ -0,0 +1,10 @@
+public class Angel {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n";
+        String greeting = " Hello! I'm Angel\n What can I do for you?\n";
+        String farewell = " Bye. Hope to see you again soon!\n";
+
+        System.out.println(logo + greeting + logo);
+        System.out.println(farewell + logo);
+    }
+}
