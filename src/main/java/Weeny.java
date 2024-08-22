791dd6e2ea227217a735d386d8b7f1a185f419f7
clarud <clarencejw@gmail.com>
2024-08-23 01:47:43 +0800

    Level-0

---
+++ src/main/java/Weeny.java
@@ -0,0 +1,14 @@
+public class Weeny {
+    public static void main(String[] args) {
+        boolean farewell = false;
+        line();
+        System.out.println("Hello! I'm Weeny\nWhat can I do for you?");
+        line();
+        System.out.println("Bye. Hope to see you soon!");
+        line();
+    }
+
+    public static void line() {
+        System.out.println("______________________________________________");
+    }
+}
