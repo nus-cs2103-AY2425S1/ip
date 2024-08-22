393fd5b62fbf5347360092d0b0b71e881b9a532a
Jem Zhang <zhangjem321@gmail.com>
2024-08-22 17:05:12 +0800

    feat: Added chatbot greeting

--- src/main/java/Donk.java
+++ src/main/java/Donk.java
@@ -1,10 +1,11 @@
 public class Donk {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = " ____________________________________________________________\n" +
+                " Hello! I'm Donk, the super intelligent chatbot\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greeting);
     }
 }
