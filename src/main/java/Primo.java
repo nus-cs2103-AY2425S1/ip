83182237f3dc374dc1b2c18d5446e9325b2b2dcb
choonzies <pohjingchoon@gmail.com>
2024-08-22 11:54:32 +0800

    Add skeletal version with new chatbot name

---
+++ src/main/java/Primo.java
@@ -0,0 +1,10 @@
+public class Primo {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                        "Hello! I'm El Primo!!\n" +
+                        "What can I do for you?\n" +
+                        "____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________");
+    }
+}
