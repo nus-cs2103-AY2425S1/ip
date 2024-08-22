d6eb2eb7cd04521104553e6c95fe999df2495339
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-08-22 09:12:09 +0800

    add, list added, level 2 done

--- src/main/java/Schedulo.java
+++ src/main/java/Schedulo.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Schedulo {
     public static void main(String[] args) {
         String horizontalLine = "---------------";
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> Tasks = new ArrayList<>();
         System.out.println(horizontalLine);
         System.out.println("Hello I am Schedulo!");
         System.out.println("What can I do for you?");
@@ -16,7 +18,14 @@ public class Schedulo {
                 System.out.println(horizontalLine);
                 break;
             }
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < Tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + Tasks.get(i));
+                }
+            } else {
+                Tasks.add(command);
+                System.out.println("added: " + command);
+            }
             System.out.println(horizontalLine);
         }
 
