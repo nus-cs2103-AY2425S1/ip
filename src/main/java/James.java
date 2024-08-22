b60c0479e2e60993bea2581eed4e0bbe0ea846b2
shuckycheese <darrylshuck@yahoo.com.sg>
2024-08-22 22:06:12 +0800

    upgraded to accept commands and echo them back to the user

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class James {
     public static void main(String[] args) {
         String greeting = "Hello Big Boy! I'm James \n"
                 + "How can I assist you today? \n";
         String exitMessage = "Goodbye. Come back anytime! \n";
-        System.out.println(greeting + exitMessage);
+
+        System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            System.out.print("> ");
+            command = scanner.nextLine();
+
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println(command);
+        }
+
+        System.out.println(exitMessage);
+
+        scanner.close();
     }
 }
+
