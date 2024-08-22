b8466109b93579e10ece2cb555d6d0ece6bf4bf0
Teddayz <mervynneo02@gmail.com>
2024-08-22 15:19:43 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Meeks.java
@@ -0,0 +1,9 @@
+public class Meeks {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n"
+                            + "Hello! I'm Meeks\n"
+                            + "What can I do for you?\n"
+                            + "____________________________________________________________\n"
+                            + "Bye. Hope to see you again soon!");
+    }
+}
