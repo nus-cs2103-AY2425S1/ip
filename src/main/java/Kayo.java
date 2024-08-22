0bdab2d2c38eb9d51865e5dd10ed5cded8e6505f
arnav12344 <64593433+arnav12344@users.noreply.github.com>
2024-08-22 21:16:27 +0800

    Task class added and implemented

--- src/main/java/Kayo.java
+++ src/main/java/Kayo.java
@@ -1,20 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kayo {
-
     public static void main(String[] args) {
+        List<Task> taskList = new ArrayList<>();
         Greet();
         Scanner input = new Scanner(System.in);
         String inputString = "";
-        while(!inputString.equals("Bye")) {
+        while(true) {
             inputString = input.nextLine();
-            if(inputString.equals("Bye")) {
+            if(inputString.equals("bye")) {
                 break;
+            } else if(inputString.equals("list")) {
+                ListItems(taskList);
+            } else if (inputString.contains("unmark")) {
+                String[] splitList = inputString.split(" ");
+                int index = Integer.parseInt(splitList[1])-1;
+                taskList.get(index).setDone(false);
+
+                System.out.print("OK, I've marked this task as not done yet:\n");
+                System.out.print(taskList.get(index).toString());
+                System.out.print("\n");
+
+            } else if(inputString.contains("mark")) {
+                String[] splitList = inputString.split(" ");
+                int index = Integer.parseInt(splitList[1]) -1;
+                taskList.get(index).setDone(true);
+
+                System.out.print("Nice! I've marked this task at done:\n");
+                System.out.print(taskList.get(index).toString());
+                System.out.print("\n");
             } else {
-                System.out.println(inputString);
+                Task t = new Task(inputString,false);
+                taskList.add(t);
+                System.out.println("added: " +inputString);
             }
         }
         Exit();
+    }
+    public void runBot() {
+
+    }
+    private static class Task{
+
+        private String task;
+        private boolean isDone;
+
+        public Task(String task, boolean isDone){
+            this.task = task;
+            this.isDone = isDone;
+        }
+        public void setDone(boolean isDone){
+            this.isDone = isDone;
+        }
+        public String toString(){
+            String taskString = (isDone) ? "[X]": "[ ]";
+            return taskString + task;
+        }
+    }
+    private static void ListItems(List<Task> taskList) {
+        System.out.println("Here are the tasks in your list:");
+        for(int i = 0; i < taskList.size(); i++) {
+            System.out.println(i+1 + ". "+ taskList.get(i));
+        }
     }
     public static void Greet() {
         System.out.println("Hello! I'm Kayo! ");
@@ -24,3 +73,4 @@ public class Kayo {
         System.out.println("Bye. I hope to see you again soon!");
     }
 }
+
