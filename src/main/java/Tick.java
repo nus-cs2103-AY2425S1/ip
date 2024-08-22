691402803e111e74946088054a23ef1ec5463764
wrjgold <trexwrj@gmail.com>
2024-08-22 14:40:57 +0800

    implement add and list functions

--- src/main/java/Tick.java
+++ src/main/java/Tick.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Tick {
     private static final String separator = "____________________________________________________________\n";
     private static final String botName = "Tick";
+    private String[] checklist = new String[100];
 
     public void greet() {
         System.out.print(Tick.separator);
@@ -19,8 +20,28 @@ public class Tick {
 
     public void echo(String command) {
         System.out.print(Tick.separator);
-        System.out.println(command);
+        System.out.printf("added: %s\n", command);
         System.out.println(Tick.separator);
+    }
+
+    public void displayList() {
+        System.out.print(Tick.separator);
+        for (int i = 0; i < 100; i++) {
+            if (this.checklist[i] == null) {
+                break;
+            }
+            System.out.printf("%d. %s\n", i + 1, this.checklist[i]);
+        }
+        System.out.println(Tick.separator);
+    }
+
+    private void addToList(String command) {
+        for (int i = 0; i < 100; i++) {
+            if (this.checklist[i] == null) {
+                this.checklist[i] = command;
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -31,8 +52,12 @@ public class Tick {
             String command = scn.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                bot.displayList();
+                continue;
             }
             bot.echo(command);
+            bot.addToList(command);
         }
         bot.exit();
     }
