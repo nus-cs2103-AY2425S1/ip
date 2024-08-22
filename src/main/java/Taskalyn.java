67a31fa140e3b2edde2a3f2f5faa3a1782542136
psh12320 <pshricharan2020@gmail.com>
2024-08-22 23:50:39 +0800

    importing scanner

--- src/main/java/Taskalyn.java
+++ src/main/java/Taskalyn.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Taskalyn {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
