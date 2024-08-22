58534290d6966cbfeed8af57911c886e59cf3da0
yuancheng <>
2024-08-22 20:06:33 +0800

    added "mark" command and changed equalsIgnoreCase to matches (?i)

--- src/main/java/Winner.java
+++ src/main/java/Winner.java
@@ -5,32 +5,44 @@ public class Winner {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("""
                 --------------------------------------
                 Hello! I am Winner.
                 What can I do for you today?
                 --------------------------------------""".indent(10));
+
         while (true) {
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("list")) {
+
+            if (input.matches("(?i)list")) {
                 int counter = 1;
-                System.out.println(" ".repeat(10) + "--------------------------------------");
-                for (String i:tasks) {
-                    System.out.println(" ".repeat(10) + counter + ". " + i);
+                System.out.println(" ".repeat(10) + "-".repeat(38));
+                for (Task i:tasks) {
+                    System.out.println(" ".repeat(10) + counter + ". " + i.taskToString());
                     counter++;
                 }
-                System.out.println(" ".repeat(10) + "--------------------------------------");
-            } else if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" ".repeat(10) + "-".repeat(38));
+
+            } else if (input.matches("(?i)mark \\d+")) {
+                int taskNumber = scanner.nextInt();
+                Task task = tasks.get(taskNumber - 1);
+                task.markDone();
+                System.out.println(("-".repeat(38) +
+                        task.markDoneToString() +
+                        "-".repeat(38)).indent(10));
+
+            } else if (input.matches("(?i)bye")) {
                 System.out.println("""
                         --------------------------------------
                         Hope I have been of service!
                         Thank you and see you again soon :D
                         --------------------------------------""".indent(10));
                 break;
+
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println("--------------------------------------".indent(10) +
                         ("added: " + input).indent(10) +
                         "--------------------------------------".indent(10));
