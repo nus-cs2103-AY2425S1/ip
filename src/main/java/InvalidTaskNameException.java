0b3bdae03604da8e1eac2a27cfd0a1c16a6b9972
Clarenceeey <clarencey100@gmail.com>
2024-08-22 17:04:11 +0800

    Add new constructor for InvalidNameException

--- src/main/java/InvalidTaskNameException.java
+++ src/main/java/InvalidTaskNameException.java
@@ -5,4 +5,8 @@ public class InvalidTaskNameException extends Exception {
     public InvalidTaskNameException (String name) {
         super("Error: The name " + name + " is invalid!");
     }
+
+    public InvalidTaskNameException () {
+        super("Error: Please provide a name!");
+    }
 }
