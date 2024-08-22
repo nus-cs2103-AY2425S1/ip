f9c459c2870bb791d901c70da0d4376c9f3e70d7
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-08-22 17:06:51 +0800

    Add echo support

--- src/main/java/JEFF.java
+++ src/main/java/JEFF.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 
 public class JEFF {
     private static final String LINE = "--------------------------------------------";
+    private static final Scanner sc = new Scanner(System.in); // Scanner object to detect user input
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Scanner object to detect user input
-
         // Init sequence
         String logo =
                 """
@@ -18,11 +17,26 @@ public class JEFF {
         System.out.println("Hello there\n" + logo);
 
         // Chat loop
+        beginChat();
+    }
+
+    public static void beginChat() {
+        System.out.println("What can I do for you?");
         boolean chatCont = true;
         while (chatCont) {
-            chatCont = promptEvent(sc, "What can I do for you?");
+            chatCont = echoEvent();
         }
         exitEvent();
+    }
+
+    private static boolean echoEvent() {
+        System.out.println(LINE);
+        String input = sc.nextLine();
+        if (Objects.equals(input, "bye")) {
+            return false;
+        }
+        System.out.println(input);
+        return true;
     }
 
     public static void exitEvent() {
@@ -30,11 +44,11 @@ public class JEFF {
         System.out.println("Bye for now!");
     }
 
-    public static boolean promptEvent(Scanner sc, String prompt) {
+    public static boolean promptEvent(String prompt) {
         System.out.println(LINE);
         System.out.println(prompt);
         String input = sc.nextLine();
-        if (Objects.equals(input, "nothing")) {
+        if (Objects.equals(input, "bye")) {
             return false;
         }
         return true;
