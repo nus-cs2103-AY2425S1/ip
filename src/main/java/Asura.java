12ce3f09d82bd67db74cc17bf7e77c455694a409
ossaris56 <ossaris56@gmail.com>
2024-08-23 04:10:30 +0800

    add mark and unmark as done

--- src/main/java/Asura.java
+++ src/main/java/Asura.java
@@ -17,24 +17,50 @@ public class Asura {
                 What can I do for you?""";
         String goodbye = """
                 Bye. Hope to see you again soon!""";
-        List<String> userInput = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         System.out.println(formatResponse(introduction));
-        String input = scanner.nextLine();
+        String[] input = scanner.nextLine().split(" ");
 
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
+        while (!input[0].equals("bye")) {
+            if (input[0].equals("list")) {
                 StringBuilder output = new StringBuilder();
-                for (int i=0; i<userInput.size(); i++) {
-                    output.append(i + 1).append(". ").append(userInput.get(i)).append("\n");
+                output.append("Here are the tasks in your list:\n");
+                for (int i=0; i<tasks.size(); i++) {
+                    output.append(i + 1).append(".").append(tasks.get(i).getStatusIcon()).append(tasks.get(i).description).append("\n");
                 }
                 System.out.println(formatResponse(output.toString()));
             }
-            else {
-                userInput.add(input);
-                System.out.println(formatResponse("added: " + input));
+            else if (input[0].equals("mark")) {
+                if (1 >= input.length) {
+                    // if user inputted mark by itself
+                    System.out.println(formatResponse("Oops! Something is wrong."));
+                } else {
+                    StringBuilder output = new StringBuilder();
+                    int selection = Integer.parseInt(input[1]) - 1;
+                    tasks.get(selection).markAsDone();
+                    output.append("Nice! I've marked this task as done:").append("\n").append(tasks.get(selection).getStatusIcon()).append(tasks.get(selection).description);
+                    System.out.println(formatResponse(output.toString()));
+                }
             }
-            input = scanner.nextLine();
+            else if (input[0].equals("unmark")) {
+                if (1 >= input.length) {
+                    // if user inputted mark by itself
+                    System.out.println(formatResponse("Oops! Something is wrong."));
+                } else {
+                    StringBuilder output = new StringBuilder();
+                    int selection = Integer.parseInt(input[1]) - 1;
+                    tasks.get(selection).markAsNotDone();
+                    output.append("OK, I've marked this task as not done yet:").append("\n").append(tasks.get(selection).getStatusIcon()).append(tasks.get(selection).description);
+                    System.out.println(formatResponse(output.toString()));
+                }
+            }
+            else {
+                String taskString = String.join(" ", input);
+                tasks.add(new Task(taskString));
+                System.out.println(formatResponse("added: " + taskString));
+            }
+            input = scanner.nextLine().split(" ");
         }
         System.out.println(formatResponse(goodbye));
     }
