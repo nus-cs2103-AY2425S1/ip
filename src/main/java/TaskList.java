04b6dfff104409e7ab43702c2fbe2f4736240529
Mattis Ong <mattisongjj@gmail.com>
2024-08-22 16:49:36 +0800

    Feat: Integrate Task class into TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,14 +2,32 @@ import java.util.ArrayList;
 
 // TaskList class encapsulates a task list tracked by Bro Bot
 public class TaskList {
-    private ArrayList<String> tasks;
+    private ArrayList<Task> tasks;
 
     public TaskList() {
-        this.tasks = new ArrayList<String>();
+        this.tasks = new ArrayList<Task>();
     }
 
-    public void addTask(String task) {
-        tasks.add(task);
+    public void addTask(String taskContent) {
+        tasks.add(new Task(taskContent));
+    }
+
+    public void markTask(int taskId) {
+        try {
+            Task task = tasks.get(taskId);
+            task.markTask();
+        } catch (IndexOutOfBoundsException e) {
+            return;
+        }
+    }
+
+    public void unmarkTask(int taskId) {
+        try {
+            Task task = tasks.get(taskId);
+            task.unmarkTask();
+        } catch (IndexOutOfBoundsException e) {
+            return;
+        }
     }
 
     public void printAllTasks() {
