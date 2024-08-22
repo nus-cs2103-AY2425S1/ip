00c0f50ddbf817317add4a848aa30edcd1a02cbd
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 00:11:01 +0800

    Add universal message builder class to send messages

---
+++ src/main/java/MessageBuilder.java
@@ -0,0 +1,7 @@
+public class MessageBuilder {
+    private void buildMessage(String message) {
+        System.out.println("______________________________________________________________________");
+        System.out.println(message);
+        System.out.println("______________________________________________________________________");
+    }
+}
