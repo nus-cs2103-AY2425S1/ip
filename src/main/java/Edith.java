eb65493b88c4f152cb3c437bd12d6029be5aa6e0
clarissatjx <claire.teo145@gmail.com>
2024-08-22 21:16:50 +0800

    Level-1

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,7 +1,9 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Edith {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         String horizontal = "____________________________________________________________";
         String greeting = " heyyy im edith! \n what can I do for you?";
@@ -10,7 +12,16 @@ public class Edith {
 
         System.out.println(horizontal + linebreak +
                 greeting + linebreak +
-                horizontal + linebreak +
-                farewell + linebreak + horizontal);
+                horizontal);
+
+        String line = scanner.nextLine();
+
+        while (!Objects.equals(line, "bye")) {
+            System.out.println(horizontal + linebreak + " " +
+                    line + linebreak + horizontal);
+            line = scanner.nextLine();
+        }
+
+        System.out.println(horizontal + linebreak + farewell + linebreak + horizontal);
     }
 }
