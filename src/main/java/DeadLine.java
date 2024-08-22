54c9dbc79b7e4729884480cbfaab50f7ac8e4c96
Clarenceeey <clarencey100@gmail.com>
2024-08-22 16:32:25 +0800

    Add throwing error into DeadLine

--- src/main/java/DeadLine.java
+++ src/main/java/DeadLine.java
@@ -4,9 +4,24 @@ public class DeadLine extends Task {
 
     protected String endDate;
 
-    public DeadLine (String name, String endDate) {
-        super(name);
-        this.endDate = endDate;
+    public DeadLine (String inputString) throws InvalidTaskNameException, NoDateException {
+        if (inputString.contains("/by ")) {
+            int index = inputString.indexOf("/by ");
+            String taskName = inputString.substring(0, index);
+            String byDate = inputString.substring(index+4);
+            if (taskName.length() == 0) {
+                throw new InvalidTaskNameException(taskName);
+            }
+
+            if (endDate.length() == 0) {
+                throw new NoDateException();
+            }
+
+            this.name = taskName;
+            this.endDate = byDate;
+
+        }
+        throw new NoDateException();
     }
 
     @Override
