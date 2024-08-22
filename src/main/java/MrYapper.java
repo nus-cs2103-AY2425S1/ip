53b68bc327eb1d1b6de8caca946ffad6d7e3a1a5
awdse22 <ngkhengyang@gmail.com>
2024-08-22 17:22:00 +0800

    Changed greeting message

--- src/main/java/MrYapper.java
+++ src/main/java/MrYapper.java
@@ -1,10 +1,11 @@
 public class MrYapper {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "____________________________________________________________\n"
+                + " Hello! I'm MrYapper\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(greeting);
     }
 }
