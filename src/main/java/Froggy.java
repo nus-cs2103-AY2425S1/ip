1333e9a945eba4e0097a52f3d3ec756485aaad64
Chen Lei Yu <128605764+ChenLeiyu@users.noreply.github.com>
2024-08-22 20:34:52 +0800

    Add task input and listing
    - Change greeting text to explain chatbot commands
    - Change text line formatting

--- src/main/java/Froggy.java
+++ src/main/java/Froggy.java
@@ -1,15 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Froggy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
+        List<String> tasks = new ArrayList<>();
 
         String greeting = "Hello! I'm Froggy!\n"
-                + "What can I do for you?\n"
-                + "_______________________________\n";
+                + "Enter tasks and I will store it.\n"
+                + "Type 'list' to view tasks or 'bye' to exit.\n"
+                + "_______________________________";
         String exit = "Bye. Hope to see you again soon!\n"
-                + "_______________________________\n";
+                + "_______________________________";
 
         System.out.println(greeting);
 
@@ -19,12 +23,18 @@ public class Froggy {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(exit);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("Task List:");
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(i + "." + tasks.get(i - 1));
+                }
+                System.out.println("_______________________________");
+            } else {
+                tasks.add(input);
+                System.out.println("Added: " + input + "\n"
+                        + "_______________________________");
             }
-
-            System.out.println(input + "\n"
-                    + "_______________________________\n");
         }
-
         scanner.close();
     }
 }
