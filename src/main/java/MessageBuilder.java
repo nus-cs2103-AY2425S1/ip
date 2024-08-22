954cbd5c6f0b4eae34b68bd9cdc5c303fb6ec17d
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 00:11:28 +0800

    Add greeting and goodbye message

--- src/main/java/MessageBuilder.java
+++ src/main/java/MessageBuilder.java
@@ -4,4 +4,12 @@ public class MessageBuilder {
         System.out.println(message);
         System.out.println("______________________________________________________________________");
     }
+
+    public void sendGreetingMessage() {
+        buildMessage("HELLO???!!! I'm Puke and I'm Puk *uwaeh* ing... .-.");
+    }
+
+    public void sendGoodbyeMessage() {
+        buildMessage("*BYE!!! *UWAGHhhHH* !!! see Ya *uWahghgh*");
+    }
 }
