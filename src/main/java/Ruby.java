f4fb645f36625550bd88d09f1dbedc6cdc964eab
adriantan101 <atjy102@gmail.com>
2024-08-23 02:43:44 +0800

    added the list feature

--- src/main/java/Ruby.java
+++ src/main/java/Ruby.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Ruby {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String greeting = """
                 Hello! I'm Ruby
                 What can I do for you?
@@ -11,10 +13,18 @@ public class Ruby {
 
         while (true) {
             String command = scanner.nextLine();
-            System.out.println(command);
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("     added: " + command);
             }
         }
     }
