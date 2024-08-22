7f6fcb60c3970b068cbd785624b219b14713a55a
myrilla12 <myrilla12@gmail.com>
2024-08-22 12:38:52 +0800

    Rename Duke
    Duke is a placeholder name.
    
    Should be changed to become more distinct.
    
    Let's rename Duke, change greeting string and add exit string.

---
+++ src/main/java/Eevee.java
@@ -0,0 +1,9 @@
+public class Eevee {
+    public static void main(String[] args) {
+        String divider = "_________________________________________________________\n";
+        String greeting = "Hello! I'm Eevee\nWhat can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(divider + greeting + divider + exit + divider);
+    }
+}
