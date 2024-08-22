a1f3014122feeb73a05a7d52b970831634db8704
Jayden-Kim-NUS <e1121727@u.nus.edu>
2024-08-22 22:52:40 +0800

    Add echo and bye command

--- src/main/java/Arona.java
+++ src/main/java/Arona.java
@@ -1,8 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Arona {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Arona.");
-        System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+        // For user input
+        Scanner in = new Scanner(System.in);
+
+        // Greeting when Opened
+        print("Hello! I'm Arona.");
+        print("What can I do for you?");
+
+        // Process inputs
+        while (true) {
+            String input = in.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            print(input);
+        }
+        print("Bye. Hope to see you again soon!");
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
+    }
+
+    private static void print(Integer lines) {
+        for (int i = 0; i < lines; i++) {
+            System.out.println();
+        }
     }
 }
