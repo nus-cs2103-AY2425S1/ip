3997efb2619716f4c86ff655560495abfe0fae64
leroychiu20 <e1122486@u.nus.edu>
2024-08-22 10:35:54 +0800

    add list

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Alex {
 
     public static final String byeMessage = "Bye. Hope to see you again soon!";
     public static final String LINE = "----------------------------------------------------";
+    ArrayList<String> list = new ArrayList<>();
 
     public void scan() {
         Scanner scanner = new Scanner(System.in);
@@ -13,9 +15,18 @@ public class Alex {
             System.out.println(LINE);
             System.out.println(byeMessage);
             System.out.println(LINE);
+        }
+        else if (userInput.equalsIgnoreCase("list")) {
+            System.out.println(LINE);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+            System.out.println(LINE);
+            scan();
         } else {
             System.out.println(LINE);
-            System.out.println(userInput);
+            System.out.println("added: " + userInput);
+            list.add(userInput);
             System.out.println(LINE);
             scan();
         }
