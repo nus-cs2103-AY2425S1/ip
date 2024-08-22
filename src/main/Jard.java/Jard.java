d0f99e0fb531b669b3395a6d2616c6bb50a43dde
WuJinhan1 <e1075517@u.nus.edu>
2024-08-22 17:09:47 +0800

    finished level 1

--- src/main/Jard.java/Jard.java
+++ src/main/Jard.java/Jard.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jard {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,7 +13,22 @@ public class Jard {
         System.out.println(" Hello! I'm Jard");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
