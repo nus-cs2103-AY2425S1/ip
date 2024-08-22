5a38924a803069ff97190852ecec1ebb1265e850
limjeremy496 <limjeremy496@gmail.com>
2024-08-22 11:29:36 +0800

    Rename main class + Add greeting, exit

---
+++ src/main/java/Vinegar.java
@@ -0,0 +1,10 @@
+public class Vinegar {
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Vinegar\n" +
+                "What Can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
