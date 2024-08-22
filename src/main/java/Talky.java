0dfd2fa0af818da630c6d7f03450c965f2fc0aa1
W3iZhi <tanweizhimail@gmail.com>
2024-08-22 18:11:01 +0800

    Added Add and List

--- src/main/java/Talky.java
+++ src/main/java/Talky.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Talky {
     public static void printSeperator(String content) {
@@ -7,17 +8,29 @@ public class Talky {
         System.out.println(seperator);
     }
     public static void main(String[] args) {
-        String seperator = "---------------------------------------";
+        ArrayList<String> userTexts = new ArrayList<>();
+
         printSeperator("Hello I'm Talky\n" + "How may I help you?");
+
         Scanner sc = new Scanner(System.in);
         while (true) {
             String command = sc.nextLine();
-            if (command.equalsIgnoreCase("bye")) {
+            if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                String textsList = "";
+                int rank = 1;
+                for (String text : userTexts) {
+                    textsList += rank + ". " + text + "\n";
+                    rank++;
+                }
+                printSeperator(textsList);
             } else {
-                printSeperator(command);
+                userTexts.add(command);
+                printSeperator("added: " + command);
             }
         }
+
         printSeperator("Bye. Do let me know if there's anything else!");
     }
 }
