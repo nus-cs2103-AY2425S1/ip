e17d63944c1a9107ddb23af9ed2fcce24dc7aa89
cshao02 <cshao02@gmail.com>
2024-08-22 17:21:31 +0800

    Added Todo class

---
+++ src/main/java/Todo.java
@@ -0,0 +1,5 @@
+public class Todo extends Task {
+    public Todo(int taskNumber, String taskName, boolean taskCompletionStatus) {
+        super(taskNumber, taskName, taskCompletionStatus);
+    }
+}
