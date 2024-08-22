6a4ec96565dc21d6a0465a1cb78fee588db4c958
tata32000 <tata32000@hotmail.com>
2024-08-22 11:54:12 +0800

    Add echo and exits

--- src/main/java/Meep.java
+++ src/main/java/Meep.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Meep {
     public static void main(String[] args) {
         System.out.println("""
@@ -5,8 +7,25 @@ public class Meep {
                 Hello! I'm Meep
                 What can I do for you?
                 -----------------------------------------
-                Bye. Hope to see you again soon!
-                -----------------------------------------
                 """);
+        Scanner scanner = new Scanner(System.in);
+        // keep scanning for user input
+        while (true) {
+            System.out.print("You: ");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("""
+                        -----------------------------------------
+                        Meep: Bye! Have a great day!
+                        -----------------------------------------
+                        """);
+                break;
+            } else {
+                System.out.println("-----------------------------------------\n" +
+                        "Meep: " +
+                        input + "\n" +
+                        "-----------------------------------------");
+            }
+        }
     }
 }
