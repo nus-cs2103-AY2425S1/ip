80de8a7de10a3372954325eb0e31c0669aadd3d8
achl1012 <>
2024-08-22 20:21:35 +0800

    Add Task.java and mark as done feature

--- src/main/java/Charlotte.java
+++ src/main/java/Charlotte.java
@@ -7,7 +7,7 @@ public class Charlotte {
         System.out.println(line + "\n Hello! I'm Charlotte!\n What can I do for you?\n" + line);
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String input;
 
 
@@ -19,13 +19,28 @@ public class Charlotte {
             }
 
             if (input.equalsIgnoreCase("list")) {
-                System.out.println(line);
+                System.out.println(line + "\n Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i).toString());
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                    tasks.get(taskNumber).markAsDone();
+                    System.out.println(line + "\n Nice! I've marked this task as done:\n  "
+                            + tasks.get(taskNumber) + "\n" + line);
+                }
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                    tasks.get(taskNumber).unmark();
+                    System.out.println(line + "\n OK, I've marked this task as not done yet:\n  "
+                            + tasks.get(taskNumber) + "\n" + line);
+                }
             } else {
-                tasks.add(input);
+                Task newTask = new Task(input);
+                tasks.add(newTask);
                 System.out.println(line + "\n added: " + input + "\n" + line);
             }
 
