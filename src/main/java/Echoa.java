d4c526b3bd4113217c8397ef7af2d58620a72f6f
jowhee3011 <joeygoh3011@gmail.com>
2024-08-22 17:08:00 +0800

    Edited Echoa to allow adding and listing of tasks

--- src/main/java/Echoa.java
+++ src/main/java/Echoa.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 public class Echoa {
     /**
      * The main method is the entry point to the application.
-     * It greets the user with a series of messages
-     * and allows input which would be echoed.
-     * The program will be terminated when the user keys in "bye".
+     * It greets the user with a series of messages and allows input.
+     * The user is able to key tasks which will be added to the tasklist,
+     * view their list of tasks using "list"
+     * and end the chat using "bye".
      * @param args
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasklist = new String[100];
+        int taskcount = 0;
 
         System.out.println("Hello! I'm Echoa.");
         System.out.println("What can I do for you?\n");
@@ -24,8 +27,16 @@ public class Echoa {
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < taskcount; i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". "  +  tasklist[i]);
+                }
+                System.out.println();
             } else {
-                System.out.println(command + "\n");
+                System.out.println("added: " + command + "\n");
+                tasklist[taskcount] = command;
+                taskcount++;
             }
         }
     }
