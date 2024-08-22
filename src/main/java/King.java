f6df8b3b340966fd0c9a7a146dc9c07c31483199
ryenl <ryanljh9@gmail.com>
2024-08-22 18:51:14 +0800

    Level-5

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class King {
@@ -18,8 +19,8 @@ class King {
         Scanner scanner = new Scanner(System.in);
 
         //Array to store tasks
-        Task[] tasks = new Task[100];
-        int taskcounter = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
+        //int taskcounter = 0;
 
         while(true) {
             String input = scanner.nextLine();
@@ -27,42 +28,50 @@ class King {
             if (input.equalsIgnoreCase("bye")) {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
-                for (int i = 0; i < taskcounter; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i].toString());
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i).toString());
                 }
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("delete")) {
+                String[] parts = input.split(" ");
+                //prone to error if format is off or value after " " is not an int
+                int num = Integer.parseInt(parts[1]);
+                System.out.println("I have relieved your suffering of:\n" + tasks.get(num - 1).toString());
+                tasks.remove(num - 1);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 System.out.println("____________________________________________________________");
             } else if (input.startsWith("mark")) {
                 String[] parts = input.split(" ");
                 //prone to error if format is off or value after " " is not an int
                 int num = Integer.parseInt(parts[1]);
-                tasks[num - 1].markAsDone();
-                System.out.println("Good job on the completion my minion!\n" + tasks[num - 1].toString());
+                tasks.get(num - 1).markAsDone();
+                System.out.println("Good job on the completion my minion!\n" + tasks.get(num - 1).toString());
                 System.out.println("____________________________________________________________");
             } else if (input.startsWith("unmark")) {
                 String[] parts = input.split(" ");
                 //prone to error if format is off or value after " " is not an int
                 int num = Integer.parseInt(parts[1]);
-                tasks[num - 1].markAsUndone();
-                System.out.println("Better get to work before I execute you!\n" + tasks[num - 1].toString());
+                tasks.get(num - 1).markAsUndone();
+                System.out.println("Better get to work before I execute you!\n" + tasks.get(num - 1).toString());
                 System.out.println("____________________________________________________________");
             } else if (input.startsWith("todo")) {
                 String description = input.substring(5);
-                tasks[taskcounter] = new Todo(description);
+                tasks.add(new Todo(description));
                 System.out.println("____________________________________________________________");
-                System.out.println("Approved. I've added this to your list of duties:\n" + tasks[taskcounter].toString());
-                taskcounter++;
-                System.out.println("Now you have " + taskcounter + " tasks in the list.");
+                System.out.println("Approved. I've added this to your list of duties:\n" + tasks.get(tasks.size() - 1).toString());
+                //taskcounter++;
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 System.out.println("____________________________________________________________");
             } else if (input.startsWith("deadline")) {
                 String[] parts = input.split(" /by ");
                 if (parts.length == 2) {
                     String description = parts[0].substring(9); // "deadline " is 9 chars
                     String by = parts[1];
-                    tasks[taskcounter] = new Deadline(description, by);
+                    tasks.add(new Deadline(description, by));
                     System.out.println("____________________________________________________________");
-                    System.out.println("Approved. I've added this to your list of duties:\n" + tasks[taskcounter].toString());
-                    taskcounter++;
-                    System.out.println("Now you have " + taskcounter + " tasks in the list.");
+                    System.out.println("Approved. I've added this to your list of duties:\n" + tasks.get(tasks.size() - 1).toString());
+                    //taskcounter++;
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     System.out.println("____________________________________________________________");
                 } else {
                     System.out.println("Invalid deadline format.");
@@ -73,11 +82,11 @@ class King {
                     String description = parts[0].substring(6); // "event " is 6 chars
                     String from = parts[1];
                     String to = parts[2];
-                    tasks[taskcounter] = new Event(description, from, to);
+                    tasks.add(new Event(description, from, to));
                     System.out.println("____________________________________________________________");
-                    System.out.println("Approved. I've added this to your list of duties:\n" + tasks[taskcounter].toString());
-                    taskcounter++;
-                    System.out.println("Now you have " + taskcounter + " tasks in the list.");
+                    System.out.println("Approved. I've added this to your list of duties:\n" + tasks.get(tasks.size() - 1).toString());
+                    //taskcounter++;
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     System.out.println("____________________________________________________________");
                 } else {
                     System.out.println("Invalid event format.");
