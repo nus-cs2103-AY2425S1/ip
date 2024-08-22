dba33c1b3b56a84879866c566dc47aad531e394c
TheRareFox <zhang.yuxiang290902@gmail.com>
2024-08-22 22:23:43 +0800

    Changed text bot to Danny

---
+++ src/main/java/Danny.java
@@ -0,0 +1,13 @@
+public class Danny {
+    public static void main(String[] args) {
+        String seperator = "____________________________________________________________";
+        System.out.println(seperator);
+        System.out.println("Hello! I'm Danny");
+        System.out.println("What can I do for you?");
+        System.out.println(seperator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(seperator);
+        return;
+    }
+
+}
