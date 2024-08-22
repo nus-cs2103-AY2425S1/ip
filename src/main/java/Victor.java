88800ab258ba625ff0f22af38522e3028bcbcef4
dasha3412 <dashasychova777@gmail.com>
2024-08-22 10:36:24 +0800

    Saving input and listing

--- src/main/java/Victor.java
+++ src/main/java/Victor.java
@@ -15,18 +15,32 @@ public class Victor {
 + "   `---`     '---'    `._____.'    '---'      '-----'    ''-'   `'-'\n";
 
         Scanner inp = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<String>();
 
         System.out.println(logo);
         System.out.println("Hello! My name is Victor!");
         System.out.println("What can I do for you?");
         System.out.println("============================================================");
         String userInput = inp.nextLine();
-        System.out.println("============================================================");
         while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println("  ~  " + userInput);
+            System.out.println("============================================================");
+            if (userInput.trim().isEmpty()) {
+                // User input is empty - ask again for input
+                System.out.println("  ~  What can I do for you?");
+            } else if (userInput.toLowerCase().contains("list")) {
+                if (inputs.isEmpty()) {
+                    System.out.println("  ~  Nothing has been input so far - nothing to list! What can I do for you?");
+                } else {
+                    for (int i = 0; i < inputs.size(); i++) {
+                        System.out.println("  ~  " + (i + 1) + ". " + inputs.get(i));
+                    }
+                }
+            } else {
+                inputs.add(userInput);
+                System.out.println("  ~  added: " + userInput);
+            }
             System.out.println("============================================================");
             userInput = inp.nextLine();
-            System.out.println("============================================================");
         }
 
         System.out.println("Goodbye! Hope to see you again soon!");
