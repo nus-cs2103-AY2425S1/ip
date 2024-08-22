7b7cf5b19dfaff0a0cde2ffa640ae7601d751dd2
nathanshew <92252099+nathanshew@users.noreply.github.com>
2024-08-22 11:18:28 +0800

    Use Text Block

--- src/main/java/NathanBot.java
+++ src/main/java/NathanBot.java
@@ -1,8 +1,10 @@
 public class NathanBot {
     public static void main(String[] args) {
         String Line = "____________________________________________________________\n";
-        String Greet = " Hello! I'm NathanBot\n" +
-                " What can I do for you?\n";
+        String Greet = """
+                        Hello! I'm NathanBot
+                        What can I do for you?
+                       """;
         String Exit = " Bye. Hope to see you again soon!\n";
         System.out.println(Line + Greet + Line + Exit + Line);
     }
