ebcf0b4b3155493948968abfeb220d660ea1c2c8
kahsuann <kahsuan@hotmail.sg>
2024-08-22 23:14:34 +0800

    Naive Implementation of Handling Errors

--- src/main/java/Susan.java
+++ src/main/java/Susan.java
@@ -8,13 +8,16 @@ public class Susan {
 
         // Prepare User Input
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
+        String userInput = "";
 
         // Initialise Task Arraylist
         List<Task> tasks = new ArrayList<>();
 
         // Add, List
         while (!userInput.equalsIgnoreCase("bye")) {
+            // Read next input
+            userInput = scanner.nextLine();
+
             // Print the list
             if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("Babes. YOU NEED TO");
@@ -41,30 +44,41 @@ public class Susan {
                 // ToDo
                 if (userInput.startsWith("todo")) {
                     // Get task description
-                    tasks.add(new ToDo(userInput.replace("todo ", "").trim()));
+                    tasks.add(new ToDo(userInput.replace("todo", "").trim()));
 
                 }
                 // Deadline
-                if (userInput.startsWith("deadline")) {
+                else if (userInput.startsWith("deadline")) {
                     // Get task description and by
                     String[] taskDetails = userInput.split("/by");
-                    tasks.add(new Deadline(taskDetails[0].replace("deadline ", "").trim(),
+                    tasks.add(new Deadline(taskDetails[0].replace("deadline", "").trim(),
                             taskDetails[1].trim()));
                 }
                 // Event
-                if (userInput.startsWith("event")) {
+                else if (userInput.startsWith("event")) {
                     // Get task description, from and to
                     String[] taskDetails = userInput.split("/");
-                    tasks.add(new Event(taskDetails[0].replace("event ", "").trim(),
+                    tasks.add(new Event(taskDetails[0].replace("event", "").trim(),
                             taskDetails[1].replace("from ", "").trim(),
                             taskDetails[2].replace("to ", "").trim()));
                 }
-                // Update Completed
-                System.out.println("OK busy girl! added: " + tasks.get(tasks.size() - 1));
-                System.out.println("You have " + tasks.size() + " tasks in the list.");
+                // Unrecognised Command
+                else {
+                    System.out.println("You cry a lot and you are not productive! " +
+                            "I do not have this command :-(");
+                    continue;
+                }
+                // Test for Validity (Naive)
+                Task newTask = tasks.get(tasks.size() - 1);
+                if (newTask.isInvalid()) {
+                    System.out.println("You want to do nothing?");
+                    tasks.remove(tasks.size() - 1);
+                } else {
+                    // Update Completed
+                    System.out.println("OK busy girl! added: " + newTask);
+                    System.out.println("You have " + tasks.size() + " tasks in the list.");
+                }
             }
-            // Read next input
-            userInput = scanner.nextLine();
         }
         // Exit
         System.out.println("Good bye, slay the day!");
