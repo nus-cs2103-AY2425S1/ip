95f1fff1fa49d31fefc22a1c499234311b34f7d2
wrjgold <trexwrj@gmail.com>
2024-08-22 14:07:23 +0800

    Add echo function and exits when user says bye

--- src/main/java/Tick.java
+++ src/main/java/Tick.java
@@ -8,17 +8,32 @@ public class Tick {
         System.out.print(Tick.separator);
         System.out.printf("Hello! I'm %s\n", Tick.botName);
         System.out.println("What can I do for you?");
-        System.out.print(Tick.separator);
+        System.out.println(Tick.separator);
     }
 
     public void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
         System.out.print(Tick.separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Tick.separator);
+    }
+
+    public void echo(String command) {
+        System.out.print(Tick.separator);
+        System.out.println(command);
+        System.out.println(Tick.separator);
     }
 
     public static void main(String[] args) {
         Tick bot = new Tick();
         bot.greet();
+        while (true) {
+            Scanner scn = new Scanner(System.in);
+            String command = scn.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            bot.echo(command);
+        }
         bot.exit();
     }
 }
