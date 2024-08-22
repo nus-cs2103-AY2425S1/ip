a12ddf173f03a3f40da3f903b961199af92c8065
dasha3412 <dashasychova777@gmail.com>
2024-08-22 12:55:21 +0800

    Adding support for To Dos, Events, and Deadlines

---
+++ src/main/java/Event.java
@@ -0,0 +1,23 @@
+public class Event extends Task {
+    String endTime;
+    String startTime;
+
+    public Event(String name, String startTime, String endTime) {
+        super(name);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getDeadline() {
+        return this.endTime;
+    }
+
+    public String getStartTime() {
+        return this.startTime;
+    }
+
+    @Override
+    public String toString() {
+        return "{E}" + super.toString() + " (from: " + this.startTime + " to: " + this.endTime + ")";
+    }
+}
--- src/main/java/Victor.java
+++ src/main/java/Victor.java
@@ -61,9 +61,97 @@ public class Victor {
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("  ~  I don't think there's a task with that number!");
                 }
+            } else if (userInput.trim().toLowerCase().startsWith("todo")) {
+                String[] parsed = userInput.trim().split(" ");
+                String taskName = "";
+                for (int i = 1; i < parsed.length; i++) {
+                    taskName += " " + parsed[i];
+                }
+                taskName = taskName.trim();
+                if (taskName.isEmpty()) {
+                    System.out.println("  ~  Please give a name for the To Do.");
+                } else {
+                    ToDo task = new ToDo(taskName);
+                    inputs.add(task);
+                    System.out.println("  ~  Cool! I added this To Do:");
+                    System.out.println("  ~    " + task);
+                    System.out.println("  ~  You now have " + inputs.size() + ((inputs.size() == 1) ? " task" : " tasks") + " in your list.");
+                }
+            } else if (userInput.trim().toLowerCase().startsWith("deadline")) {
+                String[] parsed = userInput.trim().split(" ");
+                String taskName = "";
+                String deadline = "";
+
+                boolean deadlineFlag = false;
+
+                for (int i = 1; i < parsed.length; i++) {
+                    if (parsed[i].startsWith("/")) {
+                        deadlineFlag = true;
+                        continue;
+                    }
+                    if (!deadlineFlag) {
+                        taskName += " " + parsed[i];
+                    } else {
+                        deadline += " " + parsed[i];
+                    }
+                }
+                taskName = taskName.trim();
+                deadline = deadline.trim();
+                if (taskName.isEmpty()) {
+                    System.out.println("  ~  Please give a name for the Deadline.");
+                } else if (deadline.isEmpty()) {
+                    System.out.println("  ~  Please give a deadline for the Deadline.");
+                } else {
+                    Deadline task = new Deadline(taskName, deadline);
+                    inputs.add(task);
+                    System.out.println("  ~  Cool! I added this Deadline:");
+                    System.out.println("  ~    " + task);
+                    System.out.println("  ~  You now have " + inputs.size() + ((inputs.size() == 1) ? " task" : " tasks") + " in your list.");
+                }
+            } else if (userInput.trim().toLowerCase().startsWith("event")) {
+                String[] parsed = userInput.trim().split(" ");
+                String taskName = "";
+                String start = "";
+                String end = "";
+
+                boolean startFlag = false;
+                boolean endFlag = false;
+
+                for (int i = 1; i < parsed.length; i++) {
+                    if (parsed[i].startsWith("/")) {
+                        if (startFlag) {
+                            endFlag = true;
+                        } else {
+                            startFlag = true;
+                        }
+                        continue;
+                    }
+                    if (!startFlag) {
+                        taskName += " " + parsed[i];
+                    } else if (!endFlag) {
+                        start += " " + parsed[i];
+                    } else {
+                        end += "" + parsed[i];
+                    }
+                }
+                taskName = taskName.trim();
+                start = start.trim();
+                end = end.trim();
+                if (taskName.isEmpty()) {
+                    System.out.println("  ~  Please give a name for the Event.");
+                } else if (start.isEmpty()) {
+                    System.out.println("  ~  Please give a start time for the Event.");
+                } else if (end.isEmpty()) {
+                    System.out.println("  ~  Please give an end time for the Event.");
+                } else {
+                    Event task = new Event(taskName, start, end);
+                    inputs.add(task);
+                    System.out.println("  ~  Cool! I added this Event:");
+                    System.out.println("  ~    " + task);
+                    System.out.println("  ~  You now have " + inputs.size() + ((inputs.size() == 1) ? " task" : " tasks") + " in your list.");
+                }
             } else {
-                inputs.add(new Task(userInput));
-                System.out.println("  ~  added: " + userInput);
+                System.out.println("  ~  Please specify either a To Do, a Deadline or an Event!");
             }
             System.out.println("============================================================");
             userInput = inp.nextLine();
