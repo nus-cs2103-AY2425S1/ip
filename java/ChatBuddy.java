e5b46834161324a983a2384fb299d886c84f9d10
JellyPenguinnn <>
2024-08-22 23:05:46 +0800

    Level-2

--- java/ChatBuddy.java
+++ java/ChatBuddy.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 public class ChatBuddy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100]; // Array to store up to 100 tasks
+        int taskCount = 0; // To keep track of the number of tasks stored
+
         try {
             System.out.println("_____________________________________________");
             System.out.println("Hello! I'm ChatBuddy.\nLet me know how can I assist you today?");
+            System.out.println("_____________________________________________");
 
             String userInput = scanner.nextLine();
             while (!userInput.equals("bye")) {
-                System.out.println("_____________________________________________");
-                System.out.println(userInput);
+                if (userInput.equals("list")) {
+                    System.out.println("_____________________________________________");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("_____________________________________________");
+                } else {
+                    tasks[taskCount] = userInput;
+                    taskCount++;
+                    System.out.println("_____________________________________________");
+                    System.out.println("Added: " + userInput);
+                    System.out.println("_____________________________________________");
+                }
                 userInput = scanner.nextLine();
             }
         } finally {
