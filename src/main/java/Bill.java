14907cdbd42eb0d5e292e63ed695817bb5c472df
Wnayar <wdnconsultancy@yahoo.com>
2024-08-22 20:32:36 +0800

    Refactor code structure to incorporate OOP concepts

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -2,52 +2,89 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Bill {
-    public static void main(String[] args) {
+    private ArrayList<Task> userList;
+
+    public Bill() {
+        userList = new ArrayList<>();
+    }
+
+    private void introduce() {
         System.out.println("Hello! I'm Bill");
         System.out.println("What can I do for you?");
+    }
 
-        ArrayList<Task> userList = new ArrayList<>();
+    private void conclude() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private void showList() {
+        if (userList.isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < userList.size(); i++) {
+                System.out.println((i + 1) + "." + userList.get(i));
+            }
+        }
+    }
+
+    private void addTask(Task newTask){
+        userList.add(newTask);
+        System.out.println("added: " + newTask);
+    }
+
+    private void handleMarkOfTask(String[] parsedInput) {
+        // data validation
+        if (parsedInput.length < 2) {
+            System.out.println("Please provide a second argument when marking or unmarking a task");
+            return;
+        }
+        // ensure task number is within the range of the task list
+        if (Integer.parseInt(parsedInput[1]) > userList.size() || Integer.parseInt(parsedInput[1]) < 1) {
+            System.out.println("There is no task of that number in the current list");
+            return;
+        }
+
+        int targetTaskNumber = Integer.parseInt(parsedInput[1]) - 1;
+        Task targetTask = userList.get(targetTaskNumber);
+
+        if (parsedInput[0].equals("mark")) {
+            targetTask.mark();
+        } else {
+            // unmarked
+            targetTask.unmark();
+        }
+        System.out.println(targetTask);
+    }
+
+    public void start() {
+        introduce();
         Scanner userScanner = new Scanner(System.in);
         String userCommand = userScanner.nextLine();
 
         while (!userCommand.equals("bye")) {
-            if (userCommand.equals("list")) {
-                if (userList.isEmpty()) {
-                    System.out.println("List is empty");
-                } else {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < userList.size(); i++) {
-                        System.out.println((i + 1) + "." + "[" + userList.get(i).getStatusIcon() + "] " + userList.get(i).getDescription());
-                    }
-                }
-            } else {
-                String[] parsedInput = userCommand.split(" ");
-                if (parsedInput[0].equals("mark")) {
-                    if (Integer.parseInt(parsedInput[1]) > userList.size() || Integer.parseInt(parsedInput[1]) < 1) {
-                        System.out.println("There is no task of that number in the current list");
-                    } else {
-                        System.out.println("Nice! I've marked this task as done:");
-                        Task targetTask = userList.get(Integer.parseInt(parsedInput[1]) - 1);
-                        targetTask.mark();
-                        System.out.println("[" + targetTask.getStatusIcon() + "] " + targetTask.getDescription());
-                    }
-                } else if (parsedInput[0].equals("unmark")) {
-                    if (Integer.parseInt(parsedInput[1]) > userList.size() || Integer.parseInt(parsedInput[1]) < 1) {
-                        System.out.println("There is no task of that number in the current list");
-                    } else {
-                        System.out.println("OK, I've marked this task as not done yet:");
-                        Task targetTask = userList.get(Integer.parseInt(parsedInput[1]) - 1);
-                        targetTask.unmark();
-                        System.out.println("[" + targetTask.getStatusIcon() + "] " + targetTask.getDescription());
-                    }
-                } else {
-                    userList.add(new Task(userCommand));
-                    System.out.println("added: " + userCommand);
-                }
+
+            String[] parsedInput = userCommand.split(" ");
+            String route = parsedInput[0];
+
+            switch (route) {
+                case "list":
+                    showList();
+                    break;
+                case "mark":
+                case "unmark":
+                    handleMarkOfTask(parsedInput);
+                    break;
+                default:
+                    addTask(new Task(userCommand));
+                    break;
             }
+
             userCommand = userScanner.nextLine();
         }
-
-        System.out.println("Bye. Hope to see you again soon!");
+        conclude();
+    }
+    public static void main(String[] args) {
+        new Bill().start();
     }
 }
