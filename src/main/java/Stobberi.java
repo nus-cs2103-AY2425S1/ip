1c89e3555b82f8197e9b5de4e4cdc15a13ec30ac
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-22 13:55:12 +0800

    Added greetings

---
+++ src/main/java/Stobberi.java
@@ -0,0 +1,12 @@
+public class Stobberi {
+    private static String nameOfChatBot = "Stobberi";
+    public static void main(String[] args) {
+        String greeting = "___________________________________________\n"
+                + "Hello! I'm " + nameOfChatBot + ".\n"
+                + "What can I do for you?\n"
+                + "___________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "___________________________________________\n";
+        System.out.println(greeting);
+    }
+}
