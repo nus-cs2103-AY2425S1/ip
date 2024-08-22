6892093695c36ee89f09dc02282135837c65410f
arnav12344 <64593433+arnav12344@users.noreply.github.com>
2024-08-22 17:53:17 +0800

    Added echo feature

--- src/main/java/Kayo.java
+++ src/main/java/Kayo.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Kayo {
+
     public static void main(String[] args) {
-        
+        Greet();
+        Scanner input = new Scanner(System.in);
+        String inputString = "";
+        while(!inputString.equals("Bye")) {
+            inputString = input.nextLine();
+            if(inputString.equals("Bye")) {
+                break;
+            } else {
+                System.out.println(inputString);
+            }
+        }
+        Exit();
+    }
+    public static void Greet() {
         System.out.println("Hello! I'm Kayo! ");
         System.out.println("What can I do for you?");
+    }
+    public static void Exit(){
         System.out.println("Bye. I hope to see you again soon!");
     }
 }
