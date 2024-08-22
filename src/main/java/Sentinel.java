23801536bba10f742cefb872f96d454c270ceb7f
Ricco Lim <limricco2002@gmail.com>
2024-08-22 23:14:34 +0800

    Change implementation using ArrayList

--- src/main/java/Sentinel.java
+++ src/main/java/Sentinel.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Sentinel {
 
-    public static int index = 0;
-    public static Task[] items = new Task[100];
+    public static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo
@@ -34,39 +34,39 @@ public class Sentinel {
             switch (input) {
                 case "mark" -> {
                     int num = sc.nextInt();
-                    if (num > index) {System.out.println("No such item in the list!"); break;}
-                    else if (items[num-1].isDone()) {System.out.println(items[num-1] + " has already been marked as done."); break;}
-                    items[num - 1].setDone();
+                    if (num > list.size()) {System.out.println("No such item in the list!"); break;}
+                    else if (list.get(num-1).isDone()) {System.out.println(list.get(num-1) + " has already been marked as done."); break;}
+                    list.get(num - 1).setDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("\t" + items[num - 1].getStatusIcon() + " " + items[num - 1]);
+                    System.out.println("\t" + list.get(num - 1).getStatusIcon() + " " + list.get(num - 1));
                 }
                 case "unmark" -> {
                     int num = sc.nextInt();
-                    if (num > index) {System.out.println("No such item in the list!"); break;}
-                    else if (!items[num-1].isDone()) {System.out.println(items[num-1] + " has already been marked as undone."); break;}
-                    items[num - 1].setUndone();
+                    if (num > list.size()) {System.out.println("No such item in the list!"); break;}
+                    else if (!list.get(num-1).isDone()) {System.out.println(list.get(num-1) + " has already been marked as undone."); break;}
+                    list.get(num - 1).setUndone();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("\t" + items[num - 1].getStatusIcon() + items[num - 1]);
+                    System.out.println("\t" + list.get(num - 1).getStatusIcon() + list.get(num - 1));
                 }
                 case "list" -> {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < index; i++) {
-                        System.out.println("\t" + (i + 1) + "." + items[i].classFirstChar() + items[i].getStatusIcon() + " " + items[i].listedString());
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println("\t" + (i + 1) + "." + list.get(i).classFirstChar() + list.get(i).getStatusIcon() + " " + list.get(i).listedString());
                     }
                 }
                 case "todo", "deadline", "event" -> {
                     String input2 = sc.nextLine().trim();
                     if (input2.isEmpty()) {System.out.println(input.substring(0, 1).toUpperCase() + input.substring(1) + " name cannot be empty"); continue;}
                     switch (input){
-                        case "todo" -> items[index] = new ToDo(input2);
+                        case "todo" -> list.add(new ToDo(input2));
 
                         case "deadline", "event" -> {
                             String[] stringArr = input2.split(" ");
                             String taskName = "", from = "", to = "";
                             boolean task = true, fr = false;
                             for (String word: stringArr){
-                                if (word.toLowerCase().contains("/by") || word.toLowerCase().contains("/to")) {task = false; fr = false;}
-                                else if (word.toLowerCase().contains("/from")) {task = false; fr = true;}
+                                if (word.equalsIgnoreCase("/by") || word.equalsIgnoreCase("/to")) {task = false; fr = false;}
+                                else if (word.equalsIgnoreCase("/from")) {task = false; fr = true;}
 
                                 else if (task && !fr) taskName = taskName.concat(word + " ");
                                 else if (!task && fr) from = from.concat(word + " ");
@@ -79,20 +79,20 @@ public class Sentinel {
                                         System.out.println("Please state the deadline using /by (eg: deadline return book /by Sunday)");
                                         continue;
                                     }
-                                    items[index] = new Deadline(taskName, to);
+                                    list.add(new Deadline(taskName, to));
                                 }
                                 case "event" -> {
                                     if (from.isEmpty() || to.isEmpty()) {
                                         System.out.println("Please state the start and end date using /from and /to respectively (eg: event project meeting /from Mon 2pm /to 4pm)");
                                         continue;
                                     }
-                                    items[index] = new Event(taskName, from, to);
+                                    list.add(new Event(taskName, from, to));
                                 }
                             }
                         }
                     }
-                    System.out.println("Got it. I've added this task: " + items[index]);
-                    System.out.println("\t" + items[index].classFirstChar() + items[index].getStatusIcon() + " " + items[index++].listedString());
+                    System.out.println("Got it. I've added this task: " + list.get(list.size()-1));
+                    System.out.println("\t" + list.get(list.size()-1).classFirstChar() + list.get(list.size()-1).getStatusIcon() + " " + list.get(list.size()-1).listedString());
                 }
                 case "/help" ->{
                     String helpText = """
