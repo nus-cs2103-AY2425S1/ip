b9c8037653f7cd2119977bfbd8773bc1447c45f1
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-22 17:13:17 +0800

    Implemented simple text greeting

--- src/main/java/Karen.java
+++ src/main/java/Karen.java
@@ -1,10 +1,14 @@
 public class Karen {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "_______________________\n";
+        String output = line +
+                        "Hi! I'm Karen\n" +
+                        "What can I do for you?\n" +
+                        line +
+                        "Bye. Hope to see you again soon!\n" +
+                        line;
+
+        System.out.print(output);
+
     }
 }
