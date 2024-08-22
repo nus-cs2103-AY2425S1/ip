60f96c865df5011d154769cc541ac84dcdf2f80b
muhdbhz <e1122341@u.nus.edu>
2024-08-22 16:24:18 +0800

    Add the ability to echo user's input

--- src/main/java/Moody.java
+++ src/main/java/Moody.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Moody {
     public static void main(String[] args) {
         String spacer = "____________________________________________________________\n";
-        System.out.println(spacer +
-                "Hello! I'm Moody!\nWhat can I do for you?\n"
-                + spacer
-                + "Bye. Hope to see you again soon!\n"
+        System.out.println(spacer
+                + "Hello! I'm Moody!\nWhat can I do for you?\n"
                 + spacer);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(spacer
+                        + "Bye. Hope to see you again soon!\n"
+                        + spacer);
+                break;
+            } else {
+                System.out.println(spacer + userInput + "\n" + spacer);
+            }
+        }
     }
 }
