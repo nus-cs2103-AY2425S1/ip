78a83e8a830058cbbc43a1a26b50cfca5dc77500
Xueting <xueting1966@gmail.com>
2024-08-22 18:48:20 +0800

    Add Scanner to read user Input

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Snowy {
     public static void main(String[] args) {
         String line ="____________________________________________________________\n";
-        System.out.println(line + " Hello I'm Snowy\n What can I do for you?\n" + line
-                + " Bye. Hope to see you again soon!\n" + line);
+        System.out.println(line + " Hello I'm Snowy\n What can I do for you?\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(line + "You said: " + input );
+
     }
 }
