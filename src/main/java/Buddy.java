beeb5c437176cc79711852a5080435c5a054ea36
Vinoth <vinoth2002k@gmail.com>
2024-08-22 09:55:07 +0800

    Level-2

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -4,6 +4,8 @@ public class Buddy {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int tracker = 0;
         System.out.println("    ____________________________________");
         System.out.println("    Hey there! I'm Buddy\n    What do ya need help with?");
         System.out.println("    ____________________________________\n");
@@ -16,9 +18,22 @@ public class Buddy {
                 System.out.println("    Bye! See ya soon!");
                 System.out.println("    ____________________________________");
                 break;
-            } else {
+            } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("\n    ____________________________________");
-                System.out.println("    " + userInput);
+                if (tracker == 0) {
+                    System.out.println("    List is empty!!");
+                } else {
+                    for (int i = 0; i < tracker; i++) {
+                        System.out.printf("    %d. %s%n", i+1, list[i]);
+                    }
+                }
+                System.out.println("    ____________________________________\n");
+
+            } else {
+                list[tracker] = userInput;
+                tracker++;
+                System.out.println("\n    ____________________________________");
+                System.out.println("    added: " + userInput);
                 System.out.println("    ____________________________________\n");
             }
         }
