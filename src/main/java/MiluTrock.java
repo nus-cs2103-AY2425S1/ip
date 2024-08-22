5a0fd8acc009959c134e1a17e58aed5e024e5197
MiloTruck <milotruckctf@gmail.com>
2024-08-22 18:07:31 +0800

    Add echo user input

--- src/main/java/MiluTrock.java
+++ src/main/java/MiluTrock.java
@@ -1,7 +1,18 @@
 public class MiluTrock {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm MiluTrock!");
+        String name = "MiluTrock";
+        System.out.println("Hello! I'm " + name + "!");
         System.out.println("What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+        
+        // Echo user input and exit if "bye" is entered
+        while (true) {
+            String input = System.console().readLine();
+            if (input.toLowerCase().equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
