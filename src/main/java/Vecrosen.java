d166b6a1e4c234592dccf2242c8d71b5d92ce2eb
1st2GetThisName <adrieledix@gmail.com>
2024-08-23 00:45:51 +0800

    Add special input: "unmark X" and fix numbering bug
    Tasks, once marked as complete, could not be unmarked.
    The numbering started at 46 instead of having a period
    added after the number.
    
    Let's add the command unmark and fix the aforementioned bug.

--- src/main/java/Vecrosen.java
+++ src/main/java/Vecrosen.java
@@ -26,9 +26,16 @@ public class Vecrosen {
                     list.get(itemNo-1).setDone(true);
                     speak("Task marked as complete: " + list.get(itemNo-1).getDescription());
                 }
+            } else if (input.matches("unmark \\d+")) {
+                int itemNo = Integer.parseInt(input.substring(7));
+                if (itemNo < 1 || itemNo > list.size()) speak("Invalid task number!");
+                else {
+                    list.get(itemNo-1).setDone(false);
+                    speak("Task marked as incomplete: " + list.get(itemNo-1).getDescription());
+                }
             } else if (input.equals("list")) {
                 for (int i = 0; i < list.size(); ++i) {
-                    speak((i+1) + '.' + list.get(i).toString());
+                    speak((i+1) + "." + list.get(i).toString());
                 }
             } else {
                 list.add(new Task(input));
