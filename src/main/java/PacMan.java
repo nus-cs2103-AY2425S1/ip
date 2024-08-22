d0c5519045a43e174dd702827bcc77aa1e1d3e54
hpcman <happypacman36@gmail.com>
2024-08-23 03:36:44 +0800

    Add echoing function

--- src/main/java/PacMan.java
+++ src/main/java/PacMan.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PacMan {
     public static void greet() {
         System.out.println("Hello! I'm PacMan");
@@ -8,8 +10,21 @@ public class PacMan {
         System.out.println("Good bye. Hope to see you soon!");
     }
 
+    public static void echo(String text) {
+        System.out.println(text);
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         greet();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                echo(input);
+            }
+        }
         exit();
     }
 }
