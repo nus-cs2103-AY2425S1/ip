a86eb2174759d547d363a7465fb4ce5a1bef732f
WuJinhan1 <e1075517@u.nus.edu>
2024-08-22 23:24:19 +0800

    finished level 4

--- src/main/Jard.java/Jard.java
+++ src/main/Jard.java/Jard.java
@@ -31,6 +31,48 @@ public class Jard {
         }
     }
 
+    public static class Todo extends Task {
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    public static class Deadline extends Task {
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    public static class Event extends Task {
+        protected String from;
+        protected String to;
+
+        public Event(String description, String from, String to) {
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        }
+    }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -85,16 +127,54 @@ public class Jard {
                             System.out.println("   " + task);
                             System.out.println("____________________________________________________________");
                         } else {
+                            System.out.println("____________________________________________________________");
                             System.out.println(" Task number does not exist.");
+                            System.out.println("____________________________________________________________");
                         }
                     } catch (NumberFormatException e) {
+                        System.out.println("____________________________________________________________");
                         System.out.println(" Invalid task number format.");
+                        System.out.println("____________________________________________________________");
                     }
                 }
-            } else {
-                tasks.add(new Task(input));
+            } else if (command.equals("todo") && inputParts.length > 1) {
+                tasks.add(new Todo(inputParts[1]));
                 System.out.println("____________________________________________________________");
-                System.out.println(" added: " + input);
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks.get(tasks.size() - 1));
+                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            } else if (command.equals("deadline") && inputParts.length > 1) {
+                String[] details = inputParts[1].split("/by", 2);
+                if (details.length > 1) {
+                    tasks.add(new Deadline(details[0].trim(), details[1].trim()));
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + tasks.get(tasks.size() - 1));
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Invalid format. Please specify the deadline with /by.");
+                    System.out.println("____________________________________________________________");
+                }
+            } else if (command.equals("event") && inputParts.length > 1) {
+                String[] details = inputParts[1].split("/from|/to");
+                if (details.length == 3) {
+                    tasks.add(new Event(details[0].trim(), details[1].trim(), details[2].trim()));
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + tasks.get(tasks.size() - 1));
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Invalid format. Please specify the event with /from and /to.");
+                    System.out.println("____________________________________________________________");
+                }
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Invalid command.");
                 System.out.println("____________________________________________________________");
             }
         }
