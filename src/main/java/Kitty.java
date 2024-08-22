71053a57b18297c88992eaad19fae26ceea37214
Kitty-001 <jqm002@hotmail.com>
2024-08-22 23:20:18 +0800

    Level-3

--- src/main/java/Kitty.java
+++ src/main/java/Kitty.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Kitty {
     private static final String name = "Kitty";
-    private static final ArrayList<String> list = new ArrayList<String>(100);
+    private static final ArrayList<Task> list = new ArrayList<Task>(100);
     private static final String divisionLine = "--------------------------";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -24,8 +26,8 @@ public class Kitty {
     }
 
     public static void Echo() {
-        Scanner sc = new Scanner(System.in);
         String command = "";
+        Scanner sc = new Scanner(System.in);
         while (true) {
             command = sc.nextLine();
             if (command.contains("bye")) {
@@ -33,14 +35,18 @@ public class Kitty {
                 return;
             } else if (command.contains("list")) {
                 List();
-            } else {
+            } else if (command.contains("unmark")) {
+                unmark(extractFirstNumber(command));
+            } else if (command.contains("mark")) {
+                mark(extractFirstNumber(command));
+            } else
                 add(command);
-            }
         }
     }
 
     public static void add(String item) {
-        list.add(item);
+        Task tmp = new Task(item);
+        list.add(tmp);
         System.out.println(divisionLine);
         System.out.println("added: " + item + "\n");
         System.out.println(divisionLine);
@@ -48,17 +54,55 @@ public class Kitty {
 
     public static void List() {
         int count = 1;
-        String[] tmp = new String[0];
+        Task[] tmp = new Task[0];
         System.out.println(divisionLine);
-        for (String item: list.toArray(tmp)) {
-            System.out.println(count++ + ". " + item);
+        System.out.println("Meow~ Here you are!");
+        for (Task item: list.toArray(tmp)) {
+            System.out.println(count++ + "." + item);
         }
         System.out.println("\n" + divisionLine);
     }
 
+    public static void mark(int index) {
+        Task tmp = list.get(index - 1);
+        tmp.mark();
+        System.out.println("Well done! You have completed this task!");
+        System.out.println("  " + tmp);
+        System.out.println("\n" + divisionLine);
+    }
+
+    public static void unmark(int index) {
+        Task tmp = list.get(index - 1);
+        tmp.unmark();
+        System.out.println("Meow~ Okay we can continue this task!");
+        System.out.println("  " + tmp);
+        System.out.println("\n" + divisionLine);
+    }
+
+    public static int extractFirstNumber(String input) {
+        // Replace all non-digit characters with spaces
+        String cleanedInput = input.replaceAll("\\D+", " ");
+
+        // Split the cleaned string by spaces
+        String[] parts = cleanedInput.trim().split("\\s+");
+
+        // Check if there are any parts and parse the first one
+        if (parts.length > 0) {
+            try {
+                return Integer.parseInt(parts[0]);
+            } catch (NumberFormatException e) {
+                // Handle the case where parsing fails
+                return -1;
+            }
+        }
+
+        // Return null if no number is found
+        return -1;
+    }
+
     public static void Exit() {
         System.out.println(divisionLine);
-        System.out.println("Bye. Hope I can see you again soon!\n");
+        System.out.println("Bye. Hope I can see you again soon!\nNext time bring me some cat food please!!!\n");
         System.out.println(divisionLine);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    private String name;
+    private boolean isDone = false;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+        if (isDone)
+            str += "X";
+        else
+            str += " ";
+        str += "] " + name;
+        return str;
+    }
+}
