fe3f1b49d71c5387eccc40ac4325c8f15456a12c
Bryan <bryanchenyu616@gmail.com>
2024-08-22 14:58:47 +0800

    add bot echoing and exit functionality

--- src/main/java/Crack.java
+++ src/main/java/Crack.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Crack {
     public static void main(String[] args) {
         String greeting = "____________________________________________________________\n"
-        + " Hello! I'm Crack\n"
-        + " What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + " Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________";
+                + " Hello! I'm Crack\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
 
         System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.print("You: ");
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Crack says " + input);
+            }
+        }
+
+        scanner.close();
     }
 }
