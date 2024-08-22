c7922099bcdea3bc1438ca10a45396590a95bea3
niclammm <E1122517@u.nus.edu>
2024-08-22 22:52:55 +0800

    Added Scanning function and changed system outputs

--- src/main/java/Repsmax.java
+++ src/main/java/Repsmax.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Repsmax {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                " Hello! I'm Repsmax\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+        //initialise scanner
+        Scanner scanner = new Scanner(System.in);
+        //greet the user
+        String greeting = "  ____________________________________________________________\n" +
+                "   Hello! I'm Repsmax\n" +
+                "   What can I do for you?\n" +
+                "  ____________________________________________________________\n";
+        String goodbye = "  ____________________________________________________________\n" +
+                "   Bye. Hope to see you again soon!\n" +
+                "  ____________________________________________________________\n";
+        System.out.println(greeting);
+        while(true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(goodbye);
+                break;
+            }
+
+            System.out.println("  ____________________________________________________________\n" +
+                    "   " +
+                    userInput +
+                    "\n" +
+                    "  ____________________________________________________________\n");
+        }
     }
 }
