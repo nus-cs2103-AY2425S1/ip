e2c9ce39a9936742d0452a599015fcdb07548199
Jonathan Loh <jonathanloh02@gmail.com>
2024-08-22 17:11:56 +0800

    add echo function

--- src/main/java/Vuewee.java
+++ src/main/java/Vuewee.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Vuewee {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
     System.out.println("____________________________________________________________");
     System.out.println("Hello! I'm Vuewee\nWhat can I do for you?");
     System.out.println("____________________________________________________________");
+
+    // Echo input from user until user types "bye"
+    while (true) {
+      String input = scanner.nextLine();
+      if (input.equals("bye")) {
+        break;
+      }
+      System.out.println("____________________________________________________________");
+      System.out.println(input);
+      System.out.println("____________________________________________________________");
+    }
+
+    System.out.println("____________________________________________________________");
     System.out.println("Bye. Hope to see you again soon!");
     System.out.println("____________________________________________________________");
+    scanner.close();
   }
 }
