522cec31cc1c854c8531f4f4f56f55e158f26ac4
yuancheng <>
2024-08-22 12:00:54 +0800

    created new class Winner.java with greet and exit

---
+++ src/main/java/Winner.java
@@ -0,0 +1,11 @@
+public class Winner {
+
+    public String greetUser() {
+        return "Hi! I am Winner. What can I do for you today?";
+    }
+
+    public String exit() {
+        return "Hope I have been of service. Goodbye and see you again!";
+    }
+
+}
