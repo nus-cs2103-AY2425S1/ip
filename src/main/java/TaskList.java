272cb74c4c7bebfc6860b35334de9fb1dfda976b
SamuelFoo <fooenzesamuel@gmail.com>
2024-08-22 15:44:10 +0800

    Add a `Task` class to represent tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,16 +1,19 @@
 public class TaskList {
-    private static final String[] LIST = new String[100];
+    private static final Task[] LIST = new Task[100];
     private static int numItems = 0;
 
     public void addItem(String item) {
-        LIST[numItems] = item;
+        LIST[numItems] = new Task(item);
         numItems++;
     }
 
     public String toString() {
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < numItems; i++) {
-            output.append(String.valueOf(i + 1)).append(". ").append(LIST[i]).append("\n");
+            output.append(String.valueOf(i + 1))
+                    .append(". ")
+                    .append(LIST[i].toString())
+                    .append("\n");
         }
         return output.toString();
     }
