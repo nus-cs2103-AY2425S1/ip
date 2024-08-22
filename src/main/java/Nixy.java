9fa4fe1a2d049f8b5861b0b350953994e307e884
Jabez Tho <jabeztho@gmail.com>
2024-08-23 02:34:23 +0800

    add greeting

--- src/main/java/Nixy.java
+++ src/main/java/Nixy.java
@@ -1,10 +1,15 @@
 public class Nixy {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatbotName = "Nixy";
+        printHorizontalLine();
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
     }
 }
