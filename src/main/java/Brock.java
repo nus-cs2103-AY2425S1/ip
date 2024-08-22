ca28277f73ccfdf212313ca6b24a1d88bb0603f4
YangQF2002 <yangqf18@gmail.com>
2024-08-22 17:18:11 +0800

    Add rename, greet and exit feature

---
+++ src/main/java/Brock.java
@@ -0,0 +1,9 @@
+public class Brock {
+    public static void main(String[] args) {
+        String line1 = "Hello! I'm Brock";
+        String line2 = "What can I do for you?";
+        String line3 = "Bye. Hope to see you again soon!";
+        System.out.println(line1 + '\n' + line2 + '\n' + line3 + '\n');
+    }
+}
+
