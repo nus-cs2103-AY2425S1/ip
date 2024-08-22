f12bcdc00839c393426195417dbfd41774686a1e
natlv <nathanlooenping@gmail.com>
2024-08-22 10:33:16 +0800

    Echoes commands entered by user

--- src/main/java/Natsbot.java
+++ src/main/java/Natsbot.java
@@ -1,7 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Natsbot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Natsbot\n" + "What can I do for you?\n");
-        System.out.println("Goodbye. Hope to see you again soon!");
 
+        Scanner reader = new Scanner(System.in);
+
+        while(true) {
+            String input = reader.next();
+
+            if (Objects.equals(input, "bye")) {
+                System.out.println("Goodbye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        reader.close();
     }
 }
