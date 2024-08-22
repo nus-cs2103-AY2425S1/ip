0929d2fe3e45b4f183adee36db28330d251ca749
luileng <chengluileng@gmail.com>
2024-08-23 06:36:47 +0800

    User command echoed

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Duck {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
         String welcomeMessage = "Hello! I'm Duck\nWhat can I do for you?";
         System.out.println(line + '\n' + welcomeMessage + '\n' + line);
+
+        while (true) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equals("bye")) {
+                break;
+            }
+            System.out.println(line + '\n' + userCommand + '\n' + line);
+        }
+
         String goodbyeMessage = "Bye. Hope to see you again soon!";
-        System.out.println(goodbyeMessage + '\n' + line);
+        System.out.println(line + '\n' + goodbyeMessage + '\n' + line);
     }
 }
