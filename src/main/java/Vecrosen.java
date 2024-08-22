0b795bca317d2964f5704144a66ae0b295ea5570
1st2GetThisName <adrieledix@gmail.com>
2024-08-22 18:28:16 +0800

    Add indentation and echoing
    Vecrosen does not do anything other than greet the user.
    Vecrosen's text is difficult to differentiate from the user's.
    
    Let's make the chatbot parrot the user and add 4 spaces before
    anything Vecrosen's says.
    
    Also, apparently the class name within the Vecrosen.java only got
    refactored now? Visual Studio's being weird.

--- src/main/java/Vecrosen.java
+++ src/main/java/Vecrosen.java
@@ -1,6 +1,21 @@
-public class Duke {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Vecrosen {
+
+    private static void speak(String s) {
+        System.out.print("    ");
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, I'm Vecrosen\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        speak("Hello, I'm Vecrosen\nWhat can I do for you?");
+        while (true) {
+            String input;
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            speak(input);
+        }
+        //speak("Bye. Hope to see you again soon!");
     }
 }
