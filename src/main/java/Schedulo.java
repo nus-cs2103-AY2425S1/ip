c878d39a50be5de10806efca25f560e67f4ddea4
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-08-22 09:01:10 +0800

    chatbot named - level 0 done

--- src/main/java/Schedulo.java
+++ src/main/java/Schedulo.java
@@ -1,8 +1,9 @@
 public class Schedulo {
     public static void main(String[] args) {
+        String horizontalLine = "---------------";
+        System.out.println(horizontalLine);
         System.out.println("Hello I am Schedulo!");
         System.out.println("What can I do for you?");
-        String horizontalLine = "---------------";
         System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
