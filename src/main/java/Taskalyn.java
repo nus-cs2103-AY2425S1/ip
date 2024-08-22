8c6edbffba35f1e744d76d8c8b8f2cc1c6fc875f
psh12320 <pshricharan2020@gmail.com>
2024-08-23 00:08:18 +0800

    functionality for Level-2 finished

--- src/main/java/Taskalyn.java
+++ src/main/java/Taskalyn.java
@@ -2,11 +2,31 @@ import java.util.Scanner;
 
 public class Taskalyn {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hey! I'm Taskalyn, your personal Task Manager :) \n" +
-                           "What can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        // Initialising scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Greeting user
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hey! I'm Taskalyn, your personal Task Manager :)\n" +
+                "    What can I do for you?\n");
+        System.out.println("    ____________________________________________________________\n");
+
+        // Conditions
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________\n");
+                break;
+            }
+            else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + input + "\n");
+                System.out.println("    ____________________________________________________________\n");
+            }
+        }
+
     }
 }
