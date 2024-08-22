b9f17a2168bc724ca1a018cc1b2105b75d606d8e
myrilla12 <myrilla12@gmail.com>
2024-08-22 12:56:37 +0800

    Make chatbot echo
    When user inputs a string, the chatbot should print the same string.

--- src/main/java/Eevee.java
+++ src/main/java/Eevee.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Eevee {
     public static void main(String[] args) {
-        String divider = "_________________________________________________________\n";
+        String divider = "____________________________________________________________\n";
         String greeting = "Hello! I'm Eevee\nWhat can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
 
-        System.out.println(divider + greeting + divider + exit + divider);
+        System.out.println(divider + greeting + divider);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(divider);
+            System.out.println(input);
+            System.out.println(divider);
+        }
     }
 }
