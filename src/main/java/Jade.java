5ecef16519235d9c36ff8b9e7028e92e6e351fb1
wubojin <wubojin64@gmail.com>
2024-08-22 23:01:54 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Jade.java
@@ -0,0 +1,12 @@
+public class Jade {
+    public static void main(String[] args) {
+        String message = "____________________________________________________________\n"
+                + " Hello! I'm Jade!\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(message);
+    }
+}
