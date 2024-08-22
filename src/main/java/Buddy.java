dd0e3d3e669d3eec9501fb2cedce97e174e7750d
Vinoth <vinoth2002k@gmail.com>
2024-08-22 13:26:05 +0800

    Level-6

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,11 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Buddy {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] list = new Task[100];
-        int tracker = 0;
+        ArrayList<Task> list = new ArrayList<>();
         System.out.println("    ---------------------------------------------------------");
         System.out.println("    Hey there! I'm Buddy\n    What do ya need help with?");
         System.out.println("    ---------------------------------------------------------\n");
@@ -22,33 +22,41 @@ public class Buddy {
                     break;
 
                 } else if (userInput.equalsIgnoreCase("list")) {
-                    if (tracker == 0) {
+                    if (list.isEmpty()) {
                         System.out.println("    List is empty!!");
                     } else {
                         System.out.println("    Here are the tasks in your list: ");
-                        for (int i = 0; i < tracker; i++) {
-                            System.out.printf("    %d. [%s][%s] %s%n", i + 1, list[i].getTaskType(), list[i].getStatusIcon(), list[i].description);
+                        for (int i = 0; i < list.size(); i++) {
+                            System.out.printf("    %d. [%s][%s] %s%n", i + 1, list.get(i).getTaskType(), list.get(i).getStatusIcon(), list.get(i).description);
                         }
                     }
 
-                } else if (userInput.startsWith("mark ") && userInput.matches("mark \\d+$")) {
-                    int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                } else if (userInput.startsWith("mark") && userInput.matches("mark \\d+$")) {
+                    int taskIndex = Integer.parseInt(userInput.substring(4).trim()) - 1;
 
-                    if (taskIndex >= 0 && taskIndex < tracker) {
-                        System.out.println("    Nice one buddy! Marked this as done...");
-                        list[taskIndex].markAsDone();
-                        System.out.printf("    [%s][%s] %s%n", list[taskIndex].getTaskType(), list[taskIndex].getStatusIcon(), list[taskIndex].description);
+                    if (taskIndex >= 0 && taskIndex < list.size()) {
+                        if (list.get(taskIndex).isDone) {
+                            System.out.println("    Uhh, its already been marked buddy!");
+                        } else {
+                            list.get(taskIndex).markAsDone();
+                            System.out.println("    Nice one buddy! Marked this as done...");
+                            System.out.printf("    [%s][%s] %s%n", list.get(taskIndex).getTaskType(), list.get(taskIndex).getStatusIcon(), list.get(taskIndex).description);
+                        }
                     } else {
                         throw new BuddyException("I don't think task is on your list buddy...");
                     }
 
-                } else if (userInput.startsWith("unmark ") && userInput.matches("unmark \\d+$")) {
-                    int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                } else if (userInput.startsWith("unmark") && userInput.matches("unmark \\d+$")) {
+                    int taskIndex = Integer.parseInt(userInput.substring(6).trim()) - 1;
 
-                    if (taskIndex >= 0 && taskIndex < tracker) {
-                        System.out.println("    Alright buddy, let's give that task another shot!");
-                        list[taskIndex].markAsUndone();
-                        System.out.printf("    [%s][%s] %s%n", list[taskIndex].getTaskType(), list[taskIndex].getStatusIcon(), list[taskIndex].description);
+                    if (taskIndex >= 0 && taskIndex < list.size()) {
+                        if (!list.get(taskIndex).isDone) {
+                            System.out.println("    Uhh, its already been unmarked buddy!");
+                        } else {
+                            System.out.println("    Alright buddy, let's give that task another shot!");
+                            list.get(taskIndex).markAsUndone();
+                            System.out.printf("    [%s][%s] %s%n", list.get(taskIndex).getTaskType(), list.get(taskIndex).getStatusIcon(), list.get(taskIndex).description);
+                        }
                     } else {
                         throw new BuddyException("I don't think task is on your list buddy...");
                     }
@@ -62,12 +70,11 @@ public class Buddy {
                         throw new BuddyException("The description of a todo cannot be empty.");
                     }
                     Task t = new ToDos(taskDesc);
-                    list[tracker] = t;
-                    tracker++;
+                    list.add(t);
 
                     System.out.println("    Gotcha! I've added this task: ");
                     System.out.printf("         [%s][%s] %s%n", t.getTaskType(), t.getStatusIcon(), t.description);
-                    System.out.printf("    Now, you have %d tasks in the list!", tracker);
+                    System.out.printf("    Now, you have %d tasks in the list!", list.size());
 
                 } else if (userInput.startsWith("deadline")) {
                     String taskDesc = userInput.substring(8).trim();
@@ -80,12 +87,11 @@ public class Buddy {
                         String desc = parts[0].trim();
                         String day = parts[1].trim();
                         Task t = new Deadlines(desc, day);
-                        list[tracker] = t;
-                        tracker++;
+                        list.add(t);
 
                         System.out.println("    Gotcha! I've added this task: ");
                         System.out.printf("         [%s][%s] %s%n", t.getTaskType(), t.getStatusIcon(), t.description);
-                        System.out.printf("    Now, you have %d tasks in the list!", tracker);
+                        System.out.printf("    Now, you have %d tasks in the list!", list.size());
                     } else {
                         throw new BuddyException("When do ya need to get it done by?\n    (include '/by' followed by the deadline)");
                     }
@@ -108,17 +114,28 @@ public class Buddy {
                             String endTime = dateTimeAndEndParts[1].trim();
 
                             Task t = new Events(task, dateTime, endTime);
-                            list[tracker] = t;
-                            tracker++;
+                            list.add(t);
 
                             System.out.println("    Gotcha! I've added this task: ");
                             System.out.printf("     [%s][%s] %s%n", t.getTaskType(), t.getStatusIcon(), t.description);
-                            System.out.printf("    Now, you have %d tasks in the list!", tracker);
+                            System.out.printf("    Now, you have %d tasks in the list!", list.size());
                         } else {
                             throw new BuddyException("There's no end date?\n    (include '/to' after start date)");
                         }
                     } else {
                         throw new BuddyException("There's no start date?\n    (include '/from' after your description)");
+                    }
+
+                } else if (userInput.startsWith("delete") && userInput.matches("delete \\d+$")) {
+                    int taskIndex = Integer.parseInt(userInput.substring(6)) - 1;
+
+                    if (taskIndex >= 0 && taskIndex < list.size()) {
+                        Task removedTask = list.remove(taskIndex);
+                        System.out.println("    Noted. I've removed this task:");
+                        System.out.printf("      [%s][%s] %s%n", removedTask.getTaskType(), removedTask.getStatusIcon(), removedTask.description);
+                        System.out.printf("    Now you have %d tasks in the list.%n", list.size());
+                    } else {
+                        throw new BuddyException("I don't think task is on your list buddy...");
                     }
 
                 } else {
@@ -134,3 +151,4 @@ public class Buddy {
         scanner.close();
     }
 }
+
