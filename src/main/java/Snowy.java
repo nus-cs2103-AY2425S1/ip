1a1e020f54ae7c4a1a14d7f84f844c27fdf268a5
Xueting <xueting1966@gmail.com>
2024-08-22 15:56:21 +0800

    Level 0

---
+++ src/main/java/Snowy.java
@@ -0,0 +1,7 @@
+public class Snowy {
+    public static void main(String[] args) {
+        String line ="____________________________________________________________\n";
+        System.out.println(line + " Hello I'm Snowy\n What can I do for you?\n" + line
+                + " Bye. Hope to see you again soon!\n" + line);
+    }
+}
