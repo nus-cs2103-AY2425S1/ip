685eedd16fcf936653c34ebf029fbb755226cbc1
LiHaixin000 <lihaixin000@gmail.com>
2024-08-22 10:38:26 +0800

    Implemented behaviours for ToDos, Events, Deadlines in Arts class

--- src/main/java/Arts.java
+++ src/main/java/Arts.java
@@ -58,11 +58,22 @@ public class Arts {
                 System.out.println(" " + tasks[taskIndex]);
                 System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = new Task(input);
+                if (parts[0].equalsIgnoreCase("todo")) {
+                    tasks[taskCount] = new Todo(parts[1]);
+                } else if (parts[0].equalsIgnoreCase("deadline")) {
+                    String[] deadlineParts = parts[1].split(" /by ");
+                    tasks[taskCount] = new Deadline(deadlineParts[0], deadlineParts[1]);
+                } else if (parts[0].equalsIgnoreCase("event")) {
+                    String[] eventParts = parts[1].split(" /from | /to ");
+                    tasks[taskCount] = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                } else {
+                    tasks[taskCount] = new Task(input);
+                }
                 taskCount++;
                 System.out.println("____________________________________________________________");
-                System.out.println("Added: " + input);
-                System.out.println("Wow, that's interesting! Tell me more!");
+                System.out.println("Got it. I've added this task:");
+                System.out.println(" " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " " + (taskCount == 1 ? "task" : "tasks") +  " in the list.");
                 System.out.println("____________________________________________________________");
             }
         }
