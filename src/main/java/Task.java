769ed520e6cc5181bb589314834637a1524687f5
Clarenceeey <clarencey100@gmail.com>
2024-08-22 16:33:54 +0800

    Removed default constructor in Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,9 +4,7 @@ public abstract class Task {
     protected String name;
     protected boolean isDone = false;
 
-    public Task(String name) {
-        this.name = name;
-    }
+
 
     public void mark() {
         this.isDone = true;
