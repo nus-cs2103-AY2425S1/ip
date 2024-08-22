ec4ddc4a0e541aa8ffac1c514b135ca802a2c996
achl1012 <>
2024-08-22 11:45:07 +0800

    rename file, add greeting and exit

---
+++ src/main/java/Charlotte.java
@@ -0,0 +1,7 @@
+public class Charlotte {
+    public static void main(String[] args) {
+        String line = "_________________________________________________";
+        System.out.println(line + "\nHello! I'm Charlotte!\nWhat can I do for you?\n" + line +
+                "\nBye. Hope to see you again soon!\n" + line);
+    }
+}
