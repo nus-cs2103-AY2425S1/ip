5c9206b2cb98ccdca4aa951cb015177012eece68
Zhang Jinhan <zhangjinhan338@gmail.com>
2024-08-23 00:05:19 +0800

    Added feature to mark tasks as done

--- src/main/java/Juno.java
+++ src/main/java/Juno.java
@@ -6,10 +6,14 @@ public class Juno {
     private String greeting;
     private String farewell;
 
-    private final String exitString;
+    private final String exitString = "bye";
 
-    private final String listTaskString;
-    private ArrayList<String> tasks;
+    private final String listTaskString = "list";
+    private ArrayList<Task> tasks;
+
+    private final String markTaskString = "mark";
+    private final String unmarkTaskString = "unmark";
+    private final String addTaskString = "add";
 
     public Juno() {
         this.logo = """
@@ -29,12 +33,10 @@ public class Juno {
         this.greeting = "🌟 Welcome to the Future! I'm Juno, your digital assistant.\n" +
                 "How can I assist you on your journey today?";
         this.farewell = "👋 Farewell for now! Looking forward to our next interaction.";
-        this.exitString = "bye";
-        this.listTaskString = "list";
         this.tasks = new ArrayList<>();
     }
 
-    public void run() {
+    public void startBot() {
         // start the chat bot
         System.out.println("___________________________________________________________________");
         System.out.println(this.logo);
@@ -54,13 +56,18 @@ public class Juno {
             if (userInput.equalsIgnoreCase(this.exitString)) {
                 this.farewellMessage();
                 break;
-            } else if (userInput.equalsIgnoreCase(this.listTaskString)){
+            } else if (userInput.equalsIgnoreCase(this.listTaskString)) {
                 this.listTasks();
             } else if (userInput.isEmpty()) {
                 this.invalidUserInput();
-            }
-            else {
+            } else if (userInput.startsWith(this.markTaskString)) {
+                this.toggleTaskStatus(userInput, true);
+            } else if (userInput.startsWith(this.unmarkTaskString)) {
+                this.toggleTaskStatus(userInput, false);
+            } else if (userInput.startsWith(this.addTaskString)) {
                 this.addTask(userInput);
+            } else {
+                this.invalidFunctionInput();
             }
         }
 
@@ -74,38 +81,91 @@ public class Juno {
     }
 
     private void listTasks() {
-        System.out.println("____________________________________________________________");
         if (this.tasks.isEmpty()) {
             System.out.println("No tasks added yet!");
         } else {
             System.out.println("Here's a rundown of all your tasks! \uD83D\uDE0A");
             for (int i = 0; i < this.tasks.size(); i++) {
-                System.out.println((i + 1) + ". " + this.tasks.get(i));
+                String formmattedString = String.format(
+                        "%d. %s",
+                        (i + 1),
+                        this.tasks.get(i).toString()
+                );
+                System.out.println(formmattedString);
             }
         }
-        System.out.println("____________________________________________________________");
     }
 
     private void addTask(String userInput) {
-        tasks.add(userInput);
-        System.out.println("____________________________________________________________");
-        System.out.println("Got it! I've added : \"" + userInput + "\" in my memory!");
-        System.out.println("____________________________________________________________");
+        // Check if the task already exists
+        for (Task task : this.tasks) {
+            if (task.getDescription().equalsIgnoreCase(userInput)) {
+                System.out.println("This task is already in your list! Maybe you can try renaming it and input again?");
+                return;
+            }
+        }
+        String taskDescription;
+        try {
+            taskDescription = userInput.split("\\s+", 2)[1];
+        } catch(Exception e) {
+            System.out.println("Hmm, something went wrong. Did you add task correctly? " +
+                    "(\uD83D\uDCA1 Tip: Use \"add {Input task description here}\" to add a task)");
+            return;
+        }
+        Task t = new Task(taskDescription);
+        tasks.add(t);
+        System.out.println("Got it! I've added : \"" + taskDescription + "\" in my memory!");
     }
 
     private void invalidUserInput() {
-        System.out.println("____________________________________________________________");
         System.out.println("Well, seems like you did not input anything! Please try again.");
-        System.out.println("____________________________________________________________");
     }
 
+    // Could implement a help function to see all possible commands in the future
+    private void invalidFunctionInput() {
+        System.out.println("The input you provided is invalid! " +
+                "(\uD83D\uDCA1 Tip: You can use the following commands \"add\", \"list\",\"mark\", \"unmark\".)");
+    }
 
-
-
+    private void toggleTaskStatus(String userInput, boolean markAsDone) {
+        try {
+            int taskNumber = Integer.parseInt(userInput.split("\\s+", 2)[1]) - 1;
+            if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                Task taskToMark = tasks.get(taskNumber);
+                if (markAsDone) {
+                    if (taskToMark.getIsDone()) {
+                        System.out.println("You have completed the task \"" +
+                                taskToMark.getDescription() +
+                                "\" already!");
+                    } else {
+                        taskToMark.markAsDone();
+                        System.out.println("Great work! Knew you would have completed it.");
+                    }
+                } else {
+                    if (!taskToMark.getIsDone()) {
+                        System.out.println("Task \"" +
+                                taskToMark.getDescription() +
+                                "\" is still not done! You can't unmark an undone task!");
+                    } else {
+                        tasks.get(taskNumber).markAsNotDone();
+                        System.out.println("Hey, I have unmarked this task for you. " +
+                                "Maybe you should start working on it soon?");
+                    }
+                }
+                System.out.println("  " + tasks.get(taskNumber).toString());
+            } else {
+                System.out.println("Oops! That task number is out of range. " +
+                        "(\uD83D\uDCA1 Tip: You can type \"list\" to see task numbers)");
+            }
+        } catch (Exception e) {
+            System.out.println("Hmm, something went wrong. Please enter a task number after mark/unmark. " +
+                    "(\uD83D\uDCA1 Tip: You can type \"list\" to see task numbers)");
+        }
+    }
 
     public static void main(String[] args) {
         Juno junoChatBot = new Juno();
-        junoChatBot.run();
+        junoChatBot.startBot();
 
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
