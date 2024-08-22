11a2a946e75e01951c9706974891bf272e779998
Jayden-Kim-NUS <e1121727@u.nus.edu>
2024-08-22 20:59:47 +0800

    Edit greeting and exit text

--- src/main/java/Arona.java
+++ src/main/java/Arona.java
@@ -1,5 +1,8 @@
 public class Arona {
     public static void main(String[] args) {
-        System.out.println("Hello from Arona");
+        System.out.println("Hello! I'm Arona.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
