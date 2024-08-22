ed538787e28d842bf6af95c8d817552bb4e9139c
jonushzw <jonushzw@gmail.com>
2024-08-22 11:24:45 +0800

    Commit for Level-8 Modified Deadline and Event classes to format dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,22 @@
-public class Deadline extends Task {
-    private final String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by, boolean isDone) {
+public class Deadline extends Task {
+    private LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by, boolean isDone) {
         super(description, TaskType.DEADLINE, isDone);
         this.by = by; // store the deadline
     }
 
     @Override
     public String toFileFormat() {
-        return "D | " + (getIsDone() ? "1" : "0") + " | " + getDescription() + " | " + by;
+        return "D | " + (getIsDone() ? "1" : "0") + " | " + getDescription() + " | "
+                + by.format(DateTimeFormatter.ISO_DATE_TIME);
     }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + by.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,11 @@
-public class Event extends Task {
-    private final String from;
-    private final String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to, boolean isDone) {
+public class Event extends Task {
+    private LocalDateTime from;
+    private LocalDateTime to;
+
+    public Event(String description, LocalDateTime from, LocalDateTime to, boolean isDone) {
         super(description, TaskType.EVENT, isDone);
         this.from = from; // store the start time
         this.to = to; // store the end time
@@ -10,11 +13,15 @@ public class Event extends Task {
 
     @Override
     public String toFileFormat() {
-        return "E | " + (getIsDone() ? "1" : "0") + " | " + getDescription() + " | " + from + " | " + to;
+        return "E | " + (getIsDone() ? "1" : "0") + " | " + getDescription() + " | "
+                + from.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + " | "
+                + to.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: "
+                + from.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a")) + " to: "
+                + to.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a")) + ")";
     }
 }
