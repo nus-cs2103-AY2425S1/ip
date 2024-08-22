2c39c6a70f8e168cf38a2b075d1a60cfe4552774
Ansel-Ch <achong2014@gmail.com>
2024-08-22 12:45:20 +0800

    Update Deadline constructor to catch No Due Date inputs

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,13 @@
 public class Deadline extends Task {
     private String due;
 
-    public Deadline(String description, String due) {
-        super(description.strip());
-        StringBuilder str = new StringBuilder(due.strip());
+    public Deadline(String... parems) throws IllegalArgumentException {
+        super(parems[0].strip());
+        if (parems.length < 2) {
+            throw new IllegalArgumentException("Deadline: You did not provide a due date/time.");
+        }
+
+        StringBuilder str = new StringBuilder(parems[1].strip());
         str.insert(str.indexOf(" "), ':');
         this.due = str.toString();
     }
