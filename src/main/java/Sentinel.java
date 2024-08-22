e3053eb40a545ded657f9da540078ab4ce0eae22
riccoljy <limricco2002@gmail.com>
2024-08-22 11:13:00 +0800

    + Mark Done/Undone & Task.class
    Implemented Task class that is used by main Sentinel file with methods like toString, setDone, setUndone, etc.

--- src/main/java/Sentinel.java
+++ src/main/java/Sentinel.java
@@ -1,49 +1,73 @@
+
 import java.util.Scanner;
 
 public class Sentinel {
 
     public static int index = 0;
-    public static String[] items = new String[100];
+    public static Task[] items = new Task[100];
 
     public static void main(String[] args) {
-        String logo = 
-        " _____                                                                                      _____ \n" + 
-        "( ___ )                                                                                    ( ___ )\n" + 
-        " |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | \n" + 
-        " |   |                                                                                      |   | \n" + 
-        " |   |   ________  _______   ________   _________  ___  ________   _______   ___            |   | \n" + 
-        " |   |  |\\   ____\\|\\  ___ \\ |\\   ___  \\|\\___   ___\\\\  \\|\\   ___  \\|\\  ___ \\ |\\  \\           |   | \n" + 
-        " |   |  \\ \\  \\___|\\ \\   __/|\\ \\  \\\\ \\  \\|___ \\  \\_\\ \\  \\ \\  \\\\ \\  \\ \\   __/|\\ \\  \\          |   | \n" + 
-        " |   |   \\ \\_____  \\ \\  \\_|/_\\ \\  \\\\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\\\ \\  \\ \\  \\_|/_\\ \\  \\         |   | \n" + 
-        " |   |    \\|____|\\  \\ \\  \\_|\\ \\ \\  \\\\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\\\ \\  \\ \\  \\_|\\ \\ \\  \\____    |   | \n" + 
-        " |   |      ____\\_\\  \\ \\_______\\ \\__\\  \\__\\   \\ \\__\\ \\ \\__\\ \\__\\\\ \\__\\ \\_______\\ \\_______\\  |   | \n" + 
-        " |   |     |\\_________\\|_______|\\|__| \\|__|    \\|__|  \\|__|\\|__| \\|__|\\|_______\\|_______|   |   | \n" + 
-        " |   |     \\|_________|                                                                     |   | \n" + 
-        " |   |                                                                                      |   | \n" + 
-        " |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| \n" + 
-        "(_____)                                                                                    (_____)";
+        String logo
+                = """
+                   _____                                                                                      _____ 
+                  ( ___ )                                                                                    ( ___ )
+                   |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+                   |   |                                                                                      |   | 
+                   |   |   ________  _______   ________   _________  ___  ________   _______   ___            |   | 
+                   |   |  |\\   ____\\|\\  ___ \\ |\\   ___  \\|\\___   ___\\\\  \\|\\   ___  \\|\\  ___ \\ |\\  \\           |   | 
+                   |   |  \\ \\  \\___|\\ \\   __/|\\ \\  \\\\ \\  \\|___ \\  \\_\\ \\  \\ \\  \\\\ \\  \\ \\   __/|\\ \\  \\          |   | 
+                   |   |   \\ \\_____  \\ \\  \\_|/_\\ \\  \\\\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\\\ \\  \\ \\  \\_|/_\\ \\  \\         |   | 
+                   |   |    \\|____|\\  \\ \\  \\_|\\ \\ \\  \\\\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\\\ \\  \\ \\  \\_|\\ \\ \\  \\____    |   | 
+                   |   |      ____\\_\\  \\ \\_______\\ \\__\\  \\__\\   \\ \\__\\ \\ \\__\\ \\__\\\\ \\__\\ \\_______\\ \\_______\\  |   | 
+                   |   |     |\\_________\\|_______|\\|__| \\|__|    \\|__|  \\|__|\\|__| \\|__|\\|_______\\|_______|   |   | 
+                   |   |     \\|_________|                                                                     |   | 
+                   |   |                                                                                      |   | 
+                   |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+                  (_____)                                                                                    (_____)""";
 
         System.out.println("Hello from\n" + logo);
         System.out.println("\nWhat can I do for you?\n____________________________________________________________\n");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println("____________________________________________________________\n");
-        while (true) {
-            if (input.equals("bye"))
-                break;
-            else if (input.equals("list")) {
-                for (int i = 0; i < index; i++) {
-                    System.out.println("\t" + (i + 1) + ". " + items[i]);
+        try (Scanner sc = new Scanner(System.in)) {
+            String input = sc.nextLine();
+            System.out.println("____________________________________________________________\n");
+            OUTER:
+            while (true) {
+                switch (input) {
+                    case "bye" -> {
+                        break OUTER;
+                    }
+                    case "list" -> {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < index; i++) {
+                            System.out.println("\t" + (i + 1) + ". [" + items[i].getStatusIcon() + "] " + items[i]);
+                        }
+                    }
+                    case "mark" -> {
+                        int num = sc.nextInt();
+                        if (num > index) {System.out.println("No such item in the list!"); break;}
+                        else if (items[num-1].isDone()) {System.out.println(items[num-1].toString() + " has already been marked as done."); break;}
+                        items[num - 1].setDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("\t" + "[" + items[num - 1].getStatusIcon() + "] " + items[num - 1]);
+                    }
+                    case "unmark" -> {
+                        int num = sc.nextInt();
+                        if (num > index) {System.out.println("No such item in the list!"); break;}
+                        else if (!items[num-1].isDone()) {System.out.println(items[num-1] + " has already been marked as undone."); break;}
+                        items[num - 1].setUndone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("\t" + "[" + items[num - 1].getStatusIcon() + "] " + items[num - 1]);
+                    }
+                    default -> {
+                        items[index++] = new Task(input);
+                        System.out.println("added: " + items[index-1]);
+                    }
                 }
-            } else {
-                items[index++] = input;
-                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________\n");
+                input = sc.next();
+                System.out.println("____________________________________________________________\n");
             }
-            System.out.println("____________________________________________________________\n");
-            input = sc.nextLine();
-            System.out.println("____________________________________________________________\n");
+            System.out.println("Bye. Hope to see you again soon!");
         }
-        System.out.println("Bye. Hope to see you again soon!");
-        sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString(){
+        return this.description;
+    }
+
+    public boolean isDone() {return this.isDone;}
+    public void setUndone(){ this.isDone = false;}
+    public void setDone(){ this.isDone = true;}
+}
