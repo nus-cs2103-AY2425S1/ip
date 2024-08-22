a80ba903b3a4fa4cf5dbb806705fbe4d7f6be6b3
shuckycheese <darrylshuck@yahoo.com.sg>
2024-08-22 21:39:47 +0800

    renamed the chatbot and added a custom greeting and exit message

---
+++ src/main/java/James.java
@@ -0,0 +1,8 @@
+public class James {
+    public static void main(String[] args) {
+        String greeting = "Hello Big Boy! I'm James \n"
+                + "How can I assist you today? \n";
+        String exitMessage = "Goodbye. Come back anytime! \n";
+        System.out.println(greeting + exitMessage);
+    }
+}
