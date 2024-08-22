a8beb6bc14f77d143000d2c95aa6149ef502a385
QinHaichen12 <haichensurfs@gmail.com>
2024-08-22 15:24:49 +0800

    Level 1

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 public class Bottle {
+    final static String lineBreak = "\n____________________________________________________________\n";
+
+    public static void printwithBreak(String str) {
+        System.out.println(lineBreak + str + lineBreak);
+    }
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
+
+        String welcomeMsg =
                 " Hello! I'm Bottle\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(logo);
+                " What can I do for you?";
+
+        String byeMsg =" Bye. Hope to see you again soon!";
+        printwithBreak(welcomeMsg);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                printwithBreak(byeMsg);
+                break;
+            }
+            printwithBreak(input);
+        }
     }
 }
