1aa31b0641e3d0f069589d0968983e8089072ffe
Shantaa <shanetay4425@gmail.com>
2024-08-22 13:22:32 +0800

    finish level 1

--- src/main/java/Joseph.java
+++ src/main/java/Joseph.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Joseph {
     public static void main(String[] args) {
-        String name = "Joseph";
+        final String NAME = "Joseph";
+        final String EXIT = "bye";
+        Scanner scanner = new Scanner(System.in);
         System.out.println("----------------------------------");
-        System.out.println("Hello, I'm " + name + "!");
+        System.out.println("Hello, I'm " + NAME + "!");
         System.out.println("How can I help you today?");
         System.out.println("----------------------------------");
-        System.out.println("Bye! Have a nice day :)");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals(EXIT)) {
+                System.out.println("Bye! Have a nice day :)");
+                System.out.println("----------------------------------");
+                scanner.close();
+                break;
+            }
+            System.out.println("----------------------------------");
+            System.out.println(input);
+            System.out.println("----------------------------------");
+        }
     }
 }
