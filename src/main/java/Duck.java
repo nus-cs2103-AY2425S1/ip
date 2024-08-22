90ba5af853120ecfe1c0ac62bab283be5065e26d
SamuelFoo <fooenzesamuel@gmail.com>
2024-08-22 15:00:45 +0800

    Make the chatbot capable of echoing user input

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,15 +1,63 @@
+import java.util.Scanner;
+
 public class Duck {
     private static final String CHATBOT_NAME = "Duck";
-    private static final String TEXT_SEPARATOR = "____________________________________________________________\n";
+    private static final String TEXT_SEPARATOR = "____________________________________________________________";
+    private static final String TEXT_SEPARATOR_WITH_NEWLINE = TEXT_SEPARATOR + "\n";
+
+    // Standard messages
+    private static final String GREETING = String.format("Hello! I'm %s\n", CHATBOT_NAME)
+            + "What can I do for you?";
+    private static final String GOODBYE = "Bye. Hope to see you again soon!";
+
+    private static String addSeparators(String text) {
+        return TEXT_SEPARATOR_WITH_NEWLINE
+                + text + "\n"
+                + TEXT_SEPARATOR_WITH_NEWLINE;
+    }
+
+    private static String indentText(String text, int indentLevel) {
+        StringBuilder indentedText = new StringBuilder();
+        String[] lines = text.split("\n");
+
+        for (String line : lines) {
+            indentedText.append(" ".repeat(indentLevel)).append(line).append("\n");
+        }
+
+        // Convert to String and trim the last newline character
+        return indentedText.toString().replaceAll("[\n\r]$", "");
+    }
+
+    private static String formatAsResponse(String text) {
+        return indentText(addSeparators(indentText(text, 1)), 4);
+    }
+
+    private static void printAsResponse(String text) {
+        System.out.println(formatAsResponse(text));
+    }
 
     public static void main(String[] args) {
-        // Generate greeting
-        String greeting = TEXT_SEPARATOR
-                + String.format(" Hello! I'm %s\n", CHATBOT_NAME)
-                + " What can I do for you?\n"
-                + TEXT_SEPARATOR;
+        // Start scanner
+        Scanner sc = new Scanner(System.in);
 
         // Print greeting
-        System.out.println(greeting);
+        printAsResponse(GREETING);
+
+        // User input loop
+        while (true) {
+            System.out.println("");
+
+            String line = sc.nextLine();
+
+            if (line.equals("bye")) {
+                printAsResponse(GOODBYE);
+                break;
+            } else {
+                printAsResponse(line);
+            }
+        }
+
+        // Close Scanner
+        sc.close();
     }
 }
