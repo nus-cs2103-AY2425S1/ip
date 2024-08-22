0f0fd16594cc00f7da492e304bcfc8ef8c827f9c
Christopher Tan <phertan02@gmail.com>
2024-08-22 23:38:14 +0800

    Add command error checking

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,26 @@
 public class Parser {
-    public static Query parse(String raw) {
+    public static Query parse(String raw) throws CloudException {
         String[] split = raw.split(" ", 2);
         String command = split[0].strip();
+
+        // Checking for errors in command
+        // TODO: handle errors for remaining commands
+        switch (command) {
+            case "todo":
+                if (split.length < 2) {
+                    throw new InputException("The description of a todo cannot be empty");
+                }
+                break;
+            case "deadline":
+            case "event":
+            case "list":
+            case "bye":
+            case "mark":
+            case "unmark":
+                break;
+            default:
+                throw new UnrecognisedException("Unrecognised command, please try again.");
+        }
         if (split.length > 1) {
             String details = split[1];
             return new Query(command, details);
