37fbac6bdd2437cadfa773971af029ff904e8831
LOLIPOP-INTELLIGENCE <manas@sarvam.ai>
2024-08-22 22:17:41 +0800

    Adds initial welcome message and changes bot name

---
+++ src/main/java/Appleaster.java
@@ -0,0 +1,17 @@
+public class Appleaster {
+    public static void main(String[] args) {
+        String logo = "   _____                .__                          __                \n"
+                    + "  /  _  \\ ______ ______ |  |   ____ _____    _______/  |_  ___________ \n"
+                    + " /  /_\\  \\\\____ \\\\____ \\|  | _/ __ \\\\__  \\  /  ___/\\   __\\/ __ \\_  __ \\\n"
+                    + "/    |    \\  |_> >  |_> >  |_\\  ___/ / __ \\_\\___ \\  |  | \\  ___/|  | \\/\n"
+                    + "\\____|__  /   __/|   __/|____/\\___  >____  /____  > |__|  \\___  >__|   \n"
+                    + "        \\/|__|   |__|             \\/     \\/     \\/            \\/       \n";
+        System.out.println("------------------------------------");                    
+        System.out.println(logo);
+        System.out.println("Welcome to Appleaster!");
+        System.out.println("What can I do for you?");
+        System.out.println("------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------");
+    }
+}
