2753c26f9b5c073e99363804d12ea90cd2142c8d
Ming Jun <evan.zhangmingjun@gmail.com>
2024-08-22 17:39:09 +0800

    Add ability to echo commands

--- src/main/java/ChadGPT.java
+++ src/main/java/ChadGPT.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class ChadGPT {
     public static void main(String[] args) {
         printBotMessage("Hello! I'm ChadGPT. What can I do for you?");
-        printBotMessage("Bye. Hope to see you again soon!");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            // TODO: Use regex for more advanced matching
+            if (input.startsWith("bye")) {
+                printBotMessage("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                printBotMessage(input);
+            }
+        }
     }
 
     /**
@@ -10,10 +24,6 @@ public class ChadGPT {
      * @param msg the string message to be printed
      */
     public static void printBotMessage(String msg) {
-        final String divider = "____________________________________________________________";
-
-        System.out.println(divider);
-        System.out.println(msg);
-        System.out.println(divider);
+        System.out.println(Formatter.formatBotMessage(msg));
     }
 }
--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Formatter {
+public class Formatter {
+    public static final String DIVIDER = "____________________________________________________________\n";
+    public static String formatBotMessage(String msg) {
+        return (DIVIDER + msg + "\n" + DIVIDER).indent(4);
+    }
 }
