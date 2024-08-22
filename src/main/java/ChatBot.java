25b52be8980f5cc69aa3f723f6b36a05dfad973e
he shi <e1312287@u.nus.edu>
2024-08-22 23:08:32 +0800

    level-0

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,10 +1,21 @@
-public class Duke {
+import java.util.Scanner;
+
+public class ChatBot {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // Greeting message
+        System.out.println("Hello from I am chatbot");
+        System.out.println("What can I do for you?");
+
+
+        // Goodbye message
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
+
 }
