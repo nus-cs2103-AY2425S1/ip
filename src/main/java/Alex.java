d641526748779f2b3abae5316fde33a222c5e38e
leroychiu20 <e1122486@u.nus.edu>
2024-08-22 11:05:34 +0800

    marking and unmarking

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -5,7 +5,7 @@ public class Alex {
 
     public static final String byeMessage = "Bye. Hope to see you again soon!";
     public static final String LINE = "----------------------------------------------------";
-    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Task> list = new ArrayList<>();
 
     public void scan() {
         Scanner scanner = new Scanner(System.in);
@@ -17,16 +17,48 @@ public class Alex {
             System.out.println(LINE);
         }
         else if (userInput.equalsIgnoreCase("list")) {
-            System.out.println(LINE);
+            System.out.println(LINE + "\n" +
+                    "Here are the tasks in your list: ");
             for (int i = 0; i < list.size(); i++) {
                 System.out.println((i + 1) + ". " + list.get(i));
             }
             System.out.println(LINE);
             scan();
+        } else if (userInput.startsWith("mark")) {
+            int index = Integer.parseInt(userInput.substring(5)) - 1;
+            if (index >= 0 && index < list.size()) {
+                Task task = list.get(index);
+                System.out.println(LINE);
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done: \n" +
+                        task.toString());
+                System.out.println(LINE);
+            } else {
+                System.out.println(LINE);
+                System.out.println("Invalid index. Please try again.");
+                System.out.println(LINE);
+            }
+            scan();
+        } else if (userInput.startsWith("unmark")) {
+            int index = Integer.parseInt(userInput.substring(7)) - 1;
+            if (index >= 0 && index < list.size()) {
+                Task task = list.get(index);
+                System.out.println(LINE);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet: \n" +
+                        task.toString());
+                System.out.println(LINE);
+            } else {
+                System.out.println(LINE);
+                System.out.println("Invalid index. Please try again.");
+                System.out.println(LINE);
+            }
+            scan();
         } else {
             System.out.println(LINE);
             System.out.println("added: " + userInput);
-            list.add(userInput);
+            Task task = new Task(userInput);
+            list.add(task);
             System.out.println(LINE);
             scan();
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    public String description;
+    public boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+}
