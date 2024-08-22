7f567aef7c3049d4c2b1ff57436ba00e1275a636
CreditableX <creditablecreates@gmail.com>
2024-08-22 21:49:17 +0800

    checkmark done / not done feature

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -10,29 +10,43 @@ public class Bob {
                 """;
         System.out.println(skeleton);
 
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         Scanner scanner = new Scanner(System.in);
         int position = 0;
-        
+        int markPos = 0;
+
         boolean exit = false;
         while (!exit) {
             System.out.print("Text: ");
             String input = scanner.nextLine();
 
             // check for the input case
-            switch (input.toLowerCase()) {
+            switch (input.split(" ")[0].toLowerCase()) {
                 case "bye":
                     exit = true;
                     break;
                 case "list":
                     String retString = "";
                     for (int i = 0; i < position; i++) {
-                        retString += taskList[i] + "\n";
+                        retString += (i + 1) + ". " + taskList[i].getString() + "\n";
                     }
-                    System.out.println(retString + "\n" + blankline);
+                    System.out.println("Here are the tasks in your list:" + "\n"
+                            + retString + blankline);
+                    break;
+                case "mark":
+                    markPos = Integer.parseInt(input.split(" ")[1]) - 1;
+                    taskList[markPos].setDone();
+                    System.out.println("Nice! I've marked this task as done:" + "\n"
+                            + taskList[markPos].getString() + "\n" + blankline);
+                    break;
+                case "unmark":
+                    markPos = Integer.parseInt(input.split(" ")[1]) - 1;
+                    taskList[markPos].setUndone();
+                    System.out.println("OK, I've marked this task as not done yet:" + "\n"
+                            + taskList[markPos].getString() + "\n" +blankline);
                     break;
                 default:
-                    taskList[position] = position + ". " + input;
+                    taskList[position] = new Task(input);
                     System.out.println("added: " + input + "\n" + blankline);
                     position++;
                     break;
