7cb8caa26c0d979ef5908b3ef5070b592e50ba82
jiayr99 <e1249294@u.nus.edu>
2024-08-22 18:12:49 +0800

    Support ToDo

--- src/main/java/Jia.java
+++ src/main/java/Jia.java
@@ -5,9 +5,8 @@ public class Jia {
     public static void main(String[] args) {
         //Create a scanner object to read user input
         Scanner scanner = new Scanner(System.in);
-
-        //Create an array list to store tasks
-        ArrayList<Task> tasks = new ArrayList<>();
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
         String input;
 
         //Greet the user
@@ -23,26 +22,45 @@ public class Jia {
             if (input.equalsIgnoreCase("list")) {
                 System.out.println("______________________________________________________");
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("______________________________________________________");
             } else if (input.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(input.substring(5)) - 1;
-                tasks.get(taskNumber).markAsDone();
-                System.out.println("______________________________________________________");
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(" " + tasks.get(taskNumber));
-                System.out.println("______________________________________________________");
+                if (taskNumber >= 0 && taskNumber < taskCount && taskCount > 1) {
+                    tasks[taskNumber].markAsDone();
+                    System.out.println("______________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(" " + tasks[taskNumber]);
+                    System.out.println("______________________________________________________");
+                } else {
+                    System.out.println("Invalid task number :(");
+                }
             } else if (input.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(input.substring(7)) - 1;
-                tasks.get(taskNumber).markAsNotDone();
+                if (taskNumber >= 0 && taskNumber < taskCount) {
+                    tasks[taskNumber].markAsNotDone();
+                    System.out.println("______________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(" " + tasks[taskNumber]);
+                    System.out.println("______________________________________________________");
+                } else {
+                    System.out.println("Invalid task number :(");
+                }
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                tasks[taskCount++] = new ToDo(description);
                 System.out.println("______________________________________________________");
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(" " + tasks.get(taskNumber));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(" " + tasks[taskCount - 1]);
+                if (taskCount > 1) {
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                } else {
+                    System.out.println("Now you have " + taskCount + " task in the list.");
+                }
                 System.out.println("______________________________________________________");
             } else if (!input.equalsIgnoreCase("bye")) {
-                    tasks.add(new Task(input));
                     System.out.println("______________________________________________________");
                     System.out.println(" " + "added: " + input);
                     System.out.println("______________________________________________________");
