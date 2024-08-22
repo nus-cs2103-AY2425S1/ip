d75d1b00641b0d006e882889f4527c00011fa5da
nathanshew <92252099+nathanshew@users.noreply.github.com>
2024-08-22 11:33:33 +0800

    Level 1. Echo Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/NathanBot.java
+++ src/main/java/NathanBot.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class NathanBot {
     public static void main(String[] args) {
-        String Line = "____________________________________________________________\n";
-        String Greet = """
+        String LINE = "____________________________________________________________\n";
+        String GREET = """
                         Hello! I'm NathanBot
                         What can I do for you?
                        """;
-        String Exit = " Bye. Hope to see you again soon!\n";
-        System.out.println(Line + Greet + Line + Exit + Line);
+        String EXIT = "Bye. Hope to see you again soon!\n";
+        String BREAK_COMMAND = "bye";
+        System.out.println(LINE + GREET + LINE);
+        
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String input = scanner.nextLine();
+                if (input.equals(BREAK_COMMAND)) {
+                    System.out.println(LINE + EXIT + LINE);
+                    break;
+                } else {
+                    System.out.println(LINE + input + "\n" + LINE);
+                }
+            }
+        }
     }
 }
