cfe8b7fece303c892a540653fba3c3204d64fa63
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-22 14:55:04 +0800

    added AddList

--- src/main/java/Stobberi.java
+++ src/main/java/Stobberi.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Stobberi {
@@ -6,6 +7,7 @@ public class Stobberi {
             "Hello! I'm " + nameOfChatBot + ".\n"
             + "What can I do for you?";
     private static String goodByeGreeting = "Bye. Hope to see you again soon! :)\n";
+    private static ArrayList<String> listOfWords= new ArrayList<String>();
 
     private static String displayForm(String phrase) {
         return
@@ -23,9 +25,30 @@ public class Stobberi {
             temp = scanner.nextLine();
         }
     }
+
+    private static void AddList() {
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.nextLine();
+
+        while (!temp.equals("bye")) {
+            if (temp.equals("list")) {
+                String list = "";
+                for (int i = 1; i < listOfWords.size() + 1;i++) {
+                    list += i + ". " + listOfWords.get(i - 1)+ "\n";
+                }
+                System.out.println(displayForm(list));
+            } else {
+                listOfWords.add(temp);
+                System.out.println(displayForm("added: " + temp));
+            }
+            temp = scanner.nextLine();
+        }
+    }
+
+
     public static void main(String[] args) {
         System.out.println(displayForm(helloGreeting));
-        Echo();
+        AddList();
         System.out.println(displayForm(goodByeGreeting));
     }
 }
