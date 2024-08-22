bf4a48a55d95450b52849e4e01f1e01fbdb2ffc2
he shi <e1312287@u.nus.edu>
2024-08-22 23:38:36 +0800

    level-0

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,37 +1,61 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatBot {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        tasks.add(new Task("read book"));
+        tasks.add(new Task("return book"));
+        tasks.add(new Task("buy bread"));
 
-        // Greeting message
-        System.out.println("Hello from I am chatbot");
-        System.out.println("What can I do for you?");
-
-        // Create a scanner object to read input
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> save = new ArrayList<>(100);
-        Integer index = 0;
-        // Wait for the user to enter something
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
-            save.set(index, input);
-            index += 1;
-            System.out.println("add: " + input);
-            // Wait for the user to enter something
-            input = scanner.nextLine();
-        }
-        // Goodbye message
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm chatbot lisWhat can I do for you?");
 
-        // Close the scanner
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.startsWith("list")) {
+                listTasks();
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                markTask(taskNumber);
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                unmarkTask(taskNumber);
+            } else if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("I'm not sure what you mean. Can you please clarify?");
+            }
+        }
         scanner.close();
     }
 
+    private static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + "." + tasks.get(i));
+        }
+    }
+
+    private static void markTask(int taskIndex) {
+        if (taskIndex >= 0 && taskIndex < tasks.size()) {
+            tasks.get(taskIndex).markDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + tasks.get(taskIndex));
+        } else {
+            System.out.println("Invalid task number!");
+        }
+    }
+
+    private static void unmarkTask(int taskIndex) {
+        if (taskIndex >= 0 && taskIndex < tasks.size()) {
+            tasks.get(taskIndex).unmarkDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + tasks.get(taskIndex));
+        } else {
+            System.out.println("Invalid task number!");
+        }
+    }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with [X] and not done with [ ]
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void unmarkDone() {
+        isDone = false;
+    }
+}
