b9b09a0a0a9fd305e6424771d2572f2c160991a1
jerrelllee <leejerrell337@gmail.com>
2024-08-23 00:52:21 +0800

    Added echo, make Fence customised and more like an object

--- src/main/java/Fence.java
+++ src/main/java/Fence.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Fence {
+
+    public void greet() {
+        System.out.println("nihao! I'm Fence |=|=|=|=|=|");
+    }
+
+    public void exit() {
+        System.out.println("have good day :)");
+    }
+
+    public void echo(String command) {
+        System.out.println(command);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Fence");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Fence fence = new Fence();
+        fence.greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                fence.exit();
+                break;
+            }
+            fence.echo(command);
+        }
     }
 }
