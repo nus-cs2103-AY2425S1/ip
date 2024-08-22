1065e484ee3fd0aa01dfa689a0c0ae6613d01ad6
limjeremy496 <limjeremy496@gmail.com>
2024-08-22 12:37:47 +0800

    Add support for list

--- src/main/java/Vinegar.java
+++ src/main/java/Vinegar.java
@@ -1,8 +1,12 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Vinegar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> userInputList = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Vinegar\n" +
@@ -17,11 +21,19 @@ public class Vinegar {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            }
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < userInputList.size(); i++) {
+                    System.out.println((i + 1) + ". " + userInputList.get(i));
+                }
+                System.out.println("____________________________________________________________");
 
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+            } else {
+                userInputList.add(userInput);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
         }
 
         scanner.close();
