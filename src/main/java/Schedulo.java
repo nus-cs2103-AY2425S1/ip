48dfab3055cfcf9b3ae5a8883fdd07bfd56ed138
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-08-22 09:07:30 +0800

    level 0 done, it echoes what we say

--- src/main/java/Schedulo.java
+++ src/main/java/Schedulo.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Schedulo {
     public static void main(String[] args) {
         String horizontalLine = "---------------";
+        Scanner sc = new Scanner(System.in);
         System.out.println(horizontalLine);
         System.out.println("Hello I am Schedulo!");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        while (true) {
+            String command = sc.nextLine();
+            System.out.println(horizontalLine);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+            System.out.println(command);
+            System.out.println(horizontalLine);
+        }
+
+        sc.close();
     }
 }
