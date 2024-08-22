c3b56b3aaf807de87018437d5dae608b22d08a43
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-08-23 02:07:42 +0800

    Echo user query until bye
    Chatbot is saying bye before taking in user input\nUser cannot input anything\nInfinite loop added that scans for input\n

--- src/main/java/Yap.java
+++ src/main/java/Yap.java
@@ -7,21 +7,24 @@ public class Yap {
                     + "  \\// //__\\   ||____/\n"
                     + "  || //____\\  ||\n"
                     + "  ||//      \\ ||\n";
+        String separator = "_____________________________________";
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Here are some things I can do for you: ");
 
         System.out.println("What would you like me to do for you? ");
-        System.out.println("_____________________________________");
-        System.out.println("Bye! It was really nice talking to you, see you soon :)");
-//        String userInput;
-//
-//        while (true) {
-//            userInput = scanner.nextLine();
-//            if (userInput.equalsIgnoreCase("bye")) {
-//                System.out.println("Bye! It was really nice talking to you, see you soon :)");
-//                break;
-//            }
-//        }
+        System.out.println(separator);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine();
+            System.out.println(separator);
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye! It was really nice talking to you, see you soon :)");
+                break;
+            }
+            System.out.println(userInput);
+            System.out.println(separator);
+        }
     }
 }
