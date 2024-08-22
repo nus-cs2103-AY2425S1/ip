d27a7f03c0bd2b234c1e190a5228eceef4affbcd
SleepyDay <107453007+flyingsalsa@users.noreply.github.com>
2024-08-23 07:27:33 +0800

    Modify Parse.java to save Text to Storage

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Parser {
     public static void parse() {
         Scanner scanner = new Scanner(System.in);
+        Storage storage = new Storage();
         String command;
+        String text;
          do {
-            command = scanner.nextLine().toLowerCase();
-
+            text = scanner.nextLine();
+            command = text.toLowerCase();
             switch (command) {
-                case "list": System.out.println(command);
+                case "list": System.out.println(storage.toString());
                     break;
                 case "blah":
                     System.out.println(command);
@@ -18,7 +20,7 @@ public class Parser {
                     System.out.println("Thanks for using me!\nExiting the program...");
                     break;
                 default:
-                    System.out.println("Unknown command: " + command);
+                    System.out.println("Added " + '\"'+ text + "\"" + " as a new task");
             }
             Duke.lnDiv();
         } while (!command.equals("bye"));
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+
 public class Storage {
-    Task[] store = new Task[101];
+    ArrayList<Task> store = new ArrayList<>();
+
+    public void add(String s){
+
+    }
 
     public String toString() {
         String thing = "";
         int j = 1;
         for(int i = 0; i<101; i++){
-            if(store[i].isReal()){
-                thing += j + "." + store[i].toString() + "\n";
+            if(store.get(i).isReal()){
+                thing += j + "." + store.get(i).toString() + "\n";
                 j++;
             }
         }
