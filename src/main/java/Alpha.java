b809ad0dea701cc6947f5bd6a782c2ff44328de2
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2024-08-22 15:53:58 +0800

    Handle all exceptions for invalid input format

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -52,62 +52,78 @@ public class Alpha {
             }
             
             else if (s1.split(" ")[0].equals("todo")) {
-                ToDo NewToDo = new ToDo(s1.split(" ")[1]);
-                storage.storeTask(NewToDo);
-                String echoResponse = "____________________________________________________________ \n"
-                        + "Got it. I've added this task: \n"
-                        + storage.lastTask().toString() + "\n"
-                        + storage.getLength() + "\n"
-                        + "____________________________________________________________ \n";
-                System.out.println(echoResponse);
+                try {
+                    ToDo NewToDo = new ToDo(s1.split(" ")[1]);
+                    storage.storeTask(NewToDo);
+                    String echoResponse = "____________________________________________________________ \n"
+                            + "Got it. I've added this task: \n"
+                            + storage.lastTask().toString() + "\n"
+                            + storage.getLength() + "\n"
+                            + "____________________________________________________________ \n";
+                    System.out.println(echoResponse);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    // Handle the case where the description is missing
+                    System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                }
             }
 
             else if (s1.split(" ")[0].equals("deadline")) {
-                String[] splitArray = s1.split(" ");
-                String processedInput = String.join(" ", Arrays.copyOfRange(splitArray, 1, splitArray.length));
+                try {
+                    String[] splitArray = s1.split(" ");
+                    String processedInput = String.join(" ", Arrays.copyOfRange(splitArray, 1, splitArray.length));
     
-                // Further processing to get the description before the "/"
-                String description = processedInput.split("/")[0].trim();
-                
-                //Further processing to get by date after the "/"
-                String by = processedInput.split("/")[1].trim();
-                by = by.replace("by ", "");
-                
-                Deadline NewDeadline = new Deadline(description, by);
-                storage.storeTask(NewDeadline);
-                String echoResponse = "____________________________________________________________ \n"
-                        + "Got it. I've added this task: \n"
-                        + storage.lastTask().toString() + "\n"
-                        + storage.getLength() + "\n"
-                        + "____________________________________________________________ \n";
-                System.out.println(echoResponse);
+                    // Further processing to get the description before the "/"
+                    String description = processedInput.split("/")[0].trim();
+    
+                    //Further processing to get by date after the "/"
+                    String by = processedInput.split("/")[1].trim();
+                    by = by.replace("by ", "");
+    
+                    Deadline NewDeadline = new Deadline(description, by);
+                    storage.storeTask(NewDeadline);
+                    String echoResponse = "____________________________________________________________ \n"
+                            + "Got it. I've added this task: \n"
+                            + storage.lastTask().toString() + "\n"
+                            + storage.getLength() + "\n"
+                            + "____________________________________________________________ \n";
+                    System.out.println(echoResponse);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    //Handle invalid inputs
+                    System.out.println("Deadline creation should be in this format: deadline <Description> /by <Deadline>");
+                }
             }
 
             else if (s1.split(" ")[0].equals("event")) {
-                String[] splitArray = s1.split(" ");
-                String processedInput = String.join(" ", Arrays.copyOfRange(splitArray, 1, splitArray.length));
+                try {
+                    String[] splitArray = s1.split(" ");
+                    String processedInput = String.join(" ", Arrays.copyOfRange(splitArray, 1, splitArray.length));
     
-                // Further processing to get the description before the "/"
-                String description = processedInput.split("/")[0].trim();
+                    // Further processing to get the description before the "/"
+                    String description = processedInput.split("/")[0].trim();
     
-                //Further processing to get start date after the "/"
-                String start = processedInput.split("/")[1].trim();
-                start = start.replace("from ", "");
+                    //Further processing to get start date after the "/"
+                    String start = processedInput.split("/")[1].trim();
+                    start = start.replace("from ", "");
     
-                //Further processing to get start date after the "/"
-                String end = processedInput.split("/")[2].trim();
-                end = start.replace("to" , "");
-                
-                Event NewEvent = new Event(description, start, end);
-                storage.storeTask(NewEvent);
-                String echoResponse = "____________________________________________________________ \n"
-                        + "Got it. I've added this task: \n"
-                        + storage.lastTask().toString() + "\n"
-                        + storage.getLength() + "\n"
-                        + "____________________________________________________________ \n";
-                System.out.println(echoResponse);
+                    //Further processing to get start date after the "/"
+                    String end = processedInput.split("/")[2].trim();
+                    end = start.replace("to", "");
+    
+                    Event NewEvent = new Event(description, start, end);
+                    storage.storeTask(NewEvent);
+                    String echoResponse = "____________________________________________________________ \n"
+                            + "Got it. I've added this task: \n"
+                            + storage.lastTask().toString() + "\n"
+                            + storage.getLength() + "\n"
+                            + "____________________________________________________________ \n";
+                    System.out.println(echoResponse);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    //Handle invalid inputs
+                    System.out.println("Event creation should be in this format: event <Description> /from <Start> /to <End>");
+                }
             } else {
-                System.out.println("Sorry User, Alpha only accepts, todo, deadline, event, list, bye, mark and unmark commands");
+                System.out.println("Sorry User, command is not understood." +
+                        " Alpha only accepts, todo, deadline, event, list, bye, mark and unmark commands");
             }
             
         }
