fb06fc88f8ff76a23e93e9b5f8d93bef05d6c5df
xinweichong <127038769+xinweichong@users.noreply.github.com>
2024-08-22 13:41:06 +0800

    Changed Talker.java to echo inputs from user and exit when user types command "bye"

--- src/main/java/Talker.java
+++ src/main/java/Talker.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Talker {
     public static void main(String[] args) {
         String name = "Talker";
@@ -6,6 +8,21 @@ public class Talker {
         System.out.printf("Hello! I'm %s\n", name);
         System.out.println("What can I do for you?");
         System.out.println(line);
+
+        Scanner reader = new Scanner(System.in);
+
+        while (true) {
+            String input = reader.next();
+
+            System.out.println(line);
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.print(line);
     }
