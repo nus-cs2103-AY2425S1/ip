4c63fa73ca6f4d7466ed1300a8abd297c1b9eb84
CreditableX <creditablecreates@gmail.com>
2024-08-22 17:12:56 +0800

    user interaction and bye command

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
+        String blankline = "____________________________________________________________ \s";
         String skeleton = """
                 ____________________________________________________________ \s
                  Hello! I'm Bob \s
                  What can I do for you? \s
                 ____________________________________________________________ \s
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________ \s
                 """;
         System.out.println(skeleton);
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean exit = false;
+        while (!exit) {
+            System.out.print("Text: ");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                exit = true;
+            } else {
+                System.out.println("Bob: " + input + "\n" + blankline);
+            }
+        }
+        System.out.println(blankline + "\n" + "Bye. Hope to see you again soon!");
+
     }
 }
