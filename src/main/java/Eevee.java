5e194faba9dacc417a700a80739bda931de83fb9
myrilla12 <myrilla12@gmail.com>
2024-08-22 13:11:09 +0800

    Update Eevee to exit on bye

--- src/main/java/Eevee.java
+++ src/main/java/Eevee.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Eevee {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
         String divider = "____________________________________________________________\n";
         String greeting = "Hello! I'm Eevee\nWhat can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
@@ -9,13 +11,13 @@ public class Eevee {
         System.out.println(divider + greeting + divider);
 
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
             String input = scanner.nextLine();
             System.out.println(divider);
 
             if (input.equals("bye")) {
                 System.out.println(exit);
+                break;
             } else {
                 System.out.println(input);
             }
