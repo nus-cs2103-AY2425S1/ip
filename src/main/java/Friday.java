ac742d86d30ed7c3635a931b08a1ff89cb65df55
blanklogic <koh.junxian.jaymeson@icloud.com>
2024-08-22 15:23:49 +0800

    Level 5. Implemented Error Handling, Phased out legacy Level 2 for Level 4 implementation of Task list, Added 'help' for list of commands.

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -9,9 +9,21 @@ public class Friday {
     }
     private void welcomeMessage() { // Level 0
         horizontalLine();
-        System.out.println("\tWelcome Back! I'm Friday");
+        System.out.println("\tWelcome Back! I'm Friday.");
         System.out.println("\tWhat can I do for you today?");
+        System.out.println("\tTo view the list of commands that I support, type 'help' for more information.");
         horizontalLine();
+    }
+    private void help() {
+        System.out.println("\tHere are the list of commands that I support");
+        System.out.println("\thelp - List of commands supported by me, Friday.");
+        System.out.println("\tlist - View all entries to the current list of things you have asked me to take note of.");
+        System.out.println("\tmark <index> - Mark an entry in the list as a completed task.");
+        System.out.println("\tunmark <index> - Unmark an entry in the list as a completed task.");
+        System.out.println("\ttodo <description> - Remember a TODO Task for you to revisit again.");
+        System.out.println("\tdeadline <description> /by <when> - Remember a Deadline Task for you to complete by the deadline.");
+        System.out.println("\tevent <description> /from <when> /to <when> - Remember an Event Task from when it begins to when it ends.");
+        System.out.println("\tbye - Exits this app and says Good Bye to Friday :)");
     }
     private void readInput() {
         Scanner sc = new Scanner(System.in);
@@ -22,7 +34,11 @@ public class Friday {
             input = sc.nextLine();
             horizontalLine();
             if (Objects.equals(input, "bye")) break; // Level 1
-            if (Objects.equals(input, "list")) { // Level 2
+            if (Objects.equals(input, "help")) {
+                help();
+                horizontalLine();
+            }
+            else if (Objects.equals(input, "list")) { // Level 2
                 System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
                     System.out.println("\t" + items[i].toPrettyString());
@@ -30,38 +46,49 @@ public class Friday {
                 horizontalLine();
             } else {
                 inputs = input.split(" ");
-                if (Objects.equals(inputs[0], "mark")) { // Level 3
-                    items[Integer.parseInt(inputs[1]) - 1].markAsDone();
+                try {
+                    if (inputs.length == 1)
+                        throw new FridayException("Invalid input. Please ensure that this command is supported by me and you have utilized the right syntax.\n\tCheck 'help' for more information."); // Commands that are not recognized or incorrect usage of commands
+                    if (Objects.equals(inputs[0], "mark") || Objects.equals(inputs[0], "unmark")) { // Level 3
+                        markUnmark(inputs);
+                        horizontalLine();
+                        continue;
+                    } else if (Objects.equals(inputs[0], "todo")) { // Level 4, Improvement from Level 2
+                        currTask = new Todo(input.substring(5));
+                    } else if (Objects.equals(inputs[0], "deadline")) { // Level 4
+                        inputs = input.substring(9).split(" /by ");
+                        if (inputs.length == 1) throw new FridayException("Invalid input. usage: deadline <description> /by <when>.");
+                        currTask = new Deadline(inputs[0], inputs[1]);
+                    } else if (Objects.equals(inputs[0], "event")) { // Level 4
+                        inputs = input.substring(6).split(" /from | /to ");
+                        if (inputs.length <= 2) throw new FridayException("Invalid input. usage: event <description> /from <when> /to <when>.");
+                        currTask = new Event(inputs[0], inputs[1], inputs[2]);
+                    } else {
+                        throw new FridayException("Invalid input. Please ensure that this command is supported by me and you have utilized the right syntax.\n\tCheck 'help' for more information."); // Commands that are not recognized or incorrect usage of commands
+                    }
+                    items[index++] = currTask;
+                    System.out.println("\tGot it. I've added this task:");
+                    System.out.println("\t  " + currTask);
+                    System.out.println("\tNow you have " + index + " tasks in the list.");
                     horizontalLine();
-                    continue;
-                } else if (Objects.equals(inputs[0], "unmark")) { // Level 3
-                    items[Integer.parseInt(inputs[1]) - 1].unmarkAsDone();
+                } catch (FridayException e) {
+                    System.out.println("\t" + e.getMessage());
                     horizontalLine();
-                    continue;
-                } else if (Objects.equals(inputs[0], "todo")) { // Level 4
-                    currTask = new Todo(input.substring(5));
-                } else if (Objects.equals(inputs[0], "deadline")) { // Level 4
-                    inputs = input.substring(9).split(" /by ");
-                    currTask = new Deadline(inputs[0], inputs[1]);
-                } else if (Objects.equals(inputs[0], "event")) { // Level 4
-                    inputs = input.substring(6).split(" /from | /to ");
-                    currTask = new Event(inputs[0], inputs[1], inputs[2]);
-                } else { // Level 2
-                    currTask = new Task(input);
-                    System.out.println("\tadded: " + input);
                 }
-                items[index++] = currTask;
-                if (currTask.getClass() == Task.class) {
-                    horizontalLine();
-                    continue;
-                }
-                System.out.println("\tGot it. I've added this task:");
-                System.out.println("\t  " + currTask);
-                System.out.println("\tNow you have " + index + " tasks in the list.");
-                horizontalLine();
             }
         }
     }
+    private void markUnmark(String[] inputs) throws FridayException {
+        if (!inputs[1].chars().allMatch(Character::isDigit)) throw new FridayException("Invalid input. Where would you like to " + inputs[0] + "?"); // Marking a non-digit
+        int target = Integer.parseInt(inputs[1]);
+        if (target > index || target <= 0) throw new FridayException("Invalid input. It appears you are attempting to access something that does not exist yet."); // OutOfBoundsException
+        if (Objects.equals(inputs[0], "mark")) {
+            items[target - 1].markAsDone();
+        } else {
+            items[target - 1].unmarkAsDone();
+        }
+    }
+
     private void exit() {
         System.out.println("\tGood Bye. Hope to see you again soon!");
         horizontalLine();
---
+++ src/main/java/FridayException.java
@@ -0,0 +1,5 @@
+public class FridayException extends RuntimeException {
+    public FridayException(String msg) {
+        super(msg);
+    }
+}
