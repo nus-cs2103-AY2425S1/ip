b41d2c2ab8f8050a5d430f402a7f9c576e07dd3e
Bryan <bryanchenyu616@gmail.com>
2024-08-22 14:46:27 +0800

    Change name and greeting message

---
+++ src/main/java/Crack.java
@@ -0,0 +1,12 @@
+public class Crack {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n"
+        + " Hello! I'm Crack\n"
+        + " What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + " Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________";
+
+        System.out.println(greeting);
+    }
+}
