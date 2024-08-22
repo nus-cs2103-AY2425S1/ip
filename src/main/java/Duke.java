353c60cba9d035c0d6356c000279ed4735cfc630
bbryant824 <bowenbryant24@gmail.com>
2024-08-22 13:27:19 +0800

    lv 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void main(String[] args) {
@@ -6,8 +8,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello Kobe! \n" + logo);
+        
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+        
+            userInput = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
     }
 }
