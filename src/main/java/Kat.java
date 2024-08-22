ab6c948f12f768184547bae012d40860e86c79d8
Airiinnn <alex.liswandy@hotmail.com>
2024-08-22 21:31:08 +0800

    Support echoing responses

--- src/main/java/Kat.java
+++ src/main/java/Kat.java
@@ -1,22 +1,39 @@
+import java.util.Scanner;
+
 public class Kat {
+  private static final String LOGO = """
+       _         _  \s
+      | | __ ___| |_\s
+      | |/ / _  | __|
+      |   < (_| | |_\s
+      |_|\\_\\____|\\__|
+      """;
+
+  private static final Scanner scanner = new Scanner(System.in);
+
   public static void main(String[] args) {
-    String logo = """
-         _         _  \s
-        | | __ ___| |_\s
-        | |/ / _  | __|
-        |   < (_| | |_\s
-        |_|\\_\\____|\\__|
-        """;
-    System.out.println("Hello from\n" + logo);
+    System.out.println("Hello from\n" + LOGO);
 
     respond("Hi! I'm Kat.\nHow can I help?");
+
+    while (true) {
+      System.out.println("> Me");
+
+      String inputMsg = scanner.nextLine().trim();
+      if (inputMsg.equalsIgnoreCase("bye")) {
+        break;
+      }
+
+      respond(inputMsg);
+    }
+
     respond("See you!");
   }
 
-  private static void respond(String message) {
+  private static void respond(String responseMsg) {
     System.out.println("~".repeat(50));
     System.out.println("> Kat");
-    System.out.println(message);
+    System.out.println(responseMsg);
     System.out.println("~".repeat(50));
   }
 }
