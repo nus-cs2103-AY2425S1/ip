0374320bf1fda98a835bf9c29fb978e7497bdcb6
miloaisdino <miloaisdino@gmail.com>
2024-08-23 01:28:58 +0800

    feat: Echo commands and exit upon "bye"

--- src/main/java/IpMan.java
+++ src/main/java/IpMan.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class IpMan {
+    public static final String SEPARATOR = "____________________________________________________________";
     public static void main(String[] args) {
-        System.out.println("Hello from Ip Man!");
+        System.out.println(SEPARATOR);
+        System.out.println("Hello from Ip Man!\n What can I do for you?");
+        System.out.println(SEPARATOR);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println(SEPARATOR);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(SEPARATOR);
+                break;
+            }
+            System.out.println(SEPARATOR);
+            System.out.println(line);
+            System.out.println(SEPARATOR);
+        }
     }
 }
