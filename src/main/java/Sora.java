4a8621dd19788499ff630c0a6b51286186d63eec
S-K-Y-Light <s84915656@gmail.com>
2024-08-23 00:02:52 +0800

    Implement Todo, Deadline & Event

--- src/main/java/Sora.java
+++ src/main/java/Sora.java
@@ -20,15 +20,29 @@ public class Sora {
                 taskList.get(Integer.parseInt(command[1]) - 1).setDone(true);
                 System.out.println("\tNice! Sora has marked this task as done:");
                 System.out.println("\t" + taskList.get(Integer.parseInt(command[1]) - 1));
-            } else if (command[0].equalsIgnoreCase("unmark")) {
+            } else if (command[0].equalsIgnoreCase("unmark") && command.length == 2) {
                 taskList.get(Integer.parseInt(command[1]) - 1).setDone(false);
                 System.out.println("\tOk, Sora has unmarked this task as not done:");
                 System.out.println("\t" + taskList.get(Integer.parseInt(command[1]) - 1));
+            } else if (command[0].equalsIgnoreCase("todo") && command.length == 2) {
+                taskList.add(new ToDo(command[1]));
+                System.out.println("\tGot it. Sora has added this task:");
+                System.out.println("\t" + taskList.get(taskList.size() - 1));
+            } else if (command[0].equalsIgnoreCase("deadline") && command.length == 2) {
+                String[] deadlineInformation = command[1].trim().split(" /by ", 2);
+                taskList.add(new Deadline(deadlineInformation[0], deadlineInformation[1]));
+                System.out.println("\tGot it. Sora has added this task:");
+                System.out.println("\t" + taskList.get(taskList.size() - 1));
+            } else if (command[0].equalsIgnoreCase("event") && command.length == 2) {
+                String eventFrom = command[1].trim().split(" /from ", 2)[1].split(" /to", 2)[0];
+                String eventTo = command[1].trim().split("/to ", 2)[1];
+                taskList.add(new Event(command[0], eventFrom, eventTo));
+                System.out.println("\tGot it. Sora has added this task:");
+                System.out.println("\t" + taskList.get(taskList.size() - 1));
             } else {
-                String description = command[0] + (command.length == 2 ? " " + command[1] : "");
-                taskList.add(new Task(description));
-                System.out.println("\t added: " + description);
+                // TODO
             }
+            System.out.println("\tNow, you have " + taskList.size() + " tasks in your list");
             System.out.println(horizontalLine);
         }
 
