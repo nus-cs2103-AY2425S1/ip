5698847f95dde48bc76573c6098fb7723636d70c
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-08-22 17:25:52 +0800

    Level 3, Created new class Task and mark as done feature

--- src/main/java/Lama.java
+++ src/main/java/Lama.java
@@ -7,7 +7,7 @@ public class Lama {
         Scanner scanner = new Scanner(System.in);
 
         String bar = "____________________________________________________________";
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
 
         System.out.println(bar);
@@ -17,31 +17,49 @@ public class Lama {
 
         while(scanner.hasNextLine()) {
             String input = scanner.nextLine();
+            String[] words = input.split(" ");
 
-            if (input.equals("list")) {
+            if (words[0].equals("bye")) {
                 System.out.println(bar);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(bar);
+                break;
+            } else if (words[0].equals("list")) {
+                System.out.println(bar);
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                    System.out.println((i + 1) + "." + list[i]);
+//                    System.out.println(String.format("%d. [%s] %s", i + 1, list[i].getStatusIcon(), list[i]));
                 }
                 System.out.println(bar);
-
-                continue;
+            } else if (words[0].equals("mark")){
+                int n = Integer.parseInt(words[1]) - 1;
+                list[n].markAsDone();
+                System.out.println(bar);
+                System.out.println("Nice! I've marked this task as done:");
+//                System.out.println("  [" + list[n].getStatusIcon() + "] " + list[n]);
+                System.out.println("  " + list[n]);
+                System.out.println(bar);
+            } else if (words[0].equals("unmark")){
+                int n = Integer.parseInt(words[1]) - 1;
+                list[n].markAsUnDone();
+                System.out.println(bar);
+                System.out.println("OK, I've marked this task as not done yet:");
+//                System.out.println("  [" + list[n].getStatusIcon() + "] " + list[n]);
+                System.out.println("  " + list[n]);
+                System.out.println(bar);
             }
 
-            if (!input.equals("bye")) {
-                list[count] = input;
+            else {
+                Task task = new Task(input);
+                list[count] = task;
                 count++;
                 System.out.println(bar);
                 System.out.println("added: " + input);
                 System.out.println(bar + "\n");
             }
 
-            if (input.equals("bye")) {
-                System.out.println(bar);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(bar);
-                break;
-            }
+
         }
 
         scanner.close();
