8216fd06d8fac00f79630baf48c3696e1af92cad
jason-chuah <chuahchonghee2019@gmail.com>
2024-08-22 13:12:36 +0800

    Change Duke.java

---
+++ src/main/java/Park.java
@@ -0,0 +1,8 @@
+public class Park {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hello! I'm Park
+                What can I do for you?
+                Bye. Hope to see you again soon!""");
+    }
+}
