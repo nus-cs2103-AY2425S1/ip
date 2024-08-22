072dda7f56d8e2c3b0f93f05bdca2c98ea307f80
YangQF2002 <yangqf18@gmail.com>
2024-08-22 19:34:41 +0800

    Add echo feature

--- src/main/java/Brock.java
+++ src/main/java/Brock.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 public class Brock {
+    private static final String horizontalLine = "________________________________________________\n";
+
+    // Helper function to display response with lines
+    private static void displayResponse(String response) {
+        System.out.println(Brock.horizontalLine + response + Brock.horizontalLine);
+    }
     public static void main(String[] args) {
-        String line1 = "Hello! I'm Brock";
-        String line2 = "What can I do for you?";
-        String line3 = "Bye. Hope to see you again soon!";
-        System.out.println(line1 + '\n' + line2 + '\n' + line3 + '\n');
+        // Create a scanner object
+        // Reads from standard system input
+        Scanner scanner = new Scanner(System.in);
+
+        // Initial message
+        String initialPart1 = "Hello! I'm Brock\n";
+        String initialPart2 = "What can I do for you?\n";
+        String initial = initialPart1 + initialPart2;
+        displayResponse(initial);
+
+        // Main loop
+        while (true) {
+            String command = scanner.nextLine() + '\n';
+            if (command.equals("bye\n")) {
+                String byeMessage = "Bye. Hope to see you again soon!\n";
+                displayResponse(byeMessage);
+                break;
+            } else {
+                displayResponse(command);
+            }
+        }
     }
 }
 
