7138744fd9d824e12b70b12819ade2265f4d87a8
xczheng <zhengxc105@gmail.com>
2024-08-23 03:09:47 +0800

    add support for echo, design improvements

--- src/main/java/Trackie.java
+++ src/main/java/Trackie.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class Trackie {
-    public static void main(String[] args) {
+
+    public void greet() {
         System.out.println("Hello, I'm Trackie. Nice to meet you =)");
+        System.out.println("Note: type \"bye\" to exit.");
+    }
+
+    public void exit() {
         System.out.println("Seeya!");
+    }
+
+    public void echo(String input) {
+        System.out.println(input);
+    }
+
+    public static void main(String[] args) {
+        Trackie bot = new Trackie();
+        bot.greet();
+        while (true) {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Type something: ");
+            String userInput = s.nextLine();
+            if (userInput.equals("bye")) {
+                bot.exit();
+                break;
+            }
+            bot.echo(userInput);
+        }
+
+
+
+
     }
 }
