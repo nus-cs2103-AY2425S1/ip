871aac7aa2831d834a4a1174aff75f51019a7bc8
xinweichong <127038769+xinweichong@users.noreply.github.com>
2024-08-22 15:12:01 +0800

    Amended Talker.java to support new subclasses of Task class

--- src/main/java/Talker.java
+++ src/main/java/Talker.java
@@ -29,25 +29,49 @@ public class Talker {
             // if command "list" entered, print all tasks in list
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
+
                 for (int i = 0; i < pointer; i++) {
                     System.out.printf("%d.%s\n", i + 1, list[i]);
                 }
-                System.out.println(line);
-            // if first word after parsing is mark, mark task as complete
-            } else if (parsed[0].equals("mark")) {
+            // if first word is mark, mark task as complete
+            } else if (input.startsWith("mark")) {
                 System.out.println(list[Integer.parseInt(parsed[1]) - 1].mark());
-            // if first word after parsing is unmark, mark task as incomplete
-            } else if (parsed[0].equals("unmark")){
+            // if first word is unmark, mark task as incomplete
+            } else if (input.startsWith("unmark")){
                 System.out.println(list[Integer.parseInt(parsed[1]) - 1].unmark());
             // Save input as new task
             } else {
-                list[pointer] = new Task(input);
-                pointer++;
-                System.out.printf("added: %s\n", input);
-                System.out.println(line);
-            }
-        }
+                System.out.println("Got it. I've added this task:");
 
+                Task newTask;
+
+                if (input.startsWith("todo")) {
+                    String[] split = input.split(" ", 2);
+
+                    newTask = new ToDo(split[1]);
+                } else if (input.startsWith("deadline")) {
+                    String[] split1 = input.split(" ", 2);
+                    String[] split2 = split1[1].split(" /by ");
+
+                    newTask = new Deadline(split2[0], split2[1]);
+                } else if (input.startsWith("event")) {
+                    String[] split1 = input.split(" ", 2);
+                    String[] split2 = split1[1].split(" /from ");
+                    String[] split3 = split2[1].split(" /to ");
+
+                    newTask = new Event(split2[0], split3[0], split3[1]);
+                } else {
+                    newTask = new Task(input);
+                }
+
+
+                list[pointer] = newTask;
+                System.out.println(newTask);
+                pointer++;
+                System.out.printf("Now you have %d tasks in the list. \n", pointer);
+            }
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.print(line);
     }
