c462a1ba228915cfc16574cdf1f0bbe2d899e2f0
waihin26 <138366109+waihin26@users.noreply.github.com>
2024-08-22 22:38:01 +0800

    Implemented scanner such that chatbot echos user

--- src/main/java/Blue.java
+++ src/main/java/Blue.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
 public class Blue {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("--------------------------------------------" );
-        System.out.println("Hello! I'm Blue" );
-        System.out.println("What can I do for you?" );
-        System.out.println("--------------------------------------------" );
-        System.out.println("Bye. Hope to see you again soon!" );
-        System.out.println("--------------------------------------------" );
-        //testing
+        // Greet the user
+        System.out.println("--------------------------------------------");
+        System.out.println("Hello! I'm Blue");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------------------");
+
+        String input = "";
+
+        // Keep echoing user input until "bye" is typed
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(input);
+            System.out.println("--------------------------------------------");
+        }
+
+        // Farewell message
+        System.out.println("I'm sorry I was mimicking you! Bye Bye! Hope to see you again soon!");
+        System.out.println("--------------------------------------------");
+
+        // Close the scanner
+        scanner.close();
     }
 }
