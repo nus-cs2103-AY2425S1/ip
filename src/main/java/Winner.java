3d61608c0326b8a3f3b44884d0d348b1382a1af1
yuancheng <>
2024-08-22 13:45:59 +0800

    level 0 with horizontal lines

--- src/main/java/Winner.java
+++ src/main/java/Winner.java
@@ -1,17 +1,13 @@
 public class Winner {
 
-    public static String greetUser() {
-        return "Hi! I am Winner.\n" +
-                "What can I do for you today?";
-    }
-
-    public static String exit() {
-        return "Hope I have been of service. Goodbye and see you again!";
-    }
-
-
     public static void main(String[] args) {
-        System.out.println(greetUser());
-        System.out.println(exit());
+        System.out.println("--------------------------------------\n" +
+                           "Hello! I am Winner.\n" +
+                           "What can I do for you today?\n" +
+                           "--------------------------------------");
+        System.out.println("--------------------------------------\n" +
+                           "Hope I have been of service!\n" +
+                           "Thank you and see you again soon :D\n" +
+                           "--------------------------------------");
     }
 }
