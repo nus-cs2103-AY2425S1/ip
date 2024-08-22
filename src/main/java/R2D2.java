84eed37aef668ab740a22f2d5b4dc5828706f679
marraknas <sankarram2002ybis@gmail.com>
2024-08-22 18:22:35 +0800

    added Echo

--- src/main/java/R2D2.java
+++ src/main/java/R2D2.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class R2D2 {
     public static void main(String[] args) {
         String hline = "____________________________________________________________";
         System.out.println(hline);
-        System.out.println("Hello! I'm R2D2!");
+        System.out.println("Hello! I'm R2D2! *Beep* *Boop*");
         System.out.println("What can I do for you?");
         System.out.println(hline);
-        System.out.println("Bye. Hope to see you again soon!");
+
+
+        Scanner reader = new Scanner(System.in);
+        String userInput = reader.next();
+        while (!userInput.equals("bye")) {
+            System.out.println(hline);
+            System.out.println(userInput);
+            System.out.println(hline);
+            userInput = reader.next();
+        }
+        System.out.println(hline);
+        System.out.println("Bye. Hope to see you again soon! *bzzzt*");
         System.out.println(hline);
     }
 }
