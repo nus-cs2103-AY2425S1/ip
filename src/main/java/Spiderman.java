426269b3a6fc3217feabebdc1568b4f8fe076534
Rachael-Chan <e0775723@u.nus.edu>
2024-08-22 17:43:23 +0800

    feat: Add support for adding lists

--- src/main/java/Spiderman.java
+++ src/main/java/Spiderman.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList; // import the ArrayList class
 
 public class Spiderman {
     public static void main(String[] args) {
         // Initialise scanner
         Scanner scan = new Scanner(System.in);  // Create a Scanner object
+        ArrayList<String> tasks = new ArrayList<String>(); // Create an ArrayList object
 
         // Greeting users
         System.out.println("Hello! This is your friendly neighbourhood Spiderman.");
@@ -13,12 +15,23 @@ public class Spiderman {
             // Get users input
             String input = scan.nextLine();
 
-            // User says bye; program terminates
+            // User types bye; program terminates
             if (input.equals("bye")) {
                 break;
             }
 
-            System.out.println(input);
+            // User types list; display list
+            if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + tasks.get(i));
+                }
+                continue;
+            }
+
+            // Store users input into tasks
+            tasks.add(input);
+
+            System.out.println("added: " + input);
         }
 
         // Exit program message
