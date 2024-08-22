322943aeb002e421acb9c3097e5d0f153fc2e02b
tanwartushar <>
2024-08-22 22:27:48 +0800

    class renamed, greeting added

---
+++ src/main/java/Donna.java
@@ -0,0 +1,20 @@
+public class Donna {
+    static private void printDonnaLogo() {
+        System.out.println(" ____      ");
+        System.out.println("|  _ \\  ___  _ __  _ __   __ _ ");
+        System.out.println("| | | |/ _ \\| '_ \\| '_ \\ / _` |");
+        System.out.println("| |_| | (_) | | | | | | | (_| |");
+        System.out.println("|____/ \\___/|_| |_|_| |_|\\__,_|");
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        printDonnaLogo();
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Donna");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
