66213c9e78d5d914cb24dddfcc223f1d77f979d9
JellyPenguinnn <>
2024-08-22 22:41:54 +0800

    Level-0

---
+++ java/ChatBuddy.java
@@ -0,0 +1,9 @@
+public class ChatBuddy {
+    public static void main(String[] args) {
+         System.out.println("_____________________________________________");
+         System.out.println("Hello! I'm ChatBuddy.\nLet me know how can I assist you today?");
+         System.out.println("_____________________________________________");
+         System.out.println("Bye. Hope to see you again soon!");
+         System.out.println("_____________________________________________");
+    }
+}
