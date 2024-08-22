9267431d85b93d6e464a2b0d599264f237805b74
MingYiAw <awmingyi@gmail.com>
2024-08-23 01:40:11 +0800

    add Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public void markAsNotDone(){
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString(){
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
 }
