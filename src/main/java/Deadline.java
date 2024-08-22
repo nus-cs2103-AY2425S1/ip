158d56b38c344ef7207bbcc3397b3e17994c7b3a
vinceg4 <vincegoh04@gmail.com>
2024-08-22 19:53:12 +0800

    Modify toString() methods

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,6 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         //return "[D] " + super.toString() + " (by: " + this.deadline + ")";
-        return String.format("[D] %s (by: %s)", super.toString(), this.deadline);
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
     }
 }
