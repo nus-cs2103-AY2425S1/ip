b15c234487e4e62f20820558f150583450e8e321
teamotitan <timothy.tanzijing@gmail.com>
2024-08-22 10:03:12 +0800

    Level-2

--- src/main/java/Timo.java
+++ src/main/java/Timo.java
@@ -2,22 +2,47 @@ import java.util.Scanner;
 
 public class Timo {
     public static void main(String[] args) {
-        System.out.println("----------------------------\n");
-        System.out.println("Hello! I'm Timo\nWhat can I do for you?\n");
+        System.out.println("----------------------------");
+        System.out.println("Hello! I'm Timo\nWhat can I do for you?");
+        System.out.println("----------------------------");
 
+        //Scanner to receive input
         Scanner echo = new Scanner(System.in);
+
+        //initialise array to store the values
+        String[] arr = new String[100];
+        int index = 1;
+
+
         String input = "";
-        while (true) {
+
+        //boolean to know whether input = bye
+        boolean is_bye = false;
+        while (!is_bye) {
             input = echo.nextLine();
-            if (!input.equals("bye")) {
-                System.out.println("----------------------------\n");
-                System.out.println(input);
-                System.out.println("----------------------------\n");
-            } else {
-                System.out.println("----------------------------\n");
-                System.out.println("Bye. Hope to see you again soon!\n");
-                System.out.println("----------------------------\n");
-                break;
+            switch (input) {
+                case "bye":
+                    System.out.println("----------------------------");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("----------------------------");
+                    is_bye = true;
+                    break;
+
+                case "list":
+                    System.out.println("----------------------------");
+                    for (int i = 1; i < index; i++) {
+                        System.out.println(i + ". " + arr[i - 1]);
+                    }
+                    System.out.println("----------------------------");
+                    break;
+
+                default:
+                    System.out.println("----------------------------");
+                    System.out.println("added: " + input);
+                    System.out.println("----------------------------");
+                    arr[index - 1] = input;
+                    index++;
+                    break;
             }
         }
 
