496bfa61748dab08df7b38ccdb0231e33c48c361
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 01:13:50 +0800

    Edit task manager: change elem type from string to task

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,22 +1,23 @@
 public class TaskManager {
-    private String[] tasks = new String[100];
+    private Task[] tasks = new Task[100];
     private int counter = 0;
 
-    public String addTask(String task) {
+    public String addTask(String taskDescription) {
         if (counter < tasks.length) {
-            tasks[counter++] = task;
-            return "added: " + task;
+            tasks[counter] = new Task(taskDescription);
+            counter++;
+            return "added: " + taskDescription;
         }
         return "Task list is full, unfortunately ~";
     }
 
     public String listTasks() {
         if (counter == 0) {
-            return "You haven't add any task yet! :L";
+            return "You haven't added any task yet! :L";
         }
-        StringBuilder list = new StringBuilder();
+        StringBuilder list = new StringBuilder("Here are the tasks in your list:\n");
         for (int i = 0; i < counter; i++) {
-            list.append(i + 1).append(". ").append(tasks[i]).append("\n");
+            list.append(i + 1).append(". ").append(tasks[i].toString()).append("\n");
         }
         return list.toString();
     }
