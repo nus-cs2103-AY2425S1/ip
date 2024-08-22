87029108440f26b1a69837369934b2ca98b55aef
xinweichong <127038769+xinweichong@users.noreply.github.com>
2024-08-22 14:24:29 +0800

    Changed Talker.java to utilise new Task class in Task.java

--- src/main/java/Talker.java
+++ src/main/java/Talker.java
@@ -11,22 +11,37 @@ public class Talker {
 
         Scanner reader = new Scanner(System.in);
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int pointer = 0;
 
         while (true) {
+            // read user input
             String input = reader.nextLine();
 
+            // parse the input by " "
+            String[] parsed = input.split(" ");
+
             System.out.println(line);
+
+            // if command "bye" entered, exit
             if (input.equals("bye")) {
                 break;
+            // if command "list" entered, print all tasks in list
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < pointer; i++) {
-                    System.out.printf("%d. %s\n", i+1, list[i]);
+                    System.out.printf("%d.%s\n", i + 1, list[i]);
                 }
                 System.out.println(line);
+            // if first word after parsing is mark, mark task as complete
+            } else if (parsed[0].equals("mark")) {
+                System.out.println(list[Integer.parseInt(parsed[1]) - 1].mark());
+            // if first word after parsing is unmark, mark task as incomplete
+            } else if (parsed[0].equals("unmark")){
+                System.out.println(list[Integer.parseInt(parsed[1]) - 1].unmark());
+            // Save input as new task
             } else {
-                list[pointer] = input;
+                list[pointer] = new Task(input);
                 pointer++;
                 System.out.printf("added: %s\n", input);
                 System.out.println(line);
@@ -37,3 +52,4 @@ public class Talker {
         System.out.print(line);
     }
 }
+
