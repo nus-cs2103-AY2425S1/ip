1964a5f06eb6f1a7803aa974ede9c1770107b01c
Clarenceeey <clarencey100@gmail.com>
2024-08-22 16:33:21 +0800

    ToDo now throws exception

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -2,8 +2,11 @@ package main.java;
 
 public class ToDo extends Task{
 
-    public ToDo(String name) {
-        super(name);
+    public ToDo(String inputStr) throws InvalidTaskNameException {
+        String name = inputStr.substring(5);
+        if (name.length() == 0) {
+            throw new InvalidTaskNameException(name);
+        }
     }
 
     @Override
