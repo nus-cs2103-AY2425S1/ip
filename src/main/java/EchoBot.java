61d50300bb4598dbffb34192d8b04c2e17afbe79
JJtan2002 <tanjiajun2002@gmail.com>
2024-08-22 22:50:16 +0800

    Add storing and displaying text

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class EchoBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm EchoBot");
@@ -15,9 +17,17 @@ public class EchoBot {
                 System.out.println(" Bye! Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
+            } else if (userInput.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" " + userInput);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + userInput);
                 System.out.println("____________________________________________________________");
             }
         }
