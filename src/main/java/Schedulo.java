0b76ad62a2a552503d50b7cf22d72c361432b777
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-08-22 10:17:06 +0800

    different types of tasks added, level 4 done

--- src/main/java/Schedulo.java
+++ src/main/java/Schedulo.java
@@ -17,21 +17,9 @@ public class Schedulo {
                 System.out.println(horizontalLine);
                 break;
             }
-            if (command.equals("list")) {
-                System.out.println(taskList);
-            } else {
-                String[] splitwords = command.split(" ");
-                String newcommand = splitwords[0];
-                if (newcommand.equals("mark")) {
-                    int index = Integer.valueOf(splitwords[1]);
-                    System.out.println(taskList.markTask(index));
-                } else if (newcommand.equals("unmark")) {
-                    int index = Integer.valueOf(splitwords[1]);
-                    System.out.println(taskList.unmarkTask(index));
-                } else {
-                    System.out.println(taskList.add(command));
-                }
-            }
+
+            taskList.executeCommand(command);
+            
             System.out.println(horizontalLine);
         }
 
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -8,22 +8,63 @@ public class TaskList {
         this.tasklist = new ArrayList<>();
     }
 
-    public String add(String task) {
-        Task newTask = new Task(task);
-        this.tasklist.add(newTask);
-        return "Added: " + task;
+    public void executeCommand(String command) {
+        if (command.equals("list")) {
+            this.list();
+        } else {
+            String[] splitwords = command.split(" ", 2);
+            String firstCommand = splitwords[0];
+            if (firstCommand.equals("mark")) {
+                int index = Integer.valueOf(splitwords[1]);
+                this.markTask(index);
+            } else if (firstCommand.equals("unmark")) {
+                int index = Integer.valueOf(splitwords[1]);
+                this.unmarkTask(index);
+            } else if (firstCommand.equals("deadline")) {
+                String[] taskparts = splitwords[1].split(" /by ", 2);
+                String name = taskparts[0];
+                String deadline = taskparts[1];
+                Task task = new Deadline(name, deadline);
+                this.add(task);
+            } else if (firstCommand.equals("event")) {
+                String[] taskParts = splitwords[1].split(" /from ", 2);
+                String[] deadlines = taskParts[1].split(" /to ", 2);
+                String name = taskParts[0];
+                String from = deadlines[0];
+                String to = deadlines[1];
+                Task task = new Event(name, from, to);
+                this.add(task);
+            } else if (firstCommand.equals("todo")) {
+                String name = splitwords[1];
+                Task task = new Todo(name);
+                this.add(task);
+            } else {
+                System.out.println("Invalid command, please try again");
+            }
+        }
     }
 
-    public String markTask(int index) {
+    public void list() {
+        System.out.println(this.toString());
+    }
+
+    private void add(Task task) {
+        this.tasklist.add(task);
+        System.out.println("Got it. I've added this task: ");
+        System.out.println(task);
+        System.out.println("Now you have " + this.tasklist.size() + " tasks in the list.");
+    }
+
+    private void markTask(int index) {
         Task task = this.tasklist.get(index-1);
         task.mark();
-        return " Nice! I've marked this task as done: \n" + task;
+        System.out.println(" Nice! I've marked this task as done: \n" + task);
     }
 
-    public String unmarkTask(int index) {
+    private void unmarkTask(int index) {
         Task task = this.tasklist.get(index-1);
         task.unmark();
-        return "OK, I've marked this task as not done yet: \n" + task;
+        System.out.println("OK, I've marked this task as not done yet: \n" + task);
     }
 
     @Override
