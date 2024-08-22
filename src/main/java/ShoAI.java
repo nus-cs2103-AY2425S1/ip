8230ea1ae4fecba5c8c4390024aa8ade291c577f
Shoujin <sugimotoshoujin@gmail.com>
2024-08-22 15:25:38 +0800

    Rename, Greet, Exit

---
+++ src/main/java/ShoAI.java
@@ -0,0 +1,14 @@
+public class ShoAI {
+
+    public static void main(String[] args) {
+        // Print a greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm ShoAI");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Print a farewell message
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
