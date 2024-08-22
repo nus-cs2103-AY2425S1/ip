375b08dfa1285345fccf26eeac8c5e2715fd1ce8
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-08-23 04:53:52 +0800

    Level 3. Mark as Done

---
+++ AI.md
@@ -0,0 +1,3 @@
+i used chatgpt for:
+
+a small part in Task 3
--- src/main/java/meowmeow.java
+++ src/main/java/meowmeow.java
@@ -9,20 +9,44 @@ public class meowmeow {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
         System.out.println("Hello! I'm meowmeow\n" + "What can I do for you?\n");
-        LinkedList<String> list = new LinkedList<>();
+        LinkedList<Task> list = new LinkedList<>();
         Scanner s = new Scanner(System.in);
-        int i = 0;
         String input = s.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                list.forEach(System.out::println);
+                // Print all tasks
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + "." + list.get(i));
+                }
+            } else if (input.startsWith("mark ")) {
+                // Mark a task as done
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                if (taskNumber >= 0 && taskNumber < list.size()) {
+                    list.get(taskNumber).markDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + list.get(taskNumber));
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (input.startsWith("unmark ")) {
+                // Unmark a task (mark it as not done)
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                if (taskNumber >= 0 && taskNumber < list.size()) {
+                    list.get(taskNumber).unMark();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + list.get(taskNumber));
+                } else {
+                    System.out.println("Invalid task number.");
+                }
             } else {
+                // Add a new task to the list
+                Task t = new Task(input);
+                list.add(t);
                 System.out.println("added: " + input);
-                i++;
-                list.add(i + ". " + input);
             }
             input = s.nextLine();
         }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
