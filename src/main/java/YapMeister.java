fa7fc79986d36d562baa728e4e73a65af1a875ac
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-08-22 13:50:15 +0800

    Renamed Duke to YapMeister and edited greeting

---
+++ src/main/java/YapMeister.java
@@ -0,0 +1,15 @@
+public class YapMeister {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                               Hello I'm YapMeister
+                               YAPYAPYAPYAP
+                               Bye
+                               """);
+    }
+}
