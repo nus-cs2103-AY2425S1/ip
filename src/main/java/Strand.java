d6d995406d7d1421a0b7a1d29def763f2097e72e
FionaQY <fionaandfauna@gmail.com>
2024-08-22 14:05:34 +0800

    Added Echoing and improved formating

--- src/main/java/Strand.java
+++ src/main/java/Strand.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 public class Strand {
     private static boolean running = false;
     private static final String horizontalLine = "----------------------------------------><>";
     private static void print(String s) {
-        System.out.println(s);
+        System.out.println(s.indent(4));
     }
     private static void output(String s) {
+        System.out.println(horizontalLine.indent(4));
         print(s);
-        System.out.println(horizontalLine);
+        System.out.println(horizontalLine.indent(4));
     }
     private static void chatStart() {
         running = true;
@@ -14,25 +16,30 @@ public class Strand {
                  ____ _____ ____      _    _   _ ____ \s
                 / ___|_   _|  _ \\    / \\  | \\ | |  _ \\ \s
                 \\___ \\ | | | |_) |  / _ \\ |  \\| | | | |\s
-                 ___) || | |  _ <  / ___ \\| |\\  | |_| |\s
+                 _) || | |  _ <  / _ \\| |\\  | |_| |\s
                 |____/ |_| |_| \\_\\/_/   \\_\\_| \\_|____/\s
                 """;
         output("Hello from \n" + nameLogo + "\nWhat can I do for you?");
     }
     private static void inputs(String input) {
         switch (input.toLowerCase()) {
-
-            default : {
+            case "bye": {
                 output("Bye. Hope to see you again soon!");
                 running = false;
                 break;
+            }
+
+            default : {
+                output(input);
             }
         }
     }
     public static void main(String[] args) {
         chatStart();
         while(running) {
-            inputs("");
+            Scanner scan = new Scanner(System.in);
+            String userInput = scan.next();
+            inputs(userInput);
         }
     }
 }
