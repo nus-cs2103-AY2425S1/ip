8641f44ac45a3e46c504e741f12e827e90889fb3
CreditableX <creditablecreates@gmail.com>
2024-08-22 16:48:48 +0800

    added level 0

---
+++ src/main/java/Bob.java
@@ -0,0 +1,13 @@
+public class Bob {
+    public static void main(String[] args) {
+        String skeleton = """
+                ____________________________________________________________ \s
+                 Hello! I'm Bob \s
+                 What can I do for you? \s
+                ____________________________________________________________ \s
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________ \s
+                """;
+        System.out.println(skeleton);
+    }
+}
