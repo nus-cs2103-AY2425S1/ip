3d797c5f6478903942e5dbf9d82818e796bed022
wubojin <wubojin64@gmail.com>
2024-08-23 01:24:35 +0800

    Level 3: Mark as Done

--- src/main/java/Jade.java
+++ src/main/java/Jade.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jade {
     public static void main(String[] args) {
@@ -8,8 +9,8 @@ public class Jade {
         String greet = indent + "Hello! I'm Jade!\n"
                 + indent + "What can I do for you?";
         String exit = indent + "Bye. Hope to see you again soon!";
-        String list = ""; // initial list
-        int num = 0; // number of texts
+        String listMessage = indent + "Here are the tasks in your list:";
+        ArrayList<Task> tasks = new ArrayList<>();
         String message;
 
         System.out.println(topLine + greet + botLine);
@@ -19,16 +20,42 @@ public class Jade {
 
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                System.out.println(topLine + list + botLine);
+                System.out.println(topLine + listMessage);
+                for (int i = 0; i < tasks.size(); i++) {
+                    if (i < tasks.size() - 1) {
+                        System.out.println(indent + (i + 1) + "." + tasks.get(i));
+                    } else {
+                        // last task
+                        System.out.println(indent + (i + 1) + "." + tasks.get(i)
+                                + botLine);
+                    }
+                }
+                command = sc.nextLine();
+            } else if (command.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                    tasks.get(taskIndex).markAsDone();
+                    message = indent + "Nice! I've marked this task as done:"
+                            + "\n" + indent + "  " + tasks.get(taskIndex);
+                } else {
+                    message = indent + "Hmm, no such task. Try again.";
+                }
+                System.out.println(topLine + message + botLine);
+                command = sc.nextLine();
+            } else if (command.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                    tasks.get(taskIndex).markAsNotDone();
+                    message = indent + "OK, I've marked this task as not done yet:"
+                            + "\n" + indent + "  " + tasks.get(taskIndex);
+                } else {
+                    message = indent + "Hmm, no such task. Try again.";
+                }
+                System.out.println(topLine + message + botLine);
                 command = sc.nextLine();
             } else {
-                num++;
-
-                if (num == 1) {
-                    list += indent + num + ". " + command;
-                } else {
-                    list += "\n" + indent + num + ". " + command;
-                }
+                Task newTask = new Task(command);
+                tasks.add(newTask);
 
                 message = indent + "added: " + command;
                 System.out.println(topLine + message + botLine);
