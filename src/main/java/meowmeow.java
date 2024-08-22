31ef303694267d9ad0b2d7cbb56bcaa0837761ef
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-08-23 04:21:58 +0800

    Level 2. Add, List

--- src/main/java/meowmeow.java
+++ src/main/java/meowmeow.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 public class meowmeow {
     public static void main(String[] args) {
@@ -8,10 +9,18 @@ public class meowmeow {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
         System.out.println("Hello! I'm meowmeow\n" + "What can I do for you?\n");
+        LinkedList<String> list = new LinkedList<>();
         Scanner s = new Scanner(System.in);
+        int i = 0;
         String input = s.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                list.forEach(System.out::println);
+            } else {
+                System.out.println("added: " + input);
+                i++;
+                list.add(i + ". " + input);
+            }
             input = s.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
