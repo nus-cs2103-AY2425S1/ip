a6c437849152091616cf8135181bfa7ab184ddb1
QinHaichen12 <haichensurfs@gmail.com>
2024-08-22 14:59:20 +0800

    Level 0

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -1,10 +1,5 @@
 public class Bottle {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
         String logo = "____________________________________________________________\n" +
                 " Hello! I'm Bottle\n" +
                 " What can I do for you?\n" +
