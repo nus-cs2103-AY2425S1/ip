e84467612e82bb4bca628afb70bd168af219134a
QuekBingHeng <quekbingheng.17@gmail.com>
2024-08-22 20:31:23 +0800

    add Level-2, Angel.java

--- src/main/java/Angel.java
+++ src/main/java/Angel.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Angel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String logo = "____________________________________________________________\n";
 
         System.out.println(logo + " Hello! I'm Angel\n What can I do for you?\n" + logo);
@@ -13,8 +16,16 @@ public class Angel {
             if (userInput.equals("bye")) {
                 System.out.println(logo + " Bye. Hope to see you again soon!\n" + logo);
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println(logo);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(logo);
             } else {
-                System.out.println(logo + " " + userInput + "\n" + logo);
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println(logo + " added: " + userInput + "\n" + logo);
             }
         }
 
