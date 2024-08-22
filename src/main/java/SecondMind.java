86a626f5939c3c7ab7c08a9a0b4593d664096402
Chan Guan Yu <changuanyu2904@gmail.com>
2024-08-22 16:34:33 +0800

    Create function to greet user

--- src/main/java/SecondMind.java
+++ src/main/java/SecondMind.java
@@ -5,6 +5,13 @@ public class SecondMind {
         System.out.println(line);
     }
 
+    private static void greetUser() {
+        printLineSeparator();
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        printLineSeparator();
+    }
+
     public static void main(String[] args) {
         String logo = "SecondMind";
     }
