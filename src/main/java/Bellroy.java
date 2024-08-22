8f8f737ad07fe33018624f80ea27813c39615d47
SherChew27 <shermanczj@gmail.com>
2024-08-22 23:34:57 +0800

    Add Rename, Greet, Exit

---
+++ src/main/java/Bellroy.java
@@ -0,0 +1,11 @@
+public class Bellroy {
+    public static void main(String[] args) {
+        String message = "____________________________________________________________\n" +
+                " Hello! I'm Bellroy\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(message);
+    }
+}
