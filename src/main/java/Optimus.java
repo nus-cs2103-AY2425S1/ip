ce176d0506d798272b09de5a3561f0b8361d1130
Rithik <rithiklenin@gmail.com>
2024-08-23 02:24:40 +0800

    added the next level features for file

--- src/main/java/Optimus.java
+++ src/main/java/Optimus.java
@@ -1,7 +1,17 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Optimus {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Optimus");
         System.out.println("What can I do for you?");
+        Scanner stringScanner = new Scanner(System.in);
+        while (true) {
+            String text = stringScanner.next();
+            if (text.equals("bye")){
+                break;
+            }
+            System.out.println(text);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
