c2050ad9457fd1669a878c2043e3e88f2048f278
Liu Yinze <>
2024-08-22 17:36:11 +0800

    Change file name to Genji.java

---
+++ src/main/java/Genji.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Genji {
+    private static String NAME = "Genji";
+    private static String LINE = "__________________________";
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean flag = true;
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    public static void echo(String echo) {
+        System.out.println(echo);
+        System.out.println(LINE);
+    }
+
+    public static void run() {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm " + NAME + "\nWhat can I do for you?");
+        System.out.println(LINE);
+        while (flag) {
+            String input = scanner.nextLine();
+            System.out.println(LINE);
+            if (input.equals("bye")) {
+                flag = false;
+            }
+            else {
+                echo(input);
+            }
+        }
+        bye();
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
