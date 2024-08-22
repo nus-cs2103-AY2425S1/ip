638d5d50883df797c00bbd511ca3bb1262dbb581
yixianggg <e1186210@u.nus.edu>
2024-08-22 15:57:08 +0800

    added add and list commands

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -9,11 +9,21 @@ public class Bob {
         System.out.printf("Hello! I'm %s!\n", name);
         System.out.println("What can I do for you?");
         System.out.println(line);
+        String[] list = new String[100];
+        int index = 0;
         Scanner scanner = new Scanner(System.in);
         String word = scanner.nextLine();
         while (!word.equals("bye")) {
             System.out.println(line);
-            System.out.println(word);
+            if (word.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(list[i]);
+                }
+            } else {
+                list[index] = String.valueOf(index + 1) + ". " + word;
+                System.out.printf("added: %s\n", word);
+                index++;
+            }
             System.out.println(line);
             word = scanner.nextLine();
         }
