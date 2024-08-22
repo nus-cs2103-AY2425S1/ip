057335ab1a3a68227d262bdd020132cb879ef2b2
CreditableX <creditablecreates@gmail.com>
2024-08-22 22:18:23 +0800

    created task subclasses

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+    protected String fromTo;
+
+    public Event(String description, String fromTo) {
+        super(description);
+        this.fromTo = fromTo;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (" + this.fromTo + ")";
+    }
+}
