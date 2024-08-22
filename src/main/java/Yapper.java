14550a2972c23e8e6d7390f3441547dec1162252
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-08-22 15:04:51 +0800

    Level-1: Implement echo feature and exit on 'bye' command

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class Yapper {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Yapper");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        
-        // Logic to wait for user input would go here
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
