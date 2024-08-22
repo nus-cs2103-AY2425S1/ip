cca7c182c43d7755092c69d49785d135422128e1
clarud <clarencejw@gmail.com>
2024-08-23 02:49:58 +0800

    Level-5

--- src/main/java/Weeny.java
+++ src/main/java/Weeny.java
@@ -15,89 +15,79 @@ public class Weeny {
             String input = user_input.nextLine();
             String command = firstword(input);
 
-            if (input.equals("list")) {
-                line();
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < data.size(); i++) {
-                    System.out.println((i + 1) + ". " + data.get(i).toString());
-                }
-                line();
-            } else if (input.equals("bye")) {
-                farewell = true;
-            } else if (command.equals("mark")) {
-                int val = endnum(input) - 1;
-                if (val >= data.size() || val < 0) {
-                    System.out.println("Task does not exist.");
-                } else {
+            try {
+                if (input.equals("list")) {
+                    line();
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < data.size(); i++) {
+                        System.out.println((i + 1) + ". " + data.get(i).toString());
+                    }
+                    line();
+                } else if (input.equals("bye")) {
+                    farewell = true;
+                } else if (command.equals("mark")) {
+                    int val = endnum(input) - 1;
+                    if (val >= data.size() || val < 0) {
+                        throw new IndexOutOfBoundsException("Just a reminder. You can't mark tasks that don't exist!");
+                    }
                     data.get(val).mark();
                     line();
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println(data.get(val).toString());
                     line();
-                }
-            } else if (command.equals("unmark")) {
-                int val = endnum(input) - 1;
-                if (val >= data.size() || val < 0) {
-                    System.out.println("Task does not exist.");
-                } else {
+                } else if (command.equals("unmark")) {
+                    int val = endnum(input) - 1;
+                    if (val >= data.size() || val < 0) {
+                        throw new IndexOutOfBoundsException("Just a reminder. You can't mark tasks that don't exist!");
+                    }
                     data.get(val).unmark();
                     line();
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println(data.get(val).toString());
                     line();
-                }
-            } else if (command.equals("todo")) {
-                if (input.length() <= 5) {
-                    System.out.println("The description of a todo cannot be empty.");
-                } else {
+                } else if (command.equals("todo")) {
+                    if (input.length() <= 5) {
+                        throw new IllegalArgumentException("Are you going to add nothing as your To-Do? >:(");
+                    }
                     line();
                     Task current = new Todo(input.substring(5).trim());
                     data.add(current);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(current.toString());
-                    System.out.println("Now you have " + data.size() + " tasks in the list.");
+                    System.out.println("Now you have " + data.size() + " tasks in the list");
                     line();
-                }
-            } else if (command.equals("event")) {
-                if (input.length() <= 6 || !input.contains("/from") || !input.contains("/to")) {
-                    System.out.println("Event must contain /from and /to.");
-                } else {
-                    String[] eventDetails = input_event(input);
-                    Task current = new Events(input.substring(6, input.indexOf("/from")).trim(), eventDetails[0], eventDetails[1]);
+                } else if (command.equals("event")) {
+                    if (input.length() <= 6 || !input.contains("/from") || !input.contains("/to")) {
+                        throw new IllegalArgumentException("What kind of event has no name or time? >:(");
+                    }
+                    Task current = new Events(input.substring(6, input.indexOf("/from") - 1).trim(),
+                            input_event(input)[0],
+                            input_event(input)[1]);
                     data.add(current);
                     line();
                     System.out.println("Got it. I've added this task:");
                     System.out.println(current.toString());
                     System.out.println("Now you have " + data.size() + " tasks in the list.");
                     line();
-                }
-            } else if (command.equals("deadline")) {
-                if (input.length() <= 9 || !input.contains("/by")) {
-                    System.out.println("Deadline must contain /by.");
-                } else {
-                    Task current = new Deadlines(input.substring(9, input.indexOf("/by")).trim(), input_deadline(input));
+                } else if (command.equals("deadline")) {
+                    if (input.length() <= 9 || !input.contains("/by")) {
+                        throw new IllegalArgumentException("What is a deadline without a date or a Task? >:(");
+                    }
+                    Task current = new Deadlines(input.substring(9, input.indexOf("/by") - 1).trim(),
+                            input_deadline(input));
                     data.add(current);
                     line();
                     System.out.println("Got it. I've added this task:");
                     System.out.println(current.toString());
                     System.out.println("Now you have " + data.size() + " tasks in the list.");
                     line();
-                }
-            } else if (command.equals("delete")) {
-                int val = endnum(input) - 1;
-                if (val >= data.size() || val < 0) {
-                    System.out.println("Task does not exist.");
                 } else {
-                    Task temp = data.get(val);
-                    data.remove(val);
-                    line();
-                    System.out.println("Spooof! The task magically disappeared");
-                    System.out.println(temp.toString());
-                    System.out.println("Now you have " + data.size() + " tasks in the list.");
-                    line();
+                    throw new UnsupportedOperationException("Oopsies we don't understand that command");
                 }
-            } else {
-                System.out.println("Unknown command.");
+            } catch (IllegalArgumentException | IndexOutOfBoundsException | UnsupportedOperationException e) {
+                line();
+                System.out.println("Error: " + e.getMessage());
+                line();
             }
         }
         line();
