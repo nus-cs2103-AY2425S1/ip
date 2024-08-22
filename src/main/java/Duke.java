e2735dab40199208b5f92c057a5cceb43f399dc1
Vicelya Visakha <>
2024-08-22 10:17:15 +0800

    Change Duke class name
    Duke class name changed to Tira
    String logo description changed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,6 @@
-public class Duke {
+public class Tira {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "TIRAMISU THE CAFE CAT (TIRA)"
         System.out.println("Hello from\n" + logo);
     }
 }
