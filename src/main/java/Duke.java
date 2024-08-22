964695c849be631906268412ca51600fe223165e
FlyingDonkeys <puppylongears@gmail.com>
2024-08-23 00:42:46 +0800

    level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 public class Duke {
 
-    private String username;
+    private static String username;
     public static void main(String[] args) {
         System.out.println("Hello I'm Bonnie, what is your name?");
-        Scanner username = new Scanner(System.in);
-        System.out.println(String.format("Hey %s! Welcome to the Bonnie chat bot!", username.nextLine()));
+        Scanner scanner_obj = new Scanner(System.in);
+        String my_username = scanner_obj.nextLine();
+        System.out.println(String.format("Hey %s! Welcome to the Bonnie chat bot!", my_username));
+        username = my_username;
+
+        while (true) {
+            Scanner repeated_scanner = new Scanner(System.in);
+            String input = repeated_scanner.nextLine();
+            if (input.equals("bye")) {
+                return;
+            } else {
+                System.out.println(String.format("Hey %s, I'm not sure what you meant by %s", username, input));
+            }
+        }
     }
+
 }
