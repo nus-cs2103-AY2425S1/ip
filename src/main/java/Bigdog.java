962370c29c71b8c64ce9ff92ea92c074dd855adc
weibinlim <limweibin0816@gmail.com>
2024-08-22 18:00:30 +0800

    Level 1: Echo

--- src/main/java/Bigdog.java
+++ src/main/java/Bigdog.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Bigdog {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println(STR."Hello from\n\{logo}");
-        System.out.println("Hello! I'm Bigdog.\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+
+        // Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Welcome message
+        System.out.println("Hello! I'm Bigdog!\n" + "What can I do for you?\n");
+
+        while (true) {
+
+            // Take in user input
+            String userInput = scanner.nextLine();
+
+            // Terminating condition
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.print("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            // Echo user input
+            System.out.println(userInput);
+        }
+
+        scanner.close();
     }
 }
