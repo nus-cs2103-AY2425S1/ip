31e8ef06f8e11502399be379f84afd3139ebfd27
Vicelya Visakha <>
2024-08-23 01:45:50 +0800

    Add Level-5 Error Handling
    Handle Error of:
    1. Not having the required number of words for the command.
    2. The command not in the command list

--- src/main/java/Tira.java
+++ src/main/java/Tira.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.lang.Exception;
 
 public class Tira {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TiraException {
         // variable declarations
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         PrintWriter printer = new PrintWriter(System.out);
@@ -18,63 +19,71 @@ public class Tira {
         while (true) {
             String command = scanner.nextLine();
             String[] splitCommand = command.split(" ");
-            if (command.equals("bye")) {
+            String firstWord = splitCommand[0];
+            if (command.equals("bye")) { //BYE
                 break;
-            } else { // check other commands
-                // LIST COMMAND
-                if (splitCommand[0].equals("list")) { // check if it wants to list down
-                    for (int i = 0; i < taskList.size(); i++) {
-                        Task currTask = taskList.get(i);
-                        printer.print((i + 1) + "." +
-                                currTask.getStatusIcon() +
-                                taskList.get(i).toString() + "\n");
-                        printer.flush();
-                    }
-                } else {
-                    String firstWord = splitCommand[0];
-                    // MARK OR UNMARK COMMAND
-                    if (firstWord.equals("mark")) {
-                        int currNum = Integer.parseInt(splitCommand[1]) - 1;
-                        taskList.get(currNum).markStatus();
-                        printer.print("NYA! Good job on this task: " + "\n" +
-                                taskList.get(currNum).toString() + "\n");
-                        printer.flush();
-                    } else if (firstWord.equals("unmark")) {
-                        int currNum2 = Integer.parseInt(splitCommand[1]) - 1;
-                        taskList.get(currNum2).unmarkStatus();
-                        printer.print("MRAWWW! Don't forget to return to this task: " + "\n" +
-                                taskList.get(currNum2).toString() + "\n");
-                        printer.flush();
-                    } else if (firstWord.equals("todo")) {//check if it is a todo task
-                        String description = "";
-                        for (int i = 1; i < splitCommand.length; i++) {
-                            description += (splitCommand[i]);
-                            if (i != splitCommand.length - 1) {
-                                description += " ";
-                            }
-                        }
-                        Task newTask = new ToDo(description);
-                        Tira.addTask(newTask, taskList);
-                    } else if (firstWord.equals("deadline")) { //
-                        String[] dateCommands = command.split("/");
-                        Task deadlineTask = new Deadline(dateCommands[0].substring(9), dateCommands[1]);
-                        Tira.addTask(deadlineTask, taskList);
-                    } else if (firstWord.equals("event")) {
-                        String[] dateCommands2 = command.split("/");
-                        Task eventTask = new Event(dateCommands2[0].substring(6), dateCommands2[1], dateCommands2[2]);
-                        Tira.addTask(eventTask, taskList);
-                    } else {
-                        Tira.addTask(new Task(command), taskList);
+            } else if (firstWord.equals("list")) { //LIST
+                for (int i = 0; i < taskList.size(); i++) {
+                    Task currTask = taskList.get(i);
+                    printer.print((i + 1) + "." +
+                            currTask.getStatusIcon() +
+                            taskList.get(i).toString() + "\n");
+                    printer.flush();
+                }
+            } else if (firstWord.equals("mark")) { //MARK
+                if (splitCommand.length < 2) {
+                    throw new TiraException("MRAW?? WHERE IS THE TASK?");
+                }
+                int currNum = Integer.parseInt(splitCommand[1]) - 1;
+                taskList.get(currNum).markStatus();
+                printer.print("NYA! Good job on this task: " + "\n" +
+                        taskList.get(currNum).toString() + "\n");
+                printer.flush();
+            } else if (firstWord.equals("unmark")) { //UNMARK
+                if (splitCommand.length < 2) {
+                    throw new TiraException("MRAW?? WHERE IS THE TASK?");
+                }
+                int currNum2 = Integer.parseInt(splitCommand[1]) - 1;
+                taskList.get(currNum2).unmarkStatus();
+                printer.print("MRAWWW! Don't forget to return to this task: " + "\n" +
+                        taskList.get(currNum2).toString() + "\n");
+                printer.flush();
+            } else if (firstWord.equals("todo")) {//todo
+                if (splitCommand.length < 2) {
+                    throw new TiraException("MRAW?? WHERE IS THE TASK?");
+                }
+                String description = "";
+                for (int i = 1; i < splitCommand.length; i++) {
+                    description += (splitCommand[i]);
+                    if (i != splitCommand.length - 1) {
+                        description += " ";
                     }
                 }
+                Task newTask = new ToDo(description);
+                Tira.addTask(newTask, taskList);
+            } else if (firstWord.equals("deadline")) { // DEADLINE
+                if (splitCommand.length < 2) {
+                    throw new TiraException("MRAW?? WHERE IS THE TASK?");
+                }
+                String[] dateCommands = command.split("/");
+                Task deadlineTask = new Deadline(dateCommands[0].substring(9), dateCommands[1]);
+                Tira.addTask(deadlineTask, taskList);
+            } else if (firstWord.equals("event")) { // EVENT
+                if (splitCommand.length < 2) {
+                    throw new TiraException("MRAW?? WHERE IS THE TASK?");
+                }
+                String[] dateCommands2 = command.split("/");
+                Task eventTask = new Event(dateCommands2[0].substring(6), dateCommands2[1], dateCommands2[2]);
+                Tira.addTask(eventTask, taskList);
+            } else {
+                throw new TiraException("MRA..OW? I think your brain is not braining. Rethink what you want of me...");
             }
+        }
             // NO SPECIFIC COMMAND
 //            Task task = new Task(command);
 //            printer.print("added: " + task.toString() + "\n");
 //            printer.flush();
 //            taskList.add(task);
-
-        }
         printer.println("Bye. Come back with treats, MIAO!");
         printer.close();
     }
---
+++ src/main/java/TiraException.java
@@ -0,0 +1,7 @@
+import java.lang.Exception;
+public class TiraException extends Exception{
+    public TiraException(String message) {
+        super(message);
+    }
+    // will revise the exceptions later
+}
