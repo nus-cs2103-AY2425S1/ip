599ad26cf4794d593d312931e38a0a4d7a525370
natlv <nathanlooenping@gmail.com>
2024-08-22 10:15:36 +0800

    Initial skeletal version that greets user

---
+++ src/main/java/Natsbot.java
@@ -0,0 +1,7 @@
+public class Natsbot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Natsbot\n" + "What can I do for you?\n");
+        System.out.println("Goodbye. Hope to see you again soon!");
+
+    }
+}
