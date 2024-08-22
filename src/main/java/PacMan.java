9ad0854cb61aea38d271b9667dcfb1bfcb69025a
hpcman <happypacman36@gmail.com>
2024-08-23 03:29:36 +0800

    Rename main class, add greeting and exiting

---
+++ src/main/java/PacMan.java
@@ -0,0 +1,15 @@
+public class PacMan {
+    public static void greet() {
+        System.out.println("Hello! I'm PacMan");
+        System.out.println("How can I help you?");
+    }
+
+    public static void exit() {
+        System.out.println("Good bye. Hope to see you soon!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+}
