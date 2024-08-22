b4107e5d7e415540cb0821db9a995b63b89142d1
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-08-22 10:57:00 +0800

    echo user input

--- src/main/java/Revir.java
+++ src/main/java/Revir.java
@@ -3,6 +3,15 @@ public class Revir {
         String name = "Revir";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+        // Echo user input until user types "bye"
+        while (true) {
+            String input = System.console().readLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            // Differentiate the print from the user input
+            System.out.println("You said: ");
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
