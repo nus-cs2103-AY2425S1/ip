1a2500f50d1026c407940659d937d86ae07a22f1
SherChew27 <shermanczj@gmail.com>
2024-08-22 23:57:59 +0800

    Add Echo

--- src/main/java/Bellroy.java
+++ src/main/java/Bellroy.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Bellroy {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         String message = "____________________________________________________________\n" +
                 " Hello! I'm Bellroy\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                "____________________________________________________________\n";
+
+        String userInput = "";
+
         System.out.println(message);
+        while (true) {
+
+            userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________\n");
+
+                break;
+            }
+
+            System.out.println("____________________________________________________________\n" +
+                    userInput + "\n" +
+                    "    ____________________________________________________________");
+        }
+
+        scanner.close();
+
     }
 }
