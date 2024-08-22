90e8a2925c701ad351a72fbcbef559645b996e79
awdse22 <ngkhengyang@gmail.com>
2024-08-22 17:57:10 +0800

    Add echo function and bye command

--- src/main/java/MrYapper.java
+++ src/main/java/MrYapper.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class MrYapper {
+    private static void say(String message) {
+        System.out.println("____________________________________________________________\n"
+                + message
+                + "\n____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                + " Hello! I'm MrYapper\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(greeting);
+        String greeting = " Hello! I'm MrYapper\n"
+                + " What can I do for you?";
+        String goodbye = " Bye. Hope to see you again soon!";
+        say(greeting);
+        boolean sayGoodbye = false;
+        Scanner userInputReader = new Scanner(System.in);
+
+        while (!sayGoodbye) {
+            String userInput = userInputReader.nextLine();
+            if (userInput.equals("bye")) {
+                sayGoodbye = true;
+                userInputReader.close();
+                say(goodbye);
+            } else {
+                say(" " + userInput);
+            }
+        }
     }
 }
