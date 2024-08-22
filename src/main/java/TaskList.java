3014b8d586dd6597ad2f14aa55db78e4a437eb23
tata32000 <tata32000@hotmail.com>
2024-08-22 16:28:56 +0800

    Rename items to tasks in TaskList.java

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    ArrayList<String> items;
+    ArrayList<Task> tasks;
 
     public TaskList() {
-        items = new ArrayList<>();
+        tasks = new ArrayList<>();
     }
 
     public void addItem(String item) {
-        items.add(item);
+        tasks.add(new Task(item));
     }
 
     public String getList() {
         StringBuilder list = new StringBuilder();
-        for (int i = 0; i < items.size(); i++) {
-            list.append(i + 1).append(". ").append(items.get(i)).append("\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            list.append(i + 1).append(". ").append(tasks.get(i)).append("\n");
         }
         return list.toString();
     }
