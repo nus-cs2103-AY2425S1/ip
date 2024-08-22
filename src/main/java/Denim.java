89cbfe243655a7ffb301eaf15af8acf34919aab0
t-leongchuan <t.leongchuan@gmail.com>
2024-08-22 22:46:40 +0800

    Add support for echo

--- src/main/java/Denim.java
+++ src/main/java/Denim.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Denim {
     public static void main(String[] args) {
 
         String horizontalLine = "____________________________________________________________";
         String chatBotName = "Denim";
-        String greetingMessage = String.format("%s%n Hello! I'm %s!%n What can I do for you? %n%s%n"
-                        + "Bye. Hope to see you again soon! %n%s",
-                horizontalLine, chatBotName, horizontalLine, horizontalLine);
+        String greetingMessage = String.format("%s%n Hello! I'm %s!%n What can I do for you? %n%s%n",
+                horizontalLine, chatBotName, horizontalLine);
 
         System.out.println(greetingMessage);
+
+        // Scans User Input in the CLI
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        // Echoes the input of the user until the command "bye" is given.
+        while (!input.equals("bye")) {
+            String echoMessage = String.format("%s%n %s%n%s", horizontalLine, input, horizontalLine);
+            System.out.println(echoMessage);
+            input = sc.next();
+        }
+
+        String byeMessage = String.format("%s%n %s%n%s", horizontalLine, "Bye. Hope to see you again soon!",
+                horizontalLine);
+        System.out.println(byeMessage);
+        sc.close();
     }
 }
 
