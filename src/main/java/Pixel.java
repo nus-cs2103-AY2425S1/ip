60085cf66e5b3ed1901d86c9ef77397877888956
Sornsornah <ejasonlek@gmail.com>
2024-08-22 21:01:07 +0800

    added exit

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -6,7 +6,13 @@ public class Pixel {
         System.out.println("What can I do for you?");
         System.out.println(LINE);
     }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
     public static void main(String[] args) {
         greeting();
+        exit();
     }
 }
