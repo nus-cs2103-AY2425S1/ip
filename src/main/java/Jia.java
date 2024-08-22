3ef4ad6f9cfe44b037b1bc9bb01cbeca2a685e47
jiayr99 <e1249294@u.nus.edu>
2024-08-22 14:51:07 +0800

    Added Echo

--- src/main/java/Jia.java
+++ src/main/java/Jia.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Jia {
     public static void main(String[] args) {
+        //Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        //Greet the user
         System.out.println("______________________________________________________");
         System.out.println("Hello! I'm Jia");
         System.out.println("What can I do for you?");
         System.out.println("______________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        //Read user input until they say bye
+        do {
+            input = scanner.nextLine();
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println("______________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("______________________________________________________");
+            }
+        } while (!input.equalsIgnoreCase("bye"));
+
+        //Exit
         System.out.println("______________________________________________________");
+        System.out.println(" " + "Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________________");
+
+        scanner.close();
     }
 }
