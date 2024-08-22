6aa6d5774e1e10e022ec357d8ebf7cc75d278399
Rachael-Chan <e0775723@u.nus.edu>
2024-08-22 14:29:10 +0800

    Added greeting and goodbye message

--- src/main/java/Spiderman.java
+++ src/main/java/Spiderman.java
@@ -1,10 +1,10 @@
 public class Spiderman {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greeting users
+        System.out.println("Hello! This is your friendly neighbourhood Spiderman");
+        System.out.println("What can I do for you?");
+
+        // Exit program
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
