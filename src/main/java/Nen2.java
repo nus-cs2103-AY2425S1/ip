427ea793ec1ef667c20934706b8bb7a0f51aa1fe
ny <ny@rks-Laptop.local>
2024-08-22 15:29:46 +0800

    Nen2.java: Add processMessage() method to process input of user
    Implement Echo feature

--- src/main/java/Nen2.java
+++ src/main/java/Nen2.java
@@ -1,30 +1,47 @@
+import java.util.Scanner;
+
 public class Nen2 {
-    public static String logo = " _   _                 __  \n"
+    private static String logo = " _   _                 __  \n"
             + "| \\ | |  ___  _ ___   |_  \\ \n"
             + "|  \\| | / _ \\| '__ |    ) |\n"
             + "| |\\  ||  __/| | | |   / /_ \n"
             + "|_| \\_| \\___||_| |_|  |____|\n";
-    public static String seperator = "--------------------------------------------\n";
+    private static String seperator = "--------------------------------------------";
+    private static Scanner messageReader = new Scanner(System.in);
 
     public static void main(String[] args) {
-        greet();
-        exit();
+        Nen2.greet();
+        Nen2.processMessage();
+        Nen2.exit();
+    }
+
+    public static void processMessage() {
+        String message = messageReader.nextLine();
+        System.out.println(Nen2.seperator);
+
+        if (message.equals("bye")) {
+            return;
+        } else {
+            System.out.println(message);
+        }
+        System.out.println(Nen2.seperator);
+        Nen2.processMessage();
     }
 
     /**
      * Greets user by printing out logo and greeting messages
      */
     public static void greet() {
-        System.out.print(Nen2.seperator);
-        System.out.print(logo + "Hello! I'm Nen2 \nWhat can I do for you?\n");
-        System.out.print(Nen2.seperator);
+        System.out.println(Nen2.seperator);
+        System.out.println(logo + "Hello! I'm Nen2 \nWhat can I do for you?");
+        System.out.println(Nen2.seperator);
     }
 
     /**
      * End the conversation with ending messages
      */
     public static void exit() {
-        System.out.print("Bye. Hope to see you again soon! \n");
-        System.out.print(Nen2.seperator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Nen2.seperator);
     }
 }
