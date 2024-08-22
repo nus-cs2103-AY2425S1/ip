d4ac0934284512b762fe0f44535abdb13d9c8096
Rachael-Chan <e0775723@u.nus.edu>
2024-08-22 15:21:13 +0800

    feat: Add support for echo

--- src/main/java/Spiderman.java
+++ src/main/java/Spiderman.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Spiderman {
     public static void main(String[] args) {
+        // Initialise scanner
+        Scanner scan = new Scanner(System.in);  // Create a Scanner object
+
         // Greeting users
-        System.out.println("Hello! This is your friendly neighbourhood Spiderman");
+        System.out.println("Hello! This is your friendly neighbourhood Spiderman.");
         System.out.println("What can I do for you?");
 
-        // Exit program
+        while (true) {
+            // Get users input
+            String input = scan.nextLine();
+
+            // User says bye; program terminates
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        // Exit program message
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
