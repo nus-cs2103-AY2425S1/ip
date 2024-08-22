f82578e64c267bee31e4cfc817ed3570d167da3f
keithxun <keithonghx@gmail.com>
2024-08-22 14:28:23 +0800

    Replace Duke with Bopes as new name for the project

---
+++ src/main/java/Bopes.java
@@ -0,0 +1,6 @@
+public class Bopes {
+    public static void main(String[] args) {
+        String intro = "Bopes is a personal assistant that helps you manage your tasks.";
+        System.out.println(intro);
+    }
+}
