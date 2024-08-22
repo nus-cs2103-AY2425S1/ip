05c70ec93ca26c5e0b1afbfd335b95eaed538bf2
psh12320 <pshricharan2020@gmail.com>
2024-08-23 01:04:13 +0800

    finished part of Level 2

--- src/main/java/Taskalyn.java
+++ src/main/java/Taskalyn.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Taskalyn {
     public static void main(String[] args) {
+
+        List<String> tasks = new ArrayList<String>(100);
 
         // Initialising scanner
         Scanner scanner = new Scanner(System.in);
@@ -20,8 +24,12 @@ public class Taskalyn {
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________\n");
                 break;
-            }
-            else {
+            } else if (input.equals("list")) {
+                for (int i=0; i < tasks.size(); i++) {
+                    System.out.println(i + "." + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    " + input);
                 System.out.println("    ____________________________________________________________\n");
