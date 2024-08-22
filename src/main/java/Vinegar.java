1cb30d8064e42872a5b4fe46c723e8f36ce70df9
limjeremy496 <limjeremy496@gmail.com>
2024-08-22 12:22:47 +0800

    Add support for echo

--- src/main/java/Vinegar.java
+++ src/main/java/Vinegar.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Vinegar {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Vinegar\n" +
                 "What Can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
