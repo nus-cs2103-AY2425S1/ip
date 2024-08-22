ff85a2a2a44d67794f432a2ba43370f0eed2e149
Ming Jun <evan.zhangmingjun@gmail.com>
2024-08-22 23:55:11 +0800

    Refactor command handling

--- src/main/java/ChadGPT.java
+++ src/main/java/ChadGPT.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChadGPT {
     private static final List<Task> tasks = new ArrayList<>();
@@ -11,42 +13,64 @@ public class ChadGPT {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            // TODO: Use regex for more advanced matching
-            if (input.startsWith("bye")) {
-                printBotMessage("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.startsWith("list")) {
-                printBotMessage("Here are the tasks in your list:\n" + Formatter.formatList(tasks));
-            } else if (input.startsWith("mark")) {
-                int i = getTaskIndex(input);
-                markTask(i);
-                printBotMessage("Nice! I've marked this task as done:\n" + tasks.get(i));
-            } else if (input.startsWith("unmark")) {
-                int i = getTaskIndex(input);
-                unmarkTask(i);
-                printBotMessage("OK, I've marked this task as not done yet:\n" + tasks.get(i));
-            } else {
-                addTaskToList(input);
-                printBotMessage("added: " + input);
-            }
+            handleInput(sc.nextLine());
         }
     }
 
-    private static void markTask(int index) {
-        tasks.get(index).markAsDone();
+    private static void handleInput(String input) {
+        Pattern regex = Pattern.compile("(\\w+)\\s*(.*)");
+        Matcher matcher = regex.matcher(input);
+        if (matcher.matches()) {
+            String command = matcher.group(1);
+            String args = matcher.group(2);
+
+            switch (command) {
+                case "list":
+                    handleList();
+                    break;
+                case "todo":
+                    handleAddTask(args);
+                    break;
+                case "mark":
+                    handleMarkTask(args);
+                    break;
+                case "unmark":
+                    handleUnmarkTask(args);
+                    break;
+                case "bye":
+                    printBotMessage("Bye. Hope to see you again soon!");
+                    System.exit(0);
+                default:
+                    // TODO? Possibly find a better way to handle this
+                    printBotMessage("Command not found");
+            }
+        } else {
+            // TODO: Throw exception
+        }
     }
 
-    private static void unmarkTask(int index) {
+    private static void handleList() {
+        printBotMessage("Here are the tasks in your list:\n" + Formatter.formatList(tasks));
+    }
+    private static void handleMarkTask(String args) {
+        int index = getTaskIndex(args);
+        tasks.get(index).markAsDone();
+        printBotMessage("Nice! I've marked this task as done:\n" + tasks.get(index));
+    }
+
+    private static void handleUnmarkTask(String args) {
+        int index = getTaskIndex(args);
         tasks.get(index).markAsIncomplete();
+        printBotMessage("OK, I've marked this task as not done yet:\n" + tasks.get(index));
     }
 
     private static int getTaskIndex(String input) {
-        return Integer.parseInt(input.split(" ")[1]) - 1;
+        return Integer.parseInt(input.split(" ")[0]) - 1;
     }
 
-    private static void addTaskToList(String desc) {
-        tasks.add(new Task(desc));
+    private static void handleAddTask(String args) {
+        tasks.add(new Task(args));
+        printBotMessage("added: " + args);
     }
 
     /**
