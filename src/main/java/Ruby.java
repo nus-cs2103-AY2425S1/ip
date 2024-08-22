d58b80e28bf21dc4887ea049686ced59bb79bcb6
adriantan101 <atjy102@gmail.com>
2024-08-22 15:29:53 +0800

    ruby now echoes commands and exits when using bye

--- src/main/java/Ruby.java
+++ src/main/java/Ruby.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Ruby {
     public static void main(String[] args) {
-        String message = "Hello! I'm Ruby\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!";
-        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String greeting = """
+                Hello! I'm Ruby
+                What can I do for you?
+                """;
+        System.out.println(greeting);
+
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(command);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
