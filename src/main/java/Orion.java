44c53905f2efe6a118ae13764602389412c33863
pradyuprasad <pradyu.sp@gmail.com>
2024-08-22 08:13:18 +0800

    Completed Level 4. ToDos, Events, Deadlines

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -40,11 +40,35 @@ public class Orion {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("  " + tasks[taskNumber]);
                 System.out.println(horizontalLine);
-            } else {
-                tasks[taskCount] = new Task(input);
+            } else if (parts[0].equals("todo")) {
+                tasks[taskCount] = new Todo(parts[1]);
                 taskCount++;
                 System.out.println(horizontalLine);
-                System.out.println("added: " + input);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(horizontalLine);
+            } else if (parts[0].equals("deadline")) {
+                String[] deadlineParts = parts[1].split(" /by ");
+                tasks[taskCount] = new Deadline(deadlineParts[0], deadlineParts[1]);
+                taskCount++;
+                System.out.println(horizontalLine);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(horizontalLine);
+            } else if (parts[0].equals("event")) {
+                String[] eventParts = parts[1].split(" /from | /to ");
+                tasks[taskCount] = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                taskCount++;
+                System.out.println(horizontalLine);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(horizontalLine);
+            } else {
+                System.out.println(horizontalLine);
+                System.out.println("I'm sorry, I don't understand that command.");
                 System.out.println(horizontalLine);
             }
         }
