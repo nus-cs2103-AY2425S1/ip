4acdb0b3f4b367db8d4f1c1d7613aae0294bb401
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 00:16:57 +0800

    Add constructor for the chatbot, message obj

--- src/main/java/Puke.java
+++ src/main/java/Puke.java
@@ -1,4 +1,8 @@
 public class Puke {
+    private MessageBuilder messageBuilder;
+    public Puke() {
+        this.messageBuilder = new MessageBuilder();
+    }
     public static void main(String[] args) {
         System.out.println("PUKE");
     }
