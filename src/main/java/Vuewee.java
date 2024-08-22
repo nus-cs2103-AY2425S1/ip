5be6f6bf717879f7fd7f6edc44971773f22a61ee
Jonathan Loh <jonathanloh02@gmail.com>
2024-08-22 17:14:55 +0800

    add functionality to add to list and display list

--- src/main/java/Vuewee.java
+++ src/main/java/Vuewee.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Vuewee {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    String[] taskList = new String[100];
+    int taskListIndex = 0;
 
     System.out.println("____________________________________________________________");
     System.out.println("Hello! I'm Vuewee\nWhat can I do for you?");
@@ -11,11 +13,30 @@ public class Vuewee {
     // Echo input from user until user types "bye"
     while (true) {
       String input = scanner.nextLine();
+
+      // Exit the loop if user types "bye"
       if (input.equals("bye")) {
         break;
       }
+
+      // List all tasks if user types "list"
+      if (input.equals("list")) {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < taskList.length; i++) {
+          if (taskList[i] != null) {
+            System.out.println(i + 1 + ". " + taskList[i]);
+          }
+        }
+        System.out.println("____________________________________________________________");
+        continue;
+      }
+
+      // Add task to task list
+      taskList[taskListIndex] = input;
+      taskListIndex++;
+
       System.out.println("____________________________________________________________");
-      System.out.println(input);
+      System.out.println("added: " + input);
       System.out.println("____________________________________________________________");
     }
 
