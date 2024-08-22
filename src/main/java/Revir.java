3246fa826d03abce95830a1e190f64d8e0492555
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-08-22 16:42:01 +0800

    added support for delete

--- src/main/java/Revir.java
+++ src/main/java/Revir.java
@@ -36,7 +36,7 @@ public class Revir {
             // Print the user input list
             System.out.println("List:");
             for (Task userInput : userInputList) {
-                System.out.println(userInput.toString());
+            System.out.println(userInput.toString());
             }
         } else if (command.startsWith("mark ")) {
             // Mark a task as completed
@@ -44,12 +44,12 @@ public class Revir {
 
             int taskIndex = Integer.parseInt(taskIndexStr);
             if (taskIndex >= 1 && taskIndex <= userInputList.size()) {
-                Task task = userInputList.get(taskIndex - 1);
-                task.setCompleted(true);
-                System.out.println("Task marked as completed: " + task.toString());
+            Task task = userInputList.get(taskIndex - 1);
+            task.setCompleted(true);
+            System.out.println("Task marked as completed: " + task.toString());
             } else {
-                throw new IndexOutOfBoundsException(
-                        "Invalid task index. Excepted index between 1 and " + userInputList.size());
+            throw new IndexOutOfBoundsException(
+                "Invalid task index. Expected index between 1 and " + userInputList.size());
             }
         } else if (command.startsWith("unmark ")) {
             // Unmark a completed task
@@ -57,18 +57,18 @@ public class Revir {
 
             int taskIndex = Integer.parseInt(taskIndexStr);
             if (taskIndex >= 1 && taskIndex <= userInputList.size()) {
-                Task task = userInputList.get(taskIndex - 1);
-                task.setCompleted(false);
-                System.out.println("Task unmarked as completed: \n" + task.toString());
+            Task task = userInputList.get(taskIndex - 1);
+            task.setCompleted(false);
+            System.out.println("Task unmarked as completed: \n" + task.toString());
             } else {
-                throw new IndexOutOfBoundsException(
-                        "Invalid task index. Excepted index between 1 and " + userInputList.size());
+            throw new IndexOutOfBoundsException(
+                "Invalid task index. Expected index between 1 and " + userInputList.size());
             }
         } else if (command.startsWith("todo ")) {
             // Add a todo task
             String taskDescription = command.substring(5);
             if (taskDescription.isEmpty()) {
-                throw new InvalidFormatException(Todo.format);
+            throw new InvalidFormatException(Todo.format);
             }
             userInputList.add(new Todo(taskDescription));
             System.out.println("Todo task added: " + taskDescription);
@@ -77,26 +77,38 @@ public class Revir {
             String taskDetails = command.substring(9);
             String[] taskInfo = taskDetails.split(" /by ");
             if (taskInfo.length == 2) {
-                String taskDescription = taskInfo[0];
-                String deadline = taskInfo[1];
-                userInputList.add(new Deadline(taskDescription, deadline));
-                System.out.println("Deadline task added: " + taskDescription + " (by: " + deadline + ")");
+            String taskDescription = taskInfo[0];
+            String deadline = taskInfo[1];
+            userInputList.add(new Deadline(taskDescription, deadline));
+            System.out.println("Deadline task added: " + taskDescription + " (by: " + deadline + ")");
             } else {
-                throw new InvalidFormatException(Deadline.format);
+            throw new InvalidFormatException(Deadline.format);
             }
         } else if (command.startsWith("event ")) {
             // Add an event task
             String taskDetails = command.substring(6);
             String[] taskInfo = taskDetails.split(" /from ");
             if (taskInfo.length == 2) {
-                String taskDescription = taskInfo[0];
-                String startDate = taskInfo[1].split(" /to ")[0];
-                String endDate = taskInfo[1].split(" /to ")[1];
-                userInputList.add(new Event(taskDescription, startDate, endDate));
-                System.out.println(
-                        "Event task added: " + taskDescription + " (from: " + startDate + " to: " + endDate + ")");
+            String taskDescription = taskInfo[0];
+            String startDate = taskInfo[1].split(" /to ")[0];
+            String endDate = taskInfo[1].split(" /to ")[1];
+            userInputList.add(new Event(taskDescription, startDate, endDate));
+            System.out.println(
+                "Event task added: " + taskDescription + " (from: " + startDate + " to: " + endDate + ")");
             } else {
-                throw new InvalidFormatException(Event.format);
+            throw new InvalidFormatException(Event.format);
+            }
+        } else if (command.startsWith("delete ")) {
+            // Delete a task
+            String taskIndexStr = command.substring(7);
+
+            int taskIndex = Integer.parseInt(taskIndexStr);
+            if (taskIndex >= 1 && taskIndex <= userInputList.size()) {
+            Task task = userInputList.remove(taskIndex - 1);
+            System.out.println("Task deleted: " + task.toString());
+            } else {
+            throw new IndexOutOfBoundsException(
+                "Invalid task index. Expected index between 1 and " + userInputList.size());
             }
         } else {
             throw new IllegalCommandException(command);
