0b1c210f706ffcff5208654eaa6ddcbd368746e3
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-08-22 13:37:26 +0800

    Level 0, change class name and greetings

---
+++ src/main/java/Lama.java
@@ -0,0 +1,12 @@
+public class Lama {
+    public static void main(String[] args) {
+        String bar = "____________________________________________________________";
+        System.out.println(bar);
+        System.out.println("Hello! I'm Lama");
+        System.out.println("What can I do for you?");
+        System.out.println(bar);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(bar);
+
+    }
+}
