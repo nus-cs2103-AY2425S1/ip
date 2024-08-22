b1265b4e92a179b46bc040aa8454fb344e01bf34
MITHUNA\10mit <e1155351@u.nus.edu>
2024-08-22 09:27:08 +0800

    Add repetition of userInput till user says bye Chatbot exits with exit message once user says bye

--- src/main/java/Flash.java
+++ src/main/java/Flash.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Flash {
     public static void main(String[] args) {
-        String line = "__________________________________________";
+        /* create an instance of scanner class */
+        Scanner scanner = new Scanner(System.in);
+
+        //Horizontal line for better readability
+        String line = "    __________________________________________";
+
         System.out.println(line);
-        System.out.println(" Hello! I'm Flash");
-        System.out.println(" What can I do for you?");
+        System.out.println("    Hey! I'm Flash");
+        System.out.println("    What can I do for ya?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        String userInput = scanner.nextLine();
+
+
+        while (!userInput.equals("bye")) {
+            //If user types anything other than Bye, repeat it
+            System.out.println(line);
+            System.out.println("    " + userInput);
+            System.out.println(line);
+            userInput = scanner.nextLine();
+        }
+
         System.out.println(line);
+        System.out.println("    Bye. Hope to see ya again soon!");
+        System.out.println(line);
+        scanner.close();
     }
 }
+
