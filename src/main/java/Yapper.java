06230564d929b0b8ad211d8a1dc5fcb0b1443a05
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-08-22 15:21:31 +0800

    Level-3: Add ability to mark tasks as done and not done

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -4,8 +4,8 @@ public class Yapper {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100]; // Array to store tasks
-        int taskCount = 0; // Counter to keep track of the number of tasks
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Yapper");
@@ -14,20 +14,37 @@ public class Yapper {
 
         while (true) {
             String userInput = scanner.nextLine();
+            String[] userInputParts = userInput.split(" ", 2);
+            String command = userInputParts[0];
 
-            if (userInput.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if (userInput.equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+            } else if (command.equals("mark")) {
+                int taskNumber = Integer.parseInt(userInputParts[1]) - 1;
+                tasks[taskNumber].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("____________________________________________________________");
+            } else if (command.equals("unmark")) {
+                int taskNumber = Integer.parseInt(userInputParts[1]) - 1;
+                tasks[taskNumber].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = userInput;
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + userInput);
