1e1846ac8a9ad679ce30829fa47f2acca0ae1dfe
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-23 00:36:40 +0800

    added Task class file

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+package main.java;
+
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String cross = this.isDone ? "X" : " ";
+        return String.format("[%s] %s", cross, this.name);
+    }
+}
