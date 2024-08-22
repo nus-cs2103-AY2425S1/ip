857f4069f84c4f273014aa4b83719298e82e627a
Zeepheru <changzeehang@gmail.com>
2024-08-22 14:49:21 +0800

    Add greeting and exit

--- src/main/java/ChatOutput.java
+++ src/main/java/ChatOutput.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ChatOutput {
+// import java.io.Console;
+
+/**
+ * class that handles text outputs for the chatbot.
+ */
+public class ChatOutput {
+    private static final String BORDER_LINE = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+
+    /**
+     * Takes in a text string, wraps it inside a pair of borderlines, and outputs it to the console
+     *
+     * @param text text string to be outputed.
+     */
+    public void writeText(String text) {
+        String output = BORDER_LINE + "\n" + text + "\n" + BORDER_LINE;
+
+        System.out.println(output);
+    }
 }
--- src/main/java/Torne.java
+++ src/main/java/Torne.java
@@ -1,10 +1,44 @@
-public class Duke {
+/**
+ * This is the main class for `Torne`.
+ * All the main commands are here.
+ */
+
+public class Torne {
+    private static final ChatOutput OUTPUT = new ChatOutput();
+
+    /**
+     * Shows a greeting message, to be shown when user initialises Torne.
+     */
+    private void greet() {
+        String greetingText = """
+Hello! I am
+  _______ ____  _____  _   _ ______\s
+ |__   __/ __ \\|  __ \\| \\ | |  ____|
+    | | | |  | | |__) |  \\| | |__  \s
+    | | | |  | |  _  /| . ` |  __| \s
+    | | | |__| | | \\ \\| |\\  | |____\s
+    |_|  \\____/|_|  \\_\\_| \\_|______|
+                                   \s
+short for Torment Nexus™, your F R I E N D L Y neighborhood chatbot :3
+
+How may I help you today?""";
+        OUTPUT.writeText(greetingText);
+    }
+
+    /**
+     * Prints a standard exit message and quits the bot.
+     */
+    private void exit() {
+        String exitText = """
+Aww, bye to you as well :c""";
+        OUTPUT.writeText(exitText);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Torne torne = new Torne();
+
+        // Level 0: Greet user and exit.
+        torne.greet();
+        torne.exit();
     }
 }
