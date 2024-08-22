e2e481c31c40c951dedcc552da87ad3ec7c54ff9
skyl3-r <skyler.ng.nyz@gmail.com>
2024-08-22 14:50:14 +0800

    Add support for echo

--- src/main/java/Barcus.java
+++ src/main/java/Barcus.java
@@ -1,9 +1,45 @@
+import java.util.Scanner;
+
 public class Barcus {
     public static void main(String[] args) {
-        String intro = "Beep bop! Hello I am Barcus, ready to be of assistance!\n";
+        String intro =
+"╭━━╮\n" +
+"┃╭╮┃\n" +
+"┃╰╯╰┳━━┳━┳━━┳╮╭┳━━╮\n" +
+"┃╭━╮┃╭╮┃╭┫╭━┫┃┃┃━━┫\n" +
+"┃╰━╯┃╭╮┃┃┃╰━┫╰╯┣━━┃\n" +
+"╰━━━┻╯╰┻╯╰━━┻━━┻━━╯\n" +
+                "Beep bop! Hello I am Barcus, ready to be of assistance!\n" +
+                "Write 'bye' to leave!\n";
         String goodbye = "Alright, good talk!\n";
 
-        System.out.println(intro + goodbye);
+        Scanner scanner = new Scanner(System.in);
+
+        // start
+        System.out.println(intro);
+
+        boolean exit = false;
+
+        while (!exit) {
+            String reply = receive(scanner);
+
+            if (reply.equals("bye")) {
+                exit = true;
+                talk(goodbye);
+            } else {
+                talk(reply);
+            }
+        }
+
+    }
+
+    private static void talk(String s) {
+        System.out.println("Barcus: " + s);
+    }
+
+    private static String receive(Scanner scanner) {
+        System.out.print("You: ");
+        return scanner.nextLine();
     }
 }
 
