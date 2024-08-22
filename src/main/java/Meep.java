6770496170b7e3e4cbcf07f190bae05195fd2bf3
tata32000 <tata32000@hotmail.com>
2024-08-22 11:40:32 +0800

    Add greeting

--- src/main/java/Meep.java
+++ src/main/java/Meep.java
@@ -1,10 +1,12 @@
 public class Meep {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                -----------------------------------------
+                Hello! I'm Meep
+                What can I do for you?
+                -----------------------------------------
+                Bye. Hope to see you again soon!
+                -----------------------------------------
+                """);
     }
 }
