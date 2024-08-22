9b1ad17b6ca8d076b74c13f14ae09d314361c7e8
limjeremy496 <limjeremy496@gmail.com>
2024-08-22 14:36:14 +0800

    Add Task class and support for mark as done/undone

--- src/main/java/Vinegar.java
+++ src/main/java/Vinegar.java
@@ -6,33 +6,60 @@ import java.util.Scanner;
 public class Vinegar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> userInputList = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Vinegar\n" +
                 "What Can I do for you?");
         System.out.println("____________________________________________________________");
 
+        label:
         while(true) {
             String userInput = scanner.nextLine();
+            String[] inputParts = userInput.split(" ", 2);
+            String instruction = inputParts[0].toLowerCase();
 
-            if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-            } else if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("____________________________________________________________");
-                for (int i = 0; i < userInputList.size(); i++) {
-                    System.out.println((i + 1) + ". " + userInputList.get(i));
+            switch (instruction) {
+                case "bye":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    break label;
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ".lol" + tasks.get(i));
+                    }
+                    System.out.println("____________________________________________________________");
+
+                    break;
+                case "mark": {
+                    int taskIndex = Integer.parseInt(inputParts[1]) - 1;
+                    tasks.get(taskIndex).markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks.get(taskIndex));
+                    System.out.println("____________________________________________________________");
+
+                    break;
                 }
-                System.out.println("____________________________________________________________");
+                case "unmark": {
+                    int taskIndex = Integer.parseInt(inputParts[1]) - 1;
+                    tasks.get(taskIndex).markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + tasks.get(taskIndex));
+                    System.out.println("____________________________________________________________");
 
-            } else {
-                userInputList.add(userInput);
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + userInput);
-                System.out.println("____________________________________________________________");
+                    break;
+                }
+                default:
+                    tasks.add(new Task(userInput));
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + userInput);
+                    System.out.println("____________________________________________________________");
+                    break;
             }
         }
 
