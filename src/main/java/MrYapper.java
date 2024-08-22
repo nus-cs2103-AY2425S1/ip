8e647bbeab0b47a3094ae41adab4c30236f03e28
awdse22 <ngkhengyang@gmail.com>
2024-08-22 20:19:41 +0800

    Re-organise code
    * Rename variables to follow java coding standard
    * Separate logical units and functions for enhanced readability

--- src/main/java/MrYapper.java
+++ src/main/java/MrYapper.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
 
 public class MrYapper {
+    private static final String GREETING_MESSAGE = " Hello! I'm MrYapper\n"
+            + " What can I do for you?";
+    private static final String GOODBYE_MESSAGE = " Bye. Hope to see you again soon!";
+
+    // Inserts line indentation in response messages
     private static void say(String message) {
         System.out.println("____________________________________________________________\n"
                 + message
                 + "\n____________________________________________________________");
     }
 
+    private static void echo(String str) {
+        say(" " + str);
+    }
+
     public static void main(String[] args) {
-        String greeting = " Hello! I'm MrYapper\n"
-                + " What can I do for you?";
-        String goodbye = " Bye. Hope to see you again soon!";
-        say(greeting);
-        boolean sayGoodbye = false;
+        say(GREETING_MESSAGE);
+        boolean conversationIsOngoing = true;
         Scanner userInputReader = new Scanner(System.in);
 
-        while (!sayGoodbye) {
+        do {
             String userInput = userInputReader.nextLine();
+
             if (userInput.equals("bye")) {
-                sayGoodbye = true;
+                conversationIsOngoing = false;
                 userInputReader.close();
-                say(goodbye);
+                say(GOODBYE_MESSAGE);
             } else {
-                say(" " + userInput);
+                echo(userInput);
             }
-        }
+        } while (conversationIsOngoing);
     }
 }
