b106d575ff2961a3a9ff3e8df5b4ef3de70cba10
keithxun <keithonghx@gmail.com>
2024-08-22 14:35:41 +0800

    Add echo functionality

--- src/main/java/Bopes.java
+++ src/main/java/Bopes.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Bopes {
     public static void main(String[] args) {
         String intro = "Bopes is a personal assistant that helps you manage your tasks.";
         System.out.println(intro);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.println("What can I do for you?");
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
