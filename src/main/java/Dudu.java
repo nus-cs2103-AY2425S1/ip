83be97785a6274f647df0cae78c6aa0e5118636c
ngjianyi <ngjianyi2002@gmail.com>
2024-08-23 00:41:05 +0800

    Added missing description error for todo

--- src/main/java/Dudu.java
+++ src/main/java/Dudu.java
@@ -15,57 +15,66 @@ public class Dudu {
         int count = 0;
 
         while (sc.hasNextLine()) {
-            String input = sc.nextLine().toLowerCase();
-            if (input.equals("list")) {
-                if (count == 0) {
-                    System.out.println(LineWrapper.wrap("No tasks"));
-                } else {
-                    StringBuilder output = new StringBuilder("Here are the tasks in your list:");
-                    for (int i = 0; i < count; i++) {
-                        output.append("\n" + (i + 1) + ". " + tasks[i]);
+            try {
+                String input = sc.nextLine().toLowerCase();
+                if (input.equals("list")) {
+                    if (count == 0) {
+                        System.out.println(LineWrapper.wrap("No tasks"));
+                    } else {
+                        StringBuilder output = new StringBuilder("Here are the tasks in your list:");
+                        for (int i = 0; i < count; i++) {
+                            output.append("\n" + (i + 1) + ". " + tasks[i]);
+                        }
+                        System.out.println(LineWrapper.wrap(output.toString()));
                     }
-                    System.out.println(LineWrapper.wrap(output.toString()));
+                } else if (input.equals("bye")) {
+                    break;
+                } else if (input.equals("help")) {
+                    String output = LineWrapper.wrap("hehe not done");
+                    System.out.println(output);
+                } else if (input.matches("mark\\s\\d+")) {
+                    int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
+                    tasks[pos].markCompleted();
+                    String output = LineWrapper.wrap(String.format("Nice! I've marked this task as done:\n    %s", tasks[pos]));
+                    System.out.println(output);
+                } else if (input.matches("unmark\\s\\d+")) {
+                    int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
+                    tasks[pos].markUncompleted();
+                    String output = LineWrapper.wrap(String.format("OK, I've marked this task as not done yet:\n    %s", tasks[pos]));
+                    System.out.println(output);
+                } else if (input.matches("^todo.*")) {
+                    String[] details = input.split("todo ");
+                    if (details.length <= 1) {
+                        throw new MissingDescriptionException("Please key in an exception");
+                    }
+                    String description = details[1];
+                    ToDo task = new ToDo(description);
+                    tasks[count] = task;
+                    count++;
+                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
+                    System.out.println(output);
+                } else if (input.matches("^deadline\\s/by\\s.+")) {
+                    String[] details = input.split("deadline ")[1].split(" /by ");
+                    Deadline task = new Deadline(details[0], details[1]);
+                    tasks[count] = task;
+                    count++;
+                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
+                    System.out.println(output);
+                } else if (input.matches("^event\\s/from\\s.+\\sto\\s.+")) {
+                    String details = input.split("event ")[1];
+                    String description = details.split("/from ")[0];
+                    String[] date = details.split("/from ")[1].split(" /to ");
+                    Event task = new Event(description, date[0], date[1]);
+                    tasks[count] = task;
+                    count++;
+                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
+                    System.out.println(output);
+                } else {
+                    String output = LineWrapper.wrap("Please use \"help\" to get the list of commands");
+                    System.out.println(output);
                 }
-            } else if (input.equals("bye")) {
-                break;
-            } else if (input.equals("help")) {
-                String output = LineWrapper.wrap("hehe not done");
-                System.out.println(output);
-            } else if (input.matches("mark\\s\\d+")) {
-                int pos = Integer.parseInt(input.replaceAll("\\D+","")) - 1;
-                tasks[pos].markCompleted();
-                String output = LineWrapper.wrap(String.format("Nice! I've marked this task as done:\n    %s", tasks[pos]));
-                System.out.println(output);
-            } else if (input.matches("unmark\\s\\d+")){
-                int pos = Integer.parseInt(input.replaceAll("\\D+","")) - 1;
-                tasks[pos].markUncompleted();
-                String output = LineWrapper.wrap(String.format("OK, I've marked this task as not done yet:\n    %s", tasks[pos]));
-                System.out.println(output);
-            } else if (input.matches("^todo.+")) {
-                ToDo task = new ToDo(input.split("todo ")[1]);
-                tasks[count] = task;
-                count++;
-                String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
-                System.out.println(output);
-            } else if (input.matches("^deadline\\s/by\\s.+")) {
-                String[] details = input.split("deadline ")[1].split(" /by ");
-                Deadline task = new Deadline(details[0], details[1]);
-                tasks[count] = task;
-                count++;
-                String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
-                System.out.println(output);
-            } else if (input.matches("^event\\s/from\\s.+\\sto\\s.+")) {
-                String details = input.split("event ")[1];
-                String description = details.split("/from ")[0];
-                String[] date = details.split("/from ")[1].split(" /to ");
-                Event task = new Event(description, date[0], date[1]);
-                tasks[count] = task;
-                count++;
-                String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
-                System.out.println(output);
-            } else {
-                String output = LineWrapper.wrap("Please use \"help\" to get the list of commands");
-                System.out.println(output);
+            } catch (MissingDescriptionException e) {
+                System.out.println(e);
             }
         }
 
