1a5bfe55af853f018b726e18b048e89f4fe83d21
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-08-22 08:04:13 +0000

    fix: Wrong unmark message

--- src/main/java/controllers/commands/UnmarkTaskCommand.java
+++ src/main/java/controllers/commands/UnmarkTaskCommand.java
@@ -14,7 +14,7 @@ public class UnmarkTaskCommand implements Command {
         try {
             taskList.unmarkTask(index-1);
             System.out.println("____________________________________________________________");
-            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("OK, I've marked this task as not done yet:");
             System.out.println(taskList.getTask(this.index-1));
             System.out.println("____________________________________________________________");
         } catch (IndexOutOfBoundsException e) {
