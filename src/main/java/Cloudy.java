21fe37f7ff7c68ed7d55206b53f4784420609566
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-08-22 12:51:32 +0800

    Level-1 task

--- src/main/java/Cloudy.java
+++ src/main/java/Cloudy.java
@@ -1,10 +1,33 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Cloudy {
     public static void main(String[] args) {
+        Scanner echo = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Cloudy.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String userInput = echo.nextLine();
+
+        while (true) {
+            if (Objects.equals(userInput, "bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+
+                userInput = echo.nextLine();
+            }
+        }
+
+
+
     }
 }
