105d0f7f4f5c4be3c35f9672094066872a598af5
Xueting <xueting1966@gmail.com>
2024-08-22 19:42:20 +0800

    Added logic for chat bot and end

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class Snowy {
     public static void main(String[] args) {
-        String line ="____________________________________________________________\n";
+        String line ="____________________________________________________________";
         System.out.println(line + " Hello I'm Snowy\n What can I do for you?\n");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(line + "You said: " + input );
+        boolean isBye = false;
 
-    }
+            while (!isBye) {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("bye")) {
+                    isBye = true;
+                } else {
+                    System.out.println(line + "\nYou said: " + input +"\n" + line);
+                }
+            }
+        System.out.println("Bye. See you next time!");
+        }
 }
