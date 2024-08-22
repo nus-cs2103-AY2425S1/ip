9c7123f507b4236f4a0b3e7d75607daeffd0eb99
accountexeregister <yeohchongyics@gmail.com>
2024-08-22 20:00:40 +0800

    Rename chatbot, add skeleton greeting

---
+++ src/main/java/CancelGPT.java
@@ -0,0 +1,12 @@
+public class CancelGPT {
+    public static void main(String[] args) {
+        final String CHATBOT_NAME = "CancelGPT";
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I am " + CHATBOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
