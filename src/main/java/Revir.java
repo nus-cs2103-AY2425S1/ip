0b977b94404a84abe43880c9ddad21815770384b
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-08-22 10:53:22 +0800

    renamed chatbot

---
+++ src/main/java/Revir.java
@@ -0,0 +1,8 @@
+public class Revir {
+    public static void main(String[] args) {
+        String name = "Revir";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
