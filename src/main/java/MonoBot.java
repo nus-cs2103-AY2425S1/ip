075cb45eb920156cf6a4e95ce93290d44ae59886
prave1n <pravein2002@gmail.com>
2024-08-22 21:18:50 +0800

    Add list to store tasks

--- src/main/java/MonoBot.java
+++ src/main/java/MonoBot.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MonoBot {
+
+    private static ArrayList<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
-        printGreeting();
+        MonoBot.printGreeting();
 
         Scanner sc = new Scanner(System.in);
         while (true) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                printTasks();
+            } else {
+                addTask(command);
             }
-            hLine();
-            System.out.println(command);
-            hLine();
         }
 
-        printFarewell();
+        MonoBot.printFarewell();
     }
 
     private static void hLine() {
@@ -23,13 +28,34 @@ public class MonoBot {
     }
 
     private static void printGreeting() {
-        hLine();
+        MonoBot.hLine();
         System.out.println(" Hello! I'm MonoBot");
         System.out.println(" What can I do for you?");
-        hLine();    }
+        MonoBot.hLine();
+    }
 
     private static void printFarewell() {
+        MonoBot.hLine();
         System.out.println(" Bye. Hope to see you again soon!");
-        hLine();
+        MonoBot.hLine();
+    }
+
+    private static void addTask(String str) {
+        MonoBot.tasks.add(str);
+        MonoBot.hLine();
+        System.out.println("Added: " + str);
+        MonoBot.hLine();
+    }
+
+    private static void printTasks() {
+        MonoBot.hLine();
+        if (MonoBot.tasks.isEmpty()) {
+            System.out.println("No tasks added yet");
+        } else {
+            for (int i = 0; i < MonoBot.tasks.size(); i++) {
+                System.out.printf("%d. %s%n", i + 1, MonoBot.tasks.get(i));
+            }
+        }
+        MonoBot.hLine();
     }
 }
