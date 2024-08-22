684b4246546cc890551292edc374ffd90af8caf1
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-08-22 20:05:17 +0800

    Add Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Deadline {
+public class Deadline extends Task{
+    private String date;
+
+    public Deadline(String name, String date) {
+        super(name);
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        String temp = super.toString();
+        return String.format("[D]%s (by %s )", temp, date);
+    }
 }
