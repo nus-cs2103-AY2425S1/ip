acbde76d04f9a00ef4d34839399bff5033e750c5
hansneddyanto <hans.neddy@gmail.com>
2024-08-22 16:34:38 +0800

    Handle errors (without HanaException class for now)

--- src/main/java/Hana.java
+++ src/main/java/Hana.java
@@ -18,11 +18,11 @@ public class Hana {
 
         System.out.println(line + greeting + line);
         String input = scanner.nextLine();  // Read user input
-        String output = "";
         while (!input.equals("bye")) {
             // Check for mark/unmark in input
             Matcher markMatcher = markPattern.matcher(input);
             Matcher unmarkMatcher = unmarkPattern.matcher(input);
+
             if (input.equals("list")) {
                 System.out.println(line);
                 for (int i = 0; i < numberOfTasks; i++) {
@@ -49,27 +49,39 @@ public class Hana {
                 } else {
                     System.out.println(line + "     Task number out of range.\n" + line);
                 }
-            } else if (input.startsWith("todo ")) {
-                String description = input.substring(5);
-                tasks[numberOfTasks] = new ToDo(description);
-                numberOfTasks++;
-                System.out.println(line + "     Got it. I've added this task:\n" +
-                        "       " + tasks[numberOfTasks - 1] + "\n" +
-                        "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
-            } else if (input.startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
-                tasks[numberOfTasks] = new Deadline(parts[0], parts[1]);
-                numberOfTasks++;
-                System.out.println(line + "     Got it. I've added this task:\n" +
-                        "       " + tasks[numberOfTasks - 1] + "\n" +
-                        "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
-            } else if (input.startsWith("event ")) {
-                String[] parts = input.substring(6).split(" /from | /to ");
-                tasks[numberOfTasks] = new Event(parts[0], parts[1], parts[2]);
-                numberOfTasks++;
-                System.out.println(line + "     Got it. I've added this task:\n" +
-                        "       " + tasks[numberOfTasks - 1] + "\n" +
-                        "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+            } else if (input.startsWith("todo")) {
+                try {
+                    String description = input.substring(5);
+                    tasks[numberOfTasks] = new ToDo(description);
+                    numberOfTasks++;
+                    System.out.println(line + "     Got it. I've added this task:\n" +
+                            "       " + tasks[numberOfTasks - 1] + "\n" +
+                            "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+                } catch (Exception e) {
+                    System.out.println(line + "    OOPS!!! The description of a todo cannot be empty.\n" + line);
+                }
+            } else if (input.startsWith("deadline")) {
+                try {
+                    String[] parts = input.substring(9).split(" /by ");
+                    tasks[numberOfTasks] = new Deadline(parts[0], parts[1]);
+                    numberOfTasks++;
+                    System.out.println(line + "     Got it. I've added this task:\n" +
+                            "       " + tasks[numberOfTasks - 1] + "\n" +
+                            "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+                } catch (Exception e) {
+                    System.out.println(line + "    OOPS!!! The description of a deadline cannot be empty.\n" + line);
+                }
+            } else if (input.startsWith("event")) {
+                try {
+                    String[] parts = input.substring(6).split(" /from | /to ");
+                    tasks[numberOfTasks] = new Event(parts[0], parts[1], parts[2]);
+                    numberOfTasks++;
+                    System.out.println(line + "     Got it. I've added this task:\n" +
+                            "       " + tasks[numberOfTasks - 1] + "\n" +
+                            "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+                } catch (Exception e) {
+                    System.out.println(line + "    OOPS!!! The description of an event cannot be empty.\n" + line);
+                }
             } else {
                 System.out.println(line + "     I'm sorry, I don't understand that command.\n" + line);
             }
