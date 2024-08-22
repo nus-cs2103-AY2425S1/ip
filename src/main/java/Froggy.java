9d81fccb20521592321b2717b4b2025250d4aa05
Chen Lei Yu <128605764+ChenLeiyu@users.noreply.github.com>
2024-08-22 19:48:06 +0800

    Change chatbot name from Duke to Froggy Add basic greeting and exit messages

---
+++ src/main/java/Froggy.java
@@ -0,0 +1,11 @@
+public class Froggy {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Froggy!\n"
+                + "What can I do for you?\n"
+                + "_______________________________\n";
+        String exit = "Bye. Hope to see you again soon!\n"
+                + "_______________________________\n";
+
+        System.out.println(greeting + exit);
+    }
+}
