7ddeb1908b59272bc3e87ad3e4dfc552fa71991e
althea <altheatan328@gmail.com>
2024-08-23 04:58:23 +0800

    Level-1

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duck {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -6,6 +8,16 @@ public class Duck {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);*/
-        System.out.println("Hello! I'm DUCK\n What can I do for you?\n Bye. Hope to see you again soon!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! I'm DUCK\n What can I do for you?");
+        while (true) {
+            String userCmd = scan.nextLine();
+            if (userCmd.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(userCmd);
+        }
+        scan.close();
     }
 }
