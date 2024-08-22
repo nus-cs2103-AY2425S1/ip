cfacb56f1ef005fb3293f5f942459dc5078c1173
Wei Lian <weilian2003@live.com>
2024-08-22 23:47:49 +0800

    Allow ChatBot to save tasks in memory

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Joe {
     public static void main(String[] args) {
-        System.out.println("""
-                Hello! I'm Joe
-                What can I do for you?""");
+        ArrayList<String> tasks = new ArrayList<>();
+        System.out.println("\t" + "Hello! I'm Joe");
+        System.out.println("\t" + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String EXIT_WORD = "bye";
+        String LIST_TASKS = "list";
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals(EXIT_WORD)) {
-                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t" + "Bye. Hope to see you again soon!");
                 return;
             }
-            System.out.println("\t" + userInput);
+            if (userInput.equals(LIST_TASKS)) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    String msg = String.format("%d. %s", i + 1, tasks.get(i));
+                    System.out.println("\t" + msg);
+                }
+            }
+            else {
+                tasks.add(userInput);
+                System.out.println("\t" + "added: " + userInput);
+            }
         }
     }
 }
