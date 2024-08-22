7e51a5a0957c168e05d02553ae35a7e55f5b0be3
MiloTruck <milotruckctf@gmail.com>
2024-08-22 18:42:57 +0800

    Add mark as done functionality

--- src/main/java/MiluTrock.java
+++ src/main/java/MiluTrock.java
@@ -6,22 +6,38 @@ public class MiluTrock {
         System.out.println("Hello! I'm " + name + "!");
         System.out.println("What can I do for you?");
         
-        ArrayList<String> items = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
         while (true) {
             String input = System.console().readLine();
+            String[] words = input.split("\\s+");
             
-            if (input.toLowerCase().equals("bye")) {
+            if (input.equals("bye")) {
                 // Exit if "bye" is entered
                 break;
-            } else if (input.toLowerCase().equals("list")) {
+            } else if (input.equals("list")) {
                 // List all previous inputs if "list" is entered
-                for (int i = 0; i < items.size(); i++) {
-                    System.out.println((i+1) + ". " + items.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
                 }
+            } else if (words.length == 2 && words[0].equals("mark")) {
+                // Mark task as done if "mark" is entered as the first word
+                int i = Integer.parseInt(words[1]) - 1;
+                tasks.get(i).markDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(i));
+            } else if (words.length == 2 && words[0].equals("unmark")) {
+                // Unmark task as done if "unmark" is entered as the first word
+                int i = Integer.parseInt(words[1]) - 1;
+                tasks.get(i).unmarkDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks.get(i));
             } else {
                 // Otherwise, store input
                 System.out.println("Added: " + input);
-                items.add(input);
+                tasks.add(new Task(input));
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    public String toString() { 
+        return "[" + (this.isDone ? "X" : " ") + "] " + this.name; 
+    } 
+}
