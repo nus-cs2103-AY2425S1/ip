660cd497f4ffb0e926b58bcc735754168c09ea65
marraknas <sankarram2002ybis@gmail.com>
2024-08-22 17:58:55 +0800

    Rename, Greet, Exit

---
+++ src/main/java/R2D2.java
@@ -0,0 +1,11 @@
+public class R2D2 {
+    public static void main(String[] args) {
+        String hline = "____________________________________________________________";
+        System.out.println(hline);
+        System.out.println("Hello! I'm R2D2!");
+        System.out.println("What can I do for you?");
+        System.out.println(hline);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(hline);
+    }
+}
