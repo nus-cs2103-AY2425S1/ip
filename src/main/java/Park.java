c136eded0c45c6770f9b5ae19887ac601358e645
jason-chuah <chuahchonghee2019@gmail.com>
2024-08-22 13:58:38 +0800

    Edit for Level 2

--- src/main/java/Park.java
+++ src/main/java/Park.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Park {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<String>();
         System.out.println("""
                 Hello! I'm Park
                 What can I do for you?""");
@@ -12,8 +14,16 @@ public class Park {
                 System.out.println("Bye. Hope to see you again soon!");
                 sc.close();
                 System.exit(0);
+            } else if (s.equals("list")){
+                int n = 1;
+                for (String text : arr) {
+                    System.out.print(n);
+                    System.out.println(". " + text);
+                    n++;
+                }
             } else {
-                System.out.println(s);
+                arr.add(s);
+                System.out.println("added: " + s);
             }
         }
     }
