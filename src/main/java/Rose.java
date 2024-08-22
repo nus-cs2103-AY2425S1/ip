dcf44cbe60689595cf06a20061204d913bb7c1bc
hastutiherah <hastherah@gmail.com>
2024-08-22 22:39:36 +0800

    Create Add

--- src/main/java/Rose.java
+++ src/main/java/Rose.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Rose {
+    static final String horizontal = "____________________________________________________________";
+    static final String name = "Rose";
+    static ArrayList<String> tasks = new ArrayList<>();
+
+    private static void addTask(String task) {
+        tasks.add(task);
+        System.out.println((horizontal + "\nadded: " + task +
+                "\n" + horizontal).indent(4));
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String horizontal = "____________________________________________________________";
-        String name = "Rose";
 
         String opening = horizontal + "\nHi gorgeous! I'm " + name + "~~\n"
                 + "How can I help you today?\n" + horizontal;
@@ -15,8 +24,7 @@ public class Rose {
         String command = scanner.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println((horizontal + '\n' + command +
-                    '\n' + horizontal).indent(4));
+            addTask(command);
             command = scanner.nextLine();
         }
 
