cfacad1a9325ddbc08e02666d5aa2bbcf33f5c8d
btbrandon <tzhbrandon@gmail.com>
2024-08-23 00:14:19 +0800

    Completed Level-2

--- src/main/java/BeeBot.java
+++ src/main/java/BeeBot.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -6,24 +7,39 @@ public class BeeBot {
         Scanner scanner = new Scanner(System.in);
         Boolean exit_status = false;
         String greet = "Hello! I'm BeeBot\n"
-                + "What can I do for you?";
-        String exit = "Bye. Hope to see you again!";
+                + "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again!\n";
+        ArrayList<String> list = new ArrayList<>();
+
         speak(greet);
 
         while (!exit_status) {
             String cmd = scanner.nextLine();
-            if (Objects.equals(cmd, "bye")) {
-                speak(exit);
-                exit_status = true;
-            } else {
-                speak(cmd);
+            switch (cmd) {
+                case "bye":
+                    speak(exit);
+                    exit_status = true;
+                    break;
+                case "list":
+                    int size = list.size();
+                    String listStr = "";
+                    for (int i = 0; i < size; i++) {
+                        listStr += list.get(i) + "\n";
+                    }
+                    speak(listStr);
+                    break;
+                default:
+                    int num = list.size() + 1;
+                    list.add(num + ": " + cmd);
+                    speak("added: " + cmd + "\n");
+                    break;
             }
         }
     }
 
     public static void speak(String cmd) {
         System.out.println("________________________\n"
-                + cmd + "\n"
+                + cmd
                 + "________________________\n");
     }
 }
