b6a95c5dc46b035deafaec7cf399aeeb3dbeb8d0
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-08-23 03:29:43 +0800

    Added echo for ChatBot

--- src/main/java/WenJie.java
+++ src/main/java/WenJie.java
@@ -1,12 +1,44 @@
+import java.util.Scanner;
+
 public class WenJie {
     public static void main(String[] args) {
-        String logo = " ____________________________________________________________\n" +
-                " Eh wasusp la bro, my name is Wen Jie.\n" +
-                " What you want?\n" +
-                "____________________________________________________________\n" +
-                " Eh zao liao, see you around ah bro.\n" +
-                "____________________________________________________________\n\n";
 
-        System.out.println(logo);
+        Scanner scanner = new Scanner(System.in);
+        boolean active = true;
+
+        String greeting =
+                "   ____________________________________________________________\n" +
+                "   Eh wasusp la bro, my name is Wen Jie.\n" +
+                "   What you want?\n" +
+                "   ____________________________________________________________\n";
+
+        System.out.println(greeting);
+
+        while(active) {
+            String input = scanner.nextLine();
+
+            switch(input) {
+                case "bye": {
+                    active = false;
+                    String farewell =
+                            "____________________________________________________________\n" +
+                            " Paiseh bro I zao liao, see you around ah bro.\n" +
+                            "____________________________________________________________\n";
+                    System.out.println(farewell);
+                    break;
+                }
+
+                default :
+                    String output =
+                            "____________________________________________________________\n" +
+                            input + "\n" +
+                            "____________________________________________________________\n";
+                    System.out.println(output);
+                    break;
+            }
+        }
+
+
+
     }
 }
