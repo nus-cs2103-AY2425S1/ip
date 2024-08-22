d813cdcd105673834719485e831b597b847caa9d
niclammm <E1122517@u.nus.edu>
2024-08-22 23:06:37 +0800

    Added String array to hold inputs by scanner

--- src/main/java/Repsmax.java
+++ src/main/java/Repsmax.java
@@ -4,6 +4,9 @@ public class Repsmax {
     public static void main(String[] args) {
         //initialise scanner
         Scanner scanner = new Scanner(System.in);
+        //initialise string array
+        String[] tasks = new String[100];
+        int taskcount = 0;
         //greet the user
         String greeting = "  ____________________________________________________________\n" +
                 "   Hello! I'm Repsmax\n" +
@@ -18,13 +21,19 @@ public class Repsmax {
             if (userInput.equals("bye")) {
                 System.out.println(goodbye);
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("  ____________________________________________________________\n");
+                for (int i = 0; i < taskcount; i++) {
+                    System.out.println("   " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("  ____________________________________________________________\n");
+            } else {
+                tasks[taskcount] = userInput;
+                taskcount++;
+                System.out.println("  ____________________________________________________________\n");
+                System.out.println("added: " + userInput);
+                System.out.println("  ____________________________________________________________\n");
             }
-
-            System.out.println("  ____________________________________________________________\n" +
-                    "   " +
-                    userInput +
-                    "\n" +
-                    "  ____________________________________________________________\n");
         }
     }
 }
