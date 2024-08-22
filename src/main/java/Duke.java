e30b465cff0385f5bdeb01bea7a321cd06301806
Vicelya Visakha <>
2024-08-22 13:18:51 +0800

    Rename Duke java to Tira Java

--- src/main/java/Duke.java
+++
@@ -1,6 +0,0 @@
-public class Tira {
-    public static void main(String[] args) {
-        String logo = "TIRAMISU THE CAFE CAT (TIRA)"
-        System.out.println("Hello from\n" + logo);
-    }
-}
---
+++ src/main/java/Tira.java
@@ -0,0 +1,8 @@
+public class Tira {
+    public static void main(String[] args) {
+        String logo = "TIRAMISU THE CAFE CAT (TIRA)";
+        System.out.println("MIAO (Hello) from\n" + logo + "\n" +
+                "What can I do for you, miao? + \n" +
+                "Bye. Hope to see you again, miao!") ;
+    }
+}
