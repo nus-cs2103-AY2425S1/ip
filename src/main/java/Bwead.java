6f720d3a9add40987d543e4f91dda604e58171de
jiaaaaen <girljiaen@gmail.com>
2024-08-22 23:26:56 +0800

    echo

--- src/main/java/Bwead.java
+++ src/main/java/Bwead.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Bwead {
 
     public static String name = "Bwead";
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Hello from " + name + "!");
-        System.out.println("Bye. Hope to see you again soon!");
+        //System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+
+        }
     }
 }
