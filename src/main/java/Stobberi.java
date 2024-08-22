57f591559cddeeb40949ee81b7150abbb6bf98a6
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-22 14:31:48 +0800

    added Echo

--- src/main/java/Stobberi.java
+++ src/main/java/Stobberi.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Stobberi {
     private static String nameOfChatBot = "Stobberi";
+    private static String helloGreeting =
+            "Hello! I'm " + nameOfChatBot + ".\n"
+            + "What can I do for you?";
+    private static String goodByeGreeting = "Bye. Hope to see you again soon! :)\n";
+
+    private static String displayForm(String phrase) {
+        return
+                "___________________________________________\n"
+                + phrase
+                + "\n___________________________________________\n";
+    }
+
+    private static void Echo() {
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.nextLine();
+
+        while (!temp.equals("bye")) {
+            System.out.println(displayForm(temp));
+            temp = scanner.nextLine();
+        }
+    }
     public static void main(String[] args) {
-        String greeting = "___________________________________________\n"
-                + "Hello! I'm " + nameOfChatBot + ".\n"
-                + "What can I do for you?\n"
-                + "___________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "___________________________________________\n";
-        System.out.println(greeting);
+        System.out.println(displayForm(helloGreeting));
+        Echo();
+        System.out.println(displayForm(goodByeGreeting));
     }
 }
