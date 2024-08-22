6469915f4804ef63a23dcde6929b176ec2f9021d
Joel Tio <joeltiojinhon@gmail.com>
2024-08-22 23:51:46 +0800

    Update Duke chatbot name to Ip Man

---
+++ src/main/java/IpMan.java
@@ -0,0 +1,15 @@
+public class IpMan {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        // Hello
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Ip Man.");
+        System.out.println("What can I do for you?");
+
+        // Goodbye
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("That's enough for today. See you another time.");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
