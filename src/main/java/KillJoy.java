b46720bd2de40deafc2ab963583825cd36cd4af1
thisisaditya17 <joshi.adi1734@gmail.com>
2024-08-22 11:36:22 +0800

    KillJoy.java: echo user inputs and exit on "bye"
    - Implement functionality to echo commands entered by the user.
    - Add logic to terminate the chatbot session when the user types the command 'bye'.

--- src/main/java/KillJoy.java
+++ src/main/java/KillJoy.java
@@ -1,20 +1,55 @@
-public class KillJoy {
-    public static void main(String[] args) {
-        String logo = " _  ___ _ _      _             \n" +
-                        "| |/ (_) | |    | |            \n" +
-                        "| ' / _| | |    | | ___  _   _ \n" +
-                        "|  < | | | |_   | |/ _ \\| | | |\n" +
-                        "| . \\| | | | |__| | (_) | |_| |\n" +
-                        "|_|\\_\\_|_|_|\\____/ \\___/ \\__, |\n" +
-                        "                          __/ |\n" +
-                        "                         |___/ \n";
-        System.out.println("------------------------------------");
-        System.out.println(logo);
-        System.out.println("------------------------------------");
-        System.out.println(" Hello! I'm KillJoy \n What can I do for you?");
-        System.out.println("------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------");
+import java.util.Scanner;
 
+public class KillJoy {
+
+    public void start() {
+        System.out.println(this.logoString());
+        System.out.println(this.welcomeString());
+
+        Scanner user = new Scanner(System.in);
+
+        while(true) {
+            String input = user.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(this.exitString());
+                break;
+            }
+            userInput(input);
+        }
+    }
+    private void userInput(String input) {
+        if(input.equals("bye")) {
+            exitString();
+        }
+        System.out.println("    ------------------------------------");
+        System.out.println("    " + input);
+        System.out.println("    ------------------------------------");
+    }
+    private String exitString() {
+        return "    Bye. Hope to see you again soon!\n" +
+                "   ------------------------------------";
+    }
+
+    private String welcomeString() {
+        return "   Hello! I'm KillJoy \n   What can I do for you?\n" +
+                "   ------------------------------------";
+    }
+
+    private String logoString() {
+        return "   ------------------------------------\n" +
+                "    _  ___ _ _      _             \n" +
+                "   | |/ (_) | |    | |            \n" +
+                "   | ' / _| | |    | | ___  _   _ \n" +
+                "   |  < | | | |_   | |/ _ \\| | | |\n" +
+                "   | . \\| | | | |__| | (_) | |_| |\n" +
+                "   |_|\\_\\_|_|_|\\____/ \\___/ \\__, |\n" +
+                "                             __/ |\n" +
+                "                            |___/ \n" +
+                "   ------------------------------------";
+    }
+
+    public static void main(String[] args) {
+        KillJoy kj = new KillJoy();
+        kj.start();
     }
 }
