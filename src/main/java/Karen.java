35046722be578883c87ccea76a928817de381691
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-23 01:01:11 +0800

    Added mark/unmark functionality

--- src/main/java/Karen.java
+++ src/main/java/Karen.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import main.java.Task;
 
 public class Karen {
+    private static List<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         final String LINE = "_______________________\n";
-        List<String> tasks = new ArrayList<String>();
-
         String output = LINE +
                         "Hi! I'm Karen\n" +
                         "What can I do for you?\n" +
@@ -20,32 +20,51 @@ public class Karen {
             output = "";
 
             if (input.equals("bye")) {
+                //End program
                 System.out.print(
                         LINE +
                         "Bye! Hope to see you again!\n" +
                         LINE
                 );
                 break;
-            } else if (input.equals("list")) {
-                output += LINE;
 
+            } else if (input.equals("list")) {
+                //List tasks
+                output += LINE;
                 if (tasks.isEmpty()) {
                     output += "No tasks yet!\n";
                 } else {
                     for (int i = 0; i < tasks.size(); i++) {
-                        String s = tasks.get(i);
-                        if (s.isEmpty()) {
-                            break;
-                        } else {
-                            output += String.format("%d. %s\n",i + 1, s);
-                        }
+                        Task t = tasks.get(i);
+                        output += String.format("%d. %s\n", i+1, t);
                     }
                 }
-
                 output += LINE;
-            }
-            else {
-                tasks.add(input);
+
+            } else if (input.split(" ")[0].equals("mark")) {
+                //Mark as done
+                int n = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task marked_task = tasks.get(n);
+                marked_task.mark();
+                output += LINE +
+                        "Nice! I've marked this task as done:\n\t" +
+                        marked_task + "\n" +
+                        LINE;
+
+            } else if (input.split(" ")[0].equals("unmark")) {
+                //Unmark as done
+                int n = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task unmarked_task = tasks.get(n);
+                unmarked_task.unmark();
+                output += LINE +
+                        "Ok! This task is now marked undone:\n\t" +
+                        unmarked_task + "\n" +
+                        LINE;
+
+            } else {
+                //Add new task
+                Task newTask = new Task(input);
+                tasks.add(newTask);
                 output = LINE +
                         "Added: " + input + "\n" +
                         LINE;
