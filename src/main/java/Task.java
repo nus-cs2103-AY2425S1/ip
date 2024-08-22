0dbb9bd260ee8c1014b22f429cccff24d024811a
Chan Guan Yu <changuanyu2904@gmail.com>
2024-08-22 17:30:19 +0800

    Create Task class to encapsulate data related to each class within an object

---
+++ src/main/java/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    private String description;
+    private boolean isdone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isdone = false;
+    }
+}
