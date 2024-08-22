0fd1d2861648839ad709d7ccf8bdb415fe25aef5
jarrellc1 <Jarrell.chia@gmail.com>
2024-08-23 01:13:56 +0800

    Add comments to JarException
    Testing commit.
    
    Ignore this commit.

--- src/main/java/exceptions/JarException.java
+++ src/main/java/exceptions/JarException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class JarException extends Exception {
     public JarException(String message) {
-        super(message);
+        super(message); //exception specifically for jar
     }
 }
