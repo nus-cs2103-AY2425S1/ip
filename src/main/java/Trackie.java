a0697d0ac3677ecf6baa6d0e69d7029ab89c2bd7
xczheng <zhengxc105@gmail.com>
2024-08-23 02:51:03 +0800

    refactor from "Duke" to trackie, add greeting and exit messages

---
+++ src/main/java/Trackie.java
@@ -0,0 +1,6 @@
+public class Trackie {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Trackie. Nice to meet you =)");
+        System.out.println("Seeya!");
+    }
+}
