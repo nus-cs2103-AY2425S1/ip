ce7dbe2dd987d17083e71123c3494e2a3a497163
tata32000 <tata32000@hotmail.com>
2024-08-22 23:33:11 +0800

    Add this keyword to tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -4,37 +4,37 @@ public class TaskList {
     private final ArrayList<Task> tasks;
 
     public TaskList() {
-        tasks = new ArrayList<>();
+        this.tasks = new ArrayList<>();
     }
 
     public void addItem(String item) {
-        tasks.add(new Task(item));
+        this.tasks.add(new Task(item));
     }
 
     public void addItem(Task item) {
-        tasks.add(item);
+        this.tasks.add(item);
     }
 
     public void markAsDone(int index) {
-        tasks.get(index).markAsDone();
+        this.tasks.get(index).markAsDone();
     }
 
     public void markAsUndone(int index) {
-        tasks.get(index).markAsUndone();
+        this.tasks.get(index).markAsUndone();
     }
 
     public String getTask(int index) {
-        return tasks.get(index).toString() + "\n";
+        return this.tasks.get(index).toString() + "\n";
     }
 
     public String getLastTask() {
-        return tasks.getLast().toString();
+        return this.tasks.getLast().toString();
     }
 
     public String getList() {
         StringBuilder list = new StringBuilder();
-        for (int i = 0; i < tasks.size(); i++) {
-            list.append(i + 1).append(". ").append(tasks.get(i)).append("\n");
+        for (int i = 0; i < this.tasks.size(); i++) {
+            list.append(i + 1).append(". ").append(this.tasks.get(i)).append("\n");
         }
         return list.toString();
     }
