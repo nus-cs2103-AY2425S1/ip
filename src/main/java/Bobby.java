8319ad88a0b1fa26611b37b462facd7266130266
zhiyi12345 <122374247+zhiyi12345@users.noreply.github.com>
2024-08-22 23:03:21 +0500

    Level 0 Renamed chatbot to Bobby Added Greetings and Exit

---
+++ src/main/java/Bobby.java
@@ -0,0 +1,8 @@
+public class Bobby {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Bobby\n"
+                + "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greeting + exit);
+    }
+}
