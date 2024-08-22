5544aeff78a015664ea22c3c5d7cd1935daef0cf
youngseopark05 <e1304445@u.nus.edu>
2024-08-23 02:20:44 +0800

    Add greeting and exit methods
    There is only a greeting that is triggered in the
    main() method.
    
    The lack of an exit and the lack of separation
    between a greeting and an exit complicate the
    start and end of the conversation between the
    user and the bot.
    
    Extracting the greeting and exit behaviour
    each allows us to control the interface at the
    start and end of the conversation between the
    user and the bot.
    
    Let's make a greet() method that welcomes the
    user and and exit() method that wishes them
    goodbye.
    
    Separating the greeting and exit message ensures
    they can be called separately.

--- src/main/java/HypeBot.java
+++ src/main/java/HypeBot.java
@@ -1,10 +1,29 @@
 public class HypeBot {
+    private static final String bufferLine = "______________________________________________________________________\n";
+    public static void greet() {
+        String logo = " ('-. .-.               _ (`-.    ('-. .-. .-')                .-') _   \n" +
+                "( OO )  /              ( (OO  ) _(  OO)\\  ( OO )              (  OO) )  \n" +
+                ",--. ,--.  ,--.   ,--._.`     \\(,------.;-----.\\  .-'),-----. /     '._ \n" +
+                "|  | |  |   \\  `.'  /(__...--'' |  .---'| .-.  | ( OO'  .-.  '|'--...__)\n" +
+                "|   .|  | .-')     /  |  /  | | |  |    | '-' /_)/   |  | |  |'--.  .--'\n" +
+                "|       |(OO  \\   /   |  |_.' |(|  '--. | .-. `. \\_) |  |\\|  |   |  |   \n" +
+                "|  .-.  | |   /  /\\_  |  .___.' |  .--' | |  \\  |  \\ |  | |  |   |  |   \n" +
+                "|  | |  | `-./  /.__) |  |      |  `---.| '--'  /   `'  '-'  '   |  |   \n" +
+                "`--' `--'   `--'      `--'      `------'`------'      `-----'    `--'   \n";
+        System.out.println(bufferLine
+                + "AYO WHAT'S UP IT'S ME YOUR\n\n"
+                + logo
+                + bufferLine);
+    }
+
+    public static void exit() {
+        System.out.println("Alright homie, it's been a BLAST hanging out with you. "
+                + "Have a wonderful\nday, and catch you soon again you ABSOLUTE BALLER!\n"
+                + bufferLine);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
     }
 }
