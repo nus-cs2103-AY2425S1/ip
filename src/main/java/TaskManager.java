89ceef7bd80e8475012d429dbea26e42e8d0928f
Jabez Tho <jabeztho@gmail.com>
2024-08-23 05:47:53 +0800

    switch TaskManager underlying array to ArrayList

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,40 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
-    private Task[] tasks = new Task[100];
-    private int taskCount = 0;
+    private List<Task> tasks = new ArrayList<>();
 
     public void addTask(Task task) {
-        tasks[taskCount++] = task;
+        tasks.add(task);
     }
 
     public int getTaskCount() {
-        return taskCount;
+        return tasks.size();
     }
 
     public void markTaskAsDone(int taskNumber) {
-        if (taskNumber < 1 || taskNumber > taskCount) {
+        if (taskNumber < 1 || taskNumber > this.getTaskCount()){
             throw new TaskNotExistException(String.format("BLAHH!!! The task number %s to mark as done does not exist.", taskNumber));
         }
-        tasks[taskNumber - 1].markAsDone();
+        tasks.get(taskNumber - 1).markAsDone();
         PrintUtility.wrapPrintWithHorizontalLines(
             "Nice! I've marked this task as done:",
-            "  " + tasks[taskNumber - 1]
+            "  " + tasks.get(taskNumber - 1)
         );
     }
 
     public void markTaskAsUndone(int taskNumber) {
-        if (taskNumber < 1 || taskNumber > taskCount) {
+        if (taskNumber < 1 || taskNumber > this.getTaskCount()) {
             throw new TaskNotExistException(String.format("BLAHH!!! The task number %s to mark as done does not exist.", taskNumber));
         }
-        tasks[taskNumber - 1].markAsUndone();
+        tasks.get(taskNumber - 1).markAsUndone();
         PrintUtility.wrapPrintWithHorizontalLines(
             "OK, I've marked this task as not done yet:",
-            "  " + tasks[taskNumber - 1]
+            "  " + tasks.get(taskNumber - 1)
         );
     }
 
     public void listTasks() {
+        int taskCount = this.getTaskCount();
         for (int i = 0; i < taskCount; i++) {
-            PrintUtility.indentPrint((i + 1) + ". " + tasks[i]);
+            PrintUtility.indentPrint((i + 1) + ". " + tasks.get(i));
         }
     }
 }
