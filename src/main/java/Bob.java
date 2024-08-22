ec695a4aefe8ee725099a5f3718445d868ad14f8
yixianggg <e1186210@u.nus.edu>
2024-08-22 15:42:13 +0800

    bob echos commands, closes upon bye command

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+
         String line = "____________________________________________________________";
         String name = "Bob";
         System.out.println(line);
         System.out.printf("Hello! I'm %s!\n", name);
         System.out.println("What can I do for you?");
         System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        while (!word.equals("bye")) {
+            System.out.println(word);
+            word = scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
