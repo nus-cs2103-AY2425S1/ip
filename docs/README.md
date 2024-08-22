a35d07279603c9a8a9c01d782c844872cf974829
Zhenzhi <caizhenzhi5@gmail.com>
2024-08-22 22:24:42 +0800

    Feat: renamed the bot to Mediell

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mediell User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mediell.java
@@ -0,0 +1,5 @@
+public class Mediell {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm Mediell!");
+    }
+}
