5c53eb2921e972b3ddfffd982e91089608f48706
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-08-22 08:56:07 +0800

    chatbot named - level 0 done

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Schedulo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Schedulo.java
@@ -0,0 +1,10 @@
+public class Schedulo {
+    public static void main(String[] args) {
+        System.out.println("Hello I am Schedulo!");
+        System.out.println("What can I do for you?");
+        String horizontalLine = "---------------";
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
