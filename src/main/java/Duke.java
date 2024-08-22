0c090c68adaa0c32dbfb4f43b6adf8b3796d21db
Cheng Zihao <czh152103@gmail.com>
2024-08-22 16:35:18 +0800

    added name to bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
+        String break_line = "——————————————————————————";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(break_line);
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm ChattyBuddy");
+        System.out.println("What can I do for you?");
+        System.out.println(break_line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(break_line);
     }
 }
