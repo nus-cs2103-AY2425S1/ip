5fff44e968002bad9c9d9c7a111036a44a511941
bbryant824 <bowenbryant24@gmail.com>
2024-08-22 13:43:23 +0800

    lv 2 update 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        
+        String logo = " K   K   OOOOO   BBBBB   EEEEE \n"
+                + " K  K   O     O  B    B  E     \n"
+                + " KKK    O     O  BBBBB   EEEE  \n"
+                + " K  K   O     O  B    B  E     \n"
+                + " K   K   OOOOO   BBBBB   EEEEE \n";
+
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke" + logo);
+        System.out.println("Hello! I'm Kobe :D \n" + logo);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
