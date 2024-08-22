23b6a88f8ca5f8ad5cb02217d641fe278ea4274d
Karthikeyan <145328000+SKarthikeyan28@users.noreply.github.com>
2024-08-22 15:12:47 +0800

    Added commands to echo user

--- src/main/java/Tars.java
+++ src/main/java/Tars.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Tars {
     public static void main(String[] args) {
-        System.out.println("_____________________________________________");
-        System.out.println("Hello! I'm Tars\n" + "What can I do for you");
-        System.out.println("_____________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________________");
+        //for welcome message of chatbot
+        System.out.println("    _____________________________________________");
+        System.out.println("    Hello! I'm Tars\n" + "    What can I do for you");
+        System.out.println("    _____________________________________________");
+
+
+        Scanner scanner = new Scanner(System.in); //initalising scanner to read inputs from user
+        String entry = scanner.nextLine(); //storing string input in a variable
+
+        //while loop to ensure termination of programme only when "bye" input
+        while(!entry.equals("bye")){
+            System.out.println("    _____________________________________________");
+            System.out.println("    " + entry);
+            System.out.println("    _____________________________________________");
+
+            entry = scanner.nextLine(); //updating variable to next input entry
+        }
+
+        //exit message when given input "bye"
+        System.out.println("    _____________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    _____________________________________________");
     }
 }
