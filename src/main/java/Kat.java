c57a74d72ba03eb83fde9869e2ae129329b38b51
Airiinnn <alex.liswandy@hotmail.com>
2024-08-22 20:57:13 +0800

    Rename Duke to Kat

---
+++ src/main/java/Kat.java
@@ -0,0 +1,12 @@
+public class Kat {
+  public static void main(String[] args) {
+    String logo = """
+         _         _  \s
+        | | __ ___| |_\s
+        | |/ / _  | __|
+        |   < (_| | |_\s
+        |_|\\_\\____|\\__|
+        """;
+    System.out.println("Hello from\n" + logo);
+  }
+}
