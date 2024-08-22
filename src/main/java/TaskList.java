d4773495667ee1b686db380e9e42907a44bf6f3f
uniqly <simontong09@gmail.com>
2024-08-22 19:57:41 +0800

    TaskList.java: Add checkTask() method

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -23,22 +23,38 @@ public class TaskList implements Iterable<Task> {
         return tasks.iterator();
     }
 
+    public void checkTask(int index) throws TaskListIndexOutOfBoundsException {
+        // checks if there is a task at the given index
+        // if not throw TaskListIndexOutOfBoundsException
+        if (index < 0 || index >= tasks.size()) {
+            String message = String.format(
+                    """
+                            Whats this? You tried an invalid list index!?
+                            Pfft... That's so hilarious!
+                            Lemme spell it out for ya.
+                            Your number must be between 1 and %d and clearly %d isn't""",
+                    tasks.size(),
+                    index + 1);
+            throw new TaskListIndexOutOfBoundsException(message);
+        }
+    }
+
     public void addTask(Task task) {
         tasks.add(task);
     }
 
-    public Task getTask(int index) {
-        // assumes valid index
+    public Task getTask(int index) throws TaskListIndexOutOfBoundsException {
+        checkTask(index);
         return tasks.get(index);
     }
 
-    public void markComplete(int index) {
-        // assumes valid index
+    public void markComplete(int index) throws TaskListIndexOutOfBoundsException {
+        checkTask(index);
         tasks.get(index).markAsDone();
     }
 
-    public void markIncomplete(int index) {
-        // assumes valid index
+    public void markIncomplete(int index) throws TaskListIndexOutOfBoundsException {
+        checkTask(index);
         tasks.get(index).markAsUndone();
     }
 }
