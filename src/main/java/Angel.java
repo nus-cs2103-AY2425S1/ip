854e70f58c5955c9f954712cb034d8c018c67553
QuekBingHeng <quekbingheng.17@gmail.com>
2024-08-22 20:46:34 +0800

    add Level-3, Angel.java

--- src/main/java/Angel.java
+++ src/main/java/Angel.java
@@ -4,6 +4,7 @@ public class Angel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         String logo = "____________________________________________________________\n";
@@ -17,13 +18,29 @@ public class Angel {
                 System.out.println(logo + " Bye. Hope to see you again soon!\n" + logo);
                 break;
             } else if (userInput.equals("list")) {
-                System.out.println(logo);
+                System.out.println(logo + " Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "[X]" : "[ ]";
+                    System.out.println(" " + (i + 1) + "." + status + " " + tasks[i]);
                 }
                 System.out.println(logo);
+            } else if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    isDone[taskIndex] = true;
+                    System.out.println(logo + " Nice! I've marked this task as done:\n"
+                            + "   [X] " + tasks[taskIndex] + "\n" + logo);
+                }
+            } else if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    isDone[taskIndex] = false;
+                    System.out.println(logo + " OK, I've marked this task as not done yet:\n"
+                            + "   [ ] " + tasks[taskIndex] + "\n" + logo);
+                }
             } else {
                 tasks[taskCount] = userInput;
+                isDone[taskCount] = false;  // Default status is not done
                 taskCount++;
                 System.out.println(logo + " added: " + userInput + "\n" + logo);
             }
