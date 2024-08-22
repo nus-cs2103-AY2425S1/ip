72236bfdcbabc45968b88f8c136a6f89af7b3cdc
CJianzhi <cjianzhi23@gmail.com>
2024-08-22 10:44:52 +0800

    Add user greeting and exit

---
+++ src/main/java/Bimo.java
@@ -0,0 +1,12 @@
+public class Bimo {
+    public static String name = "Bimo";
+    public static String line = "__________________________________";
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println(String.format("Hello! I'm %s", name));
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
