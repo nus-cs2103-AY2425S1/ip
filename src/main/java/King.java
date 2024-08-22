39bbeef7fdc0b3a4ac843333e67b9a9a8abb998b
ryenl <ryanljh9@gmail.com>
2024-08-22 14:14:44 +0800

    Level-1

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -1,4 +1,6 @@
-public class King {
+import java.util.Scanner;
+
+class King {
     public static void main(String[] args) {
         String logo = " _  __ _             \n" +
                 "| |/ /(_)_ __   __ _ \n" +
@@ -6,12 +8,26 @@ public class King {
                 "| . \\ | | | | | (_| |\n" +
                 "|_|\\_\\|_|_| |_|\\__, |\n" +
                 "               |___/ ";
+
         System.out.println("Greetings from the\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println("How may I assist you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Goodbye! Have a pleasant day my Queen.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+        System.out.println("You are dismissed my loyal subject.");
         System.out.println("____________________________________________________________");
 
+        scanner.close();
     }
 }
