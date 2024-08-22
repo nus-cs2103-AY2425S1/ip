b374a6493ab9b1fa12e02b347ca62316c1bb5c1c
Jabez Tho <jabeztho@gmail.com>
2024-08-23 03:02:21 +0800

    added indent print and horizontal line wrapper

--- src/main/java/Nixy.java
+++ src/main/java/Nixy.java
@@ -1,17 +1,11 @@
 import java.util.Scanner;
 
 public class Nixy {
+    static final String HORIZONTAL_LINE = "____________________________________________________________";
     public static void main(String[] args) {
         String chatbotName = "Nixy";
-        printHorizontalLine();
-        System.out.println("Hello! I'm " + chatbotName);
-        System.out.println("What can I do for you?");
-        printHorizontalLine();
+        indentPrint(wrapWithHorizontalLines("Hello! I'm " + chatbotName, "What can I do for you?"));
         echo();
-    }
-
-    private static void printHorizontalLine() {
-        System.out.println("____________________________________________________________");
     }
 
     private static void echo() {
@@ -21,15 +15,31 @@ public class Nixy {
             exit();
             return;
         }
-        printHorizontalLine();
-        System.out.println(input);
-        printHorizontalLine();
+        indentPrint(wrapWithHorizontalLines(input));
         echo();
     }
 
     private static void exit() {
-        printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printHorizontalLine();
+        indentPrint(wrapWithHorizontalLines("Bye. Hope to see you again soon!"));
+    }
+
+    private static String[] wrapWithHorizontalLines(String... lines) {
+        String[] wrappedLines = new String[lines.length + 2];
+        wrappedLines[0] = HORIZONTAL_LINE;
+        System.arraycopy(lines, 0, wrappedLines, 1, lines.length);
+        wrappedLines[wrappedLines.length - 1] = HORIZONTAL_LINE;
+        return wrappedLines;
+    }
+
+    private static void indentPrint(String... lines) {
+        for (String line : lines) {
+            System.out.println("    " + line);
+        }
+    }
+
+    private static void normalPrint(String... lines) {
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
