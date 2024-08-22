3f31667433197271dc5473f6be40caf0ededa13a
muhdbhz <e1122341@u.nus.edu>
2024-08-22 15:49:27 +0800

    Rename Duke.java to Moody.java

---
+++ src/main/java/Moody.java
@@ -0,0 +1,10 @@
+public class Moody {
+    public static void main(String[] args) {
+        String spacer = "____________________________________________________________\n";
+        System.out.println(spacer +
+                "Hello! I'm Moody!\nWhat can I do for you?\n"
+                + spacer
+                + "Bye. Hope to see you again soon!\n"
+                + spacer);
+    }
+}
