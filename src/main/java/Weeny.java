8ba0ea41b11655cdf4210e3b583ddd9fcd033e68
clarud <clarencejw@gmail.com>
2024-08-23 02:20:02 +0800

    Level-3

--- src/main/java/Weeny.java
+++ src/main/java/Weeny.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Weeny {
     public static void main(String[] args) {
         boolean farewell = false;
-        List<String> tasks = new ArrayList<>();
+        List<Task> data = new ArrayList<>();
         Scanner user_input = new Scanner(System.in);
 
         line();
@@ -15,22 +15,48 @@ public class Weeny {
 
         while (!farewell) {
             String input = user_input.nextLine();
+            String[] inputParts = input.split(" ", 2);
+            String command = inputParts[0];
 
-            if (input.equals("list")) {
+            if (command.equals("list")) {
                 line();
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < data.size(); i++) {
+                    System.out.println((i + 1) + ". " + data.get(i).toString());
                 }
                 line();
-            } else if (input.equals("bye")) {
+            } else if (command.equals("bye")) {
                 farewell = true;
                 line();
                 System.out.println("Bye. Hope to see you again soon!");
                 line();
+            } else if (command.equals("mark")) {
+                if (inputParts.length >= 2) {
+                    int val = Integer.parseInt(inputParts[1]) - 1;
+                    if (val >= 0 && val < data.size()) {
+                        data.get(val).mark();
+                        line();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(data.get(val).toString());
+                        line();
+                    }
+                }
+            } else if (command.equals("unmark")) {
+                if (inputParts.length >= 2) {
+                    int val = Integer.parseInt(inputParts[1]) - 1;
+                    if (val >= 0 && val < data.size()) {
+                        data.get(val).unmark();
+                        line();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println(data.get(val).toString());
+                        line();
+                    }
+                }
             } else {
-                tasks.add(input);
+                Task newTask = new Task(input);
+                data.add(newTask);
                 line();
-                System.out.println("added: " + input);
+                System.out.println("Added: " + newTask.toString());
                 line();
             }
         }
