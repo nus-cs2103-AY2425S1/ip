0539d52b11d76b84a0793a7ab4d8a8ce064287db
jevan13 <jevan@jevans-MacBook-Pro.local>
2024-08-22 23:23:57 +0800

    Echo

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Yapper {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Yapper \nWhat can I do for you? \n" +
-                "Bye. Hope to see you soon! \n");
+        // greeting message
+        System.out.println("Hello! I'm Yapper \n What can I do for you? \n");
+        
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String text = sc.next();
+            if (text.equals("bye")) {
+                exit();
+                break;
+            } else {
+                echo(text);
+            }
+        }
+    }
+
+    // echos command entered by the user
+    public static void echo(String text)
+    {
+        System.out.println(text);
+    }
+
+    // exits when the user types the command "bye"
+    public static void exit()
+    {
+        System.out.println("Yapper shall yap next time!");
     }
 }
