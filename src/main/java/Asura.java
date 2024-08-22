ac31b239a20fae708e3a21446b9619411fda7cbc
ossaris56 <ossaris56@gmail.com>
2024-08-23 02:59:16 +0800

    Renamed chatbot, add greet and exit

---
+++ src/main/java/Asura.java
@@ -0,0 +1,10 @@
+public class Asura {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("Hello! I'm Asura!");
+        System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------------------");
+    }
+}
