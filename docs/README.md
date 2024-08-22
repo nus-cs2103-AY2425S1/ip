e3703f36fe14a5a42ab0360ad519ce8ae048b56e
maertan <119797875+maertan@users.noreply.github.com>
2024-08-22 14:37:39 +0800

    Rename Duke to Lovelace and add skeletal version

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Lovelace User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Lovelace.java
@@ -0,0 +1,11 @@
+public class Lovelace {
+    public static void main(String[] args) {
+        final String LINE = "____________________________________________________________\n";
+        String welcomeMessage = "Hello! I'm Lovelace\n"
+                + "What can I do for you?\n";
+        String exitMessage = "Bye. Hope to see you again soon!\n";
+
+
+        System.out.println(LINE + welcomeMessage + LINE + exitMessage + LINE);
+    }
+}
