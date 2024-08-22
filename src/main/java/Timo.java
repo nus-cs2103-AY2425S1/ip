44d1bf2b742ce67ad4c6c30249bb509ec75b8614
teamotitan <timothy.tanzijing@gmail.com>
2024-08-22 10:46:12 +0800

    level 3

--- src/main/java/Timo.java
+++ src/main/java/Timo.java
@@ -1,7 +1,37 @@
 import java.util.Scanner;
 
+class Task {
+    private boolean mark;
+    private String val;
+    public Task(boolean mark, String val) {
+        this.mark = mark;
+        this.val = val;
+    }
+
+    public void markDone() {
+        this.mark = true;
+        return;
+    }
+
+    public void markUndone() {
+        this.mark = false;
+        return;
+    }
+
+    public String getStatusIcon() {
+        return (this.mark ? "X" : " ");
+    }
+
+    public String value() {
+        return this.val;
+    }
+}
+
+
+
 public class Timo {
     public static void main(String[] args) {
+        //greet
         System.out.println("----------------------------");
         System.out.println("Hello! I'm Timo\nWhat can I do for you?");
         System.out.println("----------------------------");
@@ -10,7 +40,7 @@ public class Timo {
         Scanner echo = new Scanner(System.in);
 
         //initialise array to store the values
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int index = 1;
 
 
@@ -20,6 +50,7 @@ public class Timo {
         boolean is_bye = false;
         while (!is_bye) {
             input = echo.nextLine();
+            //determine actions based on input
             switch (input) {
                 case "bye":
                     System.out.println("----------------------------");
@@ -30,19 +61,45 @@ public class Timo {
 
                 case "list":
                     System.out.println("----------------------------");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 1; i < index; i++) {
-                        System.out.println(i + ". " + arr[i - 1]);
+                        Task chosen = arr[i - 1];
+                        System.out.println(i + ". " + "[" + chosen.getStatusIcon() + "] " + chosen.value());
                     }
                     System.out.println("----------------------------");
                     break;
 
                 default:
-                    System.out.println("----------------------------");
-                    System.out.println("added: " + input);
-                    System.out.println("----------------------------");
-                    arr[index - 1] = input;
-                    index++;
-                    break;
+                    if (input.startsWith("mark")) {
+                        //to mark
+                        String num = String.valueOf(input.charAt(input.length() - 1));
+                        int target = Integer.parseInt(num);
+                        System.out.println(target);
+                        Task chosen = arr[target - 1];
+                        chosen.markDone();
+                        System.out.println("----------------------------");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + "[" + chosen.getStatusIcon() + "] " + chosen.value());
+                        System.out.println("----------------------------");
+                    } else if (input.startsWith("unmark")) {
+                        //to unmark
+                        String num = String.valueOf(input.charAt(input.length() - 1));
+                        int target = Integer.parseInt(num);
+                        Task chosen = arr[target - 1];
+                        chosen.markUndone();
+                        System.out.println("----------------------------");
+                        System.out.println("Nice! I've marked this task as not done yet:");
+                        System.out.println("  " + "[" + chosen.getStatusIcon() + "] " + chosen.value());
+                        System.out.println("----------------------------");
+                    } else {
+                        //to add to list
+                        System.out.println("----------------------------");
+                        System.out.println("added: " + input);
+                        System.out.println("----------------------------");
+                        arr[index - 1] = new Task(false, input);
+                        index++;
+                        break;
+                    }
             }
         }
 
