a52fa229ce2710da7b10ad16a74d5e156dedd352
miloaisdino <miloaisdino@gmail.com>
2024-08-23 02:18:15 +0800

    refactor: Use switch statements instead

--- src/main/java/IpMan.java
+++ src/main/java/IpMan.java
@@ -17,24 +17,38 @@ public class IpMan {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(SEPARATOR);
                 break;
-            } else if (line.equalsIgnoreCase("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < nextIdx; i++){
-                    System.out.println((i + 1) + ". " + list[i]);
+            }
+
+            switch (line.split(" ")[0]) {
+                case "list": {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < nextIdx; i++) {
+                        System.out.println((i + 1) + ". " + list[i]);
+                    }
+                    break;
                 }
-            } else if (line.toLowerCase().contains("unmark")) {
-                int idx = Integer.parseInt(line.split(" ")[1]) - 1;
-                if (idx < 0 || idx > nextIdx) { continue; }
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(list[idx].unmarkStatus());
-            } else if (line.toLowerCase().contains("mark")) {
-                int idx = Integer.parseInt(line.split(" ")[1]) - 1;
-                if (idx < 0 || idx > nextIdx) { continue; }
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(list[idx].markStatus());
-            } else {
-                System.out.println("added: " + line);
-                list[nextIdx++] = new Task(line);
+                case "unmark": {
+                    int idx = Integer.parseInt(line.split(" ")[1]) - 1;
+                    if (idx < 0 || idx > nextIdx) {
+                        continue;
+                    }
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(list[idx].unmarkStatus());
+                    break;
+                }
+                case "mark": {
+                    int idx = Integer.parseInt(line.split(" ")[1]) - 1;
+                    if (idx < 0 || idx > nextIdx) {
+                        continue;
+                    }
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(list[idx].markStatus());
+                    break;
+                }
+                default: {
+                    System.out.println("added: " + line);
+                    list[nextIdx++] = new Task(line);
+                }
             }
             System.out.println(SEPARATOR);
         }
