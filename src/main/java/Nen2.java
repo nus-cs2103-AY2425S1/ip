6511b06d3b5673cc7510a36f292954bb66c628bc
ny <ny@rks-Laptop.local>
2024-08-22 15:48:29 +0800

    Nen2.java: Add printList() method to print all the text input by user

--- src/main/java/Nen2.java
+++ src/main/java/Nen2.java
@@ -1,40 +1,56 @@
 import java.util.Scanner;
 
 public class Nen2 {
-    private static String logo = " _   _                 __  \n"
+    private final static String logo = " _   _                 __\n"
             + "| \\ | |  ___  _ ___   |_  \\ \n"
             + "|  \\| | / _ \\| '__ |    ) |\n"
             + "| |\\  ||  __/| | | |   / /_ \n"
             + "|_| \\_| \\___||_| |_|  |____|\n";
-    private static String seperator = "--------------------------------------------";
+    private final static String separator = "--------------------------------------------";
     private static Scanner messageReader = new Scanner(System.in);
 
+    private static String[] listOfText = new String[100];
+    private static int amountOfText = 0;
+
     public static void main(String[] args) {
-        Nen2.greet();
-        Nen2.processMessage();
-        Nen2.exit();
+        greet();
+        processMessage();
+        exit();
     }
 
-    public static void processMessage() {
+    private static void processMessage() {
         String message = messageReader.nextLine();
-        System.out.println(Nen2.seperator);
+        System.out.println(separator);
 
-        if (message.equals("bye")) {
-            return;
-        } else {
-            System.out.println(message);
+        switch(message) {
+            case "bye":
+                return;
+            case "list":
+                printList();
+                break;
+            default:
+                System.out.println("added: " + message);
+                listOfText[amountOfText] = message;
+                amountOfText++;
         }
-        System.out.println(Nen2.seperator);
-        Nen2.processMessage();
+
+        System.out.println(separator);
+        processMessage();
+    }
+
+    private static void printList() {
+        for (int i = 0; i < amountOfText; i++) {
+            System.out.println(String.valueOf(i + 1) + ". " + listOfText[i]);
+        }
     }
 
     /**
      * Greets user by printing out logo and greeting messages
      */
     public static void greet() {
-        System.out.println(Nen2.seperator);
+        System.out.println(separator);
         System.out.println(logo + "Hello! I'm Nen2 \nWhat can I do for you?");
-        System.out.println(Nen2.seperator);
+        System.out.println(separator);
     }
 
     /**
@@ -42,6 +58,6 @@ public class Nen2 {
      */
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(Nen2.seperator);
+        System.out.println(separator);
     }
 }
