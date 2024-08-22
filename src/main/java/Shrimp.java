49091a877b57d45876eba9b732d85e790c7256c0
Jing Hong Tee <e1122714@u.nus.edu>
2024-08-22 22:38:16 +0800

    Rename chatbot and implement initial greeting and exit functionality
    - Renamed the chatbot to shrimp.
    - Implemented a basic skeleton of the chatbot that greets the user upon start and exits after displaying a goodbye message.
    - Replaced the source file name from Duke.java to Shrimp.java to reflect the new name.

---
+++ src/main/java/Shrimp.java
@@ -0,0 +1,31 @@
+public class Shrimp {
+
+    private static final String PARTITION = "____________________________________________________________\n";
+
+    public static void main(String[] args) {
+        programStart();
+        programExit();
+    }
+
+    static void programStart() {
+        String greetings = "Domo! Same desu~ I am shrimp, and I am happy to assist you! Hewwo? <3\n";
+        String logo = """
+                
+                       .__          .__               \s
+                  _____|  |_________|__| _____ ______ \s
+                 /  ___/  |  \\_  __ \\  |/     \\\\____ \\\s
+                 \\___ \\|   Y  \\  | \\/  |  Y Y  \\  |_> >
+                /____  >___|  /__|  |__|__|_|  /   __/\s
+                     \\/     \\/               \\/|__|   \s
+                                                      \s
+                """;
+        String output = PARTITION + greetings + logo + "\n" + PARTITION;
+        System.out.println(output);
+    }
+
+    static void programExit() {
+        String output = "Byebye~ It's time to say goodbye for the day~ Hope you enjoyed and had fuuun~ " +
+                "I'll see you later~\n" + PARTITION;
+        System.out.println(output);
+    }
+}
