f318cc8dc13ff179e88b1d9d448c77b5856d1714
S-K-Y-Light <s84915656@gmail.com>
2024-08-22 20:48:56 +0800

    Add Echo Feature using Scanner

--- src/main/java/Sora.java
+++ src/main/java/Sora.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Sora {
     public static void main(String[] args) {
         String greeting = "Hello! I'm Sora!\nWhat can I do for you?\n";
         System.out.println(greeting);
+
+        Scanner commandScanner = new Scanner(System.in);
+        while (true) {
+            String command = commandScanner.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(command);
+        }
 
         String exit = "Bye. Hope to see you again soon!\n";
         System.out.println(exit);
