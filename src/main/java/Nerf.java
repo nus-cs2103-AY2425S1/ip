8f7251525b252c89bd3cb4c75fd070cc8469cd04
KennethTeo2002 <kenneth.teo2002@gmail.com>
2024-08-22 09:27:43 +0800

    Rename chatbot to Nerf

---
+++ src/main/java/Nerf.java
@@ -0,0 +1,19 @@
+public class Nerf {
+    private static final int LINE_LENGTH = 20;
+    private static final String LOGO = "##  ##   ######   #####    ######\n" 
+                                    +  "### ##   ##       ##  ##   ##    \n"
+                                    +  "######   ##       ##  ##   ##    \n"
+                                    +  "######   ####     #####    ####  \n"
+                                    +  "## ###   ##       ####     ##    \n"
+                                    +  "##  ##   ##       ## ##    ##    \n"
+                                    +  "##  ##   ######   ##  ##   ##    \n";
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void greetings(){
+        System.out.println("-".repeat(Nerf.LINE_LENGTH));
+        System.out.println("Hello from\n");
+    }
+}
