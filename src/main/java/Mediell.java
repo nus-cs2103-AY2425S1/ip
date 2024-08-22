5a229443bf57e3e97b1f4d9fc345b7a9062fc1ca
Zhenzhi <caizhenzhi5@gmail.com>
2024-08-22 22:31:02 +0800

    Feat: greets the user and exits

--- src/main/java/Mediell.java
+++ src/main/java/Mediell.java
@@ -1,5 +1,14 @@
 public class Mediell {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Mediell!");
+        printLine();
+        System.out.println("Hello! I'm Mediell!");
+        System.out.println("What can I do for you? :)");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon! :(");
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println("-------------------------------------------");
     }
 }
