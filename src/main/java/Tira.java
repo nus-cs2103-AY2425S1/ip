98368c2c9b8b274e6a4c96ecafc697a0d28fcb13
Vicelya Visakha <>
2024-08-22 17:05:57 +0800

    Add Level 2. Add, List
    Use ArrayList to keep track of the commands.

--- src/main/java/Tira.java
+++ src/main/java/Tira.java
@@ -1,21 +1,34 @@
 import java.util.Scanner;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Tira {
     public static void main(String[] args) {
+        // variable declarations
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         PrintWriter printer = new PrintWriter(System.out);
         String logo = "TIRAMISU THE CAFE CAT (TIRA)";
+        ArrayList<String> userInputs = new ArrayList<String>();
+        // printing
         System.out.println("MIAO (Hello) from\n" + logo + "\n" +
                 "What can I do for you, miao?\n");
+        // check the user input
         while (true) {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
                 break;
             } else {
-                printer.print(command + "\n");
+                printer.print("added: " + command + "\n");
                 printer.flush();
+                if (command.equals("list")) {
+                    for (int i = 0; i < userInputs.size(); i++) {
+                        printer.print((i + 1) + ". " + userInputs.get(i) + "\n");
+                        printer.flush();
+                    }
+                } else {
+                    userInputs.add(command);
+                }
             }
         }
        printer.println("Bye. Come back with treats, miao!");
