f88251d5037c81b54b9e3812f7aa46ee8b8e554d
DINESH1201 <dinesh1201@live.com.sg>
2024-08-23 01:56:02 +0800

    Level 1. Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -11,7 +11,20 @@ public class Bob {
 
         System.out.print(DIVIDER + GREETINGS + DIVIDER);
 
+        chatBot();
+
         System.out.print(EXIT + DIVIDER);
     }
 
+    public static void chatBot() {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        if (Objects.equals(input, "bye")) {
+            System.out.print(DIVIDER);
+            return;
+        }
+        System.out.print(DIVIDER + input + "\n" + DIVIDER);
+        chatBot();
+    }
 }
