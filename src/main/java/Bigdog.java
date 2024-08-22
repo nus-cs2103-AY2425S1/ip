fa9eb4bbca489a71f261b8769aece572afee3f5e
weibinlim <limweibin0816@gmail.com>
2024-08-22 21:11:00 +0800

    Level 2: Add, List

--- src/main/java/Bigdog.java
+++ src/main/java/Bigdog.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
+
 public class Bigdog {
+
+    // Array to store tasks
+    static String[] toDoList = new String[100];
+    // Counter to keep track of the number of tasks
+    static int taskCounter = 0;
+
     public static void main(String[] args) {
 
         // Scanner object to read user input
@@ -18,10 +25,20 @@ public class Bigdog {
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.print("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+
+                // Print the list
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println((i +1) + ". " + toDoList[i]);
+                }
+            } else {
+
+                // Add and Echo user input
+                toDoList[taskCounter] = userInput;
+                taskCounter++;
+                System.out.println("added: " + userInput);
             }
 
-            // Echo user input
-            System.out.println(userInput);
         }
 
         scanner.close();
