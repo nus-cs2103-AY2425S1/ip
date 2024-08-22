cff6c7e2d9aa468eb2cbf36982f423f48ab30ead
hj235 <hongjing235@gmail.com>
2024-08-22 19:09:38 +0800

    moved control flow into switch statement

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -7,17 +7,17 @@ public class Bob {
 
     private static String greeting() {
         return Bob.lineFormat(" Hello! I'm Bob\n" +
-                " What can I do for you?\n");
+                " What can I do for you?");
     }
     private static String farewell() {
-        return  Bob.lineFormat(" Bye. Hope to see you again soon!\n");
+        return  Bob.lineFormat(" Bye. Hope to see you again soon!");
     }
     private static String lineFormat(String input) {
-        return Bob.line + input + Bob.line;
+        return Bob.line + input + "\n" + Bob.line;
     }
     private static void addTask(String command) {
         Bob.taskList.addTask(command);
-        System.out.println(Bob.lineFormat("Added: " + command + "\n"));
+        System.out.println(Bob.lineFormat("Added: " + command));
     }
     private static void listCommands() {
         System.out.print(Bob.lineFormat("Here are the tasks in your list:\n" + Bob.taskList.toString()));
@@ -29,38 +29,38 @@ public class Bob {
         String[] arguments;
         Scanner scanner = new Scanner(System.in);
 
+        outerLoop:
         while (true) {
-            System.out.println("\nEnter your command: ");
+            System.out.println("Enter your command: ");
             input = scanner.nextLine();
             arguments = input.split(" ");
 
-            if (input.compareTo("bye") == 0) {
-                System.out.println(Bob.farewell());
-                break;
+            switch (arguments[0]) {
+                case ("bye"):
+                    System.out.println(Bob.farewell());
+                    break outerLoop;
+                case ("list"):
+                    Bob.listCommands();
+                    continue;
+                case ("mark"): {
+                    int idx = Integer.parseInt(arguments[1]);
+                    Bob.taskList.mark(idx);
+                    String temp = "Nice! I've marked this task as done:\n"
+                            + Bob.taskList.describeTask(idx);
+                    System.out.println(Bob.lineFormat(temp));
+                    continue;
+                }
+                case ("unmark"): {
+                    int idx = Integer.parseInt(arguments[1]);
+                    Bob.taskList.unmark(idx);
+                    String temp = "OK, I've marked this task as not done yet:\n"
+                            + Bob.taskList.describeTask(idx);
+                    System.out.println(Bob.lineFormat(temp));
+                    continue;
+                }
+                default:
+                    Bob.addTask(input);
             }
-            if (input.compareTo("list") == 0) {
-                Bob.listCommands();
-                continue;
-            }
-
-            if (arguments.length == 2 && arguments[0].compareTo("mark") == 0) {
-                int idx = Integer.parseInt(arguments[1]);
-                Bob.taskList.mark(idx);
-                String temp = "Nice! I've marked this task as done:\n"
-                        + Bob.taskList.describeTask(idx) + "\n";
-                System.out.println(Bob.lineFormat(temp));
-                continue;
-            }
-            if (arguments.length == 2 && arguments[0].compareTo("unmark") == 0) {
-                int idx = Integer.parseInt(arguments[1]);
-                Bob.taskList.unmark(idx);
-                String temp = "OK, I've marked this task as not done yet:\n"
-                        + Bob.taskList.describeTask(idx) + "\n";
-                System.out.println(Bob.lineFormat(temp));
-                continue;
-            }
-
-            Bob.addTask(input);
         }
     }
 }
