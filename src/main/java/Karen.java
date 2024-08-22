1efb2c106c4e2ad0444f60b76370cb114c79da48
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-22 17:37:42 +0800

    Added echo functionality, which repeats user input

--- src/main/java/Karen.java
+++ src/main/java/Karen.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class Karen {
     public static void main(String[] args) {
-        String line = "_______________________\n";
+        final String line = "_______________________\n";
         String output = line +
                         "Hi! I'm Karen\n" +
                         "What can I do for you?\n" +
-                        line +
-                        "Bye. Hope to see you again soon!\n" +
                         line;
 
         System.out.print(output);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                output = line +
+                        "Bye! Hope to see you again!\n" +
+                        line;
+                System.out.println(output);
+                break;
+            } else {
+                output = line +
+                         input + "\n" +
+                         line;
+                System.out.println(output);
+            }
+        }
 
     }
 }
