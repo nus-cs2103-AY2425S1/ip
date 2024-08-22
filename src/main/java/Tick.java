e82629669060a720bb486670c039270732648f02
wrjgold <trexwrj@gmail.com>
2024-08-22 13:33:57 +0800

    Add greeting and reformat to OOP

--- src/main/java/Tick.java
+++ src/main/java/Tick.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Tick {
+    private static final String separator = "____________________________________________________________\n";
+    private static final String botName = "Tick";
+
+    public void intro() {
+        System.out.print(Tick.separator);
+        System.out.printf("Hello! I'm %s\n", Tick.botName);
+        System.out.println("What can I do for you?");
+        System.out.print(Tick.separator);
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print(Tick.separator);
+    }
+
     public static void main(String[] args) {
-        String botName = "Tick";
-        System.out.printf("Hello! I'm %s\n", botName);
+        Tick bot = new Tick();
+        bot.intro();
+        bot.exit();
     }
 }
