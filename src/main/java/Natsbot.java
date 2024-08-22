5d1da36d84b103ffb60c19fe3161e2e164c88082
natlv <nathanlooenping@gmail.com>
2024-08-22 11:00:57 +0800

    Included the ability to store text, list it, and display it to the user when needed

--- src/main/java/Natsbot.java
+++ src/main/java/Natsbot.java
@@ -1,21 +1,30 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Natsbot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Natsbot\n" + "What can I do for you?\n");
 
         System.out.println("Type a prompt below or type 'bye' to exit the program.\n");
+
         Scanner reader = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
 
         while(true) {
-            String input = reader.next();
+            String input = reader.nextLine();
 
             if (Objects.equals(input, "bye")) {
                 System.out.println("Goodbye. Hope to see you again soon!");
                 break;
+            } else if (Objects.equals(input, "list")) {
+                for (int i = 0; i < inputs.size(); i++) {
+                    System.out.println(i + 1 + ". " + inputs.get(i));
+                }
             } else {
-                System.out.println(input);
+                inputs.add(input);
+                System.out.println("added: " + input);
             }
         }
 
