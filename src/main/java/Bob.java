321873007e7a50e179041a075a441545a1732739
Nathan Lye <nathanlye05@gmail.com>
2024-08-23 03:02:28 +0800

    add exit

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -2,13 +2,18 @@ public class Bob {
     private static final String SEPARATOR = "____________________________________________________________";
 
     private static void say(String text) {
-        System.out.println(text + "\n" + SEPARATOR + "\n");
+        System.out.print("\n" + text + "\n" + SEPARATOR);
     }
 
     private static void greet() {
-        System.out.println(SEPARATOR);
+        System.out.print(SEPARATOR);
         say("Hey there! Bob at your service.  \n" +
                 "Let’s roll up our sleeves and get to work!");
+    }
+
+    private static void exit() {
+        say("I'll be here if you need me. Catch you later!");
+        System.exit(0);
     }
 
     public static void main(String[] args) {
@@ -26,5 +31,6 @@ public class Bob {
         '----------------' '----------------' '----------------'""";
         System.out.println(logo);
         greet();
+        exit();
     }
 }
