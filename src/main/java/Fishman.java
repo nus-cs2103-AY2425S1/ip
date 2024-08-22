490ff063761328b28138b7a4183190489dad9619
Yong Quan <57471791+yongqqqq@users.noreply.github.com>
2024-08-22 14:06:31 +0800

    Update name of chatbot

---
+++ src/main/java/Fishman.java
@@ -0,0 +1,13 @@
+public class Fishman {
+    public static void main(String[] args) {
+        String logo =
+                "  _____ _     _                           \n"
+                        + " |  ___(_)___| |__  _ __ ___   __ _ _ __  \n"
+                        + " | |_  | / __| '_ \\| '_ ` _ \\ / _` | '_ \\ \n"
+                        + " |  _| | \\__ \\ | | | | | | | | (_| | | | |\n"
+                        + " |_|   |_|___/_| |_|_| |_| |_|\\__,_|_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+
+    }
+}
+
