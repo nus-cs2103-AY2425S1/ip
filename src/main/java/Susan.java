0d4932d8a564aecb28f452246b04d36926e23465
kahsuann <kahsuan@hotmail.sg>
2024-08-22 20:28:10 +0800

    ToDos, Events, Deadlines

--- src/main/java/Susan.java
+++ src/main/java/Susan.java
@@ -6,10 +6,12 @@ public class Susan {
         System.out.println("Hi there, Susan's at your service!");
         System.out.println("How may I assist you?");
 
-        // Initialisation
+        // Prepare User Input
         Scanner scanner = new Scanner(System.in);
-        List<Task> tasks = new ArrayList<>();
         String userInput = scanner.nextLine();
+
+        // Initialise Task Arraylist
+        List<Task> tasks = new ArrayList<>();
 
         // Add, List
         while (!userInput.equalsIgnoreCase("bye")) {
@@ -20,24 +22,46 @@ public class Susan {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
             }
-            // Mark as Undone
-            else if (userInput.contains("unmark")) {
-                int n = Integer.parseInt(userInput.replaceAll("[^0-9]", ""));
-                tasks.get(n - 1).undoMark();
-                System.out.println("OK. This task is not done yet:");
-                System.out.println(tasks.get(n - 1));
-            }
             // Mark as Done
-            else if (userInput.contains("mark")) {
+            else if (userInput.startsWith("mark")) {
                 int n = Integer.parseInt(userInput.replaceAll("[^0-9]", ""));
                 tasks.get(n - 1).markAsDone();
                 System.out.println("Good job! This task has been conquered:");
                 System.out.println(tasks.get(n - 1));
             }
-            // Add task to list
+            // Mark as Undone
+            else if (userInput.startsWith("unmark")) {
+                int n = Integer.parseInt(userInput.replaceAll("[^0-9]", ""));
+                tasks.get(n - 1).undoMark();
+                System.out.println("OK. This task is not done yet:");
+                System.out.println(tasks.get(n - 1));
+            }
+            // Add Task to list
             else {
-                tasks.add(new Task(userInput));
-                System.out.println("OK busy girl! added: " + userInput);
+                // ToDo
+                if (userInput.startsWith("todo")) {
+                    // Get task description
+                    tasks.add(new ToDo(userInput.replace("todo ", "").trim()));
+
+                }
+                // Deadline
+                if (userInput.startsWith("deadline")) {
+                    // Get task description and by
+                    String[] taskDetails = userInput.split("/by");
+                    tasks.add(new Deadline(taskDetails[0].replace("deadline ", "").trim(),
+                            taskDetails[1].trim()));
+                }
+                // Event
+                if (userInput.startsWith("event")) {
+                    // Get task description, from and to
+                    String[] taskDetails = userInput.split("/");
+                    tasks.add(new Event(taskDetails[0].replace("event ", "").trim(),
+                            taskDetails[1].replace("from ", "").trim(),
+                            taskDetails[2].replace("to ", "").trim()));
+                }
+                // Update Completed
+                System.out.println("OK busy girl! added: " + tasks.get(tasks.size() - 1));
+                System.out.println("You have " + tasks.size() + " tasks in the list.");
             }
             // Read next input
             userInput = scanner.nextLine();
