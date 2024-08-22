fa15e0066e81d9cb1b30bba0af466c65aec61302
Jalenleeruixian <e1122262@u.nus.edu>
2024-08-22 17:14:48 +0800

    Level 0

---
+++ src/main/java/BuddyBot.java
@@ -0,0 +1,8 @@
+public class BuddyBot {
+    public static void main(String[] args) {
+        String logo = " Hello! I'm BuddyBot\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println("Hello from\n" + logo);
+    }
+}
