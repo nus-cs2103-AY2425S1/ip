933d864b8efabccf698a020023efb0485216230f
Wei Lian <weilian2003@live.com>
2024-08-22 22:33:58 +0800

    Rename ChatBot to Joe

---
+++ src/main/java/Joe.java
@@ -0,0 +1,8 @@
+public class Joe {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hello! I'm Joe
+                What can I do for you?
+                Bye. Hope to see you again soon!""");
+    }
+}
