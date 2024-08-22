f84fbc4e0d3e83f9d608f66398e36bece165be4b
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 00:25:37 +0800

    Add "Puke" more personality

--- src/main/java/MessageBuilder.java
+++ src/main/java/MessageBuilder.java
@@ -1,7 +1,7 @@
 public class MessageBuilder {
     private void buildMessage(String message) {
         System.out.println("______________________________________________________________________");
-        System.out.println(message);
+        System.out.println(message + "*uwaeh*"); // Puke will always vomit after saying something
         System.out.println("______________________________________________________________________");
     }
 
