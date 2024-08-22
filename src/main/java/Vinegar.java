b76081d3b865b44561c5501fb175d1a898bdf414
limjeremy496 <limjeremy496@gmail.com>
2024-08-22 15:51:43 +0800

    Add support for ToDos, Events, Deadlines

--- src/main/java/Vinegar.java
+++ src/main/java/Vinegar.java
@@ -4,16 +4,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Vinegar {
+    private static List<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Task> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Vinegar\n" +
                 "What Can I do for you?");
         System.out.println("____________________________________________________________");
 
-        label:
+        chatLoop:
         while(true) {
             String userInput = scanner.nextLine();
             String[] inputParts = userInput.split(" ", 2);
@@ -24,12 +25,13 @@ public class Vinegar {
                     System.out.println("____________________________________________________________");
                     System.out.println("Bye. Hope to see you again soon!");
                     System.out.println("____________________________________________________________");
-                    break label;
+
+                    break chatLoop;
                 case "list":
                     System.out.println("____________________________________________________________");
                     System.out.println(" Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i + 1) + ".lol" + tasks.get(i));
+                        System.out.println((i + 1) + "." + tasks.get(i));
                     }
                     System.out.println("____________________________________________________________");
 
@@ -54,6 +56,29 @@ public class Vinegar {
 
                     break;
                 }
+                case "todo": {
+                    Task task = new Todo(inputParts[1]);
+                    tasks.add(task);
+                    printTaskAdded(task);
+
+                    break;
+                }
+                case "deadline": {
+                    String[] parts = inputParts[1].split(" /by ");
+                    Task task = new Deadline(parts[0], parts[1]);
+                    tasks.add(task);
+                    printTaskAdded(task);
+
+                    break;
+                }
+                case "event": {
+                    String[] parts = inputParts[1].split("/from | /to ");
+                    Task task = new Event(parts[0], parts[1], parts[2]);
+                    tasks.add(task);
+                    printTaskAdded(task);
+
+                    break;
+                }
                 default:
                     tasks.add(new Task(userInput));
                     System.out.println("____________________________________________________________");
@@ -64,5 +89,13 @@ public class Vinegar {
         }
 
         scanner.close();
+    }
+
+    private static void printTaskAdded(Task task) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + task);
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
     }
 }
