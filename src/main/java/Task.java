5c4d3da2e406293a3c9d2a2ada3d24ba89fe9a44
avrilmohh <avrilmoh1234@gmail.com>
2024-08-22 21:20:25 +0800

    Changed logic of Tudee.java with the addition of Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,41 +1,16 @@
-import java.util.List;
-import java.util.Scanner;
-import java.util.ArrayList;
+class Task {
+    String taskString;
+    boolean done;
+    Task(String taskString, boolean done) {
+        this.taskString = taskString;
+        this.done = done;
+    }
 
-public class Tudee {
-    public static void main(String[] args) {
-        String logo = "____________________________________________________________ \n"
-                + "Hello! I'm Tudee, your chatbot bestie! \n"
-                + "How can I help you today? :) \n"
-                + "____________________________________________________________ \n";
-        System.out.println(logo);
-        String input;
-        Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
-        while (true) {
-            String output = "";
-            input = sc.nextLine();
-            if (input.equalsIgnoreCase("list")) {
-                System.out.println("____________________________________________________________ \n");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i+1 + ". " + list.get(i) + "\n");
-                }
-                System.out.println("____________________________________________________________ \n");
-            }
-            else if (input.equalsIgnoreCase("bye")){
-                output = "____________________________________________________________ \n"
-                        + "Bye. Hope to see you again soon! Don't forget about me :( \n"
-                        + "____________________________________________________________";
-                System.out.println(output);
-                break;
-            }
-            else {
-                list.add(input);
-                System.out.println("____________________________________________________________ \n");
-                System.out.println("added: " + input);
-                System.out.println("____________________________________________________________ \n");
-            }
-        }
-        sc.close();
+    String getString() {
+        return this.taskString;
+    }
+
+    boolean getStatus() {
+        return this.done;
     }
 }
--- src/main/java/Tudee.java
+++ src/main/java/Tudee.java
@@ -11,14 +11,20 @@ public class Tudee {
         System.out.println(logo);
         String input;
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
         while (true) {
             String output = "";
             input = sc.nextLine();
+            Task currentTask = new Task(input, false);
             if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________ \n");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i+1 + ". " + list.get(i) + "\n");
+                    if (list.get(i).getStatus()) {
+                        System.out.println(i+1 + ". [X] " + list.get(i).getString() + "\n");
+                    }
+                    else {
+                        System.out.println(i+1 + ". [ ] " + list.get(i).getString() + "\n");
+                    }
                 }
                 System.out.println("____________________________________________________________ \n");
             }
@@ -29,8 +35,27 @@ public class Tudee {
                 System.out.println(output);
                 break;
             }
+            else if (input.contains("mark")) {
+                String[] inputArray = input.split(" ");
+                int index = Integer.parseInt(inputArray[1]) - 1;
+                String target = list.get(index).getString();
+                if (inputArray[0].equals("mark")) {
+                    list.set(index, new Task(target, true));
+                    System.out.println("____________________________________________________________ \n");
+                    System.out.println("Nice! I've marked this task as done: \n");
+                    System.out.println("  [X] " + target);
+                    System.out.println("____________________________________________________________ \n");
+                }
+                else if (inputArray[0].equals("unmark")) {
+                    list.set(index, new Task(target, false));
+                    System.out.println("____________________________________________________________ \n");
+                    System.out.println("Ok, I've marked this task as not done yet: \n");
+                    System.out.println("  [ ] " + target);
+                    System.out.println("____________________________________________________________ \n");
+                }
+            }
             else {
-                list.add(input);
+                list.add(currentTask);
                 System.out.println("____________________________________________________________ \n");
                 System.out.println("added: " + input);
                 System.out.println("____________________________________________________________ \n");
