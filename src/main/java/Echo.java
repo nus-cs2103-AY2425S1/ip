a21cea2404ccdec084abc9d193fcb9d8d6362dff
Teddayz <mervynneo02@gmail.com>
2024-08-22 21:05:05 +0800

    Add, List

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,19 +1,36 @@
-public class Echo {
-    String word;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Echo(String word) {
-        this.word = word;
+public class Echo {
+    private String word;
+    private List<String> list;
+
+    public Echo() {
+        this.list = new ArrayList<>();
     }
 
+    public void setWord(String word) {
+        this.word = word;
+    }
     public void echoOut() {
         switch (word.toLowerCase()) {
+            case "list":
+                if (list.isEmpty()) {
+                    System.out.println("No items in the list yet!\n");
+                }
+                for (int i = 0; i < list.size(); i++) {
+                    String response = String.format("%d. %s", i + 1, list.get(i));
+                    System.out.println(response);
+                }
+                break;
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!\n"
                                 +  "____________________________________________________________\n");
                 break;
             default:
+                list.add(word);
                 System.out.println("\n____________________________________________________________\n"
-                        + word + "\n____________________________________________________________\n");
+                        + "added: " + word + "\n____________________________________________________________\n");
                 break;
         }
     }
