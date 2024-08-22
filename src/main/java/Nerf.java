1cd94a15b3e37553abfa1a63531820ca8aa42a26
KennethTeo2002 <kenneth.teo2002@gmail.com>
2024-08-22 11:09:23 +0800

    Implement Mark and Unmark functionality

--- src/main/java/Nerf.java
+++ src/main/java/Nerf.java
@@ -15,7 +15,7 @@ public class Nerf {
                                        ##  ##   ######   ##  ##   ##    
                                        """;
 
-    private static final List<String> listings = new ArrayList<>();
+    private static final List<Task> listings = new ArrayList<>();
 
     public static void main(String[] args) {
         greetings();
@@ -23,10 +23,11 @@ public class Nerf {
     }
 
     private static void addToList(String input){
-        listings.add(input);
+        listings.add(new Task(input));
         System.out.println("added: " + input);
     }
     private static void printList(){
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < listings.size();i++){
             System.out.println(String.format("%d. %s",i+1,listings.get(i)));
         }
@@ -40,9 +41,46 @@ public class Nerf {
             switch(input){
                 case "bye" -> exit();
                 case "list" -> printList();
-                default -> addToList(input);
+                default -> {
+                    if (input.startsWith("mark ")) {
+                        markTask(input);
+                    } else if (input.startsWith("unmark ")) {
+                        unmarkTask(input);
+                    }
+                    else {
+                        addToList(input);
+                    }
+                }
             }
         } while (!input.equals("bye"));
+    }
+    private static void markTask(String input){
+        String number = input.substring(5).trim();
+        try {
+            int num = Integer.parseInt(number);
+            listings.get(num-1).setDone();
+            System.out.println("""
+                               Wow! Congrats on finishing another task!
+                               I've marked this task as done:
+                               """ + listings.get(num-1));
+            printDivider();
+        } catch (NumberFormatException e) {
+            addToList(input); // might be the task is to mark something
+        }
+    }
+    private static void unmarkTask(String input){
+        String number = input.substring(7).trim();
+        try {
+            int num = Integer.parseInt(number);
+            listings.get(num-1).setUndone();
+            System.out.println("""
+                               Ok noted!
+                               I've marked this task as undone:
+                               """ + listings.get(num-1));
+            printDivider();
+        } catch (NumberFormatException e) {
+            addToList(input); // might be the task is to unmark something
+        }
     }
 
     private static String userInput(){
