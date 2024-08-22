fb000f358c93027ff87e0e00a865164fda284360
ryenl <ryanljh9@gmail.com>
2024-08-22 15:26:58 +0800

    Level-3

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -18,7 +18,7 @@ class King {
         Scanner scanner = new Scanner(System.in);
 
         //Array to store tasks
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskcounter = 0;
 
         while(true) {
@@ -28,16 +28,31 @@ class King {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 for (int i = 0; i < taskcounter; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + ". " + tasks[i].toString());
                 }
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark")) {
+                String[] parts = input.split(" ");
+                //prone to error if format is off or value after " " is not an int
+                int num = Integer.parseInt(parts[1]);
+                tasks[num - 1].markAsDone();
+                System.out.println("Good job on the completion my minion!\n" + tasks[num - 1].toString());
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("unmark")) {
+                String[] parts = input.split(" ");
+                //prone to error if format is off or value after " " is not an int
+                int num = Integer.parseInt(parts[1]);
+                tasks[num - 1].markAsUndone();
+                System.out.println("Better get to work before I execute you!\n" + tasks[num - 1].toString());
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskcounter] = new Task(input);
+                taskcounter++;
+                System.out.println("____________________________________________________________");
             }
-            tasks[taskcounter] = input;
-            taskcounter++;
-            System.out.println("____________________________________________________________");
         }
-        System.out.println("You are dismissed my loyal subject.");
+        System.out.println("You are dismissed my humble servant.");
         System.out.println("____________________________________________________________");
-
         scanner.close();
     }
 }
