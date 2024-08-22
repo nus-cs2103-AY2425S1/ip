66cbcdd1cd4e681502294c295464387822c64d09
JellyPenguinnn <>
2024-08-22 22:51:05 +0800

    Level-1

--- java/ChatBuddy.java
+++ java/ChatBuddy.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class ChatBuddy {
     public static void main(String[] args) {
-         System.out.println("_____________________________________________");
-         System.out.println("Hello! I'm ChatBuddy.\nLet me know how can I assist you today?");
-         System.out.println("_____________________________________________");
-         System.out.println("Bye. Hope to see you again soon!");
-         System.out.println("_____________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("_____________________________________________");
+            System.out.println("Hello! I'm ChatBuddy.\nLet me know how can I assist you today?");
+
+            String userInput = scanner.nextLine();
+            while (!userInput.equals("bye")) {
+                System.out.println("_____________________________________________");
+                System.out.println(userInput);
+                userInput = scanner.nextLine();
+            }
+        } finally {
+            System.out.println("_____________________________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("_____________________________________________");
+        }
     }
 }
