628a200b74ea93dacd3ad602f197be2941219541
Jem Zhang <zhangjem321@gmail.com>
2024-08-22 23:04:15 +0800

    feat: added Task class. Unmark and mark tasks

--- src/main/java/Donk.java
+++ src/main/java/Donk.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Donk {
+
     public static void main(String[] args) {
         String greeting = " ____________________________________________________________\n" +
                 " Hello! I'm Donk, the super intelligent chatbot\n" +
@@ -12,23 +14,37 @@ public class Donk {
                 "____________________________________________________________\n";
         System.out.println(greeting);
 
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
+            String[] inputArray = userInput.split("\\s+");
+            if (userInput.isBlank()) {
+                continue;
+            } else if (userInput.equals("bye")) {
                 System.out.println(byeMsg);
                 break;
             } else if (userInput.equals("list")) {
                 for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println("    " + i + ": " + tasks.get(i-1));
+                    System.out.println("    " + i + ": " + tasks.get(i - 1).toString());
                 }
+            } else if (inputArray[0].equals("mark") && inputArray[1].matches("\\d+")) {
+                int index = Integer.parseInt(inputArray[1]) - 1;
+                System.out.println("    Yo I've marked this thingy as done");
+                tasks.get(index).markDone();
+                System.out.println("    " + tasks.get(index).toString());
+            } else if (inputArray[0].equals("unmark") && inputArray[1].matches("\\d+")) {
+                int index = Integer.parseInt(inputArray[1]) - 1;
+                System.out.println("    Aights now it's unmarked again");
+                tasks.get(index).unmarkDone();
+                System.out.println("    " + tasks.get(index).toString());
             } else {
-                tasks.add(userInput);
+                tasks.add(new Task(userInput));
                 System.out.println("    added: " + userInput);
             }
         }
 
     }
 }
+
