f07c79898079d652b71b9bcb0c1a6aaa5cf523f8
ethan-goh <ethan.goh.ye@gmail.com>
2024-08-22 13:21:09 +0800

    completed level 3

--- src/main/java/Genesis.java
+++ src/main/java/Genesis.java
@@ -1,6 +1,36 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
+class Task {
+    protected String description;
+    protected boolean isComplete;
+
+    public Task (String description) {
+        this.description = description;
+        this.isComplete = false;
+    }
+
+    @Override
+    public String toString() {
+        String marked;
+        if (isComplete) {
+            marked = "[X] ";
+        } else {
+            marked = "[ ] ";
+        }
+        return marked + this.description;
+    }
+
+    public void mark() {
+        this.isComplete = true;
+    }
+    public void unmark() {
+        this.isComplete = false;
+    }
+}
 public class Genesis {
+
+
     public static void main(String[] args) {
 
         /*String logo = " ____        _        \n"
@@ -11,7 +41,7 @@ public class Genesis {
         System.out.println("Hello! I'm Genesis!\n"
                 + "What can I do for you?\n");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Task> items = new ArrayList<>();
         while (true) {
             String input = sc.nextLine();
             if (input.equalsIgnoreCase("bye")) {
@@ -21,9 +51,30 @@ public class Genesis {
                 for (int i = 0; i < items.size(); i++) {
                     System.out.println(i + 1 + ". " + items.get(i));
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= items.size()) {
+                    System.out.println("No such task exists!");
+                    continue;
+                }
+                Task current = items.get(index);
+                current.mark();
+                System.out.println("Nice! I've marked this as done:\n" +
+                                    current.description);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                if (index >= items.size()) {
+                    System.out.println("No such task exists!");
+                    continue;
+                }
+                Task current = items.get(index);
+                current.unmark();
+                System.out.println("Ok. I've marked this task as not done yet:\n" +
+                        current.description);
             } else {
-                items.add(input);
-                System.out.println("added: " + input);
+                Task current = new Task(input);
+                items.add(current);
+                System.out.println("added: " + current.description);
             }
         }
 
