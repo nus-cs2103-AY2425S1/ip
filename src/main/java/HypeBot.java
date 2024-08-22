5fe5249e4b68c31d13e34183b3abd72e1751234a
youngseopark05 <e1304445@u.nus.edu>
2024-08-23 04:39:12 +0800

    Extracted add(), list() methods and added mark(), unmark() methods

--- src/main/java/HypeBot.java
+++ src/main/java/HypeBot.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class HypeBot {
     private static final String bufferLine = "______________________________________________________________________\n";
-    public static void greet() {
+    private static final ArrayList<Task> commandList = new ArrayList<>();
+
+    private static void greet() {
         String logo = """
                  ('-. .-.               _ (`-.    ('-. .-. .-')                .-') _  \s
                 ( OO )  /              ( (OO  ) _(  OO)\\  ( OO )              (  OO) ) \s
@@ -22,10 +24,43 @@ public class HypeBot {
                 + bufferLine);
     }
 
-    public static void exit() {
+    private static void exit() {
         System.out.println(bufferLine
                 + "Alright homie, it's been a BLAST hanging out with you. "
                 + "Have a wonderful\nday, and catch you soon again you ABSOLUTE BALLER!\n"
+                + bufferLine);
+    }
+
+    private static void list() {
+        System.out.println(bufferLine + "ALRIGHT, Here's that list!\n");
+        for (int i = 0; i < commandList.size(); i++) {
+            System.out.println(i + 1 + ". " + commandList.get(i));
+        }
+        System.out.println(bufferLine);
+    }
+
+    private static void add(String taskName) {
+        commandList.add(new Task(taskName));
+        System.out.println(bufferLine
+                + "HECK YEAH, ADDED: "
+                + taskName
+                + "!\n"
+                + bufferLine);
+    }
+
+    private static void unmark(int idx) {
+        commandList.get(idx).unmark();
+        System.out.println(bufferLine
+                + "AIGHT, LET'S GET READY TO CONQUER THIS TASK:\n  "
+                + commandList.get(idx)
+                + bufferLine);
+    }
+
+    private static void mark(int idx) {
+        commandList.get(idx).mark();
+        System.out.println(bufferLine
+                + "AIGHT, ABSOLUTELY CONQUERED THIS TASK:\n  "
+                + commandList.get(idx)
                 + bufferLine);
     }
 
@@ -34,29 +69,45 @@ public class HypeBot {
 
         Scanner sc = new Scanner(System.in);
         boolean canExit = false;
-        ArrayList<String> commandList = new ArrayList<>();
 
         while (!canExit) {
             String command = sc.nextLine();
-            String commandLowerCase = command.toLowerCase();
-            if (commandLowerCase.equals("bye")) {
-                canExit = true;
+
+            if (command.trim().isEmpty()) {
                 continue;
             }
 
-            if (commandLowerCase.equals("list")) {
-                System.out.println(bufferLine + "ALRIGHT, Here's that list!\n");
-                for (int i = 0; i < commandList.size(); i++) {
-                    System.out.println(i + 1 + ". " + commandList.get(i));
+            String commandLowerCase = command.toLowerCase();
+            if (commandLowerCase.equals("bye")) {
+                canExit = true;
+            } else if (commandLowerCase.equals("list")) {
+                list();
+            } else if (commandLowerCase.contains("unmark")) {
+                try {
+                    Integer idx = Integer.parseInt(commandLowerCase.split(" ")[1]) - 1;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(bufferLine
+                            + "I might be tripping bro, my bad, my bad - \n"
+                            + "try indicating the index of the task you wanna TAKE ON AGAIN!\n"
+                            + bufferLine);
+                    continue;
                 }
-                System.out.println(bufferLine);
-            } else {
-                commandList.add(command);
-                System.out.println(bufferLine
-                        + "HECK YEAH, ADDED: "
-                        + command
-                        + "!\n"
-                        + bufferLine);
+                int idx = Integer.parseInt(commandLowerCase.split(" ")[1]) - 1;
+                unmark(idx);
+            } else if (commandLowerCase.contains("mark")) {
+                try {
+                    Integer idx = Integer.parseInt(commandLowerCase.split(" ")[1]) - 1;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(bufferLine
+                            + "I might be tripping bro, my bad, my bad - \n"
+                            + "try indicating the index of the task you wanna mark CONQUERED!\n"
+                            + bufferLine);
+                    continue;
+                }
+                int idx = Integer.parseInt(commandLowerCase.split(" ")[1]) - 1;
+                mark(idx);
+            }  else {
+                add(command);
             }
         }
 
