effe3bda896d498134a33bfc3cce6fdcde1a472d
Zhenzhi <caizhenzhi5@gmail.com>
2024-08-22 22:48:07 +0800

    Feat: mediell echos back commands entered by user

--- src/main/java/Mediell.java
+++ src/main/java/Mediell.java
@@ -1,9 +1,24 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Mediell {
     public static void main(String[] args) {
         printLine();
         System.out.println("Hello! I'm Mediell!");
         System.out.println("What can I do for you? :)");
         printLine();
+        Scanner scanner = new Scanner(System.in);
+        String message = "";
+        while (true) {
+            System.out.println("");
+            message = scanner.next();
+            if (Objects.equals(message, "bye")) {
+                break;
+            }
+            System.out.println(message);
+            printLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon! :(");
         printLine();
     }
