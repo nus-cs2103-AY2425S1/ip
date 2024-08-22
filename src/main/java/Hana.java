fd5d5881a9fe2e9172e5b33fd8ad0bcc49b0288f
hansneddyanto <hans.neddy@gmail.com>
2024-08-22 17:03:57 +0800

    Add feature to delete task

--- src/main/java/Hana.java
+++ src/main/java/Hana.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 
 public class Hana {
     public static void main(String[] args) {
@@ -9,8 +10,7 @@ public class Hana {
                 "    What can I do for you?\n";
         String closing = "    Bye. Hope to see you again soon!\n";
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        Task[] tasks = new Task[100];
-        int numberOfTasks = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         // Regular expressions for mark and unmark commands
         Pattern markPattern = Pattern.compile("^mark (\\d+)$");
@@ -25,63 +25,69 @@ public class Hana {
 
             if (input.equals("list")) {
                 System.out.println(line);
-                for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks.get(i));
                 }
                 System.out.println(line);
             } else if (markMatcher.matches()) {
                 // taskNumber is index already
                 int taskNumber = Integer.parseInt(markMatcher.group(1)) - 1;
-                if (taskNumber >= 0 && taskNumber < numberOfTasks) {
-                    tasks[taskNumber].markAsDone();
+                if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                    tasks.get(taskNumber).markAsDone();
                     System.out.println(line + "     Nice! I've marked this task as done:");
-                    System.out.println("    " + tasks[taskNumber] + "\n" + line);
+                    System.out.println("    " + tasks.get(taskNumber) + "\n" + line);
                 } else {
                     System.out.println(line + "     Task number out of range.\n" + line);
                 }
             } else if (unmarkMatcher.matches()) {
                 // taskNumber is index already
                 int taskNumber = Integer.parseInt(unmarkMatcher.group(1)) - 1;
-                if (taskNumber >= 0 && taskNumber < numberOfTasks) {
-                    tasks[taskNumber].markAsUndone();
+                if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                    tasks.get(taskNumber).markAsUndone();
                     System.out.println(line + "     OK, I've marked this task as not done yet:");
-                    System.out.println("    " + tasks[taskNumber] + "\n" + line);
+                    System.out.println("    " + tasks.get(taskNumber) + "\n" + line);
                 } else {
                     System.out.println(line + "     Task number out of range.\n" + line);
                 }
             } else if (input.startsWith("todo")) {
                 try {
                     String description = input.substring(5);
-                    tasks[numberOfTasks] = new ToDo(description);
-                    numberOfTasks++;
+                    tasks.add(new ToDo(description));
                     System.out.println(line + "     Got it. I've added this task:\n" +
-                            "       " + tasks[numberOfTasks - 1] + "\n" +
-                            "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+                            "       " + tasks.get(tasks.size() - 1) + "\n" +
+                            "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
                 } catch (Exception e) {
                     System.out.println(line + "    OOPS!!! The description of a todo cannot be empty.\n" + line);
                 }
             } else if (input.startsWith("deadline")) {
                 try {
                     String[] parts = input.substring(9).split(" /by ");
-                    tasks[numberOfTasks] = new Deadline(parts[0], parts[1]);
-                    numberOfTasks++;
+                    tasks.add(new Deadline(parts[0], parts[1]));
                     System.out.println(line + "     Got it. I've added this task:\n" +
-                            "       " + tasks[numberOfTasks - 1] + "\n" +
-                            "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+                            "       " + tasks.get(tasks.size() - 1) + "\n" +
+                            "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
                 } catch (Exception e) {
                     System.out.println(line + "    OOPS!!! The description of a deadline cannot be empty.\n" + line);
                 }
             } else if (input.startsWith("event")) {
                 try {
                     String[] parts = input.substring(6).split(" /from | /to ");
-                    tasks[numberOfTasks] = new Event(parts[0], parts[1], parts[2]);
-                    numberOfTasks++;
+                    tasks.add(new Event(parts[0], parts[1], parts[2]));
                     System.out.println(line + "     Got it. I've added this task:\n" +
-                            "       " + tasks[numberOfTasks - 1] + "\n" +
-                            "     Now you have " + numberOfTasks + " tasks in the list.\n" + line);
+                            "       " + tasks.get(tasks.size() - 1) + "\n" +
+                            "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
                 } catch (Exception e) {
                     System.out.println(line + "    OOPS!!! The description of an event cannot be empty.\n" + line);
                 }
+            } else if (input.startsWith("delete")) {
+                int taskNumber = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (taskNumber < 0 || taskNumber >= tasks.size()) {
+                    System.out.println(line + "    OOPS!!! Task number out of range.\n" + line);
+                }
+                Task removedTask = tasks.remove(taskNumber);
+                System.out.println(line + "     Noted. I've removed this task:\n" +
+                        "       " + removedTask + "\n" +
+                        "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
             } else {
                 System.out.println(line + "     I'm sorry, I don't understand that command.\n" + line);
             }
