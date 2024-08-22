3bc71de11c8630019d035c0a23b03587b2d6263e
achl1012 <>
2024-08-22 14:04:39 +0800

    Echoes commands

--- src/main/java/Charlotte.java
+++ src/main/java/Charlotte.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Charlotte {
     public static void main(String[] args) {
         String line = "_________________________________________________";
-        System.out.println(line + "\nHello! I'm Charlotte!\nWhat can I do for you?\n" + line +
-                "\nBye. Hope to see you again soon!\n" + line);
+        System.out.println(line + "\n Hello! I'm Charlotte!\n What can I do for you?\n" + line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println(line + "\n " + input + "\n" + line);
+        }
+
+        System.out.println(line + "\n Bye. Hope to see you again soon!\n" + line);
+
+        scanner.close();
     }
 }
