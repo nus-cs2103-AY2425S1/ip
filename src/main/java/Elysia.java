3c051602550ed3eaec240e1ba665ba027056bac6
Elinengu <Eline.Ngu2003@gmail.com>
2024-08-22 17:16:16 +0800

    add Level-1

--- src/main/java/Elysia.java
+++ src/main/java/Elysia.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Elysia {
     static String line = "____________________________________________________________";
     static String welcomeMessage = "Hello! I'm Elysia\nWhat can I do for you?";
     static String exitMessage = "Bye. Hope to see you again soon!";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(line);
         System.out.println(welcomeMessage);
         System.out.println(line);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
         System.out.println(exitMessage);
         System.out.println(line);
     }
