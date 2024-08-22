f86f76ffb584803c295ab5981993c03a5ccd9fbc
WuJinhan1 <e1075517@u.nus.edu>
2024-08-22 17:50:02 +0800

    finished level 2

--- src/main/Jard.java/Jard.java
+++ src/main/Jard.java/Jard.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Jard {
     public static void main(String[] args) {
@@ -13,7 +15,9 @@ public class Jard {
         System.out.println(" Hello! I'm Jard");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
         Scanner scanner = new Scanner(System.in);
+        List<String> shard = new ArrayList<>();
         String input;
 
         while (true) {
@@ -23,10 +27,18 @@ public class Jard {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < shard.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + shard.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                shard.add(input);
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
