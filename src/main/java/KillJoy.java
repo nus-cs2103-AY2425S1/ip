c1074c3119df423ed587bec79a4c698dc9e4350b
thisisaditya17 <joshi.adi1734@gmail.com>
2024-08-22 13:07:14 +0800

    Killjoy.java: add text storage and retrieval feature
    - Add the ability to store user's tasks
    - Add feature to display stored tasks on user's request

--- src/main/java/KillJoy.java
+++ src/main/java/KillJoy.java
@@ -2,40 +2,57 @@ import java.util.Scanner;
 
 public class KillJoy {
 
+    private String[] tasks = new String[100];
+    private int taskCount = 0;
+
     public void start() {
-        System.out.println(this.logoString());
-        System.out.println(this.welcomeString());
+        System.out.println(this.getLogoString());
+        System.out.println(this.getWelcomeString());
 
         Scanner user = new Scanner(System.in);
 
         while(true) {
             String input = user.nextLine();
+
             if (input.equals("bye")) {
-                System.out.println(this.exitString());
+                System.out.println(this.getExitString());
                 break;
+            } else if (input.equals("list")){
+                this.printTasks();
+            } else {
+                this.processUserInput(input);
             }
-            userInput(input);
         }
     }
-    private void userInput(String input) {
-        if(input.equals("bye")) {
-            exitString();
+    private void processUserInput(String input) {
+
+        System.out.println("    ------------------------------------");
+        this.tasks[this.taskCount] = input;
+        taskCount++;
+        System.out.println("    added: " + input);
+        System.out.println("    ------------------------------------");
+
+    }
+
+    private void printTasks() {
+        System.out.println("    ------------------------------------");
+        for (int i = 0; i < this.taskCount; i++) {
+            System.out.println("    " + (i + 1) + ". " + this.tasks[i]);
         }
         System.out.println("    ------------------------------------");
-        System.out.println("    " + input);
-        System.out.println("    ------------------------------------");
     }
-    private String exitString() {
-        return "    Bye. Hope to see you again soon!\n" +
+    private String getExitString() {
+        return "   ------------------------------------\n" +
+                "    Bye. Hope to see you again soon!\n" +
                 "   ------------------------------------";
     }
 
-    private String welcomeString() {
+    private String getWelcomeString() {
         return "   Hello! I'm KillJoy \n   What can I do for you?\n" +
                 "   ------------------------------------";
     }
 
-    private String logoString() {
+    private String getLogoString() {
         return "   ------------------------------------\n" +
                 "    _  ___ _ _      _             \n" +
                 "   | |/ (_) | |    | |            \n" +
