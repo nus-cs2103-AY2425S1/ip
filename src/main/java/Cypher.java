67e8dc51e16234c457e5a90f8316a4a55feaa6e9
LinkesV <linkesvarun@gmail.com>
2024-08-22 13:17:30 +0800

    Level 2 (Add, list)

--- src/main/java/Cypher.java
+++ src/main/java/Cypher.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Cypher {
-    private static ArrayList<Task> taskList = new ArrayList<>();
+    private static ArrayList<Task> taskList;
     private static void lineBreak() {
         System.out.println("-------------------------------------------------------");
     }
@@ -17,8 +17,25 @@ public class Cypher {
         System.out.println("Bye. Hope to see you again soon!");
         Cypher.lineBreak();
     }
+
+    private static void addToList(String description) {
+        Task task = new Task(description);
+        taskList.add(task);
+        Cypher.lineBreak();
+        System.out.println("Added: " + description);
+        Cypher.lineBreak();
+    }
+
+    private static void printTaskList() {
+        Cypher.lineBreak();
+        for (int i = 0; i < Cypher.taskList.size(); i++) {
+            System.out.println((i + 1) + ". " + Cypher.taskList.get(i));
+        }
+        Cypher.lineBreak();
+    }
     public static void main(String[] args) {
         Cypher.greet();
+        Cypher.taskList = new ArrayList<>();
         String command = "";
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -26,10 +43,10 @@ public class Cypher {
             command = scanner.nextLine();
 
             // Echo
-            if (!command.equalsIgnoreCase("bye")) {
-                Cypher.lineBreak();
-                System.out.println(command);
-                Cypher.lineBreak();
+            if (command.equalsIgnoreCase("list")) {
+                Cypher.printTaskList();
+            } else if (!command.equalsIgnoreCase("bye")) {
+                Cypher.addToList(command);
             } else {
                 Cypher.lineBreak();
                 break;
