9b9a403ee887013fa2688dc03c1d4f09b94df298
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2024-08-22 22:08:55 +0800

    Convert "yyyy-mm-dd" to "MMM dd yyyy" format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,35 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    String by;
+    LocalDate newBy;
 
     public Deadline(String description, String by) {
         super(description);
+        this.newBy = convertDate(by);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + by + ")";
+        String date = (newBy != null) ? newBy.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) : by;
+        return "[D]" + super.toString() + "(by: " + date + ")";
     }
 
     @Override
     public String changeFormat() {
         return "D | " + (this.getStatusIcon().equals("X") ? "1" : "0") + " | " + this.description + " | " + this.by;
+    }
+
+    @Override
+    public LocalDate convertDate(String date) {
+        try {
+            return LocalDate.parse(date.trim(), DateTimeFormatter.ISO_DATE);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,39 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task{
 
     String start;
     String end;
+    LocalDate newStart;
+    LocalDate newEnd;
     public Event(String description, String start, String end) {
         super(description);
         this.start = start;
         this.end = end;
+        this.newStart = convertDate(start);
+        this.newEnd = convertDate(end);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from:" + start + "to:" + end + ")";
+        String startDate = (newStart != null) ? newStart.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) : start;
+        String endDate = (newEnd != null) ? newEnd.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) : end;
+        return "[E]" + super.toString() + " (from: " + startDate + " to: " + endDate + ")";
     }
 
     @Override
     public String changeFormat() {
         return "E | " + (this.getStatusIcon().equals("X") ? "1" : "0") + " | " + this.description + " | " + this.start + " | " + this.end;
+    }
+
+    @Override
+    public LocalDate convertDate(String date) {
+        try {
+            return LocalDate.parse(date.trim(), DateTimeFormatter.ISO_DATE);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 }
