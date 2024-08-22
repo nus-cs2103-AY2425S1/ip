0f182e81d9ed71feac91c2cfe26514fbd1d2ef99
yikyak02 <>
2024-08-22 15:03:15 +0800

    Level 2: Add list done with tell user joke function

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Alex {
 
+    // Array to store tasks
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,26 +17,42 @@ public class Alex {
         while (true) {
             String userInput = scanner.nextLine();
 
-            //tells user a joke
-            if (userInput.equalsIgnoreCase("tell me a joke")) {
+            // Command to list tasks
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            }
+
+            // Command to tell a joke
+            else if (userInput.equalsIgnoreCase("tell me a joke")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Why did the scarecrow win an award? Because he was outstanding in his field!");
                 System.out.println("____________________________________________________________");
             }
 
-            if (userInput.equalsIgnoreCase("bye")) {
+            // Command to exit
+            else if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
             }
 
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + userInput);
-            System.out.println("____________________________________________________________");
+            // For all other inputs, display "added: {input}"
+            else {
+                if (taskCount < tasks.length) {
+                    tasks[taskCount] = userInput; // Store the user input as a task
+                    taskCount++;
+                }
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
         }
 
         scanner.close();
     }
 }
-
