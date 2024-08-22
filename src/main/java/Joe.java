40cdc1dbb25c4912a8c9e098164ed5a8d1f17153
Wei Lian <weilian2003@live.com>
2024-08-22 22:54:48 +0800

    Add echo function to ChatBot

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Joe {
     public static void main(String[] args) {
         System.out.println("""
                 Hello! I'm Joe
-                What can I do for you?
-                Bye. Hope to see you again soon!""");
+                What can I do for you?""");
+        Scanner scanner = new Scanner(System.in);
+        String EXIT_WORD = "bye";
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals(EXIT_WORD)) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                return;
+            }
+            System.out.println("\t" + userInput);
+        }
     }
 }
