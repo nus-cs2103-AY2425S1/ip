0d3b34a748705cf7bacf17615181fa4339a6ff99
Clarenceeey <clarencey100@gmail.com>
2024-08-22 17:35:27 +0800

    InvalidDateException accepts messages

--- src/main/java/InvalidDateException.java
+++ src/main/java/InvalidDateException.java
@@ -4,4 +4,8 @@ public class InvalidDateException extends Exception{
     public InvalidDateException () {
         super("Error: No date provided");
     }
+
+    public InvalidDateException (String message) {
+        super("Error: " + message);
+    }
 }
