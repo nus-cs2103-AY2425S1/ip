332c888b1123d6737bdc1edc246a0fc67d55a924
Nicholas Chua <e1114227@u.nus.edu>
2024-08-22 16:28:56 +0800

    add greet and farewell methods

--- src/main/java/Fred.java
+++ src/main/java/Fred.java
@@ -1,13 +1,21 @@
 public class Fred {
+    static String line = "____________________________________________________________";
+    static String name = "Fred";
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String name = "Fred";
+        greet();
+        sayFarewell();
+    }
+
+    private static void greet() {
         String greeting = "Hello! I'm " + name + "\n" +
                 "What can I do for you?";
-        String farewell = " Bye. Hope to see you again soon!";
-
         System.out.println(line);
         System.out.println(greeting);
+        System.out.println(line);
+    }
+
+    private static void sayFarewell() {
+        String farewell = " Bye. Hope to see you again soon!";
         System.out.println(line);
         System.out.println(farewell);
         System.out.println(line);
