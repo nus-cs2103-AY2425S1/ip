f39f10813dd39859b2d48f62d7eaeffd05f4f812
Jabez Tho <jabeztho@gmail.com>
2024-08-23 02:48:47 +0800

    add echo support

--- src/main/java/Nixy.java
+++ src/main/java/Nixy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Nixy {
     public static void main(String[] args) {
         String chatbotName = "Nixy";
@@ -5,11 +7,29 @@ public class Nixy {
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printHorizontalLine();
+        echo();
     }
 
     private static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    private static void echo() {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        if (input.equals("bye")) {
+            exit();
+            return;
+        }
+        printHorizontalLine();
+        System.out.println(input);
+        printHorizontalLine();
+        echo();
+    }
+
+    private static void exit() {
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 }
