378bfac4de44278c5ea4795be4c1410ca21c4e62
maertan <119797875+maertan@users.noreply.github.com>
2024-08-22 16:08:29 +0800

    Add mark as done feature

--- src/main/java/Yoda.java
+++ src/main/java/Yoda.java
@@ -8,26 +8,43 @@ public class Yoda {
         String welcomeMessage = "Hello! For you, what can I do?\n";
         String exitMessage = "Bye. See you again soon, I hope to.\n";
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
 
         System.out.println(line + welcomeMessage + line);
 
         while (true) {
-            String input = scanner.nextLine();
-            if (Objects.equals(input, "bye")) {
+            String input = scanner.nextLine().trim();
+            String[] splitInput = input.split(" ");
+
+
+            if (Objects.equals(splitInput[0], "bye")) {
                 System.out.println(exitMessage + line);
                 break;
-            } else if (Objects.equals(input, "list")) {
+            } else if (Objects.equals(splitInput[0], "list")) {
                 System.out.println(line);
-                String[] taskArray = tasks.toArray(new String[0]);
+                System.out.println("Do or do not, there is no try.");
+                Task[] taskArray = tasks.toArray(new Task[0]);
                 for (int i = 0; i < taskArray.length; i++) {
-                    System.out.printf("%d. %s%n", i + 1, taskArray[i]);
+                    System.out.printf("%d. %s\n", i + 1, taskArray[i]);
                 }
-
                 System.out.println(line);
-            } else {
-                tasks.add(input);
+            } else if (Objects.equals(splitInput[0], "mark")) {
+                int index = Integer.parseInt(splitInput[1]);
+                Task currentTask = tasks.get(index - 1);
+                currentTask.markDone();
+                System.out.println(line + "Good job! Marked this as done, I have");
+                System.out.printf("%s\n" + line, tasks.get(index - 1));
+
+            } else if (Objects.equals(splitInput[0], "unmark")) {
+                int index = Integer.parseInt(splitInput[1]);
+                Task currentTask = tasks.get(index - 1);
+                currentTask.markNotDone();
+                System.out.println(line + "Marked this as not done, I have");
+                System.out.printf("%s\n" + line, tasks.get(index - 1));
+            }
+            else {
+                tasks.add(new Task(input));
                 System.out.println(line + input + "\n" + line);
             }
         }
