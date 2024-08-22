f548fbfb019cec680575d4b14f2a7d000f5ca73e
MingYiAw <awmingyi@gmail.com>
2024-08-23 00:55:17 +0800

    no message

---
+++ src/main/java/Dude.java
@@ -0,0 +1,13 @@
+public class Dude {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String botName = "Dude";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
