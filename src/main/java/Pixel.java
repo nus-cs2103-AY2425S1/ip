87a6331af61294db9a7b95ac5428585795a689c2
Sornsornah <ejasonlek@gmail.com>
2024-08-22 20:58:19 +0800

    added greeting

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -1,10 +1,12 @@
 public class Pixel {
+    public static String LINE = "------------------------------------";
+    public static void greeting() {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Pixel!");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greeting();
     }
 }
