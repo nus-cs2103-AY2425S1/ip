4bfce246a8f53f26676ca27d067b272b692fd4da
minhao <heminhao120@gmail.com>
2024-08-22 19:56:41 +0800

    Level 1. Echo

--- ip/src/main/java/Jarvis.java
+++ ip/src/main/java/Jarvis.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Jarvis {
     public static void main(String[] args) {
         String logo =
@@ -7,9 +8,24 @@ public class Jarvis {
                         "    / / /_/ / /   | |/ / (__  )\n"+
                         " __/ /\\__,_/_/    |___/_/____/\n"+
                         "/___/\n";
+        Scanner scanner = new Scanner(System.in);
+
 
         System.out.println("Hello from\n" + logo);
         System.out.println(" What can I do for you?\n\n");
+        System.out.println();
+
+        String input = "";
+
+        // Keep reading input until "bye" is entered
+        while (!input.equals("bye")) {
+            input = scanner.nextLine(); // Read the input once
+            if (!input.equals("bye")) { // Only print if it's not "bye"
+                System.out.println("__________________________________\n" + input+ "\n" +
+                        "__________________________________");
+            }
+        }
         System.out.println( "Bye. Hope to see you again soon!");
     }
+
 }
