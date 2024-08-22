0ca65505a42e2eebd66e8ace1b56ebdbd6c145d5
Karthikeyan <145328000+SKarthikeyan28@users.noreply.github.com>
2024-08-22 18:45:13 +0800

    Added mark and unmark methods using TASK class

--- src/main/java/Tars.java
+++ src/main/java/Tars.java
@@ -1,6 +1,26 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Task{
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+    //void since method does not return anything
+    public void mark(){
+        this.isDone = true;
+    }
+    public void unmark(){
+        this.isDone = false;
+    }
+}
+
 public class Tars {
     public static void main(String[] args) {
         //welcome/introduction message
@@ -8,32 +28,56 @@ public class Tars {
         System.out.println("    Hello! I'm Tars\n" + "    What can I do for you");
         System.out.println("    _____________________________________________");
 
-
         Scanner scanner = new Scanner(System.in); //initalising scanner to read inputs from user
         String entry = scanner.nextLine(); //storing string input in a variable
+        String[] entryParts = entry.split(" "); //when mark/unmark is given as input with TASK number
 
-        //store all input entries
-        ArrayList<String> itemsList = new ArrayList<>();
+        ArrayList<Task> itemsList = new ArrayList<>(); //store all input entries
 
         //while loop to ensure termination of programme only when "bye" input
-        while(!entry.equals("bye")) {
+        while(!entryParts[0].equals("bye")) {
+            System.out.println("    _____________________________________________");
+
             //using for loop to list all entries from arraylist
-            if (entry.equals("list")) {
-                System.out.println("    _____________________________________________");
+            if(entryParts[0].equals("list")) {
                 for (int i = 0; i < itemsList.size(); i++) {
-                    System.out.println("    " + (i + 1) + ". " + itemsList.get(i));
+                    System.out.println("    " + (i + 1) + ". " + "[" + itemsList.get(i).getStatusIcon()
+                            + "] " + itemsList.get(i).description);
                 }
                 System.out.println("    _____________________________________________");
 
-                entry = scanner.nextLine(); //updating variable to next input entry
-            } else {
-                itemsList.add(entry); //adding each input entry to list
+                entry = scanner.nextLine();
+                entryParts = entry.split(" "); //updating variable to next input entryParts
+            } else if(entryParts[0].equals("mark")){
+                Integer index = Integer.parseInt(entryParts[entryParts.length - 1]); //to convert string format of number to Integer
+                itemsList.get(index - 1).mark(); //marking TASK as done
 
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("    [" + itemsList.get(index - 1).getStatusIcon() + "] " + itemsList.get(index - 1).description);
                 System.out.println("    _____________________________________________");
+
+                entry = scanner.nextLine();
+                entryParts = entry.split(" "); //updating variable to next input entryParts
+            } else if(entryParts[0].equals("unmark")){
+                Integer index = Integer.parseInt(entryParts[entryParts.length - 1]); //to convert string format of number to Integer
+                itemsList.get(index - 1).unmark(); //unmarking TASK as not done
+
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println("    [" + itemsList.get(index - 1).getStatusIcon() + "] " + itemsList.get(index - 1).description);
+                System.out.println("    _____________________________________________");
+
+                entry = scanner.nextLine();
+                entryParts = entry.split(" "); //updating variable to next input entryParts
+            }
+            else {
+                Task t = new Task(entry);
+                itemsList.add(t); //adding Task to list
+
                 System.out.println("    added: " + entry);
                 System.out.println("    _____________________________________________");
 
-                entry = scanner.nextLine(); //updating variable to next input entry
+                entry = scanner.nextLine();
+                entryParts = entry.split(" "); //updating variable to next input entryParts
             }
         }
 
