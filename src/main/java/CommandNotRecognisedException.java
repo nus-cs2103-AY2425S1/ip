ae34e897c723e1fc6ec81bbb1f3f67ce818bfc8c
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-22 13:46:07 +0800

    Implement CommandNotRecognisedException

---
+++ src/main/java/CommandNotRecognisedException.java
@@ -0,0 +1,7 @@
+public class CommandNotRecognisedException extends Exception {
+
+    public CommandNotRecognisedException() {
+        super("Apologies, but I do not understand that command");
+    }
+
+}
