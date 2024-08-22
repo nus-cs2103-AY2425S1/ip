2f99786b981ea61f74e383f9edd7c92b162e26a5
t-leongchuan <t.leongchuan@gmail.com>
2024-08-23 00:23:24 +0800

    Add support for add and list

--- src/main/java/Denim.java
+++ src/main/java/Denim.java
@@ -8,19 +8,34 @@ public class Denim {
         String greetingMessage = String.format("%s%n Hello! I'm %s!%n What can I do for you? %n%s%n",
                 horizontalLine, chatBotName, horizontalLine);
 
+        String[] taskList = new String[100];
+        int taskSize = 0;
+
         System.out.println(greetingMessage);
 
         // Scans User Input in the CLI
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
 
-        // Echoes the input of the user until the command "bye" is given.
+        // Adds task or list tasks stated by user input until the command "bye" is given.
         while (!input.equals("bye")) {
-            String echoMessage = String.format("%s%n %s%n%s", horizontalLine, input, horizontalLine);
-            System.out.println(echoMessage);
-            input = sc.next();
+            switch (input) {
+            case "list":
+                System.out.println(horizontalLine);
+                for (int i = 0; i < taskSize; i++) {
+                    System.out.printf("%d. %s\n", i + 1, taskList[i]);
+                }
+                System.out.println(horizontalLine);
+                input = sc.nextLine();
+                break;
+            default:
+                taskList[taskSize++] = input;
+                String addMessage = String.format("%s%n added: %s%n%s", horizontalLine, input, horizontalLine);
+                System.out.println(addMessage);
+                input = sc.nextLine();
+                break;
+            }
         }
-
         String byeMessage = String.format("%s%n %s%n%s", horizontalLine, "Bye. Hope to see you again soon!",
                 horizontalLine);
         System.out.println(byeMessage);
