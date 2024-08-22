a46c3dcbabf71b79c5040e96ffc0ed432c3f87f4
luileng <chengluileng@gmail.com>
2024-08-23 07:54:40 +0800

    Add mark and unmark

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Duck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         String line = "____________________________________________________________";
         String welcomeMessage = "Hello! I'm Duck\nWhat can I do for you?";
@@ -12,18 +12,32 @@ public class Duck {
 
         while (true) {
             String userCommand = scanner.nextLine();
+            String[] commandParts = userCommand.split(" ", 2);
             if (userCommand.equals("bye")) {
                 break;
             }
             if (userCommand.equals("list")) {
-                System.out.println(line);
+                System.out.println(line + "\nHere are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
                     int num = i + 1;
                     System.out.println(num + ". " + list.get(i));
                 }
                 System.out.println(line);
+            } else if (commandParts.length == 2 && commandParts[0].equals("mark")) {
+                int taskIndex = Integer.parseInt(commandParts[1]) - 1;
+                Task task = list.get(taskIndex);
+                task.mark();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(task);
+            } else if (commandParts.length == 2 && commandParts[0].equals("unmark")) {
+                int taskIndex = Integer.parseInt(commandParts[1]) - 1;
+                Task task = list.get(taskIndex);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(task);
             } else {
-                list.add(userCommand);
+                Task task = new Task(userCommand);
+                list.add(task);
                 System.out.println(line + "\n" + "added: " + userCommand + "\n" + line);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String checkBox = isDone ? "[X]" : "[ ]";
+        return checkBox + " " + this.name;
+    }
+}
