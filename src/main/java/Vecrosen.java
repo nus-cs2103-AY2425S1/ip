0b83a5e9accb44e2ec0ade94f38e2c866a63ed95
1st2GetThisName <adrieledix@gmail.com>
2024-08-22 17:56:30 +0800

    Rename Duke.java to Vecrosen.java and Add greeting
    The chatbot is named Duke and has Duke's greeting.
    
    The chatbot needs to be distinguished from Duke.
    
    Let's change the name to Vecrosen and replace the greetings.

---
+++ src/main/java/Vecrosen.java
@@ -0,0 +1,6 @@
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Vecrosen\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
