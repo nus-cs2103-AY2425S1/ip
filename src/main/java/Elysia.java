dac5558d12953ae672a5ed9a8f0398635fded25c
Elinengu <Eline.Ngu2003@gmail.com>
2024-08-22 09:11:13 +0800

    add Level-0

---
+++ src/main/java/Elysia.java
@@ -0,0 +1,13 @@
+public class Elysia {
+    static String line = "____________________________________________________________";
+    static String welcomeMessage = "Hello! I'm Elysia\nWhat can I do for you?";
+    static String exitMessage = "Bye. Hope to see you again soon!";
+
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println(welcomeMessage);
+        System.out.println(line);
+        System.out.println(exitMessage);
+        System.out.println(line);
+    }
+}
