8e1b951946b03070cecc808910fc42fc23fbfc3a
gavinsoh <gavinsoh.02@gmail.com>
2024-08-22 13:42:07 +0800

    Level-0

---
+++ src/main/java/GavinChatBot.java
@@ -0,0 +1,46 @@
+//import java.util.Scanner;
+
+public class GavinChatBot {
+    public static void main(String[] args) {
+        /*
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        */
+
+        String horizontalLine = "_________________________________\n";
+
+        // print welcome message
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Gavin\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+
+        /*
+        // create scanner to read user input
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // echo input by user until user types bye
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                // print exit message
+                System.out.println(horizontalLine);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+        }
+
+         */
+
+    }
+}
