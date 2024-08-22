a76ae7db9bfca27ec84a0635840f084d7c6e2ed7
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-23 03:52:41 +0800

    Implement ArrayList

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,61 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
-    private Task[] tasks = new Task[100];
-    private int counter = 0;
+    private List<Task> tasks = new ArrayList<>();
 
     public String addTask(String type, String description, String... timeInfo) {
-        if (counter >= tasks.length) {
-            return "Task list is full, unfortunately ~";
-        }
+        Task task = null;
         switch (type) {
             case "todo":
-                tasks[counter] = new Todo(description);
+                task = new Todo(description);
                 break;
             case "deadline":
-                tasks[counter] = new Deadline(description, timeInfo[0]);
+                task = new Deadline(description, timeInfo[0]);
                 break;
             case "event":
-                tasks[counter] = new Event(description, timeInfo[0], timeInfo[1]);
+                task = new Event(description, timeInfo[0], timeInfo[1]);
                 break;
         }
-        counter++;
-        return "Roger that! I've added in this task:\n  " + tasks[counter - 1].toString() + "\nNow you have " + counter + " tasks in the list ~ !";
+        tasks.add(task);
+        return "Roger that! I've added in this task:\n  " + task.toString() + "\nNow you have " + tasks.size() + " tasks in the list ~ !";
     }
 
     public String deleteTask(int taskNumber) {
-        if (taskNumber > 0 && taskNumber <= counter) {
-            Task removedTask = tasks[taskNumber - 1];
-            System.arraycopy(tasks, taskNumber, tasks, taskNumber - 1, counter - taskNumber);
-            counter--;
-            return "Noted. I've removed this task:\n  " + removedTask.toString() + "\nNow you have " + counter + " tasks in the list.";
+        if (taskNumber < 1 || taskNumber > tasks.size()) {
+            return "OOPS!!! The task number " + taskNumber + " is out of bounds.";
         }
-        return "Invalid task number!!!!";
+        Task removedTask = tasks.remove(taskNumber - 1);
+        return "Noted. I've removed this task:\n  " + removedTask.toString() + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
     public String listTasks() {
-        if (counter == 0) {
+        if (tasks.isEmpty()) {
             return "You haven't added any task yet! :L";
         }
         StringBuilder list = new StringBuilder("Here are the tasks in your list:\n");
-        for (int i = 0; i < counter; i++) {
-            list.append(i + 1).append(". ").append(tasks[i].toString()).append("\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            list.append(i + 1).append(". ").append(tasks.get(i).toString()).append("\n");
         }
         return list.toString();
     }
 
     public String markTask(int taskNumber, boolean isDone) {
-        if (taskNumber > 0 && taskNumber <= counter) {
+        if (taskNumber > 0 && taskNumber <= tasks.size()) {
+            Task task = tasks.get(taskNumber - 1);
+
             if (isDone) {
-                tasks[taskNumber - 1].markAsDone();
-                return "Yippee~ *uweah* I've marked this task as done:\n  " + tasks[taskNumber - 1].toString();
+                task.markAsDone(); // Mark the task as done
+                return "Yippee~ *uweah* I've marked this task as done:\n  " + task;
             } else {
-                tasks[taskNumber - 1].unmarkAsDone();
-                return "LOL I've marked this task as not done yet:\n  " + tasks[taskNumber - 1].toString();
+                task.unmarkAsDone(); // Mark the task as not done
+                return "LOL I've marked this task as not done yet:\n  " + task;
             }
         }
         return "Invalid task number!!!!";
     }
 
     public int getTaskCount() {
-        return counter;
+        return tasks.size();
     }
 }
