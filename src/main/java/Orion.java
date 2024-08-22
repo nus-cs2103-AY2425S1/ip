b8095aee2c020d5d3a04b4aaf39f9ec863b86fad
pradyuprasad <pradyu.sp@gmail.com>
2024-08-22 11:42:03 +0800

    Fixed bugs in Level 4. ToDos, Events, Deadlines

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -11,65 +11,109 @@ public class Orion {
         System.out.println("Hello! I'm Orion");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("\n");
 
+        label:
         while (true) {
             String input = scanner.nextLine();
             String[] parts = input.split(" ", 2);
 
-            if (parts[0].equals("bye")) {
-                break;
-            } else if (parts[0].equals("list")) {
-                System.out.println(horizontalLine);
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + "." + tasks[i]);
+            switch (parts[0]) {
+                case "bye":
+                    break label;
+                case "list":
+                    System.out.println(horizontalLine);
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + "." + tasks[i]);
+                    }
+                    System.out.println(horizontalLine);
+                    break;
+                case "mark": {
+                    int taskNumber = Integer.parseInt(parts[1]) - 1;
+                    tasks[taskNumber].markAsDone();
+                    System.out.println(horizontalLine);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[taskNumber]);
+                    System.out.println(horizontalLine);
+                    break;
                 }
-                System.out.println(horizontalLine);
-            } else if (parts[0].equals("mark")) {
-                int taskNumber = Integer.parseInt(parts[1]) - 1;
-                tasks[taskNumber].markAsDone();
-                System.out.println(horizontalLine);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + tasks[taskNumber]);
-                System.out.println(horizontalLine);
-            } else if (parts[0].equals("unmark")) {
-                int taskNumber = Integer.parseInt(parts[1]) - 1;
-                tasks[taskNumber].markAsNotDone();
-                System.out.println(horizontalLine);
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + tasks[taskNumber]);
-                System.out.println(horizontalLine);
-            } else if (parts[0].equals("todo")) {
-                tasks[taskCount] = new Todo(parts[1]);
-                taskCount++;
-                System.out.println(horizontalLine);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println(horizontalLine);
-            } else if (parts[0].equals("deadline")) {
-                String[] deadlineParts = parts[1].split(" /by ");
-                tasks[taskCount] = new Deadline(deadlineParts[0], deadlineParts[1]);
-                taskCount++;
-                System.out.println(horizontalLine);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println(horizontalLine);
-            } else if (parts[0].equals("event")) {
-                String[] eventParts = parts[1].split(" /from | /to ");
-                tasks[taskCount] = new Event(eventParts[0], eventParts[1], eventParts[2]);
-                taskCount++;
-                System.out.println(horizontalLine);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println(horizontalLine);
-            } else {
-                System.out.println(horizontalLine);
-                System.out.println("I'm sorry, I don't understand that command.");
-                System.out.println(horizontalLine);
+                case "unmark": {
+                    int taskNumber = Integer.parseInt(parts[1]) - 1;
+                    tasks[taskNumber].markAsNotDone();
+                    System.out.println(horizontalLine);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[taskNumber]);
+                    System.out.println(horizontalLine);
+                    break;
+                }
+                case "todo":
+                    tasks[taskCount] = new Todo(parts[1]);
+                    taskCount++;
+                    System.out.println(horizontalLine);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(horizontalLine);
+                    break;
+                case "deadline":
+                    String[] words = parts[1].split(" ");
+                    StringBuilder description = new StringBuilder();
+                    StringBuilder by = new StringBuilder();
+                    boolean foundBy = false;
+
+                    for (String word : words) {
+                        if (word.equals("by")) {
+                            foundBy = true;
+                        } else if (foundBy) {
+                            by.append(word).append(" ");
+                        } else {
+                            description.append(word).append(" ");
+                        }
+                    }
+
+                    tasks[taskCount] = new Deadline(description.toString().trim(), by.toString().trim());
+                    taskCount++;
+                    System.out.println(horizontalLine);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(horizontalLine);
+                    break;
+                case "event":
+                    String[] eventWords = parts[1].split(" ");
+                    StringBuilder eventDescription = new StringBuilder();
+                    StringBuilder from = new StringBuilder();
+                    StringBuilder to = new StringBuilder();
+                    boolean foundFrom = false;
+                    boolean foundTo = false;
+
+                    for (String word : eventWords) {
+                        if (word.equals("from")) {
+                            foundFrom = true;
+                        } else if (word.equals("to")) {
+                            foundTo = true;
+                        } else if (foundTo) {
+                            to.append(word).append(" ");
+                        } else if (foundFrom) {
+                            from.append(word).append(" ");
+                        } else {
+                            eventDescription.append(word).append(" ");
+                        }
+                    }
+
+                    tasks[taskCount] = new Event(eventDescription.toString().trim(), from.toString().trim(), to.toString().trim());
+                    taskCount++;
+                    System.out.println(horizontalLine);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(horizontalLine);
+                    break;
+                default:
+                    System.out.println(horizontalLine);
+                    System.out.println("I'm sorry, I don't understand that command.");
+                    System.out.println(horizontalLine);
+                    break;
             }
         }
 
@@ -78,3 +122,4 @@ public class Orion {
         System.out.println(horizontalLine);
     }
 }
+
