659d5efd5960dc141609239ce134f557304d5542
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-08-22 15:30:45 +0800

    added Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String taskName;
+    private boolean completed;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    private void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        String x;
+        if (completed) {
+            x = "X";
+        } else {
+            x = " ";
+        }
+        return String.format("[%s] %s", x, this.taskName);
+    }
+}
