0539e3d3fff0b78e40b5cb7605f21a4a4b55ffe0
Bryan <bryanchenyu616@gmail.com>
2024-08-22 15:20:22 +0800

    add ability to store user text

--- src/main/java/Crack.java
+++ src/main/java/Crack.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Crack {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
+        String divider = "____________________________________________________________\n";
+        String greeting = divider
                 + " Hello! I'm Crack\n"
-                + " What can I do for you?\n";
-
+                + " What can I do for you?\n"
+                + divider;
         System.out.println(greeting);
 
         Scanner scanner = new Scanner(System.in);
         String input;
+        ArrayList<String> tasks = new ArrayList<String>();
 
         while (true) {
             System.out.print("You: ");
@@ -18,8 +21,22 @@ public class Crack {
             if (input.equals("bye")) {
                 System.out.println("Goodbye!");
                 break;
-            } else {
-                System.out.println("Crack says " + input);
+            } else if (input.equals("list")) {
+                System.out.println(divider);
+                for (int i = 0; i < tasks.size(); i++) {
+                    if (tasks.get(i) != null) {
+                        System.out.println(
+                                + (i + 1) + ". " + tasks.get(i) + "\n"
+                                );
+                    }
+                }
+                System.out.println(divider);
+            }
+            else {
+                tasks.add(input);
+                System.out.println(divider + "\n"
+                        + "added: " + input + "\n"
+                        + divider);
             }
         }
 
