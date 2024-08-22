4927b9740b69fb0ef57c3747387e8c41b8938ee4
yuancheng <>
2024-08-22 13:20:03 +0800

    level 0

--- src/main/java/Winner.java
+++ src/main/java/Winner.java
@@ -1,11 +1,17 @@
 public class Winner {
 
-    public String greetUser() {
-        return "Hi! I am Winner. What can I do for you today?";
+    public static String greetUser() {
+        return "Hi! I am Winner.\n" +
+                "What can I do for you today?";
     }
 
-    public String exit() {
+    public static String exit() {
         return "Hope I have been of service. Goodbye and see you again!";
     }
 
+
+    public static void main(String[] args) {
+        System.out.println(greetUser());
+        System.out.println(exit());
+    }
 }
