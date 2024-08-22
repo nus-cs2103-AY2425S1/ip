f4aa9887ecd84772d2cbdb9cfeda43eec64638f5
Martin Ng <martinng2002@gmail.com>
2024-08-22 12:18:12 +0800

    Level 3. Mark as Done

--- src/main/java/brainRot.java
+++ src/main/java/brainRot.java
@@ -3,8 +3,8 @@ public class brainRot {
     public static void main(String[] args) {
 
         //creating an array with a number to memorise how many memory slots have been taken
-        String[] arr = new String[100];
-        int mem = 0;
+        Task[] arr = new Task[100];
+        int mem = 1;
 
         Scanner reader = new Scanner(System.in);
 
@@ -22,15 +22,35 @@ public class brainRot {
         //logic to check if "bye" or "list" has been said
         while(!answer.equals("bye")) {
             if(answer.equals("list")) {
-                for(int i = 0 ; i < mem; i ++) {
-                    System.out.println((i+1) + "." + arr[i]);
+                System.out.println("____________________________________________________________ \n"
+                + "Here are the tasks in your list:\n");
+
+                for(int i = 1 ; i < mem; i ++) {
+                    System.out.println(i + "." + arr[i].toString());
                 }
+                System.out.println("\n"
+                + "____________________________________________________________ \n");
+
                 reader = new Scanner(System.in);
                 answer = reader.nextLine();
 
+
+            } else if (answer.contains("unmark")) {
+                int index = answer.charAt(7) - 48;
+//                System.out.println(answer.charAt(5));
+//                System.out.println(index);
+//                System.out.println(arr[1]);
+                arr[index].unmark();
+                reader = new Scanner(System.in);
+                answer = reader.nextLine();
+            } else if (answer.contains("mark")) {
+                int index = answer.charAt(5) - 48;
+                arr[index].mark();
+                reader = new Scanner(System.in);
+                answer = reader.nextLine();
             } else {
                 System.out.println(answer);
-                arr[mem] = answer;
+                arr[mem] = new Task(answer);
                 mem++;
                 reader = new Scanner(System.in);
                 answer = reader.nextLine();
