82be89499e58d531d53ed99400ffca725ba77e60
Nimastic <jerielchanzy@gmail.com>
2024-08-23 07:59:03 +0800

    Level-0

--- src/main/java/Jeriel.java
+++ src/main/java/Jeriel.java
@@ -1,14 +1,14 @@
 public class Jeriel {
     public static void main(String[] args) {  
         String logo = "     _  ____  ___   _  ____  _      \n"
-                    + "    | || ___|| _ \\| || ___|| |     \n"
+                    + "    | || ___|| _ \\ | || ___|| |     \n"
                     + "    | |||___ ||_> || |||___ | |     \n"
                     + " _  | || ___||  _/ | || ___|| |     \n"
-                    + "| |_| |||___ ||\\  | |||___ | |____ \n"
-                    + " \\___/|____||| \\ |_||____||______|\n";
+                    + "| |_| |||___ ||\\\\  | |||___ | |____ \n"
+                    + " \\___/ |____||| \\\\ |_||____||______|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello from\n" + logo);
+        // System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Jeriel");
         System.out.println(" What can I do for you?");
