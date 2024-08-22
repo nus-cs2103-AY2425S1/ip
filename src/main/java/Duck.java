20dd2c9656137edf4603310bf278c254f6643980
SamuelFoo <fooenzesamuel@gmail.com>
2024-08-22 14:16:20 +0800

    Add simple greeting.

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,5 +1,17 @@
 public class Duck {
+    private static final String CHATBOT_NAME = "Duck";
+    private static final String TEXT_SEPARATOR = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String chatbotName = "Duck";
+        // Generate greeting
+        String greeting = TEXT_SEPARATOR
+                + String.format(" Hello! I'm %s\n", CHATBOT_NAME)
+                + " What can I do for you?\n"
+                + TEXT_SEPARATOR
+                + " Bye. Hope to see you again soon!\n"
+                + TEXT_SEPARATOR;
+
+        // Print greeting
+        System.out.println(greeting);
     }
 }
