0d7bdc3c6e280795ffa4524021867ad50205db98
muhdbhz <e1122341@u.nus.edu>
2024-08-22 17:07:00 +0800

    Add the ability to mark and unmark tasks status
    Create a task class

--- src/main/java/Moody.java
+++ src/main/java/Moody.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Moody {
     public static void main(String[] args) {
         String spacer = "____________________________________________________________\n";
+        String indent = "    ";
+
         System.out.println(spacer
                 + "Hello! I'm Moody!\nWhat can I do for you?\n"
                 + spacer);
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        String[] userList = new String[100];
+        Task[] userTasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -19,14 +21,31 @@ public class Moody {
                         + "Bye. Hope to see you again soon!\n"
                         + spacer);
                 break;
-            } else if (userInput.equals("list")){
+            } else if (userInput.equals("list")) {
                 System.out.print(spacer);
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + userList[i]);
+                    System.out.println((i + 1) + ". " + userTasks[i]);
                 }
                 System.out.println(spacer);
+            } else if (userInput.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                userTasks[taskNumber].markAsDone();
+                System.out.println(spacer
+                        + "Nice! I've marked this task as done:");
+                System.out.println(indent
+                        + userTasks[taskNumber].toString());
+                System.out.println(spacer);
+            } else if (userInput.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                userTasks[taskNumber].markAsNotDone();
+                System.out.println(spacer
+                        + "OK, I've marked this task as not done yet:");
+                System.out.println(indent
+                        + userTasks[taskNumber].toString());
+                System.out.println(spacer);
             } else {
-                userList[taskCount] = userInput;
+                Task task = new Task(userInput);
+                userTasks[taskCount] = task;
                 taskCount++;
                 System.out.println(spacer
                         + "added: "
