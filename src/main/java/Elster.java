18d5c54f1aa5084623426f61043765c21be30944
SeanFoongjt <seanfoongjt@gmail.com>
2024-08-23 00:37:46 +0800

    Add the echo function

--- src/main/java/Elster.java
+++ src/main/java/Elster.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Elster {
     public static void main(String[] args) {
+        boolean byeSentinel = true;
+        String curr;
+        Scanner myScanner = new Scanner(System.in);
+
         String logo = "___________.__            __\n" +
                     "\\_   _____/|  |   _______/  |_  ___________\n" +
                     " |    __)_ |  |  /  ___/\\   __\\/ __ \\_  __ \\\n" +
@@ -8,15 +14,30 @@ public class Elster {
                     "        \\/           \\/            \\/";
         System.out.println(logo);
         printLine();
-        System.out.println("Hello, \"greetings\" from your friendly neighbourhood chatbot Elster..");
-        System.out.println("How can I help you today :|");
+
+        System.out.println("    Hello, \"greetings\" from your friendly neighbourhood chatbot Elster..");
+        System.out.println("    How can I help you today :|");
         printLine();
-        System.out.println("See you next time.");
+
+        while (byeSentinel) {
+            curr = myScanner.nextLine();
+            if (curr.equals("bye")) {
+                byeSentinel = false;
+            } else {
+                printLine();
+                System.out.println("    " + curr);
+                printLine();
+                System.out.println("");
+            }
+        }
+
+        printLine();
+        System.out.println("    See you next time.");
         printLine();
     }
 
     static public void printLine() {
-        System.out.println("____________________________________________________________________________");
+        System.out.println("    ____________________________________________________________________________");
         return;
     }
 }
