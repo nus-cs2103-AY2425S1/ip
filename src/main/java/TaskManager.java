aa3d87e29132810550b3ddbe01c0059a923f5ed5
Jabez Tho <jabeztho@gmail.com>
2024-08-23 04:02:19 +0800

    TaskManager to non-static and with indent

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,14 +1,14 @@
 public class TaskManager {
-    private static Task[] tasks = new Task[100];
-    private static int taskCount = 0;
+    private Task[] tasks = new Task[100];
+    private int taskCount = 0;
 
-    public static addTask(Task task) {
+    public void addTask(Task task) {
         tasks[taskCount++] = task;
     }
 
-    public static void listTasks() {
+    public void listTasks() {
         for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            PrintUtility.indentPrint((i + 1) + ". " + tasks[i]);
         }
     }
 }
