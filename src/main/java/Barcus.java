02582562171d512f5f079eb0b8adf22122a71b34
skyl3-r <skyler.ng.nyz@gmail.com>
2024-08-22 15:10:23 +0800

    Add support for list

--- src/main/java/Barcus.java
+++ src/main/java/Barcus.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class Barcus {
     public static void main(String[] args) {
+        // fixed dialogue
         String intro =
-"╭━━╮\n" +
-"┃╭╮┃\n" +
-"┃╰╯╰┳━━┳━┳━━┳╮╭┳━━╮\n" +
-"┃╭━╮┃╭╮┃╭┫╭━┫┃┃┃━━┫\n" +
-"┃╰━╯┃╭╮┃┃┃╰━┫╰╯┣━━┃\n" +
-"╰━━━┻╯╰┻╯╰━━┻━━┻━━╯\n" +
+                "╭━━╮\n" +
+                "┃╭╮┃\n" +
+                "┃╰╯╰┳━━┳━┳━━┳╮╭┳━━╮\n" +
+                "┃╭━╮┃╭╮┃╭┫╭━┫┃┃┃━━┫\n" +
+                "┃╰━╯┃╭╮┃┃┃╰━┫╰╯┣━━┃\n" +
+                "╰━━━┻╯╰┻╯╰━━┻━━┻━━╯\n" +
                 "Beep bop! Hello I am Barcus, ready to be of assistance!\n" +
                 "Write 'bye' to leave!\n";
         String goodbye = "Alright, good talk!\n";
 
+        // scanner object
         Scanner scanner = new Scanner(System.in);
+
+        // list to save info
+        String[] tasks = new String[100];
+        int curr = 0;
 
         // start
         System.out.println(intro);
@@ -26,8 +32,18 @@ public class Barcus {
             if (reply.equals("bye")) {
                 exit = true;
                 talk(goodbye);
+            } else if (reply.equals("list")) {
+                // for add list
+                talk("");
+                for (int i = 0; i < curr; i++) {
+                    System.out.println(String.valueOf(i+1) + ". " + tasks[i]);
+                }
             } else {
-                talk(reply);
+                // for echo
+//                talk(reply);
+                tasks[curr] = reply;
+                curr++;
+                talk("Added task '" + reply + "'");
             }
         }
 
