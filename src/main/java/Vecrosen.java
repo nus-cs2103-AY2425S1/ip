0dc299ec742df161b677bca599233df2d7dc5641
1st2GetThisName <adrieledix@gmail.com>
2024-08-22 20:19:19 +0800

    Add special input: "list"
    Vecrosen is still lacking in functionality.
    
    Let's add a function for Vecrosen to output a list of items.

--- src/main/java/Vecrosen.java
+++ src/main/java/Vecrosen.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Vecrosen {
@@ -10,12 +12,23 @@ public class Vecrosen {
 
     public static void main(String[] args) {
         speak("Hello, I'm Vecrosen\nWhat can I do for you?");
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Write code");
+        list.add("Create list");
+        list.add("???");
+        list.add("Profit!");
         while (true) {
             String input;
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextLine();
             if (input.equals("bye")) break;
-            speak(input);
+            else if (input.equals("list")) {
+                for (int i = 0; i < list.size(); ++i) {
+                    // let's avoid string concatenation
+                    System.out.print("    " + (i+1) + ". ");
+                    System.out.println(list.get(i));
+                }
+            } else speak(input);
         }
         speak("Bye. Hope to see you again soon!");
     }
