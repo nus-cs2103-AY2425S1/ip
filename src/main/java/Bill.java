f86c687da600b42b9f868565b8e43c504c36a67f
Wnayar <wdnconsultancy@yahoo.com>
2024-08-22 12:54:56 +0800

    Add list functionality to store and show user commands

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bill {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Bill");
         System.out.println("What can I do for you?");
 
+        ArrayList<String> userList = new ArrayList<String>();
         Scanner userScanner = new Scanner(System.in);
         String userCommand = userScanner.nextLine();
 
         while (!userCommand.equals("bye")) {
-            System.out.println(userCommand);
+            if (userCommand.equals("list")) {
+                if (userList.isEmpty()) {
+                    System.out.println("List is empty");
+                } else {
+                    for (int i = 0; i < userList.size(); i++) {
+                        System.out.println((i + 1) + ". " + userList.get(i));
+                    }
+                }
+            } else {
+                userList.add(userCommand);
+                System.out.println("added: " + userCommand);
+            }
             userCommand = userScanner.nextLine();
         }
 
