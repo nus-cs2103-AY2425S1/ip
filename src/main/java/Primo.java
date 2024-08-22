aec61af1ae4b47834141f51b0cf15894de8a55ac
choonzies <pohjingchoon@gmail.com>
2024-08-22 12:13:56 +0800

    Add echo feature with UI updates
    - Echoes command by user
    - Closes conversation for bye command
    - Removed dashes, added name with colon format instead

--- src/main/java/Primo.java
+++ src/main/java/Primo.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Primo {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static boolean ended = false;
+
+    private static void sayBye() {
+        String byeMessage = "El Primo: " +
+                            "Bye. Hope to see you again soon!\n";
+        System.out.println(byeMessage);
+        ended = true;
+    }
+
+    public static void assessInput(String input) {
+        if (input.equals("bye")) {
+            sayBye();
+        } else {
+            String output = "El Primo: " + input + "\n";
+            System.out.println(output);
+        }
+    }
+    public static void readInput() {
+        System.out.println("Me: ");
+        String input = scanner.nextLine();
+        assessInput(input);
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                        "Hello! I'm El Primo!!\n" +
-                        "What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________");
+        System.out.println("""
+                El Primo: Hello! I'm El Primo!!
+                El Primo: What can I do for you?
+                """);
+
+        while (!ended) {
+            readInput();
+        }
     }
 }
