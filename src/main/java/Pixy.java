2e8ad81228328f93faff0e9a27b8e81473239bc5
ritvi12 <ritvi0205@gmail.com>
2024-08-22 10:57:13 +0800

    Level-3: Mark as Done

--- src/main/java/Pixy.java
+++ src/main/java/Pixy.java
@@ -1,24 +1,67 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Pixy {
 
-    private String[] list = new String[100];
+    private List<Task> list = new ArrayList<>();
     private int numOfItems;
     private String inputTask() {
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("\n");
         return sc.next();
     }
-    private void addToList(String item) {
-        list[numOfItems++] = item;
+    private void addToList(String task) {
+        list.add(new Task(task));
+        System.out.println("added: " + task +
+                "\n____________________________________________________________\n");
     }
     private void printList() {
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] == null) {
+        for (int i = 0; i < list.size(); i++) {
+            Task task = list.get(i);
+            if (task == null) {
                 break;
             }
-            System.out.println((i + 1) + ". " + list[i]);
+            System.out.println((i + 1) + ". " + "[" + task.getStatusIcon() + "] " + task.getDescription());
+        }
+    }
+    private boolean isValidTaskNumber(int index) {
+        return index > 0 && index <= list.size();
+    }
+    private void markTask(int taskNumber, boolean done) {
+        if (isValidTaskNumber(taskNumber)) {
+            Task task = list.get(taskNumber - 1);
+            task.markAsDone(done);
+            System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription() +
+                    "\n____________________________________________________________\n");
+        } else {
+            System.out.println("Invalid task number entered!Input again.");
+        }
+    }
+    public void run() {
+        while (true) {
+            String command = inputTask();
+            System.out.println("____________________________________________________________\n");
+            if (command.equalsIgnoreCase("list")) {
+                printList();
+                System.out.println("____________________________________________________________\n");
+            } else if (command.equalsIgnoreCase("Bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            } else if (command.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(command.split(" ")[1]);
+                System.out.println("OK, I have marked this task as done:");
+                markTask(taskNumber, true);
+
+
+            } else if (command.startsWith("unmark")){
+                int taskNumber = Integer.parseInt(command.split(" ")[1]);
+                System.out.println("OK, I have marked this task as not done yet:");
+                markTask(taskNumber, false);
+            } else {
+                addToList(command);
+            }
         }
     }
     public static void main(String[] args) {
@@ -27,23 +70,6 @@ public class Pixy {
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
         Pixy pixy = new Pixy();
-        while (true) {
-            String item = pixy.inputTask();
-            System.out.println("____________________________________________________________\n");
-            if (item.equalsIgnoreCase("list")) {
-                pixy.printList();
-                System.out.println("____________________________________________________________\n");
-                continue;
-            }
-            if (item.equalsIgnoreCase("Bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n");
-                break;
-            }
-            pixy.addToList(item);
-            System.out.println("added: " +
-                    item + "\n____________________________________________________________\n");
-        }
-
+        pixy.run();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description; // mark done task with X
+    }
+    public void markAsDone(boolean done) {
+        this.isDone = done;
+    }
+}
