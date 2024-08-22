18a097487b2d9625a270ccd953758acc1538d7c5
Qiao Yi <limqiaoyi141@gmail.com>
2024-08-22 19:13:49 +0800

    Implemented Level 1: Echo

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -2,7 +2,16 @@ import java.io.*;
 import java.util.*;
 public class Sigma {
     public static void main(String[] args) {
-        String welcomeMessage = "Hello! I'm Sigma \nWhat can I do for you? \nBye. See you soon!";
+        Scanner scanner = new Scanner(System.in);
+        String welcomeMessage = "Hello! I'm Sigma \nWhat can I do for you? \n";
         System.out.println(welcomeMessage);
+        while (scanner.hasNext()) {
+            String userInput = scanner.nextLine();
+            if (userInput.contains("bye")) {
+                break;
+            }
+            System.out.println(userInput);
+        }
+        System.out.println("Bye! See you soon");
     }
 }
