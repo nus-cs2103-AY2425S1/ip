f91d8f351d777730fc160ac413ea31b1c07114a6
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-22 13:43:05 +0800

    Testament.java: implement NotIntTaskException
    add try catch blocks to deal with NotInTaskExceptions being thrown

--- src/main/java/Testament.java
+++ src/main/java/Testament.java
@@ -23,20 +23,41 @@ public class Testament {
 
             } else if (splitUserInput[0].equals("mark")) {
                 int taskNumber = Integer.parseInt(splitUserInput[1]);
-                taskList.mark(taskNumber);
-                printDialogue("Congratulations on completing your task:\n" + taskList.getTask(taskNumber));
+                String taskDetails;
+                try {
+                    taskList.mark(taskNumber);
+                    taskDetails = taskList.getTask(taskNumber);
+                } catch (NotInTaskListException e) {
+                    printDialogue(e.getMessage());
+                    continue;
+                }
+                printDialogue("Congratulations on completing your task:\n" + taskDetails);
 
             } else if (splitUserInput[0].equals("unmark")) {
                 int taskNumber = Integer.parseInt(splitUserInput[1]);
-                taskList.unMark(taskNumber);
-                printDialogue("This task has been unmarked:\n" + taskList.getTask(taskNumber));
+                String taskDetails;
+                try {
+                    taskList.unMark(taskNumber);
+                    taskDetails = taskList.getTask(taskNumber);
+                } catch (NotInTaskListException e) {
+                    printDialogue(e.getMessage());
+                    continue;
+                }
+                printDialogue("This task has been unmarked:\n" + taskDetails);
 
             } else {
                 taskList.add(Task.of(userInput));
+                String latestTask;
+                try {
+                    latestTask = taskList.getTask(taskList.getSize());
+                } catch (NotInTaskListException e) {
+                    printDialogue(e.getMessage());
+                    continue;
+                }
                 printDialogue(
                         String.format("I've added the following task to your schedule:\n%s\n" +
                                         "You have %d tasks to complete",
-                                taskList.getTask(taskList.getSize()), taskList.getSize())
+                                latestTask, taskList.getSize())
                 );
             }
         }
