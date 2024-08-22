4789e7af716b3b982969eb1ebd7f32281de0b64a
jevan13 <jevan@jevans-MacBook-Pro.local>
2024-08-22 23:08:33 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,6 @@
+public class Yapper {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Yapper \nWhat can I do for you? \n" +
+                "Bye. Hope to see you soon! \n");
+    }
+}
