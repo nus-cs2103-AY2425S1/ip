54299e97ab25bd414fc15fd0b3e586f9440780c6
btbrandon <tzhbrandon@gmail.com>
2024-08-22 23:42:07 +0800

    Completed Level-0

---
+++ src/main/java/BeeBot.java
@@ -0,0 +1,11 @@
+public class BeeBot {
+    public static void main(String[] args) {
+        String greet = "________________________\n"
+                + "Hello! I'm BeeBot\n"
+                + "What can I do for you?\n";
+        String exit = "________________________\n"
+                + "Bye. Hope to see you again!\n"
+                + "________________________\n";
+        System.out.println(greet + exit);
+    }
+}
