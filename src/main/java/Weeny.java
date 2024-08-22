f4cea44cf9c6b07395948f5cba81513989bf9547
clarud <clarencejw@gmail.com>
2024-08-23 02:02:10 +0800

    Level-2

--- src/main/java/Weeny.java
+++ src/main/java/Weeny.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Weeny {
     public static void main(String[] args) {
         boolean farewell = false;
+        List<String> tasks = new ArrayList<>();
         Scanner user_input = new Scanner(System.in);
 
-        // Print the initial greeting
         line();
         System.out.println("Hello! I'm Weeny\nWhat can I do for you?");
         line();
 
-        // Main loop for handling user input
         while (!farewell) {
             String input = user_input.nextLine();
 
-            // Print the user's input between lines
-            line();
-            System.out.println(input);
-            line();
-
-            // Handle the 'bye' command
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                line();
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                line();
+            } else if (input.equals("bye")) {
                 farewell = true;
                 line();
                 System.out.println("Bye. Hope to see you again soon!");
+                line();
+            } else {
+                tasks.add(input);
+                line();
+                System.out.println("added: " + input);
                 line();
             }
         }
