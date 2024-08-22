a57c8f72c85c916c0291b08074e4c7d48b8f1440
W3iZhi <tanweizhimail@gmail.com>
2024-08-22 17:31:48 +0800

    Added echo commands

--- src/main/java/Talky.java
+++ src/main/java/Talky.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Talky {
     public static void printSeperator(String content) {
         String seperator = "---------------------------------------";
@@ -8,6 +9,15 @@ public class Talky {
     public static void main(String[] args) {
         String seperator = "---------------------------------------";
         printSeperator("Hello I'm Talky\n" + "How may I help you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                printSeperator(command);
+            }
+        }
         printSeperator("Bye. Do let me know if there's anything else!");
     }
 }
