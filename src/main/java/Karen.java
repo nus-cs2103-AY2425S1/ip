26ff22162de5d02adaf5de45434c41ed5d43b833
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-22 18:03:39 +0800

    added Add Task & List Task functionality

--- src/main/java/Karen.java
+++ src/main/java/Karen.java
@@ -1,31 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Karen {
     public static void main(String[] args) {
-        final String line = "_______________________\n";
-        String output = line +
+        final String LINE = "_______________________\n";
+        List<String> tasks = new ArrayList<String>();
+
+        String output = LINE +
                         "Hi! I'm Karen\n" +
                         "What can I do for you?\n" +
-                        line;
+                        LINE;
 
         System.out.print(output);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            output = "";
 
             if (input.equals("bye")) {
-                output = line +
+                System.out.print(
+                        LINE +
                         "Bye! Hope to see you again!\n" +
-                        line;
-                System.out.println(output);
+                        LINE
+                );
                 break;
-            } else {
-                output = line +
-                         input + "\n" +
-                         line;
-                System.out.println(output);
+            } else if (input.equals("list")) {
+                output += LINE;
+                for (int i = 0; i < tasks.size(); i++) {
+                    String s = tasks.get(i);
+                    if (s.isEmpty()) {
+                        break;
+                    } else {
+                        output += String.format("%d. %s\n",i + 1, s);
+                    }
+                }
+                output += LINE;
             }
+            else {
+                tasks.add(input);
+                output = LINE +
+                        "Added: " + input + "\n" +
+                        LINE;
+            }
+
+            System.out.print(output);
         }
 
     }
