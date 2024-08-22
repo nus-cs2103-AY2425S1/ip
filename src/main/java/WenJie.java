e21d31a804eb24081c1c94e6dd11bd5fa9ba1393
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-08-23 03:47:53 +0800

    Added add function and list to chatbot

--- src/main/java/WenJie.java
+++ src/main/java/WenJie.java
@@ -5,12 +5,14 @@ public class WenJie {
 
         Scanner scanner = new Scanner(System.in);
         boolean active = true;
+        String[] taskList = new String[100];
+        int currentPointer = 0;
 
         String greeting =
-                "   ____________________________________________________________\n" +
-                "   Eh wasusp la bro, my name is Wen Jie.\n" +
-                "   What you want?\n" +
-                "   ____________________________________________________________\n";
+                "____________________________________________________________\n" +
+                "Eh wasusp la bro, my name is Wen Jie.\n" +
+                "What you want?\n" +
+                "____________________________________________________________\n";
 
         System.out.println(greeting);
 
@@ -22,23 +24,45 @@ public class WenJie {
                     active = false;
                     String farewell =
                             "____________________________________________________________\n" +
-                            " Paiseh bro I zao liao, see you around ah bro.\n" +
+                            "  Paiseh bro I zao liao, see you around ah bro.\n" +
                             "____________________________________________________________\n";
                     System.out.println(farewell);
+                    break;
+                }
+
+                case "list": {
+                    String list =
+                            "____________________________________________________________\n" +
+                            displayList(taskList) +
+                            "____________________________________________________________\n";
+                    System.out.println(list);
                     break;
                 }
 
                 default :
                     String output =
                             "____________________________________________________________\n" +
-                            input + "\n" +
+                             "added: " + input + "\n" +
                             "____________________________________________________________\n";
                     System.out.println(output);
+                    taskList[currentPointer] = input;
+                    currentPointer++;
                     break;
             }
         }
 
-
-
     }
+
+    public static String displayList(String[] list) {
+        String result = "";
+        int i = 0;
+        while (list[i] != null){
+            String newLine = (i + 1) + ". " + list[i] + "\n";
+            result += newLine;
+            i++;
+        }
+        return result;
+    }
+
+
 }
