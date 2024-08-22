4a274fb3e6c5c468289c2809ffbdd21a8fd8bc95
xinweichong <127038769+xinweichong@users.noreply.github.com>
2024-08-22 13:05:41 +0800

    Changed Duke.java to Talker.java

---
+++ src/main/java/Talker.java
@@ -0,0 +1,12 @@
+public class Talker {
+    public static void main(String[] args) {
+        String name = "Talker";
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.printf("Hello! I'm %s\n", name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print(line);
+    }
+}
