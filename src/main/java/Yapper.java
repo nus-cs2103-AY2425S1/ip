b5ab4065724313b7647d70f714fd7b679c16ed4d
jevan13 <jevan@jevans-MacBook-Pro.local>
2024-08-23 00:45:05 +0800

    Mark as Done

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Yapper {
-    public static int itemsInList = 0;
-    public static String[] list = new String[100];
+    public static ArrayList<Task> listOfTask = new ArrayList<Task>();
     public static void main(String[] args) {
         // greeting message
         System.out.println("Hello! I'm Yapper \n" +
@@ -29,24 +29,57 @@ public class Yapper {
         }
         else
         {
-            addToList(command);
+            int i = command.indexOf(' ');
+            String comm = command.substring(0, i);
+            if (comm.equals("mark")) {
+                String input = command.substring(i + 1);
+                int order = Integer.valueOf(input);
+                mark(order);
+            }
+            else if (comm.equals("unmark")) {
+                String input = command.substring(i + 1);
+                int order = Integer.valueOf(input);
+                unmark(order);
+            }
+            else {
+                addToList(command);
+            }
         }
     }
+    public static void mark(int taskNumber)
+    {
+        Task taskToMark = listOfTask.get(taskNumber - 1);
+        taskToMark.setDone(true);
+        System.out.println("Nice! I've marked this task as done: \n" +
+                taskToMark);
+    }
+
+    public static void unmark(int taskNumber)
+    {
+        Task taskToUnmark = listOfTask.get(taskNumber - 1);
+        taskToUnmark.setDone(false);
+        System.out.println("OK, I've marked this task as not done yet: \n" +
+                taskToUnmark);
+    }
+
 
     // return list
     public static void returnList()
     {
-        for (int i = 1; i <= itemsInList; i++)
+        System.out.println("Here are the tasks in your list: ");
+        int order = 1;
+        for (Task task : listOfTask)
         {
-            System.out.println(i + ". " + list[i]);
+            System.out.println(order + "." + task);
+            order++;
         }
     }
 
     // add to List
     public static void addToList(String text)
     {
-        itemsInList++;
-        list[itemsInList] = text;
+        Task newTask = new Task(text);
+        listOfTask.add(newTask);
         System.out.println("Added: " + text);
     }
 
