8c70ca8911783d0d25b123220fec7fc6f4613f4c
skyl3-r <skyler.ng.nyz@gmail.com>
2024-08-22 14:18:24 +0800

    Rename Chatbot to Barcus

---
+++ src/main/java/Barcus.java
@@ -0,0 +1,9 @@
+public class Barcus {
+    public static void main(String[] args) {
+        String intro = "Beep bop! Hello I am Barcus, ready to be of assistance!\n";
+        String goodbye = "Alright, good talk!\n";
+
+        System.out.println(intro + goodbye);
+    }
+}
+
