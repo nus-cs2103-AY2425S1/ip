abf0d9a554fe9d68057784e2736ba20304a9772e
jiayr99 <e1249294@u.nus.edu>
2024-08-22 21:50:39 +0800

    Add exception in ToDo class

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,6 +1,9 @@
 public class ToDo extends Task {
-    public ToDo(String description) {
+    public ToDo(String description) throws JiaException {
         super(description);
+        if (description.isEmpty()) {
+            throw new JiaException("Oh No! I don't know what task to do :(");
+        }
     }
 
     @Override
