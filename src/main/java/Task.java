67e4dc44c432b77aff06f64cc46af9de9c73e6c3
KennethTeo2002 <kenneth.teo2002@gmail.com>
2024-08-22 10:45:46 +0800

    Create Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task{
+    private final String description;
+    private boolean isDone = false;
+
+    public Task(String description){
+        this.description = description;
+    }
+    public void setDone(){
+        this.isDone = true;
+    }
+    public void setUndone(){
+        this.isDone = false;
+    }
+
+    private String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[%s] %s", getStatusIcon(),this.description);
+    }
+
+}
