93d452c62b1625fa4542a7e9c4417d5f566195b6
RuijianLu <e1297772@u.nus.edu>
2024-08-22 21:02:40 +0800

    Enable some exception handling, although badly structured.

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -31,6 +31,13 @@ public class Utility {
 
     public static void handleMessage(String message) {
         String taskType = splitTaskInfo(message)[0];
+        if (message.isBlank() ||
+                message.equalsIgnoreCase("Hi") ||
+                message.equalsIgnoreCase("Hello") ||
+                message.equalsIgnoreCase("Ciallo")) {
+            System.out.println("Ciallo～(∠・ω< )⌒★");
+            return ;
+        }
         if (taskType.equals("list")) {
             System.out.println("Here are the tasks in your list:");
             for (int i = 1; i <= toDo.size(); i++) {
@@ -39,38 +46,87 @@ public class Utility {
             return ;
         }
         if (taskType.equals("mark")) {
-            int taskIndex = divideMessage(message)[1].charAt(0) - 49;
-            toDo.get(taskIndex).markAsDone();
-            System.out.println("Nice! I've marked this task as done:\n" + toDo.get(taskIndex));
-            return ;
+            try {
+                int taskIndex = divideMessage(message)[1].charAt(0) - 49;
+                toDo.get(taskIndex).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + toDo.get(taskIndex));
+                return ;
+            } catch(IndexOutOfBoundsException e) {
+                System.out.println("The task you want to mark is not in task list, please try again.");
+                return ;
+            }
+
         }
-        String taskInfo = splitTaskInfo(message)[1];
         if (taskType.equals("unmark")) {
-            int taskIndex = divideMessage(message)[1].charAt(0) - 49;
-            toDo.get(taskIndex).markAsNotDone();
-            System.out.println("OK, I've marked this task as not done yet:\n" + toDo.get(taskIndex));
-            return ;
+            try {
+                int taskIndex = divideMessage(message)[1].charAt(0) - 49;
+                toDo.get(taskIndex).markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + toDo.get(taskIndex));
+                return ;
+            } catch(IndexOutOfBoundsException e) {
+                System.out.println("The task you want to unmark is not in task list, please try again.");
+                return ;
+            }
         }
         if (taskType.equals("todo")) {
-            TodoTask task = new TodoTask(taskInfo);
-            toDo.add(task);
-            System.out.print("Got it. I've added this task:\n  " +
-                    task.toString() +
-                    "\nNow you have " + toDo.size() + " tasks in the list.\n");
+            try {
+                String taskInfo = splitTaskInfo(message)[1];
+                TodoTask task = new TodoTask(taskInfo);
+                toDo.add(task);
+                System.out.print("Got it. I've added this task:\n  " +
+                        task.toString() +
+                        "\nNow you have " + toDo.size() + " tasks in the list.\n");
+                return ;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("The description of the todo task cannot be empty, please try again.");
+                return ;
+            }
         }
         if (taskType.equals("deadline")) {
-            DeadlineTask task = new DeadlineTask(taskInfo);
-            toDo.add(task);
-            System.out.print("Got it. I've added this task:\n  " +
-                    task.toString() +
-                    "\nNow you have " + toDo.size() + " tasks in the list.\n");
+            try {
+                String taskInfo = splitTaskInfo(message)[1];
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("The description of the deadline task cannot be empty, please try again.");
+                return ;
+            }
+            String taskInfo = splitTaskInfo(message)[1];
+            try {
+                DeadlineTask task = new DeadlineTask(taskInfo);
+                toDo.add(task);
+                System.out.print("Got it. I've added this task:\n  " +
+                        task.toString() +
+                        "\nNow you have " + toDo.size() + " tasks in the list.\n");
+                return ;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.print("The description of the deadline task must include the ddl time, please try again.\n" +
+                        "Tips: use /by to enter the ddl time.\n e.g. deadline quiz1 /by today 23:59\n");
+                return ;
+            }
         }
         if (taskType.equals("event")) {
-            EventTask task = new EventTask(taskInfo);
-            toDo.add(task);
-            System.out.print("Got it. I've added this task:\n  " +
-                    task.toString() +
-                    "\nNow you have " + toDo.size() + " tasks in the list.\n");
+            try {
+                String taskInfo = splitTaskInfo(message)[1];
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("The description of the event task cannot be empty, please try again.");
+                return ;
+            }
+            String taskInfo = splitTaskInfo(message)[1];
+            try {
+                EventTask task = new EventTask(taskInfo);
+                toDo.add(task);
+                System.out.print("Got it. I've added this task:\n  " +
+                        task.toString() +
+                        "\nNow you have " + toDo.size() + " tasks in the list.\n");
+                return ;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.print("The description of the event task must include" +
+                        " the start time and the end time, please try again.\n" +
+                        "Tips: use /from and /to to enter the start and end time.\n" +
+                        " e.g. event meeting1 /from today 12:00 /to 14:00\n");
+                return ;
+            }
         }
+
+        System.out.println("Sorry to say that I don't know what does \"" + message + "\" means. Anyway, have a good day :)");
     }
 }
