9db43b2feb737b12d4e748ffa181da500ba2bff0
clarud <clarencejw@gmail.com>
2024-08-23 01:56:10 +0800

    Level-1

--- src/main/java/Weeny.java
+++ src/main/java/Weeny.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
+
+
 public class Weeny {
     public static void main(String[] args) {
         boolean farewell = false;
+        Scanner user_input = new Scanner(System.in);
+
+        // Print the initial greeting
         line();
         System.out.println("Hello! I'm Weeny\nWhat can I do for you?");
         line();
-        System.out.println("Bye. Hope to see you soon!");
-        line();
+
+        // Main loop for handling user input
+        while (!farewell) {
+            String input = user_input.nextLine();
+
+            // Print the user's input between lines
+            line();
+            System.out.println(input);
+            line();
+
+            // Handle the 'bye' command
+            if (input.equals("bye")) {
+                farewell = true;
+                line();
+                System.out.println("Bye. Hope to see you again soon!");
+                line();
+            }
+        }
+
+        user_input.close();
     }
 
     public static void line() {
-        System.out.println("______________________________________________");
+        System.out.println("____________________________________________________________");
     }
 }
