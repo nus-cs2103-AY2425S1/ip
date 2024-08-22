da2f5243acebacb8a696216c2bd5abe8c20b9ee8
avrilmohh <avrilmoh1234@gmail.com>
2024-08-22 18:23:44 +0800

    Added more chatbot functions so the user can converse with it

--- src/main/java/Tudee.java
+++ src/main/java/Tudee.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Tudee {
     public static void main(String[] args) {
-        String logo = "---------------------------------------- \n"
-                + "Hello! I'm Tudee \n"
-                + "What can I do for you? \n"
-                + "---------------------------------------- \n"
-                + "Bye. Hope to see you again soon! \n"
-                + "---------------------------------------- \n";
+        String logo = "____________________________________________________________ \n"
+                + "Hello! I'm Tudee, your chatbot bestie! \n"
+                + "How can I help you today? :) \n"
+                + "____________________________________________________________ \n";
         System.out.println(logo);
+        String input;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String output = "";
+            input = sc.nextLine();
+            output += "____________________________________________________________ \n";
+            output += input + "\n";
+            output += "____________________________________________________________ \n";
+            System.out.println(output);
+            if (input.equalsIgnoreCase("bye")) {
+                output = "____________________________________________________________ \n"
+                        + "Bye. Hope to see you again soon! Don't forget about me :( \n"
+                        + "____________________________________________________________";
+                System.out.println(output);
+                break;
+            }
+        }
+        sc.close();
     }
 }
