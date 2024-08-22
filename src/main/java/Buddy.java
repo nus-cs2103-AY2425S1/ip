5e14b0e6f6957ab5f535c193e13369255f088a33
Vinoth <vinoth2002k@gmail.com>
2024-08-22 09:02:32 +0800

    Level-0

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,9 @@
+public class Buddy {
+    public static void main(String[] args) {
+        System.out.println("____________________________________");
+        System.out.println("Hello! I'm Buddy\nWhat can I do for you?");
+        System.out.println("____________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________");
+    }
+}
