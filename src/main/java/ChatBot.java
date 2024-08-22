dcfa7cdc28d19f10ffe41b10b75472704bd483e3
he shi <e1312287@u.nus.edu>
2024-08-22 23:21:00 +0800

    level-0

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatBot {
@@ -12,10 +14,24 @@ public class ChatBot {
         System.out.println("Hello from I am chatbot");
         System.out.println("What can I do for you?");
 
-
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> save = new ArrayList<>(100);
+        Integer index = 0;
+        // Wait for the user to enter something
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            save.set(index, input);
+            index += 1;
+            System.out.println("add: " + input);
+            // Wait for the user to enter something
+            input = scanner.nextLine();
+        }
         // Goodbye message
         System.out.println("Bye. Hope to see you again soon!");
 
+        // Close the scanner
+        scanner.close();
     }
 
 }
