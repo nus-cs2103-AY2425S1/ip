40193a12985e812afe477bd7bb5f1ccd3c44d66c
prave1n <pravein2002@gmail.com>
2024-08-22 20:14:24 +0800

    Add echo

--- src/main/java/MonoBot.java
+++ src/main/java/MonoBot.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class MonoBot {
     public static void main(String[] args) {
         printGreeting();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            hLine();
+            System.out.println(command);
+            hLine();
+        }
+
         printFarewell();
     }
 
-    private static void printGreeting() {
-        System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
-        System.out.println(" Hello! I'm MonoBot");
-        System.out.println(" What can I do for you?");
+    private static void hLine() {
         System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
     }
 
+    private static void printGreeting() {
+        hLine();
+        System.out.println(" Hello! I'm MonoBot");
+        System.out.println(" What can I do for you?");
+        hLine();    }
+
     private static void printFarewell() {
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+        hLine();
     }
 }
