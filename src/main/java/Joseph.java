a1e21c1496f1c8c6b331b17cebe006c7f2298008
Shantaa <shanetay4425@gmail.com>
2024-08-22 12:52:36 +0800

    it works!

---
+++ src/main/java/Joseph.java
@@ -0,0 +1,10 @@
+public class Joseph {
+    public static void main(String[] args) {
+        String name = "Joseph";
+        System.out.println("---------------------------------");
+        System.out.println("Hello, I'm " + name + "!");
+        System.out.println("How can I help you today?");
+        System.out.println("---------------------------------");
+        System.out.println("Bye! Have a nice day :)");
+    }
+}
