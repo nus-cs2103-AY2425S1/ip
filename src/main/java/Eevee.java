a38eb03e5458fd1bb68e4fefe7fd49bd9278e17a
myrilla12 <myrilla12@gmail.com>
2024-08-22 13:54:36 +0800

    Add mark and unmark task functionality

--- src/main/java/Eevee.java
+++ src/main/java/Eevee.java
@@ -12,18 +12,33 @@ public class Eevee {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.next();
             System.out.print(divider);
 
             if (input.equals("bye")) {
+                // response to command 'bye'
                 System.out.println(exit);
                 break;
             } else if (input.equals("list")) {
+                // response to command 'list'
                 System.out.println("Here are your tasks:");
                 tasks.forEach((task) ->
                     System.out.println((tasks.indexOf(task) + 1) + ". " + task)
                 );
+            } else if (input.equals("mark")) {
+                // response to command 'mark'
+                int taskNumber = scanner.nextInt();
+                Task t = tasks.get(taskNumber - 1);
+                t.markAsDone();
+                System.out.println("Congratulations! I've marked the following task as done:\n  " + t);
+            } else if (input.equals("unmark")) {
+                // response to command 'unmark'
+                int taskNumber = scanner.nextInt();
+                Task t = tasks.get(taskNumber - 1);
+                t.unmarkAsDone();
+                System.out.println("Ok! Task no longer marked as done:\n  " + t);
             } else {
+                // response to a task
                 Task t = new Task(input);
                 tasks.add(t);
                 System.out.println("Added '" + input + "' to your task list");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,7 +8,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
     }
 
     public void markAsDone() {
@@ -21,6 +21,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return description;
+        return getStatusIcon() + " " + description;
     }
 }
