c490832b81832fd15f617441ff0d86ee2b57476e
Jonathan Loh <jonathanloh02@gmail.com>
2024-08-23 03:10:24 +0800

    Refactor into switch case

--- src/main/java/Vuewee.java
+++ src/main/java/Vuewee.java
@@ -14,55 +14,64 @@ public class Vuewee {
     while (true) {
       String input = scanner.nextLine();
 
+      String[] inputParts = input.split(" ", 1);
+
+      if (inputParts.length == 0) {
+        throw new Error("Unknown error occured. Input error.");
+      }
+
+      String command = inputParts[0];
+
       // Exit the loop if user types "bye"
-      if (input.equals("bye")) {
+      if (command.equals("bye")) {
         break;
       }
 
-      // List all tasks with done status if user types "list"
-      if (input.equals("list")) {
-        System.out.println("____________________________________________________________");
-        for (int i = 0; i < taskListLength; i++) {
-          Task task = taskList[i];
-          System.out.println((i + 1) + ". [" + task.getStatusIcon() + "] " + task.getDescription());
-        }
-        System.out.println("____________________________________________________________");
-        continue;
-      }
-
-      // Mark task as done if user types "mark <task number>"
-      if (input.startsWith("mark")) {
-        String[] words = input.split(" ");
-        int taskNumber = Integer.parseInt(words[1]) - 1;
-        boolean success = taskList[taskNumber].markAsDone();
-
-        System.out.println("____________________________________________________________");
-        System.out.println(success ? "Nice! I've marked this task as done:" : "This task is already done:");
-        System.out.println("  [X] " + taskList[taskNumber].getDescription());
-        System.out.println("____________________________________________________________");
-        continue;
-      }
-
-      // Mark task as undone if user types "unmark <task number>"
-      if (input.startsWith("unmark")) {
-        String[] words = input.split(" ");
-        int taskNumber = Integer.parseInt(words[1]) - 1;
-        boolean success = taskList[taskNumber].markAsUndone();
-
-        System.out.println("____________________________________________________________");
-        System.out.println(
-            success ? "OK, I've marked this task as not done yet:" : "This task is already marked as not done:");
-        System.out.println("  [ ] " + taskList[taskNumber].getDescription());
-        System.out.println("____________________________________________________________");
-        continue;
-      }
-
-      // Add task to task list
-      taskList[taskListLength] = new Task(input);
-      taskListLength++;
-
       System.out.println("____________________________________________________________");
-      System.out.println("added: " + input);
+      switch (command) {
+        // List all tasks with done status if user types "list"
+        case "list": {
+          if (taskListLength == 0) {
+            System.out.println("You have no tasks in your list.");
+            break;
+          }
+
+          for (int i = 0; i < taskListLength; i++) {
+            Task task = taskList[i];
+            System.out.println((i + 1) + ". [" + task.getStatusIcon() + "] " + task.getDescription());
+          }
+          break;
+        }
+
+        // Mark task as done if user types "mark <task number>"
+        case "mark": {
+          String[] words = input.split(" ");
+          int taskNumber = Integer.parseInt(words[1]) - 1;
+          boolean success = taskList[taskNumber].markAsDone();
+
+          System.out.println(success ? "Nice! I've marked this task as done:" : "This task is already done:");
+          System.out.println("  [X] " + taskList[taskNumber].getDescription());
+          break;
+        }
+
+        case "unmark": {
+          String[] words = input.split(" ");
+          int taskNumber = Integer.parseInt(words[1]) - 1;
+          boolean success = taskList[taskNumber].markAsUndone();
+
+          System.out.println(
+              success ? "OK, I've marked this task as not done yet:" : "This task is already marked as not done:");
+          System.out.println("  [ ] " + taskList[taskNumber].getDescription());
+          break;
+        }
+        // Add task to task list
+        default: {
+          taskList[taskListLength] = new Task(input);
+          taskListLength++;
+          System.out.println("added: " + input);
+          break;
+        }
+      }
       System.out.println("____________________________________________________________");
     }
 
