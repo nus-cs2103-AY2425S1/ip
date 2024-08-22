779eb54fbe9671d7c257999b13b9ed7faa09dadd
Jem Zhang <zhangjem321@gmail.com>
2024-08-22 17:13:57 +0800

    feat: echo and bye

--- src/main/java/Donk.java
+++ src/main/java/Donk.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Donk {
     public static void main(String[] args) {
         String greeting = " ____________________________________________________________\n" +
                 " Hello! I'm Donk, the super intelligent chatbot\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        String byeMsg = "    Bye bro, catch 'ya later\n" +
                 "____________________________________________________________\n";
         System.out.println(greeting);
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(byeMsg);
+                break;
+            }
+            System.out.println("    " + userInput);
+        }
+
     }
 }
