229a10f3a2a8c180eb884f42780195b01c3864ee
hastutiherah <hastherah@gmail.com>
2024-08-22 21:24:57 +0800

    Rename Duke to Rose

---
+++ src/main/java/Rose.java
@@ -0,0 +1,13 @@
+public class Rose {
+    public static void main(String[] args) {
+        String horizontal = "____________________________________________________________";
+        String name = "Rose";
+
+        System.out.println(horizontal);
+        System.out.println("Hi gorgeous! I'm " + name + "~~");
+        System.out.println("How can I help you today?");
+        System.out.println(horizontal);
+        System.out.println("See you~~ Don't forget to drink some water ^_^");
+        System.out.println(horizontal);
+    }
+}
