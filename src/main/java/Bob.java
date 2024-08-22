376226180ecc52405b28b8a2ee94292d5b6a3c36
yixianggg <e1186210@u.nus.edu>
2024-08-22 15:34:06 +0800

    rename Duke to Bob, implement greet and exit

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,12 @@
-public class Duke {
+public class Bob {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        String name = "Bob";
+        System.out.println(line);
+        System.out.printf("Hello! I'm %s!\n", name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
