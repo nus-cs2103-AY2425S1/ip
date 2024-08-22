2416f469e7217debce41d174b31c0336f86d6f80
LiHaixin000 <lihaixin000@gmail.com>
2024-08-22 22:17:44 +0800

    Use enums in Arts class

--- src/main/java/Arts.java
+++ src/main/java/Arts.java
@@ -25,58 +25,74 @@ public class Arts {
             try {
                 String input = scanner.nextLine().trim();
                 String[] parts = input.split(" ", 2);
+                Command command = Command.fromString(parts[0]);
 
-                if (parts[0].equalsIgnoreCase("bye")) {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Bye! Hope to see you again soon!");
-                    System.out.println("____________________________________________________________");
-                    break;
-                } else if (parts[0].equalsIgnoreCase("list")) {
-                    System.out.println("____________________________________________________________");
-                    if (taskCount == 0) {
-                        System.out.println("No tasks yet! Why not add some?");
-                    } else {
-                        System.out.println("Here are the tasks in your list:");
-                        for (int i = 0; i < tasks.size(); i++) {
-                            System.out.println((i + 1) + ". " + tasks.get(i));
+                switch (command) {
+                    case BYE:
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Bye! Hope to see you again soon!");
+                        System.out.println("____________________________________________________________");
+                        return;
+
+                    case LIST:
+                        System.out.println("____________________________________________________________");
+                        if (tasks.isEmpty()) {
+                            System.out.println("No tasks yet! Why not add some?");
+                        } else {
+                            System.out.println("Here are the tasks in your list:");
+                            for (int i = 0; i < tasks.size(); i++) {
+                                System.out.println((i + 1) + ". " + tasks.get(i));
+                            }
                         }
-                    }
-                    System.out.println("____________________________________________________________");
-                } else if (parts[0].equalsIgnoreCase("mark")) {
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                        tasks.get(taskIndex).markAsDone();
-                    }
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Nice! I've marked this task as done.");
-                    System.out.println(" " + tasks.get(taskIndex));
-                    System.out.println("____________________________________________________________");
-                } else if (parts[0].equalsIgnoreCase("unmark")) {
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                        tasks.get(taskIndex).markAsNotDone();
-                    }
-                    System.out.println("____________________________________________________________");
-                    System.out.println("OK, I've marked this task as not done.");
-                    System.out.println(" " + tasks.get(taskIndex));
-                    System.out.println("____________________________________________________________");
-                } else if (parts[0].equalsIgnoreCase("delete")) {
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                        Task removedTask = tasks.remove(taskIndex);
                         System.out.println("____________________________________________________________");
-                        System.out.println("Noted. I've removed this task:");
-                        System.out.println(" " + removedTask);
-                        System.out.println("Now you have " + tasks.size() + " " + (tasks.size() == 1 ? "task" : "tasks") + " in the list.");
-                        System.out.println("____________________________________________________________");
-                    }
-                } else {
-                    if (parts[0].equalsIgnoreCase("todo")) {
+                        break;
+
+                    case MARK:
+                        int markIndex = Integer.parseInt(parts[1]) - 1;
+                        if (markIndex >= 0 && markIndex < tasks.size()) {
+                            tasks.get(markIndex).markAsDone();
+                            System.out.println("____________________________________________________________");
+                            System.out.println("Nice! I've marked this task as done.");
+                            System.out.println(" " + tasks.get(markIndex));
+                            System.out.println("____________________________________________________________");
+                        }
+                        break;
+
+                    case UNMARK:
+                        int unmarkIndex = Integer.parseInt(parts[1]) - 1;
+                        if (unmarkIndex >= 0 && unmarkIndex < tasks.size()) {
+                            tasks.get(unmarkIndex).markAsNotDone();
+                            System.out.println("____________________________________________________________");
+                            System.out.println("OK, I've marked this task as not done.");
+                            System.out.println(" " + tasks.get(unmarkIndex));
+                            System.out.println("____________________________________________________________");
+                        }
+                        break;
+
+                    case DELETE:
+                        int deleteIndex = Integer.parseInt(parts[1]) - 1;
+                        if (deleteIndex >= 0 && deleteIndex < tasks.size()) {
+                            Task removedTask = tasks.remove(deleteIndex);
+                            System.out.println("____________________________________________________________");
+                            System.out.println("Noted. I've removed this task:");
+                            System.out.println(" " + removedTask);
+                            System.out.println("Now you have " + tasks.size() + " " + (tasks.size() == 1 ? "task" : "tasks") + " in the list.");
+                        }
+                        break;
+
+                    case TODO:
                         if (parts.length < 2 || parts[1].trim().isEmpty()) {
                             throw new ArtsException("The description of a todo cannot be empty.");
                         }
                         tasks.add(new Todo(parts[1]));
-                    } else if (parts[0].equalsIgnoreCase("deadline")) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(" " + tasks.get(tasks.size() - 1));
+                        System.out.println("Now you have " + tasks.size() + " " + (tasks.size() == 1 ? "task" : "tasks") + " in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+
+                    case DEADLINE:
                         if (parts.length < 2 || parts[1].trim().isEmpty()) {
                             throw new ArtsException("The description of a deadline cannot be empty.");
                         }
@@ -85,7 +101,14 @@ public class Arts {
                             throw new ArtsException("The deadline must have a /by date.");
                         }
                         tasks.add(new Deadline(deadlineParts[0], deadlineParts[1]));
-                    } else if (parts[0].equalsIgnoreCase("event")) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(" " + tasks.get(tasks.size() - 1));
+                        System.out.println("Now you have " + tasks.size() + " " + (tasks.size() == 1 ? "task" : "tasks") + " in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+
+                    case EVENT:
                         if (parts.length < 2 || parts[1].trim().isEmpty()) {
                             throw new ArtsException("The description of an event cannot be empty.");
                         }
@@ -94,15 +117,15 @@ public class Arts {
                             throw new ArtsException("The event must have /from and /to times.");
                         }
                         tasks.add(new Event(eventParts[0], eventParts[1], eventParts[2]));
-                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(" " + tasks.get(tasks.size() - 1));
+                        System.out.println("Now you have " + tasks.size() + " " + (tasks.size() == 1 ? "task" : "tasks") + " in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+
+                    default:
                         throw new ArtsException("I'm sorry, but I don't know what that means.");
-                    }
-                    taskCount++;
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(" " + tasks.get(tasks.size() - 1));
-                    System.out.println("Now you have " + taskCount + " " + (taskCount == 1 ? "task" : "tasks") + " in the list.");
-                    System.out.println("____________________________________________________________");
                 }
             } catch (ArtsException e) {
                 System.out.println("____________________________________________________________");
@@ -114,8 +137,7 @@ public class Arts {
                 System.out.println("____________________________________________________________");
             }
         }
-
-        scanner.close();
     }
 }
+
 
