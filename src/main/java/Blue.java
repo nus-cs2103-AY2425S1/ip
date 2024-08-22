77bb99713123447935b7c39000cdd193535bb849
waihin26 <138366109+waihin26@users.noreply.github.com>
2024-08-22 23:49:18 +0800

    Added list feature to Blue.java file

--- src/main/java/Blue.java
+++ src/main/java/Blue.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class Blue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Note note = new Note();
+        int count = 0;
 
         // Greet the user
         System.out.println("--------------------------------------------");
-        System.out.println("Hello! I'm Blue");
+        System.out.println("Hello! I'm Blue! Woof Woof! Yap Yap!");
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------------------");
 
@@ -14,10 +16,21 @@ public class Blue {
         // Keep echoing user input until "bye" is typed
         while (true) {
             input = scanner.nextLine();
+
             if (input.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println(input);
+            if (input.equalsIgnoreCase("list")) {
+                //print the notes
+                System.out.println("--------------------------------------------");
+                note.printList();
+                System.out.println("--------------------------------------------");
+                continue;
+            }
+            //print out added... and add the item to the myList field in the Note object
+            note.addToList(input);
+            System.out.println("--------------------------------------------");
+            System.out.printf("added: %s%n", input);
             System.out.println("--------------------------------------------");
         }
 
