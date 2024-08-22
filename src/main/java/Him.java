6cce929efc053d20105d32d1f0b8f2c17e9d6e06
Isaac Pang <isaac.pangth@gmail.com>
2024-08-23 05:23:25 +0800

    Added support for echo

--- src/main/java/Him.java
+++ src/main/java/Him.java
@@ -1,15 +1,25 @@
-public class Him{
-    private static final String DIVIDER = "\n____________________________________________________________\n";
+import java.util.Scanner;
+
+public class Him {
+
     private static void greet() {
-        System.out.print(DIVIDER + " Hello! I'm Him\n What can I do for you?" + DIVIDER);
+        System.out.println("Him: Hello! I'm Him\n     What can I do for you?\n");
     }
 
     private static void exit() {
-        System.out.print(" Bye. Hope to see you again soon!" + DIVIDER);
+        System.out.println("\nHim: Bye. Hope to see you again soon!\n");
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         greet();
+        System.out.print("User: ");
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("\nHim: " + input + "\n");
+            System.out.print("User: ");
+            input = scanner.nextLine();
+        }
         exit();
     }
 }
