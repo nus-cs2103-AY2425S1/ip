ab684945efda6223765656eedbf9ab45667c6284
ossaris56 <ossaris56@gmail.com>
2024-08-23 03:21:25 +0800

    add echo

--- src/main/java/Asura.java
+++ src/main/java/Asura.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Asura {
+    public static String formatResponse(String msg) {
+        String startBorder = "---------------------------------------\n";
+        String endBorder = "\n---------------------------------------";
+        String formattedMsg = startBorder + msg + endBorder;
+        return formattedMsg.indent(3);
+    }
+
     public static void main(String[] args) {
-        System.out.println("---------------------------------------");
-        System.out.println("Hello! I'm Asura!");
-        System.out.println("What can I do for you?");
-        System.out.println("---------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("---------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        String introduction = """
+                Hello! I'm Asura!
+                What can I do for you?""";
+        String goodbye = """
+                Bye. Hope to see you again soon!""";
+
+        System.out.println(formatResponse(introduction));
+
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(formatResponse(input));
+            input = scanner.nextLine();
+        }
+        System.out.println(formatResponse(goodbye));
     }
 }
