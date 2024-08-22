30f6bc8b3775c3f725481cd17b6436a8d8928a93
luileng <chengluileng@gmail.com>
2024-08-23 06:24:47 +0800

    Add Duck.java

---
+++ src/main/java/Duck.java
@@ -0,0 +1,9 @@
+public class Duck {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String welcomeMessage = "Hello! I'm Duck\nWhat can I do for you?";
+        System.out.println(line + '\n' + welcomeMessage + '\n' + line);
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage + '\n' + line);
+    }
+}
