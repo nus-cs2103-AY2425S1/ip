3b348cc9db9c0be245138ffa68ee942be523c07d
avrilmohh <avrilmoh1234@gmail.com>
2024-08-22 18:07:17 +0800

    renamed bot name

---
+++ src/main/java/Tudee.java
@@ -0,0 +1,11 @@
+public class Tudee {
+    public static void main(String[] args) {
+        String logo = "---------------------------------------- \n"
+                + "Hello! I'm Tudee \n"
+                + "What can I do for you? \n"
+                + "---------------------------------------- \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "---------------------------------------- \n";
+        System.out.println(logo);
+    }
+}
