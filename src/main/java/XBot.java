1b300c95071ed25e4094282a9e50c0f604fc2520
xGladiate <sharonsohxuanhui@hotmail.com>
2024-08-22 13:35:12 +0800

    Main: Modify conditional statement to switch

--- src/main/java/XBot.java
+++ src/main/java/XBot.java
@@ -1,36 +1,59 @@
 import java.util.Scanner;
 
 public class XBot {
+    private static Task[] list = new Task[100];
+    private static int taskCount = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] list = new Task[100];
-        int taskCount = 0;
 
         System.out.println("Hello! I'm XBot\n" + "What can I do for you?");
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().trim();
         while(!input.equalsIgnoreCase("bye")) {
             String[] words = input.split("\\s+", 2);
-            String command = words[0];
+            String command = words[0].toLowerCase();
             String rest = words.length > 1 ? words[1] : "";
-            if (input.equalsIgnoreCase("list")) {
-                //display list
-                System.out.println("Here are the tasks in your list: ");
-                for (int i = 0; i < taskCount; i++) {
-                    int index = i + 1;
-                    System.out.println(index + ". " + list[i].toString());
-                }
-            } else if (!input.isEmpty()) {
-                //store input in list
-                list[taskCount] = new Task(input);
-                System.out.println("added: " + input);
-                taskCount++;
-            } else {
-                //do nothing when input is empty
+            switch(command) {
+                case "list":
+                    displayTask();
+                    break;
+                case "mark":
+                    markDone(rest);
+                    break;
+                case "unmark":
+                    markUndone(rest);
+                    break;
+                default:
+                    if (!input.isEmpty()) {
+                        addTask(input);
+                    }
             }
             input = scanner.nextLine();
         }
-
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
+    }
+
+    public static void displayTask() {
+        System.out.println("Here are the tasks in your list: ");
+        for (int i = 0; i < taskCount; i++) {
+            int index = i + 1;
+            System.out.println(index + ". " + list[i].toString());
+        }
+    }
+
+    public static void addTask(String input) {
+        list[taskCount] = new Task(input);
+        System.out.println("added: " + input);
+        taskCount++;
+    }
+
+    public static void markDone(String rest) {
+        int taskNumber = Integer.parseInt(rest.trim());
+        list[taskNumber - 1].markAsDone();
+    }
+
+    public static void markUndone(String rest) {
+        int taskNumber = Integer.parseInt(rest.trim());
+        list[taskNumber - 1].markAsUndone();
     }
 }
