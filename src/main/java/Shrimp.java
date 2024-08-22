8a6975cbbe657699f09bf53412ae05a1c4cc0286
Jing Hong Tee <e1122714@u.nus.edu>
2024-08-23 00:49:26 +0800

    Add ability to mark and unmark tasks as done
    - Implemented the ability to mark tasks as done and not done.
    - Updated the task listing to reflect the current status of each task.
    - Used an enum to handle the commands for better code structure and readability.

--- src/main/java/Shrimp.java
+++ src/main/java/Shrimp.java
@@ -5,38 +5,70 @@ public class Shrimp {
     private static final String PARTITION = "____________________________________________________________";
 
     public static void main(String[] args) {
-        //program initialize
+        //program initialise
         programStart();
 
-        //echo user's command
+        //scans for user's command
         Scanner sc = new Scanner(System.in);
-        String userInput;
+        String userInput, output;
         TaskList taskList = new TaskList();
 
         while (true) {
-            userInput = sc.nextLine();  // Read the next line of user input
-            Task newTask = new Task(userInput);
+            userInput = sc.nextLine();  //read the next line of user input
+            Task newTask = new Task(userInput); //creates a new Task object
 
-            if (userInput.equalsIgnoreCase("bye")) {
-                //todo Add this as a special case (ExitTask)
-                break;  // Exit the loop if "bye" is entered
-            }
-            if (userInput.equalsIgnoreCase("list")) {
-                System.out.println(PARTITION);
-                System.out.println("Gotchaaa~ Here's the list so far:");
-                for (int i = 0; i < taskList.getCount(); i++) {
-                    System.out.println(i+1 + ". " + taskList.getTask(i));
-                }
-                System.out.println(PARTITION);
-            } else {
-                taskList.addTask(newTask);
-                String output = "rawr! '" + userInput + "' has been added to the list~";
-                System.out.println(PARTITION);
-                System.out.println(output);
-                System.out.println(PARTITION);
+            CommandParser.CommandType commandType = CommandParser.parseCommand(userInput);
+
+            switch (commandType) {
+                case BYE: //exits the program
+                    programExit();
+                    return;
+
+                case LIST:
+                    System.out.println(PARTITION);
+                    System.out.println("Gotchaaa~ Here's the list so far:");
+                    for (int i = 0; i < taskList.getCount(); i++) {
+                        Task task = taskList.getTask(i);
+                        output =  String.format("%s.%s %s", i + 1,
+                                task.getHasCompleted() ? "[X]" : "[ ]", task);
+                        System.out.println(output);
+                    }
+                    System.out.println(PARTITION);
+                    break;
+
+                case ADD:
+                    taskList.addTask(newTask);
+                    output = "rawr! '" + userInput + "' has been added to the list~";
+                    System.out.println(PARTITION);
+                    System.out.println(output);
+                    System.out.println(PARTITION);
+                    break;
+
+                case MARK:
+                    int indexMark = getTaskNumber(userInput);
+                    Task oldTaskMark = taskList.getTask(indexMark);
+                    Task updatedTaskMark = new Task(oldTaskMark.toString(), true);
+                    taskList.replaceTask(indexMark, updatedTaskMark);
+                    output = "heya~ I've checked this task as complete! Feels good, right?";
+                    System.out.println(PARTITION);
+                    System.out.println(output);
+                    System.out.println("    [X] " + updatedTaskMark);
+                    System.out.println(PARTITION);
+                    break;
+
+                case UNMARK:
+                    int indexUnmark = getTaskNumber(userInput);
+                    Task oldTaskUnmark = taskList.getTask(indexUnmark);
+                    Task updatedTaskUnmark = new Task(oldTaskUnmark.toString());
+                    taskList.replaceTask(indexUnmark, updatedTaskUnmark);
+                    output = "Whoops~ I've unchecked the task as incomplete! Be careful next time~";
+                    System.out.println(PARTITION);
+                    System.out.println(output);
+                    System.out.println("    [ ] " + updatedTaskUnmark);
+                    System.out.println(PARTITION);
+                    break;
             }
         }
-        programExit();
     }
 
     static void programStart() {
@@ -62,5 +94,10 @@ public class Shrimp {
                 "I'll see you later~";
         System.out.println(output);
         System.out.println(PARTITION);
+    }
+
+    // Helper method to extract task number for MARK
+    private static int getTaskNumber(String userInput) {
+        return Integer.parseInt(userInput.split(" ")[1]) - 1;
     }
 }
