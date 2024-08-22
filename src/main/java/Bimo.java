e789ebd418f216cfa9bebbba66eb1955cd18bf85
CJianzhi <cjianzhi23@gmail.com>
2024-08-22 12:19:52 +0800

    Add list command feature

--- src/main/java/Bimo.java
+++ src/main/java/Bimo.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Bimo {
     public static String name = "Bimo";
-    public static String line = "    " + "__________________________________";
+    public static String line = "    " + "----------------------------------";
     public static Scanner scanner = new Scanner(System.in);
     public static int length = 0;
     public static Task[] list = new Task[100];
@@ -12,15 +12,23 @@ public class Bimo {
         System.out.println(line);
         String command = scanner.nextLine();
         while (!command.toLowerCase().equals("bye")) {
-            Task task = new Task(command);
-            Bimo.list[length] = task;
-            length += 1;
             System.out.println(line);
-            System.out.println("     added:" + task.toString());
+            if (command.toLowerCase().equals("list") && length > 0 ) {
+                for (int i = 0; i < length; i++) {
+                    String message = String.format("    %d. %s", i + 1, list[i].toString());
+                    System.out.println(message);
+                }
+            } else if (command.toLowerCase().equals("list")) {
+                System.out.println("    No tasks added");
+            } else  {
+                Task task = new Task(command);
+                Bimo.list[length] = task;
+                length += 1;
+                System.out.println("    added: " + task.toString());
+            }
             System.out.println(line);
             command = scanner.nextLine();
         }
-        System.out.println(line);
         System.out.println("    " +"Bye. Hope to see you again soon!");
         System.out.println(line);
     }
