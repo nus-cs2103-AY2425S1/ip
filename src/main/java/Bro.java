a89ec5e3967735373a18f79fde8506d99ca9def9
Mattis Ong <mattisongjj@gmail.com>
2024-08-22 16:16:21 +0800

    Feat: Implement echo and goodbye commands

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 public class Bro {
     public static void main(String[] args) {
-        String greeting = """
-                ____________________________________________________________
+        String GREETING = """
                  Hello! I'm Bro
-                 What can I do for you?
+                 What can I do for you?""";
+        String GOODBYE = "Goodbye.";
+        String EXIT_COMMAND = "bye";
+        reply(GREETING);
+        // Start conversation
+        Scanner sc = new Scanner(System.in);
+        boolean isConversing = true;
+        while (isConversing) {
+            String input = sc.nextLine();
+            if (input.equals(EXIT_COMMAND)) {
+                isConversing = false;
+            } else {
+                reply(input);
+            }
+        }
+        reply(GOODBYE);
+    }
+
+    // Prints a reply on standard output
+    public static void reply(String content) {
+        String replyStr = String.format("""
                 ____________________________________________________________
-                 Bye. Hope to see you again soon!
+                %s. Bro
                 ____________________________________________________________
-                """;
-        System.out.println(greeting);
+                """, content);
+        System.out.print(replyStr);
     }
 }
