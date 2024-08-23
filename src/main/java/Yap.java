8553d2a5a242ea476a3e65f07e48d9ff5f1d605b
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-08-23 13:39:51 +0800

    Write expected output for testcase, tested code

--- src/main/java/Yap.java
+++ src/main/java/Yap.java
@@ -5,14 +5,15 @@ public class Yap {
         Scanner scanner = new Scanner(System.in);
 
         // Print logo and introductions
-        String logo = "_    _  __     ______\n"
-                    + " \\  // //\\    ||__| |\n"
-                    + "  \\// //__\\   ||____/\n"
-                    + "  || //____\\  ||\n"
-                    + "  ||//      \\ ||\n";
+//        String logo = "_    _  __     ______\n"
+//                    + " \\  // //\\    ||__| |\n"
+//                    + "  \\// //__\\   ||____/\n"
+//                    + "  || //____\\  ||\n"
+//                    + "  ||//      \\ ||\n";
+        String logo = "Yap";
         String separator = "_____________________________________";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What would you like me to do for you? ");
+        System.out.println("Hello from " + logo);
+        System.out.println("What would you like me to do for you?");
         System.out.println(separator);
 
         // Infinite loop to get user input
@@ -27,13 +28,11 @@ public class Yap {
                 break;
             }
 
-            System.out.println(separator);
-
             // Mark functionality
             if (userInput.startsWith("mark")) {
                 int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 tasks[taskIndex].markAsDone();
-                System.out.println("I've marked this task as done: ");
+                System.out.println("I've marked this task as done:");
                 System.out.println(tasks[taskIndex]);
                 System.out.println(separator);
                 continue;
@@ -43,7 +42,7 @@ public class Yap {
             if (userInput.startsWith("unmark")) {
                 int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 tasks[taskIndex].markAsUndone();
-                System.out.println("I've marked this task as not done: ");
+                System.out.println("I've marked this task as not done:");
                 System.out.println(tasks[taskIndex]);
                 System.out.println(separator);
                 continue;
@@ -54,6 +53,7 @@ public class Yap {
                 tasks[taskCount] = InputParser.parseInputAsToDo(userInput);
                 System.out.println("Added: " + tasks[taskCount++]);
                 System.out.printf("You now have %d tasks in the list%n", taskCount);
+                System.out.println(separator);
                 continue;
             }
 
@@ -62,6 +62,7 @@ public class Yap {
                 tasks[taskCount] = InputParser.parseInputAsDeadline(userInput);
                 System.out.println("Added: " + tasks[taskCount++]);
                 System.out.printf("You now have %d tasks in the list%n", taskCount);
+                System.out.println(separator);
                 continue;
             }
 
@@ -70,11 +71,12 @@ public class Yap {
                 tasks[taskCount] = InputParser.parseInputAsEvent(userInput);
                 System.out.println("Added: " + tasks[taskCount++]);
                 System.out.printf("You now have %d tasks in the list%n", taskCount);
+                System.out.println(separator);
                 continue;
             }
 
             // List functionality
-            if (userInput.equalsIgnoreCase("list")) {
+            if (userInput.startsWith("list")) {
                 for (int input = 0; input < taskCount; ++input) {
                     System.out.println((input + 1) + ". " + tasks[input].toString());
                 }
