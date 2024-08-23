d1adaa12e5d82682c970ef387f8bd8c470fcca77
Judy1x4 <e1129873@u.nus.edu>
2024-08-23 10:48:53 +0800

    Correct Java layout and add header comments

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -1,29 +1,39 @@
 import java.util.Scanner;
 
 public class Jude {
-  public static void main(String[] args) {
-    String name = "Jude";
-    Scanner scanner = new Scanner(System.in);
-    String[] list = new String[100];
-    int listSize = 0;
+    public static void main(String[] args) {
 
-    System.out.println("Hello I'm " + name);
-    System.out.println("What can I do for you, poyo?");
+        // Initialize the variables
+        String name = "Jude";
+        String[] list = new String[100];
+        int listSize = 0;
+        Scanner scanner = new Scanner(System.in);
 
-    String userInput = scanner.nextLine();
-    while (!userInput.equals("bye")) {
-      if (userInput.equals("list")) {
-        for (int i = 0; i < listSize; i++) {
-          System.out.printf("%d. %s\n", (i + 1), list[i]);
+        // Initiate the chat
+        System.out.println("Hello I'm " + name);
+        System.out.println("What can I do for you, poyo?");
+
+        // Receive first user input
+        String userInput = scanner.nextLine();
+
+        // Terminate input if is bye, iterate otherwise.
+        while (!userInput.equals("bye")) {
+
+            // If the input is list, shows the list of tasks saved.
+            if (userInput.equals("list")) {
+                for (int i = 0; i < listSize; i++) {
+                    System.out.printf("%d. %s\n", (i + 1), list[i]);
+                }
+            } else {
+                list[listSize++] = userInput;
+                System.out.println("added: " + userInput);
+            }
+            userInput = scanner.nextLine();
         }
-      } else {
-        list[listSize++] = userInput;
-        System.out.println("added: " + userInput);
-      }
-      userInput = scanner.nextLine();
+
+        // Terminate the chat
+        System.out.println("Bye. Hope to see you again soon, poyo!");
+        scanner.close();
     }
-    System.out.println("Bye. Hope to see you again soon, poyo!");
-    scanner.close();
-  }
 }
 
