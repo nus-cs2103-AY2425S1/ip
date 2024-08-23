0c9bb778f07028a803b54121d56d8a285eff71ff
Casper Ng <casperngeen@gmail.com>
2024-08-23 12:26:43 +0800

    Added exception handling

--- src/main/java/CasperBot.java
+++ src/main/java/CasperBot.java
@@ -1,4 +1,6 @@
 package main.java;
+import exception.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +13,7 @@ public class CasperBot {
         EVENT, TODO, DEADLINE;
     }
     private static List<Task> list = new ArrayList<>();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CasperBotException {
         line();
         System.out.println("Hello! I'm CasperBot.\n" +
                 "What can I do for you?");
@@ -23,65 +25,111 @@ public class CasperBot {
         System.out.println("------------------------------------------");
     }
 
-    private static void echo() {
+    private static void echo() throws CasperBotException {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         while (!input.equalsIgnoreCase("bye")) {
             input = scanner.nextLine();
             String[] inputArray = splitInputIntoTwo(input);
             line();
-            if (inputArray[0].equalsIgnoreCase("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.printf("%d. %s%n", i + 1, list.get(i));
+            try {
+                if (inputArray[0].equalsIgnoreCase("list")) {
+                    if (list.isEmpty()) {
+                        System.out.println("You currently have no tasks in your list!");
+                    } else {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < list.size(); i++) {
+                            System.out.printf("%d. %s%n", i + 1, list.get(i));
+                        }
+                    }
                 }
-            }
-            else if (inputArray[0].equalsIgnoreCase("mark")) {
-                Task task = list.get(Integer.parseInt(inputArray[1])-1);
-                task.markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(task);
-            }
-            else if (inputArray[0].equalsIgnoreCase("unmark")) {
-                Task task = list.get(Integer.parseInt(inputArray[1])-1);
-                task.markAsNotDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(task);
-            }
-            else if (isValidCommand(inputArray[0])) {
-                System.out.println("Got it. I've added this task:");
-                HashMap<String, String> hashMap = new HashMap<>();
-                parseBySlash(inputArray[1], hashMap);
-                Command command = Command.valueOf(inputArray[0].trim().toUpperCase());
-                switch (command) {
-                    case TODO:
-                        String todoDescription = hashMap.get("description");
-                        ToDo newToDo = new ToDo(todoDescription);
-                        list.add(newToDo);
-                        System.out.println("  " + newToDo);
-                        break;
-                    case DEADLINE:
-                        String deadlineDescription = hashMap.get("description");
-                        String deadline = hashMap.get("by");
-                        Deadline newDeadline = new Deadline(deadlineDescription, deadline);
-                        list.add(newDeadline);
-                        System.out.println("  " + newDeadline);
-                        break;
-                    case EVENT:
-                        String eventDescription = hashMap.get("description");
-                        String start = hashMap.get("from");
-                        String end = hashMap.get("to");
-                        Event newEvent = new Event(eventDescription, start, end);
-                        list.add(newEvent);
-                        System.out.println("  " + newEvent);
-                        break;
+                else if (inputArray[0].equalsIgnoreCase("mark")) {
+                    try {
+                        int index = Integer.parseInt(inputArray[1]) - 1;
+                        if (index >= list.size()) {
+                            throw new CasperBotOutOfBoundsException();
+                        }
+                        Task task = list.get(index);
+                        task.markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(task);
+                    } catch (NumberFormatException e) {
+                        throw new CasperBotNumberFormatException();
+                    }
                 }
-                System.out.printf("Now you have %d tasks in the list.\n", list.size());
+                else if (inputArray[0].equalsIgnoreCase("unmark")) {
+                    try {
+                        int index = Integer.parseInt(inputArray[1]) - 1;
+                        if (index >= list.size()) {
+                            throw new CasperBotOutOfBoundsException();
+                        }
+                        Task task = list.get(index);
+                        task.markAsNotDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println(task);
+                    } catch (NumberFormatException e) {
+                        throw new CasperBotNumberFormatException();
+                    }
+                }
+                else if (isValidTaskCommand(inputArray[0])) {
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    parseBySlash(inputArray[1], hashMap);
+                    Command command = Command.valueOf(inputArray[0].trim().toUpperCase());
+                    switch (command) {
+                        case TODO:
+                            String todoDescription = hashMap.get("description");
+                            if (todoDescription.isEmpty()) {
+                                throw new CasperBotMissingInputException("description", "ToDo");
+                            }
+                            ToDo newToDo = new ToDo(todoDescription);
+                            list.add(newToDo);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println("  " + newToDo);
+                            break;
+                        case DEADLINE:
+                            String deadlineDescription = hashMap.get("description");
+                            if (deadlineDescription.isEmpty()) {
+                                throw new CasperBotMissingInputException("description", "Deadline");
+                            }
+                            String deadline = hashMap.get("by");
+                            if (deadline == null || deadline.isEmpty()) {
+                                throw new CasperBotMissingInputException("/by", "Deadline");
+                            }
+                            Deadline newDeadline = new Deadline(deadlineDescription, deadline);
+                            list.add(newDeadline);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println("  " + newDeadline);
+                            break;
+                        case EVENT:
+                            String eventDescription = hashMap.get("description");
+                            if (eventDescription.isEmpty()) {
+                                throw new CasperBotMissingInputException("description", "Event");
+                            }
+                            String start = hashMap.get("from");
+                            if (start == null || start.isEmpty()) {
+                                throw new CasperBotMissingInputException("/from", "Event");
+                            }
+                            String end = hashMap.get("to");
+                            if (end == null || end.isEmpty()) {
+                                throw new CasperBotMissingInputException("/to", "Event");
+                            }
+                            Event newEvent = new Event(eventDescription, start, end);
+                            list.add(newEvent);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println("  " + newEvent);
+                            break;
+                    }
+                    System.out.printf("Now you have %d tasks in the list.\n", list.size());
+                } else if (inputArray[0].equalsIgnoreCase("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                } else {
+                    throw new CasperBotInvalidCommandException();
+                }
+            } catch (CasperBotException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                line();
             }
-            else if (inputArray[0].equalsIgnoreCase("bye")){
-                System.out.println("Bye. Hope to see you again soon!");
-            }
-            line();
         }
         scanner.close();
     }
@@ -120,7 +168,7 @@ public class CasperBot {
         }
     }
 
-    private static boolean isValidCommand(String command) {
+    private static boolean isValidTaskCommand(String command) {
         try {
             Command.valueOf(command.trim().toUpperCase());
             return true;
