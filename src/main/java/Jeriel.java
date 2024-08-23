d42b793271bbafbbdf1a845a736eee5eaaae14c1
Nimastic <jerielchanzy@gmail.com>
2024-08-23 08:03:02 +0800

    Level-1

--- src/main/java/Jeriel.java
+++ src/main/java/Jeriel.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Jeriel {
     public static void main(String[] args) {  
         String logo = "     _  ____  ___   _  ____  _      \n"
@@ -14,7 +17,26 @@ public class Jeriel {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+
+        while (true) {
+            input = scanner.nextLine(); 
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+
+        scanner.close();
     }
+
 }
