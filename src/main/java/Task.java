9925b14acf07568742e192139ee3e68c9f9fff74
psh12320 <pshricharan2020@gmail.com>
2024-08-23 10:21:30 +0800

    added a new Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String taskItem;
+    private boolean isCompleted;
+
+    public Task(String taskItem) {
+        this.taskItem = taskItem;
+        this.isCompleted = false;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setComplete() {
+        this.isCompleted = true;
+    }
+
+    public void setIncomplete() {
+        this.isCompleted = false;
+    }
+
+    public String getTaskItem() {
+        return taskItem;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.isCompleted ? "X" : " ") + "] " + this.taskItem;
+    }
+}
