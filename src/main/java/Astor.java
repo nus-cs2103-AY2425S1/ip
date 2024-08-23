28da1e4d06e6967829d0df348e8dc79b05e1bf57
yi hao <choiyihao123@gmail.com>
2024-08-24 01:16:50 +0800

    Added functions to allow task to be marked as done or not, handled exceptions to inputs

--- src/main/java/Astor.java
+++ src/main/java/Astor.java
@@ -9,22 +9,74 @@ public class Astor {
                 "--------------------------------------");
 
         Scanner scanner = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
-            }
-            if (input.equals("list")) {
+            } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 int index = 1;
-                for (String s : list) {
-                    System.out.println(index + ". " + s);
+                for (Task t : list) {
+                    System.out.println(index + ". " + t.toString());
                     index++;
                 }
                 System.out.println("--------------------------------------");
-            } else {
-                list.add(input);
+            } else if (input.startsWith("mark")) {
+                int index = -1;
+                try {
+                    String formattedString = input.substring(4).trim();
+                    index = Integer.parseInt(formattedString);
+                } catch (NumberFormatException e) {
+                    System.out.println("Please indicated clearly which task to mark!\n" +
+                            "--------------------------------------");
+                    continue;
+                }
+                if (index >= 1 && index <= list.size()) {
+                    Task task = list.get(index - 1);
+                    if (task.isDone()) {
+                        System.out.println("This task is already done:\n" +
+                                task + "\n--------------------------------------");
+                    } else {
+                        task.markDone();
+                        System.out.println("Nice! I've marked this task as done:\n" +
+                                task + "\n--------------------------------------");
+                    }
+                } else {
+                    System.out.println("Please enter a valid task number\n" +
+                            "--------------------------------------");
+                }
+            } else if (input.startsWith("unmark")) {
+                int index = -1;
+                try {
+                    String formattedString = input.substring(6).trim();
+                    index = Integer.parseInt(formattedString);
+                } catch (NumberFormatException e) {
+                    System.out.println("Please indicated clearly which task to unmark!\n" +
+                            "--------------------------------------");
+                    continue;
+                }
+                if (index >= 1 && index <= list.size()) {
+                    Task task = list.get(index - 1);
+                    if (task.isDone()) {
+                        task.markUndone();
+                        System.out.println("OK, I've marked this task as not done yet:\n" +
+                                task + "\n--------------------------------------");
+                    } else {
+                        System.out.println("This task is already marked as uncompleted:\n" +
+                                task + "\n--------------------------------------");
+                    }
+                } else {
+                    System.out.println("Please enter a valid task number\n" +
+                            "--------------------------------------");
+                }
+            } else if (input.isEmpty()) {
+                System.out.println("Please type something!\n" +
+                        "--------------------------------------");
+            }else {
+                Task task = new Task(input);
+                list.add(task);
                 System.out.println("added: " + input + "\n--------------------------------------");
             }
         }
