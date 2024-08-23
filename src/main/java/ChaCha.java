0e12fbe6e945aff0c49fe7c76e492e0a2c39c366
SherniceSng <shernice.syx@gmail.com>
2024-08-23 18:38:31 +0800

    Add ChaChaException.java, handle various exceptions.

--- src/main/java/ChaCha.java
+++ src/main/java/ChaCha.java
@@ -35,52 +35,126 @@ public class ChaCha {
             }
 
             else if (cmd.startsWith("todo")) {
-                String description = cmd.substring(5);
-                System.out.println("     ____________________________________________________________ \n" +
-                        "     Got it. I've added this task:\n" +
-                        "       " + list.addToDo(description).printTask() + "\n" +
-                        "     Now you have " + list.getTotal() + " tasks in the list.\n" +
-                        "     ____________________________________________________________");
+                // potential exception when description is empty
+                try {
+                    if (cmd.length() <= 5) {
+                        throw new ChaChaException("     ____________________________________________________________ \n" +
+                                "     What task are you intending to add as a \'todo\'? \n" +
+                                "     Please type again! \n" +
+                                "     ____________________________________________________________ \n");
+                    } else {
+                        String description = cmd.substring(5);
+
+                        System.out.println("     ____________________________________________________________ \n" +
+                                "     Got it. I've added this task:\n" +
+                                "       " + list.addToDo(description).printTask() + "\n" +
+                                "     Now you have " + list.getTotal() + " tasks in the list.\n" +
+                                "     ____________________________________________________________");
+                    }
+                } catch (ChaChaException e) {
+                    System.out.println(e.toString());
+                }
             }
 
             else if (cmd.startsWith("deadline")) {
-                String temp = cmd.substring(9);
-                String[] arr = temp.split(" /");
-                String description = arr[0];
-                String date = arr[1].substring(3);
-                System.out.println("     ____________________________________________________________ \n" +
-                        "     Got it. I've added this task:\n" +
-                        "       " + list.addDeadline(description, date).printTask() + "\n" +
-                        "     Now you have " + list.getTotal() + " tasks in the list.\n" +
-                        "     ____________________________________________________________");
+                try {
+                    if (cmd.length() <= 9) {
+                        throw new ChaChaException("     ____________________________________________________________ \n" +
+                                "     You are missing some info needed (task description, deadline). \n" +
+                                "     Please type again! \n" +
+                                "     ____________________________________________________________");
+                    }
+                    String temp = cmd.substring(9);
+                    String[] arr = temp.split(" /");
+                    if (arr.length < 2) {
+                        // potential exception when arr does not have all elements needed
+                        throw new ChaChaException("     ____________________________________________________________ \n" +
+                                "     You are missing either the task description or deadline. \n" +
+                                "     Please type again! \n" +
+                                "     ____________________________________________________________");
+                    }
+                    String description = arr[0];
+
+                    if (!arr[1].startsWith("by")) {
+                        // potential exception when deadline is not inputted well
+                        throw new ChaChaException("     ____________________________________________________________ \n" +
+                                "     Please type deadline in the form of \'by ...\'. \n" +
+                                "     ____________________________________________________________");
+                    }
+                    String date = arr[1].substring(3);
+
+                    System.out.println("     ____________________________________________________________ \n" +
+                            "     Got it. I've added this task:\n" +
+                            "       " + list.addDeadline(description, date).printTask() + "\n" +
+                            "     Now you have " + list.getTotal() + " tasks in the list.\n" +
+                            "     ____________________________________________________________");
+
+                } catch (ChaChaException e) {
+                    System.out.println(e.toString());
+                }
             }
 
             else if (cmd.startsWith("event")) {
-                String temp = cmd.substring(6);
-                String[] arr = temp.split(" /");
-                String description = arr[0];
-                String startTime = arr[1].substring(5);
-                String endTime = arr[2].substring(3);
-                System.out.println("     ____________________________________________________________ \n" +
-                        "     Got it. I've added this task:\n" +
-                        "       " + list.addEvent(description, startTime, endTime).printTask() + "\n" +
-                        "     Now you have " + list.getTotal() + " tasks in the list.\n" +
-                        "     ____________________________________________________________");
+                try {
+                    if (cmd.length() <= 6) {
+                        // potential exception when cmd did not include any other info
+                        throw new ChaChaException("     ____________________________________________________________ \n" +
+                                "     You are missing some info needed (task description, start time, end time). \n" +
+                                "     Please type again! \n" +
+                                "     ____________________________________________________________");
+                    }
+                    String temp = cmd.substring(6);
+
+                    String[] arr = temp.split(" /");
+                    if (arr.length < 3) {
+                        // potential exception when arr does not have all elements needed
+                        throw new ChaChaException("     ____________________________________________________________ \n" +
+                                "     You are missing either the task description or start time or end time. \n" +
+                                "     Please type again! \n" +
+                                "     ____________________________________________________________");
+                    } else {
+                        String description = arr[0];
+                        if (!arr[1].startsWith("from")) {
+                            // potential exception when start time is not inputted well
+                            throw new ChaChaException("     ____________________________________________________________ \n" +
+                                    "     Please type start time in the form of \'from ...\'. \n" +
+                                    "     ____________________________________________________________");
+                        }
+
+                        if (!arr[2].startsWith("to")) {
+                            // potential exception when end time is not inputted well
+                            throw new ChaChaException("     ____________________________________________________________ \n" +
+                                    "     Please type end time in the form of \'from ...\'. \n" +
+                                    "     ____________________________________________________________");
+                        }
+
+                        String startTime = arr[1].substring(5);
+                        String endTime = arr[2].substring(3);
+                        System.out.println("     ____________________________________________________________ \n" +
+                                "     Got it. I've added this task:\n" +
+                                "       " + list.addEvent(description, startTime, endTime).printTask() + "\n" +
+                                "     Now you have " + list.getTotal() + " tasks in the list.\n" +
+                                "     ____________________________________________________________");
+                    }
+
+                } catch (ChaChaException e) {
+                    System.out.println(e.toString());
+                }
             }
 
             else if (cmd.startsWith("mark ")) {
-                int index = Integer.parseInt(cmd.substring(5));
+                int index = Integer.parseInt(cmd.substring(5)); // potential exception when index is not int
                 System.out.println(list.markDone(index));
             }
 
             else if (cmd.startsWith("unmark ")) {
-                int index = Integer.parseInt(cmd.substring(7));
+                int index = Integer.parseInt(cmd.substring(7)); // potential exception when index is not int
                 System.out.println(list.markUndone(index));
             }
 
             else {
                 System.out.println("     ____________________________________________________________ \n" +
-                        "Hmmm... I don't seem to understand this. Please input another command! \n" +
+                        "     Hmmm... I don't seem to understand this. Please input another command! \n" +
                         "     ____________________________________________________________");
             }
 
--- src/main/java/ChaChaException.java
+++ src/main/java/ChaChaException.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ChaChaException {
+public class ChaChaException extends Exception {
+    private String msg;
+    public ChaChaException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return this.msg;
+    }
 }
