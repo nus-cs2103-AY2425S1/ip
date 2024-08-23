e296f234b3772c1e6c47c15a00b83bc616a0deb3
C5hives <e1122945@u.nus.edu>
2024-08-23 14:00:49 +0800

    Lawrence.java: Add ability to store & display tasks

--- src/main/java/Lawrence.java
+++ src/main/java/Lawrence.java
@@ -1,36 +1,56 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lawrence {
     private static final String NAME = "Lawrence";
+    private static final ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         greetUser();
         Scanner sc = new Scanner(System.in);
 
-        String command;
+        String userInput;
         while (true) {
-            command = sc.nextLine();  // Get next command
-            if (command.equals("bye")) {
-                break;
+            userInput = sc.nextLine();  // Get next user input
+            switch (userInput) {
+                case "bye":
+                    printMessage("That's all folks! Hope to see you again soon!");
+                    return;
+                case "list":
+                    printList();
+                    break;
+                default:
+                    list.add(userInput);
+                    printMessage(String.format("Added '%s' to the list.", userInput));
             }
-            printCommand(command);
         }
-        exit();
-    }
+    }   
 
     private static void greetUser() {
         String greeting = String.format("Hello! I'm %s and I'm here to steal your credit card information.\n"
                 + "What can I do for you?", NAME);
-        System.out.println(greeting);
+        printMessage(greeting);
     }
 
-    private static void printCommand(String command) {
-        String line = "___________________________________";
-        System.out.println(line);
-        System.out.println(command);
-        System.out.println(line);
+    private static void printList() {
+        StringBuilder result = new StringBuilder();
+        int serialNumber = 1;
+        for (String item : list) {
+            result.append(String.format("%d. %s%n", serialNumber, item));
+            serialNumber++;
+        }
+
+        // exclude the last newline character from getting printed
+        printMessage(result.substring(0, result.length() - 1));
     }
 
-    private static void exit() {
-        System.out.println("That's all folks! Hope to see you again soon!");
+    private static void printMessage(String message) {
+        printHorizontalLine();
+        System.out.println(message);
+        printHorizontalLine();
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("____________________");
     }
 }
