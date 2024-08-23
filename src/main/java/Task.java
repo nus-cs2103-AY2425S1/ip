e95e14db93d75cafd17bdec84ca95f90514515a4
t-leongchuan <t.leongchuan@gmail.com>
2024-08-23 10:54:42 +0800

    Add JavaDoc comments for methods in Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,11 +2,21 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Returns a Task object.
+     *
+     * @param description Description of the Task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns Symbolic Representation of whether the Task is done or not.
+     *
+     * @return symbolic representation of task state
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
