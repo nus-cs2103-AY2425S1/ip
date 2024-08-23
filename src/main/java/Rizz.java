d07856a98468764fb5a91af7fd8291cfb330f37d
saajidshaik02 <saajidshaik02@gmail.com>
2024-08-23 13:32:45 +0800

    Rizz Level-1: Echo

--- src/main/java/Rizz.java
+++ src/main/java/Rizz.java
@@ -1,17 +1,48 @@
-
-
+import java.util.Scanner;
 public class Rizz {
+    Scanner input = new Scanner(System.in);
+
+    private String indent(String str) {
+        String[] lines = str.split("\n");
+        StringBuilder indentedText = new StringBuilder();
+        for (String line : lines) {
+            indentedText.append("\t").append(line).append("\n");
+        }
+        return indentedText.toString();
+    }
+
     private void greet() {
-        String logo = "____________________________________________________________\n" +
+        String str = "____________________________________________________________\n" +
                 "Hello! I'm Rizz\n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        System.out.println(this.indent(str));
+    }
+
+    private void echo(String textInput) {
+        System.out.println("\t" + textInput + "\n");
+    }
+
+    private void exit() {
+        String str =  "____________________________________________________________\n" +
                 "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.println(logo);
+        System.out.println(this.indent(str));
     }
+
+    private void caseCheck() {
+        String textInput = this.input.nextLine();
+        if (textInput.equals("bye")) {
+            this.exit();
+        } else {
+            this.echo(textInput);
+            this.caseCheck();
+        }
+    }
+
     public static void main(String[] args) {
         Rizz rizz = new Rizz();
         rizz.greet();
+        rizz.caseCheck();
     }
 }
