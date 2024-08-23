e6e6c867ba5efb163761a2de76ff9b00346b16af
shotnothing <shotnothing@protonmail.ch>
2024-08-23 13:28:46 +0800

    feat: Added basic dialogue

---
+++ src/main/java/Bob.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Bob {
+
+    private static void dialogue(String input) {
+        System.out.println("_______________________________\n");
+        System.out.println(input);
+        System.out.println("_______________________________\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        dialogue("Hello! I'm Bob\n"
+                + "What can I do for you?");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                dialogue("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                dialogue(userInput);
+            }
+        }
+
+        scanner.close();
+    }
+}
