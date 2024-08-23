2de90727d13751c4f2dc6f7237eb4c9271d9a182
Nimastic <jerielchanzy@gmail.com>
2024-08-23 08:07:56 +0800

    Level-2

--- src/main/java/Jeriel.java
+++ src/main/java/Jeriel.java
@@ -20,6 +20,9 @@ public class Jeriel {
         Scanner scanner = new Scanner(System.in);
         String input;
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
 
         while (true) {
             input = scanner.nextLine(); 
@@ -33,6 +36,18 @@ public class Jeriel {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
         }
 
