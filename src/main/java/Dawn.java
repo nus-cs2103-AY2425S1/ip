6bb141f9bed74566c2f6c21e77a55ab2ec60b228
annabellekk <yuyien.khoo@gmail.com>
2024-08-23 10:53:45 +0800

    Renamed main file and added new greetings

---
+++ src/main/java/Dawn.java
@@ -0,0 +1,11 @@
+public class Dawn {
+    public static void main(String[] args) {
+        String logo = "Dawn 🌙";
+        String line = "--".repeat(30);
+        System.out.println(line);
+        System.out.printf("Hello! I'm %s, what can I do for you? \n", logo);
+        System.out.println(line);
+        System.out.println("Nice chatting with you! See you next time, tschüss :)");
+        System.out.println(line);
+    }
+}
