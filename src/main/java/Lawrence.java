73fe471fbb4e8f3dfff5ea94ddb63e3f5ca78afb
C5hives <e1122945@u.nus.edu>
2024-08-23 12:14:06 +0800

    Lawrence.java: Implement skeletal chatbot version

--- src/main/java/Lawrence.java
+++ src/main/java/Lawrence.java
@@ -1,12 +1,17 @@
-public class Duke {
+public class Lawrence {
+    private static final String NAME = "Lawrence";
     public static void main(String[] args) {
-        String logo = ".____\n" +
-                "|    |   _____ __  _  _________   ____   ____   ____  ____\n" +
-                "|    |   \\__  \\ \\/ \\/ /\\_  __ \\_/ __ \\ /    \\_/ ___\\/ __ \\\n" +
-                "|    |___ / __ \\\\     /  |  | \\/\\  ___/|   |  \\  \\__\\  ___/\n" +
-                "|_______ (____  /\\/\\_/   |__|    \\___  >___|  /\\___  >___  >\n" +
-                "\\/    \\/                     \\/     \\/     \\/    \\/\n";
+        greetUser();
+        exit();
+    }
 
-        System.out.println("Hello from\n" + logo);
+    private static void greetUser() {
+        String greeting = String.format("Hello! I'm %s and I'm here to steal your credit card information.\n"
+                + "What can I do for you?", NAME);
+        System.out.println(greeting);
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
