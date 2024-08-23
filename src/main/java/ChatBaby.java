6de1839579e96ea4f9f70d582ad674261b4e124c
Jian Yi <e1075543@u.nus.edu>
2024-08-23 11:10:36 +0800

    level 0 implemented

---
+++ src/main/java/ChatBaby.java
@@ -0,0 +1,18 @@
+public class ChatBaby {
+    public static void main(String[] args) {
+        greet();
+        bye();
+    }
+
+    public static void greet() {
+        System.out.println("____________________________________________________________\n"
+                + "Hello! I'm ChatBaby\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________");
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________");
+    }
+}
