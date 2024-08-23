8dbc4b2d06c267ebc9bf349791792f075c616672
slidings <ngzerui@gmail.com>
2024-08-23 11:06:33 +0800

    Echoing chatbot

--- src/main/java/Pebble.java
+++ src/main/java/Pebble.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
 public class Pebble {
     public static void main(String[] args) {
-        String text =
-                "____________________________________________________________\n" +
-                        "Hello! I'm Pebble \n" + "What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________";
+        // scans keyboard
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(text);
+        //introduction text
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        //forever loop
+        while (true) {
+            String input = scanner.nextLine();
+            // break loop if says bye
+            if (input.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + input);
+            System.out.println("    ____________________________________________________________");
+        }
+
+        // terminates
+        scanner.close();
     }
 }
