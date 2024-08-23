2a93919637c8bdf940a5d685d7b2e6de9526d900
Ansel-Ch <achong2014@gmail.com>
2024-08-23 20:37:31 +0800

    Update Event Constructor

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,22 +3,17 @@ public class Event extends Task {
     private String end;
 
     /**
-     * Constructor for Event.
-     * @param params
-     * @throws IllegalArgumentException
+     * Constructor for Event class.
+     * @param description
+     * @param start
+     * @param end
      */
-    public Event(String... params) throws IllegalArgumentException {
-        super(params[0].strip());
-        if (params.length < 2) {
-            throw new IllegalArgumentException("Event: Did not provide start and end date/time");
-        } else if (params.length < 3) {
-            throw new IllegalArgumentException("Event: Did not provide end date/time");
-        }
-
-        StringBuilder str = new StringBuilder(params[1].strip());
+    public Event(String description, String start, String end) {
+        super(description.strip());
+        StringBuilder str = new StringBuilder(start.strip());
         str.insert(str.indexOf(" "), ':');
         this.start = str.toString();
-        str = new StringBuilder(params[2].strip());
+        str = new StringBuilder(end.strip());
         str.insert(str.lastIndexOf(" "), ":");
         this.end = str.toString();
     }
