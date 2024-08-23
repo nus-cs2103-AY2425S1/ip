fd76663d994d83f45089a84dd07569831bd4b75f
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-08-24 00:33:49 +0800

    Updated style for test files

--- src/test/java/StorageTest.java
+++ src/test/java/StorageTest.java
@@ -1,16 +1,18 @@
-import hoodini.Storage;
-import hoodini.Ui;
-import hoodini.ToDo;
-import hoodini.Deadline;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hoodini.Deadline;
+import hoodini.Storage;
+import hoodini.ToDo;
+import hoodini.Ui;
+
+
 
 public class StorageTest {
     private final PrintStream standardOut = System.out;
@@ -27,7 +29,8 @@ public class StorageTest {
         storage.add(new ToDo("todo read book"));
         storage.add(new Deadline("deadline return book /by 2020-12-12"));
         storage.output();
-        assertEquals("Here are the list of tasks that needs to be completed: \n1. [T] [ ] read book\n2. [D] [ ] return book (by: Dec 12 2020)\n", outputStreamCaptor.toString());
+        assertEquals("Here are the list of tasks that needs to be completed: \n"
+                + "1. [T] [ ] read book\n2. [D] [ ] return book (by: Dec 12 2020)\n", outputStreamCaptor.toString());
     }
 
 }
--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -1,12 +1,13 @@
-import hoodini.Ui;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hoodini.Ui;
+
 public class UiTest {
 
     private final PrintStream standardOut = System.out;
