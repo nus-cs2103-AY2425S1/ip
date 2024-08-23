bc480c8e48d43ef7fee90f68d17446dae67c98d0
zienxu <zienxu.ang@gmail.com>
2024-08-23 15:09:33 +0800

    added level 2 requirements to Sammy.java

--- src/main/java/Sammy.java
+++ src/main/java/Sammy.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sammy {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println(line);
         System.out.println(" Hello! I'm Sammy.");
@@ -12,12 +14,21 @@ public class Sammy {
 
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println(line);
+                System.out.println(" added: " + input);
             }
-            System.out.println(line);
-            System.out.println(" " + input);
+
             System.out.println(line);
         }
 
