b028c0b436efe5beaf3697549380ed7cfb20a976
Jollybomber <siahevan@gmail.com>
2024-08-23 15:41:01 +0800

    Feat: Nave now echoes user input and exits when bye is typed

--- src/main/java/Nave.java
+++ src/main/java/Nave.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Nave {
     public static void main(String[] args) {
         String greeting = """
@@ -5,9 +7,22 @@ public class Nave {
                 Hello! :) I'm Nave, your personal task management assistant.
                 What can I do for you today?
                 -----------------------------------------------------------------
+                """;
+        String farewell = """
+                -----------------------------------------------------------------
                 Goodbye :( Come visit me again!
                 -----------------------------------------------------------------
                 """;
         System.out.println(greeting);
+        Scanner inputReader = new Scanner(System.in);
+        String userInput = inputReader.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(
+                    "-----------------------------------------------------------------\n"
+                    + userInput +
+                    "\n-----------------------------------------------------------------");
+            userInput = inputReader.nextLine();
+        }
+        System.out.println(farewell);
     }
 }
