62a310d706ef2ab01159a4dfb9d7d314eacbcc9a
estellelim <estlim@hotmail.com>
2024-08-23 20:32:55 +0800

    Add actions

--- src/main/java/Muffin.java
+++ src/main/java/Muffin.java
@@ -1,9 +1,13 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Muffin {
+    enum Command {
+        BYE, LIST, ADD
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
         String logo = " __  __       __  __ _\n" +
                 "|  \\/  |_  _ / _|/ _(_)_ _  \n" +
                 "| |\\/| | || |  _|  _| | ' \\ \n" +
@@ -13,17 +17,36 @@ public class Muffin {
                 "What can I do for you?";
 
         System.out.println(logo + "\n" + helloMsg);
-        command(sc);
+        command(sc, 0, list);
     }
 
-    public static void command(Scanner sc) {
+    public static void command(Scanner sc, int count, String[] list) {
         String userInput = sc.nextLine();
 
-        if (userInput.equalsIgnoreCase("bye")) {
-            System.out.println("Goodbye~ Hope to see you again soon!");
-        } else {
-            System.out.println(userInput);
-            command(sc);
+        Command command;
+        try {
+            command = Command.valueOf(userInput.trim().toUpperCase());
+        } catch (IllegalArgumentException e) {
+            command = Command.ADD;
+        }
+
+        switch(command) {
+            case BYE:
+                System.out.println("Goodbye~ Hope to see you again soon!");
+                break;
+
+            case LIST:
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i+1) + ". " + list[i]);
+                }
+                command(sc, count, list);
+                break;
+
+            case ADD:
+                list[count] = userInput;
+                System.out.println("added: " + userInput);
+                command(sc, ++count, list);
+                break;
         }
     }
 }
