caaa4e1ce08c30ebf8da5e725ad27118b7dd5283
sam-theman88 <samuel.lee9525@gmail.com>
2024-08-23 13:26:36 +0800

    Chatbot can now receive user input, and added echo feature

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,16 +1,47 @@
+import java.util.Scanner;
+
 public class Edith {
-    private static final String lineBreak = "\n_______________________________________________________________\n";
-    private static final String greeting = "Hello! I am EDITH, your personal chatbot companion:)"
+    private final String lineBreak = "\n_______________________________________________________________________\n";
+    private final String greeting = "Hello! I am EDITH, your personal chatbot companion:)"
             + "\nHow may I assist you?";
-    private static final String exit = "It has been my pleasure helping you. Hope to see you again soon!";
+    private static final String prompt = "Your next instruction: ";
+    private static final String indentation = "    ";
+    private final String exit = "It has been my pleasure helping you. Hope to see you again soon!";
 
     public Edith() {
     }
 
-    public static void main(String[] args) {
-        String string0 = lineBreak + greeting + lineBreak;
-        String lastString = exit + lineBreak;
+    public void greet() {
+        System.out.println(lineBreak + greeting + lineBreak);
+    }
 
-        System.out.println(string0 + lastString);
+    public void exit() {
+        System.out.println(indentation + exit + lineBreak);
+    }
+
+    public void echo(String message) {
+        System.out.println(indentation + message + lineBreak);
+    }
+
+    public static void main(String[] args) {
+        Edith edith = new Edith();
+        edith.greet();
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            System.out.println(prompt);
+            userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                edith.exit();
+                break;
+            }
+
+            edith.echo(userInput);
+        }
+
+        scanner.close();
     }
 }
