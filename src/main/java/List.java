581f8ce060a6e4301171d208bf6d827b57a035b4
travisim <64481469+travisim@users.noreply.github.com>
2024-08-23 14:26:25 +0800

    added add and list

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class List {
+import java.util.ArrayList;
+public class List {
+
+    private ArrayList<String> items = new ArrayList<String>();
+
+    public List() {
+    
+    }
+
+    public void appendItem(String item) {
+        items.add(item);
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+        
+    }
+
+
 }
--- src/main/java/Sinatra.java
+++ src/main/java/Sinatra.java
@@ -1,10 +1,57 @@
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Sinatra {
-    public static void main(String[] args) {
+    
 
+    private List list;
+
+    public Sinatra() {
+        this.list = new List();
+        this.printIntro();
+     
+        this.sinatraScanner();
+    }
+    private void printIntro() {
         String initial = "Hello! I'm Sinatra \nWhat can I do for you? ";
+       
+        //print intro
+        System.out.println(initial);
+    }
 
-        String end = "Bye. Hope to see you again soon!";
+    public static void main(String[] args) {
+        Sinatra sinatra = new Sinatra();
 
-        System.out.println(initial+"\n" + end);
+      
+
+    }
+
+    private void sinatraScanner() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String message = scanner.nextLine();
+            handleInputs(message);
+        }
+      
+    }
+
+    private void handleInputs(String message) {
+        if (message.equals("list")) {
+            ArrayList<String> contents = list.getItems();
+            for (int i = 0; i < list.getItems().size(); i++) {
+                int count = i + 1;
+                System.out.println(count + ". " + contents.get(i));
+
+            }
+        } else if (message.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            System.exit(0);
+        
+        }
+        else {
+            list.appendItem(message);
+            System.out.println("added: " + message);
+        }
     }
 }
