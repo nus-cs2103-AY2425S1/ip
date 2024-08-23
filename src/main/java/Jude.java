a48cabbdebf119dc72b1d614e4dcde0c4c075887
Judy1x4 <e1129873@u.nus.edu>
2024-08-23 08:44:22 +0800

    Improve the skeletal version
    it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class Jude {
     public static void main(String[] args) {
         String name = "Jude";
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("What can I do for you, poyo?");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon, poyo!");
     }
 }
