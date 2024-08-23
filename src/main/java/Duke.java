6c9be8d77fa5505ef6e2dc999640645e8947b9f7
SleepyDay <107453007+flyingsalsa@users.noreply.github.com>
2024-08-23 08:28:52 +0800

    Add mark and unmark to Parser for Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 public class Duke {
 
     public static void lnDiv() {
-        System.out.println("_______________________________________________");
+        System.out.println("_________________________________________________________");
     }
     public static void main(String[] args) {
 
@@ -12,8 +12,9 @@ public class Duke {
                 + "| |\\  | (_) | |_  | (_| | |_) | |_ \n"
                 + "|_| \\_|\\___/ \\__|  \\__, | .__/ \\__|\n"
                 + "                   |___/|_|        \n";
-        System.out.println(logo+ "hi, I'm Not-gpt,");
-        System.out.println("do you really need me to do sth for you?");
+        System.out.println(logo+ "hi, I'm Not-gpt,\ndo you really need me to do sth for you?");
+        lnDiv();
+        System.out.println("*the first word will always be read as the command*");
         lnDiv();
         Parser.parse();
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,21 @@
 public class Task {
     private String task;
+    private boolean done;
     public Task(String s){
         task = s;
+        done = false;
+    }
+    public void complete(){
+        done = true;
+    }
+    public void uncomplete(){
+        done = false;
     }
     public String toString() {
-        return task;
+        String status;
+        if(done) status = "[X] ";
+        else status = "[ ] ";
+        return status + task;
     }
     public boolean isReal(){
         return task != "";
