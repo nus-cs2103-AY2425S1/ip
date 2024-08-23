543eba776eb53c0a0e0bc0effebef8b8d795122c
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-08-23 15:12:05 +0800

    rename bot + add greeting

---
+++ src/main/java/Gallium.java
@@ -0,0 +1,8 @@
+public class Gallium {
+    public static void main(String[] args) {
+        String helloMessage = "Hello! I am Gallium. \n What can I do for you?";
+        String byeMessage = "Bye. Hope to see you again soon!";
+        String lines = "____________________________________________________________";
+        System.out.println(lines + "\n" + helloMessage + "\n" + lines + "\n" + byeMessage + "\n" + lines + "\n");
+    }
+}
