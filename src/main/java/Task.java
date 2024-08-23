83bec1a7eaaa703bb768106a2b6a2688dc3ad3c5
C5hives <e1122945@u.nus.edu>
2024-08-23 14:34:35 +0800

    Task.java: Add new class that allows task tracking

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private final String description;
+    private boolean complete;
+
+    public Task(String description) {
+        this.description = description;
+        complete = false;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", complete ? "X" : " ", description);
+    }
 }
