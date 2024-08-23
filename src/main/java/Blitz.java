17370c61f85fb153ee1e78142ebabe3a07c81e17
Curtis <curtischang2510@gmail.com>
2024-08-23 09:50:24 +0800

    Add: Blitz.java
    Renamed Chatbot from Duke to Blitz.

---
+++ src/main/java/Blitz.java
@@ -0,0 +1,9 @@
+public class Blitz{
+    public static void main(String[] args) {
+        System.out.println("---------------\n" +
+                "Hello! I'm BLITZ \n" +
+                "What can I do for you?");
+        System.out.println("----------------\n" +
+                "Bye. Hope to see you again soon!");
+    }
+}
