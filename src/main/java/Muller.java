093e0bfeb2e26f7cfe3cdbfb292e82a0215b2312
muller317 <hhr317@gmail.com>
2024-08-23 20:15:46 +0800

    Level 4

--- src/main/java/Muller.java
+++ src/main/java/Muller.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,14 +18,41 @@ public class Muller {
                 int index = Integer.parseInt(inputs[1]) - 1;
                 list.get(index).markAsDone();
                 System.out.println(logo + "\nNice! I've marked this task as done:\n");
-                System.out.println("  " + list.get(index) + "\nlogo");
+                System.out.println("  " + list.get(index) + "\n" + logo);
             } else if (inputs[0].equals("unmark")) {
                 int index = Integer.parseInt(inputs[1]) - 1;
                 list.get(index).markAsNotDone();
                 System.out.println(logo + "\nOK, I've marked this task as not done yet:\n");
-                System.out.println("  " + list.get(index) + "\nlogo");
+                System.out.println("  " + list.get(index) + "\n" + logo);
+            } else if (inputs[0].equals("todo")) {
+                System.out.println(logo + "\nGot it. I've added this task:\n");
+                Task t = new Task(inputs[1]);
+                t.setType("T");
+                list.add(t);
+                System.out.println("  " + t);
+                System.out.println("\nNow you have " + list.size() + " tasks in the list.\n" + logo);
+            } else if (inputs[0].equals("deadline")) {
+                System.out.println(logo + "\nGot it. I've added this task:\n");
+                String[] detail = inputs[1].split("/by", 2);
+                Task t = new Task(detail[0]);
+                t.setType("D");
+                t.setDate("(by:" + detail[1] + ")");
+                list.add(t);
+                System.out.println("  " + t);
+                System.out.println("\nNow you have " + list.size() + " tasks in the list.\n" + logo);
+            } else if (inputs[0].equals("event")) {
+                System.out.println(logo + "\nGot it. I've added this task:\n");
+                String[] detail = inputs[1].split("/from");
+                String[] ending = detail[1].split("/to");
+                Task t = new Task(detail[0]);
+                t.setType("E");
+                t.setDate("(from:" + ending[0] + "to:" + ending[1] + ")");
+                list.add(t);
+                System.out.println("  " + t);
+                System.out.println("\nNow you have " + list.size() + " tasks in the list.\n" + logo);
             } else if (in.equals("list")) {
                 System.out.println(logo);
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 1; i <= list.size(); i++){
                     System.out.println(i + ": " + list.get(i-1));
                 }
@@ -39,13 +67,23 @@ public class Muller {
         System.out.println(logo + "\nBye. Hope to see you again soon!\n" + logo);
     }
 
-    static class Task {
+    public static class Task {
         private String name;
         private boolean isDone;
+        private String type = "[ ]";
+        private String date = "";
 
         public Task(String name) {
             this.name = name;
             this.isDone = false;
+        }
+
+        public void setType(String type) {
+            this.type = "[" + type + "]";
+        }
+
+        public void setDate(String date) {
+            this.date = date;
         }
 
         public void markAsDone() {
@@ -62,7 +100,7 @@ public class Muller {
 
         @Override
         public String toString() {
-            return DoneIcon() + " " +name;
+            return this.type + DoneIcon() + " " + name + this.date;
         }
     }
 }
