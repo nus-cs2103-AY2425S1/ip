329757e75ae9c5171a24553bf4d104023b1f0dc2
yuancheng <>
2024-08-23 20:15:06 +0800

    update printing such that uppercases are converted to lowercases in sout and edit input.txt, EXPECTED.TXT to do automated text UI testing

--- src/main/java/Winner.java
+++ src/main/java/Winner.java
@@ -16,7 +16,7 @@ public class Winner {
             String input = scanner.nextLine();
 
             if (input.matches("(?i)todo.*")) {
-                String description = input.split("todo", 2)[1].trim();
+                String description = input.split("todo", 2)[1].trim().toLowerCase();
                 ToDo newToDo = new ToDo(description);
                 tasks.add(newToDo);
                 System.out.println(("-".repeat(100) + "\n" +
@@ -25,8 +25,8 @@ public class Winner {
 
             } else if (input.matches("(?i)deadline.*")) {
                 String[] parts = input.split("(?i)deadline | (?i)by");
-                String description = parts[1].trim();
-                String by = parts[2].trim();
+                String description = parts[1].trim().toLowerCase();
+                String by = parts[2].trim().toLowerCase();
                 Deadline newDeadline = new Deadline(description, by);
                 tasks.add(newDeadline);
                 System.out.println(("-".repeat(100) + "\n" +
@@ -35,9 +35,9 @@ public class Winner {
 
             } else if (input.matches("(?i)event.*")) {
                 String[] parts = input.split("(?i)event | (?i)from | (?i)to");
-                String description = parts[1].trim();
-                String start = parts[2].trim();
-                String end = parts[3].trim();
+                String description = parts[1].trim().toLowerCase();
+                String start = parts[2].trim().toLowerCase();
+                String end = parts[3].trim().toLowerCase();
                 Event newEvent = new Event(description, start, end);
                 tasks.add(newEvent);
                 System.out.println(("-".repeat(100) + "\n" +
@@ -46,29 +46,29 @@ public class Winner {
 
             } else if (input.matches("(?i)list")) {
                 int counter = 1;
-                System.out.println(" ".repeat(10) + "-".repeat(38));
+                System.out.println(" ".repeat(10) + "-".repeat(100));
                 System.out.println(" ".repeat(10) + "Here are the tasks you have in your list:");
                 for (Task i:tasks) {
                     System.out.println(" ".repeat(10) + counter + ". " + i.taskToString());
                     counter++;
                 }
-                System.out.println(" ".repeat(10) + "-".repeat(38));
+                System.out.println(" ".repeat(10) + "-".repeat(100) + "\n");
 
             } else if (input.matches("(?i)mark \\d+")) {
                 int taskNumber = Integer.parseInt(input.replaceAll("[^0-9]", ""));
                 Task task = tasks.get(taskNumber - 1);
                 task.markDone();
-                System.out.println(("-".repeat(38) + "\n" +
+                System.out.println(("-".repeat(100) + "\n" +
                         task.markDoneToString() + "\n" +
-                        "-".repeat(38)).indent(10));
+                        "-".repeat(100)).indent(10));
 
             } else if (input.matches("(?i)unmark \\d+")) {
                 int taskNumber = Integer.parseInt(input.replaceAll("[^0-9]", ""));
                 Task task = tasks.get(taskNumber - 1);
                 task.unmarkDone();
-                System.out.println(("-".repeat(38) + "\n" +
+                System.out.println(("-".repeat(100) + "\n" +
                         task.unmarkDoneToString() + "\n" +
-                        "-".repeat(38)).indent(10));
+                        "-".repeat(100)).indent(10));
 
             } else if (input.matches("(?i)bye")) {
                 System.out.println(("-".repeat(100) + "\n" +
