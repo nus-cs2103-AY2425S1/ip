3b63d422027485b3f3aededf0f9530589e6545f5
Jing Hong Tee <e1122714@u.nus.edu>
2024-08-23 12:47:14 +0800

    Add support for ANSI

---
+++ src/main/java/AnsiCode.java
@@ -0,0 +1,7 @@
+public class AnsiCode {
+    public static final String RESET = "\u001B[0m";
+    public static final String ITALIC = "\u001B[3m";
+    public static final String RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+}
