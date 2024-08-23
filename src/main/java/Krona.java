9705bbeca0ceff6390d21441092b906869146622
The0nlyJuan <ongjuanwen97@gmail.com>
2024-08-23 11:37:22 +0800

    Rename Duke to Krona and added greetings and goodbye

---
+++ src/main/java/Krona.java
@@ -0,0 +1,9 @@
+public class Krona {
+    public static void main(String[] args) {
+        System.out.println("__________________________\n" +
+                "Hello! I'm Krona\n" +
+                "What can i Do for you?\n" +
+                "__________________________\n" +
+                "Bye. Hope to see you again soon!");
+    }
+}
