6e74ca556ab69167b17e413b2b3ad2d17ff60c23
C5hives <e1122945@u.nus.edu>
2024-08-23 19:57:37 +0800

    Add error messages for user
    Most user misinputs will be recognised by the program
    instead of crashing unexpectedly.
    
    The test has also been updated to include some faulty
    inputs.

--- src/main/java/Lawrence.java
+++ src/main/java/Lawrence.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lawrence {
@@ -16,47 +15,113 @@ public class Lawrence {
             String command = inputComponents[0];
             switch (command) {
                 case "bye":
-                    displayMessage("That's all folks! Hope to see you again soon!");
-                    return;
+                    exitSession();
+                    break;
                 case "list":
                     displayTasks();
                     break;
                 case "mark":
-                    Task completeTask = tasks.completeTask(Integer.parseInt(inputComponents[1]));
-                    displayMessage(
-                            String.format("I've marked the task as complete:\n%s", completeTask));
+                    if (inputComponents.length < 2) {
+                        displayMessage("Please specify the task you want to mark as complete.");
+                        break;
+                    }
+                    markTaskAsComplete(inputComponents[1]);
                     break;
                 case "unmark":
-                    Task incompleteTask = tasks.uncompleteTask(Integer.parseInt(inputComponents[1]));
-                    displayMessage(
-                            String.format("Changed your mind? The task is set to incomplete:\n%s", incompleteTask));
+                    if (inputComponents.length < 2) {
+                        displayMessage("Please specify the task you want to mark as incomplete.");
+                        break;
+                    }
+                    markTaskAsIncomplete(inputComponents[1]);
                     break;
                 case "todo":
-                    String todoDescription = inputComponents[1];
-                    addTask(new ToDo(todoDescription));
+                    if (inputComponents.length < 2) {
+                        displayMessage("Todo description cannot be empty! Please try again.");
+                        break;
+                    }
+                    addTodo(inputComponents[1]);
                     break;
                 case "deadline":
-                    String[] deadlineComponents = inputComponents[1].split(" /by ");
-                    addTask(new Deadline(deadlineComponents[0], deadlineComponents[1]));
+                    if (inputComponents.length < 2) {
+                        displayMessage("Deadline information not found! Please try again.");
+                        break;
+                    }
+                    addDeadline(inputComponents[1]);
                     break;
                 case "event":
-                    String[] eventComponents = inputComponents[1].split(" /from | /to ");
-                    addTask(new Event(eventComponents[0], eventComponents[1], eventComponents[2]));
+                    if (inputComponents.length < 2) {
+                        displayMessage("Event information not found! Please try again.");
+                        break;
+                    }
+                    addEvent(inputComponents[1]);
                     break;
                 default:
-                    displayMessage("Unable to recognise command. Please try again.");
+                    displayMessage(String.format("Unable to recognise command '%s'. Please try again.", command));
             }
         }
     }
 
+    private static void addTodo(String description) {
+        addTask(new ToDo(description));
+    }
+
+    private static void addDeadline(String parameters) {
+        String[] deadlineComponents = parameters.split(" /by ");
+        if (deadlineComponents.length < 2) {
+            displayMessage("Not enough information to create deadline.");
+            return;
+        }
+        addTask(new Deadline(deadlineComponents[0], deadlineComponents[1]));
+    }
+
+    private static void addEvent(String parameters) {
+        String[] eventComponents = parameters.split(" /from | /to ");
+        if (eventComponents.length < 3) {
+            displayMessage("Not enough information to create event.");
+            return;
+        }
+        addTask(new Event(eventComponents[0], eventComponents[1], eventComponents[2]));
+    }
+
     private static void addTask(Task t) {
         tasks.add(t);
+        int numberOfTasks = tasks.getSize();
+        String verb = numberOfTasks == 1 ? "is" : "are";
+        String plural = numberOfTasks == 1 ? "" : "s";
         displayMessage(String.format("Alright, added task:%n%s to the list.%n"
-                + "There are currently %d tasks in the list", t, tasks.getSize()));
+                + "There %s currently %d task%s in the list.", t, verb, numberOfTasks, plural));
+    }
+
+    private static void markTaskAsComplete(String parameters) {
+        try {
+            int taskNumber = Integer.parseInt(parameters);
+            Task completeTask = tasks.completeTask(taskNumber);
+            displayMessage(
+                    String.format("I've marked the task as complete:\n%s", completeTask));
+        } catch (NumberFormatException e) {
+            displayMessage("Please input a number to select a task.");
+        } catch (IllegalArgumentException e) {
+            displayMessage(
+                    String.format("%s Please try again.", e.getMessage()));
+        }
+    }
+
+    private static void markTaskAsIncomplete(String parameters) {
+        try {
+            int taskNumber = Integer.parseInt(parameters);
+            Task incompleteTask = tasks.uncompleteTask(taskNumber);
+            displayMessage(
+                    String.format("Changed your mind? The task is set to incomplete:\n%s", incompleteTask));
+        } catch (NumberFormatException e) {
+            displayMessage("Please input a number to select a task.");
+        } catch (IllegalArgumentException e) {
+            displayMessage(
+                    String.format("%s Please try again.", e.getMessage()));
+        }
     }
 
     private static void greetUser() {
-        String greeting = String.format("Hello! I'm %s and I'm here to steal your credit card information.\n"
+        String greeting = String.format("Hello! I'm %s and I'm here to establish another GST hike.\n"
                 + "What can I do for you?", NAME);
         displayMessage(greeting);
     }
@@ -67,6 +132,11 @@ public class Lawrence {
             return;
         }
         displayMessage(String.format("Here's your laundry list:%n%s", tasks.toString()));
+    }
+
+    private static void exitSession() {
+        displayMessage("That's all folks! Hope to see you again soon!");
+        System.exit(0);
     }
 
     private static void displayMessage(String message) {
--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -12,12 +12,20 @@ public class Tasks {
     }
 
     public Task completeTask(int taskNumber) {
+        if (taskNumber < 1 || taskNumber > list.size()) {
+            throw new IllegalArgumentException(
+                    String.format("Task does not exist. Number must be within the range 1 to %s.", list.size()));
+        }
         Task t = list.get(taskNumber - 1);
         t.setComplete(true);
         return t;
     }
 
     public Task uncompleteTask(int taskNumber) {
+        if (taskNumber < 1 || taskNumber > list.size()) {
+            throw new IllegalArgumentException(
+                    String.format("Task does not exist. Number must be within the range 1 to %s.", list.size()));
+        }
         Task t = list.get(taskNumber - 1);
         t.setComplete(false);
         return t;
