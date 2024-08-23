9de295fa0b429e60ad0c41b6deac3aac47c04217
lutfir-cpu <lutfir.rn@gmail.com>
2024-08-23 17:06:43 +0800

    added echo

--- src/main/java/Lutchat.java
+++ src/main/java/Lutchat.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 public class Lutchat {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         greet();
+
+        String userInput = sc.nextLine();
+        while (conversation(userInput)) {
+            userInput = sc.nextLine();
+        }
+
         exit();
     }
 
     static void greet() {
+        System.out.print("______________________________________________\n");
         System.out.print("Hello! I'm Lutchat!\n");
         System.out.print("What can I do for you?\n");
+        System.out.print("______________________________________________\n");
     }
 
     static void exit() {
         System.out.print("Bye! Hope to see you again soon!\n");
+        System.out.print("______________________________________________\n");
+    }
+
+    static boolean conversation(String userInput) {
+        switch (userInput) {
+            case "bye":
+                return false;
+            default:
+                System.out.print(userInput + "\n");
+                System.out.print("______________________________________________\n");
+                return true;
+        }
     }
 }
