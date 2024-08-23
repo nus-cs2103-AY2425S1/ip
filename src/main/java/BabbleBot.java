4923e2067960d83560c82890d8c13c2f7eeb50b1
DanzaSeah <e0959436@u.nus.edu>
2024-08-23 16:41:15 +0800

    Added support for list (Can add items to a list and display them)

--- src/main/java/BabbleBot.java
+++ src/main/java/BabbleBot.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class BabbleBot {
     public static void main(String[] args) {
         Boolean notBye = true;
+        String[] storedString = new String[100];
+        int counter = 0;
         Scanner in = new Scanner(System.in);
         String separator = "---------------------------------------------";
         System.out.println(separator);
@@ -13,9 +15,17 @@ public class BabbleBot {
             String userInp = in.nextLine();
             if (userInp.equals("bye")) {
                 notBye = false;
+            } else if (userInp.equals("list")) {
+                System.out.println(separator);
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(i + 1 + ". " + storedString[i]);
+                }
+                System.out.println(separator);
             } else {
                 System.out.println(separator);
-                System.out.println(userInp);
+                System.out.println("added: " + userInp);
+                storedString[counter] = userInp;
+                counter++;
                 System.out.println(separator);
             }
         }
