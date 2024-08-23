b101ba8ae3ba1f15fe8ad3ab5453aa18c20edee0
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2024-08-23 15:03:15 +0800

    added function for adding horizontal lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
 public class Duke {
+    public String addHorizontalLines(String dialog) {
+        String res = "____________________________________________________________\n" +
+                dialog + "\n" +
+                "____________________________________________________________";
+        return res;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
