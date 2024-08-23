cc1e663e65432454a83259ec59e76ec0f9983135
meerkatboy <sohjunanerwin@gmail.com>
2024-08-23 09:28:18 +0800

    changed file name, changed greeting

---
+++ IP/src/main/java/Meerkat.java
@@ -0,0 +1,17 @@
+public class Meerkat {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String greeting = """
+                ____________________________________________________________
+                Hello! I'm Meerkat
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                """;
+        System.out.println(greeting);
+    }
+}
