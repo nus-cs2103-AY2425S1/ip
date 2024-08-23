ca18f1eee43c015967d211b2c81499972840d131
arjun2598 <arjunkumanan123@gmail.com>
2024-08-23 15:53:47 +0800

    Add user inputs to a list, list them back when user requests

--- src/main/java/Michael.java
+++ src/main/java/Michael.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Michael {
     private static final String border = "--------------------------------------";
-    public static void printer(String text) {
+    public static void printer(String text) { // Function to help print each interaction
         System.out.println(border);
         System.out.println(text);
         System.out.println(border);
     }
+
+    private static ArrayList<String> texts = new ArrayList<>(); // store user inputs
 
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in); // scanner for user input
@@ -17,10 +20,21 @@ public class Michael {
         // Read user's input
         while (true) {
             String input = user.nextLine();
-            if (input.equals("bye")) { // special bye command
+            if (input.equals("bye")) { // special bye command to exit
                 break;
             }
-            printer(input);
+
+            if (input.equals("list")) { // list user inputs thus far
+                String list = "";
+                for (int i = 0; i < texts.size(); i++) {
+                    String elem = String.valueOf(i + 1) + ". " + texts.get(i) + "\n";
+                    list = list.concat(elem);
+                }
+                printer(list.substring(0, list.length() - 1)); // substring to remove last line break
+            } else {
+                texts.add(input);
+                printer("added: " + input);
+            }
         }
 
         // Exit
