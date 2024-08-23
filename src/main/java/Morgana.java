b491253588dffc14cf0fd22a326eca22e4185f6f
Gra7ityIC3 <kwtg20@outlook.com>
2024-08-23 11:26:23 +0800

    Add support for echoing user input

--- src/main/java/Morgana.java
+++ src/main/java/Morgana.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Morgana {
     private static final String NAME = "Morgana";
     private static final String HORIZONTAL_LINE = "============================================================\n";
@@ -6,6 +8,16 @@ public class Morgana {
         System.out.print(HORIZONTAL_LINE);
         System.out.printf("Hello! I'm %s.%n", NAME);
         System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+
+        Scanner sc = new Scanner(System.in);
+        String line;
+        while (!(line = sc.nextLine()).equals("bye")) {
+            System.out.print(HORIZONTAL_LINE);
+            System.out.println(line);
+            System.out.println(HORIZONTAL_LINE);
+        }
+
         System.out.print(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.print(HORIZONTAL_LINE);
