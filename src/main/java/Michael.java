3c29ca9cc08c8b0bef9e298147108aa5c6a4c168
arjun2598 <arjunkumanan123@gmail.com>
2024-08-23 14:57:57 +0800

    Rename chatbot, add greeting and exit

---
+++ src/main/java/Michael.java
@@ -0,0 +1,15 @@
+public class Michael {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+
+        System.out.println("Hello! I'm Michael.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+
+        System.out.println("--------------------------------------");
+
+        System.out.println("Bye! Hope to see you again soon!");
+
+        System.out.println("--------------------------------------");
+    }
+}
