1bf30dadaa405b55ae8424cd6f4826159b753a9c
Cheng Zihao <czh152103@gmail.com>
2024-08-23 11:57:41 +0800

    change name to chattyBuddy

--- src/main/java/ChattyBuddy.java
+++ src/main/java/ChattyBuddy.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class ChattyBuddy {
     public static void main(String[] args) {
         String break_line = "——————————————————————————";
         String logo = " ____        _        \n"
