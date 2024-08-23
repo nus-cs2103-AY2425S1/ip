a50961aecbf35b04d61ee538e52f0e4c33395ea4
Rithik <rithiklenin@gmail.com>
2024-08-23 10:09:33 +0800

    added changes to file to have added functionality but having some problem with list

--- src/main/java/Optimus.java
+++ src/main/java/Optimus.java
@@ -1,16 +1,28 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.*;
 public class Optimus {
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Optimus");
         System.out.println("What can I do for you?");
         Scanner stringScanner = new Scanner(System.in);
+        String [] tasks = new String [100];
+        int i = 0;
         while (true) {
             String text = stringScanner.next();
             if (text.equals("bye")){
                 break;
             }
-            System.out.println(text);
+            if (text.equals("list")){
+                for (int j = 0; j < tasks.length; j++) {
+                    System.out.println(j + ". " + text);
+                }
+            }
+            tasks[i] = text;
+            System.out.println("added: " + text);
+            i++;
+
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
