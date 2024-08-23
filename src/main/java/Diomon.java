7ddaa78395744de9fff9049bb21e9f39ec772de4
EyuGongYi <145592984+EyuGongYi@users.noreply.github.com>
2024-08-23 21:31:10 +0800

    Add ability to store and list text inputs

--- src/main/java/Diomon.java
+++ src/main/java/Diomon.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Diomon {
+    private static ArrayList<String> taskList = new ArrayList<String>();
     public static void greeting() {
         String greetingMessage = """
                            ________________________________________________________________
                            Hello! I'm Diomon
-                           What can I do for you?
+                           What do you need recorded?
                            ________________________________________________________________
                            """;
         System.out.println(greetingMessage);
@@ -26,12 +28,17 @@ public class Diomon {
             if (input.equalsIgnoreCase("bye")) {
                 break;
             }
+            taskList.add(input);
+            System.out.println("""
+                               ________________________________________________________________
+                              """);
 
-            System.out.printf("""
+            for (int i = 0; i < taskList.size(); i++){
+                System.out.printf("%d-> %s\n", i + 1,taskList.get(i));
+            }
+            System.out.println("""
                                ________________________________________________________________
-                               ~~%s xD~~
-                               ________________________________________________________________
-                               """, input);
+                               """);
         }
     }
 
