8dea19aaa64be60c4a9589d1a63147cd38e1643e
JoanneHing <yenxinghing@gmail.com>
2024-08-23 09:32:31 +0800

    Implement initial skeletal version with greetings and exit message.

---
+++ src/main/java/Toothless.java
@@ -0,0 +1,24 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.sql.SQLOutput;
+
+public class Toothless {
+    public static void main(String[] args) {
+        String toothlessLogo = " _____            _   _     _           \n" +
+                "|_   _|___   ___ | |_| |__ | | ___  ___ ___ \n" +
+                "  | |/ _ \\ / _ \\| __| '_ \\| |/ _ \\/ __/ __|\n" +
+                "  | | (_) | (_) | |_| | | | |  __/\\__ \\__ \\\n" +
+                "  |_|\\___/ \\___/ \\__|_| |_|_|\\___||___/___/\n";
+
+        System.out.println("Welcome to the dragon's den!\n" + toothlessLogo);
+        System.out.println("Greetings, Dragon Rider! \n\n" +
+                "I'm Toothless, your friendly dragon companion. \n" +
+                "What adventure shall we embark on today? \n\n" +
+
+                "____________________________________________________________\n" +
+                "Until next time, dragon rider! \n" +
+                "Toothless the Night Fury, signing off. \n" +
+                "See you soon! \n" +
+                "____________________________________________________________\n");
+
+    }
+}
