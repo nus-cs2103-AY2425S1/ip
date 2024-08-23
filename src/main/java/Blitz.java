344fc67ac96a3bda29b8d7a01946857a8b214040
Curtis <curtischang2510@gmail.com>
2024-08-23 10:24:41 +0800

    Revert "Add user input functionality in Blitz.java"
    This reverts commit 78f52167dd513642d0fcc98d5035c4982e69ee28.

--- src/main/java/Blitz.java
+++ src/main/java/Blitz.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;
-
 public class Blitz{
     public static void main(String[] args) {
         System.out.println("---------------\n" +
                 "Hello! I'm BLITZ \n" +
                 "What can I do for you?");
-
-        Scanner scanner = new Scanner(System.in);
-        String userInput = "";
-
-        while (!userInput.equalsIgnoreCase("bye")) {
-            userInput = scanner.nextLine();
-            System.out.println("----------------\n" +
-                    userInput + "\n" +
-                    "----------------\n");
-        }
         System.out.println("----------------\n" +
-                "Till we meet again, GOODBYE!");
+                "Bye. Hope to see you again soon!");
     }
 }
