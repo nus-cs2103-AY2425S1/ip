a2709efe2367c53e9dfd500a2fb2eebe917776d3
sam-theman88 <samuel.lee9525@gmail.com>
2024-08-23 12:53:26 +0800

    Implemented OOP paradigm

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,10 +1,16 @@
 public class Edith {
-    public static void main(String[] args) {
-        String lineBreak = "\n____________________________________________________________\n";
-        String greeting = "Hello! I am EDITH, your personal chatbot companion:)"
-                            + "\nHow may I assist you?";
-        String exit = "It has been my pleasure helping you. Hope to see you again soon!";
+    private static final String lineBreak = "\n_______________________________________________________________\n";
+    private static final String greeting = "Hello! I am EDITH, your personal chatbot companion:)"
+            + "\nHow may I assist you?";
+    private static final String exit = "It has been my pleasure helping you. Hope to see you again soon!";
 
-        System.out.println(lineBreak + greeting + lineBreak + exit + lineBreak);
+    public Edith() {
+    }
+
+    public static void main(String[] args) {
+        String string0 = lineBreak + greeting + lineBreak;
+        String lastString = exit + lineBreak;
+
+        System.out.println(string0 + lastString);
     }
 }
