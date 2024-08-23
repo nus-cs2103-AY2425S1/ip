d2298ee46f337dacbcfe7c2eded5f7786eca7fa4
jewiteo <jewiteozw@gmail.com>
2024-08-23 14:11:07 +0800

    Renamed and added greeting message

---
+++ src/main/java/ChatGPT.java
@@ -0,0 +1,11 @@
+public class ChatGPT {
+    public static void main(String[] args) {
+        String name = "ChatGPT";
+        System.out.println("________________________________________________");
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________________");
+    }
+}
