d7402f67e7ee2d6a4b057544704ab0e3fc8f5999
Bonzzz3 <kellybongjiaqi2708@gmail.com>
2024-08-23 12:36:05 +0800

    Updated some ui

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ToDoList {
-    List<Task> list = new ArrayList<Task>();
+    List<Task> list = new ArrayList<>();
 
     public void addToList(Task task) throws TrackBotException {
         if (task == null) {
@@ -10,18 +10,18 @@ public class ToDoList {
         }
         list.add(task);
         System.out.println("````````````````````````````````````````````````````````````");
-        System.out.println("Successfully added this task:\n  " + task.toString());
+        System.out.println("Successfully added this task:\n  " + task);
         System.out.println("Now you have " + list.size() + " tasks in the list.");
         System.out.println("````````````````````````````````````````````````````````````");
     }
 
     public void markTask(int num) throws TrackBotException {
         if (num < 0 || num > list.size() - 1) {
-            throw new TrackBotException("Invalid task number.");
+            throw new TrackBotException("Please enter a valid task number.");
         }
         list.get(num).mark();
         System.out.println("````````````````````````````````````````````````````````````");
-        System.out.println("Successfully marked task " + (num + 1) + " as done.");
+        System.out.println("Successfully marked task " + (num + 1) + " as done:");
         System.out.println("  " + list.get(num).toString());
         System.out.println("````````````````````````````````````````````````````````````");
 
@@ -29,11 +29,11 @@ public class ToDoList {
 
     public void unmarkTask(int num) throws TrackBotException {
         if (num < 0 || num > list.size() - 1) {
-            throw new TrackBotException("Invalid task number.");
+            throw new TrackBotException("Please enter a valid task number.");
         }
         list.get(num).unmark();
         System.out.println("````````````````````````````````````````````````````````````");
-        System.out.println("Successfully marked task " + (num + 1) + " as not done yet.");
+        System.out.println("Successfully marked task " + (num + 1) + " as not done yet:");
         System.out.println("  " + list.get(num).toString());
         System.out.println("````````````````````````````````````````````````````````````");
     }
@@ -53,16 +53,14 @@ public class ToDoList {
 
        public void deleteFromList(int num) throws TrackBotException {
         if (num < 0 || num > list.size() - 1) {
-            throw new TrackBotException("Invalid task number.");
+            throw new TrackBotException("Please enter a valid task number.");
         }
         String deletedTask = list.get(num).toString();
-
         list.remove(num);
         System.out.println("````````````````````````````````````````````````````````````");
-        System.out.println("Successfully deleted task " + (num + 1) + " from list.");
+        System.out.println("Successfully deleted task " + (num + 1) + " from list:");
         System.out.println("  " + deletedTask);
         System.out.println("Now you have " + list.size() + " tasks in the list.");
         System.out.println("````````````````````````````````````````````````````````````");
-
     }
 }
