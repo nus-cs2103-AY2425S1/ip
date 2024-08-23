7b2d2edcc56bbc516d2a8df88479011e19d9a2c5
cweijin <chongweijin0172@gmail.com>
2024-08-23 15:36:16 +0800

    Chatbot renamed to Mylo. Modified the greeting and goodbye messages.

---
+++ src/main/java/Mylo.java
@@ -0,0 +1,14 @@
+public class Mylo {
+    public static void main(String[] args) {
+        String name = "Mylo";
+        String greet = "Hello! Thanks for using " + name + ".";
+        String opening_query = "What can I help you?";
+        String goodbye = "Goodbye. Have a nice day ahead!";
+        System.out.println("--------------------------------");
+        System.out.println(greet);
+        System.out.println(opening_query);
+        System.out.println("--------------------------------");
+        System.out.println(goodbye);
+        System.out.println("--------------------------------");
+    }
+}
