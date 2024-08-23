2ad7de995c81df8d374f95dfe15c430749bce73d
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-08-23 23:47:32 +0800

    Deadline class: Added JavaDoc to class

--- src/main/java/hoodini/Deadline.java
+++ src/main/java/hoodini/Deadline.java
@@ -10,6 +10,11 @@ import java.time.format.DateTimeParseException;
 public class Deadline extends Input {
     private LocalDate deadline;
 
+    /**
+     * Constructor to store the deadline input by the user
+     * @param input User input, parses user input and stores the deadline.
+     * @throws DateTimeParseException If the date is not in the correct format.
+     */
     public Deadline(String input) throws DateTimeParseException{
         super(input.split(" ",2)[1]
                 .split("/",2)[0]);
@@ -25,12 +30,21 @@ public class Deadline extends Input {
 
     }
 
+    /**
+     * Alternative Constructor to handle input read
+     * @param input Handles the task read from file in the OS
+     * @param deadline Deadline of the task
+     */
     public Deadline(String input, String deadline) {
         super(input);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
         this.deadline = LocalDate.parse(deadline, formatter);
     }
 
+    /**
+     * Returns the string representation of the Deadline task.
+     * @return String representation of the Deadline task.
+     */
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
