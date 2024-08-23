9fb94a682c320ea27dd02444bb2efb3f539e1a39
LimKaiWei <E1122652@u.nus.edu>
2024-08-23 14:42:51 +0800

    Level-1 commit - updated gitignore to ignore .swp for vim, hopefully works after this push

--- src/main/java/Elliot.java
+++ src/main/java/Elliot.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Elliot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
         IntroSay();
-        ByeSay();
+        while(running) {
+            System.out.print("> ");
+            String userInput = CaptureUserInput(scanner);
+            Say("");
+            switch(userInput.toLowerCase()) {
+                case "bye":
+                    ByeSay();
+                    running = false;
+                    break;
+                default:
+                    Say(userInput.strip()+"\n");
+            }
+        }
     }
 
     private static void Say(String textToSay) {
@@ -25,5 +41,9 @@ public class Elliot {
     private static void ByeSay() {
         String bye = " Bye. Hope to see you again soon!\n";
         Say(bye);
+    }
+
+    private static String CaptureUserInput(Scanner scanner) {
+        return scanner.nextLine();
     }
 }
