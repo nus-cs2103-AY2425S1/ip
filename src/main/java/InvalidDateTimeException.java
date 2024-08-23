6eaab9060020c56f02a66973ddb6c8a983ae6ce2
wkxcass <e1129889@u.nus.edu>
2024-08-23 16:08:58 +0800

    Generalize InvalidDateTimeException
    The "InvalidDateTimeException" is thrown only for "LocalDateTime"
    parsing errors.
    
    The "LocalDate" class to be introduced later can also throw parsing
    errors. These are also expected to be handled using the
    "InvalidDateTimeException" class.
    
    Let's,
    * generalize "InvalidDateTimeException" to represent errors thrown by
    either "LocalDateTime" and "LocalDate" parsing methods
    * ensure that the exception returns appropriate error messages specific
    to the type of date-time object being parsed
    
    This implementation prepares MoiMoi to handle both "LocalDateTime" and
    "LocalDate" parsing errors. Users are able to receive clear and relevant
    error messages if such issues arise.

--- src/main/java/InvalidDateTimeException.java
+++ src/main/java/InvalidDateTimeException.java
@@ -1,9 +1,22 @@
 public class InvalidDateTimeException extends MoiMoiException {
 
+    String type;
+    String formatter;
+
+    public InvalidDateTimeException(String type) {
+        this.type = type;
+        if (this.type.equals("date-time")) {
+            this.formatter = "yyyy-MM-dd HH:mm";
+        } else if (this.type.equals("date")) {
+            this.formatter = "yyyy-MM-dd";
+        }
+
+    }
     @Override
     public String getMessage() {
-        return super.getMessage() + "This date-time doesn't make sense to me.. This calls for a fix!\n"
-                + "** Pro-tip: pass me valid date-times in the format 'yyyy-MM-dd HH:mm' ;)";
+        return super.getMessage() + "The " + this.type
+                + " format doesn't make sense to me.. This calls for a fix!\n"
+                + "** Pro-tip: pass me valid " + this.type + "s in the format '" + this.formatter + "' ;)";
     }
 
 }
