a9a6590d267c2832c5f0fa8d930945774c8c0cf7
colinhia <coase28@icloud.com>
2024-08-23 14:11:04 +0800

    Level-0

---
+++ src/main/java/Julie.java
@@ -0,0 +1,11 @@
+public class Julie {
+    private static final String NAME = "Julie";
+    private static final String HORIZONTAL_LINE = "_______________________________________";
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hi!! My name is: " + NAME + "!\nHow may I help?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye!! See you soon!!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
