78f52167dd513642d0fcc98d5035c4982e69ee28
Curtis <curtischang2510@gmail.com>
2024-08-23 10:11:51 +0800

    Add user input functionality in Blitz.java
    Accepts user input and echos input. Terminates when user types "bye" (case insensitive)

--- src/main/java/Blitz.java
+++ src/main/java/Blitz.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Blitz{
     public static void main(String[] args) {
         System.out.println("---------------\n" +
                 "Hello! I'm BLITZ \n" +
                 "What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while (!userInput.equalsIgnoreCase("bye")) {
+            userInput = scanner.nextLine();
+            System.out.println("----------------\n" +
+                    userInput + "\n" +
+                    "----------------\n");
+        }
         System.out.println("----------------\n" +
-                "Bye. Hope to see you again soon!");
+                "Till we meet again, GOODBYE!");
     }
 }
