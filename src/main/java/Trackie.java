d52af6a803221065b763bdfccf889d392e1e1a79
xczheng <zhengxc105@gmail.com>
2024-08-23 13:24:18 +0800

    add support for add tasks and list tasks feature, remove the echo feature

--- src/main/java/Trackie.java
+++ src/main/java/Trackie.java
@@ -1,18 +1,35 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Trackie {
+    private ArrayList<String> tasks;
+
+    public Trackie() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+        System.out.println(String.format("Added: %s", task));
+    }
+
+    public void listTasks() {
+        int counter = 1;
+        for (String s : tasks) {
+            System.out.println(String.format("%d. %s", counter, s));
+            counter++;
+        }
+    }
 
     public void greet() {
         System.out.println("Hello, I'm Trackie. Nice to meet you =)");
+        System.out.println("Type something to add it as a task.");
         System.out.println("Note: type \"bye\" to exit.");
+        System.out.println("Note: type \"list\" to see the list of tasks");
     }
 
     public void exit() {
         System.out.println("Seeya!");
-    }
-
-    public void echo(String input) {
-        System.out.println(input);
     }
 
     public static void main(String[] args) {
@@ -25,10 +42,12 @@ public class Trackie {
             if (userInput.equals("bye")) {
                 bot.exit();
                 break;
+            } else if (userInput.equals("list")) {
+                bot.listTasks();
+            } else {
+                bot.addTask(userInput);
             }
-            bot.echo(userInput);
         }
-
 
 
 
