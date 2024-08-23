9a63ba8fe983c5c5e39a362ca3ab6cc8e60d9e1d
neohengkai <neohengkai2002@gmail.com>
2024-08-23 12:18:01 +0800

    Level-1: Echo

--- src/main/java/Ontos.java
+++ src/main/java/Ontos.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Ontos {
+    public static String hello = " Hello! I'm Ontos \n"
+                   + " What can I do for you?\n";
+    public static String line = "____________________________________________________________\n";
+    public static String bye = " Bye. Hope to see you again soon!\n";
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
         //         + "|  _ \\ _   _| | _____ \n"
         //         + "| | | | | | | |/ / _ \\\n"
         //         + "| |_| | |_| |   <  __/\n"
         //         + "|____/ \\__,_|_|\\_\\___|\n";
-        String hello = " Hello! I'm Ontos \n"
-                   + " What can I do for you?\n";
-        String line = "____________________________________________________________\n";
-        String bye = " Bye. Hope to see you again soon!\n";
-        System.out.println(line + hello + line + bye + line);
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(line + hello + line);
+
+        while (true){
+            String input = sc.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line + bye + line);
+                break;
+            } else {
+                System.out.println(line + " " + input + "\n" + line);
+            }
+        }
+        sc.close();
     }
 }
