4915cab6e91d9794e423de012e4a38bf5cdb6d96
yuancheng <>
2024-08-23 18:31:58 +0800

    add support for tracking three types of tasks - ToDos, Deadlines, Events

--- src/main/java/Winner.java
+++ src/main/java/Winner.java
@@ -15,7 +15,36 @@ public class Winner {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.matches("(?i)list")) {
+            if (input.matches("(?i)todo.*")) {
+                String description = input.split("todo", 2)[1].trim();
+                ToDo newToDo = new ToDo(description);
+                tasks.add(newToDo);
+                System.out.println(("-".repeat(100) + "\n" +
+                        newToDo.addTaskToString() + "\n" +
+                        "-".repeat(100)).indent(10));
+
+            } else if (input.matches("(?i)deadline.*")) {
+                String[] parts = input.split("(?i)deadline | (?i)by");
+                String description = parts[1].trim();
+                String by = parts[2].trim();
+                Deadline newDeadline = new Deadline(description, by);
+                tasks.add(newDeadline);
+                System.out.println(("-".repeat(100) + "\n" +
+                        newDeadline.addTaskToString() + "\n" +
+                        "-".repeat(100)).indent(10));
+
+            } else if (input.matches("(?i)event.*")) {
+                String[] parts = input.split("(?i)event | (?i)from | (?i)to");
+                String description = parts[1].trim();
+                String start = parts[2].trim();
+                String end = parts[3].trim();
+                Event newEvent = new Event(description, start, end);
+                tasks.add(newEvent);
+                System.out.println(("-".repeat(100) + "\n" +
+                        newEvent.addTaskToString() + "\n" +
+                        "-".repeat(100)).indent(10));
+
+            } else if (input.matches("(?i)list")) {
                 int counter = 1;
                 System.out.println(" ".repeat(10) + "-".repeat(38));
                 System.out.println(" ".repeat(10) + "Here are the tasks you have in your list:");
@@ -49,12 +78,14 @@ public class Winner {
                         "-".repeat(100)).indent(10));
                 break;
 
-            } else {
-                tasks.add(new Task(input));
-                System.out.println(("-".repeat(100) + "\n" +
-                        "added: " + input + "\n" +
-                        "-".repeat(100)).indent(10));
             }
+
+            //else {
+            //    tasks.add(new Task(input));
+            //    System.out.println(("-".repeat(100) + "\n" +
+            //            "added: " + input + "\n" +
+            //            "-".repeat(100)).indent(10));
+            //}
 
         }
         scanner.close();
