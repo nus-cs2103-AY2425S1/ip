e9b3cd57875ce108cb27a2e70c3f1f231fa280e8
ArtillerySun <e1297745@u.nus.edu>
2024-08-23 14:24:27 +0800

    level 1

--- src/main/java/ArsenBot.java
+++ src/main/java/ArsenBot.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class ArsenBot {
     public static void main(String[] args) {
         System.out.println("Hello! I'm ArsenBot");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String input = reader.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
