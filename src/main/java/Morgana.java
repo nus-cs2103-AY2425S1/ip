64ab52bd5ca06b2873c70ee0d0cbc361ab9dffa2
Gra7ityIC3 <kwtg20@outlook.com>
2024-08-23 11:03:32 +0800

    Rename main class and add greeting

---
+++ src/main/java/Morgana.java
@@ -0,0 +1,13 @@
+public class Morgana {
+    private static final String NAME = "Morgana";
+    private static final String HORIZONTAL_LINE = "============================================================\n";
+
+    public static void main(String[] args) {
+        System.out.print(HORIZONTAL_LINE);
+        System.out.printf("Hello! I'm %s.%n", NAME);
+        System.out.println("What can I do for you?");
+        System.out.print(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print(HORIZONTAL_LINE);
+    }
+}
