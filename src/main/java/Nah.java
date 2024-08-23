427cb77d98ddc424ee8a8749c198e7f04c11bd07
blackpanther9229 <hoangnguyen09022004@gmail.com>
2024-08-23 21:48:34 +0800

    fix acces modifiers in Nah class, adjust readTask function, create mark and unmark function and adjust the main function

--- src/main/java/Nah.java
+++ src/main/java/Nah.java
@@ -1,32 +1,47 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Nah {
-    static String greetLine = "____________________________________________________________\n"
+    private static String greetLine = "____________________________________________________________\n"
             + " Hello! I'm NAH \n"
             + " What can I do for you? \n"
             + "____________________________________________________________\n";
-    static String byeLine = " Bye. Hope to see you again soon!\n";
+    private static String byeLine = " Bye. Hope to see you again soon!\n";
 
-    String[] task = new String[100];
-    int taskCount = 0;
-    void greet() {
+    private Task[] task = new Task[100];
+    private int taskCount = 0;
+    private void greet() {
         System.out.println(greetLine);
     }
 
-    void exit() {
+    private void exit() {
         System.out.println(byeLine);
     }
 
-    void add(String input) {
-        task[taskCount] = input;
-        taskCount  ++;
+    private void add(String input) {
+        task[taskCount] = new Task(input);
+        taskCount ++;
         System.out.println(" added " + input + "\n");
     }
 
-    void readTask() {
+    private void readTask() {
+        System.out.println(" Here are the tasks in your list:\n");
         for (int i = 1; i <= taskCount; i ++) {
-            System.out.println( " " + i + ". " + task[i - 1] + "\n");
+            System.out.println( " " + i + ". " + task[i - 1].toString() + "\n");
         }
+    }
+
+    private void mark(int i) {
+        task[i - 1].mark();
+        System.out.println(" Nice! I've marked this task as done:\n"
+                        + "   " + task[i - 1].toString());
+    }
+
+    private void unMark(int i) {
+        task[i - 1].unMark();
+        System.out.println(" OK, I've marked this task as not done yet:\n"
+                + "   " + task[i - 1].toString());
     }
 
     public static void main(String[] args) {
@@ -48,17 +63,34 @@ public class Nah {
         while (true) {
             input = scanner.nextLine();
             System.out.println("____________________________________________________________ \n");
-            if (input.equals("bye")) {
-                nah.exit();
-                break;
+
+            String[] command = input.split(" ", 2);
+            switch (command[0]) {
+                case "bye": {
+                    nah.exit();
+                    return;
+                }
+                case "list": {
+                    nah.readTask();
+                    break;
+                }
+                case "mark": {
+                    int i = parseInt(command[1]);
+                    nah.mark(i);
+                    break;
+                }
+                case "unmark": {
+                    int i = parseInt(command[1]);
+                    nah.unMark(i);
+                    break;
+                }
+                default:
+                    nah.add(input);
+                    break;
+
             }
-            else if(input.equals("list")) {
-                nah.readTask();
-            }
-            else {
-                nah.add(input);
-            }
+            System.out.println("____________________________________________________________ \n");
         }
-        System.out.println("____________________________________________________________ \n");
+
     }
 }
