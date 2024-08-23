aed34749fd1de5ac93ddf78f90238f1f2744b2eb
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-08-23 23:49:11 +0800

    Todo class: Added Javadoc to ToDo class

--- src/main/java/hoodini/ToDo.java
+++ src/main/java/hoodini/ToDo.java
@@ -4,10 +4,19 @@ package hoodini;
  */
 public class ToDo extends Input{
 
+    /**
+     * Constructor for ToDo class.
+     * @param input The input string
+     */
     public ToDo(String input) {
         super(input.split(" ", 2)[1]);
 
     }
+
+    /**
+     * Returns the string representation of the ToDo task.
+     * @return String representation of the ToDo task.
+     */
 
     @Override
     public String toString() {
