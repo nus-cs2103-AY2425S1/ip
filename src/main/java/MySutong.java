63c5161405978dd49ff8c661b90645250ee06b9e
yu-sutong <156512625+yu-sutong@users.noreply.github.com>
2024-08-23 14:52:09 +0800

    edited MySutong.java for Level-1

--- src/main/java/MySutong.java
+++ src/main/java/MySutong.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class MySutong {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm MySutong");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
+
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
+
+        scanner.close();
     }
 }
