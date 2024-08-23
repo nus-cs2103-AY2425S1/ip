87de3f79bc53e9c6cf4e2ea3670e9d4401179ac0
ngjianyi <ngjianyi2002@gmail.com>
2024-08-23 13:18:38 +0800

    Changed array to ArrayList

--- src/main/java/Dudu.java
+++ src/main/java/Dudu.java
@@ -1,4 +1,4 @@
-import javax.sound.sampled.Line;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.StringBuilder;
 
@@ -11,19 +11,18 @@ public class Dudu {
         System.out.println(welcomeMessage);
 
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int count = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (sc.hasNextLine()) {
             try {
                 String input = sc.nextLine().toLowerCase();
                 if (input.equals("list")) {
-                    if (count == 0) {
+                    if (tasks.isEmpty()) {
                         System.out.println(LineWrapper.wrap("No tasks"));
                     } else {
                         StringBuilder output = new StringBuilder("Here are the tasks in your list:");
-                        for (int i = 0; i < count; i++) {
-                            output.append("\n" + (i + 1) + ". " + tasks[i]);
+                        for (int i = 0; i < tasks.size(); i++) {
+                            output.append("\n" + (i + 1) + ". " + tasks.get(i));
                         }
                         System.out.println(LineWrapper.wrap(output.toString()));
                     }
@@ -34,26 +33,25 @@ public class Dudu {
                     System.out.println(output);
                 } else if (input.matches("mark\\s\\d+")) {
                     int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
-                    if (pos < 0 || pos >= count) {
+                    if (pos < 0 || pos >= tasks.size()) {
                         throw new IllegalArgumentException("Please input a valid count");
                     }
-                    tasks[pos].markCompleted();
-                    String output = LineWrapper.wrap(String.format("Nice! I've marked this task as done:\n    %s", tasks[pos]));
+                    tasks.get(pos).markCompleted();
+                    String output = LineWrapper.wrap(String.format("Nice! I've marked this task as done:\n    %s", tasks.get(pos)));
                     System.out.println(output);
                 } else if (input.matches("unmark\\s\\d+")) {
                     int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
-                    if (pos < 0 || pos >= count) {
+                    if (pos < 0 || pos >= tasks.size()) {
                         throw new IllegalArgumentException("Please input a valid count");
                     }
-                    tasks[pos].markUncompleted();
-                    String output = LineWrapper.wrap(String.format("OK, I've marked this task as not done yet:\n    %s", tasks[pos]));
+                    tasks.get(pos).markUncompleted();
+                    String output = LineWrapper.wrap(String.format("OK, I've marked this task as not done yet:\n    %s", tasks.get(pos)));
                     System.out.println(output);
                 } else if (input.matches("^todo.*")) {
                     String description = getDetails(input, "todo");
                     ToDo task = new ToDo(description);
-                    tasks[count] = task;
-                    count++;
-                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
+                    tasks.add(task);
+                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
                     System.out.println(output);
                 } else if (input.matches("^deadline.*")) {
                     String[] details = getDetails(input, "deadline").split(" /by ");
@@ -64,9 +62,8 @@ public class Dudu {
                         throw new MissingDescriptionException("Missing description after deadline");
                     }
                     Deadline task = new Deadline(details[0], details[1]);
-                    tasks[count] = task;
-                    count++;
-                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
+                    tasks.add(task);
+                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
                     System.out.println(output);
                 } else if (input.matches("^event.*")) {
                     String[] details = getDetails(input, "event").split("/from");
@@ -82,9 +79,8 @@ public class Dudu {
                         throw new MissingDateTimeException("Missing to or from time");
                     }
                     Event task = new Event(description, date[0], date[1]);
-                    tasks[count] = task;
-                    count++;
-                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, count));
+                    tasks.add(task);
+                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
                     System.out.println(output);
                 } else {
                     String output = LineWrapper.wrap("Please use \"help\" to get the list of commands");
