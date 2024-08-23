d1b3eb20dbc07737347757defac5afe1d8cb9903
althea <altheatan328@gmail.com>
2024-08-23 13:56:47 +0800

    A-TextUiTesting

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -10,7 +10,7 @@ public class Duck {
         while (true) {
             String userCmd = scan.nextLine();
             if (userCmd.toLowerCase().equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.print("Bye. Hope to see you again soon!");
                 break;
             } else if (userCmd.toLowerCase().equals("list")) {
                 if (cmdNum==0) {
@@ -34,7 +34,7 @@ public class Duck {
                 System.out.println("OK, I've marked this task as not done yet:\n "+ cmds[num-1]);
             } else if ((userCmd.length() > 4) && (userCmd.substring(0,4).toLowerCase().equals("todo"))) {
                 cmds[cmdNum] = new Todo(userCmd.substring(5));
-                System.out.println("Got it. I've added this task: \n  " + cmds[cmdNum]);
+                System.out.println("Got it. I've added this task:\n  " + cmds[cmdNum]);
                 cmdNum++;
                 System.out.println("Now you have " + cmdNum + " tasks in the list.");
             } else if ((userCmd.length() > 8) && (userCmd.substring(0,8).toLowerCase().equals("deadline"))) {
@@ -42,19 +42,18 @@ public class Duck {
                 int n = userCmd.indexOf("/");
 
                 cmds[cmdNum] = new Deadline(userCmd.substring(0, n), userCmd.substring(n+4));
-                System.out.println("Got it. I've added this task: \n  " + cmds[cmdNum]);
+                System.out.println("Got it. I've added this task:\n  " + cmds[cmdNum]);
                 cmdNum++;
                 System.out.println("Now you have " + cmdNum + " tasks in the list.");
             } else if ((userCmd.length() > 5) && (userCmd.substring(0,5).toLowerCase().equals("event"))) {
                 userCmd = userCmd.substring(6);
                 int start = userCmd.indexOf("/");
                 int end = userCmd.substring(start+1).indexOf("/");
-                System.out.println(start + " "+end);
 
                 cmds[cmdNum] = new Event(userCmd.substring(0, start),
                         userCmd.substring(start+6, start+end),
                         userCmd.substring(start+1+end+4));
-                System.out.println("Got it. I've added this task: \n  " + cmds[cmdNum]);
+                System.out.println("Got it. I've added this task:\n  " + cmds[cmdNum]);
                 cmdNum++;
                 System.out.println("Now you have " + cmdNum + " tasks in the list.");
             }
