0bb750c323602ecc56544a4d412cb43b9cd41e44
lim-jt <>
2024-08-23 11:40:23 +0800

    Add ASCII art

--- src/main/java/Dash.java
+++ src/main/java/Dash.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class Dash {
     public static void main(String[] args) {
+        String logo = " .----------------.  .----------------.  .----------------.  .----------------.\n" +
+                "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
+                "| |  ________    | || |      __      | || |    _______   | || |  ____  ____  | |\n" +
+                "| | |_   ___ `.  | || |     /  \\     | || |   /  ___  |  | || | |_   ||   _| | |\n" +
+                "| |   | |   `. \\ | || |    / /\\ \\    | || |  |  (__ \\_|  | || |   | |__| |   | |\n" +
+                "| |   | |    | | | || |   / ____ \\   | || |   '.___`-.   | || |   |  __  |   | |\n" +
+                "| |  _| |___.' / | || | _/ /    \\ \\_ | || |  |`\\____) |  | || |  _| |  | |_  | |\n" +
+                "| | |________.'  | || ||____|  |____|| || |  |_______.'  | || | |____||____| | |\n" +
+                "| |              | || |              | || |              | || |              | |\n" +
+                "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
+                " '----------------'  '----------------'  '----------------'  '----------------'";
+
         //Greeting
         String horizontalLine = "________________________________________";
         System.out.println(horizontalLine);
-        System.out.println("Hello! I'm Dash.\nWhat can I do for you?");
+        System.out.println("Hello! I'm\n" + logo + "\nWhat can I do for you?");
 
         //Exit
         System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
-
 }
