11ff3fdb4ea31d99c409ba731ba5deb865005ff2
Curtis <curtischang2510@gmail.com>
2024-08-23 10:31:17 +0800

    bug fix: code used to still echo "bye" before printing "till we meet again..."
    Changed the while loop condition. Trim trailing white space before checking

--- src/main/java/Blitz.java
+++ src/main/java/Blitz.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Blitz{
     public static void main(String[] args) {
         System.out.println("---------------\n" +
                 "Hello! I'm BLITZ \n" +
                 "What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.trim().toLowerCase().equals("bye")) {
+                break;
+            }
+
+            System.out.println("----------------\n" +
+                    userInput + "\n" +
+                    "----------------\n");
+        }
         System.out.println("----------------\n" +
-                "Bye. Hope to see you again soon!");
+                "Till we meet again, GOODBYE!");
     }
 }
