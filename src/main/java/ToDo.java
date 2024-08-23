71083d2f4931a3f430a38e01bbbb18c72b7e94ab
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-08-23 15:55:20 +0800

    Added Todos, Events and Deadline classes

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,14 @@
+public class ToDo extends Task {
+
+    protected String by;
+
+    public ToDo(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
