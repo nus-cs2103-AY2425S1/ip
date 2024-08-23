abd20b8bb5feff3d8fd11881e215323d7ac8965f
travisim <64481469+travisim@users.noreply.github.com>
2024-08-23 13:45:49 +0800

    rename duke to Sinatra

---
+++ src/main/java/Sinatra.java
@@ -0,0 +1,10 @@
+public class Sinatra {
+    public static void main(String[] args) {
+
+        String initial = "Hello! I'm Sinatra \nWhat can I do for you? ";
+
+        String end = "Bye. Hope to see you again soon!";
+
+        System.out.println(initial+"\n" + end);
+    }
+}
