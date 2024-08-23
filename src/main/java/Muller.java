b10c5c3f11d59641f6071dd33082f87fd8c64fa5
muller317 <hhr317@gmail.com>
2024-08-23 18:42:29 +0800

    Level-1

--- src/main/java/Muller.java
+++ src/main/java/Muller.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Muller {
     public static void main(String[] args) {
         String logo = "____________________________________________________________";
         System.out.println(logo + "\nHello! I'm Muller\nWhat can I do for you?\n"
-                                + logo + "\nBye. Hope to see you again soon!\n" + logo);
+                                + logo);
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String in = sc.nextLine();
+            if(in.equals("bye")){
+                break;
+            } else {
+                System.out.println(logo + "\n" + in + "\n" + logo);
+            }
+        }
+
+        System.out.println(logo + "\nBye. Hope to see you again soon!\n" + logo);
     }
 }
