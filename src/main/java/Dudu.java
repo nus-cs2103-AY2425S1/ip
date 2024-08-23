8738e732414e542a6bfe5eb5e67932b0743214b5
ngjianyi <ngjianyi2002@gmail.com>
2024-08-23 15:02:22 +0800

    Added enum and changed to switch case

--- src/main/java/Dudu.java
+++ src/main/java/Dudu.java
@@ -3,96 +3,93 @@ import java.util.Scanner;
 import java.lang.StringBuilder;
 
 public class Dudu {
+    enum Command {
+        BYE, LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, HELP
+    }
+
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String welcomeMessage = LineWrapper.wrap("Hello! I'm dudu\n"
                                                 + "What can I do for you?");
         String goodbyeMessage = LineWrapper.wrap("Bye. Hope to see you again soon!");
-
         System.out.println(welcomeMessage);
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
 
         while (sc.hasNextLine()) {
             try {
-                String input = sc.nextLine().toLowerCase();
-                if (input.equals("list")) {
-                    if (tasks.isEmpty()) {
-                        System.out.println(LineWrapper.wrap("No tasks"));
-                    } else {
-                        StringBuilder output = new StringBuilder("Here are the tasks in your list:");
-                        for (int i = 0; i < tasks.size(); i++) {
-                            output.append("\n" + (i + 1) + ". " + tasks.get(i));
+                String[] input = sc.nextLine().split(" ", 2);
+                Command command = Command.valueOf(input[0].trim().toUpperCase());
+                switch (command) {
+                    case BYE: {
+                        System.out.println(goodbyeMessage);
+                        break;
+                    } case LIST: {
+                        printTasks();
+                        break;
+                    } case HELP: {
+                        String output = LineWrapper.wrap("hehe not done");
+                        System.out.println(output);
+                        break;
+                    } case TODO: {
+                        String description = getContent(input);
+                        ToDo task = new ToDo(description);
+                        addTask(task);
+                    }
+                    case DEADLINE: {
+                        String content = getContent(input);
+                        if (!content.matches(".*/by.*")) {
+                            throw new InvalidFormatException("Please use deadline [description] /by [deadline]");
                         }
-                        System.out.println(LineWrapper.wrap(output.toString()));
+                        String[] details = content.split("/by", 2);
+                        if (details[0].trim().isEmpty()) {
+                            throw new MissingDescriptionException("Missing description after deadline");
+                        }
+                        if (details.length == 1 || details[1].trim().isEmpty()) {
+                            throw new MissingDateTimeException("Missing by time");
+                        }
+                        Deadline task = new Deadline(details[0].trim(), details[1].trim());
+                        addTask(task);
+                        break;
                     }
-                } else if (input.equals("bye")) {
-                    break;
-                } else if (input.equals("help")) {
-                    String output = LineWrapper.wrap("hehe not done");
-                    System.out.println(output);
-                } else if (input.matches("mark\\s\\d+")) {
-                    int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
-                    if (pos < 0 || pos >= tasks.size()) {
-                        throw new IllegalArgumentException("Please input a valid count");
+                    case EVENT: {
+                        String content = getContent(input);
+                        if (!content.matches(".*/from.*/to.*")) {
+                            throw new InvalidFormatException("Please use event [description] /from [time] /to [time]");
+                        }
+                        String[] details = content.split("/from", 2);
+                        if (details[0].trim().isEmpty()) {
+                            throw new MissingDescriptionException("Missing description after event");
+                        }
+                        String description = details[0].trim();
+                        if (details.length == 1 || details[1].trim().isEmpty()) {
+                            throw new MissingDateTimeException("Missing from / to time");
+                        }
+                        String[] date = details[1].split("/to", 2);
+                        if (date.length <= 1 || date[0].trim().isEmpty() || date[1].trim().isEmpty()) {
+                            throw new MissingDateTimeException("Missing from / to time");
+                        }
+                        Event task = new Event(description, date[0].trim(), date[1].trim());
+                        addTask(task);
+                        break;
+                    } case MARK: {
+                        int index = getIndex(input);
+                        markTask(index);
+                        break;
+                    } case UNMARK: {
+                        int index = getIndex(input);
+                        unmarkTask(index);
+                        break;
+                    } case DELETE: {
+                        int index = getIndex(input);
+                        deleteTask(index);
+                        break;
+                    } default: {
+                        String output = LineWrapper.wrap("Please use help to get the list of commands");
+                        System.out.println(output);
+                        break;
                     }
-                    tasks.get(pos).markCompleted();
-                    String output = LineWrapper.wrap(String.format("Nice! I've marked this task as done:\n    %s", tasks.get(pos)));
-                    System.out.println(output);
-                } else if (input.matches("unmark\\s\\d+")) {
-                    int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
-                    if (pos < 0 || pos >= tasks.size()) {
-                        throw new IllegalArgumentException("Please input a valid count");
-                    }
-                    tasks.get(pos).markUncompleted();
-                    String output = LineWrapper.wrap(String.format("OK, I've marked this task as not done yet:\n    %s", tasks.get(pos)));
-                    System.out.println(output);
-                } else if (input.matches("^delete\\s\\d+")) {
-                    int pos = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
-                    if (pos < 0 || pos >= tasks.size()) {
-                        throw new IllegalArgumentException("Please input a valid count");
-                    }
-                    tasks.remove(pos);
-                    String output = LineWrapper.wrap(String.format(" Noted. I've removed this task:\n    %s", tasks.get(pos)));
-                    System.out.println(output);
-                } else if (input.matches("^todo.*")) {
-                    String description = getDetails(input, "todo");
-                    ToDo task = new ToDo(description);
-                    tasks.add(task);
-                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
-                    System.out.println(output);
-                } else if (input.matches("^deadline.*")) {
-                    String[] details = getDetails(input, "deadline").split(" /by ");
-                    if (details.length <= 1 || details[1].trim().isEmpty()) {
-                        throw new MissingDescriptionException("Missing by time");
-                    }
-                    if (details[0].trim().isEmpty()) {
-                        throw new MissingDescriptionException("Missing description after deadline");
-                    }
-                    Deadline task = new Deadline(details[0], details[1]);
-                    tasks.add(task);
-                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
-                    System.out.println(output);
-                } else if (input.matches("^event.*")) {
-                    String[] details = getDetails(input, "event").split("/from");
-                    if (details[0].trim().isEmpty()) {
-                        throw new MissingDescriptionException("Missing description");
-                    }
-                    String description = details[0];
-                    if (details.length == 1) {
-                        throw new MissingDateTimeException("Missing by time");
-                    }
-                    String[] date = details[1].split(" /to ");
-                    if (date.length <= 1 || date[0].trim().isEmpty() || date[1].trim().isEmpty()) {
-                        throw new MissingDateTimeException("Missing to or from time");
-                    }
-                    Event task = new Event(description, date[0], date[1]);
-                    tasks.add(task);
-                    String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
-                    System.out.println(output);
-                } else {
-                    String output = LineWrapper.wrap("Please use \"help\" to get the list of commands");
-                    System.out.println(output);
                 }
             } catch (IllegalArgumentException e) {
                 System.out.println(e);
@@ -100,17 +97,66 @@ public class Dudu {
                 System.out.println(e);
             } catch (MissingDateTimeException e) {
                 System.out.println(e);
+            } catch (InvalidFormatException e) {
+                System.out.println(e);
             }
         }
 
         System.out.println(goodbyeMessage);
     }
 
-    public static String getDetails(String input, String task) throws MissingDescriptionException {
-        String[] details = input.split(task, 2);
-        if (details.length <= 1 || details[1].trim().isEmpty()) {
-            throw new MissingDescriptionException(String.format("Please key in a description after %s", task));
+    public static String getContent(String[] input) throws MissingDescriptionException {
+        if (input.length <= 1 || input[1].trim().isEmpty()) {
+            throw new MissingDescriptionException("Please include a description");
         }
-        return details[1];
+        return input[1].trim();
+    }
+
+    public static void printTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println(LineWrapper.wrap("No tasks"));
+        } else {
+            StringBuilder output = new StringBuilder("Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                output.append("\n" + (i + 1) + ". " + tasks.get(i));
+            }
+            System.out.println(LineWrapper.wrap(output.toString()));
+        }
+    }
+
+    public static void addTask(Task task) {
+        tasks.add(task);
+        String output = LineWrapper.wrap(String.format("Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.", task, tasks.size()));
+        System.out.println(output);
+    }
+
+    public static int getIndex(String[] input) throws MissingDescriptionException {
+        if (input.length <= 1 || input[1].replaceAll("\\D+", "").isEmpty()) {
+            throw new MissingDescriptionException("Please input a number");
+        }
+        int index = Integer.parseInt(input[1].replaceAll("\\D+", "")) - 1;
+        if (index < 0 || index >= tasks.size()) {
+            throw new IllegalArgumentException("Please input a valid count");
+        }
+        return index;
+    }
+
+    public static void markTask(int index) {
+        tasks.get(index).markCompleted();
+        String output = LineWrapper.wrap(String.format("Nice! I've marked this task as done:\n    %s", tasks.get(index)));
+        System.out.println(output);
+    }
+
+    public static void unmarkTask(int index) {
+        tasks.get(index).markUncompleted();
+        String output = LineWrapper.wrap(String.format("OK, I've marked this task as not done yet:\n    %s", tasks.get(index)));
+        System.out.println(output);
+    }
+
+    public static void deleteTask(int index) {
+        Task task = tasks.get(index);
+        tasks.remove(index);
+        String output = LineWrapper.wrap(String.format("Noted. I've removed this task:\n    %s", task));
+        System.out.println(output);
     }
 }
