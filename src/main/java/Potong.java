c6328909c495d0c82acba321ac20fced9b4b8923
BunnyHoppp <e1122434@u.nus.edu>
2024-08-23 12:28:16 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Potong.java
@@ -0,0 +1,12 @@
+public class Potong {
+
+    private static final String LINE = "_________________________";
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Potong");
+        System.out.println("What can I do for you?\n");
+        System.out.println(Potong.LINE);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(Potong.LINE);
+    }
+
+}
