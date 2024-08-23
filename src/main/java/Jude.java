ab0dc54edfb86836d2dd46f650bcb9f603b8f3a1
Judy1x4 <e1129873@u.nus.edu>
2024-08-23 08:58:27 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.
    Assume there will be no more than 100 tasks.

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 
 public class Jude {
-    public static void main(String[] args) {
-        String name = "Jude";
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    String name = "Jude";
+    Scanner scanner = new Scanner(System.in);
+    String[] list = new String[100];
+    int listSize = 0;
 
-        System.out.println("Hello I'm " + name);
-        System.out.println("What can I do for you, poyo?");
+    System.out.println("Hello I'm " + name);
+    System.out.println("What can I do for you, poyo?");
 
-        String userInput = scanner.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(userInput);
-            userInput = scanner.nextLine();
+    String userInput = scanner.nextLine();
+    while (!userInput.equals("bye")) {
+      if (userInput.equals("list")) {
+        for (int i = 0; i < listSize; i++) {
+          System.out.printf("%d. %s\n", (i + 1), list[i]);
         }
-        System.out.println("Bye. Hope to see you again soon, poyo!");
+      } else {
+        list[listSize++] = userInput;
+        System.out.println("added: " + userInput);
+      }
+      userInput = scanner.nextLine();
     }
+    System.out.println("Bye. Hope to see you again soon, poyo!");
+    scanner.close();
+  }
 }
+
