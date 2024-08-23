192881f7404d06c04eccfe5b68bc9a2f5e7aaac6
Dino-Nuggies <heyiheng2130@gmail.com>
2024-08-23 15:05:46 +0800

    Added list to lemon
    Added a list of task lemon will track
    Assuming less than 100 tasks

--- src/main/java/Lemon.java
+++ src/main/java/Lemon.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Lemon {
     public static void main(String[] args) {
+        /*** Initialising ***/
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm Lemon\n"
                 + " What can I do for you?\n";
@@ -13,19 +14,30 @@ public class Lemon {
 
         Scanner scan = new Scanner(System.in);
         String input = null;
+        String[] tasks = new String[100];
+        int numTasks = 0;
 
+        /*** Program starts here ***/
         System.out.print(logo);
 
-        while(true) {
+        while (true) {
             System.out.println(bar);
+
             input = scan.nextLine();
             if (input.equals("bye") || input.equals("Bye")) {
                 System.out.println(bar);
                 break;
+            } else if (input.equals("list") || input.equals("List")) {
+                System.out.println(bar);
+                for (int i = 0; i < numTasks; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+            } else {
+                System.out.println(bar);
+                System.out.println("added: " + input);
+                tasks[numTasks] = input;
+                numTasks++;
             }
-
-            System.out.println(bar);
-            System.out.println(input);
         }
 
         System.out.println(end);
