8439868306a40d13923a32d786f8a806a5c03cf8
Arren11111 <arrengithub11111@gmail.com>
2024-08-23 18:38:06 +0800

    Changed Chatbot name, Implemented start and exit text

---
+++ src/main/java/FlyChat.java
@@ -0,0 +1,18 @@
+public class FlyChat {
+    public static void main(String[] args) {
+        start();
+        end();
+    }
+
+    public static void start() {
+        System.out.println("--------------------");
+        System.out.println("Hello! I'm FlyChat\nWhat can I do for you?");
+        System.out.println("\n--------------------");
+    }
+
+    public static void end() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\n--------------------");
+    }
+
+}
