d92f8c256869e8af290a36f40c8b336760170e7a
junyi73 <lerjunyi@gmail.com>
2024-08-23 10:02:13 +0800

    feat: added echo feature and leave feature

--- src/main/java/Noisy.java
+++ src/main/java/Noisy.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Noisy {
+
     public static void main(String[] args) {
-        String string = "____________________________________________________________\n"
+        String welcomeMessage = "____________________________________________________________\n"
                 + " Hello! I'm Noisy\n"
                 + " What can I do for you?\n"
                 + "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
-        System.out.println(string);
+        System.out.println(welcomeMessage);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String goodbyeMessage = "____________________________________________________________\n"
+                    + " Bye human :(\n"
+                    + "____________________________________________________________";
+            if (input.equals("bye")) {
+                System.out.println(goodbyeMessage);
+                scanner.close();
+                break;
+            }
+            String echoMessage = "____________________________________________________________\n"
+                    + " " + input + "\n"
+                    + "____________________________________________________________";
+            System.out.println(echoMessage);
+        }
     }
 }
