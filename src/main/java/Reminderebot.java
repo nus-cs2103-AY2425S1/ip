b98e908ea176cf49c34988e7ca699e1ac851abf4
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-08-23 09:32:54 +0800

    Add Level 0
    Reminderebot class:
    rename Duke.java
    simply greets user and exits

--- src/main/java/Reminderebot.java
+++ src/main/java/Reminderebot.java
@@ -2,16 +2,16 @@ public class Reminderebot {
     private static String greetingText = "____________________________________________________________\n" +
             " Hello! I'm [Reminderebot]\n" +
             " What can I do for you?\n" +
-            "____________________________________________________________\n" +
-            " Bye. Hope to see you again soon!\n" +
-            "____________________________________________________________";
+            "____________________________________________________________\n";
 
+    private static String goodbyeText = " Bye. Hope to see you again soon!\n" +
+            "____________________________________________________________";
 
     public static void main(String[] args) {
         greeting();
     }
 
     public static void greeting() {
-        System.out.println(greetingText);
+        System.out.println(greetingText + goodbyeText);
     }
 }
