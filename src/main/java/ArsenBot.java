e3bcdbd0ccb85c65774650b7b287a363fea98a72
ArtillerySun <e1297745@u.nus.edu>
2024-08-23 16:01:04 +0800

    level 6

--- src/main/java/ArsenBot.java
+++ src/main/java/ArsenBot.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class ArsenBot {
 
-    private static final Task[] mem = new Task[105];
-    private static int cnt = 0;
+    private static final ArrayList<Task> mem = new ArrayList<Task>();
 
     public static void parseCommand(String input) throws TaskManagerException {
         if (input.startsWith("todo")) {
@@ -11,10 +13,10 @@ public class ArsenBot {
                 throw new TaskManagerException("Error: The description of a 'todo' cannot be empty.");
             }
             String description = input.substring(5).trim();
-            mem[++ cnt] = new Todo(description);
+            mem.add(new Todo(description));
             System.out.println("Got it. I've added this task:");
-            System.out.println("\t" + mem[cnt].toString());
-            System.out.println("Now you have " + cnt + " tasks in the list");
+            System.out.println("\t" + mem.get(mem.size() -1).toString());
+            System.out.println("Now you have " + mem.size() + " tasks in the list");
         } else if (input.startsWith("deadline")) {
             if (input.length() <= 9) {
                 throw new TaskManagerException("Error: The deadline command requires both a task description and a '/by' date.");
@@ -23,10 +25,10 @@ public class ArsenBot {
             if (parts.length < 2 || parts[0].trim().isEmpty() || parts[1].trim().isEmpty()) {
                 throw new TaskManagerException("Error: The deadline command requires both a task description and a '/by' date.");
             }
-            mem[++ cnt] = new Deadline(parts[0], parts[1]);
+            mem.add(new Deadline(parts[0], parts[1]));
             System.out.println("Got it. I've added this task:");
-            System.out.println("\t" + mem[cnt].toString());
-            System.out.println("Now you have " + cnt + " tasks in the list");
+            System.out.println("\t" + mem.get(mem.size() -1).toString());
+            System.out.println("Now you have " + mem.size() + " tasks in the list");
         } else if (input.startsWith("event")) {
             if(input.length() <= 6) {
                 throw new TaskManagerException("Error: The event command requires a description, start time, and end time.");
@@ -40,10 +42,10 @@ public class ArsenBot {
             if (timeParts[1].isEmpty()) {
                 throw new TaskManagerException("Error: The event command requires a description, start time, and end time.");
             }
-            mem[++ cnt] = new Event(description, timeParts[0], timeParts[1]);
+            mem.add(new Event(description, timeParts[0], timeParts[1]));
             System.out.println("Got it. I've added this task:");
-            System.out.println("\t" + mem[cnt].toString());
-            System.out.println("Now you have " + cnt + " tasks in the list");
+            System.out.println("\t" + mem.get(mem.size() -1).toString());
+            System.out.println("Now you have " + mem.size() + " tasks in the list");
         } else {
             throw new TaskManagerException("Error: Unrecognized command. Please enter a valid task command.");
         }
@@ -64,8 +66,8 @@ public class ArsenBot {
 
             else if (input.startsWith("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 1; i <= cnt; i ++) {
-                    System.out.println(i + "." + mem[i]);
+                for (int i = 0; i < mem.size(); i ++) {
+                    System.out.println((i+1) + "." + mem.get(i));
                 }
                 continue;
             }
@@ -73,17 +75,25 @@ public class ArsenBot {
             else if (input.startsWith("mark")) {
                 System.out.println("Nice! I've marked this task as done:");
                 int pos = Integer.parseInt(input.substring(5));
-                mem[pos].markAsDone();
-                System.out.println(mem[pos]);
+                mem.get(pos).markAsDone();
+                System.out.println(mem.get(pos));
                 continue;
             }
 
             else if (input.startsWith("unmark")) {
                 System.out.println("OK, I've marked this task as not done yet:");
                 int pos = Integer.parseInt(input.substring(7));
-                mem[pos].markAsUndone();
-                System.out.println(mem[pos]);
+                mem.get(pos).markAsUndone();
+                System.out.println(mem.get(pos));
                 continue;
+            }
+
+            else if (input.startsWith("delete")) {
+                System.out.println("Noted. I've removed this task:");
+                int pos = Integer.parseInt(input.substring(7)) - 1;
+                System.out.println(mem.get(pos));
+                mem.remove(pos);
+                System.out.println("Now you have " + mem.size() + " tasks in the list.");
             } else {
                 try {
                     parseCommand(input);
