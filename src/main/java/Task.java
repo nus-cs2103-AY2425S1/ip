bcd08171d6f828ca953118995ae9063fb4d9c8ae
Tan Wen Xuan <>
2024-08-23 19:56:25 +0800

    Created a new Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+    public String getTaskName() {
+        return this.taskName;
+    }
+    public boolean isDone() {
+        return this.isDone;
+    }
+    public void markAsDone() {
+        this.isDone = true;
+    }
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+    @Override
+    public String toString() {
+        return (this.isDone ? "[X] " : "[ ] ") + this.taskName;
+    }
+}
