5dbdcd4f479c75d6b3ed24111741baddf7d21078
ashea10 <enasveen13@gmail.com>
2024-08-23 10:10:29 +0800

    Implement lvl 0

--- src/main/java/ConverSage.java
+++ src/main/java/ConverSage.java
@@ -1,10 +1,16 @@
-public class Duke {
+public class ConverSage {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____                          ____                   \n" +
+                " / ___|___  _ ____   _____ _ __/ ___|  __ _  __ _  ___ \n" +
+                "| |   / _ \\| '_ \\ \\ / / _ \\ '__\\___ \\ / _` |/ _` |/ _ \\\n" +
+                "| |__| (_) | | | \\ V /  __/ |   ___) | (_| | (_| |  __/\n" +
+                " \\____\\___/|_| |_|\\_/ \\___|_|  |____/ \\__,_|\\__, |\\___|\n" +
+                "                                            |___/      ";
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(logo);
+        System.out.println("Hello! I'm your ConverSage!");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
