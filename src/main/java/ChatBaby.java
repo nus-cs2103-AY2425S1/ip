6ff6fe9476d9808b570428b9348dca6fb86df333
Jian Yi <e1075543@u.nus.edu>
2024-08-23 12:02:30 +0800

    level 4 implemented; all responsibilities of the chatbox become functions

--- src/main/java/ChatBaby.java
+++ src/main/java/ChatBaby.java
@@ -7,45 +7,27 @@ public class ChatBaby {
         ArrayList<Task> tasks = new ArrayList<>();
         greet();
 
-        String input = scanner.nextLine();
         while (true) {
+            String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 bye();
                 break;
+            } else if (input.equals("list")) {
+                printTasks(tasks);
+            } else if (input.startsWith("mark")) {
+                markTask(tasks, input);
+            } else if (input.startsWith("unmark")) {
+                unmarkTask(tasks, input);
+            } else if (input.startsWith("todo ")) {
+                addTodoTask(tasks, input.substring(5));
+            } else if (input.startsWith("deadline ")) {
+                addDeadlineTask(tasks, input.substring(9));
+            } else if (input.startsWith("event ")) {
+                addEventTask(tasks, input.substring(6));
+            } else {
+                addTask(tasks, input);
             }
-
-            else if (input.equals("list")) {
-                System.out.println("____________________________________________________________\n");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
-                }
-                System.out.println("\n____________________________________________________________");
-            }
-
-            else if (input.length() >= 6 && input.substring(0, 4).equals("mark")) {
-                tasks.get(Integer.parseInt(input.substring(5)) - 1).markAsDone();
-                System.out.println("____________________________________________________________\n"
-                        + "Nice! I've marked this task as done:\n"
-                        +  tasks.get(Integer.parseInt(input.substring(5)) - 1).toString());
-                System.out.println();
-            }
-
-            else if (input.length() >= 8 && input.substring(0, 6).equals("unmark")) {
-                tasks.get(Integer.parseInt(input.substring(7)) - 1).unMarkAsDone();
-                System.out.println("____________________________________________________________\n"
-                        + "OK, I've marked this task as not done yet:\n"
-                        +  tasks.get(Integer.parseInt(input.substring(7)) - 1).toString());
-                System.out.println();
-            }
-
-            else {
-                tasks.add(new Task(input));
-                System.out.println("____________________________________________________________\n"
-                        + "added: " + input + "\n"
-                        + "____________________________________________________________");
-            }
-
-            input = scanner.nextLine();
         }
     }
 
@@ -62,9 +44,66 @@ public class ChatBaby {
                 + "____________________________________________________________");
     }
 
-    public static void echo(String input) {
+    public static void printTasks(ArrayList<Task> tasks) {
         System.out.println("____________________________________________________________\n"
-                + input + "\n"
+                + "Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).toString());
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void markTask(ArrayList<Task> tasks, String input) {
+        int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+        tasks.get(taskNumber).markAsDone();
+        System.out.println("____________________________________________________________\n"
+                + "Nice! I've marked this task as done:\n"
+                + tasks.get(taskNumber).toString()
+                + "\n____________________________________________________________");
+    }
+
+    public static void unmarkTask(ArrayList<Task> tasks, String input) {
+        int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+        tasks.get(taskNumber).unMarkAsDone();
+        System.out.println("____________________________________________________________\n"
+                + "OK, I've marked this task as not done yet:\n"
+                + tasks.get(taskNumber).toString()
+                + "\n____________________________________________________________");
+    }
+
+    public static void addTask(ArrayList<Task> tasks, String name) {
+        tasks.add(new Task(name));
+        System.out.println("____________________________________________________________\n"
+                + "added: " + name + "\n"
                 + "____________________________________________________________");
+    }
+
+    public static void addTodoTask(ArrayList<Task> tasks, String name) {
+        tasks.add(new ToDo(name));
+        System.out.println("____________________________________________________________\n"
+                + "Got it. I've added this task:\n"
+                + "  " + tasks.get(tasks.size() - 1)
+                + "\nNow you have " + tasks.size() + " tasks in the list."
+                + "\n____________________________________________________________");
+    }
+
+    public static void addDeadlineTask(ArrayList<Task> tasks, String input) {
+        String[] parts = input.split(" /by ");
+        tasks.add(new Deadline(parts[0], parts[1]));
+        System.out.println("____________________________________________________________\n"
+                + "Got it. I've added this task:\n"
+                + "  " + tasks.get(tasks.size() - 1)
+                + "\nNow you have " + tasks.size() + " tasks in the list."
+                + "\n____________________________________________________________");
+    }
+
+    public static void addEventTask(ArrayList<Task> tasks, String input) {
+        String[] parts = input.split(" /from | /to ");
+        tasks.add(new Event(parts[0], parts[1], parts[2]));
+        System.out.println("____________________________________________________________\n"
+                + "Got it. I've added this task:\n"
+                + "  " + tasks.get(tasks.size() - 1)
+                + "\nNow you have " + tasks.size() + " tasks in the list."
+                + "\n____________________________________________________________");
     }
 }
