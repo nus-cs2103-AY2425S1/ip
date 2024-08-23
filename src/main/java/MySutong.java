2444f9f3608d5b3da0925a1a3944d90317b7600b
yu-sutong <156512625+yu-sutong@users.noreply.github.com>
2024-08-23 14:39:08 +0800

    add MySutong.java

---
+++ src/main/java/MySutong.java
@@ -0,0 +1,11 @@
+public class MySutong {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm MySutong");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
