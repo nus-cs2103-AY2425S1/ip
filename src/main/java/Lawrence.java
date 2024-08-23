d4ccf3804d7f220d262d4e321253f10ebdc4cab6
C5hives <e1122945@u.nus.edu>
2024-08-23 14:36:02 +0800

    Lawrence.java: Modify class to use Tasks class for tracking

--- src/main/java/Lawrence.java
+++ src/main/java/Lawrence.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Lawrence {
     private static final String NAME = "Lawrence";
-    private static final ArrayList<String> list = new ArrayList<>();
+    private static final Tasks tasks = new Tasks();
 
     public static void main(String[] args) {
         greetUser();
@@ -12,45 +12,49 @@ public class Lawrence {
         String userInput;
         while (true) {
             userInput = sc.nextLine();  // Get next user input
-            switch (userInput) {
+            String[] components = userInput.split(" ");
+            switch (components[0]) {
                 case "bye":
-                    printMessage("That's all folks! Hope to see you again soon!");
+                    displayMessage("That's all folks! Hope to see you again soon!");
                     return;
                 case "list":
-                    printList();
+                    displayTasks();
+                    break;
+                case "mark":
+                    Task completeTask = tasks.completeTask(Integer.parseInt(components[1]));
+                    displayMessage(
+                            String.format("I've marked the task as complete:\n%s", completeTask));
+                    break;
+                case "unmark":
+                    Task incompleteTask = tasks.unCompleteTask(Integer.parseInt(components[1]));
+                    displayMessage(
+                            String.format("Changed your mind? The task is set to incomplete:\n%s", incompleteTask));
                     break;
                 default:
-                    list.add(userInput);
-                    printMessage(String.format("Added '%s' to the list.", userInput));
+                    Task t = new Task(userInput);
+                    tasks.addTask(t);
+                    displayMessage(String.format("Alright, added task: %s to the list.", t));
             }
         }
-    }   
+    }
 
     private static void greetUser() {
         String greeting = String.format("Hello! I'm %s and I'm here to steal your credit card information.\n"
                 + "What can I do for you?", NAME);
-        printMessage(greeting);
+        displayMessage(greeting);
     }
 
-    private static void printList() {
-        StringBuilder result = new StringBuilder();
-        int serialNumber = 1;
-        for (String item : list) {
-            result.append(String.format("%d. %s%n", serialNumber, item));
-            serialNumber++;
-        }
-
-        // exclude the last newline character from getting printed
-        printMessage(result.substring(0, result.length() - 1));
+    private static void displayTasks() {
+        displayMessage(String.format("Here's your laundry list:%n%s", tasks.toString()));
     }
 
-    private static void printMessage(String message) {
-        printHorizontalLine();
+    private static void displayMessage(String message) {
+        displayHorizontalLine();
         System.out.println(message);
-        printHorizontalLine();
+        displayHorizontalLine();
     }
 
-    private static void printHorizontalLine() {
+    private static void displayHorizontalLine() {
         System.out.println("____________________");
     }
 }
