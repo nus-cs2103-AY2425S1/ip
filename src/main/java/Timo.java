8a006b294ddaafcb43692b23d84e50bbe8bfbbbb
teamotitan <timothy.tanzijing@gmail.com>
2024-08-23 11:14:46 +0800

    Level 4

--- src/main/java/Timo.java
+++ src/main/java/Timo.java
@@ -22,8 +22,55 @@ class Task {
         return (this.mark ? "X" : " ");
     }
 
-    public String value() {
-        return this.val;
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.val;
+    }
+}
+
+class Todo extends Task {
+    private final String tasktype = "T";
+
+
+    public Todo(boolean mark, String val) {
+        super(mark, val);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.tasktype + "]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    private String date;
+    private final String tasktype = "D";
+
+    public Deadline(boolean mark, String val, String date){
+        super(mark, val);
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.tasktype + "]" + super.toString() + " (by: " + this.date + ")";
+    }
+}
+
+class Event extends Task {
+    private String from;
+    private String to;
+    private final String tasktype = "E";
+
+    public Event(boolean mark, String val, String from, String to) {
+        super(mark, val);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.tasktype + "]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
     }
 }
 
@@ -64,7 +111,7 @@ public class Timo {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 1; i < index; i++) {
                         Task chosen = arr[i - 1];
-                        System.out.println(i + ". " + "[" + chosen.getStatusIcon() + "] " + chosen.value());
+                        System.out.println(i + ". " + chosen);
                     }
                     System.out.println("----------------------------");
                     break;
@@ -79,7 +126,7 @@ public class Timo {
                         chosen.markDone();
                         System.out.println("----------------------------");
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("  " + "[" + chosen.getStatusIcon() + "] " + chosen.value());
+                        System.out.println(chosen);
                         System.out.println("----------------------------");
                     } else if (input.startsWith("unmark")) {
                         //to unmark
@@ -89,8 +136,39 @@ public class Timo {
                         chosen.markUndone();
                         System.out.println("----------------------------");
                         System.out.println("Nice! I've marked this task as not done yet:");
-                        System.out.println("  " + "[" + chosen.getStatusIcon() + "] " + chosen.value());
+                        System.out.println(chosen);
                         System.out.println("----------------------------");
+                    } else if (input.startsWith("todo")) {
+                        //have an array that splits the input by space to obtain task
+                        String[] tmp = input.split(" ", 2);
+                        Todo task = new Todo(false, tmp[1]);
+                        System.out.println("----------------------------");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(task.toString());
+                        System.out.println("Now you have " + index + " tasks in the list.");
+                        System.out.println("----------------------------");
+                        arr[index - 1] = task;
+                        index++;
+                    } else if (input.startsWith("deadline")) {
+                        String[] tmp = input.split("deadline |/by ");
+                        Deadline task = new Deadline(false, tmp[1], tmp[2]);
+                        System.out.println("----------------------------");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(task.toString());
+                        System.out.println("Now you have " + index + " tasks in the list.");
+                        System.out.println("----------------------------");
+                        arr[index - 1] = task;
+                        index++;
+                    } else if (input.startsWith("event")) {
+                        String[] tmp = input.split("event |/from |/to ");
+                        Event task = new Event(false, tmp[1], tmp[2], tmp[3]);
+                        System.out.println("----------------------------");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(task.toString());
+                        System.out.println("Now you have " + index + " tasks in the list.");
+                        System.out.println("----------------------------");
+                        arr[index - 1] = task;
+                        index++;
                     } else {
                         //to add to list
                         System.out.println("----------------------------");
