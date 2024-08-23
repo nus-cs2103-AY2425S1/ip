217dee3705c6f8bfe9503ed8bd19addc07676a50
Nigeltzy <nigel.teo.zy@gmail.com>
2024-08-23 14:07:04 +0800

    Update Duke.java to Shnoop.java

---
+++ src/main/java/Shnoop.java
@@ -0,0 +1,9 @@
+public class Shnoop {
+    public static void main(String[] args) {
+        System.out.println("Greetings loved one. Let's take a journey. \n -✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-" +
+                "-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿- \n" +
+                "It's Shnoop, my dawg. I'm all up on ya. Whatchu need?");
+
+        System.out.println("\nI'll check ya later, cause you represent. Don't worry we got it on lock.");
+    }
+}
