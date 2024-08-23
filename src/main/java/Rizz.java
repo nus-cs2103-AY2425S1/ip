eab8ecf0aa3c207497b248db490a60a028b3ffa6
saajidshaik02 <saajidshaik02@gmail.com>
2024-08-23 13:02:13 +0800

    Rizz Level-0: Rename, Greet, Exit

---
+++ src/main/java/Rizz.java
@@ -0,0 +1,17 @@
+
+
+public class Rizz {
+    private void greet() {
+        String logo = "____________________________________________________________\n" +
+                "Hello! I'm Rizz\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+    public static void main(String[] args) {
+        Rizz rizz = new Rizz();
+        rizz.greet();
+    }
+}
