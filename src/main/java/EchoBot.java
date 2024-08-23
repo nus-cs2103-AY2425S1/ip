2242172f08429337b7bd4f6c397ac0d83ab3b615
JJtan2002 <tanjiajun2002@gmail.com>
2024-08-23 10:29:07 +0800

    Deal with errors such as incorrect inputs entered by the user

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class EchoBot {
+    private static final Task[] tasks = new Task[100];
+    private static int taskCount = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm EchoBot");
@@ -24,65 +25,26 @@ public class EchoBot {
                     System.out.println("____________________________________________________________");
                     break label;
                 case "list":
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println(" " + (i + 1) + "." + tasks[i]);
-                    }
-                    System.out.println("____________________________________________________________");
+                    listTasks();
                     break;
                 case "mark": {
-                    int taskNumber = Integer.parseInt(inputParts[1]) - 1;
-                    if (taskNumber < taskCount) {
-                        tasks[taskNumber].markAsDone();
-                        System.out.println("____________________________________________________________");
-                        System.out.println(" Nice! I've marked this task as done:");
-                        System.out.println("   " + tasks[taskNumber]);
-                        System.out.println("____________________________________________________________");
-                    }
+                    handleMarkCommand(inputParts);
                     break;
                 }
                 case "unmark": {
-                    int taskNumber = Integer.parseInt(inputParts[1]) - 1;
-                    if (taskNumber < taskCount) {
-                        tasks[taskNumber].markAsNotDone();
-                        System.out.println("____________________________________________________________");
-                        System.out.println(" OK, I've marked this task as not done yet:");
-                        System.out.println("   " + tasks[taskNumber]);
-                        System.out.println("____________________________________________________________");
-                    }
+                    handleUnmarkCommand(inputParts);
                     break;
                 }
                 case "todo": {
-                    tasks[taskCount] = new Todo(inputParts[1]);
-                    taskCount++;
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + tasks[taskCount - 1]);
-                    System.out.println(" Now you have " + taskCount + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
+                    handleTodoCommand(inputParts);
                     break;
                 }
                 case "deadline": {
-                    String[] details = inputParts[1].split(" /by ");
-                    tasks[taskCount] = new Deadline(details[0], details[1]);
-                    taskCount++;
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + tasks[taskCount - 1]);
-                    System.out.println(" Now you have " + taskCount + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
+                    handleDeadlineCommand(inputParts);
                     break;
                 }
                 case "event": {
-                    String[] details = inputParts[1].split(" /from | /to ");
-                    tasks[taskCount] = new Event(details[0], details[1], details[2]);
-                    taskCount++;
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + tasks[taskCount - 1]);
-                    System.out.println(" Now you have " + taskCount + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
+                    handleEventCommand(inputParts);
                     break;
                 }
                 default:
@@ -94,5 +56,115 @@ public class EchoBot {
         }
 
         scanner.close();
+    }
+    private static void listTasks() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(" " + (i + 1) + "." + tasks[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
+    private static void handleMarkCommand(String[] inputParts) {
+        if (inputParts.length < 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Please specify the task number to mark as done.");
+            System.out.println("____________________________________________________________");
+        } else {
+            int taskNumber = Integer.parseInt(inputParts[1]) - 1;
+            if (taskNumber < taskCount && taskNumber > 0) {
+                tasks[taskNumber].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Great! I've marked this task as done:");
+                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Oops! That task number doesn't exist.");
+                System.out.println("____________________________________________________________");
+            }
+        }
+    }
+    private static void handleUnmarkCommand(String[] inputParts) {
+        if (inputParts.length < 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Please specify the task number to unmark.");
+            System.out.println("____________________________________________________________");
+        } else {
+            int taskNumber = Integer.parseInt(inputParts[1]) - 1;
+            if (taskNumber < taskCount && taskNumber > 0) {
+                tasks[taskNumber].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK! I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Oops! That task number doesn't exist.");
+                System.out.println("____________________________________________________________");
+            }
+        }
+    }
+
+    private static void handleTodoCommand(String[] inputParts) {
+        if (inputParts.length < 2 || inputParts[1].trim().isEmpty()) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" The description of a todo cannot be empty.");
+            System.out.println("____________________________________________________________");
+        } else {
+            tasks[taskCount] = new Todo(inputParts[1]);
+            taskCount++;
+            System.out.println("____________________________________________________________");
+            System.out.println(" Got it. I've added this task:");
+            System.out.println("   " + tasks[taskCount - 1]);
+            System.out.println(" Now you have " + taskCount + " tasks in the list.");
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    private static void handleDeadlineCommand(String[] inputParts) {
+        if (inputParts.length < 2 || !inputParts[1].contains("/by ")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" The description of a deadline must include a due date.");
+            System.out.println("____________________________________________________________");
+        } else {
+            String[] details = inputParts[1].split(" /by ");
+            if (details.length < 2 || details[0].trim().isEmpty()) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" The description of a deadline cannot be empty.");
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = new Deadline(details[0], details[1]);
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            }
+        }
+    }
+
+    private static void handleEventCommand(String[] inputParts) {
+        if (inputParts.length < 2 || !inputParts[1].contains("/from ") || !inputParts[1].contains("/to ")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" The description of an event must include start and end times.");
+            System.out.println("____________________________________________________________");
+        } else {
+            String[] details = inputParts[1].split(" /from | /to ");
+            if (details.length < 3 || details[0].trim().isEmpty()) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" The description of an event cannot be empty.");
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = new Event(details[0], details[1], details[2]);
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
