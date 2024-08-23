c8f5d106b0d15c4084f43bcca5dbac6bbad762bb
The0nlyJuan <ongjuanwen97@gmail.com>
2024-08-23 12:31:40 +0800

    Change Krona.java to handle Task.java usage

--- src/main/java/Krona.java
+++ src/main/java/Krona.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Krona {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int allTasks = 0;
 
         System.out.println("Hello! I'm Krona\n" +
@@ -10,15 +10,28 @@ public class Krona {
 
         while (true) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
+            String[] words = input.split(" ", 2);
+
+            if (words[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
+            } else if (words[0].equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < allTasks; i++ ) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
+            } else if (words[0].equals("mark")) {
+                int taskIndex = Integer.parseInt(words[1]) - 1;
+                tasks[taskIndex].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[taskIndex]);
+            } else if (words[0].equals("unmark")) {
+                int taskIndex = Integer.parseInt(words[1]) - 1;
+                tasks[taskIndex].markNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[taskIndex]);
             } else {
-                tasks[allTasks] = input;
+                tasks[allTasks] = new Task(input);
                 allTasks++;
                 System.out.println("added: " + input);
             }
