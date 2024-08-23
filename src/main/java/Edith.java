e1e45ce1d6dfb7407b45fb315631a98378135604
sam-theman88 <samuel.lee9525@gmail.com>
2024-08-23 12:32:49 +0800

    Renamed chatbot to Edith, and added greeting

---
+++ src/main/java/Edith.java
@@ -0,0 +1,8 @@
+public class Edith {
+    public static void main(String[] args) {
+        String lineBreak = "\n____________________________________________________________\n";
+        String greeting = "Hello! I am EDITH, your personal chatbot companion:)"
+                            + "\nHow may I assist you?";
+        System.out.println(lineBreak + greeting + lineBreak);
+    }
+}
