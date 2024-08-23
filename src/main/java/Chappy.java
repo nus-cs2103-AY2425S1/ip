3eaa87fd25ead5c3ecc993f0973dd5c9b0ddc7d3
RyanWarwick <spdshadow_w@yahoo.com>
2024-08-23 08:13:34 +0800

    Chappy.java: Add static String array to store user input Chappy.java: Print String array contents (if not null) on "list" input

--- src/main/java/Chappy.java
+++ src/main/java/Chappy.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Chappy {
+
+    private static String[] userInputArray = new String[100];
+    private static int userInputArrayPointer = 0;
+
     public static void main(String[] args) {
         String logo = """            
             :..               
@@ -37,9 +41,20 @@ public class Chappy {
             String userInput = scannerObj.nextLine();
             if (userInput.equalsIgnoreCase("bye")) {
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < userInputArray.length; i++) {
+                    if (userInputArray[i] == null) {
+                        break;
+                    }
+                    System.out.println(i+1 + ". " + userInputArray[i]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
                 System.out.println("____________________________________________________________");
-                System.out.println(userInput);
+                userInputArray[userInputArrayPointer] = userInput;
+                userInputArrayPointer++;
+                System.out.println("added: " + userInput);
                 System.out.println("____________________________________________________________");
             }
             
