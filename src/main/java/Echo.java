b2a5826be3da15ba50b159a7726051572e0934db
Plishh <101051455+Plishh@users.noreply.github.com>
2024-08-23 16:23:53 +0800

    Rename main class

---
+++ src/main/java/Echo.java
@@ -0,0 +1,10 @@
+import java.util.Objects;
+import java.util.Scanner;
+public class Echo {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String logo = "____________________________________________________________\n";
+        System.out.println("Hello, I'm Echo\n" + logo);
+        
+    }
+}
