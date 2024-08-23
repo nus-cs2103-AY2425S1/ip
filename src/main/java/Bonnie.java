e38233dbf75396b5792ea0730091149f1a1295ed
FlyingDonkeys <puppylongears@gmail.com>
2024-08-23 11:36:50 +0800

    done with level 3

--- src/main/java/Bonnie.java
+++ src/main/java/Bonnie.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import src.main.java.Task;
 
 public class Bonnie {
 
     // Variables that are
     private static String username;
-    private static ArrayList<String> tasklist = new ArrayList<>();
+    private static ArrayList<Task> tasklist = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Hello I'm Bonnie, what is your name?");
@@ -13,8 +14,9 @@ public class Bonnie {
         String my_username = scanner_obj.nextLine();
         System.out.println(String.format("Hey %s! Welcome to the Bonnie chat bot!\n", my_username));
         System.out.println("What would you like to do next?\n" +
-                "1. bye : exits the chat bot\n" +
-                "2. any other string : Bonnie adds that string into your task list\n");
+                "1. bye : leaves the conversation with Bonnie\n" +
+                "2. mark/unmark {task number (accepts numerical input only)}: marks or unmarks that task as done\n" +
+                "3. any other string : Bonnie adds that task into your task list\n");
         username = my_username;
 
         while (true) {
@@ -29,11 +31,42 @@ public class Bonnie {
                     list += String.format("%d. %s\n", i, tasklist.get(i - 1));
                 }
                 System.out.println(list);
+            } else if (check_mark_command(input)) {
+                String[] arr = input.split(" ", 2);
+                Integer taskNum = Integer.valueOf(arr[1]);
+                if (arr[0].equals("mark")) {
+                    // Mark task "taskNum" as done
+                    // Note that the actual task is off by -1 in the list!
+                    tasklist.get(taskNum - 1).markAsDone();
+                } else if (arr[0].equals("unmark")) {
+                    // Mark task "taskNum" as not done
+                    tasklist.get(taskNum - 1).unmarkAsDone();
+                }
             } else {
                 // Want to add task to task_list
-                tasklist.add(input);
-                System.out.println(String.format("Hey %s, I have added \"%s\" into your task list!", username, input));
+                tasklist.add(new Task(input));
+                System.out.println(String.format("Hey %s, I have added \"%s\" into your task list!\n", username, input));
             }
+        }
+    }
+
+    public static boolean check_mark_command(String targetString) {
+        String[] arr = targetString.split(" ");
+        if ((arr[0].equals("mark") || arr[0].equals("unmark")) && arr.length == 2) {
+            try {
+                Integer taskNum = Integer.valueOf(arr[1]);
+                if (taskNum < tasklist.size() && taskNum >= 0) {
+                    return true;
+                } else {
+                    System.out.println("You might have tried to mark or unmark a task that does not exist.\n" +
+                                       "If so, please delete this wrongly added task using the delete command.\n");
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else {
+            return false;
         }
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,29 @@
 package src.main.java;
 
 public class Task {
+
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println(String.format("%s has been successfully marked as done!", description));
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+        System.out.println(String.format("%s has been successfully marked as not done!", description));
+    }
+
+    @Override
+    public String toString() {
+        return isDone ? String.format("✅: %s", this.description)
+                      : String.format("❌: %s", this.description);
+    }
+
 }
