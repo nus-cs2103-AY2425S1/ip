e0259a6a3b854a10cc5d0c1a25920fd9071eca92
rithanisk <rithanisk@gmail.com>
2024-08-23 13:44:28 +0800

    Add getTaskListLength() method

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,18 +1,28 @@
 public class TaskList {
     private static Task[] taskList;
+    private static int length;
 
     /**
      * Constructor for TaskList class, taskList initialized with array of Tasks
      */
     public TaskList() {
         taskList = new Task[100];
+        length = 0;
     }
 
     /**
      *
      * Returns the list of Tasks
      */
-    public Task[] getTaskList() {
+    public static Task[] getTaskList() {
         return taskList;
+    }
+
+    /**
+     *
+     * Returns the length of the list of Tasks
+     */
+    public static int getTaskListLength() {
+        return length;
     }
 }
