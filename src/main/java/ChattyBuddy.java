3cab1cfab5c30e5ca87b4413c070430cddf3262d
Cheng Zihao <czh152103@gmail.com>
2024-08-23 12:30:00 +0800

    added list feature

--- src/main/java/ChattyBuddy.java
+++ src/main/java/ChattyBuddy.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class ChattyBuddy {
     public static void main(String[] args) {
+        ArrayList<String> inputList = new ArrayList<>();
         String break_line = "——————————————————————————";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,10 +17,20 @@ public class ChattyBuddy {
         Scanner userInput = new Scanner(System.in);
         String response = userInput.nextLine();
         while (!response.equals("bye")) {
-            System.out.println(break_line);
-            System.out.println(response);
-            System.out.println(break_line);
-            response = userInput.nextLine();
+            if (response.equals("list")) {
+                System.out.println(break_line);
+                for (int i = 0; i < inputList.size(); i++) {
+                    System.out.printf("%d. %s%n", i + 1, inputList.get(i));
+                }
+                System.out.println(break_line);
+                response = userInput.nextLine();
+            } else {
+                inputList.add(inputList.size(), response);
+                System.out.println(break_line);
+                System.out.printf("added: %s%n", response);
+                System.out.println(break_line);
+                response = userInput.nextLine();
+            }
         }
         System.out.println(break_line);
         System.out.println("Bye. Hope to see you again soon!");
