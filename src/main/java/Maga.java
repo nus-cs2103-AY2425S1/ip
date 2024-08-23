48c6db5eee878d75d28f2f6f08b2fa7b114321ad
audipras <e1137077@u.nus.edu>
2024-08-23 11:42:38 +0800

    level 0

---
+++ src/main/java/Maga.java
@@ -0,0 +1,12 @@
+public class Maga {
+    public static void main(String[] args) {
+        String logo = "  __  __                    \n"
+                + " |  \\/  |  __ _   __ _   __ _  \n"
+                + " | |\\/| | / _` | / _` | / _` | \n"
+                + " | |  | || (_| || (_| || (_| || \n"
+                + " |_|  |_| \\__,_| \\__, | \\__,_|  \n"
+                + "                  |___/                           \n";
+        System.out.println("Hello from\n" + logo +"\nWhat can I do for you?" );
+        System.out.println("Bye! Hope to see you again :)");
+    }
+}
