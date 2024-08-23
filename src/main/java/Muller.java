7d6688b7cf0d190d33f49a127a7d9c0ab7947421
muller317 <hhr317@gmail.com>
2024-08-23 18:33:39 +0800

    Level-0

---
+++ src/main/java/Muller.java
@@ -0,0 +1,7 @@
+public class Muller {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________";
+        System.out.println(logo + "\nHello! I'm Muller\nWhat can I do for you?\n"
+                                + logo + "\nBye. Hope to see you again soon!\n" + logo);
+    }
+}
