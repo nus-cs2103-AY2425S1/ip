0cf3a904355912c60f50f808c0e6cef0555527eb
zhiyi12345 <122374247+zhiyi12345@users.noreply.github.com>
2024-08-23 13:44:53 +0500

    Level 1, created an echo feature for the chatbot to echo what is inputed by the user unless bye is replied

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 public class Bobby {
+    public static String greeting = "Hello! I'm Bobby\n"
+            + "What can I do for you?\n";
+    public static String exit = "Bye. Hope to see you again soon!\n";
+
+    public static Scanner scan = new Scanner(System.in);
+
+    /* create a checker function that takes in a
+     String input to echo everytime there is an
+     input from user. Chatbot will only return an
+     exit signal when user reply bye.
+     */
+    public static void checker(String input) {
+        if (input.equals("bye")) {
+            System.out.println(exit);
+        } else {
+            System.out.println(input);
+            checker(scan.next());
+        }
+    }
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Bobby\n"
-                + "What can I do for you?\n";
-        String exit = "Bye. Hope to see you again soon!";
-        System.out.println(greeting + exit);
+        System.out.println(greeting);
+        checker(scan.next());
     }
 }
