b724fff630258365ad10db6512ca7f33e2281cde
linedoestrolling <yiheng.yh.zhao@gmail.com>
2024-08-23 13:46:29 +0800

    Change bot name to raiden and implement skeleton

---
+++ src/main/java/Raiden.java
@@ -0,0 +1,12 @@
+public class Raiden {
+
+    public static void main(String[] args) {
+        String lines = "____________________________________________________________";
+
+        System.out.println(lines);
+        System.out.println("Hello! I'm Raiden");
+        System.out.println("What can I do for you? \n" + lines);
+
+        System.out.println("Bye. Hope to see you again soon! \n" + lines);
+    }
+}
