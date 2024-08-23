88253f0eceaa3870e67c0a2712c4ecb6595acde6
estellelim <estlim@hotmail.com>
2024-08-23 19:42:42 +0800

    Add echo

--- src/main/java/Muffin.java
+++ src/main/java/Muffin.java
@@ -1,7 +1,9 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Muffin {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " __  __       __  __ _\n" +
                 "|  \\/  |_  _ / _|/ _(_)_ _  \n" +
                 "| |\\/| | || |  _|  _| | ' \\ \n" +
@@ -10,10 +12,18 @@ public class Muffin {
         String helloMsg = "Hello~ I'm Muffin \n" +
                 "What can I do for you?";
 
-        String byeMsg = "Goodbye~ Hope to see you again soon!";
-
         System.out.println(logo + "\n" + helloMsg);
+        command(sc);
+    }
 
-        System.out.println("\n" + byeMsg);
+    public static void command(Scanner sc) {
+        String userInput = sc.nextLine();
+
+        if (userInput.equalsIgnoreCase("bye")) {
+            System.out.println("Goodbye~ Hope to see you again soon!");
+        } else {
+            System.out.println(userInput);
+            command(sc);
+        }
     }
 }
