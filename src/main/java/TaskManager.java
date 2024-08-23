3590b43f406c2fe8c6dbf8ccc0956dd5694a0090
psh12320 <pshricharan2020@gmail.com>
2024-08-23 11:55:48 +0800

    complete and incomplete methods added in TaskManager

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -2,28 +2,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    // String type can be changed to Tasks later on
-    private List<String> tasks;
+    private List<Task> tasks;
 
     public TaskManager() {
         tasks = new ArrayList<>(100);
     }
 
-    public void addTask(String task) {
+    public void addTask(Task task) {
         tasks.add(task);
+        printLines("added: " + task.getTaskItem());
+    }
+
+    public int getTaskSize() {
+        return tasks.size();
     }
 
     public void listTasks() {
         String taskString = "";
+
         for (int i=0; i<tasks.size(); i++) {
             if (i != tasks.size() - 1) {
-                taskString += i + 1 + ". " + tasks.get(i) + "\n    ";
+                taskString += i + 1 + "." + tasks.get(i).toString() + "\n    ";
             }
             else {
-                taskString += i + 1 + ". " + tasks.get(i);
+                taskString += i + 1 + "." + tasks.get(i).toString();
             }
         }
+
         printLines(taskString);
+    }
+
+    public void completeTask(int taskId) {
+        if (taskId < tasks.size() + 1) {
+            tasks.get(taskId - 1).setComplete();
+            printLines("Nice, I've marked this task as complete:\n   " +
+                    tasks.get(taskId - 1).toString());
+        }
+    }
+
+    public void incompleteTask(int taskId) {
+        if (taskId < tasks.size() + 1) {
+            tasks.get(taskId - 1).setIncomplete();
+            printLines("Ok, I've marked this task as incomplete:\n   " +
+                    tasks.get(taskId - 1).toString());
+        }
     }
 
     public void printLines(String input) {
--- src/main/java/Taskalyn.java
+++ src/main/java/Taskalyn.java
@@ -14,18 +14,79 @@ public class Taskalyn {
                 "    What can I do for you?");
 
         // Conditions
+//        while (true) {
+//            String input = scanner.nextLine();
+//            if (input.equals("bye")) {
+//                taskManager.printLines("Bye. Hope to see you again soon!");
+//                break;
+//            } else if (input.equals("list")) {
+//                taskManager.listTasks();
+//            } else if (input.startsWith("mark ")) {
+//                String[] completeString = input.split(" ");
+//
+//                if (completeString.length == 2 && isInteger completeString[1].) {
+//
+//                }
+//            } else if () {
+//
+//            } else {
+//                taskManager.addTask(new Task(input));
+//                taskManager.printLines(input);
+//            }
+//        }
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                taskManager.printLines("Bye. Hope to see you again soon!");
+            String input = scanner.nextLine().trim();
+
+            String[] completeString = input.split(" ");
+
+            String command = completeString[0];
+
+            switch (command) {
+                case "bye":
+                    taskManager.printLines("Bye. Hope to see you again soon!");
+                    break;
+
+                case "list":
+                    taskManager.listTasks();
+                    break;
+
+                case "mark":
+                    if (completeString.length == 2) {
+                        try {
+                            Integer i = Integer.parseInt(completeString[1]);
+                            if (i <= taskManager.getTaskSize() + 1) {
+                                taskManager.completeTask(i);
+                            }
+                        } catch (NumberFormatException e) {
+                            taskManager.printLines("Oops, there's an error.");
+                        }
+                    }
+                    break;
+
+                case "unmark":
+                    if (completeString.length == 2) {
+                        try {
+                            Integer i = Integer.parseInt(completeString[1]);
+                            if (i <= taskManager.getTaskSize() + 1) {
+                                taskManager.incompleteTask(i);
+                            }
+                        } catch (NumberFormatException e) {
+                            taskManager.printLines("Oops, there's an error.");
+                        }
+                    }
+                    break;
+
+                default:
+                    taskManager.addTask(new Task(input));
+                    break;
+            }
+
+            if (command.equals("bye")) {
                 break;
-            } else if (input.equals("list")) {
-                taskManager.listTasks();
-            } else {
-                taskManager.addTask("added: " + input);
-                taskManager.printLines(input);
             }
         }
+
+        scanner.close();
 
     }
 }
