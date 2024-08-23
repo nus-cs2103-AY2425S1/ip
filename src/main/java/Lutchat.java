b296e941af3105bae832e83b1a2d86eac0bf388d
lutfir-cpu <lutfir.rn@gmail.com>
2024-08-23 15:59:04 +0800

    Changed name of chatbot, added greet and exit

---
+++ src/main/java/Lutchat.java
@@ -0,0 +1,15 @@
+public class Lutchat {
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    static void greet() {
+        System.out.print("Hello! I'm Lutchat!\n");
+        System.out.print("What can I do for you?\n");
+    }
+
+    static void exit() {
+        System.out.print("Bye! Hope to see you again soon!\n");
+    }
+}
