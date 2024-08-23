cebf948fda33ddced128dd6a4330c021688493b0
Gra7ityIC3 <kwtg20@outlook.com>
2024-08-23 11:31:42 +0800

    Add support for listing user input

--- src/main/java/Morgana.java
+++ src/main/java/Morgana.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Morgana {
     private static final String NAME = "Morgana";
     private static final String HORIZONTAL_LINE = "============================================================\n";
+
+    private static final List<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.print(HORIZONTAL_LINE);
@@ -13,9 +17,18 @@ public class Morgana {
         Scanner sc = new Scanner(System.in);
         String line;
         while (!(line = sc.nextLine()).equals("bye")) {
-            System.out.print(HORIZONTAL_LINE);
-            System.out.println(line);
-            System.out.println(HORIZONTAL_LINE);
+            if (line.equals("list")) {
+                System.out.print(HORIZONTAL_LINE);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d. %s%n", i + 1, tasks.get(i));
+                }
+                System.out.println(HORIZONTAL_LINE);
+            } else {
+                tasks.add(line);
+                System.out.print(HORIZONTAL_LINE);
+                System.out.printf("added: %s%n", line);
+                System.out.println(HORIZONTAL_LINE);
+            }
         }
 
         System.out.print(HORIZONTAL_LINE);
