f6f08348893f45aee4eb52b490352a9dd9e975eb
yu-sutong <156512625+yu-sutong@users.noreply.github.com>
2024-08-23 15:06:36 +0800

    Added Level-2 requirements to MySutong.java

--- src/main/java/MySutong.java
+++ src/main/java/MySutong.java
@@ -4,6 +4,8 @@ public class MySutong {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100]; // Array to store tasks
+        int taskCount = 0; // Counter to keep track of the number of tasks
 
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm MySutong");
@@ -13,14 +15,25 @@ public class MySutong {
         String input;
         while (true) {
             input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(horizontalLine);
+            } else {
+                tasks[taskCount] = input;
+                taskCount = taskCount + 1;
+                System.out.println(horizontalLine);
+                System.out.println("added: " + input);
+                System.out.println(horizontalLine);
             }
-            System.out.println(horizontalLine);
-            System.out.println(input);
-            System.out.println(horizontalLine);
         }
 
+        System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
 
