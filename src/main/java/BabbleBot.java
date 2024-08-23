ab1ded201e6e9820450bdbcfe5a5c0d9e522bdee
DanzaSeah <e0959436@u.nus.edu>
2024-08-23 16:21:23 +0800

    Add support for echo

--- src/main/java/BabbleBot.java
+++ src/main/java/BabbleBot.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class BabbleBot {
     public static void main(String[] args) {
+        Boolean notBye = true;
+        Scanner in = new Scanner(System.in);
         String separator = "---------------------------------------------";
         System.out.println(separator);
         System.out.println("Hello! I'm BabbleBot!\nWhat can I do for you?");
+        System.out.println(separator);
+
+        while(notBye) {
+            String userInp = in.nextLine();
+            if (userInp.equals("bye")) {
+                notBye = false;
+            } else {
+                System.out.println(separator);
+                System.out.println(userInp);
+                System.out.println(separator);
+            }
+        }
         System.out.println(separator);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(separator);
