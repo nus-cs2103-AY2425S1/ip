791c2807034143088950dcb75b6be9934aa8b72b
EyuGongYi <145592984+EyuGongYi@users.noreply.github.com>
2024-08-23 20:57:22 +0800

    Add echo feature

--- src/main/java/Diomon.java
+++ src/main/java/Diomon.java
@@ -1,17 +1,43 @@
+import java.util.Scanner;
+
 public class Diomon {
-    public static void Greeting() {
+    public static void greeting() {
         String greetingMessage = """
                            ________________________________________________________________
-                           Hello! I'm Diomon\n
+                           Hello! I'm Diomon
                            What can I do for you?
-                           
                            ________________________________________________________________
-                           Bye. Hope to see you again soon!
                            """;
         System.out.println(greetingMessage);
     }
+    public static void bye() {
+        String byeMessage = """
+                            ________________________________________________________________
+                            Bye. Hope to see you again soon!
+                            ________________________________________________________________
+                            """;
+        System.out.println(byeMessage);
+    }
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.printf("""
+                               ________________________________________________________________
+                               ~~%s xD~~
+                               ________________________________________________________________
+                               """, input);
+        }
+    }
 
     public static void main(String[] args) {
-        Greeting();
+        greeting();
+        echo();
+        bye();
     }
 }
