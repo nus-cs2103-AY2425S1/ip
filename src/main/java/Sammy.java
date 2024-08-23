1f3b36f41e17b1dbb50323765dbf576be1dc4c2b
zienxu <zienxu.ang@gmail.com>
2024-08-23 17:11:37 +0800

    added level 5 requirements to Sammy.java

--- src/main/java/Sammy.java
+++ src/main/java/Sammy.java
@@ -1,6 +1,29 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class SammyException extends Exception {
+    public SammyException(String message) {
+        super(message);
+    }
+}
+
+class InvalidDescriptionException extends SammyException {
+    public InvalidDescriptionException() {
+        super("The description is invalid. It cannot be empty.");
+    }
+}
+
+class InvalidCommandException extends SammyException {
+    public InvalidCommandException() {
+        super("I'm sorry, your reply is not in my dictionary.");
+    }
+}
+
+class InvalidTaskNumberException extends SammyException {
+    public InvalidTaskNumberException() {
+        super("Wrong task number.");
+    }
+}
 class Task {
     protected String description;
     protected boolean isDone;
@@ -81,79 +104,94 @@ public class Sammy {
         System.out.println(line);
 
         while (true) {
-            String input = scanner.nextLine();
+            try {
+                String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println(line);
-                break;
-            } else if (input.equals("list")) {
-                System.out.println(line);
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + "." + tasks.get(i));
-                }
-                System.out.println(line);
-            } else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5).trim()) - 1;
-                if (index >= 0 && index < tasks.size()) {
-                    tasks.get(index).markAsDone();
+                if (input.equals("bye")) {
                     System.out.println(line);
-                    System.out.println(" Nice! I've marked this task as done:");
-                    System.out.println(" " + tasks.get(index));
+                    System.out.println(" Bye. Hope to see you again soon!");
                     System.out.println(line);
-                } else {
-                    System.out.println("Invalid task number.");
-                }
-            } else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7).trim()) - 1;
-                if (index >= 0 && index < tasks.size()) {
-                    tasks.get(index).markAsNotDone();
+                    break;
+                } else if (input.equals("list")) {
                     System.out.println(line);
-                    System.out.println(" OK, I've marked this task as not done yet:");
-                    System.out.println(" " + tasks.get(index));
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + "." + tasks.get(i));
+                    }
                     System.out.println(line);
-                } else {
-                    System.out.println("Invalid task number.");
-                }
-            } else if (input.startsWith("todo ")) {
-                String description = input.substring(5).trim();
-                tasks.add(new Todo(description));
-                System.out.println(line);
-                System.out.println(" Got it. I've added this task:");
-                System.out.println(" " + tasks.get(tasks.size() - 1));
-                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println(line);
-            } else if (input.startsWith("deadline ")) {
-                int byIndex = input.indexOf("/by ");
-                if (byIndex != -1) {
+                } else if (input.startsWith("mark ")) {
+                    int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        tasks.get(index).markAsDone();
+                        System.out.println(line);
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println(" " + tasks.get(index));
+                        System.out.println(line);
+                    } else {
+                        throw new InvalidTaskNumberException();
+                    }
+                } else if (input.startsWith("unmark ")) {
+                    int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        tasks.get(index).markAsNotDone();
+                        System.out.println(line);
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println(" " + tasks.get(index));
+                        System.out.println(line);
+                    } else {
+                        throw new InvalidTaskNumberException();
+                    }
+                } else if (input.startsWith("todo ")) {
+                    String description = input.substring(5).trim();
+                    if (description.isEmpty()) {
+                        throw new InvalidDescriptionException();
+                    }
+                    tasks.add(new Todo(description));
+                    System.out.println(line);
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println(" " + tasks.get(tasks.size() - 1));
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(line);
+                } else if (input.startsWith("deadline ")) {
+                    int byIndex = input.indexOf("/by ");
+                    if (byIndex == -1) {
+                        throw new InvalidCommandException();
+                    }
                     String description = input.substring(9, byIndex).trim();
                     String by = input.substring(byIndex + 4).trim();
+                    if (description.isEmpty() || by.isEmpty()) {
+                        throw new InvalidDescriptionException();
+                    }
                     tasks.add(new Deadline(description, by));
                     System.out.println(line);
                     System.out.println(" Got it. I've added this task:");
                     System.out.println(" " + tasks.get(tasks.size() - 1));
                     System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
                     System.out.println(line);
-                }
-            } else if (input.startsWith("event ")) {
-                int fromIndex = input.indexOf("/from ");
-                int toIndex = input.indexOf("/to ");
-                if (fromIndex != -1 && toIndex != -1) {
+                } else if (input.startsWith("event ")) {
+                    int fromIndex = input.indexOf("/from ");
+                    int toIndex = input.indexOf("/to ");
+                    if (fromIndex == -1 || toIndex == -1) {
+                        throw new InvalidCommandException();
+                    }
                     String description = input.substring(6, fromIndex).trim();
-                    String from = input.substring(fromIndex + 6, toIndex).trim();
-                    String to = input.substring(toIndex + 4).trim();
-                    tasks.add(new Event(description, from, to));
+                    String startTime = input.substring(fromIndex + 6, toIndex).trim();
+                    String endTime = input.substring(toIndex + 4).trim();
+                    if (description.isEmpty() || startTime.isEmpty() || endTime.isEmpty()) {
+                        throw new InvalidDescriptionException();
+                    }
+                    tasks.add(new Event(description, startTime, endTime));
                     System.out.println(line);
                     System.out.println(" Got it. I've added this task:");
                     System.out.println(" " + tasks.get(tasks.size() - 1));
                     System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
                     System.out.println(line);
+                } else {
+                    throw new InvalidCommandException();
                 }
-            } else {
+            } catch (SammyException e) {
                 System.out.println(line);
-                System.out.println("Invalid command. Please try again.");
+                System.out.println(" OOPS!!! " + e.getMessage());
                 System.out.println(line);
             }
         }
