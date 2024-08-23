f365c3a4dc7aa2fd7aa154f216f6301ea2f88e37
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-08-23 17:31:45 +0800

    add list

--- src/main/java/Gallium.java
+++ src/main/java/Gallium.java
@@ -5,13 +5,27 @@ public class Gallium {
         String helloMessage = "Hello! I am Gallium. \n    What can I do for you?";
         String byeMessage = "Bye. Hope to see you again soon!";
         String lines = "____________________________________________________________";
+        String[] taskList = new String[100];
         Scanner userInput = new Scanner(System.in);
         System.out.println("    " + lines + "\n    " + helloMessage + "\n    " + lines);
         String Message = userInput.nextLine();
         String bye = "bye";
+        String list = "list";
+        int x = 0;
         while (!Message.equals(bye)) {
-            System.out.println("    " + lines + "\n    " + Message + "\n    " + lines + "\n");
-            Message = userInput.nextLine();
+            if (Message.equals(list)) {
+                System.out.println("    " + lines);
+                for (int i = 1; i <= x; i++) {
+                    System.out.println("\n    " + i + ". " + taskList[i - 1]);
+                }
+                System.out.println("    " + lines + "\n    ");
+                Message = userInput.nextLine();
+            } else {
+                System.out.println("    " + lines + "\n    " + "added: " + Message + "\n    " + lines + "\n");
+                taskList[x] = Message;
+                x++;
+                Message = userInput.nextLine();
+            }
         }
         System.out.println("    " + lines + "\n    " + byeMessage + "\n    " + lines + "\n");
         userInput.close();
