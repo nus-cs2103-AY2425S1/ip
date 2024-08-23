365cdcc8b082ea0469ace47aeec1acb3bdcd59b0
Dino-Nuggies <heyiheng2130@gmail.com>
2024-08-23 15:30:30 +0800

    Added lemon mark as done
    Added the ability to mark task as done and unmark\
    Added a task class
    Fixed indentation bugs

--- src/main/java/Lemon.java
+++ src/main/java/Lemon.java
@@ -6,15 +6,16 @@ public class Lemon {
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm Lemon\n"
                 + " What can I do for you?\n";
-
         String end = " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-
         String bar = "____________________________________________________________";
+        String list = " Here are the tasks in your list:";
+        String mark = " Nice! I've marked this task as done:";
+        String unmark = " OK, I've marked this task as not done yet:";
 
         Scanner scan = new Scanner(System.in);
         String input = null;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numTasks = 0;
 
         /*** Program starts here ***/
@@ -23,19 +24,36 @@ public class Lemon {
         while (true) {
             System.out.println(bar);
 
-            input = scan.nextLine();
+            input = scan.next();
+            System.out.println(bar);
             if (input.equals("bye") || input.equals("Bye")) {
-                System.out.println(bar);
                 break;
             } else if (input.equals("list") || input.equals("List")) {
-                System.out.println(bar);
+                System.out.println(list);
                 for (int i = 0; i < numTasks; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
+            } else if (input.equals("mark") || input.equals("Mark")) {
+                System.out.println(mark);
+
+                int next = scan.nextInt();
+                tasks[next-1].markDone();
+
+                System.out.println("   [" + tasks[next-1].getStatusIcon() + "] " + tasks[next-1].description);
+            } else if (input.equals("unmark") || input.equals("Unmark")) {
+                System.out.println(unmark);
+
+                int next = scan.nextInt();
+                tasks[next-1].unmarkDone();
+
+                System.out.println("   [" + tasks[next-1].getStatusIcon() + "] " + tasks[next-1].description);
             } else {
-                System.out.println(bar);
-                System.out.println("added: " + input);
-                tasks[numTasks] = input;
+                String next = scan.nextLine();
+                System.out.println("added: " + input + next);
+
+                Task t = new Task(input + next);
+
+                tasks[numTasks] = t;
                 numTasks++;
             }
         }
