14ac7e443611be2ac47f33f2fe887309f171974d
JJtan2002 <tanjiajun2002@gmail.com>
2024-08-23 10:44:39 +0800

    Add delete function

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class EchoBot {
-    private static final Task[] tasks = new Task[100];
-    private static int taskCount = 0;
+    private static ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -47,6 +48,9 @@ public class EchoBot {
                     handleEventCommand(inputParts);
                     break;
                 }
+                case "delete":
+                    handleDeleteCommand(inputParts);
+                    break;
                 default:
                     System.out.println("____________________________________________________________");
                     System.out.println(" I'm sorry, I don't recognize that command.");
@@ -60,8 +64,8 @@ public class EchoBot {
     private static void listTasks() {
         System.out.println("____________________________________________________________");
         System.out.println(" Here are the tasks in your list:");
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println(" " + (i + 1) + "." + tasks[i]);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(" " + (i + 1) + "." + tasks.get(i));
         }
         System.out.println("____________________________________________________________");
     }
@@ -72,11 +76,11 @@ public class EchoBot {
             System.out.println("____________________________________________________________");
         } else {
             int taskNumber = Integer.parseInt(inputParts[1]) - 1;
-            if (taskNumber < taskCount && taskNumber > 0) {
-                tasks[taskNumber].markAsDone();
+            if (taskNumber < tasks.size() && taskNumber >= 0) {
+                tasks.get(taskNumber).markAsDone();
                 System.out.println("____________________________________________________________");
                 System.out.println(" Great! I've marked this task as done:");
-                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("   " + tasks.get(taskNumber));
                 System.out.println("____________________________________________________________");
             } else {
                 System.out.println("____________________________________________________________");
@@ -92,11 +96,11 @@ public class EchoBot {
             System.out.println("____________________________________________________________");
         } else {
             int taskNumber = Integer.parseInt(inputParts[1]) - 1;
-            if (taskNumber < taskCount && taskNumber > 0) {
-                tasks[taskNumber].markAsNotDone();
+            if (taskNumber < tasks.size() && taskNumber >= 0) {
+                tasks.get(taskNumber).markAsNotDone();
                 System.out.println("____________________________________________________________");
                 System.out.println(" OK! I've marked this task as not done yet:");
-                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("   " + tasks.get(taskNumber));
                 System.out.println("____________________________________________________________");
             } else {
                 System.out.println("____________________________________________________________");
@@ -112,12 +116,11 @@ public class EchoBot {
             System.out.println(" The description of a todo cannot be empty.");
             System.out.println("____________________________________________________________");
         } else {
-            tasks[taskCount] = new Todo(inputParts[1]);
-            taskCount++;
+            tasks.add(new Todo(inputParts[1]));
             System.out.println("____________________________________________________________");
             System.out.println(" Got it. I've added this task:");
-            System.out.println("   " + tasks[taskCount - 1]);
-            System.out.println(" Now you have " + taskCount + " tasks in the list.");
+            System.out.println("   " + tasks.get(tasks.size() - 1));
+            System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
             System.out.println("____________________________________________________________");
         }
     }
@@ -134,12 +137,11 @@ public class EchoBot {
                 System.out.println(" The description of a deadline cannot be empty.");
                 System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = new Deadline(details[0], details[1]);
-                taskCount++;
+                tasks.add(new Deadline(details[0], details[1]));
                 System.out.println("____________________________________________________________");
                 System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[taskCount - 1]);
-                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("   " + tasks.get(tasks.size() - 1));
+                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
                 System.out.println("____________________________________________________________");
             }
         }
@@ -157,12 +159,32 @@ public class EchoBot {
                 System.out.println(" The description of an event cannot be empty.");
                 System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = new Event(details[0], details[1], details[2]);
-                taskCount++;
+                tasks.add(new Event(details[0], details[1], details[2]));
                 System.out.println("____________________________________________________________");
                 System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[taskCount - 1]);
-                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("   " + tasks.get(tasks.size() - 1));
+                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            }
+        }
+    }
+    private static void handleDeleteCommand(String[] inputParts) {
+        if (inputParts.length < 2) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Please specify the task number to delete.");
+            System.out.println("____________________________________________________________");
+        } else {
+            int taskNumber = Integer.parseInt(inputParts[1]) - 1;
+            if (taskNumber < tasks.size() && taskNumber >= 0) {
+                Task removedTask = tasks.remove(taskNumber);
+                System.out.println("____________________________________________________________");
+                System.out.println(" Noted. I've removed this task:");
+                System.out.println("   " + removedTask);
+                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Oops! That task number doesn't exist.");
                 System.out.println("____________________________________________________________");
             }
         }
