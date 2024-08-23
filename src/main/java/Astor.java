5afba9e53b70237d56dc6f76e3919ded06d75074
yi hao <choiyihao123@gmail.com>
2024-08-23 23:59:30 +0800

    Made Astor echo user inputs and terminates with bye

--- src/main/java/Astor.java
+++ src/main/java/Astor.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Astor {
     public static void main(String[] args) {
         System.out.println("Hello, I'm Astor! \n" +
-                "What can I do for you? \n" +
-                "Bye. Hope to see you again!");
+                "What can I do for you?\n" +
+                "--------------------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                break;
+            }
+            System.out.println(input + "\n--------------------------------------");
+        }
+
     }
 }
