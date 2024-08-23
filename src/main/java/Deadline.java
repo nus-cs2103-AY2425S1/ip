5ec62b2069ae30593758f98e8419c096ef448bfb
rithanisk <rithanisk@gmail.com>
2024-08-23 16:20:07 +0800

    Add toString() method to subclasses of Task class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,4 +2,9 @@ public class Deadline extends Task {
     public Deadline(String description) {
         super(description);
     }
+
+    @Override
+    public String toString() {
+        return "[D] " + super.toString();
+    }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,4 +2,9 @@ public class Event extends Task {
     public Event(String description) {
         super(description);
     }
+
+    @Override
+    public String toString() {
+        return "[E] " + super.toString();
+    }
 }
