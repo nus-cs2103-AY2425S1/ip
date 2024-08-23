b4be167892517a4c26456ec840cf1e761a02fbb2
linedoestrolling <yiheng.yh.zhao@gmail.com>
2024-08-23 15:28:41 +0800

    Implement mark as done

--- src/main/java/Elysia.java
+++ src/main/java/Elysia.java
@@ -3,7 +3,62 @@ import java.util.Scanner;
 
 public class Elysia {
 
+
     public static void main(String[] args) {
+
+        class Task {
+            protected String description;
+            protected boolean isDone;
+
+            public Task(String description) {
+                this.description = description;
+                this.isDone = false;
+            }
+            public void updateStatus(boolean status) {
+                isDone = status;
+            }
+
+            @Override
+            public String toString() {
+                return "[" + (isDone ? "X" : " ") + "] " + description + "\n";
+            }
+        }
+
+        class TaskList {
+            private Task[] list;
+            private int listPointer = 0;
+
+            public TaskList(int size) {
+                list = new Task[size];
+            }
+
+            public void addTask(Task task) {
+                list[listPointer] = task;
+                listPointer++;
+            }
+
+            public void markTask(int taskNumber) {
+                list[taskNumber-1].updateStatus(true);
+            }
+
+            public void unmarkTask(int taskNumber) {
+                list[taskNumber-1].updateStatus(false);
+            }
+
+            public String printTask(int taskNumber) {
+                return list[taskNumber-1].toString();
+            }
+
+            @Override
+            public String toString() {
+                String output = "";
+                for (int i = 0; i < listPointer; i++) {
+                    output += (i+1) + "." + list[i].toString();
+                }
+                return output;
+            }
+        }
+
         String lines = "________________________________________________________________________________________";
 
         System.out.println(lines);
@@ -14,27 +69,33 @@ public class Elysia {
         String input = "";
         String output = "";
 
-        String[] list = new String[100];
-        int listPointer = 0;
+        TaskList tasklist = new TaskList(100);
 
         while (!Objects.equals(input, "bye")) {
             input = command.nextLine();
-            switch(input) {
-                case "bye":
-                    break;
-                case "list":
-                    output = "Here's your list! \n";
-                    for (int i = 0; i < listPointer; i++) {
-                        output += "\n" + (i+1) + ". " + list[i];
-                    }
-                    System.out.println(lines + "\n" + output + "\n" + lines + "\n");
-                    break;
-                default:
-                    list[listPointer] = input;
-                    listPointer++;
-                    output = "Added " + input + " to your list~";
-                    System.out.println(lines + "\n" + output + "\n" + lines + "\n");
-                    break;
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                output = "Here's your list! \n";
+                output += tasklist.toString();
+                System.out.println(lines + "\n" + output + lines + "\n");
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                tasklist.markTask(taskNumber);
+                output = "Amazing! You've completed this task! \n";
+                output += tasklist.printTask(taskNumber);
+                System.out.println(lines + "\n" + output + lines + "\n");
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                tasklist.unmarkTask(taskNumber);
+                output = "Making a pretty girl undo her work is not good for her health! \n";
+                output += tasklist.printTask(taskNumber);
+                System.out.println(lines + "\n" + output + lines + "\n");
+            } else {
+                Task newTask = new Task(input);
+                tasklist.addTask(newTask);
+                output = "Added " + input + " to your list~";
+                System.out.println(lines + "\n" + output + "\n" + lines + "\n");
             }
         }
 
