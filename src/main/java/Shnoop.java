48dc948ddbc4ddbe8875f48ea0a7d1d469128e9c
Nigeltzy <nigel.teo.zy@gmail.com>
2024-08-23 14:59:45 +0800

    Update Shnoop.java with replies

--- src/main/java/Shnoop.java
+++ src/main/java/Shnoop.java
@@ -1,9 +1,45 @@
+import java.util.Scanner;
 public class Shnoop {
-    public static void main(String[] args) {
-        System.out.println("Greetings loved one. Let's take a journey. \n -✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-" +
-                "-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿- \n" +
-                "It's Shnoop, my dawg. I'm all up on ya. Whatchu need?");
+    private boolean completion = false;
+    public Shnoop() {
+    }
 
-        System.out.println("\nI'll check ya later, cause you represent. Don't worry we got it on lock.");
+    ;
+
+    public String parseInput(String input) {
+        switch (input) {
+        case "bye":
+            System.out.println("\n✿ Snoop ✿: I'll check ya later, cause you represent. Don't worry we got it on lock. ♡");
+            completion = true;
+            return "exit";
+
+        default:
+            System.out.println("\n✿ Snoop ✿: " + input);
+            return "echo";
+        }
+    }
+
+    public void startIntroSpeech() {
+        System.out.println("\n ... Greetings loved one ʚ♡ɞ Let's take a journey ... \n\n\n ✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-"
+                + "✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿ \n"
+                + " ✿ It's Shnoop, my dawg. I'm all up on ya. Whatchu need? ✿ \n"
+                + " ✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿-✿ \n");
+    }
+
+    public boolean isCompleted() {
+        return completion;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        Shnoop shnoop = new Shnoop();
+
+        shnoop.startIntroSpeech();
+
+        while (!shnoop.isCompleted()) {
+            input = scanner.next();
+            String result = shnoop.parseInput(input);
+        }
     }
 }
