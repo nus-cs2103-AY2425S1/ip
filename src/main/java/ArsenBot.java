a29722b2711495ddad4e80f4da4b853c253399ee
ArtillerySun <e1297745@u.nus.edu>
2024-08-23 14:12:26 +0800

    level 0

---
+++ src/main/java/ArsenBot.java
@@ -0,0 +1,7 @@
+public class ArsenBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ArsenBot");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
