6d8b035ca1868eee1f31f89fa5ebb2b992409012
neohengkai <neohengkai2002@gmail.com>
2024-08-23 11:59:30 +0800

    level-0 : Rename, Greet, Exit

---
+++ src/main/java/Ontos.java
@@ -0,0 +1,14 @@
+public class Ontos {
+    public static void main(String[] args) {
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        String hello = " Hello! I'm Ontos \n"
+                   + " What can I do for you?\n";
+        String line = "____________________________________________________________\n";
+        String bye = " Bye. Hope to see you again soon!\n";
+        System.out.println(line + hello + line + bye + line);
+    }
+}
