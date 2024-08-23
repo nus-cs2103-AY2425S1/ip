17fbba61fb7228b959c28ebe9473c58a53ad867b
Hoodini231 <hundin231@gmail.com>
2024-08-23 11:26:08 +0800

    Add support for automated ui testing via runtest, Simple test which checks for adding a todo task, marking it and whether it throws an error.

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class Meow {
     static private String logo =  
-    "     ___           ___           ___           ___ \n" +     
-    "    /  /\\         /  /\\         /  /\\         /  /\\ \n" +   
-    "   /  /::|       /  /::\\       /  /::\\       /  /:/_ \n" +  
-    "  /  /:|:|      /  /:/\\:\\     /  /:/\\:\\     /  /:/ /\\ \n" +
-    " /  /:/|:|__   /  /::\\ \\:\\   /  /:/  \\:\\   /  /:/ /:/_ \n" +
-    "/__/:/_|::::\\ /__/:/\\:\\ \\:\\ /__/:/ \\__\\:\\ /__/:/ /:/ /\\ \n" +
-    "\\__\\/  /~~/:/ \\  \\:\\ \\:\\_\\/ \\  \\:\\ /  /:/ \\  \\:\\/:/ /:/ \n" +
-    "      /  /:/   \\  \\:\\ \\:\\    \\  \\:\\  /:/   \\  \\::/ /:/  \n" +
-    "     /  /:/     \\  \\:\\_\\/     \\  \\:\\/:/     \\  \\:\\/:/   \n" +
-    "    /__/:/       \\  \\:\\        \\  \\::/       \\  \\::/    \n" +
-    "    \\__\\/         \\__\\/         \\__\\/         \\__\\/     ";
+    "     ___           ___           ___           ___\n" +     
+    "    /  /\\         /  /\\         /  /\\         /  /\\\n" +   
+    "   /  /::|       /  /::\\       /  /::\\       /  /:/_\n" +  
+    "  /  /:|:|      /  /:/\\:\\     /  /:/\\:\\     /  /:/ /\\\n" +
+    " /  /:/|:|__   /  /::\\ \\:\\   /  /:/  \\:\\   /  /:/ /:/_\n" +
+    "/__/:/_|::::\\ /__/:/\\:\\ \\:\\ /__/:/ \\__\\:\\ /__/:/ /:/ /\\\n" +
+    "\\__\\/  /~~/:/ \\  \\:\\ \\:\\_\\/ \\  \\:\\ /  /:/ \\  \\:\\/:/ /:/\n" +
+    "      /  /:/   \\  \\:\\ \\:\\    \\  \\:\\  /:/   \\  \\::/ /:/\n" +
+    "     /  /:/     \\  \\:\\_\\/     \\  \\:\\/:/     \\  \\:\\/:/\n" +
+    "    /__/:/       \\  \\:\\        \\  \\::/       \\  \\::/\n" +
+    "    \\__\\/         \\__\\/         \\__\\/         \\__\\/";
     static private String openingMessage = logo + 
     "\n    _____________________________________________________________________" +   
     "\n    Welcome to the Meow Chatbot!!! I love meowing hue hue"
@@ -44,10 +44,7 @@ public class Meow {
     private static void outputTask(String inputType) {
         if (inputType.startsWith("mark")) {
             if (inputType.length() > 5) {
-                System.out.println(inputType.substring(5).trim());
-                System.out.println(inputType.charAt(4) == ' ');
                 if (inputType.charAt(4) == ' ' && !inputType.substring(5).trim().isEmpty()) {
-                    System.out.println("meow");
                     markTask(inputType);
                     return;
                 }
@@ -182,7 +179,7 @@ public class Meow {
             } else {
                 taskList.get(number - 1).setDone(true);
                 String icon = "[" + taskList.get(number - 1).getCompetionChar() +"]";
-                System.out.println("    Meow has marked this task as done: \n        " + icon +taskList.get(number - 1).getTaskName());
+                System.out.println("    Meow has marked this task as done:\n        " + icon +taskList.get(number - 1).getTaskName());
             }
             
             System.out.println("    " + "_____________________________________________________________________\n");
@@ -207,7 +204,7 @@ public class Meow {
             } else {
                 taskList.get(number - 1).setDone(false);
                 String icon = "[" + taskList.get(number - 1).getCompetionChar() +"]";
-                System.out.println("    Meow has unmarked this task as done: \n        " + icon +taskList.get(number - 1).getTaskName());
+                System.out.println("    Meow has unmarked this task as done:\n      " + icon +taskList.get(number - 1).getTaskName());
             }
             System.out.println("    " + "_____________________________________________________________________\n");
             
@@ -216,11 +213,11 @@ public class Meow {
 
     private static void addTask(Task task) {
         taskList.add(task);
-        System.out.println("_____________________________________________________________________\n   Meow has added a task:\n     " + task.toString() + "\n_____________________________________________________________________");
+        System.out.println("_____________________________________________________________________\n     Meow has added a task:\n    " + task.toString() + "\n_____________________________________________________________________");
     }
 
     private static void displayList() {
-        System.out.println("_____________________________________________________________________\n   Meow here is your task list:");
+        System.out.println("_____________________________________________________________________\n     Meow here is your task list:");
         for (int i = 0; i < taskList.size(); i++) {
             System.out.println("    " + (i + 1) + ". " + taskList.get(i));
 
