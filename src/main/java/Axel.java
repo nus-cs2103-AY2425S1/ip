3f6b692a7d8a1196a9c46d90174e45b0f945dd90
Tan Wen Xuan <>
2024-08-23 18:24:39 +0800

    Implemented a greeting and exit message

--- src/main/java/Axel.java
+++ src/main/java/Axel.java
@@ -1,10 +1,13 @@
 public class Axel {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //Greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Axel");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        //Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
