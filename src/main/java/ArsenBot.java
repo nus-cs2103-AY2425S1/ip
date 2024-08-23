e22b5a375daf1177befff1aca7aeb7b5d773fbbe
ArtillerySun <e1297745@u.nus.edu>
2024-08-23 14:50:03 +0800

    level 3

--- src/main/java/ArsenBot.java
+++ src/main/java/ArsenBot.java
@@ -5,25 +5,43 @@ public class ArsenBot {
         System.out.println("Hello! I'm ArsenBot");
         System.out.println("What can I do for you?\n");
 
-        String[] mem = new String[105];
+        Task[] mem = new Task[105];
         int cnt = 0;
         Scanner reader = new Scanner(System.in);
         while (true) {
             String input = reader.nextLine();
+            String[] parts = input.split(" ");
 
-            if (input.equals("bye")) {
+            if (parts[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
             }
 
-            if (input.equals("list")) {
+            if (parts[0].equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= cnt; i ++) {
-                    System.out.println(i + ". " + mem[i]);
+                    System.out.println(i + "." + mem[i]);
                 }
                 continue;
             }
 
-            mem[++ cnt] = input;
+            if (parts[0].equals("mark")) {
+                System.out.println("Nice! I've marked this task as done:");
+                int pos = Integer.parseInt(parts[1]);
+                mem[pos].markAsDone();
+                System.out.println(mem[pos]);
+                continue;
+            }
+
+            if (parts[0].equals("unmark")) {
+                System.out.println("OK, I've marked this task as not done yet:");
+                int pos = Integer.parseInt(parts[1]);
+                mem[pos].markAsUndone();
+                System.out.println(mem[pos]);
+                continue;
+            }
+
+            mem[++ cnt] = new Task(input);
             System.out.println("added: " + input);
         }
     }
