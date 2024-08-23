b470b2f6278080dbb6984f6662b0013df7d42b95
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-23 13:11:06 +0800

    Rainy.java: add implementation to delete task from tm

--- src/main/java/Rainy.java
+++ src/main/java/Rainy.java
@@ -12,7 +12,7 @@ public class Rainy {
         String[] splitByTask = messages.split("/");
         String message = input[0];
         int count;
-        if (message.equals("mark") || message.equals("unmark")) {
+        if ((message.equals("mark") || message.equals("unmark") || message.equals("delete")) && input.length == 2) {
             try {
                 count = Integer.parseInt(input[1]);
             } catch (Exception e) {
@@ -29,6 +29,8 @@ public class Rainy {
                 tm.markDone(count - 1);
             } else if (message.equals("unmark") && count != -1) {
                 tm.unmarkDone(count - 1);
+            } else if (message.equals("delete") && count != -1) {
+                tm.delete(count - 1);
             } else if (message.equals("todo")) {
                 if (input.length == 1) {
                     System.out.println("You neeed to provide a description of your ToDo task, please try again!");
@@ -63,7 +65,7 @@ public class Rainy {
             input = messages.split(" ");
             splitByTask = messages.split("/");
             message = input[0];
-            if (message.equals("mark") || message.equals("unmark")) {
+            if ((message.equals("mark") || message.equals("unmark") || message.equals("delete")) && input.length == 2) {
                 try {
                     count = Integer.parseInt(input[1]);
                 } catch (Exception e) {
