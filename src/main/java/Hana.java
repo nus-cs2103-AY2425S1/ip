cca3a98155dc1c1719890d387a85ad1edbbf397c
hansneddyanto <hans.neddy@gmail.com>
2024-08-23 14:07:33 +0800

    Handle errors using HanaException class

--- src/main/java/Hana.java
+++ src/main/java/Hana.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 public class Hana {
     public static void main(String[] args) {
         String line = "    ____________________________________________________________\n";
-        String greeting = "    Hello I'm Hana\n" +
-                "    What can I do for you?\n";
+        String greeting = """
+                Hello I'm Hana
+                What can I do for you?
+            """;
         String closing = "    Bye. Hope to see you again soon!\n";
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         ArrayList<Task> tasks = new ArrayList<>();
@@ -23,73 +25,80 @@ public class Hana {
             Matcher markMatcher = markPattern.matcher(input);
             Matcher unmarkMatcher = unmarkPattern.matcher(input);
 
-            if (input.equals("list")) {
-                System.out.println(line);
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks.get(i));
-                }
-                System.out.println(line);
-            } else if (markMatcher.matches()) {
-                // taskNumber is index already
-                int taskNumber = Integer.parseInt(markMatcher.group(1)) - 1;
-                if (taskNumber >= 0 && taskNumber < tasks.size()) {
-                    tasks.get(taskNumber).markAsDone();
-                    System.out.println(line + "     Nice! I've marked this task as done:");
-                    System.out.println("    " + tasks.get(taskNumber) + "\n" + line);
-                } else {
-                    System.out.println(line + "     Task number out of range.\n" + line);
-                }
-            } else if (unmarkMatcher.matches()) {
-                // taskNumber is index already
-                int taskNumber = Integer.parseInt(unmarkMatcher.group(1)) - 1;
-                if (taskNumber >= 0 && taskNumber < tasks.size()) {
-                    tasks.get(taskNumber).markAsUndone();
-                    System.out.println(line + "     OK, I've marked this task as not done yet:");
-                    System.out.println("    " + tasks.get(taskNumber) + "\n" + line);
-                } else {
-                    System.out.println(line + "     Task number out of range.\n" + line);
-                }
-            } else if (input.startsWith("todo")) {
-                try {
+            try {
+                if (input.equals("list")) {
+                    System.out.println(line);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println(line);
+                } else if (markMatcher.matches()) {
+                    // taskNumber is index already
+                    int taskNumber = Integer.parseInt(markMatcher.group(1)) - 1;
+                    if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                        tasks.get(taskNumber).markAsDone();
+                        System.out.println(line + "     Nice! I've marked this task as done:");
+                        System.out.println("    " + tasks.get(taskNumber) + "\n" + line);
+                    } else {
+                        System.out.println(line + "     Task number out of range.\n" + line);
+                    }
+                } else if (unmarkMatcher.matches()) {
+                    // taskNumber is index already
+                    int taskNumber = Integer.parseInt(unmarkMatcher.group(1)) - 1;
+                    if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                        tasks.get(taskNumber).markAsUndone();
+                        System.out.println(line + "     OK, I've marked this task as not done yet:");
+                        System.out.println("    " + tasks.get(taskNumber) + "\n" + line);
+                    } else {
+                        System.out.println(line + "     Task number out of range.\n" + line);
+                    }
+                } else if (input.startsWith("todo")) {
+                    if (input.trim().equals("todo")) {
+                        throw new HanaException("OOPS!!! The description of a todo cannot be empty.");
+                    }
                     String description = input.substring(5);
                     tasks.add(new ToDo(description));
                     System.out.println(line + "     Got it. I've added this task:\n" +
                             "       " + tasks.get(tasks.size() - 1) + "\n" +
                             "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
-                } catch (Exception e) {
-                    System.out.println(line + "    OOPS!!! The description of a todo cannot be empty.\n" + line);
-                }
-            } else if (input.startsWith("deadline")) {
-                try {
+                } else if (input.startsWith("deadline")) {
+                    if (input.trim().equals("deadline") || input.startsWith("deadline /by")) {
+                        throw new HanaException("OOPS!!! The description of a deadline cannot be empty.");
+                    } else if (!input.contains(" /by ")) {
+                        throw new HanaException("OOPS!!! Please add deadline date/time by adding /by");
+                    }
                     String[] parts = input.substring(9).split(" /by ");
                     tasks.add(new Deadline(parts[0], parts[1]));
                     System.out.println(line + "     Got it. I've added this task:\n" +
                             "       " + tasks.get(tasks.size() - 1) + "\n" +
                             "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
-                } catch (Exception e) {
-                    System.out.println(line + "    OOPS!!! The description of a deadline cannot be empty.\n" + line);
-                }
-            } else if (input.startsWith("event")) {
-                try {
+                } else if (input.startsWith("event")) {
+                    if (input.trim().equals("event")  || input.startsWith("event /from")) {
+                        throw new HanaException("OOPS!!! The description of an event cannot be empty.");
+                    } else if (!input.contains(" /from ") || !input.contains(" /to ")) {
+                        throw new HanaException("OOPS!!! Please add event time by adding /from and /to");
+                    } else if (input.indexOf(" /to ") < input.indexOf(" /from ")) {
+                        throw new HanaException("OOPS!!! Please add /from before /to");
+                    }
                     String[] parts = input.substring(6).split(" /from | /to ");
                     tasks.add(new Event(parts[0], parts[1], parts[2]));
                     System.out.println(line + "     Got it. I've added this task:\n" +
                             "       " + tasks.get(tasks.size() - 1) + "\n" +
                             "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
-                } catch (Exception e) {
-                    System.out.println(line + "    OOPS!!! The description of an event cannot be empty.\n" + line);
+                } else if (input.startsWith("delete")) {
+                    int taskNumber = Integer.parseInt(input.substring(7).trim()) - 1;
+                    if (taskNumber < 0 || taskNumber >= tasks.size()) {
+                        throw new HanaException("OOPS!!! Task number out of range.");
+                    }
+                    Task removedTask = tasks.remove(taskNumber);
+                    System.out.println(line + "     Noted. I've removed this task:\n" +
+                            "       " + removedTask + "\n" +
+                            "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
+                } else {
+                    throw new HanaException("I'm sorry, I don't understand that command.");
                 }
-            } else if (input.startsWith("delete")) {
-                int taskNumber = Integer.parseInt(input.substring(7).trim()) - 1;
-                if (taskNumber < 0 || taskNumber >= tasks.size()) {
-                    System.out.println(line + "    OOPS!!! Task number out of range.\n" + line);
-                }
-                Task removedTask = tasks.remove(taskNumber);
-                System.out.println(line + "     Noted. I've removed this task:\n" +
-                        "       " + removedTask + "\n" +
-                        "     Now you have " + tasks.size() + " tasks in the list.\n" + line);
-            } else {
-                System.out.println(line + "     I'm sorry, I don't understand that command.\n" + line);
+            } catch (HanaException e) {
+                System.out.println(line + "     " + e.getMessage() + "\n" + line);
             }
             input = scanner.nextLine();
         }
