3afb3c56eb9a4d4a2ba889c1639049006958aea9
cweijin <chongweijin0172@gmail.com>
2024-08-23 16:05:47 +0800

    Implemented echo-ing. Added static method separator() for ease of use.

--- src/main/java/Mylo.java
+++ src/main/java/Mylo.java
@@ -1,14 +1,31 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Mylo {
+    private static void separator() {
+        System.out.println("--------------------------------");
+    }
     public static void main(String[] args) {
         String name = "Mylo";
         String greet = "Hello! Thanks for using " + name + ".";
         String opening_query = "What can I help you?";
         String goodbye = "Goodbye. Have a nice day ahead!";
-        System.out.println("--------------------------------");
+
         System.out.println(greet);
         System.out.println(opening_query);
-        System.out.println("--------------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        while(!Objects.equals(input, "bye")) {
+            separator();
+            System.out.println(input);
+            separator();
+            input = scanner.next();
+        }
+
+        separator();
         System.out.println(goodbye);
-        System.out.println("--------------------------------");
+        separator();
     }
 }
