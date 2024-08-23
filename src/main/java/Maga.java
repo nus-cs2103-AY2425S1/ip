c11a2cc850ba9764b9621daee2784e64f9a4ca87
audipras <e1137077@u.nus.edu>
2024-08-23 11:50:04 +0800

    level 1

--- src/main/java/Maga.java
+++ src/main/java/Maga.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Maga {
     public static void main(String[] args) {
         String logo = "  __  __                    \n"
@@ -6,7 +8,15 @@ public class Maga {
                 + " | |  | || (_| || (_| || (_| || \n"
                 + " |_|  |_| \\__,_| \\__, | \\__,_|  \n"
                 + "                  |___/                           \n";
-        System.out.println("Hello from\n" + logo +"\nWhat can I do for you?" );
-        System.out.println("Bye! Hope to see you again :)");
+        System.out.println("Hello from\n" + logo +"\nI am THE best chatbot from the one and only" +
+                " US of A trust me everyone says I'm the best. How can I help you serve the American people?" );
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while(!input.equalsIgnoreCase("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Yeah goodbye. Remember a vote for me is a vote for America!");
     }
 }
