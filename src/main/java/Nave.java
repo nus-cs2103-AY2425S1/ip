33268c73dc5423490444f15cb8fcc260caed0b5c
Jollybomber <siahevan@gmail.com>
2024-08-23 15:24:29 +0800

    Renamed chatbot to Nave and changed greeting message.

---
+++ src/main/java/Nave.java
@@ -0,0 +1,11 @@
+public class Nave {
+    public static void main(String[] args) {
+        String greeting = "-----------------------------------------------------------------\n" +
+                "Hello! :) I'm Nave, your personal task management assistant.\n" +
+                "What can I do for you today?\n" +
+                "-----------------------------------------------------------------\n" +
+                "Goodbye :( Come visit me again!\n" +
+                "-----------------------------------------------------------------\n";
+        System.out.println(greeting);
+    }
+}
