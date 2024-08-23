b8705e59766851ff4a8fba0e02bae0299cdc5965
Dino-Nuggies <heyiheng2130@gmail.com>
2024-08-23 14:38:43 +0800

    Added echo to Lemon
    Added Scanner to lemon to be able to read input and echo it out

--- src/main/java/Lemon.java
+++ src/main/java/Lemon.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Lemon {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm Lemon\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n";
+                + " What can I do for you?\n";
 
         String end = " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+
+        String bar = "____________________________________________________________";
+
+        Scanner scan = new Scanner(System.in);
+        String input = null;
+
         System.out.print(logo);
+
+        while(true) {
+            System.out.println(bar);
+            input = scan.nextLine();
+            if (input.equals("bye") || input.equals("Bye")) {
+                System.out.println(bar);
+                break;
+            }
+
+            System.out.println(bar);
+            System.out.println(input);
+        }
+
         System.out.println(end);
     }
 }
