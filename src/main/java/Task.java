2b803fcb3034c2d5372871936b741bec3442d71d
Curtis <curtischang2510@gmail.com>
2024-08-23 16:00:59 +0800

    Add factory method
    Creation of new Task() handled in main
    
    Makes code main() more tidy. Factory method can determine the correct
    task subclass based on the entire description

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,10 +2,30 @@ public class Task {
     protected String description;
     protected  boolean isDone;
 
-    public Task(String description) {
+    public static Task createTask(String description) {
+        String strippedDescription = description.trim().toLowerCase();
+        if (strippedDescription.startsWith("todo")) {
+            return new Todo(description);
+        } else if(strippedDescription.startsWith("deadline")) {
+            String[] splitStr = description.split("/");
+            String des = splitStr[0].trim();
+            String deadline = splitStr[1].split(" ")[1];
+            return new Deadline(des, deadline);
+        } else if (strippedDescription.startsWith("event")) {
+            String[] splitStr = description.split("/");
+            String des = splitStr[0].trim();
+            String start = splitStr[1].replace("from ", "").trim();
+            String end = splitStr[2].replace("to ", "").trim();
+            return new Event(des, start, end);
+        }
+        return null;
+    }
+
+    protected Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
 
     public void changeStatus(boolean newIsDone) {
        this.isDone = newIsDone;
