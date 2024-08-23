e6cf9288279d33e163bc832aa8980423e54bcaa4
Jing Hong Tee <e1122714@u.nus.edu>
2024-08-23 11:22:54 +0800

    Add support for new commands

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -1,6 +1,6 @@
 public class CommandParser {
     public enum CommandType {
-        LIST, BYE, MARK, UNMARK, ADD
+        LIST, BYE, MARK, UNMARK, ADD, DEADLINE, EVENT, ERROR
     }
 
     public static CommandType parseCommand(String userInput) {
@@ -12,8 +12,14 @@ public class CommandParser {
             return CommandType.MARK;
         } else if (userInput.startsWith("unmark")) {
             return CommandType.UNMARK;
-        } else {
+        } else if (userInput.startsWith("deadline")) {
+            return CommandType.DEADLINE;
+        } else if (userInput.startsWith("event")) {
+            return CommandType.EVENT;
+        } else if (userInput.startsWith("todo")) {
             return CommandType.ADD;
+        } else {
+            return CommandType.ERROR;
         }
     }
 }
