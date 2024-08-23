2afa80753832a8e72201164c02ee2af6ba25bba1
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2024-08-23 14:10:55 +0800

    Rename Chatbot Make it Greet and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________ \n" +
+                "Hello ! I'm AVA\n" +
+                "What can I do for you ?\n" +
+                "____________________________________________________________\n" +
+                "Bye.Hope to see you again soon !\n" +
+                "____________________________________________________________");
     }
 }
+
+
+/*
+custom hello message after tag 0
+Hi,  I'm AVA (Artificial Virtual Assistant).
+I am a virtual personal assistant created by Nikhil.
+I am currently an infant and can't do much 🙁 but
+don't worry I should soon be very capable 😉
+
+Byee
+
+ */
