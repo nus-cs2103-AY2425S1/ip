9a85da9d1c3168433198991088a9acb9ef602bc1
lutfir-cpu <lutfir.rn@gmail.com>
2024-08-23 18:58:16 +0800

    added 'add' and 'list' functionality

--- src/main/java/Lutchat.java
+++ src/main/java/Lutchat.java
@@ -1,39 +1,54 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Lutchat {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    ArrayList<String> taskList = new ArrayList<>();
+    String userInput;
 
-        greet();
-
-        String userInput = sc.nextLine();
-        while (conversation(userInput)) {
-            userInput = sc.nextLine();
-        }
-
-        exit();
-    }
-
-    static void greet() {
+    void greet() {
         System.out.print("______________________________________________\n");
         System.out.print("Hello! I'm Lutchat!\n");
         System.out.print("What can I do for you?\n");
         System.out.print("______________________________________________\n");
     }
 
-    static void exit() {
+    void exit() {
         System.out.print("Bye! Hope to see you again soon!\n");
         System.out.print("______________________________________________\n");
     }
 
-    static boolean conversation(String userInput) {
+    boolean conversation(String userInput) {
+
         switch (userInput) {
             case "bye":
                 return false;
+            case "list":
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.print( (i+1) + ". " + taskList.get(i) + "\n");
+                }
+                System.out.print("______________________________________________\n");
+                return true;
             default:
-                System.out.print(userInput + "\n");
+                taskList.add(userInput);
+                System.out.print("added: " + userInput + "\n");
                 System.out.print("______________________________________________\n");
                 return true;
         }
+    }
+
+    public static void main(String[] args) {
+        Lutchat lutchat = new Lutchat();
+        Scanner sc = new Scanner(System.in);
+
+        lutchat.greet();
+
+        lutchat.userInput = sc.nextLine();
+
+        while (lutchat.conversation(lutchat.userInput)) {
+            lutchat.userInput = sc.nextLine();
+        }
+
+        lutchat.exit();
     }
 }
