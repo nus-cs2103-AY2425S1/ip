f7589b4c4fa1ae58023783e8e51537d2c347d09d
lim-jt <>
2024-08-23 11:35:12 +0800

    Rename and add greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class Dash {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //Greeting
+        String horizontalLine = "________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Dash.\nWhat can I do for you?");
+
+        //Exit
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
+
 }
