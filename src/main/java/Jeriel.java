7fd7b4d5c5af790cd20e470d60c4ace3bef9d9c2
Nimastic <jerielchanzy@gmail.com>
2024-08-23 08:15:06 +0800

    Level-3

--- src/main/java/Jeriel.java
+++ src/main/java/Jeriel.java
@@ -20,34 +20,57 @@ public class Jeriel {
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
 
         while (true) {
             input = scanner.nextLine(); 
-
             System.out.println("____________________________________________________________");
             System.out.println(" " + input);
             System.out.println("____________________________________________________________");
 
-            if (input.equals("bye")) {
 
+            if (input.equals("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
+
                 break;
+
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+
+
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                tasks[taskNumber].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("____________________________________________________________");
+
+
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                tasks[taskNumber].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskNumber]);
+                System.out.println("____________________________________________________________");
+
+
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
+
+                
             }
         }
 
