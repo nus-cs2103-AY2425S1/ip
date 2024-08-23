e46ce9030c1aa9d2aa26dca93e43863ba0add310
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-08-23 23:35:29 +0800

    Implement Level 1
    Chatbot echos commands entered by user, and exits when user types the command "bye".
    
    HAMYO logo changed to a minimalistic version.
    
    Added functions to printLine() and printLogo() to improve readability.

--- src/main/java/Hamyo.java
+++ src/main/java/Hamyo.java
@@ -1,22 +1,54 @@
+import java.util.Scanner;
+
 public class Hamyo {
 
-    private static String line = "_______________________________________________________\n";
-    private static String logo = " /$$   /$$\n" +
-        "| $$  | $$\n" +
-        "| $$  | $$  /$$$$$$  /$$$$$$/$$$$  /$$   /$$  /$$$$$$\n" +
-        "| $$$$$$$$ |____  $$| $$_  $$_  $$| $$  | $$ /$$__  $$\n" +
-        "| $$__  $$  /$$$$$$$| $$ \\ $$ \\ $$| $$  | $$| $$  \\ $$\n" +
-        "| $$  | $$ /$$__  $$| $$ | $$ | $$| $$  | $$| $$  | $$\n" +
-        "| $$  | $$|  $$$$$$$| $$ | $$ | $$|  $$$$$$$|  $$$$$$/\n" +
-        "|__/  |__/ \\_______/|__/ |__/ |__/ \\____  $$ \\______/\n" +
-        "                                   /$$  | $$\n" +
-        "                                  |  $$$$$$/\n" +
-        "                                   \\______/";
+    private static boolean active = true;
+    private static String[] items = new String[100];
+    private static int nItems = 0;
 
     public static void main(String[] args) {
 
-        System.out.printf(Hamyo.line + Hamyo.logo + "\nAnnyeonghaseyo! Hamyo here!\nHow may I assist you today?\n" + Hamyo.line);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Annyeong! Till we meet again. <3\n" + Hamyo.line);
+        greet();
+
+        while (active) {
+            String str = scanner.nextLine();
+            if (str.startsWith("bye")) {
+                terminate();
+            } else {
+                echo(str);
+            }
+        }
+    }
+
+    public static void printLine() {
+        System.out.println("_______________________________________________________");
+    }
+
+    private static void printLogo() {
+        System.out.println(
+            "$$   $$   $$$$    $$$$ $$$$   $$   $$  $$$$$$\n" +
+            "$$   $$  $$  $$  $$  $$$  $$  $$   $$  $$  $$\n" +
+            "$$$$$$$  $$$$$$  $$  $$$  $$  $$$$$$$  $$  $$\n" +
+            "$$   $$  $$  $$  $$  $$$  $$       $$  $$  $$\n" +
+            "$$   $$  $$  $$  $$  $$$  $$  $$$$$$   $$$$$$");
+    }
+    public static void greet() {
+        printLine();
+        printLogo();
+        System.out.println("\nAnnyeonghaseyo! Hamyo here!\nHow may I assist you today?");
+        printLine();
+    }
+
+    public static void terminate() {
+        active = false;
+        System.out.println("Annyeong! Till we meet again. <3");
+        printLine();
+    }
+
+    public static void echo(String str) {
+        System.out.println(str);
+        printLine();
     }
 }
