0433ab766752d393a384378f9f8d67b7e6a5d24f
brianchoon <brianchoonweihao@gmail.com>
2024-08-23 13:47:15 +0800

    Rename main class and add greeting and goodbye

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Devon User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Devon.java
@@ -0,0 +1,25 @@
+public class Devon {
+
+    private void printLongLine() {
+        String LINE_SEPARATOR = "____________________";
+        System.out.println(LINE_SEPARATOR);
+    }
+
+    private void introduction() {
+        printLongLine();
+        System.out.println("Hello! I'm Devon.");
+        System.out.println("What can I do for you?");
+        printLongLine();
+    }
+
+    private void goodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        printLongLine();
+    }
+
+    public static void main(String[] args) {
+        Devon bot = new Devon();
+        bot.introduction();
+        bot.goodbye();
+    }
+}
