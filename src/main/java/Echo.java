3530c8b12c29ae46b86f8d2b73b39a4a66ae1147
Teddayz <mervynneo02@gmail.com>
2024-08-23 14:11:51 +0800

    Improved Line Spacing within the chatbot

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -30,11 +30,11 @@ public class Echo {
                     String response = String.format("%d. %s", i + 1, task.getDescription());
                     System.out.println(response);
                 }
+                System.out.println(line());
                 break;
             case "mark":
                 if (parts.length < 2) {
-                    System.out.println("Please specify the task number to mark \n"
-                                +  "____________________________________________________________\n");
+                    System.out.println("Please specify the task number to mark \n" + line());
                 } else {
                     try {
                         int index = Integer.parseInt(parts[1]) - 1;
@@ -43,8 +43,7 @@ public class Echo {
                             t.markAsDone();
                             String response = String.format("%s", t.getDescription());
                             System.out.println("Nice I've marked this task as done: \n"
-                                    +  "____________________________________________________________\n"
-                                    + response);
+                                    + line() + response);
                         } else {
                             System.out.println("Invalid Task Number.\n");
                         }
@@ -53,12 +52,12 @@ public class Echo {
                         System.out.println("Please enter a valid task number\n");
                     }
                 }
+                System.out.print(line());
                 break;
 
             case "unmark":
                 if (parts.length < 2) {
-                    System.out.println("Please specify the task number to unmark \n"
-                            +  "____________________________________________________________\n");
+                    System.out.println("Please specify the task number to unmark \n" + line());
                 } else {
                     try {
                         int index = Integer.parseInt(parts[1]) - 1;
@@ -67,8 +66,7 @@ public class Echo {
                             t.unMark();
                             String response = String.format("%s", t.getDescription());
                             System.out.println("OK, I've marked this task as not done yet: \n"
-                                    +  "____________________________________________________________\n"
-                                    + response);
+                                    + line() + response);
                         } else {
                             System.out.println("Invalid Task Number.\n");
                         }
@@ -77,6 +75,7 @@ public class Echo {
                         System.out.println("Please enter a valid task number\n");
                     }
                 }
+                System.out.print(line());
                 break;
 
             case "todo":
@@ -84,6 +83,7 @@ public class Echo {
                     Task toDoTask = new ToDoTask(parts[1]);
                     list.add(toDoTask);
                     System.out.print("Got it. I've added this task: \n" + toDoTask.getDescription() +
+                            "\n" + line() +
                             String.format("Now you have %d tasks in the list\n", list.size()));
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.print("Oh no! Please input a ToDo description!");
@@ -91,17 +91,19 @@ public class Echo {
                 break;
 
             case "deadline":
-                int byIndex = parts[1].indexOf("/by");
-                if (byIndex == -1) {
+                try {
+                    int byIndex = parts[1].indexOf("/by");
+                    String desc = parts[1].substring(0, byIndex);
+                    String deadline = parts[1].substring(byIndex + 3);
+                    Task deadlineTask = new DeadlineTask(desc, deadline);
+                    list.add(deadlineTask);
+                    System.out.print("Got it. I've added this task: \n" + deadlineTask.getDescription() +
+                                    "\n" + line() + String.format("Now you have %d tasks in the list\n", list.size()));
+                } catch  (StringIndexOutOfBoundsException e) {
                     System.out.println("Incorrect format of adding deadline tasks. " +
                             "Use '/by to specify the deadline after the task description");
                 }
-                String desc = parts[1].substring(0, byIndex);
-                String deadline = parts[1].substring(byIndex + 3);
-                Task deadlineTask = new DeadlineTask(desc, deadline);
-                list.add(deadlineTask);
-                System.out.print("Got it. I've added this task: \n" + deadlineTask.getDescription() +
-                        String.format("Now you have %d tasks in the list\n", list.size()));
+
 
                 break;
             case "event":
@@ -117,8 +119,8 @@ public class Echo {
                 String end = parts[1].substring(toIndex + 3);
                 Task eventTask = new EventTask(details, start, end);
                 list.add(eventTask);
-                System.out.print("Got it. I've added this task: \n" + eventTask.getDescription() + "\n" +
-                                String.format("Now you have %d tasks in the list\n", list.size()));
+                System.out.print("Got it. I've added this task: \n" + eventTask.getDescription() +
+                            "\n" + line() + String.format("Now you have %d tasks in the list\n", list.size()));
                 break;
             case "delete":
                 int index = Integer.parseInt(parts[1]) - 1;
@@ -134,17 +136,17 @@ public class Echo {
                 break;
 
             case "bye":
-                System.out.println("Bye. Hope to see you again soon!\n"
-                                +  "____________________________________________________________\n");
+                System.out.println("Bye. Hope to see you again soon!\n" + line());
                 break;
             default:
-                System.out.println("Oh no! You have input an unknown command!");
+                System.out.println("Oh no! You have input an unknown command!" + line());
                 break;
         }
     }
     public void greet() {
-        System.out.print("____________________________________________________________\n"
-                + "Hello! I'm Meeks! Your friendly chatbot!\n"
-                + "What can I do for you?\n");
+        System.out.print(line() + "Hello! I'm Meeks! Your friendly chatbot!\n" + "What can I do for you?\n");
+    }
+    public String line() {
+        return "____________________________________________________________\n";
     }
 }
