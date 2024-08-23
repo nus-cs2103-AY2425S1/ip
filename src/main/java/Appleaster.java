8915d81bd963e6527bd4eff9b3a3a9d9e4dfade2
LOLIPOP-INTELLIGENCE <manas@sarvam.ai>
2024-08-23 10:28:41 +0800

    completes level 2 requirements

--- src/main/java/Appleaster.java
+++ src/main/java/Appleaster.java
@@ -14,6 +14,10 @@ public class Appleaster {
         "Farewell! I'll be here, hanging on my digital tree, waiting for your return!"
     };
 
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         String logo = "   _____                .__                          __                \n"
                     + "  /  _  \\ ______ ______ |  |   ____ _____    _______/  |_  ___________ \n"
@@ -40,17 +44,40 @@ public class Appleaster {
             }
             
             System.out.println("------------------------------------");
-            System.out.println("you said: " + input);
+            processInput(input);
             System.out.println("------------------------------------");
-            
-            if (input.equalsIgnoreCase("bye")) {
-                break;
-            }
         }
         System.out.println("------------------------------------");
         System.out.println(GOODBYES[random.nextInt(GOODBYES.length)]);
         System.out.println("------------------------------------");
         
         scanner.close();
+    }
+
+    private static void processInput(String input) {
+        if (input.equalsIgnoreCase("list")) {
+            listTasks();
+        } else {
+            addTask(input);
+        }
+    }
+
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount++] = task;
+            System.out.println("added: " + task);
+        } else {
+            System.out.println("Task list is full. Cannot add more tasks.");
+        }
+    }
+
+    private static void listTasks() {
+        if (taskCount == 0) {
+            System.out.println("No tasks in the list.");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
     }
 }
