6e64f665cc579f85360ef3ca7ecab002b4676065
Jollybomber <siahevan@gmail.com>
2024-08-23 16:21:40 +0800

    Feat: User can now add tasks and list tasks

--- src/main/java/Nave.java
+++ src/main/java/Nave.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 
 public class Nave {
+    private static final String greeting =
+            "Hello! :) I'm Nave, your personal task management assistant.\n" +
+            "What can I do for you today?";
+
+    private static final String farewell = "Goodbye :( Come visit me again";
     public static void main(String[] args) {
-        String greeting = """
-                -----------------------------------------------------------------
-                Hello! :) I'm Nave, your personal task management assistant.
-                What can I do for you today?
-                -----------------------------------------------------------------
-                """;
-        String farewell = """
-                -----------------------------------------------------------------
-                Goodbye :( Come visit me again!
-                -----------------------------------------------------------------
-                """;
-        System.out.println(greeting);
+        //Greet User
+        System.out.println(formatResponse(greeting));
+
         Scanner inputReader = new Scanner(System.in);
+        //Get user's input
         String userInput = inputReader.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(
-                    "-----------------------------------------------------------------\n"
-                    + userInput +
-                    "\n-----------------------------------------------------------------");
+            if (userInput.equals("list")) {
+                System.out.println(formatResponse(List.listItems()));
+            } else {
+                System.out.println(formatResponse(List.addItem(userInput)));
+            }
             userInput = inputReader.nextLine();
         }
-        System.out.println(farewell);
+        //Say goodbye
+        System.out.println(formatResponse(farewell));
+    }
+
+    private static String formatResponse(String input) {
+        return "-----------------------------------------------------------------\n"
+                + input + "\n"
+                + "-----------------------------------------------------------------";
     }
 }
