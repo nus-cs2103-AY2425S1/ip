178eb10103b9680b8647cb4fc0c4da0b40a8f5b6
Dino-Nuggies <heyiheng2130@gmail.com>
2024-08-23 14:24:38 +0800

    Remove Duke in place of Lemon
    Removed Duke
    Added Lemon

---
+++ src/main/java/Lemon.java
@@ -0,0 +1,13 @@
+public class Lemon {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + " Hello! I'm Lemon\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+        String end = " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.print(logo);
+        System.out.println(end);
+    }
+}
