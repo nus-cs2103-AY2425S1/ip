e76285bc77b9d9cea7e68ba707f4934d7e8ba63f
junyi73 <lerjunyi@gmail.com>
2024-08-23 09:47:48 +0800

    feat: update chatbot name and welcome message

---
+++ src/main/java/Noisy.java
@@ -0,0 +1,11 @@
+public class Noisy {
+    public static void main(String[] args) {
+        String string = "____________________________________________________________\n"
+                + " Hello! I'm Noisy\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(string);
+    }
+}
