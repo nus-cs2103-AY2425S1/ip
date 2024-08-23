2dd0fa7fd04ef149c9f8d743c04af769c36dd409
FooChao <foochao01@gmail.com>
2024-08-23 23:20:32 +0800

    Edited the print statement for unknow syntax of command to give expected command

--- src/main/java/WrongSyntaxForCommandException.java
+++ src/main/java/WrongSyntaxForCommandException.java
@@ -2,6 +2,11 @@ public class WrongSyntaxForCommandException extends Exception {
     public WrongSyntaxForCommandException(Command command) {
         super("Sumo understood your command but dunno what you want! Please utilise \""
                 + command
-                + "\" the correct way.");
+                + "\" the correct way."
+                + "\n"
+                + "The correct syntax is "
+                + command.expectedFormat()
+                + '.'
+                );
     }
 }
