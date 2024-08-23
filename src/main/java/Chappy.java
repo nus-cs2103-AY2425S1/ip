e304af082e2ced5d47bf741dd3c5af3213e7c702
RyanWarwick <spdshadow_w@yahoo.com>
2024-08-23 16:20:14 +0800

    Chappy.java: Add delete feature Chappy.java: Change userInputArray from Task array to ArrayList Chappy.java: Fix bug allowing for empty description and information (leading/trailing whitespace)

--- src/main/java/Chappy.java
+++ src/main/java/Chappy.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Chappy {
-
-    private static Task[] userInputArray = new Task[100];
+    
+    private static ArrayList<Task> userInputArray = new ArrayList<Task>();
     private static int userInputArrayPointer = 0;
 
     public static void main(String[] args) {
@@ -47,43 +48,42 @@ public class Chappy {
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < userInputArray.length; i++) {
-                    if (userInputArray[i] == null) {
-                        break;
-                    }
-                    System.out.println(i+1 + "." + userInputArray[i].toString());
+                for (int i = 0; i < userInputArray.size(); i++) {
+                    System.out.println(i+1 + "." + userInputArray.get(i).toString());
                 }
                 System.out.println("____________________________________________________________");
+
             } else if (userInput.toLowerCase().contains("unmark")) {
                 int t = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                userInputArray[t].markAsNotDone();
+                userInputArray.get(t).markAsNotDone();
                 System.out.println("____________________________________________________________");
                 System.out.println("Aww.. I've marked this task as not done yet..");
-                System.out.println(userInputArray[t].toString());
+                System.out.println(userInputArray.get(t).toString());
                 System.out.println("____________________________________________________________");
 
             }  else if (userInput.toLowerCase().contains("mark")) {
                 int t = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                userInputArray[t].markAsDone();
+                userInputArray.get(t).markAsDone();
                 System.out.println("____________________________________________________________");
                 System.out.println("Fantastic! I've marked this task as done!");
-                System.out.println(userInputArray[t].toString());
+                System.out.println(userInputArray.get(t).toString());
                 System.out.println("____________________________________________________________");
+
             } else if (userInput.toLowerCase().contains("todo")) {
-                String[] t = userInput.split("todo");
+                String[] t = userInput.trim().split("todo");
                 if (t.length < 2) {
                     System.out.println("Oh SIR! The description of a todo cannot be empty!");
                     continue;
                 }
                 Todo task = new Todo(t[1]);
-                userInputArray[userInputArrayPointer] = task;
-                userInputArrayPointer++;
+                userInputArray.add(task);
                 System.out.println("____________________________________________________________");
                 System.out.println("Alright sir! I've added this task:");
                 System.out.println(task.toString());
                 System.out.println("____________________________________________________________");
+
             } else if (userInput.toLowerCase().contains("deadline")) {
-                String[] t = userInput.split("deadline");
+                String[] t = userInput.trim().split("deadline");
                 if (t.length < 2) {
                     System.out.println("Oh SIR! The description of a deadline cannot be empty!");
                     continue;
@@ -94,14 +94,14 @@ public class Chappy {
                     continue;
                 }
                 Deadline task = new Deadline(t2[0], t2[1]);
-                userInputArray[userInputArrayPointer] = task;
-                userInputArrayPointer++;
+                userInputArray.add(task);
                 System.out.println("____________________________________________________________");
                 System.out.println("Alright sir! I've added this task:");
                 System.out.println(task.toString());
                 System.out.println("____________________________________________________________");
+
             } else if (userInput.toLowerCase().contains("event")) {
-                String[] t = userInput.split("event");
+                String[] t = userInput.trim().split("event");
                 if (t.length < 2) {
                     System.out.println("____________________________________________________________");
                     System.out.println("Oh SIR! The description of an event cannot be empty!");
@@ -119,18 +119,27 @@ public class Chappy {
                 String[] t3 = t2[1].split("/to");
                 if (t3.length < 2) {
                     System.out.println("____________________________________________________________");
-                    System.out.println("Oh SIR! The \"by\" description of an event cannot be empty!");
+                    System.out.println("Oh SIR! The \"to\" description of an event cannot be empty!");
                     System.out.println("____________________________________________________________");
                     continue;
                 }
 
                 Event task = new Event(t2[0], t3[0], t3[1]);
-                userInputArray[userInputArrayPointer] = task;
-                userInputArrayPointer++;
+                userInputArray.add(task);
                 System.out.println("____________________________________________________________");
                 System.out.println("Alright sir! I've added this task:");
                 System.out.println(task.toString());
                 System.out.println("____________________________________________________________");
+
+            } else if (userInput.toLowerCase().contains("delete")) {
+                int t = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                Task task = userInputArray.get(t);
+                userInputArray.remove(t);
+                System.out.println("____________________________________________________________");
+                System.out.println("Unfortunate.. I'll remove this task from the list..");
+                System.out.println(task.toString());
+                System.out.println("____________________________________________________________");
+
             } else if (userInput != "") {
                 System.out.println("____________________________________________________________");
                 System.out.println("Oh SIR! I can't understand what you are saying!");
