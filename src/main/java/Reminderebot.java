8c40f057d4049b8f74cf4b3d3ae08562b3e4686b
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-08-23 09:52:42 +0800

    Add Level 1 Edit Reminderebot class: Echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Reminderebot.java
+++ src/main/java/Reminderebot.java
@@ -1,17 +1,33 @@
-public class    Reminderebot {
+import java.util.Scanner;
+public class Reminderebot {
     private static String greetingText = "____________________________________________________________\n" +
             " Hello! I'm [Reminderebot]\n" +
             " What can I do for you?\n" +
             "____________________________________________________________\n";
 
-    private static String goodbyeText = " Bye. Hope to see you again soon!\n" +
+    private static String goodbyeText = "____________________________________________________________\n" +
+            " Bye. Hope to see you again soon!\n" +
             "____________________________________________________________";
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         greeting();
+        while (true) {
+            System.out.println("");
+            String command = sc.nextLine();
+            if (command.equals("bye")) break;
+            System.out.println("____________________________________________________________\n" +
+                    command + "\n____________________________________________________________"
+            );
+        }
+        goodbye();
     }
 
     public static void greeting() {
-        System.out.println(greetingText + goodbyeText);
+        System.out.println(greetingText);
+    }
+
+    public static void goodbye() {
+        System.out.println(goodbyeText);
     }
 }
