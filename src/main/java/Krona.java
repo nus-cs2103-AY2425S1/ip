3aeb541f3635d24883d0b72d998f039e5c33b53f
The0nlyJuan <ongjuanwen97@gmail.com>
2024-08-23 11:55:48 +0800

    Add command echo and exit

--- src/main/java/Krona.java
+++ src/main/java/Krona.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 public class Krona {
     public static void main(String[] args) {
-        System.out.println("__________________________\n" +
-                "Hello! I'm Krona\n" +
-                "What can i Do for you?\n" +
-                "__________________________\n" +
-                "Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Krona\n" +
+                "What can i Do for you?");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
