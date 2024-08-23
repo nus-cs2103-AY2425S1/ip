5b29acb36a383cc89e777b3f2925948f0a128b13
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-08-23 10:24:30 +0800

    Add Level 2 Reminderebot class: Add the ability to store whatever text entered by the user and display them back to the user when requested add printTasks() for showing tasklist add addTasks() for saving tasklist

--- src/main/java/Reminderebot.java
+++ src/main/java/Reminderebot.java
@@ -1,33 +1,64 @@
 import java.util.Scanner;
 public class Reminderebot {
-    private static String greetingText = "____________________________________________________________\n" +
+    private String[] tasks = new String[100];
+    private static final String topBuffer = "____________________________________________________________\n";
+    private static final String bottomBuffer = "____________________________________________________________";
+    static int index = 0;
+    private static final String greetingText = topBuffer +
             " Hello! I'm [Reminderebot]\n" +
             " What can I do for you?\n" +
-            "____________________________________________________________\n";
+            topBuffer;
 
-    private static String goodbyeText = "____________________________________________________________\n" +
+    private static final String goodbyeText = topBuffer +
             " Bye. Hope to see you again soon!\n" +
-            "____________________________________________________________";
+            bottomBuffer;
 
     public static void main(String[] args) {
+        Reminderebot reminderebot = new Reminderebot();
         Scanner sc = new Scanner(System.in);
-        greeting();
+        reminderebot.greeting();
         while (true) {
             System.out.println("");
             String command = sc.nextLine();
+            switch (command) {
+                case "bye":
+                    reminderebot.goodbye();
+                    break;
+                case "list":
+                    reminderebot.printTasks();
+                    break;
+                default:
+                    reminderebot.addTasks(command);
+                    System.out.println("____________________________________________________________\n" +
+                            "added: " + command +
+                            "\n____________________________________________________________"
+                    );
+            }
             if (command.equals("bye")) break;
-            System.out.println("____________________________________________________________\n" +
-                    command + "\n____________________________________________________________"
-            );
         }
-        goodbye();
     }
 
-    public static void greeting() {
+    private void greeting() {
         System.out.println(greetingText);
     }
 
-    public static void goodbye() {
+    private void goodbye() {
         System.out.println(goodbyeText);
+    }
+
+    private void printTasks() {
+        StringBuilder output = new StringBuilder();
+        output.append(topBuffer);
+        for (int i=0; i<index; i++) {
+            output.append(tasks[i]).append("\n");
+        }
+        output.append(bottomBuffer);
+        String taskList = output.toString();
+        System.out.println(taskList);
+    }
+
+    private void addTasks(String task) {
+        tasks[index] = (index+1) + ". " + task;
+        index++;
     }
 }
