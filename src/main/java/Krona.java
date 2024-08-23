dbdb0b3346df98fe53d36295372c779e09dbb7c9
The0nlyJuan <ongjuanwen97@gmail.com>
2024-08-23 12:11:32 +0800

    Add text storing and displaying

--- src/main/java/Krona.java
+++ src/main/java/Krona.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Krona {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int allTasks = 0;
+
         System.out.println("Hello! I'm Krona\n" +
                 "What can i Do for you?");
 
@@ -10,8 +13,14 @@ public class Krona {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < allTasks; i++ ) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(input);
+                tasks[allTasks] = input;
+                allTasks++;
+                System.out.println("added: " + input);
             }
         }
     }
