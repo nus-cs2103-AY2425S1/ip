1533041979569458979177723720a8b26a3412fe
neohengkai <neohengkai2002@gmail.com>
2024-08-23 14:39:01 +0800

    Level-4: ToDo, Event, Deadline

--- src/main/java/Ontos.java
+++ src/main/java/Ontos.java
@@ -9,6 +9,7 @@ public class Ontos {
     public static String listPrompt = " Here are the tasks in your list:";
     public static String completeTaskPrompt = " Nice! I've marked this task as done:\n   ";
     public static String uncompleteTaskPrompt = " OK, I've marked this task as not done yet:\n   ";
+    public static String taskAdded = " Got it. I've added this task:\n";
 
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
@@ -41,16 +42,14 @@ public class Ontos {
                 try {
                     index = Integer.parseInt(input.split(" ")[1]) - 1;
                 } catch (Exception e) {
-                    inputs[elements] = new Task(input);
-                    elements++;
-                    System.out.println(line + " added: " + input + "\n" + line);
+                    System.out.println(line + "The correct usage of 'mark' is: mark n, where n is a natural number (ℕ)." + line);
                     continue;
                 }
 
                 try {
                     inputs[index].completeTask();
                 } catch (Exception e) {
-                    System.out.println(line + "I'm sorry this task does not exist.\n" + line);
+                    System.out.println(line + "I'm sorry, but this task doesn't exist.\n" + line);
                     continue;
                 }
                 System.out.println(line + completeTaskPrompt + inputs[index].toString() + "\n" + line); 
@@ -59,23 +58,75 @@ public class Ontos {
                 try {
                     index = Integer.parseInt(input.split(" ")[1]) - 1;
                 } catch (Exception e) {
-                    inputs[elements] = new Task(input);
-                    elements++;
-                    System.out.println(line + " added: " + input + "\n" + line);
+                    System.out.println(line + "To use 'unmark' correctly, you should format it as: unmark n, where n belongs to the set of natural numbers (ℕ)." + line);
                     continue;
                 }
 
                 try {
                     inputs[index].uncompleteTask();
                 } catch (Exception e) {
-                    System.out.println(line + "I'm sorry this task does not exist.\n" + line);
+                    System.out.println(line + "I'm sorry, but this task doesn't exist.\n" + line);
                     continue;
                 }
                 System.out.println(line + uncompleteTaskPrompt + inputs[index].toString() + "\n" + line); 
-            } else {
-                inputs[elements] = new Task(input);
+            } else if (input.startsWith("todo")) {
+                try {
+                    inputs[elements] = Task.toDo(input.split(" ", 2)[1]);
+                } catch (Exception e) {
+                    System.out.println(line + " OOPS!!! The description of a todo cannot be empty." + line);
+                    continue;
+                }
+
                 elements++;
-                System.out.println(line + " added: " + input + "\n" + line);
+                System.out.println(line 
+                + taskAdded 
+                + " " + inputs[elements - 1].toString() + "\n" 
+                + " Now you have" + elements + " tasks in the list.\n" 
+                + line);
+            } else if (input.startsWith("deadline")) {
+                try {
+                    int startOfDesc = input.indexOf(" ");
+                    int endOfDesc = input.indexOf(" /by");
+
+                    String description = input.substring(startOfDesc, endOfDesc).trim();
+                    String dueBy = input.substring(endOfDesc + 4).trim();
+
+                    inputs[elements] = Task.deadline(description, dueBy);
+                } catch (Exception e) {
+                    System.out.println(line + " OOPS!!! The description of a deadline cannot be empty." + line);
+                    continue;
+                }
+
+                elements++;
+                System.out.println(line 
+                + taskAdded 
+                + " " + inputs[elements - 1].toString() + "\n" 
+                + " Now you have" + elements + " tasks in the list.\n" 
+                + line);
+            } else if (input.startsWith("event")) {
+                try {
+                    int startOfDesc = input.indexOf(" ");
+                    int endOfDesc = input.indexOf(" /from");
+                    int endOfFrom = input.indexOf(" /to");
+
+                    String description = input.substring(startOfDesc, endOfDesc).trim();
+                    String start = input.substring(endOfDesc + 6, endOfFrom).trim();
+                    String end = input.substring(endOfFrom + 4).trim();
+
+                    inputs[elements] = Task.event(description, start, end);
+                } catch (Exception e) {
+                System.out.println(line + " OOPS!!! The description of a event cannot be empty." + line);
+                continue;
+                }
+
+                elements++;
+                System.out.println(line 
+                + taskAdded 
+                + " " + inputs[elements - 1].toString() + "\n" 
+                + " Now you have" + elements + " tasks in the list.\n" 
+                + line);
+            } else {
+                System.out.println("placeholder text before level-5");
             }
         }
         sc.close();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,22 @@
-public class Task {
+public abstract class Task {
   protected String description;
   protected Boolean isDone;
 
   public Task(String description) {
       this.description = description;
       this.isDone = false;
+  }
+
+  public static Task toDo(String description) {
+    return new ToDo(description);
+  }
+
+  public static Task deadline(String description, String time) {
+    return new Deadline(description, time);
+  }
+
+  public static Task event(String description, String start, String end) {
+    return new Event(description, start, end);
   }
 
   public String getStatusIcon() {
@@ -22,5 +34,50 @@ public class Task {
   public String toString() {
     String statusIcon = this.getStatusIcon();
     return "[" + statusIcon + "] " + this.description;
+  }
+
+  protected static class ToDo extends Task {
+    public ToDo(String description) {
+      super(description);
+    }
+
+    @Override
+    public String toString() {
+      String body = super.toString();
+      return "[T]" + body;
+    }
+  }
+  
+  protected static class Deadline extends Task {
+    
+    protected String time;
+
+    public Deadline(String description, String time) {
+      super(description);
+      this.time = time;
+    }
+    
+    @Override
+    public String toString() {
+      String body = super.toString();
+      return "[D]" + body + " (by: " + time + ")";
+    }
+  }
+
+  protected static class Event extends Task {
+    protected String start;
+    protected String end;
+
+    public Event(String description, String start, String end) {
+      super(description);
+      this.start = start;
+      this.end = end;
+    }
+
+    @Override
+    public String toString() {
+      String body = super.toString();
+      return "[E]" + body + " (from: " + start + " to: " + end + ")";
+    }
   }
 }
