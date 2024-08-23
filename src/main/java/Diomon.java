e0f197bb144b4af0267f1390bdab04cbaedcc452
EyuGongYi <145592984+EyuGongYi@users.noreply.github.com>
2024-08-23 18:45:30 +0800

    Add Diomon.java, Changed README.md

---
+++ src/main/java/Diomon.java
@@ -0,0 +1,17 @@
+public class Diomon {
+    public static void Greeting() {
+        String greetingMessage = """
+                           ________________________________________________________________
+                           Hello! I'm Diomon\n
+                           What can I do for you?
+                           
+                           ________________________________________________________________
+                           Bye. Hope to see you again soon!
+                           """;
+        System.out.println(greetingMessage);
+    }
+
+    public static void main(String[] args) {
+        Greeting();
+    }
+}
