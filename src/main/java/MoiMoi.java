3248bdac814f7fd4a6b0aa0a8881f6f60ef72dc3
wkxcass <e1129889@u.nus.edu>
2024-08-24 11:34:25 +0800

    MoiMoi.java: utilize Parser class and Command objects
    MoiMoi.java contains both input parsing logic and the logic for
    executing all operations.
    
    This makes the codebase difficult to maintain and extend.
    
    Let's,
    * refactor "Parser" methods to convert user input directly into
    executable Command objects
    * remove parsing and command execution logic from MoiMoi.java, while
    delegating these responsibilities to both "Parser" and "Command" classes
    
    This simplifies MoiMoi.java and improves maintainability with a better
    separation of concerns.

--- src/main/java/MoiMoi.java
+++ src/main/java/MoiMoi.java
@@ -1,7 +1,3 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
 public class MoiMoi {
@@ -24,153 +20,22 @@ public class MoiMoi {
     public void run() {
 
         this.ui.showGreeting();
+        boolean isExit = false;
 
-        this.ui.showUserHeader();
-        String input = this.ui.getInput();
-        String command = Parser.inputToCommand(input);
-
-        while (!command.equals("bye")) {
+        while (!isExit) {
             try {
+                this.ui.showUserHeader();
+                String input = this.ui.getInput();
+                
                 this.ui.showMoiMoiHeader();
-                switch (command) {
-                case "todo":
-                    this.todo(Parser.inputToArgs(input));
-                    break;
-                case "deadline":
-                    this.deadline(Parser.inputToArgs(input));
-                    break;
-                case "event":
-                    this.event(Parser.inputToArgs(input));
-                    break;
-                case "delete":
-                    this.delete(Parser.inputToArgs(input));
-                    break;
-                case "mark":
-                    this.mark(Parser.inputToArgs(input));
-                    break;
-                case "unmark":
-                    this.unmark(Parser.inputToArgs(input));
-                    break;
-                case "list":
-                    this.list();
-                    break;
-                case "filter":
-                    this.filter(Parser.inputToArgs(input));
-                    break;
-                default:
-                    throw new InvalidCommandException();
-                }
+                Command command = Parser.parse(input);
+                command.execute(this.storage, this.tasks, this.ui);
+                isExit = command.isExit();
             } catch (MoiMoiException e) {
-                this.ui.showMoiMoiException(e);
+                ui.showMoiMoiException(e);
             }
-            this.ui.showUserHeader();
-            input = this.ui.getInput();
-            command = Parser.inputToCommand(input);
         }
 
-        try {
-            storage.save(tasks);
-        } catch (StorageIOException e) {
-            this.ui.showMoiMoiException(e);
-        }
-
-        this.ui.showExitMessage();
-
-    }
-
-    public void todo(String description) {
-        Todo task = new Todo(description);
-        this.tasks.add(task);
-        this.ui.showCompletionMessage("Aight! Todo task added: " + task.stringUI()
-                + "\nWe have " + tasks.size() + " tasks in the bag~");
-    }
-
-    public void deadline(String description) throws MoiMoiException {
-        try {
-            String[] descBy = description.split(" /by ", 2);
-            String desc = descBy[0];
-            String byString = descBy[1];
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            LocalDateTime by = LocalDateTime.parse(byString, formatter);
-            Deadline task = new Deadline(desc, by);
-
-            this.tasks.add(task);
-            this.ui.showCompletionMessage("Got it! Deadline task added: " + task.stringUI()
-                    + "\nWe have " + tasks.size() + " tasks in the bag~");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new MissingArgumentException();
-        } catch (DateTimeParseException e) {
-            throw new InvalidDateTimeException("date-time");
-        }
-    }
-
-    public void event(String description) throws MoiMoiException {
-        try {
-            String[] descFromTo = description.split(" /from ", 2);
-            String desc = descFromTo[0];
-            String[] fromTo = descFromTo[1].split(" /to ", 2);
-            String fromString = fromTo[0];
-            String toString = fromTo[1];
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            LocalDateTime from = LocalDateTime.parse(fromString, formatter);
-            LocalDateTime to = LocalDateTime.parse(toString, formatter);
-            Event task = new Event(desc, from, to);
-
-            this.tasks.add(task);
-            this.ui.showCompletionMessage("Here you go! Event task added: " + task.stringUI()
-                    + "\nWe have " + tasks.size() + " tasks in the bag~");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new MissingArgumentException();
-        } catch (DateTimeParseException e) {
-            throw new InvalidDateTimeException("date-time");
-        }
-    }
-
-    public void delete(String index) throws InvalidIndexException {
-        try {
-            int i = Integer.parseInt(index);
-            Task task = tasks.get(i);
-            tasks.delete(i);
-            this.ui.showCompletionMessage("Aju nice! I've got rid of this task: " + task.stringUI()
-                    + "\nWe have " + tasks.size() + " tasks left in the bag~");
-        } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            throw new InvalidIndexException();
-        }
-    }
-
-    public void mark(String index) throws InvalidIndexException {
-        try {
-            Task task = tasks.get(Integer.parseInt(index));
-            task.mark();
-            this.ui.showCompletionMessage("YAY!! One down!!\n" + task.stringUI());
-        } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            throw new InvalidIndexException();
-        }
-    }
-
-    public void unmark(String index) throws InvalidIndexException {
-        try {
-            Task task = tasks.get(Integer.parseInt(index));
-            task.unmark();
-            this.ui.showCompletionMessage("Oof, it's OK! Let's get it done soon ;)\n" + task.stringUI());
-        } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            throw new InvalidIndexException();
-        }
-    }
-
-    public void list() {
-        this.ui.showList(tasks);
-    }
-
-    public void filter(String dateString) throws InvalidDateTimeException {
-        try {
-            LocalDate date = LocalDate.parse(dateString);
-            this.ui.showList(tasks, date);
-        } catch (DateTimeParseException e) {
-            throw new InvalidDateTimeException("date");
-        }
     }
 
     public static void main(String[] args) {
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,15 +1,46 @@
 public class Parser {
 
-    public static String inputToCommand(String input) {
-       return input.split(" ", 2)[0];
-    }
+    public static Command parse(String input) throws MoiMoiException {
+        String[] commandArgs = input.split(" ", 2);
+        String commandString = commandArgs[0];
+        Command command;
 
-    public static String inputToArgs(String input) throws MissingArgumentException{
         try {
-            return input.split(" ", 2)[1];
+            switch (commandString) {
+            case "todo":
+            case "deadline":
+            case "event":
+                command = new AddCommand(commandString, Parser.extractArgs(commandArgs));
+                break;
+            case "delete":
+                command = new DeleteCommand(Parser.extractArgs(commandArgs));
+                break;
+            case "mark":
+                command = new MarkCommand(Parser.extractArgs(commandArgs));
+                break;
+            case "unmark":
+                command = new UnmarkCommand(Parser.extractArgs(commandArgs));
+                break;
+            case "list":
+                command = new ListCommand();
+                break;
+            case "filter":
+                command = new FilterCommand(Parser.extractArgs(commandArgs));
+                break;
+            case "bye":
+                command = new ExitCommand();
+                break;
+            default:
+                throw new InvalidCommandException();
+            }
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new MissingArgumentException();
         }
+        return command;
+    }
+
+    private static String extractArgs(String[] commandArgs) {
+        return commandArgs[1];
     }
 
 }
