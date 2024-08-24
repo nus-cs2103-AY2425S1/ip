3e4d0aeff81ebba54e7c54ff511cfcee67a03d1f
lutfir-cpu <lutfir.rn@gmail.com>
2024-08-24 12:55:47 +0800

    added error handling and delete functionality

--- src/main/java/Lutchat.java
+++ src/main/java/Lutchat.java
@@ -18,6 +18,24 @@ public class Lutchat {
         System.out.print("______________________________________________\n");
     }
 
+    boolean invalidInputResponse(String message) {
+        System.out.print("OOPS!!! " + message +"\n");
+        System.out.print("______________________________________________\n");
+        return true;
+    }
+
+    boolean isInteger(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.valueOf(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     boolean conversation(String userInput) {
         String[] userInputArr = userInput.split(" ");
         String keyword = userInputArr[0];
@@ -27,13 +45,21 @@ public class Lutchat {
             case "bye":
                 return false;
             case "list":
-                System.out.print("Here are the tasks in yout list:\n");
+                System.out.print("Here are the task(s) in your list:\n");
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.print( (i+1) + "." + taskList.get(i).toString() + "\n");
                 }
                 System.out.print("______________________________________________\n");
                 return true;
             case "mark":
+                if (userInputArr.length < 2) {
+                    return invalidInputResponse("Please indicate which task you would like to mark.");
+                } else if (userInputArr.length > 2 || !isInteger(userInputArr[1])) {
+                    return invalidInputResponse("Please indicate an integer for which task you would like to mark.");
+                } else if (Integer.valueOf(userInputArr[1]) - 1 < 0
+                        || Integer.valueOf(userInputArr[1]) - 1 > taskList.size()) {
+                    return invalidInputResponse("Task does not exist, please try again!");
+                }
                 int itemNum1 = Integer.valueOf(userInputArr[1]) - 1;
                 taskList.get(itemNum1).markAsDone();
                 System.out.print("Nice! I've marked this task as done:\n");
@@ -41,10 +67,34 @@ public class Lutchat {
                 System.out.print("______________________________________________\n");
                 return true;
             case "unmark":
+                if (userInputArr.length < 2) {
+                    return invalidInputResponse("Please indicate which task you would like to unmark.");
+                } else if (userInputArr.length > 2 || !isInteger(userInputArr[1])) {
+                    return invalidInputResponse("Please indicate an integer for which task you would like to unmark.");
+                } else if (Integer.valueOf(userInputArr[1]) - 1 < 0
+                        || Integer.valueOf(userInputArr[1]) - 1 > taskList.size()) {
+                    return invalidInputResponse("Task does not exist, please try again!");
+                }
                 int itemNum2 = Integer.valueOf(userInputArr[1]) - 1;
                 taskList.get(itemNum2).markAsUndone();
                 System.out.print("OK, I've marked this task as not done yet:\n");
                 System.out.print(taskList.get(itemNum2).toString() + "\n");
+                System.out.print("______________________________________________\n");
+                return true;
+            case "delete":
+                if (userInputArr.length < 2) {
+                    return invalidInputResponse("Please indicate which task you would like to delete.");
+                } else if (userInputArr.length > 2 || !isInteger(userInputArr[1])) {
+                    return invalidInputResponse("Please indicate an integer for which task you would like to delete.");
+                } else if (Integer.valueOf(userInputArr[1]) - 1 < 0
+                        || Integer.valueOf(userInputArr[1]) - 1 > taskList.size()) {
+                    return invalidInputResponse("Task does not exist, please try again!");
+                }
+                int itemNum3 = Integer.valueOf(userInputArr[1]) - 1;
+                System.out.print("Noted. I've removed this task:\n");
+                System.out.print(taskList.get(itemNum3).toString() + "\n");
+                taskList.remove(itemNum3);
+                System.out.print("Now you have " + taskList.size() + " task(s) in the list.\n");
                 System.out.print("______________________________________________\n");
                 return true;
             case "todo":
@@ -52,14 +102,19 @@ public class Lutchat {
                 for (int i = 1; i < userInputArr.length; i++) {
                     desc += userInputArr[i] + " ";
                 }
-                desc = desc.substring(0, desc.length()-1);
+
+                desc = desc.substring(0, desc.equals("") ? 0 : desc.length()-1);
+
+                if (desc.length() == 0) {
+                    return invalidInputResponse("Todo 'description' is missing...");
+                }
 
                 Todo todo = new Todo(desc);
                 taskList.add(todo);
 
                 System.out.print("Got it. I've added this task:\n");
                 System.out.print(todo + "\n");
-                System.out.print("Now you have " + taskList.size() + " task in the list.\n");
+                System.out.print("Now you have " + taskList.size() + " task(s) in the list.\n");
                 System.out.print("______________________________________________\n");
                 return true;
             case "deadline":
@@ -70,19 +125,23 @@ public class Lutchat {
                         for (int j = i + 1; j < userInputArr.length; j++) {
                             by += userInputArr[j] + " ";
                         }
-                        by = by.substring(0, by.length()-1);
+                        by = by.substring(0, by.equals("") ? 0 : by.length()-1);
                         break;
                     }
                     desc1 += userInputArr[i] + " ";
                 }
-                desc1 = desc1.substring(0, desc1.length()-1);
+                desc1 = desc1.substring(0, desc1.equals("") ? 0 : desc1.length()-1);
+
+                if (desc1.length() == 0 || by.length() == 0) {
+                    return invalidInputResponse("Deadline 'description' or 'by' input(s) is/are missing...");
+                }
 
                 Deadline deadline = new Deadline(desc1, by);
                 taskList.add(deadline);
 
                 System.out.print("Got it. I've added this task:\n");
                 System.out.print(deadline + "\n");
-                System.out.print("Now you have " + taskList.size() + " task in the list.\n");
+                System.out.print("Now you have " + taskList.size() + " task(s) in the list.\n");
                 System.out.print("______________________________________________\n");
                 return true;
             case "event":
@@ -96,31 +155,32 @@ public class Lutchat {
                                 for (int k = j + 1; k < userInputArr.length; k++) {
                                     to += userInputArr[k] + " ";
                                 }
-                                to = to.substring(0, to.length()-1);
+                                to = to.substring(0, to.equals("") ? 0 : to.length()-1);
                                 break;
                             }
                             from += userInputArr[j] + " ";
                         }
-                        from = from.substring(0, from.length()-1);
+                        from = from.substring(0, from.equals("") ? 0 : from.length()-1);
                         break;
                     }
                     desc2 += userInputArr[i] + " ";
                 }
-                desc2 = desc2.substring(0, desc2.length()-1);
+                desc2 = desc2.substring(0, desc2.equals("") ? 0 : desc2.length()-1);
+
+                if (desc2.length() == 0 || from.length() == 0 || to.length() == 0) {
+                    return invalidInputResponse("Event 'description', 'from' or 'to' input(s) is/are missing...");
+                }
 
                 Event event = new Event(desc2, from, to);
                 taskList.add(event);
 
                 System.out.print("Got it. I've added this task:\n");
                 System.out.print(event + "\n");
-                System.out.print("Now you have " + taskList.size() + " task in the list.\n");
+                System.out.print("Now you have " + taskList.size() + " task(s) in the list.\n");
                 System.out.print("______________________________________________\n");
                 return true;
             default:
-                taskList.add(new Task(userInput));
-                System.out.print("added: " + userInput + "\n");
-                System.out.print("______________________________________________\n");
-                return true;
+                return invalidInputResponse("I don't know what that means... :(");
         }
     }
 
