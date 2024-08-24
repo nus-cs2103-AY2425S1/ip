1c417dc61cd776bb5b45adc8488c27ed38029b28
Jaehyeok Choi <77jack0105@gmail.com>
2024-08-24 17:56:14 +0800

    Add BlacknutExceptions.java

---
+++ src/main/java/BlacknutExceptions.java
@@ -0,0 +1,27 @@
+
+public class BlacknutExceptions {
+
+    public static class InvalidCommandException extends Exception {
+        public InvalidCommandException(String message) {
+            super(message);
+        }
+    }
+
+    public static class EmptyDescriptionException extends Exception {
+        public EmptyDescriptionException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidTaskNumberException extends Exception {
+        public InvalidTaskNumberException(String message) {
+            super(message);
+        }
+    }
+
+    public static class IncorrectFormatException extends Exception {
+        public IncorrectFormatException(String message) {
+            super(message);
+        }
+    }
+}
