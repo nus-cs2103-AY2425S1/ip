a60ca91dfc304ca7ac5438ce4b886d8471c4f2c9
KrashKart <e1113755@u.nus.edu>
2024-08-24 20:32:26 +0800

    More OOP: Add new class files
    Previously, Jackson.java handled most of the functionality for the
    chatbot (eg. User Interaction and Save File input-output), which
    made the file messy and convoluted.
    
    In addition, TaskList.java also had untidy code due to placing too
    much functionality in one file. Overall code was not segmented well
    in accordance to OOP principles.
    
    Let's do a few changes:
    * Create new Ui class to handle printing output and exceptions
    * Transfer printing responsibility from Jackson to Ui
    * Create new Storage class to handle save file input-output
    * Transfer read-write duties from TaskList to Storage
    * Change Jackson from static to instance class
    * Separate main and run methods in Jackson, so now main creates
    instance of Jackson and runs the main loop instead
    * Other code standard changes (naming, etc)

---
+++ src/main/java/Storage.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class to handle reading and writing to save file.
+ */
+public class Storage {
+    /* Regex to read tasks from text file */
+    private final Pattern TASK_FORMAT = Pattern.compile("\\[([TED])]\\[([X ])] " +
+            "([\\w ]+\\w)(?: \\((?:from|by): ([\\w\\d, :]+)\\)*)?(?: \\| to: ([\\w\\d, :]+)\\))?");
+
+    /* String path to save file */
+    private String path;
+
+    public Storage(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Saves task list to specified save file.
+     * if IOException raised, will print error statement.
+     * @param taskList{@code TaskList} object that the tasks are written from
+     */
+    public void save(TaskList taskList) {
+        File f = new File(this.path);
+        try {
+            f.createNewFile();
+            FileWriter fw = new FileWriter(f);
+            for (int i = 0; i < taskList.getSize(); i++) {
+                fw.write(taskList.getTask(i).toString());
+                if (i < taskList.getSize() - 1) {
+                    fw.write("\n");
+                }
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error! Please check your file permissions!");
+        }
+    }
+
+    /**
+     * Loads task list from save file.
+     * Will skip tasks if task format is wrong, will not load the full list if save file cannot be opened.
+     * @param taskList {@code TaskList} object to load tasks into
+     */
+    public void load(TaskList taskList) {
+        File f = new File(this.path);
+        Matcher m;
+        Task t;
+        try {
+            Scanner sc = new Scanner(f);
+            while (sc.hasNextLine()) {
+                m = TASK_FORMAT.matcher(sc.nextLine());
+                if (m.matches()) {
+                    switch (m.group(1)) {
+                    case "T":
+                        t = new Todo(m.group(3));
+                        if (m.group(2).equals("X")) {
+                            t.mark();
+                        }
+                        taskList.addTask(t);
+                        break;
+                    case "D":
+                        t = new Deadline(m.group(3), m.group(4));
+                        if (m.group(2).equals("X")) {
+                            t.mark();
+                        }
+                        taskList.addTask(t);
+                        break;
+                    case "E":
+                        t = new Event(m.group(3), m.group(4), m.group(5));
+                        if (m.group(2).equals("X")) {
+                            t.mark();
+                        }
+                        taskList.addTask(t);
+                        break;
+                    }
+                } else {
+                    System.out.println("Error with loading task, skipping to next one...");
+                }
+            }
+            sc.close();
+            System.out.println("Tasks loaded successfully!");
+        } catch (IOException e) {
+            System.out.printf("IOException! %s! Aborting loading...\n", e);
+        } catch (Exception e) {
+            System.out.printf("Unknown Error! %s! Aborting loading...\n", e);
+        }
+    }
+}
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,10 +1,4 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Class used to store tasks.
@@ -12,13 +6,6 @@ import java.util.regex.Pattern;
 public class TaskList {
     /* ArrayList to store tasks */
     private ArrayList<Task> tasks;
-
-    /* Path to save list data */
-    private final String PATH = "src/main/java/data.txt";
-
-    /* Regex to read tasks from text file */
-    private final Pattern TASK_FORMAT = Pattern.compile("\\[([TED])]\\[([X ])] " +
-            "([\\w ]+\\w)(?: \\((?:from|by): ([\\w\\d, :]+)\\)*)?(?: \\| to: ([\\w\\d, :]+)\\))?");
 
     /**
      * Constructor for task list.
@@ -29,136 +16,82 @@ public class TaskList {
     }
 
     /**
+     * Returns number of tasks in the list
+     * @return integer containing how many tasks are in the list
+     */
+    public int getSize() {
+        return this.tasks.size();
+    }
+
+    /**
      * Adds task to the list and prints list adding message.
-     * @param task Task object to be added
+     * @param task {@code Task} object to be added
      */
     public void addTask(Task task) {
-        System.out.println("Ya la, adding this task to your list!");
         this.tasks.add(task);
-        System.out.printf("\t%s\n", task);
-        System.out.printf("You now got %s tasks in your list leh\n", this.tasks.size());
-        save();
+    }
+
+    /**
+     * Gets a specified task according to index
+     * @param index index of task (from 0) to get
+     * @return {@code Task} object at index {@code index}
+     */
+    public Task getTask(int index) {
+        return this.tasks.get(index);
     }
 
     /**
      * Deletes Task from the list at specified index.
      * @param index Integer index to delete at
      * @throws OutOfListException Thrown if invalid index is given, contains current task size
+     * @return {@code Task} object that was deleted
      */
-    public void deleteTask(int index) throws OutOfListException {
-        if (index < 0 || index >= this.tasks.size()) throw new OutOfListException(String.valueOf(this.tasks.size()));
-        Task task = this.tasks.remove(index);
-        System.out.println("Deleting now hor!");
-        System.out.printf("\t%s\n", task);
-        System.out.printf("You now got %s tasks in your list leh\n", this.tasks.size());
-        save();
+    public Task deleteTask(int index) throws OutOfListException {
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new OutOfListException(String.valueOf(this.tasks.size()));
+        }
+        Task curr = this.tasks.remove(index);
+        return curr;
     }
 
     /**
      * Marks task as completed at specified index.
      * @param index Integer index to mark task at
      * @throws OutOfListException Thrown if invalid index is given, contains current task size
+     * @return {@code Task} object that was marked
      */
-    public void mark(int index) throws OutOfListException {
-        if (index < 0 || index >= this.tasks.size()) throw new OutOfListException(String.valueOf(this.tasks.size()));
+    public Task mark(int index) throws OutOfListException {
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new OutOfListException(String.valueOf(this.tasks.size()));
+        }
         Task curr = this.tasks.get(index);
-        System.out.println("Solid lah, marked already");
         curr.mark();
-        System.out.printf("\t%s\n", curr);
-        save();
+        return curr;
     }
 
     /**
      * Unmarks task as completed at specified index.
      * @param index Integer index to unmark task at
      * @throws OutOfListException Thrown if invalid index is given, contains current task size
+     * @return {@code Task} object that was unmarked
      */
-    public void unmark(int index) throws OutOfListException {
-        if (index < 0 || index >= this.tasks.size()) throw new OutOfListException(String.valueOf(this.tasks.size()));
+    public Task unmark(int index) throws OutOfListException {
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new OutOfListException(String.valueOf(this.tasks.size()));
+        }
         Task curr = this.tasks.get(index);
-        System.out.println("Walao, ok la I unmark already...");
         curr.unmark();
-        System.out.printf("\t%s\n", curr);
-        save();
-    }
-
-    /**
-     * Saves task list to PATH static variable.
-     * if IOException raised, will print error statement.
-     */
-    public void save() {
-        File f = new File(PATH);
-        try {
-            f.createNewFile();
-            FileWriter fw = new FileWriter(f);
-            for (int i = 0; i < this.tasks.size(); i++) {
-                fw.write(this.tasks.get(i).toString());
-                if (i < this.tasks.size() - 1) {
-                    fw.write("\n");
-                }
-            }
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Error! Please check your file permissions!");
-        }
-    }
-
-    /**
-     * Loads task list from save file.
-     * Will skip tasks if task format is wrong, will not load the full list if save file cannot be opened.
-     */
-    public void load() {
-        File f = new File(PATH);
-        Matcher m;
-        Task t;
-        try {
-            Scanner sc = new Scanner(f);
-            while (sc.hasNextLine()) {
-                m = TASK_FORMAT.matcher(sc.nextLine());
-                if (m.matches()) {
-                    switch (m.group(1)) {
-                    case "T":
-                        t = new Todo(m.group(3));
-                        if (m.group(2).equals("X")) {
-                            t.mark();
-                        }
-                        this.tasks.add(t);
-                        break;
-                    case "D":
-                        t = new Deadline(m.group(3), m.group(4));
-                        if (m.group(2).equals("X")) {
-                            t.mark();
-                        }
-                        this.tasks.add(t);
-                        break;
-                    case "E":
-                        t = new Event(m.group(3), m.group(4), m.group(5));
-                        if (m.group(2).equals("X")) {
-                            t.mark();
-                        }
-                        this.tasks.add(t);
-                        break;
-                    }
-                } else {
-                    System.out.println("Error with loading task, skipping to next one...");
-                }
-            }
-            sc.close();
-            System.out.println("Tasks loaded successfully!");
-        } catch (IOException e) {
-            System.out.printf("IOException! %s! Aborting loading...\n", e.getMessage());
-        } catch (Exception e) {
-            System.out.printf("Error! %s! Aborting loading...\n", e.toString());
-        }
+        return curr;
     }
 
     /**
      * Returns string representation of the list.
      * Includes task index, task type and whether it is marked or unmarked.
      */
-    public void showList() {
+    @Override
+    public String toString() {
         if (this.tasks.isEmpty()) {
-            System.out.println("Nothing in list lah!");
+            return "Nothing in list lah!";
         } else {
             StringBuilder output = new StringBuilder("Your list here leh!\n");
             Task curr;
@@ -166,7 +99,7 @@ public class TaskList {
                 curr = tasks.get(i);
                 output.append(String.format("%d. %s\n", i + 1, curr));
             }
-            System.out.println(output.toString().strip());
+            return output.toString().strip();
         }
     }
 }
