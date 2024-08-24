6dda3120b3e15b37e9fb84e429f993d992042005
dez2003 <desiree2021325@gmail.com>
2024-08-24 10:24:36 +0800

    Add greeting and bye message

--- src/main/java/Alisa.java
+++ src/main/java/Alisa.java
@@ -1,10 +1,10 @@
 public class Alisa {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divider = "---------------------------------------------";
+
+        System.out.println("Hey, Alisa here! What do you need help with?");
+        System.out.println(divider);
+        System.out.println("Alrighty, see ya next time!");
+        System.out.println(divider);
     }
 }
