59fbca4d3130a0e622703319ba6caba20ff14a38
DanzaSeah <e0959436@u.nus.edu>
2024-08-24 10:15:21 +0800

    Add ability to mark task as done

--- src/main/java/BabbleBot.java
+++ src/main/java/BabbleBot.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BabbleBot {
     public static void main(String[] args) {
         Boolean notBye = true;
-        String[] storedString = new String[100];
+        Task[] storedTasks = new Task[100];
         int counter = 0;
         Scanner in = new Scanner(System.in);
         String separator = "---------------------------------------------";
@@ -13,18 +13,33 @@ public class BabbleBot {
 
         while(notBye) {
             String userInp = in.nextLine();
-            if (userInp.equals("bye")) {
+            String[] userInpSplit = userInp.split(" ");
+            if (userInpSplit[0].equals("bye")) {
                 notBye = false;
-            } else if (userInp.equals("list")) {
+            } else if (userInpSplit[0].equals("list")) {
                 System.out.println(separator);
                 for (int i = 0; i < counter; i++) {
-                    System.out.println(i + 1 + ". " + storedString[i]);
+                    System.out.println(i + 1 + ". " + "[" + storedTasks[i].getStatusIcon() + "] " + storedTasks[i].description);
                 }
                 System.out.println(separator);
-            } else {
+            } else if (userInpSplit.length == 2) {
+                if (userInpSplit[0].equals("mark")) {
+                    //mark code'
+                    int index = Integer.valueOf(userInpSplit[1]) - 1;
+                    storedTasks[index].markAsDone();
+                    System.out.println(separator);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("   " + "[" + storedTasks[index].getStatusIcon() + "] " + storedTasks[index].description);
+                    System.out.println(separator);
+                } else if (userInpSplit[0].equals("unmark")) {
+                    //unmark code
+                    break;
+                }
+            }
+            else {
                 System.out.println(separator);
                 System.out.println("added: " + userInp);
-                storedString[counter] = userInp;
+                storedTasks[counter] = new Task(userInp);
                 counter++;
                 System.out.println(separator);
             }
