514141e0be8ff94b169afa3e9a2888a0edfc6b1d
Jaehyeok Choi <77jack0105@gmail.com>
2024-08-24 15:21:02 +0800

    Add Echo to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,9 +16,25 @@ public class Duke {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine().trim();
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
 
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
