ebf51a415fdd255f04cf10573d58d6c7789a9d69
Jaehyeok Choi <77jack0105@gmail.com>
2024-08-24 16:18:47 +0800

    Add marking to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,7 +18,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine().trim();
@@ -29,21 +29,56 @@ public class Duke {
                 System.out.println(" Bye. Hope to see you again soon!");
                 break;
             } else if (input.equalsIgnoreCase("list")) {
-                if (tasks.isEmpty()) {
-                    System.out.println(" The list is empty.");
-                } else {
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
-                    }
-                }
+                listTasks(tasks);
+            } else if (input.startsWith("mark ")) {
+                markTask(tasks, input, true);
+            } else if (input.startsWith("unmark ")) {
+                markTask(tasks, input, false);
             } else {
-                tasks.add(input);
-                System.out.println(" added: " + input);
+                addTask(tasks, input);
             }
 
             System.out.println("____________________________________________________________");
         }
 
         scanner.close();
+    }
+
+    private static void listTasks(ArrayList<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println(" The list is empty.");
+        } else {
+            System.out.println(" Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(" " + (i + 1) + "." + tasks.get(i));
+            }
+        }
+    }
+
+    private static void markTask(ArrayList<Task> tasks, String input, boolean markAsDone) {
+        try {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (index >= 0 && index < tasks.size()) {
+                Task task = tasks.get(index);
+                if (markAsDone) {
+                    task.markAsDone();
+                    System.out.println(" Nice! I've marked this task as done:");
+                } else {
+                    task.markAsNotDone();
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                }
+                System.out.println("   " + task);
+            } else {
+                System.out.println(" Invalid task number. Please try again.");
+            }
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println(" Invalid command. Please try again.");
+        }
+    }
+
+    private static void addTask(ArrayList<Task> tasks, String input) {
+        Task newTask = new Task(input);
+        tasks.add(newTask);
+        System.out.println(" added: " + newTask);
     }
 }
