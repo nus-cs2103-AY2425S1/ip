df7a0b4723205267de1df2b93ce5c494a926a582
NorbertLoh <lohnorbert@gmail.com>
2024-08-24 23:29:42 +0800

    Add command usage to help list

--- src/main/java/cow/commands/HelpCommand.java
+++ src/main/java/cow/commands/HelpCommand.java
@@ -16,7 +16,15 @@ public class HelpCommand extends Command {
     public void execute(TodoList todoList, FileSaver fileSaver) {
         Message.print(
                 ListCommand.MESSAGE_USAGE
-                + "\n" + HelpCommand.MESSAGE_USAGE
+                        + "\n" + ByeCommand.MESSAGE_USAGE
+                        + "\n" + TodoCommand.MESSAGE_USAGE
+                        + "\n" + DeadlineCommand.MESSAGE_USAGE
+                        + "\n" + EventCommand.MESSAGE_USAGE
+                        + "\n" + MarkCommand.MESSAGE_USAGE
+                        + "\n" + UnmarkCommand.MESSAGE_USAGE
+                        + "\n" + DeleteCommand.MESSAGE_USAGE
+                        + "\n" + DueCommand.MESSAGE_USAGE
+                        + "\n" + HelpCommand.MESSAGE_USAGE
         );
     }
 }
