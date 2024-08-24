26986d4151fca70bc7ce5457f13e4f2e866719db
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-08-24 14:50:20 +0800

    Rizzler.java: add in line separations

--- src/main/java/Rizzler.java
+++ src/main/java/Rizzler.java
@@ -1,8 +1,11 @@
 public class Rizzler {
     public static void main(String[] args) {
-        System.out.println("____________");
+        System.out.println("#############################");
         System.out.println("hey. i'm the rizzler.");
         System.out.println("what do you want");
+        System.out.println("#############################");
         System.out.println("cya.");
+        System.out.println("#############################");
+
     }
 }
