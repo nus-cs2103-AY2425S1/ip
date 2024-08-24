95f23aab8943a4c47a86847ee59abb0d28d59945
dez2003 <desiree2021325@gmail.com>
2024-08-24 12:58:15 +0800

    Add support for list in main class

--- src/main/java/Alisa.java
+++ src/main/java/Alisa.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Alisa {
@@ -6,21 +7,40 @@ public class Alisa {
         String divider = "------------------------------------------------------------------------------------";
         String exitCommand = "bye";
         String input = "";
+        ArrayList<Task> taskList = new ArrayList<>();
 
         System.out.println("Hey, Alisa here! What do you need help with?");
-        System.out.println("BTW Say the word bye in any part of your sentence to get out of this conversation");
+        System.out.println("BTW Say the word bye to get out of this conversation");
         System.out.println(divider);
-        input = sc.nextLine();
-        while (!input.contains(exitCommand)) {
-            System.out.println(divider);
-            System.out.println(input);
-            System.out.println(divider);
+
+        while (true) {
             input = sc.nextLine();
+            System.out.println(divider);
+
+            if (input.equals(exitCommand)) {
+                System.out.println("Since you technically said bye, see ya next time!");
+                System.out.println(divider);
+                sc.close();
+                break;
+            }
+            else if (input.equals("list")) {
+                if (taskList.isEmpty()) {
+                    System.out.println("The list is empty, sorry :(");
+                } else {
+                    for (int i = 1; i <= taskList.size(); i++) {
+                        System.out.println(i + ". " + taskList.get(i-1));
+                    }
+                    System.out.println(divider);
+                }
+            }
+            else {
+                Task newTask = new Task(input);
+                taskList.add(newTask);
+                System.out.println("added: " + newTask);
+                System.out.println(divider);
+            }
         }
-        System.out.println(divider);
-        System.out.println("Since you technically said bye, see ya next time!");
-        System.out.println(divider);
-        sc.close();
+
     }
 }
 
