2a96bdd7492aad38e433b2c969b5cf1fb1bca519
DanzaSeah <e0959436@u.nus.edu>
2024-08-24 15:06:23 +0800

    Added Exception handling capabilities for 2 basic cases

--- src/main/java/BabbleBot.java
+++ src/main/java/BabbleBot.java
@@ -12,65 +12,74 @@ public class BabbleBot {
         System.out.println(separator);
 
         while(notBye && in.hasNextLine()) {
-            String userInp = in.nextLine();
-            String[] userInpSplit = userInp.split(" ");
-            if (userInpSplit[0].equals("bye")) {
-                notBye = false;
-            } else if (userInpSplit[0].equals("list")) {
-                System.out.println(separator);
-                for (int i = 0; i < counter; i++) {
-                    System.out.println(i + 1 + ". " + storedTasks[i].toString());
+            try {
+                String userInp = in.nextLine();
+                String[] userInpSplit = userInp.split(" ");
+                if (userInpSplit[0].equals("bye")) {
+                    notBye = false;
+                } else if (userInpSplit[0].equals("list")) {
+                    System.out.println(separator);
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println(i + 1 + ". " + storedTasks[i].toString());
+                    }
+                    System.out.println(separator);
+                } else if (userInpSplit[0].equals("todo")) {
+                    try {
+                        storedTasks[counter] = new Todo(userInp.split("todo ")[1]); //i can try here
+                        System.out.println(separator);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("   " + storedTasks[counter].toString());
+                        counter++;
+                        System.out.println("Now you have " + counter + " tasks in the list.");
+                        System.out.println(separator);
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(separator);
+                        System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                        System.out.println(separator);
+                    }
+                } else if (userInpSplit[0].equals("deadline")) {
+                    String content = userInp.split("deadline ")[1].split(" /by")[0];
+                    String by = userInp.split("/by ")[1];
+                    storedTasks[counter] = new Deadline(content, by);
+                    System.out.println(separator);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("   " + storedTasks[counter].toString());
+                    counter++;
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println(separator);
+                } else if (userInpSplit[0].equals("event")) {
+                    String content = userInp.split("event ")[1].split(" /from")[0];
+                    String splitDates = userInp.split("/from ")[1];
+                    String[] parsedDates = splitDates.split(" /to ");
+                    storedTasks[counter] = new Event(content, parsedDates[0], parsedDates[1]);
+                    System.out.println(separator);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("   " + storedTasks[counter].toString());
+                    counter++;
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    System.out.println(separator);
+                } else if (userInpSplit.length == 2) {
+                    if (userInpSplit[0].equals("mark")) {
+                        //mark code
+                        int index = Integer.valueOf(userInpSplit[1]) - 1;
+                        storedTasks[index].markAsDone();
+                        System.out.println(separator);
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("   " + storedTasks[index].toString());
+                        System.out.println(separator);
+                    } else if (userInpSplit[0].equals("unmark")) {
+                        //unmark code
+                        int index = Integer.valueOf(userInpSplit[1]) - 1;
+                        storedTasks[index].maskAsUndone();
+                        System.out.println(separator);
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("   " + storedTasks[index].toString());
+                        System.out.println(separator);
+                    }
+                } else {
+                    throw new BabbleBotException();
                 }
-                System.out.println(separator);
-            } else if (userInpSplit[0].equals("todo")) {
-                storedTasks[counter] = new Todo(userInp.split("todo ")[1]);
-                System.out.println(separator);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("   " + storedTasks[counter].toString());
-                counter++;
-                System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println(separator);
-            } else if (userInpSplit[0].equals("deadline")) {
-                String content = userInp.split("deadline ")[1].split(" /by")[0];
-                String by = userInp.split("/by ")[1];
-                storedTasks[counter] = new Deadline(content, by);
-                System.out.println(separator);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("   " + storedTasks[counter].toString());
-                counter++;
-                System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println(separator);
-            } else if (userInpSplit[0].equals("event")) {
-                String content = userInp.split("event ")[1].split(" /from")[0];
-                String splitDates = userInp.split("/from ")[1];
-                String[] parsedDates = splitDates.split(" /to ");
-                storedTasks[counter] = new Event(content, parsedDates[0], parsedDates[1]);
-                System.out.println(separator);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("   " + storedTasks[counter].toString());
-                counter++;
-                System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println(separator);
-            } else if (userInpSplit.length == 2) {
-                if (userInpSplit[0].equals("mark")) {
-                    //mark code'
-                    int index = Integer.valueOf(userInpSplit[1]) - 1;
-                    storedTasks[index].markAsDone();
-                    System.out.println(separator);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("   " + storedTasks[index].toString());
-                    System.out.println(separator);
-                } else if (userInpSplit[0].equals("unmark")) {
-                    //unmark code
-                    int index = Integer.valueOf(userInpSplit[1]) - 1;
-                    storedTasks[index].maskAsUndone();
-                    System.out.println(separator);
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("   " + storedTasks[index].toString());
-                    System.out.println(separator);
-                }
-            }
-            else {
+            } catch (BabbleBotException b) {
                 System.out.println(separator);
                 System.out.println("Whoopsie daisy! Looks like I got all tangled up in my words there!\n" +
                         "Let's try that again in a way that might make a bit more sense.\n" +
