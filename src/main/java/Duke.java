3f092ef1f3fbfc800c3960d535a805fcbe5628e0
Jaehyeok Choi <77jack0105@gmail.com>
2024-08-24 15:03:11 +0800

    Rename chatbot in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____  _            _                _   \n"
+                + "|  _ \\| |          | |              | |  \n"
+                + "| |_) | | __ _  ___| | ___ __  _   _| |_ \n"
+                + "|  _ <| |/ _` |/ __| |/ / '_ \\| | | | __|\n"
+                + "| |_) | | (_| | (__|   <| | | | |_| | |_ \n"
+                + "|____/|_|\\__,_|\\___|_|\\_\\_| |_|\\__,_|\\__|\n";
+
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Blacknut");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
