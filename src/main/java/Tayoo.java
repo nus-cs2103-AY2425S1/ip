078fc9665efd6497a10f135b09cb9de516c1e23c
Jeremy <jeremytoo989@gmail.com>
2024-08-24 14:36:41 +0800

    Added echo

--- src/main/java/Tayoo.java
+++ src/main/java/Tayoo.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Tayoo {
     public static void main(String[] args) {
         String name = "Tayoo";
+        Scanner scanner = new Scanner(System.in);
 
         //Introduce self
         printHoriLine();
@@ -8,14 +11,18 @@ public class Tayoo {
         System.out.println("What can I do for you?\n");
         printHoriLine();
 
+        //Await command
+        awaitCommand(scanner);
+
+
 
         //Exit programme
-        System.out.println("Bye. Hope to see you again soon!\n");
-        printHoriLine();
+        scanner.close();
+        System.exit(0);
     }
 
     private static void printHoriLine(int length) {
-        StringBuilder line = new StringBuilder();
+        StringBuilder line = new StringBuilder("\t");
 
         for (int i = 0; i<length;i++) {
             line.append("_");
@@ -25,6 +32,35 @@ public class Tayoo {
     }
 
     private static void printHoriLine() {
-        System.out.println("_______________________________________");
+        System.out.println("\t_______________________________________");
     }
+
+    private static void exitBot(Scanner scanner) {
+        printHoriLine();
+        System.out.println("\tBye. Hope to see you again soon!\n");
+        printHoriLine();
+        scanner.close();
+        System.exit(0);
+    }
+
+    private static void awaitCommand(Scanner scanner) {
+        while(true) {
+            String command = scanner.nextLine();
+            String input = command.toUpperCase();
+            switch(input) {
+                case "BYE":
+                case "EXIT":
+                case "CLOSE":
+                case "GOODBYE":
+                    exitBot(scanner);
+                    break;
+                default:
+                    printHoriLine();
+                    System.out.println("\t" + command + "\n");
+                    printHoriLine();
+                    System.out.println("\n");
+            }
+        }
+    }
+
 }
