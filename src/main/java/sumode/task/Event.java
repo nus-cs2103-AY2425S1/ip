2c3728b0d69c4308cd65077788abd47f038a0d80
FooChao <foochao01@gmail.com>
2024-08-25 02:27:10 +0800

    Add javadoc for classes in sumode.task

--- src/main/java/sumode/task/Event.java
+++ src/main/java/sumode/task/Event.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * A class for tasks with specific timeframe.
+ */
 public class Event extends Task {
 
     private final String start;
@@ -11,6 +14,13 @@ public class Event extends Task {
     private LocalDate startDate;
     private LocalDate endDate;
 
+    /**
+     * Constructor for Event
+     *
+     * @param name Name of task.
+     * @param start Starting date of task.
+     * @param end Ending date of task
+     */
     public Event (String name, String start, String end) {
         super(name);
         this.start = start;
@@ -40,6 +50,10 @@ public class Event extends Task {
                 + ")";
     }
 
+    /**
+     * Returns a String in the format to be stored in data file.
+     * @return a String in the format to be stored in data file.
+     */
     @Override
     public String savedString() {
         return "E | " + super.savedString() + " | " + this.start + " | " + this.end;
