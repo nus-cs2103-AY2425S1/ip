1e2ccaa1c943ef6a2bed13ae71dc9e08ebbe4249
yanqiyqh <yanqi.yqh@gmail.com>
2024-08-24 16:59:22 +0800

    Echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
         Ynch chatbot = new Ynch();
         System.out.println(chatbot.greet());
-        System.out.println(chatbot.exit());
+        while (true) {
+            userInput = scanner.nextLine();
+            
+            if (userInput.equals("bye")) {
+                System.out.println(chatbot.exit());
+                break;
+            }
+            System.out.println(userInput);
+        }
+        scanner.close();  
     }
 }
