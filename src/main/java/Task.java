6e214b455c0d233807015df3683b3c72582199a5
dez2003 <desiree2021325@gmail.com>
2024-08-24 13:26:17 +0800

    Add methods in Task class to support mark as done feature

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,23 @@
 public class Task {
-    private String taskDescription;
+    protected String taskDescription;
+    protected boolean isDone;
 
     public Task(String taskDescription) {
         this.taskDescription = taskDescription;
+        this.isDone = false;
     }
 
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
     @Override
     public String toString() {
         return taskDescription;
