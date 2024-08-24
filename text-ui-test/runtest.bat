02baa0bd8d062545fb13707f1e56616dc700316e
FooChao <foochao01@gmail.com>
2024-08-24 18:57:23 +0800

    Update runtest to make it faster according to chatGPT suggestion

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,24 +1,33 @@
 @ECHO OFF
 
-REM create bin directory if it doesn't exist
-if not exist "..\bin" mkdir "..\bin"
+REM Create bin directory if it doesn't exist
+if not exist "..\bin\" mkdir "..\bin"
 
-REM delete output from previous run
+REM Delete output from previous run
 if exist "ACTUAL.TXT" del "ACTUAL.TXT"
 
-REM compile the code into the bin folder
-for /r "..\src\main\java" %%a in (*.java) do (
-    javac -cp "..\src\main\java" -Xlint:none -d "..\bin" "%%a"
+REM Compile the code into the bin folder, make sure to handle errors
+pushd "..\src\main\java"
+for %%a in (*.java) do (
+    javac -cp "." -Xlint:none -d "..\..\..\bin" "%%a"
     IF ERRORLEVEL 1 (
         echo ********** BUILD FAILURE **********
+        popd
         exit /b 1
     )
 )
-REM no error here, errorlevel == 0
+popd
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+REM No error here, errorlevel == 0
+
+REM Run the program, feed commands from input.txt file and redirect the output to ACTUAL.TXT
 java -classpath "..\bin" sumode.SumoDE < "input.txt" > "ACTUAL.TXT"
 
-REM compare the output to the expected output
+REM Compare the output to the expected output
 FC "ACTUAL.TXT" "EXPECTED.TXT"
+IF ERRORLEVEL 1 (
+    echo Output mismatch detected.
+    exit /b 1
+)
+
 
