12eba0c35b4c5f8c9f6541f61f3f46e8e1fb643e
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-08-24 14:46:08 +0800

    Replace Duke.java with Rizzler.java

---
+++ src/main/java/Rizzler.java
@@ -0,0 +1,8 @@
+public class Rizzler {
+    public static void main(String[] args) {
+        System.out.println("____________");
+        System.out.println("hey. i'm the rizzler.");
+        System.out.println("what do you want");
+        System.out.println("cya.");
+    }
+}
