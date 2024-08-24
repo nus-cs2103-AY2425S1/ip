28f014c4aff84169e465bf22c4829e9ce5f773b7
beginner-web-developer <e1156242@u.nus.edu>
2024-08-24 15:30:25 +0800

    Update dates to be interpreted as java.time.LocalDate instead of a string

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
     
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)",
+                super.toString(),
+                this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy")),
+                this.to.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
