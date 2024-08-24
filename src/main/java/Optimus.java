098b22641a708f4caa7d1b69cf5a72bb4d252b9e
Rithik <rithiklenin@gmail.com>
2024-08-24 11:31:16 +0800

    Added the final logic for level 3

--- src/main/java/Optimus.java
+++ src/main/java/Optimus.java
@@ -6,12 +6,12 @@ public class Optimus {
         System.out.println("Hello! I'm Optimus");
         System.out.println("What can I do for you?");
         Scanner stringScanner = new Scanner(System.in);
-        Task [] record = new Task [100];
+        Task[] record = new Task[100];
         int count = 0;
 
         while (true) {
             String text = stringScanner.nextLine();
-            if (text.equals("bye")){
+            if (text.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
@@ -20,33 +20,32 @@ public class Optimus {
                 for (int j = 0; j < count; j++) {
                     System.out.println((j + 1) + ". " + record[j].toString());
                 }
-            }
-            else if (text.startsWith("mark ")) {
+            } else if (text.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(text.split(" ")[1]) - 1;
                 if (taskNumber >= 0 && taskNumber < count) {
-                    record [taskNumber].setDone();
+                    record[taskNumber].setDone();
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println("  " + record[taskNumber].toString());
                 } else {
                     System.out.println("Invalid task number.");
                 }
-            }
-            else if (text.startsWith("unmark ")) {
+            } else if (text.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(text.split(" ")[1]) - 1;
                 if (taskNumber >= 0 && taskNumber < count) {
-                    record [taskNumber].setNotDone();
+                    record[taskNumber].setNotDone();
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println("  " + record[taskNumber].toString());
                 } else {
                     System.out.println("Invalid task number.");
                 }
+            } else {
+                if (count < 100) {
+                    record[count] = new Task(text);
+                    System.out.println("added: " + text);
+                    count++;
+                }
             }
-            else {
-                record[i] = text;
-                System.out.println("added: " + text);
-                i++;
-            }
-        }
 
+        }
     }
 }
