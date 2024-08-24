4fa27bc7160ce0824d6f8fee9057866edfb53c9e
xqtann <xqxinquan@gmail.com>
2024-08-24 15:21:43 +0800

    Add loadTasks method to read saved tasks from data/pikappi.txt

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -6,8 +6,13 @@ public class DeadlineTask extends Task {
         this.by = by;
     }
 
+    public DeadlineTask(String description, String by, boolean isDone) {
+        super(description, isDone);
+        this.by = by;
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -8,8 +8,14 @@ public class EventTask extends Task {
         this.to = to;
     }
 
+    public EventTask(String description, String from, String to, boolean isDone) {
+        super(description, isDone);
+        this.from = from;
+        this.to = to;
+    }
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from:" + from + " to:" + to + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
