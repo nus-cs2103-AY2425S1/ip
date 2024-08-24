2235dc83895148b8b15bae6baaa23fc8b858c94e
zhiyi12345 <122374247+zhiyi12345@users.noreply.github.com>
2024-08-24 10:14:28 +0500

    Level 2, ability to add whatever that is inputed by user except bye and list which returns the exit statement and list of items respectfully Changed from using scan.next to scan.nextLine to read the whole line

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -3,8 +3,26 @@ public class Bobby {
     public static String greeting = "Hello! I'm Bobby\n"
             + "What can I do for you?\n";
     public static String exit = "Bye. Hope to see you again soon!\n";
-
     public static Scanner scan = new Scanner(System.in);
+    public static String[] list = new String[100];
+    public static int counter = 1;
+
+    /*create an add_list function that takes in a
+    String input to add it to the list of items.
+     */
+    public static void add_list(String input) {
+        list[counter - 1] = counter + ". " +input;
+        counter++;
+    }
+
+    /* create a print_list function that takes in a
+    list to print each element in a new line
+     */
+    public static void print_list(String[] list) {
+        for (int x = 0; list[x] != null; x++) {
+            System.out.println(list[x] + "\n");
+        }
+    }
 
     /* create a checker function that takes in a
      String input to echo everytime there is an
@@ -14,13 +32,18 @@ public class Bobby {
     public static void checker(String input) {
         if (input.equals("bye")) {
             System.out.println(exit);
+            return;
+        } if (input.equals("list")) {
+            print_list(list);
+            checker(scan.nextLine());
         } else {
-            System.out.println(input);
-            checker(scan.next());
+            System.out.println("added: " + input);
+            add_list(input);
+            checker(scan.nextLine());
         }
     }
     public static void main(String[] args) {
         System.out.println(greeting);
-        checker(scan.next());
+        checker(scan.nextLine());
     }
 }
