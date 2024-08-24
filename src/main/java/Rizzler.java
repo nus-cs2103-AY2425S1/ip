e860a5692c945371f6f088208df9c902e5bd01be
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-08-24 15:46:12 +0800

    Rizzler.java: update echo function

--- src/main/java/Rizzler.java
+++ src/main/java/Rizzler.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Rizzler {
     public static void main(String[] args) {
-        System.out.println("#############################");
-        System.out.println("hey. i'm the rizzler.");
-        System.out.println("what do you want");
-        System.out.println("#############################");
-        System.out.println("cya.");
-        System.out.println("#############################");
+        Scanner scanner = new Scanner(System.in);
+        String LINE_BREAK = "\t#############################";
+
+        // greet user
+        System.out.println(LINE_BREAK);
+        System.out.println("\they. i'm the rizzler.");
+        System.out.println("\twhat do you want");
+        System.out.println(LINE_BREAK);
+
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(LINE_BREAK);
+            System.out.println("\t" + userInput);
+            System.out.println(LINE_BREAK);
+            userInput = scanner.nextLine();
+        }
+        scanner.close();
+
+        // bid farewell to user
+        System.out.println(LINE_BREAK);
+        System.out.println("\tsure, cya.");
+        System.out.println(LINE_BREAK);
+
 
     }
 }
