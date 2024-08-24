5bac7c115038041db0281bb5e42628b185ef4511
NorbertLoh <lohnorbert@gmail.com>
2024-08-24 15:42:55 +0800

    Add parsing of date and time and due command

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,14 +1,21 @@
 import exceptions.MissingParametersException;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadlines extends Task{
-    private String by;
+    private LocalDateTime by;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
 
     /**
      * @param description The Description of the Deadline with /by to state the dateline
      */
-    public Deadlines(String description, String by) {
+    public Deadlines(String description, String by) throws MissingParametersException {
         super(description);
-        this.by = by;
+        this.by = parseDateTime(by);
+
     }
 
     /**
@@ -17,14 +24,33 @@ public class Deadlines extends Task{
      * @param description The Description of the Deadline
      * @param by by to state the dateline of the task
      */
-    public Deadlines(String isDone, String description, String by) {
+    public Deadlines(String isDone, String description, String by) throws MissingParametersException {
         super(isDone, description);
-        this.by = by;
+        this.by = parseDateTime(by);
     }
 
+    private LocalDateTime parseDateTime(String by) throws MissingParametersException {
+        try {
+            return this.by = LocalDateTime.parse(by, formatter);
+        } catch (DateTimeParseException e) {
+            throw new MissingParametersException("deadline", "deadline return book /by 2/12/2019 1800");
+        }
+    }
+
+    private String printDateTime() {
+        return this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy HHmm"));
+    }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + printDateTime() + ")";
+    }
+
+    /**
+     * Returns the deadlines date
+     * @return deadline's date
+     */
+    public LocalDate getDate() {
+        return this.by.toLocalDate();
     }
 
     /**
@@ -33,6 +59,6 @@ public class Deadlines extends Task{
      */
     @Override
     public String getSaveData() {
-        return "D | " + super.getSaveData() + " | " + this.by;
+        return "D | " + super.getSaveData() + " | " + this.by.format(formatter);
     }
 }
