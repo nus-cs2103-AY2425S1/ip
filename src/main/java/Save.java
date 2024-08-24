047bb4ad8088374ce12255cbfe9433cd68e75b8d
qk <chiangqinkang@gmail.com>
2024-08-25 00:08:09 +0800

    Level-7

---
+++ src/main/java/Save.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Save {
+    private File saveFile;
+    private final String savePath = "C:\\Users\\chian\\Desktop\\ip\\saves\\save.txt";
+
+    public Save(){
+        this.saveFile = new File("C:\\Users\\chian\\Desktop\\ip\\saves\\save.txt");
+    }
+
+    public String getPath() {
+        return this.savePath;
+    }
+
+    public void writeToSave(String s) {
+        try {
+            FileWriter writer = new FileWriter(this.savePath);
+            writer.write(s);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the save file");
+        }
+    }
+
+    public void loadTasksFromSave(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.saveFile))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                ;
+            }
+
+        } catch (IOException | IllegalArgumentException e) {
+            System.out.println("Error loading from save file");
+        }
+    }
+
+}
