ceab70c01fa83273e7014211aba25581f05650b1
blackpanther9229 <hoangnguyen09022004@gmail.com>
2024-08-25 11:28:15 +0800

    add delete function in Nah class

--- src/main/java/InvalidTaskNumber.java
+++ src/main/java/InvalidTaskNumber.java
@@ -1,6 +1,6 @@
 public class InvalidTaskNumber extends Exception{
     InvalidTaskNumber() {
-        super("Sorry, there is currently no task no mark or unmark\n");
+        super("Sorry, there is currently no task in the list to work with\n");
     }
     InvalidTaskNumber(int i, int t) {
         super("NAH!!! Task " + i + " doesn't exist. Please give an number between 1 and " + t + "\n");
