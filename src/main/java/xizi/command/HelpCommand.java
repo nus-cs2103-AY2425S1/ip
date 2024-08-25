a05cadb9b969b4a2be24ba17111749e986932231
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-08-26 00:12:34 +0800

    Test with gradle build and make slight adjustments

--- src/main/java/xizi/command/HelpCommand.java
+++ src/main/java/xizi/command/HelpCommand.java
@@ -7,9 +7,7 @@ import xizi.Ui;
 public class HelpCommand implements Command {
     @Override
     public void execute(TaskList actions, Storage storage, Ui ui) {
-        ui.showLine();
         ui.printHelp();
-        ui.showLine();
     }
 }
 
