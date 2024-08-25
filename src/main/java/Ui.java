53cf105386b8d6453d4d33383784486318c32259
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-08-25 21:34:42 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        System.out.println(); // blank line before user input
+        return scanner.nextLine();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void showError(String message) {
+        System.out.println("\nWell done! An error has occurred:\n" + message);
+    }
+
+    public void showGreeting() {
+        System.out.println("""
+
+                It's your worst buggy nightmare, Qwerty.
+                What can I do for you?""");
+    }
+
+    public void showGoodbye() {
+        System.out.println("""
+                
+                Goodbye, and I'll see you within 3 business days.""");
+    }
+
+}
