ed6803a64694092b8572329bd02c8feac642028e
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-26 00:21:36 +0800

    Event.java: fixed typo in toString method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -39,6 +39,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("%s (from: %s to: %s", super.getTaskName(), this.startTime, this.endTime);
+        return String.format("%s (from: %s to: %s)", super.getTaskName(), this.startTime, this.endTime);
     }
 }
