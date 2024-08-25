30f4c52783280f3298460cfae3849b4ac512474a
Potatoad88 <>
2024-08-25 19:02:48 +0800

    Update tasks constructors

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -2,8 +2,8 @@ public class Todo extends Task {
 
     protected String by;
 
-    public Todo(String description) {
-        super(description);
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
     }
 
     @Override
