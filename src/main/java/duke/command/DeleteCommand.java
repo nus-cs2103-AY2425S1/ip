40dbbe31eceb160f9741f4b88454a74788c68739
engyuhan <e1136866@u.nus.edu>
2024-08-25 16:21:45 +0800

    feat: Add DeleteCommand class

---
+++ src/main/java/duke/command/DeleteCommand.java
@@ -0,0 +1,47 @@
+package duke.command;
+
+import duke.BobException;
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
+import duke.tasks.Task;
+
+import java.io.IOException;
+
+/**
+ * Represents a delete command.
+ */
+public class DeleteCommand extends Command {
+    /**
+     * The item number of the task to delete.
+     */
+    private int itemNum;
+
+    /**
+     * Constructor for a delete command.
+     *
+     * @param itemNum The item number of the task to delete.
+     */
+    public DeleteCommand(int itemNum) {
+        this.itemNum = itemNum;
+    }
+
+    /**
+     * Deletes the task from the list of tasks and storage, and writes the response message.
+     *
+     * @param tasks The list of tasks to delete the task from.
+     * @param ui The ui to write the response message.
+     * @param storage The storage to delete the task from.
+     * @throws BobException If an I/O error occurs.
+     */
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws BobException {
+        try {
+            storage.delete(this.itemNum);
+            Task task = tasks.delete(this.itemNum);
+            ui.delete(task, tasks.size());
+        } catch (IOException e) {
+            throw new BobException("Sorry, something went wrong when updating your storage.");
+        }
+    }
+}
