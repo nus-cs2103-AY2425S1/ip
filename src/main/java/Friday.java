7ffa93113f7d985cffef40a5cc85e63a9aad976d
Leow Sze Theng, Jolyn <jolynleow2004@gmail.com>
2024-08-25 14:20:11 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Friday.java
@@ -0,0 +1,7 @@
+public class Friday {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Friday\nWhat can I do for you?\n";
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(greeting + farewell);
+    }
+}
