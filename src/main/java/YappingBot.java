e14a88bf4bffb32bc0d0bd2f988981d9c93e8caa
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-25 18:25:52 +0800

    YappingBot.java: added quoteText function to quote bot's text;

--- src/main/java/YappingBot.java
+++ src/main/java/YappingBot.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class YappingBot {
     // Text strings
     private static final String BOT_NAME = "YappingBot";
@@ -8,8 +11,20 @@ public class YappingBot {
     private static final String EXIT_TEXT = "Bye. Hope to see you again soon!";
     // End of text strings
 
+    private static String quoteText(String text) {
+        // annotates text with pipe to denote speech from bot
+        String[] lines = text.split("\n");
+        StringBuilder sb = new StringBuilder();
+        for (String l : lines) {
+            sb.append("\n |  ");
+            sb.append(l);
+        }
+        sb.append("\n"); // pad the end with another newline
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println(GREETING_TEXT);
-        System.out.println(EXIT_TEXT);
+        System.out.println(quoteText(GREETING_TEXT));
+        System.out.println(quoteText(EXIT_TEXT));
     }
 }
