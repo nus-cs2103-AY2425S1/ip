1bd8abf7bcf7a9c676a88a67f477a66c811b4fd8
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-08-25 16:07:34 +0800

    add delete feature

--- src/main/java/Gallium.java
+++ src/main/java/Gallium.java
@@ -8,7 +8,7 @@ public class Gallium {
         String listMessage = "Here are the tasks in your list:";
         String byeMessage = "Bye. Hope to see you again soon!";
         String lines = "____________________________________________________________";
-        Task[] taskList = new Task[100];
+        ArrayList<Task> taskList = new ArrayList<Task>();
         Scanner userInput = new Scanner(System.in);
         System.out.println("    " + lines + "\n    " + helloMessage + "\n    " + lines);
         String Message = userInput.nextLine();
@@ -23,26 +23,26 @@ public class Gallium {
                     System.out.println("    " + lines);
                     System.out.println("    " + listMessage);
                     for (int i = 1; i < Task.count; i++) {
-                        Task task = taskList[i - 1];
+                        Task task = taskList.get(i - 1);
                         System.out.println("\n    " + i + ". " + task.toString());
                     }
                     System.out.println("    " + lines + "\n    ");
-                    Message = userInput.nextLine();
+                    // Message = userInput.nextLine();
                 } else if (Message.matches(mark + " \\d+") || Message.matches(unmark + " \\d+")) {
                     boolean isMark = Message.startsWith(mark);
                     Pattern pattern = Pattern.compile((isMark ? mark : unmark) + " (\\d+)");
                     Matcher matcher = pattern.matcher(Message);
                     if (matcher.matches()) {
                         int index = Integer.parseInt(matcher.group(1));
-                        Task task = taskList[index - 1];
+                        Task task = taskList.get(index - 1);
                         task.setIsDone(isMark);
                         System.out.println("    " + lines);
                         System.out.println("    " + (isMark ? "Nice! I've marked this task as done: "
                                 : "OK, I've marked this task as not done yet: ") + "\n" + "    "
                                 + task.toString());
                         System.out.println("    " + lines + "\n    ");
-                        Message = userInput.nextLine();
                     }
+                    // Message = userInput.nextLine();
                 } else if (Message.equals("todo") || Message.equals("todo ") || Message.equals("deadline")
                         || Message.equals("deadline ") || Message.equals("event") || Message.equals("event ")) {
                     throw new GalliumException("OOPS!!! The description of a " + Message + " cannot be empty.");
@@ -51,7 +51,7 @@ public class Gallium {
                     System.out.println("    " + lines + "\n    " + "Got it. I've added this task: \n" + "    "
                             + todo.toString()
                             + "\n    Now you have " + Task.count + " tasks in the list.\n" + "    " + lines + "\n");
-                    taskList[Task.count - 1] = todo;
+                    taskList.add(todo);
                     Task.count++;
                 } else if (Message.startsWith("deadline ")) {
                     Deadline deadline = new Deadline(Message);
@@ -60,22 +60,38 @@ public class Gallium {
                                     + "    " + deadline.toString()
                                     + "\n    Now you have " + Task.count + " tasks in the list.\n" + "    " + lines
                                     + "\n");
-                    taskList[Task.count - 1] = deadline;
+                    taskList.add(deadline);
                     Task.count++;
                 } else if (Message.startsWith("event ")) {
                     Event event = new Event(Message);
                     System.out.println("    " + lines + "\n    " + "Got it. I've added this task: \n" + "    "
                             + event.toString()
                             + "\n    Now you have " + Task.count + " tasks in the list.\n" + "    " + lines + "\n");
-                    taskList[Task.count - 1] = event;
+                    taskList.add(event);
                     Task.count++;
+                } else if (Message.startsWith("delete ")) {
+                    Pattern pattern = Pattern.compile(("delete") + " (\\d+)");
+                    Matcher matcher = pattern.matcher(Message);
+                    if (matcher.matches()) {
+                        int index = Integer.parseInt(matcher.group(1));
+                        Task task = taskList.get(index - 1);
+                        Task.count--;
+                        System.out.println("    " + lines);
+                        System.out.println("    " + "Noted. I've removed this task:" + "\n" + "    "
+                                + task.toString());
+                        System.out.println("\n    Now you have " + (Task.count - 1) + " tasks in the list.\n");
+                        System.out.println("    " + lines + "\n    ");
+                        taskList.remove(index - 1);
+                        // Message = userInput.nextLine();
+                    }
+                    // Message = userInput.nextLine();
                 } else {
                     throw new GalliumException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                Message = userInput.nextLine();
+                // Message = userInput.nextLine();
             } catch (GalliumException e) {
                 System.out.println("    " + lines + "\n    " + e.getMessage() + "\n    " + lines + "\n");
-                Message = userInput.nextLine();
+                // Message = userInput.nextLine();
             } catch (ArrayIndexOutOfBoundsException e) {
                 if (Message.startsWith("deadline ")) {
                     System.out.println("    " + lines + "\n    " + "Please put the date of the deadline!!" + "\n    "
@@ -85,8 +101,15 @@ public class Gallium {
                             .println("    " + lines + "\n    " + "Please put the from and to of the event!!" + "\n    "
                                     + lines + "\n");
                 }
-                Message = userInput.nextLine();
+            } catch (IndexOutOfBoundsException e) {
+                if (Message.startsWith(mark) || Message.startsWith(unmark) || Message.startsWith("delete")) {
+                    System.out
+                            .println("    " + lines + "\n    " + "Please put a number between 1 and " + (Task.count - 1)
+                                    + "!" + "\n    Now you have " + (Task.count - 1) + " tasks in the list.\n" + "    "
+                                    + lines + "\n");
+                }
             }
+            Message = userInput.nextLine();
         }
         System.out.println("    " + lines + "\n    " + byeMessage + "\n    " + lines + "\n");
         userInput.close();
