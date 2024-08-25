d59215d9b5f42ed2d2c97188b537ac6ec03f98cf
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-08-26 00:51:39 +0800

    Level 0 - Greet

--- src/main/java/Dave.java
+++ src/main/java/Dave.java
@@ -1,12 +1,4 @@
 public class Dave {
-    /*public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }*/
 
     public static void main(String[] args) {
         String logo = " ____    _    __     ______\n"
@@ -14,6 +6,11 @@ public class Dave {
                 + "| | | |/ _ \\   \\ \\ / /|  _|\n"
                 + "| |_| / ___ \\   \\ V / | |___\n"
                 + "|____/_/   \\_\\   \\_/  |_____|\n";
+        String horizontal = "__________________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(horizontal);
+        System.out.println("Hello! I'm Dave");
+        System.out.println("What can I do for you ?");
+        System.out.println(horizontal);
     }
 }
