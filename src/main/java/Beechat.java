d5fbd1bfb3a92205b50753dd2ffd5cf30a1ea485
Bryan <e0958519@u.nus.edu>
2024-08-25 23:08:37 +0800

    Level 1 - Echo

--- src/main/java/Beechat.java
+++ src/main/java/Beechat.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Beechat {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Beechat!\nWhat can I do for you?" +
-                "\n\nBye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Beechat!\nWhat can I do for you?\n");
+        String msg = sc.nextLine();
+        while (!msg.equals("bye")) {
+            System.out.println(msg);
+            msg = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
