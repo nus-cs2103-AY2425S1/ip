23eb09810e54a30d72b108899ba0d1120c3f1e4a
raageth <chaygareth@gmail.com>
2024-08-25 22:04:44 +0800

    Add ability to save tasks to hard drive

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,11 +1,13 @@
+package task;
+
 public class Deadline extends Task {
     private String symbol = "D";
     private String deadline;
 
     /**
-     * Initialises a Deadline Task with name and deadline.
-     * @param name A string of the Task's name.
-     * @param deadline A string indicating the Task's deadline.
+     * Initialises a task.Deadline task.Task with name and deadline.
+     * @param name A string of the task.Task's name.
+     * @param deadline A string indicating the task.Task's deadline.
      */
     public Deadline(String name, String deadline) {
         super(name);
@@ -19,5 +21,9 @@ public class Deadline extends Task {
     @Override
     public String getTaskInfo() {
         return super.getTaskInfo() + String.format(" (by: %s)", this.deadline);
+    }
+    @Override
+    public String toCsv() {
+        return super.toCsv() + "," + this.deadline;
     }
 }
