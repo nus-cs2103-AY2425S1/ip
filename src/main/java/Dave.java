6babd725d819bff13e16a7f4cf2874526b8c5a7c
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-08-26 00:37:35 +0800

    Level 0 - Rename Chatbot

---
+++ src/main/java/Dave.java
@@ -0,0 +1,19 @@
+public class Dave {
+    /*public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }*/
+
+    public static void main(String[] args) {
+        String logo = " ____    _    __     ______\n"
+                + "|  _ \\  / \\   \\ \\   / / ___|\n"
+                + "| | | |/ _ \\   \\ \\ / /|  _|\n"
+                + "| |_| / ___ \\   \\ V / | |___\n"
+                + "|____/_/   \\_\\   \\_/  |_____|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
