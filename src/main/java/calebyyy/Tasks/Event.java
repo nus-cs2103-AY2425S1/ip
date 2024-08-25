7a9e510b805a0cb17c6902581f80d89df26cdf81
calebyyy <caleb.ng.yy@gmail.com>
2024-08-25 17:07:56 +0800

    Add for date time for event too

--- src/main/java/calebyyy/Tasks/Event.java
+++ src/main/java/calebyyy/Tasks/Event.java
@@ -1,21 +1,28 @@
 package calebyyy.Tasks;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        this.from = LocalDateTime.parse(from, inputFormatter);
+        this.to = LocalDateTime.parse(to, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+        return "[E]" + super.toString() + " (from: " + from.format(outputFormatter) + " to: " + to.format(outputFormatter) + ")";
     }
 
     @Override
     public String toSaveFormat() {
-        return "E" + super.toSaveFormat() + " | " + from + " | " + to;
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        return "E" + super.toSaveFormat() + " | " + from.format(inputFormatter) + " | " + to.format(inputFormatter);
     }
 }
