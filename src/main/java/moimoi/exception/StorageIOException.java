fe5814acf55ddfb88dbf8947e036ba63660c4469
wkxcass <e1129889@u.nus.edu>
2024-08-26 02:32:53 +0800

    Update files for I/O redirection testing
    Files for I/O redirection testing do not include test cases for the
    newly added find-by-keyword feature.
    
    These files need to be updated to reflect the increments made.
    
    Let's,
    * update input.txt and EXPECTED.TXT to include test cases for the
    "find" feature
    * adjust the configuration in runtest.bat to correctly redirect I/O
    during testing
    * modify StorageIOException.java to align with expected output
    formats
    
    This helps to validate that the find-by-keyword functionality and
    its related outputs are working as intended.

--- src/main/java/moimoi/exception/StorageIOException.java
+++ src/main/java/moimoi/exception/StorageIOException.java
@@ -5,7 +5,7 @@ public class StorageIOException extends MoiMoiException {
     @Override
     public String getMessage() {
         return super.getMessage() + "there's something VERY wrong with storage I/O...\n"
-                + "Sorry master, I'm afraid I'm unable to save tasks for you TT";
+                + "Sorry master, I'm afraid I'm unable to save tasks for you TT\n";
     }
 
 }
