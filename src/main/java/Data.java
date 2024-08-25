ef40a4b0d1c67f53810db0cbba1472f2c3ae8dec
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-08-25 13:37:42 +0800

    Auto-save tasks and load on startup
    * update EchoBot.java to load data from data/data.txt
    * add getData and saveData method to class Data

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,7 +1,8 @@
-import java.io.File;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 
 public class Data {
@@ -12,5 +13,55 @@ public class Data {
             return;
         }
         File f = new File(DATA_PATH);
+        try {
+            File parentDir = f.getParentFile();
+            if (parentDir.isDirectory() && !parentDir.exists()) {
+                parentDir.mkdir();
+                f.createNewFile();
+            }
+        } catch (IOException e) {
+            System.out.println("Init Error: " + e.getMessage());
+        }
+    }
+
+    public static void setData(ArrayList<Task> allTask) {
+        try {
+            // FileWriter fw = new FileWriter(Data.DATA_PATH);
+            FileOutputStream fileStream = new FileOutputStream(Data.DATA_PATH);
+            ObjectOutputStream os = new ObjectOutputStream(fileStream);
+            for (Task t : allTask) {
+                os.writeObject(t);
+            }
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static ArrayList<Task> getData() {
+        ArrayList<Task> allTask = new ArrayList<>();
+        File f = new File(Data.DATA_PATH);
+        if (f.length() == 0) {
+            return allTask;
+        }
+        try {
+            FileInputStream fileStream =new FileInputStream(Data.DATA_PATH);
+            ObjectInputStream outputStream = new ObjectInputStream(fileStream);
+            while (true) {
+                try {
+                    Task t = (Task) outputStream.readObject();
+                    allTask.add(t);
+                } catch (EOFException e) {
+                    break;
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Class not found: " + e.getMessage());
+                    break;
+                }
+            }
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println("Get Data Error: " + e.getMessage());
+        }
+        return allTask;
     }
 }
