6319a7ad91615a3c405bf2a012af0aece29b04bc
Shruthiiii03 <muralishruthi222@gmail.com>
2024-08-25 15:37:02 +0800

    Level 3 task completed

--- src/main/java/Prince.java
+++ src/main/java/Prince.java
@@ -1,37 +1,60 @@
 import java.util.Scanner;
 import java.util.ArrayList; // because i need to create a list of String items
-
+import static java.lang.Integer.parseInt;
 
 
 public class Prince {
-    static ArrayList<String> list = new ArrayList<>(); //static variable
+    static ArrayList<Task> list = new ArrayList<>(); //static variable
     public static String conversation(String command) {
         if(command.equals("bye")) { //string cannot do ==
             return "Bye! Hope to see you again soon!";
         } else if(command.equals("list")) {
             return listDisplay(list);
+        } else if(command.startsWith("mark") || command.startsWith("unmark")) {
+            // used the library function .startsWith() to match the prefix to mark/unmark
+            // use.split("") to split up the words
+            // use.parseInt(num) to extract integer from the string
+
+            // if mark, then extract integer and mark that task of the list as done
+            // if unmark then extract integer and unmark that task of the list as not done
+
+            String[] stringList = command.split(" ");
+            int taskNum = Integer.parseInt(stringList[1]); //second word is the number
+            Task t = list.get(taskNum - 1);
+
+            if (taskNum < 1 || taskNum > list.size()) {
+                return "Task number is out of range. Please retry.";
+            }
+
+            if(stringList[0].equals("mark")) {
+                return t.markDone();
+            } else {
+                return t.markIncomplete();
+            }
+
         } else {
-            return addTask(command);
+            Task tsk = new Task(command);
+            return addTask(tsk);
         }
     }
 
-    public static String listDisplay(ArrayList<String> list) {
+    public static String listDisplay(ArrayList<Task> list) {
         int length = list.size();
         // use String Builder to ensure that the string can be created on another line
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i < length; i++) {
-            sb.append(i + 1 + ". " + list.get(i)).append("\n");
+            sb.append(i + 1 + ". " + list.get(i).printTask()).append("\n");
         }
 
         return sb.toString();
     }
 
-    public static String addTask(String task){
+    public static String addTask(Task task){
         // add task to the List
         // return a string
         list.add(task);
-        return "added: " + task;
+        return "added: " + task.getDescription();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    public void setProgress(boolean b) {
+        this.isDone = b;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String printTask() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+    public String markDone() {
+        this.setProgress(true);
+        return "Nice! I've marked this task as done: \n  " + this.printTask();
+    }
+
+    public String markIncomplete() {
+        this.setProgress(false);
+        return "OK! I've marked this task as not done yet: \n  " + this.printTask();
+    }
+
+}
