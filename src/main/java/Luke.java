9df305e1104d7b3dacebdf08b22670fb4bde48ff
MinkhantEain <khantkyaw.pkk@gmail.com>
2024-08-25 11:43:59 +0800

    Add save

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -7,19 +7,22 @@ public class Luke {
     public static void save(ArrayList<Task> tasks) throws IOException {
         File saveFile = new File(SAVE_PATH);
         if (!saveFile.exists()) {
-            if (!saveFile.createNewFile()) {
+            File dir = new File("./data");
+            boolean dirCreated = dir.mkdir();
+            if (dirCreated && !saveFile.createNewFile()) {
                 throw new FileNotFoundException("Could not create file " + saveFile.getAbsolutePath());
             }
         }
         FileWriter fw = new FileWriter(SAVE_PATH);
         for (Task task : tasks) {
-
+            fw.write(task.getSaveFormat() + "\n");
         }
+        fw.close();
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Task> l = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String logo =
                 " __\n"
                         + "| |    _   _ _  _____\n"
@@ -38,35 +41,35 @@ public class Luke {
                 break;
             } else if (splittedLine[0].equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < l.size(); i++) {
-                    System.out.println(String.format("%d. %s", i + 1, l.get(i).toString()));
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(String.format("%d. %s", i + 1, tasks.get(i).toString()));
                 }
             } else if (splittedLine[0].equals("mark")) {
                 System.out.println("Nice! I've marked this task as done:");
-                Task t = l.get(Integer.parseInt(splittedLine[1]) - 1);
+                Task t = tasks.get(Integer.parseInt(splittedLine[1]) - 1);
                 t.mark();
                 System.out.println(t);
             } else if (splittedLine[0].equals("unmark")) {
                 System.out.println("OK, I've marked this task as not done yet:");
-                Task t = l.get(Integer.parseInt(splittedLine[1]) - 1);
+                Task t = tasks.get(Integer.parseInt(splittedLine[1]) - 1);
                 t.unMark();
                 System.out.println(t);
             } else if (splittedLine[0].equals("delete")) {
                 int index = Integer.parseInt(splittedLine[1]) - 1;
-                Task t = l.get(index);
-                l.remove(index);
+                Task t = tasks.get(index);
+                tasks.remove(index);
                 System.out.println("Noted. I've removed this task:");
                 System.out.println(t);
-                System.out.printf("Now you have %d tasks in the list.\n", l.size());
+                System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
             } else {
                 Task t;
                 if (splittedLine[0].equals("todo")) {
                     t = new TodoBuilder().build(line.substring(4).trim());
-                    l.add(t);
+                    tasks.add(t);
                 } else if (splittedLine[0].equals("deadline")) {
                     try {
                         t = new DeadlineBuilder().build(line.substring(8).trim());
-                        l.add(t);
+                        tasks.add(t);
                     } catch (LukeException e) {
                         System.out.println(e.getMessage());
                         continue;
@@ -74,7 +77,7 @@ public class Luke {
                 } else if (splittedLine[0].equals("event")) {
                     try {
                         t = new EventBuilder().build(line.substring(5).trim());
-                        l.add(t);
+                        tasks.add(t);
                     } catch (LukeException e) {
                         System.out.println(e.getMessage());
                         continue;
@@ -85,9 +88,10 @@ public class Luke {
                 }
                 System.out.println("Got it. I've added this task:");
                 System.out.println(t);
-                System.out.printf("Now you have %d tasks in the list.\n", l.size());
+                System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
             }
         }
+        save(tasks);
         System.out.println("Aight, Cya later.");
         br.close();
     }
