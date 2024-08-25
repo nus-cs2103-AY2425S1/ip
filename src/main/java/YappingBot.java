2262835b0984f7a5a0d24f17411211d18ae591f0
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-25 18:43:09 +0800

    YappingBot.java: split quoteLines into quoteMultiline and quoteSingleline.

--- src/main/java/YappingBot.java
+++ src/main/java/YappingBot.java
@@ -10,23 +10,28 @@ public class YappingBot {
     private static final String EXIT_TEXT = "Bye. Hope to see you again soon!";
     // End of text strings
 
-    private static String quoteText(String text) {
+
+    private static void quoteSinglelineText(String l, StringBuilder sb) {
+        sb.append("\n |  ");
+        sb.append(l);
+    }
+    private static String quoteMultilineText(String text) {
         // annotates text with pipe to denote speech from bot
         String[] lines = text.split("\n");
         StringBuilder sb = new StringBuilder();
         for (String l : lines) {
-            sb.append("\n |  ");
-            sb.append(l);
+            quoteSinglelineText(l, sb);
         }
         sb.append("\n"); // pad the end with another newline
         return sb.toString();
     }
 
+
     public static void main(String[] args) {
         // scanner for user input
         Scanner userInputScanner = new Scanner(System.in);
         // start
-        System.out.println(quoteText(GREETING_TEXT));
+        System.out.println(quoteMultilineText(GREETING_TEXT));
 
         programmeLoop:
         while (true) {
@@ -35,12 +40,12 @@ public class YappingBot {
                case "bye":
                    break programmeLoop;
                default:
-                   System.out.println(quoteText(String.format("added: %s", userInput)));
+                   System.out.println(quoteMultilineText(String.format("added: %s", userInput)));
                    break; // sanity break
            }
         }
 
         // exit
-        System.out.println(quoteText(EXIT_TEXT));
+        System.out.println(quoteMultilineText(EXIT_TEXT));
     }
 }
