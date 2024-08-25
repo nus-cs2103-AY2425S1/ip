f69417d89be551288d419e9f44bc760aa1347afc
dominopizzaaaa <domksj23@gmail.com>
2024-08-25 21:41:28 +0800

    Add test case for Todo command

--- src/test/java/papadom/commands/AddTodoCommandTest.java
+++ src/test/java/papadom/commands/AddTodoCommandTest.java
@@ -1,16 +1,44 @@
 package papadom.commands;
-import papadom.Exceptions.NoTaskException;
-import papadom.Storage.*;
-import papadom.tasks.Todo;
+
+import org.junit.jupiter.api.Test;
+import papadom.Storage.Storage;
 import papadom.Ui;
-public class AddTodoCommand extends Command {
-    private String text;
-    public AddTodoCommand(String text) {
-        this.text = text;
-    }
-    @Override
-    public void execute(TaskList taskList, Ui ui, Storage storage) throws NoTaskException {
-        Todo todoTask = new Todo(this.text.substring(5));
-        ui.output(taskList.addToList(todoTask));
+import papadom.Storage.TaskList;
+import papadom.Exceptions.NoTaskException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AddTodoCommandTest {
+    @Test
+    public void testExecute() throws NoTaskException {
+        // Arrange
+        Storage storage = new Storage("testStorage.txt");
+        TaskList taskList = new TaskList(storage);
+        Ui ui = new Ui();
+        AddTodoCommand command = new AddTodoCommand("todo buy milk");
+
+        // Capture the system output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        // Act
+        command.execute(taskList, ui, storage);
+
+        // Reset System.out to its original stream
+        System.setOut(originalOut);
+
+        // Assert
+        String expectedOutput = "____________________________________________________________\n" +
+                " Got it. I've added this task:\n" +
+                "  [T][ ] buy milk\n" +
+                " Now you have 1 tasks in the list.\n" +
+                "____________________________________________________________\n";
+        assertEquals(expectedOutput, outputStream.toString());
     }
 }
+
