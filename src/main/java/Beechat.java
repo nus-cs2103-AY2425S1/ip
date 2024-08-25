1613b956dbb7e3303a313b876af48c7fbc958ac7
Bryan <e0958519@u.nus.edu>
2024-08-25 22:46:35 +0800

    Level 0 - Rename, Greet, Exit

---
+++ src/main/java/Beechat.java
@@ -0,0 +1,6 @@
+public class Beechat {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Beechat!\nWhat can I do for you?" +
+                "\n\nBye. Hope to see you again soon!");
+    }
+}
