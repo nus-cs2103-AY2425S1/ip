1610ed45da498804f6f87eb7e1746647139c9d82
Bryan <e0958519@u.nus.edu>
2024-08-25 23:37:58 +0800

    Level 2 - Add, List

--- src/main/java/Beechat.java
+++ src/main/java/Beechat.java
@@ -1,12 +1,23 @@
+import java.lang.String;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Beechat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Beechat!\nWhat can I do for you?\n");
+        ArrayList<String> list = new ArrayList<>();
         String msg = sc.nextLine();
         while (!msg.equals("bye")) {
-            System.out.println(msg);
+            if (msg.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(String.format("%d. %s", i + 1, list.get(i)));
+                }
+                msg = sc.nextLine();
+                continue;
+            }
+            list.add(msg);
+            System.out.println("added: " + msg);
             msg = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
