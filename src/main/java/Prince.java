5b1407bc8822fc75bf239d51f0fd2631f4376c0d
Shruthiiii03 <muralishruthi222@gmail.com>
2024-08-25 14:52:44 +0800

    Level 2 task completed

--- src/main/java/Prince.java
+++ src/main/java/Prince.java
@@ -1,13 +1,39 @@
 import java.util.Scanner;
+import java.util.ArrayList; // because i need to create a list of String items
+
+
 
 public class Prince {
+    static ArrayList<String> list = new ArrayList<>(); //static variable
     public static String conversation(String command) {
         if(command.equals("bye")) { //string cannot do ==
             return "Bye! Hope to see you again soon!";
+        } else if(command.equals("list")) {
+            return listDisplay(list);
         } else {
-            return command;
+            return addTask(command);
         }
     }
+
+    public static String listDisplay(ArrayList<String> list) {
+        int length = list.size();
+        // use String Builder to ensure that the string can be created on another line
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < length; i++) {
+            sb.append(i + 1 + ". " + list.get(i)).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static String addTask(String task){
+        // add task to the List
+        // return a string
+        list.add(task);
+        return "added: " + task;
+    }
+
     public static void main(String[] args) {
         // if any words, repeat scanning, but the moment the word is bye,
         // then exit and print bye
@@ -15,13 +41,13 @@ public class Prince {
         System.out.println("Hello! I'm Prince!");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Type something!");
+        System.out.println("What would you like me to add to your TODO list today?");
 
         line = scanner.nextLine(); // what the user replied
 
         while(!line.equals("bye")) {
             System.out.println(conversation(line));
-            System.out.println("Is there anything else you'd like to say?");
+            System.out.println("How else would you like me to edit your TODO list today?");
             line = scanner.nextLine();
         }
 
