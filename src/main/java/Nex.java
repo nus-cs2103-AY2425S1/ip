ba4fe4a00eaf10d97137c2904992dcca182d611a
quahkk <quahkaikiat1@gmail.com>
2024-08-26 00:57:55 +0800

    Level-0

---
+++ src/main/java/Nex.java
@@ -0,0 +1,8 @@
+public class Nex {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Nex.");
+        System.out.println("What can I do for you?");
+        System.out.println("");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
