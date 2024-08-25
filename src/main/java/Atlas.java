7b8454b4ec06af0c688a3fb81a5f15b927a53862
Chong Tze Zhao <47619363+chongtzezhao@users.noreply.github.com>
2024-08-25 16:39:06 +0800

    renamed, greet, exit

---
+++ src/main/java/Atlas.java
@@ -0,0 +1,17 @@
+public class Atlas {
+    public static void main(String[] args) {
+        introduction();
+        exit();
+    }
+
+    private static void introduction() {
+        System.out.println("Hello! I'm Atlas.");
+        System.out.println("How can I help you today?");
+        System.out.println("___________________________________________\n");
+    }
+
+    private static void exit() {
+        System.out.println("Goodbye! Have a great day ahead!");
+        System.out.println("___________________________________________\n");
+    }
+}
