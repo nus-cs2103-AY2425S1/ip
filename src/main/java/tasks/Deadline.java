bdbc77024ba144fc736ea1bb69ed7ba17622f4da
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-25 17:20:48 +0800

    Add support for reading from / writing to a file

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -11,4 +11,9 @@ public class Deadline extends Task {
     public String toString() {
         return "[D] " + super.toString() + " (by: " + by + ")";
     }
+
+    @Override
+    public String saveFormat() {
+        return "D" + super.saveFormat() + " | " + this.by;
+    }
 }
--- src/main/java/tasks/ToDo.java
+++ src/main/java/tasks/ToDo.java
@@ -8,4 +8,9 @@ public class ToDo extends Task {
     public String toString() {
         return "[T] " + super.toString();
     }
+
+    @Override
+    public String saveFormat() {
+        return "T" + super.saveFormat();
+    }
 }
