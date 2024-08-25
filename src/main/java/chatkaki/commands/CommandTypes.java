6b6a5d6457ae138cd21fa8aecc8758385b49a970
limyuquan <limyuquan02@gmail.com>
2024-08-25 16:48:42 +0800

    Add Find Function

--- src/main/java/chatkaki/commands/CommandTypes.java
+++ src/main/java/chatkaki/commands/CommandTypes.java
@@ -1,7 +1,7 @@
 package chatkaki.commands;
 
 public enum CommandTypes {
-    DELETE, TODO, DEADLINE, EVENT, BYE, MARK, UNMARK, LIST, UNKNOWN;
+    DELETE, TODO, DEADLINE, EVENT, BYE, MARK, UNMARK, LIST, FIND, UNKNOWN;
 
     public static CommandTypes fromString(String command) {
         try {
