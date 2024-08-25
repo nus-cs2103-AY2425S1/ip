4cf5869b6f329a2cbc94c55fd783e22277d7e541
calebyyy <caleb.ng.yy@gmail.com>
2024-08-25 17:02:50 +0800

    Add time and date feature

--- src/main/java/calebyyy/Tasks/Deadline.java
+++ src/main/java/calebyyy/Tasks/Deadline.java
@@ -1,19 +1,26 @@
 package calebyyy.Tasks;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        this.by = LocalDateTime.parse(by, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+        return "[D]" + super.toString() + " (by: " + by.format(outputFormatter) + ")";
     }
 
     @Override
     public String toSaveFormat() {
-        return "D" + super.toSaveFormat() + " | " + by;
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        return "D" + super.toSaveFormat() + " | " + by.format(inputFormatter);
     }
 }
