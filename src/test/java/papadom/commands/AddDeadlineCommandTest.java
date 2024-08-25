394884282f7ed4130b2c1924954c326b5c522666
dominopizzaaaa <domksj23@gmail.com>
2024-08-26 00:36:35 +0800

    Add test cases for all tasks adding

--- src/test/java/papadom/commands/AddDeadlineCommandTest.java
+++ src/test/java/papadom/commands/AddDeadlineCommandTest.java
@@ -10,6 +10,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AddDeadlineCommandTest {
     @Test
@@ -38,28 +39,48 @@ public class AddDeadlineCommandTest {
         assertEquals(expectedOutput, outputStream.toString());
     }
     @Test
-    public void testExecuteWithoutTime() throws IncorrectTaskInputFormatException{
+    public void testExecuteWithoutTime() {
+        // Arrange
         Storage storage = new Storage("testStorage.txt");
         TaskList taskList = new TaskList(storage);
         Ui ui = new Ui();
-        AddDeadlineCommand command = new AddDeadlineCommand("deadline return book /by 2024-12-12");
+        AddDeadlineCommand command = new AddDeadlineCommand("deadline homework");
 
-        // Capture the system output
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream originalOut = System.out;
-        System.setOut(new PrintStream(outputStream));
+        // Act & Assert
+        IncorrectTaskInputFormatException thrown = assertThrows(
+                IncorrectTaskInputFormatException.class,
+                () -> command.execute(taskList, ui, storage),
+                "Expected command.execute() to throw IncorrectTaskInputFormatException, but it didn't"
+        );
 
-        // Act
-        command.execute(taskList, ui, storage);
-        // Reset System.out to its original stream
-        System.setOut(originalOut);
-
-        // Assert
-        String expectedOutput = "____________________________________________________________\n" +
-                " Got it. I've added this task:\n" +
-                "  [D][ ] return book (by: Dec 12 2024)\n" +
-                " Now you have 1 tasks in the list.\n" +
-                "____________________________________________________________\n";
-        assertEquals(expectedOutput, outputStream.toString());
+        // Optionally check the exception message
+        String expectedMessage = " Please enter the correct format!\n" +
+                "  For todo tasks: todo [task]\n" +
+                "  For deadline tasks: deadline [task] /by yyyy-mm-dd OR yyyy-mm-dd hh-mm\n" +
+                "  For event tasks: event [task] /from [date & time] /to [date & time]";
+        assertEquals(expectedMessage, thrown.getMessage());
     }
+    @Test
+    public void testExecuteWithWrongTimeFormat() {
+        // Arrange
+        Storage storage = new Storage("testStorage.txt");
+        TaskList taskList = new TaskList(storage);
+        Ui ui = new Ui();
+        AddDeadlineCommand command = new AddDeadlineCommand("deadline homework 12/12/2001");
+
+        // Act & Assert
+        IncorrectTaskInputFormatException thrown = assertThrows(
+                IncorrectTaskInputFormatException.class,
+                () -> command.execute(taskList, ui, storage),
+                "Expected command.execute() to throw IncorrectTaskInputFormatException, but it didn't"
+        );
+
+        // Optionally check the exception message
+        String expectedMessage = " Please enter the correct format!\n" +
+                "  For todo tasks: todo [task]\n" +
+                "  For deadline tasks: deadline [task] /by yyyy-mm-dd OR yyyy-mm-dd hh-mm\n" +
+                "  For event tasks: event [task] /from [date & time] /to [date & time]";
+        assertEquals(expectedMessage, thrown.getMessage());
+    }
+
 }
--- src/test/java/papadom/commands/AddEventCommandTest.java
+++ src/test/java/papadom/commands/AddEventCommandTest.java
@@ -10,10 +10,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AddEventCommandTest {
     @Test
-    public void testExecute() throws IncorrectTaskInputFormatException {
+    public void testExecuteWithValidInput() throws IncorrectTaskInputFormatException {
         Storage storage = new Storage("testStorage.txt");
         TaskList taskList = new TaskList(storage);
         Ui ui = new Ui();
@@ -37,4 +38,49 @@ public class AddEventCommandTest {
                 "____________________________________________________________\n";
         assertEquals(expectedOutput, outputStream.toString());
     }
+    @Test
+    public void testExecuteWithInvalidInput() {
+        // Arrange
+        Storage storage = new Storage("testStorage.txt");
+        TaskList taskList = new TaskList(storage);
+        Ui ui = new Ui();
+        AddEventCommand command = new AddEventCommand("event concert");
+
+        // Act & Assert
+        IncorrectTaskInputFormatException thrown = assertThrows(
+                IncorrectTaskInputFormatException.class,
+                () -> command.execute(taskList, ui, storage),
+                "Expected command.execute() to throw IncorrectTaskInputFormatException, but it didn't"
+        );
+
+        // Optionally check the exception message
+        String expectedMessage = " Please enter the correct format!\n" +
+                "  For todo tasks: todo [task]\n" +
+                "  For deadline tasks: deadline [task] /by yyyy-mm-dd OR yyyy-mm-dd hh-mm\n" +
+                "  For event tasks: event [task] /from [date & time] /to [date & time]";
+        assertEquals(expectedMessage, thrown.getMessage());
+    }
+    @Test
+    public void testExecuteWithOnlyFrom() {
+        // Arrange
+        Storage storage = new Storage("testStorage.txt");
+        TaskList taskList = new TaskList(storage);
+        Ui ui = new Ui();
+        AddEventCommand command = new AddEventCommand("event concert /from 21 Aug 2023");
+
+        // Act & Assert
+        IncorrectTaskInputFormatException thrown = assertThrows(
+                IncorrectTaskInputFormatException.class,
+                () -> command.execute(taskList, ui, storage),
+                "Expected command.execute() to throw IncorrectTaskInputFormatException, but it didn't"
+        );
+
+        // Optionally check the exception message
+        String expectedMessage = " Please enter the correct format!\n" +
+                "  For todo tasks: todo [task]\n" +
+                "  For deadline tasks: deadline [task] /by yyyy-mm-dd OR yyyy-mm-dd hh-mm\n" +
+                "  For event tasks: event [task] /from [date & time] /to [date & time]";
+        assertEquals(expectedMessage, thrown.getMessage());
+    }
+
 }
