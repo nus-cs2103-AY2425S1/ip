2e6737d4a43f1f023304b652263381356c2812b9
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-08-26 04:14:53 +0800

    Move task classes into tasks package

--- src/main/java/OuiOuiBaguetteException.java
+++
@@ -1,5 +0,0 @@
-public class OuiOuiBaguetteException extends Exception {
-    public OuiOuiBaguetteException(String msg) {
-        super("Non non. " + msg);
-    }
-}
--- src/main/java/tasks/DeadlineException.java
+++ src/main/java/tasks/DeadlineException.java
@@ -1,3 +1,7 @@
+package tasks;
+
+import ouiouibaguette.OuiOuiBaguetteException;
+
 public class DeadlineException extends OuiOuiBaguetteException {
     public DeadlineException(String msg) {
         super(msg);
