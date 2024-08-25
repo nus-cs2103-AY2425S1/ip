a55eda63669433bbed790b7e960ce2ab3e70c67a
Shruthiiii03 <muralishruthi222@gmail.com>
2024-08-25 14:16:49 +0800

    Level 1 task completed

--- src/main/java/Prince.java
+++ src/main/java/Prince.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Prince {
+    public static String conversation(String command) {
+        if(command.equals("bye")) { //string cannot do ==
+            return "Bye! Hope to see you again soon!";
+        } else {
+            return command;
+        }
+    }
     public static void main(String[] args) {
+        // if any words, repeat scanning, but the moment the word is bye,
+        // then exit and print bye
+        String line = "";
         System.out.println("Hello! I'm Prince!");
-        System.out.println("What can I do for you today?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type something!");
+
+        line = scanner.nextLine(); // what the user replied
+
+        while(!line.equals("bye")) {
+            System.out.println(conversation(line));
+            System.out.println("Is there anything else you'd like to say?");
+            line = scanner.nextLine();
+        }
+
+        System.out.println(conversation(line));
     }
 }
 
