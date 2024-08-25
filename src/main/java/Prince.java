8ec078f34b7f7fe0bd4e231ae43a06fd59077237
Shruthiiii03 <muralishruthi222@gmail.com>
2024-08-25 12:26:19 +0800

    Level 0

---
+++ src/main/java/Prince.java
@@ -0,0 +1,8 @@
+public class Prince {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Prince!");
+        System.out.println("What can I do for you today?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
