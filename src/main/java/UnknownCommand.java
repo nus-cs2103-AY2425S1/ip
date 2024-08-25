a1680ce6f42d1d8a6f7121c25b41688fa9d1bbdd
blackpanther9229 <hoangnguyen09022004@gmail.com>
2024-08-25 11:44:25 +0800

    Some white space adjustment for better output

--- src/main/java/UnknownCommand.java
+++ src/main/java/UnknownCommand.java
@@ -1,6 +1,6 @@
 public class UnknownCommand extends Exception{
     UnknownCommand() {
-        super(" NAH!!! I'm sorry, but I don't know what that means.\n" +
+        super(" Nah!!! I'm sorry, but I don't know what that means.\n" +
                 " Please give me valid command. eg: mark, unmark, list, ...\n");
     }
 }
