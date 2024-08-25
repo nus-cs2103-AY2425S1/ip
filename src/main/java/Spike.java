d5df604606ba8aeb1d81fad424ef3e3e49ff92dc
seantham21 <seantham21@gmail.com>
2024-08-26 01:48:44 +0800

    Level 1; Echo

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,20 +1,39 @@
+import java.util.Scanner;
+
 public class Spike {
 
     public static void helloMessage() {
-        System.out.print("_________________________________________________________\n");
-        System.out.print("Hello! I'm Spike\nWhat can I do for you?\n");
-        System.out.print("_________________________________________________________\n");
+        System.out.println("_________________________________________________________");
+        System.out.println("Hello! I'm Spike\nWhat can I do for you?");
+        System.out.println("_________________________________________________________");
         return;
     }
 
     public static void byeMessage() {
-        System.out.print("Bye. Hope to see you again soon!\n");
-        System.out.print("_________________________________________________________");
+        System.out.println("     _________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("     _________________________________________________________");
         return;
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.equals("bye")) {
+                byeMessage();
+                break;
+            } else {
+                System.out.println("     _________________________________________________________");
+                System.out.println("     " + input);
+                System.out.println("     _________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 
     public static void main(String[] args) {
         helloMessage();
-        byeMessage();
+        echo();
     }
 }
