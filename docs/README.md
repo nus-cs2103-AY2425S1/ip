d0e773bff391ddf6173fb4d4251c56b88fab814e
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-25 18:11:18 +0800

    Duke.java: renamed to YappingBot.java; YappingBot.java: added welcome and exit text.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# YappingBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/YappingBot.java
@@ -0,0 +1,15 @@
+public class YappingBot {
+    // Text strings
+    private static final String BOT_NAME = "YappingBot";
+    private static final String GREETING_TEXT = String.format(
+            "Hello! I'm %s\nWhat can I do for you?",
+            "BOT_NAME"
+    );
+    private static final String EXIT_TEXT = "Bye. Hope to see you again soon!";
+    // End of text strings
+
+    public static void main(String[] args) {
+        System.out.println(GREETING_TEXT);
+        System.out.println(EXIT_TEXT);
+    }
+}
