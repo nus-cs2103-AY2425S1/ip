529dcd18f7bdeebc05d5479b5505cd60dedccaaf
engyuhan <e1136866@u.nus.edu>
2024-08-25 16:22:07 +0800

    feat: Add ExitCommand class

---
+++ src/main/java/duke/command/ExitCommand.java
@@ -0,0 +1,37 @@
+package duke.command;
+
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
+
+/**
+ * Represents an exit command.
+ */
+public class ExitCommand extends Command {
+    /**
+     * Constructor for an exit command.
+     */
+    public ExitCommand() {}
+
+    /**
+     * Writes the exit message.
+     *
+     * @param tasks The list of tasks for the command to interact with.
+     * @param ui The ui for the command to interact with.
+     * @param storage The storage for the command to interact with.
+     */
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.exit();
+    }
+
+    /**
+     * Returns true for whether the program should exit after execution.
+     *
+     * @return True for whether the program should exit after execution.
+     */
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+}
