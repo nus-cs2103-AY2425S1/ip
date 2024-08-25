641d62bb09a2b08363f74e9a82e220db4ab2b991
Chong Tze Zhao <47619363+chongtzezhao@users.noreply.github.com>
2024-08-25 17:08:40 +0800

    Added ability to store tasks in a list

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,24 +1,61 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList;
+
 
 public class Atlas {
     private static final String BOT_INDENT = "\t";
     private static final String SEP = BOT_INDENT + "___________________________________________";
     private static final String EXIT = "bye";
-    private static Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+    private static final Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+
+    private static final ArrayList<String> list = new ArrayList<>(); 
 
     public static void main(String[] args) {
         introduction();
         String input = null;
         do {
-            echo(input);
+            parse(input);
             input = listen();
         } while (!input.equals(EXIT));
         exit();
         scanner.close();
     }
 
-    private static final void separate() {
+    private static void separate() {
         System.out.println(SEP);
+    }
+
+    private static void parse(String input) {
+        if (input == null) {
+            return;
+        }
+        switch (input) {
+            case "bye": // do nothing: handled in main loop
+                break;
+            case "list":
+                listShow();
+                break;
+            default:  // add the item
+                listAdd(input);
+                listAddLog(input);
+        }
+    }
+
+    private static void listAdd(String item) {
+        list.add(item);
+    }
+
+    private static void listAddLog(String item) {
+        System.out.println(BOT_INDENT + "added: " + item);
+        separate();
+    }
+
+    private static void listShow() {
+        System.out.println(BOT_INDENT + "Here are the items in your list:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(BOT_INDENT + (i + 1) + ". " + list.get(i));
+        }
+        separate();
     }
 
     private static void introduction() {
