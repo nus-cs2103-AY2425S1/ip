854437789bef1c41a4276588f50abc4925a153ca
Rachael-Chan <e0775723@u.nus.edu>
2024-08-26 15:08:11 +0800

    feat: Tasks can be marked as done and undone

--- src/main/java/Spiderman.java
+++ src/main/java/Spiderman.java
@@ -5,7 +5,9 @@ public class Spiderman {
     public static void main(String[] args) {
         // Initialise scanner
         Scanner scan = new Scanner(System.in);  // Create a Scanner object
-        ArrayList<String> tasks = new ArrayList<>(); // Create an ArrayList object
+
+        // Initialise arrays for tasks
+        ArrayList<Task> taskList = new ArrayList<>();
 
         // Greeting users
         System.out.println("Hello! This is your friendly neighbourhood Spiderman.");
@@ -14,6 +16,7 @@ public class Spiderman {
         while (true) {
             // Get users input
             String input = scan.nextLine();
+            String[] splitInput = input.split("\\s+");
 
             // User types bye; program terminates
             if (input.equals("bye")) {
@@ -22,14 +25,32 @@ public class Spiderman {
 
             // User types list; display list
             if (input.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i+1) + ". " + tasks.get(i));
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i+1) + ". " + "[" + taskList.get(i).getStatusIcon() + "] " + taskList.get(i).getDescription());
                 }
                 continue;
             }
 
+            // User types mark; mark as done
+            if (splitInput[0].equals("mark")) {
+                int number = Integer.parseInt(splitInput[1]) - 1;
+                taskList.get(number).markAsDone();
+                System.out.println("Great! I've marked this task as done:");
+                System.out.println("[" + taskList.get(number).getStatusIcon() + "] " + taskList.get(number).getDescription());
+                continue;
+            }
+
+            // User types unmark; mark as not done
+            if (splitInput[0].equals("unmark")) {
+                int number = Integer.parseInt(splitInput[1]) - 1;
+                taskList.get(number).markAsNotDone();
+                System.out.println("OK, this task will be marked as not done yet:");
+                System.out.println("[" + taskList.get(number).getStatusIcon() + "] " + taskList.get(number).getDescription());
+                continue;
+            }
+
             // Store users input into tasks
-            tasks.add(input);
+            taskList.add(new Task(input));
 
             System.out.println("added: " + input);
         }
