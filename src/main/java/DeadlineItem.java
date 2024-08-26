85bdfc51569836f3fd483b32edaedab0e1bbabdd
Dong Qianbo <pencilate@outlook.com>
2024-08-26 14:17:21 +0800

    DeadlineItem.java: Update deadline field to be LocalDateTime type

--- src/main/java/DeadlineItem.java
+++ src/main/java/DeadlineItem.java
@@ -1,9 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * TodoItem represents a to-do entry with a specified deadline
  */
 public class DeadlineItem extends TodoItem{
     /** Deadline that specifies when the to-do is due **/
-    private final String deadline;
+    private final LocalDateTime deadline;
 
     /**
      * Creates a new to-do item with the specified content and deadline
@@ -11,15 +14,16 @@ public class DeadlineItem extends TodoItem{
      * @param content The task description of the to-do item
      * @param deadline The deadline of the to-do item
      */
-    public DeadlineItem(String content ,String deadline) {
+    public DeadlineItem(String content ,LocalDateTime deadline) {
         super(content);
-        this.deadline = deadline.trim();
+        this.deadline = deadline;
     }
 
     @Override
     public String toString() {
         String baseString = super.toString();
-        return String.format("%s (by: %s)", baseString.replaceFirst("T", "D"), this.deadline);
+        return String.format("%s (by: %s)", baseString.replaceFirst("T", "D"),
+                this.deadline.format(DateTimeFormatter.ofPattern("E dd MMM yyyy HHmm")));
 
     }
 
