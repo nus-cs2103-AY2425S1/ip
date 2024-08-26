d256e6a2350b87ed7a6977740a8ed2f89d46767b
naythee169 <jordan.mitchell.chan@gmail.com>
2024-08-26 22:38:18 +0800

    Add Javadocs comments for Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,4 +1,16 @@
+/**
+ * Provides the Parsing functionality that allows Delphi to process different types of input
+ *
+ * @author jordanchan
+ */
 public class Parser {
+    /**
+     * Checks if the first part of the input matches a certain string up to a given number of characters
+     * @param original
+     * @param index
+     * @param comparison
+     * @return
+     */
     public static boolean checkStringPrefix(String original, int index, String comparison) {
         // Ensure the index is within the bounds of the original string
         if (index > original.length()) {
