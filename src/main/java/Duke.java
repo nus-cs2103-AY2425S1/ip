d65bf5cf75e2dcf73465d5baa291dc8c85263fbc
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2024-08-27 07:22:15 +0800

    Task classes done. Mark as done is also done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,44 +1,64 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static ArrayList<String> list = new ArrayList<>();
-    public static String addHorizontalLines(String dialog) {
-        String res = "____________________________________________________________\n" +
-                dialog + "\n" +
-                "____________________________________________________________";
+    private static List<Task> taskList = new ArrayList<>();
+
+    public static String addHorizontalLinesAndIndentation(String dialog) {
+        String res =  "    ____________________________________________________________\n";
+        Scanner sc = new Scanner(dialog);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            res += "      " + line + "\n";
+        }
+        res += "    ____________________________________________________________";
         return res;
     }
 
     public static void addToList(String dialog) {
-        list.add(dialog);
-        String res = "____________________________________________________________\n" +
-                "added: " + dialog + "\n" +
-                "____________________________________________________________";
+        taskList.add(new Task(dialog));
+        String res = addHorizontalLinesAndIndentation("added: " + dialog );
         System.out.println(res);
     }
 
     public static void displayList() {
         String lString = "";
         int index = 1;
-        for (String s : list) {
-            if (index == list.size()) {
-                lString += index + ". " + s;
-                index++;
+        for (Task task : taskList) {
+            lString += String.valueOf(index) + "."  + task.toString();
+            if (index == taskList.size()) {
                 break;
             }
-            lString += index + ". " + s + "\n";
+            lString += "\n";
             index++;
         }
-        lString = addHorizontalLines(lString);
+        lString = addHorizontalLinesAndIndentation(lString);
         System.out.println(lString);
     }
 
+    public static void mark(int index) {
+        taskList.get(index - 1).setIsDone(true);
+        System.out.println(addHorizontalLinesAndIndentation(
+                        "Nice! I've marked this task as done:\n" +
+                                taskList.get(index - 1) + "\n"
+                ));
+    }
+
+    public static void unmark(int index) {
+        taskList.get(index - 1).setIsDone(false);
+        System.out.println(
+                addHorizontalLinesAndIndentation(
+                        "Nice! I've marked this task as undone:\n" +
+                                taskList.get(index - 1) + "\n"
+                )
+        );
+    }
 
     public static void main(String[] args) {
-        String hi = " Hello! I'm Foo\n" +
-                " What can I do for you?";
-        System.out.println(addHorizontalLines(hi));
+        String hi = "Hello! I'm Foo\n" +
+                "What can I do for you?";
+        System.out.println(addHorizontalLinesAndIndentation(hi));
         Scanner sc = new Scanner(System.in);
         while (true) {
             String line = sc.nextLine();
@@ -47,9 +67,17 @@ public class Duke {
             } else if (line.equals("list")) {
                 displayList();
                 continue;
+            } else if (line.length() >= 6 && line.substring(0, 4).equals("mark")) {
+                int index = Integer.parseInt(line.substring(5));
+                mark(index);
+                continue;
+            } else if (line.length() >= 8 && line.substring(0, 6).equals("unmark")) {
+                int index = Integer.parseInt(line.substring(7));
+                unmark(index);
+                continue;
             }
             addToList(line);
         }
-        System.out.println(addHorizontalLines("Bye. Hope to see you again soon!"));
+        System.out.println(addHorizontalLinesAndIndentation("Bye. Hope to see you again soon!"));
     }
 }
