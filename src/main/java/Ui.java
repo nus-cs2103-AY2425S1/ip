69f1ab4fb91cee9bf68a564e0eac94a5df6e0489
gohqingkhang <gohqingkhang@gmail.com>
2024-08-26 22:13:37 +0800

    Add readCommand method to take in user input Edit showLine method to add line divider string Add showWelcome method to print welcome message Add showError method to print error message

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Ui {
+    public String readCommand() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
     public void showLine() {
-        System.out.println("");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("""
+                \t Hello! I'm Delta, your favourite Task Management Chatbot!
+                \t What can I do for you?""");
+        showLine();
+    }
+
+    public void showError(String message) {
+        System.out.println("\t " + message);
     }
 }
