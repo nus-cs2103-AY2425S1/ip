3bec1e8550cc1b94fbdf70c4ce7c728f8a0777d3
Jonathan Loh <jonathanloh02@gmail.com>
2024-08-26 22:54:48 +0800

    Fix DeleteCommand

--- src/main/java/vuewee/command/DeleteCommand.java
+++ src/main/java/vuewee/command/DeleteCommand.java
@@ -1,18 +1,12 @@
 package vuewee.command;
 
-import vuewee.parser.CommandOption;
 import vuewee.parser.CommandParser;
-import vuewee.task.DeadlineTask;
 import vuewee.task.TaskList;
 import vuewee.TaskListUI;
-import vuewee.task.TaskLocalDate;
 
 class DeleteCommand extends Command {
   public void execute(TaskListUI ui, TaskList taskList, CommandParser parser) {
-    CommandOption<TaskLocalDate> byOption = new CommandOption<TaskLocalDate>("by", "date",
-        TaskLocalDate::parse);
-    parser.parse(true, false, byOption);
-    ui.addTask(new DeadlineTask(parser.getDescription(),
-        byOption.getParsedValue()));
+    parser.parse(true, true);
+    ui.deleteTask(parser.getIntParam());
   }
 }
