e2fb80af44abe032894fb1e6b3e5857a4abb57ee
LowXiSi <e0960422@u.nus.edu>
2024-08-26 13:09:36 +0800

    Add support for echo
    * Take in user input
    * Update chat display format
    * Echo user command
    * End chat on "bye" command

--- src/main/java/Loafy.java
+++ src/main/java/Loafy.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Loafy {
     public static void main(String[] args) {
-        System.out.println("Hellooo, I'm Loafy!\n" +
-                "What can I do for you? :D\n");
-        System.out.println("Byeee see you soon! ;)");
+        System.out.println("Loafy: Hellooo, I'm Loafy!\n" +
+                "Loafy: What can I do for you? :D\n");
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("You: ");
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println("Loafy: " + command + "\n");
+            }
+        }
+
+        System.out.println("Loafy: Byeee see you soon! ;)");
     }
 }
