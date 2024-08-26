e0086858e4a9a45a5d4db1cc14d464c17c7c9ffd
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-08-26 19:46:28 +0800

    Level 3 - Marking system completed

--- src/main/java/Dave.java
+++ src/main/java/Dave.java
@@ -13,6 +13,7 @@ public class Dave {
         Boolean[] booleanList = new Boolean[100];
         int dataIndex = 0;
         String state;
+        String statement;
         System.out.println("Hello from\n" + logo);
         System.out.println(horizontal);
         System.out.println("Hello! I'm Dave.");
@@ -20,9 +21,16 @@ public class Dave {
         System.out.println(horizontal);
         Scanner scanner = new Scanner(System.in);
         String userInput;
+
+        for (int i = 0; i < 100 ; i ++)
+        {
+            booleanList[i] = false;
+        }
+
         while (true)
         {
-            userInput = scanner.nextLine().trim();
+            String[] reply = scanner.nextLine().trim().split(" ");
+            userInput = reply[0];
 
             if (userInput.isEmpty()) {
                 continue;
@@ -39,17 +47,55 @@ public class Dave {
                 case "list":
                     System.out.println(horizontal);
                     for (int i = 0; i < dataIndex; i++) {
-                        state = booleanList[i] == null ? "" : "X";
-                        String statement = String.format("%d.[%s] %s", i+1, state, dataList[i]);
+                        state = booleanList[i] ? "X" : "";
+                        statement = String.format("%d.[%s] %s", i+1, state, dataList[i]);
+                        System.out.println(statement);
+                    }
+                    System.out.println(horizontal);
+                    break;
+
+                case "mark":
+                    if (reply.length > 0)
+                    {
+                        int taskNumber = Integer.parseInt(reply[1]);
+                        if (dataList[taskNumber - 1] == null)
+                        {
+                            System.out.println(horizontal);
+                            System.out.println("Oh no! You have entered invalid number. Please enter again! ");
+                            System.out.println(horizontal);
+                            continue;
+                        }
+                        booleanList[taskNumber - 1] = true;
+                        System.out.println(horizontal);
+                        System.out.println("Nice! I've marked this task as done:");
+                        statement = String.format("[X] %s", dataList[taskNumber - 1]);
+                        System.out.println(statement);
+                    }
+                    System.out.println(horizontal);
+                    break;
+
+                case "unmark":
+                    if (reply.length > 0)
+                    {
+                        int taskNumber = Integer.parseInt(reply[1]);
+                        if (dataList[taskNumber - 1] == null)
+                        {
+                            System.out.println(horizontal);
+                            System.out.println("Oh no! You have entered invalid number. Please enter again! ");
+                            System.out.println(horizontal);
+                            continue;
+                        }
+                        booleanList[taskNumber - 1] = false;
+                        System.out.println(horizontal);
+                        System.out.println("Ok, I've marked this task as not done yet:");
+                        statement = String.format("[] %s", dataList[taskNumber - 1]);
                         System.out.println(statement);
                     }
                     System.out.println(horizontal);
                     break;
 
                 default:
-
                     dataList[dataIndex] = userInput;
-                    booleanList[dataIndex] = true;
                     dataIndex ++;
                     System.out.println(horizontal);
                     System.out.printf("added: %s%n", userInput);
