f075bf6d59a8c620a90d5773deb26cf5ce71c2fb
Quasant <benjaminthechin@gmail.com>
2024-08-26 21:11:29 +0800

    Renamed all instances of Duke to Alvis. Modified initial greeting and goodbye.

---
+++ src/main/java/Alvis.java
@@ -0,0 +1,6 @@
+public class Alvis {
+    public static void main(String[] args) {
+        System.out.println("So you require my assistance?");
+        System.out.println("Understood.");
+    }
+}
