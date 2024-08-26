60e495dfe9cb03a7f8570ff287aef4f6bdfdb181
chashaobao123 <chooi.shaoxian@gmail.com>
2024-08-26 15:26:59 +0800

    ip_wk2_level-0

---
+++ src/main/java/LittleMissHelpful.java
@@ -0,0 +1,21 @@
+public class LittleMissHelpful {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+
+
+        String name = "Little Miss Helpful";
+        String lineBreak = "___________________________________";
+
+        String greeting = "Hello! I'm " + name + ".\nWhat can I do for you?";
+
+        String exitLine = "Bye. Hope to see you again soon!";
+
+        System.out.println(lineBreak);
+        System.out.println(greeting + "\n" + lineBreak);
+        System.out.println(exitLine + "\n" + lineBreak);
+    }
+}
