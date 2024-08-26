b8d2a4cbad4e390884f1c7f42f719e9f8d3e9cd1
calebyyy <caleb.ng.yy@gmail.com>
2024-08-26 13:00:45 +0800

    Add JavaDoc for ListCommand.java

--- src/main/java/calebyyy/commands/ListCommand.java
+++ src/main/java/calebyyy/commands/ListCommand.java
@@ -4,11 +4,26 @@ import calebyyy.Calebyyy;
 import calebyyy.TaskList;
 import calebyyy.Ui;
 
+/**
+ * Represents a command to list all tasks.
+ */
 public class ListCommand extends Command {
+    /**
+     * Constructor for ListCommand.
+     * 
+     * @param calebyyy The main Calebyyy object.
+     * @param ui The Ui object responsible for user interaction.
+     * @param taskList The TaskList object responsible for storing tasks.
+     */
     public ListCommand(Calebyyy calebyyy, Ui ui, TaskList taskList) {
         super(calebyyy, ui, taskList);
     }
 
+    /**
+     * Lists all tasks.
+     * 
+     * @param input The user input.
+     */
     @Override
     public void execute(String input) {
         taskList.listTasks();
