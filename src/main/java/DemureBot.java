6a2e0de7a6f782aae3be285061287d27a30919cb
Potatoad88 <>
2024-08-26 17:23:22 +0800

    Add Storage class

--- src/main/java/DemureBot.java
+++ src/main/java/DemureBot.java
@@ -1,14 +1,13 @@
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.File;
-import java.io.FileWriter;
+import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
 
 public class DemureBot {
     private static TaskList list;
+    private static Storage storage;
 
     /**
      * Checks if user command is valid and executes the command.
@@ -187,122 +186,7 @@ public class DemureBot {
             LocalDateTime dateTimeParsed = LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
             return dateTimeParsed.format(DateTimeFormatter.ofPattern("MMM dd yyyy, HHmm"));
         } catch (DateTimeParseException e) {
-            throw new DemureBotException("Invalid date/time format. Please enter date in yyyy-MM-dd, time in HHmm, or date and time in yyyy-MM-dd HHmm format.\n");
-        }
-    }
-
-    /**
-     * Checks if the folder exists.
-     *
-     * @param folderPath Path of folder containing the file with the saved data
-     */
-    private static void checkFolder(String folderPath) {
-        File folder = new File(folderPath);
-        if (!folder.exists()) {
-            boolean isFolderCreated = folder.mkdirs();
-            if (!isFolderCreated) {
-                System.out.println("Failed to create directory: " + folderPath);
-            }
-        }
-    }
-
-    /**
-     * Returns a Boolean indicating if the file exists.
-     *
-     * @param filePath Path of file containing saved data
-     * @return Boolean indicating if the file exists
-     */
-    private static boolean checkFile(String filePath) {
-        File file = new File(filePath);
-        if (!file.exists()) {
-            try {
-                boolean isFileCreated = file.createNewFile();
-                if (!isFileCreated) {
-                    System.out.println("Failed to create file: " + filePath);
-                }
-                return false;
-            } catch (IOException e) {
-                System.out.println("Error creating file: " + e.getMessage());
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Returns a formatted string of the task to be saved.
-     *
-     * @param task Task to be saved.
-     * @return Formatted string of the task to be saved
-     */
-    private static String formatTask(Task task) {
-        String[] parts = task.toString().split("]", 3);
-        String type = parts[0].substring(1, 2);
-        String status = parts[1].charAt(1) == 'X' ? "1" : "0";
-        String description = parts[2].trim();
-
-        if (type.equals("D")) {
-            int byIndex = description.indexOf("(by: ");
-            String by = description.substring(byIndex + 5, description.length() - 1);
-            description = description.substring(0, byIndex - 1);
-            return type + " | " + status + " | " + description + " | " + by;
-        } else if (type.equals("E")) {
-            int fromIndex = description.indexOf("(from: ");
-            int toIndex = description.indexOf(" to: ");
-            String from = description.substring(fromIndex + 7, toIndex);
-            String to = description.substring(toIndex + 5, description.length() - 1);
-            description = description.substring(0, fromIndex - 1);
-            return type + " | " + status + " | " + description + " | " + from + " | " + to;
-        } else {
-            return type + " | " + status + " | " + description;
-        }
-    }
-
-    /**
-     * Returns a Task created from saved data.
-     *
-     * @param line Line of text read from saved data.
-     * @return Task created from saved data.
-     * @throws DemureBotException If the saved task has invalid format.
-     */
-    private static Task getTask(String line) throws DemureBotException {
-        String[] split = line.split(" \\| ");
-        validateTaskFormat(split, 3);
-        String type = split[0];
-        boolean isDone = split[1].equals("1");
-        String description = split[2];
-        Task task = null;
-        switch (type) {
-        case "T":
-            task = new Todo(description, isDone);
-            break;
-        case "D":
-            validateTaskFormat(split, 4);
-            String by = split[3];
-            task = new Deadline(description, by, isDone);
-            break;
-        case "E":
-            validateTaskFormat(split, 5);
-            String from = split[3];
-            String to = split[4];
-            task = new Event(description, from, to, isDone);
-            break;
-        }
-        if (task == null) {
-            throw new DemureBotException("Saved task has invalid format");
-        }
-        return task;
-    }
-
-    /**
-     * Throws an exception if the array length is less than the expected length.
-     *
-     * @param split Array of strings.
-     * @param expectedLength Expected length of array.
-     * @throws DemureBotException If the array length is less than the expected length.
-     */
-    private static void validateTaskFormat(String[] split, int expectedLength) throws DemureBotException {
-        if (split.length < expectedLength) {
-            throw new DemureBotException("Saved task has invalid format");
+            throw new DemureBotException("Invalid date/time format. Please enter date and time in yyyy-MM-dd HHmm format.\n");
         }
     }
 
@@ -311,8 +195,6 @@ public class DemureBot {
 
         // check if user ended session
         boolean isFinished = false;
-        // list of items to do
-        DemureBot.list = new TaskList(new ArrayList<Task>());
 
         // introduction to chatbot
         System.out.println("""
@@ -324,27 +206,30 @@ public class DemureBot {
             """
         );
 
+        DemureBot.storage = new Storage();
+
         // check if data folder exists if not create it
         String folderPath = "./data";
-        checkFolder(folderPath);
+        DemureBot.storage.checkFolder(folderPath);
 
         // check if saved data exists if not create it
         String filePath = "./data/tasks.txt";
-        boolean savedDataExists = checkFile(filePath);
+        boolean savedDataExists = DemureBot.storage.checkFile(filePath);
 
         // load saved data if exists
         if (savedDataExists) {
-            try (Scanner fileScanner = new Scanner(new File(filePath))) {
-                while (fileScanner.hasNext()) {
-                    String line = fileScanner.nextLine();
-                    Task task = getTask(line);
-                    list.addTask(task);
-                }
+            try {
+                // fetch list of items to do
+                DemureBot.list = new TaskList(DemureBot.storage.load(filePath));
             } catch (IOException e) {
                 System.out.println("Error reading file: " + e.getMessage());
             } catch (DemureBotException e) {
                 System.out.println("Error loading task: " + e.getMessage());
             }
+        }
+
+        if (list == null) {
+            DemureBot.list = new TaskList(new ArrayList<>());
         }
 
         // while user hasn't ended session
@@ -379,25 +264,7 @@ public class DemureBot {
             """
         );
 
-        // save the tasks
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(filePath);
-            for (int i = 0; i < list.getSize(); i++) {
-                Task task = list.getTask(i);
-                String formattedTask = formatTask(task);
-                writer.write(formattedTask + "\n");
-            }
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    System.out.println("Error closing FileWriter: " + e.getMessage());
-                }
-            }
-        }
+        // save task list
+        DemureBot.storage.save(filePath, DemureBot.list);
     }
 }
