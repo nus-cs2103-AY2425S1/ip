c3d677aeeca72f9f1ef9034fb92234787028ae81
quahkk <quahkaikiat1@gmail.com>
2024-08-26 14:10:25 +0800

    Level 1 commit. Made changes to add personality and separated methods.

--- src/main/java/Nex.java
+++ src/main/java/Nex.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
 public class Nex {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Nex.");
+        Nex nex = new Nex();
+        nex.run();
+    }
+
+    public void run() {
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                exit();
+                break;
+            }
+            echo(input);
+        }
+
+        scanner.close();
+    }
+    public void greet() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Nex, your friendly personal chat buddy.");
         System.out.println("What can I do for you?");
-        System.out.println("");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public void echo(String input) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Did I hear you say \"" + input + "\"?");
+        System.out.println("Is there anything else on your mind?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public void exit() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Aw, it's time for you to go huh?");
+        System.out.println("Bye then! Hope to catch you again soon!");
+        System.out.println(HORIZONTAL_LINE);
     }
 }
