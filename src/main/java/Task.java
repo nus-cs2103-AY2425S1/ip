c3662efff46f68d6e120cc930e5c969a9f64ebb1
LowXiSi <e0960422@u.nus.edu>
2024-08-26 16:23:07 +0800

    Add setter for task status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,21 @@
 class Task {
     private String name;
+    private boolean isDone;
 
     Task(String name) {
         this.name = name;
+        this.isDone = false;
     }
+
+    public void changeStatus(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
     public String toString() {
-        return this.name;
+        return String.format("[%s] %s", getStatusIcon(), this.name);
     }
 }
