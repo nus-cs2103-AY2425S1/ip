2568a7041d058bcb39952565f20bf653f7f991f5
jarrellc1 <Jarrell.chia@gmail.com>
2024-08-26 13:41:58 +0800

    Refactor code to follow java coding standard Remove unused Ui statements

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -12,11 +12,6 @@ public class Ui {
     }
 
     public void showWelcome() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Jar\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
