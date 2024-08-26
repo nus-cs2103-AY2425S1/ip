48f9b91fc0d5124dde1f7b136af084cc91ff71de
chashaobao123 <chooi.shaoxian@gmail.com>
2024-08-26 20:45:55 +0800

    ip wk2 level-2

--- src/main/java/LittleMissHelpful.java
+++ src/main/java/LittleMissHelpful.java
@@ -16,22 +16,38 @@ public class LittleMissHelpful {
         System.out.println(greeting);
         System.out.println(lineBreak);
 
-        String input = "";
+        String[] list = new String[100];
 
-            while (true) { //there is continuous inputs
-                input = br.readLine();
+        try {
+            int counter = 0;
+            while (true) { //there is continuous input
+                String input = br.readLine();
 
-                // Check if the user input is "bye"
+                // Check if the user input is "bye" -> exit
                 if (input.equalsIgnoreCase("bye")) {
                     System.out.println(exitLine);
                     System.out.println(lineBreak);
-                    break; // Exit the loop if the user types "bye"
+                    break; // Exit the loop
                 }
 
-                // Echos commands
+                // Check if the user input is "list" -> print list
+                if (input.equalsIgnoreCase("list")) {
+                    System.out.println(lineBreak);
+                    for (int i = 0 ; i < counter ; i++) {
+                        int listNumber = i + 1;
+                        String listItem = list[i];
+                        System.out.println(listNumber + ". " + listItem);
+                    }
+                    System.out.println(lineBreak);
+                    continue;
+                }
+
+                // Adds commands to list
+                list[counter] = input;
                 System.out.println(lineBreak);
                 System.out.println(input);
                 System.out.println(lineBreak);
+                counter++;
             }
         } catch (IOException e) {
             System.out.println("An error occurred while reading input.");
