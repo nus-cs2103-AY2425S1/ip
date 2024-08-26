1ed5df6815840fb4ecc5e3cd74d9ab9e185341b4
gohqingkhang <gohqingkhang@gmail.com>
2024-08-27 00:57:09 +0800

    Add overloaded constructor to create empty TaskList or loaded TaskList with contents given by ArrayList tasks Add getter method to return tasks in TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
 }
