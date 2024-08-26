eb9416a620a5b597ee5ee98d0fc0674bbac46bd1
brendanng7 <brendanngjj@gmail.com>
2024-08-26 15:27:50 +0800

    Ui.java: Add basic skeleton and functions

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class UI {
+public class Ui {
+    public void showLoadingError() {
+
+    }
+    public void printHorizontalLine() {
+        System.out.println("-------------------------------------");
+    }
+    public void greet() {
+        printHorizontalLine();
+        String greetingMessage = "Hello! I'm Soju\nWhat can I do for you?";
+        System.out.println(greetingMessage);
+        printHorizontalLine();
+    }
+    public void exit() {
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(exitMessage);
+        printHorizontalLine();
+    }
 }
