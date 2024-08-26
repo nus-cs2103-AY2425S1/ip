993eb1baabccc65d071878c669c531abf0fa41eb
CliffKoes <cliffkoes@gmail.com>
2024-08-27 03:42:10 +0800

    Fix switch-case indentation

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -19,124 +19,113 @@ public class Bibi {
                     + "How can I help you?");
         printHorizontalLine();
         String cmd;
+        int index;
 
         while (scanner.hasNext()) {
             // Preconfigured commands
             switch (cmd = scanner.next()) {
-                case "bye": {
-                    // Exit
-                    printHorizontalLine();
-                    System.out.println("See you soon :3");
-                    printHorizontalLine();
-                    return;
+            case "bye":
+                // Exit
+                printHorizontalLine();
+                System.out.println("See you soon :3");
+                printHorizontalLine();
+                return;
+            case "list":
+                printHorizontalLine();
+                printTaskList(tasks);
+                printHorizontalLine();
+                break;
+            case "mark":
+                printHorizontalLine();
+                if (!(cmd = scanner.nextLine().trim()).matches("\\d+")) {
+                    System.out.println("Invalid command syntax: Please use \"mark <int>\"");
+                } else if ((index = Integer.parseInt(cmd)) - 1 >= tasks.size() || index <= 0) {
+                    System.out.println("Invalid task index");
+                } else {
+                    System.out.printf("Alrighty, marked the following task as done:%n");
+                    Task t = tasks.get(index - 1);
+                    t.markAsDone();
+                    System.out.println(t);
                 }
-                case "list": {
-                    printHorizontalLine();
-                    printTaskList(tasks);
-                    printHorizontalLine();
-                    break;
+                printHorizontalLine();
+                break;
+            case "unmark":
+                printHorizontalLine();
+                if (!(cmd = scanner.nextLine().trim()).matches("\\d+")) {
+                    System.out.println("Invalid command syntax: Please use \"unmark <int>\"");
+                } else if ((index = Integer.parseInt(cmd)) - 1 >= tasks.size() || index <= 0) {
+                    System.out.println("Invalid task index");
+                } else {
+                    System.out.printf("Oops, we'll get 'em next time:%n");
+                    Task t = tasks.get(index - 1);
+                    t.markAsNotDone();
+                    System.out.println(t);
                 }
-                case "mark": {
-                    printHorizontalLine();
-                    int index;
-                    if (!scanner.hasNext()) {
-                        System.out.println("Invalid command syntax: Please use \"mark <int>\"");
-                    } else if ((index = scanner.nextInt()) - 1 >= tasks.size() || index <= 0) {
-                        System.out.println("Invalid task index");
-                    } else {
-                        System.out.printf("Alrighty, marked the following task as done:%n");
-                        Task t = tasks.get(index - 1);
-                        t.markAsDone();
-                        System.out.println(t);
-                    }
-                    printHorizontalLine();
-                    break;
-                }
-                case "unmark": {
-                    printHorizontalLine();
-                    int index;
-                    if (!scanner.hasNext()) {
-                        System.out.println("Invalid command syntax: Please use \"mark <int>\"");
-                    } else if ((index = scanner.nextInt()) - 1 >= tasks.size() || index <= 0) {
-                        System.out.println("Invalid task index");
-                    } else {
-                        System.out.printf("Oops, we'll get 'em next time:%n");
-                        Task t = tasks.get(index - 1);
-                        t.markAsNotDone();
-                        System.out.println(t);
-                    }
-                    printHorizontalLine();
-                    break;
-                }
-                case "todo": {
-                    printHorizontalLine();
-                    if (!(cmd = scanner.nextLine()).matches(".+")) {
-                        System.out.println("Invalid todo syntax: Please use \"todo <description>\"");
-                    } else {
-                        ToDo td = new ToDo(cmd.stripIndent());
-                        addToTaskList(tasks, td);
+                printHorizontalLine();
+                break;
+            case "todo":
+                printHorizontalLine();
+                if (!(cmd = scanner.nextLine()).matches(".+")) {
+                    System.out.println("Invalid todo syntax: Please use \"todo <description>\"");
+                } else {
+                    ToDo td = new ToDo(cmd.stripIndent());
+                    addToTaskList(tasks, td);
 
-                        // Console
-                        System.out.printf("added: \"%s\" to task list%n", td);
-                        System.out.printf("You now have %d task(s) to do%n", tasks.size());
-                    }
-                    printHorizontalLine();
-                    break;
-                }
-                case "deadline": {
-                    printHorizontalLine();
-                    if (!(cmd = scanner.nextLine()).matches(".+ /by .+")) {
-                        System.out.println("Invalid deadline syntax: Please use \"deadline <description> /by <deadline>\"");
-                    } else {
-                        String[] input = cmd.split(" /by ");
-                        Deadline dl = new Deadline(input[0].stripIndent(), input[1]);
-                        addToTaskList(tasks, dl);
-
-                        // Console
-                        System.out.printf("added: \"%s\" to task list%n", dl);
-                        System.out.printf("You now have %d task(s) to do%n", tasks.size());
-                    }
-                    printHorizontalLine();
-                    break;
-                }
-                case "event": {
-                    printHorizontalLine();
-                    if (!(cmd = scanner.nextLine()).matches(".+ /from .+ /to .+")) {
-                        System.out.println("Invalid event syntax: Please use \"event <description> /from <time> /to <time>\"");
-                    } else {
-                        String[] input = cmd.split(" /from ");
-                        String[] interval = input[1].split(" /to ");
-                        Event e = new Event(input[0].stripIndent(), interval[0], interval[1]);
-                        tasks.add(e);
-
-                        // Console
-                        System.out.printf("added: \"%s\" to task list%n", e);
-                        System.out.printf("You now have %d task(s) to do%n", tasks.size());
-                    }
-                    printHorizontalLine();
-                    break;
-                }
-                case "remove": {
-                    printHorizontalLine();
-                    int index;
-                    if (!scanner.hasNextInt()) {
-                        System.out.println("Invalid event syntax: Please use \"remove <index>\"");
-                    } else if ((index = scanner.nextInt()) > tasks.size() || index <= 0) {
-                        System.out.println("Invalid task index");
-                    } else {
-                        System.out.println("You will never see this task ever again >:(");
-                        System.out.printf("Removed %s from task list%n", removeFromTaskList(tasks, index).toString());
-                        System.out.printf("You now have %d task(s) to do%n", tasks.size());
-                    }
-                    printHorizontalLine();
-                    break;
-                }
-                default: {
                     // Console
-                    printHorizontalLine();
-                    System.out.printf("%s is an unknown command%n", cmd);
-                    printHorizontalLine();
+                    System.out.printf("added: \"%s\" to task list%n", td);
+                    System.out.printf("You now have %d task(s) to do%n", tasks.size());
                 }
+                printHorizontalLine();
+                break;
+            case "deadline":
+                printHorizontalLine();
+                if (!(cmd = scanner.nextLine()).matches(".+ /by .+")) {
+                    System.out.println("Invalid deadline syntax: Please use \"deadline <description> /by <deadline>\"");
+                } else {
+                    String[] input = cmd.split(" /by ");
+                    Deadline dl = new Deadline(input[0].stripIndent(), input[1]);
+                    addToTaskList(tasks, dl);
+
+                    // Console
+                    System.out.printf("added: \"%s\" to task list%n", dl);
+                    System.out.printf("You now have %d task(s) to do%n", tasks.size());
+                }
+                printHorizontalLine();
+                break;
+            case "event":
+                printHorizontalLine();
+                if (!(cmd = scanner.nextLine()).matches(".+ /from .+ /to .+")) {
+                    System.out.println("Invalid event syntax: Please use \"event <description> /from <time> /to <time>\"");
+                } else {
+                    String[] input = cmd.split(" /from ");
+                    String[] interval = input[1].split(" /to ");
+                    Event e = new Event(input[0].stripIndent(), interval[0], interval[1]);
+                    tasks.add(e);
+
+                    // Console
+                    System.out.printf("added: \"%s\" to task list%n", e);
+                    System.out.printf("You now have %d task(s) to do%n", tasks.size());
+                }
+                printHorizontalLine();
+                break;
+            case "remove":
+                printHorizontalLine();
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Invalid event syntax: Please use \"remove <index>\"");
+                } else if ((index = scanner.nextInt()) > tasks.size() || index <= 0) {
+                    System.out.println("Invalid task index");
+                } else {
+                    System.out.println("You will never see this task ever again >:(");
+                    System.out.printf("Removed %s from task list%n", removeFromTaskList(tasks, index).toString());
+                    System.out.printf("You now have %d task(s) to do%n", tasks.size());
+                }
+                printHorizontalLine();
+                break;
+            default:
+                // Console
+                printHorizontalLine();
+                System.out.printf("%s is an unknown command%n", cmd);
+                printHorizontalLine();
             }
         }
     }
@@ -161,6 +150,6 @@ public class Bibi {
     }
 
     private static void printHorizontalLine() {
-        System.out.println("--------------------------------");
+        System.out.println("------------------------------------------------------------");
     }
 }
