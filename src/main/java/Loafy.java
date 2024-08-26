f2989bf6025941df996b34f1280699c9ad9d4b54
LowXiSi <e0960422@u.nus.edu>
2024-08-26 16:28:14 +0800

    Support for task marking

--- src/main/java/Loafy.java
+++ src/main/java/Loafy.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class Loafy {
     public static void main(String[] args) {
-        System.out.println("Loafy: Hellooo, I'm Loafy!\n" +
-                "Loafy: What can I do for you? :D\n");
+
+        Consumer<String> reply = msg -> System.out.println("Loafy: " + msg + "\n");
+        reply.accept("Hellooo, I'm Loafy!");
+        reply.accept("What can I do for you? :D");
+
         Scanner input = new Scanner(System.in);
         TaskList tl = new TaskList();
 
         while (true) {
             System.out.print("You: ");
             String command = input.nextLine();
+            String[] arr = command.split(" ");
+
             if (command.equals("bye")) {
                 break;
             } else if (command.equals("list")) {
-                System.out.println(tl.view());
+                reply.accept(tl.view());
+            } else if (arr[0].equals("mark")
+                    || arr[0].equals("unmark")) {
+                // set isDone to true if "mark", false if "unmark"
+                boolean isDone = arr[0].equals("mark");
+                int taskId = Integer.parseInt(arr[1]);
+                String msg = tl.markTask(isDone, taskId);
+                reply.accept(msg);
             } else {
                 Task task = new Task(command);
                 String msg = tl.add(task);
-                System.out.println("Loafy: " + msg + "\n");
+                reply.accept(msg);
             }
         }
 
-        System.out.println("Loafy: Byeee see you soon! ;)");
+        reply.accept("Byeee see you soon! ;)");
     }
 }
