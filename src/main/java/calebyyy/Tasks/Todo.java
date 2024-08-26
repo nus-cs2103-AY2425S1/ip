f2a6d141f8a231e1f23c293c05e9722b4f7a94b7
calebyyy <caleb.ng.yy@gmail.com>
2024-08-26 13:06:12 +0800

    Add JavaDoc for Todo.java

--- src/main/java/calebyyy/Tasks/Todo.java
+++ src/main/java/calebyyy/Tasks/Todo.java
@@ -1,14 +1,33 @@
 package calebyyy.Tasks;
+
+/**
+ * Represents a todo task.
+ */
 public class Todo extends Task {
+    /**
+     * Constructor for Todo.
+     *
+     * @param description The description of the todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns the string representation of the todo task.
+     *
+     * @return The string representation of the todo task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
+    /**
+     * Returns the string representation of the todo task in save format.
+     *
+     * @return The string representation of the todo task in save format.
+     */
     @Override
     public String toSaveFormat() {
         return "T" + super.toSaveFormat();
