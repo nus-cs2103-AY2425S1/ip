ba0d665b3e7b0ec7518d3ff927cb2b66659b8f70
LowXiSi <e0960422@u.nus.edu>
2024-08-26 19:25:34 +0800

    Clean up Loafy class

--- src/main/java/Loafy.java
+++ src/main/java/Loafy.java
@@ -1,14 +1,9 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.Consumer;
 
 public class Loafy {
     public static void main(String[] args) {
-
-        Consumer<String> reply = msg -> System.out.println("Loafy: " + msg + "\n");
-        reply.accept("Hellooo, I'm Loafy!");
-        reply.accept("What can I do for you? :D");
-
+        greeting();
         Scanner input = new Scanner(System.in);
         TaskList tl = new TaskList();
 
@@ -20,27 +15,26 @@ public class Loafy {
             if (command.equals("bye")) {
                 break;
             } else if (command.equals("list")) {
-                reply.accept(tl.view());
+                reply(tl.view());
             } else if (arr[0].equals("mark")
                     || arr[0].equals("unmark")) {
                 // set isDone to true if "mark", false if "unmark"
                 boolean isDone = arr[0].equals("mark");
                 int taskId = Integer.parseInt(arr[1]);
                 String msg = tl.markTask(isDone, taskId);
-                reply.accept(msg);
+                reply(msg);
             } else if (arr[0].equals("todo")) {
                 String name = joinRange(arr, 1, arr.length);
                 Task task = new Todo(name);
                 String msg = tl.add(task);
-                reply.accept(msg);
+                reply(msg);
             } else if (arr[0].equals("deadline")) {
                 int i = Arrays.asList(arr).indexOf("/by");
-                System.out.println(i);
                 String name = joinRange(arr, 1, i);
                 String date = joinRange(arr, i + 1, arr.length);
                 Task task = new Deadline(name, date);
                 String msg = tl.add(task);
-                reply.accept(msg);
+                reply(msg);
             } else if (arr[0].equals("event")) {
                 int fromIndex = Arrays.asList(arr).indexOf("/from");
                 int toIndex = Arrays.asList(arr).indexOf("/to");
@@ -49,15 +43,30 @@ public class Loafy {
                 String to = joinRange(arr, toIndex + 1, arr.length);
                 Task task = new Event(name, from, to);
                 String msg = tl.add(task);
-                reply.accept(msg);
+                reply(msg);
             }
         }
 
-        reply.accept("Byeee see you soon! ;)");
+        exit();
     }
 
     static String joinRange(String[] arr, int start, int end) {
         String[] subArr = Arrays.copyOfRange(arr, start, end);
         return String.join(" ", subArr);
     }
+
+    static void reply(String str) {
+        System.out.println("Loafy: " + str + "\n");
+    }
+
+    static void greeting() {
+        reply("Hellooo, I'm Loafy!");
+        reply("What can I do for you? :D");
+    }
+
+    static void exit() {
+        reply("Byeee see you soon! ;)");
+    }
+
+
 }
