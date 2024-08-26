122bdbadcde1ffb1b0297fd69f66893736e26f38
seantham21 <seantham21@gmail.com>
2024-08-26 16:58:04 +0800

    Level 2; Add, List

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Spike {
 
@@ -18,14 +20,25 @@ public class Spike {
 
     public static void echo() {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> toDoList = new ArrayList<>(100);
         while (true) {
             String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
                 byeMessage();
                 break;
-            } else {
+            } else if (input.equals("list")) {
                 System.out.println("     _________________________________________________________");
-                System.out.println("     " + input);
+                int counter = 0;
+                for (String item : toDoList) {
+                    counter++;
+                    System.out.println("     " + counter + ". " + item);
+                }
+                System.out.println("     _________________________________________________________");
+            } else {
+                // addTask();
+                toDoList.add(input);
+                System.out.println("     _________________________________________________________");
+                System.out.println("     " + "added: " + input);
                 System.out.println("     _________________________________________________________");
             }
         }
