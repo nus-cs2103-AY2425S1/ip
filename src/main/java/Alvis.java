d3251a10337510dcee3c658b4e81f73274942c0c
Quasant <benjaminthechin@gmail.com>
2024-08-26 21:35:13 +0800

    Modified Alvis to repeat user input and terminate if 'bye' is entered

--- src/main/java/Alvis.java
+++ src/main/java/Alvis.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Alvis {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("So you require my assistance?");
-        System.out.println("Understood.");
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Understood.");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
