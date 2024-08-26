51e0520aa3da45d1cbdbb4a7992d1571bf7c8e7a
keithxun <keithonghx@gmail.com>
2024-08-26 11:49:47 +0800

    Improve OOP

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void deleteTask(int index) throws BopesException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new BopesException("Error: The task index is out of range.");
+        }
+        tasks.remove(index);
+    }
+
+    public Task markTask(int index) throws BopesException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new BopesException("Error: The task index is out of range.");
+        }
+        Task task = tasks.get(index);
+        task.markAsDone();
+        return task;
+    }
+
+    public Task unmarkTask(int index) throws BopesException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new BopesException("Error: The task index is out of range.");
+        }
+        Task task = tasks.get(index);
+        task.markAsUndone();
+        return task;
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public void printTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
