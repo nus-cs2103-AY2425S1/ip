46f4e60d3fdf45ab40d48dfc010e61442ae20f77
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-08-26 21:22:10 +0800

    create branch-level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,22 @@
-public class Deadline extends Task {
-    private String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    private final LocalDate by;
+
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + (isDone ? "[X] " : "[ ] ") + desc + " (by: " + this.by + ")";
+        return "[D]" + (isDone ? "[X] " : "[ ] ") + desc + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     @Override
     public String toFile() {
-        return "D | " + (isDone ? "1" : "0") + " | " + desc + " | " + this.by;
+        return "D | " + (isDone ? "1" : "0") + " | " + desc + " | " + this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 }
