f816b6cc870915eb1e418efca43270489989bcdc
ethanwangkangen <ethanwangkangen@gmail.com>
2024-08-26 15:03:18 +0800

    Add dateTime feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,28 @@
 package main.java;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
     protected String from;
     protected String to;
+
+    protected LocalDateTime dtFrom;
+    protected LocalDateTime dtTo;
 
     public Event (String from, String to, String desc) {
         super('E',desc);
         this.from = from;
         this.to = to;
+        this.dtFrom = parseDateTime(from);
+        this.dtTo = parseDateTime(to);
     }
 
     @Override
     public String getDesc() {
-        return super.getDesc() + " (from: " + from + " to: " + to + ")";
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
+        String fromOut = (dtFrom == null) ? from : dtFrom.format(outputFormatter);
+        String toOut = (dtTo == null) ? to : dtTo.format(outputFormatter);
+        return super.getDesc() + " (from: " + fromOut + " to: " + toOut + ")";
     }
 }
