f9e0986220edea93a25369316e9f3a04012d872c
LowXiSi <e0960422@u.nus.edu>
2024-08-26 12:46:35 +0800

    Replace Duke with Loafy
    * Rename main class to Loafy
    * Add greeting and exit statements

---
+++ src/main/java/Loafy.java
@@ -0,0 +1,7 @@
+public class Loafy {
+    public static void main(String[] args) {
+        System.out.println("Hellooo, I'm Loafy!\n" +
+                "What can I do for you? :D\n");
+        System.out.println("Byeee see you soon! ;)");
+    }
+}
