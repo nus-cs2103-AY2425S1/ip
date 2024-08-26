66cbc03774e4a710b82c7a6bad44c385893f971c
JunLong Ling <>
2024-08-26 18:59:10 +0800

    add deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,35 @@
-class Deadline extends Task {
-    protected String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+class Deadline extends Task {
+    private LocalDateTime by;
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+
+    public Deadline(String description, LocalDateTime by) {
         super(description, TaskType.DEADLINE);
         this.by = by;
     }
 
     @Override
     public String toFileFormat() {
-        return String.format("D | %d | %s | %s",
-                isDone ? 1 : 0,
-                description,
-                by);
+        if (by != null) {
+            return String.format("D | %d | %s | %s",
+                    isDone ? 1 : 0,
+                    description,
+                    by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm")));
+        } else {
+            return String.format("D | %d | %s | [Invalid Date]",
+                    isDone ? 1 : 0,
+                    description);
+        }
     }
+
     @Override
     public String toString() {
-        return super.toString() + " (by: " + by + ")";
+        if (by != null) {
+            return super.toString() + " (by: " + by.format(OUTPUT_FORMATTER) + ")";
+        } else {
+            return super.toString() + " (by: [Invalid Date])";
+        }
     }
 }
