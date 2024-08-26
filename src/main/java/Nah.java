df165fb8d98c943ba2125462f62d7ecfe28b37a0
blackpanther9229 <hoangnguyen09022004@gmail.com>
2024-08-26 13:02:10 +0800

    Create Nah.txt as hard disk memory and apply in Nah.java

--- src/main/java/Nah.java
+++ src/main/java/Nah.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
@@ -13,7 +17,7 @@ public class Nah {
     private enum Command {BYE, TODO, DEADLINE, EVENT, LIST, DELETE, MARK, UNMARK, UNKNOWN};
     private LinkedList<Task> task = new LinkedList<Task>();
     private int taskCount = 0;
-
+    private String hardDisk = Paths.get("D:","cs2103T_week_2", "Data", "Nah.txt").toString();
     private void greet() {
         System.out.println(greetLine);
     }
@@ -51,51 +55,72 @@ public class Nah {
 
         }
     }
+
+    private static void writeToFile(String filePath, String textToAdd) {
+        try {
+            FileWriter fw = new FileWriter(filePath);
+            fw.write(textToAdd);
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage() +"\n");
+        }
+    }
+
+    private String readTask() {
+        String s = " Here are the tasks in your list:\n";
+        for (int i = 1; i <= taskCount; i ++) {
+            s += " " + i + ". " + task.get(i - 1).toString() + "\n";
+        }
+        return s;
+    }
+    private void rewrite() {
+        writeToFile(hardDisk, readTask());
+    }
     private void exit() {
         System.out.println(byeLine);
         System.out.println("____________________________________________________________\n");
+
     }
 
-    private void add(Task newTask) {
+    private String add(Task newTask) {
         task.add(newTask);
         taskCount ++;
-        System.out.println(" Got it. I've added this task:\n"
+        rewrite();
+        return " Got it. I've added this task:\n"
                         + "   " + newTask.toString() + "\n"
-                        + " Now you have " + taskCount + " tasks in the list.\n");
+                        + " Now you have " + taskCount + " tasks in the list.\n";
+
     }
 
-    private void readTask() {
-        System.out.println(" Here are the tasks in your list:\n");
-        for (int i = 1; i <= taskCount; i ++) {
-            System.out.println( " " + i + ". " + task.get(i - 1).toString() + "\n");
-        }
-    }
-
-    private void mark(int i) throws InvalidTaskNumber{
+    private String mark(int i) throws InvalidTaskNumber{
         if (taskCount == 0) {
             throw new InvalidTaskNumber();
         }
-        if (i <= 0 || i >= taskCount) {
+        if (i <= 0 || i > taskCount) {
             throw new InvalidTaskNumber(i, taskCount);
         }
         task.get(i - 1).mark();
-        System.out.println(" Nice! I've marked this task as done:\n"
-                        + "   " + task.get(i - 1).toString());
+        rewrite();
+        return " Nice! I've marked this task as done:\n"
+                        + "   " + task.get(i - 1).toString();
+
     }
 
-    private void unMark(int i) throws InvalidTaskNumber{
+    private String unMark(int i) throws InvalidTaskNumber{
         if (taskCount == 0) {
             throw new InvalidTaskNumber();
         }
-        if (i <= 0 || i >= taskCount) {
+        if (i <= 0 || i > taskCount) {
             throw new InvalidTaskNumber(i, taskCount);
         }
         task.get(i - 1).unMark();
-        System.out.println(" OK, I've marked this task as not done yet:\n"
-                + "   " + task.get(i - 1).toString());
+        rewrite();
+        return " OK, I've marked this task as not done yet:\n"
+                + "   " + task.get(i - 1).toString();
+
     }
 
-    private void delete(int i) throws InvalidTaskNumber{
+    private String delete(int i) throws InvalidTaskNumber{
         if (taskCount == 0) {
             throw new InvalidTaskNumber();
         }
@@ -103,10 +128,12 @@ public class Nah {
             throw new InvalidTaskNumber(i, taskCount);
         }
         taskCount --;
-        System.out.println(" Noted. I've removed this task:\n"
+        String s = " Noted. I've removed this task:\n"
                 + "   " + task.get(i - 1).toString() + "\n"
-                + " Now you have " + taskCount + " tasks in the list.\n");
+                + " Now you have " + taskCount + " tasks in the list.\n";
         task.remove(i - 1);
+        rewrite();
+        return s;
     }
 
     public static void main(String[] args) {
@@ -139,29 +166,29 @@ public class Nah {
                     return;
                 }
                 case LIST: {
-                    nah.readTask();
+                    System.out.println(nah.readTask());
                     break;
                 }
                 case MARK: {
                     int i = parseInt(command[1]);
-                    nah.mark(i);
+                    System.out.println(nah.mark(i));
                     break;
                 }
                 case UNMARK: {
                     int i = parseInt(command[1]);
-                    nah.unMark(i);
+                    System.out.println(nah.unMark(i));
                     break;
                 }
                 case DELETE: {
                     int i = parseInt(command[1]);
-                    nah.delete(i);
+                    System.out.println(nah.delete(i));
                     break;
                 }
                 case TODO: {
                     if (command.length < 2 || command[1].trim().isEmpty()) {
                         throw new LackDescription(" Nahhhh!!! Todo needs description\n");
                     }
-                    nah.add(new ToDos(command[1].trim()));
+                    System.out.println(nah.add(new ToDos(command[1].trim())));
                     break;
                 }
                 case DEADLINE: {
@@ -172,7 +199,7 @@ public class Nah {
                     if (des.length < 2 || des[1].trim().isEmpty()) {
                         throw new LackDescription((" Nahhhhhhh!!! We need deadline for Deadline\n"));
                     }
-                    nah.add(new Deadlines(des[0].trim(), des[1].trim()));
+                    System.out.println(nah.add(new Deadlines(des[0].trim(), des[1].trim())));
                     break;
                 }
                 case EVENT: {
@@ -187,7 +214,7 @@ public class Nah {
                     if (time.length < 2 || time[1].trim().isEmpty()) {
                         throw new LackDescription((" Nahhhhhhhhhhhhhh!!! We need ending time for an Event\n"));
                     }
-                    nah.add(new Events(des[0].trim(),time[0].trim(),time[1].trim()));
+                    System.out.println(nah.add(new Events(des[0].trim(),time[0].trim(),time[1].trim())));
                     break;
                 }
                 default:
