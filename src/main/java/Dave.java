d745fc4dcc7e843931237a57059684cd70f59e23
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-08-26 19:06:56 +0800

    Level 3 List Modification

--- src/main/java/Dave.java
+++ src/main/java/Dave.java
@@ -10,7 +10,9 @@ public class Dave {
                 + "|____/_/   \\_\\   \\_/  |_____|\n";
         String horizontal = "__________________________________________________________";
         String[] dataList = new String[100];
+        Boolean[] booleanList = new Boolean[100];
         int dataIndex = 0;
+        String state;
         System.out.println("Hello from\n" + logo);
         System.out.println(horizontal);
         System.out.println("Hello! I'm Dave.");
@@ -21,6 +23,10 @@ public class Dave {
         while (true)
         {
             userInput = scanner.nextLine().trim();
+
+            if (userInput.isEmpty()) {
+                continue;
+            }
 
             switch (userInput) {
                 case "bye":
@@ -33,20 +39,23 @@ public class Dave {
                 case "list":
                     System.out.println(horizontal);
                     for (int i = 0; i < dataIndex; i++) {
-                        System.out.println((i + 1) + ". " + dataList[i]);
+                        state = booleanList[i] == null ? "" : "X";
+                        String statement = String.format("%d.[%s] %s", i+1, state, dataList[i]);
+                        System.out.println(statement);
                     }
                     System.out.println(horizontal);
                     break;
 
                 default:
+
+                    dataList[dataIndex] = userInput;
+                    booleanList[dataIndex] = true;
+                    dataIndex ++;
                     System.out.println(horizontal);
-                    dataList[dataIndex++] = userInput;
                     System.out.printf("added: %s%n", userInput);
                     System.out.println(horizontal);
                     break;
             }
         }
-
-
     }
 }
