fddb9e926e3529e562675c787dbbaefe8c0f7963
chashaobao123 <chooi.shaoxian@gmail.com>
2024-08-26 16:03:54 +0800

    ip wk2 level-1

--- src/main/java/LittleMissHelpful.java
+++ src/main/java/LittleMissHelpful.java
@@ -1,21 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class LittleMissHelpful {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-
-
         String name = "Little Miss Helpful";
         String lineBreak = "___________________________________";
-
         String greeting = "Hello! I'm " + name + ".\nWhat can I do for you?";
-
         String exitLine = "Bye. Hope to see you again soon!";
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Print greeting
         System.out.println(lineBreak);
-        System.out.println(greeting + "\n" + lineBreak);
-        System.out.println(exitLine + "\n" + lineBreak);
+        System.out.println(greeting);
+        System.out.println(lineBreak);
+
+        String input = "";
+
+            while (true) { //there is continuous inputs
+                input = br.readLine();
+
+                // Check if the user input is "bye"
+                if (input.equalsIgnoreCase("bye")) {
+                    System.out.println(exitLine);
+                    System.out.println(lineBreak);
+                    break; // Exit the loop if the user types "bye"
+                }
+
+                // Echos commands
+                System.out.println(lineBreak);
+                System.out.println(input);
+                System.out.println(lineBreak);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading input.");
+            e.printStackTrace();
+        }
     }
 }
