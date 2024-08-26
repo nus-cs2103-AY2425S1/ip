7569e7af49e4e698a8dddded9814cf36b07a615d
CYX22222003 <e1155533@u.nus.edu>
2024-08-26 19:25:31 +0800

    Implement unittest for DateParser

--- src/test/java/parser/DateParserTest.java
+++ src/test/java/parser/DateParserTest.java
@@ -15,13 +15,38 @@ public class DateParserTest {
     }
 
     @Test
+    public void byStatement_wrongFormat_exceptionThrown() {
+        try {
+            assertEquals(DateParser.parseBy("/by day1 to day2"),
+                    "");
+            fail();
+        } catch (Exception e) {
+            assertEquals("Fail to extract information: Wrong format of dates"
+                    , e.getMessage());
+        }
+    }
+
+    @Test
     public void fromStatement_correctFormat_success() throws Exception {
         assertEquals(DateParser.parseFrom("/from 2023-01-20"),
                 "2023-01-20T00:00");
         assertEquals(DateParser.parseFrom("/from 2024-01-20"),
                 "2024-01-20T00:00");
-        assertEquals(DateParser.parseFrom("/from 2023-11-20 22:03"),
-                "2023-11-20T22:03");
+        assertEquals(DateParser.parseFrom(
+                "/from 2023-11-20 22:03 /to 2023-11-21 23:00")
+                , "2023-11-20T22:03");
+    }
+
+    @Test
+    public void fromStatement_wrongFormat_exceptionThrown() {
+        try {
+            assertEquals(DateParser.parseFrom("/from day1 to day2"),
+                        "");
+            fail();
+        } catch (Exception e) {
+            assertEquals("Fail to extract information: Wrong format of dates"
+                    , e.getMessage());
+        }
     }
 
     @Test
@@ -35,10 +60,34 @@ public class DateParserTest {
     }
 
     @Test
+    public void toStatement_wrongFormat_exceptionThrown() {
+        try {
+            assertEquals(DateParser.parseFrom("/to day1 to day2"),
+                    "");
+            fail();
+        } catch (Exception e) {
+            assertEquals("Fail to extract information: Wrong format of dates"
+                    , e.getMessage());
+        }
+    }
+
+    @Test
     public void recordStatement_correctFormat_success() throws Exception {
         assertEquals(DateParser.parseRecordedDate("00:00 Jan 1 2023"),
                 "2023-01-01T00:00");
         assertEquals(DateParser.parseRecordedDate("22:03 Nov 20 2023"),
                 "2023-11-20T22:03");
+    }
+
+    @Test
+    public void recordStatement_wrongFormat_exceptionThrown() {
+        try {
+            assertEquals(DateParser.parseFrom("2002-09-21"),
+                    "");
+            fail();
+        } catch (Exception e) {
+            assertEquals("Fail to extract information: Wrong format of dates"
+                    , e.getMessage());
+        }
     }
 }
