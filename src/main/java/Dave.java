97015f5eeaecdf7b10880c3473cf5cd70f08047c
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-08-26 11:55:49 +0800

    Level 2. Add, List

--- src/main/java/Dave.java
+++ src/main/java/Dave.java
@@ -9,6 +9,8 @@ public class Dave {
                 + "| |_| / ___ \\   \\ V / | |___\n"
                 + "|____/_/   \\_\\   \\_/  |_____|\n";
         String horizontal = "__________________________________________________________";
+        String[] dataList = new String[100];
+        int dataIndex = 0;
         System.out.println("Hello from\n" + logo);
         System.out.println(horizontal);
         System.out.println("Hello! I'm Dave.");
@@ -18,21 +20,33 @@ public class Dave {
         String userInput;
         while (true)
         {
-            userInput = scanner.nextLine();
-            if (userInput.trim().equals("bye"))
-            {
-                break;
-            }
-            else
-            {
-                System.out.println(horizontal);
-                System.out.println(userInput);
-                System.out.println(horizontal);
+            userInput = scanner.nextLine().trim();
+
+            switch (userInput) {
+                case "bye":
+                    System.out.println(horizontal);
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(horizontal);
+                    scanner.close();
+                    return;
+
+                case "list":
+                    System.out.println(horizontal);
+                    for (int i = 0; i < dataIndex; i++) {
+                        System.out.println((i + 1) + ". " + dataList[i]);
+                    }
+                    System.out.println(horizontal);
+                    break;
+
+                default:
+                    System.out.println(horizontal);
+                    dataList[dataIndex++] = userInput;
+                    System.out.printf("added: %s%n", userInput);
+                    System.out.println(horizontal);
+                    break;
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontal);
-        scanner.close();
+
 
     }
 }
