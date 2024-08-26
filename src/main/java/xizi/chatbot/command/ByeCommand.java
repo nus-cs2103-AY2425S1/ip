d13b726952c585c1e957fa7009ee8795e3b6c415
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-08-26 18:37:00 +0800

    Add JavaDoc comments for bye command and command interface

--- src/main/java/xizi/chatbot/command/ByeCommand.java
+++ src/main/java/xizi/chatbot/command/ByeCommand.java
@@ -4,10 +4,22 @@ import xizi.chatbot.Storage;
 import xizi.chatbot.task.TaskList;
 import xizi.chatbot.Ui;
 
+/**
+ * The {@code ByeCommand} class represents a command to terminate the application.
+ * This command displays a goodbye message to the user.
+ */
 public class ByeCommand implements Command {
+
+    /**
+     * Executes the bye command, which displays a goodbye message to the user.
+     * This method is called when the user wants to exit the application.
+     *
+     * @param actions the {@code TaskList} containing the user's tasks
+     * @param storage the {@code Storage} used to save and load tasks
+     * @param ui      the {@code Ui} used to interact with the user
+     */
     @Override
     public void execute(TaskList actions, Storage storage, Ui ui) {
         ui.showGoodbyeMessage();
     }
 }
-
--- src/main/java/xizi/chatbot/command/Command.java
+++ src/main/java/xizi/chatbot/command/Command.java
@@ -7,7 +7,22 @@ import xizi.chatbot.XiziException;
 
 import java.io.IOException;
 
+/**
+ * The {@code Command} interface represents an executable command in the Xizi chatbot.
+ */
 public interface Command {
+
+    /**
+     * Executes the command, performing the necessary operations on the task list,
+     * storage, and user interface.
+     *
+     * @param actions the {@code TaskList} containing the user's tasks
+     * @param storage the {@code Storage} used to save and load tasks
+     * @param ui      the {@code Ui} used to interact with the user
+     * @throws IOException   if an I/O error occurs during command execution
+     * @throws XiziException if a specific error related to the Xizi chatbot occurs
+     */
     void execute(TaskList actions, Storage storage, Ui ui) throws IOException, XiziException;
 }
+
 
