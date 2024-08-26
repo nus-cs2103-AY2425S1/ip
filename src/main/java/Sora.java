923965745de9855a1deca01eb4203814a6b9c4f9
S-K-Y-Light <s84915656@gmail.com>
2024-08-26 23:10:10 +0800

    Add "Save" & "Load" Feature

--- src/main/java/Sora.java
+++ src/main/java/Sora.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,6 +9,7 @@ import java.util.Scanner;
 
 public class Sora {
     private static final String HORIZONTAL_LINE = "---------------------------------------------------";
+    private static final String FILENAME = "savedtasks.txt";
     private final List<Task> taskList;
     private boolean isLive;
 
@@ -16,6 +21,7 @@ public class Sora {
     public static void main(String[] args) {
         Sora sora = new Sora();
         Scanner commandScanner = new Scanner(System.in);
+        sora.loadTaskList();
 
         System.out.println(greeting());
 
@@ -99,6 +105,7 @@ public class Sora {
                 return;
             }
             task.markAsDone();
+            saveTaskList();
             System.out.println("\tNice! Sora has marked this task as done:");
             System.out.println("\t" + task);
         } catch (NumberFormatException e) {
@@ -116,6 +123,7 @@ public class Sora {
                 return;
             }
             task.markAsNotDone();
+            saveTaskList();
             System.out.println("\tOk, Sora has unmarked this task as not done:");
             System.out.println("\t" + task);
         } catch (NumberFormatException e) {
@@ -149,6 +157,7 @@ public class Sora {
                     throw new SoraException("\tPlease Enter - Event (Description) /from (from) /to (to)");
                 }
         }
+        saveTaskList();
         System.out.println("\tGot it. Sora has added this task:");
         System.out.println("\t" + taskList.get(taskList.size() - 1));
         System.out.println("\tNow, you have " + taskList.size() + " tasks in your list");
@@ -158,12 +167,87 @@ public class Sora {
         try {
             int index = Integer.parseInt(value) - 1;
             Task deletedTask = this.taskList.remove(index);
+            saveTaskList();
             System.out.println("\tNoted. Sora has removed this task:");
             System.out.println("\t" + deletedTask);
         } catch (NumberFormatException e) {
             throw new SoraException("\tPlease Enter - Delete (int)\n" + HORIZONTAL_LINE);
         } catch (IndexOutOfBoundsException e) {
             throw new SoraException("\tPlease Enter Integer Value within List Size\n" + HORIZONTAL_LINE);
+        }
+    }
+
+    public String saveTaskString() {
+        return taskList.stream()
+                .map(Task::getTaskDetails)
+                .map(x -> String.join(" | ", x) + "\n")
+                .reduce("", (x, y) -> x + y);
+    }
+
+    private void saveTaskList() {
+        try {
+            FileWriter fileWriter = new FileWriter(FILENAME);
+            fileWriter.write(saveTaskString());
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("\tSora is unable to write to file" + HORIZONTAL_LINE);
+        }
+    }
+
+    private void loadTaskList() {
+        File file = new File(FILENAME);
+        try {
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            Scanner fileScanner = new Scanner(file);
+            int lineNumber = 1;
+            while (fileScanner.hasNext()) {
+                try {
+                    loadTaskListHelper(lineNumber, fileScanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    lineNumber++;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void loadTaskListHelper(int lineNumber, String fileTaskDetails) throws Exception {
+        String[] parsedFileTaskDetails = fileTaskDetails.split(" \\| ", 5);
+        switch (parsedFileTaskDetails[0]) {
+        case "T":
+            if (parsedFileTaskDetails.length != 3) {
+                throw new Exception("\tSora is unable to read file from line " + String.valueOf(lineNumber));
+            }
+            taskList.add(new ToDo(parsedFileTaskDetails[2]));
+            break;
+        case "D":
+            if (parsedFileTaskDetails.length != 4) {
+                throw new Exception("\tSora is unable to read file from line " + String.valueOf(lineNumber));
+            }
+            taskList.add(new Deadline(parsedFileTaskDetails[2], parsedFileTaskDetails[3]));
+            break;
+        case "E":
+            if (parsedFileTaskDetails.length != 5) {
+                throw new Exception("\tSora is unable to read file from line " + String.valueOf(lineNumber));
+            }
+            taskList.add(new Event(parsedFileTaskDetails[2], parsedFileTaskDetails[3], parsedFileTaskDetails[4]));
+            break;
+        }
+        if (parsedFileTaskDetails[1].equalsIgnoreCase("X")) {
+            try {
+                markTask(String.valueOf(lineNumber));
+            } catch (SoraException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
