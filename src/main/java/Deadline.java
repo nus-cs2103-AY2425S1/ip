d5ae670362b4e27e2ef9392a170876ee7cc7a1a5
Aditi Gawande <>
2024-08-26 13:29:42 +0800

    Almost done with Level 8. Deadline task accepts both a date that it can parse and a string.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,33 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDateTime date;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    public Deadline(String description, LocalDateTime date) {
+        super(description);
+        this.date = date;
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        //return String.format("[D]" + super.toString() + " (by: %s)", date == null ? by : date);
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        String formattedDate = (date != null) ? date.format(outputFormatter) : by;
+        return String.format("[D]" + super.toString() + " (by: %s)", formattedDate);
     }
 
     @Override
     public String toFileFormat() {
-        return String.format("D | %d | %s | %s", isDone ? 1 : 0, description, by);
+        DateTimeFormatter converter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        String formattedDate = (date != null) ? date.format(converter) : by;
+        return String.format("D | %d | %s | %s", isDone ? 1 : 0, description, formattedDate);
     }
 
 }
