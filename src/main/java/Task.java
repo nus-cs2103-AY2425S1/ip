2bff4ab84e2372f93c24fc463134fb6a1b5360c6
kvernnn <156299531+kvernnn@users.noreply.github.com>
2024-08-27 20:33:25 +0800

    Revert "Storage class to load and save files"
    This reverts commit 34b569b21bf09b04a994c3cd839301635305617d.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,49 +1,10 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Task {
     private String description;
     private boolean isDone;
-    private static DateTimeFormatter fileDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
-
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
-    }
-
-    public static Task fromString(String string) {
-        String[] parts = string.split(" \\| ");
-        String type = parts[0];
-        boolean isDone = parts[1].equals("1");
-        String description = parts[2];
-
-        switch (type) {
-            case "T" -> {
-                return new ToDo(description);
-            }
-            case "D" -> {
-                LocalDateTime dateAndTime = LocalDateTime.parse(parts[3], fileDateFormat);
-                Deadline deadline = new Deadline(description, dateAndTime);
-                if (isDone) {
-                    deadline.mark();
-                }
-                return deadline;
-            }
-            case "E" -> {
-                String[] duration = parts[3].split("-");
-                LocalDateTime from = LocalDateTime.parse(duration[0], fileDateFormat);
-                LocalDateTime to = LocalDateTime.parse(duration[1], fileDateFormat);
-                Event event = new Event(description, from, to);
-                if (isDone) {
-                    event.mark();
-                }
-                return event;
-            }
-            default -> {
-                throw new IllegalArgumentException("Bao doesn't know what this task type is");
-            }
-        }
     }
 
     public void mark() {
