e93944d823163f5c066c92f3849421f21a3a003e
KengHian <e1264781@u.nus.edu>
2024-08-27 19:33:19 +0800

    feat: Add ability for chatbot to save Tasks in a file

--- src/main/java/Struggling.java
+++ src/main/java/Struggling.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 enum Commands
@@ -8,21 +13,76 @@ enum Commands
 
 public class Struggling {
     final private String name = "struggling";
+    final private String fileName = "./data/Struggling.txt";
+
     private ArrayList<Task> taskArr = new ArrayList<>();
+    private boolean isActive;
 
     Struggling() {
         reply(String.format("Hello! I'm %s\nWhat can I do for you?", this.name));
+
+        try {
+            this.isActive = true;
+            String directory = fileName.substring(0, fileName.lastIndexOf("/"));
+            new File(directory).mkdir();
+            if(!new File(fileName).createNewFile()) {
+                loadTask();
+            }
+        } catch (FileNotFoundException e) {
+            reply("Save file not found, please try again!");
+            this.isActive = false;
+        } catch (IOException e) {
+            reply("Failed to create a save file, please contact the developer!");
+            this.isActive = false;
+        } catch (StrugglingException e) {
+            reply("Save file corrupted, using a fresh save file");
+            resetSaveFile();
+            this.taskArr.clear();
+        }
     }
 
-    public boolean read(String cmd) {
+    private void loadTask() throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String[] args = s.nextLine().split(" \\| ");
+            Task task = switch (args[0]) {
+                case "T" -> new ToDo(args[2]);
+                case "D" -> new Deadline(args[2], args[3]);
+                case "E" -> new Event(args[2], args[3], args[4]);
+                default -> throw new StrugglingException("Save file corrupted");
+            };
 
+            if (Objects.equals(args[1], "1")) {
+                task.mark();
+            } else if (!Objects.equals(args[1], "0")) {
+                throw new StrugglingException("Save file corrupted");
+            }
+
+            this.taskArr.add(task);
+        }
+    }
+
+    private void resetSaveFile() {
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            fw.write("");
+            fw.close();
+        } catch (Exception e) {
+            reply("Recovery failed, please contact the developer");
+            this.isActive = false;
+        }
+    }
+
+    public void read(String cmd) {
         try {
             String[] args = cmd.split(" ");
 
             switch (Commands.valueOf(args[0])) {
                 case bye:
                     reply("Bye. Hope to see you again soon!");
-                    return false;
+                    this.isActive = false;
+                    break;
                 case list:
                     list();
                     break;
@@ -62,8 +122,6 @@ public class Struggling {
         } catch (StrugglingException e) {
             reply(e.getMessage());
         }
-
-        return true;
     }
 
     private void reply(String str) {
@@ -80,10 +138,18 @@ public class Struggling {
     }
 
     private void addTask(Task task) {
-        this.taskArr.add(task);
+        try {
+            FileWriter fw = new FileWriter(fileName, true);
+            fw.write(task.getState() + System.lineSeparator());
+            fw.close();
 
-        reply(String.format("Got it. I've added this task:\n\t%s\nNow you have %d tasks in the list.",
-                task, this.taskArr.size()));
+            this.taskArr.add(task);
+            reply(String.format("Got it. I've added this task:\n\t%s\nNow you have %d tasks in the list.",
+                    task, this.taskArr.size()));
+        } catch (IOException e) {
+            reply("Saving failed due to IO error, please contact developer!");
+            this.isActive = false;
+        }
     }
 
     private  void list() {
@@ -124,14 +190,17 @@ public class Struggling {
                 t, this.taskArr.size()));
     }
 
+    public boolean isActive() {
+        return this.isActive;
+    }
+
     public static void main(String[] args) {
         Struggling bot = new Struggling();
         Scanner sc = new Scanner(System.in);
-        boolean isActive = true;
 
         do {
-            isActive = bot.read(sc.nextLine());
-        } while (isActive);
+            bot.read(sc.nextLine());
+        } while (bot.isActive());
 
     }
 }
