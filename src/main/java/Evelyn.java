2c86a5be483e53fe7ac10a58d25db028c8b5b51f
Vatinius <102.23.dylan.chan@gmail.com>
2024-08-27 23:01:40 +0800

    Add parser, storage, task list and ui classes with zero functionality

--- src/main/java/Evelyn.java
+++ src/main/java/Evelyn.java
@@ -10,25 +10,33 @@ import java.util.ArrayList;
 
 public class Evelyn {
     private static String chatbotName = "Evelyn";
-    private static String horizontalLine = "-----------------------------------------";
-    private static Scanner scanner = new Scanner(System.in);
+
     private static ArrayList lst = new ArrayList(100);
     private static boolean isChatting = true;
     private static String dataFilePath = "src/main/data/evelyn.txt";
     private static File file = new File(dataFilePath);
+    private Ui ui;
+    private Storage storage;
+    private TaskList taskList;
+    private Parser parser;
+
+    public Evelyn(String filePath) {
+        this.storage = new Storage(dataFilePath);
+        try {
+            this.taskList = new TaskList(storage.load());
+        } catch (IOException e) {
+            System.err.println("Error reading/writing file");
+        }
+        this.parser = new Parser(this.storage, this.taskList);
+        this.ui = new Ui(this.parser);
+    }
+
+    public void run() {
+        this.ui.start();
+    }
 
     public static void main(String[] args) throws IOException {
-        String text = null;
-        System.out.println(horizontalLine);
-        System.out.println("Hi! I am Evelyn");
-        System.out.println("Here are my keywords:");
-        System.out.println("\n");
-        System.out.println("todo [task description]");
-        System.out.println("deadline [task description] /by [date in YYYY-MM-DD] [Optional: time]");
-        System.out.println("event [task description] /from [date in YYYY-MM-DD] [Optional: time] /to [date in YYYY-MM-DD] [Optional: time]");
-        System.out.println("\n");
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        new Evelyn(dataFilePath).run();
 
         try {
             if (!file.exists()) {
@@ -68,101 +76,6 @@ public class Evelyn {
             } catch (IOException e) {
                 System.out.println("Error reading/writing file");
             }
-        }
-    }
-
-    private static void decipher(String text) throws NoInputException, IOException {
-        if ((Objects.equals(text, "bye")) || (Objects.equals(text, "BYE")) || (Objects.equals(text, "Bye"))) {
-            file.delete();
-            file.createNewFile();
-            for (int i = 0; i < lst.size(); i++) {
-                Task task = (Task) lst.get(i);
-                writeToFile(dataFilePath, task.toString() + System.lineSeparator());
-            }
-            System.out.println(horizontalLine);
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println(horizontalLine);
-            isChatting = false;
-        } else if (text.startsWith("delete")) {
-            int index = Integer.parseInt(text.substring(7)) - 1;
-            Task task = (Task) lst.get(index);
-            lst.remove(index);
-            System.out.println(horizontalLine);
-            System.out.println("Noted. I've removed this task:");
-            System.out.println("   " + task.toString());
-            System.out.println(lst.size() > 1 ? "Now you have " + lst.size() + " tasks in this list"
-                    : "Now you have " + lst.size() + " task in this list");
-        } else if (Objects.equals(text, "list")) {
-            System.out.println(horizontalLine);
-            System.out.println("Here are the tasks in your list: ");
-            for (int i = 0; i < lst.size(); i++) {
-                System.out.println((i + 1) + "." + lst.get(i));
-            }
-            System.out.println(horizontalLine);
-        }  else if (text.startsWith("mark")) {
-            int index = Integer.parseInt(text.substring(5)) - 1;
-            Task task = (Task) lst.get(index);
-            System.out.println(horizontalLine);
-            task.mark();
-            System.out.println(horizontalLine);
-        } else if (text.startsWith("unmark")) {
-            int index = Integer.parseInt(text.substring(7)) - 1;
-            Task task = (Task) lst.get(index);
-            System.out.println(horizontalLine);
-            task.unmark();
-            System.out.println(horizontalLine);
-        } else if (text.startsWith("todo")) {
-            if (text.length() <= 5 || Character.isWhitespace(text.charAt(5))) {
-                throw new InvalidInputException("invalid input for todo");
-            }
-            String description = text.substring(5);
-            Todo newTodo = new Todo(description, false);
-            lst.add(newTodo);
-            writeToFile(dataFilePath, newTodo.toString() + System.lineSeparator());
-            System.out.println(horizontalLine);
-            System.out.println("Got it. I've added this task:");
-            System.out.println("  " + newTodo.toString());
-            System.out.println(lst.size() > 1 ? "Now you have " + lst.size() + " tasks in this list"
-                    : "Now you have " + lst.size() + " task in this list");
-            System.out.println(horizontalLine);
-        } else if (text.startsWith("deadline")) {
-            if (text.length() <= 9 || Character.isWhitespace(text.charAt(9))) {
-                throw new InvalidInputException("invalid input for deadline");
-            }
-            String input = text.substring(9);
-            String[] parts = input.split(" /by ");
-            String description = parts[0];
-            String deadline = parts[1];
-            Deadline newDeadline = new Deadline(description, deadline, false);
-            lst.add(newDeadline);
-            writeToFile(dataFilePath, newDeadline.toString() + System.lineSeparator());
-            System.out.println(horizontalLine);
-            System.out.println("Got it. I've added this task:");
-            System.out.println("  " + newDeadline.toString());
-            System.out.println(lst.size() > 1 ? "Now you have " + lst.size() + " tasks in this list"
-                    : "Now you have " + lst.size() + " task in this list");
-            System.out.println(horizontalLine);
-        } else if (text.startsWith("event")) {
-            if (text.length() <= 6 || Character.isWhitespace(text.charAt(6))) {
-                throw new InvalidInputException("invalid input for event");
-            }
-            String input = text.substring(6);
-            String[] partA = input.split(" /from " );
-            String description = partA[0];
-            String[] partB = partA[1].split(" /to ");
-            String start = partB[0];
-            String end = partB[1];
-            Event newEvent = new Event(description, start, end, false);
-            lst.add(newEvent);
-            writeToFile(dataFilePath, newEvent.toString() + System.lineSeparator());
-            System.out.println(horizontalLine);
-            System.out.println("Got it. I've added this task:");
-            System.out.println("  " + newEvent.toString());
-            System.out.println(lst.size() > 1 ? "Now you have " + lst.size() + " tasks in this list"
-                    : "Now you have " + lst.size() + " task in this list");
-            System.out.println(horizontalLine);
-        } else {
-            throw new NoInputException("no input!");
         }
     }
 
