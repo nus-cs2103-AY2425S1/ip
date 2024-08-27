037ef774cdbdb09f6c1502d3ad66657b788b105b
rayray39 <e0772883@u.nus.edu>
2024-08-27 14:55:03 +0800

    Main.java: Remove findDeadlineDetails and findEventDetails

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,85 +12,6 @@ import java.util.Scanner;
 public class Main {
 
     /**
-     * Returns a String array based on user's commands.
-     * First element = a String of Deadline's description.
-     * Second element = a String of Deadline's dueDate (MMM dd yyyy hh:mm a).
-     * input date and time format must be "yyyy-mm-dd HH:MM"
-     *
-     * @param commandDetails a String[], where each element corresponds to a word of the user input.
-     * @return a String[], where first elem = Deadline.description, second elem = Deadline.dueDate.
-     */
-    public static String[] findDeadlineDetails(String[] commandDetails) {
-        int indexOfBy = 0;
-        // first word in commandDetails must be deadline, so start from the i=1 word
-        for (int i = 1; i < commandDetails.length; i++) {
-            if (commandDetails[i].equals("/by")) {
-                indexOfBy = i;
-            }
-        }
-        // get description of Deadline
-        String[] descriptionArray = Arrays.copyOfRange(commandDetails, 1, indexOfBy);
-        String description = String.join(" ", descriptionArray);
-        // get due date
-        String inputDate  = commandDetails[commandDetails.length - 2];    // yyyy-mm-dd
-        LocalDate date = LocalDate.parse(inputDate);
-        String outputDate = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
-        // get time
-        String inputTime = commandDetails[commandDetails.length - 1];
-        LocalTime time = LocalTime.parse(inputTime);
-        String outputTime = time.format(DateTimeFormatter.ofPattern("hh:mm a"));
-        String dueDate = outputDate + " " + outputTime;
-        return new String[]{description, dueDate};
-    }
-
-    /**
-     * Returns a String array based on user's commands.
-     * First element = a String of Event's description.
-     * Second element = a String of Event's startDate (MMM dd yyyy hh:mm a).
-     * Third element = a String of Event's endDate (MMM dd yyyy hh:mm a).
-     * input date and time format must be "yyyy-mm-dd HH:MM"
-     *
-     * @param commandDetails a String[], where each element corresponds to a word of the user input.
-     * @return a String[], where first elem = Event.description, second elem = Event.startDate, third elem = Event.endDate.
-     */
-    public static String[] findEventDetails(String[] commandDetails) {
-        int indexOfFrom = 0;
-        int indexOfTo = 0;
-        // first word in commandDetails must be event, so start from i=1 word
-        for (int i = 1; i < commandDetails.length; i++) {
-            if (commandDetails[i].equals("/from")) {
-                indexOfFrom = i;
-            }
-            if (commandDetails[i].equals("/to")) {
-                indexOfTo = i;
-            }
-        }
-        // get the description
-        String[] descriptionArray = Arrays.copyOfRange(commandDetails, 1, indexOfFrom);
-        String description = String.join(" ", descriptionArray);
-
-        // get the start date (yyyy-mm-dd)
-        LocalDate startDate = LocalDate.parse(commandDetails[indexOfFrom + 1]);
-        String outputStartDate = startDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
-
-        // get the start time (hh:mm, 24hr format)
-        LocalTime inputStartTime = LocalTime.parse(commandDetails[indexOfFrom + 2]);
-        String outputStartTime = inputStartTime.format(DateTimeFormatter.ofPattern("hh:mm a"));
-        String startDateAndTime = outputStartDate + " " + outputStartTime;
-
-        // get the end date (yyyy-mm-dd)
-        LocalDate endDate = LocalDate.parse(commandDetails[indexOfTo + 1]);
-        String outputEndDate = endDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
-
-        // get the end time
-        LocalTime inputEndTime = LocalTime.parse(commandDetails[indexOfTo + 2]);
-        String outputEndTime = inputEndTime.format(DateTimeFormatter.ofPattern("hh:mm a"));
-        String endDateAndTime = outputEndDate + " " + outputEndTime;
-        return new String[]{description, startDateAndTime, endDateAndTime};
-    }
-
-
-    /**
      * Throws JanetException when,
      * 1. mark/unmark/delete X, where X cannot be parsed into an Integer.
      * 2. mark/unmark/delete X, where X can be parsed into an Integer but, is <= 0 or > number of tasks in list.
@@ -212,24 +133,13 @@ public class Main {
                     }
                     case DEADLINE -> {
                         // get the details of the deadline task and create a new Deadline object
-                        String[] deadlineDetails = findDeadlineDetails(commandDetails);
-                        String dateAndTimeString = deadlineDetails[1];    // MMM dd yyyy hh:mm a
-                        DateTimeFormatter stringToDateTime = DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a");
-                        LocalDateTime dateAndTime = LocalDateTime.parse(dateAndTimeString, stringToDateTime);
-                        Task deadline = new Deadline(deadlineDetails[0], "D", dateAndTime);
+                        Task deadline = Parser.createDeadlineCommand(command);
                         String addTaskSuccess = janet.addTaskToList(deadline);
                         System.out.println(addTaskSuccess);
                     }
                     case EVENT -> {
                         // get the details of the event task and create a new Event object
-                        String[] eventDetails = findEventDetails(commandDetails);
-                        String startDateAndTimeString = eventDetails[1];
-                        DateTimeFormatter stringToDateTime = DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a");
-                        LocalDateTime startDateAndTime = LocalDateTime.parse(startDateAndTimeString, stringToDateTime);
-
-                        String endDateAndTimeString = eventDetails[2];
-                        LocalDateTime endDateAndTime = LocalDateTime.parse(endDateAndTimeString, stringToDateTime);
-                        Task event = new Event(eventDetails[0], "E", startDateAndTime, endDateAndTime);
+                        Task event = Parser.createEventCommand(command);
                         String addTaskSuccess = janet.addTaskToList(event);
                         System.out.println(addTaskSuccess);
                     }
