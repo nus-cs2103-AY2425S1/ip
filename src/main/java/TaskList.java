ae81b46c7818bc0d45e25e5a581e8a3f993a482f
LeeZeHao <zehaolee2004@gmail.com>
2024-08-28 06:57:28 +0800

    Add TaskList.java

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -13,6 +13,7 @@ public class TaskList {
 
     /**
      * Constructs a TaskList object.
+     * Tries to get list of tasks from file path. If it does not exist, create an empty save file.
      * @param filePath Path where the local task list file is stored.
      */
     public TaskList(String filePath) {
@@ -24,5 +25,40 @@ public class TaskList {
         } catch (FileNotFoundException e) {
             storage.writeTasksToFile(taskList);
         }
+    }
+
+    /**
+     * Gets the task at the specified index in the list.
+     * @param index The list number of the task.
+     * @return Task the task at the specific index
+     */
+    public Task get(int index) {
+        if (index >= taskList.size() || index < 0) {
+            throw new NoSuchTaskException();
+        }
+        return taskList.get(index);
+    }
+
+    /**
+     * Adds the task to the end of the task list.
+     * @param task
+     */
+    public void add(Task task) {
+        taskList.add(task);
+    }
+
+    /**
+     * Updates task list according to file. If file not found, creates a blank file.
+     */
+    public void readFromFile() {
+        try {
+            taskList = storage.readTasksFromFile();
+        } catch (FileNotFoundException e) {
+            storage.writeTasksToFile(taskList);
+        }
+    }
+
+    public void writeToFile() {
+        storage.writeTasksToFile(taskList);
     }
 }
