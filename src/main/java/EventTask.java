9b6acfea6f7fcdb4b826d219b2d1af1d90617dc9
Teddayz <mervynneo02@gmail.com>
2024-08-27 16:37:59 +0800

    Save

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -2,20 +2,26 @@ public class EventTask extends Task {
     String start;
     String end;
 
-    public EventTask(String description, String start, String end) {
-        super(description);
+    public EventTask(String description, boolean isDone, String start, String end) {
+        super(description, isDone);
         this.start = start;
         this.end = end;
     }
 
     @Override
     public String getTaskType() {
-        return "[E]";
+        return "E";
     }
     @Override
     public String getDescription() {
-        return this.getTaskType() + super.getDescription().replace("\n", "")
-                + String.format("(from:%s to:%s)", this.start, this.end);
+        return this.getTaskType() + " | " +
+                super.getDescription().replace("\n", "") +
+                "| " + String.format("from:%s to:%s", this.start, this.end);
     }
 
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s | %s | %s",
+                this.getTaskType(), getStatusIcon(), super.description, this.start, this.end);
+    }
 }
--- src/main/java/ToDoTask.java
+++ src/main/java/ToDoTask.java
@@ -1,16 +1,22 @@
 public class ToDoTask extends Task {
 
-    public ToDoTask(String description) {
-        super(description);
+    public ToDoTask(String description, boolean isDone) {
+        super(description, isDone);
     }
 
     @Override
     public String getTaskType() {
-        return "[T]";
+        return "T";
     }
 
     @Override
     public String getDescription() {
-        return this.getTaskType() + super.getDescription();
+        return this.getTaskType() + " | " + super.getDescription();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s",
+                this.getTaskType(), getStatusIcon(), super.description);
     }
 }
