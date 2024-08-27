8c1ad7f357d539d706185f2c0f550a0fc681e95d
Quasant <benjaminthechin@gmail.com>
2024-08-27 18:43:39 +0800

    Added ability to store text and display stored text when needed

--- src/main/java/Alvis.java
+++ src/main/java/Alvis.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Alvis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> tracker = new ArrayList<>();
         System.out.println("So you require my assistance?");
+
         while (true) {
-            String userInput = sc.nextLine();
+            String userInput = sc.nextLine().toLowerCase();
             if (userInput.equals("bye")) {
                 System.out.println("Understood.");
                 break;
+            } else if (userInput.equals("list")) {
+                for (String s: tracker) {
+                    System.out.println(s);
+                }
             } else {
-                System.out.println(userInput);
+                tracker.add(userInput);
+                System.out.println("Added " + userInput);
             }
         }
     }
