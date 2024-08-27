1925f02f280179f0e940bc97e64c5047c9d9e7fe
Teddayz <mervynneo02@gmail.com>
2024-08-28 00:57:00 +0800

    Dates and Times

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,11 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+
 public class EventTask extends Task {
-    String start;
-    String end;
+    LocalDateTime start;
+    LocalDateTime end;
+    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h:mm a");
 
     public EventTask(String description, boolean isDone, String start, String end) {
         super(description, isDone);
-        this.start = start;
-        this.end = end;
+        this.start = parseTime(start);
+        this.end = parseTime(end);
+    }
+
+    private LocalDateTime parseTime(String time) throws DateTimeParseException {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        return LocalDateTime.parse(time, inputFormatter);
     }
 
     @Override
@@ -16,12 +27,14 @@ public class EventTask extends Task {
     public String getDescription() {
         return this.getTaskType() + " | " +
                 super.getDescription().replace("\n", "") +
-                "| " + String.format("from:%s to:%s", this.start, this.end);
+                "| " + String.format("from %s to %s",
+                this.start.format(outputFormatter), this.end.format(outputFormatter));
     }
 
     @Override
     public String toString() {
         return String.format("%s | %s | %s | %s | %s",
-                this.getTaskType(), getStatusIcon(), super.description, this.start, this.end);
+                this.getTaskType(), getStatusIcon(), super.description,
+                this.start.format(outputFormatter), this.end.format(outputFormatter));
     }
 }
