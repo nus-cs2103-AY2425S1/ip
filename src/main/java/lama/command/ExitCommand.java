8905c821502004760b41550549a8416da4a867bd
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-08-27 19:45:17 +0800

    Modify the java doc for exit command class

--- src/main/java/lama/command/ExitCommand.java
+++ src/main/java/lama/command/ExitCommand.java
@@ -5,14 +5,14 @@ import lama.TaskList;
 import lama.Ui;
 
 /**
- * Represent a command to list down the task list.
+ * Represents a command to exit the application.
  * Subclass of command class.
  */
 public class ExitCommand extends Command {
 
     /**
-     * Run the command by listing down the current task list.
-     * @param taskList Task list which will be listed.
+     * Run the command to exit the application.
+     * @param taskList Task list, although not used in this command.
      * @param storage Storage used to save or load tasks, although not used in this command.
      * @param ui User interface that interacts with user.
      */
