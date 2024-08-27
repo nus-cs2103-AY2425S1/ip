755076ffc2ff8643b06013c9e2203fbe5cb6b1dd
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-08-27 15:07:38 +0800

    Add javadocs to find command

--- src/main/java/command/FindCommand.java
+++ src/main/java/command/FindCommand.java
@@ -7,14 +7,32 @@ import tasklist.TaskList;
 import tasks.Task;
 import ui.CommandLineUI;
 
+/**
+ * Represents a command to find tasks in the task list that match a given keyword.
+ */
 public class FindCommand extends Command {
+    /** 
+     * The keyword to search for in the task descriptions.
+     * */
     protected String keyword;
 
+    /**
+     * Constructs a FindCommand with the specified keyword.
+     *
+     * @param keyword The keyword to search for in the task list.
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
 
     }
 
+    /**
+     * Executes the command by searching the task list for tasks that contain the specified keyword
+     * and displaying the matching tasks in the command line interface.
+     *
+     * @param tasklist The TaskList to search for matching tasks.
+     * @param ui       The CommandLineUI used to interact with the user.
+     */
     public void execute(TaskList tasklist, CommandLineUI ui) {
         List<Task> matches = new ArrayList<Task>();
 
@@ -34,6 +52,11 @@ public class FindCommand extends Command {
 
     }
 
+    /**
+     * Indicates whether this command causes the application to exit.
+     *
+     * @return false, as this command does not cause the application to exit.
+     */
     public boolean isExit() {
         return false;
     }
