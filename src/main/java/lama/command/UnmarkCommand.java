76e211aabd83b39b1a19d5d01df0f273d906f220
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-08-27 19:55:01 +0800

    Java doc for unmark command class

--- src/main/java/lama/command/UnmarkCommand.java
+++ src/main/java/lama/command/UnmarkCommand.java
@@ -5,14 +5,29 @@ import lama.Storage;
 import lama.TaskList;
 import lama.Ui;
 
+/**
+ * Represent a command to unmark task in the task list.
+ * Subclass of command.
+ */
 public class UnmarkCommand extends Command {
 
     private int indexOfUnmark;
 
+    /**
+     * Construct UnmarkCommand with specified index.
+     * @param indexOfUnmark Integer index of element in the task list that wanted to be unmarked.
+     */
     public UnmarkCommand(int indexOfUnmark) {
         this.indexOfUnmark = indexOfUnmark;
     }
 
+    /**
+     * Run the command by making the index of task in the task list as not done.
+     * @param taskList Task list which the task will be unmarked.
+     * @param storage Storage used to save latest task list.
+     * @param ui User interface that interacts with user.
+     * @throws LamaException Thrown if an error occurs during execution of command.
+     */
     @Override
     public void run(TaskList taskList, Storage storage, Ui ui) throws LamaException {
         try {
