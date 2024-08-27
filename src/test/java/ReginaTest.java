789006dd8c38afd847efae7acec9470a51830500
yooplo <jjwr2002@gmail.com>
2024-08-27 18:11:00 +0800

    Add test classes for Ui and Regina

---
+++ src/test/java/ReginaTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ReginaTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}
