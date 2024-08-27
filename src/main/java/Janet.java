e2e8984c30c3a19e2ff17e6fd85d2cecb9b69a8a
rayray39 <e0772883@u.nus.edu>
2024-08-27 23:48:10 +0800

    Janet.java: Update run method using code from Main.main()

--- src/main/java/Janet.java
+++ src/main/java/Janet.java
@@ -1,22 +1,79 @@
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Janet {
-    private final TaskList tasks;
+    private TaskList tasks;
+    private final Ui ui;
+    private final Storage storage;
 
-    Janet() {
-        this.tasks = new TaskList();
+    Janet(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.textFileToArrayList());
+        } catch (JanetException e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        }
     }
 
-    Janet(ArrayList<Task> listOfTasks) {
-        this.tasks = new TaskList(listOfTasks);
+    public void run() throws IOException {
+        ui.showWelcome();
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNext()) {
+            String userCommand = input.nextLine();
+            String[] commandDetails = Parser.getCommandDetails(userCommand);
+            try {
+                // validateCommand and checkInaccurateCommand will throw out a JanetException
+                Parser.validateCommand(commandDetails, tasks.getNumberOfTasks());
+                Parser.checkInaccurateCommand(commandDetails);
+
+                CommandType commandType = Parser.getCommand(commandDetails);
+                switch (commandType) {
+                case BYE:
+                    ui.exitMessage();
+                    break;
+                case LIST:
+                    ui.showTasks(tasks);
+                    break;
+                case MARK:
+                    String markedResult = tasks.markAsDone(Integer.parseInt(commandDetails[1]));
+                    ui.showMarkedMessage(markedResult, tasks.getTask(Integer.parseInt(commandDetails[1]) - 1));
+                case UNMARK:
+                    String unmarkResult = tasks.unmark(Integer.parseInt(commandDetails[1]));
+                    ui.showUnmarkedMessage(unmarkResult, tasks.getTask(Integer.parseInt(commandDetails[1]) - 1));
+                    break;
+                case DELETE:
+                    ui.showDeleteTaskMessage(tasks.getTask(Integer.parseInt(commandDetails[1]) - 1),
+                            tasks.getNumberOfTasks() - 1);
+                    tasks.deleteTask(Integer.parseInt(commandDetails[1]));
+                    break;
+                case TODO:
+                    Task todo = new ToDo(userCommand);
+                    tasks.addTaskToList(todo);
+                    ui.showSuccessfulTaskAddition(todo, tasks.getNumberOfTasks());
+                    break;
+                case DEADLINE:
+                    Task deadline = new Deadline(userCommand);
+                    tasks.addTaskToList(deadline);
+                    ui.showSuccessfulTaskAddition(deadline, tasks.getNumberOfTasks());
+                    break;
+                case EVENT:
+                    Task event = new Event(userCommand);
+                    tasks.addTaskToList(event);
+                    ui.showSuccessfulTaskAddition(event, tasks.getNumberOfTasks());
+                    break;
+                }
+            } catch (JanetException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        storage.saveToJanetTextFile(tasks.getListOfTasks());
     }
 
-
-    /**
-     * @return the listOfTasks ArrayList
-     */
-    public ArrayList<Task> getListOfTasks() {
-        return tasks.getListOfTasks();
+    public static void main(String[] args) throws IOException {
+        new Janet("./data/janet.txt").run();
     }
 
 }
