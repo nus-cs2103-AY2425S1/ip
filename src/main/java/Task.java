ec3a264cf77af7aab475e0a35d30f75232a00bc5
Haris-Irfan <harisirfan@gmail.com>
2024-08-28 02:08:25 +0800

    Implement Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void setTrue() {
+        this.isDone = true;
+    }
+
+    public void setFalse() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
