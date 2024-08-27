e1a88fd39984d83bfeb694794f787f963199e78c
Yifeng Li <charlesliyifeng@gmail.com>
2024-08-27 21:20:01 +0800

    Update tasks to store dates using LocalDateTime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,26 +1,35 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private final String from;
-    private final String to;
+    private static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    private final LocalDateTime from;
+    private final LocalDateTime to;
 
     public Event(String name, String from, String to) {
         super(name);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, inputFormatter);
+        this.to = LocalDateTime.parse(to, inputFormatter);
     }
 
     public Event(boolean done, String name, String from, String to) {
         super(done, name);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, inputFormatter);
+        this.to = LocalDateTime.parse(to, inputFormatter);
     }
 
     @Override
     public String toText() {
-        return String.format("E | %s | %s | %s", super.toText(), from, to);
+        return String.format("E | %s | %s | %s",
+                super.toText(),
+                from.format(inputFormatter),
+                to.format(inputFormatter));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", from, to);
+        return "[E]" + super.toString()
+                + String.format(" (from: %s to: %s)", from.format(outputFormatter), to.format(outputFormatter));
     }
 }
