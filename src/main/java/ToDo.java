6bdfcfffc36ca6cae3bf6f0bc0a19611fdd56cad
Ivan <ivanjerrickkoh@gmail.com>
2024-08-27 20:09:37 +0800

    BotimusPrime class: add save, load Task classes: add save formatting

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,6 +1,10 @@
 public class ToDo extends Task {
-    public ToDo(String description) {
-        super(description);
+    public ToDo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    public String saveString() {
+        return String.format("T | %s | %s", description, isDone);
     }
 
     @Override
