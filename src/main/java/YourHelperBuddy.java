958906df815fb21764fa365f4caa6a9d14ea4fc8
calebchongsj <calebchongsj@gmail.com>
2024-08-27 22:01:25 +0800

    Level-7 increment. Save the tasks in a hard disk automatically and load the tasks when the chatbot starts up.

--- src/main/java/YourHelperBuddy.java
+++ src/main/java/YourHelperBuddy.java
@@ -1,12 +1,69 @@
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class YourHelperBuddy {
+    /**
+     * Get user's home directory
+     */
+    private static String home = System.getProperty("user.home");
+
+    /**
+     * Define the directory path in user's computer
+     */
+    private static String directoryPath = home + "/Documents/";
+
+    /**
+     * Define the file path in user's computer
+     */
+    private static String filePath = directoryPath + "TaskInfo.txt";
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm YourHelperBuddy.");
         System.out.println("How may I assist you today?");
         System.out.println("________________________________________________");
         Scanner myObj = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<>();
+
+        /**
+         * Ensure the directory exists
+         * If it does not exist, create a new directory
+         */
+        File directory = new File(directoryPath);
+        if (!directory.exists()){
+            if (directory.mkdirs()) {
+                System.out.println("Directory created: " + directoryPath);
+            }
+            else {
+                System.out.println("Failed to create directory: " + directoryPath);
+                return;
+            }
+        }
+
+        /**
+         * Ensure the task info file exists
+         * If it does not exist, create a new file
+         */
+        File file = new File(filePath);
+        if (!file.exists()) {
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + filePath);
+                }
+                else {
+                    System.out.println("Failed to create file: " + filePath);
+                    return;
+                }
+            }
+            catch (IOException e) {
+                System.out.println("________________________________________________");
+                System.out.println("An error occurred while creating the file.");
+                System.out.println("________________________________________________");
+                e.printStackTrace();
+                return;
+            }
+        }
+        loadTasksFromFile(taskList);
         while (true) {
             System.out.println("Enter your task: ");
             if (!myObj.hasNextLine()) {
@@ -37,6 +94,7 @@ public class YourHelperBuddy {
                     System.out.println("  " + removedTask);
                     System.out.println("Now you have " + taskList.size() + " tasks in the list.");
                     System.out.println("________________________________________________");
+
                 }
                 catch (IndexOutOfBoundsException | NumberFormatException e) {
                     System.out.println("________________________________________________");
@@ -156,9 +214,54 @@ public class YourHelperBuddy {
                 System.out.println("________________________________________________");
             }
         }
+        saveTasksToFile(taskList);
         myObj.close();
         System.out.println("________________________________________________");
         System.out.println("Goodbye. Take care and see you again!");
         System.out.println("________________________________________________");
+    }
+
+    private static void loadTasksFromFile(ArrayList<Task> taskList) {
+        File file = new File(filePath);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("T")) {
+                    ToDo task = ToDo.parseTask(line);
+                    taskList.add(task);
+                }
+                else if (line.startsWith("D")) {
+                    Deadline task = Deadline.parseTask(line);
+                    taskList.add(task);
+                }
+                else if (line.startsWith("E")) {
+                    Event task = Event.parseTask(line);
+                    taskList.add(task);
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println("________________________________________________");
+            System.out.println("An error occurred while loading tasks from file.");
+            System.out.println("________________________________________________");
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveTasksToFile(ArrayList<Task> taskList) {
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            for (Task task : taskList) {
+                writer.write(task.toFileFormat() + System.lineSeparator());
+            }
+            writer.close();
+        }
+        catch (IOException e) {
+            System.out.println("________________________________________________");
+            System.out.println("An error occurred while saving tasks to file.");
+            System.out.println("________________________________________________");
+            e.printStackTrace();
+        }
     }
 }
