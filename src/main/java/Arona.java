817e1c48864650cc83b5920310f533a55559a4dd
Jayden-Kim-NUS <e1121727@u.nus.edu>
2024-08-27 16:38:25 +0800

    Add delete command, updated exceptions to use inbuilt methods

--- src/main/java/Arona.java
+++ src/main/java/Arona.java
@@ -35,59 +35,78 @@ public class Arona {
                     }
                 }
 
-                // Mark and unmark command
-                else if (input.toLowerCase().startsWith("mark ") || input.toLowerCase().startsWith("unmark ")) {
-
-                    // Data from command
-                    int index = -1;
-                    boolean action;
-
-                    // Extract and save action
-                    action = input.toLowerCase().startsWith("mark ");
+                // Delete
+                else if (input.toLowerCase().startsWith("delete")) {
 
                     // Extract and save task number
-                    String[] data = input.split(" ", 2);
-
-                    if (data.length == 1) {
-                        throw new AronaException("Error! Please input a positive number");
-                    }
+                    String[] data = input.split("delete", 2);
 
                     // Check if number
-                    if (!data[1].matches("^[1-9]\\d*$")) {
+                    if (!data[1].stripLeading().matches("^[1-9]\\d*$")) {
                         throw new AronaException("Error! Please input a positive number");
-                    } else {
-                        index = Integer.parseInt(data[1]);
                     }
+
+                    // Save index as integer
+                    int index = Integer.parseInt(data[1].stripLeading());
 
                     // Check if exist
                     if ((index > list.size())) {
                         throw new AronaException("Error! This task does not exist!");
                     }
 
-                    // Process
-                    else {
-                        Task task = list.get(index - 1);
-                        task.setStatus(action);
-                        if (action) {
-                            print("Nice! I've marked this task as done:");
-                        } else {
-                            print("OK, I've marked this task as not done yet:");
-                        }
-                        print(task.getStatusIcon() + task.getCategory() + ": " + task.toString());
+                    // Process and Reply
+                    print("Got it. I've removed this task:");
+                    Task task = list.get(index-1);
+                    print(task.getStatusIcon() + task.getCategory() + ": " + task.toString());
+                    list.remove(index-1);
+                    print("Now you have " + list.size() + " tasks in the list.");
+
+
+                }
+
+                // Mark and unmark command
+                else if (input.toLowerCase().startsWith("mark") || input.toLowerCase().startsWith("unmark")) {
+
+                    // Extract and save task number
+                    String[] data = input.split("mark", 2);
+
+                    // Extract and save action
+                    boolean action = input.toLowerCase().startsWith("mark");
+
+                    // Check if number
+                    if (!data[1].stripLeading().matches("^[1-9]\\d*$")) {
+                        throw new AronaException("Error! Please input a positive number");
                     }
+
+                    // Save index as integer
+                    int index = Integer.parseInt(data[1].stripLeading());
+
+                    // Check if exist
+                    if ((index > list.size())) {
+                        throw new AronaException("Error! This task does not exist!");
+                    }
+
+                    // Process and Reply
+                    Task task = list.get(index - 1);
+                    task.setStatus(action);
+                    if (action) {
+                        print("Nice! I've marked this task as done:");
+                    } else {
+                        print("OK, I've marked this task as not done yet:");
+                    }
+                    print(task.getStatusIcon() + task.getCategory() + ": " + task.toString());
                 }
 
                 // Todos or events or deadline command
-                else if (input.toLowerCase().startsWith("todo ") || input.toLowerCase().startsWith("event ") || input.toLowerCase().startsWith("deadline ")) {
+                else if (input.toLowerCase().startsWith("todo") || input.toLowerCase().startsWith("event") || input.toLowerCase().startsWith("deadline")) {
                     // Data from command
-                    boolean con = false;
                     String[] data;
 
                     // Extract and save description
                     data = input.split(" ", 2);
 
                     // Check if empty
-                    if (data[1].isBlank()) {
+                    if (data.length == 1 || data[1].isBlank()) {
                         throw new AronaException("Error! Please input a task description");
                     }
 
@@ -95,38 +114,39 @@ public class Arona {
                     switch (data[0]) {
                         case "todo": {
                             list.add(new Todos(data[1]));
-                            con = true;
                             break;
                         }
                         case "deadline": {
-                            String[] taskData = data[1].split("/by ", 2);
+                            String[] taskData = data[1].split("/by", 2);
                             if (taskData.length == 2) {
                                 list.add(new Deadline(taskData[0], taskData[1]));
-                                con = true;
                             } else {
                                 throw new AronaException("Error! Please specify a by");
                             }
                             break;
                         }
                         case "event": {
-                            String[] taskData = data[1].split("/from |/to ", 3);
+                            String[] taskData = data[1].split("/from|/to", 3);
                             if (taskData.length == 3) {
                                 list.add(new Events(taskData[0], taskData[1], taskData[2]));
-                                con = true;
                             } else {
                                 throw new AronaException("Error! Please specify a from and to");
                             }
                             break;
                         }
                     }
-                    if (con) {
-                        print("Got it. I've added this task:");
-                        int listSize = list.size();
-                        Task task = list.get(listSize - 1);
-                        print(task.getStatusIcon() + task.getCategory() + ": " + task.toString());
-                        print("Now you have " + listSize + " tasks in the list.");
-                    }
-                } else {
+
+                    // Reply
+                    print("Got it. I've added this task:");
+                    int listSize = list.size();
+                    Task task = list.get(listSize - 1);
+                    print(task.getStatusIcon() + task.getCategory() + ": " + task.toString());
+                    print("Now you have " + listSize + " tasks in the list.");
+
+                }
+
+                // All other unrecognised commands
+                else {
                     throw new AronaException("Error, unrecognised command.");
                 }
             }
