8fe81222bb8b7ba93619bd5c78d8b1e3f764b6c8
Chronoxy <adriannpcc@gmail.com>
2024-08-28 01:35:26 +0800

    Add Patrick Package
    Move DateFormatChecker and Patrick into new Patrick Package

---
+++ src/main/java/patrick/Patrick.java
@@ -0,0 +1,47 @@
+package patrick;
+
+import patrick.parser.Parser;
+import patrick.storage.Storage;
+import patrick.tasklist.TaskList;
+import patrick.ui.Ui;
+
+public class Patrick {
+    private final Ui ui;
+
+    private static String FILE_PATH = "./data/tasks.txt";
+
+    public Patrick(String filePath) {
+        ui = new Ui();
+        Storage storage = new Storage(filePath);
+        TaskList tasks;
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (Parser.PatrickException e) {
+            Ui.showErrorMsg(e.toString());
+            tasks = new TaskList();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Patrick(FILE_PATH).run();
+    }
+
+    private void run() {
+        ui.welcomeMessage();
+        runTillBye();
+        exit();
+    }
+
+    private void exit() {
+        ui.showGoodbyeMsg();
+        System.exit(0);
+    }
+
+    private void runTillBye() {
+        Parser.Type input;
+        do {
+            String userInputMsg = ui.getUserCommand();
+            input = new Parser().parseTask(userInputMsg);
+        } while (!input.equals(Parser.Type.BYE));
+    }
+}
