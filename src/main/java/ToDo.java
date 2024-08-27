764fd5b51a3968c238cb1625baca7e36daa23ac8
rayray39 <e0772883@u.nus.edu>
2024-08-27 21:55:24 +0800

    ToDo.java: Add static createToDo method and new constructor

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class ToDo extends Task {
+
+    ToDo(String inputLine) {
+        super(createToDo(inputLine).getDescription(), createToDo(inputLine).getSymbol());
+    }
 
     ToDo(String description, String symbol) {
         super(description, symbol);
+    }
+
+    public static ToDo createToDo(String inputLine) {
+        // get the todo description and create a new Todo object
+        String[] commandDetails = inputLine.split(" ");
+        String[] todoItem = Arrays.copyOfRange(commandDetails, 1, commandDetails.length);
+        String todoDescription = String.join(" ", todoItem);
+        return new ToDo(todoDescription, "T");
     }
 }
