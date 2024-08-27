252304d147ce507f39cf87b9f302811ead8e78f0
zwe <zwezeya02@gmail.com>
2024-08-27 15:42:59 +0800

    Fix code style

--- src/main/java/seedu/avo/storage/TaskParser.java
+++ src/main/java/seedu/avo/storage/TaskParser.java
@@ -17,20 +17,20 @@ public class TaskParser extends FileParser<Task> {
         String type = inputs[0];
         Task task;
         switch (type) {
-            case "T":
-                task = new ToDo(inputs[2]);
-                break;
-            case "D":
-                LocalDate dueDate = DateTime.parse(inputs[3], DateTimeFormatter.ofPattern("MMM d yyyy"));
-                task =  new Deadline(inputs[2], dueDate);
-                break;
-            case "E":
-                LocalDate startTime = DateTime.parse(inputs[3], DateTimeFormatter.ofPattern("MMM d yyyy"));
-                LocalDate endTime = DateTime.parse(inputs[4], DateTimeFormatter.ofPattern("MMM d yyyy"));
-                task =  new Event(inputs[2], startTime, endTime);
-                break;
-            default:
-                throw new AvoException("Invalid task type in storage");
+        case "T":
+            task = new ToDo(inputs[2]);
+            break;
+        case "D":
+            LocalDate dueDate = DateTime.parse(inputs[3], DateTimeFormatter.ofPattern("MMM d yyyy"));
+            task =  new Deadline(inputs[2], dueDate);
+            break;
+        case "E":
+            LocalDate startTime = DateTime.parse(inputs[3], DateTimeFormatter.ofPattern("MMM d yyyy"));
+            LocalDate endTime = DateTime.parse(inputs[4], DateTimeFormatter.ofPattern("MMM d yyyy"));
+            task =  new Event(inputs[2], startTime, endTime);
+            break;
+        default:
+            throw new AvoException("Invalid task type in storage");
         }
         boolean isCompleted = Integer.parseInt(inputs[1]) == 1;
         if (isCompleted) {
