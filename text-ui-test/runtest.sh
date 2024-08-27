9731e94f5b1e1881ba832c2f0528a1676e49481f
jankai <android.agent.k@gmail.com>
2024-08-27 18:51:55 +0800

    adds save, creates data directory where program is run to track history

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -21,7 +21,7 @@ fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin Chatterbox < input.txt > ACTUAL.TXT
-``
+
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
