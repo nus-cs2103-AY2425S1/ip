c8904e90d41ec745765ad192dfc0b651c23802bc
Eru111 <>
2024-08-28 00:05:23 +0800

    Change date and time from String to LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,22 @@
-public class Deadline extends Task {
-    private String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy HH:mm");
+    private LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
-    public Deadline(String description, String by, boolean done) {
+    public Deadline(String description, LocalDateTime by, boolean done) {
         super(description, done);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "D | " + super.toString() + " | by: " + this.by;
+        return "D | " + super.toString() + " | by: " + this.by.format(formatter);
     }
 }
