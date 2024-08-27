8d92f32ac8e442d5ffab53207a12fdfc19b56182
ZShunRen <zhengshunren@outlook.com>
2024-08-28 00:33:15 +0800

    Add javadocs for public methods in AddDeadlineCommand class

--- src/main/java/ned/commands/AddDeadlineCommand.java
+++ src/main/java/ned/commands/AddDeadlineCommand.java
@@ -3,15 +3,24 @@ package ned.commands;
 import ned.Storage;
 import ned.TaskList;
 import ned.exceptions.NedException;
-
 import ned.Ui;
-
 import ned.tasks.Task;
-
 import ned.tasks.Deadline;
 
 public class AddDeadlineCommand implements Command {
     private final String REGEX = "^deadline.*";
+
+    /**
+     * Will carry out the adding of a new Deadline object with information parsed from the command to the taskList of
+     * the Ned instance's tasklist
+     * The NedException is thrown if the user does not specify a description for the task
+     * and if there is no deadline specified in the command using /by
+     * @param taskList
+     * @param uiInstance
+     * @param storageInstance
+     * @param userInput
+     * @throws NedException
+     */
     public void execute(TaskList taskList, Ui uiInstance, Storage storageInstance, String userInput) throws NedException {
         String[] parsed_inputs = userInput.split("deadline|/by", 3);
         int parsed_inputs_len = Task.checkSizeOfInput(parsed_inputs);
@@ -24,6 +33,12 @@ public class AddDeadlineCommand implements Command {
         taskList.addTask(newTask, uiInstance);
         storageInstance.save(taskList);
     }
+
+    /**
+     * Returns the regex expression used to identify the deadline command
+     *  Used in
+     * @return
+     */
     @Override
     public String getRegex() {
         return this.REGEX;
