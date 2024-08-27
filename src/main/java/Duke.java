6b9dc47ae4aa3aede47dcb7a6f8fb7505494091c
Haris-Irfan <harisirfan@gmail.com>
2024-08-27 18:01:20 +0800

    Implement switch statement and scanner in main

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String separationLine = "_________________________________________";
         String chatBotName = "Sughandi";
 
@@ -7,7 +10,28 @@ public class Duke {
         System.out.println("Hello! I'm " + chatBotName);
         System.out.println("What can I do for you?");
         System.out.println(separationLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separationLine);
+
+        while(true) {
+            String instruction = scanner.nextLine();
+            switch (instruction.toUpperCase()) {
+                case "LIST":
+                    System.out.println("list");
+                    System.out.println(separationLine);
+                    break;
+                case "BYE":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(separationLine);
+                    break;
+                default:
+                    System.out.println("blah");
+                    System.out.println(separationLine);
+                    break;
+            }
+            if (instruction.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
