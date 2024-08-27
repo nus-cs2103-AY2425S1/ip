aab15a1e8ec57fa9992d2439c7251633bf2e7119
Christopher Tan <phertan02@gmail.com>
2024-08-27 18:23:53 +0800

    Create Storage class

---
+++ src/main/java/Storage.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Storage {
+    private final String filePath = "./data/Cloud.txt";
+    private final File file;
+
+    public Storage() throws IOException {
+        this.file = new File(this.filePath);
+        // create directories if they do not exist yet
+        this.file.getParentFile().mkdirs();
+        // create file if it does not exist yet
+        this.file.createNewFile();
+    }
+
+    public void saveData(TaskList taskList) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            int taskCount = taskList.getTaskCount();
+            for (int i = 0; i < taskCount; i++) {
+                writer.write(taskList.getTask(i).formatSave());
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to save file!");
+        }
+    }
+
+    public TaskList readData() {
+        TaskList taskList = new TaskList();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Task task = null;
+                String[] splits = line.split(" \\s*\\|\\s* ");
+                // split[1] is reserved for the marked indicator
+                String description = splits[2];
+                switch (splits[0]) {
+                case "T":
+                    task = new Todo(description);
+                    break;
+                case "D":
+                    task = new Deadline(description, splits[3]);
+                    break;
+                case "E":
+                    task = new Event(description, splits[3], splits[4]);
+                    break;
+                default:
+                    break;
+                }
+                if (task != null) {
+                    if (splits[1].equals("1")) {
+                        task.markDone();
+                    }
+                    taskList.add(task);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from save file!");
+        }
+        return taskList;
+    }
+}
