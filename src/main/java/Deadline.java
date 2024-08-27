aeadec740277fbeee4834fa46791169d21c4d697
Xan <iamxanng@gmail.com>
2024-08-27 22:28:26 +0800

    Add date parsing for deadlines and events

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
     public Deadline(String description, String by) throws MurphyException {
         super(description);
         String byTrimmed = by.trim();
         if (byTrimmed.isEmpty()) {
             throw new MurphyException("Deadline by date cannot be empty!");
         }
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(byTrimmed);
+        } catch (DateTimeParseException e) {
+            throw new MurphyException("Date should be in the format yyyy-mm-dd.");
+        }
     }
 
     public Deadline(String description, boolean isDone, String by) throws MurphyException {
@@ -15,12 +23,17 @@ public class Deadline extends Task {
         if (byTrimmed.isEmpty()) {
             throw new MurphyException("Deadline by date cannot be empty!");
         }
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(byTrimmed);
+        } catch (DateTimeParseException e) {
+            throw new MurphyException("Date should be in the format yyyy-mm-dd.");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
+        return "[D]" + super.toString() + " (by: " + this.by.format(formatter) + ")";
     }
 
     @Override
--- src/main/java/Murphy.java
+++ src/main/java/Murphy.java
@@ -47,7 +47,7 @@ public class Murphy {
             else if (input.startsWith("mark ")) {
                 String[] split = input.split(" ");
                 if (split.length > 2) {
-                    System.out.println("mark usage: \"mark [task number]\"");
+                    System.err.println("mark usage: \"mark [task number]\"");
                     prLine();
                     continue;
                 }
@@ -55,12 +55,12 @@ public class Murphy {
                 try {
                     index = Integer.parseInt(split[1]);
                 } catch (NumberFormatException e) {
-                    System.out.println("mark usage: \"mark [task number]\"");
+                    System.err.println("mark usage: \"mark [task number]\"");
                     prLine();
                     continue;
                 }
                 if (index > Murphy.tasks.size() || index <= 0) {
-                    System.out.println("The task number you chose is outside of the range of tasks!");
+                    System.err.println("The task number you chose is outside of the range of tasks!");
                     prLine();
                     continue;
                 }
@@ -70,7 +70,7 @@ public class Murphy {
             else if (input.startsWith("unmark ")){
                 String[] split = input.split(" ");
                 if (split.length > 2) {
-                    System.out.println("unmark usage: \"unmark [task number]\"");
+                    System.err.println("unmark usage: \"unmark [task number]\"");
                     prLine();
                     continue;
                 }
@@ -78,12 +78,12 @@ public class Murphy {
                 try {
                     index = Integer.parseInt(split[1]);
                 } catch (NumberFormatException e) {
-                    System.out.println("unmark usage: \"unmark [task number]\"");
+                    System.err.println("unmark usage: \"unmark [task number]\"");
                     prLine();
                     continue;
                 }
                 if (index > Murphy.tasks.size() || index <= 0) {
-                    System.out.println("The task number you chose is outside of the range of tasks!");
+                    System.err.println("The task number you chose is outside of the range of tasks!");
                     prLine();
                     continue;
                 }
@@ -93,7 +93,7 @@ public class Murphy {
             else if (input.startsWith("delete ")){
                 String[] split = input.split(" ");
                 if (split.length > 2) {
-                    System.out.println("delete usage: \"delete [task number]\"");
+                    System.err.println("delete usage: \"delete [task number]\"");
                     prLine();
                     continue;
                 }
@@ -101,14 +101,14 @@ public class Murphy {
                 try {
                     index = Integer.parseInt(split[1]);
                 } catch (NumberFormatException e) {
-                    System.out.println("delete usage: \"delete [task number]\"");
+                    System.err.println("delete usage: \"delete [task number]\"");
                     prLine();
                     continue;
                 }
                 try {
                     Murphy.deleteItem(index);
                 } catch (MurphyException e) {
-                    System.out.println(e.getMessage());
+                    System.err.println(e.getMessage());
                     prLine();
                 }
             }
@@ -118,15 +118,15 @@ public class Murphy {
                     Task todo = new Todo(input.substring(5));
                     Murphy.addItem(todo);
                 } catch (MurphyException e) {
-                    System.out.println(e.getMessage());
-                    System.out.println("todo usage: \"todo [description]\"");
+                    System.err.println(e.getMessage());
+                    System.err.println("todo usage: \"todo [description]\"");
                     prLine();
                 }
             }
 
             else if(input.startsWith("deadline ")) {
                 if (!input.contains("/by ")) {
-                    System.out.println("deadline usage: \"deadline [description] /by [by when]\"");
+                    System.err.println("deadline usage: \"deadline [description] /by [date]\"");
                     prLine();
                     continue;
                 }
@@ -135,15 +135,15 @@ public class Murphy {
                     Task deadline = new Deadline(split[0].substring(9).trim(), split[1]);
                     Murphy.addItem(deadline);
                 } catch (MurphyException e) {
-                    System.out.println(e.getMessage());
-                    System.out.println("deadline usage: \"deadline [description] /by [by when]\"");
+                    System.err.println(e.getMessage());
+                    System.err.println("deadline usage: \"deadline [description] /by [date]\"");
                     prLine();
                 }
             }
 
             else if (input.startsWith("event ")) {
                 if (!input.contains("/from ") || !input.contains("/to ")) {
-                    System.out.println("event usage: \"event [description] /from [by when] /to [to when]\"");
+                    System.err.println("event usage: \"event [description] /from [date] /to [date]\"");
                     prLine();
                     continue;
                 }
@@ -153,14 +153,14 @@ public class Murphy {
                     Task event = new Event(split[0].substring(6).trim(), split2[0].trim(), split2[1]);
                     Murphy.addItem(event);
                 } catch (MurphyException e) {
-                    System.out.println(e.getMessage());
-                    System.out.println("event usage: \"event [description] /from [by when] /to [to when]\"");
+                    System.err.println(e.getMessage());
+                    System.err.println("event usage: \"event [description] /from [date] /to [date]\"");
                     prLine();
                 }
             }
 
             else {
-                System.out.println("Command not found");
+                System.err.println("Command not found");
                 prLine();
             }
         }
