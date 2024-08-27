889a6f27df675d2e27589fab6091f396b40673e7
jktang14 <tangboy14@gmail.com>
2024-08-27 23:48:04 +0800

    Add TaskList manipulation methods

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void DeleteTask(int index) {
+        tasks.remove(index);
+    }
 }
