597e83dd6c577e8cc56feee2f1893eeb8e3bbbde
sarahteonin <E1249329@u.nus.edu>
2024-08-27 15:52:31 +0800

    Fix minor edits in FileReading.java

--- src/main/java/FileReading.java
+++ src/main/java/FileReading.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 public class FileReading {
     private static final String SEPARATOR = " \\| ";
-    private final static String FILE_PATH = "data/bob.txt";
 
     protected static void createDirectory(String dirName) {
         File directory = new File(dirName);
@@ -23,23 +22,27 @@ public class FileReading {
     }
 
     protected static ArrayList<Task> loadTasks(String filePath) throws FileNotFoundException {
-        ArrayList<Task> list = new ArrayList<Task>();
+        ArrayList<Task> list = new ArrayList<>();
         File f = new File(filePath);
         Scanner s = new Scanner(f);
         while (s.hasNext()) {
             String[] parts = s.nextLine().split(SEPARATOR);
-            if (parts[0].equals("T")) {
-                Task todo = new Todo(parts[2]);
-                todo.isDone = isDone(Integer.parseInt(parts[1]));
-                list.add(todo);
-            } else if (parts[0].equals("D")) {
-                Task deadline = new Deadline(parts[2], parts[3]);
-                deadline.isDone = isDone(Integer.parseInt(parts[1]));
-                list.add(deadline);
-            } else if (parts[0].equals("E")) {
-                Task event = new Event(parts[2], parts[3], parts[4]);
-                event.isDone = isDone(Integer.parseInt(parts[1]));
-                list.add(event);
+            switch (parts[0]) {
+                case "T" -> {
+                    Task todo = new Todo(parts[2]);
+                    todo.isDone = isDone(Integer.parseInt(parts[1]));
+                    list.add(todo);
+                }
+                case "D" -> {
+                    Task deadline = new Deadline(parts[2], parts[3]);
+                    deadline.isDone = isDone(Integer.parseInt(parts[1]));
+                    list.add(deadline);
+                }
+                case "E" -> {
+                    Task event = new Event(parts[2], parts[3], parts[4]);
+                    event.isDone = isDone(Integer.parseInt(parts[1]));
+                    list.add(event);
+                }
             }
         }
         return list;
@@ -47,10 +50,5 @@ public class FileReading {
 
     protected static boolean isDone(int i) {
         return i == 1;
-    }
-
-    public static boolean exists(String filePath) {
-        File f = new File(filePath);
-        return f.exists();
     }
 }
