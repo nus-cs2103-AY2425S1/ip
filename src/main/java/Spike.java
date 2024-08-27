1f511c3a6d75c7aaaa8b6b2d73b0774a4180d9b1
seantham21 <seantham21@gmail.com>
2024-08-27 15:37:33 +0800

    Level 5;Handle Errors

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -35,41 +35,47 @@ public class Spike {
     public static void echo() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine().trim();
-            String[] inputSplit = input.split(" ", 2);
-            InputType inputType = parseInput(inputSplit[0]);
+            try {
+                String input = scanner.nextLine().trim();
+                String[] inputSplit = input.split(" ", 2);
+                if (inputSplit.length == 1) {
+                    throw new SpikeException("Please enter a valid command");
+                }
+                InputType inputType = parseInput(inputSplit[0]);
 
-            switch (inputType) {
-                case BYE:
-                    byeMessage();
-                    break;
-                case LIST:
-                    listTasks();
-                    break;
-                case MARK:
-                    markTask(inputSplit[1]);
-                    break;
-                case UNMARK:
-                    unmarkTask(inputSplit[1]);
-                    break;
-                case TODO:
-                    addToDo(inputSplit[1]);
-                    break;
-                case DEADLINE:
-                    addDeadline(inputSplit[1]);
-                    break;
-                case EVENT:
-                    addEvent(inputSplit[1]);
-                    break;
-                case ERROR:
-                    System.out.println("     _________________________________________________________");
-                    System.out.println("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    System.out.println("     _________________________________________________________");
-                    break;
-            }
+                switch (inputType) {
+                    case BYE:
+                        byeMessage();
+                        break;
+                    case LIST:
+                        listTasks();
+                        break;
+                    case MARK:
+                        markTask(inputSplit[1]);
+                        break;
+                    case UNMARK:
+                        unmarkTask(inputSplit[1]);
+                        break;
+                    case TODO:
+                        addToDo(inputSplit[1]);
+                        break;
+                    case DEADLINE:
+                        addDeadline(inputSplit[1]);
+                        break;
+                    case EVENT:
+                        addEvent(inputSplit[1]);
+                        break;
+                    case ERROR:
+                        throw new SpikeException("Please enter a valid command");
+                }
 
-            if (inputType == InputType.BYE) {
-                break;
+                if (inputType == InputType.BYE) {
+                    break;
+                }
+            } catch (SpikeException e) {
+                System.out.println("     _________________________________________________________");
+                System.out.println("     " + e.getMessage());
+                System.out.println("     _________________________________________________________");
             }
         }
         scanner.close();
@@ -106,34 +112,65 @@ public class Spike {
         System.out.println("     _________________________________________________________");
     }
 
-    public static void markTask(String input) {
-        int index = Integer.parseInt(input) - 1;
-        toDoList.get(index).markAsDone();
+    public static void markTask(String input) throws SpikeException {
+        try {
+            int index = Integer.parseInt(input) - 1;
+            toDoList.get(index).markAsDone();
+        } catch (NumberFormatException e) {
+            throw new SpikeException("Please enter a valid number");
+        } catch (IndexOutOfBoundsException e) {
+            throw new SpikeException("Please enter a valid task number");
+        }
     }
 
-    public static void unmarkTask(String input) {
-        int index = Integer.parseInt(input) - 1;
-        toDoList.get(index).unmark();
+    public static void unmarkTask(String input) throws SpikeException {
+        try {
+            int index = Integer.parseInt(input) - 1;
+            toDoList.get(index).unmark();
+        } catch (NumberFormatException e) {
+            throw new SpikeException("Please enter a valid number");
+        } catch (IndexOutOfBoundsException e) {
+            throw new SpikeException("Please enter a valid task number");
+        }
     }
 
-    public static void addToDo(String input) {
+    public static void checkDescription (String input) throws SpikeException {
+        if (input.isEmpty() || !input.matches("\\S*?")) {
+            throw new SpikeException("The description of a todo cannot be empty.");
+        }
+    }
+
+    public static void addToDo(String input) throws SpikeException {
+        checkDescription(input);
         ToDo toDo = new ToDo(input);
         toDoList.add(toDo);
         addTaskString(toDo.toString());
     }
 
-    public static void addEvent(String input) {
+    public static void addEvent(String input) throws SpikeException {
+        checkDescription(input);
         String[] split = input.split(" /from | /to ");
-        Event toDo = new Event(split[0], split[1], split[2]);
-        toDoList.add(toDo);
-        addTaskString(toDo.toString());
+        if (split.length == 3) {
+            Event formattedEvent = new Event(split[0], split[1], split[2]);
+            toDoList.add(formattedEvent);
+            addTaskString(formattedEvent.toString());
+        } else {
+            throw new SpikeException("Please enter a valid event in the right format: " +
+                    "event description /from start time /to end time");
+        }
     }
 
-    public static void addDeadline(String input) {
+    public static void addDeadline(String input) throws SpikeException {
+        checkDescription(input);
         String[] split = input.split(" /by ");
-        Deadline toDo = new Deadline(split[0], split[1]);
-        toDoList.add(toDo);
-        addTaskString(toDo.toString());
+        if (split.length == 2) {
+            Deadline formattedDeadline = new Deadline(split[0], split[1]);
+            toDoList.add(formattedDeadline);
+            addTaskString(formattedDeadline.toString());
+        } else {
+            throw new SpikeException("Please enter a valid deadline in the right format: " +
+                    "deadline description /by due date");
+        }
     }
 
     public static void addTaskString(String taskToString) {
