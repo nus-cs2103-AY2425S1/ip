2d0c3a91bcc3d2b314a1049a5fc000a7c2c8e2d3
ywllowsensor <isyazani@gmail.com>
2024-08-27 16:59:49 +0800

    Event inputs will successfullly format if in yyyy-MM-dd format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task{
 
     private String from;
@@ -9,6 +13,26 @@ public class Event extends Task{
         this.to = to;
     }
 
+    private String formatDate(String input) {
+        if (isValidFormat(input)) {
+            LocalDate date = LocalDate.parse(input);
+            String formatted = date.format(DateTimeFormatter.ofPattern("d MMM uuuu"));
+            return formatted;
+        } else {
+            return input;
+        }
+    }
+
+    public boolean isValidFormat(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate.parse(input, formatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
     @Override
     public String toFileFormat() {
         return "E .. " + super.toFileFormat() + " .. " + this.from + " .. " + this.to;
@@ -16,6 +40,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + " (from: " + formatDate(this.from) + " to: " + formatDate(this.to) + ")";
     }
 }
