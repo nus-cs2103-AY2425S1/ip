6b806704ca23388df38b5eea7453e00f44e55e7a
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-08-27 21:56:55 +0800

    Add DateTime Support
    Added support for DateTime in Deadline and Events on top of Date.
    
    Updated test cases.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,28 +1,59 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
 
-  private final LocalDate from;
-  private final LocalDate to;
+  private final LocalDate fromDate;
+  private final LocalDateTime fromDateTime;
+  private final LocalDate toDate;
+  private final LocalDateTime toDateTime;
 
   public Event(String[] task) throws HamyoException {
     super(task);
     try {
-      this.from = LocalDate.parse(task[1]);
-      this.to = LocalDate.parse(task[2]);
+      LocalDateTime fromDateTimeTemp;
+      LocalDateTime toDateTimeTemp;
+      try {
+        fromDateTimeTemp = LocalDateTime.parse(task[1], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+      } catch (Exception e) {
+        fromDateTimeTemp = null;
+      } finally {
+        this.fromDate = LocalDate.parse(task[1].split(" ")[0]);
+      }
+      try {
+        toDateTimeTemp = LocalDateTime.parse(task[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+      } catch (Exception e) {
+        toDateTimeTemp = null;
+      } finally {
+        this.toDate = LocalDate.parse(task[2].split(" ")[0]);
+      }
+      this.fromDateTime = fromDateTimeTemp;
+      this.toDateTime = toDateTimeTemp;
     } catch (Exception e) {
-      throw new HamyoException("Invalid date format. yyyy-mm-dd.");
+      throw new HamyoException("Invalid date/time format. yyyy-MM-dd OR yyyy-MM-dd HH:mm.");
     }
   }
 
   @Override
   public String toString() {
-    return "[E] " + super.toString() + " (from: " + this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " to: " + this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+    String fromString = this.fromDateTime != null ?
+        this.fromDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")) + "HRS":
+        this.fromDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    String toString = this.toDateTime != null ?
+        this.toDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm"))  + "HRS":
+        this.toDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    return "[E] " + super.toString() + " (from: " + fromString + " to: " + toString + ")";
   }
 
   @Override
   public String toFileFormat() {
-    return "E" + " | " + super.toFileFormat() + " | " + this.from.toString() + " | " + this.to.toString();
+    String fromString = this.fromDateTime != null ?
+        this.fromDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")):
+        this.fromDate.toString();
+    String toString = this.toDateTime != null ?
+        this.toDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) :
+        this.toDate.toString();
+    return "E" + " | " + super.toFileFormat() + " | " + fromString + " | " + toString;
   }
 }
