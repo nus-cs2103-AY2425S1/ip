a90ceca8f3f72e9fa9959d529c4da8c73902b011
OrangeCatLoves <e1121707@u.nus.edu>
2024-08-27 15:19:55 +0800

    A-Gradle v1

--- src/main/java/TheOrangeRatchetCat.java
+++ src/main/java/TheOrangeRatchetCat.java
@@ -27,11 +27,9 @@ public class TheOrangeRatchetCat {
     public static void main(String[] args) {
         // The ChatBot named OrangeRatchetCat
         List<Task> items = new ArrayList<>();
-        Storage.loadTasks(items);
-        //loadTasks(items); // Load tasks from file at startup
+        Storage.loadTasks(items); // Load tasks from file at startup
         ratchetCatBot(items); // Pass the list of tasks to the chatbot
-        Storage.saveTasks(items);
-        //saveTasks(items); // Save tasks to file before exiting
+        Storage.saveTasks(items); // Save tasks to file before exiting
     }
 
     private static void ratchetCatBot(List<Task> items) {
@@ -44,26 +42,22 @@ public class TheOrangeRatchetCat {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 input = TaskList.checkList(input, items, scanner);
-                //input = TheOrangeRatchetCat.checkList(input, items, scanner);
                 continue;
             }
             if (input.startsWith("mark")) {
                 String numberPart = input.substring(4).trim();
                 input = TaskList.markingTask(numberPart, items, scanner);
-                //input = TheOrangeRatchetCat.markingTask(numberPart, items, scanner);
                 continue;
             }
             if (input.startsWith("unmark")) {
                 String numberPart = input.substring(6).trim();
                 input = TaskList.unmarkingTask(numberPart, items, scanner);
-                //input = TheOrangeRatchetCat.unmarkingTask(numberPart, items, scanner);
                 continue;
             }
             if (input.startsWith("todo")) {
                 try {
                     String taskDescription = input.substring(4).trim();
                     input = TaskList.addingToDo(taskDescription, items, scanner);
-                    //input = TheOrangeRatchetCat.addingToDo(taskDescription, items, scanner);
                     continue;
                 } catch (TheOrangeRatchetCatException e) {
                     System.out.println(e.getMessage());
@@ -74,7 +68,6 @@ public class TheOrangeRatchetCat {
             if (input.startsWith("deadline")) {
                 try {
                     input = TaskList.addingDeadline(input, items, scanner);
-                    //input = TheOrangeRatchetCat.addingDeadline(input, items, scanner);
                     continue;
                 } catch (TheOrangeRatchetCatException e) {
                     System.out.println(e.getMessage());
@@ -85,7 +78,6 @@ public class TheOrangeRatchetCat {
             if (input.startsWith("event")) {
                 try {
                     input = TaskList.addingEvent(input, items, scanner);
-                    //input = TheOrangeRatchetCat.addingEvent(input, items, scanner);
                     continue;
                 } catch (TheOrangeRatchetCatException e) {
                     System.out.println(e.getMessage());
@@ -100,14 +92,12 @@ public class TheOrangeRatchetCat {
             if (input.startsWith("delete")) {
                 int indexToDelete = Integer.parseInt(input.substring(6).trim());
                 input = TaskList.deleteTask(indexToDelete, items, scanner);
-                //input = TheOrangeRatchetCat.deleteTask(indexToDelete, items, scanner);
                 continue;
             }
             System.out.print("Inappropriate Command try again with adding either a Deadline/Todo/Event: ");
             input = scanner.nextLine(); // Reads the next line of input text again
         }
         Ui.bidFarewell();
-        //TheOrangeRatchetCat.bidFarewell();
         scanner.close(); // Close the scanner to avoid resource leaks
     }
 }
---
+++ src/test/java/tasklist/TaskListTest.java
@@ -0,0 +1,18 @@
+package tasklist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void test1() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(1, 1);
+    }
+}
