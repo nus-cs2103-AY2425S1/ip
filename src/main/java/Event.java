9d6576a0cb2a0899569313a38a8eacb63586937a
Nicholas Chua <e1114227@u.nus.edu>
2024-08-27 23:20:03 +0800

    Event.java: Change fields to store localDateTime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,26 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    DateTimeFormatter formatter;
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
+        formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), from, to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), from.format(formatter), to.format(formatter));
     }
 
     @Override
     public String getDataFormat() {
-        return "E" + super.getDataFormat() + " | " + from + "-" + to;
+        return "E" + super.getDataFormat() + " | " + from.format(formatter) + "-" + to.format(formatter);
     }
 }
+
