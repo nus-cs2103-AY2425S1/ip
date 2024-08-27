5e0b7045f4ef9778db866e86fe48c1e7c83f9302
Yan Hwee <yanhweeyee@gmail.com>
2024-08-27 21:06:55 +0800

    Add junit test cases

---
+++ src/test/java/gray/UtilityTest.java
@@ -0,0 +1,33 @@
+package gray;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class UtilityTest {
+    @Test
+    public void serialise_badFile_exceptionThrown() {
+        try {
+            Utility.serialise(new File("./awd/awd/awd"), new TaskList());
+            fail();
+        } catch (IOException e) {
+            assertEquals("./awd/awd/awd (No such file or directory)", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deserialise_badFile_exceptionThrown() {
+        try {
+            Utility.deserialise(new File("./awd/awd/awd"));
+            fail();
+        } catch (IOException e) {
+            assertEquals("./awd/awd/awd (No such file or directory)", e.getMessage());
+        } catch (ClassNotFoundException e) {
+            fail();
+        }
+    }
+}
