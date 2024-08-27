bbdcd8f0a66669dce2a9e78def7d5f194d8e7be2
suhayl13 <suhayl.pervez@gmail.com>
2024-08-27 21:50:30 +0800

    Add getter methods for deadline and event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,6 +7,12 @@ public class Event extends Task {
         this.from = from;
         this.to = to;
     }
+    public String getFrom() {
+        return this.from;
+    }
+    public String getTo() {
+        return this.to;
+    }
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.from + " to: " + to + ")";
