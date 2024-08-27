5846fbba4b4d7751f979ccf1b5444fd93ced5bd3
KennethTeo2002 <kenneth.teo2002@gmail.com>
2024-08-27 19:05:40 +0800

    Add deadline task test cases

---
+++ src/test/java/nerf/task/DeadlinesTest.java
@@ -0,0 +1,36 @@
+package nerf.task;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DeadlinesTest {
+    @Test
+    public void testNormalCreationOfTask(){
+        LocalDate due = LocalDate.parse("2024-08-27");
+        Task t = new Deadlines("Work", due);
+
+        // test 1: the print string is in the right format
+        assertEquals("[D][ ] Work (by: Aug 27 2024)", t.toString());
+
+        // test 2: the save string is in the right format
+        assertEquals("D | 0 | Work | 2024-08-27", t.getSaveFormat());
+    }
+
+    
+
+    @Test
+    public void testLoadedCreationOfTask(){
+        LocalDate due = LocalDate.parse("2024-08-27");
+        Task t = new Deadlines("Work", true, due);
+        
+        // test 1: the print string is in the right format
+        assertEquals("[D][X] Work (by: Aug 27 2024)", t.toString());
+
+        // test 2: the save string is in the right format
+        assertEquals("D | 1 | Work | 2024-08-27", t.getSaveFormat());
+    }
+}
