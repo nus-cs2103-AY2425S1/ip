c98049f7ce7444092d902139abfaa76933fef64b
harithh07 <harith.hakim2002@gmail.com>
2024-08-27 17:29:54 +0800

    Change if else blocks in Rasputin.java to switch case block

--- src/main/java/Rasputin.java
+++ src/main/java/Rasputin.java
@@ -1,6 +1,14 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 import java.util.Scanner;
 import java.util.ArrayList;
+
+
 
 public class Rasputin {
 
@@ -57,6 +65,7 @@ public class Rasputin {
 
     public static void main(String[] args) {
         String name = "Rasputin";
+        boolean isTerminated = false;
         // initialize task list
         ArrayList<Task> ls = new ArrayList<>();
 
@@ -82,169 +91,132 @@ public class Rasputin {
         // greeting
         printText("Hello, I'm " + name + "!\nWhat can I do for you?");
 
-        while (true) {
+        while (!isTerminated) {
             String input = scanner.nextLine().trim();
             String command = input.split(" ")[0];
-            // if user types "bye", break out of loop and bid farewell
-            if (command.equals("bye")) {
-                break;
+
+            switch (command) {
+                case "bye":
+                    isTerminated = true;
+                    break;
+                case "list":
+                    if (ls.isEmpty()) {
+                        printText("No tasks in list!");
+                        break;
+                    }
+                    int index = 1;
+                    System.out.println(lineBreak);
+                    for (Task item : ls) {
+                        System.out.println(index + "." + item.toString());
+                        index++;
+                    }
+                    System.out.println(lineBreak + "\n");
+                    break;
+                case "mark":
+                    if (ls.isEmpty()) {
+                        printText("No tasks in list!");
+                        break;
+                    }
+                    try {
+                        index = (input.charAt(5) - '0' - 1);
+                        ls.get(index).markAsDone();
+                        String output = "Marked that as done for you.\n" +
+                                ls.get(index).toString();
+                        printText(output);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        printText("ERROR! Task to be marked not specified.");
+                    } catch (IndexOutOfBoundsException e) {
+                        printText("ERROR! Task not found.");
+                    }
+                    break;
+
+                case "unmark":
+                    if (ls.isEmpty()) {
+                        printText("No tasks in list!");
+                        break;
+                    }
+                    try {
+                        index = (input.charAt(7) - '0' - 1);
+                        ls.get(index).markAsNotDone();
+                        String output = "Task has been unmarked.\n" +
+                                ls.get(index).toString();
+                        printText(output);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        printText("ERROR! Task to be unmarked not specified.");
+                    } catch (IndexOutOfBoundsException e) {
+                        printText("ERROR! Task not found.");
+                    }
+                    break;
+
+                case "todo":
+                    try {
+                        String desc = input.substring(5);
+                        Todo task = new Todo(desc);
+                        ls.add(task);
+                        String output = "Added Todo task:\n" + task.toString();
+                        output += "\nYou currently have " + ls.size() + " task/s in your list.";
+                        printText(output);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        printText("ERROR! The description of a todo cannot be empty.");
+                    } finally {
+                        break;
+                    }
+                case "deadline":
+                    try {
+                        String str = input.trim().substring(9);
+                        String desc = str.split(" /by ")[0];
+                        String deadline = str.split(" /by ")[1];
+                        Deadline task = new Deadline(desc, deadline);
+                        ls.add(task);
+                        String output = "Added Deadline task:\n" + task.toString();
+                        output += "\nYou currently have " + ls.size() + " task/s in your list.";
+                        printText(output);
+
+                    } catch (StringIndexOutOfBoundsException e) {
+                        printText("ERROR! The description of a deadline cannot be empty.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        printText("ERROR! Deadline tasks require a deadline to be completed by.");
+                    } finally {
+                        break;
+                    }
+                case "event":
+                    try {
+                        String str = input.substring(6);
+                        String desc = str.split(" /from ")[0];
+                        String duration = str.split(" /from ")[1];
+                        String from = duration.split(" /to ")[0];
+                        String to = duration.split(" /to ")[1];
+
+                        Event task = new Event(desc, from, to);
+                        ls.add(task);
+                        String output = "Added Event task:\n" + task.toString();
+                        output += "\nYou currently have " + ls.size() + " task/s in your list.";
+                        printText(output);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        printText("ERROR! The description of an event cannot be empty.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        printText("ERROR! Event tasks require a duration for the event.");
+                    } finally {
+                        break;
+                    }
+                case "delete":
+                    try {
+                        index = (input.charAt(7) - '0' - 1);
+                        String output = "Done, removed that task for you.\n" +
+                                ls.get(index).toString();
+                        ls.remove(index);
+                        printText(output);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        printText("ERROR! Task to be deleted not specified.");
+                    } catch (IndexOutOfBoundsException e) {
+                        printText("ERROR! Task not found.");
+                    } finally {
+                        break;
+                    }
+                default:
+                    printText("I'm sorry, I don't understand that command.");
+                    break;
             }
-
-            // display current list of tasks
-            if (command.equals("list")) {
-                if (ls.isEmpty()) {
-                    printText("No tasks in list!");
-                    continue;
-                }
-                int index = 1;
-                System.out.println(lineBreak);
-                for (Task item: ls) {
-                    System.out.println(index + "." + item.toString());
-                    index++;
-                }
-                System.out.println(lineBreak + "\n");
-                continue;
-            }
-
-            // unmark task
-            if (command.equals("unmark")) {
-                if (ls.isEmpty()) {
-                    printText("No tasks in list!");
-                    continue;
-                }
-                try {
-                    int index = (input.charAt(7) - '0' - 1);
-                    ls.get(index).markAsNotDone();
-                    String output = "Task has been unmarked.\n" +
-                            ls.get(index).toString();
-                    printText(output);
-                }
-                catch (StringIndexOutOfBoundsException e) {
-                    printText("ERROR! Task to be unmarked not specified.");
-                }
-                catch (IndexOutOfBoundsException e) {
-                    printText("ERROR! Task not found.");
-                }
-                continue;
-            }
-
-            // mark task as done
-            if (command.equals("mark")) {
-                if (ls.isEmpty()) {
-                    printText("No tasks in list!");
-                    continue;
-                }
-                try {
-                    int index = (input.charAt(5) - '0' - 1);
-                    ls.get(index).markAsDone();
-                    String output = "Marked that as done for you.\n" +
-                            ls.get(index).toString();
-                    printText(output);
-                }
-                catch (StringIndexOutOfBoundsException e) {
-                    printText("ERROR! Task to be marked not specified.");
-                }
-                catch (IndexOutOfBoundsException e) {
-                    printText("ERROR! Task not found.");
-                }
-                finally {
-                    continue;
-                }
-            }
-
-            // create todo task
-            if (command.equals("todo")) {
-                try {
-                    String desc = input.substring(5);
-                    Todo task = new Todo(desc);
-                    ls.add(task);
-                    String output = "Added Todo task:\n" + task.toString();
-                    output += "\nYou currently have " + ls.size() + " task/s in your list.";
-                    printText(output);
-                }
-                catch (StringIndexOutOfBoundsException e) {
-                    printText("ERROR! The description of a todo cannot be empty.");
-                }
-                finally {
-                    continue;
-                }
-
-            }
-
-            // create deadline task
-            if (command.equals("deadline")) {
-                try {
-                    String str = input.trim().substring(9);
-                    String desc = str.split(" /by ")[0];
-                    String deadline = str.split(" /by ")[1];
-                    Deadline task = new Deadline(desc, deadline);
-                    ls.add(task);
-                    String output = "Added Deadline task:\n" + task.toString();
-                    output += "\nYou currently have " + ls.size() + " task/s in your list.";
-                    printText(output);
-                    continue;
-                }
-                catch (StringIndexOutOfBoundsException e) {
-                    printText("ERROR! The description of a deadline cannot be empty.");
-                }
-
-                catch (ArrayIndexOutOfBoundsException e) {
-                    printText("ERROR! Deadline tasks require a deadline to be completed by.");
-                }
-                finally {
-                    continue;
-                }
-            }
-
-            // create event task
-            if (command.equals("event")) {
-                try {
-                    String str = input.substring(6);
-                    String desc = str.split(" /from ")[0];
-                    String duration = str.split(" /from ")[1];
-                    String from = duration.split(" /to ")[0];
-                    String to = duration.split(" /to ")[1];
-
-                    Event task = new Event(desc, from, to);
-                    ls.add(task);
-                    String output = "Added Event task:\n" + task.toString();
-                    output += "\nYou currently have " + ls.size() + " task/s in your list.";
-                    printText(output);
-                }
-                catch (StringIndexOutOfBoundsException e) {
-                    printText("ERROR! The description of an event cannot be empty.");
-                }
-
-                catch (ArrayIndexOutOfBoundsException e) {
-                    printText("ERROR! Event tasks require a duration for the event.");
-                }
-                finally {
-                    continue;
-                }
-            }
-
-            if (command.equals("delete")) {
-                try {
-                    int index = (input.charAt(7) - '0' - 1);
-                    String output = "Done, removed that task for you.\n" +
-                            ls.get(index).toString();
-                    ls.remove(index);
-                    printText(output);
-                }
-                catch (StringIndexOutOfBoundsException e) {
-                    printText("ERROR! Task to be deleted not specified.");
-                }
-                catch (IndexOutOfBoundsException e) {
-                    printText("ERROR! Task not found.");
-                }
-                finally {
-                    continue;
-                }
-            }
-
-
-
-            printText("I'm sorry, I don't understand that command.");
 
         }
 
