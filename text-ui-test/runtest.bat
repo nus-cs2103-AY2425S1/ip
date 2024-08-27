fa13c180b5955068a74368dd94d9bea5bfae9f49
ZShunRen <zhengshunren@outlook.com>
2024-08-27 13:43:25 +0800

    Update runtest.bat logic and finished refactoring

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,9 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+for /r ..\src\main\java %%f in (*.java) do (
+    javac -d ..\bin -cp ..\src\main\java -Xlint:none "%%f"
+)
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
