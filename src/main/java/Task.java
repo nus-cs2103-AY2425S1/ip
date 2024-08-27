958f1cd23bebe956292a2208e5ebc276934eeeee
hovanleong <hovanleong1@gmail.com>
2024-08-27 19:42:40 +0800

    Debug error which loads all tasks as done when bot is terminated and run again

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,6 +4,7 @@ public class Task {
 
     private final String description;
     private boolean isDone;
+    // For now, Deadline Tasks will take in date only, while Event Tasks will take in date and time
     protected static final DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     protected static final DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
     protected static final DateTimeFormatter dtf3 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
