e5a98b76e901b81291f85ba32d8efa9faa18676c
Kan_Jun_Hong <junhongkannot@gmail.com>
2024-08-28 00:04:23 +0800

    level-7
    added changes required for level 7

--- src/main/java/slaveFiles/Slave.java
+++ src/main/java/slaveFiles/Slave.java
@@ -117,7 +117,7 @@ public class Slave {
             System.out.println("Good to know that I have less things to remember now...");
             System.out.println("I'll forget about " + list.get(i - 1));
             list.remove(i - 1);
-//            save();
+            save();
         } catch (NumberFormatException nfe) {
             System.out.println("That's not a task number");
         } catch (IllegalArgumentException ile) {
@@ -194,7 +194,7 @@ public class Slave {
             if (insert) {
                 System.out.println("Hey maybe try using some of that memory of yours to remember these things...");
                 System.out.println("added: " + list.get(list.size() - 1));
-//                save();
+                save();
             }
         }
 
@@ -216,7 +216,7 @@ public class Slave {
             }
             Task t = list.get(i - 1);
             t.completed();
-//            save();
+            save();
             System.out.println("Finally doing something useful with your life eh...");
             System.out.println(t);
         } catch (NumberFormatException nfe) {
@@ -239,7 +239,7 @@ public class Slave {
             }
             Task t = list.get(i - 1);
             t.incomplete();
-//            save();
+            save();
             System.out.println("Slacking off now, are you?");
             System.out.println(t);
         } catch (NumberFormatException nfe) {
@@ -329,116 +329,116 @@ public class Slave {
         }
     }
 
-//    /**
-//     * converts the List<Task> to a string format and writes it to the savefile "./src/main/data/savefile.txt"
-//     * every line contains only 1 task
-//     * string format is as per the return value of toString() method of the respective task
-//     * creates a new file at "./src/main/data" called "savefile.txt" in the event of a missing save file
-//     */
-//    private static void save() {
-//        try {
-//            StringBuilder sb = new StringBuilder();
-//            for (Task t : list) {
-//                sb.append(t);
-//                sb.append("\n");
-//            }
-//            BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/data/savefile.txt"));
-//            writer.write(sb.toString());
-//            writer.close();
-//            System.out.println("saved");
-//        } catch (IOException ioe) {
-//            System.out.println("Save failed");
-//            System.out.println(ioe.getMessage());
-//        }
-//    }
-//
-//    /**
-//     * prints the contents of a string array
-//     * used for debugging purposes
-//     * @param arr is the array to be printed
-//     */
-//    private static void printArr(String[] arr) {
-//        for (String s : arr) {
-//            System.out.print(s + ",");
-//        }
-//        System.out.println("\n");
-//    }
-//
-//    /**
-//     * attempts to load the pre saved tasks from the save file "./src/main/data/savefile.txt"
-//     * if file does not exist at the path, does nothing
-//     * in the event of corrupted save file, skips the line
-//     *
-//     * save file format: as per toString() function of each task, with one task per line
-//     */
-//    private static void load() {
-//        try {
-//            Scanner sc = new Scanner(new File("./src/main/data/savefile.txt"));
-//            int success = 0;
-//            int failed = 0;
-//            while (sc.hasNextLine()) {
-//                try {
-//                    String task = sc.nextLine();
-//                    char taskType = task.charAt(1);
-//                    char taskCompleted = task.charAt(4);
-//                    int firstSpacePos = task.indexOf(" ");
-//                    boolean completed = false;
-//                    if (taskCompleted == ']') {
-//                        // do nothing
-//                    } else if (taskCompleted == 'X') {
-//                        completed = true;
-//                    } else {
-//                        throw new InvalidSaveFileFormatException("invalid completed status");
-//                    }
-//                    // identify the type of task:
-//                    switch (taskType) {
-//                        case 'T':
-//                            list.add(new Todo(completed, task.substring(firstSpacePos + 1)));
-//                            success++;
-//                            break;
-//                        case 'D':
-//                            // not sure how to get rid of error here for string formatting
-//                            String[] deadlineArray = task.split(" \\(by: ");
-//                            String deadlineName = deadlineArray[0].substring(firstSpacePos + 1);
-//                            String by = deadlineArray[1].substring(0, deadlineArray[1].length() - 1);
-//                            list.add(new Deadline(completed, deadlineName, by));
-//                            success++;
-//                            break;
-//                        case 'E':
-//                            // not sure how to get rid of error here for string formatting
-//                            String[] eventArray = task.split(" \\(from: ");
-//                            String eventName = eventArray[0].substring(firstSpacePos + 1);
-//                            String[] eventDetails = eventArray[1].split(" to: ");
-//                            String from = eventDetails[0];
-//                            String to = eventDetails[1].substring(0, eventDetails[1].length() - 1);
-//                            list.add(new Event(completed, eventName, from, to));
-//                            success++;
-//                            break;
-//                        default:
-//                            throw new InvalidSaveFileFormatException("invalid Task type");
-//                    }
-//                } catch (InvalidSaveFileFormatException | IndexOutOfBoundsException e) {
-//                    failed++;
-//                    System.out.println("error in event save file format: " + e.getMessage());
-//                }
-//            }
-//            if (success == 0) {
-//                System.out.println("No valid save file found");
-//            }
-//            if (failed > 0) {
-//                System.out.println("Deleting buggy save file");
-//                try {
-//                    BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/data/savefile.txt"));
-//                    writer.write("");
-//                    writer.close();
-//                    System.out.println("Buggy save file deleted");
-//                } catch (IOException e) {
-//                    System.out.println("Failed to purge buggy save file");
-//                }
-//            }
-//        } catch (FileNotFoundException fnfe) {
-//            System.out.println("no save file found");
-//        }
-//    }
+    /**
+     * converts the List<Task> to a string format and writes it to the savefile "./src/main/data/savefile.txt"
+     * every line contains only 1 task
+     * string format is as per the return value of toString() method of the respective task
+     * creates a new file at "./src/main/data" called "savefile.txt" in the event of a missing save file
+     */
+    private static void save() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            for (Task t : list) {
+                sb.append(t);
+                sb.append("\n");
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/data/savefile.txt"));
+            writer.write(sb.toString());
+            writer.close();
+            System.out.println("saved");
+        } catch (IOException ioe) {
+            System.out.println("Save failed");
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    /**
+     * prints the contents of a string array
+     * used for debugging purposes
+     * @param arr is the array to be printed
+     */
+    private static void printArr(String[] arr) {
+        for (String s : arr) {
+            System.out.print(s + ",");
+        }
+        System.out.println("\n");
+    }
+
+    /**
+     * attempts to load the pre saved tasks from the save file "./src/main/data/savefile.txt"
+     * if file does not exist at the path, does nothing
+     * in the event of corrupted save file, skips the line
+     *
+     * save file format: as per toString() function of each task, with one task per line
+     */
+    private static void load() {
+        try {
+            Scanner sc = new Scanner(new File("./src/main/data/savefile.txt"));
+            int success = 0;
+            int failed = 0;
+            while (sc.hasNextLine()) {
+                try {
+                    String task = sc.nextLine();
+                    char taskType = task.charAt(1);
+                    char taskCompleted = task.charAt(4);
+                    int firstSpacePos = task.indexOf(" ");
+                    boolean completed = false;
+                    if (taskCompleted == ']') {
+                        // do nothing
+                    } else if (taskCompleted == 'X') {
+                        completed = true;
+                    } else {
+                        throw new InvalidSaveFileFormatException("invalid completed status");
+                    }
+                    // identify the type of task:
+                    switch (taskType) {
+                        case 'T':
+                            list.add(new Todo(completed, task.substring(firstSpacePos + 1)));
+                            success++;
+                            break;
+                        case 'D':
+                            // not sure how to get rid of error here for string formatting
+                            String[] deadlineArray = task.split(" \\(by: ");
+                            String deadlineName = deadlineArray[0].substring(firstSpacePos + 1);
+                            String by = deadlineArray[1].substring(0, deadlineArray[1].length() - 1);
+                            list.add(new Deadline(completed, deadlineName, by));
+                            success++;
+                            break;
+                        case 'E':
+                            // not sure how to get rid of error here for string formatting
+                            String[] eventArray = task.split(" \\(from: ");
+                            String eventName = eventArray[0].substring(firstSpacePos + 1);
+                            String[] eventDetails = eventArray[1].split(" to: ");
+                            String from = eventDetails[0];
+                            String to = eventDetails[1].substring(0, eventDetails[1].length() - 1);
+                            list.add(new Event(completed, eventName, from, to));
+                            success++;
+                            break;
+                        default:
+                            throw new InvalidSaveFileFormatException("invalid Task type");
+                    }
+                } catch (InvalidSaveFileFormatException | IndexOutOfBoundsException e) {
+                    failed++;
+                    System.out.println("error in event save file format: " + e.getMessage());
+                }
+            }
+            if (success == 0) {
+                System.out.println("No valid save file found");
+            }
+            if (failed > 0) {
+                System.out.println("Deleting buggy save file");
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/data/savefile.txt"));
+                    writer.write("");
+                    writer.close();
+                    System.out.println("Buggy save file deleted");
+                } catch (IOException e) {
+                    System.out.println("Failed to purge buggy save file");
+                }
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("no save file found");
+        }
+    }
 
 }
