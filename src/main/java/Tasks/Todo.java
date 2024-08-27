764acb4a5be57b1e1d7747ff1fc111fbafd36795
ywllowsensor <isyazani@gmail.com>
2024-08-28 02:15:03 +0800

    Moved file into folder

---
+++ src/main/java/Tasks/Todo.java
@@ -0,0 +1,18 @@
+package Tasks;
+
+public class Todo extends Task {
+    
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "T .. " + super.toFileFormat();
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
