941e8a9f5b82b1db1455b385999d6180d14b97bb
Jeremy <e1155908@u.nus.edu>
2024-08-28 01:22:01 +0800

    A-MoreOOP: Abstracted TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TaskList {
+import java.util.ArrayList;
+
+public class TaskList {
+
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Task getTask(int index) {
+        return this.tasks.get(index - 1);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        return this.tasks.remove(index - 1);
+    }
+
+    public boolean isEmpty() {
+        return this.tasks.isEmpty();
+    }
 }
