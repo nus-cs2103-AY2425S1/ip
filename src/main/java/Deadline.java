208969154f97d52286f8520ab00eda1e179c6a77
ywllowsensor <isyazani@gmail.com>
2024-08-27 16:52:54 +0800

    Deadline dates of yyyy-MM-dd can be formatted

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     private String by;
@@ -7,6 +11,26 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+    private String formatDate() {
+        if (isValidFormat(this.by)) {
+            LocalDate date = LocalDate.parse(this.by);
+            String formatted = date.format(DateTimeFormatter.ofPattern("d MMM uuuu"));
+            return formatted;
+        } else {
+            return this.by;
+        }
+    }
+
+    public boolean isValidFormat(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate.parse(input, formatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
     @Override
     public String toFileFormat() {
         return "D .. " + super.toFileFormat() + " .. " + this.by;
@@ -14,6 +38,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + formatDate() + ")";
     }
 }
