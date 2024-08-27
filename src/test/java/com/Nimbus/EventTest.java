1ba39ce3d5e7e7ec0ffc19eb3ea729522a14b750
rimgik <karon503.cpp@gmail.com>
2024-08-27 21:01:56 +0800

    Add Junit test

---
+++ src/test/java/com/Nimbus/EventTest.java
@@ -0,0 +1,14 @@
+package com.Nimbus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    void testToString() {
+        Event event = new Event("Test", true, "2024-01-01", "2025-12-12");
+        assertEquals(event.toString(), "[E][X] Test (from: Jan 1 2024 to: Dec 12 2025)");
+    }
+}
---
+++ src/test/java/com/Nimbus/ParserTest.java
@@ -0,0 +1,18 @@
+package com.Nimbus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void readOption_NormalInput_readSuccess() {
+        try {
+            String tmp = "A very very long string /with /some Hello World /option ";
+            assertEquals(Parser.readOption(tmp, "some"), "Hello World");
+            assertEquals(Parser.readOption(tmp, "with"), "");
+        } catch (InvalidArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
