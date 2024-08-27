0f578ca6cc47f021610c370ddf89a4afd8cbfa31
Jollybomber <siahevan@gmail.com>
2024-08-27 17:20:36 +0800

    Edited exception class

--- src/main/java/WrongInputException.java
+++ src/main/java/WrongInputException.java
@@ -1,11 +1,10 @@
 public class WrongInputException extends Exception {
-    private final String errorMessage;
     public WrongInputException(String errorMessage) {
-        this.errorMessage = errorMessage;
+        super(errorMessage);
     }
 
     @Override
     public String toString() {
-        return errorMessage;
+        return super.getMessage();
     }
 }
