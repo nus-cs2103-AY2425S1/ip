fd2b17bfb0b1a514bcdc7fe83ffa2ef6505d86b2
LiuYJ2002 <liuyujia2002@gmail.com>
2024-08-28 00:48:44 +0800

    Add date time recognition functionality to chatbot

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 public class Deadline extends Task {
 
-    protected String by;
-
+    LocalDateTime by;
+    String savedBy;
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.savedBy = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("M/d/yyyy HHmm"));
     }
     public Deadline(String description, String by, boolean isDone) {
         super(description, isDone);
-        this.by = by;
+        this.savedBy = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("M/d/yyyy HHmm"));
     }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        //return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                by.format(DateTimeFormatter.ofPattern("MMMM dd yyyy, ha", Locale.ENGLISH)) + ")";
     }
 
     @Override
     public String save() {
-        return "D | " + super.save() + " | " + by;
+        return "D | " + super.save() + " | " + savedBy;
     }
 }
