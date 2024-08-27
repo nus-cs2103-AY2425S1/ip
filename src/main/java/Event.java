d2f3f09e3fc7b20e8b34452bf1eca1de9ca981bb
rimgik <karon503.cpp@gmail.com>
2024-08-27 14:06:27 +0800

    Add support for saving file

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,27 @@ public class Event extends Task{
         this.to = to;
     }
 
+    Event(String description, boolean isDone , String from, String to) {
+        super(description, isDone);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFromDate() {
+        return this.from;
+    }
+
+    public String getToDate() {
+        return this.to;
+    }
+
+    @Override
+    public String toFileFormat() {
+        String isDoneString = this.isDone ? "1" : "0";
+        return getTypeIcon().charAt(1) + "|" + isDoneString + "|" +
+                this.getDescription() + "|" + this.getFromDate() + "|" + this.getToDate();
+    }
+
     @Override
     public String toString() {
         return getTypeIcon() + super.toString()
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -4,6 +4,16 @@ public class Todo extends Task{
         super(description);
     }
 
+    Todo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    @Override
+    public String toFileFormat() {
+        String isDoneString = this.isDone ? "1" : "0";
+        return getTypeIcon().charAt(1) + "|" + isDoneString + "|" + this.getDescription();
+    }
+
     @Override
     public String toString() {
         return getTypeIcon() + super.toString();
