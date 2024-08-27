8dd719a221fe8c845e07f53b9289fb90c4c08427
LeeZeHao <zehaolee2004@gmail.com>
2024-08-28 04:59:01 +0800

    Add WrongDateFormatException.java

---
+++ src/main/java/WrongDateFormatException.java
@@ -0,0 +1,10 @@
+/** Exception thrown when the date given by user for a task is not of the following format.
+ * Child of StelleException.
+ * @author Lee Ze Hao (A0276123J)
+ */
+
+public class WrongDateFormatException extends StelleException {
+    public WrongDateFormatException() {
+        super("Dates must be in \"yyyy-MM-dd HH:mm\" format!");
+    }
+}
