0a39c321750700d47ce46fea941532cc04a72af6
SooYap <tansooyap@Tans-MacBook-Pro-4.local>
2024-08-27 22:44:34 +0800

    Rainy.java: Add support for saving data using File and FileWriter objects

--- src/main/java/TaskTracker.java
+++ src/main/java/TaskTracker.java
@@ -3,27 +3,36 @@ import java.util.*;
 public class TaskTracker {
     private ArrayList<Task> taskList;
     private int counter;
+    private boolean receivedInputs;
 
     public TaskTracker() {
         this.taskList = new ArrayList<Task>();
         this.counter = 0;
+        this.receivedInputs = false;
     }
     public int getCounter() {
         return this.counter;
     }
-    public void getList() {
+    public boolean getReceivedInputs() {
+        return this.receivedInputs;
+    }
+    public String getList() {
         if (this.counter == 0) {
-            System.out.println("No tasks currently!");
+            return "No tasks currently!";
         } else {
-            System.out.println("Here are your tasks!!! Remember to complete them!!!");
+            String result = "";
+            result += "Here are your tasks!!! Remember to complete them!!!" + "\n";
             for (int i = 0; i < this.counter; i++) {
                 if (this.taskList.get(i).getIsDone() == false) {
-                    System.out.println("" + (i + 1) + ". [ ] " + this.taskList.get(i));
+                    result += "" + (i + 1) + ". [ ] " + this.taskList.get(i);
                 } else {
-                    System.out.println("" + (i + 1) + ". [X] " + this.taskList.get(i));
+                    result += "" + (i + 1) + ". [X] " + this.taskList.get(i);
                 }
-
+                if (i != this.counter - 1) {
+                    result += "\n";
+                }
             }
+            return result;
         }
     }
 
@@ -37,6 +46,9 @@ public class TaskTracker {
             Task newTask = this.taskList.get(z);
             newTask.mark();
             this.taskList.set(z, newTask);
+            if (this.receivedInputs) {
+                System.out.println("Well Done Champ! I've marked this task as done!");
+            }
         }
     }
 
@@ -50,31 +62,40 @@ public class TaskTracker {
             Task newTask = this.taskList.get(q);
             newTask.unmark();
             this.taskList.set(q, newTask);
+            if (this.receivedInputs) {
+                System.out.println("I've marked this task as undone! Please remember to complete it!");
+            }
         }
     }
 
     public void updateListToDo(String s) {
         this.taskList.add(new ToDo(s));
         this.counter++;
-        System.out.println("Gotcha!! Added this task to your list:");
-        System.out.println(this.taskList.get(this.counter - 1));
-        System.out.println("Take note you currently have " + this.counter + " task/s on your list. You got this Champ!!!");
+        if (this.receivedInputs) {
+            System.out.println("Gotcha!! Added this task to your list:");
+            System.out.println(this.taskList.get(this.counter - 1));
+            System.out.println("Take note you currently have " + this.counter + " task/s on your list. You got this Champ!!!");
+        }
     }
 
     public void updateListDeadline(String s, String t) {
         this.taskList.add(new Deadline(s, t));
         this.counter++;
-        System.out.println("Gotcha!! Added this task to your list:");
-        System.out.println(this.taskList.get(this.counter - 1));
-        System.out.println("Take note you currently have " + this.counter + " task/s on your list. You got this Champ!!!");
+        if (this.receivedInputs) {
+            System.out.println("Gotcha!! Added this task to your list:");
+            System.out.println(this.taskList.get(this.counter - 1));
+            System.out.println("Take note you currently have " + this.counter + " task/s on your list. You got this Champ!!!");
+        }
     }
 
     public void updateListEvent(String s, String t, String u) {
         this.taskList.add(new Event(s, t, u));
         this.counter++;
-        System.out.println("Gotcha!! Added this task to your list:");
-        System.out.println(this.taskList.get(this.counter - 1));
-        System.out.println("Take note you currently have " + this.counter + " task/s on your list. You got this Champ!!!");
+        if (this.receivedInputs) {
+            System.out.println("Gotcha!! Added this task to your list:");
+            System.out.println(this.taskList.get(this.counter - 1));
+            System.out.println("Take note you currently have " + this.counter + " task/s on your list. You got this Champ!!!");
+        }
     }
 
     public void delete(int i) throws InvalidIndexException {
@@ -88,5 +109,8 @@ public class TaskTracker {
         this.taskList.remove(i);
         counter--;
         System.out.println("You currently have " + counter + " task/s on hand. Please remember to complete them!");
+    }
+    public void receivedFirstInput() {
+        this.receivedInputs = true;
     }
 }
