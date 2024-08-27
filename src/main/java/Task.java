9da56d74e3f08f334d0053f7a28d100d14edc8e3
uniqly <simontong09@gmail.com>
2024-08-27 11:19:37 +0800

    Task.java: Add factory method for taskSaveString to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,5 @@
+import java.text.Normalizer;
+
 public abstract class Task {
     protected String description;
     protected boolean isDone;
@@ -8,6 +10,36 @@ public abstract class Task {
         }
         this.description = description;
         isDone = false;
+    }
+
+    public static Task getTaskFromSave(String taskSaveString) {
+        try {
+            taskSaveString = taskSaveString.trim(); // removes extra new lines
+            String[] args = taskSaveString.split("(\\s\\|\\s)"); // splits string along < | > delimiter
+
+            String type = args[0];
+            boolean isDone = args[1].equals("1");
+            Task task = switch (type) {
+                case "T" -> new TodoTask(args[2]);
+                case "D" -> new DeadlineTask(args[2], args[3]);
+                case "E" -> new EventTask(args[2], args[3], args[4]);
+                default -> null;
+            };
+
+            if (task != null) {
+                task.isDone = isDone;
+            }
+
+            return task;
+
+        } catch (IndexOutOfBoundsException | TaskArgumentMissingException e) {
+            FormatPrinter.printIndent(
+                    String.format(
+                            "Warning: <%s> is missing required arguments, ignoring task.",
+                            taskSaveString),
+                    Ekud.OUTPUT_PREFIX);
+            return null;
+        }
     }
 
     public abstract String getEmptyDescriptionErrorMessage();
