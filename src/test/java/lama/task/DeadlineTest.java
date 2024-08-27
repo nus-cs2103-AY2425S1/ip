f7c330ce45ef64a212fb4c519c8f2d4178aeb9fe
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-08-27 14:45:21 +0800

    Test cases for deadline

---
+++ src/test/java/lama/task/DeadlineTest.java
@@ -0,0 +1,32 @@
+package lama.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void ConstructorTest() {
+        String description = "Return Book";
+        LocalDate by = LocalDate.of(2025, 12, 12);
+        Deadline deadline = new Deadline(description, by);
+
+        String output = "[D][ ] Return Book (by: Dec 12 2025)";
+
+        assertEquals(output, deadline.toString());
+    }
+
+    @Test
+    public void toFileTest() {
+        String description = "Return Book";
+        LocalDate by = LocalDate.of(2025, 12, 12);
+        Deadline deadline = new Deadline(description, by);
+
+        String output = "D | 0 | Return Book | 2025-12-12";
+
+        assertEquals(output, deadline.toFile());
+    }
+}
