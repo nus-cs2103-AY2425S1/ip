92c0954954e29f854d658016865cbccf100e7050
Teddayz <mervynneo02@gmail.com>
2024-08-28 00:21:45 +0800

    Update chatbot to understand Date and Time for Deadline Tasks

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,9 +1,15 @@
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Storage {
     private final String filePath;
+    private final DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("MMM dd yyyy h:mm a");
+    private final DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
     public Storage(String filePath) {
         this.filePath = filePath;
@@ -42,8 +48,11 @@ public class Storage {
                     System.out.print("File corrupted");
                     return null;
                 }
-                String deadline = parts[3];
-                return new DeadlineTask(description, isDone, deadline);
+                String deadlineStr = parts[3];
+                // Parse the string in the current inputFormat
+                LocalDateTime deadline = LocalDateTime.parse(deadlineStr, inputFormat);
+                // Convert the string into the desired format for me to create a new Task.
+                return new DeadlineTask(description, isDone, deadline.format(outputFormat));
             case "E":
                 if (parts.length < 5) {
                     System.out.print("File corrupted");
