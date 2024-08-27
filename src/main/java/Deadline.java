8ab5b53c1e85468e48e26791801e360421d441c2
chuajunyu <chuajunyu1@gmail.com>
2024-08-28 00:48:36 +0800

    Feature: Add datetime parsing and formatting for `Deadline` and `Event` classes
    Previously, the DateTime for the Deadline and Event classes were stored as string. This made it
    not very clear on the DateTime and also means we cannot implement features such as search by DateTime
    in the future.
    
    Let's standardise the DateTime input to YYYY-MM-DD HH:mm (24 hour format). We can then parse this into
    a Java `LocalDateTime` object and format it into a more clear and informative string.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    private String deadline;
-    public Deadline(String taskDescription, String deadline) {
+    private LocalDateTime deadline;
+    private DateTimeFormatter saveFormatter;
+    private DateTimeFormatter uiFormatter;
+
+    public Deadline(String taskDescription, LocalDateTime deadline) {
         super(taskDescription);
         this.deadline = deadline;
+        this.saveFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.uiFormatter = DateTimeFormatter.ofPattern("MMM d yyyy hh:mma");
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        return "[D]" + super.toString() + " (by: " + this.deadline.format(uiFormatter) + ")";
     }
 
     @Override
     public String toSaveRepresentation() {
-        return "D | " + super.toSaveRepresentation() + " | " + this.deadline;
+        return "D | " + super.toSaveRepresentation() + " | " + deadline.format(saveFormatter);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,28 @@
-public class Event extends Task {
-    private String from;
-    private String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String eventDescription, String from, String to) {
+
+public class Event extends Task {
+    private LocalDateTime from;
+    private LocalDateTime to;
+    private DateTimeFormatter saveFormatter;
+    private DateTimeFormatter uiFormatter;
+
+    public Event(String eventDescription, LocalDateTime from, LocalDateTime to) {
         super(eventDescription);
         this.from = from;
         this.to = to;
+        this.saveFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.uiFormatter = DateTimeFormatter.ofPattern("MMM d yyyy hh:mma");
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + " (from: " + this.from.format(uiFormatter) + " to: " + this.to.format(uiFormatter) + ")";
     }
 
     @Override
     public String toSaveRepresentation() {
-        return "E | " + super.toSaveRepresentation() + " | " + this.from + " | " + this.to;
+        return "E | " + super.toSaveRepresentation() + " | " + this.from.format(saveFormatter) + " | " + this.to.format(saveFormatter);
     }
 }
