38554b060e974525c5f2e5425a8a3cb6f3a55ef5
giahien0505 <giahien0505@gmail.com>
2024-08-28 16:22:32 +0800

    Add support for echos

--- src/main/java/Hien.java
+++ src/main/java/Hien.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Hien {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Hien");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
