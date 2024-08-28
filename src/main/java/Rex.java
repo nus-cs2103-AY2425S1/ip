6af6dc175a1ba90e33150f79ed65163b38ba0002
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-28 12:49:05 +0800

    Refactor code to decouple chatbot functionalities in Rex class

--- src/main/java/Rex.java
+++ src/main/java/Rex.java
@@ -1,335 +1,97 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Rex {
-    // File path of task list and temp list
-    private static String filepath = "./data/rex.txt";
-    private static String temppath = "./data/tmp.txt";
-
-    // Horizontal line separation
-    private static String separation = "____________________________________________________________";
-
-    // "rawr" string added to end of each print statement
-    private static String rawr = "rawr";
-
-    // "RAWRRRR" that comes with each error message
-    private static String errorPrefix = "RAWRRRR!!!";
-
-    public static void main(String[] args) {
-        // ArrayList to store added tasks
-        ArrayList<Task> list = new ArrayList<>();
-        
+    private Storage storage;
+    private TaskList taskList;
+    private Ui ui;
+    public Rex() {
         try {
-            loadFile(list);
+            this.storage = new Storage();
+            this.taskList = new TaskList(storage);
+            this.ui = new Ui();
         } catch (IOException e) {
             System.out.println("An error has occurred.");
-            return;
-        } catch (InvalidTaskException e) {
-            return;
         }
-        // Rex's greeting message
-        System.out.println(separation);
-        System.out.println("Hello! I'm Rex! " + rawr);
-        System.out.println("What can I do for you? " + rawr);
+    }
 
-        // Initialize scanner to take in user input
-        Scanner scanner = new Scanner(System.in);
+    public void run() {
+        ui.greeting();
 
         while (true) {
-            System.out.println(separation);
-            // Takes in user input and extract command
-            String[] input = scanner.nextLine().split(" ", 2);
-
-            Command command;
+            Command command = null;
             try {
-                command = Command.inputToCommand(input[0]);
-            } catch (InvalidInputException e) {
-                System.out.println(separation);
-                System.out.println(errorPrefix + " " + e.getMessage());
-                System.out.println("Enter \"help\" for a list of what I do know! " + rawr);
-                continue;
-            }
+                String input = ui.readInput();
+                String[] inputTokens = Parser.parseInput(input);
 
-            // Process user command
-            try {
+                // Process user command
+                command = Command.inputToCommand(inputTokens[0]);
                 switch (command) {
-                    // List all valid commands
-                    case HELP:
-                        // help should be used alone
-                        if (input.length > 1) {
-                            throw new InvalidInputException(errorPrefix + " Too many arguments!");
-                        }
-
-                        System.out.println(Command.listCommands());
-                        break;
-                    // Adding a task to list
-                    case TODO:
-                    case DEADLINE:
-                    case EVENT:
-                        // Argument cannot be empty
-                        if (input.length <= 1) {
-                            throw new InvalidInputException(errorPrefix + " Task description cannot be empty!");
-                        }
-
-                        // Create task and add to list
-                        String argument = input[1];
-                        Task newTask = createTask(list, command, argument);
-                        System.out.println(separation);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println("  " + newTask);
-                        System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in the list.");
-
-                        updateFile(list);
-                        break;
-                    // Display items added as a numbered list
-                    case LIST:
-                        // list should be used alone
-                        if (input.length > 1) {
-                            throw new InvalidInputException(errorPrefix + " Too MANY arguments!");
-                        }
-
-                        displayList(list);
-                        break;
-                    // Mark/unmark task in specified index as done
-                    case MARK:
-                    case UNMARK:
-                    case DELETE:
-                        // Must have a number argument
-                        if (input.length <= 1) {
-                            throw new InvalidInputException(errorPrefix + " Too FEW arguments!");
-                        }
-
-                        // Get task from index
-                        System.out.println(separation);
-                        argument = input[1];
-                        int index = Integer.parseInt(argument);
-                        Task actionTask = list.get(index - 1);
-
-                        // Mark/unmark task according to command
-                        if (command.equals(Command.MARK)) {
-                            actionTask.markDone();
-                            System.out.println("Nice! I've marked this task as done:");
-                            System.out.println("  " + actionTask);
-                        } else if (command.equals(Command.UNMARK)) {
-                            actionTask.unmarkDone();
-                            System.out.println("OK, I've marked this task as not done yet:");
-                            System.out.println("  " + actionTask);
-                        } else {
-                            // Delete task from list
-                            deleteTask(list, actionTask);
-                        }
-
-                        updateFile(list);
-                        break;
-                    case RAWR:
-                        System.out.println(separation);
-                        System.out.println(rawr + "!");
-                        break;
-                    // Exit program
-                    case BYE:
-                        // bye should be used alone
-                        if (input.length > 1) {
-                            throw new InvalidInputException(errorPrefix + " Too many arguments!");
-                        }
-
-                        // Rex's goodbye message
-                        System.out.println(separation);
-                        System.out.println("Bye. Hope to see you again soon! " + rawr);
-                        System.out.println(separation);
-
-                        // Close scanner to avoid memory leaks
-                        scanner.close();
-                        return;
+                case HELP:
+                    ui.help();
+                    break;
+                case TODO:
+                    String argument = inputTokens[1];
+                    ToDo toDo = taskList.addToDo(argument);
+                    ui.addTask(toDo);
+                    break;
+                case DEADLINE:
+                    argument = inputTokens[1];
+                    Deadline deadline = taskList.addDeadline(argument);
+                    ui.addTask(deadline);
+                    break;
+                case EVENT:
+                    argument = inputTokens[1];
+                    Event event = taskList.addEvent(argument);
+                    ui.addTask(event);
+                    break;
+                case LIST:
+                    ui.displayList(taskList);
+                    break;
+                case MARK:
+                    argument = inputTokens[1];
+                    Task marked = taskList.markTask(argument);
+                    ui.markTask(marked);
+                    break;
+                case UNMARK:
+                    argument = inputTokens[1];
+                    Task unmarked = taskList.unmarkTask(argument);
+                    ui.unmarkTask(unmarked);
+                    break;
+                case DELETE:
+                    argument = inputTokens[1];
+                    Task deleted = taskList.deleteTask(argument);
+                    ui.deleteTask(deleted);
+                    break;
+                case RAWR:
+                    ui.rawr();
+                    break;
+                case BYE:
+                    ui.goodbye();
+                    return;
                 }
             } catch (InvalidInputException e) {
-                System.out.println(separation);
-                System.out.println(e.getMessage());
-                System.out.println("Usage: " + Command.usageMessage(command));
-            } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                System.out.println(errorPrefix + " Invalid task number!");
-                System.out.println("Usage: " + Command.usageMessage(command));
-            } catch (InvalidTaskException e) {
-                System.out.println("Usage: " + Command.usageMessage(command));
+                if (command != null) {
+                    String usageMessage = Command.usageMessage(command);
+                    ui.errorMessage(e.getMessage() + "\nUsage: " + usageMessage);
+                } else {
+                    ui.errorMessage(e);
+                }
             } catch (IOException e) {
                 System.out.println("An error has occurred.");
+                throw new RuntimeException(e);
+            } catch (DateTimeParseException e) {
+                String usageMessage = Command.usageMessage(command);
+                ui.errorMessage("Wrong date/time format!\nUsage: " + usageMessage);
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                String usageMessage = Command.usageMessage(command);
+                ui.errorMessage("Invalid task number!\nUsage: " + usageMessage);
             }
         }
     }
 
-    private static Task createTask(List<Task> list, Command command, String argument)
-            throws InvalidTaskException, IOException {
-        Task newTask;
-        try {
-            if (command.equals(Command.TODO)) {
-                // Create new ToDo task
-                newTask = new ToDo(argument);
-            } else if (command.equals(Command.DEADLINE)) {
-                // Create new Deadline task
-                newTask = createDeadline(argument);
-            } else {
-                // Create new Event task
-                newTask = createEvent(argument);
-            }
-        } catch (InvalidInputException e) {
-            System.out.println(separation);
-            System.out.println(e.getMessage());
-            throw new InvalidTaskException();
-        } catch (DateTimeParseException e) {
-            System.out.println(separation);
-            System.out.println(errorPrefix + " Wrong date/time format!");
-            throw new InvalidTaskException();
-        }
-
-        // Add task to list
-        list.add(newTask);
-
-        return newTask;
-    }
-
-    private static Deadline createDeadline(String argument) throws InvalidInputException {
-        String[] descriptionBy = argument.split("/by ", 2);
-        if (descriptionBy.length < 2) {
-            throw new InvalidInputException(errorPrefix + " /by not found in input!");
-        }
-
-        // Parse String to LocalDateTime
-        LocalDateTime by = LocalDateTime.from(LocalDateTime.parse(descriptionBy[1],
-                DateTimeFormatter.ofPattern("dd-MM-yy HHmm")));
-
-        return new Deadline(descriptionBy[0], by);
-    }
-
-    private static Event createEvent(String argument) throws InvalidInputException {
-        String[] tokens = argument.split("/from ", 2);
-        if (tokens.length < 2) {
-            throw new InvalidInputException(errorPrefix + " /from not found in input!");
-        }
-
-        String description = tokens[0];
-        String[] fromTo = tokens[1].split(" /to ", 2);
-
-        if (fromTo.length < 2) {
-            throw new InvalidInputException(errorPrefix + " /to not found in input!");
-        }
-
-        // Parse String to LocalDateTime
-        LocalDateTime from = LocalDateTime.from(LocalDateTime.parse(fromTo[0],
-                DateTimeFormatter.ofPattern("dd-MM-yy HHmm")));
-
-        LocalDateTime to = LocalDateTime.from(LocalDateTime.parse(fromTo[1],
-                DateTimeFormatter.ofPattern("dd-MM-yy HHmm")));
-
-        return new Event(description, from, to);
-    }
-
-    private static void displayList(List<Task> list) {
-        System.out.println(separation);
-
-        // Print statement for an empty list
-        if (list.size() == 0) {
-            System.out.println("The list is empty! " + rawr);
-            return;
-        }
-
-        for (int i = 1; i <= list.size(); i++) {
-            System.out.println(i + "." + list.get(i - 1));
-        }
-    }
-
-    private static void deleteTask(List<Task> list, Task actionTask) {
-        // Remove task from list and decrease number
-        list.remove(actionTask);
-        Task.removeTask();
-
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(actionTask);
-        System.out.println("Now you have " + Task.getNumberOfTasks() + " tasks in the list.");
-    }
-
-    private static void loadFile(List<Task> list) 
-            throws IOException, InvalidTaskException {
-        // Create new file and directory for filepath
-        File f = new File(filepath);
-        File dir = f.getParentFile();
-
-        // Create new directory if does not exist
-        if (!dir.isDirectory()) {
-            dir.mkdirs();
-        }
-
-        // Create new file if does not exist
-        if (!f.exists()) {
-            f.createNewFile();
-        }
-
-        // Initialize reader to read from save file
-        FileReader r = new FileReader(f);
-        BufferedReader reader = new BufferedReader(r);
-        String currentLine;
-
-        // Iterate through each line in save file
-        while ((currentLine = reader.readLine()) != null) {
-            String[] taskStrings = currentLine.split(" \\| ");
-            String taskType = taskStrings[0];
-            int isMarked = Integer.parseInt(taskStrings[1]);
-
-            // Add to task list
-            Task newTask = null;
-            String argument = taskStrings[2];
-            switch (taskType) {
-            case "T":
-                newTask = createTask(list, Command.TODO, argument);
-                break;
-            case "D":
-                argument += " /by " + taskStrings[3];
-                newTask = createTask(list, Command.DEADLINE, argument);
-                break;
-            case "E":
-                argument += " /from " + taskStrings[3] + " /to " + taskStrings[4];
-                newTask = createTask(list, Command.EVENT, argument);
-                break;
-            }
-            
-            if (isMarked == 1) {
-                newTask.markDone();
-            }
-        }
-
-        // Close readers
-        reader.close();
-        r.close();
-
-    }
-
-    private static void updateFile(ArrayList<Task> list) throws IOException {
-        File f = new File(filepath);
-        File tmp = new File(temppath);
-
-        // Create temp file to copy taskList from
-        Files.deleteIfExists(Paths.get(temppath));
-        tmp.createNewFile();
-
-        FileWriter writer = new FileWriter(tmp, true);
-
-        // Create new file with updated list
-        for (int i = 1; i <= list.size(); i++) {
-            Task task = list.get(i - 1);
-            writer.write(task.formatted() + System.lineSeparator());
-        }
-
-        // Close writer
-        writer.close();
-
-        // Copy to save file
-        Files.copy(Paths.get(temppath), Paths.get(filepath), StandardCopyOption.REPLACE_EXISTING);
-        Files.delete(Paths.get(temppath));
+    public static void main(String[] args) {
+        Rex rex = new Rex();
+        rex.run();
     }
 }
