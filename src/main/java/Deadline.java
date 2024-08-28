1bc27d01b1e6bfc7da6b3b4862fd0bed40809d87
Shaun <Hundin231@gmail.com>
2024-08-29 01:08:12 +0800

    Add support for DateTime objects

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
-    public Deadline(String taskName, String by) {
+    private String by;
+    private LocalDateTime byDateTime; 
+    
+    public Deadline(String taskName, String by) throws Meowception {
         super(taskName);
-        this.by = by;
+        //this.by = by;
+        try {
+            byDateTime = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        } catch (java.time.format.DateTimeParseException e) {
+            throw new Meowception("300");
+        }
     }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmm MMM dd yyyy");
+        String formattedDateTime = byDateTime.format(formatter);
+        return "[D]" + super.toString() + " (by: " + formattedDateTime + ")";
     }
 
     @Override
     public String getExtra() {
-        return "/by " + by;
+        return "/by " + byDateTime.toString();
     }
     
     @Override
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,34 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Event extends Task {
     protected String start;
     protected String end;
-    public Event(String taskName, String timeframe) {
+
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HHmm MMM dd yyyy");
+    public Event(String taskName, String timeframe) throws Meowception {
         super(taskName);
         this.start= timeframe.substring(timeframe.indexOf("/from") + 6, timeframe.indexOf("/to"));
         this.end = timeframe.substring(timeframe.indexOf("/to") + 4);
+        try {
+            startDateTime = LocalDateTime.parse(start, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+            endDateTime = LocalDateTime.parse(end, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        } catch (java.time.format.DateTimeParseException e) {
+            throw new Meowception("300");
+        }
     }
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + start + " to: "+ end + ")";
+        
+        String startFormated = startDateTime.format(FORMATTER);
+        String endFormated = endDateTime.format(FORMATTER);
+        return "[E]" + super.toString() + " (at: " + startFormated + " to: "+ endFormated + ")";
     }
 
     @Override
     public String getExtra() {
-        return "/from " + start + " /to " + end;
+        return "/from " + startDateTime.toString() + " /to " + endDateTime.toString();
     }
 
     @Override
