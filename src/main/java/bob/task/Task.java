57c15da25c7207b1d60ee6020a2539a42b046f15
Chen Zi Yi <ziyichen2003@gmail.com>
2024-08-29 01:42:36 +0800

    Organise into packages bob and bob.task

--- src/main/java/bob/task/Task.java
+++ src/main/java/bob/task/Task.java
@@ -1,3 +1,5 @@
+package bob.task;
+
 import java.time.LocalDate;
 
 public abstract class Task {
@@ -5,27 +7,23 @@ public abstract class Task {
     protected boolean isDone;
 
     abstract String getSymbol();
-    abstract String getTaskLine();
-    abstract boolean isRelevant(LocalDate date);
+    public abstract String getTaskLine();
+    public abstract boolean isRelevant(LocalDate date);
 
     public Task(String description, boolean isDone) {
         this.description = description;
         this.isDone = isDone;
     }
 
-//    public Task(String description) {
-//        this(description, false);
-//    }
-
     String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    void markAsDone() {
+    public void markAsDone() {
         this.isDone = true;
     }
 
-    void markAsUndone() {
+    public void markAsUndone() {
         this.isDone = false;
     }
 
