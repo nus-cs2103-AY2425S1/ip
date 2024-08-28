e21ccdf0a333b18f8821ca506cb1292990ec2881
sandyk0105 <sandykristianwaluyo3@gmail.com>
2024-08-28 12:30:27 +0800

    Level 0 : Bot Rename
    I renamed my bot to "Rei"

---
+++ src/main/java/Rei.java
@@ -0,0 +1,14 @@
+public class Rei {
+    public static void main(String[] args) {
+        String logo = "  ____  _____ ___ \n"
+                + " |  _ \\| ____|_ _|\n"
+                + " | |_) |  _|  | | \n"
+                + " |  _ <| |___ | | \n"
+                + " |_| \\_\\_____|___|\n";
+
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("----------------------");
+        System.out.println("Annyeong. Hope to see you soon.");
+    }
+}
