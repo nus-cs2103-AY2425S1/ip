9ac39edeff0108909587f35ac9b19eae61beb458
Runus01 <rodriguesj.aaron@gmail.com>
2024-08-29 01:30:19 +0800

    Added a method that prints a horizontal line and a method to say goodbye

--- src/main/java/Elsa.java
+++ src/main/java/Elsa.java
@@ -1,6 +1,22 @@
 public class Elsa {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Elsa");
-        System.out.println("What can I do for you?");
+        System.out.println("Hi! I'm Elsa");
+        System.out.println("It's nice to meet you.\nHow can I help you?");
+        addLine();
+        goodbye();
+    }
+
+    /**
+     * This method adds a horizontal separator line consisting of underscores.
+     */
+    public static void addLine() {
+        System.out.println("______________________________________");
+    }
+
+    /**
+     * This method ends the conversation and says bye to the user.
+     */
+    public static void goodbye() {
+        System.out.println("Bye! It was so nice chatting with you.\nSee you again soon!");
     }
 }
