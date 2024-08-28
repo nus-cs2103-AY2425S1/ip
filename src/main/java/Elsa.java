4ee61754d02836a27a168e84e27531ee407e5625
Runus01 <rodriguesj.aaron@gmail.com>
2024-08-29 02:34:53 +0800

    Added a method to process userInput

--- src/main/java/Elsa.java
+++ src/main/java/Elsa.java
@@ -1,22 +1,44 @@
+import java.util.Scanner;
+
 public class Elsa {
     public static void main(String[] args) {
         System.out.println("Hi! I'm Elsa");
         System.out.println("It's nice to meet you.\nHow can I help you?");
         addLine();
-        goodbye();
+        processUserInput();
     }
 
     /**
      * This method adds a horizontal separator line consisting of underscores.
      */
-    public static void addLine() {
+    private static void addLine() {
         System.out.println("______________________________________");
     }
 
     /**
      * This method ends the conversation and says bye to the user.
      */
-    public static void goodbye() {
+    private static void goodbye() {
         System.out.println("Bye! It was so nice chatting with you.\nSee you again soon!");
+    }
+
+    /**
+     * This method creates a new scanner object to process user input.
+     */
+    private static void processUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        if (userInput.toLowerCase().startsWith("bye")) {
+            addLine();
+            goodbye();
+            addLine();
+            scanner.close();
+        } else {
+            addLine();
+            System.out.println(userInput);
+            addLine();
+            processUserInput();
+        }
     }
 }
