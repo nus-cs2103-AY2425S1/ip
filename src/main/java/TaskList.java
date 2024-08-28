29a5bb529b772e15f7e507f3020d71eabd05d124
vinceg4 <vincegoh04@gmail.com>
2024-08-28 20:55:41 +0800

    Add TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class TaskList {
+import java.util.ArrayList;
+
+public class TaskList {
+    ArrayList<Task> listOfTasks;
+
+    public TaskList() {
+        this.listOfTasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> taskList) {
+        this.listOfTasks = taskList;
+    }
+
+    public int getNumberOfTasks() {
+        return this.listOfTasks.size();
+    }
+
+    public Task getTask(int x) {
+        return this.listOfTasks.get(x);
+    }
+
+    public ArrayList<Task> getListOfTasks() {
+        return this.listOfTasks;
+    }
+
+    public void addTask(Task t) {
+        this.listOfTasks.add(t);
+    }
+
+    public void mark(int x) {
+        Task t = this.listOfTasks.get(x - 1);
+        t.indComplete();
+    }
+
+    public void unmark(int x) {
+        Task t = this.listOfTasks.get(x - 1);
+        t.indIncomplete();
+    }
+
+    public void deleteTask(int x) {
+        this.listOfTasks.remove(x - 1);
+    }
 }
