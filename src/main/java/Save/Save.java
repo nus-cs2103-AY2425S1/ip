1d5b788180078966293f17cedb422ab66c2170eb
qk <chiangqinkang@gmail.com>
2024-08-28 10:54:18 +0800

    Refactor for coding standard

--- src/main/java/Save/Save.java
+++ src/main/java/Save/Save.java
@@ -1,5 +1,6 @@
 package Save;
 import java.io.*;
+import Task.Task;
 
 public class Save {
     private File saveFile;
@@ -11,27 +12,5 @@ public class Save {
 
     public String getPath() {
         return this.savePath;
-    }
-
-    public void writeToSave(String s) {
-        try {
-            FileWriter writer = new FileWriter(this.savePath);
-            writer.write(s);
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred while writing to the save file");
-        }
-    }
-
-    public void loadTasksFromSave(){
-        try (BufferedReader reader = new BufferedReader(new FileReader(this.saveFile))){
-            String line;
-            while ((line = reader.readLine()) != null) {
-                ;
-            }
-
-        } catch (IOException | IllegalArgumentException e) {
-            System.out.println("Error loading from save file");
-        }
     }
 }
