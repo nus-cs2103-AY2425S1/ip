d4647a6faf211a2c6c11854fa37e6ce6440b755a
potaotototo <jingjingwang2004@outlook.com>
2024-08-28 17:17:09 +0800

    Completed Level 1 – echo

--- src/main/java/Noosy.java
+++ src/main/java/Noosy.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Noosy {
     public static void main(String[] args) {
         String greeting = "Heyo! This is Noosy! \n" +
                 "What do ya need from me?";
+        String goodbye = "Alright, see ya!";
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println(greeting);
+
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput); // this echos anything but "bye"
+            userInput = scanner.nextLine(); // continue scanning for input
+        }
+
+        // after "bye"
+        System.out.println(goodbye);
+        scanner.close();
     }
 }
