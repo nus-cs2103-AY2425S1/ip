42b0c496b10849e01283f99ed11de7b38a5cac70
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-28 12:44:40 +0800

    Add InvalidCommandException

---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommandException extends InvalidInputException {
+    public InvalidCommandException() {
+        super("I don't know what that means!!!\nEnter \"help\" for a list of what I do know! rawr");
+    }
+}
