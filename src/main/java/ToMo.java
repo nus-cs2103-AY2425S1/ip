fcf94dac9c9b3af19693e0e9c5e78b05326131e9
kien <hoangkienvu11@gmail.com>
2024-08-29 02:40:22 +0800

    Add mark and unmark features

--- src/main/java/ToMo.java
+++ src/main/java/ToMo.java
@@ -5,14 +5,25 @@ import java.util.ArrayList;
 public class ToMo {
     public static class Task {
         private String description;
-    
+        private boolean done;
+
         Task(String description) {
             this.description = description;
+            this.done = false;
         }
-    
+        
+        public void mark() {
+            this.done = true;
+        }
+
+        public void unmark() {
+            this.done = false;
+        }
+
         @Override
         public String toString() {
-            return description;
+            String pref = done ? "[X] " : "[ ] ";
+            return pref + description;
         }
     }
     
@@ -27,12 +38,32 @@ public class ToMo {
             if (cmd.equals("bye")) {
                 break;
             } else if (cmd.equals("list")) {
+                System.out.println("Your tasks list as follow:");
                 for (int i = 0; i < tasks.size(); ++i) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
             } else {
-                tasks.add(new Task(cmd));
-                System.out.println("added: " + cmd);
+                String[] words = cmd.split(" ");
+                if (words[0].equals("mark")) {
+                    int i = Integer.parseInt(words[1]) - 1;
+                    Task newTask = tasks.get(i);
+                    newTask.mark();
+                    tasks.set(i, newTask);
+                    System.out.println("Your task is marked");
+                    System.out.println(newTask);
+                } else if (words[0].equals("unmark")) {
+                    int i = Integer.parseInt(words[1]) - 1;
+                    Task newTask = tasks.get(i);
+                    newTask.unmark();
+                    tasks.set(i, newTask);
+                    System.out.println("Your task is unmarked");
+                    System.out.println(newTask);
+                } else {
+                    Task newTask = new Task(cmd);
+                    tasks.add(newTask);
+                    System.out.println("Your task is added");
+                    System.out.println(newTask);
+                }
             }
         }
         sc.close();
