18691774b833d017a3d561081974212100994658
calebchongsj <calebchongsj@gmail.com>
2024-08-28 19:56:21 +0800

    Level-9 increment. Add the search functionality for user keyword

--- src/main/java/myapp/HelperBuddy/TaskList.java
+++ src/main/java/myapp/HelperBuddy/TaskList.java
@@ -1,6 +1,8 @@
 package myapp.helperbuddy;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskList {
     private final ArrayList<Task> tasks;
@@ -15,7 +17,6 @@ public class TaskList {
 
     /**
      * Adds a Task to the list.
-     *
      * @param task The Task to be added to the list.
      */
     public void addTask(Task task) {
@@ -26,7 +27,6 @@ public class TaskList {
      * Retrieves a Task from the list at the specified index.
      * If the index is out of bounds (i.e., less than 0 or greater than or equal to the size of the list),
      * this method returns null
-     *
      * @param index The index of the Task to retrieve.
      * @return The Task at the specified index, or null if the index is out of bounds.
      */
@@ -41,7 +41,6 @@ public class TaskList {
      * Deletes and returns the Task from the list at the specified index.
      * If the index is out of bounds (i.e., less than 0 or greater than or equal to the size of the list),
      * this method may throw an IndexOutOfBoundsException
-     *
      * @param index The index of the Task to delete.
      * @return The Task that was removed from the list.
      * @throws IndexOutOfBoundsException If the index is out of bounds.
@@ -52,7 +51,6 @@ public class TaskList {
 
     /**
      * Returns the number of Task objects in the list.
-     *
      * @return The number of tasks in the list.
      */
     public int size() {
@@ -61,10 +59,20 @@ public class TaskList {
 
     /**
      * Returns the list of Task objects.
-     *
      * @return The ArrayList of tasks.
      */
     public ArrayList<Task> getTasks() {
         return tasks;
+    }
+
+    /**
+     * Searches for tasks that contain the specified keyword in their descriptions.
+     * @param keyword The keyword to search for.
+     * @return A list of tasks containing the keyword.
+     */
+    public List<Task> searchTasks(String keyword) {
+        return tasks.stream()
+                .filter(task -> task.getDescription().toLowerCase().contains(keyword.toLowerCase()))
+                .collect(Collectors.toList());
     }
 }
