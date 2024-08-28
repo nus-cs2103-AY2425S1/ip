6f3b439a44eb188c06b4d89ccbeca59ac488332f
yockcheng <e1121069@u.nus.edu>
2024-08-28 15:21:46 +0800

    Added saveData method

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -10,4 +10,10 @@ public class Deadline extends Task {
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.by + ")";
     }
+
+    @Override
+    public String toData() {
+        String status = this.isDone ? "1" : "0";
+        return String.format("DEADLINE | %s | %s | %s\n", status, this.description, this.by);
+    }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,10 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
     }
+
+    @Override
+    public String toData() {
+        String status = this.isDone ? "1" : "0";
+        return String.format("EVENT | %s | %s | %s | %s\n", status, this.description, this.from, this.to);
+    }
 }
