0374082741f4e65b07dca81952a5bf9f7112211a
jiayr99 <e1249294@u.nus.edu>
2024-08-28 21:13:30 +0800

    Update Event class to store the correct date and time format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,23 +1,44 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+/**
+ * Event class represents a task that occurs during a specific period of time
+ */
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
+    /**
+     * Constructs an event task with the given description and period of time
+     *
+     * @param description The description of the event
+     * @param from The data and time the event starts
+     * @param to The date and time the event ends
+     * @throws SageException if the description is empty or if the time format is inavlid
+     */
     public Event(String description, String from, String to) throws SageException {
         super(description);
-        this.from = from;
-        this.to = to;
-        if (description.isEmpty() || this.from.isEmpty() || this.to.isEmpty()) {
+        try {
+            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+            this.from = LocalDateTime.parse(from, inputFormatter);
+            this.to = LocalDateTime.parse(to, inputFormatter);
+        } catch (DateTimeParseException e) {
+            throw new SageException("Invalid date format! Please use the format 'yyyy-mm-dd HHmm'.");
+        }
+        if (description.isEmpty()) {
             throw new SageException("What time is your event?? :o");
         }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DateTimeFormatter.ofPattern("dd MMM yyyy h:mm a")) + " to: " + to.format(DateTimeFormatter.ofPattern("dd MMM yyyy h:mm a")) + ")";
     }
 
     @Override
     public String toFileFormat() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+        DateTimeFormatter fileFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from.format(fileFormatter) + " | " + to.format(fileFormatter);
     }
 }
