6e5e7e2fe5cda27515261a831d263fa9ae6178e9
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-28 13:39:09 +0800

    Add date time system to Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    private LocalDateTime by;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm");
+    private static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
 
     public Deadline(String description, boolean isDone, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, inputFormatter);
         if (isDone) markAsDone();
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(formatter) + ")";
     }
 
     @Override
     public String toFileFormat() {
-        return "D | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + by;
+        return "D | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + by.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 }
