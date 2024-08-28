d0a09d4fcd6db43aa19ea61738083fdc49b0a14d
qk <chiangqinkang@gmail.com>
2024-08-28 10:44:39 +0800

    Add documentation

--- src/main/java/Save/Save.java
+++ src/main/java/Save/Save.java
@@ -9,10 +9,21 @@ public class Save {
         this.saveFile = new File("C:\\Users\\chian\\Desktop\\ip\\saves\\save.txt");
     }
 
+    /**
+     * Returns the path of the save file
+     * @return     the path of the save file as a string
+     */
     public String getPath() {
         return this.savePath;
     }
 
+    /**
+     * Writes the supplied string to the save file
+     * This method is to be used when writing to the
+     * save file, and throws an exception if
+     * @param s the string to be written to the save file
+     * @throws IOException if error occurs while writing to file
+     */
     public void writeToSave(String s) {
         try {
             FileWriter writer = new FileWriter(this.savePath);
@@ -23,7 +34,14 @@ public class Save {
         }
     }
 
-    public void loadTasksFromSave(){
+    /**
+     * Writes the supplied string to the save file
+     * This method is to be used when writing to the
+     * save file, and throws an exception
+     * @throws IOException if error occurs while reading from file
+     * @throws IllegalArgumentException if invalid file is supplied
+     */
+    public void loadTasksFromSave() {
         try (BufferedReader reader = new BufferedReader(new FileReader(this.saveFile))){
             String line;
             while ((line = reader.readLine()) != null) {
