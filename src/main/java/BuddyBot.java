4f1df6ce31a804e5c9bf21e6f3d8981a7ea8d412
Jalenleeruixian <e1122262@u.nus.edu>
2024-08-28 21:22:47 +0800

    test

--- src/main/java/BuddyBot.java
+++ src/main/java/BuddyBot.java
@@ -10,7 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class BuddyBot {
-
+    //second test
     static FileStorage store = new FileStorage("BuddyBot.txt");
     public static void main(String[] args) {
         //Scanner object
