72d4757fa2f6e6cd92a046880a9d5ec9cd363636
giahien0505 <giahien0505@gmail.com>
2024-08-28 16:36:58 +0800

    Add storing tasks feature

--- src/main/java/Hien.java
+++ src/main/java/Hien.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Hien {
+    private static final String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,18 +15,40 @@ public class Hien {
         while (true) {
             String input = scanner.nextLine().trim();
 
+            System.out.println("____________________________________________________________");
+
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                listTasks();
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
-                System.out.println("____________________________________________________________");
+                addTask(input);
             }
+
+            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
+    }
+
+    private static void addTask(String task) {
+        if (taskCount < 100) {
+            tasks[taskCount++] = task;
+            System.out.println(" added: " + task);
+        } else {
+            System.out.println(" Task list is full. Cannot add more tasks.");
+        }
+    }
+
+    private static void listTasks() {
+        if (taskCount == 0) {
+            System.out.println(" The task list is empty.");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+        }
     }
 }
