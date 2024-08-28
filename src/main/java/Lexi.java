0b6b52bbe4d258eecd57e3b4aa6aeb097886a2a7
suhayl13 <suhayl.pervez@gmail.com>
2024-08-28 10:38:20 +0800

    Add support for save

--- src/main/java/Lexi.java
+++ src/main/java/Lexi.java
@@ -1,40 +1,19 @@
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
-
-enum Command {
-    TODO("Adds a todo task."),
-    DEADLINE("Adds a task with a deadline."),
-    EVENT("Adds an event with a start and end time."),
-    MARK("Marks a task as done."),
-    UNMARK("Unmarks a task."),
-    DELETE("Deletes a task."),
-    LIST("Lists all tasks."),
-    BYE("Exits the application.");
-
-    private final String description;
-
-    Command(String description) {
-        this.description = description;
-    }
-    @Override
-    public String toString() {
-        return this.name() + ": " + this.description;
-    }
-
-    public static void printCommands() {
-        for(Command command: Command.values()) {
-            System.out.println(command);
-        }
-    }
-}
+import java.io.File;
+import java.io.FileWriter;
 
 public class Lexi {
-    private static ArrayList<Task> tasks = new ArrayList<>();
-    private static String LINE_BREAK = "____________________________________________________________";
-
-    public static void main(String[] args) {
+    private final static ArrayList<Task> tasks = new ArrayList<>();
+    private final static String LINE_BREAK = "____________________________________________________________";
+    private final static File directory = new File("./data");
+    private static File data = null;
+    public static void main(String[] args) throws IOException {
         Scanner userInput = new Scanner(System.in);
         greet();
+        loadTasks();
         String response = userInput.nextLine();
         while (!response.toUpperCase().equals(Command.BYE.name())) {
             try {
@@ -74,6 +53,38 @@ public class Lexi {
             bye();
         }
 
+    private static void loadTasks() throws IOException {
+        if (!directory.exists()) {
+            directory.mkdirs(); // Create the directory if it doesn't exist
+        }
+        File file = new File(directory, "data.txt");
+        file.createNewFile();
+        data = file;
+        Scanner contents = new Scanner(data);
+        while (contents.hasNextLine()) {
+            String[] parts = contents.nextLine().split(" !- ");
+            if (parts[0].equals("T")) {
+                Todo task = new Todo(parts[2]);
+                if (parts[1].equals("1")) {
+                    task.doTask();
+                }
+                tasks.add(task);
+            } else if (parts[0].equals("D")) {
+                Deadline task = new Deadline(parts[2], parts[3]);
+                if (parts[1].equals("1")) {
+                    task.doTask();
+                }
+                tasks.add(task);
+            } else if (parts[0].equals("E")) {
+                Event task = new Event(parts[2], parts[3], parts[4]);
+                if (parts[1].equals("1")) {
+                    task.doTask();
+                }
+                tasks.add(task);
+            }
+        }
+    }
+
     private static void handleDelete(String[] parts) throws LexiException {
         if(parts.length != 2) {
             throw new LexiException("Please key in the command in this format " +
@@ -81,14 +92,16 @@ public class Lexi {
         }
         try {
             int taskNumber = Integer.parseInt(parts[1]);
-            if(taskNumber <= 0 || taskNumber > tasks.size()) throw new LexiException("You can't delete a task that doesn't exist");
+            if(taskNumber <= 0 || taskNumber > tasks.size()) {
+                throw new LexiException("You can't delete a task that doesn't exist");
+            }
             Task removedTask = tasks.remove(taskNumber-1);
+            handleChangeInData(tasks);
             System.out.println(LINE_BREAK);
             System.out.println(" Noted. I've removed this task:");
             System.out.println("   " + removedTask);
             System.out.printf("Now you have %d tasks in the list.%n", tasks.size());
             System.out.println(LINE_BREAK);
-
         } catch(NumberFormatException e) {
             throw new LexiException("Please enter a valid task number as follows:\n" +
                     "\"delete <task number>\"\n");
@@ -118,18 +131,19 @@ public class Lexi {
         addTask(new Event(taskName, from, to));
     }
 
-    private static void handleDeadline(String response) throws LexiException {
+    private static void handleDeadline(String response) throws LexiException, IOException {
         String[] parts = response.split(" /by ");
-        if(parts[0].equals(response)) {
+        if (parts[0].equals(response) || parts.length != 2) {
             throw new LexiException("Please key in the command in this format\n"
             + "\"deadline <task> /by <deadline>\"\n");
         }
         String taskName = parts[0].substring(9);
+        System.out.println(parts[1]);
         String by = parts[1];
         addTask(new Deadline(taskName, by));
     }
 
-    private static void handleTodo(String response) throws LexiException{
+    private static void handleTodo(String response) throws LexiException, IOException{
         if(response.length() < 6) {
             throw new LexiException("Sorry! The description of a todo cannot be empty\n" +
                     "Please write in this format \"todo <description>\"\n");
@@ -156,16 +170,19 @@ public class Lexi {
         }
         Task taskToBeMarked = tasks.get(taskNumber);
         if(parts[0].equals("unmark")) {
-            unmarkTask(taskToBeMarked);
+            unmarkTask(taskToBeMarked, taskNumber);
         } else {
-            markTask(taskToBeMarked);
+            markTask(taskToBeMarked, taskNumber);
         }
     }
-    private static void unmarkTask(Task taskToBeMarked) throws LexiException {
+    private static void unmarkTask(Task taskToBeMarked, int taskNumber) throws LexiException {
             if(!taskToBeMarked.getIsDone()) {
                 throw new LexiException("This task has already been unmarked!\n");
             }
             taskToBeMarked.undoTask();
+            tasks.remove(taskNumber);
+            tasks.add(taskNumber, taskToBeMarked);
+            handleChangeInData(tasks);
             System.out.println(LINE_BREAK);
             System.out.println(" OK, I've marked this task as not done yet:");
             System.out.println("   " + taskToBeMarked);
@@ -173,11 +190,14 @@ public class Lexi {
 
     }
 
-    public static void markTask(Task taskToBeMarked) throws LexiException {
+    public static void markTask(Task taskToBeMarked, int taskNumber) throws LexiException {
         if(taskToBeMarked.getIsDone()) {
             throw new LexiException("This task has already been marked!\n");
         }
         taskToBeMarked.doTask();
+        tasks.remove(taskNumber);
+        tasks.add(taskNumber, taskToBeMarked);
+        handleChangeInData(tasks);
         System.out.println(LINE_BREAK);
         System.out.println(" Nice! I've marked this task as done:");
         System.out.println("   " + taskToBeMarked);
@@ -205,8 +225,36 @@ public class Lexi {
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(LINE_BREAK + "\n");
     }
-    public static void addTask(Task task) {
+    public static void handleChangeInData(ArrayList<Task> tasks) throws LexiException {
+        try {
+            FileWriter fw = new FileWriter(data);
+            for (Task task: tasks) {
+                char taskType = task.toString().split(" ")[0].charAt(1);
+                if (taskType == 'T') {
+                    fw.write(String.format("%c !- %d !- %s", taskType,
+                            task.getIsDone() ? 1 : 0, task.getTaskName()) + System.lineSeparator());
+                } else if (taskType == 'D') {
+                    Deadline obj = (Deadline) task;
+                    fw.write(String.format("%c !- %d !- %s !- %s", taskType,
+                            obj.getIsDone() ? 1 : 0, obj.getTaskName(), obj.getBy()) + System.lineSeparator());
+                } else if (taskType == 'E') {
+                    Event obj = (Event) task;
+                    fw.write(String.format("%c !- %d !- %s !- %s !- %s", taskType,
+                            obj.getIsDone() ? 1 : 0, obj.getTaskName(),
+                            obj.getFrom(), obj.getTo()) + System.lineSeparator());
+                }
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw new LexiException("Something went wrong with writing to data\n"
+                    + "Please contact your system administrator!\n"
+                    + "Error Message:\n"
+                    + e.getMessage());
+        }
+    }
+    public static void addTask(Task task) throws LexiException {
         tasks.add(task);
+        handleChangeInData(tasks);
         int taskSize = tasks.size();
         System.out.println(LINE_BREAK);
         System.out.println(" Got it. I've added this task:");
