65ce8c2e79b9e014ebe71a203647aa7a5eb23298
kien <hoangkienvu11@gmail.com>
2024-08-28 14:24:35 +0800

    Rename chatbot and add greeings

---
+++ src/main/java/ToMo.java
@@ -0,0 +1,7 @@
+public class ToMo {
+    public static void main(String[] args) {
+        System.out.println("What's up, it's ToMo here!");
+        System.out.println("How can I help you?");
+        System.out.println("Bye, see you later!");
+    }
+}
