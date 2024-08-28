7ffedd2c91f13b8c3aed441272e15e495b4d69fc
RuijianLu <e1297772@u.nus.edu>
2024-08-28 19:31:34 +0800

    Enable creating task history file

--- src/main/java/LuToDo.java
+++ src/main/java/LuToDo.java
@@ -1,7 +1,18 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 public class LuToDo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        File taskListFile = new File("docs/taskListFile.txt");
+        if (!taskListFile.exists()) {
+            try {
+                taskListFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
         Utility.greeting();
         while (true) {
