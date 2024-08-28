26a4f78f62047800a10c9b4bbab29f23a00da7a2
T1duS <udit121sanghi@gmail.com>
2024-08-28 21:48:47 +0800

    Level-2

--- src/main/java/Opus.java
+++ src/main/java/Opus.java
@@ -7,18 +7,23 @@ public class Opus {
         System.out.println(" Hello! I'm Opus");
         System.out.println(" What can I do for you?");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while(true) {
             String s = scanner.nextLine();
             if (s.equals("bye")) {
                 break;
+            } else if (s.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount++] = s;
+                System.out.println("added: " + s);
             }
-            System.out.println(s);
         }
-
         System.out.println(" Bye. Hope to see you again soon!");
     }
 }
 
-// how to run this file?
-// javac Opus.java
-// java Opus
