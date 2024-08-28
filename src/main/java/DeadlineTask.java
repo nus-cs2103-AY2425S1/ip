fab7c727b5b9647b9f8ef6b59ba4fafe0acaf1e1
RuijianLu <e1297772@u.nus.edu>
2024-08-29 01:49:58 +0800

    Enable understanding ISO format date and time

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,10 +1,16 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DeadlineTask extends Task{
     protected String ddl;
+    protected LocalDate deadLine;
 
     public DeadlineTask(String wholeInfo) {
         super(splitInfo(wholeInfo)[0]);
         String[] info = splitInfo(wholeInfo);
         ddl = info[1].split("\\s+", 2)[1];
+        deadLine = manageDate(ddl);
     }
 
     public static String[] splitInfo(String wholeInfo) {
@@ -13,6 +19,7 @@ public class DeadlineTask extends Task{
 
     @Override
     public String toString() {
-        return "[D]" + "[" + getStatusIcon() + "] " + super.description + "/by: " + ddl;
+        return "[D]" + "[" + getStatusIcon() + "] " + super.description + " /by: " +
+                deadLine.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,18 @@
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 public class Task {
 
     protected String description;
     protected boolean isDone;
 
     public Task(String description) {
-        this.description = description;
+        this.description = description.trim();
         this.isDone = false;
     }
 
@@ -18,6 +26,14 @@ public class Task {
 
     public String getStatusIcon() {
         return (isDone ? "X" : "_"); // mark done task with X
+    }
+
+    public static LocalDate manageDate(String dateMessage) {
+        return LocalDate.parse(dateMessage,DateTimeFormatter.ISO_DATE);
+    }
+
+    public static LocalTime manageTime(String timeMessage) {
+        return LocalTime.parse(timeMessage,DateTimeFormatter.ISO_TIME);
     }
 
     @Override
