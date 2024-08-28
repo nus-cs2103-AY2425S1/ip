832768092b97e6394f9fcdc7d2e34ef6b31afb7e
jktang14 <tangboy14@gmail.com>
2024-08-29 00:41:28 +0800

    Add exception handling

--- src/main/java/Azir/Parser.java
+++ src/main/java/Azir/Parser.java
@@ -3,30 +3,86 @@ package Azir;
 import Azir.AzirException;
 
 public class Parser {
-    public static String[] parse(String command) throws AzirException {
+    public static String[] parse(String command, int size) throws AzirException {
         if (command.equals("list")) {
             return new String[] {"list"};
         } else if (command.equals("bye")) {
             return new String[] {"bye"};
-        }
-        else if (command.startsWith("mark")) {
-            return command.split(" ");
+        } else if (command.startsWith("mark")) {
+            String[] result = command.split(" ");
+            if (result.length == 1) {
+                throw new AzirException("Input the task number you would like to mark.");
+            }
+            if (Integer.valueOf(result[1]) < 1 || (Integer.valueOf(result[1]) > size)) {
+                throw new AzirException("Invalid value");
+            }
+            return result;
         } else if (command.startsWith("unmark")) {
-            return command.split(" ");
+            String[] result = command.split(" ");
+            if (result.length == 1) {
+                throw new AzirException("Input the task number you would like to unmark.");
+            }
+            if (Integer.valueOf(result[1]) < 1 || (Integer.valueOf(result[1]) > size)) {
+                throw new AzirException("Invalid value");
+            }
+            return result;
         } else if (command.startsWith("delete")) {
-            return command.split(" ");
-        }
-        else if (command.startsWith("todo")) {
+            String[] result = command.split(" ");
+            if (result.length == 1) {
+                throw new AzirException("Input the task number you would like to delete.");
+            }
+            if (Integer.valueOf(result[1]) < 1 || (Integer.valueOf(result[1]) > size)) {
+                throw new AzirException("Invalid value");
+            }
+            return result;
+        } else if (command.startsWith("todo")) {
+            int todoIndex = command.indexOf("todo");
+            if (command.substring(5).trim().isEmpty()) {
+                throw new AzirException("todo cannot have an empty description. " +
+                        "Format: todo [description]");
+            }
             String description = command.substring(5);
             return new String[] {"todo", description};
         } else if (command.startsWith("deadline")) {
             int byIndex = command.indexOf("/by");
+            if (byIndex == -1) {
+                throw new AzirException("deadline needs a /by date. " +
+                        "Format: deadline [description] /by [date]");
+            }
+            if (command.substring(8, byIndex).trim().isEmpty()) {
+                throw new AzirException("deadline needs a description. " +
+                        "Format: deadline [description] /by [date]");
+            }
+            if (command.trim().endsWith("/by")) {
+                throw new AzirException("You need a deadline day. " +
+                        "Format: deadline [description] /by [date]");
+            }
             String description = command.substring(9, byIndex - 1);
             String day = command.substring(byIndex + 4);
             return new String[] {"deadline", description, day};
         } else if (command.startsWith("event")) {
             int fromIndex = command.indexOf("/from");
             int toIndex = command.indexOf("/to");
+            if (fromIndex == -1) {
+                throw new AzirException("event needs a /from. " +
+                        "Format: event [description] /from [date] /to [date]");
+            }
+            if (toIndex == -1) {
+                throw new AzirException("event needs a /to. " +
+                        "Format: event [description] /from [date] /to [date]");
+            }
+            if (command.substring(fromIndex + 5, toIndex).trim().isEmpty()) {
+                throw new AzirException("event needs a from date. " +
+                        "Format: event [description] /from [date] /to [date]");
+            }
+            if (command.substring(5, fromIndex).trim().isEmpty()) {
+                throw new AzirException("event needs a description. " +
+                        "Format: event [description] /from [date] /to [date]");
+            }
+            if (command.trim().endsWith("/to")) {
+                throw new AzirException("You need an ending date. " +
+                        "Format: event [description] /from [date] /to [date]");
+            }
             String description = command.substring(6, fromIndex - 1);
             String startDay = command.substring(fromIndex + 6, toIndex - 1);
             String endDay = command.substring(toIndex + 4);
