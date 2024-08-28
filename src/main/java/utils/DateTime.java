a0d812f5ff0d251cff14d446a76cc269e3bbee14
Raghava <chinnu4148@gmail.com>
2024-08-28 23:41:57 +0800

    feat: Add date time formatter class

---
+++ src/main/java/utils/DateTime.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTime {
+    public static final DateTimeFormatter DateTimePrintOutputFormatter = DateTimeFormatter.ofPattern("MMM d yyyy HH:mm");
+    public static final DateTimeFormatter DateTimeFileOutputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+
+    public static LocalDateTime formatDate(String dateTime) {
+        String[] dateTimes = dateTime.split(" ");
+        LocalDate date = LocalDate.parse(dateTimes[0]);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
+        LocalTime time = LocalTime.parse(dateTimes[1], timeFormatter);
+        return LocalDateTime.of(date, time);
+    }
+}
