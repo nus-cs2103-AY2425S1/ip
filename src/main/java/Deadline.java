5ed3d25430e3884d1638692a458711c03709845c
jason-chuah <chuahchonghee2019@gmail.com>
2024-08-28 17:29:31 +0800

    Add DateTime functionality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,15 @@
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = DTFormatter.format(by);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException("please input DateTime in format yyyy-MM-dd HHmm", e.getParsedString(), e.getErrorIndex());
+        }
     }
 
     @Override
@@ -11,8 +17,9 @@ public class Deadline extends Task{
         return "D/" + this.getStatusIcon() + "/" + description + "/" + by;
     }
 
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by + ")";
     }
 }
