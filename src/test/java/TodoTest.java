50e59fd4107eb012ba32527f7015df499e3de287
dominic2412 <521hxt@gmail.com>
2024-08-28 18:48:06 +0800

    A-JUnit

---
+++ src/test/java/TodoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import skywalker.task.Todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+
+    @Test
+    public void testTodoCreation() {
+        Todo todo = new Todo("read book");
+        assertEquals("[T][ ] read book", todo.toString());
+    }
+
+    @Test
+    public void testMarkDone() {
+        Todo todo = new Todo("read book");
+        todo.markDone();
+        assertEquals("[T][X] read book", todo.toString());
+    }
+}
