e4ee10ba0aa384cf0ed228da11f0077157509983
Christopher Tan <phertan02@gmail.com>
2024-08-28 23:29:15 +0800

    Create Ui class to manage Ui I/O

---
+++ src/main/java/Ui.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showGreeting() {
+        System.out.println(
+            "Hello! I'm Cloud\n" +
+            "What can I do for you?"
+        );
+    }
+
+    public void showLine() {
+        System.out.println(
+            "____________________________________________________________"
+        );
+    }
+
+    public void showExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void echo(String message) {
+        System.out.println(message);
+    }
+
+    public void showMarked(String taskStatus) {
+        System.out.println("Task marked as done!\n" + taskStatus);
+    }
+
+    public void showUnmarked(String taskStatus) {
+        System.out.println("Task marked as not done\n" + taskStatus);
+    }
+
+    public void showList(String list) {
+        System.out.println("Here is a list of all your tasks:\n" + list);
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public String readCommand() {
+        System.out.print(">> ");
+        return scanner.nextLine().strip();
+    }
+}
