043e2d1844e2cf11bb2a2607d0755830d9d64e8e
giahien0505 <giahien0505@gmail.com>
2024-08-28 17:42:41 +0800

    Add Todos, Events, Deadline Task. Restructure repo.

--- src/main/java/Hien/main/Hien.java
+++ src/main/java/Hien/main/Hien.java
@@ -1,32 +1,8 @@
+package Hien.main;
+
+import Hien.task.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-class Task {
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public String getStatusIcon() {
-        return (isDone ? "X" : " ");
-    }
-
-    public void markAsDone() {
-        isDone = true;
-    }
-
-    public void markAsUndone() {
-        isDone = false;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + getStatusIcon() + "] " + description;
-    }
-}
 
 public class Hien {
     private static final ArrayList<Task> tasks = new ArrayList<>();
@@ -53,8 +29,14 @@ public class Hien {
                 markTask(input, true);
             } else if (input.startsWith("unmark ")) {
                 markTask(input, false);
+            } else if (input.startsWith("todo ")) {
+                addTodo(input);
+            } else if (input.startsWith("deadline ")) {
+                addDeadline(input);
+            } else if (input.startsWith("event ")) {
+                addEvent(input);
             } else {
-                addTask(input);
+                System.out.println(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
 
             System.out.println("____________________________________________________________");
@@ -63,12 +45,39 @@ public class Hien {
         scanner.close();
     }
 
-    private static void addTask(String description) {
-        Task newTask = new Task(description);
-        tasks.add(newTask);
+    private static void addTodo(String input) {
+        String description = input.substring(5).trim();
+        Todo todo = new Todo(description);
+        tasks.add(todo);
         System.out.println(" Got it. I've added this task:");
-        System.out.println("   " + newTask);
+        System.out.println("   " + todo);
         System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    private static void addDeadline(String input) {
+        String[] parts = input.substring(9).split(" /by ");
+        if (parts.length == 2) {
+            Deadline deadline = new Deadline(parts[0].trim(), parts[1].trim());
+            tasks.add(deadline);
+            System.out.println(" Got it. I've added this task:");
+            System.out.println("   " + deadline);
+            System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        } else {
+            System.out.println(" ☹ OOPS!!! The deadline format is incorrect. Please use: deadline <description> /by <date>");
+        }
+    }
+
+    private static void addEvent(String input) {
+        String[] parts = input.substring(6).split(" /from | /to ");
+        if (parts.length == 3) {
+            Event event = new Event(parts[0].trim(), parts[1].trim(), parts[2].trim());
+            tasks.add(event);
+            System.out.println(" Got it. I've added this task:");
+            System.out.println("   " + event);
+            System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        } else {
+            System.out.println(" ☹ OOPS!!! The event format is incorrect. Please use: event <description> /from <start-date> /to <end-date>");
+        }
     }
 
     private static void listTasks() {
@@ -96,10 +105,10 @@ public class Hien {
                 }
                 System.out.println("   " + task);
             } else {
-                System.out.println(" Invalid task number. Please try again.");
+                System.out.println(" ☹ OOPS!!! The task number is invalid.");
             }
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
-            System.out.println(" Invalid command. Please use 'mark <task number>' or 'unmark <task number>'.");
+            System.out.println(" ☹ OOPS!!! The command format is incorrect. Please use 'mark <task number>' or 'unmark <task number>'.");
         }
     }
 }
