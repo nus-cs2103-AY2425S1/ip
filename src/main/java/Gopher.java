d8a25bc2afa808834b78a77e9961800468c259b8
MAOXIONGKAI <conan7153@gmail.com>
2024-08-28 21:39:33 +0800

    Fix the issue of tasks only saved when added new task

--- src/main/java/Gopher.java
+++ src/main/java/Gopher.java
@@ -83,6 +83,8 @@ public class Gopher {
         int taskIndex = taskNumber - 1;
         Task task = taskList.get(taskIndex);
         task.markAsDone();
+        TaskManager.saveTasks(taskList);
+
         System.out.println(horizontalSeparator);
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(task);
@@ -94,6 +96,8 @@ public class Gopher {
         int taskIndex = taskNumber - 1;
         Task task = taskList.get(taskIndex);
         task.markAsNotDone();
+        TaskManager.saveTasks(taskList);
+
         System.out.println(horizontalSeparator);
         System.out.println("Ok, I've marked this task as not done yet:");
         System.out.println(task);
@@ -110,6 +114,7 @@ public class Gopher {
         System.out.println(horizontalSeparator + "\n");
 
         taskList.remove(taskIndex);
+        TaskManager.saveTasks(taskList);
     }
 
     // Say goodbye to the user and exit the application
