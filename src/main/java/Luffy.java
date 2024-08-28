29eed6533ae8251fe366e351be3eb8b180c3eb1d
Solomon0519 <ng.eswen1@gmail.com>
2024-08-28 10:54:13 +0800

    Level-1 Changes

--- src/main/java/Luffy.java
+++ src/main/java/Luffy.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Luffy {
     public static void main(String[] args) {
 
+        // Variables
+        ArrayList<String> textList = new ArrayList<>();
+
+        // Interactions
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Luffy");
-        System.out.println("What can I do for you?");
+        System.out.println(" Hello! I'm Luffy");
+        System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
 
         while (true) {
 
@@ -15,12 +21,18 @@ public class Luffy {
 
             if (echoPhrase.equals("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again!");
+                System.out.println(" Bye. Hope to see you again!");
                 System.out.println("____________________________________________________________");
                 return;
-            } else {
+            } else if (echoPhrase.equals("list")) {
+                for (int i = 0; i < textList.size(); i++) {
+                    System.out.println(String.format(" %d. %s", i + 1, textList.get(i)));
+                }
                 System.out.println("____________________________________________________________");
-                System.out.println(echoPhrase);
+            } else {
+                textList.add(echoPhrase);
+                System.out.println("____________________________________________________________");
+                System.out.println(String.format(" added: %s",echoPhrase));
                 System.out.println("____________________________________________________________");
             }
         }
