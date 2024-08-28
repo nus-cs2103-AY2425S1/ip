74002655c6f7695f21f5ab5f63a577e99deb0251
Solomon0519 <ng.eswen1@gmail.com>
2024-08-28 09:53:35 +0800

    Level-1 Changes

--- src/main/java/Luffy.java
+++ src/main/java/Luffy.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Luffy {
     public static void main(String[] args) {
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Luffy");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+
+            Scanner echo = new Scanner(System.in);
+            String echoPhrase = echo.nextLine();
+
+            if (echoPhrase.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again!");
+                System.out.println("____________________________________________________________");
+                return;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(echoPhrase);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
