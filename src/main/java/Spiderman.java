bfa77287c0b20483c8bf1e7969ef84b3e54c3dee
Rachael-Chan <e0775723@u.nus.edu>
2024-08-28 14:16:44 +0800

    feat: Added deadline, event and todo

--- src/main/java/Spiderman.java
+++ src/main/java/Spiderman.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList; // import the ArrayList class
 
@@ -17,6 +18,9 @@ public class Spiderman {
             // Get users input
             String input = scan.nextLine();
             String[] splitInput = input.split("\\s+");
+            String[] splitInputByCommand = input.split("/");
+//            System.out.println("User input: " + Arrays.toString(splitInput));
+//            System.out.println("User input split by /: " + Arrays.toString(splitInputByCommand));
 
             // User types bye; program terminates
             if (input.equals("bye")) {
@@ -26,7 +30,7 @@ public class Spiderman {
             // User types list; display list
             if (input.equals("list")) {
                 for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i+1) + ". " + "[" + taskList.get(i).getStatusIcon() + "] " + taskList.get(i).getDescription());
+                    System.out.println((i+1) + ". " + taskList.get(i).toString());
                 }
                 continue;
             }
@@ -36,7 +40,7 @@ public class Spiderman {
                 int number = Integer.parseInt(splitInput[1]) - 1;
                 taskList.get(number).markAsDone();
                 System.out.println("Great! I've marked this task as done:");
-                System.out.println("[" + taskList.get(number).getStatusIcon() + "] " + taskList.get(number).getDescription());
+                System.out.println(taskList.get(number).toString());
                 continue;
             }
 
@@ -45,14 +49,29 @@ public class Spiderman {
                 int number = Integer.parseInt(splitInput[1]) - 1;
                 taskList.get(number).markAsNotDone();
                 System.out.println("OK, this task will be marked as not done yet:");
-                System.out.println("[" + taskList.get(number).getStatusIcon() + "] " + taskList.get(number).getDescription());
+                System.out.println(taskList.get(number).toString());
                 continue;
             }
 
             // Store users input into tasks
-            taskList.add(new Task(input));
+            // Check what kind of tasks to add
+            if (splitInput[0].equals("deadline")) {
+                String description = splitInputByCommand[0].replaceAll("deadline ", "");
+                String by = splitInputByCommand[1].replaceAll("by ", "");
 
-            System.out.println("added: " + input);
+                taskList.add(new Deadline(description, by));
+            } else if (splitInput[0].equals("event")) {
+                String description = splitInputByCommand[0].replaceAll("deadline ", "");
+                String from = splitInputByCommand[1].replaceAll("from ", "");
+                String to = splitInputByCommand[2].replaceAll("to ", "");
+
+                taskList.add(new Event(description, from, to));
+            } else {
+                taskList.add(new Todo(splitInput[1] + " " ));
+            }
+
+            System.out.println("Cool! I'll add this to your task list!");
+            System.out.println("You now have " + taskList.size() + " tasks in your task list.");
         }
 
         // Exit program message
