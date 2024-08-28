c71db093d2d27aeda886bc6e689942d7e424693b
Gra7ityIC3 <kwtg20@outlook.com>
2024-08-29 01:36:13 +0800

    Change format of date/time from String to LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,23 @@
-public class Deadline extends Task {
-    private final String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    private final LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by) {
         super(TaskType.DEADLINE, description);
         this.by = by;
     }
 
     @Override
     public String toFileFormat() {
-        return "%s | %s".formatted(super.toFileFormat(), by);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        return "%s | %s".formatted(super.toFileFormat(), by.format(formatter));
     }
 
     @Override
     public String toString() {
-        return "%s (by: %s)".formatted(super.toString(), by);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
+        return "%s (by: %s)".formatted(super.toString(), by.format(formatter));
     }
 }
