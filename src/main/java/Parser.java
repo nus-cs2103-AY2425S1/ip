a3194da0d8b0811f5c6152c9a740b3f280944312
Clarenceeey <clarencey100@gmail.com>
2024-08-28 17:37:12 +0800

    Add Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class Parser {
+
+    public PrefixString parseCommand(String input) {
+        return PrefixString.checkPrefixString(input);
+    }
+
+    public boolean checkValidIndex(String command, int start) {
+        String trimmed = command.substring(start);
+        try {
+            Integer.parseInt(trimmed);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public int checkIndex(String command, int start) {
+        String trimmed = command.substring(start);
+        try {
+            return Integer.parseInt(trimmed);
+        } catch (Exception ex) {
+            return Integer.MAX_VALUE;
+        }
+    }
+}
