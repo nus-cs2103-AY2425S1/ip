59866581687459f497cbbfa2a045918ac9707817
ethan-goh <ethan.goh.ye@gmail.com>
2024-08-28 15:14:39 +0800

    Add simple test cases for taskmanager and configured JUnit

---
+++ src/test/java/genesis/GenesisTest.java
@@ -0,0 +1,27 @@
+package genesis;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class GenesisTest {
+    private TaskManager taskManager;
+
+
+    @Test
+    public void testAddTask() {
+        Task todo = new Todo("Test Task", "todo Test Task");
+        taskManager = new TaskManager();
+        taskManager.addTask(todo, true);
+        assertEquals(1, taskManager.getTasks().size());
+        assertEquals("Test Task", taskManager.getTasks().get(0).description);
+    }
+
+    @Test
+    public void testMarkTask() {
+        Task todo = new Todo("Test Task", "todo Test Task");
+        taskManager = new TaskManager();
+        taskManager.addTask(todo, true);
+        taskManager.markTask(0);
+        assertTrue(taskManager.getTasks().get(0).isComplete);
+    }
+}
