f9dd2dc54194f35a89340f601c82537a16709d87
sandyk0105 <sandykristianwaluyo3@gmail.com>
2024-08-28 12:50:59 +0800

    Level 1
    Customized Rei bot

--- src/main/java/Rei.java
+++ src/main/java/Rei.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Rei {
     public static void main(String[] args) {
         String logo = "  ____  _____ ___ \n"
@@ -6,9 +8,22 @@ public class Rei {
                 + " |  _ <| |___ | | \n"
                 + " |_| \\_\\_____|___|\n";
 
-        System.out.println("Hello I'm\n" + logo);
+        System.out.println("Annyeong! I'm\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("----------------------");
+        System.out.println("-----------YOU------------");
+
+        Scanner scanner = new Scanner(System.in);
+        String prompt = scanner.nextLine();
+        while (!prompt.equals("annyeong")  && !prompt.equals("Annyeong")) {
+            System.out.println("-----------REI♥-----------");
+            System.out.println(prompt);
+            System.out.println("-----------YOU------------");
+            prompt = scanner.nextLine();
+        }
+        scanner.close();
+
+        System.out.println("-----------REI♥-----------");
         System.out.println("Annyeong. Hope to see you soon.");
+
     }
 }
