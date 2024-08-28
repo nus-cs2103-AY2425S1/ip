b0690c7438a12de4da30920285495e7bea0f7d26
xGladiate <sharonsohxuanhui@hotmail.com>
2024-08-29 01:12:52 +0800

    Add changeDateFormat to Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     protected String by;
@@ -7,8 +11,23 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+    public String changeDateFormat(String by) {
+        if (by == null || by.trim().isEmpty()) {
+            System.out.println("The deadline cannot be empty!");
+            return "Invalid date";
+        }
+
+        try {
+            LocalDate date = LocalDate.parse(by);
+            return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) {
+            System.out.println("Invalid date format: " + by);
+            return "Invalid date";
+        }
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + changeDateFormat(by) + ")";
     }
 }
