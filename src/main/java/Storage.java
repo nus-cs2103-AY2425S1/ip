258fa2703daad7bb1e29bdaa609b02a3dc4b4128
itsnotsherm <sherman.ho@u.nus.edu>
2024-08-28 12:46:09 +0800

    Add new classes for OOP

---
+++ src/main/java/Storage.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Storage {
+    private String filepath = "./data/rex.txt";
+    private String temppath = "./data/tmp.txt";
+
+    public void loadFile(TaskList list) throws IOException {
+        // Create new file and directory for filepath
+        File file = new File(filepath);
+        File dir = file.getParentFile();
+
+        createDirectory(dir);
+        createFile(file);
+
+        // Parse file data into TaskList
+        Parser.parseFile(list, file);
+    }
+
+    public void updateFile(TaskList list) throws IOException {
+        File file = new File(filepath);
+        File temp = new File(temppath);
+
+        // Create temp file to copy taskList from
+        Files.deleteIfExists(Paths.get(temppath));
+        createFile(temp);
+
+        FileWriter writer = new FileWriter(temp, true);
+
+        // Create new file with updated list
+        for (int i = 1; i <= list.size(); i++) {
+            Task task = list.getTask(i);
+            writer.write(task.formatted() + System.lineSeparator());
+        }
+
+        // Close writer
+        writer.close();
+
+        // Copy to save file
+        Files.copy(Paths.get(temppath), Paths.get(filepath), StandardCopyOption.REPLACE_EXISTING);
+        Files.delete(Paths.get(temppath));
+    }
+
+    private void createDirectory(File f) {
+        // Create new directory if it does not exist
+        if (!f.isDirectory()) {
+            f.mkdirs();
+        }
+    }
+
+    private void createFile(File f) throws IOException {
+        // Create new file if does not exist
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,112 @@
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class TaskList {
+    private Storage storage;
+    private ArrayList<Task> list;
+
+    public TaskList(Storage storage) throws IOException {
+        this.storage = storage;
+        this.list = new ArrayList<>();
+        loadList();
+    }
+
+    public ToDo addToDo(String argument) throws IOException {
+        ToDo toDo = new ToDo(argument, false);
+        list.add(toDo);
+        updateStorage();
+        return toDo;
+    }
+
+    public Deadline addDeadline(String argument) throws IOException, InvalidInputException {
+        String[] argumentTokens = Parser.parseDeadline(argument);
+        String description = argumentTokens[0];
+        LocalDateTime by = Parser.parseDateTime(argumentTokens[1]);
+
+        Deadline deadine = new Deadline(description, false, by);
+        list.add(deadine);
+        updateStorage();
+        return deadine;
+    }
+
+    public Event addEvent(String argument) throws IOException, InvalidInputException {
+        String[] argumentTokens = Parser.parseEvent(argument);
+        String description = argumentTokens[0];
+        LocalDateTime from = Parser.parseDateTime(argumentTokens[1]);
+        LocalDateTime to = Parser.parseDateTime(argumentTokens[2]);
+
+        Event event = new Event(description, false, from, to);
+        list.add(event);
+        updateStorage();
+        return event;
+    }
+
+    public void loadTask(String description, boolean isDone) {
+        list.add(new ToDo(description, isDone));
+    }
+
+    public void loadTask(String description, boolean isDone, LocalDateTime by) {
+        list.add(new Deadline(description, isDone, by));
+    }
+
+    public void loadTask(String description, boolean isDone, LocalDateTime from, LocalDateTime to) {
+        list.add(new Event(description, isDone, from, to));
+    }
+
+    public Task getTask(String argument) {
+        return getTask(Integer.parseInt(argument));
+    }
+
+    public Task getTask(int index) {
+        return list.get(index - 1);
+    }
+
+    public String getListDisplay() {
+        if (isEmpty()) return "The list is empty! rawr\n";
+
+        String output = "";
+        for (int i = 1; i <= list.size(); i++) {
+            output += i + "." + getTask(i) + "\n";
+        }
+
+        return output;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    private boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public Task markTask(String argument) throws IOException {
+        Task actionTask = getTask(argument);
+        actionTask.markDone();
+        updateStorage();
+        return actionTask;
+    }
+
+    public Task unmarkTask(String argument) throws IOException {
+        Task actionTask = getTask(argument);
+        actionTask.unmarkDone();
+        updateStorage();
+        return actionTask;
+    }
+
+    public Task deleteTask(String argument) throws IOException {
+        Task actionTask = getTask(argument);
+        list.remove(actionTask);
+        Task.removeTask();
+        updateStorage();
+        return actionTask;
+    }
+
+    private void loadList() throws IOException {
+        storage.loadFile(this);
+    }
+    private void updateStorage() throws IOException {
+        storage.updateFile(this);
+    }
+}
