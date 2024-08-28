9252c9f56fc2770585f3bcbbd99d6b89138cd272
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-28 21:20:10 +0800

    Revert "Re-implement Find"
    This reverts commit e611c472c86004070947a501dc69bac025c63eff.

--- src/main/java/hue/command/FindCommand.java
+++ src/main/java/hue/command/FindCommand.java
@@ -1,5 +1,4 @@
 package hue.command;
-import hue.Hue;
 import hue.HueException;
 import hue.UI.UI;
 import hue.storage.Storage;
@@ -9,34 +8,18 @@ import hue.task.TaskList;
 import java.io.IOException;
 import java.util.ArrayList;
 public class FindCommand extends Command {
-    private final String input;
-    /**
-     * Constructs a {@code FindCommand} with the given input string.
-     *
-     * @param input The full command string input by the user.
-     */
-    public FindCommand(String input) {
-        this.input = input;
+    private final String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
     }
 
     @Override
-    public void execute(TaskList tasks, UI ui, Storage storage) throws HueException {
-        String keyword = this.input.substring(5).trim();
-        if (keyword.isEmpty()) {
-            throw new HueException("The keyword for the find command cannot be empty!");
-        }
+    public void execute(TaskList tasks, UI ui, Storage storage) {
         ArrayList<Task> result = new ArrayList<>();
 
         for (Task task : tasks.getTasks()) {
-            String description = task.getDescription();
-            if (description.contains(keyword)) {
-                result.add(task);
-            }
-        }
-        if (result.isEmpty()) {
-            ui.showError("No tasks found that matches the keyword: " + keyword);
-        } else {
-            ui.showMatchingTasks(result, keyword);
+            String description = task.getDes
         }
     }
 }
