b3eb61cd7ecc6dd467e40764cfbaf9141cfd6936
Curtis <curtischang2510@gmail.com>
2024-08-28 16:48:07 +0800

    Add support for LocalDate & LocalTime
    Dates are taken as strings
    
    Lets,
    - Specify a date format for clients to follow and store them as LocalDate
    and LocalTime instances
    - Display date and time in different formats
    - Allow for easy editing of task time and date (feature not implemented
    yet)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,29 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate deadlineDate;
+    protected LocalTime deadlineTime;
 
     public Deadline(String description, String by) {
         super(TaskType.DEADLINE, description);
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+        String[] deadlineArr = by.split(" ");
+        deadlineDate = LocalDate.parse(deadlineArr[0], formatter);
+        if (deadlineArr.length == 2) {
+            deadlineTime = LocalTime.parse(deadlineArr[1]);
+        }
     }
 
     @Override
     public String toString() {
-       return "[D]" + super.toString() + " (by: " + by + ")" ;
+        String altFormat = deadlineDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String timeStr = deadlineTime == null ? "" : deadlineDate.toString();
+        String deadline = (altFormat + " " + timeStr).trim();
+        return "[D]" + super.toString() + " (by: " + deadline + ")" ;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,42 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
 
-    String start;
-    String end;
+    LocalDateTime endDateTime;
+    LocalDate startDate;
+    LocalTime startTime;
+    LocalDate endDate;
+    LocalTime endTime;
 
     public Event(String description, String start, String end) {
         super(TaskType.EVENT, description);
-        this.start = start;
-        this.end = end;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+        String[] startArr = start.split(" ");
+        startDate = LocalDate.parse(startArr[0], formatter);
+        if (startArr.length == 2) {
+           startTime = LocalTime.parse(startArr[1]);
+        }
+
+
+        String[] endArr = start.split(" ");
+        endDate = LocalDate.parse(endArr[0], formatter);
+        if (endArr.length == 2) {
+            endTime = LocalTime.parse(endArr[1]);
+        }
     }
 
     @Override
     public String toString() {
-        String dateStr = "from: " + start + " to: " + end;
+        String altFormatStart = startDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String altFormatEnd = endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        String startString = startTime == null ? "" : startTime.toString();
+        String endString = endTime == null ? "" : endTime.toString();
+        String start = altFormatStart + " " + startString;
+        String end = altFormatEnd + " " + endString;
+        String dateStr = ("from: " + start + " to: " + end).trim();
         return "[E]" + super.toString() + " (" + dateStr + ")";
     }
 }
