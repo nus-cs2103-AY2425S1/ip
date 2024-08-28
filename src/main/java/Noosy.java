261dab90b57c24f9f3e4591cf9ef5328469cd3e0
potaotototo <jingjingwang2004@outlook.com>
2024-08-28 16:21:52 +0800

    Completed Level 0 – added greeting

---
+++ src/main/java/Noosy.java
@@ -0,0 +1,7 @@
+public class Noosy {
+    public static void main(String[] args) {
+        String greeting = "Heyo! This is Noosy! \n" +
+                "What do ya need from me?";
+        System.out.println(greeting);
+    }
+}
