98a8d075ebb1d316856183d33fe55bb9bd4d5b52
kien <hoangkienvu11@gmail.com>
2024-08-28 14:53:50 +0800

    Add echo to skeletal

--- src/main/java/ToMo.java
+++ src/main/java/ToMo.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class ToMo {
     public static void main(String[] args) {
         System.out.println("What's up, it's ToMo here!");
-        System.out.println("How can I help you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("How can I help you?");
+            String cmd = sc.nextLine();
+            if (cmd.equals("bye")) {
+                break;
+            } else {
+                System.out.println(cmd);
+            }
+        }
+        
         System.out.println("Bye, see you later!");
     }
 }
