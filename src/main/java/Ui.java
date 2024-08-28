90cb4f41faf2dbd577317c057db921ab8246ab8b
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-28 15:02:19 +0800

    create Ui file

---
+++ src/main/java/Ui.java
@@ -0,0 +1,9 @@
+public class Ui {
+    private static final String NAME_OF_CHATBOT = "Stobberi";
+    private static final String HELLO_GREETING =
+            "Hello! I'm " + NAME_OF_CHATBOT + ".\n"
+                    + "What can I do for you?";
+    private static final String GOODBYE_GREETING = "Bye. Hope to see you again soon! :)\n";
+
+
+}
