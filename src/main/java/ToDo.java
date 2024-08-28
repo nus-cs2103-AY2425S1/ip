c08974fb7efb7171707537d5670d718d8b8058df
Zhenzhi <caizhenzhi5@gmail.com>
2024-08-29 00:36:52 +0800

    Add storage to remember various tasks

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,4 +1,9 @@
+import java.util.Objects;
+
 public class ToDo extends Task{
+    public ToDo() {
+        super("");
+    }
 
     public ToDo(String taskName) {
         super(taskName);
@@ -7,5 +12,20 @@ public class ToDo extends Task{
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    public static boolean isToDoFormat(String format) {
+        return format.startsWith("T");
+    }
+
+    @Override
+    public String taskToStorageFormat() {
+        return "T|" + super.taskToStorageFormat();
+    }
+
+    @Override
+    public void initStorageFormat(String format) {
+        String[] temp = format.split("\\|", 2);
+        super.initStorageFormat(temp[1]);
     }
 }
