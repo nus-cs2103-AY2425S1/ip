2945f8a1ee300ccbc1653c3258d05327b5109778
Cheng Zihao <czh152103@gmail.com>
2024-08-28 23:27:46 +0800

    added save feature

--- src/main/java/ChattyBuddy.java
+++ src/main/java/ChattyBuddy.java
@@ -1,10 +1,63 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class ChattyBuddy {
+
+    private static void loadDataFile(ArrayList<Task> inputList) throws FileNotFoundException {
+        File dataSaved = new File("./data/chattybuddy.txt");
+        Scanner s = new Scanner(dataSaved);
+
+        while (s.hasNext()) {
+            String[] dataArr = s.nextLine().split(" \\| ");
+            if (dataArr[0].equals("T")) {
+                for (int i = 0; i < dataArr.length; i++) {
+                    System.out.println(dataArr[i]);
+                }
+                Todo newTodo = new Todo();
+                inputList.add(newTodo);
+                newTodo.convertSavedDataToTask(dataArr);
+            } else if (dataArr[0].equals("D")) {
+                Deadline newDeadline = new Deadline();
+                inputList.add(newDeadline);
+                newDeadline.convertSavedDataToTask(dataArr);
+            } else if (dataArr[0].equals("E")) {
+                Event newEvent = new Event();
+                inputList.add(newEvent);
+                newEvent.convertSavedDataToTask(dataArr);
+            }
+        }
+    }
+
+    private static void saveDataToFile(ArrayList<Task> inputList) throws IOException {
+        FileWriter fw = new FileWriter("./data/chattybuddy.txt");
+        String separation = " | ";
+        for (Task task: inputList) {
+            if (task instanceof Todo) {
+                fw.write(task.type + separation + (task.isDone? "1" : "0") + separation + task.description + "\n");
+            } else if (task instanceof Deadline) {
+                Deadline deadline = (Deadline) task;
+                fw.write(task.type + separation + (task.isDone? "1" : "0") + separation + task.description + separation + deadline.dueTime + "\n");
+            } else if (task instanceof Event) {
+                Event event = (Event) task;
+                fw.write(task.type + separation + (task.isDone? "1" : "0") + separation + task.description + separation + event.startTime + separation + event.endTime + "\n");
+            }
+        }
+        fw.close();
+    }
+
     public static void main(String[] args) {
         ArrayList<Task> inputList = new ArrayList<>();
+        try {
+            loadDataFile(inputList);
+        } catch (FileNotFoundException e) {
+            System.out.println("error :" + e.getMessage());
+        }
+
         String breakLine = "——————————————————————————";
-        String logo = " ____        _        \n"
+        String logo = " ____        _           \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
@@ -28,7 +81,7 @@ public class ChattyBuddy {
                     System.out.println(breakLine);
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < inputList.size(); i++) {
-                        System.out.printf("%d.[%s][%s] %s%n", i + 1,inputList.get(i).type,inputList.get(i).getStatusIcon(), inputList.get(i).description);
+                        System.out.printf("%d.[%s][%s] %s%n", i + 1,inputList.get(i).type,inputList.get(i).getStatusIcon(), inputList.get(i));
                     }
                     System.out.println(breakLine);
 
@@ -40,13 +93,18 @@ public class ChattyBuddy {
                         }
                         Task target = inputList.get(Integer.parseInt(slicedStr[1]) - 1);
                         target.setMarkStatus(true);
+                        saveDataToFile(inputList);
                         System.out.println(breakLine);
                         System.out.println("OK, I've marked this task as done:");
-                        System.out.printf("[%s][%s] %s%n", target.type, target.getStatusIcon(), target.description);
+                        System.out.printf("[%s][%s] %s%n", target.type, target.getStatusIcon(), target);
                         System.out.println(breakLine);
                     } catch (TaskIndexOutOfBound e) {
                         System.out.println(breakLine);
                         System.out.println(e.getMessage());
+                        System.out.println(breakLine);
+                    } catch (IOException e) {
+                        System.out.println(breakLine);
+                        System.out.println("error: " + e.getMessage());
                         System.out.println(breakLine);
                     }
 
@@ -58,13 +116,18 @@ public class ChattyBuddy {
                         }
                             Task target = inputList.get(Integer.parseInt(slicedStr[1]) - 1);
                             target.setMarkStatus(false);
+                            saveDataToFile(inputList);
                             System.out.println(breakLine);
                             System.out.println("OK, I've marked this task as not done yet:");
-                            System.out.printf("[%s][%s] %s%n", target.type, target.getStatusIcon(), target.description);
+                            System.out.printf("[%s][%s] %s%n", target.type, target.getStatusIcon(), target);
                             System.out.println(breakLine);
                     } catch (TaskIndexOutOfBound e) {
                         System.out.println(breakLine);
                         System.out.println(e.getMessage());
+                        System.out.println(breakLine);
+                    } catch (IOException e) {
+                        System.out.println(breakLine);
+                        System.out.println("error: " + e.getMessage());
                         System.out.println(breakLine);
                     }
 
@@ -77,14 +140,19 @@ public class ChattyBuddy {
                         Todo newTodo = new Todo();
                         newTodo.convertStringToTask(slicedStr);
                         inputList.add(newTodo);
+                        saveDataToFile(inputList);
                         System.out.println(breakLine);
                         System.out.println("Got it. I've added this task:");
-                        System.out.printf("  [%s][%s] %s%n", newTodo.type, newTodo.getStatusIcon(), newTodo.description);
+                        System.out.printf("  [%s][%s] %s%n", newTodo.type, newTodo.getStatusIcon(), newTodo);
                         System.out.printf("Now you have %d tasks in the list%n", inputList.size());
                         System.out.println(breakLine);
                     } catch (EmptyTaskException e) {
                         System.out.println(breakLine);
                         System.out.println(e.getMessage());
+                        System.out.println(breakLine);
+                    } catch (IOException e) {
+                        System.out.println(breakLine);
+                        System.out.println("error: " + e.getMessage());
                         System.out.println(breakLine);
                     }
 
@@ -98,14 +166,19 @@ public class ChattyBuddy {
                         Deadline newDeadline = new Deadline();
                         newDeadline.convertStringToTask(slicedStr);
                         inputList.add(newDeadline);
+                        saveDataToFile(inputList);
                         System.out.println(breakLine);
                         System.out.println("Got it. I've added this task:");
-                        System.out.printf("  [%s][%s] %s%n", newDeadline.type, newDeadline.getStatusIcon(), newDeadline.description);
+                        System.out.printf("  [%s][%s] %s%n", newDeadline.type, newDeadline.getStatusIcon(), newDeadline);
                         System.out.printf("Now you have %d tasks in the list%n", inputList.size());
                         System.out.println(breakLine);
                     } catch (EmptyTaskException e) {
                         System.out.println(breakLine);
                         System.out.println(e.getMessage());
+                        System.out.println(breakLine);
+                    } catch (IOException e) {
+                        System.out.println(breakLine);
+                        System.out.println("error: " + e.getMessage());
                         System.out.println(breakLine);
                     }
 
@@ -119,16 +192,23 @@ public class ChattyBuddy {
                         Event newEvent = new Event();
                         newEvent.convertStringToTask(slicedStr);
                         inputList.add(newEvent);
+                        saveDataToFile(inputList);
                         System.out.println(breakLine);
                         System.out.println("Got it. I've added this task:");
-                        System.out.printf("  [%s][%s] %s%n", newEvent.type, newEvent.getStatusIcon(), newEvent.description);
+                        System.out.printf("  [%s][%s] %s%n", newEvent.type, newEvent.getStatusIcon(), newEvent);
                         System.out.printf("Now you have %d tasks in the list%n", inputList.size());
                         System.out.println(breakLine);
                     } catch (EmptyTaskException e) {
                         System.out.println(breakLine);
                         System.out.println(e.getMessage());
                         System.out.println(breakLine);
+                    } catch (IOException e) {
+                        System.out.println(breakLine);
+                        System.out.println("error: " + e.getMessage());
+                        System.out.println(breakLine);
                     }
+
+                // user enters "delete xxx"
                 } else if (slicedStr.length == 2 && slicedStr[0].equals("delete")){
                     try {
                         if (Integer.parseInt(slicedStr[1]) > inputList.size() || Integer.parseInt(slicedStr[1]) <= 0 ) {
@@ -136,14 +216,19 @@ public class ChattyBuddy {
                         }
                         Task target = inputList.get(Integer.parseInt(slicedStr[1]) - 1);
                         inputList.remove(Integer.parseInt(slicedStr[1]) - 1);
+                        saveDataToFile(inputList);
                         System.out.println(breakLine);
                         System.out.println("Noted. I've removed this task:");
-                        System.out.printf("[%s][%s] %s%n", target.type, target.getStatusIcon(), target.description);
+                        System.out.printf("[%s][%s] %s%n", target.type, target.getStatusIcon(), target);
                         System.out.printf("Now you have %d tasks in the list%n", inputList.size());
                         System.out.println(breakLine);
                     } catch (TaskIndexOutOfBound e) {
                         System.out.println(breakLine);
                         System.out.println(e.getMessage());
+                        System.out.println(breakLine);
+                    } catch (IOException e) {
+                        System.out.println(breakLine);
+                        System.out.println("error: " + e.getMessage());
                         System.out.println(breakLine);
                     }
 
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class Deadline extends Task{
+    protected String dueTime;
     public Deadline() {
         super("", "D");
     }
@@ -8,7 +9,19 @@ public class Deadline extends Task{
     public void convertStringToTask(String[] slicedStr) {
         String[] task = Arrays.copyOfRange(slicedStr, 1, slicedStr.length);
         String deadlineTaskDetail = String.join(" ", task);
-        String[] deadlineTask = deadlineTaskDetail.split("/");
-        this.description = deadlineTask[0] + "(by: " + deadlineTask[1].substring(3) + ")";
+        String[] deadlineTask = deadlineTaskDetail.split(" /");
+        this.description = deadlineTask[0];
+        this.dueTime = deadlineTask[1].substring(3);
+    }
+
+    public void convertSavedDataToTask(String[] dataArr) {
+        this.setMarkStatus(dataArr[1].equals("1"));
+        this.description = dataArr[2];
+        this.dueTime = dataArr[3];
+    }
+
+    @Override
+    public String toString() {
+        return this.description + " (by: " + this.dueTime + ")";
     }
 }
