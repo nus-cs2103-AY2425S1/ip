4e5f50c6cd14fb3de93cc30ae93179dae6016ac1
seah-minlong <86313670+seah-minlong@users.noreply.github.com>
2024-08-28 16:11:13 +0800

    Update Events to use LocalDate instead of String

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,30 +1,43 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Events extends Task {
-    private String from;
-    private String to;
+    private LocalDate from;
+    private LocalDate to;
 
-    public Events(String description, String from, String to) {
+    public Events(String description, String from, String to) throws AlfredException {
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            this.from = LocalDate.parse(from);
+            this.to = LocalDate.parse(to);
+        } catch (DateTimeParseException e) {
+            throw new AlfredException("There was an invalid deadline Sir. It should go yyyy-mm-dd.");
+        }
     }
 
-    public Events(String description, String from, String to, boolean isDone) {
+    public Events(String description, String from, String to, boolean isDone) throws AlfredException {
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            this.from = LocalDate.parse(from);
+            this.to = LocalDate.parse(to);
+        } catch (DateTimeParseException e) {
+            throw new AlfredException("There was an invalid deadline Sir. It should go yyyy-mm-dd.");
+        }
         this.isDone = isDone;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to: " + to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     public static Task createTask(String input) throws AlfredException {
-        String regex = "^event\\s+(.+?)\\s+/from\\s+(.+?)\\s+/to\\s+(.+)$";
+        String regex = "^event\\s+(.+?)\\s+/from\\s+" +
+                "(\\d{4}-\\d{2}-\\d{2})\\s+/to\\s+(\\d{4}-\\d{2}-\\d{2})$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
@@ -36,7 +49,7 @@ public class Events extends Task {
             return new Events(description, from, to);
         } else {
             throw new AlfredException("That is the wrong events format Sir. It goes event <task> " +
-                    "/from <date> /to <date>");
+                    "/from yyyy-mm-dd /to yyyy-mm-dd");
         }
     }
 
