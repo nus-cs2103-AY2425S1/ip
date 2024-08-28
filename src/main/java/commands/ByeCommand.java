165d5981896f2d3b85bc061b88d21d0f79692104
wesley <wesleywong08@gmail.com>
2024-08-29 00:56:42 +0800

    Add support for more commands

---
+++ src/main/java/commands/ByeCommand.java
@@ -0,0 +1,18 @@
+package commands;
+
+import util.Storage;
+import util.TaskList;
+import util.Ui;
+
+public class ByeCommand extends Command {
+  private final String exitMsg = "Bye. Hope to see you again soon!";
+
+  public ByeCommand(CommandTypes type) {
+    this.command = type;
+  }
+
+  @Override
+  public void execute(TaskList tl, Ui ui, Storage storage, String... details) {
+    ui.printResponse(this.exitMsg);
+  }
+}
--- src/main/java/commands/Command.java
+++ src/main/java/commands/Command.java
@@ -9,25 +9,60 @@ public abstract class Command {
   protected CommandTypes command;
 
   public static enum CommandTypes {
-    TODO,
-    DEADLINE,
-    EVENT;
+    TODO, DEADLINE, EVENT, LIST, MARK, UNMARK, DELETE, BYE;
 
     public static CommandTypes toCommandType(String type) throws MizzException {
-      switch (type.toLowerCase()) {
+      switch (type.strip().toLowerCase()) {
         case "todo":
           return TODO;
         case "deadline":
           return DEADLINE;
         case "event":
           return EVENT;
+        case "list":
+          return LIST;
+        case "mark":
+          return MARK;
+        case "unmark":
+          return UNMARK;
+        case "delete":
+          return DELETE;
+        case "bye":
+          return BYE;
         default:
           throw new MizzException("Unknown command type: " + type);
       }
     }
+
+    public static Command toCommand(CommandTypes type) throws MizzException {
+      switch (type) {
+        case TODO:
+          return new ToDoCommand(type);
+        case DEADLINE:
+          return new DeadlineCommand(type);
+        case EVENT:
+          return new EventCommand(type);
+        case LIST:
+          return new ListCommand(type);
+        case MARK:
+        case UNMARK:
+          return new MarkCommand(type);
+        case DELETE:
+          return new DeleteCommand(type);
+        case BYE:
+          return new ByeCommand(type);
+        default:
+          throw new MizzException("Unknown command type: " + type.toString());
+      }
+    }
   }
 
-  abstract void execute(TaskList tl, Ui ui, Storage storage, String... details) throws MizzException;
+  public static Command of(String type) throws MizzException {
+    CommandTypes cmdType = CommandTypes.toCommandType(type);
+    return CommandTypes.toCommand(cmdType);
+  }
+
+  public abstract void execute(TaskList tl, Ui ui, Storage storage, String... details) throws MizzException;
 
   @Override
   public String toString() {
