8871f74d3212f67bd5b1edbad1b235688f5c964e
leroytan <mingxiang130@gmail.com>
2024-08-29 04:31:54 +0800

    Implement saved list
    The program currently lacks the functionality to persist data across
    sessions, which means any information stored in the list is lost when
    the program closes.
    
    This limitation makes the application less user-friendly, as users
    have to re-enter their data every time they run the program.
    Persistent storage is necessary to improve usability.
    
    Add methods to read and write to file to handle data storage and
    retrieval.
    
    The 'writeToFile()' method is used to store the current state of the
    list into a file, 'arrayToNumberedString()' formats the data for
    readability, and 'readFromFile()' is used to retrieve the stored data
    when the program runs again. This approach ensures that data is
    consistently saved and retrieved in a structured manner.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.io.File;
 
 public class Duke {
     public static void main(String[] args) {
+        String savedListPath = "." + File.separator + "data" + File.separator + "saved.txt";
         String lineBreak = "____________________________________________________________\n";
         String intro = lineBreak + "Hello! I'm Light\nWhat can I do for you?\n" + lineBreak;
         String exit = lineBreak + " Bye. Hope to see you again soon!\n" + lineBreak;
@@ -15,7 +22,8 @@ public class Duke {
         String[] splitedBySlash;
         Task event;
         ArrayList<Task> list = new ArrayList<>();
-
+        //fetch the savedlist
+        list = readFromFile(savedListPath);
         while (scanner.hasNextLine()) {
             event = null;
             command = scanner.nextLine();
@@ -29,6 +37,7 @@ public class Duke {
                         int itemNumber = Integer.parseInt(splitedBySpace[1]);
                         list.get(itemNumber - 1).markAsDone();
                         System.out.println("Nice! I've marked this task as done:\n" + list.get(itemNumber - 1));
+                        writeToFile(savedListPath, arrayToNumberedString(list));
                     } catch (NumberFormatException e) {
                         System.out.println(e);
                     }
@@ -38,13 +47,14 @@ public class Duke {
                         int itemNumber = Integer.parseInt(splitedBySpace[1]);
                         list.get(itemNumber - 1).markAsUndone();
                         System.out.println("Nice! I've marked this task as undone:\n" + list.get(itemNumber - 1));
+                        writeToFile(savedListPath, arrayToNumberedString(list));
                     } catch (NumberFormatException e) {
                         System.out.println(e);
                     }
                     break;
                 case "list":
                     System.out.print(lineBreak);
-                    IntStream.range(0, list.size()).mapToObj(number -> number + 1 + ". " + list.get(number)).forEach(System.out::println);
+                    System.out.println(arrayToNumberedString(list));
                     System.out.print(lineBreak);
                     break;
                 case "delete":
@@ -52,6 +62,7 @@ public class Duke {
                         int itemNumber = Integer.parseInt(splitedBySpace[1]);
                         list.remove(itemNumber - 1);
                         System.out.println(lineBreak + "Noted. I've removed this task:\n" + list.get(itemNumber - 1) + "\nNow you have " + list.size() + " tasks in the list.\n" + lineBreak);
+                        writeToFile(savedListPath, arrayToNumberedString(list));
                     } catch (NumberFormatException e) {
                         System.out.println(e);
                     }
@@ -60,7 +71,7 @@ public class Duke {
                     switch (splitedBySpace[0]) {
                         case "todo":
                             try {
-                                event = new Todo(command.replace("todo", ""));
+                                event = new Todo(splitedBySpace[1]);
                             } catch (LightException e) {
                                 System.out.println(e);
                             }
@@ -69,7 +80,7 @@ public class Duke {
                         case "deadline":
                             try {
                                 splitedBySlash = splitedBySpace[1].split("/");
-                                event = new Deadline(splitedBySlash[0], splitedBySlash[1].replace("by", ""));
+                                event = new Deadline(splitedBySlash[0], splitedBySlash[1].replace("by ", ""));
                             } catch (ArrayIndexOutOfBoundsException e) {
                                 System.out.println("Not enough arguments");
                             } catch (LightException e) {
@@ -80,7 +91,7 @@ public class Duke {
                         case "event":
                             try {
                                 splitedBySlash = splitedBySpace[1].split("/");
-                                event = new Event(splitedBySlash[0], splitedBySlash[1].replace("from", ""), splitedBySlash[2].replace("to", ""));
+                                event = new Event(splitedBySlash[0], splitedBySlash[1].replace("from ", "").stripTrailing(), splitedBySlash[2].replace("to ", ""));
                             } catch (ArrayIndexOutOfBoundsException e) {
                                 System.out.println("Not enough arguments");
                             } catch (LightException e) {
@@ -96,6 +107,7 @@ public class Duke {
                         System.out.println(lineBreak + "Got it. I've added this task:\n" +
                                 event +
                                 "\nNow you have " + list.size() + " tasks in the list.\n" + lineBreak);
+                        writeToFile(savedListPath, arrayToNumberedString(list));
                     }
 
             }
@@ -103,4 +115,88 @@ public class Duke {
 
 
     }
+    public static void writeToFile(String filePath, String textToAdd) {
+        try {
+            File fileObj = new File(filePath);
+            if (!fileObj.getParentFile().exists()) {
+                fileObj.getParentFile().mkdirs();
+            }
+            if (fileObj.createNewFile()) {
+                //file created
+                System.out.println("Created file");
+            } else {
+                //file already exist
+                System.out.println("File exist");
+            }
+            FileWriter myWriter = new FileWriter(filePath);
+            myWriter.write(textToAdd);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+    public static <T> String arrayToNumberedString(ArrayList<T> array) {
+        return IntStream.range(0, array.size()).mapToObj(number -> number + 1 + ". " + array.get(number)).map(Objects::toString)
+                .collect(Collectors.joining("\n"));
+    }
+    public static ArrayList<Task> readFromFile(String filePath) {
+        ArrayList<Task> list = new ArrayList<>();
+        try {
+            File fileObj = new File(filePath);
+            Scanner scanner = new Scanner(fileObj);
+            Task event = null;
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                Boolean marked = data.charAt(7) == 'X';
+                switch (data.charAt(4)) {
+                    case 'T':
+                        try {
+                            event = new Todo(data.substring(10));
+                        } catch (LightException e) {
+                            System.out.println(e);
+                        }
+
+                        break;
+                    case 'D':
+                        try {
+                            String betweenBrackets = data.substring(data.lastIndexOf("(")+1, data.lastIndexOf(")"));
+                            String byDate = betweenBrackets.replace("by: ", "");
+                            event = new Deadline(data.substring(10, data.indexOf("(by:")), byDate);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("Not enough arguments");
+                        } catch (LightException e) {
+                            System.out.println(e);
+                        }
+
+                        break;
+                    case 'E':
+                        try {
+
+                            String betweenBrackets = data.substring(data.lastIndexOf("(")+1, data.lastIndexOf(")"));
+                            String fromDate = betweenBrackets.substring(betweenBrackets.indexOf("from: ")+6, betweenBrackets.indexOf("to: ")).stripTrailing();
+                            String toDate = betweenBrackets.substring(betweenBrackets.indexOf("to: ")+4);
+                            event = new Event(data.substring(10, data.indexOf("(from:")), fromDate, toDate);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("Not enough arguments");
+                        } catch (LightException e) {
+                            System.out.println(e);
+                        }
+
+                        break;
+                }
+                if (event != null && list.add(event)) {
+                    if (marked) {
+                        event.markAsDone();
+                    }
+                    System.out.println(event);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
 }
+
