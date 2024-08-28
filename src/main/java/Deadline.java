9fec4ccb1a6209f93aba6688f68d2f7f87489f37
SooYap <tansooyap@gmail.com>
2024-08-28 12:47:47 +0800

    Deadline.java: Add Date and Time parser for user input

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     private String endDate;
 
@@ -12,6 +16,14 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D] " + super.getName() + "(" + this.endDate + ")";
+        try {
+            return "[D] " + super.getName() + "(by " +
+                    LocalDate.parse(this.endDate.substring(0, 10))
+                            .format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " " +
+                    LocalTime.parse(this.endDate.substring(11, 13) + ":" + this.endDate.substring(13,15))
+                            .format(DateTimeFormatter.ofPattern("HH:mm")) + ")";
+        } catch (Exception e) {
+            return "[D] " + super.getName() + "(" + this.endDate + ")";
+        }
     }
 }
