77a8cda0c9a19099608ceb13afd1cfc6edb6f968
Karthikeyan <145328000+SKarthikeyan28@users.noreply.github.com>
2024-08-28 12:54:21 +0800

    Added javadoc comments

--- src/main/java/Tars.java
+++ src/main/java/Tars.java
@@ -1,68 +1,164 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Class represents a generic class with a Description and isDone status
+ * Serves as a base class for other Task types like (ToDos, Deadline, Event)
+ *
+ *
+ * @author csk
+ * @version 1
+ */
 class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for new Task with specified description
+     * The task will be initialised with marked as not done
+     *
+     * @param description (description of Task)
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns a status icon depending on the isDone status of the Task
+     *
+     * @return "X" if task is done, " " otherwise returned
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    //void since method does not return anything
+    /**
+     * Marks the task as done
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    /**
+     * Returns a string representation of the status and description of the task
+     *
+     * @return String Representation of task
+     */
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
+/**
+ * Class represents ToDos task, which only has a description
+ * Extends from Task class
+ *
+ * @see Task
+ */
 class ToDos extends Task {
-
+    /**
+     * Constructs for new ToDos Task with description stated
+     *
+     * @param description (of specified ToDos task)
+     */
     public ToDos(String description) {
         super(description);
     }
+    /**
+     * Returns a string representation of the status and description of the task with "[T]"
+     *
+     * @return String Representation of ToDos task
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 }
+/**
+ * Class represents Deadline Task, which has a specific deadline
+ * Extends from Task class and also has an extra field for deadline of task
+ *
+ * @see Task
+ */
 class Deadline extends Task {
     protected String by;
+    /**
+     * Constructs new Deadline Task with deadline stated
+     *
+     * @param description (of specified ToDos task)
+     * @param by (deadline of Task)
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
+    /**
+     * Returns a string representation of the description of the Deadline task with "[D]"
+     *
+     * @return String Representation of Deadline task
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
+/**
+ * Class represents Event task, which has a description, and start and end time
+ * Extends from Task class and has extra fields to store start and end time of event
+ *
+ * @see Task
+ */
 class Event extends Task {
     protected String from, to;
+    /**
+     * Constructs new Deadline Task with deadline stated
+     *
+     * @param description (of specified ToDos task)
+     * @param from (start of Event)
+     * @param to (end of Event)
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
+    /**
+     * Returns a string representation of the description of the Event task with "[E]"
+     * String also includes information on Start and End time of Event
+     *
+     * @return String Representation of Event task
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
+
+/**
+ * This class is Chatbot application which handles Task Management, mainly ToDos, Deadline and Event tasks
+ * Users can add the 3 different type of tasks and carry out other functions like mark, unmark and delete
+ * Users can also view all the tasks added in a LIST
+ * This application will exist till "bye" command entered by User
+ *
+ * @author csk
+ * @version 1
+ */
 public class Tars {
     static String line = "    _____________________________________________";
+
+    /**
+     * Runs the Tars application, where it takes in users inputs until "bye" is entered by user
+     *
+     * @param args (command line arguments given by User)
+     */
     public static void main(String[] args) {
         //welcome/introduction message
         System.out.println(line + "\n" + "    Hello! I'm Tars\n" + "    What can I do for you" + "\n" + line);
