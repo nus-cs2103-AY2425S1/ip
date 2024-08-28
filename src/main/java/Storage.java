0dee89cb4b333110f157e2a0046d25d8151c948d
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-28 10:53:06 +0800

    Create Storage.java

---
+++ src/main/java/Storage.java
@@ -0,0 +1,25 @@
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Storage {
+
+    private String filePath;
+
+    public Storage (String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void saveTasks(ArrayList<Task> tasks) throws IOException {
+        FileWriter writer = new FileWriter(filePath);
+        for (Task task : tasks) {
+            writer.write(task.toFileFormat() + System.lineSeparator())
+        }
+        writer.close();
+
+    }
+
+   
+
+}
