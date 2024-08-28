ab459aaa6b2e5a15b84e3947905d89f849250181
Raghava <chinnu4148@gmail.com>
2024-08-28 23:42:54 +0800

    feat: Add date time support for event and deadline

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,35 +1,26 @@
 package tasks;
 
-import java.time.LocalDate;
+import utils.DateTime;
+
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     private final LocalDateTime deadline;
-    private final DateTimeFormatter DateTimePrintOutputFormatter = DateTimeFormatter.ofPattern("MMM d yyyy HH:mm");
-    private final DateTimeFormatter DateTimeFileOutputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
     public Deadline(String name, String deadline) {
         super(name);
-        this.deadline = this.formatDate(deadline);
+        this.deadline = DateTime.formatDate(deadline);
     }
 
     @Override
     public String toFileString() {
-        return String.format("D %s | %s", super.toFileString(), this.deadline.format(DateTimeFileOutputFormatter));
+        return String.format("D %s | %s", super.toFileString(),
+                this.deadline.format(DateTime.DateTimeFileOutputFormatter));
     }
 
     @Override
     public String toString() {
-        return String.format("[D] %s (by: %s)", super.toString(), this.deadline.format(DateTimePrintOutputFormatter));
-    }
-
-    private LocalDateTime formatDate(String dateTime) {
-        String[] dateTimes = dateTime.split(" ");
-        LocalDate date = LocalDate.parse(dateTimes[0]);
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HHmm");
-        LocalTime time = LocalTime.parse(dateTimes[1], timeFormatter);
-        return LocalDateTime.of(date, time);
+        return String.format("[D] %s (by: %s)", super.toString(),
+                this.deadline.format(DateTime.DateTimePrintOutputFormatter));
     }
 }
--- src/main/java/tasks/Event.java
+++ src/main/java/tasks/Event.java
@@ -1,22 +1,30 @@
 package tasks;
 
+import utils.DateTime;
+
+import java.time.LocalDateTime;
+
 public class Event extends Task {
-    private final String startTime;
-    private final String endTime;
+    private final LocalDateTime startTime;
+    private final LocalDateTime endTime;
 
     public Event(String name, String startTime, String endTime) {
         super(name);
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.startTime = DateTime.formatDate(startTime);
+        this.endTime = DateTime.formatDate(endTime);
     }
 
     @Override
     public String toFileString() {
-        return String.format("E %s | %s-%s", super.toFileString(), this.startTime, this.endTime);
+        return String.format("E %s | %s-%s", super.toFileString(),
+                this.startTime.format(DateTime.DateTimeFileOutputFormatter),
+                this.endTime.format(DateTime.DateTimeFileOutputFormatter));
     }
 
     @Override
     public String toString() {
-        return String.format("[E] %s (from: %s to: %s)", super.toString(), this.startTime, this.endTime);
+        return String.format("[E] %s (from: %s to: %s)", super.toString(),
+                this.startTime.format(DateTime.DateTimeFileOutputFormatter),
+                this.endTime.format(DateTime.DateTimeFileOutputFormatter));
     }
 }
