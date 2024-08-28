aae5198259d59c6dbcca8312b8d7469b3901cd84
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-08-28 11:11:49 +0800

    Add LoadTasks() and CreateTaskFromData() to Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
 public class Storage {
 
     private String filePath;
@@ -20,6 +21,51 @@ public class Storage {
 
     }
 
+    public ArrayList<Task> loadTasks() throws FileNotFoundException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        File file = new File(filePath);
 
+        if (!file.exists()) {
+            File parentFile = file.getParentFile();
+            parentFile.mkdirs();
+            return tasks;
+        }
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] taskData = line.split(" \\| ");
+            Task task = createTaskFromData(taskData);
+            tasks.add(task);
+        }
+        scanner.close();
+        return tasks;
+
+    }
+
+    private Task createTaskFromData(String[] taskData) {
+        String type = taskData[1];
+        boolean isDone = taskData[2].equals("1");
+        String description = taskData[2];
+
+        Task task = null;
+        switch (type) {
+        case "T":
+            task = new Todo(description);
+            break;
+        case "D":
+            String by = taskData[3];
+            task = new Deadline(description, by);
+        case "E":
+            String from = taskData[3];
+            String to = taskData[4];
+            task = new Event(description, from, to);
+            break;
+        }
+        if (isDone) {
+            task.markDone();
+        }
+        return task;
+    }
 
 }
