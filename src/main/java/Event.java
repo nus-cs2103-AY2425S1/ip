4b40ba5a1518a5ff2edc8844a24967199ae98085
kuiktzecheng <kuiktzecheng17@gmail.com>
2024-08-29 22:34:00 +0800

    Add dates and times feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,31 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String start;
-    protected String end;
+    protected LocalDateTime start;
+    protected LocalDateTime end;
+
+    static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a");
+    static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.start = parseDateTime(start);
+        this.end = parseDateTime(end);
     }
+    private LocalDateTime parseDateTime(String dateTime) {
+        return LocalDateTime.parse(dateTime, inputFormatter);
+    }
+
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+        return "[E][" + (this.isDone ? "X" : " ") + "] " + this.description +
+                " (from: " + this.start.format(outputFormatter) + " to: " + this.end.format(outputFormatter) + ")";
     }
     @Override
     public String parseToFile() {
-        return "E | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.start +
-                " | " + this.end;
+        return "E | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " +
+                this.start.format(inputFormatter) + " | " + this.end.format(inputFormatter);
     }
 }
