4ad0fa3f71b09900262f1169fd67da81958e8650
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-08-30 00:16:54 +0800

    Add Ui class

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -8,14 +8,8 @@ import java.time.LocalDate;
 public class Snowy {
 
 
-    private static final String LINE = "-----------------------------------\n";
-    private static final String GREETING = LINE
-            + "Hello! My name is Snowy\n"
-            + "What can I do for you?\n"
-            + LINE;
 
-    private static final String ENDING = "Bye! Hope to see you again soon!\n"
-            + LINE;
+
 
     private static ArrayList<Task> tasks = new ArrayList<>();
 
