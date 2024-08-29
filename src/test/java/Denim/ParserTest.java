5c34d8a1c3653b1fb7453768a686a49be7cbd4f6
t-leongchuan <t.leongchuan@gmail.com>
2024-08-29 20:39:40 +0800

    Add support for testing Parser and TaskList

---
+++ src/test/java/Denim/ParserTest.java
@@ -0,0 +1,94 @@
+package Denim;
+
+import Denim.Commands.*;
+import Denim.Parser;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParserTest {
+
+    private final Parser parser = new Parser();
+    @Test
+    public void prepareEvent_validInput_returnsEventCommand() {
+        String simulatedInput = "event dinner /from 19/02/2002 1900 /to 19/02/2002 2200";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof EventCommand);
+    }
+
+    @Test
+    public void prepareEvent_invalidDateTimeFormat_returnsInvalidCommand() {
+        String simulatedInput = "event dinner /from 19-02-2002 1900 /to 19-02-2002 2200";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareEvent_impossibleDateTime_returnsInvalidCommand() {
+        String simulatedInput = "event dinner /from 19/13/2004 1800 /to 19/13/2004 2000";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareEvent_missingTime_returnsInvalidCommand() {
+        String simulatedInput = "event wilderness camping /from 19/02/2002 /to 20/02/2002";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareEvent_missingDate_returnsInvalidCommand() {
+        String simulatedInput = "event dinner /from 1900 /to 2200";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareDeadline_validInput_returnsDeadlineCommand() {
+        String simulatedInput = "deadline MT2000 Assignment 0 /by 19/04/2002 1900";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof DeadlineCommand);
+    }
+
+    @Test
+    public void prepareDeadline_invalidDateTimeFormat_returnsInvalidCommand() {
+        String simulatedInput = "deadline MT2000 Assignment 0 /by 19-04-2002 1900";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareDeadline_impossibleDateTime_returnsInvalidCommand() {
+        String simulatedInput = "deadline MT2000 Assignment 0 /by 40/20/2002 19000";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareDeadline_missingTime_returnsInvalidCommand() {
+        String simulatedInput = "deadline MT2000 Assignment 0 /by 19/04/2002";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareDeadline_missingDate_returnsInvalidCommand() {
+        String simulatedInput = "deadline MT2000 Assignment 0 /by 1900";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+}
