f837735d33b4279562545837d6ae165b3440d3e6
Casper Ng <casperngeen@gmail.com>
2024-08-29 14:07:58 +0800

    Adjust imports to display all classes

--- src/main/java/casper/Storage.java
+++ src/main/java/casper/Storage.java
@@ -3,7 +3,13 @@ package casper;
 import exception.CasperBotIOException;
 import exception.CasperBotOutOfBoundsException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
