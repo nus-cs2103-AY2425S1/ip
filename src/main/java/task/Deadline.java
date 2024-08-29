ad31448771beb62178a184368fc4cb323f2640aa
LeongWZ <leongweizhi03@gmail.com>
2024-08-29 22:38:08 +0800

    Restore changes from local history

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,11 +1,31 @@
 package task;
 
 public class Deadline extends Task {
-    private String dueBy;
+    private final String dueBy;
 
     public Deadline(String description, String dueBy) {
         super(description);
         this.dueBy = dueBy;
+    }
+
+    public Deadline(String description, boolean isDone, String dueBy) {
+        super(description, isDone);
+        this.dueBy = dueBy;
+    }
+
+    @Override
+    public Deadline setAsDone() {
+        return new Deadline(this.getDescription(), true, this.getDueBy());
+    }
+
+    @Override
+    public Deadline setAsUndone() {
+        return new Deadline(this.getDescription(), false, this.getDueBy());
+    }
+
+    @Override
+    public Deadline setDescription(String description) {
+        return new Deadline(this.getDescription(), this.isDone(), this.getDueBy());
     }
 
     public String getDueBy() {
