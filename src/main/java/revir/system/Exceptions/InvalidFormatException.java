d460f06a4c35453f21ba5bc54e8094a682c45ea3
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-08-30 01:08:20 +0700

    Add JavaDoc for Exceptions

--- src/main/java/revir/system/Exceptions/InvalidFormatException.java
+++ src/main/java/revir/system/Exceptions/InvalidFormatException.java
@@ -1,6 +1,15 @@
 package revir.system.Exceptions;
 
+/**
+ * Represents an exception that is thrown when an invalid format is encountered.
+ * This exception is typically thrown when a format different from the expected format is encountered.
+ */
 public class InvalidFormatException extends Exception {
+    /**
+     * Constructs a new InvalidFormatException with the specified format.
+     *
+     * @param format the expected format that caused the exception
+     */
     public InvalidFormatException(String format) {
         super("Invalid Format. Expected: " + format);
     }
