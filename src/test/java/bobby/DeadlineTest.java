8e056293147b4d9164708c312b1fb61c482d17b5
vinceg4 <vincegoh04@gmail.com>
2024-08-29 22:58:18 +0800

    Add JavaDoc comments for Test files and fix the naming of test methods in EventTest and DeadlineTest

--- src/test/java/bobby/DeadlineTest.java
+++ src/test/java/bobby/DeadlineTest.java
@@ -6,7 +6,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Tests for {@link Deadline} class.
+ */
 public class DeadlineTest {
+
+    /**
+     * Tests the {@link Deadline#createDeadline(String)} method with correct input.
+     * Validates that a Deadline task is correctly created.
+     */
     @Test
     public void validDeadlineTask() throws EmptyDescriptionException {
         String input = "deadline Watch Lecture /by 2024-09-01";
@@ -15,12 +23,20 @@ public class DeadlineTest {
         assertEquals("D", deadlineTask.getTaskType());
     }
 
+    /**
+     * Tests the {@link Deadline#toString()} method.
+     * Validates that a Deadline task is correctly formatted as a string.
+     */
     @Test public void validDeadlineStringFormat() throws EmptyDescriptionException {
         String input = "deadline Watch Lecture /by 2024-09-01";
         Deadline deadlineTask = Deadline.createDeadline(input);
         assertEquals("[D][ ] Watch Lecture (by: Sep 1 2024)", deadlineTask.toString());
     }
 
+    /**
+     * Tests {@link Deadline#taskInFile()} method.
+     * Validates that a Deadline task is correctly formatted in a file.
+     */
     @Test
     public void validDeadlineTaskFormatInFile() throws EmptyDescriptionException {
         String input = "deadline Watch Lecture /by 2024-09-01";
@@ -28,8 +44,12 @@ public class DeadlineTest {
         assertEquals("D |  | Watch Lecture | /by 2024-09-01", deadlineTask.taskInFile());
     }
 
+    /**
+     * Tests the {@link Deadline#createDeadline(String)} method with incorrect input.
+     * Validates that a Deadline task is not created with empty description.
+     */
     @Test
-    public void invalidTodoTask() throws EmptyDescriptionException {
+    public void invalidDeadlineTask() throws EmptyDescriptionException {
         String input = "deadline";
         EmptyDescriptionException e = assertThrows(EmptyDescriptionException.class, () -> Deadline.createDeadline(input));
     }
--- src/test/java/bobby/EventTest.java
+++ src/test/java/bobby/EventTest.java
@@ -6,7 +6,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Tests for {@link Event} class.
+ */
 public class EventTest {
+
+    /**
+     * Tests the {@link Event#createEvent(String)} method with correct input.
+     * Validates that a Event task is correctly created.
+     */
     @Test
     public void validEventTask() throws EmptyDescriptionException {
         String input = "event Watch Lecture /from Monday /to Thursday";
@@ -15,12 +23,21 @@ public class EventTest {
         assertEquals("E", eventTask.getTaskType());
     }
 
+    /**
+     * Tests the {@link Event#toString()} method.
+     * Validates that a Event task is correctly formatted as a string.
+     */
     @Test public void validEventStringFormat() throws EmptyDescriptionException {
         String input = "event Watch Lecture /from Monday /to Thursday";
         Event eventTask = Event.createEvent(input);
         assertEquals("[E][ ] Watch Lecture (from: Monday to: Thursday)", eventTask.toString());
     }
 
+
+    /**
+     * Tests {@link Event#taskInFile()} method.
+     * Validates that a Event task is correctly formatted in a file.
+     */
     @Test
     public void validDeadlineTaskFormatInFile() throws EmptyDescriptionException {
         String input = "event Watch Lecture /from Monday /to Thursday";
@@ -28,8 +45,13 @@ public class EventTest {
         assertEquals("E |  | Watch Lecture | /from Monday /to Thursday", eventTask.taskInFile());
     }
 
+
+    /**
+     * Tests the {@link Event#createEvent(String)} method with incorrect input.
+     * Validates that a Event task is not created with empty description.
+     */
     @Test
-    public void invalidTodoTask() throws EmptyDescriptionException {
+    public void invalidEventTask() throws EmptyDescriptionException {
         String input = "event";
         EmptyDescriptionException e = assertThrows(EmptyDescriptionException.class, () -> Event.createEvent(input));
     }
