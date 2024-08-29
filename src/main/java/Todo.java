bfe0befefc6fbb9660d3a54854381729b974c6cc
dovlin <dovlincs01@gmail.com>
2024-08-29 22:56:58 +0800

    Add to file string parsing for ToDo

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,12 +1,15 @@
 public class Todo extends Task {
-    public Todo(String task) {
-        super(task);
+    public Todo(String task, boolean isCompleted) {
+        super(task, isCompleted);
+    }
+
+    @Override
+    public String toFileString() {
+        return "T|" + super.toFileString();
     }
 
     public String toString() {
         return "[T]" + super.toString();
     }
-
-
 
 }
