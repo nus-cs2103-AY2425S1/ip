6ab1bfcb63a42ba15330a0fee85240e08ddf9bad
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-29 23:39:16 +0800

    Add test for Storage.saveListFromFile

--- src/test/java/yappingbot/storage/StorageTest.java
+++ src/test/java/yappingbot/storage/StorageTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import yappingbot.tasks.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Collection;
+import java.util.Scanner;
 
 class StorageTest {
     @Test
@@ -26,7 +29,30 @@ class StorageTest {
     }
 
     @Test
-    void testSaveListFromFile() {
-        Storage s = new Storage("resources/savefile_test");
+    void testSaveListFromFile() throws FileNotFoundException {
+        String savefile_path = "src/test/resources/savefile_test";
+        Storage s = new Storage(savefile_path);
+        TaskList t = new TaskList();
+        t.add(new Todo("a", false));
+        t.add(new Todo("b", true));
+        t.add(new Deadline("adadsdd", false, "2023-12-12"));
+        t.add(new Event("abc", false, "2023-12-23","2025-02-01"));
+        s.saveListToFile(t);
+
+        String expected = """
+TODO:a:false
+TODO:b:true
+DEADLINE:adadsdd:false:2023-12-12
+EVENT:abc:false:2023-12-23:2025-02-01
+                """;
+
+        File saveFile = new File(savefile_path);
+        Scanner scanner = new Scanner(saveFile);
+        StringBuilder sb = new StringBuilder();
+        while (scanner.hasNext()) {
+            sb.append(scanner.nextLine());
+            sb.append("\n");
+        }
+        assertEquals(sb.toString(), expected);
     }
 }
