e4173f64e8043327e115b63c3fc74f9085888ede
matthewyip1511 <e0938566@u.nus.ed>
2024-08-30 00:17:34 +0800

    Add back function lost while fixing merge conflict

--- src/main/java/poChat/bot/TaskList.java
+++ src/main/java/poChat/bot/TaskList.java
@@ -31,6 +31,18 @@ public class TaskList {
         return this.listTasks.get(index);
     }
 
+    public void printMatchingTasks(String keyword) {
+        System.out.println("Here are the matching tasks in your list:");
+
+        int index = 1;
+        for (Task task : this.listTasks) {
+            if (task.contains(keyword)) {
+                System.out.println(index + ". " + task);
+                index++;
+            }
+        }
+    }
+
     public ArrayList<Task> toList() {
         return this.listTasks;
     }
