928832a257dd35412bb8fd93420b98f98492473c
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-08-29 20:22:12 +0800

    Edit current code to comply with Coding standard
    Code does not comply with coding standard
    
    I will lose marks.
    
    Let's edit the code so it complies with the standard.
    
    Also I did not read all the tasks so a commit to fit the standard is
    supposed to be added later. I will make another commit then.

--- src/main/java/YapMeister.java
+++ src/main/java/YapMeister.java
@@ -5,78 +5,73 @@ import static java.lang.Integer.parseInt;
 
 public class YapMeister {
     final static int MAX_TASKS = 100;
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
-                               Hello I'm YapMeister
-                               YAPYAPYAPYAP
-                               """);
+                Hello I'm YapMeister
+                YAPYAPYAPYAP
+                """);
         String input = "";
-        boolean running = true;
+        boolean isRunning = true;
         ArrayList<Task> tasks = new ArrayList<>();
-        String[] inputParsed;
-        while (running) {
+        while (isRunning) {
             System.out.print("\n");
             input = scanner.nextLine();
             String[] command = input.split(" ");
             try {
                 switch (command[0]) {
-                    case "bye":
-                        running = false;
-                        break;
-                    case "list":
-                        int i = 0;
-                        for (Task task : tasks) {
-                            System.out.println(String.format("%d. %s", i + 1, task.toString()));
-                            i++;
-                        }
-                        break;
-                    case "mark":
-                        int index = parseInt(command[1]) - 1;
-                        if (index >= tasks.size() || index < 0) {
-                            throw new InvalidMarkException("No task at that index");
-                        }
-                        tasks.get(index).setCompleted(true);
-                        System.out.println("You did this:");
-                        System.out.println(tasks.get(index).toString());
-                        break;
-                    case "unmark":
-                        int umindex = parseInt(command[1]) - 1;
-                        if (umindex >= tasks.size() || umindex < 0) {
-                            throw new InvalidMarkException("No task at that index");
-                        }
-                        tasks.get(umindex).setCompleted(false);
-                        System.out.println("You undid this:");
-                        System.out.println(tasks.get(umindex).toString());
-                        break;
-                    case "todo":
-                    case "deadline":
-                    case "event":
-                        Task task = null;
-                        task = createTask(command[0], input);
-                        System.out.println("Added:");
-                        task.toString();
-                        tasks.add(task);
-                        System.out.println(String.format("You have %d tasks", tasks.size()));
-                        break;
-                    case "delete":
-                        int dindex = parseInt(command[1]) - 1;
-                        if (dindex >= tasks.size() || dindex < 0) {
-                            throw new InvalidMarkException("No task at that index");
-                        }
-                        Task removedTask = tasks.remove(dindex);
-                        System.out.println("Removed this task");
-                        System.out.println(removedTask.toString());
-                        System.out.println(String.format("You have %d tasks", tasks.size()));
-                        break;
-                    default:
-                        System.out.println("Invalid input please yap yapology");
+                case "bye":
+                    isRunning = false;
+                    break;
+                case "list":
+                    int i = 0;
+                    for (Task task : tasks) {
+                        System.out.println(String.format("%d. %s", i + 1, task.toString()));
+                        i++;
+                    }
+                    break;
+                case "mark":
+                    int index = parseInt(command[1]) - 1;
+                    if (index >= tasks.size() || index < 0) {
+                        throw new InvalidMarkException("No task at that index");
+                    }
+                    tasks.get(index).setCompleted(true);
+                    System.out.println("You did this:");
+                    System.out.println(tasks.get(index).toString());
+                    break;
+                case "unmark":
+                    int umindex = parseInt(command[1]) - 1;
+                    if (umindex >= tasks.size() || umindex < 0) {
+                        throw new InvalidMarkException("No task at that index");
+                    }
+                    tasks.get(umindex).setCompleted(false);
+                    System.out.println("You undid this:");
+                    System.out.println(tasks.get(umindex).toString());
+                    break;
+                case "todo":
+                    //Fallthrough
+                case "deadline":
+                    //Fallthrough
+                case "event":
+                    Task task = null;
+                    task = createTask(command[0], input);
+                    System.out.println("Added:");
+                    tasks.add(task);
+                    System.out.println(String.format("You have %d tasks", tasks.size()));
+                    break;
+                case "delete":
+                    int dindex = parseInt(command[1]) - 1;
+                    if (dindex >= tasks.size() || dindex < 0) {
+                        throw new InvalidMarkException("No task at that index");
+                    }
+                    Task removedTask = tasks.remove(dindex);
+                    System.out.println("Removed this task");
+                    System.out.println(removedTask.toString());
+                    System.out.println(String.format("You have %d tasks", tasks.size()));
+                    break;
+                default:
+                    System.out.println("Invalid input please yap yapology");
                 }
             } catch (InvalidDescriptionException | InvalidMarkException e) {
                 System.out.println(e.getMessage());
@@ -87,31 +82,32 @@ public class YapMeister {
         System.out.println("Fine. Bye. Leave and never return");
     }
 
+
     private static Task createTask(String type, String input) throws InvalidDescriptionException {
         Task task = null;
         String[] format;
         switch (type) {
-            case "todo":
-                format = input.split("todo ");
-                if (format.length <= 1) {
-                    throw new InvalidDescriptionException("Invalid Task description");
-                }
-                task = new ToDo(format[1]);
-                break;
-            case "deadline":
-                format = input.split("deadline | /by ");
-                if (format.length <= 2) {
-                    throw new InvalidDescriptionException("Invalid Task description");
-                }
-                task = new Deadline(format[1], format[2]);
-                break;
-            case "event":
-                format = input.split("event | /from | /to ");
-                if (format.length <= 3) {
-                    throw new InvalidDescriptionException("Invalid Task description");
-                }
-                task = new Event(format[1], format[2], format[3]);
-                break;
+        case "todo":
+            format = input.split("todo ");
+            if (format.length <= 1) {
+                throw new InvalidDescriptionException("Invalid Task description");
+            }
+            task = new ToDo(format[1]);
+            break;
+        case "deadline":
+            format = input.split("deadline | /by ");
+            if (format.length <= 2) {
+                throw new InvalidDescriptionException("Invalid Task description");
+            }
+            task = new Deadline(format[1], format[2]);
+            break;
+        case "event":
+            format = input.split("event | /from | /to ");
+            if (format.length <= 3) {
+                throw new InvalidDescriptionException("Invalid Task description");
+            }
+            task = new Event(format[1], format[2], format[3]);
+            break;
         }
         return task;
     }
