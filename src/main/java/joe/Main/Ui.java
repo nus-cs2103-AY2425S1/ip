33c5df7a596c75ca02285bcb75726ce6c091ce6e
Ansel-Ch <achong2014@gmail.com>
2024-08-29 19:28:47 +0800

    Ui.java: Update methods to make use of Command classes

--- src/main/java/joe/Main/Ui.java
+++ src/main/java/joe/Main/Ui.java
@@ -1,5 +1,6 @@
 package joe.Main;
 
+import joe.commands.*;
 import joe.exceptions.InvalidCommandException;
 import joe.exceptions.InvalidIndexException;
 import joe.tasks.TaskList;
@@ -45,31 +46,35 @@ public class Ui {
     public void responseToCommand(String userCmd) {
         Parser parser = new Parser(tasks);
         boolean isExit = false;
-        while (!isExit) {
+        Command c = null;
+        while (c == null || !c.isBye()) {
+            System.out.println(line);
             switch (userCmd) {
             case "/help":
-                sendHelp();
+                c = new HelpCommand();
                 break;
             case "bye":
-                sayGoodbye();
-                isExit = true;
+                c = new ByeCommand();
                 break;
             case "list":
-                tasks.listTasks();
+                c = new ListCommand(tasks);
                 break;
             case "save":
-                tasks.saveTasks();
+                c = new SaveCommand(tasks);
                 break;
             default:
                 try {
-                    parser.parseCommand(userCmd);
+                    c = parser.parseCommand(userCmd);
                 } catch (IllegalArgumentException | InvalidIndexException | InvalidCommandException e) {
-                    System.out.println(line);
                     System.out.println(e.getMessage());
                     System.out.println(line);
+                    continue;
                 }
             }
-            if (!isExit) {
+            c.execute();
+            System.out.println(line);
+
+            if (!c.isBye()) {
                 userCmd = receiveCommand();
             }
         }
@@ -89,32 +94,6 @@ public class Ui {
     private static void showWelcome() {
         System.out.println(line);
         System.out.println("Hello! I'm Joe\nWhat can I do for you?\nType /help to see the list of available commands.");
-        System.out.println(line);
-    }
-
-    /**
-     * Prints a goodbye message.
-     */
-    private static void sayGoodbye() {
-        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
-    }
-
-    /**
-     * Prints a list of available commands.
-     */
-    private static void sendHelp() {
-        System.out.println(line);
-        System.out.println("bye: ends our interaction :-(");
-        System.out.println("deadline <description> /by <due date/time>: Creates a Deadline task");
-        System.out.println("delete <idx>: Deletes the task at your chosen index");
-        System.out.println("event <description> /from <start date/time> /to <end date/time>: Creates an Event task");
-        System.out.println("find <queryString>: Finds task with descriptions matching your query regex");
-        System.out.println("list: Displays your current tasks");
-        System.out.println("mark <idx>: Marks the task at your chosen index");
-        System.out.println("save : Saves all tasks in your current list to the database that will be automatically " +
-                "loaded during your next session");
-        System.out.println("todo <description>: Creates a ToDo task");
-        System.out.println("unmark <idx>: Unmarks the task at your chosen index");
         System.out.println(line);
     }
 }
