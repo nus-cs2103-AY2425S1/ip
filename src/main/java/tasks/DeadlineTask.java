0aa5b8374229a461c797bc465738eb4b887f2283
Teddayz <mervynneo02@gmail.com>
2024-08-30 01:04:31 +0800

    Add JavaDoc Header Messages

--- src/main/java/tasks/DeadlineTask.java
+++ src/main/java/tasks/DeadlineTask.java
@@ -5,23 +5,35 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class DeadlineTask extends Task {
-    private LocalDateTime deadline;
+    private final LocalDateTime deadline;
 
     public DeadlineTask(String description, boolean isDone, String deadline) {
         super(description, isDone);
         this.deadline = parseDeadline(deadline);
     }
-
+    /**
+     * Changes the time format and then converts into a string to initialise the DeadlineTask variables.
+     * @param deadline a string whose format is to be changed.
+     * @return A converted date and time to initialise the DeadlineTask variables.
+     * @throws DateTimeParseException for when there is a parsing error.
+     */
     private LocalDateTime parseDeadline(String deadline) throws DateTimeParseException {
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
         return LocalDateTime.parse(deadline, inputFormatter);
     }
 
+    /**
+     * A symbol denoting the task type.
+     * @return a string denoting the task type.
+     */
     @Override
     public String getTaskType() {
         return "D";
     }
-
+    /**
+     * Gets the description of the task.
+     * @return a string containing all the relevant information of the Task.
+     */
     @Override
     public String getDescription() {
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h:mm a");
@@ -30,6 +42,10 @@ public class DeadlineTask extends Task {
                 String.format("%s", this.deadline.format(outputFormatter));
     }
 
+    /**
+     * Description of the task to be parsed.
+     * @return a string to be parsed.
+     */
     @Override
     public String toString() {
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h:mm a");
--- src/main/java/tasks/EventTask.java
+++ src/main/java/tasks/EventTask.java
@@ -16,15 +16,28 @@ public class EventTask extends Task {
         this.end = parseTime(end);
     }
 
+    /**
+     * Changes the time format and then converts into a string to initialise the EventTask variables.
+     * @param time a string whose format is to be changed.
+     * @return A converted date and time to initialise the EventTask variables.
+     * @throws DateTimeParseException for when there is a parsing error.
+     */
     private LocalDateTime parseTime(String time) throws DateTimeParseException {
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
         return LocalDateTime.parse(time, inputFormatter);
     }
-
+    /**
+     * A symbol denoting the task type.
+     * @return a string denoting the task type.
+     */
     @Override
     public String getTaskType() {
         return "E";
     }
+    /**
+     * Gets the description of the task.
+     * @return a string containing all the relevant information of the Task.
+     */
     @Override
     public String getDescription() {
         return this.getTaskType() + " | " +
@@ -32,7 +45,10 @@ public class EventTask extends Task {
                 "| " + String.format("from %s to %s",
                 this.start.format(outputFormatter), this.end.format(outputFormatter));
     }
-
+    /**
+     * Description of the task to be parsed.
+     * @return a string to be parsed.
+     */
     @Override
     public String toString() {
         return String.format("%s | %s | %s | %s | %s",
