e7fbc9a306baa5bfa66e1c82d0c4deb3c69d3618
ngjianyi <ngjianyi2002@gmail.com>
2024-08-29 17:25:17 +0800

    Change Event class to store LocalDate object

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,22 @@
-public class Event extends Task {
-    private String from;
-    private String to;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    private LocalDate from;
+    private LocalDate to;
+
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
     public String formatString() {
-        return String.format("E | %s | %s | %s", super.formatString(), this.from, this.to);
+        return String.format("E | %s | %s | %s", super.formatString(), this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy")), this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 
     @Override
     public String toString() {
-        return String.format("[E] %s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E] %s (from: %s to: %s)", super.toString(), this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy")), this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
