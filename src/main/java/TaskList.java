94c29c3045a0e8f9ab2faadb4d7a3c282f14ab90
M-kew <e0958776@u.nus.edu>
2024-08-30 00:46:07 +0800

    Implement OOP by adding Parser, Storage, Ui and TaskList

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> inputList;
+
+    public TaskList(ArrayList<Task> inputList) {
+        this.inputList = inputList;
+    }
+
+    public void addTask(Task task) {
+        inputList.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        return inputList.remove(index);
+    }
+
+    public Task getTask(int index) {
+        return inputList.get(index);
+    }
+
+    public int getSize() {
+        return inputList.size();
+    }
+
+    public ArrayList<Task> getInputList() {
+        return inputList;
+    }
+}
