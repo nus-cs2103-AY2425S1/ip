8d3c7ede5c5a24518d6e48d72a391591d7994a30
Jing Hong Tee <e1122714@u.nus.edu>
2024-08-30 02:34:46 +0800

    Add Javadocs for task types

--- src/main/java/shrimp/task/Task.java
+++ src/main/java/shrimp/task/Task.java
@@ -1,32 +1,79 @@
 package shrimp.task;
 
+/**
+ * Represents an abstract task with a description and a completion status.
+ * Subclasses of {@code Task} must implement methods to mark the task as done or not done, and to return the type of the task.
+ */
 public abstract class Task {
-    private final String description;
-    private final Boolean isDone;
+    private final String description; //description of the task
+    private final Boolean isDone; //boolean value for whether it's done or not
 
+    /**
+     * Constructs a {@code Task} with the specified description and completion status.
+     *
+     * @param description The description of the task.
+     * @param isDone      The completion status of the task.
+     */
     Task(String description, Boolean isDone) {
         this.description = description;
         this.isDone = isDone;
     }
 
+    /**
+     * Retrieves a string representation of the task's completion status.
+     *
+     * @return "X" if the task is done, otherwise " ".
+     */
     private String getIsDone() {
         return (isDone ? "X" : " "); // mark done shrimp.task with X
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The task description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns whether the task is completed.
+     *
+     * @return {@code true} if the task is done, {@code false} otherwise.
+     */
     public Boolean isDone() {
         return isDone;
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return A string in the format "[status] description".
+     */
     @Override
     public String toString() {
         return "[" + getIsDone() + "] " + description;
     }
 
+    /**
+     * Marks this task as done and returns a new instance with the updated status.
+     *
+     * @return A new {@code Task} instance marked as done.
+     */
     public abstract Task markAsDone();
+
+    /**
+     * Marks this task as not done and returns a new instance with the updated status.
+     *
+     * @return A new {@code Task} instance marked as not done.
+     */
     public abstract Task markAsNotDone();
+
+    /**
+     * Returns the type of the task.
+     *
+     * @return A string representing the type of the task.
+     */
     public abstract String getType();
 }
--- src/main/java/shrimp/task/Todo.java
+++ src/main/java/shrimp/task/Todo.java
@@ -1,25 +1,56 @@
 package shrimp.task;
 
+/**
+ * Represents a simple task that does not have a specific deadline or event time.
+ * A {@code Todo} task has a description and a completion status.
+ */
 public class Todo extends Task {
 
+    /**
+     * Constructs a {@code Todo} task with the specified description and completion status.
+     *
+     * @param description The description of the task.
+     * @param isDone      The completion status of the task.
+     */
     public Todo(String description, boolean isDone) {
         super(description, isDone);
     }
 
+    /**
+     * Marks this task as done and returns a new {@code Todo} instance with the updated status.
+     *
+     * @return A new {@code Todo} instance marked as done.
+     */
     @Override
     public Todo markAsDone() {
         return new Todo(getDescription(), true);
     }
 
+    /**
+     * Marks this task as not done and returns a new {@code Todo} instance with the updated status.
+     *
+     * @return A new {@code Todo} instance marked as not done.
+     */
     @Override
     public Todo markAsNotDone() {
         return new Todo(getDescription(), false);
     }
 
+    /**
+     * Returns the type of the task.
+     *
+     * @return A string representing the type of the task, "[T]" for Todo.
+     */
+
     public String getType() {
         return "[T]";
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return A string in the format "[T][status] description".
+     */
     @Override
     public String toString() {
         return getType() + super.toString();
