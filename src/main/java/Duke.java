6d11112aa0d4a6812d6540fd677c2f5b5a7f4a63
shadhankkk <shashank.gorantla@gmail.com>
2024-08-30 01:54:14 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,46 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
 
-        String horizontalLine = "----------------------------------------------------------";
-
+    public static String horizontalLine = "----------------------------------------------------------";
+    public static void printOpening() {
         String openingText = "Hello! I'm Jeff\n " +
-                             "What can I do for you?";
-
-        String closingText = "Bye. Hope to see you again soon!";
+                "What can I do for you?";
 
         System.out.println(horizontalLine);
         System.out.println(openingText);
         System.out.println(horizontalLine);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(horizontalLine);
+        System.out.println(message);
+        System.out.println(horizontalLine);
+    }
+
+    public static void printClosing() {
+        String closingText = "Bye. Hope to see you again soon!";
+
+        System.out.println(horizontalLine);
         System.out.println(closingText);
         System.out.println(horizontalLine);
+    }
+    public static void main(String[] args) {
+
+        String exitCommand = "bye";
+        Scanner inputReader = new Scanner(System.in);
+
+        printOpening();
+
+        while(true)
+        {
+            String input = inputReader.nextLine();
+
+            if(input.equals(exitCommand)) {
+                printClosing();
+                break;
+            } else {
+                printMessage(input);
+            }
+        }
     }
 }
