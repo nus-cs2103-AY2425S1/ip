677509a1007a621f7468bae5771791b2321369ce
Vatinius <102.23.dylan.chan@gmail.com>
2024-08-30 00:00:41 +0800

    Implement test cases for all task types

--- src/test/java/evelyn/task/TaskTest.java
+++ src/test/java/evelyn/task/TaskTest.java
@@ -18,8 +18,26 @@ public class TaskTest {
     }
 
     @Test
+    public void uncheckedDeadlineTest() {
+        Deadline deadline = new Deadline("Test 3", "2020-03-11", false);
+        assertEquals("[D][ ] Test 3 (by: 2020-03-11)", deadline.toString());
+    }
+
+    @Test
+    public void checkedDeadlineTest() {
+        Deadline deadline = new Deadline("Test 4", "2020-03-11", true);
+        assertEquals("[D][X] Test 4 (by: 2020-03-11)", deadline.toString());
+    }
+
+    @Test
     public void uncheckedEventTest() {
-        Event event = new Event("Test 3", "2020-03-11", "2020-03-11", false);
-        assertEquals("[E][ ] Test 3 from");
+        Event event = new Event("Test 5", "2020-04-04", "2020-04-04", false);
+        assertEquals("[E][ ] Test 5 (from: 2020-04-04 to: 2020-04-04)", event.toString());
+    }
+
+    @Test
+    public void checkedEventTest() {
+        Event event = new Event("Test 6", "2020-04-04", "2020-04-04", true);
+        assertEquals("[E][X] Test 6 (from: 2020-04-04 to: 2020-04-04)", event.toString());
     }
 }
