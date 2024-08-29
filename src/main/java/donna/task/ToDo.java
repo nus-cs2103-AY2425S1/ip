909650d923bb8a97034234ee8fc62bc061cea596
tanwartushar <>
2024-08-30 02:09:01 +0800

    ToDo.java: Add Javadocs

--- src/main/java/donna/task/ToDo.java
+++ src/main/java/donna/task/ToDo.java
@@ -4,11 +4,23 @@ import donna.DonnaException;
 
 public class ToDo extends Task {
     private final String description;
+
+    /**
+     * Constructs a ToDo task with the specified description.
+     *
+     * @param description The description of the task.
+     * @throws DonnaException If the description is empty.
+     */
     public ToDo(String description) throws DonnaException {
         super(description);
         this.description = description;
     }
 
+    /**
+     * Returns a string representation of the to-do task in a format suitable for saving to a file.
+     *
+     * @return A string representing the to-do task in the format "T | done status | description".
+     */
     @Override
     public String toFileFormat() {
         return "T | " + (this.isDone() ? "1" : "0") + " | " + description;
