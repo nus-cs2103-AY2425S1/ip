77029eb6f0ece0d657398e787d102f18c888bb04
ngjianyi <ngjianyi2002@gmail.com>
2024-08-29 19:51:49 +0800

    Edit creation of folders and file in Storage constructor

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,6 +2,7 @@ import com.sun.security.jgss.GSSUtil;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
@@ -9,78 +10,86 @@ import java.util.Scanner;
 
 public class Storage {
     File file;
+    ArrayList<Task> tasks = new ArrayList<>();
+
     public Storage(String filePath) {
         File file = new File(filePath);
-        if (!file.exists()) {
-            file.mkdirs();
-            // file = new ...??
+        file.getParentFile().mkdirs();
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e);
         }
         this.file = file;
     }
 
     public ArrayList<Task> load() {
-        ArrayList<Task> tasks = new ArrayList<>();
         try {
             Scanner sc = new Scanner(this.file);
             while (sc.hasNext()) {
                 String line = sc.nextLine();
-                if (!line.matches("^[TDE] .+")) {
-                    throw new InvalidFormatException("Invalid task found in file");
-                }
-
-                String[] input = line.split("\\|", 3);
-                Dudu.TaskType type = Dudu.TaskType.valueOf(input[0].trim());
-                boolean marked = Integer.parseInt(input[1].trim()) == 1;
-                String content = input[2].trim();
-                Task task = null;
-                switch (type) {
-                    case T:
-                        if (!line.matches("^T \\| [01] \\| .+")) {
-                            throw new InvalidFormatException("Format should be T | [1/0] | [description]");
-                        }
-                        task = new ToDo(content);
-                        break;
-                    case D: {
-                        if (!line.matches("^D \\| [01] \\| .+ \\| \\d{4}-\\d{2}-\\d{2}")) {
-                            throw new InvalidFormatException("Format should be D | [1/0] | [description] | yyyy-mm-dd");
-                        }
-                        String[] tmp = content.split("\\|");
-                        String description = tmp[0].trim();
-                        try {
-                            LocalDate date = LocalDate.parse(tmp[1].trim());
-                            task = new Deadline(description, date);
-                        } catch (DateTimeParseException e) {
-                            System.out.println(e);
-                        }
-                        break;
-                    } case E: {
-                        if (!line.matches("^E \\| [01] \\| .+ \\| \\d{4}-\\d{2}-\\d{2} \\| \\d{4}-\\d{2}-\\d{2}")) {
-                            throw new InvalidFormatException("Format should be E | [1/0] | [description] | yyyy-mm-dd | yyyy-mm-dd");
-                        }
-                        String[] tmp = content.split("\\|");
-                        String description = tmp[0].trim();
-                        try {
-                            LocalDate from = LocalDate.parse(tmp[1].trim());
-                            LocalDate to = LocalDate.parse(tmp[2].trim());
-                            task = new Event(description, from, to);
-                        } catch (DateTimeParseException e) {
-                            System.out.println(e);
-                        }
-                        break;
-                    }
-                }
-                if (task != null) {
-                    if (marked) {
-                        task.markCompleted();
-                    }
-                    tasks.add(task);
+                try {
+                    addTask(line);
+                } catch (InvalidFormatException e) {
+                    System.out.println(e);
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
-        } catch (InvalidFormatException e) {
-            System.out.println(e);
         }
-        return tasks;
+        return this.tasks;
+    }
+
+    private void addTask(String line) throws InvalidFormatException {
+        if (!line.matches("^[TDE] .+")) {
+            throw new InvalidFormatException("Invalid task type found in file");
+        }
+        String[] input = line.split("\\|", 3);
+        Dudu.TaskType type = Dudu.TaskType.valueOf(input[0].trim());
+        boolean marked = Integer.parseInt(input[1].trim()) == 1;
+        String content = input[2].trim();
+        Task task = null;
+        switch (type) {
+            case T:
+                if (!line.matches("^T \\| [01] \\| .+")) {
+                    throw new InvalidFormatException("Format should be T | [1/0] | [description]");
+                }
+                task = new ToDo(content);
+                break;
+            case D: {
+                if (!line.matches("^D \\| [01] \\| .+ \\| \\d{4}-\\d{2}-\\d{2}")) {
+                    throw new InvalidFormatException("Format should be D | [1/0] | [description] | yyyy-mm-dd");
+                }
+                String[] tmp = content.split("\\|");
+                String description = tmp[0].trim();
+                try {
+                    LocalDate date = LocalDate.parse(tmp[1].trim());
+                    task = new Deadline(description, date);
+                } catch (DateTimeParseException e) {
+                    System.out.println(e);
+                }
+                break;
+            } case E: {
+                if (!line.matches("^E \\| [01] \\| .+ \\| \\d{4}-\\d{2}-\\d{2} \\| \\d{4}-\\d{2}-\\d{2}")) {
+                    throw new InvalidFormatException("Format should be E | [1/0] | [description] | yyyy-mm-dd | yyyy-mm-dd");
+                }
+                String[] tmp = content.split("\\|");
+                String description = tmp[0].trim();
+                try {
+                    LocalDate from = LocalDate.parse(tmp[1].trim());
+                    LocalDate to = LocalDate.parse(tmp[2].trim());
+                    task = new Event(description, from, to);
+                } catch (DateTimeParseException e) {
+                    System.out.println(e);
+                }
+                break;
+            }
+        }
+        if (task != null) {
+            if (marked) {
+                task.markCompleted();
+            }
+            this.tasks.add(task);
+        }
     }
 }
