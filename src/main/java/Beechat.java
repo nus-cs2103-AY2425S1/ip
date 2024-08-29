4fecdb376263292cae40137de28a5a6001a14cae
Bryan <e0958519@u.nus.edu>
2024-08-29 22:21:48 +0800

    Level 3 - Mark as Done

--- src/main/java/Beechat.java
+++ src/main/java/Beechat.java
@@ -6,8 +6,10 @@ public class Beechat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Beechat!\nWhat can I do for you?\n");
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         String msg = sc.nextLine();
+        Task task;
+        int j;
         while (!msg.equals("bye")) {
             if (msg.equals("list")) {
                 for (int i = 0; i < list.size(); i++) {
@@ -16,7 +18,22 @@ public class Beechat {
                 msg = sc.nextLine();
                 continue;
             }
-            list.add(msg);
+            task = new Task(msg);
+            if ((msg.split(" "))[0].equals("mark")) {
+                j = Integer.valueOf((msg.split(" "))[1]) - 1;
+                list.get(j).mark();
+                System.out.println("Nice! I've marked this task as done:\n" + list.get(j));
+                msg = sc.nextLine();
+                continue;
+            }
+            if ((msg.split(" "))[0].equals("unmark")) {
+                j = Integer.valueOf((msg.split(" "))[1]) - 1;
+                list.get(j).unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + list.get(j));
+                msg = sc.nextLine();
+                continue;
+            }
+            list.add(task);
             System.out.println("added: " + msg);
             msg = sc.nextLine();
         }
