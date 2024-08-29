ca0252d92c58576db801ba7dda40ed31af3569cf
estellelim <estlim@hotmail.com>
2024-08-30 01:52:23 +0800

    Add date format for Event task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + " to: " + to + ")";
+        String f = from.format(DateTimeFormatter.ofPattern("E, MMM d yyyy"));
+        String t = to.format(DateTimeFormatter.ofPattern("E, MMM d yyyy"));
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), f, t);
     }
 }
