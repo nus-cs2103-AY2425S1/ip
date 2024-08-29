0cb51f439eebc3228f20daedcd7e81d21072581e
kien <hoangkienvu11@gmail.com>
2024-08-29 12:22:12 +0800

    Handle some invalid format exceptions

--- src/main/java/ToMo.java
+++ src/main/java/ToMo.java
@@ -6,55 +6,100 @@ public class ToMo {
     public static void main(String[] args) {
         System.out.println("What's up, it's ToMo here!");
         Scanner sc = new Scanner(System.in);
-        List<Task> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<Task>();
 
         while (true) {
             System.out.println("How can I help you?");
             String cmd = sc.nextLine();
-            if (cmd.equals("bye")) {
-                break;
-            } else if (cmd.equals("list")) {
-                System.out.println("Your tasks list as follow:");
-                for (int i = 0; i < tasks.size(); ++i) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
-                }
-            } else {
+
+            try {
                 String[] words = cmd.split(" ");
-                if (words[0].equals("mark")) {
-                    int i = Integer.parseInt(words[1]) - 1;
-                    Task newTask = tasks.get(i);
-                    newTask.mark();
-                    tasks.set(i, newTask);
-                    System.out.println("Your task is marked");
-                    System.out.println(newTask);
-                } else if (words[0].equals("unmark")) {
-                    int i = Integer.parseInt(words[1]) - 1;
-                    Task newTask = tasks.get(i);
-                    newTask.unmark();
-                    tasks.set(i, newTask);
-                    System.out.println("Your task is unmarked");
-                    System.out.println(newTask);
-                } else {
-                    Task newTask;
-                    if (words[0].equals("todo")) {
-                        String description = cmd.substring(5); // remove "todo "
-                        newTask = new ToDo(description);
-                    } else if (words[0].equals("deadline")) {
-                        words = cmd.split("/");
-                        String description = words[0].substring(9); // remove "deadline "
-                        String deadline = words[1].substring(3); // remove "by "
-                        newTask = new Deadline(description, deadline);
-                    } else {
-                        words = cmd.split("/");
-                        String description = words[0].substring(6); // remove "event "
-                        String start = words[1].substring(5); // remove "from "
-                        String end = words[2].substring(3); // remove "to "
-                        newTask = new Event(description, start, end);
+                if (words.length == 0) continue;
+                if (words[0].equals("bye")) {
+                    if (words.length != 1) {
+                        throw new Exception("Wrong number of arguments in bye command: expected 1, got " + words.length);
                     }
-                    tasks.add(newTask);
+                    break;
+                } else if (words[0].equals("list")) {
+                    if (words.length != 1) {
+                        throw new Exception("Wrong number of arguments in list command: expected 1, got " + words.length);
+                    }
+
+                    System.out.println("Your tasks list as follow:");
+                    for (int i = 0; i < tasks.size(); ++i) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                } else if (words[0].equals("mark")) {
+                    if (words.length != 2) {
+                        throw new Exception("Wrong number of arguments in mark command: expected 2, got " + words.length);
+                    }
+                    int i = Integer.parseInt(words[1]) - 1;
+                    if (i >= 0 && i < tasks.size()) {
+                        Task newTask = tasks.get(i);
+                        newTask.mark();
+                        tasks.set(i, newTask);
+                        System.out.println("Your task is marked");
+                        System.out.println(tasks.get(i));
+                    } else {
+                        throw new Exception("Index out of range for command mark");
+                    }
+                } else if (words[0].equals("unmark")) {
+                    if (words.length != 2) {
+                        throw new Exception("Wrong number of arguments in unmark command: expected 2, got " + words.length);
+                    }
+                    int i = Integer.parseInt(words[1]) - 1;
+                    if (i >= 0 && i < tasks.size()) {
+                        Task newTask = tasks.get(i);
+                        newTask.unmark();
+                        tasks.set(i, newTask);
+                        System.out.println("Your task is unmarked");
+                        System.out.println(newTask);
+                    } else {
+                        throw new Exception("Index out of range for command mark");
+                    }
+                } else if (words[0].equals("todo")) {
+                    if (cmd.length() <= 5) {
+                        throw new Exception("Empty description for new ToDo task");
+                    }
+                    String description = cmd.substring(5);
+                    Task task = new ToDo(description);
+                    tasks.add(task);
                     System.out.println("A new task is added");
-                    System.out.println(newTask);
+                    System.out.println(task);
+                } else if (words[0].equals("deadline")) {
+                    words = cmd.split(" /by ");
+                    if (words.length != 2) {
+                        throw new Exception("Wrong number of arguments for new Deadline task: expected 2, got " + words.length);
+                    }
+                    if (words[0].length() <= 9) {
+                        throw new Exception("Empty description for Deadline task");
+                    }
+                    String description = words[0].substring(9);
+                    String deadline = words[1];
+                    Task task = new Deadline(description, deadline);
+                    tasks.add(task);
+                    System.out.println("A new task is added");
+                    System.out.println(task);
+                } else if (words[0].equals("event")) {
+                    words = cmd.split(" /from | /to ");
+                    if (words.length != 3) {
+                        throw new Exception("Wrong number of arguments for new Event task: expected 3, got " + words.length);
+                    }
+                    if (words[0].length() <= 6) {
+                        throw new Exception("Empty description for Event task");
+                    }
+                    String description = words[0].substring(6);
+                    String start = words[1];
+                    String end = words[2];
+                    Task task = new Event(description, start, end);
+                    tasks.add(task);
+                    System.out.println("A new task is added");
+                    System.out.println(task);
+                } else {
+                    throw new Exception("Unknown command");
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
         sc.close();
