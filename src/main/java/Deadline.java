087f38d934293c5e9796836b877b71ba42c8278a
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-08-29 10:55:52 +0800

    Add Javadoc comments into Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,25 @@
+/**
+ * Represents a Task with the addition of a <code>deadlineTime</code>
+ * that it should be finished by
+ */
 public class Deadline extends Task {
     private String deadlineTime;
 
 
+    /**
+     * Create a <code>Deadline</code> object
+     * @param deadlineDesc
+     * @param deadlineTime
+     */
     Deadline(String deadlineDesc, String deadlineTime) {
         super(deadlineDesc);
         this.deadlineTime = deadlineTime;
     }
 
+    /**
+     * Generates a string ready for printing as a description of a <code>Deadline</code>.
+     * This includes the <code>Deadline</code> description and deadline time.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + "(by:" + this.deadlineTime + ")";
