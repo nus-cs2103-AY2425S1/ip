43323a62824b28fe9438406c2f56940a310a5215
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 01:11:13 +0800

    Implement Enumerations.java

---
+++ src/main/java/Enumerations.java
@@ -0,0 +1,24 @@
+public enum Enumerations {
+    BYE("bye"),
+    SCHEDULE("schedule"),
+    MARK("mark"),
+    UNMARK("unmark"),
+    DELETE("delete"),
+    TASK("task");
+
+    private final String text;
+    private Enumerations(String s) {
+        text = s;
+    }
+
+    public static Enumerations convertString(String s) {
+        for (Enumerations e : Enumerations.values()) {
+            if (e.text.equalsIgnoreCase(s)) {
+                return e;
+            }
+        }
+
+        return Enumerations.TASK;
+    }
+
+}
