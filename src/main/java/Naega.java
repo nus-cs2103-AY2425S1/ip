1cbe875a74091a552dfcd8be50d2687aff5aac58
rynlwhh <e1121363.u.nus.edu>
2024-08-30 00:06:51 +0800

    Added Todo, Deadline, Event classes

--- src/main/java/Naega.java
+++ src/main/java/Naega.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Naega {
     public static void main(String[] args) {
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Naega");
         System.out.println(" What can I do for you?");
@@ -36,20 +37,48 @@ public class Naega {
                     System.out.println("   " + tasks.get(taskIndex));
                     System.out.println("____________________________________________________________");
                 } else if (userInput.startsWith("unmark ")) {
-                    int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
-                    tasks.get(taskIndex).markAsNotDone();
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" OK, I've marked this task as not done yet:");
-                    System.out.println("   " + tasks.get(taskIndex));
-                    System.out.println("____________________________________________________________");
-                } else {
-                    Task newTask = new Task(userInput);
+                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                tasks.get(taskIndex).markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks.get(taskIndex));
+                System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("todo ")) {
+                    String description = userInput.substring(5);
+                    Task newTask = new Todo(description);
                     tasks.add(newTask);
                     System.out.println("____________________________________________________________");
-                    System.out.println(" added: " + userInput);
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + newTask);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else if (userInput.startsWith("deadline ")) {
+                    String[] parts = userInput.substring(9).split(" /by ");
+                    Task newTask = new Deadline(parts[0], parts[1]);
+                    tasks.add(newTask);
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + newTask);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else if (userInput.startsWith("event ")) {
+                    String[] parts = userInput.substring(6).split(" /from ");
+                    String description = parts[0];
+                    String[] timeParts = parts[1].split(" /to ");
+                    Task newTask = new Event(description, timeParts[0], timeParts[1]);
+                    tasks.add(newTask);
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + newTask);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Oops! I don't know how to handle that command.");
                     System.out.println("____________________________________________________________");
                 }
             }
+
             scanner.close();
         }
 }
