df62088896c8f58994f535d6abbae690d4a032c4
Wu Zengfu <wuzengfu666@gmail.com>
2024-08-29 20:25:07 +0800

    Add JUnit tests for some methods in TaskList.java

---
+++ src/test/java/echobot/task/TaskStub.java
@@ -0,0 +1,38 @@
+package echobot.task;
+
+public class TaskStub extends Task {
+    private final String taskName;
+    private boolean isDone;
+
+    public TaskStub(boolean isDone, String taskName) {
+        super();
+        this.taskName = taskName;
+        this.isDone = isDone;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.isDone ? "X" : " ") + "] " + taskName;
+    }
+
+    @Override
+    public String save() {
+        return "| " + (isDone ? "1" : "0") + " | " + this.taskName;
+    }
+
+    public boolean equals(TaskStub task) {
+        return this.taskName.equals(task.taskName) && this.isDone == task.isDone;
+    }
+}
