fbaeee276f4686fb7cdc52acf829c757cdf7459d
jessica2828 <jessica.sylviana.js@gmail.com>
2024-08-30 00:01:15 +0800

    Refactor Command to be an abstract class

--- src/main/java/Bob/Command/Command.java
+++ src/main/java/Bob/Command/Command.java
@@ -7,6 +7,6 @@ import Bob.Storage.Storage;
 import Bob.Ui.Ui;
 
 public abstract class Command {
-    abstract void execute(ArrayList<Task> tasks, Storage storage, Ui ui) throws BobException;
+    public abstract void execute(ArrayList<Task> tasks, Storage storage, Ui ui) throws BobException;
 }
 
