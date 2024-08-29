399723dbe30433fb13448f7095a05bf982b4f6dc
Teddayz <mervynneo02@gmail.com>
2024-08-29 18:09:37 +0800

    Add Parser and Storage

---
+++ src/main/java/Parser.java
@@ -0,0 +1,24 @@
+public class Parser {
+
+    public Task parseDeadlineTask(String deadlineCommand) throws StringIndexOutOfBoundsException {
+        int byIndex = deadlineCommand.indexOf("/by");
+        String desc = deadlineCommand.substring(0, byIndex);
+        String deadline = deadlineCommand.substring(byIndex + 4);
+        return new DeadlineTask(desc, false, deadline);
+    }
+
+    public Task parseEventTask(String eventCommand) throws StringIndexOutOfBoundsException {
+        int fromIndex = eventCommand.indexOf("/from");
+        int toIndex = eventCommand.indexOf("/to");
+
+        String details = eventCommand.substring(0, fromIndex);
+        String start = eventCommand.substring(fromIndex + 6, toIndex - 1);
+        String end = eventCommand.substring(toIndex + 4);
+        return new EventTask(details, false, start, end);
+    }
+
+    public Task parseToDoTask(String toDoCommand) {
+        return new ToDoTask(toDoCommand, false);
+    }
+
+}
