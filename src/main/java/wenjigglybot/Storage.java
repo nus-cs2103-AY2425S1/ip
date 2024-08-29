facf94045b95e78e0c5303568802074332a70c2e
wenjebs <wenjieb2@gmail.com>
2024-08-29 19:24:28 +0800

    Write Javadocs for Parser, Storage, Task, Tasklist, Ui, Wenjigglybot

--- src/main/java/wenjigglybot/Storage.java
+++ src/main/java/wenjigglybot/Storage.java
@@ -9,11 +9,22 @@ import java.io.IOException;
 public class Storage {
     String filePath;
 
+    /**
+     * Constructs a {@link Storage} object with the specified file path.
+     *
+     * @param filePath The path to the file where tasks will be stored.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
-
     }
 
+    /**
+     * Loads tasks from the file into the provided {@link TaskList}.
+     * If the file does not exist, it prints a message and returns.
+     *
+     * @param tasks The {@link TaskList} to load tasks into.
+     * @throws WenJigglyBotException If an error occurs while reading the file.
+     */
     public void load(TaskList tasks) throws WenJigglyBotException {
         File file = new File(filePath);
 
@@ -37,6 +48,12 @@ public class Storage {
         }
     }
 
+    /**
+     * Saves the tasks from the provided {@link TaskList} to a file.
+     * Creates the directory if it does not exist.
+     *
+     * @param tasks The {@link TaskList} containing tasks to be saved.
+     */
     public static void saveTasksToFile(TaskList tasks) {
         String tasksString = tasksToString(tasks);
         // Create a File object for the directory
@@ -44,7 +61,7 @@ public class Storage {
 
         // If the directory doesn't exist, create it
         if (!directory.exists()) {
-            boolean mkdirs = directory.mkdirs();// This will create the directory and any necessary but nonexistent parent directories
+            boolean mkdirs = directory.mkdirs(); // This will create the directory and any necessary but nonexistent parent directories
             if (!mkdirs) {
                 System.out.println("Failed to make directory!");
             }
@@ -57,6 +74,12 @@ public class Storage {
         }
     }
 
+    /**
+     * Converts the {@link TaskList} into a string representation suitable for saving to a file.
+     *
+     * @param tasks The {@link TaskList} to be converted.
+     * @return A string representation of the tasks.
+     */
     private static String tasksToString(TaskList tasks) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
@@ -64,6 +87,4 @@ public class Storage {
         }
         return sb.toString();
     }
-
-
 }
--- src/main/java/wenjigglybot/TaskList.java
+++ src/main/java/wenjigglybot/TaskList.java
@@ -6,22 +6,48 @@ import java.util.List;
 public class TaskList {
     static List<Task> tasks = new ArrayList<>(100);
 
+    /**
+     * Retrieves the list of tasks.
+     *
+     * @return A {@link List} of {@link Task} objects.
+     */
     public List<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Adds a new task to the task list.
+     *
+     * @param task The {@link Task} to be added.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Returns the number of tasks in the task list.
+     *
+     * @return The size of the task list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     *
+     * @param i The index of the task to retrieve.
+     * @return The {@link Task} at the specified index.
+     */
     public Task get(int i) {
         return tasks.get(i);
     }
 
+    /**
+     * Removes the task at the specified index from the task list.
+     *
+     * @param i The index of the task to remove.
+     */
     public void remove(int i) {
         tasks.remove(i);
     }
