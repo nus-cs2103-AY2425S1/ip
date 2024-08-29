ca25728d6d4cbeb13b5dc5beff098c0c3a75ac2f
josh1248 <joshthoo@gmail.com>
2024-08-29 22:07:10 +0800

    Grok.java: Increase robustness against non-valid user input

--- src/main/java/grok/Grok.java
+++ src/main/java/grok/Grok.java
@@ -30,6 +30,9 @@ public class Grok {
     public static void main(String[] args) {
         ArrayList<Task> tasks = TextFileReader.parseFile(TEXT_FILE_DIRECTORY);
 
+        // If data changes, prompt a save to text file
+        boolean hasUnsavedChanges = false;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println(padMessage("Hello! I'm Grok.Grok\nWhat ya wanna do to grok your way to success?"));
         while (true) {
@@ -75,8 +78,10 @@ public class Grok {
                     continue;
                 }
 
+
                 Task task = tasks.get(taskIndex - 1);
                 task.markUndone();
+                hasUnsavedChanges = true;
                 System.out.println(padMessage("Ok, I've marked this task as not done yet:\n  " + task));
             } else if (userInput.contains("mark")) {
                 if (userInput.length() < 6) {
@@ -99,6 +104,7 @@ public class Grok {
 
                 Task task = tasks.get(taskIndex - 1);
                 task.markDone();
+                hasUnsavedChanges = true;
                 System.out.println(padMessage("Nice! I've marked this task as done:\n  " + task));
             } else if (userInput.contains("todo")) {
                 if (userInput.length() < 6) {
@@ -114,16 +120,22 @@ public class Grok {
                 }
 
                 tasks.add(newTask);
+                hasUnsavedChanges = true;
                 System.out.println(padMessage(addTaskMessage(newTask, tasks)));
             } else if (userInput.contains("deadline")) {
+                String deadlineUsage = padMessage("Deadline command usage: deadline (task description here) " +
+                        "/by (due date and time)");
                 if (userInput.length() < 10 || !userInput.contains("/by")) {
-                    System.out.println(padMessage(
-                        "Deadline command usage: deadline (task description here) /by (due date and time)"
-                    ));
+                    System.out.println(padMessage(deadlineUsage));
                     continue;
                 }
 
                 String[] components = userInput.split("/by");
+
+                if (components.length != 2) {
+                    System.out.println(padMessage(deadlineUsage));
+                    continue;
+                }
                 String description = components[0].substring(9);
                 String due = components[1];
 
@@ -136,17 +148,28 @@ public class Grok {
                 }
 
                 tasks.add(newTask);
+                hasUnsavedChanges = true;
                 System.out.println(padMessage(addTaskMessage(newTask, tasks)));
             } else if (userInput.contains("event")) {
+                String eventUsage = padMessage("Event command usage: event (task description here) " +
+                        "/from (start date and time) /to (end date and time)");
                 if (userInput.length() < 7 || !userInput.contains("/from") || !userInput.contains("/to")) {
-                    System.out.println(padMessage(
-                            "Event command usage: event (task description here) /from (start date and time) /to (end date and time)"
-                    ));
+                    System.out.println(padMessage(eventUsage));
                     continue;
                 }
 
                 String[] components = userInput.split("/from");
+                if (components.length != 2) {
+                    System.out.println(padMessage(eventUsage));
+                    continue;
+                }
+
                 String[] subcomponents = components[1].split("/to");
+                if (subcomponents.length != 2) {
+                    System.out.println(padMessage(eventUsage));
+                    continue;
+                }
+
                 String description = components[0];
                 String from = subcomponents[0];
                 String to = subcomponents[1];
@@ -160,6 +183,7 @@ public class Grok {
                 }
 
                 tasks.add(newTask);
+                hasUnsavedChanges = true;
                 System.out.println(padMessage(addTaskMessage(newTask, tasks)));
             } else if (userInput.contains("delete")) {
                 if (userInput.length() < 8) {
@@ -183,6 +207,7 @@ public class Grok {
                 }
 
                 Task task = tasks.remove(taskIndex - 1);
+                hasUnsavedChanges = true;
                 System.out.println(padMessage(
                         "Noted. I've removed this task:\n  "
                                 + task
@@ -194,6 +219,10 @@ public class Grok {
                                 "Available commands are: " +
                                 "bye, list, mark, unmark, todo, deadline, event, delete"
                 ));
+            }
+
+            if (hasUnsavedChanges) {
+                TextFileReader.writeToFile(TEXT_FILE_DIRECTORY);
             }
         }
 
