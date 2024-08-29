10d38964e0c978f1e2588b883ff2448380a32374
Rachael-Chan <e0775723@u.nus.edu>
2024-08-30 01:04:10 +0800

    feat: Tasks added to hard disk

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,11 @@ public class Event extends Task{
     }
 
     @Override
+    public String toTextFormat() {
+        return "E|" + (super.isTaskDone() ? "T" : "F") + "|" + super.getDescription() + "|" + this.from + "|" + this.to;
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
