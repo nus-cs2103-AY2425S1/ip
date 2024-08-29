c18c560a6fe001b1d880c215c8cf2ce8c3000d01
prave1n <pravein2002@gmail.com>
2024-08-29 15:45:50 +0800

    Test new branch

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,15 @@
 public class Deadline extends Task{
-    private String due;
+    private String dueBy;
 
     public Deadline(String str, String due) {
         super(str);
-        this.due = due.trim();
+        this.dueBy = due.trim();
     }
 
     @Override
     public String toString() {
         String str = this.isDone ? "[D][X] " : "[D][ ] ";
-        str += this.description.trim() + " (by: " + this.due + ")";
+        str += this.description.trim() + " (by: " + this.dueBy + ")";
         return str;
     }
 }
