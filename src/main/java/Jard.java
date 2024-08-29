f2831a0153fed7a0b372eacb3c1f33c2ecaa5550
WuJinhan1 <e1075517@u.nus.edu>
2024-08-29 19:18:49 +0800

    Add A-MoreOOP

---
+++ src/main/java/Jard.java
@@ -0,0 +1,63 @@
+import java.util.List;
+
+public class Jard {
+    private Storage storage;
+    private List<Task> tasks;
+    private Ui ui;
+
+    public Jard(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        tasks = storage.loadTasks();
+    }
+
+    public static class JardException extends RuntimeException {
+        public JardException(String message) {
+            super(message);
+        }
+    }
+
+    public void run() {
+        ui.showWelcome();
+        while (true) {
+            String input = ui.readCommand();
+            String[] inputParts = Parser.parseCommand(input);
+            String command = inputParts[0];
+
+            try {
+                if (command.equals("bye")) {
+                    ui.showBye();
+                    break;
+                } else if (command.equals("list")) {
+                    ui.showTaskList(tasks);
+                } else if (command.equals("mark")) {
+                    int taskIndex = Parser.parseTaskIndex(inputParts[1], tasks.size());
+                    Task task = tasks.get(taskIndex);
+                    task.markAsDone();
+                    ui.showMarkTask(task);
+                } else if (command.equals("unmark")) {
+                    int taskIndex = Parser.parseTaskIndex(inputParts[1], tasks.size());
+                    Task task = tasks.get(taskIndex);
+                    task.markAsNotDone();
+                    ui.showUnmarkTask(task);
+                } else if (command.equals("delete")) {
+                    int taskIndex = Parser.parseTaskIndex(inputParts[1], tasks.size());
+                    Task task = tasks.remove(taskIndex);
+                    ui.showDeleteTask(task, tasks.size());
+                } else {
+                    Task task = Parser.parseTask(input);
+                    tasks.add(task);
+                    ui.showAddTask(task, tasks.size());
+                }
+                storage.saveTasks(tasks);
+            } catch (Jard.JardException e) {
+                ui.showError(e.getMessage());
+            }
+        }
+        ui.close();
+    }
+
+    public static void main(String[] args) {
+        new Jard("./data/jard.txt").run();
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.List;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Jard.");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showBye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showTaskList(List<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("Nothing in the list!");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+            }
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    public void showAddTask(Task task, int size) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Got it. I've added this task:");
+        System.out.println("   " + task);
+        System.out.println("Now you have " + size + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showMarkTask(Task task) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("   " + task);
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showUnmarkTask(Task task) {
+        System.out.println("____________________________________________________________");
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("   " + task);
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showDeleteTask(Task task, int size) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Noted. I've removed this task:");
+        System.out.println("   " + task);
+        System.out.println(" Now you have " + size + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showError(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Error: Jard! " + message);
+        System.out.println("____________________________________________________________");
+    }
+
+    public String readCommand() {
+        return scanner.nextLine().trim();
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
