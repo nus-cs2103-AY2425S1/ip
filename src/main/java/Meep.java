a2dac98e20d1fd643aa50d081e707b4e9d2307be
tata32000 <tata32000@hotmail.com>
2024-08-29 23:37:21 +0800

    Refactor Main method to use more OOP

--- src/main/java/Meep.java
+++ src/main/java/Meep.java
@@ -1,17 +1,41 @@
 import java.util.Scanner;
 
 public class Meep {
-    public static void main(String[] args) {
-        FormattedPrint.greeting();
+    private final Storage storage;
+    private TaskList taskList;
+    private final Ui ui;
+    private final Parser parser;
+
+    public Meep(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        parser = new Parser();
+        try {
+            taskList = new TaskList(storage.loadTasks());
+        } catch (MeepException e) {
+            ui.errorLoadingTask();
+            taskList = new TaskList();
+        }
+    }
+
+    public void run() {
+        ui.greeting();
         Scanner scanner = new Scanner(System.in);
-        TaskList taskList = FileUtils.loadTasks();
         boolean isDone = false;
         // keep scanning for user input
         while (!isDone) {
-            FormattedPrint.inputWaiting();
-            String input = scanner.nextLine();
-            isDone = Parser.checkCommand(input, taskList);
-            FileUtils.saveTasks(taskList);
+            try {
+                ui.inputWaiting();
+                String input = scanner.nextLine();
+                isDone = this.parser.checkCommand(input, taskList);
+                storage.saveTasks(taskList);
+            } catch (MeepException e) {
+                ui.error();
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        new Meep("data.txt").run();
     }
 }
