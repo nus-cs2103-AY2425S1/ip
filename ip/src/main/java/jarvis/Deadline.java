0c24c188bfb016f57838e3df1ee00d7b204d15da
minhao <heminhao120@gmail.com>
2024-08-29 18:21:04 +0800

    Level 8

--- ip/src/main/java/jarvis/Deadline.java
+++ ip/src/main/java/jarvis/Deadline.java
@@ -1,15 +1,29 @@
 package jarvis;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class Deadline extends Task {
+
+    LocalDate dateDeadline;
     String deadline;
     public Deadline(String name, String deadline){
         super(name);
-        this.deadline = deadline;
+        try {
+            this.dateDeadline = LocalDate.parse(deadline);
+        } catch(DateTimeParseException e){
+            this.deadline = deadline;
+        }
+    }
+
+    public String getDeadline() {
+        return deadline == null
+                ? dateDeadline.toString()
+                : deadline;
     }
     @Override
     public String toString(){
         String checkbox = this.done ? "[X]" : "[ ]";
-        String result = String.format(" %s %s\n", checkbox, this.name);
+        String result = String.format(" %s %s %s\n", checkbox, this.name, this.getDeadline());
         return "[D]" + result;
     }
 }
--- ip/src/main/java/jarvis/Event.java
+++ ip/src/main/java/jarvis/Event.java
@@ -1,16 +1,41 @@
 package jarvis;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class Event extends Task {
+
     String start;
     String end;
+    LocalDate dateStart;
+    LocalDate dateEnd;
     public Event(String name, String start, String end){
         super(name);
-        this.start = start;
-        this.end = end;
+        try {
+            this.dateStart = LocalDate.parse(start);
+        } catch(DateTimeParseException e) {
+            this.start = start;
+        }
+        try {
+            this.dateEnd = LocalDate.parse(end);
+        } catch(DateTimeParseException e) {
+            this.end = end;
+        }
+    }
+
+    public String getStart() {
+        return start == null
+                ? dateStart.toString()
+                : start;
+    }
+    public String getEnd() {
+        return end == null
+                ? dateEnd.toString()
+                : end;
     }
     public String toString(){
         String checkbox = this.done ? "[X]" : "[ ]";
-        String result = String.format(" %s %s\n", checkbox, this.name);
+        String result = String.format(" %s %s  from: %s, to %s\n",
+                checkbox, this.name, this.getStart(), this.getEnd());
         return "[E]" + result;
     }
 }
