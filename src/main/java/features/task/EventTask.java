b1ca2c09621b9eb0da81b86c56b2f300dc2f58b2
raihahahan <mraihandev@gmail.com>
2024-08-30 03:39:31 +0800

    added Date handling

--- src/main/java/features/task/EventTask.java
+++ src/main/java/features/task/EventTask.java
@@ -1,25 +1,33 @@
 package features.task;
 
-public class EventTask extends Task {
-	private String startAt;
-	private String endAt;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-	public EventTask(String name, String startAt, String endAt) {
+public class EventTask extends Task {
+	private LocalDateTime startAt;
+	private LocalDateTime endAt;
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+	public EventTask(String name, String startAt, String endAt) throws Exception {
 		super(name);
-		this.startAt = startAt;
-		this.endAt = endAt;
+		this.startAt = LocalDateTime.parse(startAt, formatter);
+		this.endAt = LocalDateTime.parse(endAt, formatter);
+		if (this.startAt.isAfter(this.endAt)) {
+			throw new Exception("Start date must be before end date.");
+		}
 	}
 
 	@Override
 	public String toString() {
-		return "[E]" + super.toString() + " (from: " + this.startAt + " to: " + this.endAt + ")";
+		return "[E]" + super.toString() + " (from: " + this.startAt.toString() + " to: " + this.endAt.toString() + ")";
 	}
 
 	public String getStartAt() {
-		return this.startAt;
+		return this.startAt.toString();
 	}
 
 	public String getEndAt() {
-		return this.endAt;
+		return this.endAt.toString();
 	}
 }
