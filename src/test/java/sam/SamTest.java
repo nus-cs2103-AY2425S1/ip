88c1d22ed338ce609c44c93eaee8afe296a9545f
Varun Shukla <vslpahm@gmail.com>
2024-08-30 01:33:52 +0800

    Add javadoc for test case

--- src/test/java/sam/SamTest.java
+++ src/test/java/sam/SamTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class SamTest {
+    /**
+     * Test case for the markItemDone method in the Item class.
+     * It verifies that the status icon of an item is correctly updated to "X" when marked as done.
+     */
     @Test
     public void markItemDone() {
         Item item = new Item("task");
