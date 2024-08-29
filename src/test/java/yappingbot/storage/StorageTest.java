6dbbd433e2f87d20504a29a8080699001597dbee
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-29 23:00:56 +0800

    Generate jUnit test files and Resource (savefile) for storage

---
+++ src/test/java/yappingbot/storage/StorageTest.java
@@ -0,0 +1,15 @@
+package yappingbot.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StorageTest {
+
+    @Test
+    void loadListFromFile() {
+    }
+
+    @Test
+    void saveListToFile() {
+    }
+}
