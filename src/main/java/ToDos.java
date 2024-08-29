e67a490e05ffd90a7e660ace5778ec271797fdec
IanCheah <iancheah24@gmail.com>
2024-08-30 02:23:59 +0800

    Update the constructor of ToDos class

--- src/main/java/ToDos.java
+++ src/main/java/ToDos.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class ToDos extends Task{
     // essentially a Task without time
     // add into ArrayList, and then print the statements
-    public ToDos(String description) throws InvalidSyntaxException, EmptyDescriptionException{
+    public ToDos(String description) throws CommandFoundButInvalidException {
         super(description);
         // check if description is valid here
         // if empty description is empty we throw
