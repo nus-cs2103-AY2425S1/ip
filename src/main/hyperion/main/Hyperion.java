dec0b1b9706c5398389178a840590c3cf101c2af
IanCheah <iancheah24@gmail.com>
2024-08-30 02:42:56 +0800

    Create main package

---
+++ src/main/hyperion/main/Hyperion.java
@@ -0,0 +1,48 @@
+package main;
+
+import exception.CommandFoundButInvalidException;
+import exception.CommandNotFoundException;
+import exception.EmptyStringException;
+
+import java.util.Scanner;
+
+public class Hyperion {
+    private Storage storage;
+    private TaskList allTasks;
+    private Ui ui;
+    boolean carryOn = true;
+    // takes in a filepath for the storage
+    public Hyperion(String filePath) {
+        try {
+            this.ui = new Ui();
+            this.ui.showWelcome();
+            this.storage = new Storage(filePath);
+            this.allTasks = new TaskList(storage.load());
+            this.ui.showList(this.allTasks.getAllTasks());
+        } catch (CommandFoundButInvalidException e ) {
+            System.out.print("There is an error" + e.getMessage());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while(carryOn) {
+            System.out.println("> ");
+            input = scanner.nextLine();
+
+            try {
+                Parser parser = new Parser(input, allTasks, this.storage, this.ui);
+                carryOn = parser.carryOn();
+            } catch (EmptyStringException |
+                     CommandFoundButInvalidException |
+                     CommandNotFoundException e) {
+                ui.displayError(e.getMessage());
+            }
+        }
+
+        scanner.close();
+    }
+    public static void main(String[] args) {
+        new Hyperion("data/tasks.txt");
+    }
+}
