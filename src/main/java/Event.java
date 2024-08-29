ca7e3f2236d551e5c38f79125bb97767b2c7de7e
arnav-goel10 <e1130141@u.nus.edu>
2024-08-29 23:54:57 +0800

    Implement Date in Event and Deadline

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,34 +1,35 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class Event extends Task {
 
-    protected String fromTime;
-    protected String toTime;
+    protected LocalDateTime fromTime;
+    protected LocalDateTime toTime;
 
     public Event(String description, String fromTime, String toTime, boolean isDone) {
         super(description.trim(), isDone);
-        this.fromTime = fromTime.trim();
-        this.toTime = toTime.trim();
+        this.fromTime = LocalDateTime.parse(fromTime, formatter);
+        this.toTime = LocalDateTime.parse(toTime, formatter);
     }
 
     public Event(String description, String fromTime, String toTime) {
         super(description.trim());
-        this.fromTime = fromTime.trim();
-        this.toTime = toTime.trim();
+        this.fromTime = LocalDateTime.parse(fromTime, formatter);
+        this.toTime = LocalDateTime.parse(toTime, formatter);
         this.saveToFile();
     }
 
     @Override
     public String toString() {
-        return "[E]" + (isDone ? "[X] " : "[ ] ") + this.description + " (from:" + this.fromTime + " to:" + this.toTime + ")";
+        return "[E]" + (isDone ? "[X] " : "[ ] ") + this.description + " (from:" + this.fromTime.format(formatter) + " to:" + this.toTime.format(formatter) + ")";
     }
 
     @Override
     public void saveToFile() {
         try {
             FileWriter fw = new FileWriter(FILE_PATH, true);
-            fw.write("E | " + (isDone ? "1" : "0") + " | " + this.description + " | " + this.fromTime + " | " + this.toTime + "\n");
+            fw.write("E | " + (isDone ? "1" : "0") + " | " + this.description + " | " + this.fromTime.format(formatter) + " | " + this.toTime.format(formatter) + "\n");
             fw.close();
         } catch (IOException e) {
             System.out.println("Something went wrong: "+ e.getMessage());
