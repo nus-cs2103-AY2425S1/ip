710f9d1aeeebf5d7e8ee27b4551afe710c4bc67e
shadhankkk <shashank.gorantla@gmail.com>
2024-08-30 02:34:39 +0800

    Completed Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,57 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Duke {
 
-    public static String horizontalLine = "----------------------------------------------------------";
+    public static class Task {
 
-    public static ArrayList<String> addedMessages = new ArrayList<String>();
+        Task(String taskName) {
+            this.taskName = taskName;
+            this.marked = false;
+        }
+
+        public void mark() {
+            this.marked = true;
+        }
+
+        public void unmark() {
+            this.marked = false;
+        }
+
+        public void print() {
+            String message = "";
+
+            if(this.marked) {
+                message += "[X] ";
+            } else {
+                message += "[ ] ";
+            }
+
+            message += taskName;
+
+            System.out.println(message);
+        }
+
+        public void print(int rank) {
+            String message = rank + ".";
+
+            if(this.marked) {
+                message += "[X] ";
+            } else {
+                message += "[ ] ";
+            }
+
+            message += taskName;
+
+            System.out.println(message);
+        }
+        private String taskName;
+        private boolean marked;
+    }
+    public static String horizontalLine = "----------------------------------------------------------";
+    public static String defaultProperties = "0";
+    public static ArrayList<Task> tasks = new ArrayList<Task>();
     public static void printOpening() {
         String openingText = "Hello! I'm Jeff\n " +
                 "What can I do for you?";
@@ -20,6 +67,10 @@ public class Duke {
         System.out.println(horizontalLine);
     }
 
+    public static void printMessageWithProperties(int rank) {
+
+    }
+
     public static void printClosing() {
         String closingText = "Bye. Hope to see you again soon!";
 
@@ -29,20 +80,45 @@ public class Duke {
     }
 
     public static void addMessage(String message) {
+        tasks.add(new Task(message));
         printMessage("added: " + message);
-        addedMessages.add(message);
     }
 
     public static void listMessages()
     {
-        int numberOfMessages = addedMessages.size();
+        int numberOfMessages = tasks.size();
 
         System.out.println(horizontalLine);
+
         for(int i=0; i<numberOfMessages; i++) {
-            System.out.println((i+1) + ". " + addedMessages.get(i));
+            tasks.get(i).print(i+1);
         }
+
         System.out.println(horizontalLine);
     }
+
+    public static void markMessage(int rank) {
+        Task task = tasks.get(rank - 1);
+
+        task.mark();
+
+        System.out.println(horizontalLine);
+        System.out.println("Nice! I've marked this task as done:");
+        task.print();
+        System.out.println(horizontalLine);
+    }
+
+    public static void unmarkMessage(int rank) {
+        Task task = tasks.get(rank - 1);
+
+        task.unmark();
+
+        System.out.println(horizontalLine);
+        System.out.println("OK, I've marked this task as not done yet:");
+        task.print();
+        System.out.println(horizontalLine);
+    }
+
     public static void main(String[] args) {
 
         String exitCommand = "bye";
@@ -61,6 +137,12 @@ public class Duke {
                 break;
             } else if(input.equals("list")) {
                 listMessages();
+            } else if(input.length() >= 4 && input.substring(0,4).equals("mark")) {
+                int rankToMark = Integer.valueOf(input.substring(5));
+                markMessage(rankToMark);
+            } else if(input.length() >= 6 && input.substring(0,6).equals("unmark")) {
+                int rankToUnmark = Integer.valueOf(input.substring(7));
+                unmarkMessage(rankToUnmark);
             } else {
                 addMessage(input);
             }
