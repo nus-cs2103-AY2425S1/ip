66b26ecc31325678cce8ca92ad1f3f1396bf2479
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-30 02:14:47 +0800

    Add saveToFile method for Storage class

--- src/main/java/util/Storage.java
+++ src/main/java/util/Storage.java
@@ -1,13 +1,15 @@
 package main.java.util;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class Storage {
+    private static final Path FILEPATH = Paths.get("/main/java/data/Karen.txt");
     private Storage() {
         return;
     }
@@ -18,14 +20,19 @@ public class Storage {
      */
     public static void initFile() {
         try {
-            Path filePath = Paths.get("/main/java/data/Karen.txt");
-
-            if (!Files.exists(filePath)) {
-                Files.createFile(filePath);
+            if (!Files.exists(FILEPATH)) {
+                Files.createFile(FILEPATH);
             }
         } catch (IOException e) {
             System.out.println("Error occured when opening the file");
         }
+    }
 
+    public static void saveToFile(ArrayList<String> textToSave) {
+        try {
+            Files.write(FILEPATH, textToSave, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println("Error while saving to file");
+        }
     }
 }
