87f301c50d701229aa76f2cfc08e04f750c6d7ef
Shruthiiii03 <muralishruthi222@gmail.com>
2024-08-29 22:29:11 +0800

    Completed OOP

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class TaskList {
+
+    private static ArrayList<Task> list;
+
+    public TaskList(ArrayList<Task> tasks) {
+        list = tasks;
+    }
+
+    public static ArrayList<Task> getList() {
+        return list;
+    }
+
+    public static void addTask(Task task){
+        // add task to the List
+        // return a string
+        list.add(task);
+        //return "added: " + task.getDescription();
+    }
+
+    public static void delTask(int num){
+        // add task to the List
+        // return a string
+        if(num >= 0 && num < list.size()) {
+            list.remove(num);
+        }
+        //return "added: " + task.getDescription();
+    }
+
+    public Task getTask(int index) {
+        return list.get(index);
+    }
+
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Ui {
+    public static String listDisplay(ArrayList<Task> list) {
+        int length = list.size();
+        // use String Builder to ensure that the string can be created on another line
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < length; i++) {
+            sb.append(i + 1 + ". " + list.get(i).printTask()).append("\n");
+        }
+
+        return "Here are the tasks in your list:\n" + sb.toString();
+    }
+
+    public static String taskAddDescription(Task task) {
+        return "Got it. I've added this task:\n" + "  " + task.printTask() + "\n" +
+                "Now you have " + TaskList.getList().size() + " tasks in the list";
+    }
+
+    public static String taskDelDescription(int num, Task task){
+        TaskList.getList().remove(num - 1);
+        return "Noted. I've removed this task:\n" + "  " + task.printTask() + "\n" +
+                "Now you have " + TaskList.getList().size() + " tasks in the list";
+    }
+
+    public void terminationMessage() {
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+
+}
