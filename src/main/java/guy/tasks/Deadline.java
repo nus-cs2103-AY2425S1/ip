257c94b226f7ccaffec3c462e3b1f5ecf9b1cf11
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-08-29 14:37:07 +0800

    Add support for searching for tasks using keywords

--- src/main/java/guy/tasks/Deadline.java
+++ src/main/java/guy/tasks/Deadline.java
@@ -24,11 +24,11 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         return "[D] " + super.toString() + " (by: "
-                + this.by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + ")";
+                + by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + ")";
     }
 
     @Override
     public String saveFormat() {
-        return "D" + super.saveFormat() + " | " + this.by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        return "D" + super.saveFormat() + " | " + by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 }
