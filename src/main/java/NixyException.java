7f07a5d74e868f73aca2d0e246faa74db46793a2
Jabez Tho <jabeztho@gmail.com>
2024-08-29 23:09:09 +0800

    Add custom NixyException
    Change Parent Class of TaskNotExistException
    
    To be consistent with the new parent class NixyException

---
+++ src/main/java/NixyException.java
@@ -0,0 +1,9 @@
+/**
+ * NixyException is a runtime exception
+ * thrown when an error occurs in Nixy during runtime.
+ */
+public class NixyException extends RuntimeException {
+    public NixyException(String message) {
+        super(message);
+    }
+}
