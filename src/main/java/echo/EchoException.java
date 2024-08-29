8bfe1cc4cd470799ee04f7de8dff189ffa431b78
kellywsq03 <kellywsq03@gmail.com>
2024-08-29 23:54:43 +0800

    Add JavaDoc hbeaders to Echo, EchoException and TaskType

--- src/main/java/echo/EchoException.java
+++ src/main/java/echo/EchoException.java
@@ -1,6 +1,12 @@
 package echo;
-
+/**
+ * The EchoException class is a custom exception that is thrown when
+ * an error specific to the Echo application occurs.
+ */
 public class EchoException extends Exception {
+    /**
+     * Constructs a new EchoException with no detailed message.
+     */
     public EchoException() {
         super();
     }
