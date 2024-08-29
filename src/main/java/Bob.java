6020d12abf03895e4ce828499d5ae7878df75a72
CreditableX <creditablecreates@gmail.com>
2024-08-29 13:42:31 +0800

    OOP refactor

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,180 +1,79 @@
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
-import java.time.*;
 import exceptions.*;
 
 public class Bob {
-    
-    public static void main(String[] args) throws CommandNotFoundException, MissingParamsException, PositionException {
+    Storage storage;
+    Tasklist tasklist;
+    Ui ui;
+    Parser parser;
+
+    public Bob() {
+        this.storage = new Storage("src/main/data/dataFile.txt");
+        this.tasklist = new Tasklist();
+        this.parser = new Parser();
+        this.ui = new Ui(tasklist);
+    }
+
+    public void run() throws CommandNotFoundException, MissingParamsException, PositionException {
         // init items
-        ArrayList<Task> taskList = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        int markPos;
-        String skeleton = """
-                ____________________________________________________________ \s
-                 Hello! I'm Bob \s
-                 What can I do for you? \s
-                ____________________________________________________________ \s
-                """;
-        String blankline = "____________________________________________________________ \s";
-
-        // starting
-        System.out.println(skeleton);
         boolean exit = false;
+        String[] strArr = new String[2];
 
-        // main logic loop
-        while (!exit) {
-            System.out.print("Text: ");
-            String input = scanner.nextLine();
-            String cmd = input.split(" ", 2)[0];
-            String rest = input.split(" ", 2).length > 1 ? input.split(" ", 2)[1] : "";
-            String[] splitString;
-            LocalDate d1;
-            LocalDate d2;
+        ui.startGame();
 
-            // check for the input case
+        while(!exit) {
+            strArr = parser.requestInput();
+            ui.retBlank();
             try {
-                switch (cmd.toLowerCase()) {
+                switch (strArr[0].toLowerCase()) {
                     case "bye":
                         exit = true;
+                        ui.endGame();
                         break;
 
                     case "list":
-                        System.out.println("Here are the tasks in your list:" + "\n"
-                                + getList(taskList) + blankline);
+                        ui.replyGetList(tasklist);
                         break;
 
                     case "mark":
-                        markPos = Integer.parseInt(rest) - 1;
-                        taskList.get(markPos).setDone();
-                        System.out.println("Nice! I've marked this task as done:" + "\n"
-                                + taskList.get(markPos) + "\n" + blankline);
-                        updateDataFile(getList(taskList));
+                        tasklist = ui.replyMarkDone(Integer.parseInt(strArr[1]) - 1, tasklist);
+                        storage.updateDataFile(tasklist.getList());
                         break;
 
                     case "unmark":
-                        markPos = Integer.parseInt(rest) - 1;
-                        taskList.get(markPos).setUndone();
-                        System.out.println("OK, I've marked this task as not done yet:" + "\n"
-                                + taskList.get(markPos) + "\n" + blankline);
-                        updateDataFile(getList(taskList));
+                        tasklist = ui.replyMarkUndone(Integer.parseInt(strArr[1]) - 1, tasklist);
+                        storage.updateDataFile(tasklist.getList());
                         break;
 
                     case "todo":
-                        if (Objects.equals(rest, "")) throw new MissingParamsException("todo");
-                        taskList.add(new Todo(rest));
-                        System.out.println(String.format(" Got it. I've added this task:" + "\n"
-                                + taskList.get(taskList.size() - 1) + "\n"
-                                + "Now you have %s tasks in the list", taskList.size()) + "\n" + blankline);
-                        updateDataFile(getList(taskList));
+                        ui.replyTodo(strArr[1], tasklist);
+                        storage.updateDataFile(tasklist.getList());
                         break;
 
                     case "event":
-                        System.out.println("here");
-                        splitString = rest.split("/from|/to", 3);
-                        System.out.println("here");
-
-                        // error handling
-                        if (splitString.length != 3 || Objects.equals(splitString[0].trim(), "") 
-                            || Objects.equals(splitString[1].trim(), "") || Objects.equals(splitString[2].trim(), "")) {
-                            throw new MissingParamsException("event");
-                        }
-
-                        try {
-                            d1 = LocalDate.parse(splitString[1].trim());
-                            d2 = LocalDate.parse(splitString[2].trim());
-                            taskList.add(new Event(splitString[0], d1, d2));
-                            System.out.println(String.format(" Got it. I've added this task:" + "\n"
-                            + taskList.get(taskList.size() - 1) + "\n"
-                            + "Now you have %s tasks in the list", taskList.size()) + "\n" + blankline);
-                            updateDataFile(getList(taskList));
-                        } catch (DateTimeException e) {
-                            System.out.println("Error parsing date: " + e.getMessage() + ". Please enter your dates in yyyy-mm-dd format");
-                        }
-
+                        ui.replyEvent(strArr[1], tasklist);
+                        storage.updateDataFile(tasklist.getList());
                         break;
 
                     case "deadline":
-                        splitString = rest.split("/by");
-                        if (Objects.equals(splitString[0], "") || Objects.equals(splitString[1], "")) {
-                            throw new MissingParamsException("deadline");
-                        }
-
-                        try {
-                            d1 = LocalDate.parse(splitString[1].trim());
-                            taskList.add(new Deadline(splitString[0], d1));
-                            System.out.println(String.format(" Got it. I've added this task:" + "\n"
-                                    + taskList.get(taskList.size() - 1) + "\n"
-                                    + "Now you have %s tasks in the list", taskList.size()) + "\n" + blankline);
-                            updateDataFile(getList(taskList));
-                        } catch (DateTimeException e) {
-                            System.out.println("Error parsing date: " + e.getMessage() + ". Please enter your dates in yyyy-mm-dd format");
-                        }
-
+                        ui.replyDeadline(strArr[1], tasklist);
+                        storage.updateDataFile(tasklist.getList());
                         break;
 
                     case "delete":
-                        markPos = Integer.parseInt(rest) - 1;
-                        if (markPos >= taskList.size() || markPos < 0) {
-                            throw new PositionException(markPos);
-                        }
-                        Task currTask = taskList.get(markPos);
-                        taskList.remove(markPos);
-                        System.out.println(String.format("Noted. I've removed this task:" + "\n"
-                                + currTask + "\n"
-                                + "Now you have %s tasks in the list", taskList.size()) + "\n" + blankline);
-                        updateDataFile(getList(taskList));
+                        ui.replyDelete(Integer.parseInt(strArr[1]) - 1, tasklist);
+                        storage.updateDataFile(tasklist.getList());
                         break;
                         
                     default:
                         throw new CommandNotFoundException();
                 }
             } catch (CommandNotFoundException | MissingParamsException | PositionException c) {
-                System.out.println(c + "\n" + blankline);
+                System.out.println(c + "\n" + ui.retBlank());
             }
-
-        }
-        scanner.close();
-        System.out.println(blankline + "\n" + "Bye. Hope to see you again soon!");
-
+        }   
     }
 
-    public static String getList(ArrayList<Task> taskList) {
-        String retString = "";
-        for (int i = 0; i < taskList.size(); i++) {
-            retString += (i + 1) + ". " + taskList.get(i) + "\n";
-        }
-        return retString;
+    public static void main(String[] args) throws CommandNotFoundException, MissingParamsException, PositionException {
+        new Bob().run();
     }
-
-    /**
-     * Updates the data file whenever tasklist is updated
-     * @param s The string to write to the data file
-     */
-    public static void updateDataFile(String s) {
-        try {
-            File dataFile = new File("src/main/data/dataFile.txt");
-
-            // check for file
-            if (!dataFile.exists()) {
-                if (dataFile.createNewFile()) {
-                    System.out.println("File created: " + dataFile.getName());
-                } else {
-                    System.out.println("Failed to create the file.");
-                }
-            } else {
-                FileWriter writer = new FileWriter("src/main/data/dataFile.txt");
-                writer.write(s);
-                writer.close();
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,112 @@
+import java.util.Objects;
+import java.time.*;
+import exceptions.MissingParamsException;
+import exceptions.PositionException;
+
+public class Ui {
+    String blankline = "____________________________________________________________ \s";
+    int markPos;
+    Tasklist tasklist;
+
+    public Ui(Tasklist tasklist) {
+        this.tasklist = tasklist;
+    }
+    
+    public void endGame() {
+        System.out.println(blankline + "\n" + "Bye. Hope to see you again soon!");
+    }
+
+    public void startGame() {
+        System.out.println(  """
+            ____________________________________________________________ \s
+            Hello! I'm Bob \s
+            What can I do for you? \s
+            ____________________________________________________________ \s
+            """);
+    }
+
+    public String retBlank() {
+        return blankline;
+    }
+
+    public void replyGetList(Tasklist tasklist) {
+        System.out.println("Here are the tasks in your list:" + "\n"
+        + tasklist.getList() + blankline);
+    }
+
+    public Tasklist replyMarkDone(int pos, Tasklist tasklist) {
+        tasklist.getStr(pos).setDone();
+        System.out.println("Nice! I've marked this task as done:" + "\n"
+                + tasklist.getStr(pos) + "\n" + blankline);
+        return tasklist;
+    }
+
+    public Tasklist replyMarkUndone(int pos, Tasklist tasklist) {
+        tasklist.getStr(pos).setUndone();
+        System.out.println("OK, I've marked this task as not done yet:" + "\n"
+                + tasklist.getStr(pos) + "\n" + blankline);
+        return tasklist;
+    }
+
+    public Tasklist replyTodo(String s, Tasklist tasklist) throws MissingParamsException {
+        if (Objects.equals(s, "")) throw new MissingParamsException("todo");
+        tasklist.add(new Todo(s));
+        System.out.println(String.format(" Got it. I've added this task:" + "\n"
+                + tasklist.getLast() + "\n"
+                + "Now you have %s tasks in the list", tasklist.getSize()) + "\n" + blankline);
+        return tasklist;
+    }
+
+    public Tasklist replyEvent(String s, Tasklist tasklist) throws MissingParamsException {
+        String[] splitString = s.split("/from|/to", 3);
+
+        // error handling
+        if (splitString.length != 3 || Objects.equals(splitString[0].trim(), "") 
+            || Objects.equals(splitString[1].trim(), "") || Objects.equals(splitString[2].trim(), "")) {
+            throw new MissingParamsException("event");
+        }
+
+        try {
+            LocalDate d1 = LocalDate.parse(splitString[1].trim());
+            LocalDate d2 = LocalDate.parse(splitString[2].trim());
+            tasklist.add(new Event(splitString[0], d1, d2));
+            System.out.println(String.format(" Got it. I've added this task:" + "\n"
+            + tasklist.getLast()+ "\n"
+            + "Now you have %s tasks in the list", tasklist.getSize()) + "\n" + blankline);
+        } catch (DateTimeException e) {
+            System.out.println("Error parsing date: " + e.getMessage() + ". Please enter your dates in yyyy-mm-dd format" + blankline);
+        }
+        return tasklist;
+    }
+
+    public Tasklist replyDeadline(String s, Tasklist tasklist) throws MissingParamsException {
+        String[] splitString = s.split("/by");
+        if (splitString.length != 2 || Objects.equals(splitString[0], "") || Objects.equals(splitString[1], "")) {
+            throw new MissingParamsException("deadline");
+        }
+
+        try {
+            LocalDate d1 = LocalDate.parse(splitString[1].trim());
+            tasklist.add(new Deadline(splitString[0], d1));
+            System.out.println(String.format(" Got it. I've added this task:" + "\n"
+                    + tasklist.getStr(tasklist.getSize() - 1) + "\n"
+                    + "Now you have %s tasks in the list", tasklist.getSize()) + "\n" + blankline);
+        } catch (DateTimeException e) {
+            System.out.println("Error parsing date: " + e.getMessage() + ". Please enter your dates in yyyy-mm-dd format");
+        }
+        return tasklist;
+    }
+
+    public Tasklist replyDelete(int pos, Tasklist tasklist) throws PositionException{
+        if (pos >= tasklist.getSize() || markPos < 0) {
+            throw new PositionException(markPos);
+        }
+        Task currTask = tasklist.getStr(markPos);
+        tasklist.delete(markPos);
+        System.out.println(String.format("Noted. I've removed this task:" + "\n"
+                + currTask + "\n"
+                + "Now you have %s tasks in the list", tasklist.getSize()) + "\n" + blankline);
+        return tasklist;
+    }
+
+}
