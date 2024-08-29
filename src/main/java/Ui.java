fdf428b3f4d07737bfb6ef68da378ed8ea7e3587
IanCheah <iancheah24@gmail.com>
2024-08-30 02:23:19 +0800

    Update Ui class to handle more functions

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -18,4 +18,28 @@ public class Ui {
     public void displayError(String message) {
         System.out.println(message);
     }
+
+    public String deleteMessage(Task t, int size) {
+        String s1 = "Noted. I've removed this task:";
+        String s2 = String.format("Now you have %d tasks in the list", size);
+        return s1 + "\n" + " " + t.toString() + "\n" + s2;
+    }
+
+    public String addedMessage(Task t, int size) {
+        String str1 = "Got it. I've added this task:\n";
+        String str2 = String.format("Now you have %d tasks in the list", size);
+        return str1 + t.toString() + "\n" + str2;
+    }
+
+    public String markedMessage(Task t) {
+        return "Nice! I've marked this task as done:\n" + t.toString();
+    }
+
+    public String unmarkedMessage(Task t) {
+        return "OK, I've marked this task as not done yet:\n" + t.toString();
+    }
+
+    public String bye() {
+        return "Bye. Hope to see you again soon!";
+    }
 }
