c88508d44785a00f0d44605ce2eced4d2b82a263
ArtillerySun <e1297745@u.nus.edu>
2024-08-30 01:47:50 +0800

    A-TextUiTesting

--- src/main/java/ArsenBot.java
+++ src/main/java/ArsenBot.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class ArsenBot {
 
-    private static final ArrayList<Task> mem = new ArrayList<Task>();
+    private static final ArrayList<Task> mem = new ArrayList<>();
 
     public static void parseCommand(String input) throws TaskManagerException {
         if (input.startsWith("todo")) {
@@ -46,60 +45,62 @@ public class ArsenBot {
             System.out.println("Got it. I've added this task:");
             System.out.println("\t" + mem.get(mem.size() -1).toString());
             System.out.println("Now you have " + mem.size() + " tasks in the list");
+        } else if (input.startsWith("list")) {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < mem.size(); i ++) {
+                System.out.println((i+1) + "." + mem.get(i));
+            }
+        } else if (input.startsWith("mark ")) {
+            int pos = Integer.parseInt(input.substring(5)) - 1;
+            if (pos < 0 || pos >= mem.size()) {
+                throw new TaskManagerException("Error: Invalid task number.");
+            }
+            System.out.println("Nice! I've marked this task as done:");
+            mem.get(pos).markAsDone();
+            System.out.println(mem.get(pos));
+        } else if (input.startsWith("unmark ")) {
+            int pos = Integer.parseInt(input.substring(7)) - 1;
+            if (pos < 0 || pos >= mem.size()) {
+                throw new TaskManagerException("Error: Invalid task number.");
+            }
+            System.out.println("OK, I've marked this task as not done yet:");
+            mem.get(pos).markAsUndone();
+            System.out.println(mem.get(pos));
+        } else if (input.startsWith("delete ")) {
+            int pos = Integer.parseInt(input.substring(7)) - 1;
+            if (pos < 0 || pos >= mem.size()) {
+                throw new TaskManagerException("Error: Invalid task number.");
+            }
+            System.out.println("Noted. I've removed this task:");
+            System.out.println(mem.get(pos));
+            mem.remove(pos);
+            System.out.println("Now you have " + mem.size() + " tasks in the list.");
         } else {
             throw new TaskManagerException("Error: Unrecognized command. Please enter a valid task command.");
         }
     }
 
-    public static void main(String[] args) {
+    public static void greeting() {
         System.out.println("Hello! I'm ArsenBot");
         System.out.println("What can I do for you?\n");
+    }
+
+    public static void main(String[] args) {
+        greeting();
 
         Scanner reader = new Scanner(System.in);
         while (true) {
             String input = reader.nextLine();
 
-            if (input.startsWith("bye")) {
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
             }
 
-            else if (input.startsWith("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < mem.size(); i ++) {
-                    System.out.println((i+1) + "." + mem.get(i));
-                }
-                continue;
-            }
-
-            else if (input.startsWith("mark")) {
-                System.out.println("Nice! I've marked this task as done:");
-                int pos = Integer.parseInt(input.substring(5));
-                mem.get(pos).markAsDone();
-                System.out.println(mem.get(pos));
-                continue;
-            }
-
-            else if (input.startsWith("unmark")) {
-                System.out.println("OK, I've marked this task as not done yet:");
-                int pos = Integer.parseInt(input.substring(7));
-                mem.get(pos).markAsUndone();
-                System.out.println(mem.get(pos));
-                continue;
-            }
-
-            else if (input.startsWith("delete")) {
-                System.out.println("Noted. I've removed this task:");
-                int pos = Integer.parseInt(input.substring(7)) - 1;
-                System.out.println(mem.get(pos));
-                mem.remove(pos);
-                System.out.println("Now you have " + mem.size() + " tasks in the list.");
-            } else {
-                try {
-                    parseCommand(input);
-                } catch (TaskManagerException e) {
-                    System.out.println(e.getMessage());
-                }
+            try {
+                parseCommand(input);
+            } catch (TaskManagerException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
