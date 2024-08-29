fa8d52fd2770ee33cac690ec6e45990685cbdf8b
dez2003 <desiree2021325@gmail.com>
2024-08-29 22:31:10 +0800

    Implement Command class and its subclasses to write main logic

---
+++ src/main/java/Command.java
@@ -0,0 +1,6 @@
+
+public abstract class Command {
+
+    public abstract void execute(TaskList taskList, Ui ui, Storage storage) throws AlisaException;
+
+}
---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,14 @@
+public class DeleteCommand extends Command {
+    private int index;
+
+    public DeleteCommand(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) throws AlisaException {
+        String message = taskList.deleteTask(index);
+        ui.showMessage(message);
+        storage.syncFile(taskList);
+    }
+}
