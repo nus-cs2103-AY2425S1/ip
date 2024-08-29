e48e218b5845b4ebe4e7718718e1074a6d2fe105
stephaneleong <stephaneleong@gmail.com>
2024-08-29 23:49:19 +0800

    Add DateTimeFormatException

---
+++ src/main/java/DateTimeFormatException.java
@@ -0,0 +1,5 @@
+public class DateTimeFormatException extends Exception {
+    DateTimeFormatException() {
+        super("Wrong DateTime Format!!! Please use the format, d/M/yyyy H:mm (e.g. 29/12/2024 12:00).");
+    }
+}
