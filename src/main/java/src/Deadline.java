524eec8ad000f0f451f4e916c905e5c14ae2d902
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-08-29 20:43:52 +0800

    Change Deadline.java to use LocalDateTime to store deadline with changed formatting

--- src/main/java/src/Deadline.java
+++ src/main/java/src/Deadline.java
@@ -1,23 +1,38 @@
 package src;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
 
+    protected LocalDateTime dateTime ;
+
+
+
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        System.out.println(by);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        String s = "19/01/2004 1600";
+        LocalDateTime parsedDateTime = LocalDateTime.parse(s, formatter);
+        this.dateTime = parsedDateTime;
     }
 
     @Override
     public String toPrettierString() {
-
-        return "D" + super.toPrettierString() + "/by: " + by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        return "D" + super.toPrettierString() + "/by: " + dateTime.format(formatter);
     }
 
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm");
+
+        return "[D]" + super.toString() + "(by: " + dateTime.format(formatter) + ")";
     }
 }
