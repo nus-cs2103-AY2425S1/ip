a0af92c743d396a2a6be0ef1e8b7861e6b4a3308
tanwartushar <>
2024-08-30 02:32:09 +0800

    DonnaException.java: Fix indentation

--- src/main/java/donna/DonnaException.java
+++ src/main/java/donna/DonnaException.java
@@ -10,25 +10,26 @@ public class DonnaException extends Exception{
     }
 
     public static DonnaException emptyDeadline() {
-        return new DonnaException("Please provide a deadline for this task!" + "\n" + "Use /by to provide a deadline.");
+        return new DonnaException("Please provide a deadline for this task!" + "\n"
+            + "Use /by to provide a deadline.");
     }
 
     public static DonnaException emptyEventTime() {
         return new DonnaException("Please provide the timings for this event!"
-                + "\n" + "Use /from and /to to provide timings.");
+            + "\n" + "Use /from and /to to provide timings.");
     }
 
     public static DonnaException invalidTaskType(String taskType) {
         return new DonnaException("Invalid request: " + taskType + "!"
-                + "\n" + "Please use a valid task type (todo / deadline / event);" + "\n" +
-                "type list to view the list of tasks, " + "\n" +
-                "or type delete to remove tasks from the list." + "\n"
-                + "Use /by (for a deadline) and /from and /to (for an event) to specify" + "\n" +
-                "timings for the task.");
+            + "\n" + "Please use a valid task type (todo / deadline / event);" + "\n" +
+            "type list to view the list of tasks, " + "\n" +
+            "or type delete to remove tasks from the list." + "\n"
+            + "Use /by (for a deadline) and /from and /to (for an event) to specify" + "\n" +
+            "timings for the task.");
     }
 
     public static DonnaException invalidTaskNumber() {
         return new DonnaException("Invalid task number :(" + "\n"
-                + "No task assigned to this number yet. Retry with a valid task number!");
+            + "No task assigned to this number yet. Retry with a valid task number!");
     }
 }
