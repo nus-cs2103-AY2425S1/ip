4bd510285af1bfac2ccec7573a93e5c90d3d5f57
bmanara <bmanaranus@gmail.com>
2024-08-29 10:41:14 +0800

    Add JavaDoc to exceptions

--- src/main/java/exceptions/InvalidTaskException.java
+++ src/main/java/exceptions/InvalidTaskException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * Represents a invalid task input exception
+ */
 public class InvalidTaskException extends Exception {
     public InvalidTaskException(String message) {
         super(message);
