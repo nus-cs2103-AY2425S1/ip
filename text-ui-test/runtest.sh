0b5594d7eac4d55e95350b88e1d9eeb19cb3e5c5
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-08-30 00:06:09 +0800

    Change file path for compilation in runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -19,7 +19,9 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/pixel/*.java \
+    ../src/main/java/pixel/command/*.java \
+    ../src/main/java/pixel/task/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
