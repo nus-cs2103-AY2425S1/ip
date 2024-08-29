2fa38ded25cc18eb4fb22588a4b5fd9ae6300a37
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2024-08-29 18:13:35 +0800

    Handle case where file is not found
    File Reading class does not handle FileNotFound Exception
    
    Let's handle the exception within FileReading class internally

--- src/main/java/FileReading.java
+++ src/main/java/FileReading.java
@@ -22,12 +22,17 @@ public class FileReading {
         }
     }
     
-    public static void rememberFileContents(String filePath, ArrayList<Task> list) throws FileNotFoundException {
-        File f = new File(filePath); // create a File for the given file path
-        Scanner s = new Scanner(f); // create a Scanner using the File as the source
-        while (s.hasNextLine()) {
-            String task = s.nextLine();
-            TaskParser.parseTask(task, list);
+    public static void rememberFileContents(String filePath, ArrayList<Task> list) {
+        try {
+            File f = new File(filePath); // create a File for the given file path
+            Scanner s = new Scanner(f); // create a Scanner using the File as the source
+            while (s.hasNextLine()) {
+                String task = s.nextLine();
+                TaskParser.parseTask(task, list);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Oh no! I can't seem to find the file :(");
         }
+        
     }
 }
