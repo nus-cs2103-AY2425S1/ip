7f06e5c8ffd062d234dfcf4ad90df554dfad60ef
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-08-29 22:55:40 +0800

    Generate jUnit test files for ui package

---
+++ src/test/java/yappingbot/ui/MultilineStringBuilderTest.java
@@ -0,0 +1,20 @@
+package yappingbot.ui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultilineStringBuilderTest {
+
+    @Test
+    void addLine() {
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
