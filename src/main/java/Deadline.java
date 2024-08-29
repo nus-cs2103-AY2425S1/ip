08862f5f15ada44cf4286837c6b7f87664e5d46b
jowhee3011 <joeygoh3011@gmail.com>
2024-08-29 19:53:15 +0800

    Add time and date formatting

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 /**
  * Deadline is a class that encapsulates the characteristics of a Deadline Task.
  * It extends from the class Task,
@@ -5,33 +9,61 @@
  * date.
  */
 public class Deadline extends Task {
-    private String date;
+    private LocalDateTime dateAndTime;
 
-    public Deadline(String description, String date) {
+    public Deadline(String description, LocalDateTime dateAndTime) {
         super(description);
-        this.date = date;
+        this.dateAndTime = dateAndTime;
     }
 
-    public Deadline(String description, boolean isDone, String date) {
+    public Deadline(String description, boolean isDone, LocalDateTime dateAndTime) {
         super(description, isDone);
-        this.date = date;
+        this.dateAndTime = dateAndTime;
     }
 
-    public String getDate() {
-        return this.date;
+    public LocalDateTime getDateAndTime() {
+        return this.dateAndTime;
+    }
+
+    public LocalDate getDate() {
+        return this.getDateAndTime().toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return this.getDateAndTime().toLocalTime();
+    }
+
+    /**
+     * The method reformat the given date into MONTH-DD-YYYY.
+     *
+     * @param date LocalDate to be reformatted.
+     * @return String representation of reformatted date.
+     */
+    public static String getReformattedDate(LocalDate date) {
+        return date.getMonth() + " " +
+                date.getDayOfMonth() + " " +
+                date.getYear();
     }
 
     /**
      * The method converts the task to its text representation in the file.
+     *
+     * @return String representation of text.
      */
     @Override
     public String toText() {
         String completed = isDone ? "1" : "0";
-        return "D | " + completed + " | " + super.description + " | " + date;
+        return "D | " +
+                completed + " | " +
+                super.description + " | " +
+                getDate().toString() + " " + getTime().toString();
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.getDate() + ")";
+        return "[D]" +
+                super.toString() +
+                " (by: " + getReformattedDate(this.getDate()) + " " +
+                this.getTime().toString() + ")";
     }
 }
