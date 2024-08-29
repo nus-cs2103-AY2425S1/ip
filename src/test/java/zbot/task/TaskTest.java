b1f719a4ec701f69272fc9c4232bb7a7a9247bd6
liauzhanyi <liauzhanyi@gmail.com>
2024-08-29 08:56:21 +0800

    refactor: Add TaskTest class for unit testing

---
+++ src/test/java/zbot/task/TaskTest.java
@@ -0,0 +1,35 @@
+package zbot.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void testTask() {
+        Task task = new Task("read book");
+        assertEquals("[ ] read book", task.toString());
+    }
+
+    @Test
+    public void testTaskMarkAsDone() {
+        Task task = new Task("read book");
+        task.markAsDone();
+        assertEquals("[X] read book", task.toString());
+    }
+
+    @Test
+    public void testTaskMarkAsUndone() {
+        Task task = new Task("read book");
+        task.markAsDone();
+        task.markAsUndone();
+        assertEquals("[ ] read book", task.toString());
+    }
+
+    @Test
+    public void testTaskGetStatusIcon() {
+        Task task = new Task("read book");
+        assertEquals(" ", task.getStatusIcon());
+        task.markAsDone();
+        assertEquals("X", task.getStatusIcon());
+    }
+}
