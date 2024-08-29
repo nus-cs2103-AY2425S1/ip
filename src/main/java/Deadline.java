ebed967093d766d0d36711468019c0df0ad3a411
tata32000 <tata32000@hotmail.com>
2024-08-29 09:34:17 +0800

    Add getSaveFormat method to Todo and Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,11 @@ public class Deadline extends Task {
     }
 
     @Override
+    public String getSaveFormat() {
+        return "D|" + super.getSaveFormat() + "|" + by;
+    }
+
+    @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -4,6 +4,11 @@ public class Todo extends Task{
     }
 
     @Override
+    public String getSaveFormat() {
+        return "T|" + super.getSaveFormat();
+    }
+
+    @Override
     public String toString() {
         return "[T]" + super.toString();
     }
