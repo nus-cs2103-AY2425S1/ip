a108a075ee0350f342c36feef0bc5650a08b5c45
rynlwhh <e1121363.u.nus.edu>
2024-08-29 23:26:17 +0800

    Add support for list

--- src/main/java/Naega.java
+++ src/main/java/Naega.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Naega {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -7,6 +9,7 @@ public class Naega {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         String userInput;
 
         // Echo loop until user types "bye"
@@ -18,7 +21,14 @@ public class Naega {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
             } else {
+                tasks.add(userInput);
                 System.out.println("____________________________________________________________");
                 System.out.println(" " + userInput);
                 System.out.println("____________________________________________________________");
