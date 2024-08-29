ade2f93dcb2a8b74e4114a26dbd113e9fac0cbf3
CJianzhi <cjianzhi23@gmail.com>
2024-08-30 01:29:19 +0800

    Add todo, deadline, event under AddCommand

---
+++ src/main/java/AddCommand.java
@@ -0,0 +1,17 @@
+public class AddCommand extends Command{
+    private Task task;
+
+    public AddCommand(Task task) {
+        this.task = task;
+    }
+
+    public void execute(TaskList tasks,Ui ui, Storage storage) {
+        System.out.println("    Got it. I've added this task:");
+        tasks.addTask(task);
+        System.out.println("        " + task.toString());
+        String word = tasks.getLength() == 1 ? "task" : "tasks";
+        System.out.println(String.format("    Now you have %d %s in the tasks.", tasks.getLength(), word));
+        storage.appendToFile(task);
+    }
+
+}
