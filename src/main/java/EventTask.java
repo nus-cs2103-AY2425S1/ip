9c3bcf4074896f28db81100a9c1684226a626158
devivy <divyalath.nus@gmail.com>
2024-08-30 00:19:35 +0800

    Complete Level 7 implementation

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,21 @@
+class EventTask extends Task {
+    protected String from;
+    protected String to;
+
+    public EventTask(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public EventTask(String description, String from, String to, boolean isDone) {
+        super(description, isDone);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
