409d622b4f768636e901bc410958d3825834330c
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-08-30 01:35:07 +0700

    Add JavaDoc for find method and format

--- src/main/java/revir/tasks/TaskList.java
+++ src/main/java/revir/tasks/TaskList.java
@@ -1,4 +1,5 @@
 package revir.tasks;
+
 import java.io.IOException;
 import revir.system.Storage;
 import revir.user.Ui;
@@ -6,7 +7,8 @@ import java.util.ArrayList;
 
 /**
  * Represents a list of tasks.
- * This class manages the tasks and provides methods to add, remove, list, and mark tasks as completed.
+ * This class manages the tasks and provides methods to add, remove, list, and
+ * mark tasks as completed.
  */
 public class TaskList {
     private ArrayList<Task> tasks;
@@ -16,7 +18,7 @@ public class TaskList {
      * Constructs a TaskList object.
      * 
      * @param storage the storage object used to load tasks from a file
-     * @param ui the user interface object used to display error messages
+     * @param ui      the user interface object used to display error messages
      */
     public TaskList(Storage storage, Ui ui) {
         this.tasks = new ArrayList<Task>();
@@ -33,7 +35,8 @@ public class TaskList {
      * Adds a task to the task list.
      *
      * @param task the task to be added
-     * @throws IOException if an I/O error occurs while saving the task list to a file
+     * @throws IOException if an I/O error occurs while saving the task list to a
+     *                     file
      */
     public void add(Task task) throws IOException {
         tasks.add(task);
@@ -45,8 +48,10 @@ public class TaskList {
      *
      * @param index the index of the task to be removed
      * @return a string indicating the task that was deleted
-     * @throws IndexOutOfBoundsException if the index is out of range (index < 1 || index > tasks.size())
-     * @throws IOException if an I/O error occurs while saving the task list to a file
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 1 ||
+     *                                   index > tasks.size())
+     * @throws IOException               if an I/O error occurs while saving the
+     *                                   task list to a file
      */
     public String remove(int index) throws IndexOutOfBoundsException, IOException {
         if (index < 1 || index > tasks.size()) {
@@ -62,17 +67,27 @@ public class TaskList {
     /**
      * Returns a string representation of the task list.
      *
-     * @return A string containing the string representation of each task in the list, separated by a newline character.
+     * @return A string containing the string representation of each task in the
+     *         list, separated by a newline character.
      */
     public String list() {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
             str.append(tasks.get(i).toString() + '\n');
         }
-        if (!str.isEmpty()) str.deleteCharAt(str.length() - 1);
+        if (!str.isEmpty())
+            str.deleteCharAt(str.length() - 1);
         return str.toString();
     }
 
+    /**
+     * Finds and returns a string representation of all tasks in the task list that
+     * contain the specified keyword.
+     *
+     * @param keyword the keyword to search for in the task list
+     * @return a string representation of all tasks that contain the specified
+     *         keyword, separated by newline characters
+     */
     public String find(String keyword) {
         StringBuilder str = new StringBuilder();
         tasks.forEach(task -> {
@@ -80,18 +95,21 @@ public class TaskList {
                 str.append(task.toString() + '\n');
             }
         });
-        if (!str.isEmpty()) str.deleteCharAt(str.length() - 1);
+        if (!str.isEmpty())
+            str.deleteCharAt(str.length() - 1);
         return str.toString();
     }
 
     /**
      * Sets the completion status of a task at the specified index.
      *
-     * @param index the index of the task to set the completion status for
+     * @param index  the index of the task to set the completion status for
      * @param status the completion status to set for the task
      * @return a string indicating the updated completion status of the task
-     * @throws IndexOutOfBoundsException if the index is out of range (index < 1 || index > tasks.size())
-     * @throws IOException if an I/O error occurs while saving the tasks to a file
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 1 ||
+     *                                   index > tasks.size())
+     * @throws IOException               if an I/O error occurs while saving the
+     *                                   tasks to a file
      */
     public String setCompleted(int index, boolean status) throws IndexOutOfBoundsException, IOException {
         if (index < 1 || index > tasks.size()) {
