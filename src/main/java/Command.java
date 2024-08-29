2e1d6454d2d4b69eacdadc67d267058b5a67f89f
suhayl13 <suhayl.pervez@gmail.com>
2024-08-29 11:54:37 +0800

    Add new classes

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,27 +1,2 @@
-enum Command {
-    TODO("Adds a todo task."),
-    DEADLINE("Adds a task with a deadline."),
-    EVENT("Adds an event with a start and end time."),
-    MARK("Marks a task as done."),
-    UNMARK("Unmarks a task."),
-    DELETE("Deletes a task."),
-    LIST("Lists all tasks."),
-    BYE("Exits the application.");
-
-    private final String description;
-
-    Command(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return this.name() + ": " + this.description;
-    }
-
-    public static void printCommands() {
-        for (Command command : Command.values()) {
-            System.out.println(command);
-        }
-    }
+package PACKAGE_NAME;public class Command {
 }
