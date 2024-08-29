62549aff17a997352d83f544830c1e67c7264fb9
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-29 15:29:54 +0800

    add Junit items

---
+++ src/test/java/Stobberi/StobberiTest.java
@@ -0,0 +1,13 @@
+package Stobberi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StobberiTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+}
