e7e53e1f7301915f2b132b8bc13d4638962627f1
arnav12344 <64593433+arnav12344@users.noreply.github.com>
2024-08-30 01:48:07 +0800

    Add oop to scripts

--- src/main/java/Kayo.java
+++ src/main/java/Kayo.java
@@ -10,100 +10,169 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Kayo {
-    public static void main(String[] args) {
-        List<Task> taskList = new ArrayList<>();
-        Greet();
-        Scanner input = new Scanner(System.in);
+    public static String filepath;
+    public static Storage storage;
+    public static List<Task> taskList;
+    public static Ui ui;
+    public static Parser parser;
+    public Kayo(String filepath) {
+        Kayo.filepath = filepath;
+        storage = new Storage(filepath);
+        taskList = storage.load();
+        ui = new Ui();
+        parser = new Parser();
+        ui.Greet();
+    }
+    public static void run() {
         String inputString;
-        try {
-            File f = new File("data/kayo.txt");
-            Scanner sc = new Scanner(f);
-            while (sc.hasNextLine()) {
-                String readString = sc.nextLine();
-                Task task = getTask(readString);
-                taskList.add(task);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
+        Scanner input = new Scanner(System.in);
         while(true) {
             inputString = input.nextLine();
-            String[] splitList = inputString.split(" ");
+            String[] splitList = parser.splitString(inputString);
             if(inputString.equals("bye")) {
                 break;
             } else if(splitList[0].equals("list")) {
-                ListItems(taskList);
+                ui.ListItems(taskList);
             } else if (splitList[0].equals("unmark")) {
                 int index = Integer.parseInt(splitList[1])-1;
                 taskList.get(index).setDone(false);
-                System.out.print("OK, I've marked this task as not done yet: \n");
-                System.out.print(taskList.get(index) + "\n");
+                ui.unmarkTask(taskList.get(index));
             } else if(splitList[0].equals("mark")) {
                 int index = Integer.parseInt(splitList[1])-1;
                 taskList.get(index).setDone(true);
-                System.out.print("Nice! I've marked this task at done: \n");
-                System.out.print(taskList.get(index) + "\n");
+                ui.markTask(taskList.get(index));
             }else if(splitList[0].equals("todo")) {
                 if(splitList.length==1) {
                     new DukeException("OOPS !! The description of a todo can't be empty");
                 } else {
                     ToDo todo = new ToDo(splitList[1],false);
                     taskList.add(todo);
-                    System.out.print("Got it. I've added this task: \n");
-                    System.out.print(todo + "\n");
-                    System.out.print("Now you have " + taskList.size() + " tasks in the list.\n");
+                    ui.addTodo(todo);
+                    ui.showTotalTasks(taskList);
                 }
             } else if(splitList[0].equals("deadline")) {
-                String[] bySplit = inputString.split("/by");
-                Deadline deadline = new Deadline(splitList[1],false,bySplit[1]);
+                Deadline deadline = parser.addDeadline(inputString);
                 taskList.add(deadline);
-                System.out.print("Got it. I've added this task: \n");
-                System.out.print(deadline + "\n");
-                System.out.print("Now you have " + taskList.size() + " tasks in the list.\n");
+                ui.addDeadline(deadline);
+                ui.showTotalTasks(taskList);
             } else if(splitList[0].equals("event")) {
-                String[] fromBySplit = inputString.split("/from|/to");
-                Event event = new Event(splitList[1],false,fromBySplit[1],fromBySplit[2]);
+                Event event = parser.addEvent(inputString);
                 taskList.add(event);
-                System.out.print("Got it. I've added this task: \n");
-                System.out.print(event + "\n");
-                System.out.print("Now you have " + taskList.size() + " tasks in the list.\n");
+                ui.addEvent(event);
+                ui.showTotalTasks(taskList);
             }else if(splitList[0].equals("delete")) {
                 int index = Integer.parseInt(splitList[1])-1;
                 Task taskToDelete = taskList.get(index);
                 taskList.remove(index);
-                System.out.print("Noted. I've removed this task: \n");
-                System.out.print(taskToDelete + "\n");
-                System.out.print("Now you have " + taskList.size() + " tasks in the list.\n");
+                ui.deleteTask(taskToDelete);
             } else {
                 new DukeException("OOPS !! Sorry i dont know what that means!");
             }
-            updateData(taskList);
+            storage.updateData(taskList);
         }
-        Exit();
+        ui.Exit();
     }
-
-    private static Task getTask(String readString) {
-        String[] splitStrings = readString.split(" ");
-        boolean isDone = !splitStrings[1].equals("[");
-        Task task = null;
-        String taskDetails = readString.substring(8);
-        String[] splitDetails = taskDetails.split(" ");
-        if (Objects.equals(splitStrings[0], "[T]")) {
-           task = new ToDo(splitDetails[0],isDone);
-        } else if (Objects.equals(splitStrings[0], "[D]")) {
-            String byString = splitDetails[2].substring(0, splitDetails[2].length()-1);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM/dd/yyyy");
-            String day = splitDetails[3].length()==1? "0" + splitDetails[3] : splitDetails[3];
-            String data = splitDetails[2]+"/"+day+"/"+splitDetails[4].substring(0,splitDetails[4].length()-1);
-            LocalDate date = LocalDate.parse(data,formatter);
-            String newDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            task = new Deadline(splitDetails[0],isDone,newDate);
-        } else if (Objects.equals(splitStrings[0], "[E]")) {
-            task = new Event(splitDetails[0],isDone, splitDetails[2],splitDetails[4].substring(0, splitDetails[4].length()-1));
+    public static void main(String[] args) {
+        Kayo kayo = new Kayo("data/kayo.txt");
+        run();
+    }
+    private static class Ui {
+        public void addDeadline(Deadline deadline) {
+            System.out.print("Got it. I've added this task: \n");
+            System.out.print(deadline + "\n");
         }
-        return task;
+        public void addEvent(Event event) {
+            System.out.print("Got it. I've added this task: \n");
+            System.out.print(event + "\n");
+        }
+        public void addTodo(ToDo todo) {
+            System.out.print("Got it. I've added this task: \n");
+            System.out.print(todo + "\n");
+        }
+        public void showTotalTasks(List<Task> taskList) {
+            System.out.print("Now you have " + taskList.size() + " tasks in the list.\n");
+        }
+        public void deleteTask(Task task) {
+            System.out.print("Noted. I've removed this task: \n");
+            System.out.print(task + "\n");
+        }
+        public void markTask(Task task) {
+            System.out.print("Nice! I've marked this task at done: \n");
+            System.out.print(task + "\n");
+        }
+        public void unmarkTask(Task task){
+            System.out.print("OK, I've marked this task as not done yet: \n");
+            System.out.print(task + "\n");
+        }
+        public void ListItems(List<Task> taskList) {
+            System.out.println("Here are the tasks in your list:");
+            for(int i = 0; i < taskList.size(); i++) {
+                System.out.println(i+1 + ". "+ taskList.get(i));
+            }
+        }
+        public void Greet() {
+            System.out.println("Hello! I'm Kayo! ");
+            System.out.println("What can I do for you?");
+        }
+        public void Exit(){
+            System.out.println("Bye. I hope to see you again soon!");
+        }
     }
-
+    private static class Storage{
+        public String filepath;
+        private static void updateData(List<Task> taskList) {
+            try {
+                FileWriter fw = new FileWriter("data/kayo.txt");
+                String dataBody = "";
+                for (int i = 0; i < taskList.size(); i++) {
+                    dataBody += taskList.get(i).toString() + System.lineSeparator();
+                }
+                fw.write(dataBody);
+                fw.close();
+            } catch (IOException e) {
+                System.out.println("File not found");
+            }
+        }
+        public Storage(String filepath){
+            this.filepath = filepath;
+        }
+        public List<Task> load () {
+            List<Task> taskList = new ArrayList<>();
+            try {
+                File f = new File(filepath);
+                Scanner sc = new Scanner(f);
+                while (sc.hasNextLine()) {
+                    String readString = sc.nextLine();
+                    Task task = getTask(readString);
+                    taskList.add(task);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+            }
+            return taskList;
+        }
+        private Task getTask(String readString) {
+            String[] splitStrings = readString.split(" ");
+            boolean isDone = !splitStrings[1].equals("[");
+            Task task = null;
+            String taskDetails = readString.substring(8);
+            String[] splitDetails = taskDetails.split(" ");
+            if (Objects.equals(splitStrings[0], "[T]")) {
+                task = new ToDo(splitDetails[0],isDone);
+            } else if (Objects.equals(splitStrings[0], "[D]")) {
+                String byString = splitDetails[2].substring(0, splitDetails[2].length()-1);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM/dd/yyyy");
+                String day = splitDetails[3].length()==1? "0" + splitDetails[3] : splitDetails[3];
+                String data = splitDetails[2]+"/"+day+"/"+splitDetails[4].substring(0,splitDetails[4].length()-1);
+                LocalDate date = LocalDate.parse(data,formatter);
+                String newDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                task = new Deadline(splitDetails[0],isDone,newDate);
+            } else if (Objects.equals(splitStrings[0], "[E]")) {
+                task = new Event(splitDetails[0],isDone, splitDetails[2],splitDetails[4].substring(0, splitDetails[4].length()-1));
+            }
+            return task;
+        }
+    }
     private static class Task{
 
         private String task;
@@ -123,34 +192,11 @@ public class Kayo {
         }
     }
 
-    private static void updateData(List<Task> taskList) {
-        try {
-            FileWriter fw = new FileWriter("data/kayo.txt");
-            String dataBody = "";
-            for (int i = 0; i < taskList.size(); i++) {
-                dataBody += taskList.get(i).toString() + System.lineSeparator();
-            }
-            fw.write(dataBody);
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("File not found");
-        }
-    }
-    private static void ListItems(List<Task> taskList) {
-        System.out.println("Here are the tasks in your list:");
-        for(int i = 0; i < taskList.size(); i++) {
-            System.out.println(i+1 + ". "+ taskList.get(i));
-        }
-    }
-    public static void Greet() {
-        System.out.println("Hello! I'm Kayo! ");
-        System.out.println("What can I do for you?");
-    }
-    public static void Exit(){
-        System.out.println("Bye. I hope to see you again soon!");
-    }
 
-    public static class ToDo extends Task{
+
+
+
+    public static class ToDo extends Task {
 
         public ToDo(String task, boolean isDone) {
             super(task, isDone);
@@ -176,7 +222,21 @@ public class Kayo {
             System.out.println(message);
         }
     }
-
+    public static class Parser {
+        public String[] splitString(String inputString) {
+            return inputString.split(" ");
+        }
+        public Deadline addDeadline(String parseString) {
+            String[] bySplit = parseString.split("/by");
+            Deadline deadline = new Deadline(parseString.split(" ")[1],false,bySplit[1]);
+            return deadline;
+        }
+        public Event addEvent(String parseString) {
+            String[] fromBySplit = parseString.split("/from|/to");
+            Event event = new Event(parseString.split(" ")[1],false,fromBySplit[1],fromBySplit[2]);
+            return event;
+        }
+    }
     public static class Event extends Task {
         protected String typeOfTask = "E";
         protected String from;
