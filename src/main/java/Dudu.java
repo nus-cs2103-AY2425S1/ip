eccbf2652a4a58b4b0c83ba8eb3b9d22fc56d094
ngjianyi <ngjianyi2002@gmail.com>
2024-08-29 14:37:05 +0800

    Add support for creating directory and files

--- src/main/java/Dudu.java
+++ src/main/java/Dudu.java
@@ -1,6 +1,11 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.lang.StringBuilder;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Dudu {
     enum Command {
@@ -9,9 +14,35 @@ public class Dudu {
 
     private static ArrayList<Task> tasks = new ArrayList<>();
 
+    public static boolean terminate = false;
+
     public static void main(String[] args) {
+        String directoryName = "./data";
+        File directory = new File(directoryName);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        File file = new File("./data/dudu.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+        try {
+            Scanner sc = new Scanner(file);
+            while (!terminate && sc.hasNext()) {
+//                String[] input = sc.nextLine().split(" ", 2);
+//                addTaskFrom(input);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
         String welcomeMessage = LineWrapper.wrap("Hello! I'm dudu\n"
-                                                + "What can I do for you?");
+                + "What can I do for you?");
         String goodbyeMessage = LineWrapper.wrap("Bye. Hope to see you again soon!");
         System.out.println(welcomeMessage);
 
@@ -38,11 +69,10 @@ public class Dudu {
                         String description = getContent(input);
                         ToDo task = new ToDo(description);
                         addTask(task);
-                    }
-                    case DEADLINE: {
+                    } case DEADLINE: {
                         String content = getContent(input);
                         if (!content.matches(".*/by.*")) {
-                            throw new InvalidFormatException("Please use deadline [description] /by [deadline]");
+                            throw new InvalidFormatException("Please use deadline [description] /by [time]");
                         }
                         String[] details = content.split("/by", 2);
                         if (details[0].trim().isEmpty()) {
@@ -54,8 +84,7 @@ public class Dudu {
                         Deadline task = new Deadline(details[0].trim(), details[1].trim());
                         addTask(task);
                         break;
-                    }
-                    case EVENT: {
+                    } case EVENT: {
                         String content = getContent(input);
                         if (!content.matches(".*/from.*/to.*")) {
                             throw new InvalidFormatException("Please use event [description] /from [time] /to [time]");
@@ -66,11 +95,11 @@ public class Dudu {
                         }
                         String description = details[0].trim();
                         if (details.length == 1 || details[1].trim().isEmpty()) {
-                            throw new MissingDateTimeException("Missing from / to time");
+                            throw new MissingDateTimeException("Missing from/to time");
                         }
                         String[] date = details[1].split("/to", 2);
                         if (date.length <= 1 || date[0].trim().isEmpty() || date[1].trim().isEmpty()) {
-                            throw new MissingDateTimeException("Missing from / to time");
+                            throw new MissingDateTimeException("Missing from/to time");
                         }
                         Event task = new Event(description, date[0].trim(), date[1].trim());
                         addTask(task);
@@ -102,6 +131,84 @@ public class Dudu {
             } catch (InvalidFormatException e) {
                 System.out.println(e);
             }
+        }
+    }
+
+    public static void addTaskFrom(String[] input) {
+        try {
+            Command command = Command.valueOf(input[0].trim().toUpperCase());
+            switch (command) {
+                case TODO: {
+                    String description = getContent(input);
+                    ToDo task = new ToDo(description);
+                    addTask(task);
+                }
+                case DEADLINE: {
+                    String content = getContent(input);
+                    if (!content.matches(".*/by.*")) {
+                        throw new InvalidFormatException("WHY Please use deadline [description] /by [time]");
+                    }
+                    String[] details = content.split("/by", 2);
+                    if (details[0].trim().isEmpty()) {
+                        throw new MissingDescriptionException("ERM Missing description after deadline");
+                    }
+                    if (details.length == 1 || details[1].trim().isEmpty()) {
+                        throw new MissingDateTimeException("Missing by time");
+                    }
+                    Deadline task = new Deadline(details[0].trim(), details[1].trim());
+                    addTask(task);
+                    break;
+                }
+                case EVENT: {
+                    String content = getContent(input);
+                    if (!content.matches(".*/from.*/to.*")) {
+                        throw new InvalidFormatException("Please use event [description] /from [time] /to [time]");
+                    }
+                    String[] details = content.split("/from", 2);
+                    if (details[0].trim().isEmpty()) {
+                        throw new MissingDescriptionException("Missing description after event");
+                    }
+                    String description = details[0].trim();
+                    if (details.length == 1 || details[1].trim().isEmpty()) {
+                        throw new MissingDateTimeException("Missing from/to time");
+                    }
+                    String[] date = details[1].split("/to", 2);
+                    if (date.length <= 1 || date[0].trim().isEmpty() || date[1].trim().isEmpty()) {
+                        throw new MissingDateTimeException("Missing from/to time");
+                    }
+                    Event task = new Event(description, date[0].trim(), date[1].trim());
+                    addTask(task);
+                    break;
+                }
+                case MARK: {
+                    int index = getIndex(input);
+                    markTask(index);
+                    break;
+                }
+                case UNMARK: {
+                    int index = getIndex(input);
+                    unmarkTask(index);
+                    break;
+                }
+                case DELETE: {
+                    int index = getIndex(input);
+                    deleteTask(index);
+                    break;
+                }
+                default: {
+                    String output = LineWrapper.wrap("Please use help to get the list of commands");
+                    System.out.println(output);
+                    break;
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        } catch (MissingDescriptionException e) {
+            System.out.println(e);
+        } catch (MissingDateTimeException e) {
+            System.out.println(e);
+        } catch (InvalidFormatException e) {
+            System.out.println(e);
         }
     }
 
