e9c94178b42e77a90559341229df0145868ec5c2
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-29 16:06:20 +0800

    clean switch case

--- src/main/java/Stobberi/components/Parser.java
+++ src/main/java/Stobberi/components/Parser.java
@@ -22,39 +22,39 @@ public class Parser {
         String command = parts[0];
         String restOfCommand = String.join(" ", java.util.Arrays.copyOfRange(parts, 1, parts.length));
         switch (command) {
-            case "bye" :
-                return new ExitCommand();
-            case "list" :
-                return new ListCommand(taskList);
-            case "mark" :
-                if (restOfCommand.matches("\\d+")) {
-                    int taskNumber = Integer.parseInt(restOfCommand);
-                    return new MarkCommand(taskList, taskNumber);
-                } else {
-                    throw new NoNumberStobberiException("Where is the number?");
-                }
-            case "unmark" :
-                if (restOfCommand.matches("\\d+")) {
-                    int taskNumber = Integer.parseInt(restOfCommand);
-                    return new UnmarkCommand(taskList, taskNumber);
-                } else {
-                    throw new NoNumberStobberiException("Where is the number?");
-                }
-            case "delete":
-                if (restOfCommand.matches("\\d+")) {
-                    int taskNumber = Integer.parseInt(restOfCommand);
-                    return new DeleteCommand(taskList, taskNumber);
-                } else {
-                    throw new NoNumberStobberiException("Where is the number?");
-                }
-            case "date":
-                return new DateCommand(taskList, restOfCommand);
-            case "todo":
-                return new TodoCommand(taskList, restOfCommand);
-            case "deadline":
-                return new DeadlineCommand(taskList, restOfCommand);
-            case "event":
-                return new EventCommand(taskList, restOfCommand);
+        case "bye" :
+            return new ExitCommand();
+        case "list" :
+            return new ListCommand(taskList);
+        case "mark" :
+            if (restOfCommand.matches("\\d+")) {
+                int taskNumber = Integer.parseInt(restOfCommand);
+                return new MarkCommand(taskList, taskNumber);
+            } else {
+                throw new NoNumberStobberiException("Where is the number?");
+            }
+        case "unmark" :
+            if (restOfCommand.matches("\\d+")) {
+                int taskNumber = Integer.parseInt(restOfCommand);
+                return new UnmarkCommand(taskList, taskNumber);
+            } else {
+                throw new NoNumberStobberiException("Where is the number?");
+            }
+        case "delete":
+            if (restOfCommand.matches("\\d+")) {
+                int taskNumber = Integer.parseInt(restOfCommand);
+                return new DeleteCommand(taskList, taskNumber);
+            } else {
+                throw new NoNumberStobberiException("Where is the number?");
+            }
+        case "date":
+            return new DateCommand(taskList, restOfCommand);
+        case "todo":
+            return new TodoCommand(taskList, restOfCommand);
+        case "deadline":
+            return new DeadlineCommand(taskList, restOfCommand);
+        case "event":
+            return new EventCommand(taskList, restOfCommand);
         }
         throw new NoSuchTaskStobberiException("I'm sorry! That is NOT an executable command");
     }
