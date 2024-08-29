8122532dfbc44db084c6005cf274cc4259072188
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-08-29 17:18:05 +0800

    Create Javadoc

--- src/main/java/Stobberi/Command/FindCommand.java
+++ src/main/java/Stobberi/Command/FindCommand.java
@@ -6,14 +6,36 @@ import Stobberi.components.TaskList;
 
 import java.time.format.DateTimeParseException;
 
+/**
+ * Represents a command to find and filter tasks in a {@link TaskList} based on a keyword.
+ */
 public class FindCommand extends Command {
+    /**
+     * The list of tasks to be filtered.
+     */
     private TaskList taskList;
+
+    /**
+     * The keyword used for filtering tasks.
+     */
     private String word;
+
+    /**
+     * Constructs a new {@code FindCommand} with the specified {@link TaskList} and keyword.
+     *
+     * @param taskList The list of tasks to be filtered.
+     * @param word     The keyword used for filtering tasks.
+     */
     public FindCommand(TaskList taskList, String word) {
         this.taskList = taskList;
         this.word = word;
     }
 
+    /**
+     * Executes the command by filtering the {@link TaskList} based on the specified keyword.
+     *
+     * @throws StobberiException if an error occurs during filtering.
+     */
     @Override
     public void execute() throws StobberiException {
         taskList.filterListByWord(word);
