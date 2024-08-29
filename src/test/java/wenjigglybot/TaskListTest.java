06ef98b85d2108a22a433168daf5ba9a22d91ffe
wenjebs <wenjieb2@gmail.com>
2024-08-29 19:03:16 +0800

    Write unit tests for TaskList

--- src/test/java/wenjigglybot/TaskListTest.java
+++ src/test/java/wenjigglybot/TaskListTest.java
@@ -1,4 +1,29 @@
 package wenjigglybot;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TaskListTest {
+    private TaskList taskList;
+    private Task testTask;
+
+    @BeforeEach
+    public void setUp() {
+        taskList = new TaskList();
+        testTask = new ToDoTask("meemaw");
+    }
+
+    @Test
+    public void addTask_emptyTaskList_success() {
+        taskList.addTask(testTask);
+        assertEquals(1, taskList.size());
+    }
+
+    @Test
+    public void getTask_oneTask_success() {
+        taskList.addTask(testTask);
+        assertEquals(testTask.toString(), taskList.get(0).toString());
+    }
 }
