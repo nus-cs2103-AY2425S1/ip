c76a114d4ae02e86d1ccb3fee29fb36b1073feac
CliffKoes <cliffkoes@gmail.com>
2024-08-29 11:32:03 +0800

    Add header comments to Bibi.java methods

--- src/main/java/bibi/Bibi.java
+++ src/main/java/bibi/Bibi.java
@@ -3,13 +3,24 @@ package bibi;
 import bibi.task.TaskList;
 import java.io.FileNotFoundException;
 
+/**
+ * Represents the chat bot.
+ */
 public class Bibi {
+    /** The Ui instance that handles printing and reading console commands */
     private Ui ui;
+    /** The list that contains all Tasks inside the save file */
     private TaskList tasks;
+    /** The class that handles writing to the save file */
     private Storage storage;
 
     private boolean isExit;
 
+    /**
+     * Constructs a new Bibi instance.
+     *
+     * @param filePath An absolute path to the save file
+     */
     public Bibi(String filePath) {
         this.ui = new Ui();
         this.tasks = new TaskList();
@@ -27,6 +38,9 @@ public class Bibi {
         ui.printHorizontalLine();
     }
 
+    /**
+     * Runs the chat bot.
+     */
     public void run() {
         while (!isExit) {
             Command cmd = Parser.parseCommand(ui.readInput());
@@ -34,6 +48,7 @@ public class Bibi {
             isExit = cmd.isExit();
         }
     }
+    
     public static void main(String[] args) {
         // Insert path to saveFile
         new Bibi("data/list.txt").run();
