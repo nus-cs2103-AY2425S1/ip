892211c272219f51b3cf796ca215bdbb6b2d640d
xGladiate <sharonsohxuanhui@hotmail.com>
2024-08-30 11:00:03 +0800

    Add javadoc commit to Ui

--- src/main/java/xbot/ui/Ui.java
+++ src/main/java/xbot/ui/Ui.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 import xbot.TaskList;
 import xbot.XBotException;
 
+/**
+ * The Ui class handles all user interactions in the XBot application.
+ * It manages input and output operations such as displaying messages, reading user commands, and showing errors.
+ */
 public class Ui {
     private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Displays the list of tasks to the user.
+     * If the list is empty, it informs the user that there are no tasks.
+     *
+     * @param list The TaskList containing the tasks to be displayed.
+     */
     public void showTaskList(TaskList list) {
         if (list.size() == 0) {
             System.out.println("Yayy!! You have no task in your list");
@@ -19,22 +29,43 @@ public class Ui {
         }
     }
 
+    /**
+     * Displays a welcome message to the user when the application starts.
+     */
     public void showWelcome() {
         System.out.println("Hello! I'm XBot\n" + "What can I do for you?");
     }
 
+    /**
+     * Displays a goodbye message to the user when the application terminates.
+     */
     public void showBye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    /**
+     * Read the next command for the user input.
+     *
+     * @return The command input by the user as a trimmed string.
+     */
     public String readCommand() {
         return scanner.nextLine().trim();
     }
 
+    /**
+     * Displays an error message to the user.
+     * This method is called when an XBotException is thrown during the application's execution.
+     *
+     * @param e The XBotException containing the error message to be displayed.
+     */
     public void mainErrorMessage(XBotException e) {
         System.out.println("Oh No!! " + e.getMessage());
     }
 
+    /**
+     * Closes the scanner resource to prevent resource leaks.
+     * This method should be called when the application is about to terminate.
+     */
     public void close() {
         scanner.close();
     }
