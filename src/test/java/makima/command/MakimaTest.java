ec0804647cb4288b9d0eaa10e78264ae4f7760d0
Edsel-Tan <riwk2015@gmail.com>
2024-08-30 11:16:17 +0800

    add JUnit test for deadline

---
+++ src/test/java/makima/command/MakimaTest.java
@@ -0,0 +1,15 @@
+package makima.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MakimaTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
+
+    
+}
