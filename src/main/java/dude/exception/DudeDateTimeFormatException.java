52aa0559384acd843706ce8cb2e439d127fc8bdd
MingYiAw <e1129986@u.nus.edu>
2024-08-30 11:09:14 +0800

    refactor: Update DudeDateTimeFormatException message

--- src/main/java/dude/exception/DudeDateTimeFormatException.java
+++ src/main/java/dude/exception/DudeDateTimeFormatException.java
@@ -9,6 +9,6 @@ public class DudeDateTimeFormatException extends DudeException {
      * Constructs a DudeDateTimeFormatException with no detail message.
      */
     public DudeDateTimeFormatException(){
-        super("Please input the date and time with the following format: \"yyyy-MM-dd HH:mm\", and with valid value");
+        super("Please input the date and time with the following format: \"yyyy-MM-dd HH:mm\", and with valid value.");
     }
 }
