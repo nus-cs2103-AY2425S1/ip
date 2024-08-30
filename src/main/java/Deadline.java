e62d241b800d94ae0fffa0fd6cec0e207a8fe272
Vicelya Visakha <>
2024-08-30 16:03:53 +0800

    Revert "Add a Comment in Deadline to test for branch merging"
    This reverts commit 95ebe89da6d50ad7ac8122c40fea7935bdd7cdf5.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -11,7 +11,6 @@ public class Deadline extends Task {
         this.by = by;
     }
 
-    // add a comment to test 
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by.format(OUT_FORMATTER) + ")";
