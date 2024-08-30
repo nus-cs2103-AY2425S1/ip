8d1259d22feb3315dc36b4ebb82e38941d5a85d2
waihin26 <138366109+waihin26@users.noreply.github.com>
2024-08-30 16:07:59 +0800

    understand date and time inputs for deadline tasks

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,19 +1,23 @@
+/**
+ * EventTask class that inherit from Task.
+ * A kind of task that has a start and end time
+ */
 public class EventTask extends Task {
-    private String from;
-    private String to;
+    private String start;  // starting time of task
+    private String end;    // ending time of task
 
-    public EventTask(String description, String from, String to) {
+    public EventTask(String description, String start, String end) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.start = start;
+        this.end = end;
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E] " + super.toString() + " (from: " + start + " to: " + end + ")";
     }
     @Override
     public String toFileString() {
-        return "E | " + super.getStatusIcon() + " | " + getDescription() + " | " + from + " | " + to;
+        return "E | " + super.getStatusIcon() + " | " + getDescription() + " | " + start + " | " + end;
     }
 }
--- src/main/java/ToDoTask.java
+++ src/main/java/ToDoTask.java
@@ -1,3 +1,8 @@
+/**
+ * TodoTask class that inherit from Task.
+ * Basically a kind of task that only has a description
+ */
+
 public class ToDoTask extends Task {
 
     public ToDoTask(String description) {
@@ -8,6 +13,8 @@ public class ToDoTask extends Task {
     public String toString() {
         return "[T] " + super.toString();
     }
+
+    /** Returns string that will be written into text file*/
     @Override
     public String toFileString() {
         return "T | " + getStatusIcon() + " | " + getDescription();
