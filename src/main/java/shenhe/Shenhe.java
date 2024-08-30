d35c1bb46413a42f20a645e595fca8dc0989bf4a
lr <lruniapp1020@gmail.com>
2024-08-31 00:44:40 +0800

    Change shenhe.java so that it follows the standard format

--- src/main/java/shenhe/Shenhe.java
+++ src/main/java/shenhe/Shenhe.java
@@ -27,7 +27,7 @@ public class Shenhe {
         while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
-                ui.showLine(); // show the divider line ("_______")
+                ui.showLine();
                 Command c = Parser.parse(fullCommand);
                 c.execute(tasks, ui, storage);
                 isExit = c.isExit();
