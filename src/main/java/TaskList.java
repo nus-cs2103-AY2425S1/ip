0c2455fb0eaa63e5731ede72f49e22a4a54351f6
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-30 22:35:31 +0800

    Add TaskList class to store and manage tasks
    Extracted task list operations in main loop into TaskList class, in
    order to closer align to OOP principles

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,44 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public Task getTask(int i) {
+        return this.tasks.get(i);
+    }
+
+    public void addTask(Task t) {
+        tasks.add(t);
+    }
+
+    public void deleteTask(int i) {
+        tasks.remove(i);
+    }
+
+    public void markTask(int i) {
+        tasks.get(i).mark();
+    }
+
+    public void unmarkTask(int i) {
+        tasks.get(i).unmark();
+    }
+
+    public String[] toTaskStrings() {
+        if (this.tasks.isEmpty()) {
+            return new String[] {};
+        } else {
+            String[] taskStrings = new String[this.tasks.size()];
+            for (int i = 0; i < this.tasks.size(); i++) {
+                taskStrings[i] = this.tasks.get(i).toString();
+            }
+            return taskStrings;
+        }
+    }
+}
