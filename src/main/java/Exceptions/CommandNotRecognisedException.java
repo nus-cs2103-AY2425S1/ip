36b48d9d37712889c0576ad1eb09c850c7b79ec1
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 13:37:34 +0800

    CommandNotRecognisedException.java: add Javadoc comments

--- src/main/java/Exceptions/CommandNotRecognisedException.java
+++ src/main/java/Exceptions/CommandNotRecognisedException.java
@@ -1,11 +1,22 @@
 package Exceptions;
 
+/**
+ * An exception that occurs when the parser cannot recognise the userInput
+ */
 public class CommandNotRecognisedException extends TestamentException {
 
+    /**
+     * Constructor for CommandNotRecognisedException
+     */
     public CommandNotRecognisedException() {
         super("Apologies, but I do not understand that command");
     }
 
+    /**
+     * Constructor for CommandNotRecognisedException
+     *
+     * @param s Message for exception
+     */
     public CommandNotRecognisedException(String s) {
         super(s);
     }
