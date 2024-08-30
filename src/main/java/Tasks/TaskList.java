62d142275e2c68ed0ab5cbbad1d085530cbd49d2
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 09:32:45 +0800

    TaskList.java: add Javadoc comments

--- src/main/java/Tasks/TaskList.java
+++ src/main/java/Tasks/TaskList.java
@@ -4,16 +4,35 @@ import java.util.ArrayList;
 
 import Exceptions.*;
 
+/**
+ * Represents a list of tasks stored by the Testament chatbot.
+ */
 public class TaskList {
     private ArrayList<Task> listOfTasks;
+
+    /**
+     * Constructor for TaskList.
+     */
     public TaskList() {
         listOfTasks = new ArrayList<>();
     }
 
+    /**
+     * Adds a task to the taskList.
+     *
+     * @param task Task to add.
+     */
     public void add(Task task) {
         listOfTasks.add(task);
     }
 
+    /**
+     * Marks a task in the taskList.
+     * If the task does not exist in the taskList, throws NotInTaskListException.
+     *
+     * @param i taskNumber of task to mark.
+     * @throws NotInTaskListException thrown if taskNumber does not exist in taskList.
+     */
     public void mark(int i) throws NotInTaskListException {
         if (i > listOfTasks.size() || i < 1) {
             throw new NotInTaskListException();
@@ -21,6 +40,13 @@ public class TaskList {
         listOfTasks.get(i - 1).done();
     }
 
+    /**
+     * unMarks a task in the taskList.
+     * If the task does not exist in the taskList, throws NotInTaskListException.
+     *
+     * @param i taskNumber of task to unMark.
+     * @throws NotInTaskListException thrown if taskNumber does not exist in taskList.
+     */
     public void unMark(int i) throws NotInTaskListException {
         if (i > listOfTasks.size() || i < 1) {
             throw new NotInTaskListException();
@@ -28,6 +54,13 @@ public class TaskList {
         listOfTasks.get(i - 1).undone();
     }
 
+    /**
+     * Retrieves a task in the taskList.
+     * If the task does not exist in the taskList, throws NotInTaskListException.
+     *
+     * @param i taskNumber of task to retrieve.
+     * @throws NotInTaskListException thrown if taskNumber does not exist in taskList.
+     */
     public Task getTask(int i) throws NotInTaskListException {
         if (i > listOfTasks.size() || i < 1) {
             throw new NotInTaskListException();
@@ -35,6 +68,13 @@ public class TaskList {
         return listOfTasks.get(i - 1);
     }
 
+    /**
+     * Deletes a task in the taskList.
+     * If the task does not exist in the taskList, throws NotInTaskListException.
+     *
+     * @param i taskNumber of task to retrieve.
+     * @throws NotInTaskListException thrown if taskNumber does not exist in taskList.
+     */
     public void deleteTask(int i) throws NotInTaskListException {
         if (i > listOfTasks.size() || i < 1) {
             throw new NotInTaskListException();
@@ -42,10 +82,20 @@ public class TaskList {
         listOfTasks.remove(i - 1);
     }
 
+    /**
+     * Returns size of tasklist.
+     *
+     * @return Size of tasklist.
+     */
     public int getSize() {
         return listOfTasks.size();
     }
 
+    /**
+     * Returns a string that stores the data of all tasks in the taskList.
+     *
+     * @return String that stores the data of all tasks in taskList.
+     */
     public String fileTaskInfo() {
         StringBuilder str = new StringBuilder();
         for (Task t : listOfTasks) {
@@ -55,6 +105,11 @@ public class TaskList {
         return str.toString();
     }
 
+    /**
+     * returns toString representation of taskList.
+     *
+     * @return toString representation of taskList.
+     */
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
