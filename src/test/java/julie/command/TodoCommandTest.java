b9638de6c7828a73368a6ee022cd8e7bf2386145
colinhia <coase28@icloud.com>
2024-08-30 15:31:35 +0800

    add JUnit 2

--- src/test/java/julie/command/TodoCommandTest.java
+++ src/test/java/julie/command/TodoCommandTest.java
@@ -1,2 +1,18 @@
-package julie.command;public class TodoCommandTest {
+package julie.command;
+import julie.task.Task;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoCommandTest {
+    @Test
+    public void todo_ListTest() {
+        List<Task> taskList = new ArrayList<>();
+        TodoCommand c = new TodoCommand("todo help");
+        c.run(taskList);
+        assertEquals("[[T] [ ] help]", taskList.toString());
+    }
 }
