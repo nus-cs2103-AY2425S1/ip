0cd46d973767125c8853b4d960eb0f1c75890f3e
quahkk <quahkaikiat1@gmail.com>
2024-08-30 14:09:56 +0800

    Add date and time logic
    Event and Deadline classes were implemented with date and time logic.
    User will have to add date and time when they specify their event
    and deadline tasks.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,39 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
 public class Deadline extends Task {
-    private String by;
+    private LocalDateTime by;
+    private ArrayList<DateTimeFormatter> formatters = new ArrayList<>(
+            List.of(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"),
+                    DateTimeFormatter.ofPattern("d/M/yyyy HH:mm"))
+    );
+    private DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM d yyyy HH:mm");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = parseDateTime(by);
     }
 
+    public LocalDateTime parseDateTime(String by) throws DateTimeParseException {
+        for (DateTimeFormatter formatter : formatters) {
+            try {
+                return LocalDateTime.parse(by, formatter);
+            } catch (DateTimeParseException e) {
+                continue;
+            }
+        }
+        throw new DateTimeParseException("Unable to parse date & time", by, 0);
+    }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(outputFormatter) + ")";
     }
 
     @Override
     public String toFileString() {
-        return String.format("D | %d | %s | %s", super.status() ? 1 : 0, getDescription(), by);
+        return String.format("D | %d | %s | %s", super.status() ? 1 : 0, getDescription(),
+                by.format(formatters.get(0)));
     }
 }
