4d7bf5598f805e49c9a99831f622d7c80626bc7b
xGladiate <sharonsohxuanhui@hotmail.com>
2024-08-30 10:59:26 +0800

    Add javadoc commit to Task

--- src/main/java/xbot/task/Task.java
+++ src/main/java/xbot/task/Task.java
@@ -1,44 +1,84 @@
 package xbot.task;
 
+/**
+ * Represents a generic task in the XBot application.
+ * A task has a description, a completion status, and a type.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
     protected TaskType type;
 
+    /**
+     * Constructs a new Task with the specified description and type.
+     * The task is initially not marked as done.
+     *
+     * @param description The description of the task.
+     * @param type The type of the task, represented by the {@link TaskType} enum.
+     */
     public Task(String description, TaskType type) {
         this.description = description;
         this.isDone = false;
         this.type = type;
     }
 
+    /**
+     * Returns the type of the task.
+     *
+     * @return The task type.
+     */
     public TaskType getType() {
         return type;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The task description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the status icon for the task.
+     * The icon is "X" if the task is done, otherwise a space " ".
+     *
+     * @return The status icon.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks the task as done without any additional output.
+     */
     public void setIsDone() {
         isDone = true;
     }
 
+    /**
+     * Marks the task as done and prints a confirmation message.
+     */
     public void markAsDone() {
         isDone = true;
         System.out.println("Nice! I've marked this task as done:\n" +
                 toString());
     }
 
+    /**
+     * Marks the task as undone and prints a confirmation message.
+     */
     public void markAsUndone() {
         isDone = false;
         System.out.println("OK, I've marked this task as not done yet:\n" +
                 toString());
     }
 
+    /**
+     * Checks if the task is done.
+     * @return {@code true} if the task is done, otherwise {@code false}.
+     */
     public boolean isDone() {
         return isDone;
     }
