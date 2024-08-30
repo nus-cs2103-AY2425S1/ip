dca203371328bb136b8eeb2badaa64e6b4eb709e
RavirajTalgeri <ravirajtalgeri@gmail.com>
2024-08-30 13:21:07 +0800

    Implement mark and unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> userInputs = new ArrayList<>();
+    private static ArrayList<Task> userInputs = new ArrayList<>();
 
     private static void addItem(String inp) {
-        userInputs.add(inp);
+        userInputs.add(new Task(inp));
     }
 
     private static void display() {
+        System.out.println("Here are the tasks in your list:");
         for (int i=0; i<userInputs.size(); i++) {
-            System.out.println((i+1) + ". " + userInputs.get(i));
+            System.out.println((i+1) + "." + userInputs.get(i));
         }
     }
 
@@ -31,11 +32,20 @@ public class Duke {
         while (!inp.equals("bye")) {
             if (inp.equals("list")) {
                 display();
-                inp = scanner.nextLine();
-                continue;
+            } else if (inp.startsWith("mark")) {
+                int idx = Integer.parseInt(String.valueOf(inp.charAt(5)))-1;
+                userInputs.get(idx).setDone(true);
+                System.out.println("Nice! I've marked this task as done:\n" +
+                                    userInputs.get(idx));
+            } else if (inp.startsWith("unmark")) {
+                int idx = Integer.parseInt(String.valueOf(inp.charAt(7)))-1;
+                userInputs.get(idx).setDone(false);
+                System.out.println("OK, I've marked this task as not done yet:\n" +
+                        userInputs.get(idx));
+            } else {
+                addItem(inp);
+                System.out.println("added: " + inp);
             }
-            addItem(inp);
-            System.out.println("added: " + inp);
             inp = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!\n");
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        setName(name);
+        setDone(false);
+    }
+
+    public Task(String name, boolean isDone) {
+        setName(name);
+        setDone(isDone);
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        String res = "[";
+        if (isDone) {
+            res = res + "X";
+        } else {
+            res = res + " ";
+        }
+        res = res + "] " + name;
+        return res;
+    }
+}
