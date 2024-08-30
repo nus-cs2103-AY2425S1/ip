edef60cf301697245f11d22ceb4cfbee279159bf
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 13:37:48 +0800

    DeadlineNoDateException.java: add Javadoc comments

--- src/main/java/Exceptions/DeadlineNoDateException.java
+++ src/main/java/Exceptions/DeadlineNoDateException.java
@@ -1,7 +1,13 @@
 package Exceptions;
 
+/**
+ * An exception that occurs when a deadline description has no date
+ */
 public class DeadlineNoDateException extends TestamentException {
 
+    /**
+     * Constructor for DeadlineNoDateException
+     */
     public DeadlineNoDateException() {
         super("You have not provided the date for this deadline.\nThe format for deadlines is as follows:\n" +
                 "deadline (details) /by (date)");
