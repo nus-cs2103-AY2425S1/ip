c6aa575f9e9a50ed96d0c41e393d8d6c8b4e0c24
xGladiate <sharonsohxuanhui@hotmail.com>
2024-08-30 10:59:51 +0800

    Add javadoc commit to ToDo

--- src/main/java/xbot/task/ToDo.java
+++ src/main/java/xbot/task/ToDo.java
@@ -1,7 +1,15 @@
 package xbot.task;
 
+/**
+ * Represents a to-do task in the XBot application.
+ * A {@code ToDo} task has a description.
+ */
 public class ToDo extends Task {
 
+    /**
+     * Constructs the {@code ToDo} task with the specified description.
+     * @param description The description of the task. 
+     */
     public ToDo(String description) {
         super(description, TaskType.T);
     }
