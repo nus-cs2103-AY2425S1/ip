3787d8ed2579f478cae7bc78931e6511ea95efcc
Tan Wen Xuan <>
2024-08-30 22:35:37 +0800

    Update Axel to understand dates and times

--- src/main/java/ToDoTask.java
+++ src/main/java/ToDoTask.java
@@ -1,9 +1,14 @@
 public class ToDoTask extends Task {
+
     public ToDoTask(String taskName) {
         super(taskName);
     }
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+    @Override
+    public String toFileFormat() {
+        return "T | " + (isDone() ? "1" : "0") + " | " + getTaskName();
     }
 }
