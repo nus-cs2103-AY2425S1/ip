dd69eb779df333ed14b47f50021b628856abd858
Anvita2110 <e1070535@u.nus.edu>
2024-08-31 02:27:26 +0800

    Add JavaDoc comments to remaining files

--- src/main/java/Ponder_Pika/Task/Deadline.java
+++ src/main/java/Ponder_Pika/Task/Deadline.java
@@ -3,19 +3,50 @@ package Ponder_Pika.Task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * This class represents a specific type of task that has a deadline.
+ * It extends the {@code Task} class and includes additional information about the deadline
+ * by which the task must be completed.
+ * The {@code Deadline} class overrides the following methods from the {@code Task} class:
+ * saveFullDetails() and toString()
+ */
 public class Deadline extends Task {
     private final LocalDateTime deadline;
 
+    /**
+     * Constructs a Deadline task with the specified description and deadline.
+     *
+     * @param description the description of the task
+     * @param deadline the deadline by which the task should be completed
+     */
     public Deadline(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
     }
 
+    /**
+     * Returns a string representing the full details of the task.
+     * The format of the returned string is:
+     * <pre>
+     *   D | [completion status] | [description] | [deadline]
+     * </pre>
+     *
+     * @return a string representing the full details of the task
+     */
     @Override
     public String saveFullDetails() {
         return String.format("D | %b | %s | %s", isDone(), getDescription(), this.deadline.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
     }
 
+    /**
+     * Returns a string representation of the task suitable for display.
+     * The format of the returned string is:
+     * <pre>
+     *   [D][description] (by: [deadline])
+     * </pre>
+     *
+     * @return a formatted string representation of the task
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + String.format(" (by: %s)", this.deadline.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")));
--- src/main/java/Ponder_Pika/Task/Task.java
+++ src/main/java/Ponder_Pika/Task/Task.java
@@ -1,37 +1,81 @@
 package Ponder_Pika.Task;
 
+/** * This class represents an abstract task with a description and status (done or not done).
+ * It provides methods to mark the task as done or undone, check its status, and get its description.
+ */
 public abstract class Task {
     private final String description;
     private boolean isDone;
 
+    /**
+     * Constructs a Task with the specified description.
+     * The task is initially marked as not done, hence isDone being false.
+     *
+     * @param description the description of the task
+     */
     public Task(String description) {
 
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done, hence isDone is assigned to true.
+     */
     public void markDone() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks the task (not done), hence isDone is assigned to false.
+     */
     public void markUndone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the status of the task as a string.
+     * The status is represented as "X" if the task is done, otherwise as a white space.
+     *
+     * @return a string representing the task status
+     */
     public String taskStatus() {
         return (this.isDone ? "X" : " ");
     }
 
+    /**
+     * Returns whether the task is marked as done.
+     *
+     * @return true if the task is done, false otherwise
+     */
     public boolean isDone() {
         return (this.isDone);
     }
 
+    /**
+     * Returns the description of the task.
+     * Acts as a getter.
+     *
+     * @return a string containing the task description
+     */
     public String getDescription() {
         return (this.description);
     }
 
+    /**
+     * Abstract method that must be implemented by subclasses to provide a
+     * string representation of the task's full details for saving and displaying purposes.
+     *
+     * @return a string containing the full details of the task
+     */
     public abstract String saveFullDetails();
 
+    /**
+     * Returns a string representation of the task.
+     * The format is "[status] description", where status is either "X" (done) or a space (not done).
+     *
+     * @return a formatted string representation of the task
+     */
     @Override
     public String toString() {
         return String.format("[%s] %s", this.taskStatus(), this.description);
