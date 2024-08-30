465fbbb2c254c89f9366516277113edf9b47bf2a
Chua Tse Hui <htcylsg@gmail.com>
2024-08-30 11:33:05 +0800

    Add more JavaDoc comments

--- src/main/java/knight2103/command/AddCommand.java
+++ src/main/java/knight2103/command/AddCommand.java
@@ -16,16 +16,17 @@ public class AddCommand extends Command {
     }
 
     /**
-     * Executes the Command which comes in three different variations:
-     * mark, unmark, delete to modify the selected task. Depending on the exact
-     * command used, the task will be modified accordingly.
+     * Executes the AddCommand which create the task(s) based on user input and
+     * add them to the list of tasks. Three different types of task can be created which
+     * correspond to their respective AddCommand: Todo, Deadline, Event.
      *
      * @param tasks The class storing the list of tasks found in the bot.
      * @param ui The user interface of the bot.
      * @param storage The class containing the file that saves the list of tasks.
-     * @throws IndexOutOfBoundsException If integer keyed in the command is
-     * out of range of the length of the list of tasks.
-     * @throws NumberFormatException If the predicate part of command is not an Integer.
+     * @throws DateTimeParseException If the deadline in Deadline() is not written in
+     * yyyy-MM-dd format or the start and end time written in Event() is not written in
+     * yyyy-MM-ddThh:mm format.
+     * @throws ArrayIndexOutOfBoundsException If the command is in inappropriate format.
      */
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         Task taskToAdd;
--- src/main/java/knight2103/command/FindCommand.java
+++ src/main/java/knight2103/command/FindCommand.java
@@ -9,6 +9,14 @@ public class FindCommand extends Command {
         super(verb, search); // verb must be CommandVerb.FIND
     }
 
+    /**
+     * Executes the FindCommand which finds all the tasks that contains the description that
+     * matches with the word to be searched and prints out the list of matched tasks.
+     *
+     * @param tasks The class storing the list of tasks found in the bot.
+     * @param ui The user interface of the bot.
+     * @param storage The class containing the file that saves the list of tasks.
+     */
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showFind(tasks, this.predicate);
     };
