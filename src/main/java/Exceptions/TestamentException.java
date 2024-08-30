025e1bd94db0df24e121297760cde21ca957243c
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 13:31:43 +0800

    TestamentException.java: add Javadoc comments

--- src/main/java/Exceptions/TestamentException.java
+++ src/main/java/Exceptions/TestamentException.java
@@ -1,11 +1,22 @@
 package Exceptions;
 
+/**
+ * Represents an exception from the Testament chatbot.
+ */
 public class TestamentException extends Exception {
 
+    /**
+     * Creates a TestamentException with no message.
+     */
     public TestamentException() {
         super();
     }
 
+    /**
+     * Creates a TestamentException with the specified message.
+     *
+     * @param s Message for TestamentException.
+     */
     public TestamentException(String s) {
         super(s);
     }
