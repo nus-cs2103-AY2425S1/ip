aec5c89378bcd6ea1ff5a613d6733b66a9bc9eea
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 11:51:22 +0800

    Events.java: add Javadoc comments

--- src/main/java/Tasks/Events.java
+++ src/main/java/Tasks/Events.java
@@ -3,6 +3,9 @@ package Tasks;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task with a start date and end date.
+ */
 public class Events extends Task {
 
     private LocalDate start;
@@ -10,18 +13,33 @@ public class Events extends Task {
 
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
 
+    /**
+     * Takes in a string for description, string for start date, and string for end date.
+     * Converts string for start and end date to LocalDate
+     *
+     * @param s Description of task
+     * @param start Start date
+     * @param end End date
+     */
     public Events(String s, String start, String end) {
         super(s);
         this.start = LocalDate.parse(start);
         this.end = LocalDate.parse(end);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public String infoForFile() {
         String str = "[E] / " + super.getDetails();
         str += " / " + start + " / " + end;
         return str;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         String str = "";
