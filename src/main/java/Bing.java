962daa982e523607f8d7df69465dd2e6515a347f
samriddh2145 <156680067+samriddh2145@users.noreply.github.com>
2024-08-30 11:48:55 +0800

    Level 0 - Rename main class and message.

---
+++ src/main/java/Bing.java
@@ -0,0 +1,11 @@
+public class Bing {
+    public static void main(String[] args) {
+        String message = "______________________________\n"
+                + "Hello! I'm Bing\n"
+                + "What can I do for you?\n"
+                + "______________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "______________________________";
+        System.out.println(message);
+    }
+}
