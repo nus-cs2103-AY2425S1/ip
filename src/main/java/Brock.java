3cb68d0651648d33d5a2a48c458efad62b5fcd95
YangQF2002 <yangqf18@gmail.com>
2024-08-30 22:53:12 +0800

    Add read, write and delete functionalities

--- src/main/java/Brock.java
+++ src/main/java/Brock.java
@@ -6,6 +6,7 @@ import Tasks.Task;
 import Tasks.ToDos;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -20,6 +21,8 @@ public class Brock {
 
     private static final String HORIZONTAL_LINE = "________________________________________________\n";
     private static final String FILE_PATH = "./src/main/java/saveFile.txt";
+
+    private static final File saveFile = new File(FILE_PATH);
     private static final ArrayList<Task> tasks = new ArrayList<>();
 
     // Helper function to display response with lines
@@ -60,32 +63,6 @@ public class Brock {
         return count;
     }
 
-    private static void createFile() throws IOException {
-        // TODO: Consider implementing some form of slight delay
-        File saveFile = new File(Brock.FILE_PATH);
-
-        displayResponse("Creating parent directories for save file...");
-        boolean directoryStatus = saveFile.getParentFile().mkdirs();
-        if (!directoryStatus) {
-            displayResponse("Parent directories already exists!");
-        } else {
-            displayResponse("Parent directories successfully created!");
-        }
-
-        displayResponse("Creating save file for tasks...");
-        boolean status = saveFile.createNewFile();
-        if (!status) {
-            displayResponse("Save file already exists!");
-        } else {
-            displayResponse("Save file successfully created");
-        }
-    }
-
-    private static void writeToFile() throws IOException {
-        // Creates a file writer object in append mode
-        FileWriter fw = new FileWriter(Brock.FILE_PATH, true);
-    }
-
     // Helper function to get task details as a string
     private static String getDetails(Task task) {
         return "[" + task.getTaskType() + "]"
@@ -104,19 +81,40 @@ public class Brock {
                 : " tasks in the list.");
     }
 
-    // Helper function to perform list command
-    private static void handleList() {
+    private static void createFile() throws IOException {
+        String dirStatus;
+        boolean isDirectoryMissing = Brock.saveFile.getParentFile().mkdirs();
+        if (!isDirectoryMissing) {
+            dirStatus = "Parent directories already exists!";
+        } else {
+            dirStatus = "Parent directories successfully created!";
+        }
+        displayResponse("Creating parent directories for save file...\n"
+                + dirStatus);
+
+        String fileStatus;
+        boolean isFileMissing = Brock.saveFile.createNewFile();
+        if (!isFileMissing) {
+            fileStatus = "Save file already exists!";
+        } else {
+            fileStatus = "Save file successfully created";
+        }
+        displayResponse("Creating save file for tasks...\n"
+                + fileStatus);
+    }
+
+    // Read from file with no exclusion
+    private static String readFromFile() throws BrockException {
         // Use SB as it is a faster way to append strings
         StringBuilder tasksString = new StringBuilder();
-        for (int i = 0; i < Brock.tasks.size(); i++) {
-            Task currTask = Brock.tasks.get(i);
-            if (currTask == null) {
-                break;
+        try {
+            Scanner s = new Scanner(Brock.saveFile);
+            while (s.hasNext()) {
+                tasksString.append(s.nextLine())
+                        .append('\n');
             }
-            String itemNumber = i + 1 + ".";
-            tasksString.append(itemNumber)
-                    .append(getDetails(currTask))
-                    .append('\n');
+        } catch (FileNotFoundException e) {
+            throw new BrockException("Unable to find and read from save file!");
         }
 
         if (!Brock.tasks.isEmpty()) {
@@ -124,13 +122,68 @@ public class Brock {
             // To prevent duplicates when displaying response
             tasksString.deleteCharAt(tasksString.length() - 1);
         }
+        return tasksString.toString();
+    }
 
+    // Read from file with exclusion
+    // Exclusion is the task number to be excluded (ie: deleted)
+    private static String readFromFile(int exclusion) throws BrockException {
+        // Use SB as it is a faster way to append strings
+        StringBuilder tasksString = new StringBuilder();
+        try {
+            Scanner s = new Scanner(Brock.saveFile);
+            int count = 1;
+            boolean hasSeenExclusion = false;
+            while (s.hasNext()) {
+                if (count == exclusion) {
+                    s.nextLine();
+                    count += 1;
+                    hasSeenExclusion = true;
+                    continue;
+                }
+                if (hasSeenExclusion) {
+                    // s.next() reads: "<task number>."
+                    // remove the ., modify the task number, append to string builder
+                    String nextToken = s.next();
+                    String taskNumber = nextToken.substring(0, nextToken.length() - 1);
+                    int newTaskNumber = Integer.parseInt(taskNumber) - 1;
+                    tasksString.append(newTaskNumber)
+                            .append('.')
+                            .append(s.nextLine());
+                } else {
+                    tasksString.append(s.nextLine())
+                            .append('\n');
+                }
+                count += 1;
+            }
+        } catch (FileNotFoundException e) {
+            throw new BrockException("Unable to find and read from save file!");
+        }
+
+        if (!Brock.tasks.isEmpty()) {
+            // Remove last new line character
+            // To prevent duplicates when displaying response
+            tasksString.deleteCharAt(tasksString.length() - 1);
+        }
+        return tasksString.toString();
+    }
+
+    private static void writeToFile(String writeContent, boolean isAppendMode) throws IOException {
+        FileWriter fw = new FileWriter(Brock.FILE_PATH, isAppendMode);
+        fw.write(writeContent);
+        fw.close();
+    }
+
+    // Helper function to perform list command
+    private static void handleList() throws BrockException {
+        String tasksString = readFromFile();
         int totalTasks = numTasks();
+
         String responseBody;
         if (totalTasks == 0) {
             responseBody = "No current tasks!";
         } else {
-            responseBody = tasksString.toString();
+            responseBody = tasksString;
         }
 
         displayResponse((totalTasks == 1
@@ -200,11 +253,10 @@ public class Brock {
         if (taskNumber > totalTasks || taskNumber < 1) {
             throw new BrockException("Task number does not exist!");
         }
-
     }
 
     // Helper function to validate & perform the delete command (if correct)
-    private static void handleDelete(String command) throws BrockException {
+    private static void handleDelete(String command) throws BrockException, IOException {
         validateDelete(command);
 
         int targetIndex = getTargetIndex(command);
@@ -215,6 +267,10 @@ public class Brock {
                 + getDetails(removedTask)
                 + '\n'
                 + getTaskSummary());
+
+        String fileContents = readFromFile(targetIndex + 1);
+        writeToFile("", false);
+        writeToFile(fileContents, true);
     }
 
     // Helper function to create task and return the created task
@@ -242,14 +298,14 @@ public class Brock {
                         .append(" ");
             }
             StringBuilder dueDate = new StringBuilder();
-            boolean dueDateActive = false;
+            boolean hasSeenDueDate = false;
             for (String word : commandWords) {
-                if (dueDateActive) {
+                if (hasSeenDueDate) {
                     dueDate.append(word)
                             .append(" ");
                 }
                 if (word.equalsIgnoreCase("/by")) {
-                    dueDateActive = true;
+                    hasSeenDueDate = true;
                 }
             }
 
@@ -263,23 +319,23 @@ public class Brock {
         } else if (firstWord.equalsIgnoreCase("event")) {
             StringBuilder startDate = new StringBuilder();
             StringBuilder endDate = new StringBuilder();
-            boolean startDateActive = false;
-            boolean endDateActive = false;
+            boolean hasSeenStartDate = false;
+            boolean hasSeenEndDate = false;
             for (String word : commandWords) {
                 if (word.equalsIgnoreCase("/from")) {
-                    startDateActive = true;
+                    hasSeenStartDate = true;
                     continue;
                 }
                 if (word.equalsIgnoreCase("/to")) {
-                    startDateActive = false;
-                    endDateActive = true;
+                    hasSeenStartDate = false;
+                    hasSeenEndDate = true;
                     continue;
                 }
-                if (startDateActive) {
+                if (hasSeenStartDate) {
                     startDate.append(word)
                             .append(" ");
                 }
-                if (endDateActive) {
+                if (hasSeenEndDate) {
                     endDate.append(word)
                             .append(" ");
                 }
@@ -313,6 +369,11 @@ public class Brock {
         return task;
     }
 
+
+//    private static void loadTasks() {
+//
+//    }
+
     public static void main(String[] args) {
         // Create a scanner object
         // Reads from standard system input
@@ -326,6 +387,8 @@ public class Brock {
             return;
         }
 
+        // Initialize the ArrayList with any pre-existing tasks from save file
+        
         // Initial message
         String initialResponse = "Hello! I'm Brock\n"
                 + "What can I do for you?";
@@ -340,10 +403,19 @@ public class Brock {
                     .replaceAll(" +", " ");
             if (command.equalsIgnoreCase("bye")) {
                 displayResponse("Bye. Hope to see you again soon!");
+                try {
+                    writeToFile("", false);
+                } catch (IOException e) {
+                    displayResponse("Failed to reset save file!");
+                }
                 break;
 
             } else if (command.equalsIgnoreCase("list")) {
-                handleList();
+                try {
+                    handleList();
+                } catch (BrockException e) {
+                    displayResponse(e.getMessage());
+                }
 
                 // Case-insensitive, convert to lower case then process
             } else if (command.toLowerCase().startsWith("mark")) {
@@ -365,6 +437,8 @@ public class Brock {
                     handleDelete(command);
                 } catch (BrockException e) {
                     displayResponse(e.getMessage());
+                } catch (IOException e) {
+                    displayResponse("Failed to reset the save file!");
                 }
 
             } else {
@@ -377,8 +451,17 @@ public class Brock {
                             + getDetails(task)
                             + '\n'
                             + getTaskSummary());
+
+                    String taskNumber = Integer.toString(numTasks());
+                    writeToFile(taskNumber
+                                    + ". "
+                                    + getDetails(task)
+                                    + '\n'
+                            , true);
                 } catch (BrockException e) {
                     displayResponse(e.getMessage());
+                } catch (IOException e) {
+                    displayResponse("Failed to write task into save file!");
                 }
             }
         }
