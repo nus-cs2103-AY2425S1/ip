7db137f750e77183e89804cd83fba4d6b12f99e7
yugan01 <E1032496@u.nus.edu>
2024-08-31 01:23:05 +0800

    Add datetime functionality for deadline
    Datetime in the format of "MMM dd yyyy HH:mm" or "dd-mm-yyyy"

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,32 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.IntStream;
+
 public class Deadline extends Task{
 
     protected String by;
+    protected LocalDateTime deadline;
+    protected final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        if (by.contains("-")) {
+            String[] parseDT = by.split("-");
+            this.deadline = LocalDateTime.of(Integer.parseInt(parseDT[0]),
+                    Integer.parseInt(parseDT[1]), Integer.parseInt(parseDT[2]), 23, 59);
+        } else {
+            this.deadline = LocalDateTime.parse(by, FORMAT);
+        }
     }
 
     @Override
     public String write() {
-        return "D" + super.write() + " | " + by;
+        return "D" + super.write() + " | " + deadline.format(FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + deadline.format(FORMAT) + ")";
     }
 }
