19eef6cdadaf0cb304469ede4cf7952dfbdfc321
rayray39 <e0772883@u.nus.edu>
2024-08-30 16:04:57 +0800

    JanetException.java: Add JavaDoc to class

--- src/main/java/janet/JanetException.java
+++ src/main/java/janet/JanetException.java
@@ -1,5 +1,8 @@
 package janet;
 
+/**
+ * Represents an exception that has unique messages to Janet.
+ */
 public class JanetException extends Exception {
 
     public JanetException(String errorMessage) {
