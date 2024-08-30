826af080baa3e263f35a43cb5f64404248bc165d
estellelim <estlim@hotmail.com>
2024-08-30 18:54:45 +0800

    Add JUnit tests

---
+++ src/test/java/muffin/TaskListTest.java
@@ -0,0 +1,27 @@
+package muffin;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void addTaskTest() {
+        // Tests add and get functionality of TaskList class
+        TaskList list = new TaskList(new ArrayList<Task>());
+        Todo t = new Todo("submit assignment");
+        list.add(t);
+        assertEquals(t, list.get(0));
+    }
+
+    @Test
+    public void markTest() {
+        TaskList list = new TaskList(new ArrayList<Task>());
+        Deadline d = new Deadline("submit assignment", "2024-09-01");
+        list.add(d);
+        list.mark(0);
+        assertEquals(d.isDone, list.get(0).isDone);
+    }
+}
