062b3128d61fd2a017f835a2c02fa22bf43efb15
LinkesV <linkesvarun@gmail.com>
2024-08-30 12:02:31 +0800

    Update JUnit

--- src/main/java/cypherchatbot/command/Command.java
+++ src/main/java/cypherchatbot/command/Command.java
@@ -8,7 +8,7 @@ import cypherchatbot.util.Ui;
 /**
  * The Command class is an abstract class that represents a user command
  * in the Cypher Chat Bot Application. Each command has two methods, one is execute which will carry
- * out the Command and the isExit method which inform the main Event loop wether the application is
+ * out the Command and the isExit method which inform the main Event loop weather the application is
  * ending.
  */
 public abstract class Command {
