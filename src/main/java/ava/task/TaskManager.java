02ce1d5bce3020d9e42a0f0539b23a763a6d28a7
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2024-08-30 15:44:05 +0800

    add basic test files

--- src/main/java/ava/task/TaskManager.java
+++ src/main/java/ava/task/TaskManager.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
+
     private final List<Task> tasks;
     private final FileManager fileManager;
     public TaskManager() {
---
+++ src/main/java/ava/task/TaskManagerTest.java
@@ -0,0 +1,30 @@
+package ava.task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void getTasks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addTask() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddTask() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddTask1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddTask2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeTask() {
+    }
+}
