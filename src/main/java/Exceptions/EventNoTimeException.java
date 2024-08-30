9e1f2dbacb9ac9d345f45f220fd8e4f48e523e80
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 13:38:06 +0800

    EventNoTimeException.java: add Javadoc comments

--- src/main/java/Exceptions/EventNoTimeException.java
+++ src/main/java/Exceptions/EventNoTimeException.java
@@ -1,7 +1,13 @@
 package Exceptions;
 
+/**
+ * An exception that occurs when an event description is missing the necessary dates
+ */
 public class EventNoTimeException extends TestamentException {
 
+    /**
+     * Constructor for EventNoTimeException
+     */
     public EventNoTimeException() {
         super("You have not provided the time for this event.\nThe format for events is as follows:\n" +
                 "event (details) /from (time) /to (time)");
