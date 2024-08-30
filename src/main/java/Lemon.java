f42e65a3ab628e1fe6c19da535a6710114f89772
Dino-Nuggies <heyiheng2130@gmail.com>
2024-08-30 12:30:30 +0800

    Add enum and switch case
    Replaced the if else with enum and switch cases

--- src/main/java/Lemon.java
+++ src/main/java/Lemon.java
@@ -20,30 +20,45 @@ public class Lemon {
     /*** Initialising ***/
     ArrayList<Task> tasks = new ArrayList<>();
     int numTasks = 0;
+
+    enum Commands {
+        BYE,
+        LIST,
+        MARK,
+        UNMARK,
+        TODO,
+        EVENT,
+        DEADLINE,
+        DELETE
+    }
     
     public static void main(String[] args) {
         Lemon lemon = new Lemon();
         Scanner scan = new Scanner(System.in);
-        String input = null;
+        boolean run = true;
 
-        /*** Program starts here ***/
+        /** Program starts here **/
         System.out.print(lemon.logoMsg);
 
-        while (true) {
+        while (run) {
             System.out.println(lemon.barMsg);
 
-            input = scan.next();
+            String input = scan.next().toUpperCase();
             System.out.println(lemon.barMsg);
             try {
-                if (input.equals("bye") || input.equals("Bye")) {
+                Commands command = Commands.valueOf(input);
+                switch (command) {
+                case BYE:
+                    run = false;
                     break;
-                } else if (input.equals("list") || input.equals("List")) {
+                case LIST:
                     if (lemon.numTasks == 0) System.out.println(lemon.emptyListMsg);
                     else {
                         System.out.println(lemon.listMsg);
                         lemon.printList();
                     }
-                } else if (input.equals("mark") || input.equals("Mark")) {
+                    break;
+                case MARK: {
                     int next = scan.nextInt();
                     if (next > lemon.numTasks || next <= 0) {
                         throw new InvalidCommandException(" OOPS!!! Please select a valid task");
@@ -53,7 +68,9 @@ public class Lemon {
                     lemon.tasks.get(next - 1).markDone();
 
                     System.out.println("   " + lemon.tasks.get(next - 1).toString());
-                } else if (input.equals("unmark") || input.equals("Unmark")) {
+                    break;
+                }
+                case UNMARK: {
                     int next = scan.nextInt();
                     if (next > lemon.numTasks || next <= 0) {
                         throw new InvalidCommandException(" OOPS!!! Please select a valid task");
@@ -63,36 +80,45 @@ public class Lemon {
                     lemon.tasks.get(next - 1).unmarkDone();
 
                     System.out.println("   " + lemon.tasks.get(next - 1).toString());
-                } else if (input.equals("todo") || input.equals("TODO")) {
+                    break;
+                }
+                case TODO: {
                     String next = scan.nextLine();
 
                     lemon.addNewTask(new Todo(next));
-                } else if (input.equals("deadline") || input.equals("Deadline")) {
+                    break;
+                }
+                case DEADLINE: {
                     String[] next = scan.nextLine().split("/by ");
 
                     lemon.addNewTask(new Deadline(next[0], next[1]));
-                } else if (input.equals("event") || input.equals("Event")) {
+                    break;
+                }
+                case EVENT: {
                     String[] next = scan.nextLine().split("/from ");
 
                     lemon.addNewTask(new Event(next[0], next[1]));
-                } else if (input.equals("delete") || input.equals("Delete")) {
+                }
+                case DELETE: {
                     int next = scan.nextInt();
                     if (next > lemon.numTasks || next <= 0) {
                         throw new InvalidCommandException(" OOPS!!! Please select a valid task");
                     }
 
                     lemon.deleteTask(next);
-                } else {
-                    throw new InvalidCommandException(" OOPS!!! I'm sowwy, but I don't know what that means :-(\n\n" +
-                            " I can help you add tasks with \"todo\", \"deadline\", \"event\"\n" +
-                            " I can also keep track of all your tasks with \"list\"\n" +
-                            " If you wanna update certain tasks, use \"mark\" or \"unmark\" and then its number\n");
+                }
                 }
             } catch (InvalidCommandException | DescriptionException e) {
                 System.out.println(e.getMessage());
                 scan.nextLine();
             } catch (InputMismatchException e) {
                 System.out.println(" OOPS!!! I cant understand your mark/unmark, please select using numbers xd");
+                scan.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(" OOPS!!! I'm sowwy, but I don't know what that means :-(\n\n" +
+                        " I can help you add tasks with \"todo\", \"deadline\", \"event\"\n" +
+                        " I can also keep track of all your tasks with \"list\"\n" +
+                        " If you wanna update certain tasks, use \"mark\" or \"unmark\" and then its number");
                 scan.nextLine();
             }
         }
