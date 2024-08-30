c618edeb165ff94f9a4625b61f9bf9360fc891ad
liauzhanyi <liauzhanyi@gmail.com>
2024-08-30 16:43:02 +0800

    chore: Add JavaDoc comments for Deadline class

--- src/main/java/zbot/task/Deadline.java
+++ src/main/java/zbot/task/Deadline.java
@@ -1,16 +1,31 @@
 package zbot.task;
 
 import java.time.LocalDateTime;
+
 import zbot.Parser;
 
+/**
+ * Represents a deadline with a description and due datetime.
+ */
 public class Deadline extends Task {
     protected LocalDateTime by;
 
+    /**
+     * Constructor for Deadline.
+     *
+     * @param description Description of the deadline.
+     * @param by          Due date of the deadline.
+     */
     public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
+    /**
+     * Returns the due datetime of the deadline.
+     *
+     * @return Due datetime of the deadline.
+     */
     public LocalDateTime getBy() {
         return by;
     }
