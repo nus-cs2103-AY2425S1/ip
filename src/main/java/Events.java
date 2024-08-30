c52f93bd6cc05eb301c764ca7c694a0009eeb81a
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-08-30 19:10:11 +0800

    Add toFile method for Events

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -9,6 +9,11 @@ public class Events extends Task {
     }
 
     @Override
+    public String toFile() {
+        return "E | " + (super.isDone() ? "1" : "0") + " | " + super.getDescription() + " | " + this.start + "-" + this.end;
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
     }
