ea1f87fd66b4cf68e5ce84921f271479f768b786
xGladiate <sharonsohxuanhui@hotmail.com>
2024-08-30 10:59:39 +0800

    Add javadoc commit to TaskType

--- src/main/java/xbot/task/TaskType.java
+++ src/main/java/xbot/task/TaskType.java
@@ -1,5 +1,27 @@
 package xbot.task;
 
+/**
+ * Represents the different types of tasks in the XBot application.
+ * Each task type is represented by a single character:
+ * <ul>
+ *     <li>{@code T}: ToDo task</li>
+ *     <li>{@code D}: Deadline task</li>
+ *     <li>{@code E}: Event task</li>
+ * </ul>
+ */
 public enum TaskType {
-    T, D, E
+    /**
+     * Represents a ToDo Task.
+     */
+    T,
+
+    /**
+     * Represents a Deadline Task.
+     */
+    D,
+
+    /**
+     * Represents a Event Task.
+     */
+    E
 }
