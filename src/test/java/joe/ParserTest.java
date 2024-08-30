b2149539384ecbe9ea12d33eff975eefd79ad825
Wei Lian <weilian2003@live.com>
2024-08-30 10:37:58 +0800

    Add test for parser class

---
+++ src/test/java/joe/ParserTest.java
@@ -0,0 +1,16 @@
+package joe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class ParserTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void anotherTest() {
+        assertEquals(2,3);
+    }
+
+}
