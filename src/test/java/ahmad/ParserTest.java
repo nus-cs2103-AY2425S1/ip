30952c11e286e7abfac38884eb89ee8b8e8ca93e
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-08-30 15:54:50 +0800

    Add test folder

---
+++ src/test/java/ahmad/ParserTest.java
@@ -0,0 +1,13 @@
+package ahmad;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ParserTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 3);
+    }
+}
