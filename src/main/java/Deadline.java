ec5bcd186f94c6068c467d5641f556ca1abe40f1
kimxw <kimaya.wanjari@nuscomputing.com>
2024-08-30 18:30:49 +0800

    Modify date fields to be of type LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    protected String due;
+    protected LocalDateTime due;
     public Deadline(String description, String due) {
         super(description);
-        this.due = due;
+        this.due = Task.dateTimeParser(due);
     }
     @Override
     public String toString() {
         String mark = isDone ? "X" : " ";
-        return String.format("[D] [%s] %s (by: %s)", mark, super.description, this.due);
+        String due = this.due.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mma"));
+        if(due.contains("12:00am")) {
+            due = due.substring(0, due.length() - 8);
+        }
+        return String.format("[D] [%s] %s (by: %s)", mark, super.description, due);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = Task.dateTimeParser(from);
+        this.to = Task.dateTimeParser(to);
     }
     @Override
     public String toString() {
         String mark = isDone ? "X" : " ";
-        return String.format("[E] [%s] %s (from: %s to: %s)", mark, super.description, this.from, this.to);
+        String from = this.from.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mma"));
+        String to = this.to.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mma"));
+
+        if(from.endsWith("12:00am")) {
+            from = from.substring(0, from.length() - 8);
+        }
+
+        if(to.endsWith("12:00am")) {
+            to = to.substring(0, to.length() - 8);
+        }
+        return String.format("[E] [%s] %s (from: %s to: %s)", mark, super.description, from, to);
     }
 }
