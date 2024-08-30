d46d60896442a26bd9563ba8202bfc52b8acf449
Nihirraa <e1186316@u.nus.edu>
2024-08-30 12:45:57 +0800

    Level1

--- src/main/java/Espresso.java
+++ src/main/java/Espresso.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Espresso {
     public static void main(String[] args) {
         System.out.println("_________________________________________");
         System.out.println("Hello! I'm Espresso");
         System.out.println("What can I do for you?");
         System.out.println("_________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_________________________________________");
+        Scanner sc = new Scanner(System.in);
+        String str;
+        while (true) {
+            str = sc.nextLine();
+
+            if (str.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(str);
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 }
