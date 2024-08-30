9e64b78ddbad42f28a14a22c0e45097ee1d44806
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-30 11:18:33 +0800

    Implement toFilesString for Todo class

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -9,4 +9,10 @@ public class Todo extends Task{
     public String toString() {
         return "[T]" + super.toString();
     }
+
+    @Override
+    public String toFileString() {
+        int markedInt = this.isMarked() ? 1 : 0;
+        return String.format("T | %d | %s", markedInt, this.getName());
+    }
 }
