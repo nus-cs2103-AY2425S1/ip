3931161ee7795f081a03ba1142405d9539f5b73c
Quasant <benjaminthechin@gmail.com>
2024-08-30 11:44:13 +0800

    Add support for tracking three types of tasks

--- src/main/java/Alvis.java
+++ src/main/java/Alvis.java
@@ -9,34 +9,57 @@ public class Alvis {
         System.out.println("So you require my assistance?");
 
         while (true) {
-            String[] userInput = sc.nextLine().toLowerCase().split(" ");
-            switch (userInput[0]) {
+            String userInput = sc.nextLine().toLowerCase();
+            switch (userInput.split(" ")[0]) {
             case "bye":
-                System.out.println("Understood.");
+                System.out.println("May we meet again");
                 break;
             case "list":
                 System.out.println("Let me help you to remember what you need to do ...");
                 for (int i = 0; i < tracker.size(); ++i) {
-                        Task task = tracker.get(i);
-                        String statusIcon = task.getStatus();
-                        String desc = task.getDesc();
-                        System.out.println("" + (i+1) + ". " + statusIcon + " " + desc);
+                        System.out.println("" + (i+1) + ". " + tracker.get(i).toString());
                     }
                 break;
             case "toggle":
                 try {
-                    int index = Integer.parseInt(userInput[1]) - 1;
+                    int index = Integer.parseInt(userInput.substring(7)) + 1;
                     tracker.get(index).toggleStatus();
                     System.out.println("As requested, I have changed the status of your task");
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("I cannot do that, it is not a valid index");
+                    System.out.println("I cannot do that, the index is invalid");
                 }
                 break;
+            case "todo":
+                Task toDo = new ToDo(userInput.substring(5));
+                addTask(toDo, tracker);
+                break;
+            case "deadline":
+                int byIndex = userInput.indexOf("/by");
+                Task deadline = new Deadline(userInput.substring(9, byIndex), userInput.substring(byIndex + 4));
+                addTask(deadline, tracker);
+                break;
+            case "event":
+                int fromIndex = userInput.indexOf("/from");
+                int toIndex = userInput.indexOf("/to");
+                Task event = new Event(userInput.substring(6, fromIndex), userInput.substring(fromIndex + 5, toIndex),
+                        userInput.substring(toIndex + 4));
+                addTask(event, tracker);
+                break;
             default:
-                Task task = new Task(userInput[0]);
-                tracker.add(task);
-                System.out.println("Added: " + task.getDesc());
+                System.out.println("I don't recognise that. Type /help for the commands");
             }
         }
     }
+
+    public static void addTask(Task task, List<Task> tracker) {
+        tracker.add(task);
+        System.out.println("Understood, I have added to your list: \n" + task.toString());
+        System.out.println("You have " + tracker.size() + " tasks");
+    }
+
+//    public static int[] getDelimiterIndex (String input) {
+//        int firstIdx = input.indexOf("/");
+//        int secondIdx = input.indexOf("/", firstIdx + 1);
+//        return new int[]{firstIdx, secondIdx};
+//    }
 }
