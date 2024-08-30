296344b8f63c488f34d9d6f23ca6aeae6c5a8643
Nicholas Chua <e1114227@u.nus.edu>
2024-08-30 17:38:44 +0800

    Ui.java: Add say, greet, sayFarewell, printTaskList, getInput methods

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,31 +1,40 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ui {
-    static String line = "____________________________________________________________";
+    Scanner scanner;
+    String line = "____________________________________________________________";
 
-    public static void say(String message) {
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+    void say(String message) {
         System.out.println(line);
         System.out.println(message);
         System.out.println(line);
     }
 
-    public static void greet() {
+    void greet() {
         String message = "Hello! I'm Fred\n" +
                 "What can I do for you?";
         say(message);
     }
 
-    public static void sayFarewell() {
+    void sayFarewell() {
         String message = "Bye. Hope to see you again soon!";
         say(message);
     }
 
-    static void printTaskList(ArrayList<Task> taskList) {
+    void printTaskList(ArrayList<Task> taskList) {
         int index = 1;
         System.out.println(line);
         for (Task task : taskList) {
             System.out.println(String.format("%s.%s", index++, task));
         }
         System.out.println(line);
+    }
+
+    String getInput() {
+        return scanner.nextLine();
     }
 }
