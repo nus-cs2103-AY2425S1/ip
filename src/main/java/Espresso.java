4853d373f1d748342222e922cf529616fbeaa818
Nihirraa <e1186316@u.nus.edu>
2024-08-30 11:48:43 +0800

    Rename main class and add greeting - Level-0

---
+++ src/main/java/Espresso.java
@@ -0,0 +1,10 @@
+public class Espresso {
+    public static void main(String[] args) {
+        System.out.println("_________________________________________");
+        System.out.println("Hello! I'm Espresso");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________");
+    }
+}
