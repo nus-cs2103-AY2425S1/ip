adb787c1179af5ad56571e578f10ae5c0cdc9f42
Lynette Lee <sayhitosiying@Lynettes-Laptop.local>
2024-08-30 23:44:28 +0800

    Add Junit Tests

---
+++ src/test/java/socchat/ParserTest.java
@@ -0,0 +1,36 @@
+package socchat;
+
+import org.junit.jupiter.api.Test;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+public class ParserTest {
+
+    @Test
+    public void parseDate_correctFormat_success() throws SocchatException {
+        String validDate = "2024-12-27 14:00";
+        LocalDateTime expectedDate = LocalDateTime.of(2024, 12, 27, 14, 0);
+        assertEquals(expectedDate, Parser.parseDate(validDate));
+    }
+
+    @Test
+    public void parseDate_incorrectFormat_exceptionThrown() {
+        try {
+            String invalidDate = "2024-27-12 14:00";
+            LocalDateTime expectedDate = LocalDateTime.of(2024, 12, 27, 14, 0);
+            assertEquals(expectedDate, Parser.parseDate(invalidDate));
+            fail();
+        } catch (Exception e) {
+            assertEquals("Please enter your dateTime as this format --- yyyy-MM-dd HH:mm", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testDateToString() throws SocchatException {
+        String expectedDate = "2024-12-27 14:00";
+        LocalDateTime validDate = LocalDateTime.of(2024, 12, 27, 14, 0);
+        assertEquals(expectedDate, Parser.dateToString(validDate));
+    }
+
+}
---
+++ src/test/java/socchat/StorageTest.java
@@ -0,0 +1,60 @@
+package socchat;
+import org.junit.jupiter.api.Test;
+import socchat.task.Task;
+import socchat.task.todo.Todo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class StorageTest {
+
+    @Test
+    public void load_fileExist_success() throws Exception {
+        File f = new File("test.txt");
+        if(!f.exists()) {
+            f.createNewFile();
+        }
+        Storage storage = new Storage("test.txt");
+        ArrayList<Task> tasks = new ArrayList<>();
+        assertEquals(tasks, Storage.load());
+    }
+
+    @Test
+    public void load_fileNotExist_exceptionThrown() throws Exception {
+        try {
+            Storage storage = new Storage("fake_test.txt");
+            ArrayList<Task> tasks = new ArrayList<>();
+            assertEquals(tasks, Storage.load());
+            fail();
+        } catch (SocchatException e) {
+            assertEquals( "Storage file not found", e.getMessage());
+        }
+    }
+
+    @Test
+    public void updateTest() throws IOException {
+        String fileName = "update_test.txt";
+        String expectedContent = "T | Not done | test check\n";
+        File file = new File(fileName);
+        Task t = new Todo("test check");
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(t);
+        file.createNewFile();
+
+        Storage storage = new Storage(fileName);
+        Storage.update(tasks, false);
+        String actualContent = new String(Files.readAllBytes(Paths.get(fileName)));
+        assertEquals(expectedContent, actualContent);
+
+        file.delete();
+    }
+}
