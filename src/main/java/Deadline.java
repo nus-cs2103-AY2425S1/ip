1ecb32d210ac08893ab42bc6075bd79b0ec823dc
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-08-30 11:36:36 +0800

    Implement date and time handling

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    private LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
-    }
-
-    @Override
-    public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        this.by = LocalDate.parse(by);
     }
 
     @Override
     public String toSaveFormat() {
         return "D | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + by;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,28 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String from;
-    private String to;
+    private LocalDate from;
+    private LocalDate to;
+
+    // Using a date-only format
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
-    }
-
-    @Override
-    public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        this.from = LocalDate.parse(from, DATE_FORMATTER);
+        this.to = LocalDate.parse(to, DATE_FORMATTER);
     }
 
     @Override
     public String toSaveFormat() {
-        return "E | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + from + " | " + to;
+        return "E | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + from.format(DATE_FORMATTER) + " | " + to.format(DATE_FORMATTER);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (from: " + from.format(DateTimeFormatter.ofPattern("MMM d yyyy")) +
+                " to: " + to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
