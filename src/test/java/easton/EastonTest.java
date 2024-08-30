33aed76ade161230dc91dc649c79a82dde1c0000
stephaneleong <stephaneleong@gmail.com>
2024-08-30 16:09:43 +0800

    Add test cases for createDeadline

--- src/test/java/easton/EastonTest.java
+++ src/test/java/easton/EastonTest.java
@@ -1,9 +1,7 @@
 package easton;
 
-import easton.model.ToDo;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -22,6 +20,38 @@ public class EastonTest {
         String input = "todo hello world";
         assertDoesNotThrow(() -> {
             Easton.createToDo(input);
+        });
+    }
+
+    @Test
+    public void createDeadline_emptyDescription_exceptionThrown() {
+        String input = " deadline ";
+        assertThrows(EmptyDescriptionException.class, () -> {
+            Easton.createDeadline(input);
+        });
+    }
+
+    @Test
+    public void createDeadline_withoutByLabel_exceptionThrown() {
+        String input = " deadline read book";
+        assertThrows(InvalidFormatException.class, () -> {
+            Easton.createDeadline(input);
+        });
+    }
+
+    @Test
+    public void createDeadline_incorrectDateTimeFormat_exceptionThrown() {
+        String input = " deadline read book /by 23-06-2001 2300";
+        assertThrows(DateTimeFormatException.class, () -> {
+            Easton.createDeadline(input);
+        });
+    }
+
+    @Test
+    public void createDeadline_validInput_success() {
+        String input = " deadline read book /by 23/06/2001 23:00";
+        assertDoesNotThrow(() -> {
+            Easton.createDeadline(input);
         });
     }
 }
