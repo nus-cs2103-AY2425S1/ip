74a5cc441bbb398ee05fd483a4175119929fd363
Ng Kay Hian <e1113766@u.nus.edu>
2024-08-30 20:06:40 +0800

    Add JUnit tests
    - TaskListTest for TaskList class
    - AddCommandTest for AddCommand class

---
+++ src/test/java/rotodo/commands/AddCommandTest.java
@@ -0,0 +1,90 @@
+package rotodo.commands;
+
+import org.junit.jupiter.api.Test;
+
+import rotodo.commands.AddCommand.TaskType;
+import rotodo.exception.InvalidInputException;
+import rotodo.tasklist.TaskList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AddCommandTest {
+    @Test
+    public void initialise_givenTaskType_addCorrectTypedTask() {
+        // Arrange
+        TaskList tasklist = new TaskList();
+        AddCommand addTodo;
+        AddCommand addDeadline;
+        AddCommand addEvent;
+        String expected = "1.[T][ ] Todo task\n"
+            + "2.[D][ ] Deadline task (by: 01/01/2001 0102)\n"
+            + "3.[E][ ] Event task (from: 01/01/2001 0102 to: 02/02/2003 0203)";
+        
+        // Act
+        try {
+            addTodo = new AddCommand(TaskType.TODO,
+                new String[] {
+                    "Todo task"
+                });
+            addDeadline = new AddCommand(TaskType.DEADLINE,
+                new String[] {
+                    "Deadline task",
+                    "01/01/2001 0102"
+                });
+            addEvent = new AddCommand(TaskType.EVENT,
+                new String[] {
+                    "Event task",
+                    "01/01/2001 0102",
+                    "02/02/2003 0203"
+                });
+            addTodo.execute(tasklist, null, null);
+            addDeadline.execute(tasklist, null, null);
+            addEvent.execute(tasklist, null, null);
+        } catch (Exception e) {
+            // should not have exception here
+        }
+        
+        // Assert
+        String actual = tasklist.toString();
+        assertEquals(expected, actual, "AddCommand added the task wrongly, or task details are wrong.");
+    }
+
+    @Test
+    public void initialise_wrongDateTimeFormat_throwsInvalidInputException() {
+        // Arrange
+        String expected = "\u001B[31mRoTodo.Error: Invalid Input\n"
+        + "  Whaaaatt? RoTodo has no idea what date that is\n"
+        + "RoTodo needs valid date/time in the form:\n"
+        + "  dd/MM/yyyy HHmm" + "\u001B[0m\n"
+        + "type 'help' to see guide";
+
+        // Act
+        InvalidInputException exception = assertThrows(InvalidInputException.class, 
+            () -> new AddCommand(TaskType.DEADLINE,
+                new String[] { "dummy task", "some bad datetime" })
+        );
+
+        // Assert
+        assertEquals(expected, exception.getMessage());
+    }
+
+    @Test
+    public void initialise_insufficientInputParams_throwsIncompleteInputException() {
+        // Arrange
+        String expected = "\u001B[31mRoTodo.Error: Invalid Input\n"
+        + "  RoTodo can't read you mind, otherwise "
+        + "RoTodo's creator would be rich!\n"
+        + "  RoTodo needs a task description, from and to date/time" 
+        + "\u001B[0m\n" + "type 'help' to see guide";
+
+        // Act
+        InvalidInputException exception = assertThrows(InvalidInputException.class, 
+            () -> new AddCommand(TaskType.EVENT,
+                new String[] { "dummy task", "01/01/2001 0102" })
+        );
+
+        // Assert
+        assertEquals(expected, exception.getMessage());
+    }
+}
---
+++ src/test/java/rotodo/tasklist/TaskListTest.java
@@ -0,0 +1,87 @@
+package rotodo.tasklist;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    interface TaskInfo {}
+    public class TodoInfo {
+        String value;
+
+    }
+    public class DeadlineInfo implements TaskInfo{
+        String value;
+        LocalDateTime by;
+    }
+    public class EventInfo implements TaskInfo {
+        String value;
+        LocalDateTime from;
+        LocalDateTime to;
+    }
+
+    @Test
+    public void add_tasks_populatesListWithTasks() {
+        // Arrange
+        TaskList tasklist = new TaskList();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        LocalDateTime date1 = LocalDateTime.parse("01/01/2001 0102", formatter);
+        LocalDateTime date2 = LocalDateTime.parse("02/02/2003 0203", formatter);
+        String expected = "1.[T][ ] Todo task\n"
+            + "2.[D][ ] Deadline task (by: 01/01/2001 0102)\n"
+            + "3.[E][ ] Event task (from: 01/01/2001 0102 to: 02/02/2003 0203)";
+
+        // Act
+        tasklist.addTask("Todo task");
+        tasklist.addTask("Deadline task", date1);
+        tasklist.addTask("Event task", date1, date2);
+
+        // Assert
+        String actual = tasklist.toString();
+        assertEquals(expected, actual, "TaskList printed is wrong");
+    }
+
+    @Test
+    public void mark_taskAsDone_setTaskToDone() {
+        // Arrange
+        TaskList tasklist = new TaskList();
+        tasklist.addTask("dummy task");
+        String expected = "1.[T][X] dummy task";
+
+        // Act
+        try {
+            tasklist.markDone(0);
+        } catch (Exception e) {
+            // should not have exception here
+        }
+        
+        // Assert
+        String actual = tasklist.toString();
+        assertEquals(expected, actual, 
+            String.format("Task should be marked as done, but is not."));
+    }
+
+    @Test
+    public void unmark_taskAsUndone_setTaskToUndone() {
+        // Arrange
+        TaskList tasklist = new TaskList();
+        tasklist.setNextStatus(true);
+        tasklist.addTask("dummy task");
+        String expected = "1.[T][ ] dummy task";
+
+        // Act
+        try {
+            tasklist.unmarkDone(0);
+        } catch (Exception e) {
+            ;
+        }
+        
+        // Assert
+        String actual = tasklist.toString();
+        assertEquals(expected, actual, 
+            String.format("Task should be marked as undone, but is not."));
+    }
+}
