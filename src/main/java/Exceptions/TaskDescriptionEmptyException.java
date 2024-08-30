26cf0c93f7929eb9a26773d8489aee3f40928cd8
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 13:33:17 +0800

    TaskDescriptionEmptyException.java: add Javadoc comments

--- src/main/java/Exceptions/TaskDescriptionEmptyException.java
+++ src/main/java/Exceptions/TaskDescriptionEmptyException.java
@@ -1,7 +1,15 @@
 package Exceptions;
 
+/**
+ * An exception that occurs when the task description is empty
+ */
 public class TaskDescriptionEmptyException extends TestamentException {
 
+    /**
+     * Creates a TaskDescriptionEmptyException for the specified type of Task
+     *
+     * @param s Type of Task
+     */
     public TaskDescriptionEmptyException(String s) {
         super(String.format("It appears that you have not provided any details for this %s.",
                 s));
