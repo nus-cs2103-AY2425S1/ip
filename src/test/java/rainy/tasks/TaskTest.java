05d41ce722a26b3d83a07918bfc3a927f1e032be
SooYap <tansooyap@gmail.com>
2024-08-30 18:52:33 +0800

    TaskTest.java: Create new TaskTest class to test the mark and unmark methods

---
+++ src/test/java/rainy/tasks/TaskTest.java
@@ -0,0 +1,34 @@
+package rainy.tasks;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import rainy.rainyexceptions.InvalidIndexException;
+import rainy.rainyexceptions.InvalidMarkAndUnmarkException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+
+    @Test
+    public void markTest() throws InvalidMarkAndUnmarkException {
+        Task newTask = new Task("Rainy");
+        newTask.mark();
+        assertEquals(true, newTask.getIsDone());
+        try {
+            newTask.mark();
+        } catch (InvalidMarkAndUnmarkException e) {
+            assertEquals("Task is already marked as done!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void unmarkTest() throws InvalidMarkAndUnmarkException {
+        Task newTask = new Task("Rainy");
+        try {
+            newTask.unmark();
+        } catch (InvalidMarkAndUnmarkException e) {
+            assertEquals("Task is still undone!", e.getMessage());
+        }
+        newTask.mark();
+        newTask.unmark();
+        assertEquals(false, newTask.getIsDone());
+    }
+}
