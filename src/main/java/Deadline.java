247bcb387bf33da9599988a8e058225f8516dd49
weibinlim <limweibin0816@gmail.com>
2024-08-30 16:10:28 +0800

    Deadline.java: Change end variable from type String to type LocalDateTime
    Deadline.java: Add new method to convert String input to LocalDateTime object
    
    Deadline.java: Edit toString() method to return appropriate representation
    
    Deadline.java: Update exception message
    
    Deadline.java: Edit constructor to take in LocalDateTime object instead of String
    
    Event.java: Change start and end variables from type String to type LocalDateTime
    
    Event.java: Add new method stringToDate(...) to convert String input to LocalDateTime object
    
    Event.java: Edit toString() method to return appropriate representation
    
    Event.java: Update exception message
    
    Event.java: Edit constructor to take in LocalDateTime object instead of String

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    String end;
+    private LocalDateTime end;
+    private static boolean withTime;
 
-    private Deadline(String str, String end, boolean marked) {
+    private Deadline(String str, LocalDateTime end, boolean marked) {
         super(str, marked);
         this.end = end;
     }
@@ -14,23 +19,53 @@ public class Deadline extends Task {
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '/') {
-                return new Deadline(s.substring(9, i - 1), s.substring(i + 1), false);
-
+                return new Deadline(s.substring(9, i - 1), stringToDate(s.substring(i + 4)), false);
             }
         }
         throw new BigdogException("Come on! Set a due by date and get to work!");
     }
     public static Deadline of(String s, boolean marked) throws BigdogException {
         if (s.length() <= 4) {
-            throw new BigdogException("data file corrupted!");
+            throw new BigdogException("data file corrupted! Cause: " + s);
         }
 
         for (int i = 5; i < s.length(); i++) {
             if (s.charAt(i) == '|') {
-                return new Deadline(s.substring(4, i - 1), s.substring(i + 2), marked);
+                LocalDateTime end = LocalDateTime.parse(s.substring(i + 2));
+                return new Deadline(s.substring(4, i - 1), end, marked);
             }
         }
-        throw new BigdogException("Come on! Set a due by date and get to work!");
+        throw new BigdogException("data file corrupted! Cause: " + s);
+    }
+
+    private static LocalDateTime stringToDate(String str) throws BigdogException {
+        long dashes = str.chars().filter(x -> x == '/').count();
+        if (dashes != 2) {
+            throw new BigdogException("Invalid date format :" + str);
+        } else {
+            String[] getTime = str.split(" ");
+            String[] getDate = getTime[0].split("/");
+            String year = getDate[2];
+            String month = getDate[1];
+            String day = getDate[0];
+
+            try {
+                if (getTime.length == 2) {
+                    withTime = true;
+                    return LocalDateTime.parse(String.format("%s-%s-%sT%s", year, month, day, getTime[1]));
+                } else if (getTime.length == 1) {
+                    withTime = false;
+                    return LocalDateTime.parse(String.format("%s-%s-%sT%s", year, month, day, "00:00"));
+                } else {
+                    throw new BigdogException("Invalid date format :" + str +
+                            "\nExample correct format: deadline return book /by 02/07/2019 18:00");
+                }
+            } catch (DateTimeParseException e) {
+                throw new BigdogException("Invalid date format :" + str +
+                        "\nExample correct format: deadline return book /by 02/07/2019 18:00");
+            }
+
+        }
     }
 
     @Override
@@ -41,7 +76,13 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (" + end.substring(0, 2) + ":" + end.substring(2) + ")";
+        if (withTime) {
+            return "[D]" + super.toString() + " (by: " +
+                    this.end.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " +
+                    this.end.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + ")";
+        }
     }
 
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task{
 
-    String start;
-    String end;
-
-    private Event(String str, String start, String end, boolean marked) {
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private static boolean withTime;
+    private Event(String str, LocalDateTime start, LocalDateTime end, boolean marked) {
         super(str, marked);
         this.start = start;
         this.end = end;
@@ -14,14 +18,17 @@ public class Event extends Task{
             throw new BigdogException("event can't be empty! If theres no event then go and sleep!");
         }
 
+        int dashCounter = 0;
         for (int j = s.length() - 1; j > 5; j--) {
-            if (s.charAt(j) != '/') {
-                continue;
+            if (s.charAt(j) == '/') {
+                dashCounter++;
             }
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '/') {
-                    return new Event(s.substring(6, i - 1), s.substring(i + 1, j - 1),
-                            s.substring(j + 1), false);
+            if (s.charAt(j) == '/' && dashCounter == 3) {
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == '/') {
+                        return new Event(s.substring(6, i - 1), stringToDate(s.substring(i + 6, j - 1)),
+                                stringToDate(s.substring(j + 4)), false);
+                    }
                 }
             }
         }
@@ -29,22 +36,56 @@ public class Event extends Task{
     }
 
     public static Event of(String s, boolean marked) throws BigdogException {
+
+
         if (s.length() <= 4) {
-               throw new BigdogException("event can't be empty! If theres no event then go and sleep!");
+            throw new BigdogException("data file corrupted! Cause: " + s);
         }
 
             for (int j = s.length() - 1; j > 3; j--) {
                 if (s.charAt(j) != '|') {
                     continue;
                 }
+                LocalDateTime end = LocalDateTime.parse(s.substring(j + 2));
                 for (int i = 5; i < s.length(); i++) {
                     if (s.charAt(i) == '|') {
-                            return new Event(s.substring(4, i - 1), s.substring(i + 2, j - 1),
-                                            s.substring(j + 2), marked);
+                        return new Event(s.substring(4, i - 1), LocalDateTime.parse(s.substring(i + 2, j - 1)),
+                                end, marked);
                     }
                 }
+
             }
-        throw new BigdogException("Event has to have a start and end!");
+        throw new BigdogException("data file corrupted! Cause: " + s);
+    }
+
+    private static LocalDateTime stringToDate(String str) throws BigdogException {
+        long dashes = str.chars().filter(x -> x == '/').count();
+        if (dashes != 2) {
+            throw new BigdogException("Invalid date format :" + str);
+        } else {
+            String[] getTime = str.split(" ");
+            String[] getDate = getTime[0].split("/");
+            System.out.println(str);
+            String year = getDate[2];
+            String month = getDate[1];
+            String day = getDate[0];
+
+            try {
+                if (getTime.length == 2) {
+                    withTime = true;
+                    return LocalDateTime.parse(String.format("%s-%s-%sT%s", year, month, day, getTime[1]));
+                } else if (getTime.length == 1) {
+                    withTime = false;
+                    return LocalDateTime.parse(String.format("%s-%s-%sT%s", year, month, day, "00:00"));
+                } else {
+                    throw new BigdogException("Invalid date format :" + str +
+                            "\nExample correct format: event meeting with John /from 02/07/2019 18:00 /to 02/07/2019 20:00");
+                }
+            } catch (DateTimeParseException e) {
+            throw new BigdogException("Invalid date format :" + str +
+                    "\nExample correct format: deadline return book /by 02/07/2019 18:00");
+            }
+        }
     }
 
     @Override
@@ -55,7 +96,15 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (" + start + " " + end + ")";
+        if (withTime) {
+            return "[E]" + super.toString() + " (from: "
+                    + this.start.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")) + " to: "
+                    + this.end.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")) + ")";
+        } else {
+            return "[E]" + super.toString() + " (from: "
+                    + this.start.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + " to: "
+                    + this.end.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + ")";
+        }
     }
 
 }
