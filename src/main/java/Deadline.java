3e289984097abf38b82235854d21504e08cf93aa
chua yuan cheng <>
2024-08-30 15:17:27 +0800

    Deadline.java: Store deadline dates and time
    Store dates and time as String.
    
    Teach chatbot to store dates and time as LocalDateTime.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,25 @@
-public class Deadline extends Task{
-    protected String by;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task{
+    protected LocalDateTime byDateTime;
+
+    public Deadline(String description, LocalDate byDate) {
         super(description);
-        this.by = by;
+        this.byDateTime = byDate.atStartOfDay();
+    }
+
+    public Deadline(String description, LocalDateTime byDateTime) {
+        super(description);
+        this.byDateTime = byDateTime;
     }
 
     @Override
     public String addTaskToString() {
         return super.addTaskToString() + "\n"
-                + " ".repeat(5) + "[D] [ ] " + description + " (by: " + by + ")" + "\n"
+                + " ".repeat(5) + "[D] [ ] " + description + " (by: " + formattedDeadline() + ")" + "\n"
                 + "\n"
                 + "You can use the command \"list\" to view your list of tasks :D";
     }
@@ -17,27 +27,34 @@ public class Deadline extends Task{
     @Override
     public String taskToString() {
         if (isDone) {
-            return "[D] [X] " + description + " (by: " + by + ")";
+            return "[D] [X] " + description + " (by: " + formattedDeadline() + ")";
         }
-        return "[D] [ ] " + description + " (by: " + by + ")";
+        return "[D] [ ] " + description + " (by: " + formattedDeadline() + ")";
     }
 
     @Override
     public String markDoneToString() {
-        return super.markDoneToString() + "\n" + " ".repeat(5) + "[D] [X] " + description + " (by: " + by + ")";
+        return super.markDoneToString() + "\n" + " ".repeat(5) + "[D] [X] " + description + " (by: " + formattedDeadline() + ")";
     }
 
     @Override
     public String unmarkDoneToString() {
-        return super.unmarkDoneToString() + "\n" + " ".repeat(5) + "[D] [ ] " + description + " (by: " + by + ")";
+        return super.unmarkDoneToString() + "\n" + " ".repeat(5) + "[D] [ ] " + description + " (by: " + formattedDeadline() + ")";
     }
 
     @Override
     public String deleteTask() {
         if (isDone) {
-            return super.deleteTask() + "\n" + " ".repeat(5) + "[D] [X] " + description + " (by: " + by + ")";
+            return super.deleteTask() + "\n" + " ".repeat(5) + "[D] [X] " + description + " (by: " + formattedDeadline() + ")";
         }
-        return super.deleteTask() + "\n" + " ".repeat(5) + "[D] [ ] " + description + " (by: " + by + ")";
+        return super.deleteTask() + "\n" + " ".repeat(5) + "[D] [ ] " + description + " (by: " + formattedDeadline() + ")";
+    }
+
+    public String formattedDeadline() {
+        if (byDateTime.toLocalTime().equals(LocalTime.MIDNIGHT)) {
+            return byDateTime.format(DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy"));
+        }
+        return byDateTime.format(DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy 'at' HHmm"));
     }
 
 }
