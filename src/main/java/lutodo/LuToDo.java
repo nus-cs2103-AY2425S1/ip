d039fad0f7aacfcb567a9ec02651778a4f462582
RuijianLu <e1297772@u.nus.edu>
2024-08-30 10:18:23 +0800

    Delete codes not needed

--- src/main/java/lutodo/LuToDo.java
+++ src/main/java/lutodo/LuToDo.java
@@ -44,35 +44,5 @@ public class LuToDo {
     public static void main(String[] args) {
         new LuToDo("docs/taskListFile.txt").run();
     }
-    /*
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        File taskListFile = new File(Utility.FILE_PATH);
-        if (!taskListFile.exists()) {
-            try {
-                taskListFile.createNewFile();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        try {
-            Utility.fileToTaskList(taskListFile);
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        Utility.greeting();
-        while (true) {
-            String message = sc.nextLine();
-            System.out.print(Utility.LINE);
-            if (message.equals("bye")) {
-                Utility.bye();
-                System.out.print(Utility.LINE);
-                break;
-            }
-            Utility.handleMessage(message);
-            System.out.print(Utility.LINE);
-            Utility.taskListToFile();
-        }
-    }
-    */
+
 }
