ee13247e4df0ad4720457084b99347fa9f93f14a
ethantyh692 <ethantyh692@gmail.com>
2024-08-30 17:00:04 +0800

    Configure to coding standards

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -30,7 +30,7 @@ public class TaskList {
     public void mark(int index, Storage storage) {
         Task task = this.toDoList.get(index - 1);
         task.markAsDone();
-        storage.replaceLineInFile(this,index - 1);
+        storage.replaceLineInFile(this, index - 1);
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(task.toString());
     }
@@ -44,7 +44,7 @@ public class TaskList {
     public void unmark(int index, Storage storage) {
         Task task = toDoList.get(index - 1);
         task.unmarkAsUndone();
-        storage.replaceLineInFile(this,index - 1);
+        storage.replaceLineInFile(this, index - 1);
         System.out.println("Ok! I've marked this task as not done yet:");
         System.out.println(task.toString());
     }
