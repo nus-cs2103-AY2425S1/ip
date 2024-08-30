fdcaedb8ef079aecff9730b0b479bbe0679d0c20
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-30 17:42:47 +0800

    Add JavaDocs to exceptions

--- src/main/java/puke/commands/AddTodoCommand.java
+++ src/main/java/puke/commands/AddTodoCommand.java
@@ -4,13 +4,28 @@ import puke.exceptions.EmptyDescriptionException;
 import puke.tasklist.TaskManager;
 import puke.ui.MessageBuilder;
 
+/**
+ * Command to add a new Todo task.
+ */
 public class AddTodoCommand extends Command {
     private String description;
 
+    /**
+     * Constructs an AddTodoCommand with the specified description.
+     *
+     * @param description the description of the Todo task to be added
+     */
     public AddTodoCommand(String description) {
         this.description = description;
     }
 
+    /**
+     * Executes the command to add a Todo task to the TaskManager and sends a confirmation message.
+     *
+     * @param taskManager the TaskManager to which the Todo task will be added
+     * @param messageBuilder the MessageBuilder used to construct and send a confirmation message
+     * @throws EmptyDescriptionException if the description is empty
+     */
     @Override
     public void execute(TaskManager taskManager, MessageBuilder messageBuilder) throws EmptyDescriptionException {
         if (description.isEmpty()) {
--- src/main/java/puke/commands/Command.java
+++ src/main/java/puke/commands/Command.java
@@ -4,6 +4,18 @@ import puke.exceptions.PukeException;
 import puke.tasklist.TaskManager;
 import puke.ui.MessageBuilder;
 
+/**
+ * Abstract base class for all commands in the application.
+ * Defines the common interface for executing a command.
+ */
 public abstract class Command {
+
+    /**
+     * Executes the command using the provided TaskManager and MessageBuilder.
+     *
+     * @param taskManager the TaskManager to perform operations on
+     * @param messageBuilder the MessageBuilder to construct and send messages
+     * @throws PukeException if an error occurs during command execution
+     */
     public abstract void execute(TaskManager taskManager, MessageBuilder messageBuilder) throws PukeException;
 }
