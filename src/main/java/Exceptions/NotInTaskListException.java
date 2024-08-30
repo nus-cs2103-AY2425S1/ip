04a654e9f8e8d8bf83ceada9928b8ef81f5296c1
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 13:34:20 +0800

    NotInTaskListException.java: add Javadoc comments

--- src/main/java/Exceptions/NotInTaskListException.java
+++ src/main/java/Exceptions/NotInTaskListException.java
@@ -1,7 +1,13 @@
 package Exceptions;
 
+/**
+ * An exception that occurs when the specified task is not in the given taskList
+ */
 public class NotInTaskListException extends TestamentException {
 
+    /**
+     * Constructor for NotInTaskListException
+     */
     public NotInTaskListException() {
         super("Apologies, but that task does not exist");
     }
