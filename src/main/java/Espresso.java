7408eb18fd7f0a05dfce1732d71e3bfe9ddbbc54
Nihirraa <e1186316@u.nus.edu>
2024-08-30 13:19:04 +0800

    Level2

--- src/main/java/Espresso.java
+++ src/main/java/Espresso.java
@@ -1,6 +1,35 @@
 import java.util.Scanner;
 public class Espresso {
-    public static void main(String[] args) {
+    private String[] tasks;
+    private int count;
+
+    public Espresso() {
+        tasks = new String[100];
+        count = 0;
+    }
+
+    void addToList(String str) {
+        if (count < tasks.length) {
+            tasks[count] = str;
+            System.out.println("____________________________________________________________");
+            System.out.println("added: " + str);
+            System.out.println("____________________________________________________________");
+            count++;
+        } else {
+            System.out.println("Task list is full");
+        }
+    }
+
+    void printList() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < count; i++) {
+            int num = i + 1;
+            System.out.println(num + ". " + tasks[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    void process() {
         System.out.println("_________________________________________");
         System.out.println("Hello! I'm Espresso");
         System.out.println("What can I do for you?");
@@ -15,11 +44,16 @@ public class Espresso {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            }else if (str.equals("list")) {
+                printList();
+            } else {
+                addToList(str);
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(str);
-            System.out.println("____________________________________________________________");
         }
         sc.close();
+    }
+    public static void main(String[] args) {
+        Espresso esp = new Espresso();
+        esp.process();
     }
 }
