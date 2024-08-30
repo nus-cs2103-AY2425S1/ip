93e006233e17f6413f17ce68417264792f580b68
samriddh2145 <156680067+samriddh2145@users.noreply.github.com>
2024-08-30 12:41:00 +0800

    Level 1

--- src/main/java/Bing.java
+++ src/main/java/Bing.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 public class Bing {
     public static void main(String[] args) {
         String message = "______________________________\n"
-                + "Hello! I'm Bing\n"
-                + "What can I do for you?\n"
-                + "______________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "______________________________";
+                + "Hi! My name is Bing\n"
+                + "How can I help you?\n"
+                + "______________________________\n";
         System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("Bye") || input.equals("bye")) {
+                System.out.println("______________________________\n"
+                        + "Bye. Have a good day.\n"
+                        + "______________________________\n");
+                break;
+            }
+            else {
+                System.out.println("______________________________\n"
+                        + input + "\n"
+                        + "______________________________\n");
+            }
+        }
     }
 }
 
