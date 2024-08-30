289df1049077627ab9ddf50dd3527067e59b51d1
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-08-30 11:54:21 +0800

    Deadline.java: add Javadoc comments

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -3,21 +3,35 @@ package Tasks;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task with a deadline.
+ */
 public class Deadline extends Task {
 
     private LocalDate deadline;
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
 
+    /**
+     * Constructor for deadline.
+     * @param s
+     * @param deadline
+     */
     public Deadline(String s, String deadline) {
         super(s);
         this.deadline = LocalDate.parse(deadline);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public String infoForFile() {
         String str = "[D] / " + super.getDetails() + " / " + deadline;
         return str;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         String str = "";
