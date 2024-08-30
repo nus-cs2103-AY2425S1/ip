dc29ce578c0915911316e3f4223ed28337928d7f
Anvita2110 <e1070535@u.nus.edu>
2024-08-30 11:26:47 +0800

    Follow coding standards - 2

--- src/main/java/Ponder_Pika/IOHandler.java
+++ src/main/java/Ponder_Pika/IOHandler.java
@@ -1,8 +1,4 @@
 package Ponder_Pika;
-import Ponder_Pika.Task.Deadline;
-import Ponder_Pika.Task.Event;
-import Ponder_Pika.Task.Task;
-import Ponder_Pika.Task.Todo;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,21 +7,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import Ponder_Pika.Task.Deadline;
+import Ponder_Pika.Task.Event;
+import Ponder_Pika.Task.Task;
+import Ponder_Pika.Task.Todo;
 
 public class IOHandler {
-    private static final File file = new File("./data/pika.txt");
+    private static final File FILE = new File("./data/pika.txt");
 
     public void create() throws PonderPikaException {
-        Path path = Paths.get(file.getPath());
+        Path path = Paths.get(FILE.getPath());
         try {
             if (Files.notExists(path.getParent())) {
                 Files.createDirectories(path.getParent());
             }
+
             if (Files.notExists(path)) {
                 Files.createFile(path);
             }
@@ -35,18 +36,19 @@ public class IOHandler {
     }
 
     public void saveData(List<Task> tasks) throws PonderPikaException {
-        try{
-            if(!file.exists()){
+        try {
+            if (!FILE.exists()) {
                 create();
             }
-            FileWriter fw = new FileWriter(file);
+
+            FileWriter fw = new FileWriter(FILE);
             for (Task task : tasks) {
                 String saveFormat = task.saveFullDetails();
                 fw.write(saveFormat);
                 fw.write("\n");
             }
             fw.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new PonderPikaException(e.getMessage());
         }
     }
@@ -55,43 +57,48 @@ public class IOHandler {
         ArrayList<Task> list = new ArrayList<>();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         try {
-            if (!file.exists()) {
+            if (!FILE.exists()) {
                 return list;
             }
-            FileReader fr = new FileReader(file);
+
+            FileReader fr = new FileReader(FILE);
             Scanner sc = new Scanner(fr);
-            while(sc.hasNext()) {
+            while (sc.hasNext()) {
                 String line = sc.nextLine();
-                String[] split = line.split("\\|");
-                switch (split[0].trim()) {
-                    case "D":
-                        Deadline d = new Deadline(split[2].trim(), LocalDateTime.parse(split[3].trim(),formatter));
-                        if (split[1].trim().equals("true")) {
-                            d.markDone();
-                        }
-                        list.add(d);
-                        break;
-                    case "E":
-                        Event e = new Event(split[2].trim(), LocalDateTime.parse(split[3].trim(),formatter), LocalDateTime.parse(split[4].trim(),formatter));
-                        if (split[1].trim().equals("true")) {
-                            e.markDone();
-                        }
-                        list.add(e);
-                        break;
-                    case "T":
-                        Todo t = new Todo(split[2].trim());
-                        if (split[1].trim().equals("true")) {
-                            t.markDone();
-                        }
-                        list.add(t);
-                        break;
+                String[] splitlines = line.split("\\|");
+                switch (splitlines[0].trim()) {
+                case "D":
+                    Deadline d = new Deadline(splitlines[2].trim(), LocalDateTime.parse(splitlines[3].trim(),
+                            formatter));
+                    if (splitlines[1].trim().equals("true")) {
+                        d.markDone();
+                    }
+
+                    list.add(d);
+                    break;
+                case "E":
+                    Event e = new Event(splitlines[2].trim(), LocalDateTime.parse(splitlines[3].trim(),formatter),
+                            LocalDateTime.parse(splitlines[4].trim(),formatter));
+                    if (splitlines[1].trim().equals("true")) {
+                        e.markDone();
+                    }
+
+                    list.add(e);
+                    break;
+                case "T":
+                    Todo t = new Todo(splitlines[2].trim());
+                    if (splitlines[1].trim().equals("true")) {
+                        t.markDone();
+                    }
+
+                    list.add(t);
+                    break;
                 }
             }
-        } catch (IOException ignored){
-
+        } catch (IOException ignored) {
+            // Ignored exception
         }
+
         return list;
-
     }
-
 }
