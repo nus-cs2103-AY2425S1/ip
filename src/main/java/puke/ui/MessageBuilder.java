996e820d78c78473818981dd0f1b6cde2750c3a1
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-08-30 18:36:30 +0800

    Add JavaDocs to ui classes

--- src/main/java/puke/ui/MessageBuilder.java
+++ src/main/java/puke/ui/MessageBuilder.java
@@ -1,6 +1,15 @@
 package puke.ui;
 
+/**
+ * Handles the construction and display of messages to the user.
+ */
 public class MessageBuilder {
+
+    /**
+     * Constructs and prints a message with a standard format.
+     *
+     * @param message the message to be displayed
+     */
     private void buildMessage(String message) {
         System.out.println("______________________________________________________________________");
         System.out.println(message);
@@ -8,14 +17,25 @@ public class MessageBuilder {
         System.out.println("______________________________________________________________________");
     }
 
+    /**
+     * Sends a greeting message to the user.
+     */
     public void sendGreetingMessage() {
         buildMessage("HELLO???!!! I'm Puke and I'm Puk *uwaeh* ing... .-.");
     }
 
+    /**
+     * Sends a goodbye message to the user.
+     */
     public void sendGoodbyeMessage() {
         buildMessage("BYE!!! *UWAGHhhHH* !!! see Ya *uWahghgh*");
     }
 
+    /**
+     * Sends a generic message to the user.
+     *
+     * @param message the message to be sent
+     */
     public void sendMessage(String message) {
         buildMessage(message);
     }
--- src/main/java/puke/ui/Ui.java
+++ src/main/java/puke/ui/Ui.java
@@ -5,15 +5,29 @@ import java.util.Scanner;
 import puke.exceptions.PukeException;
 import puke.handlers.InputManager;
 
+/**
+ * Handles the user interface, including input handling and message display.
+ */
 public class Ui {
     private MessageBuilder messageBuilder;
     private InputManager inputManager;
 
+    /**
+     * Constructs a Ui instance with the specified InputManager and MessageBuilder.
+     *
+     * @param inputManager the InputManager to handle user commands
+     * @param messageBuilder the MessageBuilder to construct and display messages
+     */
     public Ui(InputManager inputManager, MessageBuilder messageBuilder) {
         this.messageBuilder = messageBuilder;
         this.inputManager = inputManager;
     }
 
+    /**
+     * Starts the user interface, displaying the greeting message and processing user input.
+     *
+     * @throws PukeException if an error occurs while handling user input
+     */
     public void start() throws PukeException {
         messageBuilder.sendGreetingMessage();
         Scanner scanner = new Scanner(System.in);
@@ -31,6 +45,9 @@ public class Ui {
         scanner.close();
     }
 
+    /**
+     * Terminates the user interface, displaying the goodbye message.
+     */
     private void terminate() {
         messageBuilder.sendGoodbyeMessage();
     }
