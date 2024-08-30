5348a40d66bf39623a1d10543d42186e8dcbcab0
ngjianyi <ngjianyi2002@gmail.com>
2024-08-30 15:43:11 +0800

    Add JavaDocs for exceptions and task

--- src/main/java/dudu/exception/InvalidFormatException.java
+++ src/main/java/dudu/exception/InvalidFormatException.java
@@ -1,6 +1,11 @@
 package dudu.exception;
 
 public class InvalidFormatException extends Exception {
+    /**
+     * Constructs an InvalidFormatException with the specified detail message.
+     *
+     * @param message The detail message which explains the reason for the exception.
+     */
     public InvalidFormatException(String message) {
         super(message);
     }
