c2365727ca68273fb9aeb82cf80ad613602595cb
Teddayz <mervynneo02@gmail.com>
2024-08-31 20:31:46 +0800

    Revert "Add JavaDoc Header Messages"
    This reverts commit 0aa5b8374229a461c797bc465738eb4b887f2283.

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -7,27 +7,14 @@ import tasks.Task;
 
 public class Parser {
 
-    /**
-     * Returns a DeadlineTask object that can be used to get the details of the Task.
-     *
-     * @param deadlineCommand A string taken in to be parsed.
-     * @return A new Deadline task.
-     * @throws StringIndexOutOfBoundsException When parsing goes wrong.
-     */
     public DeadlineTask parseDeadlineTask(String deadlineCommand) throws StringIndexOutOfBoundsException {
         int byIndex = deadlineCommand.indexOf("/by");
         String desc = deadlineCommand.substring(0, byIndex);
         String deadline = deadlineCommand.substring(byIndex + 4);
         return new DeadlineTask(desc, false, deadline);
     }
-    /**
-     * Returns a EventTask object that can be used to get the details of the Task.
-     *
-     * @param eventCommand A string taken in to be parsed.
-     * @return A new event task.
-     * @throws StringIndexOutOfBoundsException When parsing goes wrong.
-     */
-    public EventTask parseEventTask(String eventCommand) throws StringIndexOutOfBoundsException {
+
+    public Task parseEventTask(String eventCommand) throws StringIndexOutOfBoundsException {
         int fromIndex = eventCommand.indexOf("/from");
         int toIndex = eventCommand.indexOf("/to");
 
@@ -37,14 +24,7 @@ public class Parser {
         return new EventTask(details, false, start, end);
     }
 
-    /**
-     * Returns a ToDoTask object that can be used to get the details of the Task.
-     *
-     * @param toDoCommand A string taken in to be parsed.
-     * @return A new To Do task.
-     * @throws StringIndexOutOfBoundsException When parsing goes wrong.
-     */
-    public ToDoTask parseToDoTask(String toDoCommand) {
+    public Task parseToDoTask(String toDoCommand) {
         return new ToDoTask(toDoCommand, false);
     }
 
