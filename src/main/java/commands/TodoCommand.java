764a652f63db6fad26bc6f7286cc6c467b59f434
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-31 17:09:03 +0800

    Add TodoCommand class

---
+++ src/main/java/commands/TodoCommand.java
@@ -0,0 +1,24 @@
+package main.java.commands;
+
+import main.java.TaskList;
+import main.java.Todo;
+import main.java.util.Storage;
+import main.java.util.Ui;
+
+public class TodoCommand extends Command{
+    private Todo todo;
+
+    public TodoCommand(Todo todo) {
+        this.todo = todo;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        taskList.addTask(todo);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
