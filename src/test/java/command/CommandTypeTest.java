af805b2f78e08d38671adbf7def1e0b35f3e8c7e
LimKaiWei <E1122652@u.nus.edu>
2024-08-31 20:12:56 +0800

    Add some JUnit test cases

---
+++ src/test/java/command/CommandTypeTest.java
@@ -0,0 +1,28 @@
+package command;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import exception.ElliotException;
+
+public class CommandTypeTest {
+
+    @Test
+    public void invalidArgument_exceptionThrown() {
+        assertThrows(ElliotException.class, () -> {
+            CommandType.parseStringToCommand("blah");
+        });
+    } 
+
+    @Test
+    public void parseStringToCommand_success() throws ElliotException {
+        assertEquals(CommandType.LIST, CommandType.parseStringToCommand("list"));
+        assertEquals(CommandType.MARK, CommandType.parseStringToCommand("MARK"));
+        assertEquals(CommandType.UNMARK, CommandType.parseStringToCommand("unMark"));
+        assertEquals(CommandType.DELETE, CommandType.parseStringToCommand("DELete"));
+        assertEquals(CommandType.TODO, CommandType.parseStringToCommand("ToDo"));
+        assertEquals(CommandType.DEADLINE, CommandType.parseStringToCommand("DeadLine"));
+        assertEquals(CommandType.EVENT, CommandType.parseStringToCommand("EvEnT"));
+        assertEquals(CommandType.BYE, CommandType.parseStringToCommand("bYe"));
+    }
+}
