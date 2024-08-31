7d8b49861f6b1adaf7e930f9d1fac7c482bb51ca
yanqiyqh <yanqi.yqh@gmail.com>
2024-08-31 15:20:03 +0800

    modify YnchUi to have void methods for printing

--- src/main/java/YnchUi.java
+++ src/main/java/YnchUi.java
@@ -9,30 +9,30 @@ class YnchUi {
         this.name = "YNCH";
     }
 
-    String printAdd(Task task, int size) {
-        return "Meow! I've added this task: \n" + task + 
-            "\n Now you have " + size + " tasks in the list.";
+    void printAdd(Task task, int size) {
+        System.out.println("Meow! I've added this task: \n" + task + 
+        "\n Now you have " + size + " tasks in the list.");
     }
 
-    String printDelete(Task task, int size) {
-        return "Meow! I've removed this task: \n" + task + 
-            "\n Now you have " + size + " tasks in the list.";
+    void printDelete(Task task, int size) {
+        System.out.println("Meow! I've removed this task: \n" + task + 
+        "\n Now you have " + size + " tasks in the list.");
     }
 
-    String printMark(Task task) {
-        return "Meow! I've marked this task as done: \n" + task;
+    void printMark(Task task) {
+        System.out.println("Meow! I've marked this task as done: \n" + task);
     }
 
-    String printUnmark(Task task) {
-        return "Meow! I've marked this task as not done yet: \n" + task;
+    void printUnmark(Task task) {
+        System.out.println("Meow! I've marked this task as not done yet: \n" + task);
     }
 
-    String greet() {
-        return "Meow! I'm YNCH. What can I do for you?";
+    void greet() {
+        System.out.println("Meow! I'm YNCH. What can I do for you?");
     }
     
-    String exit() {
-        return "Bye. Hope to see you again soon meow!";
+    void exit() {
+        System.out.println("Bye. Hope to see you again soon meow!");
     }
 
 }
