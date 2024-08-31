7aa5dcbb9a812901f642a911f0423c0989d97696
jiaaaaen <girljiaen@gmail.com>
2024-09-01 01:46:35 +0800

    Tweak the code to comply with a coding standard

--- src/main/java/Bwead/BweadException.java
+++ src/main/java/Bwead/BweadException.java
@@ -1,6 +1,7 @@
 package Bwead;
 
 public class BweadException extends RuntimeException {
+
     public BweadException(String message) {
         super(message);
     }
--- src/main/java/Bwead/Event.java
+++ src/main/java/Bwead/Event.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
 
-    private boolean done;
+    private boolean isDone;
     private String text;
     private LocalDate start;
     private LocalDate end;
@@ -20,14 +20,15 @@ public class Event extends Task {
         this.end = end;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.isDone = false;
     }
-    public void setDone(boolean toset) {
-        this.done = toset;
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
     }
 
     public String toString() {
         String str = "";
-        if (done) {
+        if (isDone) {
             str = "X";
         } else {
             str = " ";
