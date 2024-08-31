4d94eb707456704ed0d08ca61e9c29453dbbf80c
Hans Delano <hanscau@hotmail.com>
2024-08-31 19:28:06 +0800

    Remove useless imports

--- src/main/java/Snah.java
+++ src/main/java/Snah.java
@@ -1,9 +1,4 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Snah {
