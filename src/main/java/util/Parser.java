3e09651bc2958a0d89471c25fe424aea6d14038f
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-31 16:40:58 +0800

    Add Parser class to handle user command parsing

--- src/main/java/util/Parser.java
+++ src/main/java/util/Parser.java
@@ -1,4 +1,59 @@
 package main.java.util;
 
 public class Parser {
+
+    private static enum Keywords {
+        MARK,
+        UNMARK,
+        LIST,
+        TODO,
+        DEADLINE,
+        EVENT,
+        DELETE,
+        BYE,
+        UNKNOWN;
+    }
+
+    public static String parse(String input) {
+        //TODO future plan to return array of Command instead of String
+        String[] arr = input.split(" ", 2);
+        Keywords keyword;
+        try {
+            keyword = Keywords.valueOf(arr[0].toUpperCase());
+        } catch (IllegalArgumentException e) {
+            keyword = Keywords.UNKNOWN;
+        }
+        String result = "";
+
+        switch (keyword) {
+        case BYE:
+            result = "bye";
+            break;
+        case LIST:
+            result = "list";
+            break;
+        case TODO:
+            result = "todo";
+            break;
+        case DEADLINE:
+            result = "deadline";
+            break;
+        case EVENT:
+            result = "event";
+            break;
+        case MARK:
+            result = "mark";
+            break;
+        case UNMARK:
+            result = "unmark";
+            break;
+        case DELETE:
+            result = "delete";
+            break;
+        case UNKNOWN:
+            result = "unknown";
+            break;
+        }
+        return result;
+    }
 }
