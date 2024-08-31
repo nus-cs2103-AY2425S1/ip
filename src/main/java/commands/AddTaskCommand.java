6c8ff3e2b4d290bb6ebeb206f83e39013b3374c8
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-31 21:11:34 +0800

    Add AddTaskCommand class
    Consolidated TodoCommand, DeadlineCommand and Event Command into a
    single class.

---
+++ src/main/java/commands/AddTaskCommand.java
@@ -0,0 +1,26 @@
+package main.java.commands;
+
+import main.java.Task;
+import main.java.TaskList;
+import main.java.util.Storage;
+import main.java.util.Ui;
+
+public class AddTaskCommand extends Command{
+    private Task task;
+
+    public AddTaskCommand(Task t) {
+        this.task = t;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) {
+        taskList.addTask(this.task);
+        ui.showAddTaskMessage(this.task, taskList);
+        Storage.saveToFile(taskList);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
