9f94590d951434d3f1b91fd0c861224f924cab37
ishan-agarwal-05 <ishanagarwal270805@gmail.com>
2024-09-01 00:03:56 +0800

    Add ability to store and list tasks

--- src/main/java/JohnCena.java
+++ src/main/java/JohnCena.java
@@ -1,56 +1,35 @@
-// import java.util.Scanner;
-
-// public class JohnCena {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-
-//         System.out.println("____________________________________________________________");
-//         System.out.println(" Hello! I'm John Cena");
-//         System.out.println(" What can I do for you?");
-//         System.out.println("____________________________________________________________");
-//         System.out.println(" Bye. Hope to see you again soon!");
-//         System.out.println("____________________________________________________________");
-
-//         while (true) {
-//             String input = scanner.nextLine();
-//             if (input.equals("bye")) {
-//                 System.out.println("____________________________________________________________");
-//                 System.out.println(" Bye. Hope to see you again soon!");
-//                 System.out.println("____________________________________________________________");
-//                 break;
-//             } else {
-//                 System.out.println("____________________________________________________________");
-//                 System.out.println(" " + input);
-//                 System.out.println("____________________________________________________________");
-//             }
-            
-//         }
-
-//         scanner.close();
-//     }
-// }
 
 import java.util.Scanner;
 
 public class JohnCena {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm John Cena");
+        System.out.println(" Hello! I'm [YOUR CHATBOT NAME]");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
+            } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
             }
         }
@@ -58,3 +37,4 @@ public class JohnCena {
         scanner.close();
     }
 }
+
