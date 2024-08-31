e2685099b31e19824ec64a74ce34ef36a456efba
ishan-agarwal-05 <ishanagarwal270805@gmail.com>
2024-08-31 23:50:41 +0800

    Add echo and exit

--- src/main/java/JohnCena.java
+++ src/main/java/JohnCena.java
@@ -1,10 +1,60 @@
+// import java.util.Scanner;
+
+// public class JohnCena {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("____________________________________________________________");
+//         System.out.println(" Hello! I'm John Cena");
+//         System.out.println(" What can I do for you?");
+//         System.out.println("____________________________________________________________");
+//         System.out.println(" Bye. Hope to see you again soon!");
+//         System.out.println("____________________________________________________________");
+
+//         while (true) {
+//             String input = scanner.nextLine();
+//             if (input.equals("bye")) {
+//                 System.out.println("____________________________________________________________");
+//                 System.out.println(" Bye. Hope to see you again soon!");
+//                 System.out.println("____________________________________________________________");
+//                 break;
+//             } else {
+//                 System.out.println("____________________________________________________________");
+//                 System.out.println(" " + input);
+//                 System.out.println("____________________________________________________________");
+//             }
+            
+//         }
+
+//         scanner.close();
+//     }
+// }
+
+import java.util.Scanner;
+
 public class JohnCena {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm John Cena");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
