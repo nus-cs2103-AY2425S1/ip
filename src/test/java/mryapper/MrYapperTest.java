dd72b5cf36d66bd11aa1ba7e4f24f947fb2e1ae8
awdse22 <ngkhengyang@gmail.com>
2024-09-01 01:21:38 +0800

    Add JUnit test case directory and dummy tests

---
+++ src/test/java/mryapper/MrYapperTest.java
@@ -0,0 +1,12 @@
+package mryapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MrYapperTest {
+    @Test
+    public void dummyMrYapperTest() {
+        assertEquals(2, 2);
+    }
+}
---
+++ src/test/java/mryapper/task/TaskListTest.java
@@ -0,0 +1,12 @@
+package mryapper.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void dummyTaskListTest() {
+        assertEquals(4, 4);
+    }
+}
