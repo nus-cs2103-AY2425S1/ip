43db37ae202215d6002fc3dd40c7572e357654b2
Sidd <sr.sanagala@outlook.com>
2024-08-31 20:16:35 +0800

    Add JUnit testing for commands

---
+++ src/test/java/commands/AddCommandTest.java
@@ -0,0 +1,69 @@
+package commands;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import tasks.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDate;
+public class AddCommandTest {
+
+    @Test
+    public void addToDo() {
+
+        List<Task> taskList = new ArrayList<>();
+
+        TaskList tasks = new TaskList(taskList);
+        String input = "/todo return book";
+
+        ToDo expectedTaskCreated = new ToDo("return book");
+
+        AddCommand addCommand = new AddCommand();
+        String result = addCommand.execute(input, tasks);
+
+        String expected = String.format("Added yet another task\n   %s\nYou now have %d tasks. Are you gonna do any of them?", expectedTaskCreated, tasks.noOfTasks());
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    public void addDeadline() {
+
+        List<Task> taskList = new ArrayList<>();
+
+        TaskList tasks = new TaskList(taskList);
+        String input = "/deadline return book /by 23-04-25";
+
+        LocalDate date = LocalDate.parse("2025-04-23");
+        Deadline expectedTaskCreated = new Deadline("return book", date);
+
+        AddCommand addCommand = new AddCommand();
+        String result = addCommand.execute(input, tasks);
+
+        String expected = String.format("Added yet another task\n   %s\nYou now have %d tasks. Are you gonna do any of them?", expectedTaskCreated, tasks.noOfTasks());
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    public void addEvent() {
+
+        List<Task> taskList = new ArrayList<>();
+
+        TaskList tasks = new TaskList(taskList);
+        String input = "/event return book /from 23-04-25 /to 23-04-25";
+
+        LocalDate date = LocalDate.parse("2025-04-23");
+        Event expectedTaskCreated = new Event("return book", date, date);
+
+        AddCommand addCommand = new AddCommand();
+        String result = addCommand.execute(input, tasks);
+
+        String expected = String.format("Added yet another task\n   %s\nYou now have %d tasks. Are you gonna do any of them?", expectedTaskCreated, tasks.noOfTasks());
+        assertEquals(expected, result);
+
+    }
+
+}
