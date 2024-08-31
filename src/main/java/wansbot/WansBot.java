33cad78507f763101497387232ddf3d38ceda633
bigismols <bigismols239@gmail.com>
2024-08-31 16:54:52 +0800

    add storage package and implement its functionality in Wansbot

--- src/main/java/wansbot/WansBot.java
+++ src/main/java/wansbot/WansBot.java
@@ -18,6 +18,7 @@ import java.util.Scanner;
 
 import java.time.LocalDate;
 
+import wansbot.storage.Storage;
 import wansbot.tasks.Deadlined;
 import wansbot.tasks.Events;
 import wansbot.tasks.InputEmptyException;
@@ -27,9 +28,10 @@ import wansbot.tasks.Todos;
 import wansbot.ui.UI;
 
 public class WansBot {
-    static final String HR = "----------------------------------------------------------------------";
-    static TaskList userTaskList = new TaskList();
-    static UI ui = new UI();
+    private static final String HR = "----------------------------------------------------------------------";
+    private static TaskList userTaskList = new TaskList();
+    private static UI ui = new UI();
+    private static Storage storage = new Storage(ui);
 
     // Method that deals with empty inputs by throwing wansbot.tasks.InputEmptyException
     private static void emptyInput(String userInput) throws InputEmptyException {
@@ -167,96 +169,6 @@ public class WansBot {
         }
     }
 
-    // Handles saving of tasks into user machine
-    private static void saveTasks() {
-        try {
-            if (!Files.exists(Paths.get("data"))) {
-                File directory = new File("./data");
-                directory.mkdirs();
-            }
-            File file = new File("data", "tasklist.txt");
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            for (int i = 0; i < userTaskList.numOfTasks(); i++) {
-                writer.write(userTaskList.getTask(i) + "\n");
-            }
-            writer.close();
-        } catch (IOException e) {
-            ui.handleIoExceptionSave();
-        }
-    }
-
-    // Handles parsing of task list to load tasks
-    private static void returnTask(String fileInput) {
-        String[] splitInput = fileInput.split(" ");
-        String typeTask = splitInput[1];
-        String nameTask = "";
-        switch (typeTask) {
-            case "T":
-                if (fileInput.contains("[ X ]")) {
-                    nameTask = fileInput.substring(11);
-                    Todos next = new Todos(nameTask);
-                    next.finish();
-                    userTaskList.add(next);
-                } else {
-                    nameTask = fileInput.substring(11);
-                    Todos next = new Todos(nameTask);
-                    userTaskList.add(next);
-                }
-                break;
-            case "D":
-                String[] deadlineSplit = fileInput.split("by: ");
-                LocalDate deadline = LocalDate.parse(deadlineSplit[1].substring(0,
-                        deadlineSplit[1].length() - 1).trim(), DateTimeFormatter.ofPattern("MMM d yyyy"));
-                nameTask = deadlineSplit[0].substring(11, deadlineSplit[0].length() - 1);
-                if (fileInput.contains("[ X ]")) {
-                    Deadlined next = new Deadlined(nameTask, deadline);
-                    next.finish();
-                    userTaskList.add(next);
-                } else {
-                    Deadlined next = new Deadlined(nameTask, deadline);
-                    userTaskList.add(next);
-                }
-                break;
-            case "E":
-                String[] splitUserStartDate = fileInput.split("from: ", 3);
-                String[] splitUserEndDate = splitUserStartDate[1].split( "to: ", 2);
-                if (fileInput.contains("[ X ]")) {
-                    Events next = new Events(splitUserStartDate[0].substring(11, splitUserStartDate[0].length() - 2),
-                            LocalDate.parse(splitUserEndDate[0].substring(0, splitUserEndDate[0].length() -1).trim(),
-                                    DateTimeFormatter.ofPattern("MMM d yyyy")),
-                            LocalDate.parse(splitUserEndDate[1].substring(0, splitUserEndDate[1].length() - 2).trim(),
-                                    DateTimeFormatter.ofPattern("MMM d yyyy")));
-                    next.finish();
-                    userTaskList.add(next);
-                } else {
-                    Events next = new Events(splitUserStartDate[0].substring(11, splitUserStartDate[0].length() - 2),
-                            LocalDate.parse(splitUserEndDate[0].substring(0, splitUserEndDate[0].length() -1).trim(),
-                                    DateTimeFormatter.ofPattern("MMM d yyyy")),
-                            LocalDate.parse(splitUserEndDate[1].substring(0, splitUserEndDate[1].length() - 2).trim(),
-                                    DateTimeFormatter.ofPattern("MMM d yyyy")));
-                    userTaskList.add(next);
-                }
-                break;
-        }
-    }
-
-    // Handles loading of task function from use machine
-    private static void loadTasks() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("./data/tasklist.txt"));
-            String line = reader.readLine();
-            while (line != null) {
-                returnTask(line);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            ui.handleFileNotFound();
-        } catch (IOException e) {
-            ui.handleIoExceptionLoad();
-        }
-    }
-
     // Handles finding tasks of a specific date
     private static void findTaskDate(String userInput) {
         try {
@@ -284,7 +196,7 @@ public class WansBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ui.introduceToUser();
-        loadTasks();
+        userTaskList = storage.loadTasks();
 
         while (true) {
             System.out.println("User: ");
@@ -321,10 +233,10 @@ public class WansBot {
                     removeTask(userInput);
                     break;
                 case "save":
-                    saveTasks();
+                    storage.saveTasks(userTaskList);
                     break;
                 case "load":
-                    loadTasks();
+                    userTaskList = storage.loadTasks();
                     break;
                 case "find":
                     findTaskDate(userInput);
---
+++ src/main/java/wansbot/storage/Storage.java
@@ -0,0 +1,108 @@
+package wansbot.storage;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import wansbot.tasks.Deadlined;
+import wansbot.tasks.Events;
+import wansbot.tasks.TaskList;
+import wansbot.tasks.Todos;
+import wansbot.ui.UI;
+
+public class Storage {
+    private UI ui;
+    
+    public Storage(UI ui) {
+        ui = ui;
+    }
+
+    // handles loading tasks from ./data
+    public TaskList loadTasks() {
+        TaskList taskList = new TaskList();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("./data/tasklist.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] splitInput = line.split(" ");
+                String typeTask = splitInput[1];
+                String nameTask = "";
+                switch (typeTask) {
+                    case "T":
+                        if (line.contains("[ X ]")) {
+                            nameTask = line.substring(11);
+                            Todos next = new Todos(nameTask);
+                            next.finish();
+                            taskList.add(next);
+                        } else {
+                            nameTask = line.substring(11);
+                            Todos next = new Todos(nameTask);
+                            taskList.add(next);
+                        }
+                        break;
+                    case "D":
+                        String[] deadlineSplit = line.split("by: ");
+                        LocalDate deadline = LocalDate.parse(deadlineSplit[1].substring(0,
+                                deadlineSplit[1].length() - 1).trim(), DateTimeFormatter.ofPattern("MMM d yyyy"));
+                        nameTask = deadlineSplit[0].substring(11, deadlineSplit[0].length() - 1);
+                        if (line.contains("[ X ]")) {
+                            Deadlined next = new Deadlined(nameTask, deadline);
+                            next.finish();
+                            taskList.add(next);
+                        } else {
+                            Deadlined next = new Deadlined(nameTask, deadline);
+                            taskList.add(next);
+                        }
+                        break;
+                    case "E":
+                        String[] splitUserStartDate = line.split("from: ", 3);
+                        String[] splitUserEndDate = splitUserStartDate[1].split( "to: ", 2);
+                        if (line.contains("[ X ]")) {
+                            Events next = new Events(splitUserStartDate[0].substring(11, splitUserStartDate[0].
+                                    length() - 2), LocalDate.parse(splitUserEndDate[0].substring(0, splitUserEndDate[0]
+                                            .length() -1).trim(), DateTimeFormatter.ofPattern("MMM d yyyy")),
+                                    LocalDate.parse(splitUserEndDate[1].substring(0, splitUserEndDate[1].length() - 2)
+                                                    .trim(), DateTimeFormatter.ofPattern("MMM d yyyy")));
+                            next.finish();
+                            taskList.add(next);
+                        } else {
+                            Events next = new Events(splitUserStartDate[0].substring(11, splitUserStartDate[0].
+                                    length() - 2), LocalDate.parse(splitUserEndDate[0].substring(0, splitUserEndDate[0].
+                                            length() -1).trim(), DateTimeFormatter.ofPattern("MMM d yyyy")),
+                                    LocalDate.parse(splitUserEndDate[1].substring(0, splitUserEndDate[1].length() - 2).
+                                                    trim(), DateTimeFormatter.ofPattern("MMM d yyyy")));
+                            taskList.add(next);
+                        }
+                        break;
+                    }
+                    line = reader.readLine();
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                ui.handleFileNotFound();
+            } catch (IOException e) {
+                ui.handleIoExceptionLoad();
+            }
+            return taskList;
+        }
+
+        // handles saving of tasklist to ./data
+        public void saveTasks(TaskList taskList) {
+            try {
+                if (!Files.exists(Paths.get("data"))) {
+                    File directory = new File("./data");
+                    directory.mkdirs();
+                }
+                File file = new File("data", "tasklist.txt");
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                for (int i = 0; i < taskList.numOfTasks(); i++) {
+                    writer.write(taskList.getTask(i) + "\n");
+                }
+                writer.close();
+            } catch (IOException e) {
+                ui.handleIoExceptionSave();
+            }
+        }
+    }
