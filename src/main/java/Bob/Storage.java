64b8bacf28ad4c1c4514f4428288818e98640770
shotnothing <shotnothing@protonmail.ch>
2024-08-31 20:48:08 +0800

    Write JavaDocs

--- src/main/java/Bob/Storage.java
+++ src/main/java/Bob/Storage.java
@@ -1,14 +1,20 @@
 package bob;
 
-import bob.task.Task;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+import bob.task.Task;
+
 class Storage {
 
+    /**
+     * Saves the tasks to a file.
+     *
+     * @param tasks
+     * @throws IOException
+     */
     public static void saveTasks(List<Task> tasks) throws IOException {
         File directory = new File("./data");
         if (!directory.exists()) {
--- src/main/java/Bob/task/Task.java
+++ src/main/java/Bob/task/Task.java
@@ -10,18 +10,34 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks the task as done.
+     */
     public void unmarkAsDone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return String
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return String
+     */
     public String getDescription() {
         return description;
     }
