5bf50c8c62d449c89a42cd0eb7ecea49c727b5f7
DanzaSeah <e0959436@u.nus.edu>
2024-08-31 13:46:24 +0800

    Add Storage class

---
+++ src/main/java/Storage.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+
+    private String path;
+    public Storage(String path) {
+        this.path = path;
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        ArrayList<Task> storedTasks = new ArrayList<>();
+        File file = new File(this.path);
+        if (!file.exists()) {
+            return storedTasks; // No file exists, start with an empty task list
+        }
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] taskData = line.split(" \\| ");
+            Task task;
+            switch (taskData[0]) {
+            case "T":
+                task = new Todo(taskData[2]);
+                break;
+            case "D":
+                task = new Deadline(taskData[2], taskData[3]);
+                break;
+            case "E":
+                String description = taskData[2];
+                String[] fromTo = taskData[3].split(" to ");
+                String from = fromTo[0];
+                String to = fromTo[1];
+                task = new Event(description, from, to);
+                break;
+            default:
+                continue; // Skip unknown task types
+            }
+            if (taskData[1].equals("1")) {
+                task.markAsDone();
+            }
+            storedTasks.add(task);
+        }
+        scanner.close();
+        return storedTasks;
+    }
+
+    public void save(ArrayList<Task> storedTasks) throws IOException {
+        File f = new File(this.path);
+        f.getParentFile().mkdirs(); // create directories if they don't exist
+        FileWriter fw = new FileWriter(this.path);
+        for (Task task : storedTasks) {
+            fw.write(task.toFileFormat() + "\n");
+        }
+        fw.close();
+    }
+}
