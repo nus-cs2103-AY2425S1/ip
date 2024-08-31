793b71e217a7759b9cff4a82d7b21338df811ea3
dovlin <dovlincs01@gmail.com>
2024-08-31 22:45:08 +0800

    Delete unnecessary imports on Command

--- src/main/java/momo/command/Command.java
+++ src/main/java/momo/command/Command.java
@@ -1,9 +1,5 @@
 package momo.command;
 
-import momo.MomoException;
-import momo.Storage;
-import momo.task.Task;
-
 public abstract class Command {
 //    public abstract void run(Storage storage) throws MomoException;
 }
