a10d1da519fdf80a1b7ffdc05429ae0d61a4012b
Elinengu <Eline.Ngu2003@gmail.com>
2024-08-31 22:39:22 +0800

    Add Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,31 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a Deadline task with a description and a due date.
  * Extends the Task class to include deadline-specific details.
  */
 public class Deadline extends Task {
-
-    protected String by;
+    protected LocalDate by;
 
     /**
      * Constructs a Deadline task with the specified description and due date.
      *
      * @param description The description of the task.
-     * @param by The due date or time of the task.
+     * @param by          The due date or time of the task.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
 
     @Override
     public String saveToTxt() {
-        int i = this.isDone ? 1 : 0;
+        int i = this.isDone ? 1:0;
         return "D | " + i + " | " + this.description + " | " + this.by;
     }
 }
