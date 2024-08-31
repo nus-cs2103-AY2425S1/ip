325b500a78e0f70f4a88520282dd9b9392787c44
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-09-01 01:50:15 +0800

    Add files that contribute to JavaFX integration

---
+++ src/main/java/luke/LukeUI.java
@@ -0,0 +1,31 @@
+package luke;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Luke using FXML.
+ */
+public class LukeUI extends Application {
+
+    private Luke luke = new Luke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(LukeUI.class.getResource("/view/LukeUiWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<LukeUiWindow>getController().setLuke(luke); // inject the Luke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
