0cb444f27ea452cf47b8a42a491131084e738a76
Hans Delano <hanscau@hotmail.com>
2024-08-31 18:02:00 +0800

    Add java date time for deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    private String by;
+    private LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return String.format("%s (Deadline: by %s) %s", printStatus(), by, getDescription());
+        return String.format("%s (Deadline: by %s) %s", printStatus(),
+                by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")), getDescription());
     }
 
     public String toSaveFile() {
