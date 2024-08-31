f4b57465da7d1b1c5bbe4a54136d23b86d2e7665
ossaris56 <ossaris56@gmail.com>
2024-09-01 03:36:39 +0800

    add parse in parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,10 +1,25 @@
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Parser {
-
 
     public Parser() {
     }
 
-
+    public Command parse(String command) throws AsuraException {
+        List<String> splitCommand = Arrays.asList(command.split(" "));
+        String prefix = splitCommand.get(0);
+        return switch (prefix) {
+            case "list" -> new ListCommand();
+            case "mark" -> new MarkCommand();
+            case "unmark" -> new UnmarkCommand();
+            case "todo" -> new TodoCommand();
+            case "deadline" -> new DeadlineCommand();
+            case "event" -> new EventCommand();
+            case "delete" -> new DeleteCommand();
+            default -> throw new AsuraException("Invalid input");
+        };
+    }
 }
