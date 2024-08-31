b79a632bc9b93727d70c8137388a57c60dfaf1c0
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-08-31 11:57:08 +0800

    Edit Deadlines class to take in LocalTime and LocalDate input

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,19 +1,33 @@
-public class Deadlines extends Task {
-    private String time;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.LocalTime;
 
-    public Deadlines(String description, String time) {
+public class Deadlines extends Task {
+    private LocalDate date;
+    private LocalTime time;
+
+    public Deadlines(String description, LocalDate date, LocalTime time) {
         super(description);
+        this.date = date;
         this.time = time;
     }
 
     @Override
     public String toFile() {
-        return "D | " + (super.isDone() ? "1" : "0") + " | " + super.getDescription() + " | " + this.time;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-dd-yyyy");
+        String formattedDate = this.date.format(formatter);
+
+        return "D | " + (super.isDone() ? "1" : "0") + " | " 
+                + super.getDescription() + " | " + formattedDate + " " + this.time;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.time + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-dd-yyyy");
+        String formattedDate = this.date.format(formatter);
+        
+        return "[D]" + super.toString() + " (by: " + formattedDate + " " + this.time + ")";
     }
     
 }
