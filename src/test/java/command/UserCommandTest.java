289ff298c92b7226d543d494a4fa1d97652ea299
Zac <zacheryngky@gmail.com>
2024-08-31 12:35:55 +0800

    Add test for UserCommand

---
+++ src/test/java/command/UserCommandTest.java
@@ -0,0 +1,45 @@
+package command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+import exception.InvalidCommandException;
+
+public class UserCommandTest {
+    @Test
+    public void toCommand_exitCommand_success() {
+        assertInstanceOf(ExitCommand.class, UserCommand.toCommand("bye"));
+    }
+
+    @Test
+    public void toCommand_listCommand_success() {
+        assertInstanceOf(ListCommand.class, UserCommand.toCommand("list"));
+    }
+
+    @Test
+    public void toCommand_updateStatusCommand_success() {
+        Class<UpdateStatusCommand> expectedClass = UpdateStatusCommand.class;
+        assertInstanceOf(expectedClass, UserCommand.toCommand("mark"));
+        assertInstanceOf(expectedClass, UserCommand.toCommand("unmark"));
+    }
+
+    @Test
+    public void toCommand_addCommand_success() {
+        Class<AddCommand> expectedClass = AddCommand.class;
+        assertInstanceOf(expectedClass, UserCommand.toCommand("todo"));
+        assertInstanceOf(expectedClass, UserCommand.toCommand("deadline"));
+        assertInstanceOf(expectedClass, UserCommand.toCommand("event"));
+    }
+
+    @Test
+    public void toCommand_deleteCommand_success() {
+        assertInstanceOf(DeleteCommand.class, UserCommand.toCommand("delete"));
+    }
+
+    @Test
+    public void toCommand_invalidCommand_invalidCommandExceptionThrow() {
+        assertThrowsExactly(InvalidCommandException.class, () -> UserCommand.toCommand("invalid"));
+    }
+}
