80861ef369d7670bcf3529e541894ef504bdd205
MAOXIONGKAI <conan7153@gmail.com>
2024-08-31 11:46:33 +0800

    Add javadoc for TaskList.java

--- src/main/java/gopher/task/TaskList.java
+++ src/main/java/gopher/task/TaskList.java
@@ -6,18 +6,33 @@ import java.util.ArrayList;
 import java.lang.StringBuilder;
 
 public class TaskList {
+    /** Tasks tracked by the TaskList */
     private ArrayList<Task> tasks;
 
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds the given task to the task list.
+     * Triggers the TaskManager to update the local saved tasks.
+     * Prints the add message on the UI.
+     *
+     * @param task task to be added
+     */
     public void add(Task task) {
         this.tasks.add(task);
         TaskManager.saveTasks(tasks);
         UI.printAddTaskMessage(this, task);
     }
 
+    /**
+     * Deletes the task with the given task number from the task list.
+     * Triggers the TaskManager to update the local saved tasks.
+     * Prints the delete message on the UI.
+     *
+     * @param taskNumber number of the task to be deleted
+     */
     public void delete(int taskNumber) {
         Task task = getTask(taskNumber);
         tasks.remove(taskNumber - 1);
@@ -25,6 +40,13 @@ public class TaskList {
         UI.printDeleteTaskMessage(this, task);
     }
 
+    /**
+     * Marks the task with the given task number as done.
+     * Triggers the TaskManager to update the local saved tasks.
+     * Prints the mark as done message on the UI.
+     *
+     * @param taskNumber number of the task to be marked as done
+     */
     public void markAsDone(int taskNumber) {
         Task task = getTask(taskNumber);
         task.markAsDone();
@@ -32,6 +54,13 @@ public class TaskList {
         UI.printMarkAsDoneMessage(task);
     }
 
+    /**
+     * Marks the task with the given task number as not done.
+     * Triggers the TaskManager to update the local saved tasks.
+     * Prints the mark as not done message on the UI.
+     *
+     * @param taskNumber number of the task to be marked as not done
+     */
     public void markAsUndone(int taskNumber) {
         Task task = getTask(taskNumber);
         task.markAsNotDone();
@@ -39,10 +68,21 @@ public class TaskList {
         UI.printMarkAsUndoneMessage(task);
     }
 
+    /**
+     * Gets the task with the given task number.
+     *
+     * @param taskNumber number of the task wanted
+     * @return task with the specified number
+     */
     public Task getTask(int taskNumber) {
         return this.tasks.get(taskNumber - 1);
     }
 
+    /**
+     * Gets the number of tasks on the task list.
+     *
+     * @return number of tasks on the list
+     */
     public int getSize() {
         return this.tasks.size();
     }
