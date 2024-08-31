92b74351fbc4b30c52ff5726640c9e97ebd6e11d
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-08-31 18:55:48 +0800

    Comply with coding standard by separating import statements

--- src/main/java/colby/Storage.java
+++ src/main/java/colby/Storage.java
@@ -1,6 +1,10 @@
 package colby;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
