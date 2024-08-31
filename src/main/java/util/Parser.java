8f24b8d741aae1a50cbecb617076530b4516cbe6
Ignatius Oey <oey.ignatius@gmail.com>
2024-08-31 18:25:13 +0800

    Parser: Update parse method
    Updated the parse method to manage all possible command cases. Does not
    utilise Ui class for now.

--- src/main/java/util/Parser.java
+++ src/main/java/util/Parser.java
@@ -1,8 +1,13 @@
 package main.java.util;
 
+import main.java.Deadline;
+import main.java.Event;
+import main.java.Todo;
+import main.java.commands.*;
+
 public class Parser {
 
-    private static enum Keywords {
+    private enum Keywords {
         MARK,
         UNMARK,
         LIST,
@@ -11,11 +16,11 @@ public class Parser {
         EVENT,
         DELETE,
         BYE,
-        UNKNOWN;
+        UNKNOWN
     }
 
-    public static String parse(String input) {
-        //TODO future plan to return array of Command instead of String
+    public static Command parse(String input) {
+        //TODO implement parameter parsing for each case
         String[] arr = input.split(" ", 2);
         Keywords keyword;
         try {
@@ -23,37 +28,82 @@ public class Parser {
         } catch (IllegalArgumentException e) {
             keyword = Keywords.UNKNOWN;
         }
-        String result = "";
-
+        Command c = null;
         switch (keyword) {
         case BYE:
-            result = "bye";
+            c = new ExitCommand();
             break;
         case LIST:
-            result = "list";
+            c = new ListCommand();
             break;
         case TODO:
-            result = "todo";
+            try {
+                c = new TodoCommand(new Todo(arr[1]));
+            } catch (IndexOutOfBoundsException e) {
+                //Make Ui display missing todo name error
+                System.out.println("Missing todo name");
+            }
             break;
         case DEADLINE:
-            result = "deadline";
+            try {
+                String[] params = arr[1].split(" /by ");
+                c = new DeadlineCommand(new Deadline(params[0], params[1]));
+            } catch (IndexOutOfBoundsException e) {
+                //Make Ui display invalid Deadline syntax message
+                System.out.println("Invalid deadline syntax");
+            }
             break;
         case EVENT:
-            result = "event";
+            try {
+                String[] params = arr[1].split(" /from ");
+                String name = params[0];
+                String[] datetimes = params[1].split(" /to ");
+                c = new EventCommand(new Event(name, datetimes[0], datetimes[1]));
+            } catch (IndexOutOfBoundsException e) {
+                //Make Ui display invalid Event syntax message
+                System.out.println("Invalid Event syntax");
+            }
             break;
         case MARK:
-            result = "mark";
+            try {
+                int i = Integer.parseInt(arr[1]);
+                c = new MarkCommand(i);
+            } catch (NumberFormatException e) {
+                //arr[1] is not a number
+                System.out.println("Please enter a valid number");
+            } catch (IndexOutOfBoundsException e) {
+                //arr[1] doesn't exist
+                System.out.println("Invalid mark syntax");
+            }
             break;
         case UNMARK:
-            result = "unmark";
+            try {
+                int i = Integer.parseInt(arr[1]);
+                c = new UnmarkCommand(i);
+            } catch (NumberFormatException e) {
+                //arr[1] is not a number
+                System.out.println("Please enter a valid number");
+            } catch (IndexOutOfBoundsException e) {
+                //arr[1] doesn't exist
+                System.out.println("Invalid unmark syntax");
+            }
             break;
         case DELETE:
-            result = "delete";
+            try {
+                int i = Integer.parseInt(arr[1]);
+                c = new DeleteCommand(i);
+            } catch (NumberFormatException e) {
+                //arr[1] is not a number
+                System.out.println("Please enter a valid number");
+            } catch (IndexOutOfBoundsException e) {
+                //arr[1] doesn't exist
+                System.out.println("Invalid delete syntax");
+            }
             break;
         case UNKNOWN:
-            result = "unknown";
+            c = new UnknownCommand();
             break;
         }
-        return result;
+        return c;
     }
 }
