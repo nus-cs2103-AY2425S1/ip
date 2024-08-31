b13f1245d9f511d8a90f6e922c508231d836f48c
Zac <zacheryngky@gmail.com>
2024-08-31 13:31:43 +0800

    Add JavaDocs for exception package

--- src/main/java/exception/InvalidCommandException.java
+++ src/main/java/exception/InvalidCommandException.java
@@ -1,5 +1,8 @@
 package exception;
 
+/**
+ * The InvalidCommandException class is used for invalid commands
+ */
 public class InvalidCommandException extends LevelHundredException {
     public InvalidCommandException() {
         super("I'm sorry, but I don't know what that means :-(");
