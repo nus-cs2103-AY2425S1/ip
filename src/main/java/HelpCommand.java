e1ab21b2e50048b7d37c23990be1b68b5dc3a847
Colin Chiang <colinchiang.cc@gmail.com>
2024-08-31 17:00:00 +0800

    Add more oop

---
+++ src/main/java/HelpCommand.java
@@ -0,0 +1,14 @@
+public class HelpCommand extends Command {
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.show("Here are the list of commands:\n" +
+                "1. list\n\t- lists tasks\n" +
+                "2. mark <task number>\n\t- marks the task as done\n" +
+                "3. unmark <task number>\n\t- unmarks the task\n" +
+                "4. deadline <task description> /by <by>\n\t- Creates a deadline\n" +
+                "5. todo <task description>\n\t- Creates a todo\n" +
+                "6. event <task description> /from <from> /to <to>\n\t- Creates an event\n" +
+                "7. delete <task number>\n\t- deletes the task\n" +
+                "8. bye\n\t- exits the program");
+    }
+}
---
+++ src/main/java/Storage.java
@@ -0,0 +1,50 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Storage {
+
+    private Path filePath;
+    public Storage(String filePath) throws IOException {
+        this.filePath = Paths.get(filePath);
+        Files.createDirectories(this.filePath.getParent());
+
+        if (!Files.exists(this.filePath)) {
+            Files.createFile(this.filePath);
+        }
+    }
+
+    public List<Task> load() throws FileNotFoundException {
+        Scanner fileReader = new Scanner(this.filePath.toFile());
+        List<Task> temp = new ArrayList<>();
+        while (fileReader.hasNext()) {
+            String record = fileReader.nextLine();
+            Task task = Task.from(record);
+            temp.add(task);
+        }
+        return temp;
+    }
+    /**
+     * Saves tasklist in an appropriate format within storage
+     *
+     * @param tasklist Tasklist that is to be saved
+     */
+    public void save(TaskList tasklist) throws IOException {
+        store(tasklist.toText());
+    }
+
+    /**
+     * Stores the given string in storage, overriding any text currently in the file
+     */
+    public void store(String str) throws IOException {
+        FileWriter fw = new FileWriter(filePath.toFile());
+        fw.write(str);
+        fw.close();
+    }
+}
