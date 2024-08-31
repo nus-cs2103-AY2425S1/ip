cec97ba4fb40a0da02e6a294c8749961d6be1656
SooYap <tansooyap@gmail.com>
2024-09-01 00:18:00 +0800

    Instructions.java: Add JavaDoc comments to every method

--- src/main/java/rainy/database/Instructions.java
+++ src/main/java/rainy/database/Instructions.java
@@ -2,7 +2,17 @@ package rainy.database;
 import rainy.tasks.*;
 import rainy.rainyexceptions.*;
 
+/**
+ * Represents the set of user commands issued by the user.
+ */
+
 public enum Instructions {
+    /**
+     * <h1>Instructions created through user input</h1>
+     * An <code>Instructions</code> object
+     * is created through the following user commands: bye, list, mark, unmark, delete, todo, deadline,
+     * event, sort, and invalid if it is anything else.
+     */
     BYE,
     LIST,
     MARK,
