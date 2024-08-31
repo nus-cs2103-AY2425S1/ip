0b3b5f9c28ba1834f6556f4b2c600ce930d2b2ac
SooYap <tansooyap@gmail.com>
2024-09-01 00:19:58 +0800

    Event.java: Add JavaDoc comments to every method

--- src/main/java/rainy/tasks/Event.java
+++ src/main/java/rainy/tasks/Event.java
@@ -5,10 +5,19 @@ import java.time.format.DateTimeFormatter;
 import rainy.database.*;
 import rainy.rainyexceptions.*;
 
+/**
+ * Represents a deadline task. This class is a sub-class of the <code>Task</code> class.
+ */
 public class Event extends Task {
     private String date;
     private String time;
 
+    /**
+     * Constructs a new <code>Event</code> object.
+     * @param name  Represents the name of the task.
+     * @param date  Represents the date of the task.
+     * @param time  Represents the timeframe of the task.
+     */
     public Event(String name, String date, String time) {
         super(name);
         this.date = date;
@@ -23,6 +32,18 @@ public class Event extends Task {
         return this.time;
     }
 
+    /**
+     * Represents the task in a readable format. If the task is read from an existing file, it is directly read into
+     * name of the event. Else, this method does additional formatting to represent the date in a standard format.
+     *{@code
+     * Event event = new Event("project meeting", "02-12/2024", "1800 to 2000");
+     * System.out.println(event);
+     *
+     * // Expected output:
+     * // [E] project meeting  (Aug 19 2024 from 18:00 to 20:00)
+     * }
+     * @return  Returns a string representing the <code>Event</code> object.
+     */
     @Override
     public String toString() {
         try {
