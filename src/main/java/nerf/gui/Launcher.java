72457e877702fd33e9deb1046bc1019a3167654a
KennethTeo2002 <kenneth.teo2002@gmail.com>
2024-08-31 15:06:33 +0800

    Add template GUI from tutorial

---
+++ src/main/java/nerf/gui/Launcher.java
@@ -0,0 +1,13 @@
+package nerf.gui;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
---
+++ src/main/java/nerf/gui/Main.java
@@ -0,0 +1,135 @@
+package nerf.gui;
+
+/**
+ * Main GUI class
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import nerf.Nerf;
+
+
+public class Main extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image nerfImage = new Image(this.getClass().getResourceAsStream("/images/bot.png"));
+    private Nerf nerf = new Nerf("data/taskStorage.txt");
+    /**
+     * Initialise and run the GUI scene.
+     */
+    @Override
+    public void start(Stage stage) {
+        //Setting up required components
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        formatStage(stage, mainLayout);
+        formatComponents();
+
+        stage.setScene(scene);
+        stage.show();
+        
+
+        
+
+       
+    }
+
+    /**
+     * Styles the main frame and anchorpane of the GUI.
+     * 
+     * @param stage GUI window.
+     * @param mainLayout the anchorpane covering the stage.
+     */
+    private void formatStage(Stage stage, AnchorPane mainLayout){
+        stage.setTitle("Nerf chatbot");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
+
+    /**
+     * Styles the other components on the screen.
+     */
+    private void formatComponents(){
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        // auto-scroll to bottom
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+    }
+
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String nerfText = "test";
+        dialogContainer.getChildren().addAll(
+            DialogBox.getUserDialog(userText, userImage),
+            DialogBox.getNerfDialog(nerfText, nerfImage)
+        );
+        
+        userInput.clear();
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     * 
+     * @param input user input.
+     * @return chatbot response.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+}
