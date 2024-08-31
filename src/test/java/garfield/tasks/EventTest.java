009bf73324c4ce81c3c5a5b605bc17c5bdbb9326
chuajunyu <chuajunyu1@gmail.com>
2024-08-31 14:19:03 +0800

    Add JUnit test files for Todo and Event classes

---
+++ src/test/java/garfield/tasks/EventTest.java
@@ -0,0 +1,58 @@
+package garfield.tasks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EventTest {
+    private Event event;
+
+    @BeforeEach
+    void setUp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.event = new Event("Test Event Description",
+                LocalDateTime.parse("2024-03-25 00:00", formatter),
+                LocalDateTime.parse("2024-03-25 23:59", formatter));
+    }
+
+    @Test
+    void constructor_validDescriptionAndDateTimes_taskInitialized() {
+        assertNotNull(event);
+        assertEquals("Test Event Description", event.getTaskDescription());
+        assertEquals("[E][ ] Test Event Description (from: Mar 25 2024 12:00am to: Mar 25 2024 11:59pm)",
+                event.toString());
+    }
+
+    @Test
+    void markAsDone_taskMarkedDone_statusUpdated() {
+        event.markAsDone();
+        assertEquals("[E][X] Test Event Description (from: Mar 25 2024 12:00am to: Mar 25 2024 11:59pm)",
+                event.toString());
+    }
+
+    @Test
+    void markAsUndone_taskPreviouslyMarkedDone_statusReverted() {
+        event.markAsDone(); // Mark as done first
+        event.markAsUndone();
+        assertEquals("[E][ ] Test Event Description (from: Mar 25 2024 12:00am to: Mar 25 2024 11:59pm)",
+                event.toString());
+    }
+
+    @Test
+    void toSaveRepresentation_taskNotDone_correctSaveFormat() {
+        assertEquals("E | 0 | Test Event Description | 2024-03-25 00:00 | 2024-03-25 23:59",
+                event.toSaveRepresentation());
+    }
+
+    @Test
+    void toSaveRepresentation_taskDone_correctSaveFormat() {
+        event.markAsDone();
+        assertEquals("E | 1 | Test Event Description | 2024-03-25 00:00 | 2024-03-25 23:59",
+                event.toSaveRepresentation());
+    }
+}
---
+++ src/test/java/garfield/tasks/TodoTest.java
@@ -0,0 +1,50 @@
+package garfield.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+
+
+class TodoTest {
+
+    private Todo todo;
+
+    @BeforeEach
+    void setUp() {
+        todo = new Todo("Write unit tests");
+    }
+
+    @Test
+    void constructor_validDescription_taskInitialized() {
+        assertNotNull(todo);
+        assertEquals("Write unit tests", todo.getTaskDescription());
+        assertEquals("[T][ ] Write unit tests", todo.toString());
+    }
+
+    @Test
+    void markAsDone_taskMarkedDone_statusUpdated() {
+        todo.markAsDone();
+        assertEquals("[T][X] Write unit tests", todo.toString());
+    }
+
+    @Test
+    void markAsUndone_taskPreviouslyMarkedDone_statusReverted() {
+        todo.markAsDone(); // Mark as done first
+        todo.markAsUndone();
+        assertEquals("[T][ ] Write unit tests", todo.toString());
+    }
+
+    @Test
+    void toSaveRepresentation_taskNotDone_correctSaveFormat() {
+        assertEquals("T | 0 | Write unit tests", todo.toSaveRepresentation());
+    }
+
+    @Test
+    void toSaveRepresentation_taskDone_correctSaveFormat() {
+        todo.markAsDone();
+        assertEquals("T | 1 | Write unit tests", todo.toSaveRepresentation());
+    }
+}
+
