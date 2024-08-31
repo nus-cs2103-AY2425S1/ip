e4cc4ee3371d7891e5b3127e5e5d4d1fa5b3c99e
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-09-01 02:51:43 +0800

    Add date and time for class Event

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,7 +8,7 @@ public class Deadline extends Task {
 
     protected String desc;
     protected String by;
-    protected LocalDate date;
+    protected String date;
     protected String time;
 
     public Deadline(String description) {
@@ -16,34 +16,37 @@ public class Deadline extends Task {
         if (description.startsWith("deadline ")) {
             this.desc = description.split("deadline ")[1].split(" /by")[0];
             this.by = description.split("/by ")[1];
+            String dateString = by.split(" ")[0];
+            try {
+                this.date = LocalDate.parse(dateString).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+                SimpleDateFormat inputTime = new SimpleDateFormat("HHmm");
+                SimpleDateFormat outputTime = new SimpleDateFormat("hh:mm a");
+                Date time24 = inputTime.parse(by.split(" ")[1]);
+                this.time = outputTime.format(time24);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         } else if (description.startsWith("[D][ ] ")) {
             String[] parts = description.split("\\[D\\]\\[ \\] ");
             this.desc = parts[1].split(" \\(by:")[0];
             String[] byParts = parts[1].split("\\(by: ");
             this.by = byParts[1].split("\\)")[0];
             this.isDone = false;
+            this.date = by.split(", ")[0];
+            this.time = by.split(", ")[1];
         } else if (description.startsWith("[D][X] ")) {
             String[] parts = description.split("\\[D\\]\\[X\\] ");
             this.desc = parts[1].split(" \\(by:")[0];
             String[] byParts = parts[1].split("\\(by: ");
             this.by = byParts[1].split("\\)")[0];
             this.isDone = true;
+            this.date = by.split(", ")[0];
+            this.time = by.split(", ")[1];
         }
-        String dateString = by.split(" ")[0];
-        try {
-            this.date = LocalDate.parse(dateString);
-            SimpleDateFormat inputTime = new SimpleDateFormat("HHmm");
-            SimpleDateFormat outputTime = new SimpleDateFormat("hh:mm a");
-            Date time24 = inputTime.parse(by.split(" ")[1]);
-            this.time = outputTime.format(time24);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
     }
 
     public String toString() {
         return "[D]" + this.getStatusIcon() + this.desc + " (by: "
-                + this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " " + time + ")";
+                + this.date + ", " + this.time + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,18 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Event extends Task {
 
     protected String desc;
     protected String from;
     protected String to;
+    protected String fromDate;
+    protected String fromTime;
+    protected String toDate;
+    protected String toTime;
 
     public Event(String description) {
         super(description);
@@ -10,6 +20,26 @@ public class Event extends Task {
             this.desc = description.split("event ")[1].split(" /from")[0];
             this.from = description.split("/from ")[1].split(" /to")[0];
             this.to = description.split("/to ")[1];
+            String fromDateString = from.split(" ")[0];
+            try {
+                this.fromDate = LocalDate.parse(fromDateString).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+                SimpleDateFormat inputTime = new SimpleDateFormat("HHmm");
+                SimpleDateFormat outputTime = new SimpleDateFormat("hh:mm a");
+                Date time24 = inputTime.parse(from.split(" ")[1]);
+                this.fromTime = outputTime.format(time24);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            String toDateString = to.split(" ")[0];
+            try {
+                this.toDate = LocalDate.parse(toDateString).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+                SimpleDateFormat inputTime = new SimpleDateFormat("HHmm");
+                SimpleDateFormat outputTime = new SimpleDateFormat("hh:mm a");
+                Date time24 = inputTime.parse(to.split(" ")[1]);
+                this.toTime = outputTime.format(time24);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         } else if (description.startsWith("[E][ ] ")) {
             String[] parts = description.split("\\[E\\]\\[ \\] ");
             this.desc = parts[1].split(" \\(from:")[0];
@@ -18,6 +48,10 @@ public class Event extends Task {
             String[] toParts = fromParts[1].split(" to: ");
             this.to = toParts[1].split("\\)")[0];
             this.isDone = false;
+            this.fromDate = from.split(", ")[0];
+            this.fromTime = from.split(", ")[1];
+            this.toDate = to.split(", ")[0];
+            this.toTime = to.split(", ")[1];
         } else if (description.startsWith("[E][X] ")) {
             String[] parts = description.split("\\[E\\]\\[X\\] ");
             this.desc = parts[1].split(" \\(from:")[0];
@@ -26,11 +60,17 @@ public class Event extends Task {
             String[] toParts = fromParts[1].split(" to: ");
             this.to = toParts[1].split("\\)")[0];
             this.isDone = true;
+            this.fromDate = from.split(", ")[0];
+            this.fromTime = from.split(", ")[1];
+            this.toDate = to.split(", ")[0];
+            this.toTime = to.split(", ")[1];
         }
 
     }
 
     public String toString() {
-        return "[E]" + this.getStatusIcon() + this.desc + " (from: " + from + " to: " + to + ")";
+        return "[E]" + this.getStatusIcon() + this.desc + " (from: " +
+                this.fromDate + ", " + this.fromTime + " to: " +
+                this.toDate + ", " + this.toTime + ")";
     }
 }
