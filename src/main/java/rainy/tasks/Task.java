358afbd37d97dca1144b9efa3c6668c1f210b602
SooYap <tansooyap@gmail.com>
2024-09-01 00:20:09 +0800

    Task.java: Add JavaDoc comments to every method

--- src/main/java/rainy/tasks/Task.java
+++ src/main/java/rainy/tasks/Task.java
@@ -3,12 +3,23 @@ import java.time.LocalDate;
 import rainy.database.*;
 import rainy.rainyexceptions.*;
 
+/**
+ * Represents each individual task. Its parameters tells users of details such as its completion status,
+ * name, date, and a <code>UI</code> object to display chatbot messages.
+ */
 public class Task implements Comparable<Task> {
     private boolean isDone;
     protected String name;
     protected LocalDate compareDate;
     private UI ui;
 
+    /**
+     * Constructs a <code>Task</code> object. The date of the object is initially set to MAX. For <code>Deadline</code> objects
+     * and <codeEvent</code> objects which extend the <code>Task</code> class, the date of the object is determined by user input.
+     * For the <code>ToDo</code> class which also extends <code>Task</code>, the date will be set to MAX because the user input does not
+     * require a date. THis is important when this class sorts the tasks in a list.
+     * @param name
+     */
     public Task(String name) {
         this.isDone = false;
         this.name = name;
@@ -24,6 +35,10 @@ public class Task implements Comparable<Task> {
         return this.name;
     }
 
+    /**
+     * Marks the task itself as specified by the user.
+     * @throws InvalidMarkAndUnmarkException  Thrown by <code>Task</code> object when user wants to mark a marked tasked.
+     */
     public void mark() throws InvalidMarkAndUnmarkException {
         if (this.isDone == true) {
             throw new InvalidMarkAndUnmarkException(this.ui.taskDone());
@@ -32,6 +47,10 @@ public class Task implements Comparable<Task> {
         }
     }
 
+    /**
+     * Unmarks the task itself as specified by the user.
+     * @throws InvalidMarkAndUnmarkException  Thrown by <code>Task</code> object when user wants to unmark an unmarked task.
+     */
     public void unmark() throws InvalidMarkAndUnmarkException {
         if (this.isDone == false) {
             throw new InvalidMarkAndUnmarkException(this.ui.taskNotDone());
@@ -39,6 +58,14 @@ public class Task implements Comparable<Task> {
             this.isDone = false;
         }
     }
+
+    /**
+     * Compares tasks by their dates in ascending order. For <code>ToDo</code> tasks with no
+     * date requirement, the date parameter is set to MAX. This method sorts them by this MAX date.
+     * @param t The <code>Task</code> object to be compared.
+     * @return  Returns a -1 if this the object to be compared has a later date, 0 if it has the same
+     * date, and 1 if it has an earlier date.
+     */
     @Override
     public int compareTo(Task t) {
         return this.compareDate.compareTo(t.compareDate);
