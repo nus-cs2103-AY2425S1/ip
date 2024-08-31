73e195d375d6b80296c0afe29f80c69f1f9735e1
chuajunyu <chuajunyu1@gmail.com>
2024-08-31 15:12:10 +0800

    Add header comments to classes in garfield.tasks

--- src/main/java/garfield/tasks/Deadline.java
+++ src/main/java/garfield/tasks/Deadline.java
@@ -3,11 +3,21 @@ package garfield.tasks;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The Deadline class represents a task with a specific deadline in the Garfield chatbot application.
+ * It extends the Task class to provide a representation for tasks that have a deadline.
+ */
 public class Deadline extends Task {
     private LocalDateTime deadline;
     private DateTimeFormatter saveFormatter;
     private DateTimeFormatter uiFormatter;
 
+    /**
+     * Constructs a new Deadline task with the specified description and deadline.
+     *
+     * @param taskDescription The description of the deadline task.
+     * @param deadline The deadline of the task, represented as a LocalDateTime object.
+     */
     public Deadline(String taskDescription, LocalDateTime deadline) {
         super(taskDescription);
         this.deadline = deadline;
@@ -15,11 +25,27 @@ public class Deadline extends Task {
         this.uiFormatter = DateTimeFormatter.ofPattern("MMM d yyyy hh:mma");
     }
 
+    /**
+     * Returns a string representation of the deadline task suitable for display to the user.
+     * The representation includes a "[D]" prefix to indicate that it is a deadline task,
+     * along with the completion status, task description, and formatted deadline.
+     *
+     * @return A string representation of the deadline task, with "[D]" indicating the task type
+     *         and the deadline formatted for user interface display.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.deadline.format(uiFormatter) + ")";
     }
 
+    /**
+     * Returns a string representation of the deadline task suitable for saving to storage.
+     * The representation includes a "D | " prefix to indicate that it is a deadline task,
+     * followed by the completion status, task description, and formatted deadline.
+     *
+     * @return A string representation of the deadline task, with "D | " indicating the task type
+     *         and the deadline formatted for saving.
+     */
     @Override
     public String toSaveRepresentation() {
         return "D | " + super.toSaveRepresentation() + " | " + deadline.format(saveFormatter);
