c5393ea902b244512f56a43e2513d4c1d3282e3a
ishan-agarwal-05 <ishanagarwal270805@gmail.com>
2024-09-01 00:12:04 +0800

    Add ability to mark tasks as done and not done

--- src/main/java/JohnCena.java
+++ src/main/java/JohnCena.java
@@ -1,33 +1,47 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JohnCena {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
-        int taskCount = 0;
-        
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm [YOUR CHATBOT NAME]");
+        System.out.println(" Hello! I'm John Cena");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
         while (true) {
             String input = scanner.nextLine();
-
             if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks.get(taskIndex).markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks.get(taskIndex));
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks.get(taskIndex).markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks.get(taskIndex));
+                System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = input;
-                taskCount++;
+                tasks.add(new Task(input));
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
@@ -37,4 +51,3 @@ public class JohnCena {
         scanner.close();
     }
 }
-
