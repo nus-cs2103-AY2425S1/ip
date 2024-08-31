52f1e054898ecdf2d70477345e9ec495a63667ba
DanzaSeah <e0959436@u.nus.edu>
2024-08-31 23:35:43 +0800

    Add JavaDocs to BabbleBotException class

--- src/main/java/babblebot/exception/BabbleBotException.java
+++ src/main/java/babblebot/exception/BabbleBotException.java
@@ -1,4 +1,8 @@
 package babblebot.exception;
 
+/**
+ * The BabbleBotException class represents a custom exception used in the BabbleBot application.
+ * This exception is thrown when the application encounters an error specific to its operation.
+ */
 public class BabbleBotException extends Exception{
 }
