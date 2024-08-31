cdf1ac75bcfb5e9aadfda6c7579e3bbd635f8186
lm-44 <yauchengyong.literallyme@gmail.com>
2024-08-31 20:58:47 +0800

    Revert "Test merge conflict"
    This reverts commit a339fd1f12de6286c785eef580ae0bcd1e6e61c6.

--- src/main/java/cook/Cook.java
+++ src/main/java/cook/Cook.java
@@ -15,7 +15,7 @@ public class Cook {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-    private Parser parse;
+    private Parser parser;
 
     public Cook (File file) {
         this.storage = new Storage(file);
