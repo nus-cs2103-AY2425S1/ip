30879f49b4d29cc5df7de0bc6966ca4ac08925ff
Zac <zacheryngky@gmail.com>
2024-08-31 13:51:39 +0800

    Add JavaDocs for utility package

--- src/main/java/utility/Parser.java
+++ src/main/java/utility/Parser.java
@@ -8,11 +8,17 @@ import exception.*;
 import task.*;
 
 /**
- * The Parser class provides utility functions 
+ * The Parser class provides utility functions
  * to parse input data into useful formats
  */
 
 public class Parser {
+    /**
+     * Parses a line in the storage file to the corresonding Task
+     * @param line String representing a line in the storage file
+     * @return Task represented by the line
+     * @throws InvalidStorageFileException if the line is in an invalid format
+     */
     public static Task parseStorageFileLine(String line) throws InvalidStorageFileException {
         try {
             char START_IDX = 7;
@@ -67,7 +73,7 @@ public class Parser {
     }
 
     /**
-     * Parse user input command to generate the appropriate UserCommand to be executed
+     * Parses user input command to generate the appropriate UserCommand to be executed
      * @param userInput String representing the user input command
      * @return UserCommand to be executed
      * @throws InvalidCommandException
@@ -78,10 +84,10 @@ public class Parser {
     }
 
     /**
-     * Parse user input date from yyyy-mm-dd hhmm format to d mmm yyyy format
+     * Parses user input date from yyyy-mm-dd hhmm format to d mmm yyyy hhmm format
      * @param date User input string representing the date
      * @return Output string representing date in d mmm yyyy format
-     * @throws ParseException 
+     * @throws ParseException if an invalid date is given
      */
     public static String parseInputDate(String date) {
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
