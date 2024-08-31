13ba6f8fd15ce38f68eab4945f1d5fb01b76f8f4
wubojin <wubojin64@gmail.com>
2024-09-01 03:50:57 +0800

    Add Level 8: Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,24 +1,26 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
     }
 
     public Deadline(String description, String by, boolean isDone) {
         super(description, isDone);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
     }
 
     @Override
     public String toDataString() {
         return "D | " + (isDone ? "1" : "0") + " | " + description
-                + " | " + by;
+                + " | " + by.format(INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 }
