763a88f93d1571b8d557fd9a3248c32ce8694e07
DanzaSeah <e0959436@u.nus.edu>
2024-08-31 23:29:25 +0800

    Add JavaDocs to Todo class

--- src/main/java/babblebot/task/Todo.java
+++ src/main/java/babblebot/task/Todo.java
@@ -1,9 +1,25 @@
 package babblebot.task;
 
+/**
+ * The Todo class represents a simple task with no specific date attached.
+ * It extends the Task class and does not add any additional fields.
+ */
 public class Todo extends Task {
+    /**
+     * Constructs a Todo task with the specified description.
+     *
+     * @param description The description of the todo task.
+     */
     public Todo(String description) {
         super(description);
     }
+
+
+    /**
+     * Converts the Todo task to a format suitable for file storage.
+     *
+     * @return A string representation of the Todo task in file format.
+     */
     @Override
     public String toFileFormat() {
         if (getStatusIcon().equals("X")) {
@@ -12,6 +28,12 @@ public class Todo extends Task {
             return "T" + " | " + "0" + " | " + this.description;
         }
     }
+
+    /**
+     * Returns a string representation of the Todo task.
+     *
+     * @return A string representation of the Todo task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
