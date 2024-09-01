330d0a017e128ea4607956d74a56d8ccb42aff60
quahkk <quahkaikiat1@gmail.com>
2024-09-02 01:17:22 +0800

    Follow the coding standard

--- src/test/java/gale/GaleTest.java
+++ src/test/java/gale/GaleTest.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GaleTest {
