d7d54568bc513c281d9a25dc697b64b3216f6739
ashea10 <enasveen13@gmail.com>
2024-09-01 21:27:26 +0800

    Implement TaskList class to handle task operations

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public void deleteTask(int index) throws ConverSageException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new ConverSageException("Invalid task number!");
+        }
+        tasks.remove(index);
+    }
+
+    public Task getTask(int index) throws ConverSageException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new ConverSageException("Invalid task number!");
+        }
+        return tasks.get(index);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+
+}
