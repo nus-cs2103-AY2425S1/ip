fe82c99cea285282de255a35ba8d7c14f43f8ab8
YangQF2002 <yangqf18@gmail.com>
2024-09-01 21:07:54 +0800

    Revert "Use packages to group related types together"
    This reverts commit 0fdae8580aeeb9a9154af6fadc0bdd220744eac3.

--- src/main/java/Storage/Storage.java
+++ src/main/java/Storage/Storage.java
@@ -145,8 +145,7 @@ public class Storage {
         fileResult.append("Creating save file for tasks...\n")
                 .append(fileStatus);
 
-        return new String[] {dirResult.toString()
-                , fileResult.toString()};
+        return new String[] {dirResult.toString(), fileResult.toString()};
     }
 
     // Read from file with no exclusion
