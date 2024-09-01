5117571336c01c57b76acd3bb39e9579fdf2e828
1st2GetThisName <adrieledix@gmail.com>
2024-09-01 20:21:09 +0800

    Extend date detection to Events
    Only the field "by" under deadlines detect dates.
    
    Let's make date detection into its own class and implement it into Event.java.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -6,49 +6,15 @@ public class Deadline extends Task {
     private String byString;
     private LocalDate byDate;
     private boolean isDate;
-    private static final String[] supportedDates = {
-            "uuuu-MM-dd", "uu-MM-dd",
-            "dd MMM, uuuu", "MMM dd, uuuu", "dd MMM uuuu", "MMM dd uuuu", "uuuu MMM dd", "uuuu dd MMM",
-            "dd MMMM, uuuu", "MMMM dd, uuuu", "dd MMMM uuuu", "MMMM dd uuuu", "uuuu MMMM dd", "uuuu dd MMMM",
-            "MM/dd/uuuu", "MM/dd/uu", "MM/dd",
-            "MMM dd", "dd MMM",
-            "MMMM dd", "dd MMMM"
-    }; //TODO shorten this using []
-    private static final String[] supportedTimes = {
-            "HHmm", "HH:mm", "HH:mm:ss", "HH.mm", "HH.mm.ss",
-            "hh:mm a", "hh:mm:ss a", "hh.mm a", "hh.mm.ss a"
-    };
 
     public Deadline(String description, String by) {
         super(description);
         this.byString = by;
         isDate = false;
-        for (int i = 0; i < supportedDates.length; ++i) {
-            try {
-                byDate = LocalDate.parse(by,
-                        DateTimeFormatter.ofPattern(supportedDates[i]));
-                isDate = true;
-                return;
-            } catch (DateTimeParseException ignored) {
-            }
-            for (int j = 0; j < supportedTimes.length; ++j) {
-                try {
-                    byDate = LocalDate.parse(by,
-                            DateTimeFormatter.ofPattern(supportedDates[i] + " " + supportedTimes[j]));
-                    isDate = true;
-                    return;
-                } catch (DateTimeParseException ignored) {
-                }
-                if (j != 0) {
-                    try {
-                        byDate = LocalDate.parse(by,
-                                DateTimeFormatter.ofPattern(supportedTimes[j] + " " + supportedDates[i]));
-                        isDate = true;
-                        return;
-                    } catch (DateTimeParseException ignored) {
-                    }
-                }
-            }
+        try {
+            byDate = CustomDateTimeParser.parseDateTime(by);
+            isDate = true;
+        } catch (DateTimeParseException ignored) {
         }
     }
 
@@ -58,7 +24,7 @@ public class Deadline extends Task {
      */
     public String getBy() {
         if (isDate) {
-            return byDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy[ HH:mm[:ss]]"));
+            return byDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
         } else {
             return byString;
         }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,10 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
-    private String begin, end;
+    private final String beginString, endString;
+    private boolean isDateBegin, isDateEnd;
+    private LocalDate beginDate, endDate;
 
     public Event(String description, String begin, String end) {
         super(description);
-        this.begin = begin;
-        this.end = end;
+        this.beginString = begin;
+        this.endString = end;
+        isDateBegin = isDateEnd = false;
+        try {
+            beginDate = CustomDateTimeParser.parseDateTime(begin);
+            isDateBegin = true;
+        } catch (DateTimeParseException ignored) {
+        }
+        try {
+            endDate = CustomDateTimeParser.parseDateTime(end);
+            isDateEnd = true;
+        } catch (DateTimeParseException ignored) {
+        }
     }
 
     /**
@@ -12,7 +29,11 @@ public class Event extends Task {
      * @return The start of the event
      */
     public String getBegin() {
-        return this.begin;
+        if (isDateBegin) {
+            return beginDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+        } else {
+            return beginString;
+        }
     }
 
     /**
@@ -20,12 +41,16 @@ public class Event extends Task {
      * @return The end of the event
      */
     public String getEnd() {
-        return this.end;
+        if (isDateEnd) {
+            return endDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+        } else {
+            return endString;
+        }
     }
 
     @Override
     public String toString() {
         String prev = super.toString();
-        return "[E" + prev.substring(2) + " (begins: " + begin + ", ends: " + end + ")";
+        return "[E" + prev.substring(2) + " (begins: " + getBegin() + ", ends: " + getEnd() + ")";
     }
 }
