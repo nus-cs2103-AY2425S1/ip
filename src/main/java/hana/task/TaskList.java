77732c64ad0179d7bc5f2ad3d416c9301a537a96
hansneddyanto <hans.neddy@gmail.com>
2024-09-02 00:15:05 +0800

    Put more JavaDoc comments to the other files

--- src/main/java/hana/task/TaskList.java
+++ src/main/java/hana/task/TaskList.java
@@ -3,33 +3,70 @@ package hana.task;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a list of tasks.
+ */
 public class TaskList {
     private List<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with an initial list of tasks.
+     *
+     * @param tasks The initial list of tasks.
+     */
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the list.
+     *
+     * @param task The task to be added.
+     */
     public void add(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Removes the task at the specified index from the list.
+     *
+     * @param index The index of the task to be removed.
+     */
     public void remove(int index) {
         tasks.remove(index);
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     *
+     * @param index The index of the task to retrieve.
+     * @return The task at the specified index.
+     */
     public Task get(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The number of tasks in the list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Returns the list of tasks.
+     *
+     * @return The list of tasks.
+     */
     public List<Task> getTasks() {
         return tasks;
     }
--- src/main/java/hana/task/ToDo.java
+++ src/main/java/hana/task/ToDo.java
@@ -1,11 +1,24 @@
 package hana.task;
 
+/**
+ * Represents a ToDo task.
+ */
 public class ToDo extends Task {
 
+    /**
+     * Constructs a ToDo task with a description.
+     *
+     * @param description The description of the ToDo task.
+     */
     public ToDo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the ToDo task, including its status.
+     *
+     * @return A string representation of the task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
