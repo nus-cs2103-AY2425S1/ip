ce2dbc6d4284f5a254f4ab55e2da19b789838ea9
1st2GetThisName <adrieledix@gmail.com>
2024-09-01 22:43:36 +0800

    Make UI its own class
    Code is currently all run under a single program Vecrosen.java
    
    Modularizing the code has benefits down the line.
    
    Let's create a class called UI.

---
+++ src/main/java/Ui.java
@@ -0,0 +1,36 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner input;
+    private PrintStream output;
+    public Ui(InputStream inputStream, PrintStream outputStream) {
+        input = new Scanner(inputStream);
+        output = outputStream;
+    }
+
+    /**
+     * Outputs a string with an indentation, to differentiate Vecrosen's dialog from the user's.
+     * @param s The string to be printed.
+     */
+    public void speak(String s) {
+        output.print("    ");
+        output.println(s);
+    }
+
+    /**
+     * Outputs an alert informing the user they have formatted their command incorrectly,
+     * and elaborates on how they were supposed to use said command.
+     * @param usage The proper usage of the command.
+     */
+    public void invalidFormat(String usage) {
+        speak("Invalid format.");
+        speak("Usage: " + usage);
+    }
+
+    public String readline() {
+        return input.nextLine();
+    }
+}
--- src/main/java/Vecrosen.java
+++ src/main/java/Vecrosen.java
@@ -10,15 +10,7 @@ public class Vecrosen {
 
     private enum TaskType {todo, deadline, event};
     private static ArrayList<Task> list;
-
-    /**
-     * Outputs a string with an indentation, to differentiate Vecrosen's dialog from the user's.
-     * @param s The string to be printed.
-     */
-    private static void speak(String s) {
-        System.out.print("    ");
-        System.out.println(s);
-    }
+    private static Ui ui;
 
     /**
      * Saves the task list to vecrosen.txt.
@@ -90,16 +82,17 @@ public class Vecrosen {
         } catch (FileNotFoundException e) {
             System.err.println("File exists but cannot be found...?");
         } catch (NoSuchElementException | IndexOutOfBoundsException e) {
-            speak("Savefile is corrupted!");
+            ui.speak("Savefile is corrupted!");
             System.err.println(list.size());
             list.clear();
         }
     }
 
     public static void main(String[] args) {
+        ui = new Ui(System.in, System.out);
         list = new ArrayList<Task>();
-        speak("Hello, I'm Vecrosen.");
-        speak("What can I do for you?");
+        ui.speak("Hello, I'm Vecrosen.");
+        ui.speak("What can I do for you?");
         load(new File("data/vecrosen.txt"));
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -109,32 +102,32 @@ public class Vecrosen {
                 break;
             } else if (input.matches("mark \\d+")) {
                 int itemNo = Integer.parseInt(input.substring(5));
-                if (itemNo < 1 || itemNo > list.size()) speak("Invalid task number!");
+                if (itemNo < 1 || itemNo > list.size()) ui.speak("Invalid task number!");
                 else {
                     list.get(itemNo-1).setDone(true);
-                    speak("Task marked as complete: " + list.get(itemNo-1).getDescription());
+                    ui.speak("Task marked as complete: " + list.get(itemNo-1).getDescription());
                 }
             } else if (input.matches("unmark \\d+")) {
                 int itemNo = Integer.parseInt(input.substring(7));
-                if (itemNo < 1 || itemNo > list.size()) speak("Invalid task number!");
+                if (itemNo < 1 || itemNo > list.size()) ui.speak("Invalid task number!");
                 else {
                     list.get(itemNo-1).setDone(false);
-                    speak("Task marked as incomplete: " + list.get(itemNo-1).getDescription());
+                    ui.speak("Task marked as incomplete: " + list.get(itemNo-1).getDescription());
                 }
             } else if (input.equals("list")) {
                 for (int i = 0; i < list.size(); ++i) {
-                    speak((i+1) + "." + list.get(i).toString());
+                    ui.speak((i+1) + "." + list.get(i).toString());
                 }
             } else if (input.matches("todo .+")) {
                 String desc = input.substring(5);
                 list.add(new Task(desc));
-                speak("Todo added: " + desc);
+                ui.speak("Todo added: " + desc);
             } else if (input.matches("deadline .+ /by .+")) {
                 int byStart = input.indexOf("/by ");
                 String by = input.substring(byStart + 4);
                 String desc = input.substring(9, byStart - 1);
                 list.add(new Deadline(desc, by));
-                speak("Deadline added: " + desc);
+                ui.speak("Deadline added: " + desc);
             } else if (input.matches("event .+ /begin .+ /end .+")) {
                 int beginStart = input.indexOf("/begin ");
                 int endStart = input.indexOf("/end ");
@@ -142,7 +135,7 @@ public class Vecrosen {
                 String begin = input.substring(beginStart + 7, endStart - 1);
                 String end = input.substring(endStart + 5);
                 list.add(new Event(desc, begin, end));
-                speak("Event added: " + desc);
+                ui.speak("Event added: " + desc);
             } else if (input.matches("event .+ /end .+ /begin .+")) {
                 int beginStart = input.indexOf("/begin ");
                 int endStart = input.indexOf("/end ");
@@ -150,40 +143,34 @@ public class Vecrosen {
                 String end = input.substring(endStart + 5, beginStart - 1);
                 String begin = input.substring(beginStart + 7);
                 list.add(new Event(desc, begin, end));
-                speak("Event added: " + desc);
+                ui.speak("Event added: " + desc);
             } else if (input.matches("delete \\d+")) {
                 int itemNo = Integer.parseInt(input.substring(7));
-                if (itemNo < 1 || itemNo > list.size()) speak("Invalid task number!");
+                if (itemNo < 1 || itemNo > list.size()) ui.speak("Invalid task number!");
                 else {
-                    speak("Removing task: " + list.get(itemNo - 1).getDescription());
+                    ui.speak("Removing task: " + list.get(itemNo - 1).getDescription());
                     list.remove(itemNo - 1);
-                    speak("You now have " + list.size() + " tasks left in record.");
+                    ui.speak("You now have " + list.size() + " tasks left in record.");
                 }
             } // TODO: make helper function
             else if (input.startsWith("todo")) {
-                speak("Invalid format.");
-                speak("Usage: todo [description]");
+                ui.invalidFormat("todo [description]");
             } else if (input.startsWith("deadline")) {
-                speak("Invalid format.");
-                speak("Usage: deadline [description] /by [deadline]");
+                ui.invalidFormat("deadline [description] /by [deadline]");
             } else if (input.startsWith("event")) {
-                speak("Invalid format.");
-                speak("Usage: event [description] /begin [startTime] /end [endTime]");
+                ui.invalidFormat("event [description] /begin [startTime] /end [endTime]");
             } else if (input.startsWith("mark")) {
-                speak("Invalid format.");
-                speak("Usage: mark [taskID]");
+                ui.invalidFormat("mark [taskID]");
             } else if (input.startsWith("unmark")) {
-                speak("Invalid format.");
-                speak("Usage: unmark [taskID]");
+                ui.invalidFormat("unmark [taskID]");
             } else if (input.startsWith("delete")) {
-                speak("Invalid format.");
-                speak("Usage: delete [taskID]");
+                ui.invalidFormat("delete [taskID]");
             } else {
-                speak("Sorry, I don't understand.");
-                speak("Commands: todo deadline event mark unmark delete list bye");
+                ui.speak("Sorry, I don't understand.");
+                ui.speak("Commands: todo deadline event mark unmark delete list bye");
             }
             save();
         }
-        speak("Bye. Hope to see you again soon!");
+        ui.speak("Bye. Hope to see you again soon!");
     }
 }
