741eb85cb6690f1faece69b0b920162b9a8b900d
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-09-01 19:45:55 +0800

    Create DateTimeParser.java to interconvert between strings and datetime

---
+++ src/main/java/DateTimeParser.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateTimeParser {
+
+    public static LocalDate to_datetime(String input_str) throws DateTimeParseException {
+        // String[] splitInput = input_str.split(" ");
+        LocalDate date = LocalDate.parse(input_str);
+        return date;
+    }
+
+    public static String to_str(LocalDate input_date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM, yyyy");
+        return input_date.format(formatter);
+    }
+}
