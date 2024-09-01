5e04bb663d894fd49e28063a02183f36466cf7b1
yxtan02 <yxtan02@gmail.com>
2024-09-02 01:05:05 +0800

    Add GUI using JavaFX

---
+++ src/main/java/thanos/ui/components/DialogBox.java
@@ -0,0 +1,94 @@
+package thanos.ui.components;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+
+/**
+ * Represents a dialog box in the application, displaying a text message and an image.
+ * <p>
+ * The {@code DialogBox} class extends {@code HBox} and is used to create a UI component
+ * that shows a message along with a corresponding image, such as a user's profile picture or a bot's avatar.
+ * </p>
+ */
+public class DialogBox extends HBox {
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
+
+    /**
+     * Constructs a new {@code DialogBox} with the specified text and image.
+     * <p>
+     * This constructor initializes the dialog box by loading the FXML layout,
+     * setting the text of the {@code Label}, and configuring the {@code ImageView} with the given image.
+     * The image is clipped to a circular shape.
+     * </p>
+     *
+     * @param message the text message to be displayed in the dialog box.
+     * @param image the image to be displayed in the dialog box.
+     */
+    private DialogBox(String message, Image image) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        dialog.setText(message);
+        Circle clip = new Circle(48, 48, 48);
+        displayPicture.setClip(clip);
+        displayPicture.setImage(image);
+    }
+
+    /**
+     * Reverses the order of the children in the dialog box and aligns it to the top left.
+     * <p>
+     * This method is used to flip the dialog box, typically to switch between user and bot messages.
+     * </p>
+     */
+    private void flip() {
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    /**
+     * Creates a new {@code DialogBox} for the user with the specified text and image.
+     *
+     * @param message the text message to be displayed in the dialog box.
+     * @param image the image to be displayed in the dialog box.
+     * @return a {@code DialogBox} instance configured for user messages.
+     */
+    public static DialogBox getUserDialog(String message, Image image) {
+        return new DialogBox(message, image);
+    }
+
+    /**
+     * Creates a {@code DialogBox} instance for Thanos' messages with the specified text and image.
+     *
+     * @param message the text message to be displayed in the dialog box.
+     * @param image the image to be displayed in the dialog box.
+     * @return a {@code DialogBox} instance configured for Thanos' messages.
+     */
+    public static DialogBox getThanosDialog(String message, Image image) {
+        var db = new DialogBox(message, image);
+        db.flip();
+        return db;
+    }
+}
---
+++ src/main/java/thanos/ui/components/MainWindow.java
@@ -0,0 +1,84 @@
+package thanos.ui.components;
+
+import java.util.Objects;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+import thanos.Thanos;
+
+/**
+ * Represents the main window of the application, handling user interactions and displaying dialog messages.
+ * <p>
+ * The {@code MainWindow} class extends {@code AnchorPane} and manages the user interface components,
+ * including the text input field, send button, and the container for dialog messages.
+ * </p>
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Thanos thanos;
+
+    private final Image userImage = new Image(Objects.requireNonNull(
+            this.getClass().getResourceAsStream("/images/TonyStark.jpg")));
+    private final Image thanosImage = new Image(Objects.requireNonNull(
+            this.getClass().getResourceAsStream("/images/Thanos.jpg")));
+
+    /**
+     * Initializes the main window to ensure that the scroll pane scrolls automatically to show the
+     * most recent dialog messages.
+     */
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /**
+     * Sets the {@code Thanos} instance for processing user commands.
+     *
+     * @param thanos the {@code Thanos} instance to be set.
+     */
+    public void setThanos(Thanos thanos) {
+        this.thanos = thanos;
+    }
+
+    /**
+     * Handles user input from the text field and updates the dialog container with the user's input and the response.
+     * <p>
+     * This method processes the user's text input, generates a response from the {@code Thanos} instance,
+     * and updates the dialog container with both the user's message and the response. If the response indicates
+     * a farewell, the application will exit after a short delay.
+     * </p>
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = thanos.getResponse(input);
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getThanosDialog(response, thanosImage)
+        );
+        userInput.clear();
+
+        if (response.equals("Bye. Hope to see you again soon!\n")) {
+            PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+            pause.setOnFinished(event -> Platform.exit());
+            pause.play();
+        }
+    }
+}
