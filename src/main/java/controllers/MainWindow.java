84765d4563fa439550c47305bff343c3f1ce2117
Clarenceeey <clarencey100@gmail.com>
2024-09-01 17:29:48 +0800

    Handle welcoming users

--- src/main/java/controllers/MainWindow.java
+++ src/main/java/controllers/MainWindow.java
@@ -37,6 +37,16 @@ public class MainWindow extends AnchorPane {
         this.chatbot = chatbot;
     }
 
+    /** Outputs welcome message when user starts app */
+    public void welcomeUser() {
+        String welcomeMessage = chatbot.getResponse("welcome");
+        String listTasks = chatbot.getResponse("list");
+        dialogContainer.getChildren().addAll(
+                DialogBox.getChatBotDialog(welcomeMessage, dukeImage),
+                DialogBox.getChatBotDialog(listTasks, dukeImage)
+        );
+    }
+
     /**
      * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
      * the dialog container. Clears the user input after processing.
