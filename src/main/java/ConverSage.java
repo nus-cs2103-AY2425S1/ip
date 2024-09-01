809657cddea75e5f45f9129a90fd94ccf6c82cc2
ashea10 <enasveen13@gmail.com>
2024-09-01 21:31:31 +0800

    Implement A-MoreOOP

--- src/main/java/ConverSage.java
+++ src/main/java/ConverSage.java
@@ -7,237 +7,42 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ConverSage {
-    public static void main(String[] args) {
-        String logo = "  ____                          ____                   \n" +
-                " / ___|___  _ ____   _____ _ __/ ___|  __ _  __ _  ___ \n" +
-                "| |   / _ \\| '_ \\ \\ / / _ \\ '__\\___ \\ / _` |/ _` |/ _ \\\n" +
-                "| |__| (_) | | | \\ V /  __/ |   ___) | (_| | (_| |  __/\n" +
-                " \\____\\___/|_| |_|\\_/ \\___|_|  |____/ \\__,_|\\__, |\\___|\n" +
-                "                                            |___/      ";
-        String horizontalLine = "____________________________________________________________";
-        System.out.println(logo);
-        System.out.println("Greetings, I'm your ConverSage.");
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
 
-        Scanner scanner = new Scanner(System.in);
-        String input;
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-        List<Task> taskList = loadTasks();
+    public ConverSage(String filePath) {
+        this.ui = new Ui();
+        this.storage = new Storage(filePath);
+        try {
+            this.tasks = new TaskList(storage.load());
+        } catch (ConverSageException e) {
+            ui.showError(e.getMessage());
+            this.tasks = new TaskList();
+        }
+    }
 
-        while (true) {
-            input = scanner.nextLine();
-            System.out.println(horizontalLine);
-
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
             try {
-                if (input.equalsIgnoreCase("list")) {
-                    for (int i = 1; i <= taskList.size(); i++) {
-                        System.out.println(i + ". " + taskList.get(i-1));
-                    }
-                    System.out.println(horizontalLine);
-                } else if (input.equalsIgnoreCase("bye")) {
-                    System.out.println("Goodbye. We shall meet again soon.");
-                    System.out.println(horizontalLine);
-                    break;
-                } else if (input.startsWith("mark")) {
-                    int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskIndex < 0 || taskIndex >= taskList.size()) {
-                        throw new ConverSageException("Task Number is invalid...");
-                    }
-                    Task currTask = taskList.get(taskIndex);
-
-                    try {
-                        String doneTask = currTask.markAsDone();
-                        System.out.println("Good job. I've marked this task as done.");
-                        System.out.println(doneTask);
-                    } catch (ConverSageException e) {
-                        System.out.println("ERR " + e.getMessage());
-                    }
-
-                    saveTasks(taskList);
-                    System.out.println(horizontalLine);
-
-
-                } else if (input.startsWith("unmark")) {
-                    int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskIndex < 0 || taskIndex >= taskList.size()) {
-                        throw new ConverSageException("Task Number is invalid...");
-                    }
-
-                    Task currTask = taskList.get(taskIndex);
-                    try {
-                        String undoneTask = currTask.markAsUndone();
-                        System.out.println("I've marked this task as not done yet, get to it quickly.");
-                        System.out.println(undoneTask);
-                    } catch (ConverSageException e) {
-                        System.out.println("ERR: " + e.getMessage());
-                    }
-
-                    saveTasks(taskList);
-                    System.out.println(horizontalLine);
-
-                } else if (input.startsWith("todo")) {
-                    try {
-                        if (input.length() <= 5) {
-                            throw new ConverSageException("The description of a todo cannot be empty.");
-                        }
-                        String taskDesc = input.substring(5);
-                        Task newTask = new ToDo(taskDesc);
-                        taskList.add(newTask);
-
-                        System.out.println("Understood, I've added this task: ");
-                        System.out.println("  " + newTask);
-                        System.out.println("You have " + taskList.size() + " tasks in your list" );
-                    } catch (Exception e) {
-                        System.out.println("ERR: " + e.getMessage());
-                    }
-
-                    saveTasks(taskList);
-                    System.out.println(horizontalLine);
-
-                } else if (input.startsWith("deadline")) {
-                    try {
-                        if (input.length() <= 9 || !input.contains(" /by ")) {
-                            throw new ConverSageException("The deadline task requires a description and a deadline.");
-                        }
-                        String[] deadlineTaskParts = input.substring(9).split(" /by ");
-                        String taskDesc = deadlineTaskParts[0];
-                        String deadline = deadlineTaskParts[1];
-                        Task newTask = new Deadline(taskDesc, deadline);
-                        taskList.add(newTask);
-
-                        System.out.println("Understood, I've added this task: ");
-                        System.out.println("  " + newTask);
-                        System.out.println("You have " + taskList.size() + " tasks in your list" );
-                    } catch (ConverSageException e) {
-                        System.out.println("ERR: " + e.getMessage());
-                    }
-
-                    saveTasks(taskList);
-                    System.out.println(horizontalLine);
-                } else if (input.startsWith("event")) {
-                    try {
-                        if (input.length() <= 6) {
-                            throw new ConverSageException("The event task requires a description.");
-                        }
-                        if (!input.contains(" /from ") || !input.contains(" /to ")) {
-                            throw new ConverSageException("The event task requires a start time and an end time");
-                        }
-                        String[] eventTaskParts = input.substring(6).split(" /from | /to ");
-                        String taskDesc = eventTaskParts[0];
-                        String from = eventTaskParts[1];
-                        String to = eventTaskParts[2];
-                        Task newTask = new Event(taskDesc, from, to);
-                        taskList.add(newTask);
-
-                        System.out.println("Understood, I've added this task: ");
-                        System.out.println("  " + newTask);
-                        System.out.println("You have " + taskList.size() + " tasks in your list" );
-                    } catch (ConverSageException e) {
-                        System.out.println("ERR: " + e.getMessage());
-                    }
-
-                    saveTasks(taskList);
-                    System.out.println(horizontalLine);
-                } else if (input.startsWith("delete")) {
-                    int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskIndex < 0 || taskIndex >= taskList.size()) {
-                        throw new ConverSageException("Task Number is invalid...");
-                    }
-
-                    Task currTask = taskList.get(taskIndex);
-                    String deletedTask = currTask.toString();
-                    taskList.remove(taskIndex);
-                    System.out.println("Ok, I've deleted the following task: ");
-                    System.out.println("  " + deletedTask);
-                    System.out.println("You have " + taskList.size() + " tasks in your list" );
-
-                    saveTasks(taskList);
-                    System.out.println(horizontalLine);
-                } else if (input.startsWith("tasks on ")) {
-                    try {
-                        
-                        LocalDateTime date = LocalDate.parse(input.substring(9).trim(), DateTimeFormatter.ofPattern("yyyy-MM-dd")).atStartOfDay();
-                        System.out.println("Here are the tasks on " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ":");
-                        for (Task task : taskList) {
-                            if (task instanceof Deadline && ((Deadline) task).deadline.toLocalDate().equals(date.toLocalDate())) {
-                                System.out.println(task);
-                            } else if (task instanceof Event && ((Event) task).from.toLocalDate().equals(date.toLocalDate())) {
-                                System.out.println(task);
-                            }
-                        }
-
-                    } catch (Exception e) {
-                        System.out.println("ERR: Invalid date/time format! Please use this format: yyyy-MM-dd HHmm");
-                    }
-                    System.out.println(horizontalLine);
-
-                } else {
-                    throw new ConverSageException("Invalid command, please try again");
-                }
+                String fullCommand = ui.readCommand();
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
             } catch (ConverSageException e) {
-                System.out.println("ERR: " + e.getMessage());
-                System.out.println(horizontalLine);
-            }
-
+                ui.showLine();
+                ui.showError(e.getMessage());
+            } 
+                    
         }
     }
-
-    /*
-        method to save a list of tasks to a file myTasks.txt inside a folder called data
-     */
-    private static void saveTasks(List<Task> taskList) {
-        // File object representing the dir
-        // Note: ./ means directory is relative to wd
-        File dataDir = new File("./data");
-
-        // If dir doesn't exist, create!
-        if (!dataDir.exists()) {
-            dataDir.mkdir();
-        }
-
-        // Next, create the myTasks file inside this directory!
-        File myTasksFile = new File("./data/myTasks.txt");
-
-        // Now, we need to try to write to the file, create a FileWriter object using myTasksFile
-        try (FileWriter writer = new FileWriter(myTasksFile)) {
-            for (Task currTask : taskList) {
-                // write the currTask into file!
-                writer.write(currTask.toFileFormat() + "\n");
-            }
-        } catch (IOException e) {
-            System.out.println("ERR: Failed to save tasks to file.");
-        }
-
+    public static void main(String[] args) {
+        new ConverSage("data/tasks.txt").run();
     }
 
-    /*
-        This method is to load all the tasks from the file whenever application starts
-     */
-    private static List<Task> loadTasks() {
-        List<Task> taskList = new ArrayList<>();
-        File myTasksFile = new File("./data/myTasks.txt");
-
-        // if the above file doesn't exist, no tasks so return empty list
-        if (!myTasksFile.exists()) {
-            return taskList;
-        }
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(myTasksFile))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                try {
-                    taskList.add(Task.fromFileFormat(line));
-                } catch (ConverSageException e) {
-                    System.out.println("ERR: Skipping corrupted line: " + line);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("ERR: Failed to load tasks from file!");
-        }
 
 
-
-        return taskList;
-    }
 }
