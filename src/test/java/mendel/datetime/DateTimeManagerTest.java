b43a4cdd47e425c1da12e23929bb96524cbb738d
Virusrwj223 <mhrishiraj@gmail.com>
2024-09-01 11:40:01 +0800

    Fix style tests

--- src/test/java/mendel/datetime/DateTimeManagerTest.java
+++ src/test/java/mendel/datetime/DateTimeManagerTest.java
@@ -1,11 +1,12 @@
 package mendel.datetime;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class DateTimeManagerTest {
     @Test
-    public void detectDate(){
+    public void detectDate() {
         assertEquals("Jan 01 2024", new DateTimeManager("01/Jan/2024").toString());
         assertEquals("Jan 01 2024", new DateTimeManager("01/01/2024").toString());
         assertEquals("Jan 01 2024", new DateTimeManager("1/1/2024").toString());
@@ -17,7 +18,7 @@ public class DateTimeManagerTest {
     }
 
     @Test
-    public void detectDateTime(){
+    public void detectDateTime() {
         assertEquals("Jan 01 2024, 15:00", new DateTimeManager("01/Jan/2024 1500").toString());
         assertEquals("Jan 01 2024, 15:00", new DateTimeManager("01/01/2024 1500").toString());
         assertEquals("Jan 01 2024, 15:00", new DateTimeManager("1/1/2024 1500").toString());
@@ -47,7 +48,7 @@ public class DateTimeManagerTest {
     }
 
     @Test
-    public void removeTimeStamp(){
+    public void removeTimeStamp() {
         assertEquals("Jan 01 2024", new DateTimeManager("01/Jan/2024 1500").removeTimeStamp());
         assertEquals("Jan 01 2024", new DateTimeManager("01/01/2024 1500").removeTimeStamp());
         assertEquals("Jan 01 2024", new DateTimeManager("1/1/2024 1500").removeTimeStamp());
