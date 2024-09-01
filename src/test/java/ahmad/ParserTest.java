e6b2a0ce7434c45e9be48b2d47bd5817be123bf4
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-09-01 11:33:52 +0800

    Add parser tester

--- src/test/java/ahmad/ParserTest.java
+++ src/test/java/ahmad/ParserTest.java
@@ -1,13 +1,96 @@
 package ahmad;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import ahmad.exceptions.AhmadException;
+import ahmad.exceptions.CommandInvalidException;
+import ahmad.processor.task.Task;
+import ahmad.processor.task.TaskList;
+import ahmad.processor.task.TaskType;
+import ahmad.response.Response;
 import org.junit.jupiter.api.Test;
+import ahmad.processor.Processor;
+import ahmad.processor.task.Mark;
 
 
 public class ParserTest {
+
     @Test
-    public void dummyTest() {
-        assertEquals(2, 3);
+    public void markTest() {
+        try {
+            Parser.parse("mark 1");
+        } catch (CommandInvalidException e) {
+            fail("Mark fail");
+        }
+    }
+
+    @Test
+    public void unMarkTest() {
+        try {
+            Parser.parse("unmark 12");
+        } catch (CommandInvalidException e) {
+            fail("Unmark fail");
+        }
+    }
+
+    @Test
+    public void listTest() {
+        try {
+            Parser.parse("list");
+        } catch (CommandInvalidException e) {
+            fail("List fail");
+        }
+    }
+
+    @Test
+    public void byeTest() {
+        try {
+            Parser.parse("bye");
+        } catch (CommandInvalidException e) {
+            fail("Bye fail");
+        }
+    }
+
+    @Test
+    public void todoTest() {
+        try {
+            Parser.parse("todo test");
+        } catch (CommandInvalidException e) {
+            fail("Todo fail");
+        }
+    }
+
+    @Test
+    public void deadlineTest() {
+        try {
+            Parser.parse("deadline test /by tomorrow");
+        } catch (CommandInvalidException e) {
+            fail("Deadline fail");
+        }
+    }
+
+    @Test
+    public void eventTest() {
+        try {
+            Parser.parse("event test /from today /to tomorrow");
+        } catch (CommandInvalidException e) {
+            fail("event fail");
+        }
+    }
+
+    @Test
+    public void deleteTest() {
+        try {
+            Parser.parse("delete 12");
+        } catch (CommandInvalidException e) {
+            fail("Delete fail");
+        }
+    }
+
+    @Test
+    public void invalidCommandTest() {
+        assertThrows(CommandInvalidException.class, () -> Parser.parse("invalid command"));
     }
 }
