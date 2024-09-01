5063036643081ed69401b27adb79b0fc62a948f1
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-09-01 21:25:31 +0800

    Add new FutureYouException class

---
+++ src/main/java/FutureYouException.java
@@ -0,0 +1,14 @@
+/**
+ * FutureYouException is an exception that is thrown when an error occurs.
+ */
+public class FutureYouException extends Exception {
+    /**
+     * Constructs an FutureYouException
+     * The message can be specified to describe the error.
+     *
+     * @param message The message describing the error.
+     */
+    public FutureYouException(String message) {
+        super(message);
+    }
+}
