7f2974b7fbbf2a443ae1a91585cb10717da79f2e
Chan Guan Yu <changuanyu2904@gmail.com>
2024-09-01 21:15:21 +0800

    Remove unnecessary import statements

--- src/main/java/SecondMind.java
+++ src/main/java/SecondMind.java
@@ -1,8 +1,5 @@
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
 
