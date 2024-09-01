dcacf9032923222a12a519fba8bdf146ad5c189b
tata32000 <tata32000@hotmail.com>
2024-09-02 00:08:42 +0800

    Add JavaDoc comments to MeepException class

--- src/main/java/meep/MeepException.java
+++ src/main/java/meep/MeepException.java
@@ -1,5 +1,9 @@
 package meep;
 
+/**
+ * The {@code MeepException} class represents exceptions that are specific to the Meep application.
+ * It extends the {@code Exception} class and provides custom error messages for exceptions that occur within the application.
+ */
 public class MeepException extends Exception {
     public MeepException(String message) {
         super(message);
