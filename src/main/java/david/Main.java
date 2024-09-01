84b17c00743f2588d87bf1fafe06e6e8acc4c6fc
Incogdino <javiertmw@gmail.com>
2024-09-01 14:11:38 +0800

    Add JavaFx GUI for better user experience
    Current CLI user interface is not very pleasing. A GUI is added using
    JavaFx to provide a better user experience.

---
+++ src/main/java/david/Main.java
@@ -0,0 +1,31 @@
+package david;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private David chatbot = new David("./src/main/java/David/Data/database.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDavid(chatbot); // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/david/MainWindow.java
@@ -0,0 +1,86 @@
+package david;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private David david;
+
+    private Image userImage = getImage("/images/user.png");
+
+    private Image davidImage = getImage("/images/David.png");
+
+    public Image getImage(String path) {
+        InputStream inputStream = this.getClass().getResourceAsStream(path);
+        try {
+            if (inputStream == null) {
+                throw new FileNotFoundException();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No user image found.");
+        }
+        return new Image(inputStream);
+    }
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance */
+    public void setDavid(David d) {
+        david = d;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing David's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = david.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, davidImage)
+        );
+        userInput.clear();
+
+        if (input.equals("bye")) {
+            exitProgram();
+        }
+    }
+
+    /**
+     * Ends the chatbot after a 1-second timer
+     */
+    private void exitProgram() {
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                System.exit(0);
+            }
+        }, 1000);
+    }
+}
