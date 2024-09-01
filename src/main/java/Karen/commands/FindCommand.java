271659e2141a17e4601a02e437dedaaf897a7e23
Ignatius Oey <oey.ignatius@gmail.com>
2024-09-02 00:13:42 +0800

    Add FindCommand class

--- src/main/java/Karen/commands/FindCommand.java
+++ src/main/java/Karen/commands/FindCommand.java
@@ -1,4 +1,30 @@
 package Karen.commands;
 
-public class FindCommand {
+import Karen.tasks.Task;
+import Karen.tasks.TaskList;
+import Karen.util.Ui;
+
+import java.util.List;
+
+public class FindCommand extends Command{
+    private String searchWord;
+
+    public FindCommand(String searchWord) {
+        this.searchWord = searchWord;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) {
+        List<Task> foundTasks = taskList.searchTasks(this.searchWord);
+        if (foundTasks.isEmpty()) {
+            System.out.println("Couldnt find any matching tasks :(");
+        } else {
+            System.out.println("Found tasks!");
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
