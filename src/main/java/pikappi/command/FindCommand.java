9b1d1a9c9d87d5d94d60ed886f7f167c8daab1e5
xqtann <xqxinquan@gmail.com>
2024-09-02 15:16:58 +0800

    Add Varargs in FindCommand

--- src/main/java/pikappi/command/FindCommand.java
+++ src/main/java/pikappi/command/FindCommand.java
@@ -9,15 +9,15 @@ import pikappi.exception.PikappiException;
  * Represents a command by user to find tasks that match a keyword.
  */
 public class FindCommand extends Command {
-    private String keyword;
+    private String[] keywords;
 
-    public FindCommand(String keyword) {
-        this.keyword = keyword;
+    public FindCommand(String... keywords) {
+        this.keywords = keywords;
     }
 
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws PikappiException {
-        TaskList matchingTasks = tasks.findTask(keyword);
+        TaskList matchingTasks = tasks.findTask(keywords);
         return ui.showMatchingTasks(matchingTasks);
     }
 }
