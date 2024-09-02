2edf93d67f4ad2a19cdfb692fa87a0125792d40c
SamsonChew <samsonchewjunjie@gmail.com>
2024-09-03 00:38:42 +0800

    Add Find feature

--- src/main/java/samson/Ui.java
+++ src/main/java/samson/Ui.java
@@ -3,6 +3,7 @@ package samson;
 import samson.task.Task;
 import samson.task.TaskList;
 
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -144,6 +145,24 @@ public class Ui {
     public void showTaskNumInvalid() {
         showLine();
         System.out.println(" Task number out of range.");
+        showLine();
+    }
+
+    /**
+     * Displays the results of a find operation.
+     *
+     * @param matchingTasks The list of tasks that match the search keyword.
+     */
+    public void showFindResults(List<Task> matchingTasks) {
+        showLine();
+        if (matchingTasks.isEmpty()) {
+            System.out.println("No matching tasks found.");
+        } else {
+            System.out.println("Here are the matching tasks in your list:");
+            for (int i = 0; i < matchingTasks.size(); i++) {
+                System.out.println((i + 1) + "." + matchingTasks.get(i));
+            }
+        }
         showLine();
     }
 }
