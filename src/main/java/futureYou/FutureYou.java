7c2a00889b59ee64843e3636eb0cf6a98a9d0c41
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-09-02 13:24:27 +0800

    Update FutureYou to work with new Storage class

--- src/main/java/futureYou/FutureYou.java
+++ src/main/java/futureYou/FutureYou.java
@@ -1,23 +1,36 @@
 package futureYou;
 
+import java.io.IOException;
+
 public class FutureYou {
-    private TaskList taskList;
     private Ui ui;
 
     /**
      * Constructs an FutureYou instance with a new TaskList and Ui.
+     * @throws IOException 
      */
-    public FutureYou() {
-        taskList = new TaskList();
+    public FutureYou() throws IOException {
         ui = new Ui();
+        initializeFile(ui);
+
+    }
+
+    private void initializeFile(Ui ui) throws IOException {
+        try {
+            new TaskList("./data/futureYou.txt");
+        } catch (IOException e) {
+            String message = "Error when getting file: " + e.getMessage();
+            ui.displayMessage(message);
+        }
     }
 
     /**
      * The main method that runs the FutreYou Program.
      *
      * @param args The command-line arguments.
+     * @throws IOException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FutureYou futureYou = new FutureYou();
         futureYou.ui.hello();
         String userCommand = "";
