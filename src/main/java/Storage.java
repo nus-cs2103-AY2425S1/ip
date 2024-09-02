fcabfbc0eb397ecc519457a761e00d7d935bb947
wassilililily <e0725370@u.nus.edu>
2024-09-02 12:34:19 +0800

    Add Parser, Storage, Tasklist and Ui classes

---
+++ src/main/java/Storage.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * The {@code Storage} class handles loading and saving tasks from/to a file.
+ * It ensures that task data is persisted between sessions.
+ */
+public class Storage {
+    public Path path;
+
+    /**
+     * Constructs a {@code Storage} object with the specified file path.
+     *
+     * @param filepath The file path where tasks will be loaded from and saved to.
+     */
+    public Storage(String filepath) {
+        this.path = Paths.get(filepath);
+    }
+
+    /**
+     * Loads tasks from the specified file into the given {@code Tasklist}.
+     * If the file does not exist, it is created. The method parses the file content and
+     * converts each line into a {@code Task} object, adding it to the list.
+     *
+     * @param tasks The {@code Tasklist} to load the data into.
+     * @return The updated {@code Tasklist} after loading the data.
+     */
+    public Tasklist load(Tasklist tasks) {
+        try {
+            if (Files.exists(path)) {
+                String content = Files.readString(path).trim();
+                if (!content.isEmpty()) {
+                    String[] lines = content.split("\n");
+                    for (String line : lines) {
+                        String[] word = line.split("\\|");
+                        String type = word[0];
+                        boolean status = word[1].contains("X");
+
+                        if (type.equals("T")) {
+                            Task newTask = new ToDo(word[2], status);
+                            tasks.addTask(newTask);
+                        } else if (type.equals("D")) {
+                            Task newTask = new Deadline(word[2], status, word[3]);
+                            tasks.addTask(newTask);
+                        } else if (type.equals("E")) {
+                            Task newTask = new Event(word[2], status, word[3], word[4]);
+                            tasks.addTask(newTask);
+                        }
+                    }
+                }
+            } else {
+                Files.createDirectories(path.getParent());
+                Files.createFile(path);
+            }
+        } catch (IOException e) {
+            System.out.println("Invalid");
+        }
+        return tasks;
+    }
+
+    /**
+     * Saves the current list of tasks to the specified file.
+     * The tasks are serialized into a specific format and written to the file.
+     *
+     * @param tasks The {@code Tasklist} to be saved.
+     */
+    public void save(Tasklist tasks) {
+        String outputFile = "";
+        for (int i = 0; i < tasks.getSize(); i++) {
+            Task curr = tasks.getTask(i);
+            outputFile = outputFile + String.format("%s|%s|%s|%s%n", curr.getType(), curr.getStatusIcon(),
+                    curr.getDescription(), curr.getTime());
+        }
+        try {
+            Files.writeString(this.path, outputFile);
+        } catch (IOException e) {
+            System.out.println("Invalid");
+        }
+    }
+
+}
---
+++ src/main/java/Tasklist.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+
+/**
+ * The {@code Tasklist} class manages a list of tasks, allowing operations such as adding,
+ * deleting, marking, and unmarking tasks. It provides functionality to interact with the
+ * task list and retrieve information about the tasks.
+ */
+public class Tasklist {
+    public int index;
+    public ArrayList<Task> tasklist;
+
+    /**
+     * Constructs an empty {@code Tasklist} with an initial index of 0.
+     */
+    public Tasklist() {
+        this.index = 0;
+        this.tasklist = new ArrayList<Task>();
+    }
+
+    /**
+     * Constructs a {@code Tasklist} with a predefined list of tasks.
+     *
+     * @param tasklist The list of tasks to be managed.
+     */
+    public Tasklist(ArrayList<Task> tasklist) {
+        this.index = tasklist.size();
+        this.tasklist = tasklist;
+    }
+
+    /**
+     * Adds a new task to the task list.
+     *
+     * @param toAdd The task to be added.
+     */
+    public void addTask(Task toAdd) {
+        this.tasklist.add(toAdd);
+        this.index++;
+    }
+
+    /**
+     * Deletes a task from the task list based on its index.
+     *
+     * @param num The 1-based index of the task to be deleted.
+     */
+    public void deleteTask(int num) {
+        this.tasklist.remove(num - 1);
+        this.index = this.tasklist.size();
+    }
+
+    /**
+     * Marks a task as done based on its index.
+     *
+     * @param num The 1-based index of the task to be marked as done.
+     */
+    public void markTask(int num) {
+        Task markedTask = this.tasklist.get(num - 1).markAsDone();
+        this.tasklist.set(num - 1, markedTask);
+    }
+
+    /**
+     * Unmarks a task as not done based on its index.
+     *
+     * @param num The 1-based index of the task to be unmarked.
+     */
+    public void unmarkTask(int num) {
+        Task unmarkedTask = this.tasklist.get(num - 1).markAsUndone();
+        this.tasklist.set(num - 1, unmarkedTask);
+    }
+
+    /**
+     * Returns the size of the task list.
+     *
+     * @return The number of tasks in the task list.
+     */
+    public int getSize() {
+        return this.tasklist.size();
+    }
+
+    /**
+     * Retrieves a task based on its 0-based index.
+     *
+     * @param index The 0-based index of the task to retrieve.
+     * @return The task at the specified index.
+     */
+    public Task getTask(int index) {
+        return this.tasklist.get(index);
+    }
+
+    /**
+     * Returns a string representation of the entire task list.
+     *
+     * @return A formatted string of the task list with each task numbered.
+     */
+    public String showTasklist() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < this.tasklist.size(); i++) {
+            output.append(String.format("%d. %s%n", i + 1, this.tasklist.get(i)));
+        }
+        return output.toString();
+    }
+
+}
+
