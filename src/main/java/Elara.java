71626d7464599fbb0fb5e012bb059a7dcc736d20
ylchin <chin.yiling0715@gmail.com>
2024-09-02 13:50:47 +0800

    Support reading from and writing to file
    Elara did not support reading to and writing from file, so user has to
    retype todo list every time Elara is run
    
    Restructuring Elara by adding methods to read files, parse files for
    tasks and save tasks to file allows us to store the data in a specified
    file.
    
    Let's reorganise code for Elara by breaking up into smaller blocks
    and add methods readFile(), setupFile(), parseTask() and
    saveTasksToFile().

--- src/main/java/Elara.java
+++ src/main/java/Elara.java
@@ -1,12 +1,98 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Elara {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> list = new ArrayList<>();
 
-        // Greeting
+    private static final String DATA_DIRECTORY = "./data";
+    private static final String DATA_FILE = DATA_DIRECTORY + "/Elara.txt";
+    private ArrayList<Task> tasks;
+
+    public Elara() {
+        tasks = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Elara elara = new Elara();
+        elara.setupFile();
+        elara.tasks = elara.readFile();
+        elara.runChatbot();
+    }
+
+    private void setupFile() {
+        try {
+            File file = new File(DATA_FILE);
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file");
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Task> readFile() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(DATA_FILE));
+            for (String line : lines) {
+                String[] taskDetails = line.split(" \\| ");
+                if (taskDetails.length > 0) {
+                    Task task = parseTask(taskDetails);
+                    tasks.add(task);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
+    private Task parseTask(String[] taskDetails) {
+        String taskType = taskDetails[0];
+        boolean isDone = taskDetails[1].trim().equals("1");
+        String description = taskDetails[2].trim();
+
+        switch (taskType) {
+            case "T":
+                return new ToDoTask(description, isDone);
+            case "D":
+                if (taskDetails.length == 4) {
+                    String deadline = taskDetails[3];
+                    return new DeadlineTask(description, deadline, isDone);
+                }
+                throw new IllegalArgumentException("Invalid deadline task format");
+            case "E":
+                if (taskDetails.length == 5) {
+                    String startTime = taskDetails[3];
+                    String endTime = taskDetails[4];
+                    return new EventTask(description, startTime, endTime, isDone);
+                }
+                throw new IllegalArgumentException("Invalid event task format");
+            default:
+                throw new IllegalArgumentException("Invalid task type");
+        }
+    }
+
+    public void saveTasksToFile() {
+        try (FileWriter writer = new FileWriter(DATA_FILE)) {
+            for (Task task : tasks) {
+                writer.write(task.toFileFormat() + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving tasks to file");
+            e.printStackTrace();
+        }
+    }
+
+    private void runChatbot() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Elara");
         System.out.println("What can I do for you?");
 
@@ -19,83 +105,37 @@ public class Elara {
             try {
                 switch (command) {
                     case "bye":
-                        // exit greeting
                         System.out.println("Bye. Hope to see you again!");
                         break;
                     case "list":
-                        System.out.println("Here are the tasks in your list:");
-                        for (int i = 0; i < list.size(); i++) {
-                            if (list.get(i) == null) {
-                                break;
-                            }
-                            System.out.println((i + 1) + ". " + list.get(i));
-                        }
+                        listTasks();
                         break;
                     case "mark":
-                        // mark an item as completed
                         int i = Integer.parseInt(arg) - 1;
-                        if (i >= 0 && i < list.size()) {
-                            list.get(i).mark();
-                            System.out.println("Nice! I've marked this task as done:");
-                            System.out.println(list.get(i));
+                        if (i >= 0 && i < tasks.size()) {
+                            mark(i);
                             break;
                         }
                         throw new InvalidInputException("Index out of range!");
                     case "unmark":
-                        // unmark an item; i.e. mark as uncompleted
                         int j = Integer.parseInt(arg) - 1;
-                        if (j >= 0 && j < list.size()) {
-                            list.get(j).unmark();
-                            System.out.println("OK, I've marked this task as not done yet:");
-                            System.out.println(list.get(j));
+                        if (j >= 0 && j < tasks.size()) {
+                            unmark(j);
                             break;
                         }
                         throw new InvalidInputException("Index out of range!");
                     case "delete":
                         int k = Integer.parseInt(arg) - 1;
-                        if (k >= 0 && k < list.size()) {
-                            System.out.println("Noted. I've removed this task:");
-                            System.out.println(list.get(k));
-                            list.remove(k);
-                            System.out.println("Now you have " + list.size() + " tasks in the list.");
+                        if (k >= 0 && k < tasks.size()) {
+                            delete(k);
                             break;
                         }
                         throw new InvalidInputException("Index out of range!");
                     case "todo":
-                        // add a new todo task
-                        if (arg.isEmpty()) {
-                            throw new ToDoException();
-                        }
-                        ToDoTask toDoTask = new ToDoTask(arg);
-                        list.add(toDoTask);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(toDoTask);
-                        System.out.printf("Now you have %d tasks in the list%n", list.size());
-                        break;
                     case "deadline":
-                        // add a new deadline task
-                        String[] deadlineArgs = arg.split("/by ");
-                        if (deadlineArgs.length == 2) {
-                            DeadlineTask deadlineTask = new DeadlineTask(deadlineArgs[0], deadlineArgs[1]);
-                            list.add(deadlineTask);
-                            System.out.println("Got it. I've added this task:");
-                            System.out.println(deadlineTask);
-                            System.out.printf("Now you have %d tasks in the list%n", list.size());
-                            break;
-                        }
-                        throw new DeadlineException();
                     case "event":
-                        // add a new event task
-                        String[] eventArgs = arg.split("/from |/to ");
-                        if (eventArgs.length == 3) {
-                            EventTask eventTask = new EventTask(eventArgs[0], eventArgs[1], eventArgs[2]);
-                            list.add(eventTask);
-                            System.out.println("Got it. I've added this task:");
-                            System.out.println(eventTask);
-                            System.out.printf("Now you have %d tasks in the list%n", list.size());
-                            break;
-                        }
-                        throw new EventException();
+                        addTask(command, arg);
+                        break;
                     default:
                         throw new InvalidInputException("Errrrrrrr... I don't know what you are trying to say...\n" +
                                 "Try one of our commands: list mark unmark bye deadline todo event");
@@ -103,6 +143,77 @@ public class Elara {
             } catch (InvalidInputException e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    private void listTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i) == null) {
+                break;
+            }
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+
+    private void mark(int index) {
+        tasks.get(index).mark();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(tasks.get(index));
+        saveTasksToFile();
+    }
+
+    private void unmark(int index) {
+        tasks.get(index).unmark();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(tasks.get(index));
+        saveTasksToFile();
+    }
+
+    private void delete(int index) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(tasks.get(index));
+        tasks.remove(index);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        saveTasksToFile();
+    }
+
+    private void addTask(String command, String arg) {
+        Task newTask = null;
+        try {
+            switch (command) {
+                case "todo":
+                    if (arg.isEmpty()) {
+                        throw new ToDoException();
+                    }
+                    newTask = new ToDoTask(arg);
+                    break;
+                case "deadline":
+                    String[] deadlineArgs = arg.split("/by ");
+                    if (deadlineArgs.length == 2) {
+                        newTask = new DeadlineTask(deadlineArgs[0], deadlineArgs[1]);
+                        break;
+                    }
+                    throw new DeadlineException();
+                case "event":
+                    String[] eventArgs = arg.split("/from |/to ");
+                    if (eventArgs.length == 3) {
+                        newTask = new EventTask(eventArgs[0], eventArgs[1], eventArgs[2]);
+                        break;
+                    }
+                    throw new EventException();
+            }
+
+        } catch (InvalidInputException e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (newTask != null) {
+            tasks.add(newTask);
+            saveTasksToFile();
+            System.out.println("Got it. I've added this task:");
+            System.out.println(newTask);
+            System.out.printf("Now you have %d tasks in the list%n", tasks.size());
         }
     }
 }
