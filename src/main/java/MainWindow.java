d55889dd80487a0bccd950edf7ce6d3848f9bb8d
neohengkai <neohengkai2002@gmail.com>
2024-09-03 01:00:16 +0800

    Do Level-10: GUI

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,61 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import ontos.Ontos;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Ontos ontos;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.jpg"));
+    private Image ontosImage = new Image(this.getClass().getResourceAsStream("/images/chatbot.jpg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Ontos instance */
+    public void setOntos(Ontos o) {
+        ontos = o;
+        this.greetUser();
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = ontos.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getOntosDialog(response, ontosImage)
+        );
+        userInput.clear();
+    }
+
+    /**
+     * Greets the user when the chatbot is launched.
+     */
+    @FXML
+    private void greetUser() {
+        dialogContainer.getChildren().add(DialogBox.getOntosDialog(ontos.getGreeting(), ontosImage));
+    }
+}
