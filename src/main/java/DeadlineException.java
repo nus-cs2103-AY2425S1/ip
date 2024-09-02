90f51482e941de8f8b8c662da75fc735ad6326fe
ylchin <chin.yiling0715@gmail.com>
2024-09-02 14:42:47 +0800

    Update DeadlineTaskException and EventException error message
    Previous error message did not specify format for deadline and start and end
    time for deadline and event tasks.
    
    Updating the error messages allows users to know the proper format to send into
    Elara such that they can efficiently use the task list.
    
    Let's update the error message to include the format to parse for LocalDateTime

--- src/main/java/DeadlineException.java
+++ src/main/java/DeadlineException.java
@@ -1,5 +1,6 @@
 public class DeadlineException extends InvalidInputException {
     public DeadlineException() {
-        super("Your input format is invalid. Please use: 'deadline <description> /by <actual deadline>'");
+        super("Your input format is invalid. Please use: 'deadline <description> /by <actual deadline>' \n"
+                + "<actual deadline> format: yyyy-MM-dd HHmm");
     }
 }
--- src/main/java/EventException.java
+++ src/main/java/EventException.java
@@ -1,5 +1,6 @@
 public class EventException extends InvalidInputException {
     public EventException() {
-        super("Your input format is invalid. Please use: 'event <description> /from <start> /to <end>");
+        super("Your input format is invalid. Please use: 'event <description> /from <start> /to <end> \n"
+                + "<start> and <end> format: yyyy-MM-dd HHmm");
     }
 }
