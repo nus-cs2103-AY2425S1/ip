8958f0921e61cfe3e934a8e7d7e5aeac00c9ae7c
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-09-02 16:30:16 +0800

    Add two JUnit test cases for creation of new Task

---
+++ src/test/java/futureYou/DeadlineTest.java
@@ -0,0 +1,27 @@
+package futureYou;
+
+import org.junit.jupiter.api.Test;
+
+import futureYou.task.Deadline;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for the {@link Deadline} class.
+ */
+public class DeadlineTest {
+    @Test
+    public void Events_validCommand_eventCreated() {
+        String taskName = "Submit 2103";
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("d MMM yyyy - HH:mm");
+        LocalDateTime deadline = LocalDateTime.of(2024, 9, 05, 14, 0);
+        Deadline newDeasdlineTask = new Deadline(taskName, deadline);
+
+        assertEquals(deadline.format(format), newDeasdlineTask.getDeadline());
+    }
+
+}
---
+++ src/test/java/futureYou/EventsTest.java
@@ -0,0 +1,28 @@
+package futureYou;
+
+import org.junit.jupiter.api.Test;
+
+import futureYou.task.Events;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for the {@link Events} class.
+ */
+public class EventsTest {
+    @Test
+    public void Events_validCommand_eventCreated() {
+        String taskName = "Submit 2103";
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("d MMM yyyy - HH:mm");
+        LocalDateTime startDate = LocalDateTime.of(2024, 9, 05, 14, 0);
+        LocalDateTime endDate = LocalDateTime.of(2024, 9, 20, 22, 25);
+        Events event = new Events(taskName, startDate, endDate);
+
+        assertEquals(startDate.format(format), event.getstartDate());
+        assertEquals(endDate.format(format), event.getendDate());
+    }
+
+}
