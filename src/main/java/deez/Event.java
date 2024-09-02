4436e93c35a0f3f4b7f3aad004e4d7c4c9c4fe92
Sean Lim <seanlimdev856@gmail.com>
2024-09-02 11:09:44 +0800

    Clean up `Event.java`
    code indentation

--- src/main/java/deez/Event.java
+++ src/main/java/deez/Event.java
@@ -38,14 +38,8 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return "[E]"
-            + super.toString()
-            + " (from: "
-            + this.eventStartDate.format(DEFAULT_DATE_TIME_FORMATTER)
-            + ")"
-            + " (to"
-            + ": "
-            + this.eventEndDate.format(DEFAULT_DATE_TIME_FORMATTER)
-            + ")";
+        return "[E]" + super.toString()
+            + " (from: " + this.eventStartDate.format(DEFAULT_DATE_TIME_FORMATTER) + ")"
+            + " (to: " + this.eventEndDate.format(DEFAULT_DATE_TIME_FORMATTER) + ")";
     }
 }
