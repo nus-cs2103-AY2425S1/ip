a9c54b155b13a9f44cf13f68093af1ce876b513f
BunnyHoppp <e1122434@u.nus.edu>
2024-09-02 23:53:54 +0800

    Add JavaDoc Comments

--- src/main/java/potong/command/MarkCommand.java
+++ src/main/java/potong/command/MarkCommand.java
@@ -5,16 +5,35 @@ import potong.Storage;
 import potong.TaskList;
 import potong.Ui;
 
+/**
+ * Represents the command to mark or unmark a task.
+ */
 public class MarkCommand extends Command {
 
     private boolean toMark;
     private int index;
+
+    /**
+     * Initialise the command with the index of the task.
+     *
+     * @param command Index of the task.
+     * @param toMark True to mark, False to unmark.
+     */
     public MarkCommand(String command, boolean toMark) {
         super(command);
         this.toMark = toMark;
         this.index = Integer.valueOf(command);
     }
 
+    /**
+     * Marks or unmarks the task in the list of tasks.
+     *
+     * @param tasks List of tasks at hand.
+     * @param storage Storage class for loading and saving.
+     * @param ui Ui class for printing output.
+     * @return String representation of mark or unmark.
+     * @throws PotongException If the index is out of bounds.
+     */
     @Override
     public String execute(TaskList tasks, Storage storage, Ui ui) throws PotongException {
         if (toMark) {
