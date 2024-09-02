ad96020ad18c22123880fbd34ed94dbb0ea5e85b
jerrelllee <leejerrell337@gmail.com>
2024-09-03 00:04:00 +0800

    Add JavaDocs for Ui and TaskList class

--- src/main/java/fence/tasklist/TaskList.java
+++ src/main/java/fence/tasklist/TaskList.java
@@ -3,36 +3,68 @@ package fence.tasklist;
 import fence.task.Task;
 import java.util.ArrayList;
 
+/**
+ * Represents a list of tasks.
+ */
 public class TaskList {
 
     private ArrayList<Task> items = new ArrayList<>();
 
+    /**
+     * Constructs a task list with the tasks stored in an arraylist.
+     * @param items List of tasks stored in an arraylist.
+     */
     public TaskList(ArrayList<Task> items) {
         this.items = items;
     }
 
+    /**
+     * Returns the element at the specified position in the list.
+     * @param i Index of task.
+     * @return Task at index i.
+     */
     public Task get(int i) {
         return items.get(i);
     }
 
+    /**
+     * Returns the number of elements in the list.
+     * @return Size of the task list.
+     */
     public int size() {
         return items.size();
     }
 
+    /**
+     * Appends the given task to the end of the list.
+     * @param task Task to be added.
+     */
     public void add(Task task) {
         items.add(task);
     }
 
+    /**
+     * Marks the task at the specified position within the list displayed as complete.
+     * @param i Index of the task as given by the list printed by {@link fence.ui.Ui#list(TaskList)}.
+     */
     public void mark(int i) {
         Task task = items.get(i-1);
         task.complete();
     }
 
+    /**
+     * Marks the task at the specified position within the list displayed as incomplete.
+     * @param i Index of the task as given by the list printed by {@link fence.ui.Ui#list(TaskList)}.
+     */
     public void unmark(int i) {
         Task task = items.get(i-1);
         task.undo();
     }
 
+    /**
+     * Removes the task at the specified position within the list displayed. Shifts any subsequent elements to the left.
+     * @param i Index of the task as given by the list printed by {@link fence.ui.Ui#list(TaskList)}.
+     */
     public void delete(int i) {
         items.remove(i - 1);
     }
--- src/main/java/fence/ui/Ui.java
+++ src/main/java/fence/ui/Ui.java
@@ -3,15 +3,29 @@ package fence.ui;
 import fence.task.Task;
 import fence.tasklist.TaskList;
 
+/**
+ * Represents a user interface that is in charge of interactions with the user.
+ */
 public class Ui {
+
+    /**
+     * Prints the default greeting.
+     */
     public void greet() {
         System.out.println("nihao! I'm Fence |=|=|=|=|=|");
     }
 
+    /**
+     * Prints the default goodbye message.
+     */
     public void exit() {
         System.out.println("have good day :)");
     }
 
+    /**
+     * Prints out the tasks in the task list line by line. The starting index of the printed list is 1.
+     * @param taskList List of tasks currently.
+     */
     public void list(TaskList taskList) {
         System.out.println("!plans:");
         for (int i = 0; i < taskList.size(); i++) {
@@ -19,46 +33,81 @@ public class Ui {
         }
     }
 
+    /**
+     * Prints out the size of the task list.
+     * @param size Size of the task list.
+     */
     public void count(int size) {
         System.out.println("Now you have " + size +
                 ((size == 1) ? " item " : " items ") + "in the list.");
     }
 
+    /**
+     * Prints out the default message if command is not recognised.
+     */
     public void unknownCommand() {
         System.out.println("fence is programmed to track your tasks and has long lost all ability " +
                 "to do other things ");
     }
 
+    /**
+     * Prints out the error message for an input with invalid format.
+     */
     public void missingFieldError() {
         System.out.println("doing nothing (field missing/invalid format)");
     }
 
+    /**
+     * Prints out the error message for a date with invalid format.
+     */
     public void invalidDateError() {
         System.out.println("fence only understands yyyy-mm-dd");
     }
 
+    /**
+     * Prints out the error message for a number with invalid format.
+     */
     public void invalidNumberError() {
         System.out.println("fence has only learnt numerical integers");
     }
 
+    /**
+     * Prints out the error message for a data file with invalid format.
+     */
     public void loadingError() {
         System.out.println("Data file corrupted");
     }
 
+    /**
+     * Prints out the default message that a task has been added to the task list.
+     * @param task Task that was added.
+     */
     public void add(Task task) {
         System.out.println("added: " + task);
     }
 
+    /**
+     * Prints out the default message that a task has been marked as done.
+     * @param task Task that was marked complete.
+     */
     public void mark(Task task) {
         System.out.println("good job");
         System.out.println(task);
     }
 
+    /**
+     * Prints out the default message that a task has been marked as undone.
+     * @param task Task that was marked incomplete.
+     */
     public void unmark(Task task) {
         System.out.println("huh?");
         System.out.println(task);
     }
 
+    /**
+     * Prints out the default message that a task has been removed.
+     * @param task Task that was deleted.
+     */
     public void delete(Task task) {
         System.out.println("removed: " + task);
         System.out.println("(we never make plans)");
