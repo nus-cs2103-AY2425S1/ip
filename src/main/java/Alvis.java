1af2d1e8c3e45f6f3d8702f2f29360db39b8aacc
Quasant <benjaminthechin@gmail.com>
2024-09-03 18:18:15 +0800

    Change ArrayList name and certain string outputs

--- src/main/java/Alvis.java
+++ src/main/java/Alvis.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Alvis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Task> tracker = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
         System.out.println("So you require my assistance?");
 
         while (true) {
-            String userInput = sc.nextLine().toLowerCase();
-            switch (userInput.split(" ")[0]) {
+            String userInput = sc.nextLine();
+            switch (userInput.split(" ")[0].toLowerCase()) {
             case "bye":
                 System.out.println("May we meet again");
                 break;
             case "list":
                 System.out.println("Let me help you to remember what you need to do ...");
-                for (int i = 0; i < tracker.size(); ++i) {
-                        System.out.println("" + (i+1) + ". " + tracker.get(i).toString());
+                for (int i = 0; i < taskList.size(); ++i) {
+                        System.out.println("" + (i+1) + ". " + taskList.get(i).toString());
                     }
                 break;
             case "toggle":
                 try {
                     int index = Integer.parseInt(userInput.substring(7)) + 1;
-                    tracker.get(index).toggleStatus();
+                    taskList.get(index).toggleStatus();
                     System.out.println("As requested, I have changed the status of your task");
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("I cannot do that, the index is invalid");
@@ -32,7 +32,7 @@ public class Alvis {
             case "todo":
                 try {
                     Task toDo = new ToDo(userInput.substring(5));
-                    addTask(toDo, tracker);
+                    addTask(toDo, taskList);
                 } catch (StringIndexOutOfBoundsException e) {
                     System.out.println("Please input a body for the task");
                 }
@@ -40,7 +40,7 @@ public class Alvis {
             case "delete":
                 try {
                     int index = Integer.parseInt(userInput.substring(7)) - 1;
-                    tracker.remove(index);
+                    taskList.remove(index);
                     System.out.println("As requested, I have deleted the task");
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("I cannot do that, the index is invalid");
@@ -49,17 +49,17 @@ public class Alvis {
             case "deadline":
                 int byIndex = userInput.indexOf("/by");
                 Task deadline = new Deadline(userInput.substring(9, byIndex), userInput.substring(byIndex + 4));
-                addTask(deadline, tracker);
+                addTask(deadline, taskList);
                 break;
             case "event":
                 int fromIndex = userInput.indexOf("/from");
                 int toIndex = userInput.indexOf("/to");
                 Task event = new Event(userInput.substring(6, fromIndex), userInput.substring(fromIndex + 6, toIndex - 1),
                         userInput.substring(toIndex + 4));
-                addTask(event, tracker);
+                addTask(event, taskList);
                 break;
             default:
-                System.out.println("I don't recognise that. Type /help for the commands");
+                System.out.println("Refer to the webpage for the commands");
             }
         }
     }
@@ -69,10 +69,4 @@ public class Alvis {
         System.out.println("Understood, I have added to your list:\n" + task.toString());
         System.out.println("You have " + tracker.size() + " tasks");
     }
-
-//    public static int[] getDelimiterIndex (String input) {
-//        int firstIdx = input.indexOf("/");
-//        int secondIdx = input.indexOf("/", firstIdx + 1);
-//        return new int[]{firstIdx, secondIdx};
-//    }
 }
