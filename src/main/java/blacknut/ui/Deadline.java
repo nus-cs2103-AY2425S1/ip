961f49793d47a527331cedce645b492af2bdcf15
Jaehyeok Choi <77jack0105@gmail.com>
2024-09-03 21:53:16 +0800

    Implement Level 8: Reformation of date formats

--- src/main/java/blacknut/ui/Deadline.java
+++ src/main/java/blacknut/ui/Deadline.java
@@ -1,7 +1,5 @@
 package blacknut.ui;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,21 +8,19 @@ public class Deadline extends Task {
 
     public Deadline(String description, String by) {
         super(description);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-        this.by = LocalDateTime.parse(by, formatter);
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.by = LocalDateTime.parse(by, inputFormatter);
     }
 
     @Override
     public String toFileFormat() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-        return "D | " + super.toFileFormat() + " | " + by.format(formatter);
-        //return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return super.toFileFormat() + " | " + by.format(outputFormatter);
     }
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm");
-        return "[D]" + super.toString() + " (by: " + by.format(formatter) + ")";
-        //return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        DateTimeFormatter displayFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm");
+        return "[D]" + super.toString() + " (by: " + by.format(displayFormatter) + ")";
     }
 }
