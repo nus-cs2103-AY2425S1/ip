90bf74c208aad2cf882874a727ceb84c3c7ed4bc
jovensoh <34702990+JovenSoh@users.noreply.github.com>
2024-09-03 14:50:18 +0800

    Add one more test case for dates

--- src/test/java/zero/TaskTest.java
+++ src/test/java/zero/TaskTest.java
@@ -2,6 +2,12 @@ package zero;
 
 import zero.task.Task;
 import zero.task.Todo;
+import zero.util.Parser;
+import zero.exception.ZeroException;
+
+import java.time.LocalDateTime;
+
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,5 +25,15 @@ public class TaskTest {
         assertEquals("[T][ ] Study", todo.toString());
     }
 
+    @Test
+    public void testDate() {
+        LocalDateTime dateTime = LocalDateTime.of(2024, 9, 1, 14, 30);
+        try {
+            assertEquals(dateTime, Parser.handleDate("2024-09-01 1430"));
+        } catch (ZeroException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 
 }
