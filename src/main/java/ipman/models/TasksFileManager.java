39485a9ddfd70d27bbbc630e9058fa1e950c7469
Joel Tio <joeltiojinhon@gmail.com>
2024-09-03 23:11:16 +0800

    Add Javadoc to TasksFileManager

--- src/main/java/ipman/models/TasksFileManager.java
+++ src/main/java/ipman/models/TasksFileManager.java
@@ -6,12 +6,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Stores and retrieves tasks from a <code>TaskList</code>
+ * @see TaskList
+ */
 public class TasksFileManager {
     private final String fileName;
     public TasksFileManager(String fileName) {
         this.fileName = fileName;
     }
 
+    /**
+     * Loads tasks from the file into a particular <code>TaskList</code>
+     * If no file is found, no tasks will be loaded.
+     * @param tasks <code>TaskList</code> to load the tasks into
+     */
     public void load(TaskList tasks) {
         // Scanner with file inspired by https://www.digitalocean.com/community/tutorials/java-read-file-line-by-line
         try {
@@ -25,6 +34,10 @@ public class TasksFileManager {
         }
     }
 
+    /**
+     * Saves tasks from the <code>TaskList</code> into the file
+     * @param tasks <code>TaskList</code> of tasks to save
+     */
     public void save(TaskList tasks) {
         try (FileWriter fileWriter = new FileWriter(this.fileName)) {
             for (Task task : tasks) {
@@ -36,6 +49,10 @@ public class TasksFileManager {
         }
     }
 
+    /**
+     * Saves one task to the bottom of the file
+     * @param task task to save
+     */
     public void append(Task task) {
         // Writer inspired by https://www.baeldung.com/java-write-to-file
         try (FileWriter fileWriter = new FileWriter(this.fileName, true)) {
