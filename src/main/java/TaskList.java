4d4a28156d294c9b1d35d8daf6c68f4fb7095516
Jollybomber <siahevan@gmail.com>
2024-09-03 15:17:21 +0800

    Feat: added ability to add dates to deadlines, added ability to delete tasks that are saved locally

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,8 +1,11 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class TaskList {
     private final ArrayList<Task> list;
@@ -90,6 +93,7 @@ public class TaskList {
     public String deleteItem(int place) {
         try {
             Task deleted = list.remove(place - 1);
+            deleteFromFile(place);
             return "I have taken this item off the list:\n" + deleted +
                     "\n" + countTasks();
         } catch (IndexOutOfBoundsException e) {
@@ -109,7 +113,7 @@ public class TaskList {
                         list.add(new Todo(split[0]));
                         break;
                     case 2:
-                        list.add(new Deadline(split[0], split[1]));
+                        list.add(new Deadline(split[0], LocalDate.parse(split[1])));
                         break;
                     case 3:
                         list.add(new Event(split[0], split[1], split[2]));
@@ -134,6 +138,28 @@ public class TaskList {
             writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private void deleteFromFile(int place) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            FileWriter writer = new FileWriter("./data/temp.txt");
+
+            int counter = 1;
+            String currLine;
+            while((currLine = reader.readLine() ) != null) {
+                if (counter == place) {
+                    counter++;
+                    continue;
+                }
+                writer.write(currLine + System.lineSeparator());
+                counter++;
+            }
+            writer.close();
+            Files.copy(Path.of("./data/temp.txt"), Path.of(filePath), REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.out.println("There was an error with the file input.");
         }
     }
 }
