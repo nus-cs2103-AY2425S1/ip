bf0001962297713b65dabd6cd0909f79caee346f
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-09-04 04:05:01 +0800

    Add Javadocs for FindCommand class

--- src/main/java/gallium/command/FindCommand.java
+++ src/main/java/gallium/command/FindCommand.java
@@ -6,18 +6,36 @@ import gallium.main.TaskList;
 import gallium.main.Ui;
 import gallium.task.Task;
 
+/**
+ * Represents a command to find a task from the task list.
+ */
 public class FindCommand extends Command {
     private String Message;
 
+    /**
+     * Constructs a FindCommand with the message.
+     * 
+     * @param message The message containing the keyword to filter tasks by.
+     */
     public FindCommand(String Message) {
         this.Message = Message;
     }
 
+    /**
+     * Executes the find command to filter tasks by the keyword specified in the
+     * message.
+     * 
+     * @param tasklist The list of tasks to execute the command on.
+     * @param ui       The user interface that will interact with user.
+     * @param storage  The storage that will save any changes made by the command.
+     * @throws GalliumException If an error occurs during the execution of the
+     *                          command.
+     */
     public void execute(TaskList taskList, Ui ui, Storage storage) throws GalliumException {
         StringBuilder tasksStringBuilder = new StringBuilder();
         String keyword = Message.split("find ")[1];
         for (int i = 1; i < Task.count; i++) {
-            Task task = taskList.get(i - 1);
+            Task task = taskList.getTask(i - 1);
             if (task.getDesc().contains(keyword)) {
                 tasksStringBuilder.append("\n    " + task.toString());
             }
