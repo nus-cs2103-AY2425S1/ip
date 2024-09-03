6a299ad6bd10655e59b82d7f3a3eb7edb1c112dc
choaticman <dylanchan66@yahoo.com.sg>
2024-09-03 15:24:17 +0800

    Update text-ui-test to comply with new Date acceptance

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,6 +12,11 @@ then
     rm ACTUAL.TXT
 fi
 
+if [ -d "./data" ]
+then
+    rm -rf ./data
+fi
+
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/choaticbot/*.java
 then
@@ -20,7 +25,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin choaticbot.Choaticbot < input.txt > ACTUAL.TXT
+java -classpath ../bin choaticbot.ChoaticBot < input.txt > ACTUAL.TXT 2>&1
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
