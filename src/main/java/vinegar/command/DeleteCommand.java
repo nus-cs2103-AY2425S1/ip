07ee731c1c4dcf19b6c8c055ef43c1fa8447e054
limjeremy496 <limjeremy496@gmail.com>
2024-09-03 19:49:44 +0800

    Handle invalid task number input in DeleteCommand

--- src/main/java/vinegar/command/DeleteCommand.java
+++ src/main/java/vinegar/command/DeleteCommand.java
@@ -1,8 +1,8 @@
 package vinegar.command;
 
 import vinegar.TaskList;
+import vinegar.Validator;
 import vinegar.VinegarException;
-import vinegar.command.Command;
 import vinegar.storage.Storage;
 import vinegar.task.Task;
 import vinegar.ui.Ui;
@@ -13,12 +13,20 @@ public class DeleteCommand extends Command {
     private int index;
 
     public DeleteCommand(String[] inputParts) throws VinegarException {
-        if (inputParts.length < 2) throw new VinegarException("Please specify which task to delete.");
-        this.index = Integer.parseInt(inputParts[1]) - 1;
+        Validator.validateParts(inputParts, 2,"Please specify which task to delete.");
+        try {
+            index = Integer.parseInt(inputParts[1]) - 1;
+        } catch (NumberFormatException e) {
+            throw new VinegarException("Task number must be a valid integer.");
+        }
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws VinegarException, IOException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new VinegarException("Invalid task number. Please ensure the task number is correct and try again.");
+        }
+
         Task taskToRemove = tasks.removeTask(index);
         ui.showDeleted(taskToRemove, tasks.size());
         storage.save(tasks.getTasks());
