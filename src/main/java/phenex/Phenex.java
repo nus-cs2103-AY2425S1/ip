a019c1c67f0961f6001167ad3639190281f5c46c
marchjlim <marchjlim@gmail.com>
2024-09-03 19:45:12 +0800

    Add GUI functionalities to Phenex

--- src/main/java/phenex/Phenex.java
+++ src/main/java/phenex/Phenex.java
@@ -2,11 +2,11 @@ package phenex;
 
 import phenex.command.Command;
 import phenex.command.TerminatingCommand;
+import phenex.exception.PhenexException;
+import phenex.storage.Storage;
 import phenex.task.TaskList;
 import phenex.ui.Ui;
-import phenex.storage.Storage;
 import phenex.util.Parser;
-import phenex.exception.PhenexException;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,12 +26,33 @@ public class Phenex {
     /** Encapsulates the parser of Phenex. */
     private Parser parser;
 
+    private final Path DEFAULT_FILEPATH = Paths.get(System.getProperty("user.home"),
+                                                                "Downloads",
+                                                                       "CS2103T_AY2425",
+                                                                       "iP",
+                                                                       "data",
+                                                                       "phenex.txt");
+
     public Phenex(Path filePath) {
         this.storage = new Storage(filePath);
         this.tasks = new TaskList(this.storage);
         this.ui = new Ui();
         this.parser = new Parser();
 
+    }
+
+    public Phenex() {
+        this.storage = new Storage(DEFAULT_FILEPATH);
+        this.tasks = new TaskList(this.storage);
+        this.ui = new Ui();
+        this.parser = new Parser();
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 
     public static void main(String[] args) {
