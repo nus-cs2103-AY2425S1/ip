9d87049d43f058fabda89f4fb97d1f7bfe25dbb8
stephaneleong <stephaneleong@gmail.com>
2024-09-03 11:53:05 +0800

    Add JavaDoc to public methods and class

--- src/main/java/easton/model/Event.java
+++ src/main/java/easton/model/Event.java
@@ -6,11 +6,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Represents an event task.
+ */
 public class Event extends Task{
 
     private LocalDateTime from;
     private LocalDateTime to;
 
+    /**
+     * Constructs a new event task with the specified description, date & time from and to.
+     * If the date & time format is wrong, an exception is thrown.
+     *
+     * @param description Description of the task.
+     * @param from Date & time the task begins.
+     * @param to Date & time the task ends.
+     * @throws DateTimeFormatException If the date & time indicated is in the wrong format.
+     */
     public Event(String description, String from, String to) throws DateTimeFormatException {
         super(description);
         try {
