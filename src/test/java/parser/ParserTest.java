dfeddc43e94f3e1dc67b2dfaf851037bcd83b335
wubojin <wubojin64@gmail.com>
2024-09-04 02:24:59 +0800

    Add JUnit Tests

---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,60 @@
+package parser;
+
+import exception.JadeException;
+import task.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+    @Test
+    void testParseTaskCommandTodo() throws JadeException {
+        Parser parser = new Parser();
+        Task task = parser.parseTaskCommand("todo Read a book");
+
+        assertInstanceOf(Todo.class, task);
+        assertEquals("Read a book", task.getDescription());
+    }
+
+    @Test
+    void testParseTaskCommandDeadline() throws JadeException {
+        Parser parser = new Parser();
+        Task task = parser.parseTaskCommand("deadline Submit report /by 2024-12-01 1200");
+
+        assertInstanceOf(Deadline.class, task);
+        assertEquals("Submit report", task.getDescription());
+    }
+
+    @Test
+    void testParseTaskCommandEvent() throws JadeException {
+        Parser parser = new Parser();
+        Task task = parser.parseTaskCommand("event Project meeting /from 2024-12-01 1200 /to 2024-12-01 1400");
+
+        assertInstanceOf(Event.class, task);
+        assertEquals("Project meeting", task.getDescription());
+    }
+
+    @Test
+    void testParseInvalidDeadlineFormat() {
+        Parser parser = new Parser();
+        Exception exception = assertThrows(JadeException.class, () -> {
+            parser.parseTaskCommand("deadline Submit report /by wrong-format");
+        });
+
+        assertEquals("Please use yyyy-MM-dd HHmm format for time.\n"
+                + "       eg. 2024-12-25 2130", exception.getMessage());
+    }
+
+    @Test
+    void testParseInvalidEventFormat() {
+        Parser parser = new Parser();
+        Exception exception = assertThrows(JadeException.class, () -> {
+            parser.parseTaskCommand("event Meeting /from wrong-format /to wrong-format");
+        });
+
+        assertEquals("Please use yyyy-MM-dd HHmm format for time.\n"
+                + "       eg. 2024-12-25 2130", exception.getMessage());
+    }
+}
