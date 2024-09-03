550c9d7d3cf6259fb835c4455b078aa9b0d239fd
C5hives <e1122945@u.nus.edu>
2024-09-03 19:21:11 +0800

    Add factory class to handle parse method
    The factory will call the relevant class to parse
    input strings depending on its source

--- src/main/java/TaskFactory.java
+++ src/main/java/TaskFactory.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TaskFactory {
+public class TaskFactory {
+    public static Task createTask(String input, InputSource source) {
+        TaskCreator creator;
+        switch(source) {
+        case FILE:
+            creator = new FileTaskCreator();
+            return creator.createTask(input);
+        case USER:
+            creator = new UserTaskCreator();
+            return creator.createTask(input);
+        default:
+            // This case should never be reached
+            throw new IllegalStateException("Unexpected source: " + source);
+        }
+    }
 }
