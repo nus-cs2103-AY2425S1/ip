aae69c6b550e7bc0be7fa7ef2afcd7ebdce47940
SQ77 <andrewzsq@gmail.com>
2024-09-03 18:09:44 +0800

    Add basic javaFX GUI for oliver

--- src/main/java/oliver/MainWindow.java
+++ src/main/java/oliver/MainWindow.java
@@ -1,2 +1,67 @@
-package oliver;public class MainWindow {
+package oliver;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Oliver oliver;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image oliverImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        String greeting = "Hello! I'm Oliver.\nWhat can I do for you?";
+        dialogContainer.getChildren().add(
+                DialogBox.getDukeDialog(greeting, oliverImage)
+        );
+    }
+
+    /** Injects the Oliver instance */
+    public void setOliver(Oliver oliver) {
+        this.oliver = oliver;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Oliver's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = oliver.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, oliverImage)
+        );
+        userInput.clear();
+
+        if (input.equalsIgnoreCase("bye")) {
+            // Exit the application after 2.5 seconds of delay
+            PauseTransition delay = new PauseTransition(Duration.seconds(2.5));
+            delay.setOnFinished(event -> Platform.exit());
+            delay.play();
+        }
+    }
 }
