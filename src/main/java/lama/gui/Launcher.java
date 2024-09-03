ccce291ca0bec96b40d9023ec4dade412010e963
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-09-03 18:33:07 +0800

    JavaDoc for Launcher class

--- src/main/java/lama/gui/Launcher.java
+++ src/main/java/lama/gui/Launcher.java
@@ -3,9 +3,18 @@ package lama.gui;
 import javafx.application.Application;
 
 /**
- * A launcher class to launch the application
+ * The Launcher class serves as the entry point for the JavaFX application.
+ * It is responsible for launching the JavaFX application by invoking the Application method.
+ * This class delegates the main responsibilities to the Main class, which extends Application.
  */
 public class Launcher {
+    /**
+     * The main method that serves as the entry point of the JavaFX application.
+     * It calls the Application method to start the JavaFX lifecycle.
+     *
+     * @param args The command line arguments passed to the application.
+     *             Any arguments needed by the application should be specified here.
+     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
