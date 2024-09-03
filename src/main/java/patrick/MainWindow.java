e1eab97ed128165aa2179917a1abb834a4e6975e
Chronoxy <adriannpcc@gmail.com>
2024-09-04 01:06:08 +0800

    Add GUI Files
    Files: DialogBox.java, Launcher.java, Main.java, MainWindow.java

---
+++ src/main/java/patrick/MainWindow.java
@@ -0,0 +1,61 @@
+package patrick;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI window in the Patrick application.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Patrick patrick;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    /**
+     * Initializes the MainWindow by binding the scroll pane's vertical value to the height of the dialog container.
+     * This ensures that the most recent dialog is always visible.
+     */
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /**
+     * Injects the Patrick instance to be used by this controller.
+     *
+     * @param pat The Patrick instance to be associated with this controller.
+     */
+    public void setPatrick(Patrick pat) {
+        patrick = pat;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = patrick.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
