0e72c0b7a8bd4c7ca53e1a8008dc983913f1eb0b
clarissatjx <claire.teo145@gmail.com>
2024-09-03 12:50:53 +0800

    Add JavaDocs to Edith, Parser, Storage, ToDoList and Ui

--- src/main/java/edith/Edith.java
+++ src/main/java/edith/Edith.java
@@ -3,8 +3,15 @@ package edith;
 import java.util.Objects;
 import java.util.Scanner;
 
-
+/**
+ * This class is the entry point for chatbot Edith.
+ */
 public class Edith {
+
+    /**
+     * Handles all user inputs.
+     * @param args User input.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ToDoList todoList = new ToDoList();
