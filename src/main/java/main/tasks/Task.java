8db603924faa95fd0893a5ef0f80b3a8bc216e54
ywllowsensor <isyazani@gmail.com>
2024-09-04 22:05:03 +0800

    Remove print line

--- src/main/java/main/tasks/Task.java
+++ src/main/java/main/tasks/Task.java
@@ -34,7 +34,6 @@ public class Task {
      */
     public void markAsDone() {
         this.isDone = true;
-        System.out.println("    Nice! I've marked this task as done:");
     }
 
     /**
@@ -42,7 +41,6 @@ public class Task {
      */
     public void markAsNotDone() {
         this.isDone = false;
-        System.out.println("    OK, I've marked this task as not done yet:");
     }
 
     /**
