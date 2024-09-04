aad3eacc1a9658356acb799723d56ccaa668df05
IanCheah <iancheah24@gmail.com>
2024-09-04 22:46:51 +0800

    Fix style issues in CommandFoundButInvalidException class raised by checkstyle

--- src/main/java/exception/CommandFoundButInvalidException.java
+++ src/main/java/exception/CommandFoundButInvalidException.java
@@ -1,6 +1,15 @@
 package exception;
 
+/**
+ * Exception thrown when a command is found but is of invalid syntax
+ */
 public class CommandFoundButInvalidException extends Exception {
+
+    /**
+     * Constructs a {@code CommandFoundButInvalidException} with the specified detail message.
+     *
+     * @param input the message which is retrieved later when the exception is thrown
+     */
     public CommandFoundButInvalidException(String input) {
         super(input);
     }
