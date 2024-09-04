11de21f669f21738b851afc4a3505a867e2b2292
kngys <kristiangys@gmail.com>
2024-09-05 00:53:20 +0800

    Update to follow coding standard

--- src/test/java/serenity/TodoTest.java
+++ src/test/java/serenity/TodoTest.java
@@ -1,6 +1,8 @@
 package serenity;
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class TodoTest {
 
