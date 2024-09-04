bb7d32a530181210a24c539fb4b46d48db832fa5
Meowloid <tochiex3@gmail.com>
2024-09-04 15:07:52 +0800

    Add speech bubbles to GUI

--- src/main/java/Majima/gui/Main.java
+++ src/main/java/Majima/gui/Main.java
@@ -20,6 +20,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
 
