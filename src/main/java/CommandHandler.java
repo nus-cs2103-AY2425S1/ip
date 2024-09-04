9eeb2c496835212c76b80322b6798dc7babc6730
sayomaki <sayomayomaki@gmail.com>
2024-09-05 00:43:20 +0800

    Add a basic command handler and base command class
    For the command class, we use an abstract class to ensure that it has to
    be implemented by a subclass (which is the actual command).
    
    The command handler is responsible for managing all the commands in the chatbot.

---
+++ src/main/java/CommandHandler.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandHandler {
+    private final Map<String, Command> commands;
+
+    public CommandHandler() {
+        commands = new HashMap<>();
+
+        registerAllCommands();
+    }
+
+    public void registerCommand(Command command) {
+        commands.put(command.getName(), command);
+    }
+
+    public Command getCommand(String command) {
+        return commands.get(command);
+    }
+
+    private void registerAllCommands() {
+    }
+}
