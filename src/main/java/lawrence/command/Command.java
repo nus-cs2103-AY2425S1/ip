46356d4cef1225485a49f49245d354d0ff73a3ba
C5hives <e1122945@u.nus.edu>
2024-09-04 15:38:39 +0800

    Add javadoc for Command.java

--- src/main/java/lawrence/command/Command.java
+++ src/main/java/lawrence/command/Command.java
@@ -6,13 +6,49 @@ import lawrence.ui.UserInterface;
 
 import java.io.IOException;
 
+/**
+ * Represents a general command that can be issued by the user.
+ * The command is not processed until
+ * {@link #execute(TaskList, TaskFileManager, UserInterface)}
+ * is called.
+ */
 public abstract class Command {
+    /**
+     * Executes the specified user command.
+     *
+     * @param tasks   a list of tasks the command may operate
+     *                on
+     * @param manager a {@link TaskFileManager} instance that
+     *                the command may use when saving changes
+     *                made
+     * @param ui      the {@link UserInterface} instance to
+     *                display possible messages to the user
+     */
     abstract public void execute(TaskList tasks, TaskFileManager manager, UserInterface ui);
 
+    /**
+     * Saves the current tasks in memory to a text file.
+     * <p>
+     * This operation will overwrite the original text file entirely.
+     * </p>
+     *
+     * @param tasks        the list of tasks to be saved into the
+     *                     text file
+     * @param manager      the {@link TaskFileManager} instance that
+     *                     can write to the text file
+     * @throws IOException if writing to the file is unsuccessful
+     */
     protected void saveTasks(TaskList tasks, TaskFileManager manager) throws IOException {
         manager.saveTasksToFile(tasks.getTasks());
     }
 
+    /**
+     * Returns a boolean indicating whether the program should
+     * continue running after this command.
+     *
+     * @return a boolean indicating whether the program should
+     *         continue running
+     */
     public boolean shouldContinue() {
         return true;
     }
