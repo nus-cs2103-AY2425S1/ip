8d0dde018878b48a1d3474219871a7686d572fec
GavinSin <51268353+GavinSin@users.noreply.github.com>
2024-09-05 00:39:02 +0800

    Set title on the application

--- src/main/java/mira/Main.java
+++ src/main/java/mira/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Mira Chatbot");
             stage.setMinHeight(220);
             stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setMira(mira);  // inject the Duke instance
