1e78e4bbd6b7df17f7fe572e9e53c5ba0ace110a
C5hives <e1122945@u.nus.edu>
2024-09-04 16:14:56 +0800

    Add javadoc for DisplayTasksCommand.java

--- src/main/java/lawrence/command/DisplayTasksCommand.java
+++ src/main/java/lawrence/command/DisplayTasksCommand.java
@@ -4,10 +4,29 @@ import lawrence.database.TaskFileManager;
 import lawrence.task.TaskList;
 import lawrence.ui.UserInterface;
 
+/**
+ * Represents the user command to display all existing tasks in the list.
+ */
 public class DisplayTasksCommand extends Command {
+    /**
+     * Default constructor.
+     */
     public DisplayTasksCommand() {
         // Empty constructor
     }
+
+    /**
+     * Displays all tasks present in the {@link TaskList} to the user.
+     * If no tasks exist, a different message is displayed.
+     *
+     * @param taskList a list of tasks the command may operate
+     *                 on
+     * @param manager  a {@link TaskFileManager} instance that
+     *                 the command may use when saving changes
+     *                 made
+     * @param ui       the {@link UserInterface} instance to
+     *                 display possible messages to the user
+     */
     @Override
     public void execute(TaskList taskList, TaskFileManager manager, UserInterface ui) {
         if (taskList.getSize() < 1) {
