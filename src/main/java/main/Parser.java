ca23479563609e3de3d8aebe5a4b8b1864d9e068
IanCheah <iancheah24@gmail.com>
2024-09-04 21:21:46 +0800

    Edit style issues for Parser class

--- src/main/java/main/Parser.java
+++ src/main/java/main/Parser.java
@@ -3,13 +3,13 @@ package main;
 import exception.CommandFoundButInvalidException;
 import exception.CommandNotFoundException;
 import exception.EmptyStringException;
-import task.Task;
 
-import java.util.List;
-
+/**
+ * Parses user input commands and performs corresponding actions on tasks
+ * Handles command recognition, validation, and execution, interacting with
+ * {@code TaskList}, {@code Storage}, and {@code Ui} components
+ */
 public class Parser {
-    /* goal of the parser class is to take in a string input and determine the
-    right command. After which, invokes the respective classes*/
     private String command;
     private String remainder;
     private TaskList allTasks;
@@ -30,7 +30,8 @@ public class Parser {
      * @throws CommandFoundButInvalidException if the command is recognized but the remaining string
      *         are of invalid syntax
      */
-    public Parser(String input, TaskList allTasks, Storage s, Ui ui) throws EmptyStringException, CommandNotFoundException, CommandFoundButInvalidException {
+    public Parser(String input, TaskList allTasks, Storage s, Ui ui) throws
+            EmptyStringException, CommandNotFoundException, CommandFoundButInvalidException {
         this.allTasks = allTasks;
         this.ui = ui;
         if (input.isEmpty()) {
@@ -46,47 +47,47 @@ public class Parser {
         }
         Commands cmd = Commands.fromString(command);
         switch(cmd) {
-            case TODO:
-                allTasks.addTodo(remainder);
-                s.put(allTasks);
-                ui.display(ui.addedMessage(allTasks.getLastAdded(), allTasks.getSize()));
-                break;
-            case DEADLINE:
-                allTasks.addDeadline(remainder);
-                s.put(allTasks);
-                System.out.println(ui.addedMessage(allTasks.getLastAdded(), allTasks.getSize()));
-                break;
-            case EVENT:
-                allTasks.addEvent(remainder);
-                s.put(allTasks);
-                System.out.println(ui.addedMessage(allTasks.getLastAdded(), allTasks.getSize()));
-                break;
-            case DELETE:
-                allTasks.delete(remainder);
-                s.put(allTasks);
-                System.out.println(ui.deleteMessage(this.allTasks.getLastDeleted(), this.allTasks.getSize()));
-                break;
-            case LIST:
-                System.out.println(allTasks.list());
-                break;
-            case MARK:
-                allTasks.mark(remainder, allTasks.getAllTasks());
-                s.put(allTasks);
-                System.out.println(ui.markedMessage(allTasks.getLastMarked()));
-                break;
-            case UNMARK:
-                allTasks.unmark(remainder);
-                s.put(allTasks);
-                ui.display(ui.unmarkedMessage(allTasks.getLastUnmarked()));
-                break;
-            case FIND:
-                ui.display(ui.findMessage());
-                ui.display(new TaskList(allTasks.find(remainder)).list());
-                break;
-            case BYE:
-                System.out.println(ui.bye());
-                this.isOver = true;
-                break;
+        case TODO:
+            allTasks.addTodo(remainder);
+            s.put(allTasks);
+            ui.display(ui.addedMessage(allTasks.getLastAdded(), allTasks.getSize()));
+            break;
+        case DEADLINE:
+            allTasks.addDeadline(remainder);
+            s.put(allTasks);
+            System.out.println(ui.addedMessage(allTasks.getLastAdded(), allTasks.getSize()));
+            break;
+        case EVENT:
+            allTasks.addEvent(remainder);
+            s.put(allTasks);
+            System.out.println(ui.addedMessage(allTasks.getLastAdded(), allTasks.getSize()));
+            break;
+        case DELETE:
+            allTasks.delete(remainder);
+            s.put(allTasks);
+            System.out.println(ui.deleteMessage(this.allTasks.getLastDeleted(), this.allTasks.getSize()));
+            break;
+        case LIST:
+            System.out.println(allTasks.list());
+            break;
+        case MARK:
+            allTasks.mark(remainder, allTasks.getAllTasks());
+            s.put(allTasks);
+            System.out.println(ui.markedMessage(allTasks.getLastMarked()));
+            break;
+        case UNMARK:
+            allTasks.unmark(remainder);
+            s.put(allTasks);
+            ui.display(ui.unmarkedMessage(allTasks.getLastUnmarked()));
+            break;
+        case FIND:
+            ui.display(ui.findMessage());
+            ui.display(new TaskList(allTasks.find(remainder)).list());
+            break;
+        case BYE:
+            System.out.println(ui.bye());
+            this.isOver = true;
+            break;
         }
     }
 
@@ -96,6 +97,6 @@ public class Parser {
      * @return a boolean value depending on the command of the user
      */
     public boolean isOver() {
-        return this.isOver;
+            return this.isOver;
     }
 }
