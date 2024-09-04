f61edba7665c9ec7de2af73069bdd40dd1ee3f3e
Teddayz <mervynneo02@gmail.com>
2024-09-04 14:26:01 +0800

    Add custom behavior to DialogBox

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,3 +1,6 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -20,5 +23,24 @@ public class DialogBox extends HBox {
         this.setAlignment(Pos.TOP_RIGHT);
 
         this.getChildren().addAll(text, displayPicture);
+    }
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tempList = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tempList);
+        this.getChildren().setAll(tempList);
+    }
+
+    public static DialogBox getUserDialog(String s, Image i) {
+        return new DialogBox(s, i);
+    }
+
+    public static DialogBox getMeeksDialog(String s, Image i) {
+        var dialogBox = new DialogBox(s, i);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
