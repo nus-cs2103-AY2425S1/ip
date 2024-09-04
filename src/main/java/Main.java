66d5d24f12a7aee96b94f86ccb26142173e4d276
beginner-web-developer <e1156242@u.nus.edu>
2024-09-04 16:02:29 +0800

    Separate GUI into controller and view

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,28 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.io.IOException;
 
-
+/**
+ * A GUI for Alice using FXML.
+ */
 public class Main extends Application {
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField input;
-    private Button sendButton;
-    private Scene scene;
-    private Image aliceImage = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("AliceImage.png")));
-    private Image userImage = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("UserImage.png")));
+    private Alice alice = new Alice("data.txt");
 
     @Override
     public void start(Stage stage) {
-        // set up required components
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        input = new TextField();
-        sendButton = new Button("Send");
-
-        DialogBox dialogBox = new DialogBox("Hello", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, input, sendButton);
-
-        scene = new Scene(mainLayout);
-        stage.setScene(scene);
-        stage.show();
-
-        // format nodes in the right places
-        stage.setTitle("Alice");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385.0, 535.0);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        input.setPrefWidth(325.0);
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-        AnchorPane.setLeftAnchor(input, 1.0);
-        AnchorPane.setBottomAnchor(input, 1.0);
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane anchorPane = fxmlLoader.load();
+            Scene scene = new Scene(anchorPane);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setAlice(alice);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
