f07f202e260d7e45f930e7b03f23bfd203523a2b
dovlin <dovlincs01@gmail.com>
2024-09-04 14:30:19 +0800

    Add greet ability on GUI

--- src/main/java/momo/Ui.java
+++ src/main/java/momo/Ui.java
@@ -1,21 +1,42 @@
 package momo;
 
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import momo.task.TaskList;
 
 import java.util.Scanner;
+
+import javafx.scene.layout.VBox;
+
 
 /**
  * Handles Ui related functionality in the chatbot program, handling some user interactions
  */
 public class Ui {
+    private VBox dialogContainer;
+    private TextField userInput;
+
+    public Ui(VBox dialogContainer, TextField userInput) {
+        this.userInput = userInput;
+        this.dialogContainer = dialogContainer;
+    }
+
     public static void showHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
 
     private final static Scanner sc = new Scanner(System.in);
 
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/userIcon.png"));
+    private final Image momoImage = new Image(this.getClass().getResourceAsStream("/images/momoIcon.png"));
+
+
     public void showGreeting() {
-        System.out.println("Hello! I'm Momo.\nWhat can I do for you?");
+        String input = "Greetings... I'm Momo.\n What can I do for you?";
+        dialogContainer.getChildren().addAll(
+                DialogBox.getMomoDialog(input, momoImage)
+        );
+
     }
 
     /**
@@ -23,9 +44,13 @@ public class Ui {
      *
      * @return Returns a String containing user input
      */
-    public String getUserInput() {
-        return sc.nextLine();
-    }
+//    public String getUserInput() {
+//        String input = userInput.getText();
+//        dialogContainer.getChildren().addAll(
+//                DialogBox.getUserDialog(input, userImage)
+//        );
+//        return input;
+//    }
 
     public void printList(TaskList list) {
         showHorizontalLine();
