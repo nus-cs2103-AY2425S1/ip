6985fbe7f7d6ef82ec9b915e43b4bf38d958c5fc
hyxnnii <E1156838@u.nus.edu>
2024-09-04 15:42:07 +0800

    Add A-MoreOOP: parser, storage, ui class

--- src/main/java/Kira.java
+++ src/main/java/Kira.java
@@ -1,9 +1,5 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileWriter;
 
 public class Kira {
 
