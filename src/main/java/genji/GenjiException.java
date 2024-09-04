d466e697c317477e247f6b6a710a2a0575509eb6
Liu Yinze <>
2024-09-04 10:35:36 +0800

    Delete a useless line in GenjiException Fix code style to reach standard

--- src/main/java/genji/GenjiException.java
+++ src/main/java/genji/GenjiException.java
@@ -4,8 +4,4 @@ public class GenjiException extends Exception{
     public GenjiException(String s) {
         super(s);
     }
-    /*@Override
-    public String toString() {
-        return ;
-    }*/
 }
