27d745c536eab7c979f49cf972bd65b002e46e4a
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-09-05 15:15:22 +0800

    Create GUI for Velma

---
+++ src/main/java/velma/Launcher.java
@@ -0,0 +1,10 @@
+package velma;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
