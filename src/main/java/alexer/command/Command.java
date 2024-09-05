1836845d2242f0fb1b2d7abfd66fc585e9473fbd
sayomaki <sayomayomaki@gmail.com>
2024-09-06 01:25:30 +0800

    Add javadocs for the command subpackage

--- src/main/java/alexer/command/Command.java
+++ src/main/java/alexer/command/Command.java
@@ -1,15 +1,38 @@
 package alexer.command;
 
+/**
+ * Represents a command that can be processed by the chatbot
+ *
+ * @author sayomaki
+ */
 public abstract class Command {
     private final String name;
 
+    /**
+     * Creates a new command with the given name.
+     * Used by subclasses as this class is abstract.
+     *
+     * @param name command name
+     */
     public Command(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the name of the command as a string
+     *
+     * @return string name of command
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Executes the command with the given arguments.
+     * This method is abstract and needs to be overridden by the
+     * implementation in the command subclass.
+     *
+     * @param arguments string array of arguments to be provided
+     */
     public abstract void run(String[] arguments);
 }
