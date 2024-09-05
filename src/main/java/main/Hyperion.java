8619d95b5215aa81b14188cbd5ff8aeeb649d436
IanCheah <iancheah24@gmail.com>
2024-09-05 15:08:44 +0800

    Edit files in the main package

--- src/main/java/main/Hyperion.java
+++ src/main/java/main/Hyperion.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.util.Scanner;
-
 import exception.CommandFoundButInvalidException;
 import exception.CommandNotFoundException;
 import exception.EmptyStringException;
@@ -11,6 +9,8 @@ import exception.EmptyStringException;
  * It initializes the user interface, storage, and task list, and processes user input commands.
  */
 public class Hyperion {
+    public static final String FILE_PATH = "data/tasks.txt";
+
     private Storage storage;
     private TaskList allTasks;
     private Ui ui;
@@ -19,47 +19,32 @@ public class Hyperion {
      * Constructs a {@code Hyperion} instance with a specified file path for storage.
      * Initializes the user interface, loads tasks from storage, and starts processing user input.
      *
-     * @param filePath the path to the file where tasks are stored
      */
-    public Hyperion(String filePath) {
+    public Hyperion() {
         try {
             this.ui = new Ui();
-            this.ui.showWelcome();
-            this.storage = new Storage(filePath);
+            this.storage = new Storage(FILE_PATH);
             this.allTasks = new TaskList(storage.load());
-            ui.display(this.allTasks.list());
+
         } catch (CommandFoundButInvalidException e) {
             System.out.print("There is an error" + e.getMessage());
         }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String input;
-
-        boolean isOver = false;
-        while (!isOver) {
-            System.out.println("> ");
-            input = scanner.nextLine();
-
-            try {
-                Parser parser = new Parser(input, allTasks, this.storage, this.ui);
-                isOver = parser.isOver();
-            } catch (EmptyStringException
-                     | CommandFoundButInvalidException
-                     | CommandNotFoundException e) {
-                ui.display(e.getMessage());
-            }
-        }
-
-        scanner.close();
+    public static void main(String[] args) {
     }
 
     /**
-     * The entry point of the application.
-     * Creates a new {@code Hyperion} instance with the specified file path for storage.
-     *
-     * @param args command line arguments
+     * Generates a response for the user's chat message.
      */
-    public static void main(String[] args) {
-        new Hyperion("data/tasks.txt");
+    public String getResponse(String input) {
+        try {
+            Parser parser = new Parser(input, allTasks, this.storage, this.ui);
+            return parser.run();
+        } catch (EmptyStringException
+                 | CommandFoundButInvalidException
+                 | CommandNotFoundException e) {
+            return e.getMessage();
+        }
     }
 }
