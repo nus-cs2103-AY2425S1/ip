2b72d7afd9da6174a407038ebde90c6ad33e47f2
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-09-05 16:43:31 +0800

    Using FXML

--- src/main/java/velma/DialogBox.java
+++ src/main/java/velma/DialogBox.java
@@ -1,45 +1,43 @@
 package velma;
+import java.io.IOException;
+import java.util.Collections;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Node;
-import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.shape.Circle;
-
 public class DialogBox extends HBox {
 
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        // Styling the dialog box
-        text.setWrapText(true);
-        Circle circle = new Circle(50, 50, 50);  // Circle with center (50, 50) and radius 50
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-        displayPicture.setClip(circle);
-        this.setAlignment(Pos.TOP_RIGHT);  // Align text and image to the right for user dialog
-
-        // Add padding and spacing for dialog box
-        this.setPadding(new Insets(10, 10, 10, 10));  // Add padding around the entire dialog box
-        this.setSpacing(10);  // Increase spacing between the text and image
-
-        this.getChildren().addAll(text, displayPicture);
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);  // Align text and image to the left for Duke dialog
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);  // Reverse the order of the label and image for the Duke dialog
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
     public static DialogBox getUserDialog(String s, Image i) {
