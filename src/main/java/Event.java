25cbb409585f5a4b6fe949f8e9cd8a90673ff426
Haris-Irfan <harisirfan@gmail.com>
2024-09-06 05:31:31 +0800

    Implement date time reading and filtering functionality

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,29 +1,56 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
 
-    private String start;
-    private String end;
+    private LocalDate startDate;
+    private String startTime;
+    private LocalDate endDate;
+    private String endTime;
     private Task task;
 
     public Event(String description, String start, String end) {
         super(description);
         this.task = new Task(this.description);
-        this.start = start.replaceFirst("from ", "");
-        this.end = end.replaceFirst("to ", "");
+
+        String[] beginning = start.replaceFirst("from ", "").split(" ", 2);
+        this.startDate = LocalDate.parse(beginning[0]);
+        this.startTime = beginning[1];
+
+        String[] ending  = end.replaceFirst("to ", "").split(" ", 2);
+        this.endDate = LocalDate.parse(ending[0]);
+        this.endTime = ending[1];
+    }
+
+    public String getStartDate() {
+        return this.startDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    public String getEndDate() {
+        return this.endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String markTask() {
-        return String.format("[E] %s (from: %s to: %s)", this.task.markTask() , this.start, this.end);
+        return String.format("[E] %s (from: %s %s to: %s %s)", this.task.markTask() , this.getStartDate(), this.startTime, this.getEndDate(), this.endTime);
     }
 
     @Override
     public String unmarkTask() {
-        return String.format("[E] %s (from: %s to: %s)", this.task.unmarkTask() , this.start, this.end);
+        return String.format("[E] %s (from: %s %s to: %s %s)", this.task.unmarkTask() , this.getStartDate(), this.startTime, this.getEndDate(), this.endTime);
+    }
+
+    @Override
+    public boolean compareDates(String date) {
+        LocalDate newDate = LocalDate.parse(date);
+        return (this.startDate.isBefore(newDate) && this.endDate.isAfter(newDate)) ||
+                this.startDate.equals(newDate) || this.endDate.equals(newDate);
     }
 
     @Override
     public String toString() {
-        return String.format("[E] %s (from: %s to: %s)", this.task.toString() , this.start, this.end);
+        return String.format("[E] %s (from: %s %s to: %s %s)", this.task.toString(), this.getStartDate(), this.startTime, this.getEndDate(), this.endTime);
     }
 
 }
