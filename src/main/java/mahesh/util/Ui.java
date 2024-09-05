22280cde4c65d273b873c6800eaead4a64885429
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2024-09-05 17:00:33 +0800

    Minor style changes

--- src/main/java/mahesh/util/Ui.java
+++ src/main/java/mahesh/util/Ui.java
@@ -7,29 +7,31 @@ import mahesh.task.Task;
  */
 public class Ui {
     /**
-     * The logo to be displayed when the application starts.
+     * The LOGO to be displayed when the application starts.
      */
-    private static String logo =
-          "#     #                                       ######                               ###### \n"
-        + "##   ##   ##   #    # ######  ####  #    #    #     #   ##   #      #              #      \n"
-        + "# # # #  #  #  #    # #      #      #    #    #     #  #  #  #      #              #      \n"
-        + "#  #  # #    # ###### #####   ####  ######    #     # #    # #      #      #####   #####  \n"
-        + "#     # ###### #    # #           # #    #    #     # ###### #      #              #      \n"
-        + "#     # #    # #    # #      #    # #    #    #     # #    # #      #              #      \n"
-        + "#     # #    # #    # ######  ####  #    #    ######  #    # ###### ######         ###### \n";
+    private static final String LOGO =
+          """
+          #     #                                       ######                               ###### 
+          ##   ##   ##   #    # ######  ####  #    #    #     #   ##   #      #              #      
+          # # # #  #  #  #    # #      #      #    #    #     #  #  #  #      #              #      
+          #  #  # #    # ###### #####   ####  ######    #     # #    # #      #      #####   #####  
+          #     # ###### #    # #           # #    #    #     # ###### #      #              #      
+          #     # #    # #    # #      #    # #    #    #     # #    # #      #              #      
+          #     # #    # #    # ######  ####  #    #    ######  #    # ###### ######         ###### 
+          """;
 
     /**
      * Divider line used for separating sections in the console output.
      */
-    private static String divider = "-------------------------------------------------------";
+    private static final String DIVIDER = "-------------------------------------------------------";
 
     /**
-     * Prints the startup message including the logo and a welcome message.
+     * Prints the startup message including the LOGO and a welcome message.
      */
     public static void printStartupMessage() {
-        System.out.println(Ui.logo);
+        System.out.println(Ui.LOGO);
         System.out.println("Hello! I'm Mahesh Dall-E [but you can call me Mahesh ;)]\nWhat can I do for you?");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -37,7 +39,7 @@ public class Ui {
      */
     public static void printExitMessage() {
         System.out.println("Bye! Hope to see you again soon!");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -50,7 +52,7 @@ public class Ui {
         System.out.println("Got it. I've added this task:");
         System.out.println("  " + task);
         System.out.println("Now you have " + taskCount + " tasks in the list.");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -63,7 +65,7 @@ public class Ui {
         System.out.println("Noted. I've removed this task:");
         System.out.println("  " + task);
         System.out.println("Now you have " + taskCount + " tasks in the list.");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -74,7 +76,7 @@ public class Ui {
     public static void printMarkedAsDone(Task task) {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("  " + task);
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -85,7 +87,7 @@ public class Ui {
     public static void printUnmarkedAsDone(Task task) {
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println("  " + task);
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -95,7 +97,7 @@ public class Ui {
      */
     public static void printErr(MaheshException err) {
         System.out.println(err.getMessage());
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -106,7 +108,7 @@ public class Ui {
     public static void printNoSuchTaskErr(int taskCount) {
         System.out.println("There is no such task. You currently have " + taskCount + " task(s).");
         System.out.println("Use the \"list\" command to view all your tasks.");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -117,7 +119,7 @@ public class Ui {
     public static void printIncompleteCommandErr(MaheshException err) {
         System.out.println("The command is incomplete/incorrect.");
         System.out.println(err.getMessage());
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -125,7 +127,7 @@ public class Ui {
      */
     public static void printEmptyListErr() {
         System.out.println("You have no tasks! Add a few tasks (todo, deadline or event)");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
@@ -133,13 +135,13 @@ public class Ui {
      */
     public static void printCorruptedDataErr() {
         System.out.println("Some tasks may not have loaded correctly due to corrupted data file.");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
     /**
-     * Prints a divider line.
+     * Prints a DIVIDER line.
      */
     public static void printDivider() {
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 }
