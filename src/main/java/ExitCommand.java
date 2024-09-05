9e8333d4a8cc6a5e899037eddfd16d507c75b92a
izruff <rafaelky.dev@gmail.com>
2024-09-06 03:05:27 +0800

    Add command subclass for exiting the program

---
+++ src/main/java/ExitCommand.java
@@ -0,0 +1,17 @@
+public class ExitCommand extends Command {
+    public ExitCommand() {
+        super();
+        this.isExit = true;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        try {
+            storage.save(tasks);
+
+            ui.showGoodbyeMessage();
+        } catch (MittensException e) {
+            ui.showErrorMessage(e);
+        }
+    }
+}
