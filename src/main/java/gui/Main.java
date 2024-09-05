8d2395fee91306262fb85869355ee395baff9f45
jankai <android.agent.k@gmail.com>
2024-09-06 07:15:03 +0800

    Starts integration of chatterbox with gui

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.IOException;
 
+import chatterbox.Chatterbox;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,8 +10,18 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 public class Main extends Application{
 
+    private Chatterbox chatterbox = new Chatterbox();
     @Override
-    public void start(Stage stage) throws Exception {
-
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setChatterbox(chatterbox);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
