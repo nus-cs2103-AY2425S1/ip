a5d3704d76733de69fe1fd33886724d4d269f76e
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-09-06 00:36:46 +0800

    Fix style with checkstyle

--- src/main/java/yapper/task/Event.java
+++ src/main/java/yapper/task/Event.java
@@ -3,6 +3,7 @@ package yapper.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+
 import yapper.exception.YapperException;
 
 /**
@@ -10,15 +11,15 @@ import yapper.exception.YapperException;
  * This class stores the start and end times of the event in addition to the description.
  */
 public class Event extends Task {
-    private LocalDateTime from;
-    private LocalDateTime to;
+    private final LocalDateTime from;
+    private final LocalDateTime to;
 
     /**
      * Constructs an Event task with the specified description, start time, and end time.
-     * 
+     *
      * @param description The description of the event.
-     * @param from The start time of the event in the format yyyy-MM-dd HHmm.
-     * @param to The end time of the event in the format yyyy-MM-dd HHmm.
+     * @param from        The start time of the event in the format yyyy-MM-dd HHmm.
+     * @param to          The end time of the event in the format yyyy-MM-dd HHmm.
      * @throws YapperException If the date and time format is incorrect.
      */
     public Event(String description, String from, String to) throws YapperException {
@@ -33,22 +34,27 @@ public class Event extends Task {
 
     /**
      * Returns the string representation of the event task for saving to a file.
-     * 
+     *
      * @return A string in the format "E | status | description | start time | end time".
      */
     @Override
     public String toSaveFormat() {
-        return "E | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + from.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm")) + " | " + to.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        return "E | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | "
+            + from.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm")) + " | "
+            + to.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 
     /**
      * Returns the string representation of the event task for display.
-     * 
+     *
      * @return A string in the format "[E][status] description (from: start time to: end time)".
      */
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma")) +
-                " to: " + to.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma")) + ")";
+        return "[E]" + super.toString() + " (from: "
+            + from.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma"))
+            + " to: "
+            + to.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma")) + ")";
     }
 }
+
--- src/main/java/yapper/task/TaskList.java
+++ src/main/java/yapper/task/TaskList.java
@@ -44,3 +44,4 @@ public class TaskList {
         return matchingTasks;
     }
 }
+
