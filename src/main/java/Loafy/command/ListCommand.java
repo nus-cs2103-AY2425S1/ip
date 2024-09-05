5d1999b67f579e8dccd4234c4dcf6071d1295519
LowXiSi <e0960422@u.nus.edu>
2024-09-06 00:29:15 +0800

    Add JavaDoc comments

--- src/main/java/Loafy/command/ListCommand.java
+++ src/main/java/Loafy/command/ListCommand.java
@@ -3,7 +3,17 @@ package loafy.command;
 import loafy.tasklist.TaskList;
 import loafy.ui.Ui;
 
+/**
+ * Represents a command to list the current tasks in the task list.
+ */
 public class ListCommand extends Command {
+
+    /**
+     * Displays the current tasks in the task list.
+     *
+     * @param taskList Task list to be displayed.
+     * @param ui User interface which will print the display.
+     */
     public void execute(TaskList taskList, Ui ui) {
         ui.reply(taskList.view());
     }
