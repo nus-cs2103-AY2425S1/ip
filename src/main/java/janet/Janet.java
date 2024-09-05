738f094b0c3f806cf74a8cde38d43ef1ad67da84
rayray39 <e0772883@u.nus.edu>
2024-09-05 15:32:10 +0800

    Janet.java: Add runProgram method to return response

--- src/main/java/janet/Janet.java
+++ src/main/java/janet/Janet.java
@@ -24,6 +24,75 @@ public class Janet {
     }
 
     /**
+     * Returns the response based on the user input.
+     *
+     * @param input A string input from the user
+     * @return A string message
+     */
+    public String getResponse(String input) {
+        return runProgram(input);
+    }
+
+    public String runProgram(String userCommand) {
+        String response = "";
+        String[] commandDetails = Parser.getCommandDetails(userCommand);    // converts user input into a String array
+        try {
+            Parser.userInputChecker(commandDetails, tasks.getNumberOfTasks());    // runs all the checks to verify user input
+
+            CommandType commandType = Parser.getCommand(commandDetails);    // get the CommandType
+            switch (commandType) {
+            case BYE:
+                response = ui.exitMessage();
+                break;
+            case LIST:
+                response = ui.showTasks(tasks);
+                break;
+            case MARK:
+                String markedResult = tasks.markAsDone(Integer.parseInt(commandDetails[1]));
+                response = ui.showMarkedMessage(markedResult, tasks.getTask(Integer.parseInt(commandDetails[1]) - 1));
+                break;
+            case UNMARK:
+                String unmarkResult = tasks.unmark(Integer.parseInt(commandDetails[1]));
+                response = ui.showUnmarkedMessage(unmarkResult, tasks.getTask(Integer.parseInt(commandDetails[1]) - 1));
+                break;
+            case DELETE:
+                response = ui.showDeleteTaskMessage(tasks.getTask(Integer.parseInt(commandDetails[1]) - 1),
+                        tasks.getNumberOfTasks() - 1);
+                tasks.deleteTask(Integer.parseInt(commandDetails[1]));
+                break;
+            case TODO:
+                Task todo = new ToDo(userCommand);
+                tasks.addTaskToList(todo);
+                response = ui.showSuccessfulTaskAddition(todo, tasks.getNumberOfTasks());
+                break;
+            case DEADLINE:
+                Task deadline = new Deadline(userCommand);
+                tasks.addTaskToList(deadline);
+                response = ui.showSuccessfulTaskAddition(deadline, tasks.getNumberOfTasks());
+                break;
+            case EVENT:
+                Task event = new Event(userCommand);
+                tasks.addTaskToList(event);
+                response = ui.showSuccessfulTaskAddition(event, tasks.getNumberOfTasks());
+                break;
+            case FIND:
+                String keyword = String.join(" ",
+                        Arrays.copyOfRange(commandDetails, 1, commandDetails.length));
+                response = ui.showFindMessage(tasks.findTasks(keyword));
+                break;
+            }
+        } catch (JanetException e) {
+            response  = e.getMessage();
+        }
+        try {
+            storage.saveToJanetTextFile(tasks.getListOfTasks());
+        } catch (JanetException e) {
+            response = ui.showSavingError();
+        }
+        return response;
+    }
+
+    /**
      * Runs the Janet program.
      */
     public void run() {
