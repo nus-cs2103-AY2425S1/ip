d47d1c3080b88498e92562cc3028f469cd5940cf
Ved <ved.t.joshi@gmail.com>
2024-09-05 11:41:51 +0800

    More updates for GUI - fix fxml

--- src/main/java/agave/GUI/MainWindow.java
+++ src/main/java/agave/GUI/MainWindow.java
@@ -9,7 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-public class MainWindow extends AnchorPane{
+public class MainWindow extends AnchorPane {
 
     @FXML
     private VBox dialogContainer;
