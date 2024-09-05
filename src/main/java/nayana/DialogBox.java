617d51f1abf6fb15337f20ff3bc7918517b49937
Nayana Mahesh <nayanamahesh7@gmail.com>
2024-09-05 16:09:14 +0800

    JavaFX setting up FXML

--- src/main/java/nayana/DialogBox.java
+++ src/main/java/nayana/DialogBox.java
@@ -1,73 +1,60 @@
 package nayana;
 
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.layout.VBox;
 
+/**
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
+ */
 public class DialogBox extends HBox {
-
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        // Style the image to be circular
-        Circle clip = new Circle(50, 50, 50); // CenterX, CenterY, Radius
-        displayPicture.setClip(clip);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-
-        // Style the text
-        text.setWrapText(true);
-        text.setPadding(new Insets(10)); // Add padding around the text
-
-        // Set padding between image and text
-        this.setSpacing(10);
-
-        // Set background color
-        this.setBackground(new Background(new BackgroundFill(
-              Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY
-        )));
-        this.setPadding(new Insets(10)); // Padding inside the dialog box
-
-        // Align the dialog box
-        this.setAlignment(Pos.TOP_RIGHT);
-
-        // Add children to the HBox
-        this.getChildren().addAll(text, displayPicture);
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
-    public static DialogBox getUserDialog(String s, Image i) {
-        return new DialogBox(s, i);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    public static DialogBox getNayanaDialog(String s, Image i) {
-        var db = new DialogBox(s, i);
+    public static DialogBox getNayanaDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
         db.flip();
-        db.setBackground(new Background(new BackgroundFill(
-              Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY
-        )));
         return db;
     }
 }
