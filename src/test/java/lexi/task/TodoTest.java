c08edac3291cf6b9d4089557369bee07c08d2a1f
suhayl13 <suhayl.pervez@gmail.com>
2024-09-05 11:46:40 +0800

    Ensure compliance with checkstyle for test classes

--- src/test/java/lexi/task/TodoTest.java
+++ src/test/java/lexi/task/TodoTest.java
@@ -1,9 +1,10 @@
 package lexi.task;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class TodoTest {
 
