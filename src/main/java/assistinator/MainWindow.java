09419852d7de020a6acdefb6a7a1d2fe7563121a
Soumyadip <soumyadip1107@gmail.com>
2024-09-06 12:46:25 +0800

    Add JavaFX GUI

---
+++ src/main/java/assistinator/MainWindow.java
@@ -0,0 +1,84 @@
+package assistinator;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Assistinator assistinator;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image assistinatorImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    /**
+     * Initialises app and shows welcome message
+     */
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getAssistinatorDialog(
+                        "Hello! I'm Assistinator. What can I do for you?",
+                        assistinatorImage
+                )
+        );
+    }
+
+    /** Injects the Assistinator instance */
+    public void setAssistinator(Assistinator a) {
+        assistinator = a;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = assistinator.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getAssistinatorDialog(response, assistinatorImage)
+        );
+        userInput.clear();
+        if (response.toLowerCase().contains("bye")) {
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.exit(0);
+                }
+            }, 400);
+        }
+    }
+
+    /**
+     * Closes the application
+     */
+    private void closeApplication() {
+        Platform.exit();
+    }
+}
+
+
