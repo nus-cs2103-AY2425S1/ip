ff3515cc43fa9afeb41ffea254cbcac9e8f46ca6
jevan <e1121960@u.nus.edu>
2024-09-06 23:11:01 +0800

    Change Line Separator to \n using mac2unix for Test

--- src/test/java/yapper/ToDoTest.java
+++ src/test/java/yapper/ToDoTest.java
@@ -1,1 +1,25 @@
-package yapper;import static org.junit.jupiter.api.Assertions.assertEquals;import org.junit.jupiter.api.Test;public class ToDoTest {    @Test    public void toStringTest() {        ToDo todo = new ToDo("buy bread");        todo.setDone(true);        String expected = "[T][X] buy bread";        String actual = todo.toString();        assertEquals(expected, actual);    }    @Test    public void toFileTest() {        ToDo todo = new ToDo("buy bread");        todo.setDone(true);        String expected = "T D--buy bread";        String actual = todo.toFile();        assertEquals(expected, actual);    }}
+package yapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ToDoTest {
+    @Test
+    public void toStringTest() {
+        ToDo todo = new ToDo("buy bread");
+        todo.setDone(true);
+        String expected = "[T][X] buy bread";
+        String actual = todo.toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toFileTest() {
+        ToDo todo = new ToDo("buy bread");
+        todo.setDone(true);
+        String expected = "T D--buy bread";
+        String actual = todo.toFile();
+        assertEquals(expected, actual);
+    }
+}
