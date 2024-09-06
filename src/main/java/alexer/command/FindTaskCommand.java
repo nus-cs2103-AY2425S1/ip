7b44c69db5633a6d7cf8e2bac3f06f95fe566b85
sayomaki <sayomayomaki@gmail.com>
2024-09-06 14:24:07 +0800

    Add a new command to find tasks
    This command has the name "find" and takes a single argument keyword to search for the task.

---
+++ src/main/java/alexer/command/FindTaskCommand.java
@@ -0,0 +1,32 @@
+package alexer.command;
+
+import alexer.Alexer;
+import alexer.task.Task;
+import alexer.task.TaskManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindTaskCommand extends Command {
+    public FindTaskCommand() {
+        super("find");
+    }
+
+    @Override
+    public void run(String[] arguments) {
+        if (arguments.length == 0) {
+            return;
+        }
+
+        String keyword = arguments[0];
+        TaskManager taskManager = Alexer.getInstance().getTaskManager();
+        List<Task> tasks = taskManager.findTask(keyword);
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            output.append(String.format("\t%d: %s\n", i, tasks.get(i).toString()));
+        }
+
+        Alexer.getInstance().getPrompter().printFilteredTaskList(output.toString());
+    }
+}
