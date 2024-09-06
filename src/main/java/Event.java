a5c20ad1d90f486f6f6ad8e86fde3bb0212e9dce
hpcman <happypacman36@gmail.com>
2024-09-06 13:50:54 +0800

    Event class: Change time format to LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,20 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
 public class Event extends Task {
-    private final String from;
-    private final String to;
+    private final LocalDate from;
+    private final LocalDate to;
 
     Event(String task, String from, String to) {
         super(task);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     public String toFile() { return "E/" + super.toFile() + "/" + from + "/" + to; }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + "to: " + to + ")";
+        return "[E]" + super.toString() + "(from: " + from.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + " to: " + to.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
