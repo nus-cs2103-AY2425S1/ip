b3882c14e374c287c12c1b3963069db71099d67d
jktang14 <tangboy14@gmail.com>
2024-09-07 02:34:51 +0800

    Handle errors when loading hard disk

--- src/main/java/Azir/Azir.java
+++ src/main/java/Azir/Azir.java
@@ -20,12 +20,20 @@ public class Azir {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+    private String loadMessage = "";
 
     public Azir(String filePath) throws IOException {
         ui = new Ui();
         storage = new Storage(filePath);
         try {
             tasks = new TaskList(storage.load());
+            if (tasks.getSize() == 0) {
+                loadMessage = "Your current list does not have any tasks";
+            } else {
+                for (int i = 0; i < tasks.getSize(); i++) {
+                    loadMessage += tasks.getTask(i) + "\n";
+                }
+            }
         } catch (FileNotFoundException e) {
             File newFile = new File("./data/Azir.txt");
             if (!Files.exists(Paths.get("./data"))) {
@@ -33,117 +41,104 @@ public class Azir {
             }
             newFile.createNewFile();
             tasks = new TaskList();
-            System.out.println("Your current list does not have any tasks");
+            loadMessage = "Your current list does not have any tasks";
         } catch (IOException e) {
-            System.out.println("Something went wrong");
+            e.printStackTrace();
         }
+    }
+
+    public Azir() throws IOException {
+        this("./data/Azir.txt");
+    }
+
+    public String getLoadMessage() {
+        return loadMessage;
     }
 
     /**
-     * Runs the Azir chatbot
-     *
-     * @throws IOException If there are errors with the file
+     * Generates a response for the user's chat message.
      */
-    public void run() throws IOException {
-        ui.showWelcome();
-        boolean isExit = false;
-        Scanner obj = new Scanner(System.in);
-        while (!isExit && obj.hasNextLine()) {
-            try {
-                String fullCommand = ui.readCommand(obj);
-                ui.showLine();
-                String[] result = Parser.parse(fullCommand, tasks.getSize());
-                switch (result[0]) {
-                case "list":
-                    ui.showCommandEndMessage(fullCommand);
-                    for (int i = 0; i < tasks.getSize(); i++) {
-                        System.out.printf("%d. %s\n", i + 1, tasks.getTask(i));
-                    }
-                    break;
-                case "mark":
-                    Task chosenTask = tasks.getTask(Integer.valueOf(result[1]) - 1);
-                    chosenTask.setDone();
-                    ui.showCommandEndMessage(result[0], chosenTask.toString());
-                    break;
-                case "unmark":
-                    Task task = tasks.getTask(Integer.valueOf(result[1]) - 1);
-                    task.setNotDone();
-                    ui.showCommandEndMessage(result[0], task.toString());
-                    break;
-
-                case "delete":
-                    Task deletedTask = tasks.getTask(Integer.valueOf(result[1]) - 1);
-                    tasks.DeleteTask(Integer.valueOf(result[1]) - 1);
-                    ui.showCommandEndMessage(result[0], deletedTask.toString());
-                    ui.showTaskNumber(tasks.getSize());
-                    break;
-
-                case "todo":
-                    if (fullCommand.length() == 4) {
-                        throw new AzirException("todo cannot have an empty description. " +
-                                "Format: todo [description]");
-                    }
-                    Task todoTask = new Todo(result[1]);
-                    tasks.addTask(todoTask);
-                    ui.showCommandEndMessage(result[0], todoTask.toString());
-                    ui.showTaskNumber(tasks.getSize());
-                    break;
-
-                case "deadline":
-                    String dateFormat = "yyyy-MM-dd";
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
-                    try {
-                        Task deadlineTask = new Deadline(result[1], LocalDate.parse(result[2], formatter));
-                        tasks.addTask(deadlineTask);
-                        ui.showCommandEndMessage(result[0], deadlineTask.toString());
-                        ui.showTaskNumber(tasks.getSize());
-                    } catch (DateTimeParseException e) {
-                        throw new AzirException("deadline needs to be in the following format: yyyy-mm-dd");
-                    }
-                    break;
-
-                case "event":
-                    Task eventTask = new Event(result[1], result[2], result[3]);
-                    tasks.addTask(eventTask);
-                    ui.showCommandEndMessage(result[0], eventTask.toString());
-                    ui.showTaskNumber(tasks.getSize());
-                    break;
-
-                case "find":
-                    ui.showCommandEndMessage(result[0]);
-                    for (int i = 0; i < tasks.getSize(); i++) {
-                        String description = tasks.getTask(i).getDescription();
-                        String[] arr = description.split(" ");
-                        if (Arrays.stream(arr).anyMatch(str -> str.equals(result[1]))) {
-                            System.out.printf("%d. %s\n", i + 1, tasks.getTask(i));
-                        }
-                    }
-                    break;
-
-                case "bye":
-                    isExit = true;
-                    ui.showExit();
-                }
-            } catch (AzirException e) {
-                ui.showError(e.getMessage());
-            } finally {
-                ArrayList<String> lines = new ArrayList<>();
-                // Write tasks to Azir.txt
-                for (int i = 0; i < tasks.getSize(); i++) {
-                    lines.add(tasks.getTask(i).formatText());
-                }
-                storage.write("./data/Azir.txt", lines);
-                ui.showLine();
-            }
-        }
-    }
-
-    public static void main(String[] args) {
+    public String getResponse(String input) throws IOException {
+        String response = "";
         try {
-            new Azir("./data/Azir.txt").run();
-        } catch (IOException e) {
-            System.out.println("Something went wrong");
-        }
+            String[] result = Parser.parse(input, tasks.getSize());
+            switch (result[0]) {
+            case "list":
+                response += ui.showCommandEndMessage(input);
+                for (int i = 0; i < tasks.getSize(); i++) {
+                    response += String.format("%d. %s\n", i + 1, tasks.getTask(i));
+                }
+                break;
+            case "mark":
+                Task chosenTask = tasks.getTask(Integer.valueOf(result[1]) - 1);
+                chosenTask.setDone();
+                response += ui.showCommandEndMessage(result[0], chosenTask.toString());
+                break;
+            case "unmark":
+                Task task = tasks.getTask(Integer.valueOf(result[1]) - 1);
+                task.setNotDone();
+                response += ui.showCommandEndMessage(result[0], task.toString());
+                break;
 
+            case "delete":
+                Task deletedTask = tasks.getTask(Integer.valueOf(result[1]) - 1);
+                tasks.DeleteTask(Integer.valueOf(result[1]) - 1);
+                response += ui.showCommandEndMessage(result[0], deletedTask.toString());
+                response += ui.showTaskNumber(tasks.getSize());
+                break;
+
+            case "todo":
+                if (input.length() == 4) {
+                    throw new AzirException("todo cannot have an empty description. " +
+                            "Format: todo [description]");
+                }
+                Task todoTask = new Todo(result[1]);
+                tasks.addTask(todoTask);
+                response += ui.showCommandEndMessage(result[0], todoTask.toString());
+                response += ui.showTaskNumber(tasks.getSize());
+                break;
+
+            case "deadline":
+                String dateFormat = "yyyy-MM-dd";
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+                try {
+                    Task deadlineTask = new Deadline(result[1], LocalDate.parse(result[2], formatter));
+                    tasks.addTask(deadlineTask);
+                    response += ui.showCommandEndMessage(result[0], deadlineTask.toString());
+                    response += ui.showTaskNumber(tasks.getSize());
+                    break;
+                } catch (DateTimeParseException e) {
+                    throw new AzirException("deadline needs to be in the following format: yyyy-mm-dd");
+                }
+
+            case "event":
+                Task eventTask = new Event(result[1], result[2], result[3]);
+                tasks.addTask(eventTask);
+                response += ui.showCommandEndMessage(result[0], eventTask.toString());
+                response += ui.showTaskNumber(tasks.getSize());
+                break;
+
+            case "find":
+                response += ui.showCommandEndMessage(result[0]);
+                for (int i = 0; i < tasks.getSize(); i++) {
+                    String description = tasks.getTask(i).getDescription();
+                    String[] arr = description.split(" ");
+                    if (Arrays.stream(arr).anyMatch(str -> str.equals(result[1]))) {
+                        response += String.format("%d. %s\n", i + 1, tasks.getTask(i));
+                    }
+                }
+                break;
+            }
+        } catch (AzirException e) {
+            return ui.showError(e.getMessage());
+        } finally {
+            ArrayList<String> lines = new ArrayList<>();
+            // Write tasks to Azir.txt
+            for (int i = 0; i < tasks.getSize(); i++) {
+                lines.add(tasks.getTask(i).formatText());
+            }
+            storage.write("./data/Azir.txt", lines);
+        }
+        return response;
     }
 }
