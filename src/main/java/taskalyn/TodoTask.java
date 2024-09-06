6e7a7fcf43e3d4d8a780af3fe34bd994bb7d1ca1
psh12320 <pshricharan2020@gmail.com>
2024-09-06 10:39:06 +0800

    Update to getTaskDescription method in Task subclasses

--- src/main/java/taskalyn/TodoTask.java
+++ src/main/java/taskalyn/TodoTask.java
@@ -32,6 +32,6 @@ public class TodoTask extends Task {
      */
     @Override
     public String toDatabaseFormat() {
-        return "T | " + (this.isCompleted() ? "1" : "0") + " | " + this.getTaskItem();
+        return "T | " + (this.isCompleted() ? "1" : "0") + " | " + this.getTaskDescription();
     }
 }
