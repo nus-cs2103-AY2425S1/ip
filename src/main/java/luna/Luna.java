91a73c671af3fe512cb3fcd3a4e5c69d8b453fd8
gladwintan <gladwintan@hotmail.sg>
2024-09-07 00:29:01 +0800

    Remove text UI

--- src/main/java/luna/Luna.java
+++ src/main/java/luna/Luna.java
@@ -1,7 +1,6 @@
 package luna;
 
 import luna.command.Command;
-import luna.command.ExitCommand;
 
 /**
  * Represents a chatbot that allows users to add, remove and manipulate tasks.
@@ -10,19 +9,17 @@ public class Luna {
 
     private Storage storage;
     private TaskList tasks;
-    private Ui ui;
 
     /**
      * Creates a chatbot session
      */
     public Luna() {
         this.storage = new Storage();
-        this.ui = new Ui();
 
         try {
             this.tasks = new TaskList(storage.loadTasks());
         } catch (LunaException e) {
-            ui.showError(e.getMessage());
+            System.out.println(e.getMessage());
             tasks = new TaskList();
         }
     }
