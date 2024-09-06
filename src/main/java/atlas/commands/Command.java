4655b02899f9c40b887c027be325d84c06beffb2
Raghava <chinnu4148@gmail.com>
2024-09-06 12:44:47 +0800

    refactor: Remove unused code

--- src/main/java/atlas/commands/Command.java
+++ src/main/java/atlas/commands/Command.java
@@ -8,8 +8,6 @@ import atlas.tasks.TaskList;
  * Abstract command class that all other command classes inherit from.
  */
 public abstract class Command {
-    private boolean isExit = false;
-
     /**
      * Executes a set of steps according to the command.
      *
@@ -19,17 +17,4 @@ public abstract class Command {
      * @throws AtlasException The exception to be thrown in the event of any error.
      */
     public abstract String execute(TaskList tasks, Storage storage) throws AtlasException;
-
-    /**
-     * Shows the exit status of the chatbot.
-     *
-     * @return boolean The exit status of the chatbot.
-     */
-    public boolean isExit() {
-        return this.isExit;
-    }
-
-    public void setIsExit() {
-        this.isExit = true;
-    }
 }
