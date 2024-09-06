4c8106c31ba42616192703cf446aab9a46a97bb7
jevan <e1121960@u.nus.edu>
2024-09-06 23:09:05 +0800

    Change Line Separator to \n using mac2unix

--- src/main/java/yapper/Deadline.java
+++ src/main/java/yapper/Deadline.java
@@ -1,1 +1,44 @@
-package yapper;import java.time.LocalDateTime;import java.time.format.DateTimeFormatter;/** * Represents a Deadline Task, which is a task with a LocalDateTime byDateTime to finish by. */public class Deadline extends Task {    private final LocalDateTime byDateTime;    /**     * Creates an instance of Deadline.     *     * @param taskName   Name of the task.     * @param byDateTime Date and Time of ending time of Deadline.     */    public Deadline(String taskName, LocalDateTime byDateTime) {        super(taskName);        this.byDateTime = byDateTime;    }    /**     * Returns a string representation of the Deadline object.     *     * @return String representation of the Deadline object.     */    public String toString() {        String byDateTimeToString = this.byDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm"));        String doneSymbol = super.hasDone() ? "[X]" : "[ ]";        return String.format("[T]%s %s (BY: %s)", doneSymbol, super.getName(), byDateTimeToString);    }    /**     * Returns the string representation of the Deadline object to be recorded into a file.     * String to be decoded when loading history from the file.     *     * @return String representation of the Deadline object.     */    public String toFile() {        String fileDoneSymbol = super.hasDone() ? "D" : "N";        return String.format("D %s--%s--%s", fileDoneSymbol, super.getName(), this.byDateTime);    }}
+package yapper;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Represents a Deadline Task, which is a task with a LocalDateTime byDateTime to finish by.
+ */
+public class Deadline extends Task {
+    private final LocalDateTime byDateTime;
+
+    /**
+     * Creates an instance of Deadline.
+     *
+     * @param taskName   Name of the task.
+     * @param byDateTime Date and Time of ending time of Deadline.
+     */
+    public Deadline(String taskName, LocalDateTime byDateTime) {
+        super(taskName);
+        this.byDateTime = byDateTime;
+    }
+
+    /**
+     * Returns a string representation of the Deadline object.
+     *
+     * @return String representation of the Deadline object.
+     */
+    public String toString() {
+        String byDateTimeToString = this.byDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm"));
+        String doneSymbol = super.hasDone() ? "[X]" : "[ ]";
+        return String.format("[T]%s %s (BY: %s)", doneSymbol, super.getName(), byDateTimeToString);
+    }
+
+    /**
+     * Returns the string representation of the Deadline object to be recorded into a file.
+     * String to be decoded when loading history from the file.
+     *
+     * @return String representation of the Deadline object.
+     */
+    public String toFile() {
+        String fileDoneSymbol = super.hasDone() ? "D" : "N";
+        return String.format("D %s--%s--%s", fileDoneSymbol, super.getName(), this.byDateTime);
+    }
+}
--- src/main/java/yapper/gui/Launcher.java
+++ src/main/java/yapper/gui/Launcher.java
@@ -1,1 +1,12 @@
-package yapper.gui;import javafx.application.Application;/** * A launcher class to workaround classpath issues. */public class Launcher {    public static void main(String[] args) {        Application.launch(Main.class, args);    }}
+package yapper.gui;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
