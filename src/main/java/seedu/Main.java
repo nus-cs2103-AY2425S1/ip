1bc1fd41d9eacd7fe5cf7f8bf233efca73543602
slye20 <samuellim2011@gmail.com>
2024-09-06 20:34:46 +0800

    Add name to window

--- src/main/java/seedu/Main.java
+++ src/main/java/seedu/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bob Task Tracker");
             fxmlLoader.<MainWindow>getController().setBob(bob); // inject the Bob instance
             stage.show();
         } catch (IOException e) {
