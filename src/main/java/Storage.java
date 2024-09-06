cafb474360dbede125c1280909c76ef5a91ca41c
hpcman <happypacman36@gmail.com>
2024-09-06 17:43:49 +0800

    Add Storage.java

---
+++ src/main/java/Storage.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    private final String filePath;
+
+    private void createDirectory(String path) {
+        new java.io.File(path).mkdirs();
+    }
+
+    public Storage(String path) {
+        this.filePath = path;
+        createDirectory(this.filePath.substring(0, this.filePath.lastIndexOf('/')));
+    }
+
+    public ArrayList<Task> load() {
+        ArrayList<Task> list = new ArrayList<>();
+        try {
+            File file = new File(this.filePath);
+            file.createNewFile();
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                String type = data.split("/")[0];
+                boolean isTaskDone = data.split("/")[1].equals("1");
+                String task = data.split("/")[2];
+                switch (type) {
+                case "T" -> list.add(new Todo(task));
+                case "D" -> {
+                    String by = data.split("/")[3];
+                    list.add(new Deadline(task, by));
+                }
+                case "E" -> {
+                    String from = data.split("/")[3];
+                    String to = data.split("/")[4];
+                    list.add(new Event(task, from, to));
+                }
+                }
+                list.get(list.size() - 1).setMarkDone(isTaskDone);
+            }
+            scanner.close();
+        } catch (Exception e) {}
+        return list;
+    }
+
+    public void write(String list) {
+        try {
+            File file = new File(this.filePath);
+            FileWriter fw = new FileWriter(this.filePath);
+            file.delete();
+            file.createNewFile();
+            fw.write(list);
+            fw.close();
+        } catch (Exception e) {}
+    }
+}
