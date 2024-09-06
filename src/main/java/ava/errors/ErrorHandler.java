b662db60ba192117af0770148b9d941a7d8f7d93
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2024-09-06 15:55:28 +0800

    add varargs to errors

--- src/main/java/ava/errors/ErrorHandler.java
+++ src/main/java/ava/errors/ErrorHandler.java
@@ -6,7 +6,7 @@ public class ErrorHandler {
      * //TODO:log to disk or console
      * @param error Error recieved
      */
-    public static void handle(Error error) {
+    public static void handle(Error... errors) {
 
     }
 }
