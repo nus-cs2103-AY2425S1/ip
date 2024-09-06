ea67375f2dddd7801c099bbb56727a8fb0c12327
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-09-06 16:56:20 +0800

    Edit the switchcases to follow coding standard

--- src/main/java/Papagu/Ui/Parser.java
+++ src/main/java/Papagu/Ui/Parser.java
@@ -22,32 +22,32 @@ public class Parser {
      */
     private static int monthConverter(String month) {
         switch (month) {
-            case "Jan":
-                return 1;
-            case "Feb":
-                return 2;
-            case "Mar":
-                return 3;
-            case "Apr":
-                return 4;
-            case "May":
-                return 5;
-            case "Jun":
-                return 6;
-            case "Jul":
-                return 7;
-            case "Aug":
-                return 8;
-            case "Sep":
-                return 9;
-            case "Oct":
-                return 10;
-            case "Nov":
-                return 11;
-            case "Dec":
-                return 12;
-            default:
-                return 0;
+        case "Jan":
+            return 1;
+        case "Feb":
+            return 2;
+        case "Mar":
+            return 3;
+        case "Apr":
+            return 4;
+        case "May":
+            return 5;
+        case "Jun":
+            return 6;
+        case "Jul":
+            return 7;
+        case "Aug":
+            return 8;
+        case "Sep":
+            return 9;
+        case "Oct":
+            return 10;
+        case "Nov":
+            return 11;
+        case "Dec":
+            return 12;
+        default:
+            return 0;
         }
     }
 
