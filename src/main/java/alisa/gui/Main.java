4f50d40a05bfe52dccbbce0cefc567c898b10527
dez2003 <desiree2021325@gmail.com>
2024-09-07 15:21:20 +0800

    Add GUI to chatbot with JavaFX

---
+++ src/main/java/alisa/gui/Main.java
@@ -0,0 +1,33 @@
+package alisa.gui;
+
+import java.io.IOException;
+
+import alisa.Alisa;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Alisa alisa = new Alisa("./data/alisa.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(alisa);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
