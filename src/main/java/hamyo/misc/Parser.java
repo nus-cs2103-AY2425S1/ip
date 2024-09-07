2bbf32135ff1266835f52a081793dbea47971def
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-09-08 00:07:53 +0800

    Implement CodeQuality for Misc
    Made use of Extract Method to extract methods from Parser.parse into smaller helper functions.
    
    Made use of Extract Method to extract method from constructor and loadData into smaller helper functions.

--- src/main/java/hamyo/misc/Parser.java
+++ src/main/java/hamyo/misc/Parser.java
@@ -24,92 +24,35 @@ public class Parser {
         String commandFields = fullCommand.substring(commandType.length());
         try {
             switch (commandType) {
-
             case "todo":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: todo [task description]");
-                }
-                tasks.addTask(TaskType.TODO, commandFields.substring(1));
+                parseToDo(commandFields, tasks);
                 break;
-
             case "deadline":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: deadline [task description] /by [deadline]");
-                }
-                String[] split = commandFields.substring(1).split(" /by ");
-                if (split.length != 2) {
-                    throw new HamyoException("Usage: deadline [task description] /by [deadline]");
-                }
-                tasks.addTask(TaskType.DEADLINE, split[0], split[1]);
+                parseDeadline(commandFields, tasks);
                 break;
-
             case "event":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException(
-                        "Usage: event [task description] /from [start timestamp] /to [end timestamp]");
-                }
-                split = commandFields.substring(1).split(" /from | /to ");
-                if (split.length != 3) {
-                    throw new HamyoException(
-                        "Usage: event [task description] /from [start timestamp] /to [end timestamp]");
-                }
-                tasks.addTask(TaskType.EVENT, split[0], split[1], split[2]);
+                parseEvent(commandFields, tasks);
                 break;
-
             case "list":
                 tasks.listTasks();
                 break;
-
             case "listDate":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: listDate yyyy-MM-dd.");
-                }
-                tasks.listTasksByDate(commandFields.substring(1));
+                parseListDate(commandFields, tasks);
                 break;
-
             case "find":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: find [index]");
-                }
-                tasks.listTasksByKeyword(commandFields.substring(1));
+                parseFind(commandFields, tasks);
                 break;
-
             case "mark":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: mark [index]");
-                }
-                try {
-                    tasks.markTask(Integer.parseInt(commandFields.substring(1)) - 1);
-                } catch (NumberFormatException e) {
-                    throw new HamyoException("Usage: mark [index]");
-                }
+                parseMark(commandFields, tasks);
                 break;
-
             case "unmark":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: unmark [index]");
-                }
-                try {
-                    tasks.unmarkTask(Integer.parseInt(commandFields.substring(1)) - 1);
-                } catch (NumberFormatException e) {
-                    throw new HamyoException("Usage: unmark [index]");
-                }
+                parseUnmark(commandFields, tasks);
                 break;
-
             case "delete":
-                if (commandFields.length() <= 1) {
-                    throw new HamyoException("Usage: delete [index]");
-                }
-                try {
-                    tasks.deleteTask(Integer.parseInt(commandFields.substring(1)) - 1);
-                } catch (NumberFormatException e) {
-                    throw new HamyoException("Usage: delete [index]");
-                }
+                parseDelete(commandFields, tasks);
                 break;
-
             case "bye":
                 return false;
-
             default:
                 throw new HamyoException("Invalid Command!");
             }
@@ -117,5 +60,155 @@ public class Parser {
             Ui.printException(e);
         }
         return true;
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * adds the specified To-Do Task to the tasks TaskList. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The fields input by user required to create a To-Do.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseToDo(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: todo [task description]");
+        }
+        tasks.addTask(TaskType.TODO, commandFields.substring(1));
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * adds the specified Deadline Task to the tasks TaskList. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The fields input by user required to create a Deadline
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseDeadline(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: deadline [task description] /by [deadline]");
+        }
+        String[] split = commandFields.substring(1).split(" /by ");
+        if (split.length != 2) {
+            throw new HamyoException("Usage: deadline [task description] /by [deadline]");
+        }
+        tasks.addTask(TaskType.DEADLINE, split[0], split[1]);
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * adds the specified Event Task to the tasks TaskList. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The fields input by user required to create a Event.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseEvent(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException(
+                "Usage: event [task description] /from [start timestamp] /to [end timestamp]");
+        }
+        String[] split = commandFields.substring(1).split(" /from | /to ");
+        if (split.length != 3) {
+            throw new HamyoException(
+                "Usage: event [task description] /from [start timestamp] /to [end timestamp]");
+        }
+        tasks.addTask(TaskType.EVENT, split[0], split[1], split[2]);
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * executes the command to list out tasks by the given date. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The given date.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseListDate(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: listDate yyyy-MM-dd.");
+        }
+        tasks.listTasksByDate(commandFields.substring(1));
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * executes the command to list out tasks by the given keyword. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The given keyword.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseFind(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: find [index]");
+        }
+        tasks.listTasksByKeyword(commandFields.substring(1));
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * executes the command to mark tasks of the specified index. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The specified index.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseMark(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: mark [index]");
+        }
+        try {
+            tasks.markTask(Integer.parseInt(commandFields.substring(1)) - 1);
+        } catch (NumberFormatException e) {
+            throw new HamyoException("Usage: mark [index]");
+        }
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * executes the command to unmark tasks of the specified index. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The specified index.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseUnmark(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: unmark [index]");
+        }
+        try {
+            tasks.unmarkTask(Integer.parseInt(commandFields.substring(1)) - 1);
+        } catch (NumberFormatException e) {
+            throw new HamyoException("Usage: unmark [index]");
+        }
+    }
+
+    /**
+     * Parse method takes in command fields in user input from terminal and
+     * executes the command to delete tasks of the specified index. If command fields
+     * are invalid, a HamyoException is thrown.
+     *
+     * @param commandFields The specified index.
+     * @param tasks The list of the users' tasks.
+     * @throws HamyoException Thrown when command fields are invalid.
+     */
+    private static void parseDelete(String commandFields, TaskList tasks) throws HamyoException {
+        if (commandFields.length() <= 1) {
+            throw new HamyoException("Usage: delete [index]");
+        }
+        try {
+            tasks.deleteTask(Integer.parseInt(commandFields.substring(1)) - 1);
+        } catch (NumberFormatException e) {
+            throw new HamyoException("Usage: delete [index]");
+        }
     }
 }
