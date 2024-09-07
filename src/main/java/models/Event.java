c8942077212ccc9192a98e467ed250ca4706d513
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-09-07 15:43:33 +0000

    feat: Add docs for models

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,38 +1,94 @@
 package models;
 
+/**
+ * Represents an event task in the task management system.
+ * An event task has a description, a start time, and an end time.
+ * It also has a completion status indicating whether the task is completed.
+ *
+ * <p>This class extends the {@code Task} class and adds specific functionality
+ * for handling time-bound tasks.</p>
+ */
 public class Event extends Task {
 
     private String from;
     private String to;
 
+    /**
+     * Constructs an {@code Event} with the specified description.
+     * The task is initialized as not completed.
+     *
+     * @param description The description of the event.
+     */
     public Event(String description) {
         super(description);
     }
 
+    /**
+     * Constructs an {@code Event} with the specified description, start time, and end time.
+     * The task is initialized as not completed.
+     *
+     * @param description The description of the event.
+     * @param from The start time of the event.
+     * @param to The end time of the event.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Constructs an {@code Event} with the specified description, completion status, start time, and end time.
+     *
+     * @param description The description of the event.
+     * @param isDone The completion status of the event; {@code true} if the event is completed, {@code false} otherwise.
+     * @param from The start time of the event.
+     * @param to The end time of the event.
+     */
     public Event(String description, boolean isDone, String from, String to) {
         super(description, isDone);
         this.from = from;
         this.to = to;
     }
 
-
-    private String getFrom() {return this.from;}
-    private String getTo() {return this.to;}
-
-    public String serialize() {
-        return String.format("E|%s|%s|%s|%s", this.getIsDone() ? "1" : "0", this.getDescription(),
-        this.getFrom(), this.getTo()  );
+    /**
+     * Returns the start time of the event.
+     *
+     * @return The start time of the event.
+     */
+    private String getFrom() {
+        return this.from;
     }
 
+    /**
+     * Returns the end time of the event.
+     *
+     * @return The end time of the event.
+     */
+    private String getTo() {
+        return this.to;
+    }
+
+    /**
+     * Serializes the event into a string format for saving in the database.
+     * The format is "E|<isDone>|<description>|<from>|<to>", where {@code isDone} is 1 if the event is completed, and 0 otherwise.
+     *
+     * @return A serialized string representation of the event.
+     */
+    public String serialize() {
+        return String.format("E|%s|%s|%s|%s", this.getIsDone() ? "1" : "0", this.getDescription(),
+                this.getFrom(), this.getTo());
+    }
+
+    /**
+     * Returns the string representation of the event.
+     * The format is "[E][<statusIcon>] <description> (from: <from> to: <to>)", where {@code statusIcon} is "X" if the event is completed, or a space otherwise.
+     *
+     * @return A string representing the event for display.
+     */
     @Override
     public String toString() {
-        return String.format("[E][%s] %s(from: %sto: %s)", this.getStatusIcon(), this.getDescription(),
+        return String.format("[E][%s] %s (from: %s to: %s)", this.getStatusIcon(), this.getDescription(),
                 this.getFrom(), this.getTo());
     }
 }
--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -1,42 +1,97 @@
 package models;
 
+/**
+ * Represents a task in the task management system.
+ * The {@code Task} class serves as a base class for various types of tasks,
+ * such as {@code Todo}, {@code Event}, and {@code Deadline}.
+ * Each task has a description and a status indicating whether it is completed.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a {@code Task} with the specified description.
+     * The task is initialized as not completed.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Constructs a {@code Task} with the specified description and completion status.
+     *
+     * @param description The description of the task.
+     * @param isDone The completion status of the task; {@code true} if the task is completed, {@code false} otherwise.
+     */
     public Task(String description, boolean isDone) {
         this.description = description;
         this.isDone = isDone;
     }
 
+    /**
+     * Returns whether the task is completed.
+     *
+     * @return {@code true} if the task is completed, {@code false} otherwise.
+     */
     public boolean getIsDone() {
         return this.isDone;
     }
 
+    /**
+     * Returns the status icon representing whether the task is completed.
+     * If the task is completed, returns "X", otherwise returns a space.
+     *
+     * @return A string representing the status of the task ("X" for done, " " for not done).
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " ");
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The description of the task.
+     */
     public String getDescription() {
         return this.description;
     }
 
-    public String toString() {
-        return String.format("[%s] %s", this.getStatusIcon(), this.getDescription());
-    }
-
+    /**
+     * Marks the task as completed.
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks the task, setting it as not completed.
+     */
     public void unmark() {
         this.isDone = false;
     }
 
-    public String serialize() {return "";}
+    /**
+     * Serializes the task into a string format for saving in the database.
+     * The default implementation returns an empty string, meant to be overridden by subclasses.
+     *
+     * @return A serialized string representation of the task.
+     */
+    public String serialize() {
+        return "";
+    }
+
+    /**
+     * Returns a string representation of the task.
+     * The format is "[<statusIcon>] <description>", where {@code statusIcon} is "X" if the task is completed or a space otherwise.
+     *
+     * @return A string representing the task for display.
+     */
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.getDescription());
+    }
 }
