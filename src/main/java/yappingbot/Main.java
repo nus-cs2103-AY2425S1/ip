0b2e37c52e01c5482d41ef72d16b631e6a6c8ce2
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-09-07 15:34:15 +0000

    Add basic javafx test

--- src/main/java/yappingbot/Main.java
+++ src/main/java/yappingbot/Main.java
@@ -1,6 +1,8 @@
 package yappingbot;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -9,6 +11,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        Label helloWorld = new Label("asdad");
+        Scene scene = new Scene(helloWorld);
+
+        stage.setScene(scene);
         stage.show();
     }
 }
