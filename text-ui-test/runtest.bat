53a09e53d547cfa1a5e0272df09d652538de2984
Otto Tan <OttoTan>
2024-09-07 21:26:07 +0800

    Remove unnecessary I/O redirection tests as they are not applicable to this project

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,21 +1,21 @@
 @ECHO OFF
 
 REM create bin directory if it doesn't exist
-REM if not exist ..\bin mkdir ..\bin
+if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
-REM del ACTUAL.TXT
+del ACTUAL.TXT
 
 REM compile the code into the bin folder
-REM javac -encoding ISO-8859-1 -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
-REM IF ERRORLEVEL 1 (
-REM    echo ********** BUILD FAILURE **********
-REM    exit /b 1
-REM)
+javac -encoding ISO-8859-1 -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+IF ERRORLEVEL 1 (
+    echo ********** BUILD FAILURE **********
+    exit /b 1
+)
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-REM java -classpath ..\bin Megamind < input.txt > ACTUAL.TXT
+java -classpath ..\bin Megamind < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-REM FC ACTUAL.TXT EXPECTED.TXT
+FC ACTUAL.TXT EXPECTED.TXT
