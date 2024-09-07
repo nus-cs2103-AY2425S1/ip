638aae62664e66441cd0d0d9fd976ae3638cd589
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-09-07 15:28:52 +0000

    Fix nix-os library paths for javafx

---
+++ src/main/java/yappingbot/Main.java
@@ -0,0 +1,14 @@
+package yappingbot;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class to launch GUI Application.
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
