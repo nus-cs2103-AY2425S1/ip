8188dd29d6e9cb05719fce068ab812fcbe423d84
keerthigkaarthik <keerthigkaarthik@gmail.com>
2024-09-08 02:10:43 +0800

    Add remaining files

---
+++ src/main/java/task/Deadline.java
@@ -0,0 +1,76 @@
+package task;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+/**
+ * Represents a task with a specific deadline.
+ */
+public class Deadline extends Task {
+    public final LocalDateTime deadline;
+
+    /**
+     * Constructs a Deadline task with a description and a deadline.
+     *
+     * @param description The description of the deadline task.
+     * @param deadlineStr The deadline date and time in a string format.
+     * @throws DateTimeParseException If the deadline string cannot be parsed.
+     */
+    public Deadline(String description, String deadlineStr) throws DateTimeParseException {
+        super(description);
+        try {
+            this.deadline = LocalDateTime.parse(deadlineStr, Task.toSelfFormatter);
+        } catch (DateTimeParseException exception) {
+            TaskList.mainTaskList.deleteTask(TaskList.mainTaskList.getNumTasks() - 1);
+            System.out.println(exception.getMessage());
+            throw exception;
+        }
+        this.type = "[D]";
+    }
+
+    /**
+     * Loads a Deadline task from a formatted string array obtained from splicing the save file
+     * during Storage's load method.
+     *
+     * @param properties The string array containing the task properties.
+     */
+    public static void load(String[] properties) {
+        try {
+            Deadline newDeadline = new Deadline(properties[2], properties[3]);
+            if (Objects.equals(properties[1], "1")) {
+                newDeadline.markAsDone();
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Failed to parse date for loaded deadline: " + e.getMessage());
+        }
+    }
+
+    private String getDeadline(String type) {
+        if (type.equals("in")) {
+            return this.deadline.format(Task.toSelfFormatter);
+        } else if (type.equals("out")) {
+            return this.deadline.format(Task.toUserFormatter);
+        } else {
+            return "Incorrect deadline format type specified.";
+        }
+    }
+
+    /**
+     * Returns a string representation of the deadline task formatted for saving to a file.
+     * The format includes the task type, completion status, task description, and deadline, separated by " | ".
+     * This string is used for persisting the task data in a structured text file, allowing for easy reloading of tasks.
+     *
+     * @return The formatted string for saving the deadline task to file.
+     */
+    @Override
+    public String saveFileFormat() {
+        String status = this.isCompleted() ? "1 | " : "0 | ";
+        return "D | " + status + this.getDescription() + " | " + getDeadline("in");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (By: " + getDeadline("out") + ")";
+    }
+}
---
+++ src/main/java/task/TaskList.java
@@ -0,0 +1,126 @@
+package task;
+
+import java.util.ArrayList;
+
+/**
+ * Manages a list of tasks, providing operations to manipulate and query tasks.
+ */
+public class TaskList {
+
+    public static final TaskList mainTaskList = new TaskList();
+    private final ArrayList<Task> tasks;
+
+    /**
+     * Initializes a new empty list of tasks.
+     */
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    /**
+     * Adds a task to the task list.
+     * @param task The task to add.
+     */
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    /**
+     * Returns the number of tasks in the list.
+     * @return The number of tasks.
+     */
+    public int getNumTasks() {
+        return this.tasks.size();
+    }
+
+    /**
+     * Prints all tasks in the list with their indices.
+     */
+    public String printList() {
+        String msg = "";
+        int index = 1;
+        System.out.println();
+        for (Task task : this.tasks) {
+            msg += String.format("     %d. %s\n", index, task);
+            index++;
+        }
+        msg += "\n";
+        return msg;
+    }
+
+    /**
+     * Finds and returns a new TaskList containing all tasks that include the specified term.
+     * @param term The term to search for within task descriptions.
+     * @return A TaskList of all tasks containing the term.
+     */
+    public TaskList tasksContainingTerm(String term) {
+        TaskList result = new TaskList();
+        for (Task task : this.tasks) {
+            if (task.containsTerm(term)) {
+                result.addTask(task);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Marks a task as done.
+     * @param index The index of the task to mark as done.
+     */
+    public void markTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            this.tasks.get(index).markAsDone();
+        }
+    }
+
+    /**
+     * Marks a task as not done.
+     * @param index The index of the task to mark as not done.
+     */
+    public void unmarkTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            this.tasks.get(index).markAsUndone();
+        }
+    }
+
+    /**
+     * Deletes a task from the list.
+     * @param index The index of the task to be deleted.
+     */
+    public void deleteTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            this.tasks.remove(index);
+        }
+    }
+
+    /**
+     * Returns the formatted save string for a task.
+     * @param index The index of the task.
+     * @return The formatted save string.
+     */
+    public String getTaskFileFormat(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            return tasks.get(index).saveFileFormat();
+        }
+        return null;
+    }
+
+    /**
+     * Retrieves a task based on its index.
+     * @param index The index of the task.
+     * @return The task at the specified index.
+     */
+    public Task getTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            return this.tasks.get(index);
+        }
+        return null;
+    }
+
+    /**
+     * Clears all tasks from the list.
+     */
+    public void clearTasks() {
+        tasks.clear();
+    }
+}
