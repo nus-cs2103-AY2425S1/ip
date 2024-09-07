23b6dbdb8468e0eced27da9e90e91db6bff75e11
Tanziyanghenry <e0725822@u.nus.edu>
2024-09-08 01:21:40 +0800

    Avoid deep nesting by removing unnecessary else statements.
    Deep nesting can make the code harder to read and understand. By
    eliminating unnecessary else blocks after return statements, the code
    becomes cleaner and more straightforward.

--- src/main/java/henry/util/Parser.java
+++ src/main/java/henry/util/Parser.java
@@ -27,18 +27,18 @@ public class Parser {
             return new ExitCommand();
         } else if (input.equals("list")) {
             return new PrintCommand();
+        }
+
+        String[] words = input.split(" ");
+        String command = words[0].toLowerCase();
+        if (command.equals("mark") || command.equals("unmark")) {
+            return new ChangeStatusCommand(words);
+        } else if (command.equals("delete")) {
+            return new DeleteCommand(words[1]);
+        } else if (command.equals("find")) {
+            return new FindCommand(words[1]);
         } else {
-            String[] words = input.split(" ");
-            String command = words[0].toLowerCase();
-            if (command.equals("mark") || command.equals("unmark")) {
-                return new ChangeStatusCommand(words);
-            } else if (command.equals("delete")) {
-                return new DeleteCommand(words[1]);
-            } else if (command.equals("find")) {
-                return new FindCommand(words[1]);
-            } else {
-                return new AddCommand(input);
-            }
+            return new AddCommand(input);
         }
     }
 }
