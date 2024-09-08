13ff29497b9a9e844de4449ec1a8852249a6bf40
rayray39 <e0772883@u.nus.edu>
2024-09-08 23:02:17 +0800

    Refactor code by abstracting complicated boolean expressions
    Too many boolean expressions to check for validity of user command
    
    Many boolean expressions are being used in different areas of Parser.java to check for validity of user commands and this makes it hard to track and read.
    
    Let's abstract the boolean expressions into methods to make them more readable and package it into a single method that can be easily called

--- src/main/java/janet/Parser.java
+++ src/main/java/janet/Parser.java
@@ -51,38 +51,34 @@ public class Parser {
      * @throws JanetException If user input is invalid.
      */
     public static void userInputChecker(String[] commandDetails, int numOfTasksInList) throws JanetException {
-        // validateCommand(commandDetails, numOfTasksInList);
         checkInaccurateCommand(commandDetails, numOfTasksInList);
     }
 
 
     /**
      * Throws janet.JanetException when,
-     * 1. mark/unmark/delete X, where X cannot be parsed into an Integer.
-     * 2. mark/unmark/delete X, where X can be parsed into an Integer but, is <= 0 or > number of tasks in list.
+     * 1. command is empty (eg. ' ') OR command is an enum CommandType value.
+     * 2. task description is not provided.
+     * 3. task number is not in proper format, not provided, is negative or equal to 0.
+     * 4. task number is larger than the number of tasks in the list.
      *
      * @param commandDetails A String[], where each element corresponds to a word of the user input.
-     * @throws JanetException If specified task number is out of bounds/unparsable to Integer.
+     * @throws JanetException If command is invalid, task number is not properly specified, task description omitted.
      */
-    public static void validateCommand(String[] commandDetails, int numOfTasksInList) throws JanetException {
-        // when mark/unmark/delete X, where X is too big (out or bounds) OR <= 0 OR when the list is empty.
-        if (commandDetails.length == 0) {
-            throw new JanetException("Please do not leave any empty lines!");
-        }
-        if ((commandDetails[0].equals("mark") || commandDetails[0].equals("unmark") ||
-                commandDetails[0].equals("delete")) && commandDetails.length > 1) {
-            // when the command is mark/unmark X OR delete, where X is an invalid num (too big or <= 0)
-            int taskNumber;
-            assert commandDetails.length == 2;
-            try {
-                taskNumber = Integer.parseInt(commandDetails[1]);   // commandDetails[1] could be a string
-            } catch (NumberFormatException e) {
-                // for eg. mark/unmark/delete SOMETHING, where SOMETHING cannot be parsed into an Integer
-                throw new JanetException("WHOOPS! Please provide an integer value task number!");
-            }
-            if (taskNumber <= 0) {
-                throw new JanetException("WHOOPS! Your task number cannot be negative or 0!");
-            } else if (taskNumber > numOfTasksInList) {
+    public static void checkInaccurateCommand(String[] commandDetails, int numOfTasksInlist) throws JanetException {
+        if (emptyCommand(commandDetails) || unknownCommand(commandDetails)) {
+            // when the command is gibberish OR is an empty command
+            throw new JanetException("WHOOPS! I'm only a chatbot, so I don't know what that means...");
+        } else if (notByeOrListCommand(commandDetails)) {
+            if (taskDescriptionOmitted(commandDetails)) {
+                // task description is not provided
+                throw new JanetException("WHOOPS! You can't leave out the task's description!");
+            } else if (taskNumberInvalidOrAbsent(commandDetails) || taskNumberNegativeOrZero(commandDetails)) {
+                // for marking (mark/unmark) and delete commands,
+                // task number is either invalid, absent, negative, or less than or equal to 0
+                throw new JanetException("WHOOPS! Ensure task number is present and valid!");
+            } else if (taskNumberOutOfRange(commandDetails, numOfTasksInlist)) {
+                // for marking (mark/unmark) and delete commands, task number is larger than number of tasks in list
                 throw new JanetException("WHOOPS! You don't have a task of this number!");
             }
         }
@@ -90,92 +86,131 @@ public class Parser {
 
 
     /**
-     * Throws janet.JanetException when,
-     * 1. first word in command is not todo/event/deadline/mark/unmark/delete.
-     * 2. mark/unmark/delete and the task number is not specified.
-     * 3. todo/event/deadline and the description is not stated.
+     * Returns true if the command is empty, just whitespaces (eg. '    ').
      *
      * @param commandDetails A String[], where each element corresponds to a word of the user input.
-     * @throws JanetException If task number is not specified, task description is omitted, or unknown command.
+     * @return A boolean value
      */
-    public static void checkInaccurateCommand(String[] commandDetails, int numOfTasksInlist) throws JanetException {
-        // checks for inaccurate commands 1. rubbish, 2. without any task description, 3. no number for mark/unmark/delete.
-        if (unknownCommand(commandDetails)) {
-            // when the command is gibberish and NOT one of the commands in CommandType
-            throw new JanetException("WHOOPS! I'm only a chatbot, so I don't know what that means...");
-        } else if (notByeOrListCommand(commandDetails)) {
-            if (taskNumberInvalidOrAbsent(commandDetails) || taskNumberNegativeOrZero(commandDetails)) {
-                // when the command is either (mark, unmark, delete), BUT task num not specified or invalid format
-                throw new JanetException("WHOOPS! Ensure task number is present and valid!");
-            } else if (taskNumberOutOfRange(commandDetails, numOfTasksInlist)) {
-                throw new JanetException("WHOOPS! You don't have a task of this number!");
-            } else if (taskDescriptionOmitted(commandDetails)) {
-                // when the command is either (todo, deadline, todo), BUT there is no task description
-                throw new JanetException("WHOOPS! You can't leave out the task's description!");
-            }
-        }
-    }
-
-
-    public static boolean emptyCommand(String[] commandDetails) {
+    private static boolean emptyCommand(String[] commandDetails) {
         return commandDetails.length == 0;
     }
 
-    public static boolean taskDescriptionOmitted(String[] commandDetails) {
-        return false;
+
+    private static boolean taskDescriptionOmitted(String[] commandDetails) {
+        boolean noDescriptionProvided = commandDetails.length == 1;
+        return noDescriptionProvided && isTaskCommand(commandDetails);
     }
 
 
     /**
      * Returns true if command is,
      * 1. gibberish and not an enum CommandType value (eg. marker, blah blah, events).
-     * 2. 'bye ...' where '...' represents additional texts behind the word bye.
-     * 3. 'list ...' where '...' represents additional texts behind the word list.
+     * 2. 'bye ...' where '...' represents additional texts behind the word bye (eg. bye bye).
+     * 3. 'list ...' where '...' represents additional texts behind the word list (eg. list something).
      * false otherwise.
      *
      * @param commandDetails A String[], where each element corresponds to a word of the user input.
      * @return A boolean value.
      */
-    public static boolean unknownCommand(String[] commandDetails) {
+    private static boolean unknownCommand(String[] commandDetails) {
         boolean notACommand = !(getCommandTypes().contains(commandDetails[0].toUpperCase()));
         boolean byeWithAdditionalTexts = (commandDetails.length > 1) && (commandDetails[0].equals("bye"));
         boolean listWithAdditionalTexts = (commandDetails.length > 1) && (commandDetails[0].equals("list"));
-
         return notACommand || byeWithAdditionalTexts || listWithAdditionalTexts;
     }
 
 
-    public static boolean taskNumberInvalidOrAbsent(String[] commandDetails) throws JanetException {
+    /**
+     * Returns true if,
+     * 1. task number is not provided (eg. mark).
+     * 2. task number is invalid, unable to parse into Integer (eg. unmark three).
+     * 3. more than 1 task number is provided, separated by whitespace(s) (eg. delete 3 2).
+     *
+     * @param commandDetails A String[], where each element corresponds to a word of the user input.
+     * @return A boolean value.
+     */
+    private static boolean taskNumberInvalidOrAbsent(String[] commandDetails) {
         boolean taskNumberAbsent = (commandDetails.length == 1);
         boolean taskNumberInvalid = false;
-        int taskNumber;
-        if (commandDetails.length == 2) {
+        if (commandDetails.length == 2 && isMarkingOrDeleteCommand(commandDetails)) {
             // task number is present, check for validity
             try {
-                taskNumber = Integer.parseInt(commandDetails[1]);
+                Integer.parseInt(commandDetails[1]);
             } catch (NumberFormatException e) {
-                // throw new JanetException("WHOOPS! Please provide an integer value task number!");
                 taskNumberInvalid = true;
             }
-        } else if (commandDetails.length > 2) {
+        } else if (commandDetails.length > 2  && isMarkingOrDeleteCommand(commandDetails)) {
             // more than 1 task number is provided, separated by white space(s).
             taskNumberInvalid = true;
         }
-        return taskNumberAbsent || taskNumberInvalid;
+        return taskNumberAbsent || taskNumberInvalid;  // false
     }
 
 
-    public static boolean notByeOrListCommand(String[] commandDetails) {
+    /**
+     * Returns true if the command is neither 'bye' nor 'list'.
+     *
+     * @param commandDetails A String[], where each element corresponds to a word of the user input.
+     * @return A boolean value.
+     */
+    private static boolean notByeOrListCommand(String[] commandDetails) {
+        // returns true if the command is not 'bye' and 'list', false otherwise.
         return !(commandDetails[0].equals("bye") || commandDetails[0].equals("list"));
     }
 
 
-    public static boolean taskNumberOutOfRange(String[] commandDetails, int numOfTasksInList) {
-        return Integer.parseInt(commandDetails[1]) > numOfTasksInList;
+    /**
+     * Returns true if the task number provided is greater than the number of tasks in list,
+     * false otherwise.
+     *
+     * @param commandDetails A String[], where each element corresponds to a word of the user input.
+     * @param numOfTasksInList The current total number of tasks in the list.
+     * @return A boolean value.
+     */
+    private static boolean taskNumberOutOfRange(String[] commandDetails, int numOfTasksInList) {
+        // returns true if the task number provided is greater than the number of tasks in list, false otherwise
+        return isMarkingOrDeleteCommand(commandDetails)
+                && Integer.parseInt(commandDetails[1]) > numOfTasksInList;
     }
 
 
-    public static boolean taskNumberNegativeOrZero(String[] commandDetails) {
-        return Integer.parseInt(commandDetails[1]) <= 0;
+    /**
+     * Returns true if the task number provided is less than or equal to 0,
+     * false otherwise.
+     *
+     * @param commandDetails A String[], where each element corresponds to a word of the user input.
+     * @return A boolean value.
+     */
+    private static boolean taskNumberNegativeOrZero(String[] commandDetails) {
+        // returns true if the task number provided is less than or equal to 0, false otherwise.
+        return isMarkingOrDeleteCommand(commandDetails) && Integer.parseInt(commandDetails[1]) <= 0;
+    }
+
+
+    /**
+     * Returns true if command is mark, unmark or delete,
+     * false otherwise.
+     *
+     * @param commandDetails A String[], where each element corresponds to a word of the user input.
+     * @return A boolean value.
+     */
+    private static boolean isMarkingOrDeleteCommand(String[] commandDetails) {
+        return commandDetails[0].equals("mark")
+                || commandDetails[0].equals("unmark")
+                || commandDetails[0].equals("delete");
+    }
+
+
+    /**
+     * Returns true if command is todo, deadline or event,
+     * false otherwise.
+     *
+     * @param commandDetails A String[], where each element corresponds to a word of the user input.
+     * @return A boolean value.
+     */
+    private static boolean isTaskCommand(String[] commandDetails) {
+        return commandDetails[0].equals("todo")
+                || commandDetails[0].equals("deadline")
+                || commandDetails[0].equals("event");
     }
 }
