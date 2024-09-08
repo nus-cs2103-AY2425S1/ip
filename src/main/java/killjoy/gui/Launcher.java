2e75764a37ae29c7b27eca4bf2a0111eb76b126a
thisisaditya17 <joshi.adi1734@gmail.com>
2024-09-09 02:13:29 +0800

    add resources for JavaFx

--- src/main/java/killjoy/gui/Launcher.java
+++ src/main/java/killjoy/gui/Launcher.java
@@ -1,2 +1,12 @@
-package killjoy.gui;public class Launcher {
+package killjoy.gui;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
 }
--- src/main/java/killjoy/gui/Main.java
+++ src/main/java/killjoy/gui/Main.java
@@ -1,2 +1,36 @@
-package killjoy.gui;public class Main {
+package killjoy.gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import killjoy.main.KillJoy;
+
+public class Main extends Application {
+
+    private KillJoy kj = new KillJoy();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("../../view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+
+            fxmlLoader.<MainWindow>getController().setKj(kj);
+            kj.loadTasks();
+            stage.show();
+            MainWindow mw = fxmlLoader.getController();
+            mw.welcomeMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
