565707d9d61e6e35004def867126c3cbf47a0a84
jeanyang04 <jeanyang.chen@gmail.com>
2024-09-08 17:20:21 +0800

    Add javafx

--- src/main/java/skibidi/Skibidi.java
+++ src/main/java/skibidi/Skibidi.java
@@ -56,4 +56,11 @@ public class Skibidi {
     public static void main(String[] args) {
         new Skibidi().run();
     }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
 }
