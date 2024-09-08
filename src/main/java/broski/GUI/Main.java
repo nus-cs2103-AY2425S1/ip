b45bb0e39a20bc47599ad97134711cd8b8a9ad46
AlphaPyke <jng@Joshuas-MacBook-Pro-2.local>
2024-09-08 20:56:58 +0800

    Make minor changes to remove unnecessary lines

--- src/main/java/broski/GUI/Main.java
+++ src/main/java/broski/GUI/Main.java
@@ -1,20 +1,12 @@
 package broski.GUI;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 
 import broski.output.Broski;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
