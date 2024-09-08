832aa1601e816a2b5991c118484c5b88cf415221
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-09-08 22:43:33 +0800

    Copy code Update dialog box background and text colors for improved UI design
    * Change bot dialog box color to white.
    *Adjusted text color for better contrast and readability.

--- src/main/java/justbot/MainWindow.java
+++ src/main/java/justbot/MainWindow.java
@@ -43,6 +43,7 @@ public class MainWindow extends AnchorPane {
     private void handleUserInput() {
         String input = userInput.getText();
         String response = justbot.getResponse(input);
+
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, justbotImage)
