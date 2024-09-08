195bd48dcc9775a465cd6035bf0a8987262231e4
Kieran's Laptop <kieran.koh@gmail.com>
2024-09-08 23:45:36 +0800

    Add A-CodeQuality requirements
    UI.java was leftover after changing to JavaFX for GUI
    
    Refactor Count.java to no longer use UI.java
    
    Let's implement closing of the application window when off

--- src/main/java/count/Count.java
+++ src/main/java/count/Count.java
@@ -1,7 +1,6 @@
 package count;
 
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 import count.action.Action;
 import count.action.Deactivate;
@@ -15,8 +14,6 @@ import count.exception.IncorrectFormatException;
 public class Count {
     private TaskList ls;
     private boolean isOn;
-    private Scanner sc;
-    private UI ui;
     private Storage storage;
     private Parser parser;
 
@@ -26,41 +23,15 @@ public class Count {
      */
     public Count(String filePath) {
         this.isOn = true;
-        this.sc = new Scanner(System.in);
-        this.ui = new UI();
+
         this.storage = new Storage(filePath);
 
         try {
             this.ls = new TaskList(this.storage.load());
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException | IncorrectFormatException e) {
             this.ls = new TaskList();
-        } catch (IncorrectFormatException e) {
-            this.ls = new TaskList();
-            this.ui.reply(e.getMessage());
         }
         this.parser = new Parser(this.ls, filePath);
-    }
-
-    /**
-     * start is a method that turns the Count program on,
-     * It calls ui.greet() and passes all commands typed to the parser
-     * If the command parsed returns a Deactivate object, Count turns off
-     */
-    private void start() {
-        ui.greet();
-
-        while (this.isOn) {
-            String command = sc.nextLine();
-            try {
-                Action curr = parser.parse(command);
-                if (curr instanceof Deactivate) {
-                    this.isOn = false;
-                    this.sc.close();
-                }
-            } catch (CountException e) {
-                ui.reply(e.getMessage());
-            }
-        }
     }
 
     /**
@@ -72,14 +43,14 @@ public class Count {
         String response = "";
         try {
             Action curr = parser.parse(input);
+            if (curr instanceof Deactivate) {
+                this.isOn = false;
+            }
             response = curr.run();
         } catch (CountException e) {
             response = e.getMessage();
         }
         return response;
     }
-    public static void main(String[] args) {
-        Count c = new Count("./Count.txt");
-        c.start();
-    }
+
 }
