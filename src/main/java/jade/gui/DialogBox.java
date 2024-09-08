008eae441a96e19aca6a5c797f7acd109dade7fb
wubojin <wubojin64@gmail.com>
2024-09-09 04:27:54 +0800

    Fix Level 10: GUI

--- src/main/java/jade/gui/DialogBox.java
+++ src/main/java/jade/gui/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face
@@ -36,6 +37,8 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        Circle clip = new Circle(49.5, 49.5, 49.5);
+        displayPicture.setClip(clip);
     }
 
     /**
@@ -46,6 +49,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
