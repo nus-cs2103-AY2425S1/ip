f1cf5d4305656f9b3d6399c189a817b171147f5c
ywllowsensor <isyazani@gmail.com>
2024-09-08 23:51:26 +0800

    Abstract getIndex method from child classes

--- src/main/java/main/command/Command.java
+++ src/main/java/main/command/Command.java
@@ -36,6 +36,29 @@ public abstract class Command {
     }
 
     /**
+     * Returns an integer representing the index of a task in an array.
+     * @param input Input by the user.
+     * @return Index of the task.
+     */
+    protected int getIndex(String input) {
+        String indexAsString = "";
+        if (input.contains("unmark")) {
+            indexAsString = input.substring(7);
+        }
+        if (input.contains("mark")) {
+            indexAsString = input.substring(5);
+        }
+        if (input.contains("delete")) {
+            indexAsString = input.substring(7);
+        }
+        if (indexAsString.equals("")) {
+            return -1;
+        }
+        int index = Integer.valueOf(indexAsString) - 1;
+        return index;
+    }
+
+    /**
      * Abstract method that executes a command based on the
      * input of the user. Requires overriding in subclasses.
      * @param tasks List of tasks.
