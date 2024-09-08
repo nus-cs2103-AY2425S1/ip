9e694d841d0471ab447cc29572f9cb8742bd5647
amoschee <amoscheetianee@gmail.com>
2024-09-08 17:42:49 +0800

    Refactor code for improved readability

--- src/main/java/wiggly/parser/Parser.java
+++ src/main/java/wiggly/parser/Parser.java
@@ -21,6 +21,9 @@ import wiggly.task.TaskType;
 public class Parser {
 
 
+    // This is the standard user input parsing pattern which looks like the following:
+    // <command> <description> <by date if there is a preceeding /by> <from date if there is a preceeding /from>
+    // <to date if there is a preceeding /to>
     public static final Pattern USER_INPUT_PATTERN = Pattern.compile(
         "(?<command>\\S+)"
             + "(?:\\s+(?<description>[^/]*))?"
@@ -43,44 +46,44 @@ public class Parser {
     public static Command parse(String input) throws WigglyException {
         Matcher matcher = USER_INPUT_PATTERN.matcher(input);
         // Find and print commands, descriptions, and parameters
-        if (matcher.find()) {
-
-            try {
-                String command = matcher.group("command").trim();
-                String description = matcher.group("description");
-                String by = matcher.group("by");
-                String from = matcher.group("from");
-                String to = matcher.group("to");
-
-                switch (command) {
-                case "list":
-                    return new ListCommand();
-                case "todo":
-                    return new AddCommand(TaskType.TODO, description.trim(), null, null, null);
-                case "deadline":
-                    return new AddCommand(TaskType.DEADLINE, description.trim(),
-                        LocalDate.parse(by.trim()), null, null);
-                case "event":
-                    return new AddCommand(TaskType.EVENT, description.trim(), null,
-                        LocalDate.parse(from.trim()), LocalDate.parse(to.trim()));
-                case "bye":
-                    return new ExitCommand();
-                case "mark":
-                    return new MarkCommand(Integer.parseInt(description.trim()), true);
-                case "unmark":
-                    return new MarkCommand(Integer.parseInt(description.trim()), false);
-                case "delete":
-                    return new DeleteCommand(Integer.parseInt(description.trim()));
-                case "find":
-                    return new FindCommand(description.trim());
-                default:
-                    return new InvalidCommand();
-                }
-            } catch (NullPointerException e) {
-                throw new WigglyException("Failed to parse input: " + input);
-            }
-        } else {
+        if (!matcher.find()) {
             return new InvalidCommand();
         }
+
+        try {
+            String command = matcher.group("command").trim();
+            String description = matcher.group("description");
+            String by = matcher.group("by");
+            String from = matcher.group("from");
+            String to = matcher.group("to");
+
+            switch (command) {
+            case "list":
+                return new ListCommand();
+            case "todo":
+                return new AddCommand(TaskType.TODO, description.trim(), null, null, null);
+            case "deadline":
+                return new AddCommand(TaskType.DEADLINE, description.trim(),
+                    LocalDate.parse(by.trim()), null, null);
+            case "event":
+                return new AddCommand(TaskType.EVENT, description.trim(), null,
+                    LocalDate.parse(from.trim()), LocalDate.parse(to.trim()));
+            case "bye":
+                return new ExitCommand();
+            case "mark":
+                return new MarkCommand(Integer.parseInt(description.trim()), true);
+            case "unmark":
+                return new MarkCommand(Integer.parseInt(description.trim()), false);
+            case "delete":
+                return new DeleteCommand(Integer.parseInt(description.trim()));
+            case "find":
+                return new FindCommand(description.trim());
+            default:
+                return new InvalidCommand();
+            }
+        } catch (NullPointerException e) {
+            throw new WigglyException("Failed to parse input: " + input);
+        }
+
     }
 }
