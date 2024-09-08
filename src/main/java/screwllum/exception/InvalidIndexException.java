77c28a8a4a6b10c9d445d53e82b180a56df36e80
Quasant <benjaminthechin@gmail.com>
2024-09-08 21:06:58 +0800

    Create exceptions related to the chatbot

---
+++ src/main/java/screwllum/exception/InvalidIndexException.java
@@ -0,0 +1,7 @@
+package screwllum.exception;
+
+public class InvalidIndexException extends ScrewllumException {
+    public InvalidIndexException(String index) {
+        super(index);
+    }
+}
---
+++ src/main/java/screwllum/exception/ScrewllumException.java
@@ -0,0 +1,7 @@
+package screwllum.exception;
+
+public class ScrewllumException extends Exception {
+    public ScrewllumException(String message) {
+        super(message);
+    }
+}
