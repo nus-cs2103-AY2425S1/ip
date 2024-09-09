2d8d3b4f8a582ed305becc31166e2187c2ed8864
QuekBingHeng <quekbingheng.17@gmail.com>
2024-09-09 21:20:18 +0800

    Add DialogBox.java, GUI.java, Launcher.java, at JavaFX tutorial part 3

--- src/main/java/angel/Angel.java
+++ src/main/java/angel/Angel.java
@@ -99,6 +99,79 @@ public class Angel {
     }
 
     /**
+     * Processes user input and returns the corresponding response.
+     *
+     * @param input The user input command.
+     * @return The response message.
+     */
+    public String getResponse(String input) {
+        try {
+            Parser.Command command = Parser.parseCommand(input);
+            switch (command.getType()) {
+            case "bye":
+                return "Bye. Hope to see you again soon!";
+            case "list":
+                return formatTasks(taskList.listTasks());
+            case "todo":
+                String todoDescription = (String) command.getDetails();
+                taskList.addTask(new ToDo(todoDescription));
+                return "Got it. I've added this task:\n  " + todoDescription;
+            case "deadline":
+                Parser.DeadlineDetail deadlineDetail = (Parser.DeadlineDetail) command.getDetails();
+                taskList.addTask(new Deadline(deadlineDetail.getDescription(), deadlineDetail.getDeadline()));
+                return "Got it. I've added this task:\n  " + deadlineDetail.getDescription()
+                        + " by: " + deadlineDetail.getDeadline().format(INPUT_FORMAT);
+            case "event":
+                Parser.EventDetail eventDetail = (Parser.EventDetail) command.getDetails();
+                taskList.addTask(new Event(eventDetail.getDescription(), eventDetail.getFrom(), eventDetail.getTo()));
+                return "Got it. I've added this task:\n  " + eventDetail.getDescription() + " from: "
+                        + eventDetail.getFrom().format(INPUT_FORMAT) + " to: " + eventDetail.getTo().format(INPUT_FORMAT);
+            case "mark":
+                int markIndex = (int) command.getDetails();
+                taskList.markTask(markIndex);
+                return "Nice! I've marked this task as done:\n  " + taskList.listTasks().get(markIndex);
+            case "unmark":
+                int unmarkIndex = (int) command.getDetails();
+                taskList.unmarkTask(unmarkIndex);
+                return "OK, I've marked this task as not done yet:\n  " + taskList.listTasks().get(unmarkIndex);
+            case "delete":
+                int deleteIndex = (int) command.getDetails();
+                String deletedTask = taskList.listTasks().get(deleteIndex).toString();
+                taskList.deleteTask(deleteIndex);
+                return "Noted. I've removed this task:\n  " + deletedTask;
+            case "find":
+                String keyword = (String) command.getDetails();
+                List<Task> foundTasks = taskList.findTasks(keyword);
+                return foundTasks.isEmpty() ? "No matching tasks found."
+                        : "Here are the matching tasks in your list:\n" + formatTasks(foundTasks);
+            default:
+                throw new InvalidCommandException("Unknown command: " + command.getType()
+                        + ". Please enter a valid command.");
+            }
+        } catch (InvalidCommandException e) {
+            return e.getMessage();
+        } catch (TaskNotFoundException e) {
+            return e.getMessage() + " Please check the task number and try again.";
+        } catch (AngelException e) {
+            return "An unexpected error occurred: " + e.getMessage();
+        }
+    }
+
+    /**
+     * Formats a list of tasks into a string for display.
+     *
+     * @param tasks The list of tasks to format.
+     * @return The formatted string of tasks.
+     */
+    private String formatTasks(List<Task> tasks) {
+        StringBuilder formattedTasks = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            formattedTasks.append((i + 1)).append(". ").append(tasks.get(i)).append("\n");
+        }
+        return formattedTasks.toString().trim();
+    }
+
+    /**
      * Entry point of the Angel application.
      *
      * @param args Command-line arguments.
---
+++ src/main/java/angel/Launcher.java
@@ -0,0 +1,12 @@
+package main.java.angel;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues. Delete the below
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GUI.class, args);
+    }
+}
