95ae64d06d5f8086ec07f69b617b464ee862481d
Rachael-Chan <e0775723@u.nus.edu>
2024-09-09 14:37:43 +0800

    feat: Added GUI for spiderman chatbot

--- src/main/java/spiderman/TaskList.java
+++ src/main/java/spiderman/TaskList.java
@@ -13,48 +13,54 @@ public class TaskList {
         this.tasks = tasks;
     }
 
-    public void addTask(Task task) {
+    public String addTask(Task task) {
         this.tasks.add(task);
-        System.out.println("Cool! I'll add this to your task list!");
-        System.out.println("You now have " + this.tasks.size() + " tasks in your task list.");
+        return "Cool! I'll add this to your task list!\n"
+                + "You now have " + this.tasks.size() + " tasks in your task list.";
     }
 
-    public void deleteTask(int index) {
-        System.out.println("Alright! I will delete this task for you!");
-        System.out.println(this.tasks.get(index).toString());
+    public String deleteTask(int index) {
+        String removedTask = this.tasks.get(index).toString();
         this.tasks.remove(index);
+        return "Alright! I will delete this task for you!\n"
+                + removedTask;
+
     }
 
-    public void markTaskAsDone(int index) {
+    public String markTaskAsDone(int index) {
         this.tasks.get(index).markAsDone();
-        System.out.println("Great! I've marked this task as done:");
-        System.out.println(this.tasks.get(index).toString());
+        return "Great! I've marked this task as done:\n"
+                + this.tasks.get(index).toString();
     }
 
-    public void markTaskAsNotDone(int index) {
+    public String markTaskAsNotDone(int index) {
         this.tasks.get(index).markAsNotDone();
-        System.out.println("OK, this task will be marked as not done yet:");
-        System.out.println(this.tasks.get(index).toString());
+        return "OK, this task will be marked as not done yet:\n"
+                + this.tasks.get(index).toString();
     }
 
-    public void listTasks() {
+    public String listTasks() {
+        String listOfTasks = "";
         for (int i = 0; i < this.tasks.size(); i++) {
-            System.out.println((i+1) + ". " + this.tasks.get(i).toString());
+            listOfTasks += i + 1 + ". " + this.tasks.get(i).toString() + "\n";
         }
+        return listOfTasks;
     }
 
-    public void findTasks(String keyword) {
-        System.out.println("Here are the matching tasks in your list:");
+    public String findTasks(String keyword) {
         int numOfMatches = 0;
+        String matchingTasks = "";
         for (int i = 0; i < this.tasks.size(); i++) {
             if (this.tasks.get(i).getDescription().contains(keyword)) {
                 numOfMatches++;
-                System.out.println((i + 1) + ". " + this.tasks.get(i).toString());
+                matchingTasks += i + 1 + ". " + this.tasks.get(i).toString() + "\n";
             }
         }
         if (numOfMatches == 0) {
-            System.out.println("No tasks found with the keyword: " + keyword);
+            return "No tasks found with the keyword: " + keyword;
         }
+        return "Here are the matching tasks in your list:\n"
+                + matchingTasks;
     }
 
     public ArrayList<Task> getTasks() {
