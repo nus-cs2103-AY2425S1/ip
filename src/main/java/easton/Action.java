acac85eaadb895555c46ad299fa53ec7607ebd65
stephaneleong <stephaneleong@gmail.com>
2024-09-09 10:52:34 +0800

    Change Class Javadoc

--- src/main/java/easton/Action.java
+++ src/main/java/easton/Action.java
@@ -1,7 +1,7 @@
 package easton;
 
 /**
- * Represents that the chatbot can handle.
+ * Represents the different types of actions/commands that the chatbot can handle.
  */
 public enum Action {
     BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND, INVALID
