8cc26c74cbb89646e61a404a9992a878fbc9a5c7
rayray39 <e0772883@u.nus.edu>
2024-09-09 23:14:37 +0800

    Task.java: Add scheduledDateTime attribute

--- src/main/java/janet/Task.java
+++ src/main/java/janet/Task.java
@@ -1,8 +1,6 @@
 package janet;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 /**
  * Represents a Task
@@ -11,29 +9,17 @@ public class Task {
     private final String description;
     private final String symbol;
     private boolean done;
+    private final LocalDateTime scheduledDateTime;
 
-    public Task(String description, String symbol) {
+    public Task(String description, String symbol, LocalDateTime scheduledDateTime) {
         this.description = description;
         this.done = false;
         this.symbol = symbol;
+        this.scheduledDateTime = scheduledDateTime;
     }
 
-    /**
-     * Converts and returns the input date and time into appropriate formats
-     * date -> MM dd yyyy
-     * time -> hh:mm a
-     *
-     * @param inputDate A string representing a date in the format, yyyy-MM-dd
-     * @param inputTime A string representing a time in the format, hh:mm
-     * @return A string representing the time and date (MM dd yyyy hh:mm a)
-     */
-    public static String DateAndTimeFormatter(String inputDate, String inputTime) {
-        LocalDate localDate = LocalDate.parse(inputDate);
-        String date = localDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
-
-        LocalTime localTime = LocalTime.parse(inputTime);
-        String time = localTime.format(DateTimeFormatter.ofPattern("hh:mm a"));
-        return date + " " + time;
+    public LocalDateTime getScheduledDate() {
+        return scheduledDateTime;
     }
 
     /**
