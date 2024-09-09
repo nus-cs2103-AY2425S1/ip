dda509f8248539bdb653f85e48506f96c690aa59
Sean Lim <seanlimdev856@gmail.com>
2024-09-09 14:06:17 +0800

    Switch to parsing with regex

--- src/main/java/deez/Parser.java
+++ src/main/java/deez/Parser.java
@@ -3,6 +3,8 @@ package deez;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.util.Pair;
 
@@ -12,6 +14,10 @@ import javafx.util.Pair;
  */
 public class Parser {
     private static final DateTimeFormatter DATE_TIME_INPUT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final Pattern NAME_REGEX = Pattern.compile("([^/#]* )|([^/#]*)", Pattern.MULTILINE);
+    private static final Pattern PROPS_REGEX = Pattern.compile("(\\/([^\\s]+) ([^\\/#]+))", Pattern.MULTILINE);
+    private static final Pattern TAGS_REGEX = Pattern.compile("#([^\\s\\/#]+)", Pattern.MULTILINE);
+
 
     /**
      * This method parses the given string and returns a pair containing the command and properties.
@@ -39,70 +45,107 @@ public class Parser {
         default -> Command.UNKNOWN;
         };
 
-        Properties props = switch (cmd) {
-        case TODO -> parseTodoProps(propString);
-        case EVENT -> parseEventProps(propString);
-        case DEADLINE -> parseDeadlineProps(propString);
-        case MARK, UNMARK, DELETE -> parseTaskIndexProp(propString);
-        case FIND -> parseTextInputProps(propString);
-        case EXIT, LIST, SAVE, UNKNOWN -> new Properties();
-        };
+        Properties props = new Properties();
+
+        // Parse properties
+        Matcher propsMatcher = PROPS_REGEX.matcher(propString);
+        Matcher nameMatcher = NAME_REGEX.matcher(propString);
+
+        Matcher tagsMatcher = TAGS_REGEX.matcher(propString);
+
+        while (propsMatcher.find()) {
+            props.setProperty(propsMatcher.group(2).strip(), propsMatcher.group(3).strip());
+        }
+        if (nameMatcher.find()) {
+            props.setProperty("name", nameMatcher.group(0).strip());
+        }
+
+        while (tagsMatcher.find()) {
+            System.out.println("Full match: " + tagsMatcher.group(0));
+            for (int i = 1; i <= tagsMatcher.groupCount(); i++) {
+                System.out.println("Group " + i + ": " + tagsMatcher.group(i));
+            }
+        }
+
+        // Validate properties
+        switch (cmd) {
+        case EVENT:
+            validateEventProps(props);
+            break;
+        case DEADLINE:
+            validateDeadlineProps(props);
+            break;
+        case MARK, UNMARK, DELETE:
+            validateTaskIndexProp(props);
+            break;
+        case FIND:
+            validateTextInputProps(props);
+            break;
+        default:
+            break;
+        }
+
+        System.out.println(props);
 
         return new Pair<>(cmd, props);
     }
-
-    private static Properties parseTextInputProps(String propString) {
-        Properties props = new Properties();
-        props.setProperty("keyword", propString.strip());
-        return props;
+    /**
+     * This method checks if the properties do not contain certain keys.
+     *
+     * @param props The properties to be checked
+     * @param keys The keys that should not be present in the properties
+     * @return True if the properties do not contain the given keys, false otherwise
+     */
+    private static boolean propsNotContains(Properties props, String... keys) {
+        for (String key : keys) {
+            if (!props.containsKey(key)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    private static Properties parseTaskIndexProp(String propString) {
-        Properties props = new Properties();
-        props.setProperty("index", propString.strip());
-        return props;
+    /**
+     * This method checks if the properties contain blank values for certain keys.
+     *
+     * @param props The properties to be checked
+     * @param keys The keys that should have non-blank values in the properties
+     * @return True if the properties contain blank values for the given keys, false otherwise
+     */
+    private static boolean propsAreBlank(Properties props, String... keys) {
+        for (String key : keys) {
+            assert props.containsKey(key);
+            if (props.getProperty(key).isBlank()) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    private static Properties parseTodoProps(String propString) {
-        Properties props = new Properties();
-        props.setProperty("name", propString.strip());
-        return props;
+    private static void validateTextInputProps(Properties props) {
+        props.setProperty("keyword", props.getProperty("name"));
+        props.remove("name");
     }
 
-    private static Properties parseEventProps(String propString) {
-        Properties props = new Properties();
-        String[] parts = propString.split("/from|/to", 3);
-        if (parts.length != 3 || parts[0].isBlank() || parts[1].isBlank() || parts[2].isBlank()) {
-            throw new DeezException(
-                "Please provide a description, start date, and end date.",
-                "Usage:",
+    private static void validateTaskIndexProp(Properties props) {
+        props.setProperty("index", props.getProperty("name"));
+        props.remove("name");
+    }
+
+    private static void validateEventProps(Properties props) {
+        if (propsNotContains(props, "name", "from", "to") || propsAreBlank(props, "name", "from", "to")) {
+            throw new DeezException("Please provide a description, start date, and end date.", "Usage:",
                 "event " + "project meeting /from 2019-10-15 1800 /to 2019-10-15 1900");
         }
-        props.setProperty("name", parts[0].strip());
-        props.setProperty("from", parts[1].strip());
-        props.setProperty("to", parts[2].strip());
-        return props;
     }
 
-    private static Properties parseDeadlineProps(String propString) {
-        Properties props = new Properties();
-        String[] parts = propString.split("/by", 2);
-        if (parts.length != 2) {
-            throw new DeezException(
-                "Please provide a description and deadline.",
-                "Usage:",
+    private static void validateDeadlineProps(Properties props) {
+        if (propsNotContains(props, "name", "by") || propsAreBlank(props, "name", "by")) {
+            throw new DeezException("Description and deadline must not be blank.", "Usage:",
                 "deadline " + "return book" + " /by 2019-10-15 1800");
         }
-        if (parts[0].strip().isBlank() || parts[1].strip().isBlank()) {
-            throw new DeezException(
-                "Description and deadline must not be blank.",
-                "Usage:",
-                "deadline " + "return book" + " /by 2019-10-15 1800");
-        }
-        props.setProperty("name", parts[0].strip());
-        props.setProperty("by", parts[1].strip());
-        return props;
     }
+
     /**
      * This method parses the given date-time string into a LocalDateTime object.
      *
