a43ca1d1bd806a17c8fd9581a049c3164c18dd16
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-09-10 17:17:45 +0000

    Fix test strings to use manual string concat over triple-quotes

--- src/test/java/yappingbot/ui/UiCliTest.java
+++ src/test/java/yappingbot/ui/UiCliTest.java
@@ -15,10 +15,8 @@ class UiCliTest {
     @Test
     void testPrintError() throws IOException {
 
-        final String expected = """
-                                 |  Error message here!
-                                 |  Multiline Error Message
-                                """;
+        final String expected =  " |  Error message here!\n"
+                                 + " |  Multiline Error Message\n";
         TestHelper h = new TestHelper();
         h.captureStdOut();
         ui.printError("Error message here!\nMultiline Error Message");
@@ -28,15 +26,13 @@ class UiCliTest {
 
     @Test
     void testPrint() throws IOException {
-        final String expected = """
-                                 |  test 1
-
-                                 |
-
-                                 |  multiline
-                                 |  Allowed
-                                
-                                """;
+        final String expected = " |  test 1\n"
+                                + "\n"
+                                + " |\n"
+                                + "\n"
+                                + " |  multiline\n"
+                                + " |  Allowed\n"
+                                + "\n";
         TestHelper h = new TestHelper();
         h.captureStdOut();
         ui.print("test 1");
@@ -48,14 +44,12 @@ class UiCliTest {
 
     @Test
     void testPrintln() throws IOException {
-        final String expected = """
-                                 |  test 1
-
-                                 |
-
-                                 |  multilineprevented
-                                
-                                """;
+        final String expected = " |  test 1\n"
+                                + "\n"
+                                + " |\n"
+                                + "\n"
+                                + " |  multilineprevented\n"
+                                + "\n";
         TestHelper h = new TestHelper();
         h.captureStdOut();
         ui.println("test 1");
