5f4f4899ebf1e9429af55af053fbe9f942ada935
KrashKart <e1113755@u.nus.edu>
2024-09-10 18:07:46 +0800

    A-MoreErrorHandling: Improve error handling part 2
    Jackson does not have the capability to detect conflicting task
    names before adding the tasks.
    
    The possibility of a name conflict impeding code progress later on
    is high and minimising this risk is optimal for now.
    
    Let's,
    * create DuplicatedTaskException to handle such conflicts
    * modify addTask in TaskList.java to check for name conflicts first
    * modify Javadocs to adhere to coding standard and checkStyle
    
    I forgot to stage the changes so this commit will be an extension
    to the previous one.

--- src/main/java/jackson/exceptions/DuplicatedTaskException.java
+++ src/main/java/jackson/exceptions/DuplicatedTaskException.java
@@ -1,4 +1,10 @@
 package jackson.exceptions;
 
-public class DuplicatedTaskException {
+/**
+ * Exception to handle the case when user enters same task name more than once.
+ */
+public class DuplicatedTaskException extends JacksonException {
+    public DuplicatedTaskException(String msg) {
+        super(msg);
+    }
 }
