83e98b1f348f957a9bef9d564779ed615f1b1410
LinkesV <linkesvarun@gmail.com>
2024-09-10 20:44:57 +0800

    Add Launcher.java
    > Update build.gradle dependencies
    > Change entry point into Applicaton from Main to Launcher
    > Helps to avoid file path errors

---
+++ src/main/java/cypherchatbot/controller/Launcher.java
@@ -0,0 +1,12 @@
+package cypherchatbot.controller;
+
+import javafx.application.Application;
+
+import cypherchatbot.Main;
+
+// Used as an entry point into our application
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
