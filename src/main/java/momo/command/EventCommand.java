228da64e84dc110964150e88691b546e8a419f76
dovlin <dovlincs01@gmail.com>
2024-09-10 17:40:34 +0800

    Extract getTask() method to adhere to SLAP and fix import method

--- src/main/java/momo/command/EventCommand.java
+++ src/main/java/momo/command/EventCommand.java
@@ -1,15 +1,15 @@
 package momo.command;
 
-import momo.Ui;
-import momo.exception.InvalidCommandException;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 import momo.Storage;
 import momo.StorageException;
+import momo.Ui;
+import momo.exception.InvalidCommandException;
 import momo.task.Event;
 import momo.task.Task;
 import momo.task.TaskList;
-
-import java.time.DateTimeException;
-import java.time.LocalDate;
 
 /**
  * Handles the functionality of adding an event, updating the {@link TaskList} and
@@ -18,26 +18,22 @@ import java.time.LocalDate;
  * user to submit a proper input
  */
 public class EventCommand extends AddCommand {
+
+    public static final int COMMAND_PREFIX_OFFSET = 5;
+
+    /**
+     * Runs event command
+     * @param input
+     * @param storage
+     * @param tasks
+     * @param ui
+     * @throws InvalidCommandException
+     * @throws StorageException
+     */
     public static void run(String input, Storage storage, TaskList tasks, Ui ui) throws InvalidCommandException,
             StorageException {
         try {
-            String desc = input.substring(5).trim();
-            if (!desc.contains("/from")) {
-                throw new InvalidCommandException("Your event needs a /from YYYY-MM-DD input, don't cross the line");
-            }
-            if (!desc.contains("/to")) {
-                throw new InvalidCommandException("Your event needs a /to YYYY-MM-DD input, don't cross the line");
-            }
-
-            String task = desc.split("/", 2)[0].trim();
-            if (task.isEmpty()) {
-                throw new InvalidCommandException("Where is your event description?!");
-            }
-
-            String from = desc.split("/from")[1].split("/to")[0].trim();
-            String to = desc.split("/to")[1].trim();
-
-            Task event = new Event(task, LocalDate.parse(from), LocalDate.parse(to), false);
+            Task event = getTask(input);
             tasks.addTask(event);
             addToStorage(storage, event);
             printTaskAdded(event, ui);
@@ -46,11 +42,29 @@ public class EventCommand extends AddCommand {
         } catch (IndexOutOfBoundsException e) {
             throw new InvalidCommandException("You better format your event properly or else [REDACTED]");
         } catch (DateTimeException dte) {
-            throw new InvalidCommandException("You better format your event dates in a valid YYYY-MM-DD format or " +
-                    "else...");
+            throw new InvalidCommandException("You better format your event dates in a valid YYYY-MM-DD format or "
+                    + "else...");
+        }
+    }
+
+    private static Task getTask(String input) throws InvalidCommandException {
+        String desc = input.substring(COMMAND_PREFIX_OFFSET).trim();
+        if (!desc.contains("/from")) {
+            throw new InvalidCommandException("Your event needs a /from YYYY-MM-DD input, don't cross the line");
+        }
+        if (!desc.contains("/to")) {
+            throw new InvalidCommandException("Your event needs a /to YYYY-MM-DD input, don't cross the line");
         }
 
+        String task = desc.split("/", 2)[0].trim();
+        if (task.isEmpty()) {
+            throw new InvalidCommandException("Where is your event description?!");
+        }
 
+        String from = desc.split("/from")[1].split("/to")[0].trim();
+        String to = desc.split("/to")[1].trim();
+
+        return new Event(task, LocalDate.parse(from), LocalDate.parse(to), false);
     }
 
 }
