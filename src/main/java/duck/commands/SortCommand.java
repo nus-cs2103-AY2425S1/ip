a899026d98f320e1c55058f43480396eda328a83
Feng1231 <chenfeng021231@gmail.com>
2024-09-10 23:06:51 +0800

    Refactor Sort Command and Add parameter package
    There are a lot of constants used in the `SortCommand` class.
    This reduce the readability and maintainability of the code.
    
    Let's
    - Add a parameter package
    - Create a `By` enum class under parameter package to represent relevant `By` parameters
    - Create a `Target` enum class under parameter package to represent relavant `Target` parameters
    - Refactor the `SortCommand` class to refer to the two enum classes instead of using defined constants.
    
    This will make the code more readable and extendable.

--- src/main/java/duck/commands/SortCommand.java
+++ src/main/java/duck/commands/SortCommand.java
@@ -2,12 +2,15 @@ package duck.commands;
 
 import java.util.Comparator;
 
+import duck.common.Message;
 import duck.data.TaskList;
 import duck.data.exception.DuckException;
 import duck.data.task.Deadline;
 import duck.data.task.Event;
 import duck.data.task.Task;
 import duck.data.task.ToDo;
+import duck.parameter.By;
+import duck.parameter.Target;
 import duck.storage.Storage;
 import duck.ui.Ui;
 
@@ -20,28 +23,13 @@ import duck.ui.Ui;
 public class SortCommand extends Command {
 
     private static final String ERROR_MESSAGE_SORT_COMMAND = "Invalid sort command format.";
+
     private static final String PARAMETER_TARGET = "/target";
-    private static final String INVALID_MISSING_PARAMETER_TARGET = "Missing /target parameter.";
     private static final String PARAMETER_BY = "/by";
+    private static final String INVALID_MISSING_PARAMETER_TARGET = "Missing /target parameter.";
+
     private static final String INVALID_MISSING_PARAMETER_BY = "Missing /by parameter.";
-    private static final String PARAMETER_TARGET_KEYWORD_ALL = "all";
-    private static final String PARAMETER_TARGET_KEYWORD_TODO = "todo";
-    private static final String PARAMETER_TARGET_KEYWORD_DEADLINE = "deadline";
-    private static final String PARAMETER_TARGET_KEYWORD_EVENT = "event";
     private static final String INVALID_UNKNOWN_TARGET = "Unknown target '";
-    private static final String INVALID_KEYWORD_BY_FOR_EVENT =
-            "For target 'event', you can only sort by 'description', 'start', or 'end'.";
-    private static final String INVALID_KEYWORD_BY_FOR_DEADLINE =
-            "For target 'deadline', you can only sort by 'description' or 'deadline'.";
-    private static final String INVALID_KEYWORD_BY_FOR_ALL =
-            "For target 'all', you can only sort by 'description' or 'type'.";
-    private static final String INVALID_KEYWORD_BY_FOR_TODO = "For target 'todo', you can only sort by 'description'.";
-    private static final String PARAMETER_BY_KEYWORD_DESCRIPTION = "description";
-    private static final String PARAMETER_BY_KEYWORD_TYPE = "type";
-    private static final String PARAMETER_BY_KEYWORD_START = "start";
-    private static final String PARAMETER_BY_KEYWORD_END = "end";
-    private static final String PARAMETER_BY_KEYWORD_DEADLINE = "deadline";
-    private static final String MESSAGE_SORT_COMMAND = " tasks sorted successfully by ";
 
     public SortCommand(String message) {
         super(message);
@@ -63,20 +51,13 @@ public class SortCommand extends Command {
         validateMessage(splitString);
 
         // Extract keywords of the target and by parameters from the input message
-        String target = getTarget(splitString);
-        String by = getBy(splitString);
+        Target target = getTarget(splitString);
+        By by = getBy(splitString);
 
         sortRelevantTasks(tasks, storage, target, by);
     }
 
-    private void sortRelevantTasks(TaskList tasks, Storage storage, String target, String by) throws DuckException {
-        Comparator<Task> comp = getComparator(target, by);
 
-        tasks.sortTasks(comp, target);
-        storage.writeTasks(tasks);
-
-        System.out.println(target + MESSAGE_SORT_COMMAND + by + "!");
-    }
 
     /**
      * Parses the input message and splits it into an array of strings.
@@ -112,10 +93,10 @@ public class SortCommand extends Command {
      * @return The target specified in the input message.
      * @throws DuckException If the target is missing or invalid.
      */
-    private String getTarget(String... message) throws DuckException {
+    private Target getTarget(String... message) throws DuckException {
         for (int i = 0; i < message.length - 1; i++) {
             if (message[i].equals(PARAMETER_TARGET)) {
-                return message[i + 1];
+                return Target.fromKeyword(message[i + 1]);
             }
         }
         throw new DuckException(INVALID_MISSING_PARAMETER_TARGET);
@@ -128,10 +109,10 @@ public class SortCommand extends Command {
      * @return The sorting criterion specified in the input message.
      * @throws DuckException If the sorting criterion is missing or invalid.
      */
-    private String getBy(String... message) throws DuckException {
+    private By getBy(String... message) throws DuckException {
         for (int i = 0; i < message.length - 1; i++) {
             if (message[i].equals(PARAMETER_BY)) {
-                return message[i + 1];
+                return By.fromKeyword(message[i + 1]);
             }
         }
         throw new DuckException(INVALID_MISSING_PARAMETER_BY);
@@ -145,93 +126,86 @@ public class SortCommand extends Command {
      * @return A comparator for sorting tasks based on the specified target and criterion.
      * @throws DuckException If the target and criterion combination is invalid.
      */
-    private Comparator<Task> getComparator(String target, String by) throws DuckException {
+    private Comparator<Task> getComparator(Target target, By by) throws DuckException {
         switch (target) {
-        case PARAMETER_TARGET_KEYWORD_ALL:
+        case ALL:
             return getAllComparator(by);
-        case PARAMETER_TARGET_KEYWORD_TODO:
+        case TODO:
             return getToDoComparator(by);
-        case PARAMETER_TARGET_KEYWORD_DEADLINE:
+        case DEADLINE:
             return getDeadlineComparator(by);
-        case PARAMETER_TARGET_KEYWORD_EVENT:
+        case EVENT:
             return getEventComparator(by);
         default:
             throw new DuckException(INVALID_UNKNOWN_TARGET + target);
         }
     }
 
-    private static Comparator<Task> getToDoComparator(String by) throws DuckException {
-        if (!by.equals(PARAMETER_BY_KEYWORD_DESCRIPTION)) {
-            throw new DuckException(INVALID_KEYWORD_BY_FOR_TODO);
-        }
-        return Comparator.comparing(task -> ((ToDo) task).getDescription());
-    }
-
-    /**
-     * Returns a comparator for sorting all tasks, either by description or type (todo, deadline, event).
-     *
-     * @param by The sorting criterion (either "description" or "type").
-     * @return The comparator for sorting all tasks.
-     * @throws DuckException If the sorting criterion is invalid.
-     */
-    private Comparator<Task> getAllComparator(String by) throws DuckException {
-        if (by.equals(PARAMETER_BY_KEYWORD_DESCRIPTION)) {
+    private Comparator<Task> getAllComparator(By by) throws DuckException {
+        switch (by) {
+        case DESCRIPTION:
             return Comparator.comparing(Task::getDescription);
-        } else if (by.equals(PARAMETER_BY_KEYWORD_TYPE)) {
+        case TYPE:
             return Comparator.comparing(task -> {
                 if (task instanceof ToDo) {
                     return 0;
-                }
-                if (task instanceof Deadline) {
+                } else if (task instanceof Deadline) {
                     return 1;
-                }
-                if (task instanceof Event) {
+                } else if (task instanceof Event) {
                     return 2;
                 }
                 return 3; // Unknown task types come last
             });
-        } else {
-            throw new DuckException(INVALID_KEYWORD_BY_FOR_ALL);
+        default:
+            throw new DuckException(Message.INVALID_KEYWORD_BY_FOR_ALL);
         }
     }
 
-    /**
-     * Returns a comparator for sorting deadline tasks, either by description or deadline.
-     *
-     * @param by The sorting criterion (either "description" or "deadline").
-     * @return The comparator for sorting deadline tasks.
-     * @throws DuckException If the sorting criterion is invalid.
-     */
-    private Comparator<Task> getDeadlineComparator(String by) throws DuckException {
-        if (by.equals(PARAMETER_BY_KEYWORD_DESCRIPTION)) {
+    private Comparator<Task> getToDoComparator(By by) throws DuckException {
+        if (by != By.DESCRIPTION) {
+            throw new DuckException(Message.INVALID_KEYWORD_BY_FOR_TODO);
+        }
+        return Comparator.comparing(task -> ((ToDo) task).getDescription());
+    }
+
+    private Comparator<Task> getDeadlineComparator(By by) throws DuckException {
+        switch (by) {
+        case DESCRIPTION:
             return Comparator.comparing(task -> ((Deadline) task).getDescription());
-        } else if (by.equals(PARAMETER_BY_KEYWORD_DEADLINE)) {
+        case DEADLINE:
             return Comparator.comparing(task -> ((Deadline) task).getBy());
-        } else {
-            throw new DuckException(INVALID_KEYWORD_BY_FOR_DEADLINE);
+        default:
+            throw new DuckException(Message.INVALID_KEYWORD_BY_FOR_DEADLINE);
         }
     }
 
-    /**
-     * Returns a comparator for sorting event tasks, either by description, start time, or end time.
-     *
-     * @param by The sorting criterion (either "description", "start", or "end").
-     * @return The comparator for sorting event tasks.
-     * @throws DuckException If the sorting criterion is invalid.
-     */
-    private Comparator<Task> getEventComparator(String by) throws DuckException {
-        // CHECKSTYLE.OFF: Indentation
-        return switch (by) {
-            case PARAMETER_BY_KEYWORD_DESCRIPTION -> Comparator.comparing(task -> ((Event) task).getDescription());
-            case PARAMETER_BY_KEYWORD_START -> Comparator.comparing(task -> ((Event) task).getFrom());
-            case PARAMETER_BY_KEYWORD_END -> Comparator.comparing(task -> ((Event) task).getTo());
-            default ->
-                    throw new DuckException(INVALID_KEYWORD_BY_FOR_EVENT);
-        };
-        // CHECKSTYLE.ON: Indentation
+    private Comparator<Task> getEventComparator(By by) throws DuckException {
+        switch (by) {
+        case DESCRIPTION:
+            return Comparator.comparing(task -> ((Event) task).getDescription());
+        case START:
+            return Comparator.comparing(task -> ((Event) task).getFrom());
+        case END:
+            return Comparator.comparing(task -> ((Event) task).getTo());
+        default:
+            throw new DuckException(Message.INVALID_KEYWORD_BY_FOR_EVENT);
+        }
     }
 
+    private void sortRelevantTasks(TaskList tasks, Storage storage, Target target, By by) throws DuckException {
+        Comparator<Task> comp = getComparator(target, by);
 
+        tasks.sortTasks(comp, target.getKeyword());
+        storage.writeTasks(tasks);
+
+        System.out.println(target.getKeyword() + " tasks sorted successfully by " + by.getKeyword() + "!");
+    }
+
+    /**
+     * Returns true if the command is an exit command.
+     *
+     * @return {@code false} as the sort command does not cause the program to exit.
+     */
     @Override
     public boolean isExit() {
         return false;
--- src/main/java/duck/common/Message.java
+++ src/main/java/duck/common/Message.java
@@ -8,4 +8,12 @@ public class Message {
     public static final String INVALID_INDEX_FORMAT = "Quack! you have to indicate a valid task index!\n";
     public static final String INDEX_OUT_OF_BOUNDS = "Quack! Index out of bound :( Input a valid task index.\n";
     public static final int TASK_LIST_FIRST_INDEX = 1;
+    public static final String INVALID_KEYWORD_BY_FOR_ALL =
+            "For target 'all', you can only sort by 'description' or 'type'.\n";
+    public static final String INVALID_KEYWORD_BY_FOR_TODO =
+            "For target 'todo', you can only sort by 'description'.\n";
+    public static final String INVALID_KEYWORD_BY_FOR_DEADLINE =
+            "For target 'deadline', you can only sort by 'description' or 'deadline'.\n";
+    public static final String INVALID_KEYWORD_BY_FOR_EVENT =
+            "For target 'event', you can only sort by 'description', 'start', or 'end'.\n";
 }
