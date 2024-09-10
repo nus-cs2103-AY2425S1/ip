e41717d19aafdc2f27fd0f4f30f24b1dfe614102
marchjlim <marchjlim@gmail.com>
2024-09-10 21:35:02 +0800

    Revert "Merge commit '6e326a9746d64a958b3c62ded0a16485bb130263'"
    This reverts commit 515aae4afd09142ede07d66342d70890ace95d7f, reversing
    changes made to 16c0a1ebe7342c78fa3a5a5f9e3bff5f54262352.

--- src/main/java/phenex/task/TaskList.java
+++ src/main/java/phenex/task/TaskList.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import phenex.exception.PhenexException;
 import phenex.storage.Storage;
@@ -107,9 +106,11 @@ public class TaskList {
      */
     public TaskList findTasks(String name) {
         TaskList matchingTasks = new TaskList();
-        matchingTasks.tasks = this.tasks.stream()
-                .filter(task -> task.getName().equals(name))
-                .collect(Collectors.toCollection(ArrayList::new));
+        for (Task task : this.tasks) {
+            if (task.getName().equals(name)) {
+                matchingTasks.addTask(task);
+            }
+        }
         return matchingTasks;
     }
 
@@ -121,10 +122,16 @@ public class TaskList {
      */
     public TaskList findAllTasksOn(LocalDate localDate) {
         TaskList taskList = new TaskList();
-        taskList.tasks = this.tasks.stream()
-                .filter(task -> task instanceof TaskWithDate)
-                .filter(task -> ((TaskWithDate) task).overlapsWith(localDate))
-                .collect(Collectors.toCollection(ArrayList::new));
+
+        for (Task task : this.tasks) {
+            if (task instanceof TaskWithDate) {
+                TaskWithDate taskWithDate = (TaskWithDate) task;
+                if (taskWithDate.overlapsWith(localDate)) {
+                    taskList.addTask(taskWithDate);
+                }
+            }
+        }
+
         return taskList;
     }
 
