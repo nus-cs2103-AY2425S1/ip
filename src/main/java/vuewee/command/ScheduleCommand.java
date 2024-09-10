43297f6fde347f77eb376e6a289556588b67bd06
Jonathan Loh <jonathanloh02@gmail.com>
2024-09-10 16:50:42 +0800

    Fix tests

--- src/main/java/vuewee/command/ScheduleCommand.java
+++ src/main/java/vuewee/command/ScheduleCommand.java
@@ -6,7 +6,13 @@ import vuewee.task.TaskList;
 import vuewee.task.TaskLocalDate;
 import vuewee.ui.TaskListUi;
 
+/**
+ * Represents a command to display the schedule for a given date.
+ */
 public class ScheduleCommand extends Command {
+    /**
+     * Execute schedule command to display the schedule for a given date.
+     */
     @Override
     public void executeCommand(TaskListUi ui, TaskList taskList, CommandParser parser) {
         TaskLocalDate date = parser.<TaskLocalDate>parse(new DateDescriptionParser());
