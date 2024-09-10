50d2b1a18fb301da5790362ce0bdb058e03ebccc
KengHian <e1264781@u.nus.edu>
2024-09-10 22:03:43 +0800

    refactor: Replace magic literal in Parser class
    There are a few specific numbers, which are the index
    of specific arguments in user command, used directly
    without context.
    
    Magic literals might cause confusion and replacing
    them would improve the code quality.
    
    Let's create a few constant variales for index with
    fixed values and intermidiate variables for index
    with dynamic values to provide necessary contexts.

--- src/main/java/struggling/Parser.java
+++ src/main/java/struggling/Parser.java
@@ -21,6 +21,10 @@ import struggling.task.ToDo;
  */
 public class Parser {
 
+    private static final int TODO_DESCRIPTION_INDEX = 5;
+    private static final int DEADLINE_DESCRIPTION_INDEX = 9;
+    private static final int EVENT_DESCRIPTION_INDEX = 6;
+
     /**
      * Parses the full command and creates Command
      * object with arguments.
@@ -58,7 +62,7 @@ public class Parser {
 
     private static ToDo createTodo(String cmd) {
         try {
-            String description = cmd.substring(5);
+            String description = cmd.substring(TODO_DESCRIPTION_INDEX);
             return new ToDo(description);
         } catch (StringIndexOutOfBoundsException e) {
             throw new StrugglingException("OOPS!!! The description of a todo cannot be empty.");
@@ -66,21 +70,24 @@ public class Parser {
     }
 
     private static Task createDeadline(String cmd) {
-        int byIndex = cmd.indexOf("/by ");
+        final String byKeyword = "/by ";
+        int byIndex = cmd.indexOf(byKeyword) + byKeyword.length();
 
-        String description = cmd.substring(9, byIndex).trim();
-        LocalDate by = LocalDate.parse(cmd.substring(byIndex + 4));
+        String description = cmd.substring(DEADLINE_DESCRIPTION_INDEX, cmd.indexOf(byKeyword)).trim();
+        LocalDate by = LocalDate.parse(cmd.substring(byIndex));
 
         return new Deadline(description, by);
     }
 
     private static Task createEvent(String cmd) {
-        int fromIndex = cmd.indexOf("/from ");
-        int toIndex = cmd.indexOf("/to ");
+        final String fromKeyword = "/from ";
+        final String toKeyword = "/to ";
+        int fromIndex = cmd.indexOf(fromKeyword) + fromKeyword.length();
+        int toIndex = cmd.indexOf(toKeyword) + toKeyword.length();
 
-        String description = cmd.substring(6, fromIndex).trim();
-        String from = cmd.substring(fromIndex + 6, toIndex).trim();
-        String to = cmd.substring(toIndex + 4);
+        String description = cmd.substring(EVENT_DESCRIPTION_INDEX, cmd.indexOf(fromKeyword)).trim();
+        String from = cmd.substring(fromIndex, toIndex).trim();
+        String to = cmd.substring(toIndex);
 
         return new Event(description, from, to);
     }
