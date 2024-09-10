293dccb9ea4d9486765bc3b91b6300c46efe8186
dovlin <dovlincs01@gmail.com>
2024-09-10 17:07:35 +0800

    Extract getTask method & replace magic number with GET_COMMAND_PREFIX
    The logic for extracting the task index and parsing commands
    includes a magic number (8), which reduces readability. The task
    extraction logic is inlined, making the code harder to follow and reuse,
    violating SLAP.
    
    Let's,
    * replace magic number 8 with the constant GET_COMMAND_PREFIX to
    clarify its role in extracting substring after the command prefix
    * extract the getTask() method to avoid multiple levels of abstraction
    in the code

--- src/main/java/momo/command/DeadlineCommand.java
+++ src/main/java/momo/command/DeadlineCommand.java
@@ -1,15 +1,16 @@
 package momo.command;
 
-import momo.Ui;
-import momo.exception.InvalidCommandException;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 import momo.Storage;
 import momo.StorageException;
+import momo.Ui;
+import momo.exception.InvalidCommandException;
 import momo.task.Deadline;
 import momo.task.Task;
 import momo.task.TaskList;
 
-import java.time.DateTimeException;
-import java.time.LocalDate;
 
 /**
  * Represents the Deadline command's functionalities, which includes validating
@@ -17,22 +18,22 @@ import java.time.LocalDate;
  * If user input is not properly formatted it throws a {@link InvalidCommandException}
  */
 public class DeadlineCommand extends AddCommand {
+    public static final int COMMAND_PREFIX_OFFSET = 8;
+
+    /**
+     * Runs the deadline command, adding the deadline to storage and the task list
+     * @param input is the user input
+     * @param storage is the file object
+     * @param tasks is the task list
+     * @param ui is the ui object
+     * @throws InvalidCommandException when user input is invalid
+     * @throws StorageException when there is an issue with loading the storage file
+     */
     public static void run(String input, Storage storage, TaskList tasks, Ui ui) throws InvalidCommandException,
             StorageException {
 
         try {
-            String desc = input.substring(8).trim();
-            if (!desc.contains("/by")) {
-                throw new InvalidCommandException("Your deadline needs a /by YYYY-MM-DD input, don't cross the line");
-            }
-
-            String task = desc.split("/", 2)[0].trim();
-            if (task.isEmpty()) {
-                throw new InvalidCommandException("Where is your deadline description?!");
-            }
-
-            String by = desc.split("/by", 2)[1].trim();
-            Task deadline = new Deadline(task, LocalDate.parse(by), false);
+            Task deadline = getTask(input);
             tasks.addTask(deadline);
             addToStorage(storage, deadline);
             printTaskAdded(deadline, ui);
@@ -41,8 +42,23 @@ public class DeadlineCommand extends AddCommand {
         } catch (IndexOutOfBoundsException e) {
             throw new InvalidCommandException("You better format your deadline properly or else [REDACTED]");
         } catch (DateTimeException dte) {
-            throw new InvalidCommandException("You better format your deadline date in a valid YYYY-MM-DD format or " +
-                    "else...");
+            throw new InvalidCommandException("You better format your deadline date in a valid YYYY-MM-DD format or "
+                    + "else...");
         }
+    }
+
+    private static Task getTask(String input) throws InvalidCommandException {
+        String desc = input.substring(COMMAND_PREFIX_OFFSET).trim();
+        if (!desc.contains("/by")) {
+            throw new InvalidCommandException("Your deadline needs a /by YYYY-MM-DD input, don't cross the line");
+        }
+
+        String task = desc.split("/", 2)[0].trim();
+        if (task.isEmpty()) {
+            throw new InvalidCommandException("Where is your deadline description?!");
+        }
+
+        String by = desc.split("/by", 2)[1].trim();
+        return new Deadline(task, LocalDate.parse(by), false);
     }
 }
