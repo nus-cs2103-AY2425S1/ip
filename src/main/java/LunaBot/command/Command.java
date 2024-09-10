8b073b310dfb72818751e948b474ebdca3da252e
harithhh <e1156421@u.nus.edu>
2024-09-10 22:38:20 +0800

    fixed issues again

--- src/main/java/LunaBot/command/Command.java
+++ src/main/java/LunaBot/command/Command.java
@@ -1,9 +1,9 @@
-package LunaBot.command;
+package lunabot.command;
 
-import LunaBot.exception.LunaBotException;
-import LunaBot.storage.Storage;
-import LunaBot.task.TaskList;
-import LunaBot.ui.Ui;
+import lunabot.exception.LunaBotException;
+import lunabot.storage.Storage;
+import lunabot.task.TaskList;
+import lunabot.ui.Ui;
 
 /**
  * Abstract command that is executed by LunaBot
