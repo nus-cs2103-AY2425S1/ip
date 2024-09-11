176e283f1236f43f503b45216b89dcac550d1cae
NorbertLoh <lohnorbert@gmail.com>
2024-09-11 18:07:45 +0800

    Add more JUnit testing

--- src/test/java/cow/commands/TodoCommandTest.java
+++ src/test/java/cow/commands/TodoCommandTest.java
@@ -1,6 +1,7 @@
 package cow.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,28 @@ public class TodoCommandTest {
         Ui ui = new Ui();
         todo.execute(td, ui, fs);
         assertEquals(td.toString(), "1.[T][ ] test");
+    }
+
+    @Test
+    public void executeEmptyDescriptionTest() throws CowExceptions {
+        TodoList td = new TodoList();
+
+        TodoCommand todo = new TodoCommand("");
+        FileSaver fs = new FileSaver("data/cow.txt");
+        Ui ui = new Ui();
+        todo.execute(td, ui, fs);
+        assertEquals(td.toString(), "1.[T][ ] ");
+    }
+
+    @Test
+    public void executeWithExceptionTest() {
+        TodoList td = new TodoList();
+
+        TodoCommand todo = new TodoCommand("test");
+        FileSaver fs = new FileSaver("invalid/path/cow.txt");
+        Ui ui = new Ui();
+        assertThrows(CowExceptions.class, () -> {
+            todo.execute(td, ui, fs);
+        });
     }
 }
