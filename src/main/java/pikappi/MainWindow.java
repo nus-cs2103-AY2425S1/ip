0fef4db66cc31a544968fd22ce108e1fc3d08d55
xqtann <xqxinquan@gmail.com>
2024-09-11 15:12:36 +0800

    Add Javadoc for MainWindow.java

--- src/main/java/pikappi/MainWindow.java
+++ src/main/java/pikappi/MainWindow.java
@@ -27,6 +27,9 @@ public class MainWindow extends AnchorPane {
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image pikappiImage = new Image(this.getClass().getResourceAsStream("/images/pikappi.png"));
 
+    /**
+     * Initializes the main window.
+     */
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
