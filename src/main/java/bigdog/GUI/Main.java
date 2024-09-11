9a251bc5be6ca1971592780e6e787b4b7a485724
weibinlim <limweibin0816@gmail.com>
2024-09-11 17:27:52 +0800

    Use FXML
    Rename Gui.java to Main.java

---
+++ src/main/java/bigdog/GUI/Main.java
@@ -0,0 +1,34 @@
+package bigdog.GUI;
+
+import java.io.IOException;
+
+import bigdog.Bigdog;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    String storageFile = getClass().getClassLoader().getResource("Bigdog.txt").getPath();
+    private Bigdog bigdog = new Bigdog(storageFile);
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setBigdog(bigdog);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/bigdog/GUI/MainWindow.java
@@ -0,0 +1,55 @@
+package bigdog.GUI;
+
+import bigdog.Bigdog;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.util.Objects;
+
+public class MainWindow extends AnchorPane {
+
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Bigdog bigdog;
+
+    private Image userImage = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/po.png")));
+    private Image bigdogImage = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/oog.png")));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Bigdog instance */
+    public void setBigdog(Bigdog bot) {
+        bigdog = bot;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = bigdog.getResponse(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getBigdogDialog(dukeText, bigdogImage)
+        );
+        userInput.clear();
+    }
+
+}
