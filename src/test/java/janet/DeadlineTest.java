a740dc5b9c361320f29d75756ca639d8121dcc6d
rayray39 <raynerlimfy@gmail.com>
2024-09-11 22:43:23 +0800

    DeadlineTest.java: Update test methods
    Update test methods to ensure that the invalid creation of Deadline objects will print the right error messages for the user to see.

--- src/test/java/janet/DeadlineTest.java
+++ src/test/java/janet/DeadlineTest.java
@@ -17,12 +17,12 @@ public class DeadlineTest {
         if (d1 == null || d2 == null) {
             return false;
         }
-        if (Objects.equals(d1.getDescription(), d2.getDescription()) && Objects.equals(d1.getSymbol(), d2.getSymbol())) {
-            return Objects.equals(d1.getDueDate(), d2.getDueDate());
+        if (Objects.equals(d1.getDescription(), d2.getDescription())
+                && Objects.equals(d1.getSymbol(), d2.getSymbol())) {
+            return Objects.equals(d1.getScheduledDate(), d2.getScheduledDate());
         }
         return false;
     }
-
 
     /**
      * tests whether the 2 different Deadline constructors will produce the same object
@@ -34,44 +34,46 @@ public class DeadlineTest {
         DateTimeFormatter stringToDateTime = DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a");
         LocalDateTime dueDate = LocalDateTime.of(2024, 9, 1, 17, 0);
         dueDate.format(stringToDateTime);
+
         Deadline deadline_two = new Deadline("return book", "D", dueDate);
 
         assertTrue(equals(deadline_one, deadline_two));
     }
 
+    /**
+     * tests case where description of deadline is omitted and correct error message is printed.
+     */
+    @Test
+    public void Description_omission_test() throws JanetException {
+        // set the numOfTasksInList parameter to be some random number (example: 3)
+        String[] cd = new String[] {"deadline"};
+        JanetException exception = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand(cd, 3);});
 
-//    /**
-//     * tests case where description of deadline is omitted.
-//     */
-//    @Test
-//    public void Description_omission_test() throws JanetException {
-//        String[] cd = new String[] {"deadline"};
-//        JanetException exception = assertThrows(JanetException.class, () -> {Parser.checkInaccurateCommand(cd);});
-//
-//        assertEquals(exception.getMessage(), "WHOOPS! You can't leave out the task's description!");
-//    }
+        assertEquals(exception.getMessage(), "WHOOPS! You can't leave out the task's description!");
+    }
 
-//    /**
-//     * tests case where '/by' is omitted
-//     */
-//    @Test
-//    public void Inaccurate_deadline_test() {
-//        String[] cd = new String[] {"deadline", "return", "book"};
-//        JanetException exception = assertThrows(JanetException.class, () -> {Parser.validateDeadline(cd);});
-//
-//        assertEquals(exception.getMessage(), "WHOOPS! Missing/Wrong keywords for creating deadline...");
-//    }
-//
-//    /**
-//     * tests case where due date is omitted.
-//     */
-//    @Test
-//    public void DueDate_omission_test() throws JanetException {
-//        String[] cd = new String[] {"deadline", "return", "book", "/by"};
-//        JanetException exception = assertThrows(JanetException.class, () -> {Parser.validateDeadline(cd);});
-//
-//        assertEquals(exception.getMessage(), "WHOOPS! Missing/Wrong keywords for creating deadline...");
-//    }
+    /**
+     * tests case where '/by' is omitted and correct error message is printed.
+     */
+    @Test
+    public void Invalid_deadline_test() throws JanetException {
+        String[] cd = new String[] {"deadline", "return", "book"};
+        JanetException exception = assertThrows(JanetException.class,
+                () -> {new Deadline("deadline return book");});
 
+        assertEquals(exception.getMessage(), "WHOOPS! Missing/Wrong keywords for creating deadline...");
+    }
 
+    /**
+     * tests case where due date is omitted and correct error message is printed.
+     */
+    @Test
+    public void DueDate_omission_test() throws JanetException {
+        String[] cd = new String[] {"deadline", "return", "book", "/by"};
+        JanetException exception = assertThrows(JanetException.class,
+                () -> {new Deadline("deadline return book /by");});
+
+        assertEquals(exception.getMessage(), "WHOOPS! Missing/Wrong keywords for creating deadline...");
+    }
 }
