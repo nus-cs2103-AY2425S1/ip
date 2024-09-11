d40d8b7a5afe8aee349befa89634c4fdba2e9b7a
Yong Quan <57471791+yongqqqq@users.noreply.github.com>
2024-09-11 18:05:52 +0800

    Refactor Task classes
    Some of the names and comments are unclear.
    
    Let's refactor them to make it clearer.

--- src/main/java/fishman/task/Event.java
+++ src/main/java/fishman/task/Event.java
@@ -7,34 +7,32 @@ import java.time.format.DateTimeFormatter;
  * This class extends the Task class, adding the from and to attributes to it.
  */
 public class Event extends Task {
-    /** The start time of the event. */
-    protected LocalDateTime from;
-    /** The end time of the event. */
-    protected LocalDateTime to;
+    protected LocalDateTime eventStart;
+    protected LocalDateTime eventEnd;
 
     /**
      * Constructs a new Event task.
      *
-     * @param description The description of the task.
-     * @param from The start time of the event.
-     * @param to The end time of the event.
+     * @param taskDescription The description of the task.
+     * @param eventStart The start time of the event.
+     * @param eventEnd The end time of the event.
      */
-    public Event(String description, boolean isDone, LocalDateTime from, LocalDateTime to) {
-        super(description, isDone);
-        assert description != null : "Description should not be null";
-        assert from != null : "Event 'From' date should not be null";
-        assert to != null : "Event 'To' date should not be null";
-        assert from.isBefore(to) : "Event 'From' date should be before 'To' date";
-        this.from = from;
-        this.to = to;
+    public Event(String taskDescription, boolean isTaskDone, LocalDateTime eventStart, LocalDateTime eventEnd) {
+        super(taskDescription, isTaskDone);
+        assert taskDescription != null : "Description should not be null";
+        assert eventStart != null : "Event 'From' date should not be null";
+        assert eventEnd != null : "Event 'To' date should not be null";
+        assert eventStart.isBefore(eventEnd) : "Event 'From' date should be before 'To' date";
+        this.eventStart = eventStart;
+        this.eventEnd = eventEnd;
     }
 
-    public LocalDateTime getFrom() {
-        return from;
+    public LocalDateTime getEventStart() {
+        return eventStart;
     }
 
-    public LocalDateTime getTo() {
-        return to;
+    public LocalDateTime getEventEnd() {
+        return eventEnd;
     }
 
     @Override
@@ -45,6 +43,6 @@ public class Event extends Task {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm");
-        return super.toString() + " (from: " + from.format(formatter) + " to: " + to.format(formatter) + ")";
+        return super.toString() + " (from: " + eventStart.format(formatter) + " to: " + eventEnd.format(formatter) + ")";
     }
 }
