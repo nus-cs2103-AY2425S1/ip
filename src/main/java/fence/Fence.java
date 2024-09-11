f3b7f14e20013748f23ee58ea83d2f20a60131bd
jerrelllee <leejerrell337@gmail.com>
2024-09-11 11:52:10 +0800

    Replace UI with GUI

--- src/main/java/fence/Fence.java
+++ src/main/java/fence/Fence.java
@@ -3,7 +3,6 @@ package fence;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 import fence.parser.Parser;
 import fence.storage.Storage;
@@ -11,6 +10,9 @@ import fence.task.Task;
 import fence.tasklist.TaskList;
 import fence.ui.Ui;
 
+/**
+ * Represents the chatbot Fence.
+ */
 public class Fence {
 
     private Storage storage;
@@ -18,6 +20,10 @@ public class Fence {
     private Ui ui;
     private Parser parser;
 
+    /**
+     * Constructs an instance of Fence with the task list loaded in from the storage.
+     * If the data file is corrupted, it is initialised with an empty task list instead.
+     */
     public Fence() {
         ui = new Ui();
         storage = new Storage();
@@ -30,60 +36,60 @@ public class Fence {
         }
     }
 
-    public void run() {
-        ui.greet();
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            String command = scanner.nextLine();
-            try {
-                String commandType = parser.parseInput(command);
-                if (commandType.equals("bye")) {
-                    ui.exit();
-                    break;
-                } else if (commandType.equals("list")) {
-                    ui.list(tasks);
-                } else if (commandType.equals("task")) {
-                    Task task = parser.getTask();
-                    tasks.add(task);
-                    ui.add(task);
-                    ui.count(tasks.getSize());
-                    storage.saveAppend(task);
-                } else if (commandType.equals("mark")) {
-                    int index = parser.getIndex();
-                    tasks.mark(index);
-                    ui.mark(tasks.getTask(index - 1));
-                    storage.saveRewrite(tasks);
-                } else if (commandType.equals("unmark")) {
-                    int index = parser.getIndex();
-                    tasks.unmark(index);
-                    ui.unmark(tasks.getTask(index - 1));
-                    storage.saveRewrite(tasks);
-                } else if (commandType.equals("delete")) {
-                    int index = parser.getIndex();
-                    ui.delete(tasks.getTask(index - 1));
-                    tasks.delete(index);
-                    ui.count(tasks.getSize());
-                    storage.saveRewrite(tasks);
-                } else if (commandType.equals("find")) {
-                    String keyword = parser.getString();
-                    ui.list(tasks.find(keyword));
-                } else {
-                    ui.printUnknownCommand();
-                }
-            } catch (NoSuchElementException e) {
-                ui.printMissingFieldError();
-            } catch (DateTimeParseException e) {
-                ui.printInvalidDateError();
-            } catch (NumberFormatException e) {
-                ui.printInvalidNumberError();
-            }
-        }
+    /**
+     * Returns the default message upon launch.
+     * @return Default greeting.
+     */
+    public String getGreeting() {
+        return ui.greet();
     }
 
-    public static void main(String[] args) {
-        Fence fence = new Fence();
-        fence.run();
+    /**
+     * Returns the appropriate response for the respective commands inputted.
+     * @param command User input.
+     * @return Default response for that particular command type.
+     */
+    public String getResponse(String command) {
+        try {
+            String commandType = parser.parseInput(command);
+            if (commandType.equals("bye")) {
+                return ui.exit();
+            } else if (commandType.equals("list")) {
+                return ui.list(tasks);
+            } else if (commandType.equals("task")) {
+                Task task = parser.getTask();
+                tasks.add(task);
+                storage.saveAppend(task);
+                return ui.add(task) + "\n" + ui.count(tasks.getSize());
+            } else if (commandType.equals("mark")) {
+                int index = parser.getIndex();
+                tasks.mark(index);
+                storage.saveRewrite(tasks);
+                return ui.mark(tasks.getTask(index - 1));
+            } else if (commandType.equals("unmark")) {
+                int index = parser.getIndex();
+                tasks.unmark(index);
+                storage.saveRewrite(tasks);
+                return ui.unmark(tasks.getTask(index - 1));
+            } else if (commandType.equals("delete")) {
+                int index = parser.getIndex();
+                Task task = tasks.getTask(index - 1);
+                tasks.delete(index);
+                storage.saveRewrite(tasks);
+                return ui.delete(task) + "\n" + ui.count(tasks.getSize());
+            } else if (commandType.equals("find")) {
+                String keyword = parser.getString();
+                return ui.list(tasks.find(keyword));
+            } else {
+                return ui.printUnknownCommand();
+            }
+        } catch (NoSuchElementException e) {
+            return ui.printMissingFieldError();
+        } catch (DateTimeParseException e) {
+            return ui.printInvalidDateError();
+        } catch (NumberFormatException e) {
+            return ui.printInvalidNumberError();
+        }
     }
 }
 
