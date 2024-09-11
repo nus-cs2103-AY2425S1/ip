8459837d9d384a6d729fb1300f2de1ef4c0b4838
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-09-12 05:28:19 +0800

    Update DayToDateParser

--- src/main/java/duke/additionalparsers/DayToDateParser.java
+++ src/main/java/duke/additionalparsers/DayToDateParser.java
@@ -5,38 +5,50 @@ import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
 public class DayToDateParser {
-    public static LocalDate getNextDateForDayOfWeek(String s) {
+    public static LocalDate getNextDateForDay(String s) {
         switch (s.toLowerCase()) {
         case "mon":
             s = "monday";
+            break;
         case "tue":
             s = "tuesday";
+            break;
         case "wed":
             s = "wednesday";
-        case "thu":
+            break;
+        case "thurs":
             s = "thursday";
+            break;
         case "fri":
             s = "friday";
+            break;
         case "sat":
             s = "saturday";
+            break;
         case "sun":
             s = "sunday";
-        default:
             break;
         }
+        System.out.println(s);
         try {
             DayOfWeek dayOfWeek = DayOfWeek.valueOf(s.toUpperCase());
             LocalDate today = LocalDate.now();
-            LocalDate nextDate = today.with(TemporalAdjusters.next(dayOfWeek));
-            return nextDate;
+            DayOfWeek todayDayOfWeek = today.getDayOfWeek();
+            System.out.println(today);
+            System.out.println(dayOfWeek);
+            if (todayDayOfWeek.equals(dayOfWeek)) {
+                return today;
+            } else {
+                return today.with(TemporalAdjusters.next(dayOfWeek));
+            }
         } catch (Exception e){
             return null;
         }
     }
 
     public static void main(String[] args) {
-        String day = "nigg";
-        LocalDate nextDate = getNextDateForDayOfWeek(day);
+        String day = "monff";
+        LocalDate nextDate = getNextDateForDay(day);
         System.out.println("Next " + day + " is on: " + nextDate);
     }
 }
