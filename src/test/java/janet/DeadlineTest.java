f222469553b4915f2642f162b14150f89dd5f4a4
rayray39 <raynerlimfy@gmail.com>
2024-09-11 22:49:56 +0800

    DeadlineTest.java: Update DueDate_ommision_test to correct expected output

--- src/test/java/janet/DeadlineTest.java
+++ src/test/java/janet/DeadlineTest.java
@@ -74,6 +74,6 @@ public class DeadlineTest {
         JanetException exception = assertThrows(JanetException.class,
                 () -> {new Deadline("deadline return book /by");});
 
-        assertEquals(exception.getMessage(), "WHOOPS! Missing/Wrong keywords for creating deadline...");
+        assertEquals(exception.getMessage(), "WHOOPS! Ensure that the due date is in the format: yyyy-MM-dd hh:mm (24hr)");
     }
 }
