d7d30521126e29ceb97fa8256dce3c8940b11b44
dasha3412 <dashasychova777@gmail.com>
2024-09-11 22:45:57 +0800

    Remove IO redirection

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -19,22 +19,3 @@ then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
-
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Launcher < input.txt > ACTUAL.TXT
-
-sudo apt-get update
-sudo apt-get install dos2unix
-
-# compare the output to the expected output
-dos2unix -n EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
-if [ $? -eq 0 ]
-then
-    echo "Test result: PASSED"
-    exit 0
-else
-    echo "Test result: FAILED"
-    exit 1
-fi
