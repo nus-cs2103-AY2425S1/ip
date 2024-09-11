6ce52562fc78db49a779e77a08cd5df510bdf213
kahsuann <kahsuan@hotmail.sg>
2024-09-12 00:32:04 +0800

    Improve code quality

--- src/main/java/susan/command/AddCommand.java
+++ src/main/java/susan/command/AddCommand.java
@@ -22,6 +22,43 @@ public class AddCommand extends Command {
         this.commandParts = commandParts;
     }
 
+    public Task toDoTask() {
+        return new ToDo(commandParts[1]);
+    }
+
+    public Task deadlineTask() throws SusanException {
+        Task task;
+        String[] deadlineParts = commandParts[1].split(" /by ");
+        if (deadlineParts.length < 2) {
+            throw new SusanException("To add a deadline: deadline + description + /by yyyy-MM-dd");
+        }
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate by = LocalDate.parse(deadlineParts[1], formatter);
+            task = new Deadline(deadlineParts[0], by);
+        } catch (Exception e) {
+            throw new SusanException("Please key date in the format: yyyy-MM-dd");
+        }
+        return task;
+    }
+
+    public Task eventTask() throws SusanException {
+        Task task;
+        String[] eventParts = commandParts[1].split(" /from | /to ");
+        if (eventParts.length < 3) {
+            throw new SusanException("To add an event: event + description + /from yyyy-MM-dd HHmm + /to yyyy-MM-dd HHmm");
+        }
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+            LocalDateTime from = LocalDateTime.parse(eventParts[1], formatter);
+            LocalDateTime to = LocalDateTime.parse(eventParts[2], formatter);
+            task = new Event(eventParts[0], from, to);
+        } catch (Exception e) {
+            throw new SusanException("Please key date/time in the format: yyyy-MM-dd HHmm");
+        }
+        return task;
+    }
+
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws SusanException, IOException {
         if (commandParts.length < 2) {
@@ -31,34 +68,13 @@ public class AddCommand extends Command {
         Task task;
         switch (taskType) {
         case "todo":
-            task = new ToDo(commandParts[1]);
+            task = toDoTask();
             break;
         case "deadline":
-            String[] deadlineParts = commandParts[1].split(" /by ");
-            if (deadlineParts.length < 2) {
-                throw new SusanException("To add a deadline: deadline + description + /by yyyy-MM-dd");
-            }
-            try {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                LocalDate by = LocalDate.parse(deadlineParts[1], formatter);
-                task = new Deadline(deadlineParts[0], by);
-            } catch (Exception e) {
-                throw new SusanException("Please key date in the format: yyyy-MM-dd");
-            }
+            task = deadlineTask();
             break;
         case "event":
-            String[] eventParts = commandParts[1].split(" /from | /to ");
-            if (eventParts.length < 3) {
-                throw new SusanException("To add an event: event + description + /from yyyy-MM-dd HHmm + /to yyyy-MM-dd HHmm");
-            }
-            try {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
-                LocalDateTime from = LocalDateTime.parse(eventParts[1], formatter);
-                LocalDateTime to = LocalDateTime.parse(eventParts[2], formatter);
-                task = new Event(eventParts[0], from, to);
-            } catch (Exception e) {
-                throw new SusanException("Please key date/time in the format: yyyy-MM-dd HHmm");
-            }
+            task = eventTask();
             break;
         default:
             throw new SusanException("Failed to add task.");
