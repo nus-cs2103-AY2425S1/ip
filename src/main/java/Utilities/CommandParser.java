f0c8cb1229c13d05831a666329523b4879ae40c5
jialegitgud <jiale.official@gmail.com>
2024-09-11 16:05:02 +0800

    Add Enum for code clarity

--- src/main/java/Utilities/CommandParser.java
+++ src/main/java/Utilities/CommandParser.java
@@ -13,7 +13,25 @@ import tasks.TaskTypes;
 import tasks.ToDos;
 
 public class CommandParser {
-    private final static String MESSAGE_ONLIST = "Here are the tasks in your list: ";
+    private final static String MESSAGE_ONLIST = "Here are the tasks in your list: \n";
+    private enum Commands {
+        LIST("list"),
+        MARK("mark"),
+        UNMARK("unmark"),
+        FIND("find"),
+        TAG("tag"),
+        DELETE("delete"),
+        TODO("todo"),
+        DEADLINE("deadline"),
+        EVENT("event");
+
+
+        private final String command;
+
+        Commands(String command) {
+            this.command = command;
+        }
+    }
 
     /**
      * Parses and performs actions for the given command.
@@ -25,36 +43,43 @@ public class CommandParser {
      */
     public static String parseCommand(String input, TaskList tl, Storage store) {
         String response = "";
-        if (input.startsWith("list")) {
+        if (input.startsWith(Commands.LIST.command)) {
             response += MESSAGE_ONLIST;
             response += tl.toString();
-        } else if (input.startsWith("unmark") || input.startsWith("mark")) {
+        } else if (input.startsWith(Commands.UNMARK.command) || input.startsWith(Commands.MARK.command)) {
             // extract integer value
             String intValue = input.replaceAll("[^0-9]", "");
             int index = Integer.parseInt(intValue) - 1;
             response += tl.updateTaskListStatus(index, input.startsWith("mark"));
-            store.updateFileStatus(index, input.startsWith("mark"));
-        } else if (input.startsWith("delete")) {
+            store.updateTaskStatus(index, input.startsWith(Commands.MARK.command));
+        } else if (input.startsWith(Commands.DELETE.command)) {
             // extract integer value
             String intValue = input.replaceAll("[^0-9]", "");
             int index = Integer.parseInt(intValue) - 1;
             response += tl.removeFromTaskList(index);
             store.removeFileTask(index);
-        } else if (input.startsWith("find")) {
+        } else if (input.startsWith(Commands.FIND.command)) {
             String matchValue = input.replace("find", "").strip();
             response += tl.findTasks(matchValue);
+        } else if (input.startsWith(Commands.TAG.command)) {
+            String[] splits = input.split("/");
+            String intValue = splits[0].replaceAll("[^0-9]", "");
+            assert !intValue.isEmpty();
+            int index = Integer.parseInt(intValue) - 1;
+            response += tl.addTag(index, splits[1].strip());
+            store.updateTaskTag(index, splits[1].strip());
         } else {
             try {
                 String name = "";
-                if (input.startsWith("todo")) {
+                if (input.startsWith(Commands.TODO.command)) {
                     name = input.substring(4).strip();
                     if (name.isEmpty()) {
                         throw new BadDescriptionException(TaskTypes.TODO);
                     }
                     Task t = new ToDos(name);
                     response += tl.addToTaskList(t, name);
-                    store.updateFileTasks(String.format("T, %d, %s", 0, name));
-                } else if (input.startsWith("deadline")) {
+                    store.updateFileTasks(String.format("T, %d, %s, ", 0, name));
+                } else if (input.startsWith(Commands.DEADLINE.command)) {
                     String[] splits = input.split("/");
                     name = splits[0].substring(8).strip();
                     if (splits.length != 2 || name.isEmpty()) {
@@ -69,7 +94,7 @@ public class CommandParser {
                     } catch (DateTimeParseException ex) {
                         throw new DateTimeException(name);
                     }
-                } else if (input.startsWith("event")) {
+                } else if (input.startsWith(Commands.EVENT.command)) {
                     String[] splits = input.split("/");
                     name = splits[0].substring(5).strip();
                     if (splits.length != 3 || name.isEmpty()) {
