002e123f18f04769dfe0adc1d0ed38169e7b4570
gohqingkhang <gohqingkhang@gmail.com>
2024-09-11 15:54:14 +0800

    Extract error messages into constants
    Certain states in the parse method throw the same error message.
    
    These same error messages cause code duplication, and also results in
    untidy code.
    
    Let's extract these error messages from the parse method and store
    them as constants instead.
    
    Consolidating the error messages help when changes are needed and
    prevent typos.

--- src/main/java/delta/util/Parser.java
+++ src/main/java/delta/util/Parser.java
@@ -21,6 +21,58 @@ import delta.task.Todo;
  * Deals with making sense of user input.
  */
 public class Parser {
+    private static final String DATE_TIME_ERROR = """
+            OOPS!!! The format used for date/time is wrong!
+            \t Please follow the proper format:
+            \t yyyy-MM-dd HHmm
+            \t eg. 2024-08-25 1800""";
+    private static final String FIND_ERROR = """
+            OOPS!!! Description of task to find cannot be left blank!
+            \t Please follow the proper format:
+            \t * find [description]""";
+    private static final String TODO_ERROR = """
+            OOPS!!! The format for todo is wrong!
+            \t Please follow the proper format:
+            \t * todo [description]""";
+    private static final String DEADLINE_ERROR = """
+            OOPS!!! The format for deadline is wrong!
+            \t Please follow the proper format:
+            \t * deadline [description] /by [date/time]""";
+    private static final String EVENT_ERROR = """
+            OOPS!!! The format for event is wrong!
+            \t Please follow the proper format:
+            \t * event [description] /from [start] /to [end]""";
+    private static final String PAST_TIME_ERROR = """
+            OOPS!!! The date/time cannot be in the past!
+            \t Please set to a future date/time!""";
+    private static final String END_BEFORE_START_ERROR = """
+            OOPS!!! The end date cannot be before the start date!
+            \t Please set the correct date/time!""";
+    private static final String MARK_ERROR = """
+            OOPS!!! The format to mark tasks is wrong!
+            \t Please follow the proper format:
+            \t * mark [index of task]""";
+    private static final String UNMARK_ERROR = """
+            OOPS!!! The format to unmark tasks is wrong!
+            \t Please follow the proper format:
+            \t * unmark [index of task]""";
+    private static final String DELETE_ERROR = """
+            OOPS!!! The format to delete tasks is wrong!
+            \t Please follow the proper format:
+            \t * delete [index of task]""";
+    private static final String INDEX_ERROR = """
+            OOPS!!! The index of a task must be an integer!
+            \t Please input a valid index!""";
+    private static final String UNKNOWN_ERROR = """
+            OOPS!!! I'm sorry, but I don't know what that means :-(
+               Please follow the proper formats:
+               * todo [description]
+               * deadline [description] /by [date/time]
+               * event [description] /from [start] /to [end]
+               * mark [index of task]
+               * unmark [index of task]
+               * delete [index of task]""";
+
     /**
      * Formats a user typed date/time into proper format to be used by system.
      *
@@ -34,11 +86,7 @@ public class Parser {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
             return LocalDateTime.parse(input, formatter);
         } catch (DateTimeParseException e) {
-            throw new DeltaException("""
-                    OOPS!!! The format used for date/time is wrong!
-                    \t Please follow the proper format:
-                    \t yyyy-MM-dd HHmm
-                    \t eg. 2024-08-25 1800""");
+            throw new DeltaException(DATE_TIME_ERROR);
         }
     }
 
@@ -67,10 +115,7 @@ public class Parser {
                 String taskName = description[1].strip();
                 return new FindCommand(taskName);
             } else {
-                throw new DeltaException("""
-                        OOPS!!! Description of task to find cannot be left blank!
-                        \t Please follow the proper format:
-                        \t * find [description]""");
+                throw new DeltaException(FIND_ERROR);
             }
 
         // Add Todo
@@ -80,10 +125,7 @@ public class Parser {
                 return new AddCommand(new Todo(taskName));
             // Only task given, no description
             } else {
-                throw new DeltaException("""
-                        OOPS!!! Description of todo cannot be left blank!
-                        \t Please follow the proper format:
-                        \t * todo [description]""");
+                throw new DeltaException(TODO_ERROR);
             }
 
         // Add Deadline
@@ -96,26 +138,16 @@ public class Parser {
                     LocalDateTime by = formatDateTime(details[1].strip());
                     // Check if deadline is in the past
                     if (by.isBefore(LocalDateTime.now())) {
-                        throw new DeltaException("""
-                                OOPS!!! The date/time cannot be in the past!
-                                \t Please set to a future date/time!
-                                \t Follow the proper format:
-                                \t * deadline [description] /by [date/time]""");
+                        throw new DeltaException(PAST_TIME_ERROR);
                     }
                     return new AddCommand(new Deadline(taskName, by));
                 // More than one deadline given
                 } else {
-                    throw new DeltaException("""
-                            OOPS!!! The format for deadline is wrong!
-                            \t Please follow the proper format:
-                            \t * deadline [description] /by [date/time]""");
+                    throw new DeltaException(DEADLINE_ERROR);
                 }
             // Only task given, no description
             } else {
-                throw new DeltaException("""
-                        OOPS!!! Description of deadline cannot be left blank!
-                        \t Please follow the proper format:
-                        \t * deadline [description] /by [date/time]""");
+                throw new DeltaException(DEADLINE_ERROR);
             }
 
         // Add Event
@@ -132,113 +164,70 @@ public class Parser {
                         LocalDateTime end = formatDateTime(timings[1].strip());
                         // Check if timings are in the past
                         if (start.isBefore(LocalDateTime.now()) || end.isBefore(LocalDateTime.now())) {
-                            throw new DeltaException("""
-                                    OOPS!!! The date/time cannot be in the past!
-                                    \t Please set to a future date/time!
-                                    \t Follow the proper format:
-                                    \t * event [description] /from [start] /to [end]""");
+                            throw new DeltaException(PAST_TIME_ERROR);
                         // Check if end time is before start time
                         } else if (end.isBefore(start)) {
-                            throw new DeltaException("""
-                                    OOPS!!! The end date cannot be before the start date!
-                                    \t Please set the correct date/time!
-                                    \t Follow the proper format:
-                                    \t * event [description] /from [start] /to [end]""");
+                            throw new DeltaException(END_BEFORE_START_ERROR);
                         }
                         return new AddCommand(new Event(taskName, start, end));
                     // More than one end time given
                     } else {
-                        throw new DeltaException("""
-                                OOPS!!! The format for event is wrong!
-                                \t Please follow the proper format:
-                                \t * event [description] /from [start] /to [end]""");
+                        throw new DeltaException(EVENT_ERROR);
                     }
                 // More than one start time given
                 } else {
-                    throw new DeltaException("""
-                            OOPS!!! The format for event is wrong!
-                            \t Please follow the proper format:
-                            \t * event [description] /from [start] /to [end]""");
+                    throw new DeltaException(EVENT_ERROR);
                 }
             // Only task given, no description
             } else {
-                throw new DeltaException("""
-                        OOPS!!! Description of event cannot be left blank!
-                        \t Please follow the proper format:
-                        \t * event [description] /from [start] /to [end]""");
+                throw new DeltaException(EVENT_ERROR);
             }
 
         // Mark Task
         } else if (task.equalsIgnoreCase("mark")) {
             // Only task given, no description
             if (description.length != 2) {
-                throw new DeltaException("""
-                        OOPS!!! The format to mark tasks is wrong!
-                        \t Please follow the proper format:
-                        \t * mark [index of task]""");
+                throw new DeltaException(MARK_ERROR);
             }
             try {
                 int taskIdx = Integer.parseInt(description[1].strip());
                 return new MarkCommand(taskIdx);
             // Index given not an integer
             } catch (NumberFormatException e) {
-                throw new DeltaException("""
-                        OOPS!!! The index of task to mark must be an integer!
-                        \t Please follow the proper format:
-                        \t * mark [index of task]""");
+                throw new DeltaException(INDEX_ERROR);
             }
 
         // Unmark Task
         } else if (task.equalsIgnoreCase("unmark")) {
             // Only task given, no description
             if (description.length != 2) {
-                throw new DeltaException("""
-                        OOPS!!! The format to unmark tasks is wrong!
-                        \t Please follow the proper format:
-                        \t * unmark [index of task]""");
+                throw new DeltaException(UNMARK_ERROR);
             }
             try {
                 int taskIdx = Integer.parseInt(description[1].strip());
                 return new UnmarkCommand(taskIdx);
             // Index given not an integer
             } catch (NumberFormatException e) {
-                throw new DeltaException("""
-                        OOPS!!! The index of task to unmark must be an integer!
-                        \t Please follow the proper format:
-                        \t * unmark [index of task]""");
+                throw new DeltaException(INDEX_ERROR);
             }
 
         // Delete Task
         } else if (task.equalsIgnoreCase("delete")) {
             // Only task given, no description
             if (description.length != 2) {
-                throw new DeltaException("""
-                        OOPS!!! The format to delete tasks is wrong!
-                        \t Please follow the proper format:
-                        \t * delete [index of task]""");
+                throw new DeltaException(DELETE_ERROR);
             }
             try {
                 int taskIdx = Integer.parseInt(description[1].strip());
                 return new DeleteCommand(taskIdx);
             // Index given is not an integer
             } catch (NumberFormatException e) {
-                throw new DeltaException("""
-                        OOPS!!! The index of task to delete must be an integer!
-                        \t Please follow the proper format:
-                        \t * delete [index of task]""");
+                throw new DeltaException(INDEX_ERROR);
             }
 
         // Unknown Action
         } else {
-            throw new DeltaException("""
-                    OOPS!!! I'm sorry, but I don't know what that means :-(
-                       Please follow the proper formats:
-                       * todo [description]
-                       * deadline [description] /by [date/time]
-                       * event [description] /from [start] /to [end]
-                       * mark [index of task]
-                       * unmark [index of task]
-                       * delete [index of task]""");
+            throw new DeltaException(UNKNOWN_ERROR);
         }
     }
 }
