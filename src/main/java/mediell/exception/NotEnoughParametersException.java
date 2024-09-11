a6bd1d23c2353050e06743313c9e36eb0a0c3e0c
Cai Zhenzhi <caizhenzhi@Cais-MacBook-Air-2.local>
2024-09-11 16:47:38 +0800

    Document exception classes

--- src/main/java/mediell/exception/NotEnoughParametersException.java
+++ src/main/java/mediell/exception/NotEnoughParametersException.java
@@ -1,4 +1,7 @@
 package mediell.exception;
 
+/**
+ * An Exception that is called when the user does not pass in enough parameters
+ */
 public class NotEnoughParametersException extends Exception {
 }
