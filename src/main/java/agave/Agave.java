fa993bcb36e2d65f118430b9d5c099f5f7a807b7
Ved <ved.t.joshi@gmail.com>
2024-09-11 19:34:21 +0800

    Refactor main logic into CommandHandler class
    The Agave class currently contains a long chain of else-if statements
    for handling different commands, which makes the code harder to maintain
    and read. This commit refactors the main logic into a new CommandHandler.
    
    Changes:
    - Moved command processing from Agave into CommandHandler.
    - CommandHandler now handles all task commands like list, mark, unmark,
      todo, deadline, event, delete, and find.
    - Agave class now delegates command execution to CommandHandler,
      resulting in cleaner, more maintainable code.

--- src/main/java/agave/Agave.java
+++ src/main/java/agave/Agave.java
@@ -1,14 +1,13 @@
 package agave;
 
-import agave.Task.Task;
 import agave.Task.TaskList;
 import agave.Util.AgaveException;
 import agave.Util.Parser;
 import agave.Util.Storage;
 import agave.Util.Ui;
+import agave.Util.CommandHandler;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * The Agave class represents the main application that manages tasks.
@@ -20,7 +19,6 @@ public class Agave {
     private Storage storage;
     private TaskList tasks;
     private boolean isRunning;
-
 
     /**
      * Constructs an Agave instance with the specified file path for storage.
@@ -44,42 +42,12 @@ public class Agave {
         Parser parser = new Parser(userInput);
 
         try {
-            String command = parser.getCommand();
-            ui.showWelcome();
-            if (command.equals("bye")) {
-                isRunning = false;
-                response = ui.showBye();
-            } else if (command.equals("list")) {
-                response = ui.showTasks(tasks.getTasks());
-            } else if (command.equals("mark")) {
-                tasks.markTask(parser.getTaskNumber());
-                response = ui.showMarkedTask(tasks.getTask(parser.getTaskNumber() - 1));
-            } else if (command.equals("unmark")) {
-                tasks.unmarkTask(parser.getTaskNumber());
-                response = ui.showUnmarkedTask(tasks.getTask(parser.getTaskNumber() - 1));
-            } else if (command.equals("todo")) {
-                tasks.addTask(parser.parseTodo());
-                response = ui.showTaskAdded(tasks.getLastTask(), tasks.size(), tasks.getTasks());
-            } else if (command.equals("deadline")) {
-                tasks.addTask(parser.parseDeadline());
-                response = ui.showTaskAdded(tasks.getLastTask(), tasks.size(), tasks.getTasks());
-            } else if (command.equals("event")) {
-                tasks.addTask(parser.parseEvent());
-                response = ui.showTaskAdded(tasks.getLastTask(), tasks.size(), tasks.getTasks());
-            } else if (command.equals("delete")) {
-                tasks.deleteTask(parser.getTaskNumber());
-                response = "Task deleted!";
-            } else if (command.equals("find")) {
-                String key = parser.getKey();
-                ArrayList<Task> result = tasks.findTasks(key);
-                response = ui.searchResult(result);
-            } else {
-                throw new AgaveException("I'm sorry, but I don't understand the command: " + userInput);
-            }
+            response = CommandHandler.handleCommand(parser, tasks, ui);
             storage.saveTasks(tasks.getTasks());
         } catch (AgaveException | IOException e) {
             response = ui.showError(e.getMessage());
         }
+
         return response;
     }
 
@@ -99,50 +67,6 @@ public class Agave {
             }
         }
     }
-//    public void run() {
-//        ui.showWelcome();
-//
-//        while (isRunning) {
-//            try {
-//                String userInput = ui.getUserInput("Enter a command: ");
-//                Parser parser = new Parser(userInput);
-//                String command = parser.getCommand();
-//
-//                if (command.equals("bye")) {
-//                    isRunning = false;
-//                    ui.showBye();
-//                } else if (command.equals("list")) {
-//                    ui.showTasks(tasks.getTasks());
-//                } else if (command.equals("mark")) {
-//                    tasks.markTask(parser.getTaskNumber());
-//                    ui.showMarkedTask(tasks.getTask(parser.getTaskNumber() - 1));
-//                } else if (command.equals("unmark")) {
-//                    tasks.unmarkTask(parser.getTaskNumber());
-//                    ui.showUnmarkedTask(tasks.getTask(parser.getTaskNumber() - 1));
-//                } else if (command.equals("todo")) {
-//                    tasks.addTask(parser.parseTodo());
-//                    ui.showTaskAdded(tasks.getLastTask(), tasks.size(), tasks.getTasks());
-//                } else if (command.equals("deadline")) {
-//                    tasks.addTask(parser.parseDeadline());
-//                    ui.showTaskAdded(tasks.getLastTask(), tasks.size(), tasks.getTasks());
-//                } else if (command.equals("event")) {
-//                    tasks.addTask(parser.parseEvent());
-//                    ui.showTaskAdded(tasks.getLastTask(), tasks.size(), tasks.getTasks());
-//                } else if (command.equals("delete")) {
-//                    tasks.deleteTask(parser.getTaskNumber());
-//                } else if (command.equals("find")) {
-//                    String key = parser.getKey();
-//                    ArrayList<Task> result = tasks.findTasks(key);
-//                    ui.searchResult(result);
-//                } else {
-//                    throw new AgaveException("I'm sorry, but I don't understand the command: " + userInput);
-//                }
-//                storage.saveTasks(tasks.getTasks());
-//            } catch (AgaveException | IOException e) {
-//                ui.showError(e.getMessage());
-//            }
-//        }
-//    }
 
     public static void main(String[] args) {
         new Agave("./data/agave.txt").run();
