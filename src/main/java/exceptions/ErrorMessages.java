8666fec1ba302af9c3411487f0bd02b3cdda1824
OrangeCatLoves <e1121707@u.nus.edu>
2024-09-12 02:21:36 +0800

    Replaced traditional loops and manual collection operations with Java Streams. Simplified data processing and transformation tasks, enhancing code readability. Improved performance by leveraging parallel streams where appropriate. Updated related unit tests to reflect changes and ensure functionality. This refactor enhances maintainability and leverages modern Java features for more efficient and expressive code.

--- src/main/java/exceptions/ErrorMessages.java
+++ src/main/java/exceptions/ErrorMessages.java
@@ -5,4 +5,12 @@ package exceptions;
  */
 public class ErrorMessages {
     public static final String ARRAY_OUT_OF_BOUNDS = "Array index out of bounds";
+    public static final String INCORRECT_FORMAT_FOR_ADDING_TODO_COMMAND = "";
+    public static final String INCORRECT_FORMAT_FOR_ADDING_DEADLINE_COMMAND = "";
+    public static final String INCORRECT_FORMAT_FOR_ADDING_EVENT_COMMAND = "";
+    public static final String INCORRECT_FORMAT_FOR_FIND_COMMAND = "";
+    public static final String INCORRECT_FORMAT_FOR_ON_COMMAND = "";
+    public static final String INCORRECT_FORMAT_FOR_CHANGEPRIORITY_COMMAND = "";
+    public static final String INCORRECT_FORMAT_FOR_DELETE_COMMAND = "";
+    
 }
