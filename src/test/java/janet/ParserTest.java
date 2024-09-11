e59d4c5227077baf29b0ad04ca031d3aed1865e6
rayray39 <raynerlimfy@gmail.com>
2024-09-11 23:46:14 +0800

    ParserTest.java: Update test methods
    Update test methods to ensure that invalid user commands will return the correct error messages.

--- src/test/java/janet/ParserTest.java
+++ src/test/java/janet/ParserTest.java
@@ -7,23 +7,102 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
 
-//    @Test
-//    public void unknownInputTest() {
-//        JanetException exception = assertThrows(JanetException.class,
-//                () -> Parser.checkInaccurateCommand(new String[]{"blah"}), 3);
-//
-//        assertEquals(
-//                exception.getMessage(), "WHOOPS! I'm only a chatbot, so I don't know what that means..."
-//        );
-//    }
-//
-//    @Test
-//    public void noTaskSpecifiedTest() {
-//        JanetException exception = assertThrows(JanetException.class,
-//                () -> Parser.checkInaccurateCommand(new String[]{"mark"}), 3);
-//
-//        assertEquals(
-//                exception.getMessage(), "WHOOPS! I don't know which task you are referring to..."
-//        );
-//    }
+    @Test
+    public void unknownInputTest() {
+        JanetException e1 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand(new String[]{"blah"}, 3);});   // unknown command
+
+        JanetException e2 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("".split(" "), 3);});   // empty command
+
+        JanetException e3 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("     ".split(" "), 3);});  // empty command
+
+        JanetException e4 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand(new String[]{"find"}, 3);});   // not found command
+
+        assertEquals(
+                e1.getMessage(),
+                "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+        assertEquals(
+                e2.getMessage(),
+                "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+        assertEquals(
+                e3.getMessage(),
+                "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+        assertEquals(
+                e4.getMessage(),
+                "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+    }
+
+    @Test
+    public void invalidViewCommandTest() {
+        JanetException e1 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand(new String[]{"view"}, 3);});   // no date provided
+
+        // more than 1 date provided (string)
+        JanetException e2 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("view something and something".split(" "), 3);});
+
+        // more than 1 date provided (date)
+        JanetException e3 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("view 2024-09-10 2024-09-11".split(" "), 3);});
+
+        // 1 date provided but in wrong format
+        JanetException e4 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("view 2024/09/10".split(" "), 3);});
+
+        assertEquals(
+                e1.getMessage(), "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+        assertEquals(
+                e2.getMessage(), "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+        assertEquals(
+                e3.getMessage(), "WHOOPS! I'm only a chatbot, so I don't know what that means..."
+        );
+        assertEquals(
+                e4.getMessage(), "WHOOPS! Please ensure date is in yyyy-MM-dd format!"
+        );
+    }
+
+    @Test
+    public void taskNumberAbsentTest() {
+        // no task number
+        JanetException e1 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand(new String[]{"mark"}, 3);});
+
+        // task number invalid (cannot be parsed into integer)
+        JanetException e2 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("unmark something".split(" "), 3);});
+
+        // more than 1 task number provided and invalid
+        JanetException e3 = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("delete 3 and that".split(" "), 3);});
+
+        assertEquals(
+                e1.getMessage(), "WHOOPS! Ensure task number is present and valid!"
+        );
+        assertEquals(
+                e2.getMessage(), "WHOOPS! Ensure task number is present and valid!"
+        );
+        assertEquals(
+                e3.getMessage(), "WHOOPS! Ensure task number is present and valid!"
+        );
+    }
+
+    @Test
+    public void taskNumberOutOfBoundsTest() {
+        int numOfTasksInList = 5;
+        JanetException exception = assertThrows(JanetException.class,
+                () -> {Parser.checkInaccurateCommand("mark 6".split(" "), numOfTasksInList);});
+
+        assertEquals(
+                exception.getMessage(), "WHOOPS! You don't have a task of this number!"
+        );
+    }
 }
