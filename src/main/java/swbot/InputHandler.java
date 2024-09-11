28b7f25ab813ee864d6817f4e0d7c9f3c7992de6
marraknas <sankarram2002ybis@gmail.com>
2024-09-11 23:17:32 +0800

    Refactored InputHandler.java

--- src/main/java/swbot/InputHandler.java
+++ src/main/java/swbot/InputHandler.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 public class InputHandler {
     private ArrayList<Task> data;
     private Storage storage;
-    private String hline = "____________________________________________________________";
 
     /**
      * Creates an inputHandler object that will take care of all the user inputs
@@ -27,36 +26,34 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot
      * @throws BuzzException if any of the commands given by the user is not a valid one
      */
-    public void overallHandler(String input) throws BuzzException {
+    public String overallHandler(String input) throws BuzzException {
         if (input.startsWith("mark")) {
-            this.markHandle(input);
             this.storage.saveTasks(data);
+            return this.markHandle(input);
         } else if (input.startsWith("unmark")) {
-            this.unmarkHandle(input);
             this.storage.saveTasks(data);
+            return this.unmarkHandle(input);
         } else if (input.startsWith("delete")) {
-            this.deleteHandle(input);
             this.storage.saveTasks(data);
+            return this.deleteHandle(input);
         } else if (input.equals("list")) {
-            System.out.println(hline);
-
+            StringBuilder res = new StringBuilder();
             for (int i = 0; i < data.size(); i++) {
-                System.out.println((i + 1) + "." + data.get(i).toString());
+                res.append((i + 1)).append(".").append(data.get(i).toString());
             }
-
-            System.out.println(hline);
+            return res.toString();
         } else if (input.startsWith("todo")) {
-            this.todoHandle(input);
             this.storage.saveTasks(data);
+            return this.todoHandle(input);
         } else if (input.startsWith("deadline")) {
-            this.deadlineHandle(input);
             this.storage.saveTasks(data);
+            return this.deadlineHandle(input);
         } else if (input.startsWith("event")) {
-            this.eventHandle(input);
             this.storage.saveTasks(data);
+            return this.eventHandle(input);
         } else if (input.startsWith("find")) {
-            this.findHandle(input);
             this.storage.saveTasks(data);
+            return this.findHandle(input);
         } else {
             throw new BuzzException("GRRR! I do not know what that means. Try again! *bzzrg*");
         }
@@ -68,16 +65,13 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot
      * @throws BuzzException if the index being asked to mark is out of range/ non existent
      */
-    public void markHandle(String input) throws BuzzException {
+    public String markHandle(String input) throws BuzzException {
         int index = Integer.parseInt(input.split(" ")[1]) - 1;
         if (index < 0 || index >= data.size()) {
             throw new BuzzException("OOPS!!! The task doesn't exist *BEEP*");
         }
         data.get(index).setDone(true);
-        System.out.println(hline);
-        System.out.println("Mission accomplished! *bzzt*");
-        System.out.println(data.get(index).toString());
-        System.out.println(hline);
+        return "Mission accomplished! *bzzt*\n" + data.get(index).toString();
     }
 
     /**
@@ -86,16 +80,13 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot
      * @throws BuzzException if the index being asked to unmark is out of range/non-existent
      */
-    public void unmarkHandle(String input) throws BuzzException {
+    public String unmarkHandle(String input) throws BuzzException {
         int index = Integer.parseInt(input.split(" ")[1]) - 1;
         if (index < 0 || index >= data.size()) {
             throw new BuzzException("OOPS!!! The task doesn't exist *BEEP*");
         }
         data.get(index).setDone(false);
-        System.out.println(hline);
-        System.out.println("Argh next time! *bzzt*");
-        System.out.println(data.get(index).toString());
-        System.out.println(hline);
+        return "Argh next time! *bzzt*\n" + data.get(index).toString();
     }
 
     /**
@@ -104,18 +95,14 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot for deletion
      * @throws BuzzException if the index being asked to delete is out of range or does not exist in the list
      */
-    public void deleteHandle(String input) throws BuzzException {
+    public String deleteHandle(String input) throws BuzzException {
         int index = Integer.parseInt(input.split(" ")[1]) - 1;
         if (index < 0 || index >= data.size()) {
             throw new BuzzException("OOPS!!! The task doesn't exist *BEEP*");
         }
         Task task = data.remove(index);
-        System.out.println(hline);
-        System.out.println("*POOF* Circuits fried! Deleted the mission.");
-        System.out.println(task.toString());
-        System.out.println("You currently have " + data.size() + " missions available *reeeee* ");
-        System.out.println(hline);
-
+        return "*POOF* Circuits fried! Deleted the mission.\n" + task.toString() + "\n"
+                + "You currently have " + data.size() + " missions available *reeeee*";
     }
 
     /**
@@ -124,17 +111,14 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot to add to the list
      * @throws BuzzException if the description of the task was not specified
      */
-    public void todoHandle(String input) throws BuzzException {
+    public String todoHandle(String input) throws BuzzException {
         if (input.length() <= 5) {
             throw new BuzzException("NOOO! Description is empty *crash*");
         }
         Task task = new Todo(input.substring(5));
         data.add(task);
-        System.out.println(hline);
-        System.out.println("Understood boss. Added!");
-        System.out.println(task.toString());
-        System.out.println("You currently have " + data.size() + " missions available *reeeee* ");
-        System.out.println(hline);
+        return "Understood boss. Added!\n" + task.toString() + "\n"
+                + "You currently have " + data.size() + " missions available *reeeee*";
     }
 
     /**
@@ -143,18 +127,15 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot to add as a deadline
      * @throws BuzzException if the description of the deadline task is empty
      */
-    public void deadlineHandle(String input) throws BuzzException {
+    public String deadlineHandle(String input) throws BuzzException {
         String[] parts = input.split("/by");
         if (parts.length < 2 || parts[0].length() <= 9) {
             throw new BuzzException("NOOO! Description is empty *crash*");
         }
         Task task = new Deadline(parts[0].substring(9), parts[1].trim());
         data.add(task);
-        System.out.println(hline);
-        System.out.println("Understood boss. Added!");
-        System.out.println(task.toString());
-        System.out.println("You currently have " + data.size() + " missions available *reeeee* ");
-        System.out.println(hline);
+        return "Understood boss. Added!\n" + task.toString() + "\n"
+                + "You currently have " + data.size() + " missions available *reeeee*";
     }
 
     /**
@@ -163,18 +144,15 @@ public class InputHandler {
      * @param input user input that is being passed to the chatbot to add as an event
      * @throws BuzzException if description of the event is empty
      */
-    public void eventHandle(String input) throws BuzzException {
+    public String eventHandle(String input) throws BuzzException {
         String[] parts = input.split("/");
         if (parts.length < 3 || parts[0].length() <= 6) {
             throw new BuzzException("OOPS!!! The description of an event cannot be empty.");
         }
         Task task = new Event(parts[0].substring(6), parts[1].substring(5), parts[2].substring(3));
         data.add(task);
-        System.out.println(hline);
-        System.out.println("Understood boss. Added!");
-        System.out.println(task.toString());
-        System.out.println("You currently have " + data.size() + " missions available *reeeee* ");
-        System.out.println(hline);
+        return "Understood boss. Added!\n" + task.toString() + "\n"
+                + "You currently have " + data.size() + " missions available *reeeee*";
     }
 
     /**
@@ -183,24 +161,23 @@ public class InputHandler {
      * @param input user input provided to the chatbot
      * @throws BuzzException if description is empty
      */
-    public void findHandle(String input) throws BuzzException {
+    public String findHandle(String input) throws BuzzException {
+        StringBuilder result = new StringBuilder("I have found a few matches sir! *wooop*\n");
         if (input.length() <= 5) {
             throw new BuzzException("NOOO! Description is empty *crash*");
         }
         String wordSearch = input.substring(5).trim();
-        System.out.println(hline);
-        System.out.println("I have found a few matches sir! *wooop*");
         int tasks = 1;
         for (int i = 0; i < data.size(); i++) {
             Task task = data.get(i);
             if (task.description.contains(wordSearch)) {
-                System.out.println(tasks + "." + task.toString());
+                result.append(tasks).append(".").append(task.toString());
                 tasks++;
             }
         }
         if (tasks == 1) {
-            System.out.println("Sorry boss can't find anything :(");
+            return "Sorry boss can't find anything :(";
         }
-        System.out.println(hline);
+        return result.toString();
     }
 }
