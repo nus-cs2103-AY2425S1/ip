2f6164559224760644c9563db7b226de4906f49c
WindiStone <e0958609@u.nus.edu>
2024-09-11 11:05:25 +0800

    Add assert to command

--- src/main/java/commands/AddEvent.java
+++ src/main/java/commands/AddEvent.java
@@ -31,25 +31,22 @@ public class AddEvent extends Command {
     public boolean execute(String input, Reminder reminder, Ui ui, History history)
             throws EmptyDescriptionException {
         String[] command = input.split(" ", 2);
-        if (command.length == 2) {
-            String[] order = command[1].split(" /from ");
-            if (order.length == 2) {
-                String[] fillerName = order[1].split(" /to ");
-                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-                DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-                LocalDateTime start = LocalDateTime.parse(fillerName[0], dateFormatter);
-                LocalDateTime end = LocalDateTime.parse(fillerName[1], dateFormatter);
-                Event eventTask = new Event(order[0], start, end);
-                reminder.addEvent(eventTask);
-                ui.print("Got it. I've added this task:");
-                ui.print("    " + eventTask.toString());
-                ui.print("Now you have " + reminder.size() + " tasks in the list.");
-                history.save(reminder.getSchedule());
-            } else {
-                throw new EmptyDescriptionException("WHEN EVENT DATE!");
-            }
+        assert(command.length == 2);
+        String[] order = command[1].split(" /from ");
+        if (order.length == 2) {
+            String[] fillerName = order[1].split(" /to ");
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+            LocalDateTime start = LocalDateTime.parse(fillerName[0], dateFormatter);
+            LocalDateTime end = LocalDateTime.parse(fillerName[1], dateFormatter);
+            Event eventTask = new Event(order[0], start, end);
+            reminder.addEvent(eventTask);
+            ui.print("Got it. I've added this task:");
+            ui.print("    " + eventTask.toString());
+            ui.print("Now you have " + reminder.size() + " tasks in the list.");
+            history.save(reminder.getSchedule());
         } else {
-            throw new EmptyDescriptionException("I NEED TO KNOW WHAT I'M EVENT-ING!");
+            throw new EmptyDescriptionException("WHEN EVENT DATE!");
         }
         return true;
     }
--- src/main/java/commands/AddUnmark.java
+++ src/main/java/commands/AddUnmark.java
@@ -29,6 +29,7 @@ public class AddUnmark extends Command {
     public boolean execute(String input, Reminder reminder, Ui ui, History history)
             throws EmptyDescriptionException, TooManyParametersException {
         String[] command = input.split(" ");
+        assert(command.length == 2);
         if (command.length == 2) {
             ui.print("OK, I've marked this task as not done yet:");
             reminder.unmark(Integer.parseInt(command[1]) - 1);
