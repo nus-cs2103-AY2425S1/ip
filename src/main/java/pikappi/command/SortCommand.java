073c297b47126388a68340ff77922b4ee91bb7a5
xqtann <xqxinquan@gmail.com>
2024-09-11 17:50:57 +0800

    Add Javadocs in SortCommand.java

--- src/main/java/pikappi/command/SortCommand.java
+++ src/main/java/pikappi/command/SortCommand.java
@@ -5,13 +5,30 @@ import pikappi.TaskList;
 import pikappi.Ui;
 import pikappi.exception.PikappiException;
 
+/**
+ * Represents a command by user to sort the list of tasks.
+ */
 public class SortCommand extends Command {
     private String sortBy;
 
+    /**
+     * Returns a SortCommand object that sorts the list of tasks by the specified criteria.
+     *
+     * @param sortBy Criteria to sort the list of tasks by
+     */
     public SortCommand(String sortBy) {
         this.sortBy = sortBy;
     }
 
+    /**
+     * Sorts the list of tasks by the specified criteria.
+     *
+     * @param tasks List of tasks.
+     * @param ui Ui object that interacts with the user.
+     * @param storage The storage object.
+     * @return List of tasks sorted by the specified criteria.
+     * @throws PikappiException If the specified criteria is invalid.
+     */
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws PikappiException {
         assert tasks != null : "TaskList cannot be null";
