555f5b832b26fc1f8537d1dc638b1d6a9a59e4fb
wenjebs <wenjieb2@gmail.com>
2024-09-11 17:53:12 +0800

    Refactor WenJigglyBot class

--- src/main/java/wenjigglybot/WenJigglyBot.java
+++ src/main/java/wenjigglybot/WenJigglyBot.java
@@ -38,8 +38,10 @@ public class WenJigglyBot {
     /**
      * Runs the main loop of the WenJigglyBot application.
      * Continuously takes user input, parses commands, and executes the appropriate actions.
+     *
+     * @return
      */
-    public void run() {
+    public String run() {
         Scanner scanner = new Scanner(System.in);
         ui.intro();
         String task;
@@ -54,97 +56,17 @@ public class WenJigglyBot {
                 System.out.println(e);
                 continue;
             }
-
-            String[] strings;
-            String action;
-            int idx;
-            String taskName;
-
-            switch (Objects.requireNonNull(command)) {
-            case LIST:
-                ui.displayTasks(tasks);
-                break;
-            case MARK:
-                strings = task.split(" ");
-                action = "mark";
-                idx = Integer.parseInt(strings[1].trim()) - 1;
-                toggleTask(action, idx);
-                break;
-            case UNMARK:
-                action = "unmark";
-                strings = task.split(" ");
-                idx = Integer.parseInt(strings[1].trim()) - 1;
-                toggleTask(action, idx);
-                break;
-            case TODO:
-                taskName = task.replaceFirst("todo", "").trim();
-                addTask(new ToDoTask(taskName));
-                break;
-            case DEADLINE:
-                try {
-                    String[] parts = Parser.processDeadlineTask(task);
-                    taskName = parts[0].trim();
-                    String deadline = parts[1].trim();
-                    LocalDate date;
-                    try {
-                        date = LocalDate.parse(deadline);
-                        addTask(new DeadlineTask(taskName, date));
-                    } catch (DateTimeParseException e) {
-                        System.out.println("Incorrect date format, please input in yyyy-mm-dd format!");
-                    }
-                } catch (DeadlineException deadlineException) {
-                    System.out.println(deadlineException);
-                }
-                break;
-            case EVENT:
-                // Split the string by "/from" and "/to"
-                try {
-                    String[] processedEvent = Parser.processEventTask(task);
-                    addTask(new EventTask(processedEvent[0], processedEvent[1], processedEvent[2]));
-                } catch (EventException eventException) {
-                    System.out.println(eventException);
-                }
-                break;
-            case FIND:
-                strings = task.split(" ");
-                String title = strings[1];
-                List<Task> matchingTasks = tasks.searchAndListTasks(title);
-                ui.displayTasks(new TaskList(matchingTasks));
-                break;
-            case DELETE:
-                strings = task.split(" ");
-                idx = Integer.parseInt(strings[1].trim()) - 1;
-                deleteTask(idx);
-                break;
-            case BYE:
-                flag = false;
-                break;
-            default:
-                System.out.println("Invalid command!");
-            }
-
+            return handleCommand(command, task);
         }
         System.out.println("Goodbye!");
+        return "";
     }
 
-    /**
-     * Parses and processes a task
-     */
-    public String parseAndProcess(String task) {
-        task = task.trim();
-        Command command = null;
-        try {
-            command = Parser.parseCommand(task);
-        } catch (InvalidCommandException e) {
-            System.out.println(e);
-            return "Invalid command!";
-        }
-
+    private String handleCommand(Command command, String task) {
+        int idx;
         String[] strings;
         String action;
-        int idx;
         String taskName;
-
         switch (Objects.requireNonNull(command)) {
         case LIST:
             return ui.displayTasks(tasks);
@@ -204,8 +126,22 @@ public class WenJigglyBot {
         default:
             System.out.println("Invalid command!");
         }
-        System.out.println("Goodbye!");
         return "";
+    }
+
+    /**
+     * Parses and processes a task
+     */
+    public String parseAndProcess(String task) {
+        task = task.trim();
+        Command command = null;
+        try {
+            command = Parser.parseCommand(task);
+        } catch (InvalidCommandException e) {
+            System.out.println(e);
+            return "Invalid command!";
+        }
+        return handleCommand(command, task);
     }
 
     /**
