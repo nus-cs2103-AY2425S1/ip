a556a1a938eeeef35cac4b63347529efc65dd01b
volleyballkickedme <89293409+volleyballkickedme@users.noreply.github.com>
2024-09-12 15:25:50 +0800

    Add assert statements

--- src/main/java/FRIDAY/Ui.java
+++ src/main/java/FRIDAY/Ui.java
@@ -7,22 +7,7 @@ import java.util.Scanner;
  * User interface of the app
  */
 public class Ui {
-    private static final String GREETING_MESSAGE = "Hello! I'm FRIDAY\nWhat can I do for you?\n";
-    private static final String FAREWELL_MESSAGE = "Bye. Hope to see you again soon!\n";
     private static final String DIVIDER = "----------------------------------------\n";
-    private Scanner scanner;
-
-    public Ui() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public String greeting() {
-        return DIVIDER + GREETING_MESSAGE + DIVIDER;
-    }
-
-    public String farewell() {
-        return DIVIDER + FAREWELL_MESSAGE + DIVIDER;
-    }
 
     /**
      * Prints out message upon adding of task to the task list
@@ -51,18 +36,6 @@ public class Ui {
     public String printUncheck() {
         return DIVIDER + "Noted. I've marked this task as incomplete\n" + DIVIDER;
     }
-    public String readUserInput() {
-        return this.scanner.nextLine();
-    }
-
-    public boolean isActive() {
-        return this.scanner.hasNextLine();
-    }
-
-    public void display(String input) {
-        System.out.println(input);
-    }
-
     /**
      * method displays the list of tasks in a specific format
      * @param searchResults array list of tasks
@@ -70,7 +43,7 @@ public class Ui {
     public String displaySearchResults(ArrayList<Task> searchResults) {
         StringBuilder output = new StringBuilder(DIVIDER + "Here are the matching tasks from your list\n:");
         searchResults.forEach((task) -> {
-            output.append(task);
+            output.append(task + "\n");
         });
         output.append("\n" + DIVIDER);
         return output.toString();
