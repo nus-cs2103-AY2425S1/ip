42815514d6dd38b5376532ad37d59877dcb15e5f
myrilla12 <myrilla12@gmail.com>
2024-09-12 14:22:07 +0800

    Refactor getResponse method
    Method is very long and does not fully follow SLAP due to all the command handling being thrown into the switch case block.
    
    Extracting methods improves the code quality.
    
    As a step towards improving the code quality, let's extract all the command handling into separate methods for each command. Doing so will make the method shorter and improve readability as well as ensure adherence to SLAP.

--- src/main/java/eevee/Eevee.java
+++ src/main/java/eevee/Eevee.java
@@ -33,117 +33,125 @@ public class Eevee {
     }
 
     public Eevee() {
-        this.ui = new Ui();
-        this.storage = new Storage(DEFAULT_FILE_PATH);
-        this.tasks = new TaskList();
-        this.parser = new Parser();
+        this(DEFAULT_FILE_PATH);
+    }
 
-        try {
-            storage.loadTasks(tasks);
-        } catch (FileNotFoundException e) {
-            ui.printMessage("File not found!");
+    public String getResponse(String input) throws EeveeException, IOException {
+        Parser.Command command = parser.parseCommand(input);
+
+        switch (command) {
+        case BYE:
+            return ui.getExit();
+        case LIST:
+            return tasks.listTasks();
+        case MARK:
+            return handleMarkCommand(input);
+        case UNMARK:
+            return handleUnmarkCommand(input);
+        case DELETE:
+            return handleDeleteCommand(input);
+        case TODO:
+            return handleTodoCommand(input);
+        case DEADLINE:
+            return handleDeadlineCommand(input);
+        case EVENT:
+            return handleEventCommand(input);
+
+        case FIND:
+            return handleFindCommand(input);
+        default:
+            throw new EeveeException("You seemed to have typed wrong. This is not a valid command.");
         }
     }
 
-    public String getResponse(String input) {
-        try {
-            Parser.Command command = parser.parseCommand(input);
-
-            switch (command) {
-            case BYE:
-                return ui.getExit();
-            case LIST:
-                return tasks.listTasks();
-            case MARK: {
-                int taskNumber = parser.parseTaskNumber(input);
-                Task t = tasks.getTask(taskNumber);
-                if (t.isDone) {
-                    throw new EeveeException("Task has already been marked as done.");
-                }
-                t.markAsDone();
-                storage.saveTasks(tasks);
-                return "Congratulations! I've marked the following task as done:\n  " + t;
-            }
-            case UNMARK: {
-                int taskNumber = parser.parseTaskNumber(input);
-                Task t = tasks.getTask(taskNumber);
-                if (!t.isDone) {
-                    throw new EeveeException("Task is not marked as done. "
-                            + "Needs to be marked done in order to unmark it.");
-                }
-                t.unmarkAsDone();
-                storage.saveTasks(tasks);
-                return "Ok! Task no longer marked as done:\n  " + t;
-            }
-            case DELETE: {
-                int taskNumber = parser.parseTaskNumber(input);
-                Task t = tasks.getTask(taskNumber);
-                tasks.removeTask(taskNumber);
-                storage.saveTasks(tasks);
-                return "As you wish, this task has been removed:\n " + t;
-            }
-            case TODO: {
-                String s = input.substring(5).trim();
-                if (s.isEmpty()) {
-                    throw new EeveeException("No task found :( "
-                            + "Please input the task details and description correctly");
-                }
-                Todo t = new Todo(s);
-                tasks.addTask(t);
-                storage.saveTasks(tasks);
-                return "Added the following task to your list:\n" + t;
-            }
-            case DEADLINE: {
-                String[] info = input.substring(9).trim().split("/by", 2);
-                if (info.length < 2 || info[0].isEmpty() || info[1].isEmpty()) {
-                    throw new EeveeException("Description or deadline not given for task type 'deadline'. "
-                            + "Make sure the deadline is denoted by '/by'.");
-                }
-
-                // Create and store task
-                Deadline d = new Deadline(info[0], info[1]);
-                tasks.addTask(d);
-                storage.saveTasks(tasks);
-                return "Added the following task to your list:\n" + d;
-            }
-            case EVENT: {
-                String[] info = input.substring(6).trim().split("/from|/to", 3);
-                if (info.length < 3 || info[0].isEmpty() || info[1].isEmpty() || info[2].isEmpty()) {
-                    throw new EeveeException("Event description, start and/or end timings not provided. "
-                            + "Please input a start time denoted by '/from' "
-                            + "and an end time denoted by '/to' when using task type eevee.Event");
-                }
-
-                // Create and store task
-                Event e = new Event(info[0], info[1], info[2]);
-                tasks.addTask(e);
-                storage.saveTasks(tasks);
-                return "Added the following task to your list:\n" + e;
-            }
-            case FIND: {
-                String keyword = input.substring(5).trim();
-                if (keyword.isEmpty()) {
-                    throw new EeveeException("No keyword found :( "
-                            + "Please input the keyword to find matching tasks");
-                }
-                ArrayList<Task> results = tasks.findTasks(keyword);
-                if (results.isEmpty()) {
-                    return "No tasks found matching keyword: " + keyword;
-                } else {
-                    StringBuilder sb = new StringBuilder("Here are the tasks containing keyword " + keyword + " :\n");
-                    for (Task t : results) {
-                        sb.append(t.toString()).append("\n");
-                    }
-                    return sb.toString();
-                }
-            }
-            default:
-                throw new EeveeException("You seemed to have typed wrong. This is not a valid command.");
-            }
-        } catch (EeveeException | IOException e) {
-            ui.printMessage(e.getMessage());
+    private String handleMarkCommand(String input) throws EeveeException, IOException {
+        int taskNumber = parser.parseTaskNumber(input);
+        Task t = tasks.getTask(taskNumber);
+        if (t.isDone) {
+            throw new EeveeException("Task has already been marked as done.");
         }
-        return input;
+        t.markAsDone();
+        storage.saveTasks(tasks);
+        return "Congratulations! I've marked the following task as done:\n  " + t;
+    }
+
+    private String handleUnmarkCommand(String input) throws EeveeException, IOException {
+        int taskNumber = parser.parseTaskNumber(input);
+        Task t = tasks.getTask(taskNumber);
+        if (!t.isDone) {
+            throw new EeveeException("Task is not marked as done. "
+                    + "Needs to be marked done in order to unmark it.");
+        }
+        t.unmarkAsDone();
+        storage.saveTasks(tasks);
+        return "Ok! Task no longer marked as done:\n  " + t;
+    }
+
+    private String handleDeleteCommand(String input) throws EeveeException, IOException {
+        int taskNumber = parser.parseTaskNumber(input);
+        Task t = tasks.getTask(taskNumber);
+        tasks.removeTask(taskNumber);
+        storage.saveTasks(tasks);
+        return "As you wish, this task has been removed:\n " + t;
+    }
+
+    private String handleTodoCommand(String input) throws EeveeException, IOException {
+        String s = input.substring(5).trim();
+        if (s.isEmpty()) {
+            throw new EeveeException("No task found :( "
+                    + "Please input the task details and description correctly");
+        }
+        Todo t = new Todo(s);
+        tasks.addTask(t);
+        storage.saveTasks(tasks);
+        return "Added the following task to your list:\n" + t;
+    }
+
+    private String handleDeadlineCommand(String input) throws EeveeException, IOException {
+        String[] info = input.substring(9).trim().split("/by", 2);
+        if (info.length < 2 || info[0].isEmpty() || info[1].isEmpty()) {
+            throw new EeveeException("Description or deadline not given for task type 'deadline'. "
+                    + "Make sure the deadline is denoted by '/by'.");
+        }
+
+        // Create and store task
+        Deadline d = new Deadline(info[0], info[1]);
+        tasks.addTask(d);
+        storage.saveTasks(tasks);
+        return "Added the following task to your list:\n" + d;
+    }
+
+    private String handleEventCommand(String input) throws EeveeException, IOException {
+        String[] info = input.substring(6).trim().split("/from|/to", 3);
+        if (info.length < 3 || info[0].isEmpty() || info[1].isEmpty() || info[2].isEmpty()) {
+            throw new EeveeException("Event description, start and/or end timings not provided. "
+                    + "Please input a start time denoted by '/from' "
+                    + "and an end time denoted by '/to' when using task type eevee.Event");
+        }
+
+        // Create and store task
+        Event e = new Event(info[0], info[1], info[2]);
+        tasks.addTask(e);
+        storage.saveTasks(tasks);
+        return "Added the following task to your list:\n" + e;
+    }
+
+    private String handleFindCommand(String input) throws EeveeException, IOException {
+        String keyword = input.substring(5).trim();
+        if (keyword.isEmpty()) {
+            throw new EeveeException("No keyword found :( "
+                    + "Please input the keyword to find matching tasks");
+        }
+        ArrayList<Task> results = tasks.findTasks(keyword);
+        if (results.isEmpty()) {
+            return "No tasks found matching keyword: " + keyword;
+        } else {
+            StringBuilder sb = new StringBuilder("Here are the tasks containing keyword " + keyword + " :\n");
+            for (Task t : results) {
+                sb.append(t.toString()).append("\n");
+            }
+            return sb.toString();
+        }
     }
 
     /** 
