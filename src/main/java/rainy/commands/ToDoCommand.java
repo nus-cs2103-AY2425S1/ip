793cbdc425f25c03617bf56a87536c6d5a92f20e
SooYap <tansooyap@gmail.com>
2024-09-13 05:58:02 +0800

    ToDoCommand.java: Refactor ToDo Command implementation

---
+++ src/main/java/rainy/commands/ToDoCommand.java
@@ -0,0 +1,24 @@
+package rainy.commands;
+
+import rainy.tasks.TaskTracker;
+
+public class ToDoCommand extends Command {
+    private String[] userInput;
+    private String taskName;
+    private TaskTracker taskTracker;
+
+    public ToDoCommand(String[] userInput, String taskName, TaskTracker taskTracker) {
+        this.userInput = userInput;
+        this.taskName = taskName;
+        this.taskTracker = taskTracker;
+    }
+
+    public TaskTracker getResponse() {
+        if (userInput.length == 1) {
+            this.ui.noToDoDescription();
+        } else {
+            this.taskTracker.updateListToDo(taskName);
+        }
+        return this.taskTracker;
+    }
+}
