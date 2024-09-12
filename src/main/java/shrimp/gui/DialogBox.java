e23cb3ad12e9d1eb83e03786c78beea3b6d4b503
Jing Hong Tee <e1122714@u.nus.edu>
2024-09-12 20:29:32 +0800

    Refactor GUI package

--- src/main/java/shrimp/gui/DialogBox.java
+++ src/main/java/shrimp/gui/DialogBox.java
@@ -20,7 +20,7 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
-    private DialogBox(String text, Image img) {
+    private DialogBox(String messageText, Image image) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
@@ -30,27 +30,27 @@ public class DialogBox extends HBox {
             e.printStackTrace();
         }
 
-        dialog.setText(text);
-        displayPicture.setImage(img);
+        dialog.setText(messageText);
+        displayPicture.setImage(image);
     }
 
     /**
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
     private void flip() {
-        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        Collections.reverse(tmp);
-        getChildren().setAll(tmp);
+        ObservableList<Node> childNode = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(childNode);
+        getChildren().setAll(childNode);
         setAlignment(Pos.TOP_LEFT);
         dialog.getStyleClass().add("reply-label");
     }
 
-    public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+    public static DialogBox getUserDialog(String messageText, Image userImage) {
+        return new DialogBox(messageText, userImage);
     }
 
-    public static DialogBox getShrimpDialog(String text, Image img) {
-        var db = new DialogBox(text, img);
+    public static DialogBox getShrimpDialog(String messageText, Image shrimpImage) {
+        var db = new DialogBox(messageText, shrimpImage);
         db.flip();
         return db;
     }
