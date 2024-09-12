4366aa5fe610a37144c3af0c61e4a776ac28d7c5
CreditableX <creditablecreates@gmail.com>
2024-09-12 17:23:54 +0800

    add workflow

--- src/main/java/mainstuff/Main.java
+++ src/main/java/mainstuff/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setBob(bob);  // inject the Bob instance
+            fxmlLoader.<MainWindow>getController().setBob(bob); // inject the Bob instance
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
