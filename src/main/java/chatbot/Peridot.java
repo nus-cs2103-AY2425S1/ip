ae7b94f100657296356457708de701db8c323f80
KennyLewi <kennylewi27@gmail.com>
2024-09-12 14:13:22 +0800

    Add JavaDoc comments to improve documentation.
    Add JavaDoc comments to the class Peridot.java to improve the documentation,
    and improve ease of usage of code

--- src/main/java/chatbot/Peridot.java
+++ src/main/java/chatbot/Peridot.java
@@ -3,12 +3,20 @@ package chatbot;
 import storage.Storage;
 import tasks.TaskList;
 
+/**
+ * Class for the chatbot Peridot.
+ */
 public class Peridot {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
 
+    /**
+     * Constructor for the chatbot Peridot.
+     *
+     * @param filePath filepath to local todo list storage.
+     */
     public Peridot(String filePath) {
         storage = new Storage(filePath);
         this.taskList = new TaskList(storage.load());
@@ -23,6 +31,12 @@ public class Peridot {
         storage.write(taskList.getTaskList());
     }
 
+    /**
+     * Gets the bot response from the chatbot depending on user input
+     *
+     * @param input user input to the bot.
+     * @return bot response to the inputted user input.
+     */
     public String getResponse(String input) {
         try {
             String response = Parser.answer(input, taskList);
