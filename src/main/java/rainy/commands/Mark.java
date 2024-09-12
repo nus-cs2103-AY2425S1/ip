3a27c4496b690a35715ff319d1b5659b29bb763a
SooYap <tansooyap@gmail.com>
2024-09-13 05:56:14 +0800

    Mark.java: Refactor Mark Command implementation

---
+++ src/main/java/rainy/commands/Mark.java
@@ -0,0 +1,24 @@
+package rainy.commands;
+
+import rainy.rainyexceptions.InvalidIndexException;
+import rainy.rainyexceptions.InvalidMarkAndUnmarkException;
+import rainy.tasks.TaskTracker;
+
+public class Mark extends Command {
+    private int validResponse;
+    private TaskTracker taskTracker;
+
+    public Mark(int validResponse, TaskTracker taskTracker) {
+        this.validResponse = validResponse;
+        this.taskTracker = taskTracker;
+    }
+
+    public TaskTracker getResponse() throws InvalidIndexException, InvalidMarkAndUnmarkException {
+        if (validResponse != -1) {
+            this.taskTracker.markDone(validResponse - 1);
+        } else {
+            this.ui.noCategoryDeclared();
+        }
+        return this.taskTracker;
+    }
+}
