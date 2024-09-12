3af796ba8442ac644090e01d9feb0c9d31c3c715
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-09-12 11:05:48 +0800

    Add assertion feature to avoid merge conflict with branch-A-Assertions

--- src/main/java/sunny/AddTaskCommand.java
+++ src/main/java/sunny/AddTaskCommand.java
@@ -1,11 +1,17 @@
 package sunny;
 
 import java.util.List;
+
+/**
+ * Creates tasks through TaskCreator class and adds task to list
+ * creates message if task created and stored in list successfully
+ */
 public class AddTaskCommand extends Command {
     @Override
     public String runCommand(List<Task> ls, String m) {
         try {
             Task t = TaskCreator.create(m + "|false");
+            assert t != null : "Task creation failed, task should not be null";
             ls.add(t);
             return line + "\n     "
                     + "Got it! added the task: \n     "
