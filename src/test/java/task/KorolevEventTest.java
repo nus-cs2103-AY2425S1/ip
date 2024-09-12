ecec0f3341732e5b3167ab01c795618bd3aebfd5
CYX22222003 <e1155533@u.nus.edu>
2024-09-12 22:41:26 +0800

    Update unit test to accomodate new changes

--- src/test/java/task/KorolevEventTest.java
+++ src/test/java/task/KorolevEventTest.java
@@ -7,8 +7,8 @@ public class KorolevEventTest {
     public void eventTest_markEvent_success() {
         KorolevEvent expected = new KorolevEvent("test", "2021-01-01T00:00", "2033-01-02T01:01");
         expected.markTask();
-        assertEquals(expected.toString(), "[E][X] test (from: 00:00 Jan 1 2021 to: 01:01 Jan 2 2033)");
+        assertEquals(expected.toString(), "[E][X] test (from: 00:00 Jan 1 2021 to: 01:01 Jan 2 2033)  ");
         expected.unmarkTask();
-        assertEquals(expected.toString(), "[E][ ] test (from: 00:00 Jan 1 2021 to: 01:01 Jan 2 2033)");
+        assertEquals(expected.toString(), "[E][ ] test (from: 00:00 Jan 1 2021 to: 01:01 Jan 2 2033)  ");
     }
 }
