3202769c7c2ccf7115bba267d005dfb54be881c2
SooYap <tansooyap@gmail.com>
2024-09-13 05:55:28 +0800

    List.java: Refactor List Command implementation

---
+++ src/main/java/rainy/commands/List.java
@@ -0,0 +1,17 @@
+package rainy.commands;
+
+import rainy.tasks.TaskTracker;
+
+public class List extends Command {
+    private TaskTracker taskTracker;
+
+    public List(TaskTracker taskTracker) {
+        this.taskTracker = taskTracker;
+    }
+
+    public void getResponse() {
+        System.out.println(this.taskTracker.getList());
+    }
+
+
+}
