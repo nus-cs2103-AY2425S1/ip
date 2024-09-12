6f8a98da253bf4ac627763882a9bf072d9448013
brianchoon <brianchoonweihao@gmail.com>
2024-09-12 15:46:24 +0800

    Remove extra backslash

--- src/main/java/devon/Main.java
+++ src/main/java/devon/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDevon(devon); \// inject the Duke instance
+            fxmlLoader.<MainWindow>getController().setDevon(devon); // inject the Duke instance
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
