d8fcc4780d1ee58a1f314d86e76adc61e7b14eac
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-09-13 23:58:37 +0800

    Add comments for methods in abstract class Task

--- src/main/java/cloudy/Task.java
+++ src/main/java/cloudy/Task.java
@@ -21,20 +21,46 @@ public abstract class Task {
         this.isMarked = isMarked;
     }
 
+
+    /**
+     * Marks a task as completed by setting the isMarked attribute to true.
+     */
     public void markTask() {
         this.isMarked = true;
     }
 
+    /**
+     * Marks a task as not completed by setting the isMarked attribute to false.
+     */
     public void unmarkTask() {
         this.isMarked = false;
     }
 
+    /**
+     * Checks if a task is marked as completed.
+     *
+     * @return true if the task is marked as completed, otherwise false.
+     */
     public boolean isMarked() {
         return isMarked;
     }
 
+    /**
+     * Abstract method to print the task in a format suitable for a list display.
+     * Each subclass of Task must implement this method to define how the task should
+     * be represented in a task list.
+     *
+     * @return A string representing the task in a list format.
+     */
     public abstract String printTaskOnList();
 
+    /**
+     * Abstract method to represent the task in a form suitable for file storage.
+     * Each subclass of Task must implement this method to define how the task should be saved
+     * to a file
+     *
+     * @return A string representing the task in a file format. 
+     */
     public abstract String toFileFormat();
 
 }
