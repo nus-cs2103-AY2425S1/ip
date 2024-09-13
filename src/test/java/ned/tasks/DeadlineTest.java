fb1e894346172d401edd3dfa0216bc141b27b904
ZShunRen <zhengshunren@outlook.com>
2024-09-13 17:17:30 +0800

    Fix checkstyle error in test files

--- src/test/java/ned/tasks/DeadlineTest.java
+++ src/test/java/ned/tasks/DeadlineTest.java
@@ -1,35 +1,34 @@
 package ned.tasks;
 
-import ned.Ui;
-import ned.exceptions.NedException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import ned.Ui;
+import ned.exceptions.NedException;
+
 
 public class DeadlineTest {
     @Test
-    public void createDeadline_validDescription_validByDate_DeadlineObjectCreated() throws NedException {
+    public void createDeadline_validDescriptionValidByDate_deadlineObjectCreated() throws NedException {
         assertEquals(Deadline.createDeadline("read", "2024-08-20", false),
                 new Deadline("read", "2024-08-20", false));
     }
 
     @Test
-    public void createDeadline_validDescription_invalidByDate_NedExceptionThrown() {
+    public void createDeadline_validDescriptionInvalidByDate_nedExceptionThrown() {
         try {
             Deadline.createDeadline("read", "2024/08/20", false);
         } catch (NedException e) {
-            assertEquals("M'lord, the time formatting in /by does not follow ISO 8601 (yyyy-mm-dd). Here " +
-                            "are " +
-                            "examples of" +
-                            " " +
-                            "valid " +
-                            "timings:\n" + Ui.INDENTATIONS + "2015-08-04\n" + Ui.INDENTATIONS + "2015-08-04T10:11:30"
-                    , e.getMessage());
+            assertEquals("M'lord, the time formatting in /by does not follow ISO 8601 (yyyy-mm-dd). Here "
+                            + "are examples of valid timings:\n"
+                            + Ui.INDENTATIONS + "2015-08-04\n" + Ui.INDENTATIONS + "2015-08-04T10:11:30",
+                    e.getMessage());
         }
     }
 
     @Test
-    public void createDeadline_invalidDescription_NedExceptionThrown() {
+    public void createDeadline_invalidDescription_nedExceptionThrown() {
         try {
             Deadline.createDeadline("", "2024/08/20", false);
         } catch (NedException e) {
@@ -37,7 +36,8 @@ public class DeadlineTest {
         }
     }
 
-    public void createDeadline_validDescription_emptyByDate_NedExceptionThrown() {
+    @Test
+    public void createDeadline_validDescriptionEmptyByDate_nedExceptionThrown() {
         try {
             Deadline.createDeadline("read", "", false);
         } catch (NedException e) {
