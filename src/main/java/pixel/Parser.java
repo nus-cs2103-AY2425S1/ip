7eafaaf55c12aeb2c6c9cefadb4dda122698a15b
Sornsornah <ejasonlek@gmail.com>
2024-09-13 10:02:23 +0800

    Refactor code in Parser

--- src/main/java/pixel/Parser.java
+++ src/main/java/pixel/Parser.java
@@ -12,6 +12,126 @@ public class Parser {
     }
 
     /**
+     * Creates a ToDo
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display confirmation of addition
+     */
+    public static String createToDo(String command, TaskList tasks, Storage storage) {
+        String description = command.replace("todo", "").trim();
+        if (description.isEmpty()) {
+            return Ui.handleEmptyDescriptionException();
+        }
+        ToDo newToDo = new ToDo(description);
+        tasks.addTask(newToDo);
+        storage.updateFile(tasks);
+        return Ui.getAddConfirmation(tasks);
+    }
+
+    /**
+     * Creates a Deadline
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display confirmation of addition
+     */
+    public static String createDeadline(String command, TaskList tasks, Storage storage) {
+        String[] stringArray = command.split("/", 0);
+        assert stringArray.length == 2;
+        String description = stringArray[0].replace("deadline", "").trim();
+        String by = stringArray[1].replace("by", "").trim();
+        Deadline newDeadline = new Deadline(description, by);
+        tasks.addTask(newDeadline);
+        storage.updateFile(tasks);
+        return Ui.getAddConfirmation(tasks);
+    }
+
+    /**
+     * Creates an Event
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display confirmation of addition
+     */
+    public static String createEvent(String command, TaskList tasks, Storage storage) {
+        String[] stringArray = command.split("/", 0);
+        assert stringArray.length == 3;
+        String description = stringArray[0].replace("event", "").trim();
+        String from = stringArray[1].replace("from", "").trim();
+        String to = stringArray[2].replace("to", "").trim();
+        Event newEvent = new Event(description, from, to);
+        tasks.addTask(newEvent);
+        storage.updateFile(tasks);
+        return Ui.getAddConfirmation(tasks);
+    }
+
+    /**
+     * Marks a Task as complete
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display confirmation of marking
+     */
+    public static String markTask(String command, TaskList tasks, Storage storage) {
+        String[] stringArray = command.split(" ", 0);
+        Task currentTask = tasks.getTask(Integer.parseInt(stringArray[1]) - 1);
+        currentTask.markAsDone();
+        storage.updateFile(tasks);
+        return Ui.getMarkConfirmation(currentTask);
+    }
+
+    /**
+     * Unmarks a Task as incomplete
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display confirmation of unmarking
+     */
+    public static String unmarkTask(String command, TaskList tasks, Storage storage) {
+        String[] stringArray = command.split(" ", 0);
+        Task currentTask = tasks.getTask(Integer.parseInt(stringArray[1]) - 1);
+        currentTask.markAsUndone();
+        storage.updateFile(tasks);
+        return Ui.getUnmarkConfirmation(currentTask);
+    }
+
+    /**
+     * Deletes a Task from TaskList
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display confirmation of deletion
+     */
+    public static String deleteTask(String command, TaskList tasks, Storage storage) {
+        String[] stringArray = command.split(" ", 0);
+        Integer index = Integer.parseInt(stringArray[1]) - 1;
+        Task removedTask = tasks.getTask(index);
+        tasks.deleteTask(index);
+        storage.updateFile(tasks);
+        return Ui.getDeleteConfirmation(removedTask, tasks);
+    }
+
+    /**
+     * Finds tasks that matches the keyword
+     *
+     * @param command command obtained from Ui
+     * @param tasks TaskList for processing of tasks
+     * @param storage Storage for processing of files
+     * @return String for Ui to display all tasks that matches the keywords
+     */
+    public static String findTask(String command, TaskList tasks, Storage storage) {
+        String searchKey = command.replace("find ", "").trim();
+        return Ui.getMatchingTasks(searchKey, tasks);
+    }
+
+    /**
      * Act based on command given from the user's input
      *
      * @param command command obtained from Ui
@@ -22,61 +142,23 @@ public class Parser {
         if (command.equals("list")) {
             return Ui.getListAsString(tasks);
         } else if (command.startsWith("todo")) {
-            String description = command.replace("todo", "").trim();
-            if (description.isEmpty()) {
-                return Ui.handleEmptyDescriptionException();
-            } else {
-                ToDo newToDo = new ToDo(description);
-                tasks.addTask(newToDo);
-                storage.updateFile(tasks);
-                return Ui.getAddConfirmation(tasks);
-            }
+            return createToDo(command, tasks, storage);
         } else if (command.startsWith("deadline")) {
-            String[] stringArray = command.split("/", 0);
-            assert stringArray.length == 2;
-            String description = stringArray[0].replace("deadline", "").trim();
-            String by = stringArray[1].replace("by", "").trim();
-            Deadline newDeadline = new Deadline(description, by);
-            tasks.addTask(newDeadline);
-            storage.updateFile(tasks);
-            return Ui.getAddConfirmation(tasks);
+            return createDeadline(command, tasks, storage);
         } else if (command.startsWith("event")) {
-            String[] stringArray = command.split("/", 0);
-            assert stringArray.length == 3;
-            String description = stringArray[0].replace("event", "").trim();
-            String from = stringArray[1].replace("from", "").trim();
-            String to = stringArray[2].replace("to", "").trim();
-            Event newEvent = new Event(description, from, to);
-            tasks.addTask(newEvent);
-            storage.updateFile(tasks);
-            return Ui.getAddConfirmation(tasks);
+            return createEvent(command, tasks, storage);
         } else if (command.startsWith("mark")) {
-            String[] stringArray = command.split(" ", 0);
-            Task currentTask = tasks.getTask(Integer.parseInt(stringArray[1]) - 1);
-            currentTask.markAsDone();
-            storage.updateFile(tasks);
-            return Ui.getMarkConfirmation(currentTask);
+            return markTask(command, tasks, storage);
         } else if (command.startsWith("unmark")) {
-            String[] stringArray = command.split(" ", 0);
-            Task currentTask = tasks.getTask(Integer.parseInt(stringArray[1]) - 1);
-            currentTask.markAsUndone();
-            storage.updateFile(tasks);
-            return Ui.getUnmarkConfirmation(currentTask);
+            return unmarkTask(command, tasks, storage);
         } else if (command.startsWith("delete")) {
-            String[] stringArray = command.split(" ", 0);
-            Integer index = Integer.parseInt(stringArray[1]) - 1;
-            Task removedTask = tasks.getTask(index);
-            tasks.deleteTask(index);
-            storage.updateFile(tasks);
-            return Ui.getDeleteConfirmation(removedTask, tasks);
+            return deleteTask(command, tasks, storage);
         } else if (command.startsWith("find")) {
-            String searchKey = command.replace("find ", "").trim();
-            return Ui.getMatchingTasks(searchKey, tasks);
+            return findTask(command, tasks, storage);
         } else if (command.equals("bye")) {
             return Ui.getExit();
         } else {
             return Ui.handleInvalidCommandException();
         }
-
     }
 }
