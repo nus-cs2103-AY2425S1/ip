93997f4eb0f33639bfcdd4a8c8eb6b274ac21913
RezwanAhmed123 <maxtalon94@gmail.com>
2024-09-13 09:29:06 +0800

    Modify TaskListCreatorTest.java
    There is repetition within the test methods of
    TaskListCreatorTest.java.
    
    This reduces the code quality of the testing phase and makes new
    methods more difficult to test as the setup will have to be repeated
    again.
    
    Let's extract out the setup from each method into a class level
    implementation.
    
    This reduces the amount of repetition and allows us to change the
    test cases as quickly as possible. This also causes the test to test
    the behaviour of each class rather than the execution only.

--- src/test/java/mortalreminder/backend/tasklistmanager/TaskCreatorTest.java
+++ src/test/java/mortalreminder/backend/tasklistmanager/TaskCreatorTest.java
@@ -9,8 +9,15 @@ import org.junit.jupiter.api.Test;
 import mortalreminder.backend.TaskListStorage;
 import mortalreminder.commands.CommandType;
 import mortalreminder.errorhandling.MortalReminderException;
+import mortalreminder.tasks.Deadline;
+import mortalreminder.tasks.Event;
+import mortalreminder.tasks.Task;
+import mortalreminder.tasks.ToDoStub;
 
 class TaskCreatorTest {
+    TaskListStub testTaskListStub = new TaskListStub();
+    Task testTask;
+    String commandDetails;
 
     @BeforeEach
     void setUp() throws MortalReminderException {
@@ -22,14 +29,16 @@ class TaskCreatorTest {
         TaskListStorage.clearListFile();
     }
 
-    // Some ideas of Edge Cases were caught by ChatGPT, code was done by myself.
+    /* Some ideas of Edge Cases were caught by ChatGPT, code was done by myself.
+     The setup idea was obtained from:
+     https://stackoverflow.com/questions/63819472/how-can-i-use-the-beforeeach-method-for-testing-in-java */
 
     @Test
     void createTask_validToDo() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("Read Book", stub, CommandType.TODO);
-            assertEquals("Read Book", stub.getTaskList().get(0).getDescription());
+            commandDetails = "Read Book";
+            TaskCreator.createTask(commandDetails, testTaskListStub, CommandType.TODO);
+            assertEquals(commandDetails, testTaskListStub.getTaskList().get(0).getDescription());
         } catch (MortalReminderException e) {
             fail(e.getMessage());
         }
@@ -38,9 +47,10 @@ class TaskCreatorTest {
     @Test
     void createTask_validDeadline() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("Return Book /by 19-08-2024 1900", stub, CommandType.DEADLINE);
-            assertEquals("Return Book (by: 19 Aug 2024 7:00 pm)", stub.getTaskList().get(0).getDescription());
+            commandDetails = "Return Book /by 19-08-2024 1900";
+            TaskCreator.createTask(commandDetails, testTaskListStub, CommandType.DEADLINE);
+            testTask = new Deadline(commandDetails);
+            assertEquals(testTask.getDescription(), testTaskListStub.getTaskList().get(0).getDescription());
         } catch (MortalReminderException e) {
             fail(e.getMessage());
         }
@@ -49,11 +59,10 @@ class TaskCreatorTest {
     @Test
     void createTask_validEvent() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("project meeting /from 19-08-2024 1900 /to 19-08-2024 2000",
-                    stub, CommandType.EVENT);
-            assertEquals("project meeting (from: 19 Aug 2024 7:00 pm, to: 19 Aug 2024 8:00 pm)",
-                    stub.getTaskList().get(0).getDescription());
+            commandDetails = "project meeting /from 19-08-2024 1900 /to 19-08-2024 2000";
+            TaskCreator.createTask(commandDetails, testTaskListStub, CommandType.EVENT);
+            testTask = new Event(commandDetails);
+            assertEquals(testTask.getDescription(), testTaskListStub.getTaskList().get(0).getDescription());
         } catch (MortalReminderException e) {
             fail(e.getMessage());
         }
@@ -62,8 +71,7 @@ class TaskCreatorTest {
     @Test
     void createTask_invalidTaskType() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("Blah blah", stub, CommandType.UNKNOWN);
+            TaskCreator.createTask("Blah blah", testTaskListStub, CommandType.UNKNOWN);
             fail();
         } catch (MortalReminderException e) {
             assertEquals(e.getMessage(), "This statement should be unreachable, code has an error!");
@@ -73,8 +81,7 @@ class TaskCreatorTest {
     @Test
     void createTask_invalidTaskDescription() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("", stub, CommandType.TODO);
+            TaskCreator.createTask("", testTaskListStub, CommandType.TODO);
             fail();
         } catch (MortalReminderException e) {
             assertEquals(e.getMessage(), "Description cannot be empty!");
@@ -84,8 +91,7 @@ class TaskCreatorTest {
     @Test
     void createTask_invalidDeadlineDate() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("Return Book /by tmr", stub, CommandType.DEADLINE);
+            TaskCreator.createTask("Return Book /by tmr", testTaskListStub, CommandType.DEADLINE);
             fail();
         } catch (MortalReminderException e) {
             assertEquals("Please enter a valid date in dd-MM-yyy HHmm (24hr format)!", e.getMessage());
@@ -95,8 +101,7 @@ class TaskCreatorTest {
     @Test
     void createTask_invalidEventDate() {
         try {
-            TaskListStub stub = new TaskListStub();
-            TaskCreator.createTask("Return Book /from tmr /to day after", stub, CommandType.EVENT);
+            TaskCreator.createTask("Return Book /from tmr /to day after", testTaskListStub, CommandType.EVENT);
             fail();
         } catch (MortalReminderException e) {
             assertEquals("Please enter a valid date in dd-MM-yyy HHmm (24hr format)!", e.getMessage());
