ef6d695167c407804fa746640b79d934d7b19a23
KennyLewi <kennylewi27@gmail.com>
2024-09-13 12:10:58 +0800

    Add JavaDoc comments to Deadline class
    There are methods in the Deadline class that is not documented with
    JavaDoc comments.
    
    Adding JavaDoc comments would improve the documentation.
    
    As a step towards better documentation, let's add JavaDoc comments to
    the Deadline class. Doing so will make subsequent function calls easier
    as there are JavaDoc comments to refer to.

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -3,15 +3,31 @@ package tasks;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Class for deadline-type tasks.
+ */
 public class Deadline extends Task {
 
     private LocalDate endTime;
 
+    /**
+     * Creates a deadline task.
+     *
+     * @param name Name for deadline task.
+     * @param endTime Deadline date.
+     */
     public Deadline(String name, LocalDate endTime) {
         super(name);
         this.endTime = endTime;
     }
 
+    /**
+     * Creates a deadline task, marking it as done or undone.
+     *
+     * @param name Name for deadline task.
+     * @param endTime Deadline date.
+     * @param done True if the task is done.
+     */
     public Deadline(String name, LocalDate endTime, boolean done) {
         super(name, done);
         this.endTime = endTime;
