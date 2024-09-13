7f338a836acd5bdcd4bf3980eb60539282ce6ba1
ishan-agarwal-05 <ishanagarwal270805@gmail.com>
2024-09-13 11:53:55 +0800

    Add JavaDocs comments

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,11 +1,36 @@
+
+/**
+ * Represents a deadline task.
+ * A Deadline object corresponds to a task represented by a description and a deadline date.
+ */
 public class Deadline extends Task {
     protected String by;
 
+    /**
+     * Constructor for Deadline class.
+     *
+     * @param description The description of the task.
+     * @param by The deadline of the task.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    /**
+     * Returns the deadline of the task.
+     *
+     * @return Deadline of the task.
+     */
+    public String getBy() {
+        return this.by;
+    }
+
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return String representation of the task.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
