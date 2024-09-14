d3bd7f7595ae086c5ab512af20d426459a64c605
rithanisk <rithanisk@gmail.com>
2024-09-15 04:38:07 +0800

    Add ByeCommand class

--- src/main/java/ByeCommand.java
+++ src/main/java/ByeCommand.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ByeCommand {
+import java.io.IOException;
+
+public class ByeCommand extends Command {
+
+    public ByeCommand(String description) {
+        super(description);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
+        System.out.println(ui.goodbye());
+        storage.saveTaskListToTextFile(TaskList.getTaskList());
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
 }
