b8a38d496e29454718b58215f900884bd851cfd4
Varun Shukla <vslpahm@gmail.com>
2024-09-14 13:42:20 +0800

    Improve code quality
    Add private enum class CommandType to handle inputs better
    Use switch cases to determine command type

--- src/main/java/sam/Main.java
+++ src/main/java/sam/Main.java
@@ -8,6 +8,12 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+/**
+ * The Main class is the entry point for the application.
+ * It extends the JavaFX Application class and provides the start method.
+ * The start method loads the MainWindow.fxml file, sets up the scene, and displays the stage.
+ * It also injects an instance of the Sam class into the MainWindow controller.
+ */
 public class Main extends Application {
 
     private Sam sam = new Sam();
@@ -19,7 +25,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDuke(sam);  // inject the Duke instance
+            fxmlLoader.<MainWindow>getController().setDuke(sam); // inject the Duke instance
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
