bb48a6cc9d2515a0858a569bf8d808f293130010
rithanisk <rithanisk@gmail.com>
2024-09-15 04:39:47 +0800

    Add UnmarkCommand class

--- src/main/java/UnmarkCommand.java
+++ src/main/java/UnmarkCommand.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class UnmarkCommand {
+import java.io.IOException;
+
+public class UnmarkCommand extends Command {
+
+    public UnmarkCommand(String description) {
+        super(description);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
+        String command = getDescription();
+        int taskNum = Parser.splitCommandAndTaskNumber(command);
+        System.out.println(tasks.unmarkTask(taskNum));
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
