711f5c97052764920ac769d16019a32c60e0055d
Zonlim <zonlim77@gmail.com>
2024-09-14 16:28:13 +0800

    Add more javadoc comments
    There are many different commands.
    In order for users to know what each command does
    more specific information should be given.

--- src/main/java/Gumball/ExitCommand.java
+++ src/main/java/Gumball/ExitCommand.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class ExitCommand extends Command{
     /**
-     * 
+     *
      * @param list The taskList which may be changed or where information will be extracted.
      * @param ui Class which contains ui functions.
      * @param fileManager The location where the information on the list is stored.
--- src/main/java/Gumball/FindCommand.java
+++ src/main/java/Gumball/FindCommand.java
@@ -7,6 +7,17 @@ public class FindCommand extends Command{
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
+
+    /**
+     * Returns String representing list of tasks which contain the keyword provided
+     * in the constructor.
+     * @param list The taskList where information will be extracted.
+     * @param ui Class which contains ui functions.
+     * @param fileManager The location where the information on the list is stored.
+     * @return
+     * @throws InputErrorException
+     * @throws IOException
+     */
     @Override
     public String execute(TaskList list, UI ui, FileManager fileManager) throws InputErrorException, IOException {
         UI.print(list.find(keyword));
