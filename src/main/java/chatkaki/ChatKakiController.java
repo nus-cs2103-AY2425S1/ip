93ad4d47607ffba3975c8782a94289d5e9ddbbec
limyuquan <limyuquan02@gmail.com>
2024-09-14 15:08:10 +0800

    Add user guide

--- src/main/java/chatkaki/ChatKakiController.java
+++ src/main/java/chatkaki/ChatKakiController.java
@@ -1,12 +1,86 @@
+//package chatkaki;
+//
+//import chatkaki.commands.Command;
+//import chatkaki.tasks.TaskList;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.TextArea;
+//import javafx.scene.control.TextField;
+//
+//
+//
+///**
+// * The controller for ChatKaki's GUI.
+// */
+//public class ChatKakiController {
+//
+//    @FXML
+//    private TextArea chatArea;
+//
+//    @FXML
+//    private TextField inputField;
+//
+//    @FXML
+//    private TextArea taskArea;
+//
+//    @FXML
+//    public void initialize() {
+//        greetUser();
+//    }
+//
+//    private String getAllTasks() {
+//        assert TaskList.getTasks() != null : "TaskList array should not be null";
+//        StringBuilder listMessage = new StringBuilder();
+//        for (int i = 0; i < TaskList.getSize(); i++) {
+//            if (i > 0) {
+//                listMessage.append("\n");
+//            }
+//            listMessage.append(i + 1).append(". ").append(TaskList.getTask(i));
+//        }
+//        return listMessage.toString();
+//    }
+//    @FXML
+//    private void handleUserInput() {
+//        String userInput = inputField.getText();
+//        inputField.clear();
+//        chatArea.clear();
+//        taskArea.clear();
+//        chatArea.appendText("User: " + userInput + "\n\n");
+//
+//        Command command = Parser.parse(userInput);
+//        try {
+//            String output = command.execute();
+//            assert output != null : "Command output should not be null";
+//            chatArea.appendText("ChatKaki: " + output + "\n");
+//        } catch (Exception e) {
+//            chatArea.appendText("Error: " + e.getMessage() + "\n");
+//        }
+//
+//        taskArea.appendText(getAllTasks());
+//
+//        if (command.isExit()) {
+//            System.exit(0);
+//        }
+//    }
+//
+//    private void greetUser() {
+//        Storage.loadTasksFromFile();
+//        chatArea.appendText("Hello! I'm ChatKaki\nWhat can I do for you?\n");
+//        taskArea.appendText(getAllTasks());
+//    }
+//}
+
 package chatkaki;
 
 import chatkaki.commands.Command;
 import chatkaki.tasks.TaskList;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
-
+import javafx.scene.image.Image;
 
 /**
  * The controller for ChatKaki's GUI.
@@ -14,7 +88,7 @@ import javafx.scene.control.TextField;
 public class ChatKakiController {
 
     @FXML
-    private TextArea chatArea;
+    private ListView<Message> chatListView;
 
     @FXML
     private TextField inputField;
@@ -22,9 +96,46 @@ public class ChatKakiController {
     @FXML
     private TextArea taskArea;
 
+    private ObservableList<Message> messages = FXCollections.observableArrayList();
+
+    private Image userImage = new Image("/images/user.jpg");
+    private Image botImage = new Image("/images/bot.png");
+
+    /**
+     * Initializes the controller.
+     */
     @FXML
     public void initialize() {
+        chatListView.setItems(messages);
+        chatListView.setCellFactory(param -> new MessageCell());
         greetUser();
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String userInput = inputField.getText();
+        inputField.clear();
+        taskArea.clear();
+
+        // Add user message
+        messages.add(new Message(userInput, true, userImage));
+
+        Command command = Parser.parse(userInput);
+        try {
+            String output = command.execute();
+            // Add bot response
+            messages.add(new Message(output, false, botImage));
+        } catch (Exception e) {
+            messages.add(new Message("Error: " + e.getMessage(), false, botImage));
+        }
+
+        chatListView.scrollTo(messages.size() - 1);
+
+        taskArea.appendText(getAllTasks());
+
+        if (command.isExit()) {
+            Platform.exit();
+        }
     }
 
     private String getAllTasks() {
@@ -38,33 +149,14 @@ public class ChatKakiController {
         }
         return listMessage.toString();
     }
-    @FXML
-    private void handleUserInput() {
-        String userInput = inputField.getText();
-        inputField.clear();
-        chatArea.clear();
-        taskArea.clear();
-        chatArea.appendText("User: " + userInput + "\n\n");
-
-        Command command = Parser.parse(userInput);
-        try {
-            String output = command.execute();
-            assert output != null : "Command output should not be null";
-            chatArea.appendText("ChatKaki: " + output + "\n");
-        } catch (Exception e) {
-            chatArea.appendText("Error: " + e.getMessage() + "\n");
-        }
-
-        taskArea.appendText(getAllTasks());
-
-        if (command.isExit()) {
-            System.exit(0);
-        }
-    }
 
     private void greetUser() {
         Storage.loadTasksFromFile();
-        chatArea.appendText("Hello! I'm ChatKaki\nWhat can I do for you?\n");
+        messages.add(new Message("Hello! I'm ChatKaki\nWhat can I do for you?", false, botImage));
         taskArea.appendText(getAllTasks());
     }
+
+
+
 }
+
