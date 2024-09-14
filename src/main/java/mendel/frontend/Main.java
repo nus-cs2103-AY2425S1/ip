fcdafd47d5f1a32f8f4480afbe940521599df088
Virusrwj223 <mhrishiraj@gmail.com>
2024-09-14 22:24:10 +0800

    Revert "Set UI min size"
    This reverts commit 6b59e48993b3420be865575bbdd55aebee5cda3b.

--- src/main/java/mendel/frontend/Main.java
+++ src/main/java/mendel/frontend/Main.java
@@ -22,8 +22,6 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            stage.setMinHeight(220);
-            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setMendel(mendel);
             stage.show();
         } catch (IOException e) {
