25f71d95998c2b961e8bcdbd70690de3e6a415c5
Nigeltzy <nigel.teo.zy@gmail.com>
2024-09-14 21:25:55 +0800

    Add Sorting functionality for List Command
    List command did not support sorted listing.
    Users may want to view tasks in alphabetical order.
    
    For a small starting point, let's add a basic sorting functionality
    that allows users to input "1" after list to sort their tasks in
    a simple defined alphabetical order. In future commits, we can improve
    this sorting functionality to allow for more flexibility.

--- src/main/java/shnoop/command/ListCommand.java
+++ src/main/java/shnoop/command/ListCommand.java
@@ -8,9 +8,18 @@ import shnoop.ui.Ui;
  * Encapsulates all the relevant actions to be taken when a List Command is issued.
  */
 public class ListCommand extends Command {
+    int mode;
+
+    public ListCommand(int mode) {
+        this.mode = mode;
+    }
+
+    public ListCommand() {
+        mode = 0;
+    }
 
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
-        return ui.list(tasks.list());
+        return ui.list(tasks.sortedList(mode));
     }
 }
