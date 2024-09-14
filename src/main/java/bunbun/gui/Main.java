26c655e66375442b9f45976597ac1868b22a9230
EdaYeo <wye.yeo@gmail.com>
2024-09-14 18:20:40 +0800

    Add user guide

--- src/main/java/bunbun/gui/Main.java
+++ src/main/java/bunbun/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bunbun");
             fxmlLoader.<MainWindow>getController().setBunbun(bunbun);  // inject the Bunbun instance
             stage.setOnCloseRequest(e -> bunbun.close());
             stage.show();
