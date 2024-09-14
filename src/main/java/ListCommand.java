8c213ccc76351106f46f6ccd0b72c62c01f57b79
rithanisk <rithanisk@gmail.com>
2024-09-15 04:39:18 +0800

    Add ListCommand class

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ListCommand {
+public class ListCommand extends Command {
+    public ListCommand(String description) {
+        super(description);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        System.out.println(ui.displayList());
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
