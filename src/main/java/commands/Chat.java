c7b0c224dfdf99cf3f81f21aa4132a1369ddefab
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-09-14 14:54:13 +0800

    Fix display bug in greet message from EchoBot

--- src/main/java/commands/Chat.java
+++ src/main/java/commands/Chat.java
@@ -4,20 +4,11 @@ package commands;
  * Deals with interactions with the user.
  */
 public class Chat {
-    private static final String LOGO = "  ______      _           \n"
-            + " |  ____|    | |          \n"
-            + " | |__   __ _| | ___  ___ \n"
-            + " |  __| / _` | |/ _ \\/ _ \\\n"
-            + " | |___| (_| | |  __/  __/\n"
-            + " |______\\__,_|_|\\___|\\___|\n";
-
-
-
     /**
      * Sends a greet message to the user.
      */
     public static String greet() {
-        return "Hello! I'm EchoBot.\n" + LOGO + "What can I do for you?";
+        return "Hello! I'm EchoBot.\n" + "What can I do for you?";
     }
 
     /** Sends goodbye message to user */
