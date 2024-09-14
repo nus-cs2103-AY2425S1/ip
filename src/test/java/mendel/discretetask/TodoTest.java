2905ef885fbe756d4ed1a4d45f70576bd3f742b2
Virusrwj223 <mhrishiraj@gmail.com>
2024-09-14 23:07:03 +0800

    Test Event

--- src/test/java/mendel/discretetask/TodoTest.java
+++ src/test/java/mendel/discretetask/TodoTest.java
@@ -1,8 +1,6 @@
 package mendel.discretetask;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
