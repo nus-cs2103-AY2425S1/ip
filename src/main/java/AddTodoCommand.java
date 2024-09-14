a0edf47580b8de0bb0a895f99126f1d9e88e19eb
rithanisk <rithanisk@gmail.com>
2024-09-15 04:37:39 +0800

    Add AddTodoCommand class

--- src/main/java/AddTodoCommand.java
+++ src/main/java/AddTodoCommand.java
@@ -1,11 +1,21 @@
-public class AddCommand extends Command {
+public class AddTodoCommand extends Command {
 
-    public AddCommand(String description) {
+    public AddTodoCommand(String description) {
         super(description);
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
+        String command = getDescription();
 
+        String taskDescription = Parser.splitCommandAndTaskDescription(command);
+        Task newTodo = new Todo(taskDescription);
+        String addedTodo = tasks.addTask(newTodo);
+        System.out.println(addedTodo);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
     }
 }
