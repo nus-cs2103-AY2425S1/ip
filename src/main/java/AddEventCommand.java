9207521bc07ff6de2444399cef9294d9c7629d88
rithanisk <rithanisk@gmail.com>
2024-09-15 04:37:23 +0800

    Add AddEventCommand class

--- src/main/java/AddEventCommand.java
+++ src/main/java/AddEventCommand.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class AddEventCommand {
+import java.io.IOException;
+
+public class AddEventCommand extends Command {
+
+    public AddEventCommand(String description) {
+        super(description);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
+        String command = getDescription();
+
+        String taskInfo = Parser.splitCommandAndTaskDescription(command);
+        String taskDescriptionEvent = Parser.splitEventCommand(taskInfo)[0];
+        String startInfo = Parser.splitEventCommand(taskInfo)[1];
+        String endInfo = Parser.splitEventCommand(taskInfo)[2];
+
+        String taskStart = Parser.splitCommandAndTaskDescription(startInfo);
+        String taskEnd = Parser.splitCommandAndTaskDescription(endInfo);
+
+        Task newEvent = new Event(taskDescriptionEvent, taskStart, taskEnd);
+        String addedEvent = tasks.addTask(newEvent);
+        System.out.println(addedEvent);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
