51b805f340a3a61dfe7d6b59edf74c8a39db7e7b
rithanisk <rithanisk@gmail.com>
2024-09-15 04:39:33 +0800

    Add MarkCommand class

--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MarkCommand {
+import java.io.IOException;
+
+public class MarkCommand extends Command {
+
+    public MarkCommand(String description) {
+        super(description);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
+        String command = getDescription();
+        int taskNum = Parser.splitCommandAndTaskNumber(command);
+        System.out.println(tasks.markTask(taskNum));
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
