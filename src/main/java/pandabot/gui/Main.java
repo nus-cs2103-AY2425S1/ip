5d84d1ae7e55dc39b31a5f63fa9af22964ee2c10
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-09-15 15:35:31 +0800

    Add speech bubbles for DialogBox

--- src/main/java/pandabot/gui/Main.java
+++ src/main/java/pandabot/gui/Main.java
@@ -35,6 +35,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setPandaBot(pandaBot);
             stage.setTitle("PandaBot");
