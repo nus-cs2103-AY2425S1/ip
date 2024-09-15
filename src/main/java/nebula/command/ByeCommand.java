96be4ec9569f5096355935ca367a85c1bf110768
rithanisk <rithanisk@gmail.com>
2024-09-16 04:58:24 +0800

    Add JavaDoc comments to all the Command classes

--- src/main/java/nebula/command/ByeCommand.java
+++ src/main/java/nebula/command/ByeCommand.java
@@ -8,16 +8,35 @@ import java.io.IOException;
 
 public class ByeCommand extends Command {
 
+    /**
+     * Constructs a ByeCommand object with the specified description.
+     *
+     * @param description The command description (not used in this command).
+     */
     public ByeCommand(String description) {
         super(description);
     }
 
+    /**
+     * Executes the bye command.
+     * Displays a goodbye message and saves the current task list to a text file.
+     *
+     * @param tasks   The task list to be saved (not used directly in this method).
+     * @param ui      The UI component to display the goodbye message.
+     * @param storage The storage component used to save the task list to a file.
+     * @throws IOException If an I/O error occurs during saving the task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
         System.out.println(ui.goodbye());
         storage.saveTaskListToTextFile(TaskList.getTaskList());
     }
 
+    /**
+     * Indicates whether this command will exit the application.
+     *
+     * @return true since the bye command exits the application.
+     */
     @Override
     public boolean isExit() {
         return true;
--- src/main/java/nebula/command/MarkCommand.java
+++ src/main/java/nebula/command/MarkCommand.java
@@ -9,10 +9,24 @@ import java.io.IOException;
 
 public class MarkCommand extends Command {
 
+    /**
+     * Constructs a MarkCommand object with the specified description.
+     *
+     * @param description The command description containing the task number to mark.
+     */
     public MarkCommand(String description) {
         super(description);
     }
 
+    /**
+     * Executes the mark command on the specified task list.
+     * Marks the task at the task number parsed from the command as done.
+     *
+     * @param tasks   The task list where the task will be marked.
+     * @param ui      The UI component for user interaction.
+     * @param storage The storage component for saving the task list after marking the task.
+     * @throws IOException If an I/O error occurs when saving the updated task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
         String command = getDescription();
@@ -20,6 +34,11 @@ public class MarkCommand extends Command {
         System.out.println(tasks.markTask(taskNum));
     }
 
+    /**
+     * Indicates whether this command will exit the application.
+     *
+     * @return false since the mark command does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
