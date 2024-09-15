a639cb2762101c2f17fc6495f89e94b37863e263
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-09-15 17:09:18 +0800

    Add formatting to Event display

--- src/main/java/pandabot/tasks/Event.java
+++ src/main/java/pandabot/tasks/Event.java
@@ -90,7 +90,7 @@ public class Event extends Task {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
-        return "[E]" + super.toString() + " (from: " + from.format(formatter) + " to: " + to.format(formatter) + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(formatter) + ", to: " + to.format(formatter) + ")";
     }
 
     /**
