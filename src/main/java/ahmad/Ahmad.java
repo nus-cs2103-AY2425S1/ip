c9eb6e9786606aecdf0b7e6d81534223bd7c6682
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-09-15 16:27:28 +0800

    Fix shadowjar launcher

--- src/main/java/ahmad/Ahmad.java
+++ src/main/java/ahmad/Ahmad.java
@@ -76,26 +76,4 @@ public class Ahmad extends Application {
         }
     }
 
-    /**
-     * Runs the program.
-     * Loads file (if exists), and starts interaction.
-     *
-     * @param args Arguments from the compiler.
-     */
-    public static void main(String... args) {
-
-
-        if (!Arrays.asList(args).contains("--without-file")) {
-            Storage.load();
-        }
-
-        launch(args);
-
-        /*
-            Ui.print(welcomeMsg);
-
-
-            startInteraction();
-        */
-    }
 }
