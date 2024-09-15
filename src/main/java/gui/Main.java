5e1a82bdbfa4c663c661cdffb38faef482ec92af
wesley <wesleywong08@gmail.com>
2024-09-15 23:40:58 +0800

    Add custom font

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -23,6 +23,9 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
+            scene.getStylesheets()
+                    .add(getClass().getResource("/css/dialog-box.css").toExternalForm());
             stage.setMinWidth(417);
             stage.setMinHeight(617);
             fxmlLoader.<MainWindow>getController().setMizz(this.mizz); // inject the Duke instance
