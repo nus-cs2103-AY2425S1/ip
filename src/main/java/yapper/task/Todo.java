330813e44fe85373558aca0220eb36bdd193b2a8
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-09-16 02:31:51 +0800

    Enhance Java basics using Copilot

--- src/main/java/yapper/task/Todo.java
+++ src/main/java/yapper/task/Todo.java
@@ -2,15 +2,24 @@ package yapper.task;
 
 public class Todo extends Task {
 
+    /*
+     * Constructs a new Todo object with the specified description.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /*
+     * Returns the string representation of the todo task for saving to a file.
+     */
     @Override
     public String toSaveFormat() {
         return "T | " + (isDone() ? "1" : "0") + " | " + getDescription();
     }
 
+    /*
+     * Returns the string representation of the todo task for display.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
