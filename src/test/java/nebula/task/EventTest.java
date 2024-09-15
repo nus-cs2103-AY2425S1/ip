07e8a3d11e3e733d43fb89e5fad9e9d41d91d3ea
rithanisk <rithanisk@gmail.com>
2024-09-15 18:03:54 +0800

    Add createEvent_success method to EventTest

--- src/test/java/nebula/task/EventTest.java
+++ src/test/java/nebula/task/EventTest.java
@@ -2,16 +2,14 @@ package nebula.task;  //same package as the class being tested
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EventTest {
     @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
-    }
-
-    @Test
-    public void anotherDummyTest(){
-        assertEquals(4, 4);
+    public void createEvent_success(){
+        assertEquals(new Event("birthday party", "2024-09-12 12:30", "2024-09-12 04:30").toString(),
+                "[E] [ ] birthday party (from: September 12, 2024 12:30 to: September 12, 2024 04:30)");
     }
 }
