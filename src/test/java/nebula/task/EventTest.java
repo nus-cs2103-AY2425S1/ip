a2bfd7e4e3be7b991aac0ecfa1f43db2b52293f7
rithanisk <rithanisk@gmail.com>
2024-09-15 18:08:16 +0800

    Add assertEquals to test event without time in createEvent_success

--- src/test/java/nebula/task/EventTest.java
+++ src/test/java/nebula/task/EventTest.java
@@ -1,15 +1,20 @@
 package nebula.task;  //same package as the class being tested
 
+import nebula.exception.NebulaException;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EventTest {
     @Test
-    public void createEvent_success(){
+    public void createEvent_success() {
         assertEquals(new Event("birthday party", "2024-09-12 12:30", "2024-09-12 04:30").toString(),
                 "[E] [ ] birthday party (from: September 12, 2024 12:30 to: September 12, 2024 04:30)");
+
+        assertEquals(new Event("math lecture", "2024-10-12", "2024-10-12").toString(),
+                "[E] [ ] math lecture (from: October 12, 2024 00:00 to: October 12, 2024 00:00)");
     }
 }
