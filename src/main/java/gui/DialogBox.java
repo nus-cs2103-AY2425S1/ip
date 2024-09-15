a98e5b793385b9f6cbecaec7428cefdc2ad36811
wesley <wesleywong08@gmail.com>
2024-09-15 20:53:38 +0800

    Add css

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -47,6 +47,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     /**
