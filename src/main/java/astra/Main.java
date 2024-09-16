a294e76ebd5d28d4dffbf5749e85b3f46cdd1f7f
Yifeng Li <charlesliyifeng@gmail.com>
2024-09-16 21:28:46 +0800

    Add title and icon

--- src/main/java/astra/Main.java
+++ src/main/java/astra/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,6 +20,11 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+
+            stage.setTitle("Astra");
+            Image image = new Image(this.getClass().getResourceAsStream("/images/Astra.png"));
+            stage.getIcons().add(image);
+
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
