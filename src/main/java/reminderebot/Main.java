31fe8889b10e80db296d7b5ad19d5b22a67f0880
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-09-16 15:51:30 +0800

    Add A-Personality Class Main: Set title and image for Reminderebot Class MainWindow: Set profile image for Reminderebot
    Let's add personality to Reminderebot by:
    - Set the title of app to Reminderebot
    - Set the profile image for Reminderebot

--- src/main/java/reminderebot/Main.java
+++ src/main/java/reminderebot/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import reminderebot.controls.MainWindow;
@@ -21,6 +22,11 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+
+            stage.setTitle("Reminderebot");
+            Image image = new Image("images/Remindere.png");
+            stage.getIcons().add(image);
+
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setReminderebot(reminderebot); // inject the Reminderebot instance
