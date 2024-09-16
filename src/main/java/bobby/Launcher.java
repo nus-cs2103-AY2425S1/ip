0ae534e22df03c4ea3b113485ea24b188d5dd8e1
vinceg4 <vincegoh04@gmail.com>
2024-09-16 11:31:36 +0800

    Add Javadocs comment

--- src/main/java/bobby/Launcher.java
+++ src/main/java/bobby/Launcher.java
@@ -3,6 +3,9 @@ package bobby;
 import bobby.gui.Main;
 import javafx.application.Application;
 
+/**
+ * Launches the Bobby chatbot.
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
