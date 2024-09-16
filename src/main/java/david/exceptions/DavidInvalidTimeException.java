2261d5f470c087766cc7c7b0d71b38395ca7294b
Incogdino <javiertmw@gmail.com>
2024-09-16 17:55:13 +0800

    Refactor code to fit java coding standard
    Better code quality can improve readability of the code. By making the
    code more logical and easier to understand.
    
    The code adheres to a logical structure. Magic numbers have been
    drastically reduced/ removed. Complicated functions have been extracted
    into separate functions adhering to SLAP principle. KISS principle have
    also been applied to keep code easy to understand.

--- src/main/java/david/exceptions/DavidInvalidTimeException.java
+++ src/main/java/david/exceptions/DavidInvalidTimeException.java
@@ -1,11 +1,15 @@
 package david.exceptions;
 
+
+/**
+ * Invalid time exception class for David class
+ */
 public class DavidInvalidTimeException extends DavidException {
     public DavidInvalidTimeException() {};
     @Override
     public String toString() {
-        return "Please ensure that the time inputted is after the current time. If you are trying" +
-                " to input an event task, make sure \"\\from\" field is a valid time before  \"\\to\" field";
+        return "Please ensure that the time inputted is after the current time. If you are trying"
+                + " to input an event task, make sure \"\\from\" field is a valid time before  \"\\to\" field";
     }
 
 }
