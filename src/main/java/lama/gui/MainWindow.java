6f52bc4387e8523191735bb97270b96009755891
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-09-16 19:54:14 +0800

    Remove unnecessary imported button

--- src/main/java/lama/gui/MainWindow.java
+++ src/main/java/lama/gui/MainWindow.java
@@ -1,7 +1,6 @@
 package lama.gui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -22,11 +21,7 @@ public class MainWindow extends AnchorPane {
     private VBox dialogContainer;
     @FXML
     private TextField userInput;
-    @FXML
-    private Button sendButton;
-
     private Lama lama;
-
     private Image userImage;
     private Image lamaImage;
 
