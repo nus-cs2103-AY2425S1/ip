c9431815612b5c559ff6eae6057a4237d41dafbe
Chan Guan Yu <changuanyu2904@gmail.com>
2024-09-17 03:07:13 +0800

    Update test cases to reflect new behaviour of TaskList class

--- src/test/java/secondmind/TaskListTest.java
+++ src/test/java/secondmind/TaskListTest.java
@@ -10,7 +10,9 @@ class TaskListTest {
     @Test
     void testAddToTaskList_todoCommand_success() throws Exception {
         TaskList taskList = new TaskList(new ArrayList<Task>());
-        Task task = taskList.addToTaskList("todo read book");
+        String[] instruction = new String[] {"add", "todo read book"};
+        Task task = taskList.createTask(instruction);
+        taskList.addToTaskList(task);
         assertEquals(1, taskList.getTaskCount());
         assertTrue(task instanceof ToDoTask);
         assertEquals("read book", task.getDescription());
@@ -20,7 +22,9 @@ class TaskListTest {
     @Test
     void testAddToTaskList_deadlineCommand_success() throws Exception {
         TaskList taskList = new TaskList(new ArrayList<Task>());
-        Task task = taskList.addToTaskList("deadline submit report /by 2024-09-01T23:59:59");
+        String[] instruction = new String[] {"add", "deadline submit report /by 2024-09-01T23:59:59"};
+        Task task = taskList.createTask(instruction);
+        taskList.addToTaskList(task);
         assertEquals(1, taskList.getTaskCount());
         assertTrue(task instanceof DeadlineTask);
         assertEquals("submit report", task.getDescription());
@@ -30,8 +34,10 @@ class TaskListTest {
     @Test
     void testAddToTaskList_eventCommand_success() throws Exception {
         TaskList taskList = new TaskList(new ArrayList<Task>());
-        Task task = taskList.addToTaskList("event school project meeting " +
-                "/from 2024-09-10T16:00:00 /to 2024-09-10T18:00:00");
+        String[] instruction = new String[] {"add", "event school project meeting " +
+                "/from 2024-09-10T16:00:00 /to 2024-09-10T18:00:00"};
+        Task task = taskList.createTask(instruction);
+        taskList.addToTaskList(task);
         assertEquals(1, taskList.getTaskCount());
         assertTrue(task instanceof EventTask);
         assertEquals("school project meeting", task.getDescription());
@@ -43,14 +49,18 @@ class TaskListTest {
     void addToTaskList_invalidDateFormat_dateTimeParseExceptionThrown() {
         TaskList taskList = new TaskList(new ArrayList<Task>());
         assertThrows(DateTimeParseException.class, () -> {
-            taskList.addToTaskList("deadline submit report /by wrongFormat");
+            String[] instruction = new String[] {"add", "deadline submit report /by wrongFormat"};
+            Task task = taskList.createTask(instruction);
+            taskList.addToTaskList(task);
         });
     }
 
     @Test
     void markAsDone_validTaskNumber_success() throws Exception {
         TaskList taskList = new TaskList(new ArrayList<Task>());
-        Task task = taskList.addToTaskList("todo read book");
+        String[] instruction = new String[] {"add", "todo read book"};
+        Task task = taskList.createTask(instruction);
+        taskList.addToTaskList(task);
         assertEquals("T|0|read book", task.getStorageRepresentation());
         taskList.markAsDone(1);
         assertEquals("T|1|read book", task.getStorageRepresentation());
@@ -59,7 +69,9 @@ class TaskListTest {
     @Test
     void markAsDone_invalidTaskNumber_InvalidTaskNumberExceptionThrown() throws Exception {
         TaskList taskList = new TaskList(new ArrayList<Task>());
-        Task task = taskList.addToTaskList("todo read book");
+        String[] instruction = new String[] {"add", "todo read book"};
+        Task task = taskList.createTask(instruction);
+        taskList.addToTaskList(task);
         assertEquals("T|0|read book", task.getStorageRepresentation());
         try {
             taskList.markAsDone(5);
@@ -71,7 +83,9 @@ class TaskListTest {
     @Test
     void markAsUndone_validTaskNumber_success() throws Exception {
         TaskList taskList = new TaskList(new ArrayList<Task>());
-        Task task = taskList.addToTaskList("todo read book");
+        String[] instruction = new String[] {"add", "todo read book"};
+        Task task = taskList.createTask(instruction);
+        taskList.addToTaskList(task);
         task.markAsDone();
         assertEquals("T|1|read book", task.getStorageRepresentation());
         taskList.markAsUndone(1);
