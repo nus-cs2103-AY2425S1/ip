4a9bd54b41f39e5fda48642eea1fd4bc51f4b836
Jabez Tho <jabeztho@gmail.com>
2024-09-16 15:25:54 +0800

    Reduce method length in Parser parse
    Abstract Parser parse into varioud methods
    
    There are several repeated patterns in parse. In addition
    the switch case is too long and hard to reason with.
    
    Separating into different smaller methods improves code quality.

--- src/main/java/nixy/parser/Parser.java
+++ src/main/java/nixy/parser/Parser.java
@@ -52,106 +52,137 @@ public class Parser {
         Task task;
         input = input.trim();
         String[] tokens = input.split(" ", 2);
-        tokens[0] = tokens[0].trim();
+        // first token is the command
+        String commandString = tokens[0].trim();
+        String args = tokens.length > 1 ? tokens[1].trim() : "";
         switch (tokens[0]) {
         case "bye":
             return new ByeCommand(ui);
         case "list":
             return new ListCommand(ui, tasks);
         case "mark":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The task number to mark as done cannot be empty.");
-            }
-            try {
-                int parsedInt = Integer.parseInt(tokens[1].trim());
-                if (parsedInt < 1) {
-                    throw new NixyException("BLAHH!!! The task number to mark as done must be a positive integer.");
-                }
-                return new MarkCommand(ui, tasks, storage, parsedInt, true);
-            } catch (NumberFormatException e) {
-                throw new NixyException("BLAHH!!! The task number to mark as done must be an integer.");
-            }
+            return parseMarkCommad(args, true);
         case "unmark":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The task number to mark as undone cannot be empty.");
-            }
-            try {
-                int parsedInt = Integer.parseInt(tokens[1].trim());
-                if (parsedInt < 1) {
-                    throw new NixyException("BLAHH!!! The task number to mark as undone must be a positive integer.");
-                }
-                return new MarkCommand(ui, tasks, storage, parsedInt, false);
-            } catch (NumberFormatException e) {
-                throw new NixyException("BLAHH!!! The task number to mark as undone must be an integer.");
-            }
+            return parseMarkCommad(args, false);
         case "delete":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The task number to delete cannot be empty.");
-            }
-            try {
-                int parsedInt = Integer.parseInt(tokens[1].trim());
-                if (parsedInt < 1) {
-                    throw new NixyException("BLAHH!!! The task number to delete must be a positive integer.");
-                }
-                return new DeleteCommand(ui, tasks, parsedInt, storage);
-            } catch (NumberFormatException e) {
-                throw new NixyException("BLAHH!!! The task number to delete must be an integer.");
-            }
+            return parseDeleteCommand(args);
         case "todo":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The task description cannot be empty.");
-            }
-            task = new TodoTask(tokens[1].trim());
-            return new AddTaskCommand(ui, tasks, storage, CommandType.TODO, task);
+            return parseAddTodoCommand(args);
         case "deadline":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The task description cannot be empty.");
-            }
-            // idx 0: task name, idx 1: deadline
-            String[] deadlineTokens = tokens[1].split(" /by ", 2);
-            if (deadlineTokens.length < 2) {
-                throw new NixyException("BLAHH!!! The deadline of a deadline task must be specified.");
-            }
-            try {
-                task = new DeadlineTask(deadlineTokens[0].trim(),
-                    LocalDate.parse(deadlineTokens[1].trim()));
-            } catch (DateTimeParseException e) {
-                throw new NixyException(
-                    "BLAHH!!! The deadline of a deadline task must be a valid date. (e.g. 2024-12-31)");
-            }
-            return new AddTaskCommand(ui, tasks, storage, CommandType.DEADLINE, task);
+            return parseAddDeadlineCommand(args);
         case "event":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The task description cannot be empty.");
-            }
-            // idx 0: task name, idx 1: event times (unparsed)
-            String[] eventTokens = tokens[1].split(" /from ", 2);
-            if (eventTokens.length < 2) {
-                throw new NixyException("BLAHH!!! The start time of an event task must be specified.");
-            }
-            // idx 0: start time, idx 1: end time
-            String[] eventTimeTokens = eventTokens[1].split(" /to ", 2);
-            if (eventTimeTokens.length < 2) {
-                throw new NixyException("BLAHH!!! The end time of an event task must be specified.");
-            }
-            try {
-                task = new EventTask(
-                    eventTokens[0].trim(),
-                    LocalDate.parse(eventTimeTokens[0].trim()),
-                    LocalDate.parse(eventTimeTokens[1].trim())
-                );
-            } catch (DateTimeParseException e) {
-                throw new NixyException(
-                    "BLAHH!!! The start and end times of an event task must be valid dates. (e.g. 2024-12-31)");
-            }
-            return new AddTaskCommand(ui, tasks, storage, CommandType.EVENT, task);
+            return parseAddEventCommand(args);
         case "find":
-            if (tokens.length < 2) {
-                throw new NixyException("BLAHH!!! The keyword to search for cannot be empty.");
-            }
-            return new FindCommand(ui, tasks, tokens[1].trim());
+            return parseFindCommand(args);
         default:
             return new InvalidCommand(ui);
         }
+    }
+
+    private MarkCommand parseMarkCommad(String args, boolean isDone) {
+        String cleanedArgs = args.trim();
+        if (cleanedArgs.isEmpty()) {
+            throw new NixyException(
+                String.format("BLAHH!!! The task number to mark as %s cannot be empty.",
+                    isDone ? "done" : "undone"));
+        }
+        try {
+            int parsedInt = Integer.parseInt(cleanedArgs);
+            if (parsedInt < 1) {
+                throw new NixyException(
+                    String.format("BLAHH!!! The task number to mark as %s must be a positive integer.",
+                        isDone ? "done" : "undone"));
+            }
+            return new MarkCommand(ui, tasks, storage, parsedInt, isDone);
+        } catch (NumberFormatException e) {
+            throw new NixyException(
+                String.format("BLAHH!!! The task number to mark as %s must be an integer.",
+                    isDone ? "done" : "undone"));
+        }
+    }
+
+    private DeleteCommand parseDeleteCommand(String args) {
+        String cleanedArgs = args.trim();
+        if (cleanedArgs.isEmpty()) {
+            throw new NixyException("BLAHH!!! The task number to delete cannot be empty.");
+        }
+        try {
+            int parsedInt = Integer.parseInt(cleanedArgs);
+            if (parsedInt < 1) {
+                throw new NixyException("BLAHH!!! The task number to delete must be a positive integer.");
+            }
+            return new DeleteCommand(ui, tasks, parsedInt, storage);
+        } catch (NumberFormatException e) {
+            throw new NixyException("BLAHH!!! The task number to delete must be an integer.");
+        }
+    }
+
+    private AddTaskCommand parseAddTodoCommand(String args) {
+        String cleanedArgs = args.trim();
+        if (cleanedArgs.isEmpty()) {
+            throw new NixyException("BLAHH!!! The task description cannot be empty.");
+        }
+        Task task = new TodoTask(cleanedArgs);
+        return new AddTaskCommand(ui, tasks, storage, CommandType.TODO, task);
+    }
+
+    private AddTaskCommand parseAddDeadlineCommand(String args) {
+        String cleanedArgs = args.trim();
+        if (cleanedArgs.isEmpty()) {
+            throw new NixyException("BLAHH!!! The task description cannot be empty.");
+        }
+        // idx 0: task name, idx 1: deadline
+        String[] deadlineTokens = cleanedArgs.split(" /by ", 2);
+        if (deadlineTokens.length < 2) {
+            throw new NixyException("BLAHH!!! The deadline of a deadline task must be specified.");
+        }
+        Task task;
+        try {
+            task = new DeadlineTask(deadlineTokens[0].trim(),
+                LocalDate.parse(deadlineTokens[1].trim()));
+        } catch (DateTimeParseException e) {
+            throw new NixyException(
+                "BLAHH!!! The deadline of a deadline task must be a valid date. (e.g. 2024-12-31)");
+        }
+        assert task != null : "Task should not be null";
+        return new AddTaskCommand(ui, tasks, storage, CommandType.DEADLINE, task);
+    }
+
+    private AddTaskCommand parseAddEventCommand(String args) {
+        String cleanedArgs = args.trim();
+        if (cleanedArgs.isEmpty()) {
+            throw new NixyException("BLAHH!!! The task description cannot be empty.");
+        }
+        // idx 0: task name, idx 1: event times (unparsed)
+        String[] eventTokens = cleanedArgs.split(" /from ", 2);
+        if (eventTokens.length < 2) {
+            throw new NixyException("BLAHH!!! The start time of an event task must be specified.");
+        }
+        // idx 0: start time, idx 1: end time
+        String[] eventTimeTokens = eventTokens[1].split(" /to ", 2);
+        if (eventTimeTokens.length < 2) {
+            throw new NixyException("BLAHH!!! The end time of an event task must be specified.");
+        }
+        Task task;
+        try {
+            task = new EventTask(
+                eventTokens[0].trim(),
+                LocalDate.parse(eventTimeTokens[0].trim()),
+                LocalDate.parse(eventTimeTokens[1].trim())
+            );
+        } catch (DateTimeParseException e) {
+            throw new NixyException(
+                "BLAHH!!! The start and end times of an event task must be valid dates. (e.g. 2024-12-31)");
+        }
+        assert task != null : "Task should not be null";
+        return new AddTaskCommand(ui, tasks, storage, CommandType.EVENT, task);
+    }
+
+    private FindCommand parseFindCommand(String args) {
+        String cleanedArgs = args.trim();
+        if (cleanedArgs.isEmpty()) {
+            throw new NixyException("BLAHH!!! The keyword to search for cannot be empty.");
+        }
+        return new FindCommand(ui, tasks, cleanedArgs);
     }
 }
