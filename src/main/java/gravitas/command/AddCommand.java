7de206d74b1f04b0eb6b01db082d303704a1c220
hweinian <hweinian@gmail.com>
2024-09-16 19:58:51 +0800

    Fix add command bugs pertaining to whitespaces

--- src/main/java/gravitas/command/AddCommand.java
+++ src/main/java/gravitas/command/AddCommand.java
@@ -13,11 +13,7 @@ import gravitas.tasklist.TaskList;
  */
 public class AddCommand extends Command {
 
-    private static final String EMPTY_TODO = "OOPS!!! The description of a todo cannot be empty.";
-    private static final String EMPTY_EVENT = "OOPS!!! The description of a event cannot be empty "
-            + "and must contain /from and /to.";
-    private static final String EMPTY_DEADLINE = "OOPS!!! The description of a deadline "
-            + "cannot be empty and must contain /by ";
+    public static final String INVALID_TASK_FORMAT = "Invalid task format. Try using the help command!";
     private static final String ADDED = "Got it. I've added this task:";
     private String userInput;
 
@@ -37,33 +33,75 @@ public class AddCommand extends Command {
     public String executeCommand(TaskList taskList, Storage storage) throws GravitasException {
         String[] msgFrag = userInput.split(" ", 2);
         Task task;
-        if (msgFrag[0].equals("deadline")) {
-            if (msgFrag.length <= 1 || !userInput.contains("/by ")) {
-                throw new GravitasException(EMPTY_DEADLINE);
-            }
+        String taskType = msgFrag[0];
+        if (!validateTaskData(userInput)) {
+            throw new GravitasException(INVALID_TASK_FORMAT);
+        }
 
-            String[] deadline = userInput.split("/by ", 2);
-            //description format: [deadline, description]
-            String[] description = deadline[0].split(" ", 2);
+        String[] description = formatTaskData(userInput);
+
+        if (taskType.equals("deadline")) {
             assert description.length == 2 : "Description should not be empty";
-            task = new Deadline(description[1], deadline[1]);
-        } else if (msgFrag[0].equals("event")) {
-            if (msgFrag.length <= 1 || !userInput.contains("/from ") || !userInput.contains("/to ")) {
-                throw new GravitasException(EMPTY_EVENT);
-            }
-
-            String[] event = userInput.split("/from ", 2);
-            String[] formattedDeadline = event[1].split("/to ", 2);
-            task = new Event(event[0], formattedDeadline[0], formattedDeadline[1]);
+            task = new Deadline(description[0], description[1]);
+        } else if (taskType.equals("event")) {
+            task = new Event(description[0], description[1], description[2]);
+        } else if (taskType.equals("todo")) {
+            task = new Todo(description[0]);
         } else {
-            if (msgFrag.length <= 1) {
-                throw new GravitasException(EMPTY_TODO);
-            }
-
-            task = new Todo(msgFrag[1]);
+            throw new GravitasException(INVALID_TASK_FORMAT);
         }
         taskList.addTask(task);
         System.out.println(ADDED);
         return (ADDED + "\n" + taskList.printTask(task));
+    }
+
+    private boolean validateTaskData(String data) {
+        String[] trimmedData = data.split(" ");
+        String taskType = trimmedData[0];
+        boolean isValidOrder;
+        //Check for whitespace
+        for (String s : trimmedData) {
+            boolean isEmptyData = s.trim().isEmpty();
+            if (isEmptyData) {
+                return false;
+            }
+        }
+        //Assign order based on task type
+        switch (taskType) {
+        case "todo":
+            isValidOrder = (trimmedData.length == 2);
+            break;
+        case "deadline":
+            isValidOrder = (trimmedData.length == 5 && trimmedData[2].equals("/by"));
+            break;
+        case "event":
+            isValidOrder = (trimmedData.length == 8 && trimmedData[2].equals("/from")
+                    && trimmedData[5].equals("/to"));
+            break;
+        default:
+            isValidOrder = false;
+        }
+        return isValidOrder;
+    }
+
+    private String[] formatTaskData(String data) {
+        String[] trimmedData = data.split(" ");
+        String taskType = trimmedData[0];
+        String[] formattedData;
+        switch (taskType) {
+        case "todo":
+            formattedData = new String[]{trimmedData[1]};
+            break;
+        case "deadline":
+            formattedData = new String[]{trimmedData[1], trimmedData[3] + " " + trimmedData[4]};
+            break;
+        case "event":
+            formattedData = new String[]{trimmedData[1], trimmedData[3] + " " + trimmedData[4],
+                    trimmedData[6] + " " + trimmedData[7]};
+            break;
+        default:
+            formattedData = new String[]{};
+        }
+        return formattedData;
     }
 }
