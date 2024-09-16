8a2e1a98c945be07529ca153d32dba6b9066a0b9
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-09-16 11:34:43 +0800

    modify gui to make more engaging

--- src/main/java/schedulo/MainWindow.java
+++ src/main/java/schedulo/MainWindow.java
@@ -24,8 +24,8 @@ public class MainWindow extends AnchorPane {
 
     private Schedulo schedulo;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image scheduloImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image scheduloImage = new Image(this.getClass().getResourceAsStream("/images/chatbot.png"));
 
     @FXML
     public void initialize() {
