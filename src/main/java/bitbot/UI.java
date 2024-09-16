2ff4b97b66d7163a888f6050752c1676080601cf
SViswa02 <sviswa2002@gmail.com>
2024-09-16 22:43:07 +0800

    Edit phrases used by Bitbot for unique personality

--- src/main/java/bitbot/UI.java
+++ src/main/java/bitbot/UI.java
@@ -18,6 +18,8 @@ public class UI {
             + "deadline \n          "
             + "event\n          "
             + "list\n          "
+            + "tag\n          "
+            + "untag\n          "
             + "delete\n          "
             + "bye\n          "
             + "\n          "
@@ -26,11 +28,19 @@ public class UI {
             + "\n          "
             + "When keying in the date / time, do so in one of these formats:\n          "
             + "dd-MM-yyyy HH:mm or just dd-MM-yyyy or just HH:mm\n          "
+            + "\n          "
+            + "For tag, key it in this format: tag <number> #<tag name>\n          "
+            + "For untag, key it in this format: untag <number>\n          "
+            + "\n          "
+            + "For new users, please type in one of the keywords to begin :)\n          "
+            + "For existing users, you can view your list and start completing your assignments!\n          "
             + "____________________________________";
 
     private final String conclusion = "          ____________________________________\n"
-            + "          Bye. Thank you for using BitBot and hope you enjoyed my service!\n"
-            + "          Hope to see you again soon!\n"
+            + "          Sad to see you go :( Thank you for using BitBot and hope you enjoyed my service!\n"
+            + "          Don't worry, your tasks are saved and are safe with me :)"
+            + "          The next time you come, you can complete your tasks!!!!."
+            + "          Hope to see you again soon and goodbye!\n"
             + "          ____________________________________\n";
 
     public UI() {
