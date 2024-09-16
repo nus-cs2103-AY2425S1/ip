5d2a36add447bb0148062b9d53c41ded1c100fe3
Bryan <e0958519@u.nus.edu>
2024-09-16 17:14:56 +0800

    Level 9 part 1 - A-JavaDoc

--- src/main/java/beechat/util/Ui.java
+++ src/main/java/beechat/util/Ui.java
@@ -4,33 +4,66 @@ import beechat.task.TaskList;
 
 import java.util.Scanner;
 
+/**
+ * Handles the user interface.
+ * Displays messages to the user and reads user input.
+ */
 public class Ui {
+
+    /** Scanner to read user input from the command line. */
     private final Scanner sc;
 
+    /**
+     * Constructs a Ui object and initializes the scanner for reading user input.
+     */
     public Ui() {
         sc = new Scanner(System.in);
     }
 
+    /**
+     * Displays the welcome message when the application starts.
+     */
     public void showWelcome() {
         System.out.println("Hello! I'm Beechat!\nWhat can I do for you?\n");
     }
 
+    /**
+     * Reads the user's input.
+     *
+     * @return The user's input as a String.
+     */
     public String readCommand() {
         return sc.nextLine();
     }
 
+    /**
+     * Displays an error message if there was an error loading the task list.
+     */
     public void showLoadingError() {
         System.out.println("There was an error loading your task list.");
     }
 
+    /**
+     * Displays an error message.
+     *
+     * @param message The error message to be displayed.
+     */
     public void showError(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Displays the bye message when the user is leaving the application.
+     */
     public void showBye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    /**
+     * Displays all tasks in the task list to the user.
+     *
+     * @param tasks The TaskList object containing all tasks.
+     */
     public void showTasks(TaskList tasks) {
         if (tasks.isEmpty()) {
             System.out.println("No tasks so far");
