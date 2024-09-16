c4fe1b1fee4016f57ecda452e1c55512fd5470f0
Chen Zi Yi <ziyichen2003@gmail.com>
2024-09-17 00:03:33 +0800

    Add title to GUI window

--- src/main/java/bob/gui/Main.java
+++ src/main/java/bob/gui/Main.java
@@ -30,6 +30,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bob");
             fxmlLoader.<MainWindow>getController().setBob(bob); // inject the Bob instance
             stage.show();
         } catch (IOException e) {
