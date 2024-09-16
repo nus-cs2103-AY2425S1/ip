c1e3224c6f2244e10b48a59b4c7e1b4287217867
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-09-17 02:37:31 +0800

    Update GUI to show Yapper

--- src/main/java/yapper/Main.java
+++ src/main/java/yapper/Main.java
@@ -9,28 +9,39 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
- * The type Main.
+ * Main class that serves as the entry point for the Yapper application.
+ * This class extends {@code Application} and initializes the GUI by loading the FXML layout.
  */
 public class Main extends Application {
     private final Yapper yapper = new Yapper("data/tasks.txt");
 
+    /**
+     * The main method that launches the JavaFX application.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
 
+    /**
+     * Starts the JavaFX application by loading the main window from FXML and setting the scene.
+     *
+     * @param stage the primary stage for this application
+     */
     @Override
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+
+            stage.setTitle("Yapper");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setYapper(yapper);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 }
