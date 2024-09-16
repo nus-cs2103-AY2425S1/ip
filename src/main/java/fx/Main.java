5efd55772e53b450503901f023a60425a4e7c85c
lrongyi <iygnormal@gmail.com>
2024-09-16 21:55:02 +0800

    Update GUI and Ui.png

--- src/main/java/fx/Main.java
+++ src/main/java/fx/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Dook");
             stage.show();
             fxmlLoader.<MainWindow>getController().setDook(dook);
         } catch (IOException e) {
