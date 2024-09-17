ba5c046cee63b3718738040f82d4ef032f4c6fac
jevan <e1121960@u.nus.edu>
2024-09-18 01:56:51 +0800

    Update JUnit Test
    Add 2 more JUnit Test for Event toString() and toFile().
    Required at least 2 methods unit tested well using JUnit.

--- src/test/java/yapper/StringTest.java
+++ src/test/java/yapper/StringTest.java
@@ -42,4 +42,23 @@ public class StringTest {
         String actual = deadline.toFile();
         assertEquals(expected, actual);
     }
+    @Test
+    public void eventToStringTest() {
+        Event event = new Event("buy bread", LocalDateTime.of(2024, 12, 31, 18, 0),
+                LocalDateTime.of(2024, 12, 31, 21, 0));
+        event.setIsDone(true);
+        String expected = "[E][X] buy bread (FROM: Dec 31 2024 18:00 TO: Dec 31 2024 21:00)";
+        String actual = event.toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void eventToFileTest() {
+        Event event = new Event("buy bread", LocalDateTime.of(2024, 12, 31, 18, 0),
+                LocalDateTime.of(2024, 12, 31, 21, 0));
+        event.setIsDone(true);
+        String expected = "E D--buy bread--2024-12-31T18:00--2024-12-31T21:00";
+        String actual = event.toFile();
+        assertEquals(expected, actual);
+    }
 }
