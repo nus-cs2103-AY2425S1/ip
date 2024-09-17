3ef12949ab6ba29aa84ae6edda2496d0e91abd57
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-09-17 17:14:23 +0800

    Add HelpCommand for users to view available commands

--- src/main/java/rizzler/command/HelpCommand.java
+++ src/main/java/rizzler/command/HelpCommand.java
@@ -16,14 +16,24 @@ public class HelpCommand extends Command {
     }
 
     /**
-     * Prints out some lines of information that may be helpful to a new user.
+     * Returns a few lines of information that may be helpful to a new user.
      * @param storage <code>Storage</code> object instantiated in main <code>Rizzler</code> class.
      * @param taskLog <code>TaskLog</code> object instantiated in main <code>Rizzler</code> class.
      * @return Information on common commands and where to read the documentation.
      */
     @Override
     public String[] execute(Storage storage, TaskLog taskLog) {
-        return new String[] {"sorry, this command is not available yet."};
+        return new String[] {"welcome darlin'...",
+            "here are a few of the things you can do with this bot.",
+            "you can enter \"list\" to view all tasks you have at the moment",
+            "you can also enter \"todo\", \"deadline\", or \"event\" to create tasks of those types",
+            "once you've completed a task, you can \"mark\" it as done, or \"unmark\" it if you" +
+                    " realise afterwards that it's not quite done yet.",
+            "if you'd like, you can delete tasks that you no longer want to see with \"delete\"",
+            "and lastly, if ya' ever get bored of lil' ol' me, you can enter \"bye\" to exit the program.",
+            "",
+            "looking forward to workin' with ya' honey.",
+            "love, Rizzler."};
     }
 
 }
