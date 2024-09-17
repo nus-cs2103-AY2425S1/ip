03f69780c61c5e5c9b13a6419b95cd0575fdcac8
hovanleong <hovanleong1@gmail.com>
2024-09-18 04:36:05 +0800

    Change app icon and title

--- src/main/java/slothingwaffler/Main.java
+++ src/main/java/slothingwaffler/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -17,6 +18,8 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+            stage.setTitle("Slothing Waffler");
+            stage.getIcons().add(new Image("/images/sloth.png"));
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setSlothingWaffler(slothingWaffler);
