dac7dbf716ded26a3ebf380ffc38a18391673caa
jiaaaaen <girljiaen@gmail.com>
2024-09-17 15:14:38 +0800

    Improve code quality
    Shortened a very long method in the ui class by splitting into smaller methods.

--- src/main/java/Bwead/Bwead.java
+++ src/main/java/Bwead/Bwead.java
@@ -31,16 +31,6 @@ public class Bwead {
         }
         ui.set(history, tasks);
     }
-    /*
-    /**
-     * Starts the chatbot by calling ui.handleCommands(input) to handle the command inputs.
-     *
-     * @throws IOException when a failure occurs while performing read or write operations
-     *     from the handleCommands() method.
-     *
-    public void run() throws IOException {
-        System.out.println("Hello from " + name + "!");
-    }*/
 
     public String getResponse(String input) {
         try {
@@ -49,17 +39,5 @@ public class Bwead {
             return e.getMessage();
         }
     }
-
-    /*
-    /**
-     * Main method of Bwead.
-     *
-     * @param args
-     * @throws IOException when a failure occurs while performing read or write operations
-     *     from the run() method.
-     *
-    public static void main(String[] args) throws IOException {
-        new Bwead("./src/main/java/Bwead/historyFile.txt").run();
-    }*/
 }
 
--- src/main/java/Bwead/Ui.java
+++ src/main/java/Bwead/Ui.java
@@ -42,9 +42,7 @@ public class Ui {
      * @throws IOException when a failure occurs while performing scanning or write operations.
      */
     public String handleCommands(String input) throws IOException {
-
         Parser parser = new Parser(input);
-
         try {
             if (parser.isOneWord()) {
                 if (input.equals("todo")) {
@@ -64,73 +62,147 @@ public class Ui {
         } catch (BweadException e) {
             return e.getMessage();
         }
-
         try {
             if (input.equals("bye")) {
                 return ("Bye. Hope to see you again soon!");
             } else if (input.equals("list")) {
                 return taskList.printlist();
             } else if (input.startsWith("mark")) {
-                int toadd = parser.getTaskToMark();
-                Task task = taskList.getCurrentList().get(toadd - 1);
-                task.setDone(true);
-                history.updateFile(taskList.getCurrentList());
-                return ("Nice! I've marked this task as done: " + task.getText());
+                return handleMark(parser);
             } else if (input.startsWith("unmark")) {
-                int toadd = parser.getTaskToMark();
-                Task task = taskList.getCurrentList().get(toadd - 1);
-                task.setDone(false);
-                history.updateFile(taskList.getCurrentList());
-                return ("OK, I've marked this task as not done yet: " + task.getText());
+                return handleUnmark(parser);
             } else if (input.startsWith("todo ")) {
-                String todoName = parser.getTodoName();
-                Todo task = new Todo(todoName);
-                taskList.getCurrentList().add(task);
-                history.updateFile(taskList.getCurrentList());
-                return ("Got it. I've added this task: " + task.toString() + "\n" + "Now you have "
-                        + taskList.getCurrentList().size() + " tasks in the list.");
+                return handleTodo(parser);
             } else if (input.startsWith("deadline ")) {
-                Deadline task = new Deadline(parser.getDeadlineName(), parser.getDeadlineDate(),
-                        parser.getDeadlineTime());
-                taskList.getCurrentList().add(task);
-                history.updateFile(taskList.getCurrentList());
-                return ("Got it. I've added this task: " + task.toString() + "\n" + "Now you have "
-                        + taskList.getCurrentList().size() + " tasks in the list.");
+                return handleDeadline(parser);
             } else if (input.startsWith("event ")) {
-                Event task = new Event(parser.getEventName(), parser.getEventStart(), parser.getEventEnd(),
-                        parser.getEventStartTime(), parser.getEventEndTime());
-                taskList.getCurrentList().add(task);
-                history.updateFile(taskList.getCurrentList());
-                return ("Got it. I've added this task: " + task.toString() + "\n" + "Now you have "
-                        + taskList.getCurrentList().size() + " tasks in the list.");
+                return handleEvent(parser);
             } else if (input.startsWith("delete")) {
-                Task toremove = taskList.getCurrentList().get(parser.getDeleteIndex() - 1);
-                taskList.getCurrentList().remove(toremove);
-                history.updateFile(taskList.getCurrentList());
-                return ("Noted. I've removed this task: " + toremove.toString() + "\n" + "Now you have "
-                        + taskList.getCurrentList().size() + " tasks in the list.");
+                return handleDelete(parser);
             } else if (input.startsWith("find")) {
-                String keyword = parser.getKeyword();
-                ArrayList<String> matches = new ArrayList<>();
-                for (int i = 0; i < taskList.getCurrentList().size(); i++) {
-                    if (taskList.getCurrentList().get(i).getName().contains(keyword)) {
-                        matches.add(taskList.getCurrentList().get(i).toString());
-                    }
-                }
-                if (matches.size() == 0) {
-                    throw new BweadException("no matching tasks found");
-                }
-                String matchingTasksString = "Here are the matching tasks in your list:" + "\n";
-                for (int i = 0; i < matches.size(); i++) {
-                    matchingTasksString = matchingTasksString + (i + 1) + "." + matches.get(i) + "\n";
-                }
-                return matchingTasksString;
+                return handleFind(parser);
             }
         } catch (BweadException e) {
             return (e.getMessage());
         }
-
         return "";
+    }
+
+    /**
+     * Handles a mark command.
+     *
+     * @param parser
+     * @return string to return to user.
+     * @throws IOException
+     */
+    public String handleMark(Parser parser) throws IOException {
+        int toadd = parser.getTaskToMark();
+        Task task = taskList.getCurrentList().get(toadd - 1);
+        task.setDone(true);
+        history.updateFile(taskList.getCurrentList());
+        return ("Nice! I've marked this task as done: " + task.getText());
+    }
+
+    /**
+     * Handles an unmark command.
+     *
+     * @param parser
+     * @return string to return to user.
+     * @throws IOException
+     */
+    public String handleUnmark(Parser parser) throws IOException {
+        int toadd = parser.getTaskToMark();
+        Task task = taskList.getCurrentList().get(toadd - 1);
+        task.setDone(false);
+        history.updateFile(taskList.getCurrentList());
+        return ("OK, I've marked this task as not done yet: " + task.getText());
+    }
+
+    /**
+     * Handles a todo command by adding the todo task.
+     *
+     * @param parser
+     * @return string to return to user.
+     * @throws IOException
+     */
+    public String handleTodo(Parser parser) throws IOException {
+        String todoName = parser.getTodoName();
+        Todo task = new Todo(todoName);
+        taskList.getCurrentList().add(task);
+        history.updateFile(taskList.getCurrentList());
+        return ("Got it. I've added this task: " + task.toString() + "\n" + "Now you have "
+                + taskList.getCurrentList().size() + " tasks in the list.");
+    }
+
+    /**
+     * Handles a deadline command by adding the deadline task.
+     *
+     * @param parser
+     * @return string to return to user.
+     * @throws IOException
+     */
+    public String handleDeadline(Parser parser) throws IOException {
+        Deadline task = new Deadline(parser.getDeadlineName(), parser.getDeadlineDate(),
+                parser.getDeadlineTime());
+        taskList.getCurrentList().add(task);
+        history.updateFile(taskList.getCurrentList());
+        return ("Got it. I've added this task: " + task.toString() + "\n" + "Now you have "
+                + taskList.getCurrentList().size() + " tasks in the list.");
+    }
+
+    /**
+     * Handles an event command by adding the event task.
+     *
+     * @param parser
+     * @return string to return to user.
+     * @throws IOException
+     */
+    public String handleEvent(Parser parser) throws IOException {
+        Event task = new Event(parser.getEventName(), parser.getEventStart(), parser.getEventEnd(),
+                parser.getEventStartTime(), parser.getEventEndTime());
+        taskList.getCurrentList().add(task);
+        history.updateFile(taskList.getCurrentList());
+        return ("Got it. I've added this task: " + task.toString() + "\n" + "Now you have "
+                + taskList.getCurrentList().size() + " tasks in the list.");
+    }
+
+    /**
+     * Handles a delete command by deleting the task.
+     *
+     * @param parser
+     * @return string to return to user.
+     * @throws IOException
+     */
+    public String handleDelete(Parser parser) throws IOException {
+        Task toremove = taskList.getCurrentList().get(parser.getDeleteIndex() - 1);
+        taskList.getCurrentList().remove(toremove);
+        history.updateFile(taskList.getCurrentList());
+        return ("Noted. I've removed this task: " + toremove.toString() + "\n" + "Now you have "
+                + taskList.getCurrentList().size() + " tasks in the list.");
+    }
+
+    /**
+     * Handles a find command by searching the list.
+     *
+     * @param parser
+     * @return string to return to user.
+     */
+    public String handleFind(Parser parser) {
+        String keyword = parser.getKeyword();
+        ArrayList<String> matches = new ArrayList<>();
+        for (int i = 0; i < taskList.getCurrentList().size(); i++) {
+            if (taskList.getCurrentList().get(i).getName().contains(keyword)) {
+                matches.add(taskList.getCurrentList().get(i).toString());
+            }
+        }
+        if (matches.size() == 0) {
+            throw new BweadException("no matching tasks found");
+        }
+        String matchingTasksString = "Here are the matching tasks in your list:" + "\n";
+        for (int i = 0; i < matches.size(); i++) {
+            matchingTasksString = matchingTasksString + (i + 1) + "." + matches.get(i) + "\n";
+        }
+        return matchingTasksString;
     }
 }
 
