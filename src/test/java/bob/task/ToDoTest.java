42f7a17bbfadc843cc1dd7b917a45132863ffcf7
Colin Chiang <colinchiang.cc@gmail.com>
2024-09-17 20:23:57 +0800

    Update unit tests

--- src/test/java/bob/task/ToDoTest.java
+++ src/test/java/bob/task/ToDoTest.java
@@ -1,4 +1,4 @@
-package bob;
+package bob.task;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,25 +7,23 @@ import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoTest {
+public class ToDoTest {
     @Test
     public void testStringConversion() {
-        assertEquals(
-                new ToDo(
-                        "a todo").toString(),
-                "[T][ ] a todo");
+        Task todo = new ToDo("a todo");
+        assertEquals(todo.toString(), "[T][ ] a todo");
 
     }
     @Test
     public void testStringConversionForMarkedTodo() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HHmm");
+        LocalDateTime dummy = LocalDateTime.parse("12/09/24 0200", formatter);
+
         assertEquals(
                 new ToDo(
-                        "a todo", true).toString(),
+                        "a todo", true, dummy, dummy).toString(),
                 "[T][X] a todo");
     }
-
-
-
 
 
 }
