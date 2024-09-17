37599428c90de136018bb5caf980e211ead344f0
Ponharish <e1123017@u.nus.edu>
2024-09-17 13:41:13 +0800

    update ParserTest.java, TaskTest.java

--- src/test/java/meeju/ParserTest.java
+++ src/test/java/meeju/ParserTest.java
@@ -1,9 +1,8 @@
 package meeju;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
     private TaskList taskList = new TaskList(new StorageStub());
@@ -55,6 +54,68 @@ public class ParserTest {
         }
     }
 
+    @Test
+    public void emptyCommand() throws MeejuException {
+        String instruction = "  ";
+        String result = parser.parse(taskList, instruction);
+        assertEquals("Empty Command!", result);
+    }
+
+    @Test
+    public void hiCommand() throws MeejuException {
+        String instruction = "hi";
+        String result = parser.parse(taskList, instruction);
+        assertEquals("Meow! Hello There! How can i help you?", result);
+    }
+
+    @Test
+    public void parse_ListCommand() throws MeejuException {
+        String instruction = "list";
+        taskList.addTodoTask("Buy groceries");
+        String result = parser.parse(taskList, instruction);
+        assertEquals("1. [T][ ] Buy groceries\n", result);
+    }
+
+
+    @Test
+    public void parse_FindCommand() throws MeejuException {
+        String instruction = "find groceries";
+        taskList.addTodoTask("Buy groceries");
+        String result = parser.parse(taskList, instruction);
+        assertTrue(result.contains("Buy groceries"));
+    }
+
+    @Test
+    public void testParse_DeleteCommand() throws MeejuException {
+        taskList.addTodoTask("Read a book");
+        String instruction = "delete 1";
+        String result = parser.parse(taskList, instruction);
+        String expected = "Meow! I've removed this task:\n"
+                + "\t" + "[T][ ] Read a book"
+                + "\nNow you have " + 0 + " tasks in the list.";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void parse_MarkCommand() throws MeejuException {
+        taskList.addTodoTask("Read a book");
+        String instruction = "mark 1";
+        String result = parser.parse(taskList, instruction);
+        String expected = "Meow! I've marked this task as done:\n"
+                + "\t" + "[T][X] Read a book";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void parse_UnmarkCommand() throws MeejuException {
+        taskList.addTodoTask("Read a book");
+        parser.parse(taskList, "mark 1");  // Mark task as done first
+        String instruction = "unmark 1";
+        String result = parser.parse(taskList, instruction);
+        String expected = "Meow! I've marked this task as not done yet:\n"
+                + "\t" + "[T][ ] Read a book";
+        assertEquals(expected, result);
+    }
 
     //CHECKSTYLE.ON: MethodName
 }
