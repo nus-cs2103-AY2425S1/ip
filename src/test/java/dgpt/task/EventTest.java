c71bade4d427fec3222711dc6b79946662617f40
daronoh <daronohchundai@gmail.com>
2024-09-18 02:29:04 +0800

    Bug fix EventTest class
    Expected outputs for the EventTest class are incorrect.
    
    The expected output should have a 0 in front of the date if the
    date is only 1 digit.
    
    Let's add a 0 in front of the expected output so that the test will
    work as intended.

--- src/test/java/dgpt/task/EventTest.java
+++ src/test/java/dgpt/task/EventTest.java
@@ -12,14 +12,14 @@ public class EventTest {
     @Test
     public void toString_dateTime_success() throws DateTimeParseException {
         // normal creation of Event with proper time date formatting
-        assertEquals("[E][ ] test (from: 1 Jan 1999, 6:00am to: 2 Feb 2000, 8:00pm)",
+        assertEquals("[E][ ] test (from: 01 Jan 1999, 6:00am to: 02 Feb 2000, 8:00pm)",
                 new Event("test", "01/01/1999 0600", "02/02/2000 2000").toString());
     }
 
     @Test
     public void toString_dateTime_dateTimeParseExceptionThrown() {
         try {
-            assertEquals("[E][ ] test (from: 1 Jan 1999, 6:00am to: 2 Feb 2000, 8:00pm)",
+            assertEquals("[E][ ] test (from: 01 Jan 1999, 6:00am to: 02 Feb 2000, 8:00pm)",
                     new Event("test", "01-01-1999 0600", "02-02-2000 2000").toString());
             fail();
         } catch (DateTimeParseException e) {
