8485527e7eebcedc7eb61e705ba9f192b739f4b4
PresleyChew <E1115692@u.nus.edu>
2024-09-17 22:29:36 +0800

    Add JavaDocs for DialogBox

--- src/main/java/gutti/DialogBox.java
+++ src/main/java/gutti/DialogBox.java
@@ -24,6 +24,13 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    /**
+     * Constructs a DialogBox with the specified text and image.
+     * Loads the corresponding FXML layout and sets the text and image for the dialog.
+     *
+     * @param text The text to be displayed in the dialog box.
+     * @param img  The image representing the speaker.
+     */
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/DialogBox.fxml"));
@@ -48,10 +55,26 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_LEFT);
     }
 
+    /**
+     * Creates a dialog box for the user.
+     * The image and text are aligned with the user's perspective (text on the right, image on the left).
+     *
+     * @param text The user's input text to display.
+     * @param img  The user's avatar image.
+     * @return A DialogBox representing the user's dialog.
+     */
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
 
+    /**
+     * Creates a dialog box for Gutti.
+     * The image and text are flipped (text on the left, image on the right).
+     *
+     * @param text The response text from Gutti.
+     * @param img  The image representing Gutti.
+     * @return A DialogBox representing Gutti's dialog, with the image on the left and text on the right.
+     */
     public static DialogBox getGuttiDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
