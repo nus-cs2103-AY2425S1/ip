9e008c2c2074356a58755b4d49aefcef3f9ba5fe
sayomaki <sayomayomaki@gmail.com>
2024-09-18 01:56:37 +0800

    Setup and start the chat bot for GUI mode

--- src/main/java/alexer/Launcher.java
+++ src/main/java/alexer/Launcher.java
@@ -5,6 +5,8 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        Alexer alexer = new Alexer();
+        alexer.start();
         Application.launch(Gui.class, args);
     }
 }
