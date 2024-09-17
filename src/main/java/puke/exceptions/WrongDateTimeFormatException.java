82db14929565f006fad7b71847f95c72969a9368
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-09-18 06:23:02 +0800

    Resolve typo error in JavaDocs

--- src/main/java/puke/exceptions/WrongDateTimeFormatException.java
+++ src/main/java/puke/exceptions/WrongDateTimeFormatException.java
@@ -8,7 +8,7 @@ public class WrongDateTimeFormatException extends PukeException {
     /**
      * Constructs a WrongDateTimeFormatException with a message indicating the expected date and time format.
      *
-     * @param ExpectedPattern the expected date and time format pattern.
+     * @param expectedPattern the expected date and time format pattern.
      */
     public WrongDateTimeFormatException(String expectedPattern) {
         super("OOPS!! Please use the date and time format: \"" + expectedPattern + "\"");
