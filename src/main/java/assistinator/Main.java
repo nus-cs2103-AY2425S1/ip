c4d0e103fd6bed9fc840b70aebd78e46b350d8f1
Soumyadip <soumyadip1107@gmail.com>
2024-09-17 12:28:19 +0800

    Set title and app icon

--- src/main/java/assistinator/Main.java
+++ src/main/java/assistinator/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -18,6 +19,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setTitle("Assistinator");
+            stage.getIcons().add(new Image("/images/assistinator.png"));
             stage.setMinHeight(220);
             stage.setMinWidth(417);
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
