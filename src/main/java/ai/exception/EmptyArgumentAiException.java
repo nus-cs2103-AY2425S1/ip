d25725acc4391734e3c1e92604f1e3253f6b896f
Uselesstrash06 <uselesstrash06@gmail.com>
2024-09-17 14:33:08 +0800

    Change output on how empty fields are handled

--- src/main/java/ai/exception/EmptyArgumentAiException.java
+++ src/main/java/ai/exception/EmptyArgumentAiException.java
@@ -5,7 +5,7 @@ package ai.exception;
  */
 public class EmptyArgumentAiException extends AiException {
     public EmptyArgumentAiException(String command, String suggestion) {
-        super(String.format("Whoopsies... %s cannot be empty >.< \n Try something like \"%s\" instead!!!\n",
+        super(String.format("Whoopsies... %s cannot be empty >.< \n\nTry something like\n\n\"%s\"",
                 command, suggestion));
     }
 }
