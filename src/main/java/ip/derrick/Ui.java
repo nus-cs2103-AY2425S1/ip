de8f45734a23b521e02f37a4b188f249661a7371
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2024-09-18 18:10:23 +0800

    Remove method that has no usage

--- src/main/java/ip/derrick/Ui.java
+++ src/main/java/ip/derrick/Ui.java
@@ -6,20 +6,6 @@ package ip.derrick;
 public class Ui {
 
     /**
-     * Show the list of commands that the chatbot can perform.
-     */
-    public void showCommands() {
-        System.out.println("_____________________________________________________________________________");
-        System.out.println("List of Commands available : ");
-        int counter = 1;
-        for (Commands command : Commands.values()) {
-            System.out.println(counter + ". " + command.toString());
-            counter += 1;
-        }
-        System.out.println("_____________________________________________________________________________");
-    }
-
-    /**
      * Greets the user and introduce the chatbot to the user.
      */
     public void greetings() {
