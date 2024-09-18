2a8b6243bba4b9bc05d6191166a7f7e30b933be2
Jaehyeok Choi <77jack0105@gmail.com>
2024-09-18 15:39:46 +0800

    Adjust UI Blacknut Logo message

--- src/main/java/blacknut/ui/Ui.java
+++ src/main/java/blacknut/ui/Ui.java
@@ -8,12 +8,13 @@ import java.util.Scanner;
  * The Ui class handles user interaction by displaying messages and reading input.
  */
 class Ui {
-    private static final String LOGO = " ____  _            _                _   \n"
-            + "|  _ \\| |          | |              | |  \n"
-            + "| |_) | | __ _  ___| | ___ __  _   _| |_ \n"
-            + "|  _ <| |/ _ |/ __| |/ / '_ \\| | | | __|\n"
-            + "| |_) | | (_| | (__|   <| | | | |_| | |_ \n"
-            + "|____/|_|\\__,_|\\___|_|\\_\\_| |_|\\__,_|\\__|\n";
+    private static final String LOGO = " ____  _               _     _   _       _   _   \n"
+            + "|  _ \\| |             | |   | \\ | |     | | | |  \n"
+            + "| |_) | | __ _ _ __ __| |   |  \\| | __ _| |_| |_ \n"
+            + "|  _ <| |/ _` | '__/ _` |   | . ` |/ _` | __| __|\n"
+            + "| |_) | | (_| | | | (_| |_  | |\\  | (_| | |_| |_ \n"
+            + "|____/|_|\\__,_|_|  \\__,_(_) |_| \\_|\\__,_|\\__|\\__|\n";
+
 
     public void showWelcome() {
         System.out.println("Hello from\n" + LOGO);
