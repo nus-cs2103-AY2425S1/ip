432d83d798615fc2de81ebbd70453bc71c1580ea
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-09-18 10:36:39 +0800

    Update javadocs accordingly

--- src/main/java/stobberi/stobberiexception/InvalidNumberStobberiException.java
+++ src/main/java/stobberi/stobberiexception/InvalidNumberStobberiException.java
@@ -1,15 +1,15 @@
 package stobberi.stobberiexception;
 
 /**
- * Custom exception for cases where a Stobberi object lacks a number.
+ * Custom exception for cases where the number is not valid.
  *
  * <p>This exception extends {@link StobberiException} and indicates that
- * an operation failed because the Stobberi object does not contain a number.</p>
+ * an operation failed because the number is too big or too small.</p>
  */
 public class InvalidNumberStobberiException extends StobberiException {
 
     /**
-     * Constructs a new NoNumberStobberiException with the specified detail message.
+     * Constructs a new InvalidNumberStobberiException with the specified detail message.
      *
      * @param message the detail message, which is saved for later retrieval by
      *                the {@link #getMessage()} method.
