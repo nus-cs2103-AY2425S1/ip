d56fea4dda0361ea6a87d7ee4c46692cd574f46d
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2024-09-19 03:12:12 +0800

    Use final static field for file path

--- src/main/java/DPlusPlusE.java
+++ src/main/java/DPlusPlusE.java
@@ -8,6 +8,7 @@ public class DPlusPlusE {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+    private static final String FILE_PATH = "./data/duke.txt";
 
     public DPlusPlusE(String filePath) {
         storage = new Storage(filePath);
@@ -21,8 +22,8 @@ public class DPlusPlusE {
     }
 
     public DPlusPlusE() {
-        this("./data/duke.txt");
-        assert new File("./data/duke.txt").exists() : "File should exist";
+        this(FILE_PATH);
+        assert new File(FILE_PATH).exists() : "File should exist";
         assert storage != null : "Storage should not be null";
         assert tasks != null : "Tasks should not be null";
         assert ui != null : "UI should not be null";
