47ba9bc7e74f9dcd04cdce6b838669e88b8fbd74
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-09-18 16:14:00 +0800

    Change greeting

--- src/main/java/stobberi/components/Ui.java
+++ src/main/java/stobberi/components/Ui.java
@@ -12,7 +12,7 @@ public class Ui {
                     "\n"
                     + "What would you like to do with your task list today?\n" +
                     "\n" +
-                    "If you dunno what you to do, just type '?'!!  :)   ";
+                    "If you dunno what to do, just type '?'!!  :)   ";
     private static final String GOODBYE_GREETING = "Baiii baiiii!\n" +
             "\n" +
             "Seeee yaaaa! :)";
