fecbfb51b0b652b0a9a2491c15bd81443f765ec8
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-09-18 09:24:12 +0800

    Add error handling for extra input in command

--- src/main/java/stobberi/command/ExitCommand.java
+++ src/main/java/stobberi/command/ExitCommand.java
@@ -1,18 +1,26 @@
 package stobberi.command;
 
 import stobberi.components.Ui;
+import stobberi.stobberiexception.NoSuchTaskStobberiException;
+import stobberi.stobberiexception.StobberiException;
 
 /**
  * Represents a command that terminates the application.
  * This command sets the exit flag to true, indicating that the application should exit.
  */
 public class ExitCommand extends Command {
+    /**
+     * The rest of the command given.
+     */
+    private String restOfCommand;
 
     /**
      * Constructs a new {@code ExitCommand} and initializes the command with the exit flag set to false.
+     *
+     * @param restOfCommand The rest of the command given.
      */
-    public ExitCommand() {
-        super();
+    public ExitCommand(String restOfCommand) {
+        this.restOfCommand = restOfCommand;
     }
 
     /**
@@ -20,7 +28,11 @@ public class ExitCommand extends Command {
      * This indicates that the application should terminate.
      */
     @Override
-    public String execute() {
+    public String execute() throws StobberiException {
+        if (!restOfCommand.isEmpty()) {
+            throw new NoSuchTaskStobberiException("Do you want exit? Just say 'bye'!");
+        }
+
         setExitTrue();
         return Ui.sayGoodbye();
     }
