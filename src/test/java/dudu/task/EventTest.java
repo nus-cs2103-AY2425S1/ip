69030a9308bd9c32408bfae7ba9ec3f1cfff65c0
ngjianyi <ngjianyi2002@gmail.com>
2024-09-18 23:50:43 +0800

    Edit JUnit tests

--- src/test/java/dudu/task/EventTest.java
+++ src/test/java/dudu/task/EventTest.java
@@ -8,14 +8,28 @@ import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
-    public void testEventToString() {
+    public void parseString_unmarkedEventTask_displayFormat() {
         Event task = new Event("this", LocalDate.parse("2024-08-30"), LocalDate.parse("2024-08-31"));
         assertEquals("[E] [ ] this (from: Aug 30 2024 to: Aug 31 2024)", task.toString());
     }
 
     @Test
-    public void testEventFormatString() {
+    public void parseString_unmarkedEventTask_storageFormat() {
         Event task = new Event("this", LocalDate.parse("2024-07-20"), LocalDate.parse("2024-09-13"));
-        assertEquals("E | 0 | this | 2024-07-20 | 2024-09-13", task.formatString());
+        assertEquals("E | 0 | this | 2024-07-20 | 2024-09-13", task.toStorageString());
+    }
+
+    @Test
+    public void parseString_markedEventTask_displayFormat() {
+        Event task = new Event("this", LocalDate.parse("2024-09-30"), LocalDate.parse("2024-10-12"));
+        task.markCompleted();
+        assertEquals("[E] [X] this (from: Sept 30 2024 to: Oct 12 2024)", task.toString());
+    }
+
+    @Test
+    public void parseString_markedEventTask_storageFormat() {
+        Event task = new Event("this", LocalDate.parse("2024-01-24"), LocalDate.parse("2024-03-13"));
+        task.markCompleted();
+        assertEquals("E | 1 | this | 2024-01-24 | 2024-03-13", task.toStorageString());
     }
 }
--- src/test/java/dudu/utils/ParserTest.java
+++ src/test/java/dudu/utils/ParserTest.java
@@ -2,19 +2,25 @@ package dudu.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.time.format.DateTimeParseException;
+import java.time.LocalDate;
 
+import dudu.command.AddCommand;
 import org.junit.jupiter.api.Test;
 
 import dudu.command.Command;
 import dudu.command.InvalidCommand;
+import dudu.exception.DuduException;
 import dudu.exception.InvalidFormatException;
 import dudu.exception.MissingDateTimeException;
 import dudu.exception.MissingDescriptionException;
+import dudu.task.Deadline;
+
+
+
 
 public class ParserTest {
     @Test
-    public void parse_invalidCommand_invalidCommandReturned() {
+    public void parseInvalid_invalidCommand_invalidCommandReturned() {
         try {
             Command outputCommand = Parser.parse("invalid");
             Command expectedCommand = new InvalidCommand();
@@ -23,42 +29,43 @@ public class ParserTest {
             System.out.println(exception);
         } catch (InvalidFormatException exception) {
             System.out.println(exception);
-        } catch (DateTimeParseException exception) {
+        } catch (DuduException exception) {
             System.out.println(exception);
         } catch (MissingDateTimeException exception) {
             System.out.println(exception);
         }
     }
-//    @Test
-//    public void testDeadlineCommand() {
-//        try {
-//            Command command = Parser.parse("deadline this /by 2024-08-30");
-//            Deadline task = new Deadline("this", LocalDate.parse("2024-08-30"));
-//            assertEquals(new CommandDeadline(task, false), command);
-//        } catch (MissingDescriptionException exception) {
-//            System.out.println(exception);
-//        } catch (InvalidFormatException exception) {
-//            System.out.println(exception);
-//        } catch (DateTimeParseException exception) {
-//            System.out.println(exception);
-//        } catch (MissingDateTimeException exception) {
-//            System.out.println(exception);
-//        }
-//    }
 
-//    @Test
-//    public void testListCommand() {
-//        try {
-//            Command command = Parser.parse("list");
-//            assertEquals(new CommandList(), command);
-//        } catch (MissingDescriptionException exception) {
-//            System.out.println(exception);
-//        } catch (InvalidFormatException exception) {
-//            System.out.println(exception);
-//        } catch (DateTimeParseException exception) {
-//            System.out.println(exception);
-//        } catch (MissingDateTimeException exception) {
-//            System.out.println(exception);
-//        }
-//    }
+    @Test
+    public void parseDeadline_validDate_addCommandReturned() {
+        try {
+            Command command = Parser.parse("deadline this /by 2024-08-30");
+            Deadline task = new Deadline("this", LocalDate.parse("2024-08-30"));
+            assertEquals(new AddCommand(task, false), command);
+        } catch (MissingDescriptionException exception) {
+            System.out.println(exception);
+        } catch (InvalidFormatException exception) {
+            System.out.println(exception);
+        } catch (DuduException exception) {
+            System.out.println(exception);
+        } catch (MissingDateTimeException exception) {
+            System.out.println(exception);
+        }
+    }
+
+    @Test
+    public void parseEvent_invalidDate_addCommandReturned() {
+        try {
+            Parser.parse("deadline this /by 2024-08-30");
+        } catch (MissingDescriptionException exception) {
+            System.out.println(exception);
+        } catch (InvalidFormatException exception) {
+            System.out.println(exception);
+        } catch (DuduException exception) {
+            assertEquals(exception.getMessage(), "Please use YYYY-MM-DD as the date format");
+            System.out.println(exception);
+        } catch (MissingDateTimeException exception) {
+            System.out.println(exception);
+        }
+    }
 }
