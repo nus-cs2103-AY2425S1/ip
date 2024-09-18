685f1c4a1ffb90314470b374f8607825dcdc90ae
Apollo <shixintan01@gmail.com>
2024-09-18 23:18:43 +0800

    Add title and icon for greater personalisation

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,8 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import ui.MainWindow;
+import javafx.scene.image.Image;
+
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * A GUI for Duke using FXML.
@@ -15,6 +18,8 @@ import java.io.IOException;
 public class Main extends Application {
 
     private KukiShinobu kukiShinobu = new KukiShinobu();
+
+    private static final String APPLICATION_TITLE = "Kuki Shinobu";
 
     @Override
     public void start(Stage stage) {
@@ -30,6 +35,14 @@ public class Main extends Application {
 
             // Assert that the kukiShinobu field of MainWindow is not null
             assert kukiShinobu != null : "kukiShinobu should not be null";
+
+            // Set the title of the window
+            stage.setTitle(Main.APPLICATION_TITLE);
+
+            // Set the app logo to the avatar of Kuki Shinobu
+            Image icon = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/KukiShinobuAvatar.png")));
+            stage.getIcons().add(icon);
+
 
             controller.setStage(stage);
             stage.show();
