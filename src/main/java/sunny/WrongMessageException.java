eb10a333d6ebf9f19a07a28619e0bec8a90cb2e4
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-09-18 09:43:02 +0800

    Add javadoc for WrongMessageException class

--- src/main/java/sunny/WrongMessageException.java
+++ src/main/java/sunny/WrongMessageException.java
@@ -1,5 +1,9 @@
 package sunny;
-public class WrongMessageException  extends Exception{
+
+/**
+ * Inherits from exception, thrown when user input a invalid message or invalid timeline
+ */
+public class WrongMessageException extends Exception{
     public WrongMessageException() {
         super("Please enter your task in a proper format");
     }
