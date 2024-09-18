b50a821ffe5a8cf570a43be63c820ea455c05eee
Jian Yi <e1075543@u.nus.edu>
2024-09-18 18:09:32 +0800

    Set title for the main window using JavaFX

--- src/main/java/chatbaby/Main.java
+++ src/main/java/chatbaby/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("ChatBaby");
             fxmlLoader.<MainWindow>getController().setChatBaby(baby); // inject the ChatBaby instance
             stage.show();
         } catch (IOException e) {
