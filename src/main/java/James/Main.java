1b61fd8140c9f384f0b2a04d29401df64b2db41e
shuckycheese <darrylshuck@yahoo.com.sg>
2024-09-19 03:07:28 +0800

    Add Ui picture to docs

--- src/main/java/James/Main.java
+++ src/main/java/James/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("James");
             fxmlLoader.<MainWindow>getController().setJames(james);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
