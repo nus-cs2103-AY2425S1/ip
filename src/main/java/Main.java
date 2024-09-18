1c4d1f8d386a2ff88424d57c66c7e82778fb28c8
Isaac Pang <isaac.pangth@gmail.com>
2024-09-18 22:05:26 +0800

    Add title

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Him");
             fxmlLoader.<control.MainWindow>getController().setHim(him);  // inject the Him instance
             stage.show();
         } catch (IOException e) {
