a2c06cf6274ea113e71e7bb624f44917776e0149
uniqly <simontong09@gmail.com>
2024-09-18 22:24:26 +0800

    EventTask: Add check for if from date is before to date

--- src/main/java/ekud/task/EventTask.java
+++ src/main/java/ekud/task/EventTask.java
@@ -35,6 +35,9 @@ public class EventTask extends Task implements IHasDeadline {
     private static final String WRONG_DATE_FORMAT_MESSAGE = """
             Man... What's wrong with you!!
             Why can't you just follow the correct 'd/M/yyyy HHmm' date format!""";
+    private static final String START_BEFORE_END_MESSAGE = """
+            You might have missed it, but the start of the event must before the end!
+              %s is NOT before %s!!""";
     private static final String SAVE_STRING_FORMAT = "E | %s | %s | %s";
     private static final String STRING_FORMAT = "[E]%s (from: %s to: %s)";
 
@@ -54,17 +57,27 @@ public class EventTask extends Task implements IHasDeadline {
      */
     public EventTask(String description, String from, String to) throws EkudException {
         super(description);
+        // check for empty strings
         if (from == null || from.isEmpty()) {
             throw new EkudException(EMPTY_FROM_MESSAGE);
         } else if (to == null || to.isEmpty()) {
             throw new EkudException(EMPTY_TO_MESSAGE);
         }
 
+        // try to parse dates
         try {
             this.from = LocalDateTime.parse(from, READ_DATE_FORMAT);
             this.to = LocalDateTime.parse(to, READ_DATE_FORMAT);
         } catch (DateTimeParseException e) {
             throw new EkudException(WRONG_DATE_FORMAT_MESSAGE);
+        }
+
+        // check for date errors
+        if (this.from.isAfter(this.to)) {
+            throw new EkudException(
+                    String.format(START_BEFORE_END_MESSAGE,
+                            this.from.format(PRINT_DATE_FORMAT),
+                            this.to.format(PRINT_DATE_FORMAT)));
         }
     }
 
