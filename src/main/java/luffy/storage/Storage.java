66df6fe85c04a45e9ec9f6767dbfc7266ecac192
Solomon0519 <ng.eswen1@gmail.com>
2024-09-18 11:45:31 +0800

    Change file system so that it tackles file not file
    Previous iterations forget to take into account that other computers may not have the file.
    
    Rewrite code to create a directory and new file in their local computer.

--- src/main/java/luffy/storage/Storage.java
+++ src/main/java/luffy/storage/Storage.java
@@ -20,16 +20,17 @@ import luffy.task.Todo;
  */
 public class Storage {
 
-    private static final String DEST_FILE = "./LuffyData/TaskData";
-    private final String filePath;
+    private static final String DATA_DIR = "data/";
+    private static final String DEFAULT_FILE = "luffy.txt";
+    private static Storage storage;
 
-    public Storage(String filePath) {
-        this.filePath = filePath;
+    public static Storage getStorage() {
+        if (storage == null) {
+            storage = new Storage();
+        }
+        return storage;
     }
 
-    public Storage() {
-        this.filePath = DEST_FILE;
-    }
     /**
      * This method reads a task list and saves the content of the
      * task list into a text file
@@ -38,17 +39,19 @@ public class Storage {
      * @throws IOException if there is issues with the input to file
      */
     public void saveToFile(TaskList taskList) throws IOException {
+        try {
+            FileWriter fileStream = new FileWriter(DATA_DIR + DEFAULT_FILE);
+            BufferedWriter fileInfo = new BufferedWriter(fileStream);
 
-        File saveFile = new File(this.filePath);
-        FileWriter fileStream = new FileWriter(saveFile);
-        BufferedWriter fileInfo = new BufferedWriter(fileStream);
+            for (Task task : taskList.getTasks()) {
+                fileInfo.write(String.format("%s%n", task.dataToSave()));
+            }
 
-        for (Task task : taskList.getTasks()) {
-            fileInfo.write(String.format("%s%n", task.dataToSave()));
+            fileInfo.flush();
+            fileInfo.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-
-        fileInfo.flush();
-        fileInfo.close();
     }
 
     /**
@@ -61,37 +64,52 @@ public class Storage {
     public TaskList loadFromFile() throws FileNotFoundException {
 
         TaskList loadedTasks = new TaskList();
-        BufferedReader fileReader = new BufferedReader(new FileReader(this.filePath));
-
         try {
-            String currentLine = fileReader.readLine();
-            while (currentLine != null) {
-
-                String[] taskInfo = currentLine.split("\\|");
-                String taskToDo = taskInfo[2].trim();
-                boolean isDone = Integer.parseInt(taskInfo[1].trim()) == 1;
-
-                switch (taskInfo[0].trim()) {
-                case "TO-DO":
-                    loadedTasks.addTask(new Todo(taskToDo, isDone));
-                    break;
-                case "DEADLINE":
-                    String deadline = taskInfo[3].trim().substring(4);
-                    loadedTasks.addTask(new Deadline(taskToDo, deadline, isDone));
-                    break;
-                case "EVENT":
-                    String start = taskInfo[3].trim().substring(7);
-                    String end = taskInfo[4].trim().substring(5);
-                    loadedTasks.addTask(new Event(taskToDo, start, end, isDone));
-                    break;
-                default:
-                    break;
-                }
-                currentLine = fileReader.readLine();
+            File saveFileDirectory = new File(DATA_DIR);
+            if (!saveFileDirectory.exists()) {
+                boolean madeDirectory = saveFileDirectory.mkdir();
             }
+
+            File saveFile = new File(DATA_DIR + DEFAULT_FILE);
+            if (!saveFile.exists()) {
+                boolean madeFile = saveFile.createNewFile();
+            }
+
+            BufferedReader fileReader = new BufferedReader(new FileReader(DATA_DIR + DEFAULT_FILE));
+
+            try {
+                String currentLine = fileReader.readLine();
+                while (currentLine != null) {
+
+                    String[] taskInfo = currentLine.split("\\|");
+                    String taskToDo = taskInfo[2].trim();
+                    boolean isDone = Integer.parseInt(taskInfo[1].trim()) == 1;
+
+                    switch (taskInfo[0].trim()) {
+                    case "TO-DO":
+                        loadedTasks.addTask(new Todo(taskToDo, isDone));
+                        break;
+                    case "DEADLINE":
+                        String deadline = taskInfo[3].trim().substring(4);
+                        loadedTasks.addTask(new Deadline(taskToDo, deadline, isDone));
+                        break;
+                    case "EVENT":
+                        String start = taskInfo[3].trim().substring(7);
+                        String end = taskInfo[4].trim().substring(5);
+                        loadedTasks.addTask(new Event(taskToDo, start, end, isDone));
+                        break;
+                    default:
+                        break;
+                    }
+                    currentLine = fileReader.readLine();
+                }
+            } catch (IOException e) {
+                System.out.println("File has not content");
+            }
+            return loadedTasks;
         } catch (IOException e) {
-            System.out.println("File has not content");
+            System.out.println(e.getMessage());
         }
-        return loadedTasks;
+        return new TaskList();
     }
 }
