52c75afa4861059827b0a42757b04add4d70e809
WindiStone <e0958609@u.nus.edu>
2024-09-18 20:52:54 +0800

    runtest.sh: Change directory

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp /Users/winston/Desktop/Code/CS2103/ip/src/main/java -Xlint:none -d ../bin /Users/winston/Desktop/Code/CS2103/ip/src/main/java/windebot/Winde.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
