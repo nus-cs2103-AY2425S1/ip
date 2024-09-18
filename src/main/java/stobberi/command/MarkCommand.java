5af8ced38a327bfc7e4c81fea907950f5fedc01c
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-09-18 08:56:43 +0800

    Throw an error

--- src/main/java/stobberi/command/MarkCommand.java
+++ src/main/java/stobberi/command/MarkCommand.java
@@ -1,6 +1,7 @@
 package stobberi.command;
 
 import stobberi.components.TaskList;
+import stobberi.stobberiexception.NoNumberStobberiException;
 import stobberi.stobberiexception.StobberiException;
 
 /**
@@ -11,21 +12,20 @@ public class MarkCommand extends Command {
      * The list of tasks where the task will be marked as completed.
      */
     private TaskList taskList;
-
     /**
-     * The number of the task to be marked as completed.
+     * The rest of the command given.
      */
-    private int taskNumber;
+    private String restOfCommand;
 
     /**
      * Constructs a new {@code MarkCommand} with the specified {@link TaskList} and task number.
      *
      * @param taskList   The list of tasks where the task will be marked as completed.
-     * @param taskNumber The number of the task to be marked as completed.
+     * @param restOfCommand The rest of command given.
      */
-    public MarkCommand(TaskList taskList, int taskNumber) {
+    public MarkCommand(TaskList taskList, String restOfCommand) {
         this.taskList = taskList;
-        this.taskNumber = taskNumber;
+        this.restOfCommand = restOfCommand;
     }
 
     /**
@@ -33,6 +33,12 @@ public class MarkCommand extends Command {
      */
     @Override
     public String execute() throws StobberiException {
+        int taskNumber;
+        if (restOfCommand.matches("\\d+")) {
+            taskNumber = Integer.parseInt(restOfCommand);
+        } else {
+            throw new NoNumberStobberiException("Where is the number???");
+        }
         return taskList.markTask(taskNumber);
     }
 }
