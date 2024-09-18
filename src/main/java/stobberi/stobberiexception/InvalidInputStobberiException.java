8f47bba521dd75282d460df873881bc2676f9162
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-09-18 10:35:34 +0800

    Edit the javadoc

--- src/main/java/stobberi/stobberiexception/InvalidInputStobberiException.java
+++ src/main/java/stobberi/stobberiexception/InvalidInputStobberiException.java
@@ -1,20 +1,20 @@
 package stobberi.stobberiexception;
 
 /**
- * Custom exception for cases where a Stobberi object lacks a number.
+ * Custom exception for cases where a Stobberi object has wrong input.
  *
  * <p>This exception extends {@link StobberiException} and indicates that
- * an operation failed because the Stobberi object does not contain a number.</p>
+ * an operation failed because command input is not what is expected.</p>
  */
-public class InvalidNumberStobberiException extends StobberiException {
+public class InvalidInputStobberiException extends StobberiException {
 
     /**
-     * Constructs a new NoNumberStobberiException with the specified detail message.
+     * Constructs a new InvalidInputStobberiException with the specified detail message.
      *
      * @param message the detail message, which is saved for later retrieval by
      *                the {@link #getMessage()} method.
      */
-    public InvalidNumberStobberiException(String message) {
+    public InvalidInputStobberiException(String message) {
         super(message);
     }
 }
