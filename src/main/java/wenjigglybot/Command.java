a40020ba87bf31fe9f077b519c2186c7a7c14e3e
wenjebs <wenjieb2@gmail.com>
2024-09-18 16:31:19 +0800

    Add more javadocs

--- src/main/java/wenjigglybot/Command.java
+++ src/main/java/wenjigglybot/Command.java
@@ -1,5 +1,9 @@
 package wenjigglybot;
 
+/**
+ * Enumeration representing the various commands available in the wenjigglybot system.
+ * Each command corresponds to a specific action that the user can perform.
+ */
 public enum Command {
     LIST,
     MARK,
--- src/main/java/wenjigglybot/DeadlineException.java
+++ src/main/java/wenjigglybot/DeadlineException.java
@@ -1,6 +1,18 @@
 package wenjigglybot;
 
+/**
+ * Custom exception class for handling incorrect deadline input in the wenjigglybot system.
+ * This exception is thrown when the deadline format provided by the user is invalid.
+ * <p>
+ * Example of a correct deadline input: "deadline (name) /by (date)".
+ */
 public class DeadlineException extends Exception {
+
+    /**
+     * Overrides the default toString method to provide a custom error message.
+     *
+     * @return A string explaining the correct deadline format to the user.
+     */
     @Override
     public String toString() {
         return "You have provided an incorrect deadline input!!! Correct format is deadline (name) /by (date)";
