b1032a320accc870f91e557fcf583341fe086f5c
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2024-09-19 03:48:42 +0800

    Update markdown file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("D++E");
             fxmlLoader.<MainWindow>getController().setDPlusPlusE(dPlusPlusE);
             stage.show();
         } catch (IOException e) {
