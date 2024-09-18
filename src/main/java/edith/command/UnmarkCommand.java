2807e09b7d51e341ea0a4cecfd1153479a4958ea
sam-theman88 <samuel.lee9525@gmail.com>
2024-09-18 15:46:40 +0800

    Fix indexing bug in error message

--- src/main/java/edith/command/UnmarkCommand.java
+++ src/main/java/edith/command/UnmarkCommand.java
@@ -44,7 +44,8 @@ public class UnmarkCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws EdithException {
         if (index < 0 || index >= tasks.getNumOfTasks()) {
-            throw new EdithException("Task " + index + " does not exist. Please enter a valid task number.");
+            int num = index + 1;
+            throw new EdithException("Task " + num + " does not exist. Please enter a valid task number.");
         }
 
         Task taskToUnmark = tasks.getTask(index);
