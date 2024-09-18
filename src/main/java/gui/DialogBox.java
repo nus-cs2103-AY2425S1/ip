b811d07b32c8467faedc497f9a9448b723b0746b
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-09-18 23:01:35 +0800

    Add assertions to multiple classes

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -25,6 +25,8 @@ public class DialogBox extends HBox {
     private ImageView displayPicture;
 
     private DialogBox(String text, Image img) {
+        assert text != null : "text must not be null";
+        assert img != null : "img must not be null";
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
--- src/main/java/rizzler/ui/parser/DateTimeParser.java
+++ src/main/java/rizzler/ui/parser/DateTimeParser.java
@@ -13,9 +13,9 @@ public class DateTimeParser {
      * Handles conversion of a string in <code>YYYY-MM-DD</code> format to a datetime object.
      * @param inputStr String representing a date in <code>YYYY-MM-DD</code> format.
      * @return <code>LocalDate</code> object representing the same date provided by the user.
-     * @throws DateTimeParseException If the user's input string cannot be interpreted as a date.
      */
-    public static LocalDate toDatetime(String inputStr) throws DateTimeParseException {
+    public static LocalDate toDatetime(String inputStr) {
+        assert inputStr != null : "inputStr is null";
         return LocalDate.parse(inputStr);
     }
 
@@ -23,9 +23,10 @@ public class DateTimeParser {
      * Returns a boolean representing whether the <code>DateTimeParser</code> is able to parse
      * this particular input string.
      * @param inputStr User's input string.
-     * @return Whether this input string is possible to parse.
+     * @return Whether this input string can be parsed by <code>toDatetime</code>.
      */
     public static boolean canParse(String inputStr) {
+        assert inputStr != null : "inputStr is null";
         try {
             toDatetime(inputStr);
             return true;
@@ -37,9 +38,10 @@ public class DateTimeParser {
     /**
      * Returns a string formatting a particular datetime.
      * @param inputDate LocalDate object representing the date.
-     * @return Nicely formatting string for a particular date.
+     * @return Nicely formatted string for a particular date.
      */
     public static String toStr(LocalDate inputDate) {
+        assert inputDate != null : "inputDate is null";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM, yyyy");
         return inputDate.format(formatter);
     }
