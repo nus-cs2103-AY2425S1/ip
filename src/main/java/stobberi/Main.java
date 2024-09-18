762f840d2fee00e6bd45d19658427425b26dcd16
ahmadsyuaib <ahmadsyuaib02@gmail.com>
2024-09-18 15:57:11 +0800

    Add icon and application name onto windows title bar

--- src/main/java/stobberi/Main.java
+++ src/main/java/stobberi/Main.java
@@ -2,6 +2,7 @@ package stobberi;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -21,10 +22,13 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            Image icon = new Image(this.getClass().getResourceAsStream("/images/Stobberi_icon.png"));
             stage.setScene(scene);
 
             stage.setMinHeight(220);
             stage.setMinWidth(417);
+            stage.setTitle("Stobberi TaskBot");
+            stage.getIcons().add(icon);
 
             fxmlLoader.<MainWindow>getController().setStobberi(stobberi);  // inject the Stobberi instance
             stage.show();
