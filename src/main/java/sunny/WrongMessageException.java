e431385ebe7c96a9f0a7e75739ddf09182dd8cf5
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-09-18 11:03:00 +0800

    Make minor coding standard improvements

--- src/main/java/sunny/WrongMessageException.java
+++ src/main/java/sunny/WrongMessageException.java
@@ -3,7 +3,7 @@ package sunny;
 /**
  * Inherits from exception, thrown when user input a invalid message or invalid timeline
  */
-public class WrongMessageException extends Exception{
+public class WrongMessageException extends Exception {
     public WrongMessageException() {
         super("Please enter your task in a proper format");
     }
