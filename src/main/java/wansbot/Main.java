c0ecd924e7a9128d47b5a73464b764981e78a63e
bigismols <bigismols239@gmail.com>
2024-09-18 19:30:56 +0800

    feat: add icon and title to window.

--- src/main/java/wansbot/Main.java
+++ src/main/java/wansbot/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,7 +24,10 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            Image icon = new Image(this.getClass().getResourceAsStream("/images/daWansBot.png"));
             stage.setScene(scene);
+            stage.setTitle("WansBot");
+            stage.getIcons().add(icon);
             fxmlLoader.<MainWindow>getController().setWansBot(wansBot);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
