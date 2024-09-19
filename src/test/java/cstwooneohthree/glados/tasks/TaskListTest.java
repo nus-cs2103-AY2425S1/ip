30b86144eacb285f4418b78d76e6def63dd3122b
positivelyjon <positivelyjon@gmail.com>
2024-09-19 13:35:30 +0800

    feat: create stubs for tasklist unit tests

--- src/test/java/cstwooneohthree/glados/tasks/TaskListTest.java
+++ src/test/java/cstwooneohthree/glados/tasks/TaskListTest.java
@@ -4,94 +4,93 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import cstwooneohthree.glados.enums.TaskType;
 import cstwooneohthree.glados.exceptions.GladosException;
+import cstwooneohthree.glados.utils.ParsedInfo;
+import cstwooneohthree.glados.utils.ParserStub;
 
 public class TaskListTest {
 
-    @Test
-    public void testAddTodo() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        assertArrayEquals(new String[]{"[T][ ] test todo", "1"}, taskList.add(TaskType.TODO, "test todo"));
-    }
+    private class TaskListWithStubbedParser extends TaskList {
 
-    @Test
-    public void testAddTodoWithTrailingSpace() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        assertArrayEquals(new String[]{"[T][ ] test todo", "1"}, taskList.add(TaskType.TODO, "   test todo   "));
-    }
+        public TaskListWithStubbedParser(boolean shouldLoadTasks) {
+            super(shouldLoadTasks);
+        }
 
-    @Test
-    public void testAddTodoWithNoDescription() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        try {
-            taskList.add(TaskType.TODO, "");
-            fail();
-        } catch (Exception e) {
-            assertEquals("Description for a task cannot be empty.", e.getMessage());
+        /**
+         * Adds a new task to tasks based on taskType.
+         *
+         * @param taskType Type of task to be added.
+         * @param input Arguments after add comand.
+         * @return Added task description and updated index in String array.
+         * @throws GladosException If there is a parsing or task list error.
+         */
+        @Override
+        public String[] add(TaskType taskType, String input) throws GladosException {
+
+            assert taskType == TaskType.TODO || taskType == TaskType.DEADLINE || taskType == TaskType.EVENT;
+
+            switch (taskType) {
+            case TODO:
+                ParsedInfo parsedTodoInputs = ParserStub.parseTask(taskType, input);
+                tasks.add(new Todo(
+                        parsedTodoInputs.getDescription()));
+                break;
+            case EVENT:
+                ParsedInfo parsedEventInputs = ParserStub.parseTask(taskType, input);
+                LocalDate[] eventDates = parsedEventInputs.getDates();
+                tasks.add(new Event(
+                        parsedEventInputs.getDescription(),
+                        eventDates[0],
+                        eventDates[1]));
+                break;
+            case DEADLINE:
+                ParsedInfo parsedDeadlineInputs = ParserStub.parseTask(taskType, input);
+                tasks.add(new Deadline(
+                        parsedDeadlineInputs.getDescription(),
+                        parsedDeadlineInputs.getDates()[0]));
+                break;
+            default:
+                break;
+            }
+            listIndex++;
+
+            return new String[]{tasks.get(listIndex - 1).toString(), String.valueOf(listIndex)};
         }
     }
 
     @Test
+    public void testAddTodo() throws GladosException {
+        TaskListWithStubbedParser taskList = new TaskListWithStubbedParser(false);
+        assertArrayEquals(new String[]{"[T][ ] test todo", "1"}, taskList.add(TaskType.TODO, "test todo"));
+    }
+
+    @Test
     public void testAddEvent() throws GladosException {
-        TaskList taskList = new TaskList(false);
+        TaskListWithStubbedParser taskList = new TaskListWithStubbedParser(false);
         assertArrayEquals(
                 new String[]{"[E][ ] test event (from: 2025-08-19 to: 2025-08-19)", "1"},
                 taskList.add(TaskType.EVENT, "test event /from 2025-08-19 /to 2025-08-19"));
     }
 
     @Test
-    public void testAddEventWithNoFromDate() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        try {
-            taskList.add(TaskType.EVENT, "test event /to 2025-08-19");
-            fail();
-        } catch (Exception e) {
-            assertEquals("Date range between '/from' and '/to' not specified or invalid", e.getMessage());
-        }
-    }
-
-    @Test
-    public void testAddEventWithNoDateRange() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        try {
-            taskList.add(TaskType.EVENT, "test event");
-            fail();
-        } catch (Exception e) {
-            assertEquals("Date range between '/from' and '/to' not specified or invalid", e.getMessage());
-        }
-    }
-
-    @Test
     public void testAddDeadline() throws GladosException {
-        TaskList taskList = new TaskList(false);
+        TaskListWithStubbedParser taskList = new TaskListWithStubbedParser(false);
         assertArrayEquals(
                 new String[]{"[D][ ] test deadline (by: 2025-08-19)", "1"},
                 taskList.add(TaskType.DEADLINE, "test deadline /by 2025-08-19"));
     }
 
     @Test
-    public void testAddEventWithNoDescription() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        try {
-            taskList.add(TaskType.EVENT, "/from 2025-08-19 /to 2025-08-19");
-            fail();
-        } catch (Exception e) {
-            assertEquals("Description for a task cannot be empty.", e.getMessage());
-        }
-    }
-
-    @Test
-    public void testAddDeadlineWithNoDescription() throws GladosException {
-        TaskList taskList = new TaskList(false);
-        try {
-            taskList.add(TaskType.DEADLINE, "/by 2025-08-19");
-            fail();
-        } catch (Exception e) {
-            assertEquals("Description for a task cannot be empty.", e.getMessage());
-        }
+    public void testAddMultipleTasks() throws GladosException {
+        TaskListWithStubbedParser taskList = new TaskListWithStubbedParser(false);
+        taskList.add(TaskType.TODO, "test todo");
+        taskList.add(TaskType.TODO, "test todo");
+        assertArrayEquals(new String[]{"[T][ ] test todo", "3"}, taskList.add(TaskType.TODO, "test todo"));
     }
 }
 
