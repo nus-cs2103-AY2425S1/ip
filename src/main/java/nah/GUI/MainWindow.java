b4fd5b81dfadee0e9e69ee881205418b9bc6f7b8
blackpanther9229 <hoangnguyen09022004@gmail.com>
2024-09-19 11:28:31 +0800

    update UI.png

--- src/main/java/nah/GUI/MainWindow.java
+++ src/main/java/nah/GUI/MainWindow.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
