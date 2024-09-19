db4819e08d2523e8b44d685c81da21cb6c48d018
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-09-19 21:29:10 +0800

    Add Ui screenshot

--- src/main/java/cloudy/Main.java
+++ src/main/java/cloudy/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setCloudy(cloudy);  // inject Cloudy instance
+            stage.setTitle("Cloudy bot");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
