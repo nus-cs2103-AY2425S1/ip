2c4302e96fb9f779eca72bd592f87ad6c33c32ad
Jaehyeok Choi <77jack0105@gmail.com>
2024-09-19 19:53:26 +0800

    MainWindow.java: add line between import statement and class

--- src/main/java/blacknut/ui/MainWindow.java
+++ src/main/java/blacknut/ui/MainWindow.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
 /**
  * Controller for the main GUI.
  */
