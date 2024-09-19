026f794f4fad05366bda726d47a3e3a77fbf820a
currynia <teewenrui31@gmail.com>
2024-09-19 21:08:49 +0800

    Add title to window

--- src/main/java/tissue/Main.java
+++ src/main/java/tissue/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Tissue bot");
             fxmlLoader.<MainWindow>getController().setTissue(tissue);
             stage.show();
         } catch (IOException e) {
