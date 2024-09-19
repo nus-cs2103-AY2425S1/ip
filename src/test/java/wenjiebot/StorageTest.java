f73f37aa50d20439a0594dea6835720b18714952
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-09-20 06:37:50 +0800

    Remove UiTest and fix stlying

--- src/test/java/wenjiebot/StorageTest.java
+++ src/test/java/wenjiebot/StorageTest.java
@@ -3,7 +3,6 @@ package wenjiebot;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
