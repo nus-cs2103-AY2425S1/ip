5fb0c17862676a163566fe8eac732a4de35884a6
xGladiate <sharonsohxuanhui@hotmail.com>
2024-09-20 00:06:11 +0800

    Modify JUnit to updated output

--- src/test/java/xbot/task/DeadlineTest.java
+++ src/test/java/xbot/task/DeadlineTest.java
@@ -8,38 +8,38 @@ public class DeadlineTest {
     private Deadline deadline1 = new Deadline("test case 1", "24/4/2025");
     private Deadline deadline2 = new Deadline("test case 2", "25/20/2025");
     private Deadline deadline3 = new Deadline("test case 3", "5/1/2025 0925");
-    private Deadline deadline4 = new Deadline("test case 4", "25/20/2025 2525");
-    private Deadline deadline5 = new Deadline("test case 5", "25/20/2025 6pm");
+    private Deadline deadline4 = new Deadline("test case 4", "25/2/2025 2525");
+    private Deadline deadline5 = new Deadline("test case 5", "25/2/2025 6pm");
 
     @Test
-    public void test1() {
-        assertEquals("[D][ ] test case 1 (by: Apr 24 2025)",
+    public void testCorrectDateOutput() {
+        assertEquals("[D][ ] test case 1 (by: 24 Apr 2025)",
                 deadline1.toString());
     }
 
     @Test
-    public void test2() {
+    public void testCorrectDateTimeOutput() {
+        assertEquals("[D][ ] test case 3 (by: 5 Jan 2025, 9:25am)",
+                deadline3.toString());
+
+    }
+
+    @Test
+    public void testIncorrectTimeOutput() {
         assertEquals(
                 "[D][ ] test case 2 (by: TimeDate cannot be converted to another format :'0)",
                 deadline2.toString());
     }
 
     @Test
-    public void test3() {
-        assertEquals("[D][ ] test case 3 (by: Jan 5 2025, 9:25am)",
-                deadline3.toString());
-
-    }
-
-    @Test
-    public void test4() {
+    public void testInvalidTimeOutput() {
         assertEquals("[D][ ] test case 4 (by: TimeDate cannot be converted to another format :'0)",
                 deadline4.toString());
     }
 
 
     @Test
-    public void test5() {
+    public void testInvalidTimeFormatOutput() {
         assertEquals("[D][ ] test case 5 (by: TimeDate cannot be converted to another format :'0)",
                 deadline5.toString());
     }
