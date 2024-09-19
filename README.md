38d0ad6e781dfc440cb7fb131cfe3224e876d13c
naythee169 <jordan.mitchell.chan@gmail.com>
2024-09-19 19:02:47 +0800

    Add README file

--- README.md
+++ README.md
@@ -1,24 +1,106 @@
-# Duke project template
+# Delphi User guide
 
-This is a project template for a greenfield Java project. It's named after the Java mascot _Duke_. Given below are instructions on how to use it.
+The Delphi Chat Bot is a desktop app for **creating and managing a list of tasks and appointments**, optimized for use via a Command Line Interface (CLI) while still having the benefits of a Graphical User Interface (GUI). If you can type fast, Delphi can manage your tasks faster than traditional GUI apps.
+Delphi can remember the tasks you've inputted from the last time you used the app.
+## Quick start
 
-## Setting up in Intellij
+1. Ensure you have Java 17 installed.
+2. Download the latest .jar file from here.
+3. Copy the file to the folder you want to use as the home folder for Delphi.
+4. Open a command terminal, cd into the folder you put the jar file in, and use the java -jar Delphi.jar command to run the application.
+5. Type commands into the command box and press Enter or click the Send button to execute it.
 
-Prerequisites: JDK 17, update Intellij to the most recent version.
+---
 
-1. Open Intellij (if you are not in the welcome screen, click `File` > `Close Project` to close the existing project first)
-1. Open the project into Intellij as follows:
-   1. Click `Open`.
-   1. Select the project directory, and click `OK`.
-   1. If there are any further prompts, accept the defaults.
-1. Configure the project to use **JDK 17** (not other versions) as explained in [here](https://www.jetbrains.com/help/idea/sdk.html#set-up-jdk).<br>
-   In the same dialog, set the **Project language level** field to the `SDK default` option.
-3. After that, locate the `src/main/java/Duke.java` file, right-click it, and choose `Run Duke.main()` (if the code editor is showing compile errors, try restarting the IDE). If the setup is correct, you should see something like the below as the output:
-   ```
-   Hello from
-    ____        _        
-   |  _ \ _   _| | _____ 
-   | | | | | | | |/ / _ \
-   | |_| | |_| |   <  __/
-   |____/ \__,_|_|\_\___|
-   ```
+## Features:
+
+
+### List: `list`
+
+Lists all the tasks that are currently in the task list.
+
+Format: `list`
+
+### Marking: `mark, unmark`
+
+Marks tasks as complete and marks them as incomplete again respectively. Tasks are searched for
+by index(task list is 1-indexed, index should be positive integer). Will throw an error if the index is out of range.
+
+Format: `mark INDEX, unmark INDEX`
+
+### Delete: `delete`
+
+Removes tasks from the task list entirely. Tasks are searched for by index(task list is 1-indexed, index should be positive integer). 
+Will throw an error if the index is out of range.
+
+Format: `delete INDEX`
+
+### To-do: `todo`
+
+adds a todo task to the task list. No date/time associated with this task type.
+
+
+![screenshot of todo task creation](src/main/resources/images/todo.png)
+
+Format: `todo TASK_DESCRIPTION`
+
+Example: `todo buy dinner`
+
+### Deadline: `deadline`
+
+adds a deadline task to the task list. Task has a single deadline associated with it. 
+If a date and time of the correct format is provided, it will be parsed appropriately
+otherwise whatever string is provided will be used.
+
+![screenshot of deadline creation](src/main/resources/images/deadline.png)
+
+Format: `deadline TASK_DESCRIPTION /by DEADLINE`
+
+Examples: 
+* `deadline finish cs2103T ip /by 20/09/2024 2359`
+
+* `deadline finish essay /by tomorrow`
+
+### Event: `event`
+
+adds an event task to the task list. Task has a time frame(window) associated with it,
+in other words there is a **from** date and **to** date. If a date and time of the 
+correct format is provided, it will be parsed appropriately otherwise whatever string is provided will be used
+
+
+![screenshot of event creation](src/main/resources/images/event.png)
+
+
+Format: `event TASK_DESCRIPTION /from START_TIME /to END_TIME`
+
+Examples:
+* `event basketball CCA /from 16/09/2024 2000 /to 16/09/2024 2200`
+
+* `deadline finish essay /by tomorrow`
+
+### Find: `find`
+
+Search for tasks that contain a keyword and returns them as a list
+
+![screenshot of find command](src/main/resources/images/find.png)
+
+
+Format: `find KEYWORD`
+
+### Update: `update`
+
+Update the deadlines/ time frames of deadlines/events respectively.
+
+Format: 
+* `update KEYWORD /by NEW_DEADLINE`
+*  `update KEYWORD /from NEW_START /to NEW_END`
+
+---
+
+## Things to note
+
+1. The `find` and `update` commands search for the keyword as a substring, so it is important
+to be precise and exact. For example, if `basketball games` and `buy noodles again` are task
+descriptions, then the search keyword `"ga"` will return both of them.
+2. The date formats that can be parsed are also limited. In general, the format should be
+"dd/mm/yyyy xxxx" where "xxxx" is the time in 24 hour format.
--- src/main/java/parser/DateParser.java
+++ src/main/java/parser/DateParser.java
@@ -1,17 +1,19 @@
 package parser;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
+
 /**
  * Provides the Date Parsing functionality that allows ui.Delphi to process dates and times in different formats.
- *
+ * Written with the help of chatgpt
  * @author Jordan Chan
  */
 public class DateParser {
     /**
-     * list of possible date-time formats
+     * List of possible date-time formats.
      */
     private static final DateTimeFormatter[] INPUT_FORMATTERS = {
             DateTimeFormatter.ofPattern("d/M/yyyy HHmm", Locale.ENGLISH),
@@ -26,42 +28,73 @@ public class DateParser {
     };
 
     /**
-     * Parses a date and time string and converts it to a formatted string using default output format.
+     * Parses a date and time string or a date-only string and converts it to a formatted string using default output format.
      *
-     * @param dateTimeString The date and time string to parse.
+     * @param dateTimeString The date and time or date-only string to parse.
      * @return A formatted date and time string, or null if parsing fails.
      */
     public String parseAndFormatDateTime(String dateTimeString) {
-        return parseAndFormatDateTime(dateTimeString, null); // Calls the overloaded method with default format
+        return parseAndFormatDateTime(dateTimeString, null);
     }
 
     /**
-     * Parses a date and time string and converts it to a formatted string using the specified output format.
+     * Parses a date and time string or a date-only string and converts it to a formatted string using the specified output format.
      *
-     * @param dateTimeString The date and time string to parse.
+     * @param dateTimeString The date and time or date-only string to parse.
      * @param outputPattern  The output format pattern.
      * @return A formatted date and time string, or null if parsing fails.
      */
     public String parseAndFormatDateTime(String dateTimeString, String outputPattern) {
-        DateTimeFormatter defaultOutputFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy, h:mma", Locale.ENGLISH);
+        DateTimeFormatter defaultDateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy, h:mma", Locale.ENGLISH);
         DateTimeFormatter defaultDateFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
 
         for (DateTimeFormatter formatter : INPUT_FORMATTERS) {
             try {
                 LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
-                DateTimeFormatter outputFormatter = (outputPattern == null || outputPattern.isEmpty())
-                        ? defaultOutputFormatter
-                        : DateTimeFormatter.ofPattern(outputPattern, Locale.ENGLISH);
-                String formattedDateTime = dateTime.format(outputFormatter);
-                int day = dateTime.getDayOfMonth();
-                String ordinalDay = day + OrdinalSuffix.getSuffix(day);
-                formattedDateTime = formattedDateTime.replaceFirst("\\d+", ordinalDay);
-                return formattedDateTime.toLowerCase(); // Convert "AM/PM" to "am/pm"
+                return formatDateTime(dateTime, outputPattern);
             } catch (DateTimeParseException e) {
-                // empty
+                // Continue to date-only parsing
             }
         }
+
+        try {
+            LocalDate dateOnly = LocalDate.parse(dateTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH));
+            return formatDateOnly(dateOnly, outputPattern);
+        } catch (DateTimeParseException e) {
+            try {
+                LocalDate dateOnly = LocalDate.parse(dateTimeString, DateTimeFormatter.ofPattern("d/M/yyyy", Locale.ENGLISH));
+                return formatDateOnly(dateOnly, outputPattern);
+            } catch (DateTimeParseException ex) {
+                // Return original string if parsing fails
+            }
+        }
+
         return dateTimeString;
+    }
+
+    private String formatDateTime(LocalDateTime dateTime, String outputPattern) {
+        DateTimeFormatter outputFormatter = getOutputFormatter(outputPattern, true);
+        String formattedDateTime = dateTime.format(outputFormatter);
+        int day = dateTime.getDayOfMonth();
+        String ordinalDay = day + OrdinalSuffix.getSuffix(day);
+        return formattedDateTime.replaceFirst("\\d+", ordinalDay).toLowerCase();
+    }
+
+    private String formatDateOnly(LocalDate date, String outputPattern) {
+        DateTimeFormatter outputFormatter = getOutputFormatter(outputPattern, false);
+        String formattedDateOnly = date.format(outputFormatter);
+        int day = date.getDayOfMonth();
+        String ordinalDay = day + OrdinalSuffix.getSuffix(day);
+        return formattedDateOnly.replaceFirst("\\d+", ordinalDay).toLowerCase();
+    }
+
+    private DateTimeFormatter getOutputFormatter(String outputPattern, boolean hasTime) {
+        if (outputPattern == null || outputPattern.isEmpty()) {
+            return hasTime
+                    ? DateTimeFormatter.ofPattern("d MMMM yyyy, h:mma", Locale.ENGLISH)
+                    : DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
+        }
+        return DateTimeFormatter.ofPattern(outputPattern, Locale.ENGLISH);
     }
 
     /**
@@ -91,16 +124,11 @@ public class DateParser {
                 return TH.getSuffix();
             }
             switch (day % 10) {
-            case 1:
-                return ST.getSuffix();
-            case 2:
-                return ND.getSuffix();
-            case 3:
-                return RD.getSuffix();
-            default:
-                return TH.getSuffix();
+            case 1: return ST.getSuffix();
+            case 2: return ND.getSuffix();
+            case 3: return RD.getSuffix();
+            default: return TH.getSuffix();
             }
         }
     }
 }
-
