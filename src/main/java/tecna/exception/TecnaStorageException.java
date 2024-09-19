0114f3bd138af7c9a912682788c949c8c6798ed0
DennieDan <e1129903@u.nus.edu>
2024-09-19 23:13:26 +0800

    Add JavaDoc for "exception" package

--- src/main/java/tecna/exception/TecnaStorageException.java
+++ src/main/java/tecna/exception/TecnaStorageException.java
@@ -1,6 +1,15 @@
 package tecna.exception;
 
+/**
+ * Indicates errors when working with files.
+ *
+ * @author DennieDan.
+ */
 public class TecnaStorageException extends Exception {
+    /**
+     * Constructs a TecnaStorageException instance.
+     * @param message Details of the error.
+     */
     public TecnaStorageException(String message) {
         super(message);
     }
