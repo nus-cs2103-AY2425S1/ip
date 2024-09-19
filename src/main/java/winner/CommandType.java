f655374a1c363bc67baea97d035b6aea002c6661
chua yuan cheng <>
2024-09-20 01:06:25 +0800

    Add CommandType Enum class

---
+++ src/main/java/winner/CommandType.java
@@ -0,0 +1,16 @@
+package winner;
+
+public enum CommandType {
+    HI_AGAIN,
+    HELP,
+    TODO,
+    DEADLINE,
+    EVENT,
+    LIST,
+    MARK,
+    UNMARK,
+    DELETE,
+    FIND,
+    BYE,
+    UNKNOWN
+}
