dc94b2326c28326678f88e27a8e1f60a796b7e98
hpcman <happypacman36@gmail.com>
2024-09-19 12:40:15 +0800

    Pacman.java: Rename class PacMan to class Pacman

--- src/main/java/pacman/Pacman.java
+++ src/main/java/pacman/Pacman.java
@@ -1,6 +1,6 @@
 package pacman;
 
-public class PacMan {
+public class Pacman {
     private static final Storage storage = new Storage("data/PacMan.txt");
     private static final TaskList list = new TaskList(storage.load());
     private static final Ui ui = new Ui();
