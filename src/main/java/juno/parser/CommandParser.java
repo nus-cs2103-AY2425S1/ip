b20403b02e3a51f3b7ca8c766269fc17f1498623
Zhang Jinhan <zhangjinhan338@gmail.com>
2024-09-20 02:03:06 +0800

    CommandParser class: Shorten method length for parse() method

--- src/main/java/juno/parser/CommandParser.java
+++ src/main/java/juno/parser/CommandParser.java
@@ -21,6 +21,16 @@ import juno.ui.JunoUi;
  */
 public class CommandParser {
 
+    private static final String MARK_TASK_STRING = "mark";
+    private static final String UNMARK_TASK_STRING = "unmark";
+    private static final String DELETE_TASK_STRING = "delete";
+    private static final String ADD_TODO_TASK_STRING = "add todo";
+    private static final String ADD_DEADLINE_TASK_STRING = "add deadline";
+    private static final String ADD_EVENT_TASK_STRING = "add event";
+    private static final String LIST_TASK_STRING = "list";
+    private static final String EXIT_STRING = "bye";
+    private static final String FIND_TASK_STRING = "find";
+    private static final String REMIND_STRING = "remind";
     /**
      * Parses the user's input and returns the command to be executed.
      * Needs to pass in these parameters for use in creating the command.
@@ -32,45 +42,31 @@ public class CommandParser {
      * @return The Command object corresponding to the user's input.
      * @throws TaskManagerException If the input is invalid or an error occurs while parsing.
      */
-    public Command parse(
-            String userInput,
-            JunoUi junoUi,
-            FileManager fileManager,
-            TaskManager taskManager) throws TaskManagerException {
+    public Command parse(String userInput, JunoUi junoUi, FileManager fileManager, TaskManager taskManager)
+            throws TaskManagerException {
 
-        String markTaskString = "mark";
-        String unmarkTaskString = "unmark";
-        String deleteTaskString = "delete";
-        String addTodoTaskString = "add todo";
-        String addDeadlineTaskString = "add deadline";
-        String addEventTaskString = "add event";
-        String listTaskString = "list";
-        String exitString = "bye";
-        String findString = "find";
-        String remindString = "remind";
-
-        if (userInput.equalsIgnoreCase(exitString)) {
+        if (userInput.equalsIgnoreCase(EXIT_STRING)) {
             return new ExitCommand(junoUi);
-        } else if (userInput.equalsIgnoreCase(listTaskString)) {
+        } else if (userInput.equalsIgnoreCase(LIST_TASK_STRING)) {
             return new ListCommand(taskManager);
         } else if (userInput.isEmpty()) {
             throw new TaskManagerException(junoUi.invalidUserInput(),
                     TaskManagerException.ErrorType.EMPTY_INPUT);
-        } else if (userInput.startsWith(markTaskString)) {
+        } else if (userInput.startsWith(MARK_TASK_STRING)) {
             return new MarkCommand(userInput, taskManager, fileManager, true);
-        } else if (userInput.startsWith(unmarkTaskString)) {
+        } else if (userInput.startsWith(UNMARK_TASK_STRING)) {
             return new MarkCommand(userInput, taskManager, fileManager, false);
-        } else if (userInput.startsWith(deleteTaskString)) {
+        } else if (userInput.startsWith(DELETE_TASK_STRING)) {
             return new DeleteCommand(userInput, taskManager, fileManager);
-        } else if (userInput.startsWith(addTodoTaskString)) {
+        } else if (userInput.startsWith(ADD_TODO_TASK_STRING)) {
             return new AddTodoCommand(userInput, taskManager, fileManager);
-        } else if (userInput.startsWith(addDeadlineTaskString)) {
+        } else if (userInput.startsWith(ADD_DEADLINE_TASK_STRING)) {
             return new AddDeadlineCommand(userInput, taskManager, fileManager);
-        } else if (userInput.startsWith(addEventTaskString)) {
+        } else if (userInput.startsWith(ADD_EVENT_TASK_STRING)) {
             return new AddEventCommand(userInput, taskManager, fileManager);
-        } else if (userInput.startsWith(findString)) {
+        } else if (userInput.startsWith(FIND_TASK_STRING)) {
             return new FindCommand(userInput, taskManager);
-        } else if (userInput.equalsIgnoreCase(remindString)) {
+        } else if (userInput.equalsIgnoreCase(REMIND_STRING)) {
             return new RemindCommand(taskManager);
         } else {
             throw new TaskManagerException(junoUi.invalidFunctionInput(),
