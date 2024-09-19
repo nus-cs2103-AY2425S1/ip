84721bc64683d7875aca50cac921791049ed881b
chuajunyu <chuajunyu1@gmail.com>
2024-09-19 20:15:29 +0800

    Add header comments for DialogBox methods

--- src/main/java/garfield/gui/DialogBox.java
+++ src/main/java/garfield/gui/DialogBox.java
@@ -49,10 +49,24 @@ public class DialogBox extends HBox {
         dialog.getStyleClass().add("reply-label");
     }
 
+    /**
+     * Creates and returns a DialogBox for the user.
+     *
+     * @param text The text to be displayed in the user's dialog box.
+     * @param img The image to be displayed in the user's dialog box.
+     * @return A DialogBox object representing the user's dialog.
+     */
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
 
+    /**
+     * Creates and returns a DialogBox for Duke, with the dialog box flipped.
+     *
+     * @param text The text to be displayed in Duke's dialog box.
+     * @param img The image to be displayed in Duke's dialog box.
+     * @return A DialogBox object representing Duke's flipped dialog.
+     */
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
