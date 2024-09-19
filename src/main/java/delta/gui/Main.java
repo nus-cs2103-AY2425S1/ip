a0cf8814ad04c6c0b1ce573695358224ffc091de
gohqingkhang <gohqingkhang@gmail.com>
2024-09-19 13:49:55 +0800

    Add title for app window

--- src/main/java/delta/gui/Main.java
+++ src/main/java/delta/gui/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Delta ChatBot");
             stage.setMinHeight(643);
             stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setDelta(delta); // inject the Delta instance
