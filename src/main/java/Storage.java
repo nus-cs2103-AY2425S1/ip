fa67e34f55755d8cfa2a08fab2ec52302f39592c
darkmoongreatsword <chin02@comp.nus.edu.sg>
2024-09-19 21:36:26 +0800

    Add Storage.java

---
+++ src/main/java/Storage.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.nio.file.Files;
+/**
+ * This class implements a storage, which saves and loads the tasklist from
+ * and to file. This class stores data as a comma-separated-variable file.
+ * It supports the following methods:
+ * (i)
+ */
+public class Storage {
+
+    private Storage() {
+    }
+
+    /** The singular instance of storage.*/
+    private final static Storage STORAGE = new Storage();
+    /** The default file path for storing and writing data */
+    private static final Path FILE_PATH = Path.of("./data/tasks.csv");
+    /** The default directory path for storing and writing data */
+    private static final Path DIRECTORY_PATH = Path.of("./data/");
+
+    /**
+     * Default factory method of Storage. Returns the singular instance
+     * STORAGE, with the default file and directory paths
+     * @return The singular instance of storage
+     */
+    public static Storage of() {
+        return STORAGE;
+    }
+
+    /**
+     * Saves the current TaskList to file. The tasks are converted into csv
+     * representations as per their .toCsv() methods, and written to a file
+     * indicated by FILE_PATH.
+     */
+    static void save() {
+        //Checks if the directory already exists. If not, creates the directory.
+        if (Files.notExists(DIRECTORY_PATH)) {
+            try {
+                Files.createDirectories(DIRECTORY_PATH);
+            } catch (IOException e) {
+                System.out.println("Oh no! I can't create the file directory.\n" +
+                        "Check out the error message to see what went wrong\n");
+                e.printStackTrace();
+            }
+        }
+        //Checks if the save file already exists. If not, creates the file.
+        if (Files.notExists(FILE_PATH)) {
+            try {
+                Files.createFile(FILE_PATH);
+            } catch (IOException e) {
+                System.out.println("Oh no! I can't create your save file.\n" +
+                        "Check out the error message to see what went wrong\n");
+                e.printStackTrace();
+            }
+        }
+
+        //Writes all tasks to file, overwriting the old file
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(FILE_PATH)) {
+            ArrayList<String> saveFile = TaskList.toSaveFile();
+            for (String lineToWrite: saveFile) {
+                bufferedWriter.write(lineToWrite);
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Oh no! I can't save your file. \n " +
+                    "Check out the error message to see what went wrong");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Locates the save file, and then prepares the contents for loading
+     * @return An array of strings representing tasks, in csv format.
+     */
+    static ArrayList<String> load() {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(FILE_PATH)) {
+            ArrayList<String> tasksToRead = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                tasksToRead.add(line);
+            }
+            //It is safe to cast to String[] since only strings are added to tasksToRead
+            return tasksToRead;
+        } catch (NoSuchFileException e) {
+
+            //Checks if the directory exists. If not, creates the directory.
+            if (Files.notExists(DIRECTORY_PATH)) {
+                try {
+                    Files.createDirectories(DIRECTORY_PATH);
+                } catch (IOException e2) {
+                    System.out.println("Oh no! I can't save your file.\n" +
+                            "Check out the error message to see what went wrong\n");
+                    e2.printStackTrace();
+                }
+            }
+
+            //Checks if the save file already exists. If not, creates the file.
+            if (Files.notExists(FILE_PATH)) {
+                try {
+                    Files.createFile(FILE_PATH);
+                } catch (IOException e2) {
+                    System.out.println("Oh no! I can't save your file.\n" +
+                            "Check out the error message to see what went wrong\n");
+                    e2.printStackTrace();
+                }
+            }
+        } catch (IndexOutOfBoundsException e) { //File is likely corrupted.
+            try {
+                Files.delete(FILE_PATH);
+                Files.createFile(FILE_PATH);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.out.println("Oh no, I can't read the saved file. \n" +
+                    "Check the error message to see why");
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+}
