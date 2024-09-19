abc1d23ed83257bb4fa54518dd897c10807b36dc
darkmoongreatsword <chin02@comp.nus.edu.sg>
2024-09-19 21:36:03 +0800

    Add TodoCommand.java

---
+++ src/main/java/TodoCommand.java
@@ -0,0 +1,23 @@
+public class TodoCommand extends Command {
+
+    private final Todo newTodo;
+    TodoCommand(String input) {
+        String[] arguments = input.split("todo");
+        String todoDescription = arguments[1].trim();
+        this.newTodo = new Todo(todoDescription);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static String getHelpDescription() {
+        return "Creates a todo and enters it into the list.\nUsage: todo <description>";
+    }
+
+    public void execute() {
+        TaskList.add(newTodo);
+        Storage.save();
+        Ui.printTask(newTodo);
+    }
+}
