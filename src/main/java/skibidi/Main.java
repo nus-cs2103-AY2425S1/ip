7c24ba7671235d67d998a2e3570328f31839767b
jeanyang04 <jeanyang.chen@gmail.com>
2024-09-19 22:13:54 +0800

    Update user guide

--- src/main/java/skibidi/Main.java
+++ src/main/java/skibidi/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setSkibidi(skibidi);
+            stage.setTitle("Skibidi");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
