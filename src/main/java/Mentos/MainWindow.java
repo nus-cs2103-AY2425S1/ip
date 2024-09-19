5992cda890eb69cc22d1dce11b2bf3bd424ce348
Denon Chong <e0958801@u.nus.edu>
2024-09-19 22:45:22 +0800

    Handle empty input

--- src/main/java/Mentos/MainWindow.java
+++ src/main/java/Mentos/MainWindow.java
@@ -50,6 +50,9 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
+        if (input.equals("")) {
+            return;
+        }
         String response = duke.getResponse(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
