3236231d25b82a8e7acafff55a01635f0cd49dfe
t-leongchuan <t.leongchuan@gmail.com>
2024-09-19 23:04:29 +0800

    Generate more test with use of AI

--- src/test/java/Denim/ParserTest.java
+++ src/test/java/Denim/ParserTest.java
@@ -176,4 +176,28 @@ public class ParserTest {
         Command command = parser.parseCommand(simulatedInput);
         assertTrue(command instanceof FindCommand);
     }
+
+    @Test
+    public void prepareInvalidCommand_whitespaceOnly_returnsInvalidCommand() {
+        String simulatedInput = "   ";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
+
+    @Test
+    public void prepareFind_specialCharacters_returnsFindCommand() {
+        String simulatedInput = "find project@2023";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof FindCommand);
+    }
+
+    @Test
+    public void prepareHelp_incorrectUsage_returnsInvalidCommand() {
+        String simulatedInput = "help me please";
+
+        Command command = parser.parseCommand(simulatedInput);
+        assertTrue(command instanceof InvalidCommand);
+    }
 }
