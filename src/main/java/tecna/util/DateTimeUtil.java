641771ac52d7a75ba4ee627ea2c84df1580cc45e
DennieDan <e1129903@u.nus.edu>
2024-09-19 23:43:51 +0800

    Add JavaDoc for util package

--- src/main/java/tecna/util/DateTimeUtil.java
+++ src/main/java/tecna/util/DateTimeUtil.java
@@ -3,6 +3,11 @@ package tecna.util;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
+/**
+ * Handles the DateTime format procedures used in the app.
+ *
+ * @author DennieDan.
+ */
 public class DateTimeUtil {
     public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HHmm";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
@@ -12,6 +17,12 @@ public class DateTimeUtil {
 
     }
 
+    /**
+     * Parses the string of datetime to a LocalDateTime object.
+     *
+     * @param dateTime String of date time.
+     * @return LocalDateTime object.
+     */
     public static LocalDateTime parseDateTime(String dateTime) {
         return LocalDateTime.parse(dateTime, DATE_TIME_FORMATTER);
     }
