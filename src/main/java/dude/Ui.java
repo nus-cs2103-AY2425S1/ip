b614135d635af80ac5cde75f824988f3244cc6e8
MingYiAw <e1129986@u.nus.edu>
2024-09-20 01:25:36 +0800

    feat: Rewrite few reply of Dude
    Dude's messages are bland and boring.
    
    Changing Dude's reply into something more interesting can
    enchance user experience.
    
    Let's modify Dude's phrases according to a personality.
    
    Adding personality to Dude improve the quality of the application
    and enchance user experience.

--- src/main/java/dude/Ui.java
+++ src/main/java/dude/Ui.java
@@ -29,7 +29,9 @@ public class Ui {
      * @return The String message for greeting.
      */
     public String showGreet() {
-        return "Hello! I'm " + BOT_NAME + "\nWhat can I do for you?";
+        return "Hey! I'm " + BOT_NAME
+                + "\nWhat do you want?"
+                + "\nCATCHPHRASE!!!";
     }
 
     /**
@@ -48,9 +50,14 @@ public class Ui {
      * @return A String representation of the list of tasks.
      */
     public String showList(TaskList taskList) {
-        StringBuilder s = new StringBuilder();
         int size = taskList.getLength();
-        s.append("Here are the tasks in your list:\n");
+
+        if (size == 0) {
+            return "Don't you have anything to do? Time to add some tasks!";
+        }
+
+        StringBuilder s = new StringBuilder();
+        s.append("Here's your task list! Get going and tackle those tasks right away:\n");
 
         ArrayList<Task> tasks = taskList.getTasks();
         for (int i = 1; i <= size; i++) {
@@ -71,7 +78,7 @@ public class Ui {
      * @return A String message indicating that the task has been marked as done.
      */
     public String showMark(Task task) {
-        return "Nice! I've marked this task as done:\n" + task;
+        return "Finally you have done something! I've marked this as done:\n" + task;
     }
 
     /**
@@ -81,7 +88,7 @@ public class Ui {
      * @return A String message indicating that the task has been marked as not done.
      */
     public String showUnmark(Task task) {
-        return "OK, I've marked this task as not done yet:\n" + task;
+        return "OK, Looks like you have more works to do. I've marked this as not done yet:\n" + task;
     }
 
     /**
@@ -92,7 +99,7 @@ public class Ui {
      * @return A String message confirming the task adding and showing the updated task count.
      */
     public String showAdd(Task task, TaskList taskList) {
-        return "Got it. I've added this task:\n" + task
+        return "Got it. Task added:\n" + task
                 + "\nNow you have " + taskList.getLength() + " tasks in the list.";
     }
 
@@ -104,7 +111,7 @@ public class Ui {
      * @return A String message confirming the task removal and showing the updated task count.
      */
     public String showDelete(Task task, TaskList taskList) {
-        return "Noted. I've removed this task:\n" + task
+        return "All right! Task eliminated:\n" + task
                 + "\nNow you have " + taskList.getLength() + " tasks in the list.";
     }
 
@@ -115,9 +122,9 @@ public class Ui {
      * @return A String representation of the filtered list of tasks.
      */
     public String showFind(ArrayList<Task> filteredList) {
-        StringBuilder s = new StringBuilder();
         int size = filteredList.size();
-        s.append("Here are the matching tasks in your list:\n");
+        StringBuilder s = new StringBuilder();
+        s.append("Here's what you looking for:\n");
 
         for (int i = 1; i <= size; i++) {
             s.append(i + "." + filteredList.get(i - 1));
@@ -138,7 +145,7 @@ public class Ui {
      * @return A String message indicating that a new shortcut has been defined.
      */
     public String showDefine(String shortcut, CommandType command) {
-        return "Noted. Now I know that \"" + shortcut + "\" represents \"" + command.name() + "\".";
+        return "Did you just said that \"" + shortcut + "\" means \"" + command.name() + "\"?";
     }
 
     /**
@@ -148,7 +155,7 @@ public class Ui {
      * @return A String message indicating that a shortcut has been deleted.
      */
     public String showUndefine(String shortcut) {
-        return "I got you. What does \"" + shortcut + "\" means?";
+        return "I got you. What does \"" + shortcut + "\" even means?";
     }
 
     /**
@@ -167,7 +174,8 @@ public class Ui {
      * @return A String message for farewell.
      */
     public String showBye() {
-        return "Bye. Hope to see you again soon!";
+        return "Playtime's over!!\n"
+                + "Better not bother me next time!";
     }
 
     /**
