c65699a4faa69b800bcca93eba3deb1f17b9143d
Wnayar <wdnconsultancy@yahoo.com>
2024-09-20 07:26:10 +0800

    Add more Junit tests

--- src/test/java/bill/DeadlineTest.java
+++ src/test/java/bill/DeadlineTest.java
@@ -1,19 +1,49 @@
 package bill;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
     @Test
-    public void dateFormatting_yyyyMMdd_success() {
-        // Deadline climb 2024-12-10 is [D][ ] climb (by: Dec 10 2024) task
-        assertEquals("[D][ ] climb (by: Dec 10 2024)", new Deadline("climb", "2024-12-10").toString());
+    public void dateFormattingTest_yyyyMMdd_success() {
+        // Deadline climb 2024-12-10 is [D][ ] climb (by: Dec 10 2024)
+        assertEquals("[D][ ] climb (by: Dec 10 2024)",
+                    new Deadline("climb", "2024-12-10").toString());
     }
 
     @Test
-    public void dateFormatting_monthDayYear_success() {
-        // Deadline climb Dec 10 2024 is [D][ ] climb (by: Dec 10 2024) task
-        assertEquals("[D][ ] climb (by: Dec 10 2024)", new Deadline("climb", "Dec 10 2024").toString());
+    public void dateFormattingTest_monthDayYear_success() {
+        // Deadline climb Dec 10 2024 is [D][ ] climb (by: Dec 10 2024)
+        assertEquals("[D][ ] climb (by: Dec 10 2024)",
+                    new Deadline("climb", "Dec 10 2024").toString());
+    }
+
+    @Test
+    public void descriptionTest_noDescription_throwsError() {
+        // No description of deadline throws an assertion error
+        assertThrows(AssertionError.class, () -> new Deadline("", "2024-12-10"));
+    }
+
+    @Test
+    public void dateTest_noDate_throwsError() {
+        // No date of deadline throws an assertion error
+        assertThrows(AssertionError.class, () -> new Deadline("climb", ""));
+    }
+
+
+    @Test
+    public void maxDateTest_largestDate_success() {
+        // Deadline climb 9999-12-31 is [D][ ] climb (by: Dec 31 9999)
+        assertEquals("[D][ ] climb (by: Dec 31 9999)",
+                    new Deadline("climb", "9999-12-31").toString());
+    }
+
+    @Test
+    public void leapYearTest_leapYearDate_success() {
+        // Deadline climb 2028-02-29 is [D][ ] climb (by: Feb 29 2028)
+        assertEquals("[D][ ] climb (by: Feb 29 2028)",
+                    new Deadline("climb", "2028-02-29").toString());
     }
 }
--- src/test/java/bill/TaskTest.java
+++ src/test/java/bill/TaskTest.java
@@ -20,4 +20,51 @@ public class TaskTest {
         // Juggle task unmark toString() is [ ] juggle
         assertEquals("[ ] juggle", juggle.toString());
     }
+
+    @Test
+    public void markUnmarkTest_markUnmark_noX() {
+        Task juggle = new Task("juggle");
+        juggle.mark();
+        juggle.unmark();
+        // Juggle task unmark toString() is [ ] juggle
+        assertEquals("[ ] juggle", juggle.toString());
+    }
+
+    @Test
+    public void statusIconMarkTest_markValue_withX() {
+        Task juggle = new Task("juggle");
+        juggle.mark();
+        // Juggle task getStatusIcon is "X" when marked
+        assertEquals("X", juggle.getStatusIcon());
+    }
+
+    @Test
+    public void statusIconNoMarkTest_noMarkValue_blank() {
+        Task juggle = new Task("juggle");
+        juggle.unmark();
+        // Juggle task getStatusIcon is " " when unmarked
+        assertEquals(" ", juggle.getStatusIcon());
+    }
+
+    @Test
+    public void descriptionTest_genericDescriptor_matching() {
+        Task juggle = new Task("juggle");
+        // Juggle task getDescription is "juggle"
+        assertEquals("juggle", juggle.getDescription());
+    }
+
+    @Test
+    public void defaultConstructorTest_genericConstructor_success() {
+        Task juggle = new Task("juggle");
+        // Juggle task when constructed, getStatusIcon is " " and getDescription is "juggle"
+        assertEquals(" ", juggle.getStatusIcon());
+        assertEquals("juggle", juggle.getDescription());
+    }
+
+    @Test
+    public void toStringTest_descriptor_matching() {
+        Task juggle = new Task("juggle");
+        // Juggle task toString is "[ ] juggle" as default is unmarked
+        assertEquals("[ ] juggle", juggle.toString());
+    }
 }
