8cf63e904cc924c76b7567aae4a0a99e5a0ea39a
Chan Guan Yu <changuanyu2904@gmail.com>
2024-09-20 16:33:18 +0800

    Modify error message shown

--- src/main/java/secondmind/InvalidCommandFormat.java
+++ src/main/java/secondmind/InvalidCommandFormat.java
@@ -3,7 +3,6 @@ package secondmind;
 public class InvalidCommandFormat extends Exception {
     @Override
     public String toString() {
-        return "Invalid command format detected.\n"
-                + "Type \\help for a list of available commands.";
+        return "Invalid command format detected.";
     }
 }
