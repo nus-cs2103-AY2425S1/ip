acd43b860b3800fe1fb2d8df9cc80060b23bcfbd
CreditableX <creditablecreates@gmail.com>
2024-09-20 17:13:41 +0800

    add small updates

--- src/main/java/mainstuff/Main.java
+++ src/main/java/mainstuff/Main.java
@@ -1,12 +1,12 @@
 package mainstuff;
 import java.io.IOException;
 
+import bob.Bob;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import bob.Bob;
 
 /**
  * A GUI for Duke using FXML.
