6b4921880da27759f737347364b246766d41d957
ITZXITZX <isaacteozixian@hotmail.com>
2024-09-21 00:45:03 +0800

    Set title for gui stage

--- src/main/java/bob/gui/Main.java
+++ src/main/java/bob/gui/Main.java
@@ -31,6 +31,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setTitle("Chat with Bob");
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
