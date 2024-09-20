6167ac92a9908df6bc471abe27f50f6fb9fc8c5d
Curtis <curtischang2510@gmail.com>
2024-09-20 18:21:53 +0800

    Refactor: Updated test structure to adhere to course coding style
    - Reorganised import statements to match coding style guidelines
    - Improved readability and consistency across test methods

--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -1,18 +1,18 @@
-import commands.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import commands.Command;
+import exceptions.InvalidDateException;
 import exceptions.InvalidTaskException;
 import io.Parser;
 import storage.Storage;
 import task.TaskList;
 
-import exceptions.InvalidDateException;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class CommandTest {
 
-    private final String STORAGE_PATH  = "src/main/data/blitz.txt";
+    private static final String STORAGE_PATH = "src/main/data/blitz.txt";
 
     @Test
     public void executeTodoWithoutDescription() throws InvalidTaskException {
@@ -32,7 +32,7 @@ public class CommandTest {
             Command addTaskCommand = Parser.inputToCommand("event /from 10/10/2019 /to 11/10/2019");
             assertEquals("Wah, no description then I record what?", addTaskCommand.execute(taskList));
         } catch (InvalidDateException e) {
-
+            // This block will never run, date from storage always has the right format
         }
     }
 
@@ -43,7 +43,7 @@ public class CommandTest {
             Command deleteCommand = Parser.inputToCommand("delete 10000");
             assertEquals("No valid index was given!!", deleteCommand.execute(taskList));
         } catch (InvalidDateException e) {
-
+            // This block will never run, date from storage always has the right format
         }
     }
 
@@ -54,7 +54,7 @@ public class CommandTest {
             Command fitlerByTagsCommand = Parser.inputToCommand("find -t urgent");
             assertEquals("You are finding a tag that doesn't exist!!", fitlerByTagsCommand.execute(taskList));
         } catch (InvalidDateException e) {
-
+            // This block will never run, date from storage always has the right format
         }
     }
 
@@ -65,7 +65,7 @@ public class CommandTest {
             Command exitCommand = Parser.inputToCommand("bye");
             assertEquals("Till we meet again, GOODBYE", exitCommand.execute(taskList));
         } catch (InvalidDateException e) {
-
+            // This block will never run, date from storage always has the right format
         }
     }
 
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,11 +1,12 @@
-import exceptions.InvalidDateException;
-import task.Task;
-import exceptions.InvalidTaskException;
-import exceptions.NoTaskDescriptionException;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.InvalidDateException;
+import exceptions.InvalidTaskException;
+import exceptions.NoTaskDescriptionException;
+import task.Task;
 
 public class TaskTest {
 
@@ -22,7 +23,8 @@ public class TaskTest {
      */
     @Test
     public void createEventWithoutDescription() {
-        assertThrows(NoTaskDescriptionException.class, () -> Task.createTask("event /from 10-10-2019 /to 11-10-2019"));
+        assertThrows(NoTaskDescriptionException.class, () -> Task.createTask("event /from 10-10-2019 "
+                + "/to 11-10-2019"));
     }
 
     /**
@@ -30,7 +32,8 @@ public class TaskTest {
      */
     @Test
     public void createEventWithInvalidDate() {
-        assertThrows(InvalidDateException.class, () -> Task.createTask("event wedding /from 10-10-2019 /to 11-10-2019"));
+        assertThrows(InvalidDateException.class, () -> Task.createTask("event wedding /from 10-10-2019 "
+                + "/to 11-10-2019"));
     }
 
     /**
@@ -38,11 +41,13 @@ public class TaskTest {
      */
     @Test
     public void createTaskWithInvalidTaskType() {
-        assertThrows(InvalidTaskException.class, () -> Task.createTask("invalidTask wedding /from 10-10-2019 /to 11-10-2019"));
+        assertThrows(InvalidTaskException.class, () -> Task.createTask("invalidTask wedding "
+                + "/from 10-10-2019 /to 11-10-2019"));
     }
 
     @Test
-    public void createTaskNoTagsNoTime() throws NoTaskDescriptionException, InvalidTaskException, InvalidDateException {
+    public void createTaskNoTagsNoTime() throws NoTaskDescriptionException, InvalidTaskException,
+            InvalidDateException {
         Task newTask = Task.createTask("event wedding /from 10/10/2019 /to 10/10/2019");
         assertEquals("[E][ ] wedding (from: Oct 10 2019  to: Oct 10 2019) tags: no tags", newTask.toString());
     }
@@ -50,15 +55,15 @@ public class TaskTest {
     @Test
     public void createTaskNoTags() throws NoTaskDescriptionException, InvalidTaskException, InvalidDateException {
         Task newTask = Task.createTask("event wedding /from 10/10/2019 1800 /to 10/10/2019 2300");
-        assertEquals("[E][ ] wedding (from: Oct 10 2019 18:00 to: Oct 10 2019 23:00) tags: no tags", newTask.toString());
+        assertEquals("[E][ ] wedding (from: Oct 10 2019 18:00 to: Oct 10 2019 23:00) tags: no tags",
+                newTask.toString());
     }
 
     @Test
     public void createTaskWithTags() throws NoTaskDescriptionException, InvalidTaskException, InvalidDateException {
-        Task newTask = Task.createTask("event wedding /from 10/10/2019 1800 /to 10/10/2019 2300 -t important " +
-                "leisure");
-        assertEquals("[E][ ] wedding (from: Oct 10 2019 18:00 to: Oct 10 2019 23:00) tags: important leisure ",
-                newTask.toString());
+        Task newTask = Task.createTask("event wedding /from 10/10/2019 1800 /to 10/10/2019 2300 "
+                + "-t important leisure");
+        assertEquals("[E][ ] wedding (from: Oct 10 2019 18:00 to: Oct 10 2019 23:00) "
+                + "tags: important leisure ", newTask.toString());
     }
-
 }
