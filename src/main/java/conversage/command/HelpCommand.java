f3e46c8afc1ca076f65c50a9459506dd56b05ba6
ashea10 <enasveen13@gmail.com>
2024-09-20 17:38:29 +0800

    Personalize help message

--- src/main/java/conversage/command/HelpCommand.java
+++ src/main/java/conversage/command/HelpCommand.java
@@ -7,17 +7,20 @@ import conversage.ui.Ui;
 
 public class HelpCommand extends Command {
 
-    private static final String HELP_MESSAGE = "Here are the available commands:\n"
-            + "1. todo [task description] - Adds a ToDo task\n"
-            + "2. deadline [task description] /by [date] - Adds a Deadline task\n"
-            + "3. event [task description] /from [start time] /to [end time] - Adds an Event task\n"
-            + "4. list - Lists all tasks\n"
-            + "5. mark [task number] - Marks a task as done\n"
-            + "6. unmark [task number] - Unmarks a task\n"
-            + "7. delete [task number] - Deletes a task\n"
-            + "8. find [keyword] - Finds tasks containing the keyword\n"
-            + "9. bye - Exits the application\n"
-            + "10. help - Displays this help message";
+    private static final String HELP_MESSAGE =
+            "Welcome, seeker of wisdom. Here are the commands that ConverSage understands:\n"
+                    + "1. todo [task description] - Adds a ToDo task, a step on your journey.\n"
+                    + "2. deadline [task description] /by [date] - Adds a Deadline task, marking the end of a quest.\n"
+                    + "3. event [task description] /from [start time] /to [end time] - Adds an Event task, a moment in time to remember.\n"
+                    + "4. list - Lists all tasks, revealing the path ahead.\n"
+                    + "5. mark [task number] - Marks a task as done, a milestone achieved.\n"
+                    + "6. unmark [task number] - Unmarks a task, revisiting a step.\n"
+                    + "7. delete [task number] - Deletes a task, removing an obstacle from your path.\n"
+                    + "8. find [keyword] - Finds tasks containing the keyword, seeking wisdom in your notes.\n"
+                    + "9. bye - Exits the application, bidding you farewell for now.\n"
+                    + "10. help - Displays this help message, guiding you on your journey.\n"
+                    + "\nNote: For any timings, please use the format: yyyy-MM-dd HH:mm.\n"
+                    + "May your path be clear and your tasks be light, seeker.";
 
 
     @Override
