43bd87b3c05252d5eac99a35c1a66c2faa948bdf
ITZXITZX <isaacteozixian@hotmail.com>
2024-09-21 02:59:19 +0800

    Add TaskListTest cases

--- src/test/java/bob/task/TaskListTest.java
+++ src/test/java/bob/task/TaskListTest.java
@@ -1,6 +1,7 @@
 package bob.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -9,8 +10,6 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
 
 
 public class TaskListTest {
@@ -39,5 +38,53 @@ public class TaskListTest {
         String listRecordString = taskList.getListRecordsString();
         String actual = outputStreamCaptor.toString().replaceAll("\\s+$", "");
         assertEquals(expected, actual);
+    }
+    @Test
+    public void testSearchKeywordInRecordsWithNoKeyword() {
+        TaskList taskList = new TaskList();
+        taskList.updateWithNewTask(new Todo("task 1"));
+        taskList.updateWithNewTask(new Deadline("task 2", "30 Sep 2024"));
+        ArrayList<Task> results = taskList.searchKeywordInRecords("");
+        assertTrue(results.isEmpty());
+    }
+    @Test
+    public void testSearchKeywordInRecordsWithWrongKeyword() {
+        TaskList taskList = new TaskList();
+        taskList.updateWithNewTask(new Todo("task 1"));
+        taskList.updateWithNewTask(new Deadline("task 2", "30 Sep 2024"));
+        ArrayList<Task> results = taskList.searchKeywordInRecords("happy");
+        assertTrue(results.isEmpty());
+    }
+    @Test
+    public void testSearchKeywordInRecordsWith1Match() {
+        TaskList taskList = new TaskList();
+        taskList.updateWithNewTask(new Todo("task 1"));
+        taskList.updateWithNewTask(new Deadline("task 2", "30 Sep 2024"));
+        ArrayList<Task> results = taskList.searchKeywordInRecords("1");
+        assertEquals(1, results.size());
+    }
+    @Test
+    public void testSearchKeywordInRecordsWith2Match() {
+        TaskList taskList = new TaskList();
+        taskList.updateWithNewTask(new Todo("task 1"));
+        taskList.updateWithNewTask(new Deadline("task 2", "30 Sep 2024"));
+        ArrayList<Task> results = taskList.searchKeywordInRecords("task");
+        assertEquals(2, results.size());
+    }
+    @Test
+    public void testSearchKeywordInRecordsWithTooManyKeywords() {
+        TaskList taskList = new TaskList();
+        taskList.updateWithNewTask(new Todo("task 1"));
+        taskList.updateWithNewTask(new Deadline("task 2", "30 Sep 2024"));
+        ArrayList<Task> results = taskList.searchKeywordInRecords("task 1 and 2");
+        assertTrue(results.isEmpty());
+    }
+    @Test
+    public void testSearchKeywordInRecordsWithCaseSensitiveKeyword() {
+        TaskList taskList = new TaskList();
+        taskList.updateWithNewTask(new Todo("task 1"));
+        taskList.updateWithNewTask(new Deadline("task 2", "30 Sep 2024"));
+        ArrayList<Task> results = taskList.searchKeywordInRecords("TASK");
+        assertTrue(results.isEmpty());
     }
 }
