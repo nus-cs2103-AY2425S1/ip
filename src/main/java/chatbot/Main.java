c973148506bdb1b12d50699370311f201f6684d3
Chong Tze Zhao <47619363+chongtzezhao@users.noreply.github.com>
2024-09-20 16:04:22 +0800

    Add JAR to "artifacts"

--- src/main/java/chatbot/Main.java
+++ src/main/java/chatbot/Main.java
@@ -19,7 +19,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-//import javafx.scene.image.Image;
+import javafx.scene.image.Image;
 import chatbot.impl.ui.DialogBox;
 
 
@@ -41,28 +41,28 @@ public class Main extends Application {
        bot.start();
     }
 
-    @Override
-    public void start(Stage stage) {
+   @Override
+   public void start(Stage stage) {
         //Setting up required components
 
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
+       scrollPane = new ScrollPane();
+       dialogContainer = new VBox();
+       scrollPane.setContent(dialogContainer);
 
-        userInput = new TextField();
-        sendButton = new Button("Send");
+       userInput = new TextField();
+       sendButton = new Button("Send");
 
 //        DialogBox dialogBox = new DialogBox("Hello!", userImage);
 //        dialogContainer.getChildren().addAll(dialogBox);
 
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+       AnchorPane mainLayout = new AnchorPane();
+       mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-        scene = new Scene(mainLayout);
+       scene = new Scene(mainLayout);
 
-        stage.setScene(scene);
-        stage.show();
+       stage.setScene(scene);
+       stage.show();
 
         //More code to be added here later
-    }
+   }
 }
