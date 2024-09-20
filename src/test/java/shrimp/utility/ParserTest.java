3feeca19583d2465ceac6d476d81aad8739f4a67
Jing Hong Tee <e1122714@u.nus.edu>
2024-09-20 13:21:33 +0800

    Update styling for JUnit tests

--- src/test/java/shrimp/utility/ParserTest.java
+++ src/test/java/shrimp/utility/ParserTest.java
@@ -1,8 +1,8 @@
 package shrimp.utility;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class ParserTest {
 
