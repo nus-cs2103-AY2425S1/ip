8c5f0d5d1fb89b97ec369369e14b53e0ecd677b6
SamuelFoo <fooenzesamuel@gmail.com>
2024-09-20 20:38:40 +0800

    Change naming from time to date

--- src/main/java/duck/exceptions/EventUsageException.java
+++ src/main/java/duck/exceptions/EventUsageException.java
@@ -2,7 +2,7 @@ package duck.exceptions;
 
 public class EventUsageException extends UsageException {
     public EventUsageException() {
-        super("event <description> /from <start_time> /to <end_time>",
-                "description", "start_time", "end_time");
+        super("event <description> /from <start_date> /to <end_date>",
+                "description", "start_date", "end_date");
     }
 }
