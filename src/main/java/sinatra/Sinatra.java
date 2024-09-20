33eddffaece402a04daa4134d5b183c2eb728fc3
travisim <64481469+travisim@users.noreply.github.com>
2024-09-20 19:56:28 +0800

    fix style issues

--- src/main/java/sinatra/Sinatra.java
+++ src/main/java/sinatra/Sinatra.java
@@ -1,10 +1,5 @@
 package sinatra;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * Represents the main Sinatra application.
