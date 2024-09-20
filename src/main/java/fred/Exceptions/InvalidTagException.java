a901334a025590981487917ed9fea7cd6db2ad49
Nicholas Chua <e1114227@u.nus.edu>
2024-09-20 20:21:13 +0800

    Add InvalidTagException

--- src/main/java/fred/Exceptions/InvalidTagException.java
+++ src/main/java/fred/Exceptions/InvalidTagException.java
@@ -2,6 +2,6 @@ package fred.Exceptions;
 
 public class InvalidTagException extends FredException {
     public InvalidTagException() {
-        super("OOPS!!! That's not the correct format for a tag");
+        super("OOPS!!! Please use this format: tag 1 fun");
     }
 }
