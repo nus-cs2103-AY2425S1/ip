76c255bea78834c3cd922e76198a42652a081fe1
JiaYi-Gallium369 <seahjiayi369@gmail.com>
2024-09-20 20:28:50 +0800

    Add icon to title bar

--- src/main/java/gallium/main/Main.java
+++ src/main/java/gallium/main/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,8 @@ public class Main extends Application {
             stage.setTitle("Gallium");
             AnchorPane ap = fxmlLoader.load();
             assert ap != null : "AnchorPane cannot be loaded";
+            Image icon = new Image(getClass().getResourceAsStream("/images/DaGallium.jpeg"));
+            stage.getIcons().add(icon);
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setGallium(gallium); 
