ef707e2ec464c4affb511fdb375a08a11259011e
dovlin <dovlincs01@gmail.com>
2024-09-20 23:06:21 +0800

    Update exception message to include all Momo features

--- src/main/java/momo/exception/InvalidCommandException.java
+++ src/main/java/momo/exception/InvalidCommandException.java
@@ -8,8 +8,8 @@ public class InvalidCommandException extends MomoException {
      * Exception thrown when user makes an invalid command.
      */
     public InvalidCommandException() {
-        super("Your command is INVALID. You did not properly specify the type of task (todo/deadline/event/archive) or "
-                + "command (bye/list/mark/unmark)");
+        super("Your command is INVALID. You did not properly specify the type of task you wanted to add "
+                + "(todo/deadline/event/archive) or command (bye/list/mark/unmark/find)");
     }
 
     public InvalidCommandException(String message) {
