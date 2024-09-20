09e02caf69fc8ca1f2bd87bd2f0950925b07dcc3
hpcman <happypacman36@gmail.com>
2024-09-20 20:47:48 +0800

    Add GUI

---
+++ src/main/java/pacman/Main.java
@@ -0,0 +1,31 @@
+package pacman;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Pacman pacman = new Pacman();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<pacman.MainWindow>getController().setPacman(pacman);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/pacman/MainWindow.java
@@ -0,0 +1,56 @@
+package pacman;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import pacman.gui.DialogBox;
+
+public class MainWindow extends AnchorPane {
+
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private Scene scene;
+
+    private Pacman pacman;
+
+    private Image ghostImage = new Image(this.getClass().getResourceAsStream("/images/ghost.png"));
+    private Image pacmanImage = new Image(this.getClass().getResourceAsStream("/images/pacman.png"));
+    private String greet = "Hello! I'm PacMan. How can I help you?";
+
+    @FXML
+    public void initialize() {
+        dialogContainer.getChildren().add(DialogBox.getPacmanDialog(greet, pacmanImage));
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setPacman(Pacman p) {
+        pacman = p;
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String pacmanText = pacman.getResponse(userText);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getGhostDialog(userText, ghostImage),
+                DialogBox.getPacmanDialog(pacmanText, pacmanImage)
+        );
+        userInput.clear();
+        if (userText.equals("bye")) {
+            Platform.exit();
+        }
+    }
+}
