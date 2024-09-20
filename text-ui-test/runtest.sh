3ed0e05a6f7779f0082fe70972b9be5305cdd4b9
WindiStone <e0958609@u.nus.edu>
2024-09-20 17:50:05 +0800

    Change runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/windebot/Winde.java
+# create bin directory if it doesn't exist
+if [ ! -d "../bin" ]
+then
+    mkdir ../bin
+fi
+...
+# compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ./src/main/java -Xlint:none -d ./bin ./src/main/java/windebot/Winde.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
