b74d8944838aaae9a01b6175dd36ee067266b4e1
Nicholas Chua <e1114227@u.nus.edu>
2024-09-20 20:20:50 +0800

    Add InvalidEventException

--- src/main/java/fred/Exceptions/InvalidEventException.java
+++ src/main/java/fred/Exceptions/InvalidEventException.java
@@ -1,4 +1,7 @@
 package fred.Exceptions;
 
-public class InvalidEventException {
+public class InvalidEventException extends FredException {
+    public InvalidEventException() {
+        super("OOPS!!! Please use this format: event orientation day /from 2000-01-01 00:00 /to 2000-01-02 00:00");
+    }
 }
