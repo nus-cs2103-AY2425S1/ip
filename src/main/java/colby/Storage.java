d5bce7c6dedb0e75124a82cf15f397b3baee76f3
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-09-20 10:29:20 +0800

    Edit buildList method to include adding all details of relevant tasks

--- src/main/java/colby/Storage.java
+++ src/main/java/colby/Storage.java
@@ -63,29 +63,53 @@ public class Storage {
         TaskList list = new TaskList();
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNext()) {
-                String nextLine = scanner.nextLine();;
-                if (nextLine.contains("[X]")) {
-                    if (nextLine.contains("[T]")) {
-                        String fullDescription = nextLine.split(" ")[1];
-                        if (fullDescription.split("").length > 1) {
-                            list.addTask(new ToDo(fullDescription.split("\\(needs\\) ")[0] +
-                                    "/needs" + fullDescription.split("\\(needs\\)")[1]));
-                        } else {
-                            list.addTask(new ToDo(fullDescription));
+                String nextLine = scanner.nextLine();
+                String[] parts = nextLine.split(" ", 2);
+                String fullDescription = parts[1];
+                boolean isDone = nextLine.contains("[X]");
+
+                if (nextLine.contains("[T]")) {
+                    String[] todoParts = fullDescription.split("\\(needs ", 2);
+                    Task newTask;
+                    if (todoParts.length > 1) {
+                        newTask = new ToDo(todoParts[0].trim() + "/needs " + todoParts[1].trim());
+                            newTask.isDone = true;
+                    } else {
+                            newTask = new ToDo(fullDescription);
+                    }
+                    list.addTask(newTask);
+
+                    if (isDone) {
+                        newTask.isDone = true;
+                    }
+
+                } else if (nextLine.contains("[D]")) {
+                    String[] deadlineParts = fullDescription.split("\\(by: ", 2);
+                    if (deadlineParts.length == 2) {
+                        Task newTask = new Deadline(deadlineParts[0].trim(), deadlineParts[1].replace(")", "").trim());
+                        list.addTask(newTask);
+                        if (isDone) {
+                            newTask.isDone = true;
                         }
-                    } else if (nextLine.contains("[D]")) {
-                        
+                    }
+                } else {
+                    String[] eventParts = fullDescription.split("\\(from: ", 2);
+                    if (eventParts.length == 2) {
+                        String[] duration = eventParts[1].split("to:", 2);
+                        if (duration.length == 2) {
+                            Task newTask = new Event(eventParts[0].trim(), duration[0].trim(), duration[1].replace(")", "").trim());
+                            list.addTask(newTask);
+                            if (isDone) {
+                                newTask.isDone = true;
+                            }
+                        }
                     }
                 }
-
-
-
-                Task task = Parser.parseToDoTask(scanner.nextLine());
-                list.addTask(task);
             }
         }
         return list;
     }
+
     public void checkFile() {
         try {
             File myObj = new File(filePath);
