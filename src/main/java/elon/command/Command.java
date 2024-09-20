e88d9856c7aa3e0da1a53f852f60ee571e634929
LAPTOP-20936SS7\kusha <e1121075@u.nus.edu>
2024-09-20 09:45:19 +0800

    Add JavaDoc for commands

--- src/main/java/elon/command/Command.java
+++ src/main/java/elon/command/Command.java
@@ -4,6 +4,19 @@ import elon.Storage;
 import elon.task.TaskList;
 import elon.Ui;
 
+/**
+ * Represents an abstract command that can be executed.
+ */
 public abstract class Command {
+
+    /**
+     * Executes the command with the given task list, user interface, and storage.
+     *
+     * @param list the TaskList that the command will operate on
+     * @param ui the user interface to respond to the user
+     * @param storage the storage to save or load task data
+     * @return a string representing the result of executing the command
+     * @throws Exception if any error occurs during execution
+     */
     public abstract String execute(TaskList list, Ui ui, Storage storage) throws Exception;
 }
--- src/main/java/elon/command/ExitCommand.java
+++ src/main/java/elon/command/ExitCommand.java
@@ -4,8 +4,19 @@ import elon.Storage;
 import elon.Ui;
 import elon.task.TaskList;
 
+/**
+ * Represents a command to exit the application.
+ */
 public class ExitCommand extends Command {
 
+    /**
+     * Executes the exit command, which returns a message from Ui.
+     *
+     * @param list the task list
+     * @param ui the user interface to interact with the user
+     * @param storage the storage
+     * @return a string representing the exit message
+     */
     @Override
     public String execute(TaskList list, Ui ui, Storage storage) {
         return ui.exitMessage();
