e2b7caf318cf8cf07436d5d990b21116dd09f6f9
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-09-21 23:55:42 +0800

    Update NumberCommandTest.java

--- src/test/java/notgpt/parsers/NumberCommandTest.java
+++ src/test/java/notgpt/parsers/NumberCommandTest.java
@@ -24,13 +24,13 @@ class NumberCommandTest {
         storage.clear();
         // Add mock tasks to storage for testing
         storage.todo("Task 1");
-        storage.todo("Task 3");
+        storage.todo("Task 2");
     }
 
     @Test
     void testExecuteMarkValidTask() {
         String result = NumberCommand.execute(storage, "1", "mark");
-        assertEquals("marked 1 as completed\nuse \"list\" to see changes", result);
+        assertEquals("marked 1 as completed\n\"[T][X] Task 1\"\nwow you actually did something... amazing...", result);
     }
 
     @Test
@@ -39,13 +39,14 @@ class NumberCommandTest {
         NumberCommand.execute(storage, "1", "mark");
         // Now unmark it
         String result = NumberCommand.execute(storage, "1", "unmark");
-        assertEquals("marked 1 as uncompleted\nuse \"list\" to see changes", result);
+        assertEquals("marked 1 as uncompleted\n\"[T][ ] Task 1\"\nso finishing that was a lie huh?"
+                + " Same tbh... doing stuff is hard", result);
     }
 
     @Test
     void testExecuteDeleteValidTask() {
         String result = NumberCommand.execute(storage, "1", "delete");
-        assertEquals("deleted 1\nuse \"list\" to see changes", result);
+        assertEquals("deleted 1\n\"[T][ ] Task 1\"\nHope you got that right...", result);
     }
 
     @Test
