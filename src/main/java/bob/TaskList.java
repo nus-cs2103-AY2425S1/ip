130bb60611bbfa9615e410cf647b3731f5d146ab
Nathan Lye <nathanlye05@gmail.com>
2024-09-21 20:43:11 +0800

    add header comments for TaskList

--- src/main/java/bob/TaskList.java
+++ src/main/java/bob/TaskList.java
@@ -9,34 +9,74 @@ import java.util.List;
 public class TaskList implements Iterable<Task> {
     private final List<Task> TASKS;
 
+    /**
+     * Construct an empty task list.
+     */
     public TaskList() {
         this.TASKS = new ArrayList<>();
     }
 
+    /**
+     * Construct a task list populated by the given list of tasks.
+     *
+     * @param tasks the tasks that are in the initial task list
+     */
     public TaskList(List<Task> tasks) {
         this.TASKS = tasks;
     }
 
+    /**
+     * Check if this list is empty.
+     *
+     * @return true if this list is empty, false otherwise
+     */
     public boolean isEmpty() {
         return TASKS.isEmpty();
     }
 
+    /**
+     * Add the given task to this list.
+     *
+     * @param task the task to be added
+     */
     public void add(Task task) {
         TASKS.add(task);
     }
 
+    /**
+     * Removes the task at the given index in this list.
+     *
+     * @param index index of the task to be removed
+     * @return the removed task
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
     public Task remove(int index) {
         return TASKS.remove(index);
     }
 
+    /**
+     * Get the task at the given index in this list.
+     *
+     * @param index index of the task to return
+     * @return the task at the given index in this list
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
     public Task get(int index) {
         return TASKS.get(index);
     }
 
+    /**
+     * Returns the number of tasks in this list.
+     *
+     * @return the number of tasks in this list
+     */
     public int size() {
         return TASKS.size();
     }
 
+    /**
+     * Removes all tasks in this list.
+     */
     public void reset() {
         TASKS.clear();
     }
