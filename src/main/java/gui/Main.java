847542d215695d1817efe7339c78f678a4c59f13
Quasant <benjaminthechin@gmail.com>
2024-09-22 03:54:06 +0800

    Add stage title

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Screwllum");
             fxmlLoader.<MainWindow>getController().setScrewllum(screwllum); // Inject the Screwllum instance
             fxmlLoader.<MainWindow>getController().showWelcome();
             stage.show();
