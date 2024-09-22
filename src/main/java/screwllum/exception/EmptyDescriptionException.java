fe9e6b9c16c69bf5e5ca9055879b78bb80db7af8
Quasant <benjaminthechin@gmail.com>
2024-09-22 11:24:52 +0800

    Remove EmptyDescriptionException as it's role is filled by InvalidCommandException

--- src/main/java/screwllum/exception/EmptyDescriptionException.java
+++
@@ -1,7 +0,0 @@
-package screwllum.exception;
-
-public class EmptyDescriptionException extends ScrewllumException {
-    public EmptyDescriptionException() {
-        super("Description is empty! Please name your task something!");
-    }
-}
