94f86e65e7d0e890638d7006476b989a7d08fbc2
Curtis <curtischang2510@gmail.com>
2024-09-22 22:05:28 +0800

    Refactor batch script to recursively compile all Java files
    - Updated the Windows batch script to use a `for /r` loop for
    recursively compiling all Java files under `src/main/java`.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,10 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+REM javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+for /r ..\src\main\java %%f in (*.java) do (
+    javac -cp ..\src\main\java -Xlint:none -d ..\bin "%%f"
+)
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
