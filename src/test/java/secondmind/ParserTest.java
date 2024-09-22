eec4c94ffbc0dda7b8cb319468a1dc856cb42790
Chan Guan Yu <changuanyu2904@gmail.com>
2024-09-22 21:13:39 +0800

    Modify and add test cases to reflect new behaviour of Parser class

--- src/test/java/secondmind/ParserTest.java
+++ src/test/java/secondmind/ParserTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
     @Test
-    void processInput_byeCommand_success() {
+    void processInput_byeCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
         String[] parsedInput = parser.processInput("bye");
         assertTrue(parsedInput[0].equals("bye"));
@@ -16,7 +16,7 @@ public class ParserTest {
     }
 
     @Test
-    void processInput_validMarkCommand_success() {
+    void processInput_validMarkCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
         String[] parsedInput = parser.processInput("mark 1");
         assertTrue(parsedInput[0].equals("mark"));
@@ -24,15 +24,23 @@ public class ParserTest {
     }
 
     @Test
-    void processInput_invalidMarkCommand_numberFormatExceptionThrown() {
+    void processInput_invalidMarkCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
-        assertThrows(NumberFormatException.class, () -> {
-            parser.processInput("mark wrongFormat");
+        String[] parsedInput = parser.processInput("mark wrongFormat");
+        assertTrue(parsedInput[0].equals("mark"));
+        assertTrue(parsedInput[1].equals("wrongFormat"));
+    }
+
+    @Test
+    void processInput_markCommandWithoutArguments_invalidCommandFormatExceptionThrown() {
+        Parser parser = new Parser();
+        assertThrows(InvalidCommandFormatException.class, () -> {
+            parser.processInput("mark");
         });
     }
 
     @Test
-    void processInput_validUnmarkCommand_success() {
+    void processInput_validUnmarkCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
         String[] parsedInput = parser.processInput("unmark 1");
         assertTrue(parsedInput[0].equals("unmark"));
@@ -40,15 +48,23 @@ public class ParserTest {
     }
 
     @Test
-    void processInput_invalidUnmarkCommand_numberFormatExceptionThrown() {
+    void processInput_invalidUnmarkCommand_InvalidCommandFormatExceptionThrown() throws InvalidCommandFormatException {
         Parser parser = new Parser();
-        assertThrows(NumberFormatException.class, () -> {
-            parser.processInput("unmark wrongFormat");
+        String[] parsedInput = parser.processInput("mark wrongFormat");
+        assertTrue(parsedInput[0].equals("mark"));
+        assertTrue(parsedInput[1].equals("wrongFormat"));
+    }
+
+    @Test
+    void processInput_unmarkCommandWithoutArguments_invalidCommandFormatExceptionThrown() {
+        Parser parser = new Parser();
+        assertThrows(InvalidCommandFormatException.class, () -> {
+            parser.processInput("unmark");
         });
     }
 
     @Test
-    void processInput_validDeleteCommand_success() {
+    void processInput_validDeleteCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
         String[] parsedInput = parser.processInput("delete 1");
         assertTrue(parsedInput[0].equals("delete"));
@@ -56,15 +72,23 @@ public class ParserTest {
     }
 
     @Test
-    void processInput_invalidDeleteCommand_numberFormatExceptionThrown() {
+    void processInput_invalidDeleteCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
-        assertThrows(NumberFormatException.class, () -> {
-            parser.processInput("delete wrongFormat");
+        String[] parsedInput = parser.processInput("delete wrongFormat");
+        assertTrue(parsedInput[0].equals("delete"));
+        assertTrue(parsedInput[1].equals("wrongFormat"));
+    }
+
+    @Test
+    void processInput_deleteCommandWithoutArguments_invalidCommandFormatExceptionThrown() {
+        Parser parser = new Parser();
+        assertThrows(InvalidCommandFormatException.class, () -> {
+            parser.processInput("delete");
         });
     }
 
     @Test
-    void processInput_listCommand_success() {
+    void processInput_listCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
         String[] parsedInput = parser.processInput("list");
         assertTrue(parsedInput[0].equals("list"));
@@ -72,7 +96,7 @@ public class ParserTest {
     }
 
     @Test
-    void processInput_taskCommand_success() {
+    void processInput_taskCommand_success() throws InvalidCommandFormatException {
         Parser parser = new Parser();
         String[] parsedInput = parser.processInput("todo read book");
         assertTrue(parsedInput[0].equals("add"));
@@ -83,5 +107,23 @@ public class ParserTest {
         parsedInput = parser.processInput("event project meeting /from 2024-11-11T16:00:00 /to 2024-11-11T19:00:00");
         assertTrue(parsedInput[0].equals("add"));
         assertTrue(parsedInput[1].equals("event project meeting /from 2024-11-11T16:00:00 /to 2024-11-11T19:00:00"));
+    }
+
+    @Test
+    void processInput_validFindCommand_success() throws InvalidCommandFormatException {
+        Parser parser = new Parser();
+        String[] parsedInput = parser.processInput("find keyword");
+        assertTrue(parsedInput[0].equals("find"));
+        assertTrue(parsedInput[1].equals("keyword"));
+    }
+
+    // Test case for the "find" command with multiple keywords
+    @Test
+    void processInput_findCommandWithMultipleKeywords_success() throws InvalidCommandFormatException {
+        Parser parser = new Parser();
+        String[] parsedInput = parser.processInput("find keyword1 keyword2");
+        assertTrue(parsedInput[0].equals("find"));
+        assertTrue(parsedInput[1].equals("keyword1"));
+        assertTrue(parsedInput[2].equals("keyword2"));
     }
 }
