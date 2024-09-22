b59e2f6949c3a85e5924f9b35c951e0ae1a8c4b7
Chan Guan Yu <changuanyu2904@gmail.com>
2024-09-22 20:33:35 +0800

    Rename class for invalid command format exceptions

--- src/main/java/secondmind/InvalidCommandFormatException.java
+++ src/main/java/secondmind/InvalidCommandFormatException.java
@@ -1,6 +1,6 @@
 package secondmind;
 
-public class InvalidCommandFormat extends Exception {
+public class InvalidCommandFormatException extends Exception {
     @Override
     public String toString() {
         return "Invalid command format detected.";
