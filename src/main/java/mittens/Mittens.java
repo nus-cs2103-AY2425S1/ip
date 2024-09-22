2003c7de2fe9c9aad42aea7a7a20463ca34206dd
izruff <rafaelky.dev@gmail.com>
2024-09-23 05:11:02 +0800

    Mittens.java: Refactor code and add method to load task list after initialization

--- src/main/java/mittens/Mittens.java
+++ src/main/java/mittens/Mittens.java
@@ -1,5 +1,6 @@
 package mittens;
 
+import javafx.application.Application;
 import mittens.commands.Command;
 import mittens.parser.BadInputException;
 import mittens.parser.CommandParser;
@@ -8,28 +9,39 @@ import mittens.storage.StorageFileException;
 import mittens.task.TaskList;
 import mittens.ui.TextUi;
 import mittens.ui.Ui;
+import mittens.ui.fx.MainApp;
 
 /**
  * Represents the main class where the program is initialized and the main loop is run.
  */
 public class Mittens {
-    
+    public static final boolean IS_USING_GUI = true;
+    public static final String DEFAULT_STORAGE_FILE_PATH = "data/data.txt";
+
     private final Ui ui;
     private final Storage storage;
-    private final TaskList taskList;
+    private TaskList taskList;
 
     /**
-     * Creates a new Mittens object with the specified storage file path.
-     * 
+     * Creates a new Mittens object with the specified UI object and storage file path.
+     *
+     * @param ui The UI object
      * @param storageFilePath The file path to the storage file
      */
-    public Mittens(String storageFilePath) {
-        this.ui = new TextUi();
+    public Mittens(Ui ui, String storageFilePath) {
+        this.ui = ui;
         this.storage = new Storage(storageFilePath);
 
+        this.loadTaskList();
+    }
+
+    /**
+     * Initializes the task list by loading it from the storage file.
+     */
+    public void loadTaskList() {
         TaskList temp;
         try {
-            temp = this.storage.load();
+            temp = storage.load();
         } catch (StorageFileException e) {
             ui.showErrorMessage(e);
             ui.showRegularMessage("Would you like to continue with a new list instead? (y/n)");
@@ -48,30 +60,53 @@ public class Mittens {
     }
 
     /**
+     * Parses the given input and executes the command, then returns a
+     * boolean indicating whether the program should exit.
+     *
+     * @param input The input to process
+     * @return A boolean indicating whether the program should exit
+     */
+    public boolean process(String input) {
+        CommandParser commandParser = new CommandParser();
+        try {
+            Command command = commandParser.parse(input);
+            command.execute(this.taskList, this.ui, this.storage);
+            return command.isExit();
+        } catch (BadInputException e) {
+            ui.showErrorMessage(e);
+            return false;
+        }
+    }
+
+    /**
      * Runs the main loop of the program which accepts user input.
      */
     public void run() {
+        if (taskList == null) {
+            throw new RuntimeException("Task list is not initialized");
+        }
+
         CommandParser commandParser = new CommandParser();
 
         ui.showGreetingMessage();
 
         while (true) {
             String input = ui.getUserInput();
-
-            try {
-                Command command = commandParser.parse(input);
-                command.execute(this.taskList, this.ui, this.storage);
-                if (command.isExit()) {
-                    break;
-                }
-            } catch (BadInputException e) {
-                ui.showErrorMessage(e);
+            boolean shouldExit = process(input);
+            if (shouldExit) {
+                break;
             }
         }
+
     }
 
     public static void main(String[] args) {
-        Mittens mittens = new Mittens("data/data.txt");
-        mittens.run();
+        if (IS_USING_GUI) {
+            Application.launch(MainApp.class);
+        } else {
+            Mittens mittens = new Mittens(new TextUi(), DEFAULT_STORAGE_FILE_PATH);
+            mittens.run();
+        }
+
     }
 }
