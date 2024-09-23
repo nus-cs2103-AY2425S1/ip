f9ae0469589f23f69c1fc2a954f9e8c475720183
Nathan Lye <nathanlye05@gmail.com>
2024-09-23 12:54:42 +0800

    add styling elements with css

--- src/main/java/bob/Main.java
+++ src/main/java/bob/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setMinHeight(220);
-            stage.setMinWidth(400);
+            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setBob(bob);  // inject the Bob instance
             stage.show();
         } catch (IOException e) {
--- src/main/java/bob/gui/DialogBox.java
+++ src/main/java/bob/gui/DialogBox.java
@@ -45,6 +45,7 @@ public class DialogBox extends HBox {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         FXCollections.reverse(tmp);
         this.getChildren().setAll(tmp);
+        dialog.getStyleClass().add("reply-label");
     }
 
     public static DialogBox getUserDialog(String s, Image i) {
