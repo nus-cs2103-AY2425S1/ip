e6aad38ffabbacc34614d06bb1f6202c5d8b46fe
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-09-23 18:19:33 +0800

    Remove magic literals in Parser

--- src/main/java/rizzler/ui/parser/Parser.java
+++ src/main/java/rizzler/ui/parser/Parser.java
@@ -23,11 +23,48 @@ public class Parser {
     private static final String MISSING_ARGUMENT_RESPONSE = "sorry dear, you're missing one or more arguments.";
     private static final String WRONG_ARGUMENT_RESPONSE = "sorry dear, one or more of your arguments are wrong.";
     private static final String UNKNOWN_ERROR_RESPONSE = "dreadfully sorry darlin', but there's an unexpected error.";
+    private static final String UNRECOGNISED_COMMAND_RESPONSE = " is not a recognised command.";
+
+    private static final String ERROR_MESSAGE_JOIN_CHARACTER = "\n\n";
+
+    private static final String DEADLINE_KEYWORD = "/by";
+    private static final String EVENT_START_KEYWORD = "/from";
+    private static final String EVENT_END_KEYWORD = "/to";
 
     /**
      * Constructs a <code>Parser</code> object that can parse user input strings.
      */
     public Parser() {
+    }
+
+    /**
+     * Takes in input as a given input <code>String</code>, returning the appropriate command.
+     * @param userInput <code>String</code> entered by the user.
+     * @return Command of varying types depending on user input.
+     */
+    public Command parseInput(String userInput) {
+        String trimmedUserInput = userInput.trim();
+        String[] userInputArr = trimmedUserInput.split(" ");
+        Command outputCommand;
+        String userInputFirstWord = userInputArr[0].trim();
+        outputCommand = switch (userInputFirstWord) {
+            case "bye" -> new ByeCommand();
+            case "help" -> new HelpCommand();
+            case "list" -> new ListCommand();
+            case "find" -> parseFind(userInputArr);
+            case "delete" -> parseDelete(userInputArr);
+            case "mark" -> parseMark(userInputArr);
+            case "unmark" -> parseUnmark(userInputArr);
+            case "todo" -> parseTodo(userInputArr);
+            case "deadline" -> parseDeadline(userInputArr);
+            case "event" -> parseEvent(userInputArr);
+            default -> parseUnknown(userInput);
+        };
+        return outputCommand;
+    }
+
+    private String joinErrorMessages(String... values) {
+        return String.join(ERROR_MESSAGE_JOIN_CHARACTER, values);
     }
 
     private Command parseFind(String[] userInputArr) {
@@ -115,7 +152,7 @@ public class Parser {
         try {
             String[] deadlineInfoArr = Arrays.copyOfRange(userInputArr, 1, userInputArr.length);
             String deadlineInfo = String.join(" ", deadlineInfoArr);
-            String[] deadlineInfoBySplit = deadlineInfo.split("/by");
+            String[] deadlineInfoBySplit = deadlineInfo.split(DEADLINE_KEYWORD);
             String deadlineDesc = deadlineInfoBySplit[0].trim();
             String deadlineTime = deadlineInfoBySplit[1].trim();
             outputCommand = new DeadlineCommand(deadlineDesc, deadlineTime);
@@ -133,11 +170,11 @@ public class Parser {
         try {
             String[] eventInfoArr = Arrays.copyOfRange(userInputArr, 1, userInputArr.length);
             String eventInfo = String.join(" ", eventInfoArr);
-            String[] eventInfoBySplit = eventInfo.split("/from");
+            String[] eventInfoBySplit = eventInfo.split(EVENT_START_KEYWORD);
             String eventDesc = eventInfoBySplit[0].trim();
             String eventDuration = eventInfoBySplit[1];
-            String eventStart = eventDuration.split("/to")[0].trim();
-            String eventEnd = eventDuration.split("/to")[1].trim();
+            String eventStart = eventDuration.split(EVENT_END_KEYWORD)[0].trim();
+            String eventEnd = eventDuration.split(EVENT_END_KEYWORD)[1].trim();
             outputCommand = new EventCommand(eventDesc, eventStart, eventEnd);
         } catch (IndexOutOfBoundsException | RizzlerException e) {
             String response = joinErrorMessages(MISSING_ARGUMENT_RESPONSE, EventCommand.COMMAND_FORMAT);
@@ -154,37 +191,7 @@ public class Parser {
      * @return NullCommand conveying to the user that the input is not understood.
      */
     private Command parseUnknown(String userInput) {
-        String response = joinErrorMessages(WRONG_ARGUMENT_RESPONSE, userInput + " is not a recognised command.");
+        String response = joinErrorMessages(WRONG_ARGUMENT_RESPONSE, userInput + UNRECOGNISED_COMMAND_RESPONSE);
         return new NullCommand(response);
-    }
-
-    /**
-     * Takes in input as a given input <code>String</code>, returning the appropriate command.
-     * @param userInput <code>String</code> entered by the user.
-     * @return Command of varying types depending on user input.
-     */
-    public Command parseInput(String userInput) {
-        String trimmedUserInput = userInput.trim();
-        String[] userInputArr = trimmedUserInput.split(" ");
-        Command outputCommand;
-        String userInputFirstWord = userInputArr[0].trim();
-        outputCommand = switch (userInputFirstWord) {
-            case "bye" -> new ByeCommand();
-            case "help" -> new HelpCommand();
-            case "list" -> new ListCommand();
-            case "find" -> parseFind(userInputArr);
-            case "delete" -> parseDelete(userInputArr);
-            case "mark" -> parseMark(userInputArr);
-            case "unmark" -> parseUnmark(userInputArr);
-            case "todo" -> parseTodo(userInputArr);
-            case "deadline" -> parseDeadline(userInputArr);
-            case "event" -> parseEvent(userInputArr);
-            default -> parseUnknown(userInput);
-        };
-        return outputCommand;
-    }
-
-    private String joinErrorMessages(String... values) {
-        return String.join("\n\n", values);
     }
 }
