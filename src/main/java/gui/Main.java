936d4696759fb320b74656f8238d4cbe9e8fdc56
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-09-23 22:06:03 +0800

    Add bot name to GUI window title

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -30,6 +30,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setRizzler(rizzler);
+            stage.setTitle("Rizzler");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
