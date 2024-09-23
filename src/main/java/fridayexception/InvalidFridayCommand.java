33af6a603d4b7c6a4ef59fa182d6e48948647dba
Mingyang <limingyanglmy10@gmail.com>
2024-09-23 23:22:04 +0800

    Update InvalidFridayException.java

--- src/main/java/fridayexception/InvalidFridayCommand.java
+++ src/main/java/fridayexception/InvalidFridayCommand.java
@@ -13,6 +13,6 @@ public class InvalidFridayCommand extends FridayException {
     public InvalidFridayCommand(String command) {
         super("I'm sorry, but this command is invalid: " + command + "\n"
                 + "     Please enter a valid command: list, todo, deadline, "
-                + "event, mark, unmark, delete, search, find, sort, bye.");
+                + "event, mark, unmark, delete, search, find, sort or bye.");
     }
 }
