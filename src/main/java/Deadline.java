2f7c2396655a8f8e0bde846966bc2a948fa1fdfc
RyanWarwick <spdshadow_w@yahoo.com>
2024-09-23 18:36:24 +0800

    Deadline.java: Add parsing of user input to LocalDate object Event.java: Add parsing of user input to LocalDate object Deadline.java: Move option validation to factory method Event.java: Move option validation to factory method Chappy.java: Move option validation to Class factories

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,11 +1,19 @@
 import org.json.simple.JSONObject;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     private final String completeBy;
 
+    private final LocalDate byLocalDate;
+
     public static enum Option {
         BY("/by");
+
         private final String keyword;
 
         Option(String keyword) {
@@ -18,22 +26,72 @@ public class Deadline extends Task {
 
     }
 
-    public Deadline(String description, String completeBy) {
+    private Deadline(String description, String completeBy) {
         super(description);
         this.completeBy = completeBy;
+        this.byLocalDate = null;
 
+    }
+
+    private Deadline(String description, LocalDate byLocalDate) {
+        super(description);
+        this.byLocalDate = byLocalDate;
+        this.completeBy = null;
+    }
+
+    public static Deadline of(String input) {
+        String[] values;
+        try {
+            values = validateOptions(input);
+
+        } catch (CreateTaskException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        if (values != null) {
+            LocalDate by = parseLocalDate(values[1]);
+            if (by != null) {
+                return new Deadline(values[0], by);
+            } else {
+                return new Deadline(values[0], values[1]);
+            }
+        }
+
+        return null;
+    }
+
+    private static LocalDate parseLocalDate(String input) throws DateTimeParseException {
+        try {
+            LocalDate localDate = LocalDate.parse(input, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            return localDate;
+        } catch (DateTimeParseException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + completeBy + ")";
+        if (this.byLocalDate != null) {
+            return "[D]" + super.toString() + " (by: " + byLocalDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))
+                    + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + completeBy + ")";
+        }
+
     }
 
-    public static void validateOptions(String input) throws CreateTaskException {
+    private static String[] validateOptions(String input) throws CreateTaskException {
+        String[] eventInput = input.trim().split("(?i)" + "deadline");
+        if (eventInput.length < 2) {
+            throw new CreateTaskException("Oh SIR! The description of an Event cannot be empty!");
+        }
+
         boolean isValidated = true;
         String s = "";
         for (Option option : Option.values()) {
-            if (!input.toLowerCase().contains(option.getKeyword())) {
+            if (!eventInput[1].toLowerCase().contains(option.getKeyword())) {
 
                 if (!s.equals("")) {
                     s = s + " and ";
@@ -42,14 +100,21 @@ public class Deadline extends Task {
                 isValidated = false;
             }
         }
+        String[] returnValues = new String[2];
         if (!isValidated) {
             throw new CreateTaskException("Oh SIR! The " + s + " input of a Deadline cannot be missing!");
         } else {
-            String[] s2 = input.split("/by");
+            String[] s2 = eventInput[1].trim().split("/by");
             if (s2.length < 2) {
-                throw new CreateTaskException("Oh SIR! The \"/by\" description of a Deadline cannot be empty!");
+                throw new CreateTaskException(
+                        "Oh SIR! The \"/by\" description of a Deadline cannot be empty! It must be in the format e.g. 23-09-2024 or a String");
+            } else if (s2[0].trim() == "") {
+                throw new CreateTaskException("Oh SIR! The description of an Event cannot be empty!");
             }
+            returnValues[0] = s2[0].trim();
+            returnValues[1] = s2[1].trim();
         }
+        return returnValues;
     }
 
     @Override
@@ -57,12 +122,21 @@ public class Deadline extends Task {
         JSONObject j = new JSONObject();
         j.put("type", "deadline");
         j.put("description", this.description);
-        j.put("completeBy", completeBy);
+        if (this.byLocalDate != null) {
+            j.put("completeBy", byLocalDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+
+        } else {
+            j.put("completeBy", completeBy);
+        }
         return j;
     }
 
     public static Task fromJson(JSONObject jsonObject) {
-        Deadline t = new Deadline(jsonObject.get("description").toString(), jsonObject.get("completeBy").toString());
-        return t;
+        LocalDate by = parseLocalDate(jsonObject.get("completeBy").toString());
+        if (by != null) {
+            return new Deadline(jsonObject.get("description").toString(), by);
+        } else {
+            return new Deadline(jsonObject.get("description").toString(), jsonObject.get("completeBy").toString());
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,21 @@
 import org.json.simple.JSONObject;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 
     private final String from;
     private final String to;
 
+    private final LocalDate fromLocalDate;
+    private final LocalDate toLocalDate;
+
     public static enum Option {
         FROM("/from"), TO("/to");
+
         private final String keyword;
 
         Option(String keyword) {
@@ -19,23 +28,74 @@ public class Event extends Task {
 
     }
 
-    public Event(String description, String from, String to) {
+    private Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
+        this.fromLocalDate = null;
+        this.toLocalDate = null;
+    }
 
+    private Event(String description, LocalDate fromLocalDate, LocalDate toLocalDate) {
+        super(description);
+        this.fromLocalDate = fromLocalDate;
+        this.toLocalDate = toLocalDate;
+        this.from = null;
+        this.to = null;
+    }
+
+    public static Event of(String input) {
+        String[] values;
+        try {
+            values = validateOptions(input);
+
+        } catch (CreateTaskException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        if (values != null) {
+            LocalDate from = parseLocalDate(values[1]);
+            LocalDate to = parseLocalDate(values[2]);
+            if (from != null && to != null) {
+                return new Event(values[0], from, to);
+            } else {
+                return new Event(values[0], values[1], values[2]);
+            }
+        }
+
+        return null;
+    }
+
+    private static LocalDate parseLocalDate(String input) throws DateTimeParseException {
+        try {
+            LocalDate localDate = LocalDate.parse(input, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            return localDate;
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        if (this.fromLocalDate != null && this.toLocalDate != null) {
+            return "[E]" + super.toString() + " (from: " + fromLocalDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + " to: " +
+                    toLocalDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        } else {
+            return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        }
+        
     }
 
-    public static void validateOptions(String input) throws CreateTaskException {
+    private static String[] validateOptions(String input) throws CreateTaskException {
+        String[] eventInput = input.trim().split("(?i)" + "event");
+        if (eventInput.length < 2) {
+            throw new CreateTaskException("Oh SIR! The description of an Event cannot be empty!");
+        }
         boolean isValidated = true;
         String s = "";
         for (Option option : Option.values()) {
-            if (!input.toLowerCase().contains(option.getKeyword())) {
+            if (!eventInput[1].toLowerCase().contains(option.getKeyword())) {
 
                 if (!s.equals("")) {
                     s = s + " and ";
@@ -44,18 +104,28 @@ public class Event extends Task {
                 isValidated = false;
             }
         }
+
+        String[] returnValues = new String[3];
         if (!isValidated) {
             throw new CreateTaskException("Oh SIR! The " + s + " input of an Event cannot be missing!");
         } else {
-            String[] s2 = input.split("/from");
+            String[] s2 = eventInput[1].trim().split("/from");
             if (s2.length < 2) {
-                throw new CreateTaskException("Oh SIR! The \"/from\" description of an Event cannot be empty!");
+                throw new CreateTaskException(
+                        "Oh SIR! The \"/from\" description of an Event cannot be empty! It must be in the format e.g. 23-09-2024 or a String");
+            } else if (s2[0].trim() == "") {
+                throw new CreateTaskException("Oh SIR! The description of an Event cannot be empty!");
             }
-            String[] s3 = s2[1].split("/to");
+            returnValues[0] = s2[0].trim();
+            String[] s3 = s2[1].trim().split("/to");
             if (s3.length < 2) {
-                throw new CreateTaskException("Oh SIR! The \"/to\" description of an Event cannot be empty!");
+                throw new CreateTaskException(
+                        "Oh SIR! The \"/to\" description of an Event cannot be empty! It must be in the format dd-mm-yyyy or a String");
             }
+            returnValues[1] = s3[0].trim();
+            returnValues[2] = s3[1].trim();
         }
+        return returnValues;
     }
 
     @Override
@@ -63,13 +133,24 @@ public class Event extends Task {
         JSONObject j = new JSONObject();
         j.put("type", "event");
         j.put("description", this.description);
-        j.put("from", from);
-        j.put("to", to);
+        if (this.fromLocalDate != null && this.toLocalDate != null) {
+            j.put("from", fromLocalDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+            j.put("to", toLocalDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        } else {
+            j.put("from", from);
+            j.put("to", to);
+        }
+
         return j;
     }
 
     public static Task fromJson(JSONObject jsonObject) {
-        Event t = new Event(jsonObject.get("description").toString(), jsonObject.get("from").toString(), jsonObject.get("to").toString());
-        return t;
+        LocalDate from = parseLocalDate(jsonObject.get("from").toString());
+        LocalDate to = parseLocalDate(jsonObject.get("to").toString());
+        if (from != null && to != null) {
+            return new Event(jsonObject.get("description").toString(), from, to);
+        } else {
+            return new Event(jsonObject.get("description").toString(), jsonObject.get("from").toString(), jsonObject.get("to").toString());
+        }
     }
 }
