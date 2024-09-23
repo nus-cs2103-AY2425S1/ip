45debfadbb7f59bf96a327e94cf60b9aa26473dd
thisisaditya17 <joshi.adi1734@gmail.com>
2024-09-23 23:18:38 +0800

    fix invalid command format message

--- src/main/java/killjoy/main/UserInterface.java
+++ src/main/java/killjoy/main/UserInterface.java
@@ -80,7 +80,7 @@ public class UserInterface {
      * Displays the message when the user enters an invalid command format.
      */
     public static String displayInvalidCommandFormatMessage() {
-        return ("DANGER !! Invalid command format/n"
+        return ("DANGER !! Invalid command format\n"
             + "Refer to the user guide for the correct command format.");
     }
 
