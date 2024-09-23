63ca07119cb59a95364221fba1a4be336888bb7a
sayomaki <sayomayomaki@gmail.com>
2024-09-23 21:51:17 +0800

    Create another test for registering new commands

--- src/test/java/alexer/command/CommandHandlerTest.java
+++ src/test/java/alexer/command/CommandHandlerTest.java
@@ -1,7 +1,9 @@
 package alexer.command;
 
+import alexer.ui.Response;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CommandHandlerTest {
@@ -10,5 +12,23 @@ public class CommandHandlerTest {
         String commandName = "klsdajklsjdkajsd"; // some command that will not exist
         CommandHandler commandHandler = new CommandHandler();
         assertNull(commandHandler.getCommand(commandName));
+    }
+
+    @Test
+    public void createCommand_commandFound() {
+        class TestCommand extends Command {
+            public TestCommand() {
+                super("test");
+            }
+
+            @Override
+            public Response run(String... arguments) {
+                return new Response("test");
+            }
+        }
+
+        CommandHandler handler = new CommandHandler();
+        handler.registerCommand(new TestCommand());
+        assertNotNull(handler.getCommand("test"));
     }
 }
