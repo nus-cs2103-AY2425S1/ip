5658b9ca86f02ac0b667509b89638fc4d299ccfe
sandyk0105 <sandykristianwaluyo3@gmail.com>
2024-09-23 22:29:26 +0800

    Minimizing the use of magic numbers
    Extract magic numbers in Rei.java
    into constants

--- src/main/java/rei/Rei.java
+++ src/main/java/rei/Rei.java
@@ -14,6 +14,17 @@ public class Rei {
     private Storage storage;
     private TaskList tasks;
 
+    private static final int MARK_COMMAND_LENGTH = 4;
+    private static final int UNMARK_COMMAND_LENGTH = 6;
+    private static final int TODO_COMMAND_LENGTH = 4;
+    private static final int DEADLINE_COMMAND_LENGTH = 8;
+    private static final int EVENT_COMMAND_LENGTH = 5;
+    private static final int DELETE_COMMAND_LENGTH = 6;
+    private static final int FIND_COMMAND_LENGTH = 4;
+    private static final int TAG_COMMAND_LENGTH = 3;
+    private static final int UNTAG_COMMAND_LENGTH = 5;
+    private static final int VIEWTAGS_COMMAND_LENGTH = 8;
+
     /**
      * Constructs a REI instance
      * @param filePath where to load the stored data
@@ -39,38 +50,38 @@ public class Rei {
                     output = tasks.toString();
                     break;
                 case MARK:
-                    taskIndex = Integer.parseInt(prompt.substring(4).trim());
+                    taskIndex = Integer.parseInt(prompt.substring(MARK_COMMAND_LENGTH).trim());
                     output = tasks.markTask(taskIndex);
                     break;
                 case UNMARK:
-                    taskIndex = Integer.parseInt(prompt.substring(6).trim());
+                    taskIndex = Integer.parseInt(prompt.substring(UNMARK_COMMAND_LENGTH).trim());
                     output = tasks.unmarkTask(taskIndex);
                     break;
                 case TODO:
-                    output = tasks.addTask(Task.createToDo(prompt.substring(5)));
+                    output = tasks.addTask(Task.createToDo(prompt.substring(TODO_COMMAND_LENGTH).trim()));
                     break;
                 case DEADLINE:
-                    output = tasks.addTask(Task.createDeadline(prompt.substring(9, prompt.indexOf("/by")),
-                            LocalDateTime.parse(prompt.substring(prompt.indexOf("/by") + 4))));
+                    output = tasks.addTask(Task.createDeadline(prompt.substring(DEADLINE_COMMAND_LENGTH + 1, prompt.indexOf("/by")), // +1 to remove whitespace
+                            LocalDateTime.parse(prompt.substring(prompt.indexOf("/by") + 4)))); // +4 to take the /by information
                     break;
                 case EVENT:
-                    output = tasks.addTask(Task.createEvent(prompt.substring(6, prompt.indexOf("/from")),
-                            LocalDateTime.parse(prompt.substring(prompt.indexOf("/from") + 6, prompt.indexOf("/to") - 1)),
-                            LocalDateTime.parse(prompt.substring(prompt.indexOf("/to") + 4))));
+                    output = tasks.addTask(Task.createEvent(prompt.substring(EVENT_COMMAND_LENGTH + 1, prompt.indexOf("/from")), // +1 to remove whitespace
+                            LocalDateTime.parse(prompt.substring(prompt.indexOf("/from") + 6, prompt.indexOf("/to") - 1)), // +6 and -1 to take the /from information
+                            LocalDateTime.parse(prompt.substring(prompt.indexOf("/to") + 4)))); // +4 to take the /to information
                     break;
                 case DELETE:
-                    taskIndex = Integer.parseInt(prompt.substring(6).trim());
+                    taskIndex = Integer.parseInt(prompt.substring(DELETE_COMMAND_LENGTH).trim());
                     output = tasks.deleteTask(taskIndex);
                     break;
                 case FIND:
-                    String keyword = prompt.substring(5).trim();
+                    String keyword = prompt.substring(FIND_COMMAND_LENGTH).trim();
                     output = tasks.findTasks(keyword);
                     break;
                 case ANNYEONG:
                     output = "annyeong";
                     break;
                 case TAG:
-                    details = prompt.substring(3).trim().split(" ");
+                    details = prompt.substring(TAG_COMMAND_LENGTH).trim().split(" ");
                     taskIndex = Integer.parseInt(details[0]);
                     List<String> tags = new ArrayList<>();
                     for (int i = 1; i < details.length; i++) {
@@ -79,12 +90,12 @@ public class Rei {
                     output = tasks.addTags(taskIndex, tags);
                     break;
                 case UNTAG:
-                    details = prompt.substring(5).trim().split(" ");
+                    details = prompt.substring(UNTAG_COMMAND_LENGTH).trim().split(" ");
                     taskIndex = Integer.parseInt(details[0]);
                     output = tasks.deleteTag(taskIndex, details[1]);
                     break;
                 case VIEWTAGS:
-                    taskIndex = Integer.parseInt(prompt.substring(8).trim());
+                    taskIndex = Integer.parseInt(prompt.substring(VIEWTAGS_COMMAND_LENGTH).trim());
                     output = tasks.viewTags(taskIndex);
                     break;
                 default:
