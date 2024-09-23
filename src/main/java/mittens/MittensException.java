197289d5f2abd3bea9adf7bb51c0a2e51bc78d8c
izruff <rafaelky.dev@gmail.com>
2024-09-23 23:07:07 +0800

    Remove unused method

--- src/main/java/mittens/MittensException.java
+++ src/main/java/mittens/MittensException.java
@@ -31,21 +31,4 @@ public class MittensException extends Exception {
     public String getHelpMessage() {
         return this.helpMessage;
     }
-
-    public void echo() {
-        String message = """
-                                
-                 /\\_/\\     %s
-                 >x.x<    ( %s )
-                  / \\      %s
-                 (___)_/
-                                
-                Error: %s
-                %s
-                """.formatted("_".repeat(this.getMittensMessage().length() + 2),
-                this.getMittensMessage(), "-".repeat(this.getMittensMessage().length() + 2),
-                this.getMessage(), this.helpMessage);
-
-        System.out.println(message);
-    }
 }
