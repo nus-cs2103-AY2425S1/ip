1aa21d0b27c8f5ea9699134707d69efb71845211
Judy1x4 <e1129873@u.nus.edu>
2024-09-23 18:00:40 +0800

    Debug Junit

--- src/test/java/jude/ParserTest.java
+++ src/test/java/jude/ParserTest.java
@@ -1,16 +1,15 @@
 package jude;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Test;
+
 import jude.command.AddCommand;
+import jude.command.DeleteCommand;
 import jude.command.ListCommand;
 import jude.command.MarkCommand;
 import jude.command.UnmarkCommand;
-import jude.command.DeleteCommand;
-
 import jude.task.Deadline;
 import jude.task.Event;
 import jude.task.Todo;
@@ -21,7 +20,7 @@ public class ParserTest {
     @Test
     public void parse_list_success() {
         try {
-            assertEquals(new ListCommand().toString(), new Parser().parse("list").toString());
+            assertEquals(new ListCommand().getType(), new Parser().parse("list").getType());
         } catch (JudeException je) {
             fail();
         }
@@ -31,7 +30,7 @@ public class ParserTest {
     public void parse_listWithLeadingSpace_fail() {
 
         try {
-            assertEquals(new ListCommand().toString(), new Parser().parse(" list").toString());
+            assertEquals(new ListCommand().getType(), new Parser().parse(" list").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("No valid command was provided.", je.getMessage());
@@ -41,7 +40,7 @@ public class ParserTest {
     @Test
     public void parse_listWithTrailingSpace_fail() {
         try {
-            assertEquals(new ListCommand().toString(), new Parser().parse("list ").toString());
+            assertEquals(new ListCommand().getType(), new Parser().parse("list ").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("Poyo, the description of a list must be empty.", je.getMessage());
@@ -62,7 +61,7 @@ public class ParserTest {
     @Test
     public void parse_mark_success() {
         try {
-            assertEquals(new MarkCommand(1).toString(), new Parser().parse("mark 1").toString());
+            assertEquals(new MarkCommand(1).getType(), new Parser().parse("mark 1").getType());
         } catch (JudeException je) {
             fail();
         }
@@ -71,7 +70,7 @@ public class ParserTest {
     @Test
     public void parse_markWithoutNumber_fail() {
         try {
-            assertEquals(new MarkCommand(1).toString(), new Parser().parse("mark").toString());
+            assertEquals(new MarkCommand(1).getType(), new Parser().parse("mark").getType());
             fail();
         } catch (JudeException je) {
             assertEquals(
@@ -83,7 +82,7 @@ public class ParserTest {
     @Test
     public void parse_ununmarkWithLetter_fail() {
         try {
-            assertEquals(new UnmarkCommand(1).toString(), new Parser().parse("unmark AAA".toString()));
+            assertEquals(new UnmarkCommand(1).getType(), new Parser().parse("unmark AAA").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("Invalid number format: expected an integer.", je.getMessage());
@@ -94,7 +93,7 @@ public class ParserTest {
     @Test
     public void parse_unmark_success() {
         try {
-            assertEquals(new UnmarkCommand(1).toString(), new Parser().parse("unmark 1").toString());
+            assertEquals(new UnmarkCommand(1).getType(), new Parser().parse("unmark 1").getType());
         } catch (JudeException je) {
             fail();
         }
@@ -103,7 +102,7 @@ public class ParserTest {
     @Test
     public void parse_unmarkWithoutNumber_fail() {
         try {
-            assertEquals(new UnmarkCommand(1).toString(), new Parser().parse("unmark").toString());
+            assertEquals(new UnmarkCommand(1).getType(), new Parser().parse("unmark").getType());
             fail();
         } catch (JudeException je) {
             assertEquals(
@@ -115,7 +114,7 @@ public class ParserTest {
     @Test
     public void parse_unmarkWithLetter_fail() {
         try {
-            assertEquals(new UnmarkCommand(1).toString(), new Parser().parse("unmark AAA".toString()));
+            assertEquals(new UnmarkCommand(1).getType(), new Parser().parse("unmark AAA").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("Invalid number format: expected an integer.", je.getMessage());
@@ -126,7 +125,7 @@ public class ParserTest {
     @Test
     public void parse_delete_success() {
         try {
-            assertEquals(new DeleteCommand(1).toString(), new Parser().parse("delete 1").toString());
+            assertEquals(new DeleteCommand(1).getType(), new Parser().parse("delete 1").getType());
         } catch (JudeException je) {
             fail();
         }
@@ -135,7 +134,7 @@ public class ParserTest {
     @Test
     public void parse_deleteWithoutNumber_fail() {
         try {
-            assertEquals(new DeleteCommand(1).toString(), new Parser().parse("delete").toString());
+            assertEquals(new DeleteCommand(1).getType(), new Parser().parse("delete").getType());
             fail();
         } catch (JudeException je) {
             assertEquals(
@@ -147,7 +146,7 @@ public class ParserTest {
     @Test
     public void parse_deleteWithLetter_fail() {
         try {
-            assertEquals(new DeleteCommand(1).toString(), new Parser().parse("delete AAA".toString()));
+            assertEquals(new DeleteCommand(1).getType(), new Parser().parse("delete AAA").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("Invalid number format: expected an integer.", je.getMessage());
@@ -160,8 +159,8 @@ public class ParserTest {
     public void parse_todo_success() {
         try {
             assertEquals(
-                    new AddCommand(new Todo("AAA")).toString(),
-                    new Parser().parse("to-do AAA").toString());
+                    new AddCommand(new Todo("AAA")).getType(),
+                    new Parser().parse("to-do AAA").getType());
         } catch (JudeException je) {
             fail();
         }
@@ -171,7 +170,7 @@ public class ParserTest {
     public void parse_todoWithoutDescription_fail() {
         try {
             assertEquals(
-                    new AddCommand(new Todo("AAA")).toString(), new Parser().parse("to-do".toString()));
+                    new AddCommand(new Todo("AAA")).getType(), new Parser().parse("to-do").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("Poyo, the format of the command to-do was not valid."
@@ -183,7 +182,7 @@ public class ParserTest {
     public void parse_todoWithoutDash_success() {
         try {
             assertEquals(
-                    new AddCommand(new Todo("AAA")).toString(), new Parser().parse("todo AAA".toString()));
+                    new AddCommand(new Todo("AAA")).getType(), new Parser().parse("todo AAA").getType());
             fail();
         } catch (JudeException je) {
             assertEquals("No valid command was provided.", je.getMessage());
@@ -195,8 +194,8 @@ public class ParserTest {
     public void parse_deadline_success() {
         try {
             assertEquals(
-                    new AddCommand(new Deadline("AAA", "1/1/2000 0400")).toString(),
-                    new Parser().parse("deadline AAA /by 1/1/2000 0400").toString()
+                    new AddCommand(new Deadline("AAA", "1/1/2000 0400")).getType(),
+                    new Parser().parse("deadline AAA /by 1/1/2000 0400").getType()
             );
         } catch (JudeException je) {
             fail();
@@ -207,8 +206,8 @@ public class ParserTest {
     public void parse_deadlineNoTime_fail() {
         try {
             assertEquals(
-                    new AddCommand(new Deadline("AAA", "1/1/2000 0000")).toString(),
-                    new Parser().parse("deadline AAA /by 1/1/2000").toString()
+                    new AddCommand(new Deadline("AAA", "1/1/2000 0000")).getType(),
+                    new Parser().parse("deadline AAA /by 1/1/2000").getType()
             );
             fail();
         } catch (JudeException je) {
@@ -221,8 +220,8 @@ public class ParserTest {
     public void parse_deadlineWrongBy_fail() {
         try {
             assertEquals(
-                    new AddCommand(new Deadline("AAA", "1/1/2000 0000")).toString(),
-                    new Parser().parse("deadline AAA by 1/1/2000 0000").toString()
+                    new AddCommand(new Deadline("AAA", "1/1/2000 0000")).getType(),
+                    new Parser().parse("deadline AAA by 1/1/2000 0000").getType()
             );
             fail();
         } catch (JudeException je) {
@@ -237,8 +236,8 @@ public class ParserTest {
     public void parse_event_success() {
         try {
             assertEquals(
-                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).toString(),
-                    new Parser().parse("event AAA /from 1/1/2000 0000 /to 1/1/2000 1200").toString()
+                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).getType(),
+                    new Parser().parse("event AAA /from 1/1/2000 0000 /to 1/1/2000 1200").getType()
             );
         } catch (JudeException je) {
             System.out.println(je.getMessage());
@@ -250,8 +249,8 @@ public class ParserTest {
     public void parse_eventNoTime_fail() {
         try {
             assertEquals(
-                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).toString(),
-                    new Parser().parse("event AAA /from 1/1/2000 /to 1/1/2000").toString()
+                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).getType(),
+                    new Parser().parse("event AAA /from 1/1/2000 /to 1/1/2000").getType()
             );
             fail();
         } catch (JudeException je) {
@@ -264,8 +263,8 @@ public class ParserTest {
     public void parse_eventWrongTo_fail() {
         try {
             assertEquals(
-                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).toString(),
-                    new Parser().parse("event AAA /from 1/1/2000 0000 to 1/1/2000 12000").toString()
+                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).getType(),
+                    new Parser().parse("event AAA /from 1/1/2000 0000 to 1/1/2000 12000").getType()
             );
             fail();
         } catch (JudeException je) {
@@ -279,8 +278,8 @@ public class ParserTest {
     public void parse_eventWrongFrom_fail() {
         try {
             assertEquals(
-                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).toString(),
-                    new Parser().parse("event AAA from 1/1/2000 0000 /to 1/1/2000 12000").toString()
+                    new AddCommand(new Event("AAA", "1/1/2000 0000", "1/1/2000 1200")).getType(),
+                    new Parser().parse("event AAA from 1/1/2000 0000 /to 1/1/2000 12000").getType()
             );
             fail();
         } catch (JudeException je) {
