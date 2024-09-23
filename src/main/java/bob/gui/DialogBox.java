e3ef2548090f44fe6c10f4403abde1ebbfcd2b2d
Nathan Lye <nathanlye05@gmail.com>
2024-09-23 10:52:23 +0800

    add simple styling to the gui

--- src/main/java/bob/gui/DialogBox.java
+++ src/main/java/bob/gui/DialogBox.java
@@ -1,5 +1,6 @@
 package bob.gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,6 +17,13 @@ public class DialogBox extends HBox {
     public DialogBox(String s, Image i) {
         text = new Label(s);
         displayPicture = new ImageView(i);
+
+        //Styling the dialog box
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
         this.getChildren().addAll(text, displayPicture);
     }
 }
