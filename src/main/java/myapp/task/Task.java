33a686c058d809abec34f1e7c035aa7a5fe76845
adriantan101 <atjy102@gmail.com>
2024-09-25 17:23:57 +0800

    Add fourth change to Task.java to satisfy Git convention

--- src/main/java/myapp/task/Task.java
+++ src/main/java/myapp/task/Task.java
@@ -16,7 +16,7 @@ public class Task {
      */
     public Task(String description) {
         this.description = description;
-        isDone = false;
+        this.isDone = false;
         assert description != null : "Task description cannot be empty";
     }
 
