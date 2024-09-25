0f97f9ee4769b1d93dd683c6a0192b01b10144ba
Denon Chong <e0958801@u.nus.edu>
2024-09-25 15:38:41 +0800

    Add Javadocs to Task.java

--- src/main/java/Mentos/task/Task.java
+++ src/main/java/Mentos/task/Task.java
@@ -1,37 +1,68 @@
 package Mentos.task;
 
-
 public class Task {
     protected boolean isDone;
     protected String description;
 
-
-    public Task(String description){
+    /**
+     * Constructs a Task object with the specified description.
+     * By default, the task is not marked as done.
+     *
+     * @param description The description of the task.
+     */
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Updates the description of the task.
+     *
+     * @param description The new description of the task.
+     */
     public void updateDescription(String description) {
         this.description = description;
     }
 
-    public String getStatusIcon(){
+    /**
+     * Returns the status icon for the task, indicating if it is done.
+     *
+     * @return "X" if the task is done, otherwise a blank space " ".
+     */
+    public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
-    public void markAsDone(){
+
+    /**
+     * Marks the task as done.
+     */
+    public void markAsDone() {
         this.isDone = true;
     }
-    public String getDescription(){
+
+    /**
+     * Returns the description of the task.
+     *
+     * @return The description of the task.
+     */
+    public String getDescription() {
         return this.description;
     }
-    public void markAsNotDone(){
+
+    /**
+     * Marks the task as not done.
+     */
+    public void markAsNotDone() {
         this.isDone = false;
     }
 
-
-
+    /**
+     * Returns the string representation of the task, including its status and description.
+     *
+     * @return A formatted string representing the task.
+     */
     @Override
-    public String toString(){
-        return String.format("[%s] %s",this.getStatusIcon(),this.description);
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
     }
 }
