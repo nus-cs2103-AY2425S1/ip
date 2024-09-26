29ac6f71a437ee3fe8000f9afbe874709d01147e
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-09-26 16:35:25 +0800

    Amend styling error for Launcher class
    Added a Javadoc header to describe about launcher class

--- src/main/java/velma/Launcher.java
+++ src/main/java/velma/Launcher.java
@@ -2,6 +2,9 @@ package velma;
 
 import javafx.application.Application;
 
+/**
+ * This class is the starting point used to launch the application.
+ */
 public class Launcher {
 
     public static void main(String[] args) {
