2cfc9d19772d5a208c93a4830209c630ef1184e2
Shruthiiii03 <muralishruthi222@gmail.com>
2024-09-29 13:15:13 +0800

    Add spacing for imports in MainWindow.java

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,11 +1,14 @@
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
 import prince.Prince;
+
 import ui.Ui;
 
 /**
