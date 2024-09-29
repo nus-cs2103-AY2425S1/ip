8d52dcf45941d9f88ff013d19e5cb733f0de09a3
Shruthiiii03 <muralishruthi222@gmail.com>
2024-09-29 12:57:18 +0800

    Add JavaDoc comments in ToDoCommand.java

--- src/main/java/Commands/ToDoCommand.java
+++ src/main/java/Commands/ToDoCommand.java
@@ -4,12 +4,24 @@ import task.TaskList;
 import task.ToDoTask;
 import ui.Ui;
 
+/**
+ * Represents a command to create a ToDo task.
+ * This command extends from the GeneralTaskCommand and adds a todo task
+ * to the main task list.
+ * Once done so, it returns a message from the Ui class indicating that the
+ * task has been added.
+ */
 public class ToDoCommand extends GeneralTaskCommand {
 
     public ToDoCommand(String c) {
         super(c);
     }
 
+    /**
+     * Executes the action of creating a new To Do task and adds it to the task list
+     * Returns a message indicating that the task has been added successfully
+     * @return String message confirming that the To Do task has been added
+     */
     @Override
     public String commandAction() {
         ToDoTask tsk = new ToDoTask(this.cmd);
