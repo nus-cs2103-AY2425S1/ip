a5aa341a0c24a16a79da6013ac729c089c9e52ba
jeremychiaaaaa <chiajeremy1@gmail.com>
2024-10-02 18:05:15 +0800

    Update header of Main File

--- src/main/java/nextgpt/Main.java
+++ src/main/java/nextgpt/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
- * A GUI for NextGPT using FXML.
+ * Returns A GUI for NextGPT using FXML.
  */
 public class Main extends Application {
     private NextGPT nextGPT = new NextGPT("./data/tasks.txt");
