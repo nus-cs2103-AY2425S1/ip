c5c90a7b304d35466ef65e6ee89ef8608c4cb89b
Quasant <benjaminthechin@gmail.com>
2024-10-03 18:35:15 +0800

    Remove unused imports

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -1,7 +1,5 @@
 package gui;
 
-import javafx.animation.PauseTransition;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -9,7 +7,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.util.Duration;
 import screwllum.Screwllum;
 
 /**
