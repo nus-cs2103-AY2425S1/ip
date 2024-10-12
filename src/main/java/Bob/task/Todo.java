a62c6f5cb9fca5d68b2e437fb20b0f53b31aba00
Jing Wen <shotnothing@protonmail.com>
2024-10-12 12:27:31 +0800

    Update Todo.java

--- src/main/java/Bob/task/Todo.java
+++ src/main/java/Bob/task/Todo.java
@@ -1,11 +1,18 @@
 package bob.task;
 
 public class Todo extends Task {
-
+    /**
+     * Constructor for Todo.
+     *
+     * @param description
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns string representation of Todo.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
