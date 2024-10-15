93617cfa21de3b8eaa1fded1b002bbeb9e23ec40
Wu Zengfu <67005197+wuzengfu@users.noreply.github.com>
2024-10-15 21:28:11 +0800

    Fix header comments

--- src/main/java/echobot/task/Task.java
+++ src/main/java/echobot/task/Task.java
@@ -25,14 +25,14 @@ public class Task implements Saveable {
     }
 
     /**
-     * Mark this task as done.
+     * Marks this task as done.
      */
     public void markDone() {
         this.isDone = true;
     }
 
     /**
-     * Mark this task as undone.
+     * Marks this task as undone.
      */
     public void markUndone() {
         this.isDone = false;
