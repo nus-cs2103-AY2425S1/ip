2783d786198fbb442e047af39d8e2021a70e55c0
JYL27 <junyulee27@gmail.com>
2024-12-23 22:42:27 +0800

    Add test cases for ExitCommand

--- src/test/java/colress/command/ExitCommandTest.java
+++ src/test/java/colress/command/ExitCommandTest.java
@@ -1,11 +1,47 @@
 package colress.command;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import colress.Colress;
+import colress.UiAdvanced;
+import colress.UiBeginner;
+import colress.parser.Parser;
+import colress.tasklist.ColressTaskList;
+import colress.tasklist.TaskList;
+
 public class ExitCommandTest {
+    @Test
+    public void startTest() {
+        ExitCommand exitCommand = new ExitCommand();
+        UiBeginner uiBeginner = new UiBeginner(new Colress(""));
+        TaskList taskList = new ColressTaskList();
+
+        String expectedResult = ExitCommand.MESSAGE_SUCCESSFUL_EXECUTION;
+        assertEquals(expectedResult, exitCommand.start(uiBeginner, taskList));
+    }
+
+    @Test
+    public void executeTest() {
+        ExitCommand exitCommand = new ExitCommand();
+        UiAdvanced uiAdvanced = new UiAdvanced(new Colress(""));
+        TaskList taskList = new ColressTaskList();
+
+        String expectedResult = ExitCommand.MESSAGE_SUCCESSFUL_EXECUTION;
+        assertEquals(expectedResult, exitCommand.execute(uiAdvanced, taskList));
+    }
+
+    @Test
+    public void toStringTest() {
+        ExitCommand exitCommand = new ExitCommand();
+
+        String expectedString = Parser.COMMAND_EXIT;
+        assertEquals(expectedString, exitCommand.toString());
+    }
+
     @Test
     public void equalsTest() {
         ExitCommand exitCommand = new ExitCommand();
