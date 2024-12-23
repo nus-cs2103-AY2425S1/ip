8ded1da3ef249663ca1aa69df6eae39ac0a19ca3
JYL27 <junyulee27@gmail.com>
2024-12-23 22:10:13 +0800

    Remove unused import from ColressTest.java

--- src/test/java/colress/ColressTest.java
+++ src/test/java/colress/ColressTest.java
@@ -1,6 +1,5 @@
 package colress;
 
-import static colress.testutil.TestUtil.VALID_COMMAND_TOGGLE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
