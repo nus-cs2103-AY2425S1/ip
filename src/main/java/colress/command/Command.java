ab3f0a4e8686b1b3ae46950cc4a4645d6f0995c0
JYL27 <junyulee27@gmail.com>
2024-12-24 19:17:34 +0800

    Remove unnecessary semicolons

--- src/main/java/colress/command/Command.java
+++ src/main/java/colress/command/Command.java
@@ -41,15 +41,19 @@ public abstract class Command {
     }
 
     public void initialise(LocalDate input) {
-    };
+    }
+
     public void initialise(LocalTime input) {
-    };
+    }
+
     public void initialise(String input) {
-    };
+    }
+
     public void initialise(TaskType input) {
-    };
+    }
+
     public void initialise(int... input) {
-    };
+    }
 
     public abstract String start(UiBeginner ui, TaskList taskList);
     public abstract String execute(UiBeginner ui, TaskList taskList);
