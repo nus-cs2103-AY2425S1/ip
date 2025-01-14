5bccce3db3b1f50031f9b74cdae9e7b261eb1c2f
itsapaulblem <paulcheng46@gmail.com>
2025-01-14 11:11:43 +0800

    Rename chatbot to Alden and implement Level-0: Greet and Exit

---
+++ src/main/java/Alden.java
@@ -0,0 +1,13 @@
+public class Alden {
+    public static void main(String[] args) {
+        // Print the greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Alden");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Print the exit message
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
