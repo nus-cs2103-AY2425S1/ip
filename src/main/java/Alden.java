c15c3c44f8c790f83ca562dd8fe45f77b2fd1575
itsapaulblem <paulcheng46@gmail.com>
2025-01-14 11:21:13 +0800

    Implement Level-1: Echo functionality

--- src/main/java/Alden.java
+++ src/main/java/Alden.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
 public class Alden {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
         // Print the greeting message
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Alden");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Print the exit message
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (isRunning) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                isRunning = false;
+                // Print the exit message
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
